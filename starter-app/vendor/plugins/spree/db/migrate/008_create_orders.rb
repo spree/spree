b8b43fe71c10eea7ab8df04c193327969e9aac8e
Sean Schofield <schof@apache.org>
2008-03-27 15:29:05 -0400

    fixed field length issue with ip addresses (Bug #19)

--- starter-app/vendor/plugins/spree/db/migrate/008_create_orders.rb
+++ starter-app/vendor/plugins/spree/db/migrate/008_create_orders.rb
@@ -1,24 +1,23 @@
 class CreateOrders < ActiveRecord::Migration
   def self.up
-	  create_table "orders", :force => true do |t|
-      t.column "user_id",              :integer
-      t.column "number",               :string, :limit => 15
-      t.column "status",               :integer
-      t.column "ship_method",          :integer
-      t.column "ship_amount",          :decimal,                :precision => 8, :scale => 2, :default => 0.0, :null => false
-      t.column "tax_amount",           :decimal,                :precision => 8, :scale => 2, :default => 0.0, :null => false
-      t.column "item_total",           :decimal,                :precision => 8, :scale => 2, :default => 0.0, :null => false
-      t.column "total",                :decimal,                :precision => 8, :scale => 2, :default => 0.0, :null => false
-      t.column "ip_address",           :string, :limit => 11
-      t.column "special_instructions", :text
-      t.column "ship_address_id",      :integer
-      t.column "bill_address_id",      :integer
-      t.column "created_at",           :datetime
-      t.column "updated_at",           :datetime
+	  create_table :orders, :force => true do |t|
+      t.integer :user_id
+      t.string :number, :limit => 15
+      t.integer :status
+      t.integer :ship_method
+      t.decimal :ship_amount, :precision => 8, :scale => 2, :default => 0.0, :null => false
+      t.decimal :tax_amount, :precision => 8, :scale => 2, :default => 0.0, :null => false
+      t.decimal :item_total, :precision => 8, :scale => 2, :default => 0.0, :null => false
+      t.decimal :total, :precision => 8, :scale => 2, :default => 0.0, :null => false
+      t.string :ip_address
+      t.text :special_instructions
+      t.integer :ship_address_id
+      t.integer :bill_address_id
+      t.timestamps
     end
   end
 
   def self.down
-    drop_table "orders"
+    drop_table :orders
   end
 end
