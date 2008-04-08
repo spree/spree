caeb3cd4426595471665abdb771cbedba04e22da
Sean Schofield <schof@apache.org>
2008-04-08 16:03:01 -0400

    additional refactoring related to engines move

--- refactor/starter-app/vendor/plugins/spree/routes.rb
+++
@@ -1,3 +0,0 @@
-login    '/login',        :controller => 'account', :action => 'login'
-logout   '/logout',       :controller => 'account', :action => 'logout'
-admin '/admin', :controller => 'admin/overview', :action => 'index'
