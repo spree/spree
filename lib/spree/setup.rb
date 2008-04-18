f55c5128546d60d8758d8a0b00dd8a234f72718e
Sean Schofield <schof@apache.org>
2008-04-17 23:32:01 -0400

    bootstrap is now creating sample data

--- lib/spree/setup.rb
+++ lib/spree/setup.rb
@@ -2,8 +2,6 @@
 # Substantial portions of this code were adapted from the Radiant CMS project (http://radiantcms.org) #
 #######################################################################################################
 require "highline"
-require "forwardable"
-require 'active_record/fixtures'
 require 'custom_fixtures'
 require 'find'
 
@@ -62,8 +60,8 @@ module Spree
     def load_sample_data
       # load initial database fixtures (in db/sample/*.yml) into the current environment's database
       ActiveRecord::Base.establish_connection(RAILS_ENV.to_sym)
-      Dir.glob(File.join(SPREE_ROOT, 'db', 'sample', '*.{yml,csv}')).each do |fixture_file|
-        Fixtures.create_fixtures('db/sample', File.basename(fixture_file, '.*'))
+      Dir.glob(File.join(SPREE_ROOT, "db", 'sample', '*.{yml,csv}')).each do |fixture_file|
+        Fixtures.create_fixtures("#{SPREE_ROOT}/db/sample", File.basename(fixture_file, '.*'))
       end
 
       # make product images available to the app
@@ -138,105 +136,11 @@ module Spree
           q.echo = false
           q.whitespace = :strip
         end
-        sample == "y" or sample == "yes" or sample == "true"
+        sample == "" or sample == "y" or sample == "yes" or sample == "true"
       end          
-=begin      
-      def find_template_in_path(filename)
-        [
-          filename,
-          "#{RADIANT_ROOT}/#{filename}",
-          "#{RADIANT_ROOT}/db/templates/#{filename}",
-          "#{RAILS_ROOT}/#{filename}",
-          "#{RAILS_ROOT}/db/templates/#{filename}",
-          "#{Dir.pwd}/#{filename}",
-          "#{Dir.pwd}/db/templates/#{filename}",
-        ].find { |name| File.file?(name) }
-      end
-      
-      def find_and_load_templates(glob)
-        templates = Dir[glob]
-        templates.map! { |template| load_template_file(template) }
-        templates.sort_by { |template| template['name'] }
-      end
-      
-      def load_template_file(filename)
-        YAML.load_file(filename)
-      end
-      
-      def create_records(template)
-        records = template['records']
-        if records
-          puts
-          records.keys.each do |key|
-            feedback "Creating #{key.to_s.underscore.humanize}" do
-              model = model(key)
-              model.reset_column_information
-              record_pairs = order_by_id(records[key])
-              step do
-                record_pairs.each do |id, record|
-                  model.new(record).save
-                end
-              end
-            end
-          end
-        end
-      end
-      
-      def model(model_name)
-        model_name.to_s.singularize.constantize
-      end
-      
-      def order_by_id(records)
-        records.map { |name, record| [record['id'], record] }.sort { |a, b| a[0] <=> b[0] }
-      end
-      
-      extend Forwardable
-      def_delegators :terminal, :agree, :ask, :choose, :say
-  
-      def terminal
-        @terminal ||= HighLine.new
-      end
-  
-      def output
-        terminal.instance_variable_get("@output")
-      end
-  
-      def wrap(string)
-        string = terminal.send(:wrap, string) unless terminal.wrap_at.nil?
-        string
-      end
-  
-      def print(string)
-        output.print(wrap(string))
-        output.flush
-      end
-  
-      def puts(string = "\n")
-        say string
-      end
-=end  
+
       def announce(string)
         puts "\n#{string}"
       end
-=begin            
-      def feedback(process, &block)
-        print "#{process}..."
-        if yield
-          puts "OK"
-          true
-        else
-          puts "FAILED"
-          false
-        end
-      rescue Exception => e
-        puts "FAILED"
-        raise e
-      end
-      
-      def step
-        yield if block_given?
-        print '.'
-      end
-=end   
   end
 end
