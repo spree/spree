261bd7a6c6ac61859daa553dc437e586ce364453
Sean Schofield <schof@apache.org>
2008-04-23 08:51:48 -0400

    Changed description field to text (to allow for longer descriptions in psql)

--- db/migrate/009_create_products.rb
+++ db/migrate/009_create_products.rb
@@ -2,7 +2,7 @@ class CreateProducts < ActiveRecord::Migration
   def self.up
 	  create_table :products do |t|
 	    t.string :name, :default => "", :null => false
-	    t.string :description
+	    t.text :description
 	    t.decimal :price, :precision => 8, :scale => 2, :null => false
 	    t.integer :category_id
       t.integer :viewable_id
