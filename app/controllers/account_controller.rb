31df9bdc646e3eaa975c39967c80d471ad1868f1
Paul Saieg <Paul@Yettibox.local>
2008-05-02 06:57:03 +0200

    cleaned out several unneeded includes.

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -1,7 +1,4 @@
 class AccountController < Spree::BaseController
-  # Be sure to include AuthenticationSystem in Application Controller instead
-  include AuthenticatedSystem
-  # If you want "remember me" functionality, add this before_filter to Application Controller
   before_filter :login_from_cookie
   
   layout 'admin'
