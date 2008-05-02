82ae3fd4821e1abf921e2b4c51642ca55a2563fa
Paul Saieg <Paul@Yettibox.local>
2008-05-02 08:36:30 +0200

    removed a comment - not really worth the commit, but what the hey.

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,7 +1,6 @@
-# this clas was inspired (heavily) from the mephisto admin architecture
+# this class was inspired (heavily) from the mephisto admin architecture
 
 class Admin::BaseController < Spree::BaseController
-  #before_filter :login_required
   helper :search
   layout 'admin'
 end
