bcfa21af8efd080e0ab705ffd8434242a7a8d709
mjwall <mike@mjwall.com>
2008-05-09 21:03:51 -0400

    Add require field tests and group variant method tests

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -5,44 +5,61 @@ describe Product do
   before(:each) do
     @product = Product.new
   end
-  
-  it "should report no variants when variants is empty" do
-    @product.variants?.should be_false
+
+  ['name', 'master price', 'description'].each do |field|
+    it "should require #{field}" do
+      @product.valid?.should be_false
+      @product.errors.full_messages.should include("#{field.capitalize} can't be blank")
+    end
   end
 
-  it "should report no variants when none of the variants have option values" do
-    variant_proxy = mock_model(Variant)
-    variant_proxy.should_receive(:option_values).and_return([])
-    @product.variants << variant_proxy
-    @product.variants?.should be_false
-  end
-  
-  it "should report variants when all of the variants have option values" do
-    ov_proxy = mock_model(OptionValue)
-    variant_proxy = mock_model(Variant)
-    variant_proxy.should_receive(:option_values).and_return([ov_proxy])
-    @product.variants << variant_proxy
-    @product.variants?.should be_true
-  end
-  
-  it "variant should return the emtpy variant if the product contains only empty variant" do
-    variant = mock_model(Variant)
-    variant.stub!(:option_values).and_return([])
-    product = Product.new
-    product.stub!(:variants).and_return([variant])
-    product.variant.should == variant
+  describe "#variants?" do
+    it "should be false when variants is empty" do
+      @product.variants.should be_empty
+      @product.variants?.should be_false
+    end
+
+    it "should be false when none of the variants have option values" do
+      variant_proxy = mock_model(Variant)
+      variant_proxy.should_receive(:option_values).and_return([])
+      @product.variants << variant_proxy
+      @product.variants?.should be_false
+    end
+
+    it "should be true when at least one variant has option values" do
+      ov_proxy = mock_model(OptionValue)
+      variant_proxy = mock_model(Variant)
+      variant_proxy.should_receive(:option_values).and_return([ov_proxy])
+      @product.variants << variant_proxy
+      @product.variants?.should be_true
+    end
   end
 
-  it "variant should return nil if the product contains non-empty variants" do
-    variant1 = mock_model(Variant)
-    variant1.stub!(:option_values).and_return([])
-    ov = mock_model(OptionValue)
-    variant2 = mock_model(Variant)
-    variant2.stub!(:option_values).and_return([ov])
-    product = Product.new
-    product.stub!(:variants).and_return([variant1, variant2])
-    product.variant.should be_nil
+  describe "#variant" do
+    it "should return the emtpy variant if there are only empty variant" do
+      variant = mock_model(Variant)
+      variant.stub!(:option_values).and_return([])
+      product = Product.new
+      product.stub!(:variants).and_return([variant])
+      product.variant.should == variant
+    end
+
+    it "should return nil if there are any non-empty variants" do
+      variant1 = mock_model(Variant)
+      variant1.stub!(:option_values).and_return([])
+      ov = mock_model(OptionValue)
+      variant2 = mock_model(Variant)
+      variant2.stub!(:option_values).and_return([ov])
+      product = Product.new
+      product.stub!(:variants).and_return([variant1, variant2])
+      product.variant.should be_nil
+    end
   end
+  
+
+  
+  
+
 
   # TODO - Add the rest of the unit test stuff from product_test (once we're sure about tax treatment handling)
 end
