97b4b30564f7c5098837cfa19ccbcbcc6b9d2f8c
Jorge Calás Lozano <calas@qvitta.net>
2008-06-09 19:25:50 +0200

    Renaming extension's README template to README.markdown

--- lib/generators/extension/extension_generator.rb
+++ lib/generators/extension/extension_generator.rb
@@ -1,14 +1,14 @@
 class ExtensionGenerator < Rails::Generator::NamedBase
   default_options :with_test_unit => false
-  
+
   attr_reader :extension_path, :extension_file_name
-  
+
   def initialize(runtime_args, runtime_options = {})
     super
     @extension_file_name = "#{file_name}_extension"
     @extension_path = "vendor/extensions/#{file_name}"
   end
-  
+
   def manifest
     record do |m|
       m.directory "#{extension_path}/app/controllers"
@@ -17,7 +17,7 @@ class ExtensionGenerator < Rails::Generator::NamedBase
       m.directory "#{extension_path}/app/views"
       m.directory "#{extension_path}/db/migrate"
       m.directory "#{extension_path}/lib/tasks"
-      
+
       m.template 'README.markdown',              "#{extension_path}/README.markdown"
       m.template 'extension.rb',        "#{extension_path}/#{extension_file_name}.rb"
       m.template 'tasks.rake',          "#{extension_path}/lib/tasks/#{extension_file_name}_tasks.rake"
@@ -32,7 +32,7 @@ class ExtensionGenerator < Rails::Generator::NamedBase
         m.template 'functional_test.rb',  "#{extension_path}/test/functional/#{extension_file_name}_test.rb"
       else
         m.directory "#{extension_path}/spec/controllers"
-        m.directory "#{extension_path}/spec/models"        
+        m.directory "#{extension_path}/spec/models"
         m.directory "#{extension_path}/spec/views"
         m.directory "#{extension_path}/spec/helpers"
 
@@ -42,19 +42,19 @@ class ExtensionGenerator < Rails::Generator::NamedBase
       end
     end
   end
-  
+
   def class_name
     super.to_name.gsub(' ', '') + 'Extension'
   end
-  
+
   def extension_name
     class_name.to_name('Extension')
   end
-  
+
   def add_options!(opt)
     opt.separator ''
     opt.separator 'Options:'
-    opt.on("--with-test-unit", 
+    opt.on("--with-test-unit",
            "Use Test::Unit for this extension instead of RSpec") { |v| options[:with_test_unit] = v }
   end
 end
