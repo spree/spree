abce4651138ea79459ab0a4259278b8ada8615e4
Sean Schofield <schof@apache.org>
2008-06-25 18:27:31 -0400

    Removed active_merchant plugin.  Apparently the issue with the routes being loaded prematurely has been fixed in Rails 2.1.  In any event we don't want to be carrying around all of this code anymore.  If the problem resurfaces we will try to fix via monkey patch or convince the active merchant guys to fix it.

--- vendor/plugins/active_merchant/init.rb
+++
@@ -1,3 +0,0 @@
-require 'active_merchant'
-require 'active_merchant/billing/integrations/action_view_helper'
-ActionView::Base.send(:include, ActiveMerchant::Billing::Integrations::ActionViewHelper)
