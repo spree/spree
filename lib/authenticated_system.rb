4907cbaec293e5ff219f756118f93fa8d5a694a7
Gregg Pollack <Gregg@RailsEnvy.com>
2008-07-07 13:36:09 -0400

    Named some of the routes so they can be used properly.  Also fixed authenticated system so on access denied it goes to the right page

--- lib/authenticated_system.rb
+++ lib/authenticated_system.rb
@@ -64,7 +64,7 @@ module AuthenticatedSystem
       respond_to do |format|
         format.html do
           store_location
-          redirect_to :controller => '/session', :action => 'new'
+          redirect_to login_path
         end
         format.xml do
           request_http_basic_authentication 'Web Password'
