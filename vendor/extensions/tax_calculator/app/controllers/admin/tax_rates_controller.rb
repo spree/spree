5c05f0985658b433b87e190d69ecba3bec2dd5df
Sean Schofield <schof@apache.org>
2008-07-07 17:23:47 -0400

    Removed the default admin role restriction for Admin::BaseController.  This allows extensions to define their own restrictions in their controllers using role_requirement.

--- vendor/extensions/tax_calculator/app/controllers/admin/tax_rates_controller.rb
+++ vendor/extensions/tax_calculator/app/controllers/admin/tax_rates_controller.rb
@@ -2,10 +2,12 @@ class Admin::TaxRatesController < Admin::BaseController
   resource_controller
   before_filter :load_data
   
+  require_role :admin
+  
   update.response do |wants|
     wants.html { redirect_to collection_url }
   end
-    
+  
   private 
   
     def load_data     
