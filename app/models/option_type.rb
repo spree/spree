bad19b9e13b35598e103627978bc370acf7e9b8f
Sean Schofield <schof@apache.org>
2008-08-02 14:33:05 -0400

    Refactored the option value stuff to make restful and use attribute_fu.

--- app/models/option_type.rb
+++ app/models/option_type.rb
@@ -1,4 +1,4 @@
 class OptionType < ActiveRecord::Base
-  has_many :option_values, :order => :position, :dependent => :destroy
+  has_many :option_values, :order => :position, :dependent => :destroy, :attributes => true
   validates_presence_of [:name, :presentation]
 end
