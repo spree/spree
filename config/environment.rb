ef24c1908b34351bce7d9aaf301dd6862ea09d8c
Sean Schofield <schof@apache.org>
2008-08-12 16:30:04 -0400

    Implemented new feature for allowing extensions to provide their own assets. (#64)

--- config/environment.rb
+++ config/environment.rb
@@ -37,7 +37,8 @@ Spree::Initializer.run do |config|
   # in vendor/plugins are loaded in alphabetical order.
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
-
+  config.plugins = [ :all, :extension_patches ] 
+  
   # Add additional load paths for your own custom dirs
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
 
