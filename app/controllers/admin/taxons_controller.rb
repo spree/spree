10a822aeda2cb1978af43168eb428e15c88af84d
Sean Schofield <schof@apache.org>
2008-09-12 13:10:22 -0400

    Implemented a rough interface for associating a taxon with a product.

--- app/controllers/admin/taxons_controller.rb
+++ app/controllers/admin/taxons_controller.rb
@@ -8,10 +8,32 @@ class Admin::TaxonsController < Admin::BaseController
     @taxons = @product.taxons
   end
   
+  def available
+    @available_taxons = Taxon.find(:all, :conditions => ['lower(presentation) LIKE ?', "%#{params[:q].downcase}%"])
+    respond_to do |format|
+      format.html
+      format.js {render :layout => false}
+    end
+    #@available_taxons = Taxon.all #hack
+    #
+  end
+  
   def remove
     @product.taxons.delete(@taxon)
     @product.save
     flash[:notice] = "Succesfully removed taxon."
     redirect_to selected_admin_product_taxons_url(@product)
   end  
+  
+  def select
+    @product = Product.find_by_param!(params[:product_id])
+    taxon = Taxon.find(params[:id])
+    @product.taxons << taxon
+    @product.save
+    @taxons = @product.taxons
+    render :action => :selected, :layout => false
+  end
+  
+  private 
+  
 end
--- app/helpers/admin/taxons_helper.rb
+++ app/helpers/admin/taxons_helper.rb
@@ -1,5 +1,5 @@
 module Admin::TaxonsHelper
   def taxon_path(taxon)
-    taxon.ancestors.reverse.collect { |ancestor| ancestor.display }.join( " >> ")
+    taxon.ancestors.reverse.collect { |ancestor| ancestor.presentation }.join( " >> ")
   end
 end
