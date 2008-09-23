d3d695362d1ecd15f9168496c4f176cc6c0b065a
Sean Schofield <schof@apache.org>
2008-09-23 10:10:12 -0400

    Prices should not show vat inclusive label (by default).

--- vendor/extensions/tax_calculator/lib/tax_configuration.rb
+++ vendor/extensions/tax_calculator/lib/tax_configuration.rb
@@ -1,6 +1,6 @@
 class TaxConfiguration < Configuration
 
-  preference :show_price_inc_vat, :boolean, :default => true
+  preference :show_price_inc_vat, :boolean, :default => false
   
   validates_presence_of :name
   validates_uniqueness_of :name
