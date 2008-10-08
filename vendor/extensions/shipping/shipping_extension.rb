78190a2bcf9390c7655ad98c6fe43dcf53b55329
Sean Schofield <schof@apache.org>
2008-10-08 13:53:22 -0400

    Shipping address should be limited to countries that are configured by a shipping zone.  #91

--- vendor/extensions/shipping/shipping_extension.rb
+++ vendor/extensions/shipping/shipping_extension.rb
@@ -1,5 +1,5 @@
 # Uncomment this if you reference any of your controllers in activate
-# require_dependency 'application'
+require_dependency 'application'
 
 class ShippingExtension < Spree::Extension
   version "1.0"
@@ -16,6 +16,14 @@ class ShippingExtension < Spree::Extension
     Order.class_eval do
       include Spree::ShippingCalculator
     end    
+    AddressesController.class_eval do
+      # limit the countries to the ones that are possible to ship to
+      def load_countries
+        return @countries = Country.all unless parent_model == Order
+        @countries = @order.shipping_countries
+        @countries = [Country.find(Spree::Config[:default_country_id])] if @countries.empty?
+      end
+    end
   end
   
   def deactivate
