2d7164d9ddb484d8e734190b24487322dc4d33ee
Sean Schofield <schof@apache.org>
2008-10-23 10:23:54 -0400

    Added support for additional variant fields.

--- app/controllers/admin/variants_controller.rb
+++ app/controllers/admin/variants_controller.rb
@@ -1,6 +1,7 @@
 class Admin::VariantsController < Admin::BaseController
   resource_controller
   belongs_to :product
+  before_filter :load_data, :only => [:index, :edit]
   
   new_action.response do |wants|
     wants.html {render :action => :new, :layout => false}
@@ -20,6 +21,12 @@ class Admin::VariantsController < Admin::BaseController
   # redirect to index (instead of r_c default of show view)
   update.response do |wants| 
     wants.html {redirect_to collection_url}
+  end
+  
+  private
+  def load_data
+    # this allows extensions to provide their own additional columns in the index and edit views
+    @additional_fields = Variant.column_names - ["id", "price", "sku", "product_id"]
   end
   
 end
