0856908946f67f74b3551f2429e1e4d8e52b6bfd
Sean Schofield <schof@apache.org>
2008-10-28 10:40:35 -0400

    Changed shipping calculation from static method to instance method.

--- vendor/extensions/shipping/app/models/shipment.rb
+++ vendor/extensions/shipping/app/models/shipment.rb
@@ -5,7 +5,7 @@ class Shipment < ActiveRecord::Base
   
   private 
   def calculate_shipping
-    calculator = shipping_method.shipping_calculator.constantize
+    calculator = shipping_method.shipping_calculator.constantize.new
     order.update_attribute(:ship_amount, calculator.calculate_shipping(order))
   end
 end
