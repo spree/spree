b8c53df208abf8fcf9887774135dbe4daec62d2b
Brian Quinn <briandquinn@gmail.com>
2009-01-02 20:24:13 +0000

    Added Rails specific translation files

--- config/initializers/locales.rb
+++ config/initializers/locales.rb
@@ -3,5 +3,6 @@ AVAILABLE_LOCALES =
 Dir.new(LOCALES_DIRECTORY).entries.collect do |x|
   x =~ /\.yml/ ? x.sub(/\.yml/,"") : nil
 end.compact.each_with_object({}) do |str, hsh|
-  hsh[str] = YAML.load_file(LOCALES_DIRECTORY + str + ".yml")[str]["this_file_language"]
+  locale_file = YAML.load_file(LOCALES_DIRECTORY + str + ".yml")
+  hsh[str] = locale_file[str]["this_file_language"] if locale_file.has_key? str
 end.freeze
