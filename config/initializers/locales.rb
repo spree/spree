5303437045f992f261c821895723d8045ef12c8b
Edmundo Valle Neto <edmundo.vn@gmail.com>
2009-01-15 14:14:19 -0200

    Fixed the locales initializer and added it to the instance generator.

--- config/initializers/locales.rb
+++ config/initializers/locales.rb
@@ -1,4 +1,4 @@
-LOCALES_DIRECTORY = "#{RAILS_ROOT}/config/locales/" 
+LOCALES_DIRECTORY = "#{SPREE_ROOT}/config/locales/" 
 AVAILABLE_LOCALES =
 Dir.new(LOCALES_DIRECTORY).entries.collect do |x|
   x =~ /\.yml/ ? x.sub(/\.yml/,"") : nil
