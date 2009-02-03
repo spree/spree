68707724ac03413b06dd2a737968cbe5c1aea82f
Sean Schofield <schof@apache.org>
2009-02-02 22:04:14 -0500

    Revert "More descriptive cart link"
    This reverts commit 72356417b2f5c726e7ecbc552207c48af7212836.

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,3 +1,14 @@
 module OrdersHelper  
+  def order_price(order, options={})
+    options.assert_valid_keys(:format_as_currency, :show_vat_text, :show_price_inc_vat)
+    options.reverse_merge! :format_as_currency => true, :show_vat_text => true
+    
+    # overwrite show_vat_text if show_price_inc_vat is false
+    options[:show_vat_text] = Spree::Tax::Config[:show_price_inc_vat]
 
+    amount =  order.item_total    
+    amount += Spree::VatCalculator.calculate_tax(order) if Spree::Tax::Config[:show_price_inc_vat]    
+
+    options.delete(:format_as_currency) ? format_price(amount, options) : amount
+  end
 end
