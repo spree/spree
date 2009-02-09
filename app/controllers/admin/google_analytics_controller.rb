07a439bd4041f6981372a929cded08f72bcea708
Stephanie Powell <steph@The-Laptop.(none)>
2009-02-09 16:03:05 -0700

    Removing google analytics model, controller, views

--- app/controllers/admin/google_analytics_controller.rb
+++
@@ -1,9 +0,0 @@
-class Admin::GoogleAnalyticsController < Admin::BaseController
-  resource_controller
-  create.response do |wants|
-    wants.html {redirect_to collection_url}
-  end
-  destroy.response do |wants|
-    wants.html {redirect_to collection_url}
-  end
-end
