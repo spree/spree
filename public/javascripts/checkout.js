73a9c94d71dac1949d0f96edecc2dcd016f3f098
Stephanie Powell <steph@The-Laptop.(none)>
2009-02-27 17:03:59 -0700

    Javascript to toggle state field between input and select. Ability to expand future state maps.

--- public/javascripts/checkout.js
+++ public/javascripts/checkout.js
@@ -19,23 +19,61 @@ jQuery.fn.submitWithAjax = function() {
 };
 
 $("#shipping_fieldset input, #shipping_fieldset select").each(function() {
-    var elem = $(this);
-    $("#billing_fieldset #"+ 
-        elem.attr("id").replace(/shipping/, "billing")).val(elem.val());
+    $("#billing_fieldset #"+ $(this).attr("id").replace(/shipping/, "billing")).val($(this).val());
 });
                                   
 jQuery.fn.sameAddress = function() {
   this.click(function() {
-    $("#billing :input, #billing select").each(function() {    
-      var elem = $(this);
-      $("#shipping #"+ elem.attr("id").replace(/bill/, "ship")).val(elem.val());
+    if(!$(this).attr('checked')) {
+      //Clear ship values?
+      return;
+    }
+    $('td#scountry select').val($('td#bcountry select').val());
+    update_state('s');
+    $("#billing input, #billing select").each(function() {
+      $("#shipping #"+ $(this).attr('id').replace('bill', 'ship')).val($(this).val());
     })
+    //For some reason this isn't getting picked up from above.. Debug later
+    $('#sstate :child').val($('#bstate :child').val());
   })
 }
 
-
+//On page load
 $(function() {  
-  $("#checkout_presenter_bill_address_country_id").submitWithAjax();  
+  //$("#checkout_presenter_bill_address_country_id").submitWithAjax();  
   $('#same_address').sameAddress();
+  $('td#bcountry :child').change(function() { update_state('b'); });
+  $('td#scountry :child').change(function() { update_state('s'); });
+  get_states();
   //$("#new_review").submitWithAjax();
 })
+
+//Initial state mapper on page load
+var state_mapper;
+var get_states = function() {
+  $.getJSON('/javascripts/states.js', function(json) {
+    state_mapper = json;
+  });
+};
+
+//Update state input / select
+var update_state = function(region) {
+  var name = $('td#' + region + 'state :child').attr('id');
+  var id = $('td#' + region + 'state :child').attr('name');
+  $('td#' + region + 'state :child').remove();
+  var match;
+  var selected = $('td#' + region + 'country :child :selected').html()
+  $.each(state_mapper.maps, function(i, item) {
+    if(selected == item.country) {
+      match = item.states;
+    }
+  });
+  if(match) {
+    $('td#' + region + 'state').append($(document.createElement('select')).attr('id', id).attr('name', name));
+    $.each(match, function(i, item) {
+      $('td#' + region + 'state select').append($(document.createElement('option')).attr('value', item.value).html(item.text));
+    });
+  } else {
+    $('td#' + region + 'state').append($(document.createElement('input')).attr('id', id).attr('name', name));
+  }
+};
