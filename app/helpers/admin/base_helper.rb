abc3f047f3b4c649965f6aed02579a5851cc26f8
David North <davidnorth@gmail.com>
2009-02-28 12:07:05 +0000

    fix that get_additional_field_value helper method got lost in moving admin interface from extension to core raising error on editing option type

--- app/helpers/admin/base_helper.rb
+++ app/helpers/admin/base_helper.rb
@@ -93,5 +93,20 @@ module Admin::BaseHelper
     end
   end
 
+  def get_additional_field_value(controller, field)  
+    attribute = field[:name].gsub(" ", "_").downcase
+
+    value = eval("@" + controller.controller_name.singularize + "." + attribute)  
+    
+    if value.nil? && controller.controller_name == "variants"
+      value = @variant.product.has_attribute?(attribute) ? @variant.product[attribute] : nil
+    end
+
+    if value.nil?
+      return value
+    else
+      return field.key?(:format) ? sprintf(field[:format], value) : value
+    end
+  end
   
 end
