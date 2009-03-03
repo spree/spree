cf1d69c83c3cc2334446660f75b87fa5592209be
Stephanie Powell <steph@The-Laptop.(none)>
2009-03-02 18:23:13 -0700

    First round of tab checkout with sectional validation

--- public/javascripts/checkout.js
+++ public/javascripts/checkout.js
@@ -28,7 +28,7 @@ jQuery.fn.sameAddress = function() {
       //Clear ship values?
       return;
     }
-    $('td#scountry select').val($('td#bcountry select').val());
+    $('span#scountry select').val($('span#bcountry select').val());
     update_state('s');
     $("#billing input, #billing select").each(function() {
       $("#shipping #"+ $(this).attr('id').replace('bill', 'ship')).val($(this).val());
@@ -41,23 +41,74 @@ jQuery.fn.sameAddress = function() {
 //On page load
 $(function() {  
   //$("#checkout_presenter_bill_address_country_id").submitWithAjax();  
-  $('#same_address').sameAddress();
-  $('td#bcountry select').change(function() { update_state('b'); });
-  $('td#scountry select').change(function() { update_state('s'); });
+  $('#checkout_presenter_same_address').sameAddress();
+  $('span#bcountry select').change(function() { update_state('b'); });
+  $('span#scountry select').change(function() { update_state('s'); });
   get_states();
-  $('div#validate_billing').css('cursor', 'pointer').click(function() { validate_section('billing'); });
-  $('div#validate_shipping').css('cursor', 'pointer').click(function() { validate_section('shipping'); });
-  $('div#validate_creditcard').css('cursor', 'pointer').click(function() { validate_section('creditcard'); });
+  $('div#validate_billing').css('cursor', 'pointer').click(function() { if(validate_section('billing')) { submit_billing(); }});
+  $('div#validate_shipping').css('cursor', 'pointer').click(function() { if(validate_section('shipping')) { submit_shipping(); }});
+  $('div#billing h2').click(function() { check_billing(); });
+  $('div#shipping h2').click(function() { check_shipping(); });
+  $('div#creditcard h2').click(function() { check_creditcard(); });
 })
 
+var check_billing = function() {
+  $('div#creditcard div.inner, div#shipping div.inner').hide();
+  $('div#creditcard, div#shipping').addClass('checkout_disabled');
+  $('div#billing div.inner').show();
+}
+
+var check_shipping = function() {
+  if($('div#shipping').attr('class') == 'checkout_disabled') {
+    if(validate_section('billing')) {
+      submit_billing();
+    }
+  } else {
+    $('div#creditcard div.inner').hide();
+    $('div#creditcard').addClass('checkout_disabled');
+    $('div#shipping div.inner').show();
+  }
+  return;
+}
+
+var check_creditcard = function() {
+  if($('div#creditcard').attr('class') == 'checkout_disabled') {
+    if($('div#shipping').attr('class') == 'checkout_disabled') {
+      if(validate_section('billing')) {
+        submit_billing();
+      }
+    }
+    if(validate_section('shipping')) {
+      submit_shipping();
+    }
+  }
+}
+
+var submit_billing = function() {
+  //INSIDE SUCCESS OF VALIDATION
+  $('div#billing div.inner').hide();
+  $('div#shipping div.inner').show();
+  $('div#shipping').removeClass('checkout_disabled');
+  //OTHERWISE ADD ERROR
+  return;
+}
+var submit_shipping = function() {
+  //INSIDE SUCCESS OF VALIDATION
+  $('div#shipping div.inner').hide();
+  $('div#creditcard div.inner').show();
+  //OTHERWISE ADD ERROR
+  return;
+}
+
 var validate_section = function(region) {
-	var validator = $('form#checkout_form').validate();
-	var valid = true; 
-	$('div#' + region + ' input, div#' + region + ' select, div#' + region + ' textarea').each(function() {
-		if(!validator.element(this)) {
-			valid = false;
-		}
-	});
+  var validator = $('form#checkout_form').validate();
+  var valid = true; 
+  $('div#' + region + ' input, div#' + region + ' select, div#' + region + ' textarea').each(function() {
+    if(!validator.element(this)) {
+      valid = false;
+    }
+  });
+  return valid;
 };
 
 //Initial state mapper on page load
@@ -65,34 +116,34 @@ var state_mapper;
 var get_states = function() {
   $.getJSON('/javascripts/states.js', function(json) {
     state_mapper = json;
-    $('td#bcountry select').val($('[name=submit_bcountry]').val());
+    $('span#bcountry select').val($('[name=submit_bcountry]').val());
     update_state('b');
-    $('td#bstate :child').val($('[name=submit_bstate]').val());
-    $('td#scountry select').val($('[name=submit_scountry]').val());
+    $('span#bstate :child').val($('[name=submit_bstate]').val());
+    $('span#scountry select').val($('[name=submit_scountry]').val());
     update_state('s');
-    $('td#sstate :child').val($('[name=submit_sstate]').val());
+    $('span#sstate :child').val($('[name=submit_sstate]').val());
   });
 };
 
 //Update state input / select
 var update_state = function(region) {
-  var name = $('td#' + region + 'state :child').attr('name');
-  var id = $('td#' + region + 'state :child').attr('id');
-  $('td#' + region + 'state :child').remove();
+  var name = $('span#' + region + 'state :child').attr('name');
+  var id = $('span#' + region + 'state :child').attr('id');
+  $('span#' + region + 'state :child').remove();
   var match;
-  var selected = $('td#' + region + 'country :child :selected').html()
+  var selected = $('span#' + region + 'country :child :selected').html()
   $.each(state_mapper.maps, function(i, item) {
     if(selected == item.country) {
       match = item.states;
     }
   });
   if(match) {
-    $('td#' + region + 'state').append($(document.createElement('select')));
+    $('span#' + region + 'state').append($(document.createElement('select')));
     $.each(match, function(i, item) {
-      $('td#' + region + 'state select').append($(document.createElement('option')).attr('value', item.value).html(item.text));
+      $('span#' + region + 'state select').append($(document.createElement('option')).attr('value', item.value).html(item.text));
     });
   } else {
-    $('td#' + region + 'state').append($(document.createElement('input')));
+    $('span#' + region + 'state').append($(document.createElement('input')));
   }
-  $('td#' + region + 'state select, td#' + region + 'state input').addClass('required').attr('name', name).attr('id', id);
+  $('span#' + region + 'state select, span#' + region + 'state input').addClass('required').attr('name', name).attr('id', id);
 };
