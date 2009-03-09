1c3b5deed7afc3ecbce25956656a75facd6329fe
paulcc <paulcc.two@gmail.com>
2009-03-09 18:51:14 +0000

    ensuring state lists are in name order

--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -5,12 +5,14 @@ class StatesController < Spree::BaseController
     wants.html
     wants.js do
       # @states = end_of_association_chain.find(:all, :conditions => ['lower(name) LIKE ?', "%#{params[:q].downcase}%"], :order => :name)
-      
-      usa = { '' => '' }
+     
+      # table of {country.id => { state.id => state.name }}
+      # blank added elsewhere, if needed
+      usa = {}
       State.find(:all, :conditions => "country_id = 214", 
                        :select => "id, name"
                        ).each do |s|
-        usa[s.id] = s.name
+        usa[s.name] = s.id
       end
       @state_info = { "214" => usa }	# to be generalised...
     end
