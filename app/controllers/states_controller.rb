15593b2b089d4151f36c6ea0e4f9aec4fdc7e074
paulcc <paulcc.two@gmail.com>
2009-03-09 17:42:48 +0000

    /states.js now provides a table of { country.id => { state.id => state.name }}
    (NB the inner tables need to be sorted by name... - will do later)

--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -4,7 +4,15 @@ class StatesController < Spree::BaseController
   index.response do |wants|
     wants.html
     wants.js do
-      @states = end_of_association_chain.find(:all, :conditions => ['lower(name) LIKE ?', "%#{params[:q].downcase}%"], :order => :name)
+      # @states = end_of_association_chain.find(:all, :conditions => ['lower(name) LIKE ?', "%#{params[:q].downcase}%"], :order => :name)
+      
+      usa = {}
+      State.find(:all, :conditions => "country_id = 214", 
+                       :select => "id, name",
+                       ).each do |s|
+        usa[s.id] = s.name
+      end
+      @state_info = { "214" => usa }	# to be generalised...
     end
   end
 end
