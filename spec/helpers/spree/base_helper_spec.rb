5db73dc7282ab93e6b421313aeb9430387621bd9
Tor Hovland <tor@nidaros.homedns.org>
2009-03-14 10:16:21 +0100

    Updated strings in some specs. (cherry picked from commit 46eccf6bca83d00ea85e146bc936de57abca159d)

--- spec/helpers/spree/base_helper_spec.rb
+++ spec/helpers/spree/base_helper_spec.rb
@@ -10,20 +10,20 @@ describe Spree::BaseHelper do
     describe "with :allow_backorders => true" do
       before(:each) { Spree::Config.stub!(:[]).with(:allow_backorders).and_return(true) }
       it "should not mention 'out of stock'" do
-        helper.variant_options(@variant).should_not include("OUT OF STOCK")
+        helper.variant_options(@variant).should_not include("Out of Stock")
       end
       it "should mention 'out of stock' when passing allow_back_orders = false" do
-        helper.variant_options(@variant, false).should include("OUT OF STOCK")
+        helper.variant_options(@variant, false).should include("Out of Stock")
       end
     end
 
     describe "with :allow_backorders => false" do
       before(:each) { Spree::Config.stub!(:[]).with(:allow_backorders).and_return(false) }    
       it "should mention 'out of stock'" do
-        helper.variant_options(@variant).should include("OUT OF STOCK")        
+        helper.variant_options(@variant).should include("Out of Stock")        
       end
       it "should not mention 'out of stock' when passing allow_back_orders = true" do
-        helper.variant_options(@variant, true).should_not include("OUT OF STOCK")
+        helper.variant_options(@variant, true).should_not include("Out of Stock")
       end
     end
   end
