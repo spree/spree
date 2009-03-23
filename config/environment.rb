751180f2bc54fc7d3bf1dbaa75f0ea7d33241aaf
Sean Schofield <schof@apache.org>
2009-03-23 17:41:12 -0400

    Fixed compass dependency

--- config/environment.rb
+++ config/environment.rb
@@ -33,7 +33,7 @@ Spree::Initializer.run do |config|
   config.gem 'activerecord-tableless', :lib => 'tableless', :version => '0.1.0'
   config.gem 'searchlogic', :version => '1.6.3' 
   config.gem 'haml', :version => '2.1.0'
-  #config.gem 'chriseppstein-compass', :version => '0.5.1'
+  config.gem 'chriseppstein-compass', :lib => 'compass', :version => '0.5.1'
 
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
