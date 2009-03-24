edeaee17d89e3396f27559fdad9705f80e23b0f8
David North <davidnorth@david-norths-macbook-pro-15.local>
2009-03-24 16:09:11 +0000

    sassify checkout css and include in screen.sass. bring javascripts out of checkout page into main layout so that all required javascripts can be concatenated and cached

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,19 +7,6 @@ module ApplicationHelper
     false
   end
 
-  def stylesheets
-    stylesheets = [stylesheet_link_tag("spree"), stylesheet_link_tag("application")]
-    ["#{controller.controller_name}/_controller", "#{controller.controller_name}/#{:action_name}"].each do |stylesheet|
-      if File.exists? "#{RAILS_ROOT}/public/stylesheets/#{stylesheet}.css" 
-        stylesheets << stylesheet_link_tag(stylesheet)   
-      # TODO - consider bringing this back to use with stylesheets in the extension
-      #else
-      #  stylesheets << stylesheet_link_tag(stylesheet, :plugin=>"spree") if File.exists? "#{RAILS_ROOT}/public/plugin_assets/spree/stylesheets/#{stylesheet}.css"
-      end
-    end
-    stylesheets.compact.join("\n")
-  end  
-  
   # Renders all the extension partials that may have been specified in the extensions
   def render_extra_partials(f)
     @extension_partials.inject("") do |extras, partial|
