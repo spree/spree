5ce5bbcdbf6759d3cd412f3d6aef18a19fb83ea9
David North <davidnorth@david-norths-macbook-pro-15.local>
2009-03-25 10:22:34 +0000

    make product new form work like the ones on option types etc, appears above the product list (cherry picked from commit 82bcccdca4e69d4e4e6fa61b7b084f2cdfd7ecdc)

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -9,6 +9,10 @@ class Admin::ProductsController < Admin::BaseController
     @product.tax_category = TaxCategory.find_by_name Spree::Config[:default_tax_category]
   end
   
+  new_action.response do |wants|
+    wants.html {render :action => :new, :layout => false}
+  end
+
   update.before do
     # note: we only reset the product properties if we're receiving a post from the form on that tab
     next unless params[:clear_product_properties] 
