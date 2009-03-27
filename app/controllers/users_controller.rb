7f2fa70e95445849a2d3f05bf81004c11cd23db8
Dale Hofkens <dale@infinitize.com>
2009-03-27 18:08:39 +0100

    implemented translations for the authlogic part

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,16 @@
 class UsersController < Spree::BaseController
+  resource_controller
+  
   before_filter :require_no_user, :only => [:new, :create]
   before_filter :require_user, :only => [:show, :edit, :update]
-  #  before_filter :login_required, :except => [:new, :create]
   before_filter :initialize_extension_partials
 
-  resource_controller
   actions :all, :except => [:index, :destroy]
   
-#  def new
-#    @user = User.new
-#  end
-
   def create
     @user = User.new(params[:user])
     @user.roles << Role.find_by_name("user")
     if @user.save
-      #flash nil
-      #flash[:notice] = "Account registered!"
       redirect_back_or_default products_path
     else
       render :action => :new
@@ -27,29 +21,20 @@ class UsersController < Spree::BaseController
     @orders = Order.checkout_completed(true).find_all_by_user_id(current_user.id)
   end
 
-#  create do
-#    flash nil
-#    wants.html { redirect_back_or_default(products_path) }         
-#  end
+  def update
+    @user = @current_user
+    if @user.update_attributes(params[:user])
+      flash[:notice] = t("account_updated")
+      redirect_to account_url
+    else
+      render :action => :edit
+    end
+  end
 
-#  create.after do
-#    @user.roles << Role.find_by_name("user")
-#    @user.save   
-#    #self.current_user = @user       
-#  end
 
+  private
+    def object
+      @object ||= current_user
+    end
 
-#  def edit
-#    @user = @current_user
-#  end
-
-#  def update
-#    @user = @current_user # makes our views "cleaner" and more consistent
-#    if @user.update_attributes(params[:user])
-#      flash[:notice] = "Account updated!"
-#      redirect_to account_url
-#    else
-#      render :action => :edit
-#    end
-#  end
 end
