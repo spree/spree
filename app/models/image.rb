5f8554e251ba3496532f97c1838ca781258ce0e0
Melvin Ram <melvin@volcanicmarketing.com>
2009-04-09 18:32:49 -0700

    Updated Image model to have a larger image size (600x600>) for use by designers... and for lightbox plugins.

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,6 +1,6 @@
 class Image < Asset
   has_attached_file :attachment, 
-                    :styles => { :mini => '48x48>', :small => '100x100>', :product => '240x240>' }, 
+                    :styles => { :mini => '48x48>', :small => '100x100>', :product => '240x240>', :large => '600x600>' }, 
                     :default_style => :product,
                     :url => "/assets/products/:id/:style/:basename.:extension",
                     :path => ":rails_root/public/assets/products/:id/:style/:basename.:extension"
