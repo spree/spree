f68a87519a06cdc831589c705a3bbf14e300ae48
Steph Powell steph@endpoint.com <steph@v277.spreecamps.com>
2009-04-13 14:31:37 -0400

    DB Migrations work. DB Sample changes.

--- db/migrate/20090406174646_refactor_addresses_user_nickname.rb
+++ db/migrate/20090406174646_refactor_addresses_user_nickname.rb
@@ -3,17 +3,25 @@ class RefactorAddressesUserNickname < ActiveRecord::Migration
     change_table :addresses do |t|
       t.integer :user_id
       t.string :nickname
+      t.integer :active
     end
     Address.find_in_batches do |addresses|
       addresses.each do |address|
         creditcard = Creditcard.find_by_address_id(address.id)
         shipment = Shipment.find_by_address_id(address.id)
-        address.user_id = creditcard ? creditcard.user.id : shipment.user.id
+        next if !(creditcard || shipment)
+        address.user_id = creditcard ? creditcard.order.user.id : shipment.order.user.id
+        address.nickname = address.address1
+        address.active = 1
         address.save
+        #If there is another duplicate address, remove duplicate and update order to reference address correctly
       end
     end
   end
 
   def self.down
+    remove_column :addresses, :user_id
+    remove_column :addresses, :nickname
+    remove_column :addresses, :active
   end
 end
