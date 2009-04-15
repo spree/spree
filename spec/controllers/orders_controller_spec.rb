78abeec2fd485fdcdad53650886f6aa57a76877a
Sean Schofield <schof@apache.org>
2009-04-15 17:54:45 -0400

    Fixed broken specs.

--- spec/controllers/orders_controller_spec.rb
+++ spec/controllers/orders_controller_spec.rb
@@ -3,20 +3,22 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 describe OrdersController do
   before(:each) do
     Variant.stub!(:find).with(any_args).and_return(@variant = mock_model(Variant, :price => 10, :on_hand => 50))
-    @order = Order.new
-    @order.stub!(:number).and_return("R100")
-    controller.stub!(:find_order).and_return(@order)
+    @order = Order.create
+    @token = "TOKEN123"
+    @order.token = @token
+    @order.save
   end
 
   describe "create" do
     it "should add the variant to the order" do
+      controller.stub!(:object).and_return(@order)
       @order.should_receive(:add_variant).with(@variant, 2)
 	    post :create, :variants => {"345" => 2}
     end
   
     it "should not set the state" do
       @order.should_not_receive(:state=)
-      post :create, :id => "345", :quantities => {456 => "123=1"}, :order => {:state => "paid"}
+      post :create, :id => @order.number, :quantities => {456 => "123=1"}, :order => {:state => "paid"}
     end    
   end
   
@@ -25,7 +27,7 @@ describe OrdersController do
       it "should not set #{attribute} with mass assignment" do
         #@order.send(attribute).should_not == "naughty"
         @order.should_not_receive("#{attribute}=".to_sym).with("naughty")
-        put :update, "id" => "123", "order" => {attribute => "naughty"}
+        put :update, "id" => @order.number, "order" => {attribute => "naughty"}
       end
     end
   end
