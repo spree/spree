c08e827805ac362f55332ab51b057b0c4148f657
Sean Schofield <schof@apache.org>
2009-04-27 10:56:32 -0400

    Fixed a problem with en-US rails translation.

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -27,10 +27,10 @@ describe Address do
     @address.should_not be_valid
   end
 
-  ['First Name', 'Last Name', 'Address', 'City', 'Country', 'Zip Code', 'Phone'].each do |field|
+  ['firstname', 'lastname', 'address1', 'city', 'country', 'zipcode', 'phone'].each do |field|
     it "should require #{field}" do
-      @address.should_not be_valid
-      @address.errors.full_messages.should include("#{field} #{I18n.translate("activerecord.errors.messages.blank")}")
+      @address.should_not be_valid                    
+      @address.errors.full_messages.should include("#{Address.human_attribute_name(field)} #{I18n.translate("activerecord.errors.messages.blank")}")
     end
   end
 
