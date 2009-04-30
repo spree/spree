dd114e1af0e3c1c9bcbb6b1e3f3bd2e6368d80e8
Sean Schofield <schof@apache.org>
2009-04-30 13:21:23 -0400

    Enabled page level cacheing for near static content. #405

--- app/controllers/content_controller.rb
+++ app/controllers/content_controller.rb
@@ -1,5 +1,6 @@
 class ContentController < Spree::BaseController
   rescue_from ActionView::MissingTemplate, :with => :render_404
+  caches_page :show, :index
 
   def show
     render :action => params[:path].join('/')
