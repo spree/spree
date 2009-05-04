ec7ff2c422cd9b77432aca8d79ba1a4b7b7e23de
Brian Quinn <briandquinn@gmail.com>
2009-05-04 20:12:41 +0100

    Fixes localization override issue #436

--- lib/spree/initializer.rb
+++ lib/spree/initializer.rb
@@ -148,7 +148,7 @@ module Spree
     def initialize_i18n
       extension_loader.add_locale_paths
       spree_locale_paths = Dir[File.join(SPREE_ROOT, 'config', 'locales', '*.{rb,yml}')]
-      extension_loader.configuration.i18n.load_path.concat(spree_locale_paths)
+			spree_locale_paths.concat(extension_loader.configuration.i18n.load_path)
       super
     end
 
