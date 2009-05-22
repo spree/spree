a8cad91cb9e0d4478ad4ca2f9e495c1e3892d631
Brian Quinn <briandquinn@gmail.com>
2009-05-22 17:50:56 +0100

    Added more handlers for creating / renaming / updating taxons, with progress indicators and error handling

--- public/javascripts/taxonomy.js
+++ public/javascripts/taxonomy.js
@@ -1,6 +1,26 @@
 var base_url = "/admin/taxonomies/" + taxonomy_id + "/taxons/";
+var creating = false;
+var delete_confirmed = false;
+var last_rollback = null;
 
-var handle_move = function(li, target, droppped) {
+
+var show_progress = function(){
+	$("#progress").show();
+	$("#ajax_error").hide();
+}
+
+var hide_progress = function(){
+	$("#progress").hide();
+}
+
+var handle_ajax_error = function(XMLHttpRequest, textStatus, errorThrown){
+	$.tree_rollback(last_rollback);
+	$("#progress").hide();
+	$("#ajax_error").show().html("<strong>" + server_error + "</strong><br/>" + taxonomy_tree_error);
+};
+
+var handle_move = function(li, target, droppped, tree, rb) {
+	last_rollback = rb;
   var position = $(li).prevAll().length;
 
   var parent = -1;
@@ -16,7 +36,10 @@ var handle_move = function(li, target, droppped) {
   $.ajax({
     type: "POST",
     url: base_url + li.id,
-    data: ({_method: "put", "taxon[parent_id]": parent.id, "taxon[position]": position, authenticity_token: AUTH_TOKEN})        
+    data: ({_method: "put", "taxon[parent_id]": parent.id, "taxon[position]": position, authenticity_token: AUTH_TOKEN}),
+		beforeSend: show_progress,
+    error: handle_ajax_error,
+		success: hide_progress
   });
         
   return true
@@ -26,14 +49,77 @@ var handle_dblclick = function(li, tree) {
   tree.rename();
 };
 
-var handle_rename = function(li) {
+var handle_create = function(parent, sib, created, tree, rb){
+	last_rollback = rb;
+	creating=true;	
+};
+
+var handle_created = function(id,result) {
+	hide_progress();
+	
+	$.tree_reference('taxonomy_tree').selected.attr('id', id);
+}
+
+var handle_rename = function(li, bl, tree, rb) {
   var name = $(li).children()[0].text;
   
-  $.ajax({
+	if (creating){
+		//actually creating new
+		var position = $(li).prevAll().length;
+		var parent = $(li).parents()[1];
+	  
+		$.ajax({
+	    type: "POST",
+	    url: base_url,
+	    data: ({"taxon[name]": name, "taxon[parent_id]": parent.id, "taxon[position]": position, authenticity_token: AUTH_TOKEN}),
+	    beforeSend: show_progress,
+			error: handle_ajax_error,
+	  	success: handle_created
+	  });	
+	
+		creating = false;
+	}else{
+		//just renaming
+		last_rollback = rb;
+		
+	  $.ajax({
+	    type: "POST",
+	    url: base_url + li.id,
+	    data: ({_method: "put", "taxon[name]": name, authenticity_token: AUTH_TOKEN}),
+	    beforeSend: show_progress,
+			error: handle_ajax_error,
+			success: hide_progress        
+	  });
+	}
+};
+
+var handle_before_delete = function(li){
+
+	if (!delete_confirmed){
+		jConfirm('Are you sure you want to delete this taxon?', 'Confirm Taxon Deletion', function(r) {
+			if(r){
+				delete_confirmed = true;
+				$.tree_reference('taxonomy_tree').remove(li);
+			}
+		});
+	}
+	
+	return delete_confirmed;
+};
+
+var handle_delete = function(li, tree, rb){
+	last_rollback = rb;
+		
+	$.ajax({
     type: "POST",
     url: base_url + li.id,
-    data: ({_method: "put", "taxon[name]": name, authenticity_token: AUTH_TOKEN})        
+    data: ({_method: "delete", authenticity_token: AUTH_TOKEN}),
+   	beforeSend: show_progress,
+		error: handle_ajax_error,
+		success: hide_progress		
   });
+
+	delete_confirmed = false;
 };
 
 conf = { 
@@ -45,7 +131,7 @@ conf = {
             label   : "Create", 
             icon    : "create.png",
             visible : function (NODE, TREE_OBJ) { if(NODE.length != 1) return false; return TREE_OBJ.check("creatable", NODE); }, 
-            action  : function (NODE, TREE_OBJ) { TREE_OBJ.create(false, TREE_OBJ.get_node(NODE)); } 
+            action  : function (NODE, TREE_OBJ) { TREE_OBJ.create({ attributes : { 'rel' : 'taxon' } }, TREE_OBJ.get_node(NODE)); } 
         },
         "separator",
         { 
@@ -81,23 +167,28 @@ conf = {
     ]
   },
   lang : {
-         new_node    : "<%= t('new_taxon') %>",
-         loading     : "<%= t('loading') %> ..."
+         new_node    : new_taxon,
+         loading     : loading + "..."
   },
   rules : {
     droppable : [ "tree-drop" ],
     multiple : true,
-    deletable : "all",
-    draggable : "all"
+    deletable : ["taxon"],
+    draggable : ["taxon"],
+	 	dragrules : [ "taxon * taxon", "taxon inside root", ],
   },
   callback : {
     onmove: handle_move,
     ondblclk: handle_dblclick,
-    onrename: handle_rename
+    onrename: handle_rename,
+		oncreate: handle_create,
+		beforedelete: handle_before_delete,
+		ondelete: handle_delete
   }
 };
 
 $(document).ready(function(){
+	
   tax_tree = $.tree_create();
   tax_tree.init($("#taxonomy_tree"), $.extend({},conf));
   
