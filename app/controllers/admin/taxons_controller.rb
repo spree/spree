8aed634cc1a29a9336e6d660de62c9e512ae1ad4
Brian Quinn <briandquinn@gmail.com>
2009-05-22 17:50:56 +0100

    Added more handlers for creating / renaming / updating taxons, with progress indicators and error handling

--- app/controllers/admin/taxons_controller.rb
+++ app/controllers/admin/taxons_controller.rb
@@ -4,9 +4,9 @@ class Admin::TaxonsController < Admin::BaseController
   before_filter :load_object, :only => [:selected, :available, :remove]
   belongs_to :product
   
-  create.wants.js {render :text => @taxon.to_json()}
+  create.wants.html {render :text => @taxon.id}
   update.wants.html {render :text => @taxon.name}
-  destroy.wants.js {render :text => ""}
+  destroy.wants.html {render :text => ""}
   
   create.before do 
     @taxon.taxonomy_id = params[:taxonomy_id]
