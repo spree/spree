955fa28ec44065416633acc87ab912532ffa2010
Christopher Maujean <cmaujean@gmail.com>
2009-06-20 00:27:16 -0400

    Improve admin menu for extension. Allow extensions to create admin tabs.
    [#332 state:resolved]

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,12 +1,21 @@
 class Admin::BaseController < Spree::BaseController
   helper :search
   layout 'admin'
-  
+
   before_filter :initialize_product_admin_tabs
   before_filter :initialize_order_admin_tabs
+  before_filter :initialize_extension_tabs
   before_filter :add_shipments_tab
 
 private
+  def add_extension_admin_tab(tab_args)
+    @extension_tabs << tab_args
+  end
+
+  def initialize_extension_tabs
+    @extension_tabs = []
+  end
+
   def add_shipments_tab
     @order_admin_tabs << {:name => 'Shipments', :url => "admin_order_shipments_url"}
   end
