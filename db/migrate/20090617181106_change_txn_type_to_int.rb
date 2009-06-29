667a13a7b6aeaa605dbca016baa3a9d0e61c937c
Marcin Raczkowski <marcin.raczkowski@gmail.com>
2009-06-29 10:55:50 +0200

    Fixed migration failure for postgres > 8.0
    [543 state:resolved milestone:0.8.99]

--- db/migrate/20090617181106_change_txn_type_to_int.rb
+++ db/migrate/20090617181106_change_txn_type_to_int.rb
@@ -1,6 +1,16 @@
 class ChangeTxnTypeToInt < ActiveRecord::Migration
   def self.up
-    change_column :creditcard_txns, :txn_type, :integer
+    if (ActiveRecord::Migration.connection.adapter_name == "PostgreSQL") && (postgresql_version > 80000)
+      
+      execute('
+        ALTER TABLE "creditcard_txns" 
+          ALTER COLUMN "txn_type" 
+          TYPE integer 
+          USING to_number(creditcard_txns.txn_type,\'S9999999\')
+      ')
+    else
+      change_column :creditcard_txns, :txn_type, :integer
+    end
   end
 
   def self.down
