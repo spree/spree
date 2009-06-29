c82d1c42ba080b2bea60fe171b5b29a39c413bfd
Sean Schofield <schof@apache.org>
2009-06-28 22:44:19 -0400

    Rewrote some existing rspec tests as shoulda. [#524]

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,7 +1,9 @@
 class Country < ActiveRecord::Base
   has_many :states
   named_scope :order_by_name, :order => :name  
-
+  validates_presence_of :name  
+  validates_presence_of :iso_name
+  
   def <=>(other)
     name <=> other.name
   end
