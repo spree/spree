ff040f238bdab498860565447533c367e31aaaa3
Sean Schofield <schof@apache.org>
2009-06-29 12:43:52 -0400

    allow migration to pass, even if you don't have fixtures loaded
    [#530 state:resolved milestone:0.8.99]

--- db/migrate/20080703184654_update_country_rename_name.rb
+++ db/migrate/20080703184654_update_country_rename_name.rb
@@ -4,11 +4,14 @@ class UpdateCountryRenameName < ActiveRecord::Migration
       t.rename :name, :iso_name
       t.rename :printable_name, :name
     end    
-    
-    Country.update(119, { :name => 'Macedonia' })
-    Country.update(197, { :name => 'Taiwan' })
-    Country.update(105, { :name => 'North Korea' })
-    Country.update(106, { :name => 'South Korea' })
+
+    begin
+      Country.update(119, { :name => 'Macedonia' })
+      Country.update(197, { :name => 'Taiwan' })
+      Country.update(105, { :name => 'North Korea' })
+      Country.update(106, { :name => 'South Korea' })
+    rescue Exception
+    end
   end
 
   def self.down
--- db/migrate/20090401223217_create_eu_vat_zone_again.rb
+++ db/migrate/20090401223217_create_eu_vat_zone_again.rb
@@ -3,7 +3,7 @@ class CreateEuVatZoneAgain < ActiveRecord::Migration
     # create an EU VAT zone (for optional use with EU VAT)
     zone = Zone.find_or_create_by_name_and_description "EU_VAT", "Countries that make up the EU VAT zone."
 
-    unless zone.id
+    unless zone && zone.id
       say "Zone EU_VAT ID not found"
     end
 
@@ -13,7 +13,7 @@ class CreateEuVatZoneAgain < ActiveRecord::Migration
     end
     
     # manually create the countries (instead of using ActiveRecord method due to some apparent issues with HMP plugin)
-    countries.each do |country|
+    countries.compact.each do |country|
       execute "INSERT INTO zone_members (zone_id, zoneable_id, zoneable_type, created_at, updated_at) 
                VALUES (#{zone.id}, #{country.id}, 'Country', '#{Time.now.to_s(:db)}', '#{Time.now.to_s(:db)}')"
     end
