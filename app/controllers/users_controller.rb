c3e75ebe849303b8435a908ad6cab3b62182f98c
Sean Schofield <schof@apache.org>
2009-07-01 10:56:54 -0400

    MyAccount should not include incomplete orders.  Fixed this along plus verified with new test.
    [#541 state:resolved]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,7 @@ class UsersController < Spree::BaseController
   end
 
   show.before do
-    @orders = Order.checkout_complete.find_all_by_user_id(current_user.id)
+    @orders = @user.orders.checkout_complete 
   end
 
   def update
