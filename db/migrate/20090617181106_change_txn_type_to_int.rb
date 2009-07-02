052c20cf3466768a3f214ee862a5bd89ca8b36b3
Sean Schofield <schof@apache.org>
2009-07-01 20:32:25 -0400

    Revert "Fixed migration failure for postgres > 8.0"
    This reverts commit 40bffa0909174b7e141bf717b138d74600096613.

--- db/migrate/20090617181106_change_txn_type_to_int.rb
+++ db/migrate/20090617181106_change_txn_type_to_int.rb
@@ -1,10 +1,6 @@
 class ChangeTxnTypeToInt < ActiveRecord::Migration
   def self.up
-    if postgresql_version > 80000
-      execute('ALTER TABLE "creditcard_txns" ALTER COLUMN "txn_type" TYPE integer USING to_number(creditcard_txns.txn_type,\'S9999999\')')
-    else
-      change_column :creditcard_txns, :txn_type, :integer
-    end
+    change_column :creditcard_txns, :txn_type, :integer
   end
 
   def self.down
