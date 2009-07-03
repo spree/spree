16c9ccf1fa3cf258f763441fda46a9cd4c4e471b
Sean Schofield <schof@apache.org>
2009-07-02 23:01:55 -0400

    Improved handling of seed and fixture data with new rake db:seed task (borrowed form upcoming Rails 3.0) and rake db:etl:load and rake db:etl:dump
    [#494 state:resolved]
    [#550 state:resolved]
    [#552 state:resolved]

--- db/migrate/20080703184654_update_country_rename_name.rb
+++ db/migrate/20080703184654_update_country_rename_name.rb
@@ -4,14 +4,6 @@ class UpdateCountryRenameName < ActiveRecord::Migration
       t.rename :name, :iso_name
       t.rename :printable_name, :name
     end    
-
-    begin
-      Country.update(119, { :name => 'Macedonia' })
-      Country.update(197, { :name => 'Taiwan' })
-      Country.update(105, { :name => 'North Korea' })
-      Country.update(106, { :name => 'South Korea' })
-    rescue Exception
-    end
   end
 
   def self.down
