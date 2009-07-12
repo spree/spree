e4429399bf6ec0f3bf380f93825f9cee25476e91
Eliot Sykes <eliotsykes@gmail.com>
2009-07-12 16:21:41 +0100

    Fixed minor typo

--- lib/spree/config.rb
+++ lib/spree/config.rb
@@ -4,7 +4,7 @@ module Spree
   # Usage:
   #   Spree::Config[:foo]                  # Returns the +foo+ preference
   #   Spree::Config[]                      # Returns a Hash with all the application preferences
-  #   Spree::Config.intance                # Returns the configuration object (AppConfiguration.first)
+  #   Spree::Config.instance               # Returns the configuration object (AppConfiguration.first)
   #   Spree::Config.set(preferences_hash)  # Set the application preferences as especified in +preference_hash+
   class Config
     include Singleton
