70bfe1998cd0b7849e655b9281870f72d3d74a9a
paulcc <paulcc.two@gmail.com>
2009-07-16 10:17:32 +0100

    582 - allow login_bar to be called via https, by adding ssl_allowed
    [#582 state:resolved]

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -2,6 +2,7 @@ class UserSessionsController < Spree::BaseController
   before_filter :require_no_user, :only => [:new, :create]
   before_filter :require_user, :only => :destroy
   ssl_required :new, :create, :destroy, :update
+  ssl_allowed :login_bar
     
   def new
     @user_session = UserSession.new
