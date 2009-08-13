184d2e6c7f3697c9a953b8afb45e7ddaaa2f8d89
paulcc <paulcc.two@gmail.com>
2009-08-13 18:18:06 +0100

    600 - create backup files during spree:upgrade
    "mirror_files" has been generalised to work on single files (for consistency),
    and will create backups with a ~ suffix if passed a flag. It also only copies
    new files when the content has changed (before, it was testing identity up to
    links)
    
    in the upgrade task, the same operation is used on all things to be copied
    (consistency...), and the copying operation will create backups instead of
    silent over-writing.
    
    Note that FileUtils.cp etc have certain options hard-coded, and none of
    these is the -b option, so some DIY is needed.
    
    [#600 state:resolved]

--- lib/plugins/extension_patches/lib/asset_copy.rb
+++ lib/plugins/extension_patches/lib/asset_copy.rb
@@ -8,23 +8,36 @@ module Spree
     # if they are identical already (checked via FileUtils#identical?).
     #
     # Copyright (c) 2008 James Adam (The MIT License)
-    def self.mirror_files(source, destination)
-      return unless File.directory?(source)
     
+    # added: do mirroring with backup saving turned on
+    def self.mirror_with_backup(source, destination)
+      self.mirror_files(source, destination, true)
+    end 
+
+    def self.mirror_files(source, destination, create_backups = false)
       # TODO: use Rake::FileList#pathmap?    
-      source_files = Dir[source + "/**/*"]
-      source_dirs = source_files.select { |d| File.directory?(d) }
-      source_files -= source_dirs
+      if File.directory?(source)
+        source_files  = Dir[source + "/**/*"]
+        source_dirs   = source_files.select { |d| File.directory?(d) }
+        source_files -= source_dirs
+      elsif File.exist? source
+        source_dirs   = []
+        source_files  = [source]
+        source        = File.dirname source
+      else
+        raise "Could not mirror #{source} - entity does not exist"
+      end
     
       unless source_files.empty?
         base_target_dir = File.join(destination)
+        base_target_dir = File.dirname base_target_dir unless File.directory? base_target_dir
         FileUtils.mkdir_p(base_target_dir)
       end
     
       source_dirs.each do |dir|
         # strip down these paths so we have simple, relative paths we can
         # add to the destination
-        target_dir = File.join(destination, dir.gsub(source, ''))
+        target_dir = File.join(base_target_dir, dir.gsub(source, ''))
         begin        
           FileUtils.mkdir_p(target_dir)
         rescue Exception => e
@@ -34,8 +47,12 @@ module Spree
     
       source_files.each do |file|
         begin
-          target = File.join(destination, file.gsub(source, ''))
-          unless File.exist?(target) && FileUtils.identical?(file, target)
+          target = File.join(base_target_dir, file.gsub(source, ''))
+          unless File.exist?(target) && self.same_contents(file, target)
+            # WAS FileUtils.identical?(file, target), but we want to test contents too
+            if create_backups && File.exist?(target)
+              File.rename(target, target + '~') 
+            end
             FileUtils.cp(file, target)
           end 
         rescue Exception => e
@@ -43,6 +60,12 @@ module Spree
         end
       end  
     end   
+
+    # for windows users...
+    def self.same_contents(source,destination)
+      File.read(source) == File.read(destination)
+    end
+
   end
 end
 
--- lib/tasks/upgrade.rake
+++ lib/tasks/upgrade.rake
@@ -1,15 +1,17 @@
 namespace :spree do
-  desc "Assistance for upgrading an existing Spree deployment.  WARNING: Will replace certain javascript and stylesheet assets."
+  desc "Assistance for upgrading an existing Spree deployment.  WARNING: Will replace certain javascript and stylesheet assets, but the replaced files are saved with a ~ suffix."
   task :upgrade => :environment do
-                                   
-    Spree::FileUtilz.mirror_files "#{SPREE_ROOT}/public/javascripts", "#{RAILS_ROOT}/public/javascripts"
-    Spree::FileUtilz.mirror_files "#{SPREE_ROOT}/public/stylesheets", "#{RAILS_ROOT}/public/stylesheets"
-    Spree::FileUtilz.mirror_files "#{SPREE_ROOT}/public/images", "#{RAILS_ROOT}/public/images"
-    Spree::FileUtilz.mirror_files "#{SPREE_ROOT}/config/initializers", "#{RAILS_ROOT}/config/initializers"
-    FileUtils.cp("#{SPREE_ROOT}/config/spree_permissions.yml", "#{RAILS_ROOT}/config")
-    FileUtils.cp("#{SPREE_ROOT}/config/routes.rb", "#{RAILS_ROOT}/config")
-    FileUtils.cp("#{SPREE_ROOT}/config/boot.rb", "#{RAILS_ROOT}/config")    
     sl = "#{RAILS_ROOT}/config/initializers/searchlogic.rb"; File.rename(sl, sl + '~') if File.exist?(sl)
+    items = [ "/public/javascripts", 
+              "/public/stylesheets", 
+              "/public/images", 
+              "/config/initializers", 
+              "/config/spree_permissions.yml", 
+              "/config/routes.rb", 
+              "/config/boot.rb" ]
+    items.each do |item|
+      Spree::FileUtilz.mirror_with_backup("#{SPREE_ROOT}" + item, "#{RAILS_ROOT}" + item)
+    end
   end
 end  
 
