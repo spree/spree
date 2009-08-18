40c5598ece6b20d0f270a5fe9519c8b618054e48
Roman Smirnov <POMAHC@gmail.com>
2009-08-18 15:57:08 +0400

    jQuery.validate plugin should support i18n.
    [#667 state:resolved]

---
+++ public/javascripts/localization/messages_es.js
@@ -0,0 +1,24 @@
+/*
+ * Translated default messages for the jQuery validation plugin.
+ * Language: ES
+ * Author: David Esperalta - http://www.dec.gesbit.com/
+ */
+jQuery.extend(jQuery.validator.messages, {
+  required: "Este campo es obligatorio.",
+  remote: "Por favor, rellena esta campo.",
+  email: "Por favor, escribe una dirección de correo válida",
+  url: "Por favor, escribe una URL válida.",
+  date: "Por favor, escribe una fecha válida.",
+  dateISO: "Por favor, escribe una fecha (ISO) válida.",
+  number: "Por favor, escribe un número entero válido.",
+  digits: "Por favor, escribe sólo dígitos.",
+  creditcard: "Por favor, escribe un número de tarjeta válido.",
+  equalTo: "Por favor, escribe el mismo valor de nuevo.",
+  accept: "Por favor, escribe una valor con una extensión aceptada.",
+  maxlength: jQuery.validator.format("Por favor, no escribas más de {0} caracteres."),
+  minlength: jQuery.validator.format("Por favor, no escribas menos de {0} caracteres."),
+  rangelength: jQuery.validator.format("Por favor, escribe un valor entre {0} y {1} caracteres."),
+  range: jQuery.validator.format("Por favor, escribe un valor entre {0} y {1}."),
+  max: jQuery.validator.format("Por favor, escribe un valor igual o menor que {0}."),
+  min: jQuery.validator.format("Por favor, escribe un valor igual o mayor que {0}.")
+});
