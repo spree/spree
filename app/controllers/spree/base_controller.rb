3227f7edd9a2b3475945e5aaa6a15f4c5d773387
Sean Schofield <schof@apache.org>
2009-09-02 09:27:35 -0400

    Fixed parentheses warning.

--- app/controllers/spree/base_controller.rb
+++ app/controllers/spree/base_controller.rb
@@ -56,7 +56,7 @@ class Spree::BaseController < ApplicationController
       if self.respond_to? :object_missing
         self.object_missing(params[:id])
       else 
-        render_404(Exception.new "missing object in #{self.class.to_s}")
+        render_404 Exception.new("missing object in #{self.class.to_s}")
       end
     end
   end         
