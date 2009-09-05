feb1d57c7b3946579119bce9146a453f7c59c72d
paulcc <paulcc.two@gmail.com>
2009-09-05 10:49:32 +0100

    593 - fix broken test
    stubbing 'retrieve_on_hand' was causing later order tests to fail.
    
    the inventory unit is now built for a particular variant (to avoid
    the stub) and the test object is reloaded after the inventory change
    
    [#593 state:resolved]

--- test/unit/inventory_unit_test.rb
+++ test/unit/inventory_unit_test.rb
@@ -3,13 +3,15 @@ require 'test_helper'
 class InventoryUnitTest < ActiveSupport::TestCase
   context InventoryUnit do
     setup do
-      @inventory_unit = Factory(:inventory_unit, :state => "on_hand")
-      InventoryUnit.stub!(:retrieve_on_hand, :return => [@inventory_unit])
       line_item = Factory(:line_item)
-      @order = line_item.order.reload 
+      @inventory_unit = Factory(:inventory_unit, :state => "on_hand", :variant => line_item.variant)
+      @order = line_item.order.reload
     end
     context "when sold" do
-      setup { InventoryUnit.sell_units(@order) }
+      setup do
+        InventoryUnit.sell_units(@order)
+        @inventory_unit.reload
+      end
       should "associate the inventory units with the order" do
         assert_equal @order, @inventory_unit.order
       end
