9099453e9477f9c42a5d61cb828e3c7e2d755899
Roman Smirnov <POMAHC@gmail.com>
2009-09-12 18:23:36 +0400

    Use inheritance instead alias of method_missing. Fix broken test.
    [#717 state:resolved]

--- vendor/plugins/mail_queue/lib/mail_queue.rb
+++ vendor/plugins/mail_queue/lib/mail_queue.rb
@@ -1,21 +1,20 @@
 # MailQueue
-
+  
 module ActionMailer
-  class Base
+  class QueueMailer < Base
     
     class << self
-      alias_method :orig_method_missing, :method_missing
     
       def method_missing(method_symbol, *parameters)#:nodoc:
         case method_symbol.id2name
-          when /^deliver_([_a-z]\w*)\!/ then orig_method_missing(method_symbol, *parameters)
+          when /^deliver_([_a-z]\w*)\!/ then super(method_symbol, *parameters)
           when /^deliver_([_a-z]\w*)/ then 
             if Spree::Config[:use_mail_queue] # only use the queue if it's been enabled
               queue_mail($1, *parameters)
             else
-              orig_method_missing(method_symbol, *parameters)
+              super(method_symbol, *parameters)
             end
-          else orig_method_missing(method_symbol, *parameters)
+          else super(method_symbol, *parameters)
         end
       end
     
@@ -33,8 +32,7 @@ end
 
 
 class MailQueue < ActiveRecord::Base
-  
-  
+   
   def MailQueue.process
     
     for qmail in QueuedMail.find(:all)
