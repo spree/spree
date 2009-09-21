a50475d8ebbc28b92f38db0928f37de68981262a
paulcc <paulcc.two@gmail.com>
2009-09-21 20:15:54 +0100

    re-connected special instructions handling
    the input form was sending the info to the wrong place, and the admin
    controller now needs to get/set from the checkout value

--- app/controllers/admin/shipments_controller.rb
+++ app/controllers/admin/shipments_controller.rb
@@ -10,12 +10,12 @@ class Admin::ShipmentsController < Admin::BaseController
   end
 
   edit.before do # copy into instance variable before editing
-    @shipment.special_instructions = @order.special_instructions
+    @shipment.special_instructions = @order.checkout.special_instructions
   end
 
   update.after do # copy back to order if instructions are enabled
     if Spree::Config[:shipping_instructions]
-      @order.special_instructions = object_params[:special_instructions] 
+      @order.checkout.special_instructions = object_params[:special_instructions] 
       @order.save
     end
   end
