4d188745784728e4f72d17132c7c4902a0401b3b
Sean Schofield <sean@railsdog.com>
2009-09-22 10:34:18 -0400

    Renamed application controller from application.rb
    [768 state:resolved]

--- vendor/extensions/payment_gateway/payment_gateway_extension.rb
+++ vendor/extensions/payment_gateway/payment_gateway_extension.rb
@@ -1,5 +1,5 @@
 # Uncomment this if you reference any of your controllers in activate
-require_dependency 'application'
+require_dependency 'application_controller'
 
 class PaymentGatewayExtension < Spree::Extension
   version "1.0"
