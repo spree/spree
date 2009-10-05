b2506e881adef5296895f8e5f11dac5de73104cd
Jorge Calás Lozano <calas@qvitta.net>
2009-10-05 14:07:43 +0200

    Translation of helpers and confirmation messages
    [#637]

--- app/helpers/admin/orders_helper.rb
+++ app/helpers/admin/orders_helper.rb
@@ -1,20 +1,21 @@
 module Admin::OrdersHelper
-  
+
   # Renders all the txn partials that may have been specified in the extensions
   def render_txn_partials(order)
     @txn_partials.inject("") do |extras, partial|
       extras += render :partial => partial, :locals => {:payment => order}
     end
   end
-  
+
   # Renders all the extension partials that may have been specified in the extensions
   def event_links
     links = []
-    @order_events.sort.each do |event| 
-      links << (button_link_to t(event), fire_admin_order_url(@order, :e => event), {:method => :put, :confirm => "Are you sure you want to #{event}?"}) if @order.send("can_#{event}?")
+    @order_events.sort.each do |event|
+      if @order.send("can_#{event}?")
+        links << button_link_to(t(event), fire_admin_order_url(@order, :e => event),
+                                { :method => :put, :confirm => t("order_sure_want_to", :event => t(event)) })
+      end
     end
-    return "" if links.empty?
-    links.join(' &nbsp;')    
+    links.join('&nbsp;')
   end
-  
 end
--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,5 +1,5 @@
 module ProductsHelper
-  # returns the formatted change in price (from the master price) for the specified variant (or simply return 
+  # returns the formatted change in price (from the master price) for the specified variant (or simply return
   # the variant price if no master price was supplied)
   def variant_price_diff(variant)
     return product_price(variant) unless variant.product.master.price
@@ -11,7 +11,7 @@ module ProductsHelper
       "(#{t("subtract")}: #{format_price diff.abs})"
     end
   end
-  
+
   # returns the price of the product to show for display purposes
   def product_price(product_or_variant, options={})
     options.assert_valid_keys(:format_as_currency, :show_vat_text)
@@ -21,23 +21,28 @@ module ProductsHelper
     amount += Calculator::Vat.calculate_tax_on(product_or_variant) if Spree::Config[:show_price_inc_vat]
     options.delete(:format_as_currency) ? format_price(amount, options) : ("%0.2f" % amount).to_f
   end
-  
+
   def format_price(price, options={})
     options.assert_valid_keys(:show_vat_text)
     options.reverse_merge! :show_vat_text => Spree::Config[:show_price_inc_vat]
-    options[:show_vat_text]  ?  number_to_currency(price) + ' (inc. VAT)' : number_to_currency(price)
+    formatted_price = number_to_currency(price)
+    if options[:show_vat_text]
+      I18n.t(:price_with_vat_included, :price => formatted_price)
+    else
+      formatted_price
+    end
   end
-    
+
   # converts line breaks in product description into <p> tags (for html display purposes)
   def product_description(product)
     product.description.gsub(/^(.*)$/, '<p>\1</p>')
-  end  
-  
+  end
+
   # generates nested url to product based on supplied taxon
   def seo_url(taxon, product = nil)
     return '/t/' + taxon.permalink if product.nil?
-    
+
     '/t/' + taxon.permalink + "p/" + product.permalink
   end
-  
+
 end
