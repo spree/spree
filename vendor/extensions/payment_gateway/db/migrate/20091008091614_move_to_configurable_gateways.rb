2d557b1c1817043bf0838cd12d521438ea6edec4
Jorge Calás Lozano <calas@qvitta.net>
2009-10-13 03:02:20 +0200

    826 - Fix typo in payment gateway migration
    [#826]

--- vendor/extensions/payment_gateway/db/migrate/20091008091614_move_to_configurable_gateways.rb
+++ vendor/extensions/payment_gateway/db/migrate/20091008091614_move_to_configurable_gateways.rb
@@ -1,30 +1,30 @@
 class MoveToConfigurableGateways < ActiveRecord::Migration
   def self.up
-		drop_table :gateways
-		drop_table :gateway_options
-		drop_table :gateway_option_values
-		drop_table :gateway_configurations
-		
-		create_table :gateways, :force => true do |t|
-		  t.string :type
-			t.string :name
-			t.text :description
-			t.boolean :active, :default => true
-			t.string :environment, :default => "development"
-			t.string :server, :default => "test"
-			t.boolean :test_mode, :default => "true"
-		  t.timestamps
-		end
+    drop_table :gateways
+    drop_table :gateway_options
+    drop_table :gateway_option_values
+    drop_table :gateway_configurations
+
+    create_table :gateways, :force => true do |t|
+      t.string :type
+      t.string :name
+      t.text :description
+      t.boolean :active, :default => true
+      t.string :environment, :default => "development"
+      t.string :server, :default => "test"
+      t.boolean :test_mode, :default => true
+      t.timestamps
+    end
   end
 
   def self.down
-		drop_table :gateways
+    drop_table :gateways
     create_table "gateway_configurations", :force => true do |t|
       t.integer  "gateway_id"
       t.datetime "created_at"
       t.datetime "updated_at"
     end
-    
+
     create_table "gateway_option_values", :force => true do |t|
       t.integer  "gateway_configuration_id"
       t.integer  "gateway_option_id"
@@ -32,7 +32,7 @@ class MoveToConfigurableGateways < ActiveRecord::Migration
       t.datetime "created_at"
       t.datetime "updated_at"
     end
-    
+
     create_table "gateway_options", :force => true do |t|
       t.string   "name"
       t.text     "description"
@@ -41,7 +41,7 @@ class MoveToConfigurableGateways < ActiveRecord::Migration
       t.datetime "created_at"
       t.datetime "updated_at"
     end
-    
+
     create_table "gateways", :force => true do |t|
       t.string   "clazz"
       t.string   "name"
@@ -50,6 +50,6 @@ class MoveToConfigurableGateways < ActiveRecord::Migration
       t.datetime "created_at"
       t.datetime "updated_at"
     end
-    
+
   end
 end
