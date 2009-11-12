707f1e53405bd87c51f494fc60d004c86f6b2564
Roman Smirnov <roman@railsdog.com>
2009-11-12 23:38:23 +0300

    Autocomplete with Rails helper replaced by jQuery.AutoComplete
    [#859 state:resolved]

--- app/controllers/admin/product_properties_controller.rb
+++ app/controllers/admin/product_properties_controller.rb
@@ -1,7 +1,14 @@
 class Admin::ProductPropertiesController < Admin::BaseController
   resource_controller
+  before_filter :find_properties
   
   # note: we're using attribute_fu to manage the product_properties so the products controller will be 
   # doing most of the work  
   belongs_to :product
+  
+  private
+  
+  def find_properties
+    @properties = Property.all.map(&:name).join(" ")
+  end
 end
