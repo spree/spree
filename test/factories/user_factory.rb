60103f8889c957f2a1c2f31bf29df947a1f71763
Sean Schofield <sean@railsdog.com>
2009-11-13 09:55:44 -0500

    Fixed problem with periodic test failure due to short user_names.
    [#894 state:resolved]

--- test/factories/user_factory.rb
+++ test/factories/user_factory.rb
@@ -1,7 +1,6 @@
-srand(Time.now.to_i)
 Factory.define(:user) do |record|
   record.email { Faker::Internet.email }
-  record.login { Faker::Internet.user_name }
+  record.login { Factory.next(:login) }
   record.password "spree"
   record.password_confirmation "spree"
 
@@ -9,9 +8,12 @@ Factory.define(:user) do |record|
   #record.ship_address { Factory(:address) }
 end
 
+Factory.sequence :login do |n|
+  Faker::Internet.user_name + n.to_s
+end
+
 ###### ADD YOUR CODE BELOW THIS LINE #####
 
 Factory.define(:admin_user, :parent => :user) do |u|
   u.roles { [Role.find_by_name("admin") || Factory(:admin_role)]}
 end
-
