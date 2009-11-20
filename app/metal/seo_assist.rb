19e0e12fbc3437fdb9089f9e6d70368eaeb1f064
Roman Smirnov <roman@railsdog.com>
2009-11-20 23:29:29 +0300

    Fixed restoration of get-parameters in SEO assist metal.
    [#912 state:resolved]

--- app/metal/seo_assist.rb
+++ app/metal/seo_assist.rb
@@ -7,13 +7,25 @@ class SeoAssist
   def self.call(env)
     request = Rack::Request.new(env)
     params = request.params
-    taxon_id = params['taxon']      
+    taxon_id = params['taxon']
     if !taxon_id.blank? && !taxon_id.is_a?(Hash) && @taxon = Taxon.find(taxon_id)
       params.delete('taxon')
-      get_query = params.map{|k,v| "#{k}=#{v}"}.join('&')
-      return [301, {'Location'=> "/t/#{@taxon.permalink}?#{get_query}" }, []]
+      query = build_query(params)
+      return [301, { 'Location'=> "/t/#{@taxon.permalink}?#{query}" }, []]
     end
     [404, {"Content-Type" => "text/html"}, "Not Found"]
+  end
+  
+  private
+  
+  def self.build_query(params)
+    params.map { |k, v|
+      if v.class == Array
+        build_query(v.map { |x| ["#{k}[]", x] })
+      else
+        k + "=" + Rack::Utils.escape(v)
+      end
+    }.join("&")
   end
     
 end
