1c05e90ef900a8a8c486b484bd86b0b6861fba13
Roman Smirnov <roman@railsdog.com>
2009-11-21 19:55:17 +0300

    Allowed to manage pagination from search extension.
    [#915 state:resolved]

--- lib/spree/search.rb
+++ lib/spree/search.rb
@@ -5,7 +5,9 @@ module Spree::Search
     # add taxon id to params for searcher
     params[:taxon] = @taxon.id if @taxon
     @keywords = params[:keywords]
-
+    per_page = params[:per_page] || Spree::Config[:products_per_page]
+    params[:per_page] = per_page
+    curr_page = Spree::Config.searcher.manage_pagination ? 1 : params[:page]
     # Prepare a search within the parameters
     Spree::Config.searcher.prepare(params)
 
@@ -16,20 +18,20 @@ module Spree::Search
     else
       @product_group = ProductGroup.new
     end
-    
+
     @product_group.add_scope('in_taxon', @taxon) unless @taxon.blank?
     @product_group.add_scope('keywords', @keywords) unless @keywords.blank?
     @product_group = @product_group.from_search(params[:search]) if params[:search]
     
-    params[:search] = @product_group.scopes_to_hash
+    params[:search] = @product_group.scopes_to_hash if @keywords.blank?
 
     base_scope = Spree::Config[:allow_backorders] ? Product.active : Product.active.on_hand
     @products_scope = @product_group.apply_on(base_scope)
 
     @products = @products_scope.paginate({
-        :include  => [:images, {:variants => :images}],
-        :per_page => params[:per_page],
-        :page     => params[:page],
+        :include  => [:images, :master],
+        :per_page => per_page,
+        :page     => curr_page
       })
     @products_count = @products_scope.count
 
