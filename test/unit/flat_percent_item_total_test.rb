1d73040727265fd7709a00ceffba5caea90f9fe1
Roman Smirnov <roman@railsdog.com>
2009-11-25 21:32:15 +0300

    Rewritten outdated test.
    [#814]

--- test/unit/flat_percent_item_total_test.rb
+++ test/unit/flat_percent_item_total_test.rb
@@ -14,13 +14,14 @@ class FlatPercentItemTotalTest < ActiveSupport::TestCase
     context "compute" do
       setup do
         @order = Factory(:order)
-        @order.item_total = 123
+        @order.line_items << Factory(:line_item, :price => 50, :quantity => 2)
+        @order.line_items << Factory(:line_item, :price => 23, :quantity => 1)
         @calculator = Calculator::FlatPercentItemTotal.new(:preferred_flat_percent => 10)
       end
 
       context "apply the percentage rate" do
         should "compute ten percent" do
-          assert_equal((123 * 10 / 100.0), @calculator.compute(@order))
+          assert_equal((123 * 10 / 100.0), @calculator.compute(@order.line_items))
         end
       end       
     end
