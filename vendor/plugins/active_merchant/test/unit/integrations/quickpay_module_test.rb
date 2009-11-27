c54dc6e1e57045405c1bc390cd96440cf2f2c7a9
Roman Smirnov <roman@railsdog.com>
2009-11-28 00:21:50 +0300

    Removed active_merchant from plugins. It should be installed manually, if you want use Ruby 1.9.x until ActiveMerchant 1.4.3 not released.

--- vendor/plugins/active_merchant/test/unit/integrations/quickpay_module_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class QuickpayModuleTest < Test::Unit::TestCase
-  include ActiveMerchant::Billing::Integrations
-  
-  def test_notification_method
-    assert_instance_of Quickpay::Notification, Quickpay.notification('name=cody')
-  end
-end 
