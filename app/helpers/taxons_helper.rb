7d8a5ab1e4f62fc92eb0e3cff41e6008f363dce4
Roman Smirnov <roman@railsdog.com>
2009-11-27 23:20:05 +0300

    Reduced calls count of mb_chars

--- app/helpers/taxons_helper.rb
+++ app/helpers/taxons_helper.rb
@@ -1,15 +1,15 @@
 module TaxonsHelper
   def breadcrumbs(taxon, separator="&nbsp;&raquo;&nbsp;")
     return "" if current_page?("/")
-    crumbs = [content_tag(:li, link_to(t(:home) , root_path) + separator).mb_chars]
+    crumbs = [content_tag(:li, link_to(t(:home) , root_path) + separator)]
     if taxon
       crumbs << content_tag(:li, link_to(t('products') , products_path) + separator)
       crumbs << taxon.ancestors.collect { |ancestor| content_tag(:li, link_to(ancestor.name , seo_url(ancestor)) + separator) } unless taxon.ancestors.empty?
       crumbs << content_tag(:li, content_tag(:span, taxon.name))
     else
-      crumbs << content_tag(:li, content_tag(:span, t('products'))).mb_chars
+      crumbs << content_tag(:li, content_tag(:span, t('products')))
     end
-    crumb_list = content_tag(:ul, crumbs.join)
+    crumb_list = content_tag(:ul, crumbs.flatten.map{|li| li.mb_chars}.join)
     content_tag(:div, crumb_list + tag(:br, {:class => 'clear'}, false, true), :class => 'breadcrumbs')
   end
 
