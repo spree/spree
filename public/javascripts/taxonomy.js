4c3aeac0bd4ad3fdd3f64cd5f66f70bc53f4a693
Brian Quinn <briandquinn@gmail.com>
2009-11-30 15:21:33 +0000

    Fixes IE8 bug when editing taxonomies
    [#927 state:resolved]

--- public/javascripts/taxonomy.js
+++ public/javascripts/taxonomy.js
@@ -5,17 +5,17 @@ var delete_confirmed = false;
 var last_rollback = null;
 
 var handle_ajax_error = function(XMLHttpRequest, textStatus, errorThrown){
-	jQuery.tree.rollback(last_rollback);
+  jQuery.tree.rollback(last_rollback);
 
-	jQuery("#ajax_error").show().html("<strong>" + server_error + "</strong><br/>" + taxonomy_tree_error);
+  jQuery("#ajax_error").show().html("<strong>" + server_error + "</strong><br/>" + taxonomy_tree_error);
 };
 
 var handle_move = function(li, target, droppped, tree, rb) {
-	last_rollback = rb;
+  last_rollback = rb;
   var position = jQuery(li).prevAll().length;
 
   var parent = -1;
-  
+
   if(droppped=='inside'){
     parent = target;
   }else if(droppped=='after'){
@@ -23,14 +23,14 @@ var handle_move = function(li, target, droppped, tree, rb) {
   }else if(droppped=='before'){
     parent = jQuery(target).parents()[1];
   }
- 
+
   jQuery.ajax({
     type: "POST",
     url: base_url + li.id,
     data: ({_method: "put", "taxon[parent_id]": parent.id, "taxon[position]": position, authenticity_token: AUTH_TOKEN}),
     error: handle_ajax_error
   });
-        
+
   return true
 };
 
@@ -39,152 +39,152 @@ var handle_dblclick = function(li, tree) {
 };
 
 var handle_create = function(parent, sib, created, tree, rb){
-	last_rollback = rb;
-	creating=true;	
+  last_rollback = rb;
+  creating=true;
 };
 
 var handle_created = function(id,result) {
-	jQuery.tree.reference('taxonomy_tree').selected.attr('id', id);
+  jQuery.tree.reference('taxonomy_tree').selected.attr('id', id);
 }
 
 var handle_rename = function(li, tree, rb) {
-  var name = jQuery(li).children()[0].textContent;
+  var name = jQuery(li).children().text();
   name = jQuery.trim(name);
 
-	if (creating){
-		//actually creating new
-		var position = jQuery(li).prevAll().length;
-		var parent = jQuery(li).parents()[1];
-	  
-		jQuery.ajax({
-	    type: "POST",
-	    url: base_url,
-	    data: ({"taxon[name]": name, "taxon[parent_id]": parent.id, "taxon[position]": position, authenticity_token: AUTH_TOKEN}),
-			error: handle_ajax_error,
-	  	success: handle_created
-	  });	
-	
-		creating = false;
-	}else{
-		//just renaming
-		last_rollback = rb;
-		
-		jQuery.ajax({
-			type: "POST",
-			url: base_url + li.id,
-			data: ({_method: "put", "taxon[name]": name, authenticity_token: AUTH_TOKEN}),
-			error: handle_ajax_error   
-		});
-	}
+  if (creating){
+    //actually creating new
+    var position = jQuery(li).prevAll().length;
+    var parent = jQuery(li).parents()[1];
+
+    jQuery.ajax({
+      type: "POST",
+      url: base_url,
+      data: ({"taxon[name]": name, "taxon[parent_id]": parent.id, "taxon[position]": position, authenticity_token: AUTH_TOKEN}),
+      error: handle_ajax_error,
+      success: handle_created
+    });
+
+    creating = false;
+  }else{
+    //just renaming
+    last_rollback = rb;
+
+    jQuery.ajax({
+      type: "POST",
+      url: base_url + li.id,
+      data: ({_method: "put", "taxon[name]": name, authenticity_token: AUTH_TOKEN}),
+      error: handle_ajax_error
+    });
+  }
 };
 
 var handle_before_delete = function(li){
-	if (!delete_confirmed){
-		jConfirm('Are you sure you want to delete this taxon?', 'Confirm Taxon Deletion', function(r) {
-			if(r){
-				delete_confirmed = true;
-				jQuery.tree.reference('taxonomy_tree').remove(li);
-			}
-		});
-	}
-	
-	return delete_confirmed;
+  if (!delete_confirmed){
+    jConfirm('Are you sure you want to delete this taxon?', 'Confirm Taxon Deletion', function(r) {
+      if(r){
+        delete_confirmed = true;
+        jQuery.tree.reference('taxonomy_tree').remove(li);
+      }
+    });
+  }
+
+  return delete_confirmed;
 };
 
 var handle_delete = function(li, tree, rb){
-	last_rollback = rb;
-		
-	jQuery.ajax({
+  last_rollback = rb;
+
+  jQuery.ajax({
     type: "POST",
     url: base_url + li.id,
     data: ({_method: "delete", authenticity_token: AUTH_TOKEN}),
-		error: handle_ajax_error		
+    error: handle_ajax_error
   });
 
-	delete_confirmed = false;
+  delete_confirmed = false;
 };
 
 jQuery(document).ready(function(){
-	conf = { 
-	  data : { 
-			type : "json",
-			async : true,
-			opts : {
-				method : "GET",
-				url : child_url
-			}
-		},
-		ui : {
-			theme_name : "apple"
-		},
-		lang : {
-		    new_node    : new_taxon,
-		    loading     : loading + "..."
-	  	},
+  conf = {
+    data : {
+      type : "json",
+      async : true,
+      opts : {
+        method : "GET",
+        url : child_url
+      }
+    },
+    ui : {
+      theme_name : "apple"
+    },
+    lang : {
+        new_node    : new_taxon,
+        loading     : loading + "..."
+      },
     plugins : {
-      contextmenu : { 
+      contextmenu : {
         items : {
-        	// get rid of the remove item
-        	remove :{
-        	  visible : function (NODE, TREE_OBJ) { if(jQuery(NODE[0]).attr('rel')=="root") return false; return TREE_OBJ.check("renameable", NODE); },
-        	},
-        	rename :{
-        	  visible : function (NODE, TREE_OBJ) { if(jQuery(NODE[0]).attr('rel')=="root") return false; return TREE_OBJ.check("renameable", NODE); },
-        	},
-	        cut :{ 
-	            id      : "cut",
-	            label   : "Cut",
-	            visible : function (NODE, TREE_OBJ) { if(NODE.length != 1 || NODE[0].id == 'root') return false; return true; }, 
-	            action  : function (NODE, TREE_OBJ) { TREE_OBJ.cut(NODE); jQuery(NODE).hide(); },
-							separator_before : true
-	        },
-	        paste :{ 
-	            id      : "paste",
-	            label   : "Paste",
-	            visible : function (NODE, TREE_OBJ) { if(NODE.length != 1 || NODE[0].id == 'root') return false; return true; }, 
-	            action  : function (NODE, TREE_OBJ) { TREE_OBJ.open_branch(NODE); TREE_OBJ.paste(NODE, "inside"); jQuery(NODE).find("li").show(); } 
-	        }
+          // get rid of the remove item
+          remove :{
+            visible : function (NODE, TREE_OBJ) { if(jQuery(NODE[0]).attr('rel')=="root") return false; return TREE_OBJ.check("renameable", NODE); },
+          },
+          rename :{
+            visible : function (NODE, TREE_OBJ) { if(jQuery(NODE[0]).attr('rel')=="root") return false; return TREE_OBJ.check("renameable", NODE); },
+          },
+          cut :{
+              id      : "cut",
+              label   : "Cut",
+              visible : function (NODE, TREE_OBJ) { if(NODE.length != 1 || NODE[0].id == 'root') return false; return true; },
+              action  : function (NODE, TREE_OBJ) { TREE_OBJ.cut(NODE); jQuery(NODE).hide(); },
+              separator_before : true
+          },
+          paste :{
+              id      : "paste",
+              label   : "Paste",
+              visible : function (NODE, TREE_OBJ) { if(NODE.length != 1 || NODE[0].id == 'root') return false; return true; },
+              action  : function (NODE, TREE_OBJ) { TREE_OBJ.open_branch(NODE); TREE_OBJ.paste(NODE, "inside"); jQuery(NODE).find("li").show(); }
+          }
 
         }
       }
     },
     rules : {
-			// only nodes of type root can be top level nodes
-			valid_children : [ "root" ]
-		},
-		types : {
-			// all node types inherit the "default" node type
-			"taxon" : {},
-			"root" : {
-			  deletable : false,
-				renameable : false,
-				draggable : false,
-				valid_children : [ "taxon" ]
-			}
+      // only nodes of type root can be top level nodes
+      valid_children : [ "root" ]
+    },
+    types : {
+      // all node types inherit the "default" node type
+      "taxon" : {},
+      "root" : {
+        deletable : false,
+        renameable : false,
+        draggable : false,
+        valid_children : [ "taxon" ]
+      }
     },
     callback : {
       onmove: handle_move,
       ondblclk: handle_dblclick,
       onrename: handle_rename,
-  		oncreate: handle_create,
-  		beforedelete: handle_before_delete,
-  		ondelete: handle_delete,
-  		beforedata: function (n, t) { 
-				if(n == false) t.settings.data.opts.static = initial; 
-				else t.settings.data.opts.static = false; 
+      oncreate: handle_create,
+      beforedelete: handle_before_delete,
+      ondelete: handle_delete,
+      beforedata: function (n, t) {
+        if(n == false) t.settings.data.opts.static = initial;
+        else t.settings.data.opts.static = false;
 
-				return { parent_id : $(n).attr("id") || 0 };
-				}
+        return { parent_id : $(n).attr("id") || 0 };
+        }
 
-    }   
+    }
   }
-  
+
   jQuery("#taxonomy_tree").tree(conf);
-  
-	jQuery(document).keypress(function(e){
+
+  jQuery(document).keypress(function(e){
     //surpress form submit on enter/return
     if (e.keyCode == 13){
         e.preventDefault();
-    } 
+    }
   });
 });
