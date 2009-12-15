755367b8efde89931bb97a3bb26dbf45885f958b
Roman Smirnov <roman@railsdog.com>
2009-12-15 18:42:33 +0300

    List of states does not updated when user changes country.
    [#968 state:resolved]

--- vendor/extensions/theme_default/public/javascripts/checkout.js
+++ vendor/extensions/theme_default/public/javascripts/checkout.js
@@ -1,13 +1,51 @@
 jQuery(document).ready(function($){
   
   $('input#checkout_use_billing').click(function() {
-    if(this.checked) {
-      $('#shipping .inner').hide();
-      $('#shipping .inner input').attr('disabled', 'disabled');
-    } else {
+    show_billing(!this.checked);
+  });
+
+  var show_billing = function(show) {   
+    if(show) {
       $('#shipping .inner').show();
       $('#shipping .inner input').removeAttr('disabled', 'disabled');
+    } else {
+      $('#shipping .inner').hide();
+      $('#shipping .inner input').attr('disabled', 'disabled');
     }
+  }
+
+  var update_state = function(region) {
+    var country        = $('span#' + region + 'country :only-child').val();
+    var states         = state_mapper[country];
+
+    var state_select = $('span#' + region + 'state select');
+    var state_input = $('span#' + region + 'state input');
+
+    if(states) {
+      state_select.html('');
+      var states_with_blank = [["",""]].concat(states);
+      $.each(states_with_blank, function(pos,id_nm) {
+        var opt = $(document.createElement('option'))
+                  .attr('value', id_nm[0])
+                  .html(id_nm[1]);
+        state_select.append(opt);
+      });
+      state_select.removeAttr('disabled').show();;
+      state_input.hide().attr('disabled', 'disabled');
+
+    } else {
+      state_input.removeAttr('disabled').show();
+      state_select.hide().attr('disabled', 'disabled');
+    }
+
+  };
+
+  jQuery(document).ready(function(){
+    $('span#bcountry select').change(function() { update_state('b'); });
+    $('span#scountry select').change(function() { update_state('s'); });
+    show_billing(!$('input#checkout_use_billing').attr('checked'));
+    update_state('b');
+    update_state('s');
   });
   
 });
