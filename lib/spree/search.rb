4ab5f8b1976c2df14e5d738aa9abfe2f78626fe0
Roman Smirnov <roman@railsdog.com>
2009-12-21 23:13:49 +0300

    Verify that params[:page] and params[:per_page] are an integer. Fix bug in managment of pagination by some search-extension.
    [#1004 state:resolved]

--- lib/spree/search.rb
+++ lib/spree/search.rb
@@ -5,9 +5,12 @@ module Spree::Search
     # add taxon id to params for searcher
     params[:taxon] = @taxon.id if @taxon
     @keywords = params[:keywords]
-    per_page = params[:per_page] || Spree::Config[:products_per_page]
+    
+    per_page = params[:per_page].to_i
+    per_page = per_page > 0 ? per_page : Spree::Config[:products_per_page]
     params[:per_page] = per_page
-    curr_page = Spree::Config.searcher.manage_pagination ? 1 : params[:page]
+    params[:page] = 1 if (params[:page].to_i <= 0)
+    
     # Prepare a search within the parameters
     Spree::Config.searcher.prepare(params)
 
@@ -28,6 +31,7 @@ module Spree::Search
     base_scope = Spree::Config[:allow_backorders] ? Product.active : Product.active.on_hand
     @products_scope = @product_group.apply_on(base_scope)
 
+    curr_page = Spree::Config.searcher.manage_pagination ? 1 : params[:page]
     @products = @products_scope.paginate({
         :include  => [:images, :master],
         :per_page => per_page,
