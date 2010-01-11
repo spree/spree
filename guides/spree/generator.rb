ae1b22ea0b90747811f64b7490a9f2a6ca51f4f6
Sean Schofield <schof@apache.org>
2010-01-10 22:43:09 -0500

    Additional GA fix

--- guides/spree/generator.rb
+++ guides/spree/generator.rb
@@ -86,7 +86,7 @@ module Spree
       	document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
       </script>
       <script type="text/javascript">
-      	var pageTracker = _gat._getTracker(#{ENV['ANALYTICS_ID']});
+      	var pageTracker = _gat._getTracker("#{ENV['ANALYTICS_ID']}");
       	pageTracker._initData();
       	pageTracker._trackPageview();
       </script>      
