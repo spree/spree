29efb74564470ee01fe82c1a4c1757b13ef3306a
Sean Schofield <schof@apache.org>
2010-01-26 08:00:56 -0500

    Fix XSS security vulnerability in changing of locale.  Thanks to Alexander Kozliakov for reporting.
    [#274 state:resolved]

--- vendor/extensions/localization/app/controllers/locale_controller.rb
+++ vendor/extensions/localization/app/controllers/locale_controller.rb
@@ -1,10 +1,12 @@
 class LocaleController < ApplicationController
 
   def set
-    if params[:locale]
+    if params[:locale] && AVAILABLE_LOCALES.include?(params[:locale])
       I18n.locale = params[:locale]
       session[:locale] = params[:locale]
       flash[:notice] = t("locale_changed")
+    else
+      flash[:error] = t("locale_not_changed")
     end
     redirect_back_or_default(root_path)
   end
