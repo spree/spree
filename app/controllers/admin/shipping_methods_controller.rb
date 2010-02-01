ba8bfad6157ddd7cd6f9179949cc23b100495e44
Brian Quinn <briandquinn@gmail.com>
2010-02-01 17:29:17 +0000

    Shipping Methods admin UI cleanup.
    [#1105 state:resolved]

--- app/controllers/admin/shipping_methods_controller.rb
+++ app/controllers/admin/shipping_methods_controller.rb
@@ -1,20 +1,21 @@
-class Admin::ShippingMethodsController < Admin::BaseController    
+class Admin::ShippingMethodsController < Admin::BaseController
   resource_controller
   before_filter :load_data
-  
+
   update.wants.html { redirect_to edit_object_url }
   create.wants.html { redirect_to edit_object_url }
   destroy.success.wants.js { render_js_for_destroy }
-  
-  private       
+
+  private
   def build_object
     @object ||= end_of_association_chain.send((parent? ? :build : :new), object_params)
     @object.calculator = params[:shipping_method][:calculator_type].constantize.new if params[:shipping_method]
     @object
   end
-  
+
   def load_data
-    @available_zones = Zone.find :all, :order => :name                      
+    @available_zones = Zone.find :all, :order => :name
     @calculators = ShippingMethod.calculators
-  end    
+    @calculators.sort_by(&:name)
+  end
 end
