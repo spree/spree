5cd2aa9edc1b5d2d53ece70f615a7639f474379a
Scott Jacobsen <scott@mobilefoundry.net>
2010-02-11 12:55:34 -0700

    Updated template to properly inherit from ActionController::TestCase. Also removed the setup function since that is no longer needed.
    [#1143 state:resolved]

--- lib/generators/extension_controller/templates/functional_test.rb
+++ lib/generators/extension_controller/templates/functional_test.rb
@@ -3,12 +3,7 @@ require File.dirname(__FILE__) + '<%= '/..' * class_nesting_depth %>/../test_hel
 # Re-raise errors caught by the controller.
 <%= class_name %>Controller.class_eval { def rescue_action(e) raise e end }
 
-class <%= class_name %>ControllerTest < Test::Unit::TestCase
-  def setup
-    @controller = <%= class_name %>Controller.new
-    @request    = ActionController::TestRequest.new
-    @response   = ActionController::TestResponse.new
-  end
+class <%= class_name %>ControllerTest < ActionController::TestCase
 
   # Replace this with your real tests.
   def test_truth
