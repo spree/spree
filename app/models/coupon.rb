f68c28115ff2f9ec2001b562fb0515bf4adedcea
Roman Smirnov <roman@railsdog.com>
2010-02-17 15:47:48 +0300

    Ensure that coupon_credit have order and don't apply coupon to order, if it already applied.
    [#836 state:resolved]

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -7,8 +7,9 @@ class Coupon < ActiveRecord::Base
   
   def eligible?(order)
     return false if expires_at and Time.now > expires_at
-    return false if usage_limit and coupon_credits.count >= usage_limit
+    return false if usage_limit and coupon_credits.with_order.count >= usage_limit
     return false if starts_at and Time.now < starts_at
+    return false if order.coupon_credits.detect { |credit| credit.adjustment_source_id == self.id }
     # TODO - also check items in the order (once we support product groups for coupons)
     true
   end
