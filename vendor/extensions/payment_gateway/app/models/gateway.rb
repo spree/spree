dbefd2e1c7a4970bd9993bd1272317cc345efa45
Roman Smirnov <roman@railsdog.com>
2010-02-22 16:06:38 +0300

    Fix few issues in Gateway model. For details see http://railsdog.lighthouseapp.com/projects/31096-spree/tickets/905 Special thanks to Eliot Sykes.
    [#905 state:resolved]

--- vendor/extensions/payment_gateway/app/models/gateway.rb
+++ vendor/extensions/payment_gateway/app/models/gateway.rb
@@ -1,14 +1,14 @@
 class Gateway < ActiveRecord::Base
-	delegate_belongs_to :provider, :authorize, :purchase, :capture, :void, :credit
-	
-	validates_presence_of :name, :type
+  delegate_belongs_to :provider, :authorize, :purchase, :capture, :void, :credit
+  
+  validates_presence_of :name, :type
 
   # instantiates the selected gateway and configures with the options stored in the database
   def self.current
     Gateway.find(:first, :conditions => {:active => true, :environment => ENV['RAILS_ENV']}) 
-	end
-	
-	@provier = nil
+  end
+  
+  @provider = nil
   @@providers = Set.new
   def self.register
     @@providers.add(self)
@@ -29,12 +29,12 @@ class Gateway < ActiveRecord::Base
     @provider ||= provider_class.new(gateway_options)
   end 
  
-	def validate
-		errors.add_to_base I18n.translate("only_one_active_gateway_per_environment") if self.active && Gateway.exists?(["active = ? AND environment = ? AND id <> ?" , true, self.environment, self.id])
-	end
+  def validate
+    errors.add_to_base I18n.translate("only_one_active_gateway_per_environment") if self.active && Gateway.exists?(["active = ? AND environment = ? AND id <> ?" , true, self.environment, self.id])
+  end
  
-	def options
-	  #self.respond_to? :preferences ? self.preferences : {}
+  def options
+    #self.respond_to? :preferences ? self.preferences : {}
     options_hash = {}
     self.preferences.each do |key,value| 
       #next false if value.nil? || value.empty?    
@@ -43,18 +43,19 @@ class Gateway < ActiveRecord::Base
     options_hash
     # 
     # options.length == preferences.length ? options : nil
-	end
-	
-	def method_missing(method, *args)
-	 	if @provider.nil?
-			super
-		else
-			@provider.respond_to?(method) ? provider.send(method) : super
-		end
-	end
-	
-	def payment_profiles_supported?
-	  false
+  end
+  
+  def method_missing(method, *args)
+    @provider ||= provider
+     if @provider.nil?
+      super
+    else
+      @provider.respond_to?(method) ? provider.send(method, args) : super
+    end
+  end
+  
+  def payment_profiles_supported?
+    false
   end
   
 end
