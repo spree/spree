4f516c869bb9e71144605cc131617aa0ec47bc3b
Sean Schofield <sean@railsdog.com>
2010-02-23 15:00:34 -0500

    Provide a new theme generator with precompiled screen.css based on the Less.  Eventually we should provide some nice rake task to help extension developers compile their Less (while overriding select pieces in extensions if desired.)
    [#1163 state:resolved]

--- vendor/plugins/more/config/routes.rb
+++
@@ -1,3 +0,0 @@
-ActionController::Routing::Routes.draw do |map|
-  map.connect "#{Less::More.destination_path}/*id.css", :controller => 'less_cache', :action => "show"
-end
--- vendor/plugins/more/install.rb
+++
@@ -1,4 +0,0 @@
-require "fileutils"
-include FileUtils::Verbose
-
-mkdir_p File.join(Rails.root, "app", "stylesheets")
