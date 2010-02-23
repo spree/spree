ae26d2c736498dfdcdc6eb71d35dec307e54356e
David North <davidnorth@railsdog.com>
2010-02-23 22:07:58 +0000

    migrate existing payments, cards and transactions

--- db/migrate/20100209144531_polymorphic_payments.rb
+++ db/migrate/20100209144531_polymorphic_payments.rb
@@ -1,6 +1,5 @@
 class PolymorphicPayments < ActiveRecord::Migration
   def self.up
-    # TODO - migrate legacy payments (STI no longer needed)
     remove_column :payments, :type
     remove_column :payments, :creditcard_id
     rename_column :payments, :order_id, :payable_id
@@ -9,7 +8,16 @@ class PolymorphicPayments < ActiveRecord::Migration
       t.string :payment_method
       t.references :source, :polymorphic => true
     end
-    # TODO - migrate legacy creditcards first (associate them with a payment before killing association with checkout, etc)
+    execute "UPDATE payments SET payable_type = 'Order'"
+
+    Creditcard.all.each do |creditcard|
+      if checkout = Checkout.find_by_id(creditcard.checkout_id) and checkout.order
+        if payment = checkout.order.payments.first
+          execute "UPDATE payments SET source_type = 'Creditcard', source_id = #{creditcard.id} WHERE id = #{payment.id}"
+        end
+      end
+    end
+
     change_table :creditcards do |t|
       t.remove :checkout_id
     end 
--- db/migrate/20100214212536_assign_creditcard_txns_to_payment.rb
+++ db/migrate/20100214212536_assign_creditcard_txns_to_payment.rb
@@ -1,6 +1,11 @@
 class AssignCreditcardTxnsToPayment < ActiveRecord::Migration
   def self.up
     add_column "creditcard_txns", "payment_id", :integer  
+    ActiveRecord::Base.connection.select_all("SELECT * FROM creditcard_txns").each do |txn_attrs|
+      if creditcard = Creditcard.find_by_id(txn_attrs["creditcard_id"])
+        execute "UPDATE creditcard_txns SET payment_id = #{creditcard.payments.first.id} WHERE id = #{txn_attrs['id']}"
+      end
+    end
     remove_column "creditcard_txns", "creditcard_payment_id"
   end
 
