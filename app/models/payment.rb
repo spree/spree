8d9971f2f9b44226d0152341df6e119b55ccae47
Sean Schofield <sean@railsdog.com>
2010-02-23 20:20:24 -0500

    Do not require state events to be present in order to process a refund, etc.  Primarily helps with sample data where we're too lazy to create fake events.

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -80,10 +80,10 @@ class Payment < ActiveRecord::Base
       #sorting by created_at.to_f to ensure millisecond percsision, plus ID - just in case
       events = order.state_events.sort_by { |e| [e.created_at.to_f, e.id] }.reverse
 
-
+      
       if order.returnable_units.nil? && order.return_authorizations.size >0
         order.return!
-      elsif %w(over_paid under_paid).include?(events.first.name)
+      elsif events.present? and %w(over_paid under_paid).include?(events.first.name)
         events.each do |event|
           if %w(shipped paid new).include?(event.previous_state)
             order.update_attribute("state", event.previous_state)
