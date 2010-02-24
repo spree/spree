75e349e776df111e1ea25b6a3537f362b300beb2
paulcc <paulcc.two@gmail.com>
2010-02-24 15:37:26 +0000

    Removed duplicated definitions
    searcher/searcher= defined twice here...

--- lib/spree/config.rb
+++ lib/spree/config.rb
@@ -1,11 +1,12 @@
 module Spree
-  # Singleton class to access the configuration object (AppConfiguration.first by default) and it's preferences.
+  # Singleton class to access the configuration object (AppConfiguration.first by default) and its preferences.
   #
   # Usage:
   #   Spree::Config[:foo]                  # Returns the +foo+ preference
   #   Spree::Config[]                      # Returns a Hash with all the application preferences
   #   Spree::Config.instance               # Returns the configuration object (AppConfiguration.first)
   #   Spree::Config.set(preferences_hash)  # Set the application preferences as especified in +preference_hash+
+  #   Spree::Config.searcher/searcher=     # get/set the default product search implementation
   class Config
     include Singleton
     include PreferenceAccess
@@ -16,14 +17,6 @@ module Spree
         return nil unless ActiveRecord::Base.connection.tables.include?('configurations')
         @configuration ||= AppConfiguration.find_or_create_by_name("Default configuration")
         @configuration
-      end
-      
-      def searcher
-        @searcher ||= Spree::Search::Base.new
-      end
-      
-      def searcher=(searcher_obj)
-        @searcher = searcher_obj  
       end
       
       def searcher
