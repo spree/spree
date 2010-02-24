33c651c60453fa314013497ffb06d4fe918fcb81
Sean Schofield <sean@railsdog.com>
2010-02-23 20:12:35 -0500

    Fixed several issues with sample data.

---
+++ db/sample/payments.rb
@@ -0,0 +1,23 @@
+# create payments based on the totals since they can't be known in YAML (quantities are random)
+method = PaymentMethod.create :name => "Credit Card", :description => "Foo", :active => true, :type => "Gateway::Bogus"
+
+# Hack the current method so we're able to return a gateway without a RAILS_ENV
+Gateway.class_eval do
+  def self.current 
+    Gateway::Bogus.new
+  end
+end
+
+Order.all.each do |order|
+  order.update_totals!
+  creditcard = Creditcard.create(:cc_type => "visa", 
+                                 :month => 12, 
+                                 :year => 2014, 
+                                 :last_digits => "1111", 
+                                 :first_name => "Sean", 
+                                 :last_name => "Schofield",
+                                 :gateway_customer_profile_id => "BGS-1234")
+  payment = order.checkout.payments.create(:amount => order.outstanding_balance, :source => creditcard, :payment_method => method)
+  payment.process!
+  order.update_attribute("state", "new")
+end
