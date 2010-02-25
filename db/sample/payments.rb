73362c1515185eb6e311eab9833183ed71110c32
Brian Quinn <briandquinn@gmail.com>
2010-02-25 16:22:16 +0000

    Payments admin now uses partials for different payment_methods.
    [#1172 state:resolved]

--- db/sample/payments.rb
+++ db/sample/payments.rb
@@ -1,9 +1,9 @@
 # create payments based on the totals since they can't be known in YAML (quantities are random)
-method = Gateway::Bogus.create :name => "Credit Card", :description => "Foo", :active => true
+method = PaymentMethod.find(:first, :conditions => {:name => "Credit Card", :active => true})
 
 # Hack the current method so we're able to return a gateway without a RAILS_ENV
 Gateway.class_eval do
-  def self.current 
+  def self.current
     Gateway::Bogus.new
   end
 end
@@ -12,16 +12,14 @@ orders = Order.find(:all, :include => [{:line_items => [:variant]}, :adjustments
 
 orders.each do |order|
   order.update_totals!
-  creditcard = Creditcard.create(:cc_type => "visa", 
-                                 :month => 12, 
-                                 :year => 2014, 
-                                 :last_digits => "1111", 
-                                 :first_name => "Sean", 
+  creditcard = Creditcard.create(:cc_type => "visa",
+                                 :month => 12,
+                                 :year => 2014,
+                                 :last_digits => "1111",
+                                 :first_name => "Sean",
                                  :last_name => "Schofield",
                                  :gateway_customer_profile_id => "BGS-1234")
   payment = order.checkout.payments.create(:amount => order.outstanding_balance, :source => creditcard, :payment_method => method)
   payment.process!
   order.update_attribute("state", "new")
-end  
-
-method.destroy
+end
