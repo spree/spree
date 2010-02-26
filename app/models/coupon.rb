364e3b3b5d1fabde944e649b1df3af779410f385
Roman Smirnov <roman@railsdog.com>
2010-02-26 14:34:14 +0300

    Order#update_totals should not destroy coupons.
    [#1174 state:resolved]

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -9,12 +9,12 @@ class Coupon < ActiveRecord::Base
     return false if expires_at and Time.now > expires_at
     return false if usage_limit and coupon_credits.with_order.count >= usage_limit
     return false if starts_at and Time.now < starts_at
-    return false if order.coupon_credits.detect { |credit| credit.adjustment_source_id == self.id }
     # TODO - also check items in the order (once we support product groups for coupons)
     true
   end
     
   def create_discount(order)
+    return if order.coupon_credits.reload.detect { |credit| credit.adjustment_source_id == self.id }
     if eligible?(order) and amount = calculator.compute(order.line_items)
       amount = order.item_total if amount > order.item_total
       order.coupon_credits.reload.clear unless combine? and order.coupon_credits.all? { |credit| credit.adjustment_source.combine? }
