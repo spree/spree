72394d7dd13659fdc3bd2245004434e50dad6788
paulcc <paul@railsdog.com>
2010-03-02 11:24:09 +0000

    1188 - limit payment amount to order balance
    if a payment has an associated order, then limit the payment amount to that figure
    
    [#1188 state:resolved]

--- test/factories/payment_factory.rb
+++ test/factories/payment_factory.rb
@@ -3,6 +3,13 @@ Factory.define :payment do |f|
   f.payment_method { Gateway.current }
   f.source { Factory.build(:creditcard) }
   f.payable { Factory(:checkout) }
+
+  # limit the payment amount to order's remaining balance, to avoid over-pay exceptions
+  f.after_create do |pmt| 
+    if pmt.payable && pmt.payable.respond_to?(:order) && o = pmt.payable.order
+      pmt.update_attribute(:amount, [pmt.amount, o.outstanding_balance].min) 
+    end
+  end
 end
 
 Factory.define :creditcard_txn do |f|
@@ -10,4 +17,10 @@ Factory.define :creditcard_txn do |f|
   f.amount 45.75
   f.response_code 12345
   f.txn_type CreditcardTxn::TxnType::AUTHORIZE
+
+  # match the payment amount to the payment's value
+  f.after_create do |txn| 
+    # txn.update_attribute(:amount, [txn.amount, txn.payment.payment].min)
+    txn.update_attribute(:amount, txn.payment.amount)
+  end
 end
--- test/unit/checkout_test.rb
+++ test/unit/checkout_test.rb
@@ -55,7 +55,6 @@ class CheckoutTest < ActiveSupport::TestCase
        setup do
          @checkout.state = 'confirm'
          @payment = Factory(:payment, :payable => @checkout)
-         @payment.amount = [@payment.amount, @checkout.order.outstanding_balance].min
          @checkout.next! 
        end
        should_change("@checkout.state", :to => "complete") { @checkout.state }
