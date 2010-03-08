cbf357c4505d536fec7af719b22db9a719b0d912
paulcc <paul@railsdog.com>
2010-03-08 14:16:29 +0000

    removed misleading date comment from 'activate'
    the add-tab mechanism has changed...

--- vendor/extensions/theme_default/theme_default_extension.rb
+++ vendor/extensions/theme_default/theme_default_extension.rb
@@ -14,22 +14,6 @@ class ThemeDefaultExtension < Spree::Extension
   
   def activate
 
-    # Add your extension tab to the admin.
-    # Requires that you have defined an admin controller:
-    # app/controllers/admin/yourextension_controller
-    # and that you mapped your admin in config/routes
-
-    #Admin::BaseController.class_eval do
-    #  before_filter :add_yourextension_tab
-    #
-    #  def add_yourextension_tab
-    #    # add_extension_admin_tab takes an array containing the same arguments expected
-    #    # by the tab helper method:
-    #    #   [ :extension_name, { :label => "Your Extension", :route => "/some/non/standard/route" } ]
-    #    add_extension_admin_tab [ :yourextension ]
-    #  end
-    #end
-
     # make your helper avaliable in all views
     # Spree::BaseController.class_eval do
     #   helper YourHelper
