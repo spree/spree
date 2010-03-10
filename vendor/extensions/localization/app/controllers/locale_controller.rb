2f129bcf10837e740fb8c8fb5f3a32675d9e441e
Roman Smirnov <roman@railsdog.com>
2010-03-10 16:29:04 +0300

    Should redirect back after locale switching.
    [#1226 state:resolved]

--- vendor/extensions/localization/app/controllers/locale_controller.rb
+++ vendor/extensions/localization/app/controllers/locale_controller.rb
@@ -1,6 +1,9 @@
 class LocaleController < ApplicationController
 
   def set
+    if request.referer && request.referer.starts_with?("http://" + request.host)
+      session[:return_to] = request.referer
+    end
     if params[:locale] && AVAILABLE_LOCALES.include?(params[:locale])
       I18n.locale = params[:locale]
       session[:locale] = params[:locale]
