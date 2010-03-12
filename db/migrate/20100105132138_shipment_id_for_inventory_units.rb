6a1b6406ecb13e339ce7959be12909767f0ba755
paulcc <paul@railsdog.com>
2010-03-12 12:31:05 +0000

    1229 - workaround for fixture problem
    0.9.4 sample data created 500 orders and 1000 shipments,
    and so half of the shipments do not have valid orders.
    Now this migration prints a warning and skips each invalid
    order. Problem will only appear if you are migrating
    from earlier sample data...
    
    [#1229]

--- db/migrate/20100105132138_shipment_id_for_inventory_units.rb
+++ db/migrate/20100105132138_shipment_id_for_inventory_units.rb
@@ -5,6 +5,10 @@ class ShipmentIdForInventoryUnits < ActiveRecord::Migration
     
     # migrate legacy shipments
     Shipment.all.each do |shipment|
+      unless shipment.order
+        puts "Warning: shipment has invalid order - #{shipment.id}"
+        next
+      end
       shipment.order.inventory_units.each do |unit|
         unit.update_attribute("shipment_id", shipment.id)
       end
