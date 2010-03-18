9f48a22146a3107bbd0f1c4e509ac01ede0922cb
Roman Smirnov <roman@railsdog.com>
2010-03-18 17:20:23 +0300

    Should set country to proper value before validation. Needed for Ruby 1.9.x, which not update country to selected value automatically.
    [#1234 state:resolved]

--- app/models/address.rb
+++ app/models/address.rb
@@ -27,6 +27,7 @@ class Address < ActiveRecord::Base
   end
 
   def state_name_validate
+    country = Country.find(country_id) 
     return if country.blank? || country.states.empty?
     if state_name.blank? || country.states.name_or_abbr_equals(state_name).empty?
       errors.add(:state, :invalid)
