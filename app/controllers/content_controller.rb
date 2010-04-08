c688f23e178eac325ee0697173d4fd7dafb26e89
Sean Schofield <sean@railsdog.com>
2010-04-08 16:46:35 -0400

    Make cacheing of static content from ContentController optional (default=true)
    [#1327 state:resolved]

--- app/controllers/content_controller.rb
+++ app/controllers/content_controller.rb
@@ -1,6 +1,6 @@
 class ContentController < Spree::BaseController
   rescue_from ActionView::MissingTemplate, :with => :render_404
-  caches_page :show, :index
+  caches_page :show, :index, :if => Proc.new { Spree::Config[:cache_static_content] }
 
   def show
     render :action => params[:path].join('/')
--- app/models/app_configuration.rb
+++ app/models/app_configuration.rb
@@ -49,7 +49,7 @@ class AppConfiguration < Configuration
   preference :allow_openid, :boolean, :default => true # allow use OpenID for registrations
   preference :checkout_zone, :string, :default => nil # replace with the name of a zone if you would like to limit the countries
   preference :always_put_site_name_in_title, :boolean, :default => true
-
+  preference :cache_static_content, :boolean, :default => true
 
   validates_presence_of :name
   validates_uniqueness_of :name
