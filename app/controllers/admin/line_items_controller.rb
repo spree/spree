b9519c2fb91e3fd75d53070e8f8927975ee51649
David North <davidnorth@railsdog.com>
2010-05-07 17:14:30 +0100

    fix that adding line items to order in admin broke due to checkout validation failure caused by update_totals!

--- app/controllers/admin/line_items_controller.rb
+++ app/controllers/admin/line_items_controller.rb
@@ -59,8 +59,7 @@ class Admin::LineItemsController < Admin::BaseController
       tax_charge.update_attributes(:amount => tax_charge.calculate_tax_charge)
     end
 
+    @order.checkout.enable_validation_group(@order.checkout.state.to_sym)
     @order.update_totals!
-    @order.save
-
   end
 end
