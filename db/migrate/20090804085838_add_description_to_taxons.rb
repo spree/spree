64091b1bcb5b5b7ca24877a8f1b27de71f3b4022
Brian Quinn <briandquinn@gmail.com>
2010-05-07 13:51:14 +0100

    Revert "Merge advanced_taxon extension into the Core."
    This reverts commit 7fe33dc6af3b62dafc565b874c8c7a5bc7587506.

--- db/migrate/20090804085838_add_description_to_taxons.rb
+++
@@ -1,9 +0,0 @@
-class AddDescriptionToTaxons < ActiveRecord::Migration
-    def self.up
-      add_column :taxons, :description, :text
-    end
-
-    def self.down
-      remove_column :taxons, :description
-    end
-end
