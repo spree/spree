9e377738f572df7ee44168b147df5d06248d934c
Sean Schofield <sean@railsdog.com>
2010-05-30 14:14:27 -0400

    Skip advanced taxon migrations if the relevant fields are already present.
    [#1346][#1384]

--- db/migrate/20100506185838_add_description_to_taxons.rb
+++ db/migrate/20100506185838_add_description_to_taxons.rb
@@ -1,5 +1,7 @@
 class AddDescriptionToTaxons < ActiveRecord::Migration
     def self.up
+      # skip this migration if the attribute already exists because of advanced taxon extension
+      return if Taxon.new.respond_to? :description
       add_column :taxons, :description, :text
     end
 
