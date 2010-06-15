4c4f32d69e681de1cda9e1a3bdb95ef4a4f10935
Brian Quinn <briandquinn@gmail.com>
2010-06-15 09:50:15 +0100

    Ensure adjustments set order state to paid for existing orders with balance_due or credit_owed states

--- app/controllers/admin/adjustments_controller.rb
+++ app/controllers/admin/adjustments_controller.rb
@@ -10,9 +10,9 @@ class Admin::AdjustmentsController < Admin::BaseController
   destroy.success.wants.js { render_js_for_destroy }
 
   create.before :set_type
-  create.after :update_totals_and_complete_free_order
-  update.after :update_totals
-  destroy.after :update_totals
+  create.after :set_order_state
+  update.after :set_order_state
+  destroy.after :set_order_state
 
   private
   def list_adjustment_types
@@ -28,17 +28,16 @@ class Admin::AdjustmentsController < Admin::BaseController
     object.type = params[:adjustment][:type]
   end
 
-  def update_totals
-    @order.update_totals!
-  end
-
   # Automatically complete and order where no payment is necessary because adjustments cancel out the total
-  def update_totals_and_complete_free_order
+  def set_order_state
     @order.update_totals!
-    if @order.in_progress? and @order.item_total > 0 and @order.total == 0 and @order.payments.total == 0
+
+    if @order.in_progress? and @order.item_total > 0 and @order.total == 0 and @order.payments.total == 0  #for new orders that are adjusted to zero
       until @order.checkout.complete?
         @order.checkout.next!
       end
+      @order.reload.pay!
+    elsif @order.item_total > 0 && ((@order.balance_due? && @order.outstanding_balance == 0) || (@order.credit_owed? && @order.outstanding_credit == 0)) #set existing orders back to paid, if adjustment corrects balance
       @order.reload.pay!
     end
   end
