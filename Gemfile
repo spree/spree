32643d79c8f8c5ddb102ddea8738728529183e3a
Sean Schofield <sean@railsdog.com>
2010-06-21 13:26:37 -0400

    Removed comments from routes and cleaned up whitespace.

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source :gemcutter
 
-gem "spree", :path => File.dirname(__FILE__) 
+gem "spree", :path => File.dirname(__FILE__)
 
 # eventually these can be dropped and replaced by spree.gemspec dependencies but we need the edge versions for now
 gem "activemerchant", :require => 'active_merchant', :git => "git://github.com/railsjedi/active_merchant.git"
@@ -10,14 +10,14 @@ gem 'resource_controller', :git => "git://github.com/BDQ/resource_controller.git
 #gem 'mysql'
 gem 'sqlite3-ruby'
 
-# gem 'ruby-debug'
-# 
+gem 'ruby-debug'
+#
 # group :test do
 #   gem 'shoulda', '2.10.2', :require => 'shoulda'
 #   gem 'factory_girl', '1.2.3', :require => 'factory_girl'
 #   gem 'test-unit', '~>2.0.5', :require => 'test/unit' if RUBY_VERSION.to_f >= 1.9
 # end
-# 
+#
 # group :cucumber do
 #   gem 'cucumber-rails', '>=0.2.4', :require => false
 #   gem 'database_cleaner', '>=0.4.3', :require => false
--- config/routes.rb
+++ config/routes.rb
@@ -1,60 +1,10 @@
 Spree::Application.routes.draw do |map|
 
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
-
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get :short
-  #       post :toggle
-  #     end
-  #
-  #     collection do
-  #       get :sold
-  #     end
-  #   end
-
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get :recent, :on => :collection
-  #     end
-  #   end
-
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => 'products#index'
 
   match 'login' => 'user_sessions#new'
   match 'logout' => 'user_sessions#destroy'
   match 'signup' => 'users#new'
-  root :to => 'products#index'
 
   resources :products
 
