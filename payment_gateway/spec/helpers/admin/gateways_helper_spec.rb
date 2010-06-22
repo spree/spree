06812919d3951917e406ddcba35c0560199b2994
Sean Schofield <sean@railsdog.com>
2010-06-22 17:55:37 -0400

    Removing rspec in favor of test/unit and shoulda.
    [#524]

--- payment_gateway/spec/helpers/admin/gateways_helper_spec.rb
+++
@@ -1,6 +0,0 @@
-require File.dirname(__FILE__) + '/../../spec_helper'
-
-describe Admin::GatewaysHelper do
-  
-  
-end
--- spec/models/zone_member_spec.rb
+++
@@ -1,5 +0,0 @@
-require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
-
-describe ZoneMember do
-
-end
