eec352bb69b0b55c70257252bb357b9f27e1cf66
Roman Smirnov <roman@railsdog.com>
2010-06-24 23:04:22 +0400

    Removed unobtrusive_date_picker plugin

--- vendor/plugins/unobtrusive_date_picker/init.rb
+++
@@ -1,8 +0,0 @@
-require '12_hour_time'
-require 'unobtrusive_date_picker'
-
-# Include all the necessary functions in to the appropriate point in the Rails framework
-ActionView::Base.send :include, UnobtrusiveDatePicker::UnobtrusiveDatePickerHelper
-ActionView::Helpers::DateHelper.send :include, UnobtrusiveDatePicker::UnobtrusiveDatePickerHelper
-ActionView::Base.send :include, UnobtrusiveDatePicker::AssetTagHelper
-ActionView::Helpers::AssetTagHelper.send :include, UnobtrusiveDatePicker::AssetTagHelper
