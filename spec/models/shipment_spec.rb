f3275aac8ffb4a326f247c34f28a4024949b7a12
Sean Schofield <sean@railsdog.com>
2010-06-24 10:48:00 -0400

    Removed a bunch of rspec stuff.
    [#524]

--- spec/models/shipment_spec.rb
+++
@@ -1,8 +0,0 @@
-require File.dirname(__FILE__) + '/../spec_helper'
-
-class MockCalculator
-end
-
-describe Shipment do
-
-end
--- spec/models/shipping_category_spec.rb
+++
@@ -1,8 +0,0 @@
-require File.dirname(__FILE__) + '/../spec_helper'
-
-describe ShippingCategory do
-  before(:each) do
-    @shipping_category = ShippingCategory.new
-  end
-
-end
