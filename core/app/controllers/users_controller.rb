aef2606acdb5af0620852009fd93102c7728c599
Sean Schofield <sean@railsdog.com>
2010-07-08 11:42:28 -0400

    Reverted UsersController#create back to r_c since we're not using open id in the core and the block approach didn't seem to be working in rails3.

--- core/app/controllers/users_controller.rb
+++ core/app/controllers/users_controller.rb
@@ -1,33 +1,20 @@
 class UsersController < Spree::BaseController
   resource_controller
-  
+
   before_filter :require_no_user, :only => [:new, :create]
   before_filter :require_user, :only => [:show, :edit, :update]
 
   ssl_required :new, :create, :edit, :update, :show
-  
+
   actions :all, :except => [:index, :destroy]
-	
-	#Cannot use resource_controller for create action
-	#as openID expects block passed to user.save method
-	def create
-	  @user = User.new(params[:user])
-	  @user.save do |result|
-	    if result
-	      flash.notice = t(:user_created_successfully) unless session[:return_to]
-	      @user.roles << Role.find_by_name("admin") unless admin_created?
-	      respond_to do |format|
-	        format.html { redirect_back_or_default products_url }
-	        format.js { render :js => true.to_json }
-	      end
-	    else
-	      respond_to do |format|
-	        format.html { render :action => :new }
-	        format.js { render :js => @user.errors.to_json }
-	      end
-	    end
-	  end
-	end
+
+  create do
+    flash nil
+    success.wants.html { redirect_back_or_default products_path }
+    success.wants.js { render :js => true.to_json }
+    failure.wants.html { render :new }
+    failure.wants.js { render :js => @user.errors.to_json }
+  end
 
   show.before :show_before
   new_action.before :new_action_before
@@ -47,11 +34,11 @@ class UsersController < Spree::BaseController
     def object
       @object ||= current_user
     end
-    
+
     def show_before
-      @orders = @user.orders.checkout_complete 
+      @orders = @user.orders.checkout_complete
     end
-    
+
     def new_action_before
       flash.now[:notice] = I18n.t(:please_create_user) unless admin_created?
     end
