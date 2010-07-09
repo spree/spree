92fd8a47e967eb791953edbb3527e107ef3bf81e
Roman Smirnov <roman@railsdog.com>
2010-07-09 14:56:00 +0400

    Typo fix

--- dashboard/config/routes.rb
+++ dashboard/config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do |map|
   match '/admin' => 'admin/overview#index', :as => :admin
-  match '/admin/overview/get_report_data' => 'admin/overview#get_report_data', :as => :admin
+  match '/admin/overview/get_report_data' => 'admin/overview#get_report_data'
 end
