b266eeb41ac0d51b80d831b1f5228808e75b679f
Brian Quinn <briandquinn@gmail.com>
2010-07-24 23:03:28 +0100

    Fixed creating new variants using UJS methods

--- core/app/controllers/admin/variants_controller.rb
+++ core/app/controllers/admin/variants_controller.rb
@@ -3,26 +3,26 @@ class Admin::VariantsController < Admin::BaseController
   belongs_to :product
 
   new_action.response do |wants|
-    wants.html {render :action => :new, :layout => false}
+    wants.js
   end
 
   create.before :create_before
-  
+
   # redirect to index (instead of r_c default of show view)
-  create.response do |wants| 
+  create.response do |wants|
     wants.html {redirect_to collection_url}
   end
 
   # redirect to index (instead of r_c default of show view)
-  update.response do |wants| 
+  update.response do |wants|
     wants.html {redirect_to collection_url}
   end
-  
-  # override the destory method to set deleted_at value 
+
+  # override the destory method to set deleted_at value
   # instead of actually deleting the product.
   def destroy
     @variant = Variant.find(params[:id])
-      
+
     @variant.deleted_at = Time.now()
     if @variant.save
       flash.notice = I18n.t("notice_messages.variant_deleted")
@@ -35,17 +35,17 @@ class Admin::VariantsController < Admin::BaseController
       format.js  { render_js_for_destroy }
     end
   end
-  
-  private 
-  def create_before 
+
+  private
+  def create_before
     option_values = params[:new_variant]
     option_values.each_value {|id| @object.option_values << OptionValue.find(id)}
     @object.save
   end
-  
+
   def collection
     @deleted =  (params.key?(:deleted)  && params[:deleted] == "on") ? "checked" : ""
-    
+
     if @deleted.blank?
       @collection ||= end_of_association_chain.active.find(:all)
     else
