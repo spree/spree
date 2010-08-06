aa39b3daf5a39ee1df876f1b9249e286c6ae1165
Sean Schofield <sean@railsdog.com>
2010-08-05 22:25:44 -0400

    Require spree_site by default

--- lib/spree.rb
+++ lib/spree.rb
@@ -1,2 +1,9 @@
 require 'spree_core/all'
 require 'spree_sample'
+
+begin
+  # contains site specific logic for overriding spree stuff
+  require 'spree_site'
+rescue Exception
+  # no big deal - we can generate one using 'rails g spree:site'
+end
