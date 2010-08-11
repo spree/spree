f012197eb57907ccf5508357ec12f8748375a7ca
Sean Schofield <sean@railsdog.com>
2010-08-11 10:54:30 -0400

    New install generator for spree_auth.

---
+++ auth/lib/generators/spree_auth/install_generator.rb
@@ -0,0 +1,16 @@
+module SpreeAuth
+  module Generators
+    class InstallGenerator < Rails::Generators::Base
+      source_root File.expand_path("../../templates", __FILE__)
+
+      desc "Configures your Rails application for use with spree_auth."
+
+      def copy_initializer
+        template "devise.rb", "config/initializers/devise.rb"
+      end
+      # def show_readme
+      #   readme "README" if behavior == :invoke
+      # end
+    end
+  end
+end
