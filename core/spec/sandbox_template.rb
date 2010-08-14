e3fe1a897f78f0b5324894970841e5a2b03e3d94
David North <davidnorth@railsdog.com>
2010-08-13 19:23:19 -0500

    sandbox_template had wrong gem for Fabrication. manually require the fabricators for now since the gem is looking in the sandbox rather than core/spec

--- core/spec/sandbox_template.rb
+++ core/spec/sandbox_template.rb
@@ -1,5 +1,5 @@
 # rails site template for creating a test site to be used in testing
 gem "spree_core", :path => ".."
-gem 'fabricator', :group => 'test'
 gem 'rspec', '2.0.0.beta.19', :group => 'test'
 gem 'rspec-rails', '2.0.0.beta.19', :group => 'test'
+gem 'fabrication', :group => 'test'
