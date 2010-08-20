0b0ec047fe0fc4fc306a16c2a3fbe50593b3ec7a
David North <davidnorth@railsdog.com>
2010-08-20 16:31:32 -0500

    removed the transaction models

--- core/app/models/creditcard_txn.rb
+++
@@ -1,9 +0,0 @@
-class CreditcardTxn < Transaction
-
-  enumerable_constant :txn_type, :constants => [:authorize, :capture, :purchase, :void, :credit]
-  
-  def txn_type_name
-    TxnType.from_value(txn_type)
-  end
-
-end
