45ead8528a15b21fa06a810643ddbe1c30e3f690
Roman Smirnov <roman@railsdog.com>
2010-08-30 14:22:48 +0400

    Should not send calculator_attributes params if calculator_type was changed for all calculables (not only coupons)
    [#1505]

--- vendor/extensions/theme_default/public/javascripts/calculator.js
+++ vendor/extensions/theme_default/public/javascripts/calculator.js
@@ -5,13 +5,11 @@ $j(function() {
     if ($j('#calc-type').attr('value') == original_calc_type) {
       $j('div.calculator-settings').show();
       $j('div#calculator-settings-warning').hide();
-      $j('#coupon_calculator_attributes_id').removeAttr('disabled');
-      $j('.calculator-settings .input_string').removeAttr('disabled');
+      $j('.calculator-settings input').removeAttr('disabled');
     } else {
       $j('div.calculator-settings').hide();
       $j('div#calculator-settings-warning').show();
-      $j('#coupon_calculator_attributes_id').attr('disabled', 'disabled');
-      $j('.calculator-settings .input_string').attr('disabled', 'disabled');
+      $j('.calculator-settings input').attr('disabled', 'disabled');
     }
   });
 })
