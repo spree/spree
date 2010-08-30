48b9d1b3236299c4615a4768438065753556c52a
Roman Smirnov <roman@railsdog.com>
2010-08-30 19:57:07 +0400

    Should not send calculator_attributes params if calculator_type was changed (ported from 0-11-stable)
    [#1505 state:resolved]

--- core/lib/generators/templates/public/javascripts/calculator.js
+++ core/lib/generators/templates/public/javascripts/calculator.js
@@ -5,9 +5,11 @@ $j(function() {
     if ($j('#calc-type').attr('value') == original_calc_type) {
       $j('div.calculator-settings').show();
       $j('div#calculator-settings-warning').hide();
+      $j('.calculator-settings input').removeAttr('disabled');
     } else {
       $j('div.calculator-settings').hide();
       $j('div#calculator-settings-warning').show();
+      $j('.calculator-settings input').attr('disabled', 'disabled');
     }
   });
 })
