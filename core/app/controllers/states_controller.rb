ace2e0ff63aa124e2f4a378251b6d5b4cda188ee
David North <davidnorth@railsdog.com>
2010-09-01 17:24:53 -0500

    Fixed /states.js

--- core/app/controllers/states_controller.rb
+++ core/app/controllers/states_controller.rb
@@ -1,19 +1,16 @@
 class StatesController < Spree::BaseController
-  resource_controller
 
   ssl_allowed :index
 
-  index.response do |wants|
-    wants.html
-    wants.js do
-      # table of {country.id => [ state.id , state.name ]}, arrays sorted by name
-      # blank is added elsewhere, if needed
-      # we return ALL known information, since billing country isn't restricted
-      #   by shipping country
-      @state_info = Hash.new {|h, k| h[k] = []}
-      State.find(:all, :order => "name ASC").each{|state|
-        @state_info[state.country_id.to_s].push [state.id, state.name]
-      }
-    end
+  def index
+    # table of {country.id => [ state.id , state.name ]}, arrays sorted by name
+    # blank is added elsewhere, if needed
+    # we return ALL known information, since billing country isn't restricted
+    #   by shipping country
+    @state_info = Hash.new {|h, k| h[k] = []}
+    State.find(:all, :order => "name ASC").each{|state|
+      @state_info[state.country_id.to_s].push [state.id, state.name]
+    }
   end
+
 end
