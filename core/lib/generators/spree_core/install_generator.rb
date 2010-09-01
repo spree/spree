00f74def93058d0d734cd9fad69e3b3838ed780e
Roman Smirnov <roman@railsdog.com>
2010-09-02 00:12:15 +0400

    Devise controllers should use properly base class and layout. To see in action don't forget rerun 'rails g spree_core:install'

--- core/lib/generators/spree_core/install_generator.rb
+++ core/lib/generators/spree_core/install_generator.rb
@@ -8,6 +8,11 @@ module SpreeCore
       def create_lib_files
         template 'spree_site.rb', "lib/spree_site.rb"
       end
+      
+      def configure_application_controller
+        template 'application_controller.rb', 'app/controllers/application_controller.rb'
+        remove_file 'app/views/layouts/application.html.erb'
+      end
 
       def additional_tweaks
         remove_file "public/index.html"
---
+++ core/lib/generators/templates/application_controller.rb
@@ -0,0 +1,4 @@
+class ApplicationController < Spree::BaseController
+  # This class is intentionally blank.  
+  # Keeping this class empty makes it easier to integrate Spree with an existing Rails application
+end
