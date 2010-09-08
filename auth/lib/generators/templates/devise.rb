0569c2a2b7170d22a55fa63c76799f42530134fa
Brian Quinn <briandquinn@gmail.com>
2010-09-08 12:33:16 +0100

    Change default Devise encryptor to authlogic_sha512 to allow users created on previous versions to login

--- auth/lib/generators/templates/devise.rb
+++ auth/lib/generators/templates/devise.rb
@@ -39,11 +39,11 @@ Devise.setup do |config|
   # from others authentication tools as :clearance_sha1, :authlogic_sha512 (then
   # you should set stretches above to 20 for default behavior) and :restful_authentication_sha1
   # (then you should set stretches to 10, and copy REST_AUTH_SITE_KEY to pepper)
-  config.encryptor = :bcrypt
+  config.encryptor = :authlogic_sha512
 
   # For bcrypt, this is the cost for hashing the password and defaults to 10. If
   # using other encryptors, it sets how many times you want the password re-encrypted.
-  config.stretches = 10
+  config.stretches = 20
 
   # Setup a pepper to generate the encrypted password.
   config.pepper = <%= ActiveSupport::SecureRandom.hex(64).inspect %>
