16012fda474acd99e4cdba50ce39f0ab506246a6
Sean Schofield <sean@railsdog.com>
2010-09-12 10:52:55 -0400

    Admin orders view should default to not showing incomplete orders.

--- core/app/controllers/admin/orders_controller.rb
+++ core/app/controllers/admin/orders_controller.rb
@@ -58,6 +58,7 @@ class Admin::OrdersController < Admin::BaseController
       params[:search][:created_at_less_than] = Time.zone.parse(params[:search][:created_at_less_than]).end_of_day rescue ""
     end
 
+    params[:search][:completed_at_not_null] ||= "1"
     if params[:search].delete(:completed_at_not_null) == "1"
       params[:search][:completed_at_not_null] = true
     end
