ad01ee046ab6df3d9ac3c023cba6ba4752877200
David North <davidnorth@railsdog.com>
2010-09-20 17:10:38 -0500

    update_attribute_without_callbacks and update_attributes_without_callbacks should set the attributes on the model as well as updating the database

--- core/lib/spree_core/ext/active_record.rb
+++ core/lib/spree_core/ext/active_record.rb
@@ -2,11 +2,13 @@ module ActiveRecord::Persistence
 
   # Update attributes of a record in the database without callbacks, validations etc.
   def update_attributes_without_callbacks(attributes)
+    self.send(:attributes=, attributes, false)
     self.class.update_all(attributes, { :id => id })
   end
 
   # Update a single attribute in the database
   def update_attribute_without_callbacks(name, value)
+    send("#{name}=", value)
     update_attributes_without_callbacks(name => value)
   end
 
---
+++ core/spec/lib/ext_spec.rb
@@ -0,0 +1,53 @@
+require "spec_helper"
+
+describe 'Core extensions' do
+
+  describe 'ActiveRecord::Base' do
+
+    let(:order) { Order.create } 
+
+    context "update_attribute_without_callbacks" do
+
+      it "sets the attribute" do
+        order.update_attribute_without_callbacks 'state', 'new_state'
+        order.state.should == 'new_state'
+      end
+      
+      it "updates the attribute in the database" do
+        order.update_attribute_without_callbacks 'state', 'new_state'
+        order.reload
+        order.state.should == 'new_state'
+      end
+
+      it "doesn't call valid" do
+        order.should_not_receive(:valid?)
+        order.update_attribute_without_callbacks 'state', 'new_state'
+      end
+
+    end
+
+    context "updte_attributes_without_callbacks" do
+
+      it "sets the attributes" do
+        order.update_attributes_without_callbacks :state => 'new_state', :email => 'spree@example.com'
+        order.state.should == 'new_state'
+        order.email.should == 'spree@example.com'
+      end
+
+      it "updates the attributes in the database" do
+        order.update_attributes_without_callbacks :state => 'new_state', :email => 'spree@example.com'
+        order.reload
+        order.state.should == 'new_state'
+        order.email.should == 'spree@example.com'
+      end
+
+      it "doesn't call valid" do
+        order.should_not_receive(:valid?)
+        order.update_attributes_without_callbacks :state => 'new_state', :email => 'spree@example.com'
+      end
+
+    end
+
+  end
+
+end
