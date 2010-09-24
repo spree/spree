36c7e4dedf007d9de057bfc35952b8c4b88dd376
Sean Schofield <sean@railsdog.com>
2010-09-24 15:28:32 -0400

    Reinitialize the mail settings after updates.

--- core/app/controllers/admin/mail_methods_controller.rb
+++ core/app/controllers/admin/mail_methods_controller.rb
@@ -1,6 +1,13 @@
 class Admin::MailMethodsController < Admin::BaseController
   resource_controller
 
+  after_filter :initialize_mail_settings
+
   update.wants.html { redirect_to collection_url }
   create.wants.html { redirect_to collection_url }
+
+  private
+  def initialize_mail_settings
+    Spree::MailSettings.init
+  end
 end
---
+++ core/spec/controllers/admin/mail_methods_controller_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe Admin::MailMethodsController do
+  let(:order) { mock_model(Order, :complete? => true).as_null_object }
+  let(:mail_method) { mock_model(MailMethod).as_null_object }
+
+  before do
+    #controller.stub :current_user => nil
+    Order.stub :find => order
+    MailMethod.stub :find => mail_method
+  end
+
+  context "#create" do
+    it "should reinitialize the mail settings" do
+      Spree::MailSettings.should_receive :init
+      put :create, {:order_id => "123", :id => "456", :mail_method_parmas => {:environment => "foo"}}
+    end
+  end
+
+  context "#update" do
+    it "should reinitialize the mail settings" do
+      Spree::MailSettings.should_receive :init
+      put :update, {:order_id => "123", :id => "456", :mail_method_parmas => {:environment => "foo"}}
+    end
+  end
+end
