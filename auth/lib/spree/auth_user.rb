845e55526a9732b5f31106e95e3085f62bd9e84b
Sean Schofield <sean@railsdog.com>
2010-09-25 14:48:17 -0400

    Changed implementation of cancan ability hack.  This approach seems to be more stable (previous hack was not always working - depending on load order, etc.)

--- auth/lib/spree/auth_user.rb
+++ auth/lib/spree/auth_user.rb
@@ -10,10 +10,5 @@ module Spree
       return current_user || User.find_by_persistence_token(session[:guest_token])
     end
 
-    # Overrides the default method used by Cancan so that we can use the guest_token in addition to current_user.
-    def current_ability
-      @current_ability ||= ::Ability.new(auth_user)
-    end
-
   end
 end
--- auth/lib/spree_auth.rb
+++ auth/lib/spree_auth.rb
@@ -5,12 +5,32 @@ require 'cancan'
 require 'spree/auth_user'
 require 'spree/auth/config'
 
+
+# Overrides the default current_ability method used by Cancan so that we can use the guest_token in addition to current_user.
+module CancanAbilityHack
+  module ClassMethods
+    def current_ability
+      @current_ability ||= ::Ability.new(auth_user)
+    end
+  end
+  def self.included(base)
+    base.class_eval do
+      extend ClassMethods
+      class << self
+        alias :original_current_ability :current_ability
+      end
+    end
+  end
+end
+
 module SpreeAuth
   class Engine < Rails::Engine
     def self.activate
       Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
         Rails.env == "production" ? require(c) : load(c)
       end
+
+      ActionController::Base.send :include, CancanAbilityHack
     end
     config.to_prepare &method(:activate).to_proc
   end
