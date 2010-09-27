175b8361e53ab0520f328e8e2ff1cc113ed42485
Sean Schofield <sean@railsdog.com>
2010-09-27 11:13:07 -0400

    CheckoutController now consults Order#completed? in determining if we're done the checkout process.  This removes the assumption that customized state machines will always be in a "complete" state when the checkout is finished.

--- core/app/controllers/checkout_controller.rb
+++ core/app/controllers/checkout_controller.rb
@@ -11,8 +11,7 @@ class CheckoutController < Spree::BaseController
       if @order.update_attributes(object_params)
         if @order.next
           state_callback(:after)
-          @order.next if @order.state == "adjustments"
-          if @order.state == "complete"
+          if @order.state == "complete" or @order.completed?
             flash[:notice] = I18n.t(:order_processed_successfully)
             redirect_to completion_route and return
           else
