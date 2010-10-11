4949c0a5e7b61ecb5e27146fe4780f19fcbebcee
Zac Williams <zac@railsdog.com>
2010-10-11 14:37:27 -0500

    move available_countries method to Spree::BaseHelper

--- core/app/helpers/spree/base_helper.rb
+++ core/app/helpers/spree/base_helper.rb
@@ -88,4 +88,9 @@ module Spree::BaseHelper
   def logo(image_path=Spree::Config[:logo])
     link_to image_tag(image_path), root_path
   end
+  
+  def available_countries
+    return Country.all unless zone = Zone.find_by_name(Spree::Config[:checkout_zone])
+    zone.country_list
+  end
 end
