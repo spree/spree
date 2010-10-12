96008f4dbd1d135f699de9e4c4583869dd32bb59
Zac Williams <zac@railsdog.com>
2010-10-12 09:23:00 -0500

    re-create user session after UsersController#update

--- auth/spec/controllers/users_controller_spec.rb
+++ auth/spec/controllers/users_controller_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 
 describe UsersController do
+  
+  let(:user) { mock_model(User, :email => "spree@example.com") }
+  
   context "#create" do
 
     before do
@@ -13,6 +16,19 @@ describe UsersController do
 
     it "should automatically authenticate the new user" do
       session[:user_credentials_id].should_not be_nil
+    end
+  end
+  
+  context "#update" do
+    before do 
+      controller.stub :current_user => user
+      user.stub :update_attributes => true
+      user.stub(:has_role?).with('admin').and_return(true)
+    end
+    
+    it "should create a user session after update" do
+      UserSession.should_receive :create
+      put :update, {:user => {:password => "newpw", :password_confirmation => "newpw", :email => user.email}}
     end
   end
 end
