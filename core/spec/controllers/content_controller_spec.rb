bf35737fd95fa147ebd65fc3aa0ab2d0365f3b8a
Zac Williams <zac@railsdog.com>
2010-10-13 17:42:23 -0500

    forgot to add new spec in last commit

---
+++ core/spec/controllers/content_controller_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe ContentController do
+
+  it "should understand routes" do
+    assert_routing("/content/cvv", {:controller => "content", :action => "cvv", })
+  end
+  
+end
