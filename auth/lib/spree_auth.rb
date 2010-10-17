57f4e75eb03fe5d17bea0cc33dce7f9d0be539d2
Trung LE <joneslee85@gmail.com>
2010-10-17 21:47:41 +1100

    Use preferred Rails.env syntax
    [#1691 state:resolved]

--- auth/lib/spree_auth.rb
+++ auth/lib/spree_auth.rb
@@ -9,7 +9,7 @@ module SpreeAuth
   class Engine < Rails::Engine
     def self.activate
       Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
-        Rails.env == "production" ? require(c) : load(c)
+        Rails.env.production? ? require(c) : load(c)
       end
     end
     config.to_prepare &method(:activate).to_proc
