9ffac481ba32e2cdc81e7b5c6d376524efb6c78c
Sean Schofield <sean@railsdog.com>
2010-10-18 13:37:26 -0400

    Reverted a controversial commit merging previous in progress orders after login.  This was technically in Spree 0.11.x but its not appropriate as a core feature.  Please patch in your own application if you need this behavior.

--- auth/app/controllers/user_sessions_controller.rb
+++ auth/app/controllers/user_sessions_controller.rb
@@ -54,14 +54,13 @@ class UserSessionsController < Spree::BaseController
   end
 
   def create_user_session(data)
-    guest_order = current_order
     @user_session = UserSession.new(data)
     @user_session.save do |result|
       if result
         # Should restore last uncompleted order and add current(guest) order to it, if exists.
         order = @user_session.record.orders.last(:conditions => {:completed_at => nil})
         if order
-          if guest_order
+          if (session[:order_token] && guest_order = Order.find(:first, :conditions => {:token => session[:order_token], :user_id => nil, :completed_at => nil}))
             guest_order.line_items.each do |line_item|
               order.add_variant(line_item.variant, line_item.quantity)
             end
@@ -71,7 +70,6 @@ class UserSessionsController < Spree::BaseController
           end
           session[:order_token] = order.token
           session[:order_id] = order.id
-          session[:guest_token] = nil
         end
 
         respond_to do |format|
