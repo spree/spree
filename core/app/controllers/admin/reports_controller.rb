55c1a2ae045c2e8f3116572d2e3fc1f167913615
Roman Smirnov <roman@railsdog.com>
2010-10-28 15:03:02 +0400

    Updated sales_total report to use supported SL scopes.
    [#1687 state:resolved]

--- core/app/controllers/admin/reports_controller.rb
+++ core/app/controllers/admin/reports_controller.rb
@@ -12,22 +12,24 @@ class Admin::ReportsController < Admin::BaseController
   def sales_total
     params[:search] = {} unless params[:search]
 
-    if params[:search][:created_at_after].blank?
-      params[:search][:created_at_after] = Time.zone.now.beginning_of_month
+    if params[:search][:created_at_greater_than].blank?
+      params[:search][:created_at_greater_than] = Time.zone.now.beginning_of_month
     else
-      params[:search][:created_at_after] = Time.zone.parse(params[:search][:created_at_after]).beginning_of_day rescue Time.zone.now.beginning_of_month
+      params[:search][:created_at_greater_than] = Time.zone.parse(params[:search][:created_at_greater_than]).beginning_of_day rescue Time.zone.now.beginning_of_month
     end
 
-    if params[:search] && !params[:search][:created_at_before].blank?
-      params[:search][:created_at_before] = Time.zone.parse(params[:search][:created_at_before]).end_of_day rescue ""
+    if params[:search] && !params[:search][:created_at_less_than].blank?
+      params[:search][:created_at_less_than] = Time.zone.parse(params[:search][:created_at_less_than]).end_of_day rescue ""
     end
 
+    params[:search][:completed_at_not_null] ||= "1"
+    if params[:search].delete(:completed_at_not_null) == "1"
+      params[:search][:completed_at_not_null] = true
+    end
 
+    params[:search][:order] ||= "descend_by_created_at"
+    
     @search = Order.searchlogic(params[:search])
-    @search.completed_at_not_null
-    #set order by to default or form result
-    @search.order ||= "descend_by_created_at"
-
     @orders = @search.do_search
 
     @item_total = @search.do_search.sum(:item_total)
