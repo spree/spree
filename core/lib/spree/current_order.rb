3c0bdbb9b1d9ba081393ebfc5aacea29217d0cda
Sean Schofield <sean@railsdog.com>
2010-11-02 16:32:13 -0400

    Improved order association logic.  New orders are no longer associated with intermediate anonymous user before associating with the registered user.  New orders are also now associated with the current guest user (so guest user can make back to back orders now.)

--- core/lib/spree/current_order.rb
+++ core/lib/spree/current_order.rb
@@ -1,11 +1,19 @@
 module Spree
   module CurrentOrder
+
+    # This should be overridden by an auth-related extension which would then have the opporutnity to associate the new order with the
+    # current user before saving.
+    def before_save_new_order
+    end
+
     # The current incomplete order from the session for use in cart and during checkout
     def current_order(create_order_if_necessary = false)
       return @current_order if @current_order
       @current_order ||= Order.find_by_id(session[:order_id], :include => :adjustments)
       if create_order_if_necessary and (@current_order.nil? or @current_order.completed?)
-        @current_order = Order.create!
+        @current_order = Order.new
+        before_save_new_order
+        @current_order.save #create!
       end
       session[:order_id] = @current_order ? @current_order.id : nil
       @current_order
