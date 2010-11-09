c4f1dff9974f28e029b76f3416113fbaa949286c
Sean Schofield <sean@railsdog.com>
2010-11-08 22:33:34 -0500

    Implemented order mail bcc functionality.

--- core/lib/spree/mail_interceptor.rb
+++ core/lib/spree/mail_interceptor.rb
@@ -8,9 +8,14 @@ module Spree
     def self.delivering_email(message)
       return unless mail_method = MailMethod.current
       message.from ||= mail_method.preferred_mails_from
+
       if mail_method.preferred_intercept_email.present?
         message.subject = "[#{message.to}] #{message.subject}"
         message.to = mail_method.preferred_intercept_email
+      end
+
+      if mail_method.preferred_mail_bcc.present?
+        message.bcc = mail_method.preferred_mail_bcc
       end
     end
 
