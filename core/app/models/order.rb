2fb6fabdf671496bef06a4acbd0933e406d0ba0a
Daniel Konishi <dkonishi@gmail.com>
2010-11-08 22:45:48 -0200

    Refactoring: Make Order#create_user more readable

--- core/app/models/order.rb
+++ core/app/models/order.rb
@@ -350,7 +350,7 @@ class Order < ActiveRecord::Base
 
   private
   def create_user
-    self.email = user.email if self.user and user.email !~ /example.net/
+    self.email = user.email if self.user and !user.anonymous?
     self.user ||= User.anonymous!
   end
 
