b08312fd6403ad688c9fd9278e110449db740dd0
Sean Schofield <sean@railsdog.com>
2010-11-16 23:07:07 -0500

    Fixed some index issues ignored until Rails 3.0.2

--- core/db/migrate/20100908170204_remove_unique_index_on_users_email.rb
+++
@@ -1,9 +0,0 @@
-class RemoveUniqueIndexOnUsersEmail < ActiveRecord::Migration
-  def self.up
-    remove_index :users, :email
-  end
-
-  def self.down
-    add_index :users, :email, :unique => true
-  end
-end
