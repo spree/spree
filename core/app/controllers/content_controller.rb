411ccb76b5b8b6756de66d4a8a4c3746f9ea97f5
Sean Schofield <sean@railsdog.com>
2010-11-16 23:10:39 -0500

    Fixed problematic route.  Check the regex inside ContentController since Rails is limiting what regex we can use here.

--- core/app/controllers/content_controller.rb
+++ core/app/controllers/content_controller.rb
@@ -1,13 +1,22 @@
 class ContentController < Spree::BaseController
 
+  before_filter :render_404, :if => :static_asset
+
   rescue_from ActionView::MissingTemplate, :with => :render_404
   caches_page :show, :index, :if => Proc.new { Spree::Config[:cache_static_content] }
 
   def show
     render params[:path]
   end
-  
+
   def cvv
     render "cvv", :layout => false
-  end  
+  end
+
+  private
+  # Determines if the requested resource has a path similar to that of a static asset.  In this case do not go through the
+  # overhead of trying to render a template or whatever.
+  def static_asset
+    params[:path] =~ /^\/([^.]+)$/
+  end
 end
