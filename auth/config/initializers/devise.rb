c862fc3e8f90db344f570c2fa03cbfd11e733612
John Brien <iam@hybridindie.com>
2010-11-24 09:47:19 -0500

    Explicitly enable HTTP_AUTHORIZATION on Devise edge.

--- auth/config/initializers/devise.rb
+++ auth/config/initializers/devise.rb
@@ -25,8 +25,8 @@ Devise.setup do |config|
   # Tell if authentication through request.params is enabled. True by default.
   # config.params_authenticatable = true
 
-  # Tell if authentication through HTTP Basic Auth is enabled. True by default.
-  #config.http_authenticatable = false
+  # Tell if authentication through HTTP Basic Auth is enabled. False by default.
+  config.http_authenticatable = true
 
   # Set this to true to use Basic Auth for AJAX requests.  True by default.
   #config.http_authenticatable_on_xhr = false
