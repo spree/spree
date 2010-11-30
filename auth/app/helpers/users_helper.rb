ee86010feb6ba340ea059281cd0c677cc18eca90
John Brien <iam@hybridindie.com>
2010-11-30 17:26:16 -0500

    removed left over openid stuff from core

--- auth/app/helpers/users_helper.rb
+++ auth/app/helpers/users_helper.rb
@@ -1,13 +1,3 @@
 module UsersHelper       
-  def password_style(user)
-    show_openid ? "display:none" : ""
-  end         
-  def openid_style(user) 
-    show_openid ? "": "display:none"
-  end
-  
-  private 
-  def show_openid
-    Spree::Config[:allow_openid] and @user.openid_identifier
-  end
+
 end
