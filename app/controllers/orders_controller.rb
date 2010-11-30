7a07245ea48ef0014aa146e1ff6b220aae4ca178
sbeam <sbeam@onsetcorps.net>
2010-11-30 00:11:42 -0500

    adjust to show custom flash message on empty cart; add missing translation

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -32,11 +32,10 @@ class OrdersController < Spree::BaseController
 
   #override r_c default b/c we don't want to actually destroy, we just want to clear line items
   def destroy
-    self.notice = I18n.t(:basket_successfully_cleared)
     @order.line_items.clear
     @order.update_totals!
     after :destroy
-    set_flash :destroy
+    self.notice = I18n.t(:basket_successfully_cleared)
     response_for :destroy
   end
 
