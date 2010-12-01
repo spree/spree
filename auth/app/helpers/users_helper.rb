995f2cf414d728b272b9436c065b5d9522f4c80c
John Brien <iam@hybridindie.com>
2010-12-01 08:34:55 -0500

    added depreciation notices for OpenID removal

--- auth/app/helpers/users_helper.rb
+++ auth/app/helpers/users_helper.rb
@@ -1,3 +1,11 @@
 module UsersHelper       
+  def password_style(user)
+    ""
+    puts "[SPREE] Password style has be depreciated due to the removal of OpenID from the Auth Gem. Please install the spree_social gem to regain this functionality and more."
+  end         
+  def openid_style(user) 
+    "display:none"
+    puts "[SPREE] Password style has be depreciated due to the removal of OpenID from the Auth Gem. Please install the spree_social gem to regain this functionality and more."
+  end
 
 end
