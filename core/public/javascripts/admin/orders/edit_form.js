54accd78caaa672598ba596956aa13950b72281c
Trung LE <joneslee85@gmail.com>
2010-12-11 06:52:55 +1100

    fix for Order Edit line item calculation re-populate

--- core/public/javascripts/admin/orders/edit_form.js
+++ core/public/javascripts/admin/orders/edit_form.js
@@ -1,15 +1,16 @@
   $.each($('td.qty input'), function(i, inpt){
-    $(inpt).delayedObserver(0.5, function(object, value) {
 
-      var id = object.attr('id').replace("order_line_items_attributes_", "").replace("_quantity", "");
+    $(inpt).delayedObserver(function() {
+
+      var id = $(this).attr('id').replace("order_line_items_attributes_", "").replace("_quantity", "");
       id = "#order_line_items_attributes_" + id + "_id";
 
       jQuery.ajax({
         type: "POST",
         url: "/admin/orders/" + $('input#order_number').val() + "/line_items/" + $(id).val(),
-        data: ({_method: "put", "line_item[quantity]": value}),
+        data: ({_method: "put", "line_item[quantity]": $(this).val()}),
         success: function(html){ $('#order-form-wrapper').html(html)}
       });
 
-    });
+    }, 0,5);
   });
