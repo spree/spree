49c64b71f472b2cea059773695d958592be71458
Roman Smirnov <roman@railsdog.com>
2010-12-12 18:53:42 +0300

    Use product filter only if it's defined.
    [#1797 state:resolved]

--- core/app/models/taxon.rb
+++ core/app/models/taxon.rb
@@ -18,14 +18,13 @@ class Taxon < ActiveRecord::Base
   # this method should be customized to your own site
   include ::ProductFilters  # for detailed defs of filters
   def applicable_filters
-    fs  = []
+    fs = []
     # fs << ProductFilters.taxons_below(self)
     ## unless it's a root taxon? left open for demo purposes
-    fs += [
-      ProductFilters.price_filter,
-      ProductFilters.brand_filter,
-      #ProductFilters.selective_brand_filter(self) 
-      ]
+    
+    fs << ProductFilters.price_filter if ProductFilters.respond_to?(:price_filter)
+    fs << ProductFilters.brand_filter if ProductFilters.respond_to?(:brand_filter)
+    fs
   end
 
   # Creates permalink based on .to_url method provided by stringx gem
