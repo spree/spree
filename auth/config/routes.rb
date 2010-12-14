eeae00161927a3aa8d1af94425803b474a3be942
Adam Wróbel <adam@fluxinc.ca>
2010-12-14 17:06:08 +0100

    Fix password reset routes for devise.
    /users/:id/edit was blocking access to devise's /users/password/edit.

--- auth/config/routes.rb
+++ auth/config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  resources :users, :only => [:edit, :update]
   devise_for :users,
              :controllers => { :sessions => 'user_sessions',
                                :registrations => 'user_registrations',
                                :passwords => "user_password_resets"},
              :skip => [:unlocks, :omniauth_callbacks],
              :path_names => { :sign_out => 'logout'}
+  resources :users, :only => [:edit, :update]
 
   devise_scope :user do
     get "/login" => "user_sessions#new", :as => :login
