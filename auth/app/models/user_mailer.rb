78491263530f1929d645a66bd571683e77a480c9
Adam Wróbel <adam@fluxinc.ca>
2010-12-14 16:58:19 +0100

    Fix user_mailer password reset method.
    - devise expects UserMailer to have reset_password_instructions method not password_reset_instructions
    - some routes name changed with devise
    - UserMailer should not set :from or else the MailerMethod will not overwrite it

--- auth/app/models/user_mailer.rb
+++ auth/app/models/user_mailer.rb
@@ -1,9 +1,8 @@
 class UserMailer < ActionMailer::Base
   default_url_options[:host] = Spree::Config[:site_url]
-  default :from => Spree::Config[:mails_from]
 
-  def password_reset_instructions(user)
-    @edit_password_reset_url = edit_password_reset_url(user.perishable_token)
+  def reset_password_instructions(user)
+    @edit_password_reset_url = edit_user_password_url(:reset_password_token => user.reset_password_token)
     mail(:to => user.email,
          :subject => Spree::Config[:site_name] + ' ' + I18n.t("password_reset_instructions"))
   end
