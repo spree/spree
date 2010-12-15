a47c02dbd58edd887bc6c2ae25537946dbadc866
John Brien <iam@hybridindie.com>
2010-12-15 14:46:48 -0500

    Migration adjustment for db column (fixes problem with rememberme token.)

--- auth/db/migrate/20101214150824_convert_user_remember_field.rb
+++ auth/db/migrate/20101214150824_convert_user_remember_field.rb
@@ -1,9 +1,11 @@
 class ConvertUserRememberField < ActiveRecord::Migration
   def self.up
-    change_column :users, :remember_created_at, :datetime
+    remove_column :users, :remember_created_at
+    add_column :users, :remember_created_at, :datetime
   end
 
   def self.down
-    change_column :users, :remember_created_at, :string
+    remove_column :users, :remember_created_at
+    add_column :users, :remember_created_at, :string
   end
 end
