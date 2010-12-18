beb894eb76c7f6bf2de7096a84572f0afc77ee3b
Piotr Sarnacki <drogus@gmail.com>
2010-12-18 17:26:00 +0100

    Bring back capybara

--- auth/features/step_definitions/page_content_steps.rb
+++ auth/features/step_definitions/page_content_steps.rb
@@ -1,15 +1,15 @@
 Then /^I should see "([^\"]*)" translation$/ do |key|
-  response.should contain(I18n.t(key))
+  page.should have_content(I18n.t(key))
 end
 
 Then /^I should be signed in$/ do
-  response.should contain("Logout")
+  page.should have_content("Logout")
 end
 
 Then /^I should be signed out$/ do
-  response.should contain("Log In")
+  page.should have_content("Log In")
 end
 
 Then /^I should see error messages$/ do
-  response.should contain("errors prohibited this record from being saved")
+  page.should have_css("#errorExplanation")
 end
