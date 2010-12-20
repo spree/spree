9c103fa97f1b46233b2419d87e29c58b47afbc8f
Sean Schofield <sean@railsdog.com>
2010-12-20 18:03:28 -0500

    Add monkey patch for devise until the new version comes out (otherwise it blows away session data after login.)

--- auth/lib/spree_auth.rb
+++ auth/lib/spree_auth.rb
@@ -12,9 +12,17 @@ module SpreeAuth
         Rails.env.production? ? require(c) : load(c)
       end
 
+      # monkey patch until new version of devise comes out
+      # https://github.com/plataformatec/devise/commit/ec5bfe9119d0e1e633629793b0de1f58f89622dc
+      Devise::IndifferentHash.class_eval do
+        def [](key)
+          super(convert_key(key))
+        end
+        def to_hash; Hash.new.update(self) end
+      end
     end
-    config.to_prepare &method(:activate).to_proc
 
+    config.to_prepare &method(:activate).to_proc
     ActiveRecord::Base.class_eval { include Spree::TokenResource }
   end
 end
