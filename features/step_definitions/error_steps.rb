7329ed90898d22c7234ee5c6344a73cb142a747d
Piotr Sarnacki <drogus@gmail.com>
2010-12-21 18:51:49 +0100

    Removed old cucumber features

--- features/step_definitions/error_steps.rb
+++
@@ -1,3 +0,0 @@
-Then /^I should see error messages$/ do
-  assert page.has_css?("div.errorExplanation") || page.has_css?("div.errors")
-end
--- features/step_definitions/role_steps.rb
+++
@@ -1,8 +0,0 @@
-Given /^no users exist with the role: "([^\"]*)"$/ do |role|
-  Role.find_by_name(role).users.clear
-
-  # hack to reset instance variables on rack metal
-  %w(@admin_defined @status).each { |ivar|
-    CreateAdminUser.instance_variable_set(ivar.to_sym, nil)
-  }
-end
