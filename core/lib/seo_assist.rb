3527905e48e64c048cfa71f2e2496298242c4a24
Neeraj Singh <neerajdotname@gmail.com>
2010-12-31 17:41:54 -0500

    refactor existing code and make it more DRY

--- core/lib/seo_assist.rb
+++ core/lib/seo_assist.rb
@@ -10,26 +10,26 @@ class SeoAssist
     taxon_id = params['taxon']
     if !taxon_id.blank? && !taxon_id.is_a?(Hash) && taxon = Taxon.find(taxon_id)
       params.delete('taxon')
-      query = build_query(params)
 
       #ensures no trailing / for taxon urls
       #TODO ensure that permalink is never stored with a trailing slash
       permalink = taxon.permalink[0...-1]
+      return build_response(params, "/t/#{permalink}" )
 
-      new_location = "/t/#{permalink}"
-      new_location += '?' + query unless query.blank?
-      return [301, { 'Location'=> new_location }, []]
     elsif env["PATH_INFO"] =~ /^\/(t|products)(\/\S+)?\/$/
       #ensures no trailing / for taxon and product urls
-      query = build_query(params)
-      new_location = env["PATH_INFO"][0...-1]
-      new_location += '?' + query unless query.blank?
-      return [301, { 'Location'=> new_location }, []]
+      return build_response(params, env["PATH_INFO"][0...-1])
     end
     @app.call(env)
   end
 
   private
+
+  def build_response(params, location)
+    query = build_query(params)
+    new_location = location + '?' + query unless query.blank?
+    [301, { 'Location'=> new_location }, []]
+  end
 
   def build_query(params)
     params.map { |k, v|
