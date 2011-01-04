e4aa4067a4686d77e974c22b9d642b603be1a2e8
Sean Schofield <sean@railsdog.com>
2011-01-04 16:07:07 -0500

    Make sure that an access token is stored in the session every time a new order is created and the user is not authenticated.  This fixes a previous issue where user was denied access after viewing an empty cart twice in a row without adding to the cart.

--- auth/app/models/spree_current_order_decorator.rb
+++ auth/app/models/spree_current_order_decorator.rb
@@ -5,4 +5,10 @@ Spree::CurrentOrder.module_eval do
     @current_order.user ||= current_user
   end
 
+  def after_save_new_order
+    # make sure the user has permission to access the order (if they are a guest)
+    return if current_user
+    session[:access_token] = @current_order.token
+  end
+
 end
