3776ab533218d170d446d05d5e0953cf2207a732
Sean Schofield <sean@railsdog.com>
2011-01-08 11:22:46 -0500

    Removed defunct test_template file.

--- core/spec/test_template.rb
+++
@@ -1,5 +0,0 @@
-# rails site template for creating a test site to be used in testing
-gem "spree_core", :path => ".."
-gem 'rspec', '2.0.0.beta.19', :group => 'test'
-gem 'rspec-rails', '2.0.0.beta.19', :group => 'test'
-gem 'factory_girl_rails', :group => 'test'
