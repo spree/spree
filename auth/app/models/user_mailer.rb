032b8a78cd969676df9c3c78ddf0ad8a85e1916b
Neeraj Singh <neerajdotname@gmail.com>
2011-01-10 14:53:35 -0500

    fix the failing cucumber test

--- auth/app/models/user_mailer.rb
+++ auth/app/models/user_mailer.rb
@@ -1,8 +1,10 @@
 class UserMailer < ActionMailer::Base
-  default_url_options[:host] = Spree::Config[:site_url]
 
   def reset_password_instructions(user)
+    default_url_options[:host] = Spree::Config[:site_url]
+
     @edit_password_reset_url = edit_user_password_url(:reset_password_token => user.reset_password_token)
+
     mail(:to => user.email,
          :subject => Spree::Config[:site_name] + ' ' + I18n.t("password_reset_instructions"))
   end
