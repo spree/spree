25d1dd2bbed2aa9f3ca807d29dcd047bef5d801a
Gabe Anzelini <me@gabeanzelini.com>
2011-01-09 17:50:42 -0800

    add option type to product with variants

--- core/app/models/product_option_type.rb
+++ core/app/models/product_option_type.rb
@@ -2,4 +2,15 @@ class ProductOptionType < ActiveRecord::Base
   belongs_to :product
   belongs_to :option_type
   acts_as_list :scope => :product
+  
+  before_save :set_product_variants_to_first_option_value
+  
+  
+  private
+  
+  def set_product_variants_to_first_option_value
+    product.variants.each do |variant|
+      variant.option_values << option_type.option_values.first
+    end
+  end
 end
