ffe3377b553d62d6b52634166b0156d32a23ee82
Neeraj Singh <neerajdotname@gmail.com>
2011-01-12 16:41:35 -0500

    fix cucumber api feature

--- api/features/step_definitions/orders_steps.rb
+++ api/features/step_definitions/orders_steps.rb
@@ -1,12 +1,16 @@
 Given /^I have (\d+) orders$/ do |o|
   user = Fabricate(:user)
-  @orders = Array.new(o.to_i, Fabricate(:order, :user => user))
+  Order.delete_all
+  Order.create(:email => user.email,:number => 100)
+  Order.create(:email => user.email,:number => 101)
+  Order.create(:email => user.email,:number => 102)
+  Order.create(:email => user.email,:number => 103)
+  Order.create(:email => user.email,:number => 104)
+  @orders = Order.all
 end
 
 When /^I send a GET request to "([^"]*)"$/ do |path|
   get path
-  puts last_request.inspect
-  puts last_response.inspect
 end
 
 Then /^the response status should be "([^"]*)"$/ do |status|
@@ -15,5 +19,6 @@ end
 
 Then /^the response should be an array with (\d+) "([^"]*)" elements$/ do |num, name|
   page = JSON.parse(last_response.body)
+  #puts page.inspect
   page.map { |d| d[name] }.length.should == num.to_i
 end
