9dcc7e8474f35f6d56d745908ddda2d25a13f7c6
Neeraj Singh <neerajdotname@gmail.com>
2011-01-14 11:10:57 -0500

    spec for product

---
+++ core/spec/models/product_spec.rb
@@ -0,0 +1,22 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe Product do
+
+  context "shoulda validations" do
+    it { should belong_to(:tax_category) }
+    it { should belong_to(:shipping_category) }
+    it { should have_many(:product_option_types) }
+    it { should have_many(:option_types) }
+    it { should have_many(:product_properties) }
+    it { should have_many(:properties) }
+    it { should have_many(:images) }
+    it { should have_and_belong_to_many(:product_groups) }
+    it { should have_and_belong_to_many(:taxons) }
+    it { should validate_presence_of(:price) }
+    it { should validate_presence_of(:permalink) }
+  end
+
+  context "factory_girl" do
+    specify { Factory(:product).new_record?.should be_false }
+  end
+end
