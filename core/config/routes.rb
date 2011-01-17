5fce31d1cb4e9134f53f1068137dcc19a4d89ac0
Roman Smirnov <roman@railsdog.com>
2011-01-17 14:31:01 +0300

    Restored support of dynamic product groups combinations. For example: http://localhost:3000/s/price_between/10,19/ascend_by_master_price

--- core/config/routes.rb
+++ core/config/routes.rb
@@ -49,7 +49,7 @@ Rails.application.routes.draw do
   end
 
   #   # Search routes
-  match 's/:product_group_query' => 'products#index', :as => :simple_search
+  match 's/*product_group_query' => 'products#index', :as => :simple_search
   match '/pg/:product_group_name' => 'products#index', :as => :pg_search
   match '/t/*id/s/*product_group_query' => 'taxons#show', :as => :taxons_search
   match 't/*id/pg/:product_group_name' => 'taxons#show', :as => :taxons_pg_search
