6a9f8bd04793fac3a46734b7f5f80cb2db26bc1e
Neeraj Singh <neerajdotname@gmail.com>
2011-01-18 11:39:53 -0500

    move some steps from user_steps to authentication_steps

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -1,35 +1,5 @@
-Given /^I am signed up as "(.+)\/(.+)"$/ do |email, password|
-  Factory(:user, :email => email, :password => password, :password_confirmation => password)
-end
-
-Given /^I have an admin account of "(.+)\/(.+)"$/ do |email, password|
-  Factory(:admin_user, :email => email, :password => password, :password_confirmation => password)
-end
-
-When /^I sign in as "(.*)\/(.*)"$/ do |email, password|
-  When %{I go to the sign in page"}
-  And %{I fill in "Email" with "#{email}"}
-  And %{I fill in "Password" with "#{password}"}
-  And %{I press "Log In"}
-end
-
 Given /^no user exists with an email of "(.*)"$/ do |email|
   User.find_by_email(email).should be_nil
-end
-
-Then /^I should be logged out$/ do
-  page.should have_content("Log In")
-end
-
-Then /^I should be logged in$/ do
-  page.should_not have_content("Log In")
-end
-
-Given /^I am logged out$/ do
-  begin
-    click_link("Logout")
-  rescue Capybara::ElementNotFound
-  end
 end
 
 Then /^(\d+) users should exist$/ do |c|
