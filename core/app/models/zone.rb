c119bc1b1ff865aa89beeadb4317e267d3ba1f3c
Trung Lê <joneslee85@gmail.com>
2011-01-18 16:42:40 +1100

    add destroy dependency for TaxRate <-> Zone association
    [#1823 state:resolved]

--- core/app/models/zone.rb
+++ core/app/models/zone.rb
@@ -1,6 +1,6 @@
 class Zone < ActiveRecord::Base
   has_many :zone_members
-  has_many :tax_rates
+  has_many :tax_rates, :dependent => :destroy
   has_many :shipping_methods
 
   validates :name, :presence => true, :uniqueness => true
