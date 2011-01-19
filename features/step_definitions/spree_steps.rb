3474b8ce51adf7bb14be08d0b8731c62b2dd1d2b
Neeraj Singh <neerajdotname@gmail.com>
2011-01-19 13:28:30 -0500

    add I debug step definition

--- features/step_definitions/spree_steps.rb
+++ features/step_definitions/spree_steps.rb
@@ -13,3 +13,10 @@ end
 Then /^I should see "([^\"]*)" translation$/ do |key|
   page.should have_content(I18n.t(key))
 end
+
+Then /^I debug$/ do
+  require 'ruby-debug'; debugger
+  breakpoint
+  0
+end
+
