2e90d6f1cf341a3168fc803d1b038593c9a61783
Neeraj Singh <neerajdotname@gmail.com>
2011-01-19 13:28:16 -0500

    add admin_orders feature and correspdong step definitions

--- core/spec/factories/order_factory.rb
+++ core/spec/factories/order_factory.rb
@@ -2,6 +2,7 @@ Factory.define(:order) do |record|
   # associations:
   record.association(:user, :factory => :user)
   record.association(:bill_address, :factory => :address)
+  record.completed_at nil
 end
 
 Factory.define :order_with_totals, :parent => :order do |f|
