6a600770cee947e1753bf8f96017b5bf8fc511f6
Neeraj Singh <neerajdotname@gmail.com>
2011-01-19 13:37:12 -0500

    admin orders

--- core/features/admin_orders.feature
+++ core/features/admin_orders.feature
@@ -5,9 +5,12 @@ Feature: Admin home page
   Scenario: Visiting admin home page
     Given 2 custom orders
     And I go to the admin home page
-    Then show me the page
     Then I should see listing orders tabular attributes with completed_at descending
     When I follow "Order Date"
     Then I should see listing orders tabular attributes with completed_at ascending
+    When I follow "Order"
+    Then I should see listing orders tabular attributes with order number ascending
+    When I follow "Order"
+    Then I should see listing orders tabular attributes with order number descending
 
 
--- core/features/step_definitions/admin_orders.rb
+++ core/features/step_definitions/admin_orders.rb
@@ -20,15 +20,18 @@ end
 
 Then /^I should see listing orders tabular attributes with completed_at ascending$/ do
   output = tableish('table#listing_orders tr', 'td,th')
-  data = output[0]
-  data[0].should match(/Order Date/)
-  data[1].should == "Order"
-  data[2].should == "Status"
-  data[3].should == "Payment State"
-  data[4].should == "Shipment State"
-  data[5].should == "Customer"
-  data[6].should == "Total"
-
   data = output[1]
   data[0].should == Order.limit(1).order('completed_at asc').to_a.first.completed_at.strftime('%Y-%m-%d')
+end
+
+Then /^I should see listing orders tabular attributes with order number ascending$/ do
+  output = tableish('table#listing_orders tr', 'td,th')
+  data = output[1]
+  data[1].should == Order.limit(1).order('number asc').to_a.first.number
+end
+
+Then /^I should see listing orders tabular attributes with order number descending$/ do
+  output = tableish('table#listing_orders tr', 'td,th')
+  data = output[1]
+  data[1].should == Order.limit(1).order('number desc').to_a.first.number
 end
