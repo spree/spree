2f0443da9e47dc41d39a2b466dfd870e4c0eea68
Neeraj Singh <neerajdotname@gmail.com>
2011-01-20 11:42:15 -0500

    move some factories loading code from env.rb to factories.rb

--- core/spec/factories.rb
+++ core/spec/factories.rb
@@ -1,3 +1,11 @@
+Zone.class_eval do
+  def self.global
+    find_by_name("GlobalZone") || Factory(:global_zone)
+  end
+end
+
+require 'factory_girl'
+
 Dir["#{File.dirname(__FILE__)}/factories/**"].each do |f|
   fp =  File.expand_path(f)
   require fp
