390a5bbba21c3cc6b736940e65811ec771dcbd21
Neeraj Singh <neerajdotname@gmail.com>
2011-01-20 13:00:20 -0500

    spec for shipping_method

--- core/spec/models/shipping_method_spec.rb
+++ core/spec/models/shipping_method_spec.rb
@@ -2,9 +2,9 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 describe ShippingMethod do
 
-  let(:order) { Order.new }
-
-  it "should be available if the ship address falls within the method's zone"
-  #TODO - write some tests about availability - waiting to see if we like the older implementation before we bother with the tests
+  context 'factory' do
+    let(:shipping_method) { Factory(:shipping_method) }
+    specify { shipping_method.new_record?.should be_false }
+  end
 
 end
