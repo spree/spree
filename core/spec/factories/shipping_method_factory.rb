363d410545fad81eb2a8777533bb813f148c5162
Neeraj Singh <neerajdotname@gmail.com>
2011-01-20 13:06:07 -0500

    fix warning warning: Object#id will be deprecated; use Object#object_id

--- core/spec/factories/shipping_method_factory.rb
+++ core/spec/factories/shipping_method_factory.rb
@@ -2,5 +2,5 @@ Factory.define :shipping_method do |f|
   f.zone {|a| a.association(:zone) }
   f.name 'UPS Ground'
   f.display_on :front_end
-  f.calculator {|a| a.association(:calculator, :calculable_id => id, :calculable_type => "shipping_method") }
+  f.after_create { |sm| Factory(:calculator, :calculable_id => sm.id, :calculable_type => "shipping_method") }
 end
