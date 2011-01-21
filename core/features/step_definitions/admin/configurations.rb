0669dcfd63c238d0caaf593b03d5ba352c4a5d92
Neeraj Singh <neerajdotname@gmail.com>
2011-01-21 11:27:18 -0500

    cukes for admin/states

--- core/features/step_definitions/admin/configurations.rb
+++ core/features/step_definitions/admin/configurations.rb
@@ -1,3 +1,14 @@
+Then /^I should see listing states tabular attributes$/ do
+  output = tableish('table#listing_states tr', 'td,th')
+  data = output[0]
+  data[0].should == 'Name'
+  data[1].should == "Abbreviation"
+
+  data = output[1]
+  data[0].should == State.limit(1).order('name asc').to_a.first.name
+  data[1].should == State.limit(1).order('name asc').to_a.first.abbr
+end
+
 Then /^I should see listing zones tabular attributes$/ do
   output = tableish('table#listing_zones tr', 'td,th')
   data = output[0]
