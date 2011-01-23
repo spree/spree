4ffd9408a3f11eec297e9f3efac3fcc7231b34a4
Neeraj Singh <neerajdotname@gmail.com>
2011-01-23 13:44:35 -0500

    replace and with && and break long line into two

--- features/support/stop.rb
+++ features/support/stop.rb
@@ -1,7 +1,7 @@
 After do |scenario|
-  if scenario.failed? and scenario.source_tag_names.include?("@wip") and
-     scenario.source_tag_names.include?("@stop")
-    puts "Scenario failed. We're in @wip mode, so I've stopped here. You can inspect the issue and leave the console after that."
+  if scenario.failed? && scenario.source_tag_names.include?("@wip") && scenario.source_tag_names.include?("@stop")
+    puts "Scenario failed. We're in @wip mode, so I've stopped here." <<
+          "You can inspect the issue and leave the console after that."
     require 'irb'
     require 'irb/completion'
     ARGV.clear
