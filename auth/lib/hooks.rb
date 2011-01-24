e2e6484668a517e3447d6a7bb25507898fa21516
Neeraj Singh <neerajdotname@gmail.com>
2011-01-24 17:13:02 -0500

    move shared login_bar feature from core to auth

--- auth/lib/hooks.rb
+++ auth/lib/hooks.rb
@@ -1,5 +1,6 @@
 class SpreeStaticContentHooks < Spree::ThemeSupport::HookListener
 
   replace :admin_login_navigation_bar, :partial => "layouts/admin/login_nav"
+  replace :shared_login_bar, :partial => "shared/login_bar"
 
 end
