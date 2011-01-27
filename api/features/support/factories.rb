b76cf598e0aa1bde92fedbf6450b11a8e94fae27
Neeraj Singh <neerajdotname@gmail.com>
2011-01-27 10:10:44 -0500

    remove factories that are not needed

--- api/features/support/factories.rb
+++ api/features/support/factories.rb
@@ -2,10 +2,6 @@ Factory.sequence :email do |n|
   "somebody#{n}@example.com"
 end
 
-Factory.sequence :order_number do |n|
-  n
-end
-
 Factory.sequence :user_authentication_token do |n|
   "xxxx#{Time.now.to_i}#{rand(1000)}#{n}xxxxxxxxxxxxx"
 end
@@ -16,9 +12,4 @@ Factory.define :user do |f|
   f.authentication_token { Factory.next(:user_authentication_token) }
   f.password "secret"
   f.password_confirmation "secret"
-end
-
-Factory.define :order do |f|
-  f.number { Factory.next(:order_number) }
-  f.email { Factory.next(:email) }
 end
