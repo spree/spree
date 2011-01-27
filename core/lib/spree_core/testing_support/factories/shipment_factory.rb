04f78b42f5cc5e8ba2080dae2b4e10965a5544f8
Neeraj Singh <neerajdotname@gmail.com>
2011-01-27 11:42:03 -0500

    remove comments

--- core/lib/spree_core/testing_support/factories/shipment_factory.rb
+++ core/lib/spree_core/testing_support/factories/shipment_factory.rb
@@ -7,16 +7,3 @@ Factory.define :shipment do |f|
   f.address { Factory(:address) }
   f.state "pending"
 end
-
-  #create_table "shipments", :force => true do |t|
-    #t.integer  "order_id"
-    #t.integer  "shipping_method_id"
-    #t.string   "tracking"
-    #t.datetime "created_at"
-    #t.datetime "updated_at"
-    #t.string   "number"
-    #t.decimal  "cost",               :precision => 8, :scale => 2
-    #t.datetime "shipped_at"
-    #t.integer  "address_id"
-    #t.string   "state"
-  #end
