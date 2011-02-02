c2dfb34cb3b255243356dada0f71ee0bec6b5b7b
Neeraj Singh <neerajdotname@gmail.com>
2011-02-02 13:08:21 -0500

    cuke for admin users search

--- core/features/step_definitions/admin/users.rb
+++ core/features/step_definitions/admin/users.rb
@@ -7,3 +7,9 @@ Then /^I should see listing users tabular attributes with order (.*)$/ do |order
   data[0].should == User.limit(1).order(order).to_a.first.email
 end
 
+Then /^I should see listing users tabular attributes for search result case$/ do
+  output = tableish('table#listing_users tr', 'td,th')
+  output.size.should == 2
+  data = output[1]
+  data[0].should == 'a@example.com'
+end
