92228911dc493db2cf498089520b333fb65ffaf5
Trung Lê <joneslee85@gmail.com>
2011-02-05 22:40:17 +1100

    states controller refactor

--- core/app/models/country.rb
+++ core/app/models/country.rb
@@ -1,10 +1,8 @@
 class Country < ActiveRecord::Base
   has_many :states
 
-  has_one     :zone_member, :as => :zoneable
-  has_one     :zone,        :through => :zone_member
-
-  scope :order_by_name, :order => :name
+  has_one :zone_member, :as => :zoneable
+  has_one :zone, :through => :zone_member
 
   validates :name, :iso_name, :presence => true
 
--- core/app/models/state.rb
+++ core/app/models/state.rb
@@ -1,9 +1,8 @@
 class State < ActiveRecord::Base
   belongs_to  :country
-  scope :order_by_name, order(:name)
 
-  has_one     :zone_member, :as => :zoneable
-  has_one     :zone,        :through => :zone_member
+  has_one :zone_member, :as => :zoneable
+  has_one :zone, :through => :zone_member
 
   validates :country, :name, :presence => true
 
