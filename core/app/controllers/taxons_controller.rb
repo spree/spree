d0da98403ca1096ef68aa1a023954ad5ac7b2312
Trung Lê <joneslee85@gmail.com>
2011-02-08 01:53:54 +1100

    Removed resource_controller from TaxonsController

--- core/app/controllers/taxons_controller.rb
+++ core/app/controllers/taxons_controller.rb
@@ -1,20 +1,15 @@
 class TaxonsController < Spree::BaseController
   #prepend_before_filter :reject_unknown_object, :only => [:show]
   before_filter :load_data, :only => :show
-  resource_controller
-  actions :show
+
   helper :products
 
   private
   def load_data
-    @taxon ||= object
+    @taxon ||= Taxon.where(:permalink => params[:id]).first
     params[:taxon] = @taxon.id
     @searcher = Spree::Config.searcher_class.new(params)
     @products = @searcher.retrieve_products
-  end
-
-  def object
-    @object ||= end_of_association_chain.find_by_permalink(params[:id])
   end
 
   def accurate_title
