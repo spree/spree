2a17310a600edc8c7783570b7e712fcd29276c19
Neeraj Singh <neerajdotname@gmail.com>
2011-02-08 14:19:43 -0500

    allow link_to_delete to accept title and other html_options for a delete link

--- core/app/helpers/admin/navigation_helper.rb
+++ core/app/helpers/admin/navigation_helper.rb
@@ -47,7 +47,7 @@ module Admin::NavigationHelper
     link_to_with_icon('exclamation', t("clone"), clone_admin_product_url(resource))
   end
 
-  def link_to_delete(resource, options = {})
+  def link_to_delete(resource, options = {}, html_options={})
     options.assert_valid_keys(:url, :caption, :title, :dataType, :success, :name)
 
     options.reverse_merge! :url => object_url(resource) unless options.key? :url
@@ -68,7 +68,7 @@ module Admin::NavigationHelper
           success: #{options[:success]}
         });
       }
-    });"
+    });", html_options
   end
 
   def link_to_with_icon(icon_name, text, url, options = {})
