8984e784d49ac5bf4bcd59f2d03017c34ced1c48
Neeraj Singh <neerajdotname@gmail.com>
2011-02-08 12:18:41 -0500

    add a FIXME message

--- core/lib/scopes/variant.rb
+++ core/lib/scopes/variant.rb
@@ -1,5 +1,5 @@
 module Scopes::Variant
-  # WARNING tested only under sqlite and postgresql
+  #FIXME WARNING tested only under sqlite and postgresql
   Variant.scope :descend_by_popularity, lambda{
       order('COALESCE((SELECT COUNT(*) FROM  line_items GROUP BY line_items.variant_id HAVING line_items.variant_id = variants.id), 0) DESC')
   }
