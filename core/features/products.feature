f771917b57198b2e3ab2500804a4a265d44a9cab
Neeraj Singh <neerajdotname@gmail.com>
2011-02-10 10:22:09 -0500

    more cukes for products listing

--- core/features/products.feature
+++ core/features/products.feature
@@ -1,20 +1,30 @@
 Feature: Visiting products
 
-  @wip @stop
-  Scenario: Visiting products page
-    Given the following products exist:
-      | name                |
-      | apache baseball cap |
-      | zomg shirt          |
+  Background:
     Given the following taxonomies exist:
       | name        |
       | Brand       |
       | Categories  |
-    Given the custom taxons exist
-    And I go to the home page
-    Then show me the page
+    Given the custom taxons and custom products exist
 
+  Scenario: visit products page
+    When I go to the home page
     When I fill in "keywords" with "shirt"
     When I press "Search"
-    Then I should see "zomg shirt___"
+    Then verify products listing for top search result
 
+  Scenario: visit brand Ruby on Rails
+    And I go to the home page
+    When I follow "Ruby on Rails"
+    Then verify products listing for Ruby on Rails brand
+
+  Scenario: visit brand Ruby
+    And I go to the home page
+    When I follow "Ruby"
+    Then verify products listing for Ruby brand
+
+  Scenario: visit brand Apache
+    And I go to the home page
+    When I follow "Apache"
+    Then show me the page
+    Then verify products listing for Apache brand
--- core/features/step_definitions/product.rb
+++ core/features/step_definitions/product.rb
@@ -1,10 +1,38 @@
-Given /^the custom taxons exist$/ do
+Given /^the custom taxons and custom products exist$/ do
 
   taxonomy = Taxonomy.find_by_name('Brand')
   root = taxonomy.root
-  ["Ruby on Rails", "Ruby", "Apache"].each do |name|
-    taxonomy.taxons.create(:name => name, :parent_id => root.id)
-  end
+  taxon = taxonomy.taxons.create(:name => "Ruby on Rails", :parent_id => root.id)
+
+  product = Factory(:product, :name => "Ruby on Rails Ringer T-shirt", :price => "17.99")
+  product.taxons << taxon
+
+  product = Factory(:product, :name => "Ruby on Rails Mug", :price => "13.99")
+  product.taxons << taxon
+
+  product = Factory(:product, :name => "Ruby on Rails Tote", :price => "15.99")
+  product.taxons << taxon
+
+  product = Factory(:product, :name => "Ruby on Rails Bag", :price => "22.99")
+  product.taxons << taxon
+
+  product = Factory(:product, :name => "Ruby on Rails Baseball Jersey", :price => "19.99")
+  product.taxons << taxon
+
+  product = Factory(:product, :name => "Ruby on Rails Stein", :price => "16.99")
+  product.taxons << taxon
+
+  product = Factory(:product, :name => "Ruby on Rails Jr. Spaghetti", :price => "19.99")
+  product.taxons << taxon
+
+  taxon = taxonomy.taxons.create(:name => "Ruby", :parent_id => root.id)
+  product = Factory(:product, :name => "Ruby Baseball Jersey", :price => "19.99")
+  product.taxons << taxon
+
+  taxon = taxonomy.taxons.create(:name => "Apache", :parent_id => root.id)
+  product = Factory(:product, :name => "Apache Baseball Jersey", :price => "19.99")
+  product.taxons << taxon
+
 
   taxonomy = Taxonomy.find_by_name('Categories')
   root = taxonomy.root
@@ -12,4 +40,39 @@ Given /^the custom taxons exist$/ do
     taxonomy.taxons.create(:name => name, :parent_id => root.id)
   end
 
+end
+
+Then /^verify products listing for top search result$/ do
+  page.all('ul.product-listing li').size.should == 1
+  tmp = page.all('ul.product-listing li a').map(&:text).flatten.compact
+  tmp.delete("")
+  tmp.sort!.should == ["Ruby on Rails Ringer T-shirt $17.99"]
+end
+
+Then /^verify products listing for Ruby on Rails brand$/ do
+  page.all('ul.product-listing li').size.should == 7
+  tmp = page.all('ul.product-listing li a').map(&:text).flatten.compact
+  tmp.delete("")
+  array = ["Ruby on Rails Bag $22.99",
+   "Ruby on Rails Baseball Jersey $19.99",
+   "Ruby on Rails Jr. Spaghetti $19.99",
+   "Ruby on Rails Mug $13.99",
+   "Ruby on Rails Ringer T-shirt $17.99",
+   "Ruby on Rails Stein $16.99",
+   "Ruby on Rails Tote $15.99"]
+  tmp.sort!.should == array
+end
+
+Then /^verify products listing for Ruby brand$/ do
+  page.all('ul.product-listing li').size.should == 1
+  tmp = page.all('ul.product-listing li a').map(&:text).flatten.compact
+  tmp.delete("")
+  tmp.sort!.should == ["Ruby Baseball Jersey $19.99"]
+end
+
+Then /^verify products listing for Apache brand$/ do
+  page.all('ul.product-listing li').size.should == 1
+  tmp = page.all('ul.product-listing li a').map(&:text).flatten.compact
+  tmp.delete("")
+  tmp.sort!.should == ["Apache Baseball Jersey $19.99"]
 end
