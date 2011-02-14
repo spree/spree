bc0ef827061d90a18e305fca3bb6e8fa9d5e7e93
Peter Zlatnar <peter.zlatnar@gmail.com>
2011-02-14 15:03:47 +0100

    Add localization in mailers subject.

--- core/app/mailers/shipment_mailer.rb
+++ core/app/mailers/shipment_mailer.rb
@@ -4,7 +4,7 @@ class ShipmentMailer < ActionMailer::Base
   def shipped_email(shipment, resend=false)
     @shipment = shipment
     subject = (resend ? "[RESEND] " : "")
-    subject += "#{Spree::Config[:site_name]} Shipment Notification ##{shipment.order.number}"
+    subject += "#{Spree::Config[:site_name]} #{t('subject', :scope => 'shipment_mailer.shipped_email')} ##{shipment.order.number}"
     mail(:to => shipment.order.email,
          :subject => subject)
   end
