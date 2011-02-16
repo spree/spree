a1d00a246a4a84b64d3dda7b833763748b98f84c
Sean Schofield <sean@railsdog.com>
2011-02-16 16:56:18 -0500

    Fixed a problem with state events when User.current is nil.

--- core/app/models/order.rb
+++ core/app/models/order.rb
@@ -396,7 +396,7 @@ class Order < ActiveRecord::Base
         :previous_state => old_shipment_state,
         :next_state     => self.shipment_state,
         :name           => "shipment" ,
-        :user_id        => User.current.id || self.user_id
+        :user_id        => (User.current && User.current.id) || self.user_id
       })
     end
 
@@ -425,7 +425,7 @@ class Order < ActiveRecord::Base
         :previous_state => old_payment_state,
         :next_state     => self.payment_state,
         :name           => "payment" ,
-        :user_id        =>  User.current.id || self.user_id
+        :user_id        =>  (User.current && User.current.id) || self.user_id
       })
     end
   end
