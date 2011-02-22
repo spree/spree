9058e8b1757fbb1e0f63f81d1cb4cef1be5af3fc
Neeraj Singh <neeraj@railsdog.com>
2011-02-22 14:25:11 -0500

    add relevant blog post to json security fix

--- core/app/controllers/admin/users_controller.rb
+++ core/app/controllers/admin/users_controller.rb
@@ -1,5 +1,7 @@
 class Admin::UsersController < Admin::BaseController
   resource_controller
+
+  # http://spreecommerce.com/blog/2010/11/02/json-hijacking-vulnerability/
   before_filter :check_json_authenticity, :only => :index
   before_filter :load_roles, :only => [:edit, :new, :update, :create]
 
