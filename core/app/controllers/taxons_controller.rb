c8161e8542af1e53942125f1c08d187a3f1f1473
sbeam <sbeam@onsetcorps.net>
2011-02-22 22:09:25 -0500

    Invalid taxon permalink should result in a 404
    [#1860 state:resolved]

--- core/app/controllers/taxons_controller.rb
+++ core/app/controllers/taxons_controller.rb
@@ -1,9 +1,10 @@
 class TaxonsController < Spree::BaseController
 
   helper :products
+  rescue_from ActiveRecord::RecordNotFound, :with => :render_404
 
   def show
-    @taxon = Taxon.find_by_permalink(params[:id])
+    raise ActiveRecord::RecordNotFound unless @taxon = Taxon.find_by_permalink(params[:id])
     params[:taxon] = @taxon.id
     @searcher = Spree::Config.searcher_class.new(params)
     @products = @searcher.retrieve_products
