9281c5b475fa8984274e7ba5d0a9d7442ad06d45
Jeroen Jacobs <jacobsjeroen@gmail.com>
2011-03-04 14:32:18 +0100

    Changes the way AddDescriptionToTaxons checks if the description column already exists

--- core/db/migrate/20100506185838_add_description_to_taxons.rb
+++ core/db/migrate/20100506185838_add_description_to_taxons.rb
@@ -1,11 +1,10 @@
 class AddDescriptionToTaxons < ActiveRecord::Migration
-    def self.up
-      # skip this migration if the attribute already exists because of advanced taxon extension
-      return if Taxon.new.respond_to? :description
-      add_column :taxons, :description, :text
-    end
+  def self.up
+    # skip this migration if the attribute already exists because of advanced taxon extension
+    add_column :taxons, :description, :text unless column_exists?(:taxons, :description)
+  end
 
-    def self.down
-      remove_column :taxons, :description
-    end
+  def self.down
+    remove_column :taxons, :description
+  end
 end
