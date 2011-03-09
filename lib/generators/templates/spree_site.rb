b0e0280aa231cb0135f10e262a2857897f839a0f
Adam Jahn <ajjahn@gmail.com>
2011-03-09 18:51:48 -0500

    Fix rake tasks from being invoked twice
    [#1792]

--- lib/generators/templates/spree_site.rb
+++ lib/generators/templates/spree_site.rb
@@ -3,6 +3,10 @@ module SpreeSite
     def self.activate
       # Add your custom site logic here
     end
+    
+    def load_tasks
+    end
+    
     config.to_prepare &method(:activate).to_proc
   end
 end
