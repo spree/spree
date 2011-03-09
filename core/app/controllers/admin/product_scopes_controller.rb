0cf5dba7bd9b20feb47cd486eab0758b69ef9c4a
Roman Smirnov <roman@railsdog.com>
2011-03-09 14:32:13 +0300

    Product scope should be removed from page after deleting instead of just hidden and membership products in related group should be updated.

--- core/app/controllers/admin/product_scopes_controller.rb
+++ core/app/controllers/admin/product_scopes_controller.rb
@@ -6,6 +6,8 @@ class Admin::ProductScopesController < Admin::BaseController
   belongs_to :product_group
 
   actions :create, :destroy
+  
+  destroy.after :update_memberships
 
   create.response do |wants|
     wants.html { redirect_to edit_admin_product_group_path(parent_object) }
@@ -14,6 +16,12 @@ class Admin::ProductScopesController < Admin::BaseController
   destroy.response do |wants|
     wants.html { redirect_to edit_admin_product_group_path(parent_object) }
     wants.js { render :action => 'destroy', :layout => false}
+  end
+  
+  private
+  
+  def update_memberships
+    object.product_group.update_memberships
   end
 
 end
