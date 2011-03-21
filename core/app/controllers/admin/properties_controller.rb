3f6aa82972ad979f33e567c061a735e3df169ed1
Roman Smirnov <roman@railsdog.com>
2011-03-21 23:23:20 +0300

    Fix downcase call to work with non-ascii strings too.

--- core/app/controllers/admin/properties_controller.rb
+++ core/app/controllers/admin/properties_controller.rb
@@ -2,7 +2,7 @@ class Admin::PropertiesController < Admin::ResourceController
 
   # Looks like this action is unused
   def filtered
-    @properties = Property.where('lower(name) LIKE ?', "%#{params[:q].downcase}%").order(:name)
+    @properties = Property.where('lower(name) LIKE ?', "%#{params[:q].mb_chars.downcase}%").order(:name)
     render :template => "admin/properties/filtered.html.erb", :layout => false
   end
 
