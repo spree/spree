488c69bb1be87a86daddee6d9105cd4a0192c286
Sean Schofield <sean@railsdog.com>
2011-03-22 13:39:57 -0400

    Fixed a problem with user creation due to SSL in production mode

--- auth/app/controllers/user_registrations_controller.rb
+++ auth/app/controllers/user_registrations_controller.rb
@@ -2,6 +2,7 @@ class UserRegistrationsController < Devise::RegistrationsController
   include SpreeBase
   helper :users, 'spree/base'
 
+  ssl_required
   after_filter :associate_user, :only => :create
   before_filter :check_permissions, :only => [:edit, :update]
   skip_before_filter :require_no_authentication
