2c965574b4604ec3d5d073994058c53454beed85
Railscat <modology@gmail.com>
2011-03-24 10:53:33 +1100

    fix UsersController spec

--- api/spec/controllers/admin/users_controller_spec.rb
+++ api/spec/controllers/admin/users_controller_spec.rb
@@ -10,15 +10,15 @@ describe Admin::UsersController do
   end
 
   context "#generate_api_key" do
-    it "should generate a 40 char key" do
+    it "should generate a 20 char SHA key" do
       user.should_receive :generate_api_key!
       put :generate_api_key, {:id => 1}
     end
   end
 
   context "#clear_api_key" do
-    it "should clear the key" do
-      user.stub :key => "FOOFAH"
+    it "should remove the existing api_key" do
+      user.stub :authentication_token => "FOOFAH"
       user.should_receive :clear_api_key!
       put :clear_api_key, {:id => 1}
     end
