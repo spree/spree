9e7c685a3dac8b4e22fb9cde6c8a36a5cdf24934
Sean Schofield <sean@railsdog.com>
2011-03-23 20:33:10 -0400

    Add a default version of the registered scope so core tests do not fail in isolation

--- core/app/models/user.rb
+++ core/app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
   has_many :orders
   belongs_to :ship_address, :foreign_key => "ship_address_id", :class_name => "Address"
   belongs_to :bill_address, :foreign_key => "bill_address_id", :class_name => "Address"
+  scope :registered
 
   def anonymous?
     false
