832884a7ff63d1837c7e21bcca8dcf30dfc24d49
Chetan Mittal <chetan.mittal@niamtech.com>
2011-03-31 14:41:25 +0530

    commit for the bug - to_d on price_bucket calculator - resolved at http://groups.google.com/group/spree-user/browse_thread/thread/873fbcbda0bc584f

--- core/app/models/calculator/price_bucket.rb
+++ core/app/models/calculator/price_bucket.rb
@@ -15,12 +15,18 @@ class Calculator::PriceBucket < Calculator
 
   # as object we always get line items, as calculable we have Coupon, ShippingMethod
   def compute(object)
-    if object.is_a?(Array)
-      base = object.map{ |o| o.respond_to?(:amount) ? o.amount : o.to_d }.sum
-    else
-      base = object.respond_to?(:amount) ? object.amount : object.to_d
-    end
-
+    # if object.is_a?(Array)
+    #   base = object.map{ |o| o.respond_to?(:amount) ? o.amount : o.to_d }.sum
+    # else
+    #   base = object.respond_to?(:amount) ? object.amount : object.to_d
+    # end
+    ##
+    # below patch solves to_d bug seen when using this bucket
+    # what is this to_d method anyway
+    # Thanks for Fernando for the fix
+    # http://groups.google.com/group/spree-user/browse_thread/thread/873fbcbda0bc584f
+    base = object.line_items.inject(0) {|sum, li| sum += li.amount }
+    ##
     if base >= self.preferred_minimal_amount
       self.preferred_normal_amount
     else
