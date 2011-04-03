03044e06822371144243e9ae434f046281049471
Roman Smirnov <roman@railsdog.com>
2011-04-03 19:16:01 +0400

    Removed unneeded SQL-query in case when session[:order_id] is nil

--- core/lib/spree/current_order.rb
+++ core/lib/spree/current_order.rb
@@ -14,7 +14,9 @@ module Spree
     # The current incomplete order from the session for use in cart and during checkout
     def current_order(create_order_if_necessary = false)
       return @current_order if @current_order
-      @current_order ||= Order.find_by_id(session[:order_id], :include => :adjustments)
+      if session[:order_id]
+        @current_order = Order.find_by_id(session[:order_id], :include => :adjustments)
+      end
       if create_order_if_necessary and (@current_order.nil? or @current_order.completed?)
         @current_order = Order.new
         before_save_new_order
