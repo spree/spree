ff9b9db38f3ff039d9a0b9b167214803e9fc634e
Roman Smirnov <roman@railsdog.com>
2011-04-08 00:43:37 +0400

    Removed r_c from ProductsController & TaxonsController

--- core/app/controllers/products_controller.rb
+++ core/app/controllers/products_controller.rb
@@ -1,18 +1,17 @@
 class ProductsController < Spree::BaseController
   HTTP_REFERER_REGEXP = /^https?:\/\/[^\/]+\/t\/([a-z0-9\-\/]+)$/
-
-  #prepend_before_filter :reject_unknown_object, :only => [:show]
-  before_filter :load_data, :only => :show
-
-  resource_controller
+  rescue_from ActiveRecord::RecordNotFound, :with => :render_404
   helper :taxons
-  actions :show, :index
 
-  private
+  def index
+    @searcher = Spree::Config.searcher_class.new(params)
+    @products = @searcher.retrieve_products
+  end
 
-  def load_data
-    load_object
-
+  def show
+    @product = Product.find_by_permalink!(params[:id])
+    return unless @product
+    
     @variants = Variant.active.find_all_by_product_id(@product.id,
                 :include => [:option_values, :images])
     @product_properties = ProductProperty.find_all_by_product_id(@product.id,
@@ -21,15 +20,12 @@ class ProductsController < Spree::BaseController
 
     referer = request.env['HTTP_REFERER']
 
-    if referer  && referer.match(HTTP_REFERER_REGEXP)
+    if referer && referer.match(HTTP_REFERER_REGEXP)
       @taxon = Taxon.find_by_permalink($1)
     end
   end
 
-  def collection
-    @searcher = Spree::Config.searcher_class.new(params)
-    @products = @searcher.retrieve_products
-  end
+  private
 
   def accurate_title
     @product ? @product.name : nil
--- core/app/controllers/taxons_controller.rb
+++ core/app/controllers/taxons_controller.rb
@@ -1,23 +1,16 @@
 class TaxonsController < Spree::BaseController
-  #prepend_before_filter :reject_unknown_object, :only => [:show]
-  before_filter :load_data, :only => :show
-  resource_controller
-  actions :show
-  helper :products
   rescue_from ActiveRecord::RecordNotFound, :with => :render_404
+  helper :products
 
-  private
-  def load_data
-    @taxon ||= object
-    render_404 and return if @taxon.nil?
-    params[:taxon] = @taxon.id
-    @searcher = Spree::Config.searcher_class.new(params)
+  def show
+    @taxon = Taxon.find_by_permalink!(params[:id])
+    return unless @taxon
+    
+    @searcher = Spree::Config.searcher_class.new(params.merge(:taxon => @taxon.id))
     @products = @searcher.retrieve_products
   end
 
-  def object
-    @object ||= end_of_association_chain.find_by_permalink(params[:id])
-  end
+  private
 
   def accurate_title
     @taxon ? @taxon.name : nil
