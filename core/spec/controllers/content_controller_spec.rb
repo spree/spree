5047f2025f365072b272fd22367d3c862d3c1555
John Dyer <lbrapid@gmail.com>
2011-04-08 11:54:58 -0400

    Resolve issue where ContentController#show was serving up local files

--- core/spec/controllers/content_controller_spec.rb
+++ core/spec/controllers/content_controller_spec.rb
@@ -6,4 +6,9 @@ describe ContentController do
     assert_routing("/content/cvv", {:controller => "content", :action => "cvv", })
   end
 
+  it "should not display a local file" do
+    get :show, :path => "../../Gemfile"
+    response.response_code.should == 404
+  end
+
 end
