0a2ee5fc68b22b8257e8a6cf1811598293416d33
Sean Schofield <sean@railsdog.com>
2011-04-08 20:44:47 -0400

    Use render with template option which prevents arbitrary files from being rendered.

--- core/app/controllers/content_controller.rb
+++ core/app/controllers/content_controller.rb
@@ -1,27 +1,15 @@
 class ContentController < Spree::BaseController
-
-  before_filter :static_asset
+  # Don't serve local files or static assets
+  before_filter { render_404 if params[:path] =~ /(\.|\\)/ }
 
   rescue_from ActionView::MissingTemplate, :with => :render_404
   caches_page :show, :index, :if => Proc.new { Spree::Config[:cache_static_content] }
 
   def show
-    render params[:path]
+    render :template => params[:path]
   end
 
   def cvv
     render "cvv", :layout => false
-  end
-
-  private
-  # Determines if the requested resource has a path similar to that of a static asset.  In this case do not go through the
-  # overhead of trying to render a template or whatever.
-  def static_asset
-    if params[:path] =~ /(\.|\\)/
-      render_404
-      false
-    else
-      true
-    end
   end
 end
