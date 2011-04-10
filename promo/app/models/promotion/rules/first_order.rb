682e4e57ac5303367adadc30ccb61b11a8fa817f
Chetan Mittal <chetan.mittal@niamtech.com>
2011-04-10 07:43:34 +0530

    Removes promotions related bugs from first-order and product_groups promotion rules first_order promotion changes as suggested by Romul

--- promo/app/models/promotion/rules/first_order.rb
+++ promo/app/models/promotion/rules/first_order.rb
@@ -1,7 +1,7 @@
 class Promotion::Rules::FirstOrder < PromotionRule
 
   def eligible?(order)
-    order.user && order.user.orders.checkout_complete.count == 0
+    order.user && order.user.orders.complete.count == 0
   end
 
 end
