e76be06d10c24b0b402c6815d5ed448fc9e7c02e
Alexander Shuhin <a.medium@gmail.com>
2011-04-21 17:48:37 +0400

    Improved event binding for "form#updatecart a.delete" to work properly with AJAX (ex. with spree_advanced_cart)

--- core/public/javascripts/application.js
+++ core/public/javascripts/application.js
@@ -2,10 +2,10 @@
   $(document).ready(function(){
 
     // Remove an item from the cart by setting its quantity to zero and posting the update form
-    $('form#updatecart a.delete').show().click(function(){
+    $('form#updatecart a.delete').show().live('click', function(e){
       $(this).parents('tr').find('input.line_item_quantity').val(0);
       $(this).parents('form').submit();
-      return false;
+      e.preventDefault();
     });
 
   });
