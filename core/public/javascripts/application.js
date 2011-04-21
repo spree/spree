8002b9268e9ec7cede151945b230756a462f4467
Alexander Shuhin <a.medium@gmail.com>
2011-04-21 17:48:37 +0400

    Fixed event binding for cart form

--- core/public/javascripts/application.js
+++ core/public/javascripts/application.js
@@ -2,7 +2,7 @@
   $(document).ready(function(){
 
     // Remove an item from the cart by setting its quantity to zero and posting the update form
-    $('form#updatecart a.delete').show().click(function(){
+    $('form#updatecart a.delete').show().live('click', function(){
       $(this).parents('tr').find('input.line_item_quantity').val(0);
       $(this).parents('form').submit();
       return false;
