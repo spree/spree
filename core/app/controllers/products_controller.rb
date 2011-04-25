d6e45313bf609e84127a6f2d874df3d84f44d8a5
Trung Lê <joneslee85@gmail.com>
2011-04-26 00:18:38 +1000

    Convert all AR find_all_by to ARel

--- core/app/controllers/products_controller.rb
+++ core/app/controllers/products_controller.rb
@@ -11,11 +11,9 @@ class ProductsController < Spree::BaseController
   def show
     @product = Product.find_by_permalink!(params[:id])
     return unless @product
-    
-    @variants = Variant.active.find_all_by_product_id(@product.id,
-                :include => [:option_values, :images])
-    @product_properties = ProductProperty.find_all_by_product_id(@product.id,
-                          :include => [:property])
+
+    @variants = Variant.active.includes([:option_values, :images]).where(:product_id => @product.id)
+    @product_properties = ProductProperty.includes(:property).where(:product_id => @product.id)
     @selected_variant = @variants.detect { |v| v.available? }
 
     referer = request.env['HTTP_REFERER']
