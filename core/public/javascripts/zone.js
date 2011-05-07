6555ad02d61ec5abf7b8a756561c838877658862
Trung Lê <joneslee85@gmail.com>
2011-05-07 15:12:26 +1000

    is(':checked') is better and more consistent

--- core/public/javascripts/zone.js
+++ core/public/javascripts/zone.js
@@ -1,16 +1,16 @@
-$j(function() { 
-  if ($j('#country_based').attr('checked')) {
+$j(function() {
+  if ($j('#country_based').is(':checked')) {
     show_country();
-  } else if ($j('#state_based').attr('checked')) {
+  } else if ($j('#state_based').is(':checked')) {
     show_state();
-  } else {        
+  } else {
     show_zone();
   }
   $j('#country_based').click(function() { show_country();} );
   $j('#state_based').click(function() { show_state();} );
-  $j('#zone_based').click(function() { show_zone();} ); 
-})   
-                                                        
+  $j('#zone_based').click(function() { show_zone();} );
+})
+
 var show_country = function() {
   $j('#state_members :input').each(function() { $(this).disable(); })
   $j('#state_members').hide();
