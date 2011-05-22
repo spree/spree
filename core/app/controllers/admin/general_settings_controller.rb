44000016b88c3d5379b21452deeba65559ff7fe2
Roman Smirnov <roman@railsdog.com>
2011-05-22 16:32:19 +0400

    Moved arrays of preferences to controller layer

--- core/app/controllers/admin/general_settings_controller.rb
+++ core/app/controllers/admin/general_settings_controller.rb
@@ -1,5 +1,16 @@
 class Admin::GeneralSettingsController < Admin::BaseController
 
+  def show
+    @preferences = ['site_name', 'default_seo_title', 'default_meta_keywords',
+                    'default_meta_description', 'site_url']
+  end
+  
+  def edit
+    @preferences = ['site_name', 'default_seo_title', 'default_meta_keywords',
+                    'default_meta_description', 'site_url', 'allow_ssl_in_production',
+                    'allow_ssl_in_development_and_test']
+  end
+
   def update
     @config = Spree::Config.instance
     @config.update_attributes(params[@config.class.name.underscore])
