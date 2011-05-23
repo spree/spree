aa9f1f9560559c6852238d2a5789b894ba8819aa
David North <davidnorth@railsdog.com>
2011-05-23 11:43:52 +0100

    Removed OrdersController decorator from promo
    Wasn't loaded and the clear_promotions method is now redundant.
    Extend the controller in spree_promo.rb instead.

--- promo/lib/spree_promo.rb
+++ promo/lib/spree_promo.rb
@@ -50,6 +50,27 @@ module SpreePromo
 
       end
 
+
+      OrdersController.class_eval do
+
+        def update
+          @order = current_order
+          if @order.update_attributes(params[:order])
+
+            if @order.coupon_code.present?
+              fire_event('spree.checkout.coupon_code_added', :coupon_code => @order.coupon_code)
+            end
+
+            @order.line_items = @order.line_items.select {|li| li.quantity > 0 }
+            fire_event('spree.order.contents_changed')
+            respond_with(@order) { |format| format.html { redirect_to cart_path } }
+          else
+            respond_with(@order)
+          end
+        end
+
+      end
+
       # Keep a record ot all static page paths visited for promotions that require them
       ContentController.class_eval do
         after_filter :store_visited_path
