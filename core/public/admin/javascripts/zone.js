011c6edbe711570ce4a23408e4363c58f652e8cf
Matteo Folin <matteo.folin@gmail.com>
2011-06-09 03:05:38 -0700

    Edited core/public/admin/javascripts/zone.js via GitHub

--- core/public/admin/javascripts/zone.js
+++ core/public/admin/javascripts/zone.js
@@ -12,29 +12,33 @@ $j(function() {
 })   
                                                         
 var show_country = function() {
-  $j('#state_members :input').each(function() { $(this).disable(); })
+  $j('#state_members :input').each(function() { $(this).attr('disabled', 'disabled') })
   $j('#state_members').hide();
-  $j('#zone_members :input').each(function() { $(this).disable(); })
+  $j('#zone_members :input').each(function() { $(this).attr('disabled', 'disabled') })
   $j('#zone_members').hide();
-  $j('#country_members :input').each(function() { $(this).enable(); })
+  $j('#country_members :input').each(function() { $(this).removeAttr('disabled'); })
   $j('#country_members').show();
 };
 
 var show_state = function() {
-  $j('#country_members :input').each(function() { $(this).disable(); })
+  $j('#country_members :input').each(function() { $(this).attr('disabled', 'disabled');
+ })
   $j('#country_members').hide();
-  $j('#zone_members :input').each(function() { $(this).disable(); })
+  $j('#zone_members :input').each(function() { $(this).attr('disabled', 'disabled');
+ })
   $j('#zone_members').hide();
-  $j('#state_members :input').each(function() { $(this).enable(); })
+  $j('#state_members :input').each(function() { $(this).removeAttr('disabled'); })
   $j('#state_members').show();
 };
 
 var show_zone = function() {
-  $j('#state_members :input').each(function() { $(this).disable(); })
+  $j('#state_members :input').each(function() { $(this).attr('disabled', 'disabled');
+ })
   $j('#state_members').hide();
-  $j('#country_members :input').each(function() { $(this).disable(); })
+  $j('#country_members :input').each(function() { $(this).attr('disabled', 'disabled');
+ })
   $j('#country_members').hide();
-  $j('#zone_members :input').each(function() { $(this).enable(); })
+  $j('#zone_members :input').each(function() { $(this).removeAttr('disabled'); })
   $j('#zone_members').show();
 };
 
