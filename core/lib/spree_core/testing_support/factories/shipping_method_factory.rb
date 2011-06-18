4f0f759098e4226aa459104f8584c06a0cb2dcdd
Sean Schofield <sean@railsdog.com>
2011-06-17 22:48:26 -0400

    Added default calculator to shipping method factory.

--- core/lib/spree_core/testing_support/factories/shipping_method_factory.rb
+++ core/lib/spree_core/testing_support/factories/shipping_method_factory.rb
@@ -1,7 +1,11 @@
 Factory.define :shipping_method do |f|
   f.zone {|a| Zone.find_by_name("GlobalZone") || a.association(:global_zone) }
   f.name 'UPS Ground'
-  #f.display_on :back_end
-  f.after_create {|shipping_method| shipping_method.calculator = Factory(:calculator,
-                                                                         :calculable =>  shipping_method) }
+  f.calculator { |sm| Factory(:calculator, :calculable_id => sm.object_id, :calculable_type => "ShippingMethod") }
+end
+
+Factory.define :free_shipping_method, :class => ShippingMethod do |f|
+  f.zone {|a| Zone.find_by_name("GlobalZone") || a.association(:global_zone) }
+  f.name 'UPS Ground'
+  f.calculator { |sm| Factory(:no_amount_calculator, :calculable_id => sm.object_id, :calculable_type => "ShippingMethod") }
 end
