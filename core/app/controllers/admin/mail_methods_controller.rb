df8e8d8462ebd88fa636e732d6badd9302bdb07c
Thomas von Deyen <tvdeyen@gmail.com>
2011-06-22 11:03:25 +0200

    Added TestMailer. Fixes #446

--- core/app/controllers/admin/mail_methods_controller.rb
+++ core/app/controllers/admin/mail_methods_controller.rb
@@ -1,6 +1,16 @@
 class Admin::MailMethodsController < Admin::ResourceController
   after_filter :initialize_mail_settings
-
+  
+  def testmail
+    @mail_method = MailMethod.find(params[:id])
+    if TestMailer.test_email(@mail_method, current_user).deliver
+      flash[:notice] = t("admin.mail_methods.testmail.delivery_success", :default => 'Testmail sent successfully')
+    else
+      flash[:error] = t("admin.mail_methods.testmail.delivery_error", :default => 'Testmail delivery error')
+    end
+    respond_with(@mail_method) { |format| format.html { redirect_to :back } }
+  end
+  
   private
   def initialize_mail_settings
     Spree::MailSettings.init
