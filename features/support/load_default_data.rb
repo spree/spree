58268d9da13047ebc7ace23aab8a4c44d066a846
Brian Quinn <briandquinn@gmail.com>
2011-06-22 09:46:49 +0100

    Fixed warning when running cucumber tests

--- features/support/load_default_data.rb
+++ features/support/load_default_data.rb
@@ -3,7 +3,7 @@ DatabaseCleaner.clean
 # load default data for tests
 require 'active_record/fixtures'
 fixtures_dir = File.expand_path('../../../core/db/default', __FILE__)
-Fixtures.create_fixtures(fixtures_dir, ['countries', 'zones', 'zone_members', 'states', 'roles'])
+ActiveRecord::Fixtures.create_fixtures(fixtures_dir, ['countries', 'zones', 'zone_members', 'states', 'roles'])
 PaymentMethod::Check.create(:name => 'Check' )
 
 # use transactions for faster tests
