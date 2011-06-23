20728ee43f7f059c09e35cdd4584cc0ad61d9df4
Trung Lê <joneslee85@gmail.com>
2011-06-23 18:00:28 +1000

    Fix product.js and checkout.js loading path, remove store/spree_core.js from all.js
    [Fixes #458]

--- core/app/assets/javascripts/store/product.js
+++ core/app/assets/javascripts/store/product.js
@@ -15,35 +15,35 @@ var add_image_handlers = function() {
           }
           );
 };
- 
+
 jQuery(document).ready(function() {
   add_image_handlers();
 });
- 
+
 jQuery(document).ready(function() {
   jQuery('#product-variants input[type=radio]').click(function (event) {
     var vid = this.value;
     var text = $(this).siblings(".variant-description").html();
- 
+
     jQuery("#variant-thumbnails").empty();
     jQuery("#variant-images span").html(text);
- 
+
     var link;
     if (images[vid] && images[vid].length > 0) {
       $.each(images[vid], function(i, link) {
         jQuery("#variant-thumbnails").append('<li>' + link + '</li>');
       });
- 
+
       jQuery("#variant-images").show();
       link = jQuery("#variant-thumbnails a:first");
     } else {
       jQuery("#variant-images").hide();
       link = jQuery("#product-thumbnails a:first");
     }
- 
+
     jQuery("#main-image img").attr({src: jQuery(link).attr('href')});
     add_image_handlers();
-    
+
     jQuery('ul.thumbnails li').removeClass('selected');
     jQuery(link).parent('li').addClass('selected');
   });
