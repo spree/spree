21e3c64b9bb3b48ccf24c4a518c288621e5e038a
Thomas von Deyen <tvdeyen@gmail.com>
2011-07-07 23:02:37 +0200

    Added translations into locale file

--- core/app/mailers/test_mailer.rb
+++ core/app/mailers/test_mailer.rb
@@ -2,7 +2,7 @@ class TestMailer < ActionMailer::Base
 
   def test_email(mail_method, user)
     @mail_method = mail_method
-    subject = "#{Spree::Config[:site_name]} #{t('subject', :scope => 'test_mailer.test_email', :default => 'Testmail')}"
+    subject = "#{Spree::Config[:site_name]} #{t('subject', :scope => 'test_mailer.test_email')}"
     mail(:to => user.email,
          :subject => subject)
   end
