8616745c88296273512f1fbde14ab599d3a430f2
Sean Schofield <sean@railsdog.com>
2011-07-11 18:19:43 -0400

    Revert "spree.user.signup firing twice"
    This reverts commit f9ec3604dba428ff2480dcc8461a2d36e3c7fd43.

--- auth/app/controllers/user_registrations_controller.rb
+++ auth/app/controllers/user_registrations_controller.rb
@@ -18,6 +18,7 @@ class UserRegistrationsController < Devise::RegistrationsController
     logger.debug(@user)
     if resource.save
       set_flash_message(:notice, :signed_up)
+      ActiveSupport::Notifications.instrument('spree.user.signup', default_notification_payload.merge(:user => @user))
       fire_event('spree.user.signup', :user => @user)
       sign_in_and_redirect(:user, @user)
     else
