617f6584f81b74ec35011836aeb941fe62445881
Sean Schofield <sean@railsdog.com>
2011-07-12 11:31:07 -0400

    Consolidated user factories.  We should not be defining factories in env.rb

--- core/lib/spree_core/testing_support/factories/user_factory.rb
+++ core/lib/spree_core/testing_support/factories/user_factory.rb
@@ -1,15 +1,19 @@
-unless Factory.factories.keys.include?(:user)
-  Factory.define(:user) do |f|
-    f.email { Faker::Internet.email }
-    f.password "spree123"
-    f.password_confirmation "spree123"
-  end
+Factory.sequence :login do |n|
+  Faker::Internet.user_name + n.to_s
+end
+
+Factory.sequence :user_authentication_token do |n|
+  "xxxx#{Time.now.to_i}#{rand(1000)}#{n}xxxxxxxxxxxxx"
+end
+
+Factory.define :user do |f|
+  f.email { Faker::Internet.email }
+  f.login { |u| u.email }
+  f.authentication_token { Factory.next(:user_authentication_token) }
+  f.password "secret"
+  f.password_confirmation "secret"
 end
 
 Factory.define(:admin_user, :parent => :user) do |u|
   u.roles { [Role.find_by_name("admin") || Factory(:role, :name => "admin")]}
-end
-
-Factory.sequence :login do |n|
-  Faker::Internet.user_name + n.to_s
 end
