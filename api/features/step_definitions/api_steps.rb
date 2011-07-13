fe57beb0c036946f928ac82d7102048f5d7fa268
Sean Schofield <sean@railsdog.com>
2011-07-13 10:14:08 -0400

    Removed some defunct test stuff

--- api/features/step_definitions/api_steps.rb
+++ api/features/step_definitions/api_steps.rb
@@ -14,18 +14,6 @@ Given /^I am a valid API user$/ do
 end
 
 Given /^I send and accept json$/ do
-  #header 'AUTHORIZATION', "#{@user.authentication_token}:X"
   header "Content-Type","application/json"
   header "Accept","application/json"
 end
-
-private
-
-def encode_credentials(username, password)
-  req = Net::HTTP::Get.new('/api/orders/8567520/shipments')
-  req.basic_auth(@user.authentication_token,"x")
-  return req['authorization'].gsub(/Basic\w/, "")
-  #ActionController::HttpAuthentication::Basic.encode_credentials(username,password)
-  #{}"Basic " + Base64.encode64("#{username}:#{password}").gsub(/\r\n/m, "")
-end
-
