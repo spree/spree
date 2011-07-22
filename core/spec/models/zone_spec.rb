fd324e927a454fb86401bb26cd4621455b41e9aa
Sean Schofield <sean@railsdog.com>
2011-07-22 13:59:52 -0400

    Added a test for #524
    [Fixes #524]

---
+++ core/spec/models/zone_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe Zone do
+
+  let(:zone) { Factory :zone }
+
+  context 'factory' do
+    #let(:zone){ Factory :zone }
+
+    it "should set zone members correctly" do
+      zone.zone_members.count.should == 1
+    end
+  end
+
+  context "#destroy" do
+    before { zone.destroy }
+    it "should destroy all zone members" do
+      zone.zone_members.count.should == 0
+    end
+  end
+
+end
