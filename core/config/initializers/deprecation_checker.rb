1467cc55ef867a177f7afedf82f977a829f3ddc8
Brian Quinn <briandquinn@gmail.com>
2011-08-14 17:31:54 +0100

    Remove deprecation check for 'public/assets' as that's we're precompiled assets get created.

--- core/config/initializers/deprecation_checker.rb
+++ core/config/initializers/deprecation_checker.rb
@@ -4,7 +4,4 @@
 if File.exist?(Rails.root.join("public/assets/products")) ||  File.exist?(Rails.root.join("public/assets/taxons"))
   puts %q{[DEPRECATION] Your applications public directory contains an assets/products and/or assets/taxons subdirectory. 
     Run `rake spree:assets:relocate_images` to relocate the images.}
-elsif File.exist? Rails.root.join("public/assets")
-  puts %q{[DEPRECATION] Your applications public directory contains assets subdirectory. 
-    You should remove the public/assets directory to prevent conflicts with the Rails 3.1 asset pipeline}
 end
