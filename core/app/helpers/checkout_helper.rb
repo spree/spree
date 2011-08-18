00ea612f46b52319ccf1e6bfa47720f73f5ea6f9
Trung Lê <joneslee85@gmail.com>
2011-08-19 01:24:37 +1000

    Removed address field helper and reverted original address view for the sake of Deface

--- core/app/helpers/checkout_helper.rb
+++ core/app/helpers/checkout_helper.rb
@@ -5,7 +5,7 @@ module CheckoutHelper
       %w(address delivery payment confirm complete)
     else
       %w(address delivery payment complete)
-    end  
+    end
   end
 
   def checkout_progress
@@ -32,36 +32,4 @@ module CheckoutHelper
     content_tag('ol', raw(items.join("\n")), :class => 'progress-steps', :id => "checkout-step-#{@order.state}")
   end
 
-
-  def address_field(form, method, id_prefix = "b", &handler)
-    content_tag :p, :id => [id_prefix, method].join, :class => "field" do
-      if handler
-        handler.call
-      else
-        is_required = Address.required_fields.include?(method)
-        separator = is_required ? '<span class="req">*</span><br />' : '<br />' 
-        form.label(method) + separator.html_safe + 
-        form.text_field(method, :class => is_required ? 'required' : nil)
-      end
-    end
-  end
-  
-  def address_state(form, country)
-    country ||= Country.find(Spree::Config[:default_country_id])
-    have_states = !country.states.empty?
-    state_elements = [
-      form.collection_select(:state_id, country.states.order(:name),
-                            :id, :name,
-                            {:include_blank => true},
-                            {:class => have_states ? "required" : "hidden",
-                            :disabled => !have_states}) +
-      form.text_field(:state_name,
-                      :class => !have_states ? "required" : "hidden",
-                      :disabled => have_states)
-      ].join.gsub('"', "'").gsub("\n", "")
-           
-    form.label(:state, t(:state)) + '<span class="req">*</span><br />'.html_safe +
-      content_tag(:noscript, form.text_field(:state_name, :class => 'required')) +
-      javascript_tag("document.write(\"#{state_elements.html_safe}\");")
-  end
 end
