64f295122d31f0b71f1a780db83c9dbf84a9ec86
Brian Quinn <briandquinn@gmail.com>
2011-09-06 11:32:17 +0100

    Fixed site generator inheritance

--- lib/generators/spree/site_generator.rb
+++ lib/generators/spree/site_generator.rb
@@ -3,7 +3,7 @@ require File.expand_path('../../../../core/lib/generators/spree_core/site/site_g
 
 module Spree
   module Generators
-    class SiteGenerator < SpreeCore::Generators::SiteGenerator
+    class SiteGenerator < SpreeCore::SiteGenerator
       desc "Configures an existing Rails application to use Spree."
       #dummy generator, just inherits from spree_core
     end
