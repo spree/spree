36868085da5b577d18f1f9333fdb86faddf2e5bb
Trung Lê <joneslee85@gmail.com>
2011-09-24 11:46:20 +1000

    Refactored t() syntax for mailer

--- core/app/mailers/order_mailer.rb
+++ core/app/mailers/order_mailer.rb
@@ -1,18 +1,18 @@
 class OrderMailer < ActionMailer::Base
-  helper "spree/base"
+  helper 'spree/base'
 
   def confirm_email(order, resend=false)
     @order = order
-    subject = (resend ? "[RESEND] " : "")
-    subject += "#{Spree::Config[:site_name]} #{t('subject', :scope =>'order_mailer.confirm_email')} ##{order.number}"
+    subject = (resend ? "[#{t(:resend).upcase}] " : "")
+    subject += "#{Spree::Config[:site_name]} #{t('order_mailer.confirm_email.subject')} ##{order.number}"
     mail(:to => order.email,
          :subject => subject)
   end
 
   def cancel_email(order, resend=false)
     @order = order
-    subject = (resend ? "[RESEND] " : "")
-    subject += "#{Spree::Config[:site_name]} #{t('subject', :scope => 'order_mailer.cancel_email')} ##{order.number}"
+    subject = (resend ? "[#{t(:resend).upcase}] " : "")
+    subject += "#{Spree::Config[:site_name]} #{t('order_mailer.cancel_email.subject')} ##{order.number}"
     mail(:to => order.email,
          :subject => subject)
   end
--- core/app/mailers/shipment_mailer.rb
+++ core/app/mailers/shipment_mailer.rb
@@ -1,10 +1,10 @@
 class ShipmentMailer < ActionMailer::Base
-  helper "spree/base"
+  helper 'spree/base'
 
   def shipped_email(shipment, resend=false)
     @shipment = shipment
-    subject = (resend ? "[RESEND] " : "")
-    subject += "#{Spree::Config[:site_name]} #{t('subject', :scope => 'shipment_mailer.shipped_email')} ##{shipment.order.number}"
+    subject = (resend ? "[#{t(:resend).upcase}] " : "")
+    subject += "#{Spree::Config[:site_name]} #{t('shipment_mailer.shipped_email.subject')} ##{shipment.order.number}"
     mail(:to => shipment.order.email,
          :subject => subject)
   end
