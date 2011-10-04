6f167d8a1aa3c09dd29274807042d3205fce1bd4
John Dyer <lbrapid@gmail.com>
2011-10-04 15:30:44 -0400

    Replace jquery-autocomplete plugin with jquery-ui version.

--- core/app/assets/javascripts/admin/checkouts/edit.js
+++ core/app/assets/javascripts/admin/checkouts/edit.js
@@ -76,9 +76,9 @@ $(document).ready(function(){
 
         $('#order_email').val(ui.item.data['email']);
         $('#user_id').val(ui.item.data['id']);
+        $('#user_id').hide();
         $('#guest_checkout_true').prop("checked", false);
         $('#guest_checkout_false').prop("checked", true);
-        $('#guest_checkout_true').prop("disabled", false);
         return false;
       }
     }).data("autocomplete")._renderItem = function(ul, item) {
