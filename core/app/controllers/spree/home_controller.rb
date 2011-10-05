d98b293c5d51f363e477799296fb3658dd41c6e1
Ryan Bigg <radarlistener@gmail.com>
2011-10-05 19:53:17 +1100

    Namespace home controller

---
+++ core/app/controllers/spree/home_controller.rb
@@ -0,0 +1,12 @@
+module Spree
+  class HomeController < BaseController
+    helper :products
+    respond_to :html
+  
+    def index
+      @searcher = Spree::Config.searcher_class.new(params)
+      @products = @searcher.retrieve_products
+      respond_with(@products)
+    end
+  end
+end
