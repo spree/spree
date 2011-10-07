6016c6b8ce290fbab41d4763801bd03c5c5c2852
John Dyer <lbrapid@gmail.com>
2011-10-07 17:10:18 -0400

    Updated fixtures to reflect namespaced models

--- sample/db/sample/payments.rb
+++ sample/db/sample/payments.rb
@@ -1,18 +1,18 @@
 # create payments based on the totals since they can't be known in YAML (quantities are random)
-method = PaymentMethod.find(:first, :conditions => {:name => "Credit Card", :active => true})
+method = Spree::PaymentMethod.find(:first, :conditions => {:name => "Credit Card", :active => true})
 
 # Hack the current method so we're able to return a gateway without a RAILS_ENV
-Gateway.class_eval do
+Spree::Gateway.class_eval do
   def self.current
-    Gateway::Bogus.new
+    Spree::Gateway::Bogus.new
   end
 end
 
-creditcard = Creditcard.create(:cc_type => "visa", :month => 12, :year => 2014, :last_digits => "1111",
+creditcard = Spree::Creditcard.create(:cc_type => "visa", :month => 12, :year => 2014, :last_digits => "1111",
                                :first_name => "Sean", :last_name => "Schofield",
                                :gateway_customer_profile_id => "BGS-1234")
 
-Order.all.each_with_index do |order,index|
+Spree::Order.all.each_with_index do |order,index|
   printf "\rProcessing order #{index}"
   STDOUT.flush
   order.update!
