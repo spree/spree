a07b0cc40fe100208fd1817038dafc86107db38f
Ryan Bigg <radarlistener@gmail.com>
2011-10-08 10:23:30 +1100

    Add legacy table support ot add icon to taxons migration

--- core/db/migrate/20100506180619_add_icon_to_taxons.rb
+++ core/db/migrate/20100506180619_add_icon_to_taxons.rb
@@ -1,11 +1,15 @@
 class AddIconToTaxons < ActiveRecord::Migration
     def self.up
+      # legacy table support
+      Spree::Taxon.table_name = "taxons"
       # skip this migration if the attribute already exists because of advanced taxon extension
       return if Spree::Taxon.new.respond_to? :icon_file_name
       add_column :taxons, :icon_file_name,    :string
       add_column :taxons, :icon_content_type, :string
       add_column :taxons, :icon_file_size,    :integer
       add_column :taxons, :icon_updated_at,   :datetime
+
+      Spree::Taxon.table_name = "spree_taxons"
     end
 
     def self.down
