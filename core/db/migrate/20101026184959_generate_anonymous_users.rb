dee5ad298cf92ef9762eb0369aa0430066bd8958
Ryan Bigg <radarlistener@gmail.com>
2011-10-08 10:40:05 +1100

    Add legacy table support to generate anonymous users migration

--- core/db/migrate/20101026184959_generate_anonymous_users.rb
+++ core/db/migrate/20101026184959_generate_anonymous_users.rb
@@ -1,12 +1,18 @@
 class GenerateAnonymousUsers < ActiveRecord::Migration
   def self.up
-    User.reset_column_information
-    Order.where(:user_id => nil).each do |order|
-      user = User.anonymous!
+    Spree::User.table_name = "users"
+    Spree::Order.table_name = "orders"
+
+    Spree::User.reset_column_information
+    Spree::Order.where(:user_id => nil).each do |order|
+      user = Spree::User.anonymous!
       user.email ||= order.email
       order.user = user
       order.save!
     end
+
+    Spree::User.table_name = "spree_users"
+    Spree::Order.table_name = "spree_orders"
   end
 
   def self.down
