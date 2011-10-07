cec6835027e7e6aa8a86909bd1d0a478379f411c
Ryan Bigg <radarlistener@gmail.com>
2011-10-08 10:42:48 +1100

    Legacy table support for renamed RMA cancelled state migration

--- core/db/migrate/20101111133551_renamed_rma_cancelled_state.rb
+++ core/db/migrate/20101111133551_renamed_rma_cancelled_state.rb
@@ -1,4 +1,8 @@
 class RenamedRmaCancelledState < ActiveRecord::Migration
+  class ReturnAuthorization < ActiveRecord::Base
+
+  end
+
   def self.up
     ReturnAuthorization.where(:state => 'cancelled').each do |rma|
       rma.update_attribute_without_callbacks(:state, 'canceled')
