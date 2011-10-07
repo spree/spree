d139e4a249ee17bbf494f0eda03f874710b926d3
Ryan Bigg <radarlistener@gmail.com>
2011-10-07 18:30:27 +1100

    OrderMailer is now namespaced under Spree

--- core/spec/lib/mail_interceptor_spec.rb
+++ core/spec/lib/mail_interceptor_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 # We'll use the OrderMailer as a quick and easy way to test.  IF it works here - it works for all email (in theory.)
-describe OrderMailer do
+describe Spree::OrderMailer do
   let(:mail_method) { mock("mail_method", :preferred_mails_from => nil, :preferred_intercept_email => nil, :preferred_mail_bcc => nil) }
   let(:order) { Order.new(:email => "customer@example.com") }
   let(:message) { OrderMailer.confirm_email(order) }
