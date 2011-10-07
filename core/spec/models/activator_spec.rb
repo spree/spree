184c30560641d2993034e2b276e273cff2bc0cac
Ryan Bigg <radarlistener@gmail.com>
2011-10-08 10:52:00 +1100

    Correct references to Activator class in activator_spec

--- core/spec/models/activator_spec.rb
+++ core/spec/models/activator_spec.rb
@@ -1,16 +1,16 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
-describe Activator do
+describe Spree::Activator do
 
   context "event_names" do
-    specify { Activator.event_names.is_a?(Array) }
-    specify { Activator.event_names.all?{|n| n.is_a?(String)} }
+    specify { Spree::Activator.event_names.is_a?(Array) }
+    specify { Spree::Activator.event_names.all?{|n| n.is_a?(String)} }
   end
 
   context "register_event_name" do
     it "adds the name to event_names" do
-      Activator.register_event_name('spree.new_event')
-      Activator.event_names.should include('spree.new_event')
+      Spree::Activator.register_event_name('spree.new_event')
+      Spree::Activator.event_names.should include('spree.new_event')
     end
   end
 
