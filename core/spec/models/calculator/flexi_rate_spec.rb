49f419d48be30da3ad226b3b88c730bc63cfb8cf
Ryan Bigg <radarlistener@gmail.com>
2011-10-08 10:58:35 +1100

    Fix references to Calculator, Order and LineItem in flexi_rate_spec

--- core/spec/models/calculator/flexi_rate_spec.rb
+++ core/spec/models/calculator/flexi_rate_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 
-describe Calculator::FlexiRate do
-  let(:calculator) { Calculator::FlexiRate.new }
-  let(:order) { mock_model Order, :line_items => [mock_model(LineItem, :amount => 10, :quantity => 4), mock_model(LineItem, :amount => 20, :quantity => 6)] }
+describe Spree::Calculator::FlexiRate do
+  let(:calculator) { Spree::Calculator::FlexiRate.new }
+  let(:order) { mock_model Spree::Order, :line_items => [mock_model(Spree::LineItem, :amount => 10, :quantity => 4), mock_model(Spree::LineItem, :amount => 20, :quantity => 6)] }
 
   context "compute" do
     it "should compute amount correctly when all fees are 0" do
