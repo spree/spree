0389d88ebd7e1f1cf46c8a2a0cd6be2511b5f04b
Ryan Bigg <radarlistener@gmail.com>
2011-10-07 18:18:07 +1100

    Namespace mailers

---
+++ core/app/mailers/spree/shipment_mailer.rb
@@ -0,0 +1,13 @@
+module Spree
+  class ShipmentMailer < ActionMailer::Base
+    helper 'spree/base'
+
+    def shipped_email(shipment, resend=false)
+      @shipment = shipment
+      subject = (resend ? "[#{t(:resend).upcase}] " : "")
+      subject += "#{Spree::Config[:site_name]} #{t('shipment_mailer.shipped_email.subject')} ##{shipment.order.number}"
+      mail(:to => shipment.order.email,
+           :subject => subject)
+    end
+  end
+end
