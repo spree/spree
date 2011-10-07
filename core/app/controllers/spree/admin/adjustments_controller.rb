f1c5c5960431cf5e587dbe758f5ae40b76995eaf
Ryan Bigg <radarlistener@gmail.com>
2011-10-08 07:56:18 +1100

    Correct reference to Spree::Order in admin/adjustments_controller

--- core/app/controllers/spree/admin/adjustments_controller.rb
+++ core/app/controllers/spree/admin/adjustments_controller.rb
@@ -1,5 +1,5 @@
 class Spree::Admin::AdjustmentsController < Spree::Admin::ResourceController
-  belongs_to :order, :find_by => :number
+  belongs_to 'spree/order', :find_by => :number
   destroy.after :reload_order
 
   private
