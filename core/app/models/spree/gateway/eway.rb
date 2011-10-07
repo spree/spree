a3855371bff2816c2450ae20c83889cfdb75d173
Ryan Bigg <radarlistener@gmail.com>
2011-10-08 07:56:31 +1100

    Namespace gateway/eway

--- core/app/models/spree/gateway/eway.rb
+++ core/app/models/spree/gateway/eway.rb
@@ -1,11 +1,13 @@
-class Gateway::Eway < Gateway
-  preference :login, :string
+module Spree
+  class Gateway::Eway < Gateway
+    preference :login, :string
 
-  # Note: EWay supports purchase method only (no authorize method).
-  # Ensure Spree::Config[:auto_capture] is set to true
+    # Note: EWay supports purchase method only (no authorize method).
+    # Ensure Spree::Config[:auto_capture] is set to true
 
-  def provider_class
-    ActiveMerchant::Billing::EwayGateway
+    def provider_class
+      ActiveMerchant::Billing::EwayGateway
+    end
+
   end
-
 end
