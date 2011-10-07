e62afeffa30d331d591a8e43343b71203951fd62
Ryan Bigg <radarlistener@gmail.com>
2011-10-08 10:06:15 +1100

    Correct namespace in add icon to taxons migration

--- core/db/migrate/20100506180619_add_icon_to_taxons.rb
+++ core/db/migrate/20100506180619_add_icon_to_taxons.rb
@@ -1,7 +1,7 @@
 class AddIconToTaxons < ActiveRecord::Migration
     def self.up
       # skip this migration if the attribute already exists because of advanced taxon extension
-      return if Taxon.new.respond_to? :icon_file_name
+      return if Spree::Taxon.new.respond_to? :icon_file_name
       add_column :taxons, :icon_file_name,    :string
       add_column :taxons, :icon_content_type, :string
       add_column :taxons, :icon_file_size,    :integer
