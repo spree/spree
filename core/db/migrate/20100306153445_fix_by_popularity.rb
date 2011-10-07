070d9d1273444ee74e04c4cc4209074203d04f26
Ryan Bigg <radarlistener@gmail.com>
2011-10-08 10:04:37 +1100

    Add legacy ProductScope model to fix_by_popularity migration

--- core/db/migrate/20100306153445_fix_by_popularity.rb
+++ core/db/migrate/20100306153445_fix_by_popularity.rb
@@ -1,4 +1,8 @@
 class FixByPopularity < ActiveRecord::Migration
+  # Legacy table support
+  class ProductScope < ActiveRecord::Base
+
+  end
   def self.up
     ProductScope.update_all("name='descend_by_popularity'", "name='by_popularity'")
   end
