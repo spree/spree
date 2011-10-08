6c484799f4d52da12b574c401f7a20f6d647a734
Ryan Bigg <radarlistener@gmail.com>
2011-10-08 13:45:10 +1100

    Fix reference to Order model in checkout controller spec

--- core/spec/controllers/spree/checkout_controller_spec.rb
+++ core/spec/controllers/spree/checkout_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe Spree::CheckoutController do
-  let(:order) { mock_model(Order, :checkout_allowed? => true, :completed? => false, :update_attributes => true, :payment? => false, :insufficient_stock_lines => []).as_null_object }
+  let(:order) { mock_model(Spree::Order, :checkout_allowed? => true, :completed? => false, :update_attributes => true, :payment? => false, :insufficient_stock_lines => []).as_null_object }
   before { controller.stub :current_order => order, :current_user => nil }
 
   it "should understand checkout routes" do
