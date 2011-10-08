04079d8a5a641a2de9756e0a085a616681d3058e
Ryan Bigg <radarlistener@gmail.com>
2011-10-08 15:18:51 +1100

    Simplify spree/orders_controller, making orders controller spec now pass

--- core/app/controllers/spree/orders_controller.rb
+++ core/app/controllers/spree/orders_controller.rb
@@ -15,9 +15,9 @@ module Spree
       if @order.update_attributes(params[:order])
         @order.line_items = @order.line_items.select {|li| li.quantity > 0 }
         fire_event('spree.order.contents_changed')
-        respond_with(@order) { |format| format.html { redirect_to cart_path } }
+        redirect_to cart_path
       else
-        respond_with(@order)
+        render :edit
       end
     end
 
