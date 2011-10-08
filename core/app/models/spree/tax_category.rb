25088397da10da9f69d9232581593c18bcf18104
Ryan Bigg <radarlistener@gmail.com>
2011-10-08 12:39:17 +1100

    Fix more model references

--- core/app/models/spree/tax_category.rb
+++ core/app/models/spree/tax_category.rb
@@ -8,13 +8,13 @@ class Spree::TaxCategory < ActiveRecord::Base
   def set_default_category
     #set existing default tax category to false if this one has been marked as default
 
-    if is_default && tax_category = TaxCategory.find(:first, :conditions => {:is_default => true})
+    if is_default && tax_category = Spree::TaxCategory.find(:first, :conditions => {:is_default => true})
       tax_category.update_attribute(:is_default, false)
     end
   end
 
   def effective_amount(address=nil)
-    address ||= Address.new(:country_id => Spree::Config[:default_country_id])
+    address ||= Spree::Address.new(:country_id => Spree::Config[:default_country_id])
 
     self.tax_rates.detect{|rate| rate.zone.include? address }.try(:amount)
   end
