5ef603ee6b4e2f34ca0e8d42700c2192b1ca8eb9
Ryan Bigg <radarlistener@gmail.com>
2011-10-08 11:55:04 +1100

    Fix Country reference in state factory

--- core/lib/spree/core/testing_support/factories/state_factory.rb
+++ core/lib/spree/core/testing_support/factories/state_factory.rb
@@ -3,7 +3,7 @@ FactoryGirl.define do
     name 'ALABAMA'
     abbr 'AL'
     country do |country|
-      if usa = Country.find_by_numcode(840)
+      if usa = Spree::Country.find_by_numcode(840)
         country = usa
       else
         country.association(:country)
