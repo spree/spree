b9596556d4c740c37fb87fb0eb10ee8c596406fc
Ryan Bigg <radarlistener@gmail.com>
2011-10-11 08:18:26 +1100

    Namespace Scopes module

--- core/lib/spree/scopes.rb
+++ core/lib/spree/scopes.rb
@@ -1,33 +1,33 @@
-# This module contains all custom scopes created for selecting products.
-#
-# All usable scopes *should* be included in SCOPES constant, it represents
-# all scopes that are selectable from user interface, extensions can extend
-# and modify it, but should provide corresponding translations.
-#
-# Format of constant is following:
-#
-#   {
-#     :namespace/grouping => {
-#       :name_of_the_scope => [:list, :of, :arguments]
-#     }
-#   }
-#
-#   This values are used in translation file, to describe them in the interface.
-#   So for each scope you define here you have to provide following entry in translation file
-#   product_scopes:
-#     name_of_the_group:
-#       name: Translated name of the group
-#       description: Longer description of what this scope group does, inluding
-#         any possible help user may need
-#       scopes:
-#         name_of_the_scope:
-#           name: Short name of the scope
-#           description: What does this scope does exactly
-#           arguments:
-#             arg1: Description of argument
-#             arg2: Description of second Argument
-#
 module Spree
+  # This module contains all custom scopes created for selecting products.
+  #
+  # All usable scopes *should* be included in SCOPES constant, it represents
+  # all scopes that are selectable from user interface, extensions can extend
+  # and modify it, but should provide corresponding translations.
+  #
+  # Format of constant is following:
+  #
+  #   {
+  #     :namespace/grouping => {
+  #       :name_of_the_scope => [:list, :of, :arguments]
+  #     }
+  #   }
+  #
+  #   This values are used in translation file, to describe them in the interface.
+  #   So for each scope you define here you have to provide following entry in translation file
+  #   product_scopes:
+  #     name_of_the_group:
+  #       name: Translated name of the group
+  #       description: Longer description of what this scope group does, inluding
+  #         any possible help user may need
+  #       scopes:
+  #         name_of_the_scope:
+  #           name: Short name of the scope
+  #           description: What does this scope does exactly
+  #           arguments:
+  #             arg1: Description of argument
+  #             arg2: Description of second Argument
+  #
   module Scopes
     module_function
 
@@ -50,7 +50,6 @@ module Spree
           }
         end
       end
-
       result
     end
 
