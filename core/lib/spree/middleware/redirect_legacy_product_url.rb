539f233f90c98c76e1ab2a78b158be5cc6fa479e
Ryan Bigg <radarlistener@gmail.com>
2011-10-11 08:24:18 +1100

    Namespace RedirectLegacyProductUrl into Spree::Middleware

--- core/lib/spree/middleware/redirect_legacy_product_url.rb
+++ core/lib/spree/middleware/redirect_legacy_product_url.rb
@@ -1,15 +1,17 @@
 module Spree
-  class RedirectLegacyProductUrl
-    def initialize(app)
-      @app = app
-    end
-
-    def call(env)
-      if env["PATH_INFO"] =~ %r{/t/.+/p/(.+)}
-        return [301, {'Location'=> "/products/#{$1}" }, []]
+  module Middleware
+    class RedirectLegacyProductUrl
+      def initialize(app)
+        @app = app
       end
-      @app.call(env)
-    end
 
+      def call(env)
+        if env["PATH_INFO"] =~ %r{/t/.+/p/(.+)}
+          return [301, {'Location'=> "/products/#{$1}" }, []]
+        end
+        @app.call(env)
+      end
+
+    end
   end
 end
