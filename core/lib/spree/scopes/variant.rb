bdcf795e5bf445a64f139b268563585ebcea23bb
Ryan Bigg <radarlistener@gmail.com>
2011-10-11 08:18:58 +1100

    Namespace Scopes::Variant module

--- core/lib/spree/scopes/variant.rb
+++ core/lib/spree/scopes/variant.rb
@@ -3,7 +3,7 @@ module Spree
     module Variant
 
       #FIXME WARNING tested only under sqlite and postgresql
-      Spree::Variant.scope :descend_by_popularity, lambda{
+      Spree::Variant.scope :descend_by_popularity, lambda {
         order('COALESCE((SELECT COUNT(*) FROM  line_items GROUP BY line_items.variant_id HAVING line_items.variant_id = variants.id), 0) DESC')
       }
 
