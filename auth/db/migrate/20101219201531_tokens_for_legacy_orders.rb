1d40ff401f8b8c5c1f11269a6196ac3a9a76e035
John Dyer <lbrapid@gmail.com>
2011-10-10 10:42:16 -0400

    Fix migration references to Users table

--- auth/db/migrate/20101219201531_tokens_for_legacy_orders.rb
+++ auth/db/migrate/20101219201531_tokens_for_legacy_orders.rb
@@ -1,7 +1,7 @@
 class TokensForLegacyOrders < ActiveRecord::Migration
   def self.up
     # add token permissions for legacy orders (stop relying on user persistence token)
-    Order.all.each do |order|
+    Spree::Order.all.each do |order|
       next unless order.user
       order.create_tokenized_permission(:token => order.user.persistence_token)
     end
