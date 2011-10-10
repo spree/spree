aae86ef30391a90f975c20666cc06c80f434bdf0
Ryan Bigg <radarlistener@gmail.com>
2011-10-11 08:18:14 +1100

    Namespace RedirectLegacyProductUrl

--- core/lib/spree/redirect_legacy_product_url.rb
+++ core/lib/spree/redirect_legacy_product_url.rb
@@ -1,13 +1,15 @@
-class RedirectLegacyProductUrl
-  def initialize(app)
-    @app = app
-  end
-
-  def call(env)
-    if env["PATH_INFO"] =~ %r{/t/.+/p/(.+)}
-      return [301, {'Location'=> "/products/#{$1}" }, []]
+module Spree
+  class RedirectLegacyProductUrl
+    def initialize(app)
+      @app = app
     end
-    @app.call(env)
-  end
 
+    def call(env)
+      if env["PATH_INFO"] =~ %r{/t/.+/p/(.+)}
+        return [301, {'Location'=> "/products/#{$1}" }, []]
+      end
+      @app.call(env)
+    end
+
+  end
 end
