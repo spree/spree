c1fc7a1cd656950b5a0726da11c6bc5cdf9fe55c
John Dyer <lbrapid@gmail.com>
2011-10-10 10:33:34 -0400

    Fix references to Order in api OrderDecorator

--- api/app/models/order_decorator.rb
+++ api/app/models/order_decorator.rb
@@ -1,9 +1,9 @@
-Order.class_eval do
+Spree::Order.class_eval do
   def self.find_by_param(param)
     if param.to_i > 0
-      Order.find(param)
+      Spree::Order.find(param)
     else
-      Order.where(:number => param).first
+      Spree::Order.where(:number => param).first
     end
   end
 end
