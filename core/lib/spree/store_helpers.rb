66d8e0d0ea515cc24b2051cb519a16a5b22e1bc6
Ryan Bigg <radarlistener@gmail.com>
2011-10-11 08:19:05 +1100

    Namespace StoreHelper

--- core/lib/spree/store_helpers.rb
+++ core/lib/spree/store_helpers.rb
@@ -1,10 +1,12 @@
 # Methods added to this helper will be available to all templates in the application.
-module StoreHelpers
+module Spree
+  module StoreHelpers
 
-  # helper to determine if its appropriate to show the store menu
-  def store_menu?
-    return true unless %w{thank_you}.include? @current_action
-    false
+    # helper to determine if its appropriate to show the store menu
+    def store_menu?
+      return true unless %w{thank_you}.include? @current_action
+      false
+    end
+
   end
-
 end
