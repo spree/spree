698f2c365024cb57325048bdbc82fb3bb61018be
John Dyer <lbrapid@gmail.com>
2011-10-11 15:30:37 -0400

    Namespace promotion rules models

--- promo/app/models/spree/promotion/rules/user_logged_in.rb
+++ promo/app/models/spree/promotion/rules/user_logged_in.rb
@@ -1,10 +1,10 @@
-class Promotion::Rules::UserLoggedIn < PromotionRule
+class Spree::Promotion::Rules::UserLoggedIn < Spree::PromotionRule
   def eligible?(order, options = {})
 
     # this is tricky.  We couldn't use any of the devise methods since we aren't in the controller.
     # we need to rely on the controller already having done this for us.
 
-    # The thinking is that the controller should have some sense of what state 
+    # The thinking is that the controller should have some sense of what state
     # we should be in before firing events,
     # so the controller will have to set this field.
 
