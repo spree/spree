4b7d6c1f4b67924870c3a911913f472fd5f27bdd
Trung Lê <joneslee85@gmail.com>
2011-10-13 03:06:00 +1100

    spree_api clean up

--- api/app/controllers/spree/api/states_controller.rb
+++ api/app/controllers/spree/api/states_controller.rb
@@ -2,7 +2,7 @@ class Spree::Api::StatesController < Spree::Api::BaseController
   before_filter :access_denied, :except => [:index, :show]
 
   private
-  def parent
-    @parent ||= Spree::Country.find(params[:country_id])
-  end
+    def parent
+      @parent ||= Spree::Country.find(params[:country_id])
+    end
 end
