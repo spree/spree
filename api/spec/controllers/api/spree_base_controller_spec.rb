0b8f51130910d794d543e8f095be37968947b4bd
Ryan Bigg <radarlistener@gmail.com>
2011-10-12 20:25:36 +1100

    Fix authorization issue in Spree::Api::BaseController, caused by our namespacing

--- api/spec/controllers/api/spree_base_controller_spec.rb
+++ api/spec/controllers/api/spree_base_controller_spec.rb
@@ -3,51 +3,74 @@ require 'spec_helper.rb'
 # Its difficult to test the Api::BaseController in the abstract since rspec insists on having legit routes, etc.
 # So we'll test against Api::OrdersController instead as a reprentative controller
 describe Api::OrdersController do
-  let(:user) { mock_model(Spree::User, :has_role? => true) }
-  before { controller.stub :current_user => user }
+  context "authenticated" do
+    # Admins are capable of doing everything with the API
+    context "admin" do
+      let(:user) { mock_model(Spree::User, :has_role? => true) }
+      before { controller.stub :current_user => user }
 
-  shared_examples_for "access granted" do
-    it "should allow index" do
-      get :index, :format => :json
-      response.should be_success
+      shared_examples_for "access granted" do
+        it "should allow index" do
+          get :index, :format => :json
+          response.should be_success
+        end
+        it "should allow read" do
+          get :show, {:id => 1}, :format => :json
+          response.should be_success
+        end
+        # it "should allow update"
+      end
+
+      shared_examples_for "access denied" do
+        it "should not allow index" do
+          get :index, :format => :json
+          response.code.should == "401"
+        end
+        it "should not allow read" do
+          get :show, {:id => 1}, :format => :json
+          response.code.should == "401"
+        end
+        # it "should not allow update"
+      end
+
+      context "when correct HTTP_AUTHORIZATION" do
+        before do
+          request.env['HTTP_AUTHORIZATION'] = "legit:x"
+        end
+        it "should allow index" do
+          get :index, :format => :json
+          response.code.should == "200"
+        end
+      end
+
+      context "when authenticated as admin" do
+      end
+
+      context "when no HTTP_AUTHORIZATION" do
+        it_should_behave_like "access denied"
+        context "when authenticated as admin" do
+          it_should_behave_like "access denied"
+        end
+      end
     end
-    it "should allow read" do
-      get :show, {:id => 1}, :format => :json
-      response.should be_success
+    
+    context "user" do
+      it "should have tests for user permissions"
     end
-    # it "should allow update"
   end
-
-  shared_examples_for "access denied" do
+  
+  context "unauthenticated" do
+    let(:user) { mock_model(Spree::User, :has_role? => true) }
+    before do
+      controller.stub :current_user => nil
+    end
+    
     it "should not allow index" do
       get :index, :format => :json
-      response.code.should == "401"
+      response.should_not be_success
     end
-    it "should not allow read" do
-      get :show, {:id => 1}, :format => :json
-      response.code.should == "401"
-    end
-    # it "should not allow update"
-  end
-
-  context "when correct HTTP_AUTHORIZATION" do
-    before do
-      request.env['HTTP_AUTHORIZATION'] = "legit:x"
-    end
-    it "should allow index" do
-      get :index, :format => :json
-      response.code.should == "200"
-    end
-  end
-
-  context "when authenticated as admin" do
-  end
-
-  context "when no HTTP_AUTHORIZATION" do
-    it_should_behave_like "access denied"
-    context "when authenticated as admin" do
-      it_should_behave_like "access denied"
-    end
+    
+    it "needs more tests"
   end
 
 end
