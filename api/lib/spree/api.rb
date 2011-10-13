08d8cdbca0658c51980171a1ddf37f5a2b0593fa
Ryan Bigg <radarlistener@gmail.com>
2011-10-13 16:23:40 +1100

    [api] Add lib/spree/api.rb This file is required for bootstrapping the API gem for spree

---
+++ api/lib/spree/api.rb
@@ -0,0 +1,10 @@
+require 'spree_core'
+require 'spree_auth'
+
+module Spree
+  module Api
+    
+  end
+end
+
+require 'spree/api/engine'
--- api/lib/spree/api/engine.rb
+++ api/lib/spree/api/engine.rb
@@ -1,17 +1,19 @@
-module SpreeApi
-  class Engine < Rails::Engine
-    engine_name 'spree_api'
+module Spree
+  module Api
+    class Engine < Rails::Engine
+      engine_name 'spree_api'
 
-    def self.activate
-      Dir.glob(File.join(File.dirname(__FILE__), "../../../app/**/*_decorator*.rb")) do |c|
-        Rails.application.config.cache_classes ? require(c) : load(c)
+      def self.activate
+        Dir.glob(File.join(File.dirname(__FILE__), "../../../app/**/*_decorator*.rb")) do |c|
+          Rails.application.config.cache_classes ? require(c) : load(c)
+        end
+        Dir.glob(File.join(File.dirname(__FILE__), "../../../app/overrides/*.rb")) do |c|
+          Rails.application.config.cache_classes ? require(c) : load(c)
+        end
       end
-      Dir.glob(File.join(File.dirname(__FILE__), "../../../app/overrides/*.rb")) do |c|
-        Rails.application.config.cache_classes ? require(c) : load(c)
-      end
+
+      config.autoload_paths += %W(#{config.root}/lib)
+      config.to_prepare &method(:activate).to_proc
     end
-
-    config.autoload_paths += %W(#{config.root}/lib)
-    config.to_prepare &method(:activate).to_proc
   end
 end
