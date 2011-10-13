661e10aca302d5103cd9644e06dfe1e2d3b12ec9
Trung Lê <joneslee85@gmail.com>
2011-10-13 18:43:28 +1100

    spree_dash namespace wip

--- dash/config/routes.rb
+++ dash/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  match '/admin' => 'admin/overview#index', :as => :admin
-  match '/admin/overview/get_report_data' => 'admin/overview#get_report_data'
+  namespace :spree do
+    match '/admin' => 'admin/overview#index', :as => :admin
+    match '/admin/overview/get_report_data' => 'admin/overview#get_report_data'
+  end
 end
