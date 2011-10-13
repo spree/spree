a85ea83a04e914cd1e11b9843438315248e6dc6c
John Dyer <lbrapid@gmail.com>
2011-10-13 11:02:30 -0400

    Fix migration migrate adjustments

--- promo/db/migrate/20101026184833_migrate_adjustments.rb
+++ promo/db/migrate/20101026184833_migrate_adjustments.rb
@@ -1,7 +1,7 @@
 class MigrateAdjustments < ActiveRecord::Migration
   def self.up
     execute('update spree_adjustments set amount = 0.0 where amount is null')
-    #execute('update spree_adjustments set mandatory = 'true', locked = 'true'')
+    Spree::Adjustment.update_all(:mandatory => true, :locked => true)
   end
 
   def self.down
