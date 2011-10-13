f2e8e5adc718845238b8a927a8668796861dc7da
John Dyer <lbrapid@gmail.com>
2011-10-13 15:18:49 -0400

    Namespace promo routes

--- promo/config/routes.rb
+++ promo/config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    resources :promotions do
-      resources :promotion_rules
-      resources :promotion_actions
+  namespace :spree do
+    namespace :admin do
+      resources :promotions do
+        resources :promotion_rules
+        resources :promotion_actions
+      end
     end
   end
 end
