da9a405f02275aa7cde8695d6c6fcbddceff6f1f
Trung Lê <joneslee85@gmail.com>
2011-10-13 14:58:58 +1100

    Refactored spree_api again

--- api/lib/spree/api/engine.rb
+++ api/lib/spree/api/engine.rb
@@ -3,10 +3,10 @@ module SpreeApi
     engine_name 'spree_api'
 
     def self.activate
-      Dir.glob(File.join(File.dirname(__FILE__), "../../app/**/*_decorator*.rb")) do |c|
+      Dir.glob(File.join(File.dirname(__FILE__), "../../../app/**/*_decorator*.rb")) do |c|
         Rails.application.config.cache_classes ? require(c) : load(c)
       end
-      Dir.glob(File.join(File.dirname(__FILE__), "../../app/overrides/*.rb")) do |c|
+      Dir.glob(File.join(File.dirname(__FILE__), "../../../app/overrides/*.rb")) do |c|
         Rails.application.config.cache_classes ? require(c) : load(c)
       end
     end
