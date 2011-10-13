893240f419fa8c6671df1c25a514cee29b44a8e3
Trung Lê <joneslee85@gmail.com>
2011-10-13 18:16:57 +1100

    Cleanup

--- features/support/share_connections.rb
+++ features/support/share_connections.rb
@@ -10,4 +10,3 @@ end
 # Forces all threads to share the same connection. This works on
 # Capybara because it starts the web server in a thread.
 ActiveRecord::Base.shared_connection = ActiveRecord::Base.connection
-
