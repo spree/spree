de4a3843ec04455c581a6a50aa7655a5cda9153b
Trung Lê <joneslee85@gmail.com>
2011-10-17 10:51:02 +1100

    [core] Intl a TaxCategoriesController string

--- core/app/controllers/spree/admin/tax_categories_controller.rb
+++ core/app/controllers/spree/admin/tax_categories_controller.rb
@@ -1,10 +1,9 @@
 module Spree
   module Admin
     class TaxCategoriesController < ResourceController
-
       def index
         if Spree::Config[:show_price_inc_vat] and (TaxCategory.where(:is_default => true).count != 1)
-          flash.notice = "You should configure exactly one default category with your countries default tax rate"
+          flash.notice = t(:one_default_category_with_default_tax_rate)
         end
       end
     end
