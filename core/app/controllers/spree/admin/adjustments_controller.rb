e360cc37b4496b5d0579e30dfa7157a1473b083c
Ryan Bigg <radarlistener@gmail.com>
2011-10-18 22:25:16 +1100

    Inherit from namespaced ResourceController + explain why

--- core/app/controllers/spree/admin/adjustments_controller.rb
+++ core/app/controllers/spree/admin/adjustments_controller.rb
@@ -1,6 +1,9 @@
 module Spree
   module Admin
-    class AdjustmentsController < ResourceController
+    # Need to explicitly reference namespaced ResourceController here
+    # Due to the rd_resource_controller gem, which provides a top-level
+    # ResourceController module, which is obviously not inheritable from.
+    class AdjustmentsController < Spree::Admin::ResourceController
       belongs_to 'spree/order', :find_by => :number
       destroy.after :reload_order
 
