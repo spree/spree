c410975f7680a3c8b2866ee6781247e5ad0fa12a
Trung Lê <joneslee85@gmail.com>
2011-10-19 09:24:52 +1100

    Revert "Inherit from namespaced ResourceController + explain why" This workaround is not needed anymore as rd_resource_controller is now removed
    This reverts commit 3959c7b9f008e6653e79674a3af1f907cf90cbb9.

--- core/app/controllers/spree/admin/adjustments_controller.rb
+++ core/app/controllers/spree/admin/adjustments_controller.rb
@@ -1,9 +1,6 @@
 module Spree
   module Admin
-    # Need to explicitly reference namespaced ResourceController here
-    # Due to the rd_resource_controller gem, which provides a top-level
-    # ResourceController module, which is obviously not inheritable from.
-    class AdjustmentsController < Spree::Admin::ResourceController
+    class AdjustmentsController < ResourceController
       belongs_to 'spree/order', :find_by => :number
       destroy.after :reload_order
 
