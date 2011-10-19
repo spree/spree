cb20d491ade39f7cf63a0a9091a9d4f2fa5ef764
Trung Lê <joneslee85@gmail.com>
2011-10-20 01:49:17 +1100

    [core] add class_name to model association

--- core/app/models/spree/prototype.rb
+++ core/app/models/spree/prototype.rb
@@ -1,5 +1,10 @@
 class Spree::Prototype < ActiveRecord::Base
+<<<<<<< HEAD
   has_and_belongs_to_many :properties, :class_name => 'Spree::Property', :join_table => "spree_properties_prototypes"
   has_and_belongs_to_many :option_types, :class_name => 'Spree::OptionType', :join_table => "spree_option_types_prototypes"
+=======
+  has_and_belongs_to_many :properties, :class_name => 'Spree::Property'
+  has_and_belongs_to_many :option_types, :class_name => 'Spree::OptionType'
+>>>>>>> [core] add class_name to model association
   validates :name, :presence => true
 end
