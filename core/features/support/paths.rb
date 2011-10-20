6babc9cf9aac6ab3b495ad19e574d0dd4300fbdb
Trung Lê <joneslee85@gmail.com>
2011-10-20 21:35:49 +1100

    [core] no idea why namespaced admin_path does not work, do a manual /admin for now

--- core/features/support/paths.rb
+++ core/features/support/paths.rb
@@ -11,14 +11,14 @@ module NavigationHelpers
     when /the home\s?page/
       '/'
     when /the admin home page/
-      admin_path
+      '/admin'
     when /the sign in page/
       new_user_session_path
     when /the sign up page/
       new_user_registration_path
     when /an invalid taxon page/
         "/t/totally_bogus_taxon"
-        
+
 
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
