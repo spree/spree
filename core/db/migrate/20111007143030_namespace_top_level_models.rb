f4113b8269f92f284b00236ec0e130cad946ae3b
Trung Lê <joneslee85@gmail.com>
2011-10-21 13:07:53 +1100

    [core] Rename table name to name with spree_ prefix

--- core/db/migrate/20111007143030_namespace_top_level_models.rb
+++ core/db/migrate/20111007143030_namespace_top_level_models.rb
@@ -7,13 +7,17 @@ class NamespaceTopLevelModels < ActiveRecord::Migration
     rename_table :assets,                  :spree_assets
     rename_table :calculators,             :spree_calculators
     rename_table :countries,               :spree_countries
+    rename_table :coupons,                 :spree_coupons
     rename_table :creditcards,             :spree_creditcards
+    rename_table :gateways,                :spree_gateways
     rename_table :inventory_units,         :spree_inventory_units
     rename_table :line_items,              :spree_line_items
     rename_table :log_entries,             :spree_log_entries
     rename_table :mail_methods,            :spree_mail_methods
     rename_table :option_types,            :spree_option_types
     rename_table :option_values,           :spree_option_values
+    rename_table :option_types_prototypes, :spree_option_types_prototypes
+    rename_table :option_values_variants,  :spree_option_values_variants
     rename_table :orders,                  :spree_orders
     rename_table :payments,                :spree_payments
     rename_table :payment_methods,         :spree_payment_methods
@@ -24,10 +28,13 @@ class NamespaceTopLevelModels < ActiveRecord::Migration
     rename_table :product_option_types,    :spree_product_option_types
     rename_table :product_properties,      :spree_product_properties
     rename_table :product_scopes,          :spree_product_scopes
+    rename_table :products_taxons,         :spree_products_taxons
     rename_table :properties,              :spree_properties
     rename_table :prototypes,              :spree_prototypes
+    rename_table :properties_prototypes,   :spree_properties_prototypes
     rename_table :return_authorizations,   :spree_return_authorizations
     rename_table :roles,                   :spree_roles
+    rename_table :roles_users,             :spree_roles_users
     rename_table :shipments,               :spree_shipments
     rename_table :shipping_categories,     :spree_shipping_categories
     rename_table :shipping_methods,        :spree_shipping_methods
@@ -51,7 +58,9 @@ class NamespaceTopLevelModels < ActiveRecord::Migration
     rename_table :spree_configurations,          :configurations
     rename_table :spree_assets,                  :assets
     rename_table :spree_calculators,             :calculators
+    rename_table :spree_coupons,                 :coupons
     rename_table :spree_countries,               :countries
+    rename_table :spree_gateways,                :gateways
     rename_table :spree_creditcards,             :creditcards
     rename_table :spree_inventory_units,         :inventory_units
     rename_table :spree_line_items,              :line_items
@@ -60,6 +69,8 @@ class NamespaceTopLevelModels < ActiveRecord::Migration
     rename_table :spree_option_types,            :option_types
     rename_table :spree_option_values,           :option_values
     rename_table :spree_orders,                  :orders
+    rename_table :spree_option_values_variants,  :option_values_variants
+    rename_table :spree_option_types_prototypes, :option_types_prototypes
     rename_table :spree_payments,                :payments
     rename_table :spree_payment_methods,         :payment_methods
     rename_table :spree_preferences,             :preferences
@@ -69,10 +80,13 @@ class NamespaceTopLevelModels < ActiveRecord::Migration
     rename_table :spree_product_option_types,    :product_option_types
     rename_table :spree_product_properties,      :product_properties
     rename_table :spree_product_scopes,          :product_scopes
+    rename_table :spree_products_taxons,         :products_taxons
     rename_table :spree_properties,              :properties
     rename_table :spree_prototypes,              :prototypes
+    rename_table :spree_properties_prototypes,   :properties_prototypes
     rename_table :spree_return_authorizations,   :return_authorizations
     rename_table :spree_roles,                   :roles
+    rename_table :spree_roles_users,             :roles_users
     rename_table :spree_shipments,               :shipments
     rename_table :spree_shipping_categories,     :shipping_categories
     rename_table :spree_shipping_methods,        :shipping_methods
