bde8114aa63d48937e6fe4270a1653501da55434
Trung Lê <joneslee85@gmail.com>
2011-10-26 23:53:59 +1100

    Fixed cucumber load_default_data fixture loading

--- features/support/load_default_data.rb
+++ features/support/load_default_data.rb
@@ -2,8 +2,8 @@ DatabaseCleaner.clean
 
 # load default data for tests
 require 'active_record/fixtures'
-fixtures_dir = File.expand_path('../../../core/db/default', __FILE__)
-ActiveRecord::Fixtures.create_fixtures(fixtures_dir, ['spree_countries', 'spree_zones', 'spree_zone_members', 'spree_states', 'spree_roles'])
+fixtures_dir = File.expand_path('../../../core/db/default/', __FILE__)
+ActiveRecord::Fixtures.create_fixtures(fixtures_dir, ['spree/countries', 'spree/zones', 'spree/zone_members', 'spree/states', 'spree/roles'])
 Spree::PaymentMethod::Check.create(:name => 'Check')
 
 # use transactions for faster tests
