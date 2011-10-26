cfcd1a10bc83a2e5ce27f01d6ff5f8aa35911df5
Brian Quinn <briandquinn@gmail.com>
2011-10-26 14:04:17 +0100

    [core] Working checkout

--- core/app/models/spree/gateway.rb
+++ core/app/models/spree/gateway.rb
@@ -7,7 +7,7 @@ class Spree::Gateway < Spree::PaymentMethod
   preference :test_mode, :boolean, :default => true
 
   def payment_source_class
-    Creditcard
+    Spree::Creditcard
   end
 
   # instantiates the selected gateway and configures with the options stored in the database
