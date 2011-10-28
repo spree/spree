e392f68dce78f0f0d869fa9c90ad88795668af34
John Dyer <lbrapid@gmail.com>
2011-10-28 11:19:56 -0400

    Setup api spec_helper for request specs

--- api/spec/spec_helper.rb
+++ api/spec/spec_helper.rb
@@ -3,10 +3,17 @@
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../dummy/config/environment", __FILE__)
 require 'rspec/rails'
+require 'database_cleaner'
+require 'spree_core/testing_support/factories'
 
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
+
+# load default data for tests
+require 'active_record/fixtures'
+fixtures_dir = File.expand_path('../../../core/db/default', __FILE__)
+ActiveRecord::Fixtures.create_fixtures(fixtures_dir, ['countries', 'zones', 'zone_members', 'states', 'roles'])
 
 RSpec.configure do |config|
   # == Mock Framework
@@ -23,7 +30,30 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, comment the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  #config.use_transactional_fixtures = true
 
-  include Devise::TestHelpers
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+
+  config.include SpreeApi::Engine.routes.url_helpers,
+    :example_group => {
+      :file_path => /\bspec\/controllers\//
+    }
+
+  config.include Devise::TestHelpers, :type => :controller
+  config.include Rack::Test::Methods
+end
+
+def api_login(user)
+  #post_via_redirect user_session_path, 'user[email]' => user.email, 'user[password]' => user.password
+  authorize user.authentication_token, "X"
 end
