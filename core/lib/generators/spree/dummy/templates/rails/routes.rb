74d23cf3811e1b99f0f4b8bf3eaed13079b32818
Brian Quinn <briandquinn@gmail.com>
2011-10-28 18:05:02 +0100

    Tweak test_app routes template

--- core/lib/generators/spree/dummy/templates/rails/routes.rb
+++ core/lib/generators/spree/dummy/templates/rails/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  mount <%= lib_name.classify %>::Engine => "/"
+  <%='mount Spree::Auth::Engine => "/"' if defined?(Spree::Auth) %>
+  <%='mount Spree::Api::Engine => "/"' if defined?(Spree::Api) %>
+  <%='mount Spree::Promo::Engine => "/"' if defined?(Spree::Promo) %>
+  <%='mount Spree::Dash::Engine => "/"' if defined?(Spree::Dash) %>
+  <%='mount Spree::Core::Engine => "/"' if defined?(Spree::Core) %>
 end
