150574d8898c200acefa5743573748548dd47eac
Michal Cichra <michal.cichra@gmail.com>
2011-10-29 14:45:46 +0300

    Use ActiveSupport::Multibyte::Chars#titleize instead of custom method
    Works with Unicode words like "Přehled"
    
    [Fixes #722]
    
    Conflicts:
    
    	core/app/helpers/admin/navigation_helper.rb

--- core/app/helpers/spree/admin/navigation_helper.rb
+++ core/app/helpers/spree/admin/navigation_helper.rb
@@ -16,7 +16,7 @@ module Spree
         destination_url = options[:url] || spree_core.send("#{options[:route]}_path")
 
         # if more than one form, it'll capitalize all words
-        label_with_first_letters_capitalized = t(options[:label], :default => options[:label]).gsub(/\b\w/){|c| c.upcase}
+        label_with_first_letters_capitalized = t(options[:label], :default => options[:label]).titleize
 
         link = link_to(label_with_first_letters_capitalized, destination_url)
 
