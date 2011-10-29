11460cb3edd660ccf2a2d255906793fd74301f8e
Trung Lê <joneslee85@gmail.com>
2011-10-29 12:10:08 +1100

    [core] Added :class_name for model assoc

--- core/app/models/spree/user.rb
+++ core/app/models/spree/user.rb
@@ -1,6 +1,6 @@
 # Default implementation of User.  This class is intended to be modified by extensions (ex. spree_auth)
 class Spree::User < ActiveRecord::Base
-  has_many :orders
+  has_many :orders, :class_name => 'Spree::Order'
 
   belongs_to :ship_address, :foreign_key => 'ship_address_id', :class_name => 'Spree::Address'
   belongs_to :bill_address, :foreign_key => 'bill_address_id', :class_name => 'Spree::Address'
