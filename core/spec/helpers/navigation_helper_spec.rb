d61a2aeb9c59056bcdc259fd0abe9a3e8b9bb641
Michal Cichra <michal.cichra@gmail.com>
2011-10-29 14:45:46 +0300

    Use ActiveSupport::Multibyte::Chars#titleize instead of custom method
    Works with Unicode words like "Přehled"
    
    [Fixes #722]

--- core/spec/helpers/navigation_helper_spec.rb
+++ core/spec/helpers/navigation_helper_spec.rb
@@ -1,3 +1,4 @@
+# coding: UTF-8
 require 'spec_helper'
 
 describe Admin::NavigationHelper do
@@ -6,6 +7,16 @@ describe Admin::NavigationHelper do
       it "should capitalize the first letter of each word in the tab's label" do
         admin_tab = tab(:orders)
         admin_tab.should include("Orders")
+      end
+
+      it "should accept options with label and capitalize each word of it" do
+        admin_tab = tab(:orders, :label => "delivered orders")
+        admin_tab.should include("Delivered Orders")
+      end
+
+      it "should capitalize words with unicode characters" do
+        admin_tab = tab(:orders, :label => "přehled") # overview
+        admin_tab.should include("Přehled")
       end
     end
   end
