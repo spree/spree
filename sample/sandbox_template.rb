ade9aa16ae4772b11758ddf745632c861066c0cb
Trung Lê <joneslee85@gmail.com>
2011-10-31 19:40:20 +1100

    [sample] removed defunct sandbox_template.rb

--- sample/sandbox_template.rb
+++
@@ -1,4 +0,0 @@
-#File.open("Gemfile", 'w') { |f| }
-# IMPORTANT: __FILE__ refer to (eval), not to sandbox_template.rb !!!
-gem "spree", :path => ".."
-gem 'rspec-rails', '~> 2.1.0', :group => 'test'
