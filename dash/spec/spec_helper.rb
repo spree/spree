f3178cccec733f040ceada2ae054b60581b588c7
Trung Lê <joneslee85@gmail.com>
2011-11-01 16:09:59 +1100

    [dash] add controller hacks

--- dash/spec/spec_helper.rb
+++ dash/spec/spec_helper.rb
@@ -6,7 +6,7 @@ require 'rspec/rails'
 
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
-Dir['#{File.dirname(__FILE__)}/support/**/*.rb'].each { |f| require f }
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 
 RSpec.configure do |config|
   # == Mock Framework
