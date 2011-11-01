f4e6b3d8939ded1179f30588d16c055503947708
John Dyer <lbrapid@gmail.com>
2011-11-01 10:21:15 -0400

    Setup promo spec helper

--- promo/spec/spec_helper.rb
+++ promo/spec/spec_helper.rb
@@ -3,6 +3,7 @@
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../dummy/config/environment", __FILE__)
 require 'rspec/rails'
+require 'database_cleaner'
 
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
@@ -10,11 +11,31 @@ Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
 
 require 'spree/core/testing_support/factories'
 require 'factories'
+require 'active_record/fixtures'
 
 RSpec.configure do |config|
   config.mock_with :rspec
 
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+
+  config.include SpreePromo::Engine.routes.url_helpers,
+    :example_group => {
+      :file_path => /\bspec\/controllers\//
+    }
+
+  config.include Rack::Test::Methods
 end
