e5c8b147f7b7b3ea367f8912c9b290fa190f6b08
Trung Lê <joneslee85@gmail.com>
2011-11-05 09:58:55 +1100

    Why those deleted files come back and yet not get notified in GH?

--- api/app/controllers/api/states_controller.rb
+++
@@ -1,8 +0,0 @@
-class Api::StatesController < Api::BaseController
-  before_filter :access_denied, :except => [:index, :show]
-
-  private
-  def parent
-    @parent ||= Spree::Country.find(params[:country_id])
-  end
-end
