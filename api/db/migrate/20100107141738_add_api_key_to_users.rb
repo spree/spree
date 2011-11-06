911118e77a8401923d0c45dcb46608d530d6ed94
Trung Lê <joneslee85@gmail.com>
2011-11-06 21:03:10 +1100

    [api] migration refactor

--- api/db/migrate/20100107141738_add_api_key_to_users.rb
+++ api/db/migrate/20100107141738_add_api_key_to_users.rb
@@ -1,9 +1,5 @@
 class AddApiKeyToUsers < ActiveRecord::Migration
-  def self.up
+  def change
     add_column :spree_users, :api_key, :string, :limit => 40
-  end
-
-  def self.down
-    remove_column :spree_users, :api_key
   end
 end
