e6596db121a1a754d8ac2f02c41980a88a2fb123
David Bennett <davidbennett@bravevision.com>
2011-11-09 11:21:27 +1030

    Fix eWay test mode so it looks for the :test_mode and not :test preference Fixes #554
    Conflicts:
    
    	core/app/models/gateway/eway.rb

--- core/app/models/spree/gateway/eway.rb
+++ core/app/models/spree/gateway/eway.rb
@@ -8,5 +8,16 @@ module Spree
     def provider_class
       ActiveMerchant::Billing::EwayGateway
     end
+
+    def options
+      # add :test key in the options hash, as that is what the ActiveMerchant::Billing::EwayGateway expects
+      if self.prefers? :test_mode
+        self.class.default_preferences[:test] = true
+      else
+        self.class.default_preferences.delete(:test)
+      end
+
+      super
+    end
   end
 end
