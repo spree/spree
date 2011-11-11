4120a141e4e96ba6e85bfe34e577c725d06af718
Ryan Bigg <radarlistener@gmail.com>
2011-11-11 16:22:53 +1100

    Change behaviour of has_option scope in Spree::Variant
    This now takes an option_type and then a series of possible option_values. More sensible

--- core/app/models/spree/variant/scopes.rb
+++ core/app/models/spree/variant/scopes.rb
@@ -2,16 +2,20 @@ module Spree
   class Variant < ActiveRecord::Base
     #FIXME WARNING tested only under sqlite and postgresql
     scope :descend_by_popularity, order("COALESCE((SELECT COUNT(*) FROM  #{Spree::LineItem.quoted_table_name} GROUP BY #{Spree::LineItem.quoted_table_name}.variant_id HAVING #{Spree::LineItem.quoted_table_name}.variant_id = #{Spree::Variant.quoted_table_name}.id), 0) DESC")
+
+
+    class << self
+      # Returns variants that match a given option value
+      #
+      # Example:
+      #
+      # product.variants_including_master.has_option(OptionType.find_by_name("shoe-size"),OptionValue.find_by_name("8"))
+      def has_option(option_type, *options)
+        joins(:option_values => :option_type).where("spree_option_types.name = ? AND spree_option_values.id IN (?)", option_type.name, options.map(&:id))
+      end
+
+      alias_method :has_options, :has_option
+    end
   end
 
 end
-
-# for selecting variants with an option value
-# no option type given since the value implies an option type
-# this scope can be chained repeatedly, since the join name is unique
-Spree::Variant.scope :has_option, lambda {|opt|
-  tbl = 'o' + Time.now.to_i.to_s + Time.now.usec.to_s
-  { :joins => "inner join spree_option_values_variants as #{tbl} on #{Spree::Variant.quoted_table_name}.id = #{tbl}.variant_id",
-    :conditions => ["#{tbl}.option_value_id = (?)", opt]
-  }
-}
--- core/spec/models/variant/scopes_spec.rb
+++ core/spec/models/variant/scopes_spec.rb
@@ -1,12 +1,23 @@
 require 'spec_helper'
 
 describe "Variant scopes" do
+  let!(:product) { Factory(:product) }
+  let!(:variant_1) { Factory(:variant, :product => product) }
+  let!(:variant_2) { Factory(:variant, :product => product) }
+
   it ".descend_by_popularity" do
     # Requires a product with at least two variants, where one has a higher number of orders than the other
-    product = Factory(:product)
-    variant_1 = Factory(:variant, :product => product)
-    variant_2 = Factory(:variant, :product => product)
     Factory(:line_item, :variant => variant_1)
     Spree::Variant.descend_by_popularity.first.should == variant_1
+  end
+
+  it "finding by option values" do
+    option_type = Factory(:option_type, :name => "bar")
+    option_value = Factory(:option_value, :name => "foo", :option_type => option_type)
+    variant_1.option_values << option_value
+    variant_1.save
+
+    variants = product.variants_including_master.has_option(option_type, option_value)
+    variants.should include(variant_1)
   end
 end
