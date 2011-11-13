fb9a16eac19f2ed64f8e03d77e310a3ecfe8aa2f
Sean Schofield <sean@railsdog.com>
2011-11-13 17:30:50 -0500

    Removed unnecessary module from decorator class

--- promo/app/models/spree/adjustment_decorator.rb
+++ promo/app/models/spree/adjustment_decorator.rb
@@ -1,5 +1,3 @@
-module Spree
-  class Adjustment.class_eval do
-    scope :promotion, lambda { where('label LIKE ?', "#{I18n.t(:promotion)}%") }
-  end
+class Adjustment.class_eval do
+  scope :promotion, lambda { where('label LIKE ?', "#{I18n.t(:promotion)}%") }
 end
