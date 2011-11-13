c506f8eb51a8b69d53f060ad312d7c60ea579ab3
Trung Lê <joneslee85@gmail.com>
2011-11-13 23:40:08 +1100

    [core] fix analytic tracker spec - ruby19

--- core/spec/requests/admin/configuration/analytics_tracker_spec.rb
+++ core/spec/requests/admin/configuration/analytics_tracker_spec.rb
@@ -34,6 +34,7 @@ describe "Analytics Tracker" do
     it "should be able to create a new analytics tracker" do
       click_link "admin_new_tracker_link"
       fill_in "tracker_analytics_id", :with => "A100"
+      select "Production", :from => "tracker-env"
       click_button "Create"
 
       page.should have_content("successfully created!")
