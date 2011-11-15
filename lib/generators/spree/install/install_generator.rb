342bab84c7c2d34cfa26a944d4b863111c993b32
Ryan Bigg <radarlistener@gmail.com>
2011-11-16 11:36:03 +1300

    Need to also mount Spree::Promo::Engine. Related to #768

--- lib/generators/spree/install/install_generator.rb
+++ lib/generators/spree/install/install_generator.rb
@@ -144,7 +144,8 @@ Spree::Auth::Engine.load_seed if defined?(Spree::Auth)
     def notify_about_routes
       unless options[:skip_install_data]
         puts "************************************************************************************************************"
-        puts "Be sure to add the following two lines to your application's config/routes.rb file to finish setting up spree"
+        puts "Be sure to add the following three lines to your application's config/routes.rb file to finish setting up spree"
+        puts "mount Spree::Promo::Engine, :at => '/'"
         puts "mount Spree::Auth::Engine, :at => '/'"
         puts "mount Spree::Core::Engine, :at => '/'"
       end
