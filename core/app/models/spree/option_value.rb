ad2ddfa3c8b98bb42f1f86f07411fa59ae282af9
Trung Lê <joneslee85@gmail.com>
2011-11-15 11:16:33 +1100

    [core] no need to specify :class_name

--- core/app/models/spree/option_value.rb
+++ core/app/models/spree/option_value.rb
@@ -2,7 +2,6 @@ module Spree
   class OptionValue < ActiveRecord::Base
     belongs_to :option_type
     acts_as_list :scope => :option_type
-    has_and_belongs_to_many :variants, :join_table => 'spree_option_values_variants',
-                                       :class_name => 'Spree::Variant'
+    has_and_belongs_to_many :variants, :join_table => 'spree_option_values_variants'
   end
 end
