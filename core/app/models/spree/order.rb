7d0cbd8bbfaabc3b3f68c73578b8365b47e20a8c
Ryan Bigg <radarlistener@gmail.com>
2011-11-17 09:38:37 +1300

    Fix hash keys indentation in Order#create_shipment

--- core/app/models/spree/order.rb
+++ core/app/models/spree/order.rb
@@ -284,8 +284,8 @@ module Spree
         shipment.update_attributes(:shipping_method => shipping_method)
       else
         self.shipments << Shipment.create(:order => self,
-                                                 :shipping_method => shipping_method,
-                                                 :address => self.ship_address)
+                                          :shipping_method => shipping_method,
+                                          :address => self.ship_address)
       end
 
     end
