784a2471c9e0f0de96a4cc63d8c0280d7e88dcdb
Ryan Bigg <radarlistener@gmail.com>
2011-11-17 15:39:30 +1300

    more login_path fixes

--- auth/spec/requests/password_reset_spec.rb
+++ auth/spec/requests/password_reset_spec.rb
@@ -2,14 +2,14 @@ require 'spec_helper'
 
 describe "Reset Password" do
   it "should allow the user to indicate they have forgotten their password" do
-    visit login_path
+    visit spree.login_path
     click_link "Forgot Password?"
     page.should have_content("your password will be emailed to you")
   end
 
   it "should allow a user to supply an email for the password reset" do
     user = Factory(:user, :email => "foobar@example.com", :password => "secret", :password_confirmation => "secret")
-    visit login_path
+    visit spree.login_path
     click_link "Forgot Password?"
     fill_in "user_email", :with => "foobar@example.com"
     click_button "Reset my password"
