e7b9ba3a6eba4004541a57b488c71e0430d0a354
Ryan Bigg <radarlistener@gmail.com>
2011-11-18 15:14:28 +1300

    [dash] prepend routes to core

--- dash/config/routes.rb
+++ dash/config/routes.rb
@@ -1,4 +1,4 @@
-Spree::Dash::Engine.routes.draw do
+Spree::Core::Engine.routes.prepend do
   match '/admin' => 'admin/overview#index', :as => :admin
   match '/admin/overview/get_report_data' => 'admin/overview#get_report_data'
 end
