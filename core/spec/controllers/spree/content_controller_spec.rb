b7df15c0efb99b1b0c334b914ea49898ec73f539
Ryan Bigg <radarlistener@gmail.com>
2011-11-18 15:14:15 +1300

    [core] Stub current user in content_controller_spec

--- core/spec/controllers/spree/content_controller_spec.rb
+++ core/spec/controllers/spree/content_controller_spec.rb
@@ -8,6 +8,7 @@ describe Spree::ContentController do
   end
 
   it "should not display a local file" do
+    controller.stub :current_user => Factory(:user)
     get :show, :path => "../../Gemfile"
     response.response_code.should == 404
   end
