99e23275d49454bc806f0dde2af1f27486f4420b
Ryan Bigg <radarlistener@gmail.com>
2011-11-18 15:53:43 +1300

    No need to mount indiviiual routes in dummy routes.rb

--- core/lib/generators/spree/dummy/templates/rails/routes.rb
+++ core/lib/generators/spree/dummy/templates/rails/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  <%= 'mount Spree::Auth::Engine => "/"' if defined?(Spree::Auth) %>
-  <%= 'mount Spree::Api::Engine => "/"' if defined?(Spree::Api) %>
-  <%= 'mount Spree::Promo::Engine => "/"' if defined?(Spree::Promo) %>
-  <%= 'mount Spree::Dash::Engine => "/"' if defined?(Spree::Dash) %>
   <%= 'mount Spree::Core::Engine => "/"' if defined?(Spree::Core) %>
 end
