1a117eacf2dd5870f8d514b16d42be32ff3fbed4
Ryan Bigg <radarlistener@gmail.com>
2011-11-18 15:12:23 +1300

    There's no more spree_promo
    Plus, this would only be run if spree promo is installed anyway

--- promo/app/overrides/promo_admin_tabs.rb
+++ promo/app/overrides/promo_admin_tabs.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(:virtual_path => "spree/layouts/admin",
                      :name => "promo_admin_tabs",
                      :insert_bottom => "[data-hook='admin_tabs'], #admin_tabs[data-hook]",
-                     :text => "<% if respond_to?(:spree_promo) %><%= tab(:promotions, :url => spree.admin_promotions_path) %><% end %>",
+                     :text => "<%= tab(:promotions, :url => spree.admin_promotions_path) %>",
                      :disabled => false)
