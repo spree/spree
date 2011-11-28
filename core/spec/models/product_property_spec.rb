0d913523c0546fa25221c2cbb7b7e1378a316cd1
Mauro Botelho <mauro@vitrue.com>
2011-11-28 10:27:06 -0500

    Make sure value for product_property is not longer than 255 characters.

--- core/spec/models/product_property_spec.rb
+++ core/spec/models/product_property_spec.rb
@@ -4,6 +4,12 @@ describe Spree::ProductProperty do
 
   context "validations" do
     it { should have_valid_factory(:product_property) }
+    it "should validate length of value" do
+      pp = Factory(:product_property)
+      pp.value = "x" * 256
+      pp.should_not be_valid
+    end
+
   end
 
 end
