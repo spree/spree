621b8b1d065a373ec5063630f515e397ddf7b0a3
Sean Schofield <sean.schofield@gmail.com>
2011-12-03 14:36:32 -0500

    Deprecated product_price helper

--- core/app/helpers/spree/products_helper.rb
+++ core/app/helpers/spree/products_helper.rb
@@ -14,12 +14,8 @@ module Spree
 
     # returns the price of the product to show for display purposes
     def product_price(product_or_variant, options={})
-      options.assert_valid_keys(:format_as_currency, :show_vat_text)
-      options.reverse_merge! :format_as_currency => true, :show_vat_text => Spree::Config[:show_price_inc_vat]
-
-      amount = product_or_variant.price
-      amount += Calculator::Vat.calculate_tax_on(product_or_variant) if Spree::Config[:show_price_inc_vat]
-      options.delete(:format_as_currency) ? format_price(amount, options) : amount
+       ActiveSupport::Deprecation.warn('product_price is deprecated and no longer calculates
+                                        tax.  Use number_to_currency instead.', caller)
     end
 
     # converts line breaks in product description into <p> tags (for html display purposes)
--- core/spec/helpers/products_helper_spec.rb
+++ core/spec/helpers/products_helper_spec.rb
@@ -3,32 +3,6 @@ require 'spec_helper'
 module Spree
   describe ProductsHelper do
     include ProductsHelper
-    include BaseHelper
-
-    context "#product_price" do
-      before do
-        reset_spree_preferences
-      end
-
-      let!(:tax_category) { Factory(:tax_category) }
-      let!(:product) { Factory(:product, :tax_category => tax_category) }
-
-      it "shows a product's price" do
-      reset_spree_preferences do |config|
-        config.show_price_inc_vat = false
-      end
-        product_price(product).should == "$19.99"
-      end
-
-      it "shows a product's price including tax" do
-        pending "Broken on the CI server, but not on dev machines. To be investigated later."
-        product.stub :tax_category => tax_category
-        tax_category.stub :effective_amount => BigDecimal.new("0.05", 2)
-        Spree::Config.set :show_price_inc_vat => true
-        product_price(product).should == "$20.99 (inc. VAT)"
-      end
-
-    end
 
     context "#variant_price_diff" do
       before do
