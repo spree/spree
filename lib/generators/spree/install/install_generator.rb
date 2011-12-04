9eae36b9903bca676416da4fe597eb704420a0c9
Trung Lê <joneslee85@gmail.com>
2011-12-05 00:58:39 +1100

    Added overrides load for install generator

--- lib/generators/spree/install/install_generator.rb
+++ lib/generators/spree/install/install_generator.rb
@@ -58,8 +58,13 @@ Disallow: /users
       application <<-APP
 
     config.to_prepare do
-      #loads application's model / class decorators
+      # Load application's model / class decorators
       Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
+        Rails.configuration.cache_classes ? require(c) : load(c)
+      end
+
+      # Load application's view overrides
+      Dir.glob(File.join(File.dirname(__FILE__), "../app/overrides/*.rb")) do |c|
         Rails.configuration.cache_classes ? require(c) : load(c)
       end
     end
