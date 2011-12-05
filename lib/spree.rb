48a86d4c44c25168ca70a40c1612a64e9e861b64
Ryan Bigg <radarlistener@gmail.com>
2011-12-06 10:42:12 +1100

    Add documentation for Spree.config

--- lib/spree.rb
+++ lib/spree.rb
@@ -6,6 +6,13 @@ require 'spree_promo'
 require 'spree_sample'
 
 module Spree
+  # Used to configure Spree.
+  #
+  # Example:
+  #
+  #   Spree::Config do |config|
+  #     config.site_name = "An awesome Spree site"
+  #   end
   def self.config(&block)
     yield(Spree::Config)
   end
