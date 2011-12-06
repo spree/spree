6cd41573e8175c3dcdd23844fe4ea2c1a686d2d3
Trung Lê <joneslee85@gmail.com>
2011-12-07 09:20:48 +1100

    Refactored views

--- core/app/views/spree/admin/shared/_update_order_state.js
+++ core/app/views/spree/admin/shared/_update_order_state.js
@@ -1,6 +1,6 @@
 $('#order_tab_summary h5#order_status').html('<%= "#{t(:status)}: #{t("order_state.#{@order.state}")}" %>');
 $('#order_tab_summary h5#order_total').html('<%= "#{t(:total)}: #{number_to_currency @order.total}" %>');
 <% if @order.completed? %>
-$('#order_tab_summary h5#payment_status').html('<%= "#{t(:payment)}: #{t("payment_states.#{@order.payment_state}")}"  %>');
-$('#order_tab_summary h5#shipment_status').html('<%= "#{t(:shipment)}: #{t("shipment_states.#{@order.shipment_state}")}"  %>');
+$('#order_tab_summary h5#payment_status').html('<%= "#{t(:payment)}: #{t("payment_states.#{@order.payment_state}")}" %>');
+$('#order_tab_summary h5#shipment_status').html('<%= "#{t(:shipment)}: #{t("shipment_states.#{@order.shipment_state}")}" %>');
 <% end %>
