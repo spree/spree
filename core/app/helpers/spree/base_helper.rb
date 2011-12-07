190c508a90485ad25dc7979cd9fca04193b7f879
Michal Cichra <michal@o2h.cz>
2011-12-07 21:56:05 +0100

    fixed issue when content of link to cart was escaped
    problem was following: link_to_cart calls order_currency
    (which is properly marked as html_safe), but concatenates it
    with another string, resulting to html unsafe string
    
    fix is following: if no argument is passed to link_to_cart
    use value from i18n, otherwise escape passed value
    then create content of link and mark it as html safe

--- core/app/helpers/spree/base_helper.rb
+++ core/app/helpers/spree/base_helper.rb
@@ -1,15 +1,19 @@
 module Spree
   module BaseHelper
-    def link_to_cart(text = t(:cart))
+    def link_to_cart(text = nil)
       return "" if current_page?(cart_path)
+
+      text = text ? h(text) : t('cart')
       css_class = nil
+
       if current_order.nil? or current_order.line_items.empty?
-        text = "#{text}: (#{t(:empty)})"
+        text = "#{text}: (#{t('empty')})"
         css_class = 'empty'
       else
-        text = "#{text}: (#{current_order.item_count}) #{order_subtotal(current_order)}"
+        text = "#{text}: (#{current_order.item_count}) #{order_subtotal(current_order)}".html_safe
         css_class = 'full'
       end
+
       link_to text, cart_path, :class => css_class
     end
 
