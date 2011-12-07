ecc41e07666e20b5ad4296c6c238673aab398646
Daniel Konishi <dkonishi@macbookair.fabrica.locaweb.com.br>
2011-12-07 14:03:15 -0200

    Delete the root taxon and let to nested_set gem the responsibility of deleted all children taxons.

--- core/app/models/spree/taxonomy.rb
+++ core/app/models/spree/taxonomy.rb
@@ -2,10 +2,11 @@ module Spree
   class Taxonomy < ActiveRecord::Base
     validates :name, :presence => true
 
-    has_many :taxons, :dependent => :destroy
+    has_many :taxons
     has_one :root, :conditions => { :parent_id => nil }, :class_name => 'Spree::Taxon'
 
     after_save :set_name
+    after_destroy :destroy_root_taxon
 
     private
       def set_name
@@ -14,6 +15,10 @@ module Spree
         else
           self.root = Taxon.create!({ :taxonomy_id => self.id, :name => self.name })
         end
+      end
+
+      def destroy_root_taxon
+        self.root.destroy
       end
   end
 end
--- core/spec/models/taxonomy_spec.rb
+++ core/spec/models/taxonomy_spec.rb
@@ -10,5 +10,18 @@ describe Spree::Taxonomy do
     it {should validate_presence_of(:name) }
   end
 
+  context "#destroy" do
+    before do
+       @taxonomy = Factory(:taxonomy)
+       @root_taxon = @taxonomy.root
+       @child_taxon = Factory(:taxon, :taxonomy_id => @taxonomy.id, :parent => @root_taxon)
+    end
+
+    it "should destroy all associated taxons" do
+      @taxonomy.destroy
+      expect{ Spree::Taxon.find(@root_taxon.id) }.to raise_error(ActiveRecord::RecordNotFound)
+      expect{ Spree::Taxon.find(@child_taxon.id) }.to raise_error(ActiveRecord::RecordNotFound)
+    end
+  end
 end
 
