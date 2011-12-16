77be1d3c9d5daa13abb5040b5209d80e815338b0
Ryan Bigg <radarlistener@gmail.com>
2011-12-16 17:49:40 +1100

    No more effective_amount method, remove tests for it from tax_category_spec

--- core/spec/models/tax_category_spec.rb
+++ core/spec/models/tax_category_spec.rb
@@ -14,31 +14,4 @@ describe Spree::TaxCategory do
     end
   end
 
-  context 'effective_amount' do
-    let(:rate) { Factory :tax_rate, :amount => 0.1}
-    let(:category) { Factory :tax_category, :tax_rates => [rate] }
-
-    it "should return nil when default_country is not included in zone" do
-      rate.zone.stub(:include? => false)
-      category.effective_amount.should be_nil
-    end
-
-    it "should return amount when default_country is included in zone" do
-      rate.zone.stub(:include? => true)
-      category.effective_amount.should == rate.amount
-    end
-
-    it "should return nil when address supplied is not included in zone" do
-      rate.zone.stub(:include? => false)
-      category.effective_amount(Spree::Address.new).should be_nil
-    end
-
-    it "should return amount when address supplied is included in zone" do
-      rate.zone.stub(:include? => true)
-      category.effective_amount(Spree::Address.new).should == rate.amount
-    end
-
-  end
-
-
 end
