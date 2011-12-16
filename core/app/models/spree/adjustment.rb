097a135373ed95d3c7fc6ad35a3dda6de77adc02
Sean Schofield <sean@railsdog.com>
2011-12-16 11:07:16 -0500

    New credit scope for Adjustment

--- core/app/models/spree/adjustment.rb
+++ core/app/models/spree/adjustment.rb
@@ -34,6 +34,7 @@ module Spree
     scope :shipping, lambda { where(:label => I18n.t(:shipping)) }
     scope :optional, where(:mandatory => false)
     scope :eligible, where(:eligible => true)
+    scope :credit, where("amount < 0")
 
     after_save :update_adjustable
     after_destroy :update_adjustable
