74fe963d2ae98afa0f891c3aebb84f6c45f51c0a
John Dyer <lbrapid@gmail.com>
2011-12-16 13:45:05 -0500

    Restore ContentController under /content which is needed by Promotions.
    [Fixes #882]

---
+++ promo/app/controllers/spree/content_controller_decorator.rb
@@ -0,0 +1,9 @@
+# Keep a record ot all static page paths visited for promotions that require them
+Spree::ContentController.class_eval do
+  after_filter :store_visited_path
+
+  def store_visited_path
+    session[:visited_paths] ||= []
+    session[:visited_paths] = (session[:visited_paths]  + [params[:path]]).compact.uniq
+  end
+end
