7d4833a1bc603029fbe3e9a4b734afaeae73be17
Trung Lê <joneslee85@gmail.com>
2011-12-17 15:37:34 +1100

    Tests for #879

--- core/spec/models/zone_spec.rb
+++ core/spec/models/zone_spec.rb
@@ -1,7 +1,8 @@
 require 'spec_helper'
 
 describe Spree::Zone do
-
+  let(:country) { Factory :country }
+  let(:state) { Factory :state }
   let(:zone) { Factory :zone }
 
   context 'factory' do
@@ -19,4 +20,27 @@ describe Spree::Zone do
     end
   end
 
+  context "#country_list" do
+    context "given a zone of countries" do
+      it 'should return a list of countries' do
+        zone.zone_members = [Spree::ZoneMember.create(:zoneable => country)]
+        zone.country_list.should == [country]
+      end
+    end
+    
+    context "given a zone of states" do
+      it 'should return a list of countries that states belongs to' do
+        zone.zone_members = [Spree::ZoneMember.create(:zoneable => state)]
+        zone.country_list.should == [state.country]
+      end
+    end
+
+    context "given a zone of zones" do
+      it 'should return a list of countries belong to the children zones' do
+        sub_zone = Factory.build(:zone, :zone_members => [Spree::ZoneMember.create(:zoneable => country)])
+        zone.zone_members = [Spree::ZoneMember.create(:zoneable => sub_zone)]
+        zone.country_list.should == [country]
+      end
+    end
+  end
 end
