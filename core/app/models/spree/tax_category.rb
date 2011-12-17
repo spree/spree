bf01287245e3c845c7f0b6621bf083d5ee428917
Sean Schofield <sean.schofield@gmail.com>
2011-12-17 11:41:09 -0500

    Restore default tax category - turns out it was somewhat useful.

--- core/app/models/spree/tax_category.rb
+++ core/app/models/spree/tax_category.rb
@@ -1,6 +1,18 @@
 module Spree
   class TaxCategory < ActiveRecord::Base
     validates :name, :presence => true, :uniqueness => true
+
     has_many :tax_rates, :dependent => :destroy
+
+    before_save :set_default_category
+
+    def set_default_category
+      #set existing default tax category to false if this one has been marked as default
+
+      if is_default && tax_category = self.class.where(:is_default => true).first
+        tax_category.update_attribute(:is_default, false)
+      end
+    end
+
   end
 end
--- core/db/migrate/20111215020900_remove_default_tax_category.rb
+++
@@ -1,8 +0,0 @@
-class RemoveDefaultTaxCategory < ActiveRecord::Migration
-  def up
-    remove_column :spree_tax_categories, :is_default
-  end
-
-  def down
-  end
-end
