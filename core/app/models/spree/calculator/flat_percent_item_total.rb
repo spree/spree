7f1ed182a5fcf08e0c552bff7c095a768d66f7ec
Ryan Bigg <radarlistener@gmail.com>
2011-12-18 11:43:34 +1030

    Convert preferred_flat_percent to string so that BigDecimal is happy on 1.8.7, 1.9.2 and ree

--- core/app/models/spree/calculator/flat_percent_item_total.rb
+++ core/app/models/spree/calculator/flat_percent_item_total.rb
@@ -9,7 +9,7 @@ module Spree
     def compute(object)
       return unless object.present? and object.line_items.present?
       item_total = object.line_items.map(&:amount).sum
-      value = item_total * BigDecimal(self.preferred_flat_percent) / 100.0
+      value = item_total * BigDecimal(self.preferred_flat_percent.to_s) / 100.0
       (value * 100).round.to_f / 100
     end
   end
