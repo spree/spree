0a1ca1da92e45ac3213caa70dfe5cad681f935c1
Ryan Bigg <radarlistener@gmail.com>
2011-12-30 10:24:35 +1100

    Don't put type of object in variable name for product_names_hash

--- core/app/helpers/spree/admin/base_helper.rb
+++ core/app/helpers/spree/admin/base_helper.rb
@@ -179,9 +179,9 @@ module Spree
 
       def product_picker_field(name, value)
         products = Product.with_ids(value.split(','))
-        product_names_hash = products.inject({}){|memo,item| memo[item.id] = item.name; memo}
+        product_names = products.inject({}){|memo,item| memo[item.id] = item.name; memo}
         product_rules = products.collect{ |p| { :id => p.id, :name => p.name } }
-        %(<input type="text" name="#{name}" value="#{value}" class="tokeninput products" data-names='#{product_names_hash.to_json}' data-pre='#{product_rules.to_json}'/>).html_safe
+        %(<input type="text" name="#{name}" value="#{value}" class="tokeninput products" data-names='#{product_names.to_json}' data-pre='#{product_rules.to_json}'/>).html_safe
       end
 
       # renders set of hidden fields and button to add new record using nested_attributes
