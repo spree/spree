8c6f0c6e5d9a4b69f5435e66f39a02bbcb7ace6f
Trung Lê <joneslee85@gmail.com>
2012-01-05 16:03:06 +1100

    Added workaround for tab helper in the case that engine mounted at /. See #910

--- core/app/helpers/spree/admin/navigation_helper.rb
+++ core/app/helpers/spree/admin/navigation_helper.rb
@@ -22,8 +22,9 @@ module Spree
         css_classes = []
 
         selected = if options[:match_path]
-          # TODO: Seems a Rails issue- revisit issue #910
-          request.fullpath.starts_with?("//admin#{options[:match_path]}")
+          # TODO: `request.fullpath` for engines mounted at '/' returns '//'
+          # which seems an issue with Rails routing.- revisit issue #910
+          request.fullpath.gsub('//', '/').starts_with?("#{root_path}admin#{options[:match_path]}")
         else
           args.include?(controller.controller_name.to_sym)
         end
