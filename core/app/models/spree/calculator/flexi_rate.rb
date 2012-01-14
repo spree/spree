4282e3291702793cc12812501a00ba1565b85c11
buddhi <buddhi.desilva@gmail.com>
2012-01-14 20:56:59 +0800

    Fixes/Tweaks flexi_rate.rb:5 - Doesn't have to be a float. Changed to int. flexi_rate.rb:17 - Seemingly self.preferred_max_items is returned as a string. Gives the error 'comparison of String with 0 failed'. Converting to an int before assigning. flexi_rate.rb:22, 24 - Similar issues as above. Converting to a float before calculation.
    Closes #974

--- core/app/models/spree/calculator/flexi_rate.rb
+++ core/app/models/spree/calculator/flexi_rate.rb
@@ -2,7 +2,7 @@ module Spree
   class Calculator::FlexiRate < Calculator
     preference :first_item,      :decimal, :default => 0.0
     preference :additional_item, :decimal, :default => 0.0
-    preference :max_items,       :decimal, :default => 0.0
+    preference :max_items,       :integer, :default => 0
 
     def self.description
       I18n.t(:flexible_rate)
@@ -14,14 +14,14 @@ module Spree
 
     def compute(object)
       sum = 0
-      max = self.preferred_max_items
+      max = self.preferred_max_items.to_i
       items_count = object.line_items.map(&:quantity).sum
       items_count.times do |i|
         # check max value to avoid divide by 0 errors
         if (max == 0 && i == 0) || (max > 0) && (i % max == 0)
-          sum += self.preferred_first_item
+          sum += self.preferred_first_item.to_f
         else
-          sum += self.preferred_additional_item
+          sum += self.preferred_additional_item.to_f
         end
       end
 
