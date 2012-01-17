230f98979de0d6b51c45cc6422922e2480a31eb7
Trung Lê <joneslee85@gmail.com>
2012-01-17 18:29:52 +1100

    Added functional test for admin product details edit

---
+++ core/spec/requests/admin/products/edit/products_spec.rb
@@ -0,0 +1,29 @@
+# encoding: UTF-8
+require 'spec_helper'
+
+describe 'Product Details' do
+  context 'editing a product' do
+    it 'should list the product details' do
+      Factory(:product, :name => 'apache baseball cap', :description => 'lorem ipsum',
+              :sku => 'A100', :available_on => '2011-01-01 01:01:01', :count_on_hand => 10)
+
+      sign_in_as!(Factory(:admin_user))
+      visit spree.admin_path
+      click_link 'Products'
+      within('table.index tr:nth-child(2)') { click_link 'Edit' }
+      click_link 'Product Details'
+
+      within('#content') do
+        find('h1').text.should == 'Editing Product “apache baseball cap”'
+        find('input#product_name').value.should == 'apache baseball cap'
+        find('input#product_permalink').value.should == 'apache-baseball-cap'
+        find('textarea#product_description').text.should == 'lorem ipsum'
+        find('input#product_price').value.should == '19.99'
+        find('input#product_cost_price').value.should == '17.00'
+        find('input#product_available_on').value.should == '2011-01-01 01:01:01.000000'
+        find('input#product_sku').value.should == 'A100'
+      end
+
+    end
+  end
+end
