5d829637f938be1a17fa3e8901b2a3b947bccdec
Ryan Bigg <radarlistener@gmail.com>
2012-01-18 13:59:31 +1100

    first_order promotion rule should allow for eligible?(order) (i.e. an order with a user) and eligible?(order, :user => user) (i.e. an order WITHOUT a user but with user data in payload)

--- promo/app/models/spree/promotion/rules/first_order.rb
+++ promo/app/models/spree/promotion/rules/first_order.rb
@@ -1,7 +1,8 @@
 module Spree
   class Promotion::Rules::FirstOrder < PromotionRule
     def eligible?(order, options = {})
-      !!(options[:user] && options[:user].orders.complete.count == 0)
+      user = order.try(:user) || options[:user]
+      !!(user && user.orders.complete.count == 0)
     end
   end
 end
--- promo/spec/models/promotion/rules/first_order_spec.rb
+++ promo/spec/models/promotion/rules/first_order_spec.rb
@@ -8,12 +8,21 @@ describe Spree::Promotion::Rules::FirstOrder do
     rule.should_not be_eligible(order)
   end
 
-  it "should be eligible if user does not have any other completed orders yet" do
-    user = mock_model(Spree::User)
-    # TODO: refactor, probably it would be good to change that to method in user model, like completed_orders
-    user.stub_chain(:orders, :complete, :count => 0)
+  context "should be eligible if user does not have any other completed orders yet" do
+    let(:user) { mock_model(Spree::User) }
 
-    rule.should be_eligible(order, :user => user)
+    before do
+      user.stub_chain(:orders, :complete, :count => 0)
+    end
+
+    it "for an order without a user, but with user in payload data" do
+      rule.should be_eligible(order, :user => user)
+    end
+
+    it "for an order with a user, no user in payload data" do
+      order.stub :user => user
+      rule.should be_eligible(order)
+    end
   end
 
   it "should be not eligible if user have at least one complete order" do
