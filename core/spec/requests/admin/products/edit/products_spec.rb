18ad5c7a47c4fbee1cbca63d493090383ccf266c
Trung Lê <joneslee85@gmail.com>
2012-01-18 21:52:00 +1100

    Fix escaped quotes and product name in admin product edit header

--- core/spec/requests/admin/products/edit/products_spec.rb
+++ core/spec/requests/admin/products/edit/products_spec.rb
@@ -4,8 +4,8 @@ require 'spec_helper'
 describe 'Product Details' do
   context 'editing a product' do
     it 'should list the product details' do
-      Factory(:product, :name => 'apache baseball cap', :description => 'lorem ipsum',
-              :sku => 'A100', :available_on => '2011-01-01 01:01:01', :count_on_hand => 10)
+      Factory(:product, :name => 'Bún thịt nướng', :permalink => 'bun-thit-nuong', :sku => 'A100',
+              :description => 'lorem ipsum', :available_on => '2011-01-01 01:01:01', :count_on_hand => 10)
 
       sign_in_as!(Factory(:admin_user))
       visit spree.admin_path
@@ -14,9 +14,9 @@ describe 'Product Details' do
       click_link 'Product Details'
 
       within('#content') do
-        find('h1').text.should == 'Editing Product “apache baseball cap”'
-        find('input#product_name').value.should == 'apache baseball cap'
-        find('input#product_permalink').value.should == 'apache-baseball-cap'
+        find('h1').text.should == 'Editing Product “Bún thịt nướng”'
+        find('input#product_name').value.should == 'Bún thịt nướng'
+        find('input#product_permalink').value.should == 'bun-thit-nuong'
         find('textarea#product_description').text.should == 'lorem ipsum'
         find('input#product_price').value.should == '19.99'
         find('input#product_cost_price').value.should == '17.00'
