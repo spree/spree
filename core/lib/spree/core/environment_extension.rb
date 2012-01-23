b2d014e71385feccfaed51b1a50fbf78a8f58bf3
Ryan Bigg <radarlistener@gmail.com>
2012-01-23 14:56:29 +1100

    Don't define an InstanceMethods module in Spree::Core::EnvironmentExtension
    As it causes Rails 3.2 deprecation warnings. If the module is already being included into some place, instance methods can just go in the module. No need to submodule them at all.

--- core/lib/spree/core/environment_extension.rb
+++ core/lib/spree/core/environment_extension.rb
@@ -3,26 +3,22 @@ module Spree
     module EnvironmentExtension
       extend ActiveSupport::Concern
 
-      module InstanceMethods
+      def add_class(name)
+        self.instance_variable_set "@#{name}", Set.new
+        create_method( "#{name}=".to_sym ) { |val|
+          instance_variable_set( "@" + name, val)
+        }
 
-        def add_class(name)
-          self.instance_variable_set "@#{name}", Set.new
-          create_method( "#{name}=".to_sym ) { |val| 
-            instance_variable_set( "@" + name, val)
-          }
-
-          create_method( name.to_sym ) { 
-            instance_variable_get( "@" + name ) 
-          }
-        end
-
-        private
-
-          def create_method( name, &block )
-            self.class.send( :define_method, name, &block )
-          end
-
+        create_method( name.to_sym ) {
+          instance_variable_get( "@" + name )
+        }
       end
+
+      private
+
+        def create_method( name, &block )
+          self.class.send( :define_method, name, &block )
+        end
     end
   end
 end
