563fa3c4c377eb6311d14ced3e3c0adc7d10f70c
Ryan Bigg <radarlistener@gmail.com>
2012-01-23 16:09:42 +1100

    Silence the install generator in common_rake.rb

--- core/lib/spree/core/testing_support/common_rake.rb
+++ core/lib/spree/core/testing_support/common_rake.rb
@@ -8,7 +8,7 @@ namespace :common do
     require "#{ENV['LIB_NAME']}"
 
     Spree::DummyGenerator.start ["--lib_name=#{ENV['LIB_NAME']}", "--database=#{ENV['DB_NAME']}", "--quiet"]
-    Spree::InstallGenerator.start ["--lib_name=#{ENV['LIB_NAME']}", "--auto-accept", "--migrate=false", "--seed=false", "--sample=false"]
+    Spree::InstallGenerator.start ["--lib_name=#{ENV['LIB_NAME']}", "--auto-accept", "--migrate=false", "--seed=false", "--sample=false", "--quiet"]
 
     puts "Setting up dummy database..."
     cmd = "bundle exec rake db:drop db:create db:migrate RAILS_ENV=test AUTO_ACCEPT=true"
