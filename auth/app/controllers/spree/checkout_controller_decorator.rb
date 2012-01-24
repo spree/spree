67beda3bac7990112091598ab803afe60f59fbd4
Ryan Bigg <radarlistener@gmail.com>
2012-01-24 16:24:08 +1100

    [auth] Move comment for current_order.state hack to just above hack in checkout_controller_decorator

--- auth/app/controllers/spree/checkout_controller_decorator.rb
+++ auth/app/controllers/spree/checkout_controller_decorator.rb
@@ -9,8 +9,8 @@ Spree::CheckoutController.class_eval do
   end
 
   def update_registration
-    # hack - temporarily change the state to something other than cart so we can validate the order email address
     fire_event("spree.user.signup", :order => current_order)
+    # hack - temporarily change the state to something other than cart so we can validate the order email address
     current_order.state = 'address'
     if current_order.update_attributes(params[:order])
       redirect_to checkout_path
