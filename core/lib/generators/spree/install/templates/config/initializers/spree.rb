34c9981c3e2b8dbfa46823eb669c4abe4381ca78
Jeff Dutil <jdutil21@gmail.com>
2012-01-26 08:14:26 -0500

    Update with note about overriding changes made in user interface.

--- core/lib/generators/spree/install/templates/config/initializers/spree.rb
+++ core/lib/generators/spree/install/templates/config/initializers/spree.rb
@@ -1,7 +1,10 @@
-# Used to configure Spree Preferences
+# Configure Spree Preferences
 # 
-# In order to override a default setting set: config.setting_name = 'new value'
+# Note: Initializing preferences available within the Admin will overwrite any changes that were made through the user interface when you restart.
+#       If you would like users to be able to update a setting with the Admin it should NOT be set here.
 #
+# In order to initialize a setting do: 
+# config.setting_name = 'new value'
 Spree.config do |config|
   # Example:
   # Uncomment to override the default site name.
