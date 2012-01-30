8841d2a8efbf51c216654646251ccf4f23ecbed0
Ryan Bigg <radarlistener@gmail.com>
2012-01-31 09:56:34 +1100

    Move promo specific stuff from core's new_preferences into promo migration
    See comment inside new migration as to reasoning wh=y

--- core/db/migrate/20120119024710_new_preferences.rb
+++ core/db/migrate/20120119024710_new_preferences.rb
@@ -17,46 +17,6 @@ class NewPreferences < ActiveRecord::Migration
     change_column :spree_preferences, :group_id, :integer, :null => true
     change_column :spree_preferences, :group_type, :string, :null => true
 
-    # namespace promo tables
-    rename_table :promotion_actions, :spree_promotion_actions
-    rename_table :promotion_rules, :spree_promotion_rules
-    rename_table :promotion_rules_users, :spree_promotion_rules_users
-    rename_table :promotion_action_line_items, :spree_promotion_action_line_items
-    rename_table :products_promotion_rules, :spree_products_promotion_rules
-
-    # add old promo preferences as columns
-    add_column :spree_activators, :usage_limit, :integer
-    add_column :spree_activators, :match_policy, :string, :default => 'all'
-    add_column :spree_activators, :code, :string
-    add_column :spree_activators, :advertise, :boolean, :default => false
-
-    Spree::Promotion.class_eval do
-      preference :usage_limit, :integer
-      preference :match_policy, :string, :default => 'all'
-      preference :code, :string
-      preference :advertise, :boolean, :default => false
-    end
-
-    # manually update old promotions to use the new promotion style
-    Spree::Preference.where(:owner_type => 'Spree::Activator').group_by(&:owner_id).each do |key, pref_group|
-      @promo = Spree::Promotion.new
-      pref_group.each do |pref|
-        case pref.name
-        when 'code'
-          @promo.code = pref.value.to_s
-          @promo.name = pref.value.to_s
-        when 'advertise'
-          @promo.advertise = pref.value
-        when 'usage_limit'
-          @promo.usage_limit = pref.value
-        when 'match_policy'
-          @promo.match_policy = pref.value
-        end
-        @promo.event_name = Spree::Promotion.find(pref.owner_id).event_name
-      end
-      @promo.save!
-    end
-
     OldPrefs.all.each do |old_pref|
       begin
         begin
@@ -83,14 +43,6 @@ class NewPreferences < ActiveRecord::Migration
     # Remove old promotion prefs
     Spree::Preference.where(:key => nil).delete_all
 
-    # Remove old promotions
-    Spree::Promotion.where(:code => nil).delete_all
-
-    remove_column :spree_preferences, :name
-    remove_column :spree_preferences, :owner_id
-    remove_column :spree_preferences, :owner_type
-    remove_column :spree_preferences, :group_id
-    remove_column :spree_preferences, :group_type
   end
 
   def down
@@ -102,11 +54,5 @@ class NewPreferences < ActiveRecord::Migration
     add_column :spree_preferences, :owner_type, :string
     add_column :spree_preferences, :group_id, :integer
     add_column :spree_preferences, :group_type, :string
-
-    rename_table :spree_promotion_actions, :promotion_actions
-    rename_table :spree_promotion_rules, :promotion_rules
-    rename_table :spree_promotion_rules_users, :promotion_rules_users
-    rename_table :spree_promotion_action_line_items, :promotion_action_line_items
-    rename_table :spree_products_promotion_rules, :products_promotion_rules
   end
 end
