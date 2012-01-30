9fe7f19ecf30261466bc2681a73e4162b942870e
John Dyer <lbrapid@gmail.com>
2012-01-30 11:04:40 -0500

    [Refs #1012] Fixes caching issue with old promotion preferences not being ported correctly.

--- promo/db/migrate/20111013155037_namespace_promo_tables.rb
+++
@@ -1,9 +0,0 @@
-class NamespacePromoTables < ActiveRecord::Migration
-  def change
-    rename_table :promotion_actions, :spree_promotion_actions
-    rename_table :promotion_rules, :spree_promotion_rules
-    rename_table :promotion_rules_users, :spree_promotion_rules_users
-    rename_table :promotion_action_line_items, :spree_promotion_action_line_items
-    rename_table :products_promotion_rules, :spree_products_promotion_rules
-  end
-end
