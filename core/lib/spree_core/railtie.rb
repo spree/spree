c2a71a0f1cc47a4c97eabc8573bb4547454cced5
Graham Melcher <melcher@gmail.com>
2012-01-29 17:48:33 -0800

    Fix compatability issue with s3-aws backend (a class with Bucket in the name)

--- core/lib/spree_core/railtie.rb
+++ core/lib/spree_core/railtie.rb
@@ -28,7 +28,7 @@ module SpreeCore
           Calculator::FlatRate,
           Calculator::FlexiRate,
           Calculator::PerItem,
-          Calculator::PriceBucket]
+          Calculator::PriceTier]
 
        app.config.spree.calculators.tax_rates = [
           Calculator::SalesTax,
