32aabb5a1121a2f6c22454deb558eb71074f0e8b
Alexey Topolyanskiy <devilcoders@gmail.com>
2012-01-30 17:19:42 +0200

    Fixes some cross browser form issues

--- core/app/assets/javascripts/store/spree_core.js
+++ core/app/assets/javascripts/store/spree_core.js
@@ -1,6 +1,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery.validate/jquery.validate.min
+//= require jquery.formalize.min
 //= require store/checkout
 //= require store/product
 //= require store/cart
