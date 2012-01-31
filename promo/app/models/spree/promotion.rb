417c3ef2f24867c62409512d41480f43ec331a6a
Ryan Bigg <radarlistener@gmail.com>
2012-02-01 10:57:01 +1100

    Fix advertised scope on Promotion to actually return only advertised promotions. Fixes #931.

--- promo/app/models/spree/promotion.rb
+++ promo/app/models/spree/promotion.rb
@@ -27,11 +27,7 @@ module Spree
 
     class << self
       def advertised
-        #TODO this is broken because the new preferences aren't a direct relationship returning
-        #all for now
-        scoped
-        #includes(:stored_preferences)
-        #includes(:stored_preferences).where(:spree_preferences => {:name => 'advertise', :value => '1'})
+        where(:advertise => true)
       end
     end
 
