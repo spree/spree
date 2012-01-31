8e265209dd4ff13c827d589567acfd9f0a7cc738
Ryan Bigg <radarlistener@gmail.com>
2012-01-31 14:33:47 +1100

    [dash] Make the header_tags_spec tests less brittle
    The problem was that the product attributes could have been in any particular order. Same with the cart attributes

--- dash/spec/requests/header_tags_spec.rb
+++ dash/spec/requests/header_tags_spec.rb
@@ -9,29 +9,35 @@ describe "Header Tags" do
     Spree::DashConfiguration.new.token = "test_token"
   end
 
+  let(:analytics) { page.find("script#analytics") }
+
   it "includes the site_id on the home page" do
     visit spree.root_path
-    page.should have_content('jirafe= {"id":"2222"}')
+    jirafe = analytics.text =~ /jirafe= \{"id":"2222"\}/
+    jirafe.should_not be_nil
   end
 
   it "includes the product tag on the product page" do
     visit spree.root_path
     click_link "RoR Mug"
-    page.should have_content('"product":{"name":"RoR Mug"')
+    product = analytics.text =~ /"product":\{.*?"name":"RoR Mug".*?\}/
+    product.should_not be_nil
   end
 
   it "includes the cart tag on the cart page" do
     visit spree.root_path
     click_link "RoR Mug"
     click_button "add-to-cart-button"
-    page.should have_content('"cart":{"total":"19.99"')
+    cart = analytics.text =~ /"cart":\{.*?"total":"19.99".*?\}/
+    cart.should_not be_nil
   end
 
   it "includes the search tag on the results page" do
     visit spree.root_path
     fill_in "keywords", :with => 'mug'
     click_button "Search"
-    page.should have_content('"search":{"keyword":"mug"}')
+    search = analytics.text =~ /"search":\{.*?"keyword":"mug".*?\}/
+    search.should_not be_nil
   end
 
 end
