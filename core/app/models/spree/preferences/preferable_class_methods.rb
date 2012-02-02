706884e96de942158e8d811dea87ca7145f9143a
cmar <chris@spreecommerce.com>
2012-02-02 13:16:39 -0500

    handling preferences before activerecord is saved [fixes #940 #871]

--- core/app/models/spree/preferences/preferable_class_methods.rb
+++ core/app/models/spree/preferences/preferable_class_methods.rb
@@ -7,11 +7,17 @@ module Spree::Preferences
       default = options[:default]
       description = options[:description] || name
 
+      # cache_key will be nil for new objects, then if we check if there
+      # is a pending preference before going to default
       define_method preference_getter_method(name) do
-        if preference_store.exist? preference_cache_key(name)
+        if preference_cache_key(name) && preference_store.exist?(preference_cache_key(name))
           preference_store.get preference_cache_key(name)
         else
-          send self.class.preference_default_getter_method(name)
+          if get_pending_preference(name)
+            get_pending_preference(name)
+          else
+            send self.class.preference_default_getter_method(name)
+          end
         end
       end
       alias_method prefers_getter_method(name), preference_getter_method(name)
@@ -22,7 +28,12 @@ module Spree::Preferences
         if type == :boolean && !value.is_a?(TrueClass) && !value.is_a?(FalseClass)
           value = value.to_i == 1
         end
-        preference_store.set preference_cache_key(name), value
+
+        if preference_cache_key(name)
+          preference_store.set preference_cache_key(name), value
+        else
+          add_pending_preference(name, value)
+        end
       end
       alias_method prefers_setter_method(name), preference_setter_method(name)
 
