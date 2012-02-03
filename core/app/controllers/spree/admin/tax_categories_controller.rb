bd0ade336efc882653eac1863f37ee8f30ecd8f1
John Dyer <lbrapid@gmail.com>
2012-02-03 10:42:16 -0500

    [Fixes #1040] Mark Tax Categories as deleted rather than destroying them.

--- core/app/controllers/spree/admin/tax_categories_controller.rb
+++ core/app/controllers/spree/admin/tax_categories_controller.rb
@@ -1,6 +1,19 @@
 module Spree
   module Admin
     class TaxCategoriesController < ResourceController
+      def destroy
+        if @object.mark_deleted!
+          flash.notice = flash_message_for(@object, :successfully_removed)
+          respond_with(@object) do |format|
+            format.html { redirect_to collection_url }
+            format.js   { render :partial => "spree/admin/shared/destroy" }
+          end
+        else
+          respond_with(@object) do |format|
+            format.html { redirect_to collection_url }
+          end
+        end
+      end
     end
   end
 end
