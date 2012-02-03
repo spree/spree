eba64168d3acc57098fd7c658b5a702922afaa5f
John Dyer <lbrapid@gmail.com>
2012-02-03 10:27:23 -0500

    [Fixes #935] Resolves problems with reliable setting of boolean preferences

--- core/app/models/spree/preferences/preferable_class_methods.rb
+++ core/app/models/spree/preferences/preferable_class_methods.rb
@@ -20,7 +20,13 @@ module Spree::Preferences
         # Boolean attributes can come back from forms as '0' or '1'
         # Convert them to their correct values here
         if type == :boolean && !value.is_a?(TrueClass) && !value.is_a?(FalseClass)
-          value = value.to_i == 1
+          value = value.downcase if value.respond_to? :downcase
+          case value
+          when 0, '0', 'false', 'f', "", []
+            value = false
+          else
+            value = true
+          end
         end
         preference_store.set preference_cache_key(name), value
       end
