64a3b54fb99a08877b62fe3f8af24b5103b3d0e9
Brian Quinn <briandquinn@gmail.com>
2012-02-08 22:52:23 +0000

    Tweak perference migration to migrate extension preferences

--- core/db/migrate/20120119024710_new_preferences.rb
+++ core/db/migrate/20120119024710_new_preferences.rb
@@ -6,10 +6,13 @@ class NewPreferences < ActiveRecord::Migration
     attr_accessor :owner_klass
   end
 
+
   def up
     add_column :spree_preferences, :key, :string
     add_column :spree_preferences, :value_type, :string
     add_index :spree_preferences, :key, :unique => true
+
+    remove_index :spree_preferences, :name => 'ix_prefs_on_owner_attr_pref'
 
     # remove old constraints for migration
     change_column :spree_preferences, :name, :string, :null => true
@@ -18,8 +21,15 @@ class NewPreferences < ActiveRecord::Migration
     change_column :spree_preferences, :group_id, :integer, :null => true
     change_column :spree_preferences, :group_type, :string, :null => true
 
+    cfgs = execute("select id, type from spree_configurations")
+
+    execute("select id, owner_id, name from spree_preferences where owner_type = 'Spree::Configuration'").each do |pref|
+      configuration = cfgs.detect { |c| c[0] == pref[1] }
+      execute "UPDATE spree_preferences set `key` = '#{configuration[1].underscore}/#{pref[2]}', `owner_type` = null, `owner_id` = null where id = #{pref[0]}"
+    end
+
     OldPrefs.all.each do |old_pref|
-      next unless owner = old_pref.owner
+      next unless owner = (old_pref.owner rescue nil)
 
       unless old_pref.owner_type == "Spree::Activator" || old_pref.owner_type == "Spree::PromotionRule"
         old_pref.key = [owner.class.name, old_pref.name, owner.id].join('::').underscore
