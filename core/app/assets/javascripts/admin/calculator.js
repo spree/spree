bcfb8de65f117cffa9a39703d5cc9f44c2d4c207
Chris Nighitngale <ducatista@gmail.com>
2012-02-09 13:21:24 -0700

    Fixed JS to hide and disable preference controls when changing the calculator type.
    [fixes #1116]

--- core/app/assets/javascripts/admin/calculator.js
+++ core/app/assets/javascripts/admin/calculator.js
@@ -1,14 +1,14 @@
 $(function() {
   var original_calc_type = $('#calc-type').attr('value');
-  $('div#calculator-settings-warning').hide();
+  $('div.calculator-settings-warning').hide();
   $('#calc-type').change(function() {
     if ($('#calc-type').attr('value') == original_calc_type) {
       $('div.calculator-settings').show();
-      $('div#calculator-settings-warning').hide();
+      $('div.calculator-settings-warning').hide();
       $('.calculator-settings input').prop("disabled", false);
     } else {
       $('div.calculator-settings').hide();
-      $('div#calculator-settings-warning').show();
+      $('div.calculator-settings-warning').show();
       $('.calculator-settings input').prop("disabled", true);
     }
   });
