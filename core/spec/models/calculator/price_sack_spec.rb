4a7a4cc5318dfed4fcb4dd32a44ecdb39e7d0c16
Ryan Bigg <radarlistener@gmail.com>
2012-02-20 15:04:13 -0500

    Alias amount to cost in Shipment model. Fixes #1156

--- core/spec/models/calculator/price_sack_spec.rb
+++ core/spec/models/calculator/price_sack_spec.rb
@@ -3,9 +3,15 @@ require 'spec_helper'
 describe Spree::Calculator::PriceSack do
   let(:calculator) { Spree::Calculator::PriceSack.new }
   let(:order) { stub_model(Spree::Order) }
+  let(:shipment) { stub_model(Spree::Shipment) }
 
   # Regression test for #714 and #739
   it "computes with an order object" do
     calculator.compute(order)
+  end
+
+  # Regression test for #1156
+  it "computes with a snipment object" do
+    calculator.compute(shipment)
   end
 end
