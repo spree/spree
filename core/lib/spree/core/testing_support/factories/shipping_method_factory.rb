da363be5e341e08b0319f3128da555b0a7f83d6d
Ryan Bigg <radarlistener@gmail.com>
2012-02-20 11:03:49 -0500

    Build calculator factories in ShippingMethod factories to fix 'value outside of bounds' error on PostgreSQL

--- core/lib/spree/core/testing_support/factories/shipping_method_factory.rb
+++ core/lib/spree/core/testing_support/factories/shipping_method_factory.rb
@@ -2,22 +2,22 @@ FactoryGirl.define do
   factory :shipping_method, :class => Spree::ShippingMethod do
     zone { |a| Spree::Zone.find_by_name('GlobalZone') || a.association(:global_zone) }
     name 'UPS Ground'
-    calculator { |sm| Factory(:calculator, :calculable_id => sm.object_id, :calculable_type => 'Spree::ShippingMethod') }
+    calculator { Factory.build(:calculator) }
   end
 
   factory :free_shipping_method, :class => Spree::ShippingMethod do
     zone { |a| Spree::Zone.find_by_name('GlobalZone') || a.association(:global_zone) }
     name 'UPS Ground'
-    calculator { |sm| Factory(:no_amount_calculator, :calculable_id => sm.object_id, :calculable_type => 'Spree::ShippingMethod') }
+    calculator { Factory.build(:no_amount_calculator) }
   end
 
   factory :shipping_method_with_category, :class => Spree::ShippingMethod do
     zone { |a| Spree::Zone.find_by_name('GlobalZone') || a.association(:global_zone) }
     name 'UPS Ground'
-    calculator { |sm| Factory(:calculator, :calculable_id => sm.object_id, :calculable_type => 'Spree::ShippingMethod') }
     match_none nil
     match_one nil
     match_all nil
     association(:shipping_category, :factory => :shipping_category)
+    calculator { Factory.build(:calculator) }
   end
 end
