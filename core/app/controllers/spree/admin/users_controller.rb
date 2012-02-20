3a6f5d3d42381753a146bedbd13961a2d4f13a6c
Ryan Bigg <radarlistener@gmail.com>
2012-02-20 15:50:50 -0500

    Don't rescue from User::DestroyWithOrdersError if it is not defined. Fixes #1155

--- core/app/controllers/spree/admin/users_controller.rb
+++ core/app/controllers/spree/admin/users_controller.rb
@@ -1,7 +1,9 @@
 module Spree
   module Admin
     class UsersController < ResourceController
-      rescue_from User::DestroyWithOrdersError, :with => :user_destroy_with_orders_error
+      if User.const_defined?(DestroyWithOrdersError)
+        rescue_from User::DestroyWithOrdersError, :with => :user_destroy_with_orders_error
+      end
 
       # http://spreecommerce.com/blog/2010/11/02/json-hijacking-vulnerability/
       before_filter :check_json_authenticity, :only => :index
