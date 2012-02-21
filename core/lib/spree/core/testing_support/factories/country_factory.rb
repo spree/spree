1b63661482f9f3b7c6aea938eb0dd8e1059dc3e8
Ryan Bigg <radarlistener@gmail.com>
2012-02-21 10:20:03 -0500

    Change country factory to 'United States of Foo' to prevent conflicts with real United States from fixtures

--- core/lib/spree/core/testing_support/factories/country_factory.rb
+++ core/lib/spree/core/testing_support/factories/country_factory.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :country, :class => Spree::Country do
     iso_name 'UNITED STATES'
-    name 'United States'
+    name 'United States of Foo'
     iso 'US'
     iso3 'USA'
     numcode 840
