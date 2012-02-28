628c837e508f0e4d60fa17c16936484cefe58a57
danmorin <dan.morin@gmail.com>
2012-02-28 10:15:33 -0800

    Checking for :text in same line as :string

--- core/app/models/spree/preference.rb
+++ core/app/models/spree/preference.rb
@@ -10,9 +10,7 @@ class Spree::Preference < ActiveRecord::Base
   def value
     if self[:value_type].present?
       case self[:value_type].to_sym
-      when :string
-        self[:value].to_s
-      when :text
+      when :string || :text
         self[:value].to_s
       when :password
         self[:value].to_s
