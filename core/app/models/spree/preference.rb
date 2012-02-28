f0b4aafd665acfdaa83e79b3f7c6e5c98c5b2124
danmorin <dan.morin@gmail.com>
2012-02-27 20:53:32 -0800

    Preferences with value_type of text are returned as nil. This adds a text check and returns just like a string.

--- core/app/models/spree/preference.rb
+++ core/app/models/spree/preference.rb
@@ -12,6 +12,8 @@ class Spree::Preference < ActiveRecord::Base
       case self[:value_type].to_sym
       when :string, :text
         self[:value].to_s
+      when :text
+        self[:value].to_s
       when :password
         self[:value].to_s
       when :decimal
