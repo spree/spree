75e66c5b8b9967fc74a36857d817ad1134d87f0c
Alexey Topolyanskiy <devilcoders@gmail.com>
2012-03-01 13:27:39 +0200

    make cart.js more abstract from DOM

--- core/app/assets/javascripts/store/cart.js
+++ core/app/assets/javascripts/store/cart.js
@@ -2,7 +2,7 @@
   $(document).ready(function(){
     if($('form#update-cart').is('*')){
       $('form#update-cart a.delete').show().live('click', function(e){
-        $(this).parents('tr').first().find('input.line_item_quantity').val(0);
+        $(this).parents('.line-item').first().find('input.line_item_quantity').val(0);
         $(this).parents('form').first().submit();
         e.preventDefault();
       });
