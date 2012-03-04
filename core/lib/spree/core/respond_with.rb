c998e6bea0562d70313d0706763834b1ba288dcb
Jeff Dutil <jdutil21@gmail.com>
2012-03-04 17:33:21 -0500

    Fix respond_with.
    [Fixes #1227]

--- core/lib/spree/core/respond_with.rb
+++ core/lib/spree/core/respond_with.rb
@@ -4,20 +4,25 @@ module ActionController
       raise "In order to use respond_with, first you need to declare the formats your " <<
             "controller responds to in the class level" if self.class.mimes_for_respond_to.empty?
 
-      if response = retrieve_collector_from_mimes(&block)
+      if collector = retrieve_collector_from_mimes(&block)
         options = resources.size == 1 ? {} : resources.extract_options!
-        options.merge!(:default_response => response)
 
-        # following statement is not present in rails code. The action name is needed for processing
-        options.merge!(:action_name => action_name.to_sym)
-
-        # if responder is not specified then pass in Spree::Responder
-        (options.delete(:responder) || Spree::Responder).call(self, resources, options)
+        if defined_response = collector.response and Spree::BaseController.spree_responders.empty?
+          if action = options.delete(:action)
+            render :action => action
+          else
+            defined_response.call
+          end
+        else
+          # The action name is needed for processing
+          options.merge!(:action_name => action_name.to_sym)
+          # If responder is not specified then pass in Spree::Responder
+          (options.delete(:responder) || Spree::Responder).call(self, resources, options)
+        end
       end
     end
   end
 end
-
 
 module Spree
   module Core
