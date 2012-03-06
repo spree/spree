e769b933d97e04ae82cb9f5c7eff0a34033e7866
Ryan Bigg <radarlistener@gmail.com>
2012-03-06 15:34:01 -0800

    Add test for controller helper set_user_language. For #1184

---
+++ core/spec/controllers/controller_helpers_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+# In this file, we want to test that the controller helpers function correctly
+# So we need to use one of the controllers inside Spree.
+# ProductsController is good.
+describe Spree::ProductsController do
+
+  before do
+    controller.stub(:set_current_user)
+    I18n.stub(:available_locales => [:en, :de])
+    Spree::Config[:default_locale] = nil
+    Rails.application.config.i18n.default_locale = :de
+  end
+
+  after do
+    Spree::Config[:default_locale] = :en
+    Rails.application.config.i18n.default_locale = :en
+  end
+
+  # Regression test for #1184
+  it "sets the default locale based off config.i18n.default_locale" do
+    I18n.locale.should == :en
+    get :index
+    I18n.locale.should == :de
+  end
+end
