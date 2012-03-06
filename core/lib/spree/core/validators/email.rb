aea3107e6747ccfa814e5216d16ef3cc6d3d6292
Ryan Bigg <radarlistener@gmail.com>
2012-03-06 12:36:26 -0800

    Switch to using the Mail gem to validate emails from the order model. Fixes #1238 for good, probably.

---
+++ core/lib/spree/core/validators/email.rb
@@ -0,0 +1,23 @@
+# Borrowed from http://my.rails-royce.org/2010/07/21/email-validation-in-ruby-on-rails-without-regexp/
+# Mentioned in tweet here: https://twitter.com/_sohara/status/177120126083141633
+require 'mail'
+class EmailValidator < ActiveModel::EachValidator
+  def validate_each(record,attribute,value)
+    begin
+      m = Mail::Address.new(value)
+      # We must check that value contains a domain and that value is an email address
+      r = m.domain && m.address == value
+      t = m.__send__(:tree)
+      # We need to dig into treetop
+      # A valid domain must have dot_atom_text elements size > 1
+      # user@localhost is excluded
+      # treetop must respond to domain
+      # We exclude valid email values like <user@localhost.com>
+      # Hence we use m.__send__(tree).domain
+      r &&= (t.domain.dot_atom_text.elements.size > 1)
+    rescue Exception => e
+      r = false
+    end
+    record.errors[attribute] << (options[:message] || "is invalid") unless r
+  end
+end
