f69539097f8a2419142d87001944df16727575e9
Ryan Bigg <radarlistener@gmail.com>
2012-03-12 11:45:08 -0700

    Clear cookies as well as session in UserSessionsController#destroy. Fixes #1257.

--- auth/spec/requests/sign_out_spec.rb
+++ auth/spec/requests/sign_out_spec.rb
@@ -1,12 +1,23 @@
 require 'spec_helper'
 
 describe "Sign Out" do
-  it "should allow a signed in user to logout" do
-    user = Factory(:user, :email => "email@person.com", :password => "secret", :password_confirmation => "secret")
+  let!(:user) do
+   Factory(:user,
+          :email => "email@person.com",
+          :password => "secret",
+          :password_confirmation => "secret")
+  end
+
+  before do
     visit spree.login_path
     fill_in "user_email", :with => user.email
     fill_in "user_password", :with => user.password
+    # Regression test for #1257
+    check "Remember me"
     click_button "Login"
+  end
+
+  it "should allow a signed in user to logout" do
     click_link "Logout"
     visit spree.root_path
     page.should have_content("Login")
