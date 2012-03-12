e6a882203c511c58e00e89783d74f833f0aa71b6
Ryan Bigg <radarlistener@gmail.com>
2012-03-12 10:35:25 -0700

    Remove pending routing tests from checkout + content controller specs
    Nobody cares about them.

--- core/spec/controllers/spree/content_controller_spec.rb
+++ core/spec/controllers/spree/content_controller_spec.rb
@@ -2,11 +2,6 @@ require 'spec_helper'
 
 describe Spree::ContentController do
 
-  it "should understand routes" do
-    pending("assert_routing tests are now broken, is this relevant any more?")
-    assert_routing("/spree/content/cvv", {:controller => "content", :action => "cvv", :use_route => :spree })
-  end
-
   it "should not display a local file" do
     controller.stub :current_user => Factory(:user)
     get :show, :path => "../../Gemfile"
