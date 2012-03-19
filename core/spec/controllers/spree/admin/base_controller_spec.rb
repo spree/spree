aabdf4858f9b7a054a900c835921631a051a9a46
cmar <chris@spreecommerce.com>
2012-03-19 15:56:46 -0400

    only checking alerts in production [fixes 1292]

--- core/spec/controllers/spree/admin/base_controller_spec.rb
+++ core/spec/controllers/spree/admin/base_controller_spec.rb
@@ -22,9 +22,32 @@ describe Spree::Admin::BaseController do
     end
 
     it "saves alerts into session" do
+      controller.stub(:should_check_alerts? => true)
       Spree::Alert.should_receive(:current).and_return([Spree::Alert.new(:message => "test alert", :severity => 'release')])
       process :index
       session[:alerts].first.message.should eq "test alert"
+    end
+
+    describe "should_check_alerts?" do
+      before do
+        Rails.env.stub(:production? => true)
+        Spree::Config[:check_for_spree_alerts] = true
+        Spree::Config[:last_check_for_spree_alerts] = nil
+      end
+
+      it "only checks alerts if production and preference is true" do
+        controller.send(:should_check_alerts?).should be_true
+      end
+
+      it "only checks for production" do
+        Rails.env.stub(:production? => false)
+        controller.send(:should_check_alerts?).should be_false
+      end
+
+      it "only checks if preference is true" do
+        Spree::Config[:check_for_spree_alerts] = false
+        controller.send(:should_check_alerts?).should be_false
+      end
     end
   end
 end
