fc4709ef782622685a95e02f9a87a2491dce838e
Ryan Bigg <radarlistener@gmail.com>
2012-03-19 14:24:32 -0600

    li#link-to-cart is always on pge, so check for actual link inside cart_spec

--- core/spec/requests/cart_spec.rb
+++ core/spec/requests/cart_spec.rb
@@ -3,11 +3,11 @@ require 'spec_helper'
 describe "Cart" do
   it "shows cart icon on non-cart pages" do
     visit spree.root_path
-    lambda { find("li#link-to-cart") }.should_not raise_error(Capybara::ElementNotFound)
+    lambda { find("li#link-to-cart a") }.should_not raise_error(Capybara::ElementNotFound)
   end
 
   it "hides cart icon on cart page" do
     visit spree.cart_path
-    lambda { find("li#link-to-cart") }.should raise_error(Capybara::ElementNotFound)
+    lambda { find("li#link-to-cart a") }.should raise_error(Capybara::ElementNotFound)
   end
 end
