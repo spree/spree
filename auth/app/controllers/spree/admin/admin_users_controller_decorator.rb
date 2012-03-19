9e1271fe993f5098b8b69cbc7f21e2105953a78e
Ryan Bigg <radarlistener@gmail.com>
2012-03-19 09:23:19 -0600

    Require base controller decorator into admin's users controller decorator
    The reason for this is because, in the admin users controller test, the
    base controller decorator is not being loaded. By requiring this, we add
    the rescue_from CanCan::AccessDenied that is needed to render the
    shared/unauthorized template.
    
    This fix will then bring the build back to green.

--- auth/app/controllers/spree/admin/admin_users_controller_decorator.rb
+++ auth/app/controllers/spree/admin/admin_users_controller_decorator.rb
@@ -1,3 +1,4 @@
+require File.expand_path('../../base_controller_decorator', __FILE__)
 Spree::Admin::UsersController.class_eval do
   rescue_from Spree::User::DestroyWithOrdersError, :with => :user_destroy_with_orders_error
 end
