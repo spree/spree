25543ecf73e747be54b692371bb60e537b6b85ca
Ryan Bigg <radarlistener@gmail.com>
2012-03-20 12:31:44 -0600

    Deal with lack of current_user in Admin::MailMethodsController

--- core/spec/controllers/spree/admin/mail_methods_controller_spec.rb
+++ core/spec/controllers/spree/admin/mail_methods_controller_spec.rb
@@ -7,6 +7,7 @@ describe Spree::Admin::MailMethodsController do
   before do
     Spree::Order.stub :find => order
     Spree::MailMethod.stub :find => mail_method
+    request.env["HTTP_REFERER"] = "/"
   end
 
   context "#create" do
@@ -21,5 +22,10 @@ describe Spree::Admin::MailMethodsController do
       Spree::Core::MailSettings.should_receive :init
       put :update, {:order_id => "123", :id => "456", :mail_method_parmas => {:environment => "foo"}}
     end
+  end
+
+  it "can trigger testmail without current_user" do
+    post :testmail, :id => Factory(:mail_method).id
+    flash[:error].should_not include("undefined local variable or method `current_user'")
   end
 end
