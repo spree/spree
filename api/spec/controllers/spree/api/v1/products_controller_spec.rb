ef161ec2791d7a38b38c33382e86d1d830a2133c
Ryan Bigg <radarlistener@gmail.com>
2012-03-23 14:08:32 -0500

    Wrap ProductsController spec in a module so that we don't need to reference Spree::<model> every time we want to

--- api/spec/controllers/spree/api/v1/products_controller_spec.rb
+++ api/spec/controllers/spree/api/v1/products_controller_spec.rb
@@ -1,27 +1,29 @@
 require 'spec_helper'
 
-describe Spree::Api::V1::ProductsController do
-  let!(:product) { Factory(:product) }
-  let(:attributes) { [:id, :name, :description, :price, :available_on, :permalink] }
+module Spree
+  describe Spree::Api::V1::ProductsController do
+    let!(:product) { Factory(:product) }
+    let(:attributes) { [:id, :name, :description, :price, :available_on, :permalink] }
 
-  before do
-    stub_authentication!
-  end
-
-  context "as a normal user" do
-    it "retrieves a list of products" do
-      api_get :index
-      json_response.first.should have_attributes(attributes)
+    before do
+      stub_authentication!
     end
 
-    it "gets a single product" do
-      api_get :show, :id => product.to_param
-      json_response.should have_attributes(attributes)
-    end
+    context "as a normal user" do
+      it "retrieves a list of products" do
+        api_get :index
+        json_response.first.should have_attributes(attributes)
+      end
 
-    it "cannot create a new product if not an admin" do
-      api_post :create, :name => "Brand new product!"
-      json_response.should == { "error" => "You are not authorized to perform that action." }
+      it "gets a single product" do
+        api_get :show, :id => product.to_param
+        json_response.should have_attributes(attributes)
+      end
+
+      it "cannot create a new product if not an admin" do
+        api_post :create, :name => "Brand new product!"
+        json_response.should == { "error" => "You are not authorized to perform that action." }
+      end
     end
   end
 end
