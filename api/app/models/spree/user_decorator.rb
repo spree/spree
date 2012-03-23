3474bd4929e93fa39217163d1e846034d3ed2b3b
Ryan Bigg <radarlistener@gmail.com>
2012-03-23 13:45:56 -0500

    Remove authenticate_for_api method.
    Use find_by_api_key instead to get a user, which will then act as the current user

--- api/app/models/spree/user_decorator.rb
+++ api/app/models/spree/user_decorator.rb
@@ -1,9 +1,4 @@
 Spree::User.class_eval do
-
-  def self.authenticate_for_api(key)
-    !!find_by_api_key(key)
-  end
-
   def generate_api_key!
     self.api_key = SecureRandom.hex(24)
     save!
--- api/spec/models/spree/user_spec.rb
+++ api/spec/models/spree/user_spec.rb
@@ -15,15 +15,5 @@ module Spree
       user.clear_api_key!
       user.api_key.should be_blank
     end
-
-    it "authenticates if a user exists with a key" do
-      User.stub :find_by_api_key => stub_model(User)
-      User.authenticate_for_api("fake_key").should be_true
-    end
-
-    it "does not authenticate if a user does not exist with a key" do
-      User.stub :find_by_api_key => nil
-      User.authenticate_for_api("fake_key").should be_false
-    end
   end
 end
