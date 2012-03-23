710133f67c617fce7d691f25437afc1151e8de43
Ryan Bigg <radarlistener@gmail.com>
2012-03-23 11:37:55 -0500

    Re-instate loading of decorators back in the engine

--- api/lib/spree/api/engine.rb
+++ api/lib/spree/api/engine.rb
@@ -6,6 +6,13 @@ module Spree
       isolate_namespace Spree
       engine_name 'spree_api'
 
+      def self.activate
+        Dir.glob(File.join(File.dirname(__FILE__), "../../../app/**/*_decorator*.rb")) do |c|
+          Rails.configuration.cache_classes ? require(c) : load(c)
+        end
+      end
+      config.to_prepare &method(:activate).to_proc
+
     end
   end
 end
