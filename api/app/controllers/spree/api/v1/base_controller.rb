f39f8511d124e2327acc49b1487922eb299c1ec8
Ryan Bigg <radarlistener@gmail.com>
2012-03-23 11:27:30 -0500

    Show an error if a user does not specify a key for an API request

--- api/app/controllers/spree/api/v1/base_controller.rb
+++ api/app/controllers/spree/api/v1/base_controller.rb
@@ -5,7 +5,18 @@ module Spree
         include ActionController::ImplicitRender
         include ActionController::Rendering
         include AbstractController::ViewPaths
+        include AbstractController::Callbacks
         append_view_path "app/views"
+
+        before_filter :authenticate_user
+
+        private
+
+        def authenticate_user
+          # TODO: Work out why we can't use straight render :json here.
+          render "spree/api/v1/errors/must_specify_api_key" if params[:key].blank?
+        end
+
       end
     end
   end
