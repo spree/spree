e8df2efb845d6a403b9a9bf08a0d9fe0ce3499bc
Ryan Bigg <radarlistener@gmail.com>
2012-03-23 14:23:00 -0500

    Fix include issue with ControllerSetup

--- api/lib/spree/api/controller_setup.rb
+++ api/lib/spree/api/controller_setup.rb
@@ -1,16 +1,20 @@
 module Spree
   module Api
     module ControllerSetup
-      include ActionController::ImplicitRender
-      include ActionController::Rendering
-      include AbstractController::ViewPaths
-      include AbstractController::Callbacks
-      include AbstractController::Helpers
-      include ActiveSupport::Rescuable
-      include ActionController::Rescue
-      append_view_path "app/views"
+      def self.included(klass)
+        klass.class_eval do
+          include ActionController::ImplicitRender
+          include ActionController::Rendering
+          include AbstractController::ViewPaths
+          include AbstractController::Callbacks
+          include AbstractController::Helpers
+          include ActiveSupport::Rescuable
+          include ActionController::Rescue
+          append_view_path "app/views"
 
-      include CanCan::ControllerAdditions
+          include CanCan::ControllerAdditions
+        end
+      end
     end
   end
 end
