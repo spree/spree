8a26b7585047829425ba52499d2f7e0d83ad230d
Trung Lê <joneslee85@gmail.com>
2012-03-25 13:20:50 +1100

    [promo] Fix boolean type SQL migration

--- promo/db/migrate/20101026184833_migrate_adjustments.rb
+++ promo/db/migrate/20101026184833_migrate_adjustments.rb
@@ -1,7 +1,7 @@
 class MigrateAdjustments < ActiveRecord::Migration
   def up
     execute "UPDATE spree_adjustments SET amount = 0.0 WHERE amount IS NULL"
-    execute "UPDATE spree_adjustments SET mandatory = 't' WHERE locked = 't'"
+    execute "UPDATE spree_adjustments SET mandatory = #{quoted_true} WHERE locked = #{quoted_true}"
   end
 
   def down
