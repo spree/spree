d4073342016c0a34e02f9cf7313ef0596a40310d
Ryan Bigg <radarlistener@gmail.com>
2012-03-26 10:11:50 -0500

    WIP: switching to use header authentication for API

--- api/spec/controllers/spree/api/v1/base_controller_spec.rb
+++ api/spec/controllers/spree/api/v1/base_controller_spec.rb
@@ -9,12 +9,15 @@ describe Spree::Api::V1::BaseController do
 
   context "cannot make a request to the API" do
     it "without an API key" do
-      api_get :index, :key => nil
+      request.env["X-Spree-Token"] = nil
+      api_get :index
       json_response.should == { "error" => "You must specify an API key." }
     end
 
     it "with an invalid API key" do
-      api_get :index
+      request.env["X-Spree-Token"] = "fake_key"
+      request.headers["X-Spree-Token"] = "OMG"
+      get :index, :use_route => :spree, :format => :json
       json_response.should == { "error" => "Invalid API key (fake_key) specified." }
     end
   end
