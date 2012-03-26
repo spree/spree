51b27f6b6a7636250a6209e03c5f7db0bfcb6e07
Ryan Bigg <radarlistener@gmail.com>
2012-03-26 13:41:03 -0500

    Remove AS and AP stuff from API backtrace

--- api/spec/spec_helper.rb
+++ api/spec/spec_helper.rb
@@ -11,7 +11,7 @@ Dir[File.dirname(__FILE__) + "/support/**/*.rb"].each {|f| require f}
 require 'spree/core/testing_support/factories'
 
 RSpec.configure do |config|
-  config.backtrace_clean_patterns = [/gems/]
+  config.backtrace_clean_patterns = [/gems\/activesupport/, /gems\/actionpack/]
   config.before(:suite) do
     DatabaseCleaner.strategy = :transaction
     DatabaseCleaner.clean_with(:truncation)
