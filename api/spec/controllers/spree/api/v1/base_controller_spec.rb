d90070c24c1111c31d711355c5b9e78a34fd9a1d
Ryan Bigg <radarlistener@gmail.com>
2012-03-27 08:53:45 -0500

    no API key and invalid API key requests should return 401 statuses

--- api/spec/controllers/spree/api/v1/base_controller_spec.rb
+++ api/spec/controllers/spree/api/v1/base_controller_spec.rb
@@ -11,12 +11,14 @@ describe Spree::Api::V1::BaseController do
     it "without an API key" do
       api_get :index
       json_response.should == { "error" => "You must specify an API key." }
+      response.status.should == 401
     end
 
     it "with an invalid API key" do
       request.env["X-Spree-Token"] = "fake_key"
       get :index, {}
       json_response.should == { "error" => "Invalid API key (fake_key) specified." }
+      response.status.should == 401
     end
   end
 end
