93668d718cecb2135c2f8b9add918c29f74aade8
cmar <chris@spreecommerce.com>
2012-03-27 14:13:38 -0400

    Spree Analytics registration from Admin
    Merges #1317.

---
+++ dash/app/controllers/spree/admin/analytics_controller.rb
@@ -0,0 +1,55 @@
+module Spree
+  class Admin::AnalyticsController < Admin::BaseController
+    before_filter :redirect_if_registered
+
+    def sign_up
+      @store = {
+        :first_name => '',
+        :last_name => '',
+        :email => current_user.email,
+        :currency => 'USD',
+        :time_zone => Time.zone,
+        :name => Spree::Config.site_name,
+        :url => format_url(Spree::Config.site_url)
+      }
+    end
+
+    def register
+      @store = params[:store]
+      @store[:url] = format_url(@store[:url])
+
+      unless @store.has_key? :terms_of_service
+        flash[:error] = t(:agree_to_terms_of_service)
+        return render :sign_up
+      end
+
+      unless @store.has_key? :privacy_policy
+        flash[:error] = t(:agree_to_privacy_policy)
+        return render :sign_up
+      end
+
+      begin
+        @store = Spree::Dash::Jirafe.register(@store)
+        Spree::Dash::Config.app_id = @store[:app_id]
+        Spree::Dash::Config.app_token = @store[:app_token]
+        Spree::Dash::Config.site_id = @store[:site_id]
+        Spree::Dash::Config.token = @store[:site_token]
+        redirect_to admin_path, :notice => t(:successfully_signed_up_for_analytics)
+      rescue Spree::Dash::JirafeException => e
+        flash[:error] = e.message
+        render :sign_up
+      end
+    end
+
+    private
+
+    def redirect_if_registered
+      redirect_to admin_path, :notice => t(:already_signed_up_for_analytics) if Spree::Dash::Config.configured?
+    end
+
+    def format_url(url)
+      url =~ /^http/ ? url : "http://#{url}"
+    end
+
+  end
+end
---
+++ dash/spec/requests/admin/analytics_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe "Analytics Sign Up" do
+  before(:each) do
+    @user = Factory(:admin_user)
+    sign_in_as!(@user)
+
+    Spree::Dash::Config.app_id = nil
+    Spree::Dash::Config.app_token = nil
+    Spree::Dash::Config.site_id = nil
+    Spree::Dash::Config.token = nil
+  end
+
+  it "user can sign up for spree_analytics" do
+      Spree::Dash::Jirafe.should_receive(:register).
+                          with(hash_including(:url => 'http://test.com')).
+                          and_return({ :app_id => '1', :app_token => '2', :site_id => '3', :site_token => '4' })
+
+      visit spree.admin_analytics_sign_up_path
+      check 'store[terms_of_service]'
+      check 'store[privacy_policy]'
+      fill_in 'store[first_name]', :with => "test_first_name"
+      fill_in 'store[last_name]', :with => "test_last_name"
+      fill_in 'store[url]', :with => "test.com"
+      select '(GMT+00:00) Casablanca', :from => 'store[time_zone]'
+      click_button 'Sign up'
+
+      Spree::Dash::Config.app_id.should eq '1'
+      Spree::Dash::Config.app_token.should eq '2'
+      Spree::Dash::Config.site_id.should eq '3'
+      Spree::Dash::Config.token.should eq '4'
+  end
+end
