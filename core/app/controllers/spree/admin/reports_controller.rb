87cb90ea59b3d3893831635776c57f6d9f595514
Ryan Bigg <radarlistener@gmail.com>
2012-03-28 14:05:42 -0500

    Correct search params in admin/reports controller

--- core/app/controllers/spree/admin/reports_controller.rb
+++ core/app/controllers/spree/admin/reports_controller.rb
@@ -16,15 +16,14 @@ module Spree
       def sales_total
         params[:q] = {} unless params[:q]
 
-        if params[:q][:created_at_greater_than].blank?
-          params[:q][:created_at_greater_than] = Time.zone.now.beginning_of_month
+        if params[:q][:created_at_gt].blank?
+          params[:q][:created_at_gt] = Time.zone.now.beginning_of_month
         else
-          params[:q][:created_at_greater_than] = Time.zone.parse(params[:q][:created_at_greater_than]).beginning_of_day rescue Time.zone.now.beginning_of_month
+          params[:q][:created_at_gt] = Time.zone.parse(params[:q][:created_at_gt]).beginning_of_day rescue Time.zone.now.beginning_of_month
         end
 
-        if params[:q] && !params[:q][:created_at_less_than].blank?
-          params[:q][:created_at_less_than] =
-                                          Time.zone.parse(params[:q][:created_at_less_than]).end_of_day rescue ""
+        if params[:q] && !params[:q][:created_at_lt].blank?
+          params[:q][:created_at_lt] = Time.zone.parse(params[:q][:created_at_lt]).end_of_day rescue ""
         end
 
         if params[:q].delete(:completed_at_is_not_null) == "1"
