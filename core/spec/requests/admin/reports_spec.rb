971cf44aca8fa2c02793050fdceece7b5600a591
Paul Damer <pdamer@gmail.com>
2012-03-28 13:59:12 -0400

    Sales total report should exclude incomplete orders and updated test to be less brittle

--- core/spec/requests/admin/reports_spec.rb
+++ core/spec/requests/admin/reports_spec.rb
@@ -26,6 +26,11 @@ describe "Reports" do
       order.completed_at = Time.now
       order.save!
 
+      #incomplete order
+      order = Factory(:order)
+      order.update_attributes_without_callbacks({:adjustment_total => 50})
+      order.save!
+
       order = Factory(:order)
       order.update_attributes_without_callbacks({:adjustment_total => 200})
       order.completed_at = 3.years.ago
@@ -44,8 +49,8 @@ describe "Reports" do
       click_link "Reports"
       click_link "Sales Total"
 
-      fill_in "search_created_at_greater_than", :with => "2012/01/01"
-      fill_in "search_created_at_less_than", :with => "2012/12/31"
+      fill_in "search_created_at_greater_than", :with => 1.week.ago
+      fill_in "search_created_at_less_than", :with => 1.week.from_now
       click_button "Search"
 
       page.should have_content("$300.00")
