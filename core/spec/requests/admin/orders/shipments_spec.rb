33a6e7e9f94d859b2235a7ea7356fa9865c34680
Vassil Kalkov <vassilkalkov@gmail.com>
2012-03-29 13:23:13 +0300

    [core/spec/requests/admin/orders/shipments_spec] Ensure the shipment is set and test listing of shipments

--- core/spec/requests/admin/orders/shipments_spec.rb
+++ core/spec/requests/admin/orders/shipments_spec.rb
@@ -14,18 +14,34 @@ describe "Shipments" do
     order.inventory_units.each do |iu|
       iu.update_attribute_without_callbacks('state', 'sold')
     end
-  end
-
-  it "should be able to create a new shipment for an order" do
+    
     visit spree.admin_path
     click_link "Orders"
     within('table#listing_orders tbody tr:nth-child(1)') { click_link "R100" }
+    
+  end
+
+  it "should be able to create and list shipments for an order", :js => true do
+
     click_link "Shipments"
     
     click_on "New Shipment"
     check "inventory_units_1"
     click_button "Create"
     page.should have_content("successfully created!")
+    order.reload
+    order.shipments.count.should == 1
+    
+    click_link "Shipments"
+    find('table.index tbody tr:nth-child(2) td:nth-child(1)').text.should == order.shipment.number
+    find('table.index tbody tr:nth-child(2) td:nth-child(5)').text.should == "Pending"
+    
+    within('table.index tbody tr:nth-child(2) td:nth-child(7)') { click_link "Edit" }
+    page.should have_content("Shipment ##{order.shipment.number}")
+    #save_and_open_page
+    #click_button "Update"
+    #page.should have_content("successfully updated!")
+    
   end
 
 end
