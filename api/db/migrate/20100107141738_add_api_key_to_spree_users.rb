571fa1ff1f1decd726438134d32b656c89436d03
Ryan Bigg <radarlistener@gmail.com>
2012-03-30 14:00:03 -0500

    Correct class in migration

--- api/db/migrate/20100107141738_add_api_key_to_spree_users.rb
+++ api/db/migrate/20100107141738_add_api_key_to_spree_users.rb
@@ -1,4 +1,4 @@
-class AddApiKeyToUsers < ActiveRecord::Migration
+class AddApiKeyToSpreeUsers < ActiveRecord::Migration
   def change
     add_column :spree_users, :api_key, :string, :limit => 40
   end
