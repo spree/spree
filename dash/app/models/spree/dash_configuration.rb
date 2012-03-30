8eb830e646d4a5a80f1827bc70aa57cc57527e2d
cmar <chris@spreecommerce.com>
2012-03-30 09:28:30 -0400

    using namespaced jirafe jquery, support for locales in admin dashboard [fixes #1204]
    Merges #1339.

--- dash/app/models/spree/dash_configuration.rb
+++ dash/app/models/spree/dash_configuration.rb
@@ -4,6 +4,7 @@ module Spree
     preference :app_token, :string
     preference :site_id, :string
     preference :token, :string
+    preference :locale, :string, :default => 'en_US'
 
     def configured?
       preferred_app_id.present? and preferred_site_id.present? and preferred_token.present?
