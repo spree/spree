73bff5d1dccaeebd50927e5c1bc8d5b9eef558d5
Ryan Bigg <radarlistener@gmail.com>
2012-04-02 13:30:10 -0500

    Correct count in products controller spec

--- api/spec/controllers/spree/api/v1/products_controller_spec.rb
+++ api/spec/controllers/spree/api/v1/products_controller_spec.rb
@@ -33,7 +33,7 @@ module Spree
           second_product = Factory(:product)
           api_get :index, :page => 2
           json_response["products"].first.should have_attributes(attributes)
-          json_response["count"].should == 1
+          json_response["count"].should == 2
           json_response["current_page"].should == 2
           json_response["pages"].should == 2
         end
