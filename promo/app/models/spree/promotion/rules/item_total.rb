4844daa41bc653401f7863fa2296722b5f35525e
Ryan Bigg <radarlistener@gmail.com>
2012-04-04 16:02:38 -0500

    Add preferred_amount and preferred_operator to accessible attributes for item total

--- promo/app/models/spree/promotion/rules/item_total.rb
+++ promo/app/models/spree/promotion/rules/item_total.rb
@@ -4,6 +4,8 @@ module Spree
     preference :amount, :decimal, :default => 100.00
     preference :operator, :string, :default => '>'
 
+    attr_accessible :preferred_amount, :preferred_operator
+
     OPERATORS = ['gt', 'gte']
 
     def eligible?(order, options = {})
