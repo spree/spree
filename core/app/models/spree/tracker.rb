1de5af6468d809cb3a83452e9bec58f7a188b5ef
Ryan Bigg <radarlistener@gmail.com>
2012-04-04 11:12:30 -0500

    Make analytics_id, :environment and :active mass-assignable for Tracker objects

--- core/app/models/spree/tracker.rb
+++ core/app/models/spree/tracker.rb
@@ -1,5 +1,7 @@
 module Spree
   class Tracker < ActiveRecord::Base
+    attr_accessible :analytics_id, :environment, :active
+
     def self.current
       first(:conditions => { :active => true, :environment => Rails.env })
     end
