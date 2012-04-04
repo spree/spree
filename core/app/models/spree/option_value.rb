ec059a30b7b1e7dde20aa9d007788be3661018ca
Ryan Bigg <radarlistener@gmail.com>
2012-04-04 13:12:56 -0500

    Make name and presentation accessible in OptionValue

--- core/app/models/spree/option_value.rb
+++ core/app/models/spree/option_value.rb
@@ -3,5 +3,7 @@ module Spree
     belongs_to :option_type
     acts_as_list :scope => :option_type
     has_and_belongs_to_many :variants, :join_table => 'spree_option_values_variants'
+
+    attr_accessible :name, :presentation
   end
 end
