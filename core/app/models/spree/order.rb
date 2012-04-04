0bf5c6a4435cb391fff8452532db5b5e47e205bf
Ryan Bigg <radarlistener@gmail.com>
2012-04-04 13:56:29 -0500

    Create a shipment with create_shipment without protection

--- core/app/models/spree/order.rb
+++ core/app/models/spree/order.rb
@@ -329,10 +329,10 @@ module Spree
       if shipment.present?
         shipment.update_attributes!(:shipping_method => shipping_method)
       else
-        self.shipments << Shipment.create!(:order => self,
+        self.shipments << Shipment.create!({ :order => self,
                                           :shipping_method => shipping_method,
                                           :address => self.ship_address,
-                                          :inventory_units => self.inventory_units)
+                                          :inventory_units => self.inventory_units}, :without_protection => true)
       end
 
     end
