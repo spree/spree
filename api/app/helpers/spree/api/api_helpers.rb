dbeae430662337c18befc2c78f03573bbd5a97d6
Ryan Bigg <radarlistener@gmail.com>
2012-04-06 15:50:27 -0500

    Use map(&:first) to get keys out of _validators in required_fields_for
    This is because 1.8.7 is stupid

--- api/app/helpers/spree/api/api_helpers.rb
+++ api/app/helpers/spree/api/api_helpers.rb
@@ -4,7 +4,7 @@ module Spree
       def required_fields_for(model)
         required_fields = model._validators.select do |field, validations|
           validations.any? { |v| v.is_a?(ActiveModel::Validations::PresenceValidator) }
-        end.keys
+        end.map(&:first) # get fields that are invalid
         # Permalinks presence is validated, but are really automatically generated
         # Therefore we shouldn't tell API clients that they MUST send one through
         required_fields.delete(:permalink)
