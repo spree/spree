1feb685ae0a559bb7aa2c193e5fd597b14d82ec0
cmar <chris@spreecommerce.com>
2012-04-07 06:17:23 -0400

    attr_accessible for zones, zone_members and tax_rates

--- core/app/models/spree/zone_member.rb
+++ core/app/models/spree/zone_member.rb
@@ -3,7 +3,7 @@ module Spree
     belongs_to :zone
     belongs_to :zoneable, :polymorphic => true
 
-    attr_accessible :zone, :zone_id, :zoneable
+    attr_accessible :zone, :zone_id, :zoneable, :zoneable_id, :zoneable_type
 
     def name
       return nil if zoneable.nil?
