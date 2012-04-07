04eb0bf9e8f4f28d5348b4c704571d8c6b2a10e2
Ryan Bigg <radarlistener@gmail.com>
2012-04-07 17:47:17 -0500

    Use no protection in Ruby seed files
    This was causing images to not display on the sandbox and new installs.

--- sample/db/sample/spree/inventory_units.rb
+++ sample/db/sample/spree/inventory_units.rb
@@ -1,4 +1,4 @@
 # create the inventory units associated with the line item (we need to do this after the fixture b/c quantity is random)
 Spree::LineItem.all.each do |li|
-  li.quantity.times { li.order.inventory_units.create(:variant => li.variant, :state => 'sold', :shipment => li.order.shipment) }
+  li.quantity.times { li.order.inventory_units.create({:variant => li.variant, :state => 'sold', :shipment => li.order.shipment}, :without_protection => true) }
 end
