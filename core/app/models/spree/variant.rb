469b7860c4da5a899b27246dbbf13e4bcb8c9286
Ryan Bigg <radarlistener@gmail.com>
2012-04-07 16:46:32 -0500

    Switch Variant#options_text method from being this one-line monstrosity to a cleaner multi-lined method

--- core/app/models/spree/variant.rb
+++ core/app/models/spree/variant.rb
@@ -87,7 +87,13 @@ module Spree
     end
 
     def options_text
-      self.option_values.sort { |ov1, ov2| ov1.option_type.position <=> ov2.option_type.position }.map { |ov| "#{ov.option_type.presentation}: #{ov.presentation}" }.to_sentence({ :words_connector => ", ", :two_words_connector => ", " })
+      values = self.option_values.sort_by(&:position)
+
+      values.map! do |ov|
+        "#{ov.option_type.presentation}: #{ov.presentation}"
+      end
+
+      values.to_sentence({ :words_connector => ", ", :two_words_connector => ", " })
     end
 
     def gross_profit
