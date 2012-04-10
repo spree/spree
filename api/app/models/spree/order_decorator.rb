a5d852238d0a0c427b10baf3c2e3ad6814f18d7a
Ryan Bigg <radarlistener@gmail.com>
2012-04-10 16:48:29 -0400

    Make creating orders from the API more sensible

--- api/app/models/spree/order_decorator.rb
+++ api/app/models/spree/order_decorator.rb
@@ -1,10 +1,10 @@
 Spree::Order.class_eval do
   def self.build_from_api(user, params)
     order = create
-    params[:line_items].each do |variant_id, quantity|
-      line_item_params = { :variant_id => variant_id, :quantity => quantity }
-      line_item = order.add_variant(Spree::Variant.find(variant_id), quantity)
+    params[:line_items].each do |line_item|
+      order.add_variant(Spree::Variant.find(line_item[:variant_id]), line_item[:quantity])
     end
+
     order.user = user
     order.email = user.email
     order
