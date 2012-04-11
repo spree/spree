ae2f4566104c998bf4bdc321042aa229c1e72602
Ryan Bigg <radarlistener@gmail.com>
2012-04-11 13:37:30 +0100

    Add regression test for #1370 #3

---
+++ core/spec/controllers/spree/admin/return_authorizations_controller_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe Spree::Admin::ReturnAuthorizationsController do
+  # Regression test for #1370 #3
+  let!(:order) { Factory(:order) }
+  it "can create a return authorization" do
+    post :create, :order_id => order.to_param, :return_authorization => { :amount => 0.0, :reason => "" }
+    response.should be_success
+  end
+end
