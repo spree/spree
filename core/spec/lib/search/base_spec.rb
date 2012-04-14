59eb3f5b1c0d4285b87f63b8ae9aa5c8238f1e2d
Francois Bernier <frankbernier@gmail.com>
2012-04-14 12:42:34 -0400

    Fixed issue #1395 where switching pages would not work in search results.

--- core/spec/lib/search/base_spec.rb
+++ core/spec/lib/search/base_spec.rb
@@ -11,7 +11,19 @@ describe Spree::Core::Search::Base do
   it "returns all products by default" do
     params = { :per_page => "" }
     searcher = Spree::Core::Search::Base.new(params)
-    searcher.retrieve_products.count.should eq 2
+    searcher.retrieve_products.count.should == 2
+  end
+
+  it "switches to next page according to the page parameter" do
+    @product3 = Factory(:product, :name => "RoR Pants", :price => 14.00, :on_hand => 1)
+
+    params = { :per_page => "2" }
+    searcher = Spree::Core::Search::Base.new(params)
+    searcher.retrieve_products.count.should == 2
+
+    params.merge! :page => "2"
+    searcher = Spree::Core::Search::Base.new(params)
+    searcher.retrieve_products.count.should == 1
   end
 
   it "maps search params to named scopes" do
@@ -19,7 +31,7 @@ describe Spree::Core::Search::Base do
                :search => { "price_range_any" => ["Under $10.00"] }}
     searcher = Spree::Core::Search::Base.new(params)
     searcher.send(:get_base_scope).to_sql.should match /<= 10/
-    searcher.retrieve_products.count.should eq 1
+    searcher.retrieve_products.count.should == 1
   end
 
   it "maps multiple price_range_any filters" do
@@ -28,14 +40,14 @@ describe Spree::Core::Search::Base do
     searcher = Spree::Core::Search::Base.new(params)
     searcher.send(:get_base_scope).to_sql.should match /<= 10/
     searcher.send(:get_base_scope).to_sql.should match /between 10 and 15/
-    searcher.retrieve_products.count.should eq 2
+    searcher.retrieve_products.count.should == 2
   end
 
   it "uses ransack if scope not found" do
     params = { :per_page => "",
                :search => { "name_not_cont" => "Shirt" }}
     searcher = Spree::Core::Search::Base.new(params)
-    searcher.retrieve_products.count.should eq 1
+    searcher.retrieve_products.count.should == 1
   end
 
 end
