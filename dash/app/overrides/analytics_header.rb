da1982ffdce736fc1c9daad0dddb1248e598a5d5
Andrew Hooker <GeekOnCoffee@gmail.com>
2012-04-16 07:58:53 -0500

    adding original to deface override

--- dash/app/overrides/analytics_header.rb
+++ dash/app/overrides/analytics_header.rb
@@ -1,4 +1,5 @@
 Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
                      :name => "add_analytics_header",
                      :insert_bottom => "[data-hook='inside_head']",
-                     :partial => "spree/analytics/header")
+                     :partial => "spree/analytics/header"),
+                     :original => '6f23c8af6e863d0499835c00b3f2763cb98e1d75'
