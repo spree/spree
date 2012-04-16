af4224b2e2786def03cfa4ca8c1d4035fe4fde2d
Andrew Hooker <GeekOnCoffee@gmail.com>
2012-04-16 09:16:58 -0500

    fixing multi_json version
    Merges #1408

--- common_spree_dependencies.rb
+++ common_spree_dependencies.rb
@@ -7,7 +7,7 @@ gem 'json'
 gem 'sqlite3'
 gem 'mysql2'
 gem 'pg'
-
+gem 'multi_json', "1.2.0"
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
