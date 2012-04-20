429a782809262a3c960fa4c07b38904f5a9d3d58
Chris Wise <cwise@murmurinformatics.com>
2012-04-20 10:06:53 -0400

    corrected an issue with ensuring there is only one default tax category; added corresponding tests

--- core/app/models/spree/tax_category.rb
+++ core/app/models/spree/tax_category.rb
@@ -14,7 +14,7 @@ module Spree
       #set existing default tax category to false if this one has been marked as default
 
       if is_default && tax_category = self.class.where(:is_default => true).first
-        tax_category.update_attribute(:is_default, false)
+        tax_category.update_attribute(:is_default, false) unless tax_category==self
       end
     end
 
