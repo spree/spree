7d82562f22eb57390a206b318a6af4f204c9abd8
Chris Wise <cwise@murmurinformatics.com>
2012-04-20 10:06:53 -0400

    corrected an issue with ensuring there is only one default tax category; added corresponding tests
    Fixes #1423

--- core/spec/models/tax_category_spec.rb
+++ core/spec/models/tax_category_spec.rb
@@ -9,4 +9,28 @@ describe Spree::TaxCategory do
       tax_category.deleted_at.should_not be_nil
     end
   end
+
+  context 'default tax category' do
+    let(:tax_category) { Factory(:tax_category) }
+    let(:new_tax_category) { Factory(:tax_category) }
+
+    before do
+      tax_category.update_attribute(:is_default, true)
+    end
+
+    it "should undefault the previous default tax category" do
+      new_tax_category.update_attribute(:is_default, true)
+      new_tax_category.is_default.should be_true
+
+      tax_category.reload
+      tax_category.is_default.should be_false
+    end
+
+    it "should undefault the previous default tax category except when updating the existing default tax category" do
+      tax_category.update_attribute(:description, "Updated description")
+
+      tax_category.reload
+      tax_category.is_default.should be_true
+    end
+  end
 end
