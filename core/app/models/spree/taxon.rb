1bd12094c0625b20c937d1178d58b84be51463cb
Rohan Mitchell <rohan@rohanmitchell.com>
2012-04-26 14:58:10 +1000

    Add attr_accessible for icon field of Taxon

--- core/app/models/spree/taxon.rb
+++ core/app/models/spree/taxon.rb
@@ -6,7 +6,7 @@ module Spree
     has_and_belongs_to_many :products, :join_table => 'spree_products_taxons'
     before_create :set_permalink
 
-    attr_accessible :name, :parent_id, :position, :description, :permalink
+    attr_accessible :name, :parent_id, :position, :icon, :description, :permalink
 
     validates :name, :presence => true
     has_attached_file :icon,
