744642cd052533af60365de5294ef934a2258d55
Roman Smirnov <roman@railsdog.com>
2012-04-27 15:38:55 +0400

    Should not raise exceptions like "validate_on_profile_create preference not defined" when trying to migrate old preferences

--- core/lib/spree/core/preference_rescue.rb
+++ core/lib/spree/core/preference_rescue.rb
@@ -10,10 +10,14 @@ module Spree
       OldPrefs.where(:key => nil).each do |old_pref|
         next unless owner = (old_pref.owner rescue nil)
         unless old_pref.owner_type == "Spree::Activator" || old_pref.owner_type == "Spree::Configuration"
-          old_pref.key = [owner.class.name, old_pref.name, owner.id].join('::').underscore
-          old_pref.value_type = owner.preference_type(old_pref.name)
-          puts "Migrating Preference: #{old_pref.key}"
-          old_pref.save
+          begin
+            old_pref.key = [owner.class.name, old_pref.name, owner.id].join('::').underscore
+            old_pref.value_type = owner.preference_type(old_pref.name)
+            puts "Migrating Preference: #{old_pref.key}"
+            old_pref.save
+          rescue NoMethodError => ex
+            puts ex.message
+          end
         end
       end
     end
