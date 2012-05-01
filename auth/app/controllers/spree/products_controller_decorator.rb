21458af26efb0793234c07fd84d0e252e4850821
Calvin Yu <calvin@scoutmob.com>
2012-05-01 17:40:39 -0400

    Remove extra arg in authorize! in products#show

--- auth/app/controllers/spree/products_controller_decorator.rb
+++ auth/app/controllers/spree/products_controller_decorator.rb
@@ -6,7 +6,7 @@ Spree::ProductsController.class_eval do
       @product = Spree::Product.find_by_permalink!(params[:id])
       if !@product.deleted? && (@product.available_on.nil? || @product.available_on.future?)
         # Allow admins to view any yet to be available products
-        authorize! :update, Spree::Product, @product
+        authorize! :update, Spree::Product
       end
     end
 end
