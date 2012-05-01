cadf88c1d5937fa3022cb952e15391d781fc6bee
Calvin Yu <calvin@scoutmob.com>
2012-05-01 16:35:09 -0400

    Allows admins to view inactive products

---
+++ auth/app/controllers/spree/products_controller_decorator.rb
@@ -0,0 +1,13 @@
+Spree::ProductsController.class_eval do
+  rescue_from CanCan::AccessDenied, :with => :render_404
+
+  private
+    def load_product
+      @product = Spree::Product.find_by_permalink!(params[:id])
+      if !@product.deleted? && (@product.available_on.nil? || @product.available_on.future?)
+        # Allow admins to view any yet to be available products
+        authorize! :show, Spree::Product
+      end
+    end
+end
+
