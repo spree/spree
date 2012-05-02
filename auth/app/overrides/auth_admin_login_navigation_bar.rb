12b1f71e59f582c2876b1be51d79d9e193b5b3ee
Andrew Hooker <GeekOnCoffee@gmail.com>
2012-05-02 13:40:10 -0500

    Adding Deface Originals to auth and promo overrides
    Merges #1479

--- auth/app/overrides/auth_admin_login_navigation_bar.rb
+++ auth/app/overrides/auth_admin_login_navigation_bar.rb
@@ -1,4 +1,5 @@
 Deface::Override.new(:virtual_path => "spree/layouts/admin",
                      :name => "auth_admin_login_navigation_bar",
                      :replace => "[data-hook='admin_login_navigation_bar'], #admin_login_navigation_bar[data-hook]",
-                     :partial => "spree/layouts/admin/login_nav")
+                     :partial => "spree/layouts/admin/login_nav",
+                     :original => '0a5476d4d5db90ec8dd200ebaa0109a6a54ec6bc' )
--- auth/app/overrides/auth_user_login_form.rb
+++ auth/app/overrides/auth_user_login_form.rb
@@ -2,4 +2,5 @@ Deface::Override.new(:virtual_path => "spree/checkout/registration",
                      :name => "auth_user_login_form",
                      :replace_contents => "[data-hook='registration'] #account, #registration[data-hook] #account",
                      :template => "spree/user_sessions/new",
-                     :disabled => false)
+                     :disabled => false,
+                     :original => 'ab20ac9e90baa11b847b30040aef863d2e1af17a')
