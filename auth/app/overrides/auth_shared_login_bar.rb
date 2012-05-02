e7d2343e6d2b2b8a589bbafffe0df625f2b7c7b3
Andrew Hooker <GeekOnCoffee@gmail.com>
2012-05-02 13:40:10 -0500

    Adding Deface Originals to auth and promo overrides
    Merges #1479

--- auth/app/overrides/auth_shared_login_bar.rb
+++ auth/app/overrides/auth_shared_login_bar.rb
@@ -2,5 +2,6 @@ Deface::Override.new(:virtual_path => "spree/shared/_nav_bar",
                      :name => "auth_shared_login_bar",
                      :insert_before => "li#search-bar",
                      :partial => "spree/shared/login_bar",
-                     :disabled => false)
+                     :disabled => false, 
+                     :original => 'eb3fa668cd98b6a1c75c36420ef1b238a1fc55ac')
 
--- promo/app/overrides/promo_coupon_code_field.rb
+++ promo/app/overrides/promo_coupon_code_field.rb
@@ -2,4 +2,5 @@ Deface::Override.new(:virtual_path => "spree/checkout/_payment",
                      :name => "promo_coupon_code_field",
                      :replace => "[data-hook='coupon_code_field'], #coupon_code_field[data-hook]",
                      :partial => "spree/checkout/coupon_code_field",
-                     :disabled => false)
+                     :disabled => false,
+                     :original => '9c9f7058eb6fd9236a241621ab53b43e1caa1a0b' )
