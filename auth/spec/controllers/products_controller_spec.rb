7b0813da259b8e99ffc07dc0506237edc094dc78
Ryan Bigg <radarlistener@gmail.com>
2012-05-02 14:06:16 -0500

    Check if the user is an admin in ProductsController decorator in auth
    Don't rely on arbitrary CanCan permission definition.
    
    Fixes broken build.

--- auth/spec/controllers/products_controller_spec.rb
+++ auth/spec/controllers/products_controller_spec.rb
@@ -4,12 +4,13 @@ describe Spree::ProductsController do
   let!(:product) { Factory(:product, :available_on => 1.year.from_now) }
 
   it "allows admins to view non-active products" do
-    controller.should_receive(:authorize!).with(:show, Spree::Product)
+    controller.stub :current_user => stub(:admin? => true)
     get :show, :id => product.to_param
     response.status.should == 200
   end
 
   it "cannot view non-active products" do
+    controller.stub :current_user => stub(:admin? => false)
     get :show, :id => product.to_param
     response.status.should == 404
   end
