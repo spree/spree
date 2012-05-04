01acef163940f1c07ce8ad69cf4730a952176133
Aleksey <aleksey.dem@gmail.com>
2012-05-05 00:34:51 +0400

    Fix for #1436 - helper methods .._image and custom image styles
    Merges #1495. Fixes #1436.

--- core/spec/helpers/base_helper_spec.rb
+++ core/spec/helpers/base_helper_spec.rb
@@ -10,4 +10,23 @@ describe Spree::BaseHelper do
       seo_url(taxon).should == "/t/bam"
     end
   end
+
+  # Regression test for #1436
+  context "defining custom image helpers" do
+    let(:product) { mock_model(Spree::Product, :images => []) }
+    before do
+      Spree::Image.class_eval do
+        attachment_definitions[:attachment][:styles].merge!({:very_strange => '1x1'})
+      end
+    end
+
+    it "should not raise errors when style exists" do
+      lambda { very_strange_image(product) }.should_not raise_error
+    end
+
+    it "should raise NoMethodError when style is not exists" do
+      lambda { another_strange_image(product) }.should raise_error(NoMethodError)
+    end
+
+  end
 end
