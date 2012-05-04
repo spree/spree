1cb59ad22725cd79ca8a7ac572799249bf395eed
Ben Eggett <beneggett@gmail.com>
2012-05-04 02:01:08 -0600

    Corrected, added, and normalized missing translation for admin/users
    Fixes #1488

--- api/app/controllers/spree/admin/users_controller_decorator.rb
+++ api/app/controllers/spree/admin/users_controller_decorator.rb
@@ -3,14 +3,14 @@ Spree::Admin::UsersController.class_eval do
 
   def generate_api_key
     if @user.generate_api_key!
-      flash.notice = t('api.key_generated')
+      flash.notice = t('key_generated', :scope => 'spree.api')
     end
     redirect_to edit_admin_user_path(@user)
   end
 
   def clear_api_key
     if @user.clear_api_key!
-      flash.notice = t('api.key_cleared')
+      flash.notice = t('key_cleared', :scope => 'spree.api')
     end
     redirect_to edit_admin_user_path(@user)
   end
