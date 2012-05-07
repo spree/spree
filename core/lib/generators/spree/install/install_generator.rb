9822cb6e49bf66c78272f11f04add900f7fae9c0
Andrey Melnik <druid.bpg@gmail.com>
2012-05-07 18:13:56 +0300

    Add app/overrides to watchable_dirs so config.to_prepare block get called after updates

--- core/lib/generators/spree/install/install_generator.rb
+++ core/lib/generators/spree/install/install_generator.rb
@@ -82,18 +82,21 @@ Disallow: /users
 
     def configure_application
       application <<-APP
-
+    
     config.to_prepare do
       # Load application's model / class decorators
-      Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
+      Dir.glob(Rails.root.join("app/**/*_decorator*.rb")) do |c|
         Rails.configuration.cache_classes ? require(c) : load(c)
       end
-
+    
       # Load application's view overrides
-      Dir.glob(File.join(File.dirname(__FILE__), "../app/overrides/*.rb")) do |c|
+      Dir.glob(Rails.root.join("app/overrides/*.rb")) do |c|
         Rails.configuration.cache_classes ? require(c) : load(c)
       end
     end
+    
+    config.watchable_dirs[Rails.root.join("app/overrides")] = [:rb]
+    
       APP
 
       append_file "config/environment.rb", "\nActiveRecord::Base.include_root_in_json = true\n"
