5957be7bc14d3e705b05758c85fc42f242933a4f
Ryan Bigg <radarlistener@gmail.com>
2012-05-10 09:36:00 +1000

    [api] Add roles association and override for has_spree_role? method to User decorator

--- api/app/models/spree/user_decorator.rb
+++ api/app/models/spree/user_decorator.rb
@@ -1,4 +1,6 @@
 Spree.user_class.class_eval do
+  has_and_belongs_to_many :roles, :join_table => 'spree_roles_users'
+
   def generate_api_key!
     self.api_key = SecureRandom.hex(24)
     save!
@@ -7,5 +9,10 @@ Spree.user_class.class_eval do
   def clear_api_key!
     self.api_key = nil
     save!
+  end
+
+  # has_spree_role? simply needs to return true or false whether a user has a role or not.
+  def has_spree_role?(role_in_question)
+    roles.any? { |role| role.name == role_in_question.to_s }
   end
 end
