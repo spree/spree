0565806bf6d521194cc858247d038768175fc44a
Ryan Bigg <radarlistener@gmail.com>
2012-05-10 09:04:05 +1000

    Correct prefix tokenized_permissions table with spree_

--- core/db/migrate/20120509055454_create_tokenized_permissions_table.rb
+++ core/db/migrate/20120509055454_create_tokenized_permissions_table.rb
@@ -1,7 +1,7 @@
 class CreateTokenizedPermissionsTable < ActiveRecord::Migration
   def change
     unless Spree::TokenizedPermission.table_exists?
-      create_table :tokenized_permissions do |t|
+      create_table :spree_tokenized_permissions do |t|
         t.integer :permissable_id
         t.string  :permissable_type
         t.string  :token
@@ -9,7 +9,7 @@ class CreateTokenizedPermissionsTable < ActiveRecord::Migration
         t.timestamps
       end
 
-      add_index :tokenized_permissions, [:permissable_id, :permissable_type], :name => 'index_tokenized_name_and_type'
+      add_index :spree_tokenized_permissions, [:permissable_id, :permissable_type], :name => 'index_tokenized_name_and_type'
     end
   end
 end
