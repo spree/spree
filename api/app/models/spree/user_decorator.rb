e15009ed161fb2fcf42cfe05c4fd7ac85a8c8a63
Ryan Bigg <radarlistener@gmail.com>
2012-05-10 14:49:11 +1000

    Add checks for Spree.user_class to api, dash and promo user decorators

--- api/app/models/spree/user_decorator.rb
+++ api/app/models/spree/user_decorator.rb
@@ -1,18 +1,20 @@
-Spree.user_class.class_eval do
-  has_and_belongs_to_many :roles, :join_table => 'spree_roles_users'
+if Spree.user_class
+  Spree.user_class.class_eval do
+    has_and_belongs_to_many :roles, :join_table => 'spree_roles_users'
 
-  def generate_api_key!
-    self.api_key = SecureRandom.hex(24)
-    save!
-  end
+    def generate_api_key!
+      self.api_key = SecureRandom.hex(24)
+      save!
+    end
 
-  def clear_api_key!
-    self.api_key = nil
-    save!
-  end
+    def clear_api_key!
+      self.api_key = nil
+      save!
+    end
 
-  # has_spree_role? simply needs to return true or false whether a user has a role or not.
-  def has_spree_role?(role_in_question)
-    roles.any? { |role| role.name == role_in_question.to_s }
+    # has_spree_role? simply needs to return true or false whether a user has a role or not.
+    def has_spree_role?(role_in_question)
+      roles.any? { |role| role.name == role_in_question.to_s }
+    end
   end
 end
