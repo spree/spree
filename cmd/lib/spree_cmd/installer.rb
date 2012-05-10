45b36e5371a8d1cafc50025f6cac5c6132d23613
Ryan Bigg <radarlistener@gmail.com>
2012-05-10 14:18:08 +1000

    Use radar's copy of spree_auth_devise until it is made official

--- cmd/lib/spree_cmd/installer.rb
+++ cmd/lib/spree_cmd/installer.rb
@@ -86,7 +86,7 @@ module SpreeCmd
         end
 
         if @install_default_auth
-          gem :spree_auth_devise, :git => "git://github.com/spree/spree_auth_devise"
+          gem :spree_auth_devise, :git => "git://github.com/radar/spree_auth_devise"
         end
 
         run 'bundle install', :capture => true
