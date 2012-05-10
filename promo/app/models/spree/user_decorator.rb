654ad1efe6d371af53b5b2a0b503cf45226ab888
Ryan Bigg <radarlistener@gmail.com>
2012-05-10 14:49:11 +1000

    Add checks for Spree.user_class to api, dash and promo user decorators

--- promo/app/models/spree/user_decorator.rb
+++ promo/app/models/spree/user_decorator.rb
@@ -1,3 +1,5 @@
-Spree.user_class.class_eval do
-  has_and_belongs_to_many :roles, :join_table => 'spree_roles_users'
+if Spree.user_class
+  Spree.user_class.class_eval do
+    has_and_belongs_to_many :roles, :join_table => 'spree_roles_users'
+  end
 end
