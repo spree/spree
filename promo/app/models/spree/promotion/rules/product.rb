957e661204dea4e0129d54f8bdd8c03da1159aab
Jeff Dutil <jdutil21@gmail.com>
2012-05-16 18:46:51 -0400

    [spree_promo] Explicitly nest promotion classes.

--- promo/app/models/spree/promotion/rules/product.rb
+++ promo/app/models/spree/promotion/rules/product.rb
@@ -2,32 +2,36 @@
 # Can require all or any of the products to be present.
 # Valid products either come from assigned product group or are assingned directly to the rule.
 module Spree
-  class Promotion::Rules::Product < PromotionRule
-    has_and_belongs_to_many :products, :class_name => '::Spree::Product', :join_table => 'spree_products_promotion_rules', :foreign_key => 'promotion_rule_id'
+  class Promotion
+    module Rules
+      class Product < PromotionRule
+        has_and_belongs_to_many :products, :class_name => '::Spree::Product', :join_table => 'spree_products_promotion_rules', :foreign_key => 'promotion_rule_id'
 
-    MATCH_POLICIES = %w(any all)
-    preference :match_policy, :string, :default => MATCH_POLICIES.first
+        MATCH_POLICIES = %w(any all)
+        preference :match_policy, :string, :default => MATCH_POLICIES.first
 
-    # scope/association that is used to test eligibility
-    def eligible_products
-      products
-    end
+        # scope/association that is used to test eligibility
+        def eligible_products
+          products
+        end
 
-    def eligible?(order, options = {})
-      return true if eligible_products.empty?
-      if preferred_match_policy == 'all'
-        eligible_products.all? {|p| order.products.include?(p) }
-      else
-        order.products.any? {|p| eligible_products.include?(p) }
+        def eligible?(order, options = {})
+          return true if eligible_products.empty?
+          if preferred_match_policy == 'all'
+            eligible_products.all? {|p| order.products.include?(p) }
+          else
+            order.products.any? {|p| eligible_products.include?(p) }
+          end
+        end
+
+        def product_ids_string
+          product_ids.join(',')
+        end
+
+        def product_ids_string=(s)
+          self.product_ids = s.to_s.split(',').map(&:strip)
+        end
       end
-    end
-
-    def product_ids_string
-      product_ids.join(',')
-    end
-
-    def product_ids_string=(s)
-      self.product_ids = s.to_s.split(',').map(&:strip)
     end
   end
 end
