dab7e8d8eb81fbe2bfac4d0be9374e33c9b06458
Spencer Steffen <spencer@citrusme.com>
2012-05-17 07:33:26 -0700

    default order adjustments to be ordered by created_at asc
    Fixes #1549

--- core/app/models/spree/order.rb
+++ core/app/models/spree/order.rb
@@ -30,7 +30,7 @@ module Spree
     has_many :payments, :dependent => :destroy
     has_many :shipments, :dependent => :destroy
     has_many :return_authorizations, :dependent => :destroy
-    has_many :adjustments, :as => :adjustable, :dependent => :destroy
+    has_many :adjustments, :as => :adjustable, :dependent => :destroy, :order => "created_at ASC"
 
     accepts_nested_attributes_for :line_items
     accepts_nested_attributes_for :bill_address
