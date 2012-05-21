fe71cd0d3af77397d5b34c7f7222c50f978841c0
Ryan Bigg <radarlistener@gmail.com>
2012-05-21 14:18:23 +1000

    [promo] Check for calculator on adjustment.originator in update_adjustments_with_promotion_listing before checking its type
    Related up to #1526

--- promo/app/models/spree/order_decorator.rb
+++ promo/app/models/spree/order_decorator.rb
@@ -20,7 +20,8 @@ Spree::Order.class_eval do
       most_valuable_adjustment = adjustments.promotion.eligible.max{|a,b| a.amount.abs <=> b.amount.abs}
       current_adjustments = (adjustments.promotion.eligible - [most_valuable_adjustment])
       current_adjustments.each do |adjustment|
-        unless adjustment.originator.calculator.is_a?(Spree::Calculator::PerItem)
+        calculator = adjustment.originator.try(:calculator)
+        if calculator && !calculator.is_a?(Spree::Calculator::PerItem)
           adjustment.update_attribute_without_callbacks(:eligible, false)
         end
       end
