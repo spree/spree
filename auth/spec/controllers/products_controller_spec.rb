ece0250fcbcd0e747a14b3f0cb8959c373597d6e
Ryan Bigg <radarlistener@gmail.com>
2012-05-21 12:12:49 +1000

    Use has_role? not admin? to check for user admin ability in ProductsControllerDecorator
    Fixes #1562

--- auth/spec/controllers/products_controller_spec.rb
+++ auth/spec/controllers/products_controller_spec.rb
@@ -4,13 +4,13 @@ describe Spree::ProductsController do
   let!(:product) { create(:product, :available_on => 1.year.from_now) }
 
   it "allows admins to view non-active products" do
-    controller.stub :current_user => stub(:admin? => true)
+    controller.stub :current_user => stub(:has_role? => true)
     get :show, :id => product.to_param
     response.status.should == 200
   end
 
   it "cannot view non-active products" do
-    controller.stub :current_user => stub(:admin? => false)
+    controller.stub :current_user => stub(:has_role? => false)
     get :show, :id => product.to_param
     response.status.should == 404
   end
