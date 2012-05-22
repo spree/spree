948f0baf48257f365803e32d221c2efad2d909c6
Ryan Bigg <radarlistener@gmail.com>
2012-05-22 16:26:05 +1000

    Remove sandbox_generator, replace with Rails template
    The rails template's code is much simpler and allows us to enforce specific gem requirements that the dummy application should never have.
    
    This fixes the sandbox's requirement of spree_signup_path as pointed out by BDQ here: https://github.com/spree/spree/pull/1512#issuecomment-5621353

--- core/lib/generators/spree/sandbox/templates/rails/routes.rb
+++
@@ -1,3 +0,0 @@
-Rails.application.routes.draw do
-  mount Spree::Core::Engine => "/"
-end
---
+++ sandbox.rb
@@ -0,0 +1,10 @@
+# Used in the sandbox rake task in Rakefile
+gem('spree', :path => "..")
+gem('spree_auth_devise', :path => "~/Sites/gems/spree_auth_devise")
+gem('devise-encryptable', '0.1.1')
+puts "Running Spree installer..."
+generate("spree:install --auto-accept")
+puts "Precompilign assets..."
+rake("assets:precompile:nondigest")
+puts "Done!"
+
