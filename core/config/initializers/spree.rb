32586ed4a8ec5e1b26e389b866484ba5676c8f4e
Ryan Bigg <radarlistener@gmail.com>
2012-05-23 10:39:19 +1000

    Remove very old comments from core/config/initializers/spree.rb

--- core/config/initializers/spree.rb
+++ core/config/initializers/spree.rb
@@ -3,12 +3,4 @@ require 'mail'
 # Spree Configuration
 SESSION_KEY = '_spree_session_id'
 
-# TODO - Add the lib/plugins stuff maybe?
-#::ActiveRecord::Base.send :include, Spree::Preferences::Preferable
-
-# Add extra support goodies (similar to rails active support)
-#class Array #:nodoc:
-#  include Spree::Support::CoreExtensions::Array
-#end
-
 LIKE = ActiveRecord::Base.connection.adapter_name == 'PostgreSQL' ? 'ILIKE' : 'LIKE'
