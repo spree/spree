04b84e3c3862fb65e2f25f583e84ecba8f506ab3
Ryan Bigg <radarlistener@gmail.com>
2012-05-23 10:36:35 +1000

    Move common Spree.user_class role decorations into core
    As per BDQ's comment here: https://github.com/spree/spree/pull/1512#issuecomment-5621353

--- api/app/models/spree/user_decorator.rb
+++ api/app/models/spree/user_decorator.rb
@@ -1,7 +1,5 @@
 if Spree.user_class
   Spree.user_class.class_eval do
-    has_and_belongs_to_many :roles, :join_table => 'spree_roles_users'
-
     def generate_api_key!
       self.api_key = SecureRandom.hex(24)
       save!
@@ -10,11 +8,6 @@ if Spree.user_class
     def clear_api_key!
       self.api_key = nil
       save!
-    end
-
-    # has_spree_role? simply needs to return true or false whether a user has a role or not.
-    def has_spree_role?(role_in_question)
-      roles.any? { |role| role.name == role_in_question.to_s }
     end
   end
 end
---
+++ core/config/initializers/user_class_extensions.rb
@@ -0,0 +1,11 @@
+if Spree.user_class
+  Spree.user_class.class_eval do
+    has_and_belongs_to_many :roles, :join_table => 'spree_roles_users'
+
+    # has_spree_role? simply needs to return true or false whether a user has a role or not.
+    def has_spree_role?(role_in_question)
+      roles.any? { |role| role.name == role_in_question.to_s }
+    end
+  end
+end
+
