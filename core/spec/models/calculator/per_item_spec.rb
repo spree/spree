c82264feab13ea418affa6ce01d4dbf5e3a21e41
Ryan Bigg <radarlistener@gmail.com>
2012-05-25 14:29:13 +1000

    Revert "Per item calculator should only apply to matching products"
    This reverts commit 93be6faa74a0ecf7957ab2cf92ae4029db2f2b4a.
    
    Reverted due to #1596

--- core/spec/models/calculator/per_item_spec.rb
+++ core/spec/models/calculator/per_item_spec.rb
@@ -2,20 +2,16 @@ require 'spec_helper'
 
 describe Spree::Calculator::PerItem do
   # Like an order object, but not quite...
-  let!(:line_items) { [double("LineItem", :quantity => 5, :product => :a_product )] * 3 }
+  let!(:line_items) { [double("LineItem", :quantity => 5)] * 3 }
   let!(:object) { double("Order", :line_items => line_items) }
-  let!(:mock_product_set) { double("Array") }
-  let!(:calculator) { Spree::Calculator::PerItem.new(:preferred_amount => 10) } 
+  let!(:calculator) { Spree::Calculator::PerItem.new(:preferred_amount => 10) }
 
   # regression test for #1414
   it "correctly calculates per item shipping" do
-    mock_product_set.stub(:include?).with(any_args()).and_return(true)
-    calculator.stub(:target_products).and_return(mock_product_set)
     calculator.compute(object).to_f.should == 150 # 5 x 3 x 10
   end
 
   it "returns 0 when no object passed" do
-    calculator.stub(:target_products).and_return(mock_product_set)
     calculator.compute.should == 0
   end
 
