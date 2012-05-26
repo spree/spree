36333e85917f5f2e59b9c24e3f39336d2df56e98
Trung Lê <joneslee85@gmail.com>
2012-05-27 00:33:34 +1000

    Failsafe to returning all countries if the checkout_zone kind is not a country type.
    Also, rename Spree::Zone#country_list to zone_member_list
    
    [Fixes #1584]

--- core/spec/helpers/base_helper_spec.rb
+++ core/spec/helpers/base_helper_spec.rb
@@ -3,6 +3,51 @@ require 'spec_helper'
 describe Spree::BaseHelper do
   include Spree::BaseHelper
 
+  context "available_countries" do
+    let(:country) { create(:country) }
+
+    before do
+      3.times { create(:country) }
+    end
+
+    context "with no checkout zone defined" do
+      before do
+        Spree::Config[:checkout_zone] = nil
+      end
+
+      it "return complete list of countries" do
+        available_countries.count.should == Spree::Country.count
+      end
+    end
+
+    context "with a checkout zone defined" do
+      context "checkout zone is of type country" do
+        before do
+          @country_zone = create(:zone, :name => "CountryZone")
+          @country_zone.members.create(:zoneable => country)
+          Spree::Config[:checkout_zone] = @country_zone.name
+        end
+
+        it "return only the countries defined by the checkout zone" do
+          available_countries.should == [country]
+        end
+      end
+
+      context "checkout zone is of type state" do
+        before do
+          state_zone = create(:zone, :name => "StateZone")
+          state = create(:state, :country => country)
+          state_zone.members.create(:zoneable => state)
+          Spree::Config[:checkout_zone] = state_zone.name
+        end
+
+        it "return complete list of countries" do
+          available_countries.count.should == Spree::Country.count
+        end
+      end
+    end
+  end
+
   # Regression test for #889
   context "seo_url" do
     let(:taxon) { stub(:permalink => "bam") }
