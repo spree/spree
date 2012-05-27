3fe16bfd25bbcdbcd6311c1807484ca51855304f
Ryan Bigg <radarlistener@gmail.com>
2012-05-28 09:25:29 +1000

    Remove spree_auth_devise and devise_encryptable from sandbox

--- lib/sandbox.rb
+++ lib/sandbox.rb
@@ -1,7 +1,5 @@
 # Used in the sandbox rake task in Rakefile
 gem('spree', :path => "..")
-gem('spree_auth_devise', :path => "~/Sites/gems/spree_auth_devise")
-gem('devise-encryptable', '0.1.1')
 puts "Running Spree installer..."
 generate("spree:install --auto-accept")
 puts "Precompiling assets..."
