40b1f9e2fe3aee776c8b7c3369fe1018addf4bf5
Ryan Bigg <radarlistener@gmail.com>
2012-05-28 14:43:34 +1000

    Move clone_billing_address to be with other before_validation call in Order model
    This was previously ~250 lines down in the model

--- core/app/models/spree/order.rb
+++ core/app/models/spree/order.rb
@@ -40,6 +40,8 @@ module Spree
 
     # Needs to happen before save_permalink is called
     before_validation :generate_order_number, :on => :create
+    before_validation :clone_billing_address, :if => :use_billing?
+    attr_accessor :use_billing
 
     before_create :link_by_email
     after_create :create_tax_charge!
@@ -236,9 +238,6 @@ module Spree
         #shipment.ready!
       end
     end
-
-    before_validation :clone_billing_address, :if => :use_billing?
-    attr_accessor :use_billing
 
     def clone_billing_address
       if bill_address and self.ship_address.nil?
