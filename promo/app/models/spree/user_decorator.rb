fae05e81cc168379e3ef29bdbf7a838b6df7b05a
Ryan Bigg <radarlistener@gmail.com>
2012-05-29 13:32:05 +1000

    [promo] Specify table name for roles association in user decorator

--- promo/app/models/spree/user_decorator.rb
+++ promo/app/models/spree/user_decorator.rb
@@ -1,5 +1,5 @@
 if Spree.user_class
   Spree.user_class.class_eval do
-    has_and_belongs_to_many :roles, :class_name => "Spree::Role", :foreign_key => :user_id
+    has_and_belongs_to_many :roles, :class_name => "Spree::Role", :foreign_key => :user_id, :join_table_name => "spree_roles_users"
   end
 end
