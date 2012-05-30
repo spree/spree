6106401d14ff926a42ced7f18783be52a178df43
Ryan Bigg <radarlistener@gmail.com>
2012-05-30 15:53:24 +1000

    Rename api_key field to spree_api_key
    This is to stop it conflicting with a potentially similarly named field on Spree.user_class models

--- api/app/models/spree/user_decorator.rb
+++ api/app/models/spree/user_decorator.rb
@@ -1,12 +1,12 @@
 if Spree.user_class
   Spree.user_class.class_eval do
-    def generate_api_key!
-      self.api_key = SecureRandom.hex(24)
+    def generate_spree_api_key!
+      self.spree_api_key = SecureRandom.hex(24)
       save!
     end
 
-    def clear_api_key!
-      self.api_key = nil
+    def clear_spree_api_key!
+      self.spree_api_key = nil
       save!
     end
   end
