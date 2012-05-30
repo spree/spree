7f3daec82c5d327953234f70d15927aae89193c7
Ryan Bigg <radarlistener@gmail.com>
2012-05-30 15:44:04 +1000

    No need for promo user decorator
    This is because core/config/initializers/user_class_extensions now deals
    with setting up the roles association

--- promo/app/models/spree/user_decorator.rb
+++
@@ -1,5 +0,0 @@
-if Spree.user_class
-  Spree.user_class.class_eval do
-    has_and_belongs_to_many :roles, :class_name => "Spree::Role", :foreign_key => :user_id, :join_table => "spree_roles_users"
-  end
-end
