f090c83831940e9c2fa24049c847cc7ab20893a6
Ryan Bigg <radarlistener@gmail.com>
2012-05-30 15:54:38 +1000

    Add orders association for Spree.user_class models

--- core/config/initializers/user_class_extensions.rb
+++ core/config/initializers/user_class_extensions.rb
@@ -7,6 +7,8 @@ Spree::Core::Engine.config.to_prepare do
                               :foreign_key => "user_id",
                               :class_name => "Spree::Role"
 
+      has_many :spree_orders, :foreign_key => "user_id", :class_name => "Spree::Order"
+
       # has_spree_role? simply needs to return true or false whether a user has a role or not.
       def has_spree_role?(role_in_question)
         spree_roles.any? { |role| role.name == role_in_question.to_s }
