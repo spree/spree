e84c4364066381d4e9450ef17e2083a6c627b7e5
Ryan Bigg <radarlistener@gmail.com>
2012-05-31 12:10:55 +1000

    Add last_incomplete_spree_order to test doubles in checkout and products controller specs

--- core/spec/controllers/spree/products_controller_spec.rb
+++ core/spec/controllers/spree/products_controller_spec.rb
@@ -5,7 +5,7 @@ describe Spree::ProductsController do
 
   # Regression test for #1390
   it "allows admins to view non-active products" do
-    controller.stub :spree_current_user => stub(:has_spree_role? => true)
+    controller.stub :spree_current_user => stub(:has_spree_role? => true, :last_incomplete_spree_order => nil)
     get :show, :id => product.to_param
     response.status.should == 200
   end
