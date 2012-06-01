aebc92b1cfa67fc7a2a8081f15dab65621b01a03
Ryan Bigg <radarlistener@gmail.com>
2012-06-01 13:23:30 +1000

    Correct methods from spree_current_user to just current_user

--- core/app/controllers/spree/admin/banners_controller.rb
+++ core/app/controllers/spree/admin/banners_controller.rb
@@ -3,7 +3,7 @@ module Spree
     class BannersController < Spree::Admin::BaseController
       def dismiss
         if request.xhr? and params[:id]
-          spree_current_user.dismiss_banner(params[:id])
+          current_user.dismiss_banner(params[:id])
           render :nothing => true
         end
       end
