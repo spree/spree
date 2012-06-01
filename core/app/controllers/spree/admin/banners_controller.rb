279dca05658c2c92ccb86010be7422523500428b
Ryan Bigg <radarlistener@gmail.com>
2012-06-02 08:18:33 +1000

    Replace calls to current_user with try_spree_current_user
    This is due to Stack level too deep errors reported by multiple users

--- core/app/controllers/spree/admin/banners_controller.rb
+++ core/app/controllers/spree/admin/banners_controller.rb
@@ -3,7 +3,9 @@ module Spree
     class BannersController < Spree::Admin::BaseController
       def dismiss
         if request.xhr? and params[:id]
-          current_user.dismiss_banner(params[:id])
+          if user = try_spree_current_user
+            user.dismiss_banner(params[:id])
+          end
           render :nothing => true
         end
       end
