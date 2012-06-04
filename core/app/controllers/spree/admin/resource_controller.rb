2b95ef4bd19db7d0de96e0919d241d9c3dcd810b
Thomas von Deyen <tvd@magiclabs.de>
2012-06-04 22:39:34 +0200

    Do not prepend load_resource before filter in ressources controller.
    This fixes an issue using Authlogic as custom user authentication.

--- core/app/controllers/spree/admin/resource_controller.rb
+++ core/app/controllers/spree/admin/resource_controller.rb
@@ -2,7 +2,7 @@ require 'spree/core/action_callbacks'
 
 class Spree::Admin::ResourceController < Spree::Admin::BaseController
   helper_method :new_object_url, :edit_object_url, :object_url, :collection_url
-  prepend_before_filter :load_resource
+  before_filter :load_resource
   rescue_from ActiveRecord::RecordNotFound, :with => :resource_not_found
 
   respond_to :html
