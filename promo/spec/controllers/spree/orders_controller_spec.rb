064cc0d48849a3285d9e0c5779ffe774a1d56bed
Spencer Steffen <spencer@citrusme.com>
2012-06-04 16:54:13 -0700

    add order controller decorator to apply coupon code in orders#update

---
+++ promo/spec/controllers/spree/orders_controller_spec.rb
@@ -0,0 +1,38 @@
+require 'spec_helper'
+
+describe Spree::OrdersController do
+
+  let(:user) { create(:user) }
+  let(:order) { stub_model(Spree::Order, :number => "R123", :reload => nil, :save! => true, :coupon_code => coupon_code, :user => user)}
+  let(:promotion) { Spree::Promotion.create(:name => "TestPromo", :code => "TEST1", :expires_at => Time.now + 86400, :usage_limit => 99, :event_name => "spree.checkout.coupon_code_added", :match_policy => "any") }
+  let(:coupon_code) { promotion.code }
+  let(:invalid_coupon_code) { "12345" }
+
+  before :each do
+    controller.stub :current_user => user
+    controller.stub :current_order => order
+  end
+
+  describe "#update" do
+
+    it "applies a promotion to an order" do
+      order.should_receive(:coupon_code=).
+                 with(coupon_code)
+      controller.should_receive(:fire_event).
+                 with('spree.checkout.coupon_code_added', hash_including(:coupon_code => coupon_code))
+      put :update, :order => { :coupon_code => coupon_code }
+    end
+
+    it "renders orders#edit when coupon code is invalid" do
+      order.should_receive(:coupon_code=).
+                 with(invalid_coupon_code)
+      controller.should_not_receive(:fire_event).
+                 with('spree.checkout.coupon_code_added', hash_including(:coupon_code => invalid_coupon_code))
+      put :update, :order => { :coupon_code => invalid_coupon_code }
+      flash[:error].should == I18n.t(:promotion_not_found)
+      response.should render_template :edit
+    end
+
+  end
+
+end
