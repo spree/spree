eea86abd705792c8a9d01d1142ab4c5e04ee43c6
Ryan Bigg <radarlistener@gmail.com>
2012-06-05 15:34:16 +1000

    use on and not live in cart.js

--- core/app/assets/javascripts/store/cart.js
+++ core/app/assets/javascripts/store/cart.js
@@ -1,7 +1,7 @@
 (function($){
   $(document).ready(function(){
     if($('form#update-cart').is('*')){
-      $('form#update-cart a.delete').show().live('click', function(e){
+      $('form#update-cart a.delete').show().on('click', function(e){
         $(this).parents('.line-item').first().find('input.line_item_quantity').val(0);
         $(this).parents('form').first().submit();
         e.preventDefault();
