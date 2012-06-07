b6f6f8cf7924ba4f235d96b9438f60bdec9945bf
Ryan Bigg <radarlistener@gmail.com>
2012-06-07 10:52:44 +1000

    Stub current_user inside of ProductsController spec to fix broken spec

--- core/spec/controllers/spree/products_controller_spec.rb
+++ core/spec/controllers/spree/products_controller_spec.rb
@@ -3,6 +3,10 @@ require 'spec_helper'
 describe Spree::ProductsController do
   let!(:product) { Factory(:product, :available_on => 1.year.from_now) }
 
+  before do
+    controller.stub :current_user => Factory(:user)
+  end
+
   # Regression test for #1390
   it "cannot view non-active products" do
     get :show, :id => product.to_param
