cf23e9facda89ccfeb1b13aa20ae4049f9d067d3
Ryan Bigg <radarlistener@gmail.com>
2012-06-11 10:23:17 +1000

    Don't rename api_key field to spree_api_key if User const is defined

--- api/db/migrate/20120530054546_rename_api_key_to_spree_api_key.rb
+++ api/db/migrate/20120530054546_rename_api_key_to_spree_api_key.rb
@@ -1,9 +1,7 @@
 class RenameApiKeyToSpreeApiKey < ActiveRecord::Migration
-  def up
-    rename_column :spree_users, :api_key, :spree_api_key
-  end
-
-  def down
-    rename_column :spree_users, :spree_api_key, :api_key
+  def change
+    unless defined?(User)
+      rename_column :spree_users, :api_key, :spree_api_key
+    end
   end
 end
