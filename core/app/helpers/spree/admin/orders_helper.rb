503486d375f63546ed9b58865c1c0c1d00a192ba
Trung Lê <joneslee85@gmail.com>
2012-06-13 09:56:21 +1000

    Replace deprecated :confirm with :data => { :confirm => '' }

--- core/app/helpers/spree/admin/orders_helper.rb
+++ core/app/helpers/spree/admin/orders_helper.rb
@@ -3,7 +3,7 @@ module Spree
     module OrdersHelper
       # Renders all the txn partials that may have been specified in the extensions
       def render_txn_partials(order)
-        @txn_partials.inject("") do |extras, partial|
+        @txn_partials.inject('') do |extras, partial|
           extras += render :partial => partial, :locals => { :payment => order }
         end
       end
@@ -14,7 +14,8 @@ module Spree
         @order_events.sort.each do |event|
           if @order.send("can_#{event}?")
             links << button_link_to(t(event), fire_admin_order_url(@order, :e => event),
-                                    { :method => :put, :confirm => t(:order_sure_want_to, :event => t(event)) })
+                                    :method => :put,
+                                    :data => { :confirm => t(:order_sure_want_to, :event => t(event)) })
           end
         end
         links.join('&nbsp;').html_safe
