d9d6675ce53169923e2cca3171afae9a2dd0279a
Ryan Bigg <radarlistener@gmail.com>
2012-06-13 07:45:11 +1000

    Bump to Rails 3.2.6
    As per: http://weblog.rubyonrails.org/2012/6/12/ann-rails-3-2-6-has-been-released/

--- core/lib/generators/spree/dummy/templates/rails/application.rb
+++ core/lib/generators/spree/dummy/templates/rails/application.rb
@@ -2,7 +2,7 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-Bundler.require(*Rails.groups)
+Bundler.require(*Rails.groups(:assets => %w(development test)))
 
 require '<%= lib_name %>'
 
