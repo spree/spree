da49b41c0b6fec632c24cae8a954906201bd6b1e
Ryan Bigg <radarlistener@gmail.com>
2012-06-12 14:30:24 +1000

    [api] Don't force a transition to ready or shipped if shipment is already at either state

--- api/app/controllers/spree/api/v1/shipments_controller.rb
+++ api/app/controllers/spree/api/v1/shipments_controller.rb
@@ -6,19 +6,23 @@ module Spree
         before_filter :find_and_update_shipment, :only => [:ship, :ready]
 
         def ready
-          @shipment.ready!
+          unless @shipment.ready?
+            @shipment.ready!
+          end
           render :show
         end
 
         def ship
-          @shipment.ship!
+          unless @shipment.shipped?
+            @shipment.ship!
+          end
           render :show
         end
 
         private
 
         def find_order
-          @order = Order.find_by_number!(params[:order_id])
+          @order = Spree::Order.find_by_number!(params[:order_id])
         end
 
         def find_and_update_shipment
