7f639127faa1734c09baed8365c107726e3aeea3
Ryan Bigg <radarlistener@gmail.com>
2012-06-13 10:18:01 +1000

    Corrected stubs in shipment_spec for state transition tests

--- core/spec/models/shipment_spec.rb
+++ core/spec/models/shipment_spec.rb
@@ -45,7 +45,7 @@ describe Spree::Shipment do
     end
 
     context "when order is paid" do
-      before { order.stub :payment_state => 'paid' }
+      before { order.stub :paid? => true }
       it "should result in a 'ready' state" do
         shipment.should_receive(:update_attribute_without_callbacks).with("state", "ready")
         shipment.update!(order)
@@ -55,7 +55,7 @@ describe Spree::Shipment do
     end
 
     context "when order has balance due" do
-      before { order.stub :payment_state => 'balance_due' }
+      before { order.stub :paid? => false }
       it "should result in a 'pending' state" do
         shipment.state = 'ready'
         shipment.should_receive(:update_attribute_without_callbacks).with("state", "pending")
@@ -66,7 +66,7 @@ describe Spree::Shipment do
     end
 
     context "when order has a credit owed" do
-      before { order.stub :payment_state => 'credit_owed' }
+      before { order.stub :payment_state => 'credit_owed', :paid? => true }
       it "should result in a 'ready' state" do
         shipment.state = 'pending'
         shipment.should_receive(:update_attribute_without_callbacks).with("state", "ready")
