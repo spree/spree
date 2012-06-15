5431a96814cd0de673f43a873b35edb580a7631b
Trung Lê <joneslee85@gmail.com>
2012-06-15 23:04:13 +1000

    Refactor State.states_group_by_country_id

--- core/app/models/spree/state.rb
+++ core/app/models/spree/state.rb
@@ -17,7 +17,7 @@ module Spree
     # blank is added elsewhere, if needed
     def self.states_group_by_country_id
       state_info = Hash.new { |h, k| h[k] = [] }
-      Spree::State.order('name ASC').each { |state|
+      self.order('name ASC').each { |state|
         state_info[state.country_id.to_s].push [state.id, state.name]
       }
       state_info
