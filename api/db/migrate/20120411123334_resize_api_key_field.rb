dd41c47bf4d43670573283aa4262be8fdf1949d7
Ryan Bigg <radarlistener@gmail.com>
2012-06-19 17:17:39 +1000

    Only run ResizeApiKeyField migration if spree_users table exists

--- api/db/migrate/20120411123334_resize_api_key_field.rb
+++ api/db/migrate/20120411123334_resize_api_key_field.rb
@@ -1,5 +1,7 @@
 class ResizeApiKeyField < ActiveRecord::Migration
   def change
-    change_column :spree_users, :api_key, :string, :limit => 48
+    if table_exists?(:spree_users)
+      change_column :spree_users, :api_key, :string, :limit => 48
+    end
   end
 end
