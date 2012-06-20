e70a1500fe9cae0a651fca741a8fa0888021447a
Ryan Bigg <radarlistener@gmail.com>
2012-06-20 13:22:53 +1000

    Corrected :class_name options for product and option_type associations in ProductOptionType class

--- core/app/models/spree/product_option_type.rb
+++ core/app/models/spree/product_option_type.rb
@@ -1,7 +1,7 @@
 module Spree
   class ProductOptionType < ActiveRecord::Base
-    belongs_to :product, :class_name => Spree::Product
-    belongs_to :option_type, :class_name => Spree::OptionType
+    belongs_to :product, :class_name => "Spree::Product"
+    belongs_to :option_type, :class_name => "Spree::OptionType"
     acts_as_list :scope => :product
   end
 end
