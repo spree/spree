36085b3cf78aaeaae186341691ff85a9e8fddfc5
Rein Aris <info@reinaris.nl>
2012-06-20 13:08:26 +0200

    drop ie6 support in checkout_helper

--- core/app/helpers/spree/checkout_helper.rb
+++ core/app/helpers/spree/checkout_helper.rb
@@ -26,8 +26,7 @@ module Spree
         css_classes << 'current' if state == @order.state
         css_classes << 'first' if state_index == 0
         css_classes << 'last' if state_index == states.length - 1
-        # It'd be nice to have separate classes but combining them with a dash helps out for IE6 which only sees the last class
-        content_tag('li', content_tag('span', text), :class => css_classes.join('-'))
+        content_tag('li', content_tag('span', text), :class => css_classes)
       end
       content_tag('ol', raw(items.join("\n")), :class => 'progress-steps steps-amount-%s' % [items.count], :id => "checkout-step-#{@order.state}")
     end
