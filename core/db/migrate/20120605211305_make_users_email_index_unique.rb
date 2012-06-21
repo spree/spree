d0b94bc9af921f329f580a9be278272a6731374e
Ryan Bigg <radarlistener@gmail.com>
2012-06-22 08:27:42 +1000

    Don't attempt to remove non-existent email_idx index on spree_users table

--- core/db/migrate/20120605211305_make_users_email_index_unique.rb
+++ core/db/migrate/20120605211305_make_users_email_index_unique.rb
@@ -1,11 +1,10 @@
 class MakeUsersEmailIndexUnique < ActiveRecord::Migration
   def up
-    remove_index "spree_users", :name => "email_idx"
-    add_index "spree_users", ["email"], :name => "email_idx", :unique => true
+    add_index "spree_users", ["email"], :name => "email_idx_unique", :unique => true
   end
 
   def down
-    remove_index "spree_users", :name => "email_idx"
-    add_index "spree_users", ["email"], :name => "email_idx"
+    remove_index "spree_users", :name => "email_idx_unique"
+    add_index "spree_users", ["email"], :name => "email_idx_unique"
   end
 end
