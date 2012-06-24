dd6d5f26685ab506fdc7656e458bb5c4f6c5ae66
Sean Schofield <sean@railsdog.com>
2012-06-24 09:53:50 -0400

    Version bump for 1.1.2.rc1

--- cmd/lib/spree_cmd/extension.rb
+++ cmd/lib/spree_cmd/extension.rb
@@ -51,7 +51,7 @@ module SpreeCmd
       end
 
       def spree_version
-        '1.1.2.beta'
+        '1.1.2.rc1'
       end
 
       def use_prefix(prefix)
--- core/lib/spree/core/version.rb
+++ core/lib/spree/core/version.rb
@@ -1,5 +1,5 @@
 module Spree
   def self.version
-    "1.1.2.beta"
+    "1.1.2.rc1"
   end
 end
