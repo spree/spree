e715daf46fc121bdc003a12563b16c2173fe74ca
Ryan Bigg <radarlistener@gmail.com>
2012-06-25 10:15:57 +1000

    Fix issue where we were incorrectly calling a method on an Arel::Table object, rather than using []
    Fixes #1709, and adds an actual test for the brands_any filter
    
    Conflicts:
    
    	core/lib/spree/product_filters.rb

--- core/lib/spree/product_filters.rb
+++ core/lib/spree/product_filters.rb
@@ -94,8 +94,7 @@ module Spree
     #   the (uniquely named) field "p_brand.value". There's also a test for brand info
     #   being blank: note that this relies on with_property doing a left outer join
     #   rather than an inner join.
-
-    if Spree::Property.table_exists? && @@brand_property = Spree::Property.find_by_name("brand")
+    if Spree::Property.table_exists?
       Spree::Product.add_search_scope :brand_any do |*opts|
         conds = opts.map {|o| ProductFilters.brand_filter[:conds][o]}.reject {|c| c.nil?}
         scope = conds.shift
@@ -106,7 +105,8 @@ module Spree
       end
 
       def ProductFilters.brand_filter
-        brands = Spree::ProductProperty.where(:property_id => @@brand_property).pluck(:value).uniq
+        brand_property = Spree::Property.find_by_name("brand")
+        brands = Spree::ProductProperty.where(:property_id => brand_property).pluck(:value).uniq
         pp = Spree::ProductProperty.arel_table
         conds = Hash[*brands.map { |b| [b, pp[:value].eq(b)] }.flatten]
         {
@@ -137,18 +137,18 @@ module Spree
     #
     #   The brand-finding code can be simplified if a few more named scopes were added to
     #   the product properties model.
-    if Spree::Property.table_exists? && @@brand_property
+    if Spree::Property.table_exists?
       Spree::Product.add_search_scope :selective_brand_any do |*opts|
         Spree::Product.brand_any(*opts)
       end
 
       def ProductFilters.selective_brand_filter(taxon = nil)
-        taxon = Spree::Taxonomy.first.root if taxon.nil?
-        scope = Spree::ProductProperty.where(:property_id => @@brand_property).
+        taxon ||= Spree::Taxonomy.first.root
+        brand_property = Spree::Property.find_by_name("brand")
+        scope = Spree::ProductProperty.where(:property_id => brand_property).
                                        joins(:product => :taxons).
                                        where("#{Spree::Taxon.table_name}.id" => [taxon] + taxon.descendants).
                                        scoped
-
         brands = scope.pluck(:value).uniq
         {
           :name   => "Applicable Brands",
