84eab96c6d1ee77f81a753f53ee0f336210d77f0
Ryan Bigg <radarlistener@gmail.com>
2012-06-27 11:37:31 +1000

    Tidy up guest checkout code inside checkouts/edit.js

--- core/app/assets/javascripts/admin/checkouts/edit.js
+++ core/app/assets/javascripts/admin/checkouts/edit.js
@@ -9,27 +9,12 @@ $(document).ready(function(){
     $('#checkout_email').val("");
     $('#guest_checkout_false').prop("disabled", true);
 
-    $('#order_bill_address_attributes_firstname').val("");
-    $('#order_bill_address_attributes_lastname').val("");
-    $('#order_bill_address_attributes_company').val("");
-    $('#order_bill_address_attributes_address1').val("");
-    $('#order_bill_address_attributes_address2').val("");
-    $('#order_bill_address_attributes_city').val("");
-    $('#order_bill_address_attributes_zipcode').val("");
-    $('#order_bill_address_attributes_state_id').val("");
-    $('#order_bill_address_attributes_country_id').val("");
-    $('#order_bill_address_attributes_phone').val("");
-
-    $('#order_ship_address_attributes_firstname').val("");
-    $('#order_ship_address_attributes_lastname').val("");
-    $('#order_ship_address_attributes_company').val("");
-    $('#order_ship_address_attributes_address1').val("");
-    $('#order_ship_address_attributes_address2').val("");
-    $('#order_ship_address_attributes_city').val("");
-    $('#order_ship_address_attributes_zipcode').val("");
-    $('#order_ship_address_attributes_state_id').val("");
-    $('#order_ship_address_attributes_country_id').val("");
-    $('#order_ship_address_attributes_phone').val("");
+    var fields = ["firstname", "lastname", "company", "address1", "address2",
+              "city", "zipcode", "state_id", "country_id", "phone"]
+    $.each(fields, function(i, field) {
+      $('#order_bill_address_attributes' + field).val("");
+      $('#order_ship_address_attributes' + field).val("");
+    })
   });
 
   var show_billing = function(show) {
