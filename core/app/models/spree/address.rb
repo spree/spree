c08778f2fe9175dacbf1cb5c61c1e16ffd036493
Ryan Bigg <radarlistener@gmail.com>
2012-07-04 11:28:16 +1000

    Fix issue where Address.default may not actually be assigned a country

--- core/app/models/spree/address.rb
+++ core/app/models/spree/address.rb
@@ -60,7 +60,9 @@ module Spree
 
     def self.default
       country = Spree::Country.find_by_id(Spree::Config[:default_country_id])
-      new :country_id => country.try(:id) || Country.first.id
+      address = new
+      address.country = country || Country.first
+      address
     end
 
     # can modify an address if it's not been used in an order (but checkouts controller has finer control)
