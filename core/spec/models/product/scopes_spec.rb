12c1044fe14935d96aa7e8534cb4b861e0958a62
Ryan Bigg <radarlistener@gmail.com>
2012-07-09 10:24:10 +1000

    Removed test for deprecated conditions_any scope from product/scopes_spec

--- core/spec/models/product/scopes_spec.rb
+++ core/spec/models/product/scopes_spec.rb
@@ -36,13 +36,6 @@ describe "product scopes" do
     it ".conditions_all" do
       Spree::Product.conditions("name = ?", "Alpha").first.should == product
     end
-
-    it ".conditions_any" do
-      product_2 = Factory(:product, :name => "Beta")
-      products = Spree::Product.conditions_any("name = 'Beta'", "name = 'Alpha'")
-      products.should include(product)
-      products.should include(product_2)
-    end
   end
 
   context "price" do
