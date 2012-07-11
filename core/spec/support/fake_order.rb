e98e7eeaf00ead8aa7b7c0e856c18ff4ff88cd93
Ryan Bigg <radarlistener@gmail.com>
2012-07-11 11:52:03 +1000

    Fake out associations in FakeOrder using fake_association method

--- core/spec/support/fake_order.rb
+++ core/spec/support/fake_order.rb
@@ -5,9 +5,14 @@ class FakeOrder
   include ActiveModel::Validations::Callbacks
   include ActiveModel::AttributeMethods
 
-  def self.belongs_to(name, options={})
+  def self.fake_association(name, options={})
     attr_accessor name
   end
+  class << self
+    alias_method :belongs_to, :fake_association
+    alias_method :has_many, :fake_association
+  end
+
 
   def self.accepts_nested_attributes_for(*args)
 
