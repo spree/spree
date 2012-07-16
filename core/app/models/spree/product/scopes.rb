01d3e57ceba8bc81bb3f374f5ddaf581a4071583
Ryan Bigg <radarlistener@gmail.com>
2012-07-17 09:52:44 +1000

    Remove conditions and conditions_all from scopes
    Rather than using these scopes, people should be creating their own scopes using the Ransack API

--- core/app/models/spree/product/scopes.rb
+++ core/app/models/spree/product/scopes.rb
@@ -34,17 +34,6 @@ module Spree
       joins(:variants_with_only_master).order("#{variant_table_name}.price DESC")
     end
 
-    # Ryan Bates - http://railscasts.com/episodes/112
-    # general merging of conditions, names following the searchlogic pattern
-    add_search_scope :conditions do |*args|
-      where(args)
-    end
-
-    # conditions_all is a more descriptively named enhancement of the above
-    add_search_scope :conditions_all do |*args|
-      where([args].flatten)
-    end
-
     add_search_scope :price_between do |low, high|
       joins(:master).where(Variant.table_name => { :price => low..high })
     end
