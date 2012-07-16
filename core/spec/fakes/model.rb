e707d2fae17940ae2c728effde6734fa63f98d06
Ryan Bigg <radarlistener@gmail.com>
2012-07-16 16:08:06 +1000

    Use ActiveSupport::Concern within FakeModel

--- core/spec/fakes/model.rb
+++ core/spec/fakes/model.rb
@@ -1,33 +1,40 @@
 require 'active_model'
+require 'active_support/concern'
 
 module FakeModel
-  def self.included(base)
-    base.class_eval do
-      include ActiveModel::Validations
-      include ActiveModel::Validations::Callbacks
-      include ActiveModel::AttributeMethods
+  extend ActiveSupport::Concern
+  included do
+    include ActiveModel::Validations
+    include ActiveModel::Validations::Callbacks
+    # include ActiveModel::AttributeMethods
+    include ActiveSupport::Callbacks
 
-      def self.belongs_to(name, options={})
-        attr_accessor name
-      end
+    define_callbacks :create
 
-      def self.has_many(name, options={})
-        attr_accessor name
-        # Stolen from abstract_controller/callbacks
-        class_eval <<-RUBY_EVAL, __FILE__, __LINE__ + 1
-        def #{name}
-          @#{name} ||= []
-        end
-        RUBY_EVAL
-      end
+    def self.after_create(name)
+      set_callback :create, :after, name
+    end
 
-      def self.accepts_nested_attributes_for(*args)
-        #noop
-      end
+    def self.belongs_to(name, options={})
+      attr_accessor name
+    end
 
-      def self.attr_accessible(*args)
-        #noop
+    def self.has_many(name, options={})
+      attr_accessor name
+      # Stolen from abstract_controller/callbacks
+      class_eval <<-RUBY_EVAL, __FILE__, __LINE__ + 1
+      def #{name}
+        @#{name} ||= []
       end
+      RUBY_EVAL
+    end
+
+    def self.accepts_nested_attributes_for(*args)
+      #noop
+    end
+
+    def self.attr_accessible(*args)
+      #noop
     end
   end
 
