71a3b86580a054028b3977dc013d1eaa8bbeb1dd
Ryan Bigg <radarlistener@gmail.com>
2012-07-17 14:35:07 +1000

    Add Spree::Core::ControllerHelpers hack for analytics to dash engine, NOT api engine
    Related to #1048 and #1052

--- api/lib/spree/api/engine.rb
+++ api/lib/spree/api/engine.rb
@@ -21,13 +21,4 @@ module Spree
   end
 end
 
-# add helper to all the base controllers
-# Spree::BaseController includes Spree::Core::ControllerHelpers
-require 'spree/core/controller_helpers'
-class << Spree::Core::ControllerHelpers
-  def included_with_analytics(receiver)
-    included_without_analytics(receiver)
-    receiver.send :helper, 'spree/analytics'
-  end
-  alias_method_chain :included, :analytics
-end
+
