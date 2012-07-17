13f3152d24a9464545e08cc31e3defcd04a18ad3
Ryan Bigg <radarlistener@gmail.com>
2012-07-17 15:17:57 +1000

    [promo] Use spree_orders method to get user's orders in OrdersController spec, and spree_put to make requests

--- promo/spec/controllers/spree/orders_controller_spec.rb
+++ promo/spec/controllers/spree/orders_controller_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Spree::OrdersController do
 
   let(:user) { create(:user) }
-  let(:order) { user.orders.create }
+  let(:order) { user.spree_orders.create }
   let(:promotion) { Spree::Promotion.create(:name => "TestPromo", :code => "TEST1", :expires_at => Time.now + 86400, :usage_limit => 99, :event_name => "spree.checkout.coupon_code_added", :match_policy => "any") }
   let(:coupon_code) { promotion.code }
   let(:invalid_coupon_code) { "12345" }
@@ -20,7 +20,7 @@ describe Spree::OrdersController do
                  with('spree.order.contents_changed')
       controller.should_receive(:fire_event).
                  with('spree.checkout.coupon_code_added', hash_including(:coupon_code => coupon_code))
-      put :update, :order => { :coupon_code => coupon_code }
+      spree_put :update, :order => { :coupon_code => coupon_code }
       order.coupon_code.should == coupon_code
       flash[:notice].should == I18n.t(:coupon_code_applied)
       response.should redirect_to(spree.cart_path)
@@ -29,7 +29,7 @@ describe Spree::OrdersController do
     it "renders orders#edit when coupon code is invalid" do
       controller.should_not_receive(:fire_event).
                  with('spree.checkout.coupon_code_added', hash_including(:coupon_code => invalid_coupon_code))
-      put :update, :order => { :coupon_code => invalid_coupon_code }
+      spree_put :update, :order => { :coupon_code => invalid_coupon_code }
       flash[:error].should == I18n.t(:promotion_not_found)
       response.should render_template :edit
     end
