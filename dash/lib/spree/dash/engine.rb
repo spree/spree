36dddefeb262b55cdee4daa41610a05a1f2cac5f
Ryan Bigg <radarlistener@gmail.com>
2012-07-18 16:27:06 +1000

    Remove duplicate definition of included_without_analytics

--- dash/lib/spree/dash/engine.rb
+++ dash/lib/spree/dash/engine.rb
@@ -18,14 +18,3 @@ module Spree
     end
   end
 end
-
-# add helper to all the base controllers
-# Spree::BaseController includes Spree::Core::ControllerHelpers
-require 'spree/core/controller_helpers'
-class << Spree::Core::ControllerHelpers
-  def included_with_analytics(receiver)
-    included_without_analytics(receiver)
-    receiver.send :helper, 'spree/analytics'
-  end
-  alias_method_chain :included, :analytics
-end
