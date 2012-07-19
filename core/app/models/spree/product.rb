50e1704a346dca089a6b7d34b42e1cda7baca2fe
Ryan Bigg <radarlistener@gmail.com>
2012-07-19 15:24:39 +1000

    Fix master association causing stack level too deep error when master variant is deleted
    Fixes #1780

--- core/app/models/spree/product.rb
+++ core/app/models/spree/product.rb
@@ -30,8 +30,8 @@ module Spree
     belongs_to :shipping_category
 
     has_one :master,
-      :class_name => 'Spree::Variant',
-      :conditions => ["#{Variant.quoted_table_name}.is_master = ? AND #{Variant.quoted_table_name}.deleted_at IS NULL", true]
+      :class_name => "Spree::Variant",
+      :conditions => { :is_master => true }
 
     delegate_belongs_to :master, :sku, :price, :weight, :height, :width, :depth, :is_master
     delegate_belongs_to :master, :cost_price if Variant.table_exists? && Variant.column_names.include?('cost_price')
