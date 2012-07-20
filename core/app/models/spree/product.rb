d80d46d13eac84d367f83ac421bde1b9ab45ad99
Ryan Bigg <radarlistener@gmail.com>
2012-07-20 13:56:11 +1000

    Find variant_images linked to a product through its master variant or the product itself

--- core/app/models/spree/product.rb
+++ core/app/models/spree/product.rb
@@ -60,8 +60,8 @@ module Spree
 
     def variant_images
       Image.joins("LEFT JOIN #{Variant.quoted_table_name} ON #{Variant.quoted_table_name}.id = #{Asset.quoted_table_name}.viewable_id").
-      where("#{Variant.quoted_table_name}.product_id = #{self.id}").
-      where("#{Asset.quoted_table_name}.viewable_type = '#{Variant.name}'").
+      where("(#{Asset.quoted_table_name}.viewable_type = ? AND #{Asset.quoted_table_name}.viewable_id = ?) OR
+             (#{Asset.quoted_table_name}.viewable_type = ? AND #{Asset.quoted_table_name}.viewable_id = ?)", Variant.name, self.master.id, Product.name, self.id).
       order("#{Asset.quoted_table_name}.position").
       extend(Spree::Core::RelationSerialization)
     end
