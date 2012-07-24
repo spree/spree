79909e0e98e98b399736bff996e8b3b71b3c9611
Ryan Bigg <radarlistener@gmail.com>
2012-07-25 09:35:06 +1000

    Don't rely on specific method calls in shipping_methods_controller_spec

--- core/spec/controllers/spree/admin/shipping_methods_controller_spec.rb
+++ core/spec/controllers/spree/admin/shipping_methods_controller_spec.rb
@@ -7,7 +7,8 @@ describe Spree::Admin::ShippingMethodsController do
   it "should not hard-delete shipping methods" do
     Spree::ShippingMethod.should_receive(:find).and_return(shipping_method = stub_model(Spree::ShippingMethod))
     shipping_method.should_not_receive(:destroy)
-    shipping_method.should_receive(:update_attribute)
+    shipping_method.deleted_at.should be_nil
     spree_delete :destroy, :id => 1
+    shipping_method.deleted_at.should_not be_nil
   end
 end
