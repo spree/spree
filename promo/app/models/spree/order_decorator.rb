5b35c5d14f6ef7478da9b5f812980784355df175
Ryan Bigg <radarlistener@gmail.com>
2012-07-25 16:43:07 +1000

    [promo] Moved update_adjustments override into OrderUpdater decorator

--- promo/app/models/spree/order_decorator.rb
+++ promo/app/models/spree/order_decorator.rb
@@ -12,19 +12,5 @@ Spree::Order.class_eval do
   def products
     line_items.map {|li| li.variant.product}
   end
-
-  unless self.method_defined?('update_adjustments_with_promotion_limiting')
-    def update_adjustments_with_promotion_limiting
-      update_adjustments_without_promotion_limiting
-      return if adjustments.promotion.eligible.none?
-      most_valuable_adjustment = adjustments.promotion.eligible.max{|a,b| a.amount.abs <=> b.amount.abs}
-      current_adjustments = (adjustments.promotion.eligible - [most_valuable_adjustment])
-      current_adjustments.each do |adjustment|
-        if adjustment.originator.calculator.is_a?(Spree::Calculator::PerItem)
-          adjustment.update_attribute_without_callbacks(:eligible, false)
-        end
-      end
-    end
-    alias_method_chain :update_adjustments, :promotion_limiting
-  end
 end
+
