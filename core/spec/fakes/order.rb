449f5707a616288dced44ac211d3a76d8bd5614b
Ryan Bigg <radarlistener@gmail.com>
2012-07-26 12:06:06 +1000

    Reference Spree::FakeOrder in remove_const call in fakes/order.rb so that constant is actually removed

--- core/spec/fakes/order.rb
+++ core/spec/fakes/order.rb
@@ -2,7 +2,7 @@ require 'fakes/model'
 
 module Spree
   # Every time this file is loaded, this constant needs to be reloaded
-  remove_const :FakeOrder if defined?(FakeOrder)
+  remove_const :FakeOrder if defined?(Spree::FakeOrder)
   class FakeOrder
     include FakeModel
   end
