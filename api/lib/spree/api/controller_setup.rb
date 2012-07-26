fdc4e8837c5944cbcb8ccef95256c40a1a36de83
Ryan Bigg <radarlistener@gmail.com>
2012-07-26 10:45:00 +1000

    [api] Prepend app's view path to view path lookups so views can be overriden
    Fixes #1804

--- api/lib/spree/api/controller_setup.rb
+++ api/lib/spree/api/controller_setup.rb
@@ -16,6 +16,7 @@ module Spree
           include ActionController::MimeResponds
 
           include CanCan::ControllerAdditions
+          prepend_view_path Rails.root + "app/views"
           append_view_path File.expand_path("../../../app/views", File.dirname(__FILE__))
 
           respond_to :json
