b7c50d838d93d6176cf7cc9e38fe27f707c93e61
Ryan Bigg <radarlistener@gmail.com>
2012-07-27 15:47:50 +1000

    Remove unused option_type_select method

--- core/app/helpers/spree/admin/products_helper.rb
+++ core/app/helpers/spree/admin/products_helper.rb
@@ -1,12 +1,6 @@
 module Spree
   module Admin
     module ProductsHelper
-      def option_type_select(so)
-        select(:new_variant,
-               so.option_type.presentation,
-               so.option_type.option_values.collect { |ov| [ ov.presentation, ov.id ] })
-      end
-
     end
   end
 end
