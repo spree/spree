18964ade7b87087fc4e6bb47985d7f1ecca53310
Michael Bianco <iloveitaly@gmail.com>
2012-07-27 12:27:59 -0400

    Use product description if no meta description is defined
    Fixes #1811

--- core/app/helpers/spree/base_helper.rb
+++ core/app/helpers/spree/base_helper.rb
@@ -65,12 +65,21 @@ module Spree
 
     def meta_data_tags
       object = instance_variable_get('@'+controller_name.singularize)
-      meta = { :keywords => Spree::Config[:default_meta_keywords], :description => Spree::Config[:default_meta_description] }
+      meta = {}
 
-      if object.kind_of?(ActiveRecord::Base)
+      if object.kind_of? ActiveRecord::Base
         meta[:keywords] = object.meta_keywords if object[:meta_keywords].present?
         meta[:description] = object.meta_description if object[:meta_description].present?
       end
+
+      if meta[:description].blank? && object.kind_of?(Spree::Product)
+        meta[:description] = strip_tags(object.description)
+      end
+
+      meta.reverse_merge!({
+        :keywords => Spree::Config[:default_meta_keywords],
+        :description => Spree::Config[:default_meta_description]
+      })
 
       meta.map do |name, content|
         tag('meta', :name => name, :content => content)
