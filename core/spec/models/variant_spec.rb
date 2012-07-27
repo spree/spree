7f462de65d6415682de83b2d6eea8224f5d6c833
Ryan Bigg <radarlistener@gmail.com>
2012-07-27 16:51:42 +1000

    Variant must be master for 'recalculates product's count_on_hand when saved' test

--- core/spec/models/variant_spec.rb
+++ core/spec/models/variant_spec.rb
@@ -21,6 +21,7 @@ describe Spree::Variant do
 
   # Regression test for #1778
   it "recalculates product's count_on_hand when saved" do
+    variant.stub :is_master => true
     variant.product.should_receive(:on_hand).and_return(3)
     variant.product.should_receive(:update_column).with(:count_on_hand, 3)
     variant.decrement!(:count_on_hand)
