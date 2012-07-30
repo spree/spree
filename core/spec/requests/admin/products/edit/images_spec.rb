fd0fba0940473bb9aff6ce3060860623a9621823
Ryan Bigg <radarlistener@gmail.com>
2012-07-30 14:08:20 +1000

    Clean up image path in products/edit/images_spec

--- core/spec/requests/admin/products/edit/images_spec.rb
+++ core/spec/requests/admin/products/edit/images_spec.rb
@@ -14,7 +14,7 @@ describe "Product Images" do
       click_link "Edit"
       click_link "Images"
       click_link "new_image_link"
-      absolute_path = File.expand_path(Rails.root.join('..', '..', 'spec', 'support', 'ror_ringer.jpeg'))
+      absolute_path = Rails.root + "../../spec/support/ror_ringer.jpeg"
       attach_file('image_attachment', absolute_path)
       click_button "Update"
       page.should have_content("successfully created!")
