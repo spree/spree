182acfc7a9749a01ec7ce93e956fc8f648bdb0b8
Michael Bianco <iloveitaly@gmail.com>
2012-07-31 11:20:31 -0400

    Better zone assignment for tax category factory

--- core/lib/spree/core/testing_support/factories/tax_category_factory.rb
+++ core/lib/spree/core/testing_support/factories/tax_category_factory.rb
@@ -7,7 +7,7 @@ FactoryGirl.define do
   factory :tax_category_with_rates, :parent => :tax_category do
     after_create do |tax_category|
       tax_category.tax_rates.build(:amount => 0.05, :calculator => Spree::Calculator::DefaultTax.new) do |r|
-        r.zone = Spree::Zone.global
+        r.zone = Spree::Zone.find_by_name('GlobalZone') || FactoryGirl.create(:global_zone)
       end.save!
     end
   end
