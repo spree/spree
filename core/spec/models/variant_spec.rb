c8f3fe50c137674ad02dccc818dd5d2ad86bfaf1
Ryan Bigg <radarlistener@gmail.com>
2012-08-01 11:05:31 +0930

    Track inventory levels in variant spec

--- core/spec/models/variant_spec.rb
+++ core/spec/models/variant_spec.rb
@@ -23,6 +23,7 @@ describe Spree::Variant do
 
   # Regression test for #1778
   it "recalculates product's count_on_hand when saved" do
+    Spree::Config[:track_inventory_levels] = true
     variant.stub :is_master? => true
     variant.product.should_receive(:on_hand).and_return(3)
     variant.product.should_receive(:update_column).with(:count_on_hand, 3)
