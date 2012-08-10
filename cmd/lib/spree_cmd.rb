0a2eb58a36a2178bb1416f0aa940ac215ad4b96f
Ryan Bigg <radarlistener@gmail.com>
2012-08-10 10:43:52 +1000

    Allow -v or --version to get back the version

--- cmd/lib/spree_cmd.rb
+++ cmd/lib/spree_cmd.rb
@@ -4,16 +4,17 @@ require 'thor/group'
 module SpreeCmd
   class Command
 
-    if ARGV.first == 'version'
-      puts Gem.loaded_specs['spree_cmd'].version
-    elsif ARGV.first == 'extension'
-      ARGV.shift
-      require 'spree_cmd/extension'
-      SpreeCmd::Extension.start
-    else
-      ARGV.shift
-      require 'spree_cmd/installer'
-      SpreeCmd::Installer.start
+    case ARGV.first
+      when 'version' || '-v' || '--version'
+        puts Gem.loaded_specs['spree_cmd'].version
+      when 'extension'
+        ARGV.shift
+        require 'spree_cmd/extension'
+        SpreeCmd::Extension.start
+      else
+        ARGV.shift
+        require 'spree_cmd/installer'
+        SpreeCmd::Installer.start
     end
   end
 end
