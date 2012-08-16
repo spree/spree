c69ac34e654386ad7e90ae88142973a80bbe6352
Ryan Bigg <radarlistener@gmail.com>
2012-08-16 10:20:25 +1000

    Revert "Turn sandbox script into a shell script"
    This reverts commit ee1039cfb9b2e98a28a4fd91d395e032f036cb94.

---
+++ lib/sandbox.rb
@@ -0,0 +1,7 @@
+# Used in the sandbox rake task in Rakefile
+gem('spree', :path => "..")
+puts "Running Spree installer..."
+generate("spree:install --auto-accept")
+puts "Precompiling assets..."
+rake("assets:precompile:nondigest")
+
