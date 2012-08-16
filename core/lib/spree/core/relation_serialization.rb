6a52d585001fde98905edae69330c81219a4d428
Trung Lê <joneslee85@gmail.com>
2012-08-16 10:17:52 +1000

    Remove defunct Spree::Core::RelationSerialization module

--- core/lib/spree/core/relation_serialization.rb
+++
@@ -1,9 +0,0 @@
-module Spree
-  module Core
-    module RelationSerialization
-      def serializable_hash(options = nil)
-        self.map { |a| a.serializable_hash(options) }
-      end
-    end
-  end
-end
