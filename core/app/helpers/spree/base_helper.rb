d39ac801569338d28464cd71c5c9529cfea22d21
Georges Auberger <georges@ternarylabs.com>
2012-08-16 17:12:16 -0700

    Nuke breadcrumbs helper
    This method conflicts with our breadcrumbs. Removing from a decorator
    turned out to be unreliable with the introduction of resque workers.
    Nuking it since we have no use for it.

--- core/app/helpers/spree/base_helper.rb
+++ core/app/helpers/spree/base_helper.rb
@@ -93,21 +93,6 @@ module Spree
       nil
     end
 
-    def breadcrumbs(taxon, separator="&nbsp;&raquo;&nbsp;")
-      return "" if current_page?("/") || taxon.nil?
-      separator = raw(separator)
-      crumbs = [content_tag(:li, link_to(t(:home) , root_path) + separator)]
-      if taxon
-        crumbs << content_tag(:li, link_to(t(:products) , products_path) + separator)
-        crumbs << taxon.ancestors.collect { |ancestor| content_tag(:li, link_to(ancestor.name , seo_url(ancestor)) + separator) } unless taxon.ancestors.empty?
-        crumbs << content_tag(:li, content_tag(:span, link_to(taxon.name , seo_url(taxon))))
-      else
-        crumbs << content_tag(:li, content_tag(:span, t(:products)))
-      end
-      crumb_list = content_tag(:ul, raw(crumbs.flatten.map{|li| li.mb_chars}.join), :class => 'inline')
-      content_tag(:nav, crumb_list, :id => 'breadcrumbs')
-    end
-
     def taxons_tree(root_taxon, current_taxon, max_level = 1)
       return '' if max_level < 1 || root_taxon.children.empty?
       content_tag :ul, :class => 'taxons-list' do
