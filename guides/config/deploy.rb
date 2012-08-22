c7fcce50e738be80beef90b344b7612a71e69be1
Sean Schofield <sean.schofield@gmail.com>
2012-08-22 04:32:59 -0400

    Deploy latest stable guides

--- guides/config/deploy.rb
+++ guides/config/deploy.rb
@@ -20,7 +20,7 @@ set :repository,  "git://github.com/spree/spree-guides.git"
 if exists?(:edge)
   set :branch, "master"
 else
-  set :branch, "1-1-x"
+  set :branch, "1-2-x"
 end
 
 set :deploy_to,   "/data/#{application}"
