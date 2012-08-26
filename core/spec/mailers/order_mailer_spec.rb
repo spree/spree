7968ff8c1e29523233e4622289497ff08ffc2863
Rodrigo Pinto <rodrigopqn@gmail.com>
2012-08-26 08:55:50 -0300

    Translate order cancel + confirmation emails, as well as shipped item email
    Merges #1884
    
    Conflicts:
    
    	core/app/views/spree/order_mailer/cancel_email.text.erb
    	core/app/views/spree/order_mailer/confirm_email.text.erb
    	core/spec/mailers/shipment_mailer_spec.rb

--- core/spec/mailers/order_mailer_spec.rb
+++ core/spec/mailers/order_mailer_spec.rb
@@ -31,7 +31,7 @@ describe Spree::OrderMailer do
     end
 
     let!(:confirmation_email) { Spree::OrderMailer.confirm_email(order) }
-    let!(:cancel_email) { Spree::OrderMailer.confirm_email(order) }
+    let!(:cancel_email) { Spree::OrderMailer.cancel_email(order) }
 
     specify do
       confirmation_email.body.should_not include("Ineligible Adjustment")
@@ -42,4 +42,53 @@ describe Spree::OrderMailer do
     end
   end
 
+  context "emails must be translatable" do
+    context "en locale" do
+      before do
+        en_confirm_mail = { :order_mailer => { :confirm_email => { :dear_customer => 'Dear Customer,' } } }
+        en_cancel_mail = { :order_mailer => { :cancel_email => { :order_summary_canceled => 'Order Summary [CANCELED]' } } }
+        I18n.backend.store_translations :en, en_confirm_mail
+        I18n.backend.store_translations :en, en_cancel_mail
+        I18n.locale = :en
+      end
+
+      context "confirm_email" do
+        specify do
+          confirmation_email = Spree::OrderMailer.confirm_email(order)
+          confirmation_email.body.should include("Dear Customer,")
+        end
+      end
+
+      context "cancel_email" do
+        specify do
+          cancel_email = Spree::OrderMailer.cancel_email(order)
+          cancel_email.body.should include("Order Summary [CANCELED]")
+        end
+      end
+    end
+
+    context "pt-BR locale" do
+      before do
+        pt_br_confirm_mail = { :order_mailer => { :confirm_email => { :dear_customer => 'Caro Cliente,' } } }
+        pt_br_cancel_mail = { :order_mailer => { :cancel_email => { :order_summary_canceled => 'Resumo da Pedido [CANCELADA]' } } }
+        I18n.backend.store_translations :'pt-BR', pt_br_confirm_mail
+        I18n.backend.store_translations :'pt-BR', pt_br_cancel_mail
+        I18n.locale = :'pt-BR'
+      end
+
+      context "confirm_email" do
+        specify do
+          confirmation_email = Spree::OrderMailer.confirm_email(order)
+          confirmation_email.body.should include("Caro Cliente,")
+        end
+      end
+
+      context "cancel_email" do
+        specify do
+          cancel_email = Spree::OrderMailer.cancel_email(order)
+          cancel_email.body.should include("Resumo da Pedido [CANCELADA]")
+        end
+      end
+    end
+  end
 end
---
+++ core/spec/mailers/shipment_mailer_spec.rb
@@ -0,0 +1,53 @@
+require 'spec_helper'
+require 'email_spec'
+
+describe Spree::ShipmentMailer do
+  include EmailSpec::Helpers
+  include EmailSpec::Matchers
+
+  let(:shipment) do
+    order = stub_model(Spree::Order)
+    product = stub_model(Spree::Product, :name => %Q{The "BEST" product})
+    variant = stub_model(Spree::Variant, :product => product)
+    variant.stub(:in_stock? => false)
+    line_item = stub_model(Spree::LineItem, :variant => variant, :order => order, :quantity => 1, :price => 5)
+    shipment = stub_model(Spree::Shipment)
+    shipment.stub(:line_items => [line_item], :order => order)
+    shipment
+  end
+
+  it "doesn't include out of stock html span in the email body" do
+    Spree::Config.allow_backorders = false
+    shipment_email = Spree::ShipmentMailer.shipped_email(shipment)
+    shipment_email.body.should_not include(%Q{span class="out-of-stock"})
+  end
+
+  context "emails must be translatable" do
+    context "shipped_email" do
+      context "en locale" do
+        before do
+          en_shipped_email = { :shipment_mailer => { :shipped_email => { :dear_customer => 'Dear Customer,' } } }
+          I18n.backend.store_translations :en, en_shipped_email
+          I18n.locale = :en
+        end
+
+        specify do
+          shipped_email = Spree::ShipmentMailer.shipped_email(shipment)
+          shipped_email.body.should include("Dear Customer,")
+        end
+      end
+      context "pt-BR locale" do
+        before do
+          pt_br_shipped_email = { :shipment_mailer => { :shipped_email => { :dear_customer => 'Caro Cliente,' } } }
+          I18n.backend.store_translations :'pt-BR', pt_br_shipped_email
+          I18n.locale = :'pt-BR'
+        end
+
+        specify do
+          shipped_email = Spree::ShipmentMailer.shipped_email(shipment)
+          shipped_email.body.should include("Caro Cliente,")
+        end
+      end
+    end
+  end
+end
