2cc68b6f43e42ea5cde8707860eeeb0fb34842d9
Rodrigo Pinto <rodrigopqn@gmail.com>
2012-08-26 10:29:28 -0300

    Make OrderMailer.cancel_email localized

--- core/spec/mailers/order_mailer_spec.rb
+++ core/spec/mailers/order_mailer_spec.rb
@@ -42,30 +42,52 @@ describe Spree::OrderMailer do
     end
   end
 
-  context "emails must be translatable by locale" do
-    context "en" do
+  context "emails must be translatable" do
+    context "en locale" do
       before do
-        en = { :order_mailer => { :confirm_email => { :dear_customer => 'Dear Customer,' } } }
-        I18n.backend.store_translations :en, en
+        en_confirm_mail = { :order_mailer => { :confirm_email => { :dear_customer => 'Dear Customer,' } } }
+        en_cancel_mail = { :order_mailer => { :cancel_email => { :order_summary_canceled => 'Order Summary [CANCELED]' } } }
+        I18n.backend.store_translations :en, en_confirm_mail
+        I18n.backend.store_translations :en, en_cancel_mail
         I18n.locale = :en
       end
 
-      specify do
-        confirmation_email = Spree::OrderMailer.confirm_email(order)
-        confirmation_email.body.should include("Dear Customer,")
+      context "confirm_email" do
+        specify do
+          confirmation_email = Spree::OrderMailer.confirm_email(order)
+          confirmation_email.body.should include("Dear Customer,")
+        end
+      end
+
+      context "cancel_email" do
+        specify do
+          cancel_email = Spree::OrderMailer.cancel_email(order)
+          cancel_email.body.should include("Order Summary [CANCELED]")
+        end
       end
     end
 
-    context "pt-BR" do
+    context "pt-BR locale" do
       before do
-        pt_br = { :order_mailer => { :confirm_email => { :dear_customer => 'Caro Cliente,' } } }
-        I18n.backend.store_translations :'pt-BR', pt_br
+        pt_br_confirm_mail = { :order_mailer => { :confirm_email => { :dear_customer => 'Caro Cliente,' } } }
+        pt_br_cancel_mail = { :order_mailer => { :cancel_email => { :order_summary_canceled => 'Resumo da Pedido [CANCELADA]' } } }
+        I18n.backend.store_translations :'pt-BR', pt_br_confirm_mail
+        I18n.backend.store_translations :'pt-BR', pt_br_cancel_mail
         I18n.locale = :'pt-BR'
       end
 
-      specify do
-        confirmation_email = Spree::OrderMailer.confirm_email(order)
-        confirmation_email.body.should include("Caro Cliente,")
+      context "confirm_email" do
+        specify do
+          confirmation_email = Spree::OrderMailer.confirm_email(order)
+          confirmation_email.body.should include("Caro Cliente,")
+        end
+      end
+
+      context "cancel_email" do
+        specify do
+          cancel_email = Spree::OrderMailer.cancel_email(order)
+          cancel_email.body.should include("Resumo da Pedido [CANCELADA]")
+        end
       end
     end
   end
