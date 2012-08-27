b622352e49bd24f1f521e2a7753370814d4c97a7
Ryan Bigg <radarlistener@gmail.com>
2012-08-27 15:49:20 +0100

    Remove unnecessary Command module wrapping from spree_cmd.rb
    Also fix my #derp for when multiple matches (i.e. [a, b], not [a || b])
    
    Fixes #1870

--- cmd/lib/spree_cmd.rb
+++ cmd/lib/spree_cmd.rb
@@ -1,20 +1,16 @@
 require 'thor'
 require 'thor/group'
 
-module SpreeCmd
-  class Command
-
-    case ARGV.first
-      when 'version' || '-v' || '--version'
-        puts Gem.loaded_specs['spree_cmd'].version
-      when 'extension'
-        ARGV.shift
-        require 'spree_cmd/extension'
-        SpreeCmd::Extension.start
-      else
-        ARGV.shift
-        require 'spree_cmd/installer'
-        SpreeCmd::Installer.start
-    end
-  end
+case ARGV.first
+  when 'version', '-v', '--version'
+    puts "outputting version"
+    puts Gem.loaded_specs['spree_cmd'].version
+  when 'extension'
+    ARGV.shift
+    require 'spree_cmd/extension'
+    SpreeCmd::Extension.start
+  else
+    ARGV.shift
+    require 'spree_cmd/installer'
+    SpreeCmd::Installer.start
 end
