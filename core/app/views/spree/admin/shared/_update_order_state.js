d1c32353fa9770cc0baaa41a59394524592acece
Ryan Bigg <radarlistener@gmail.com>
2012-08-29 17:50:03 +0100

    Convert display_total to string so that rendering the template doesn't bomb out

--- core/app/views/spree/admin/shared/_update_order_state.js
+++ core/app/views/spree/admin/shared/_update_order_state.js
@@ -1,5 +1,5 @@
 $('#order_tab_summary h5#order_status').html('<%= j t(:status) %>: <%= j t(@order.state, :scope => :order_state) %>');
-$('#order_tab_summary h5#order_total').html('<%= j t(:total) %>: <%= j @order.display_total %>');
+$('#order_tab_summary h5#order_total').html('<%= j t(:total) %>: <%= j @order.display_total.to_s %>');
 
 <% if @order.completed? %>
   $('#order_tab_summary h5#payment_status').html('<%= j t(:payment) %>: <%= j t(@order.payment_state, :scope => :payment_states, :default => [:missing, "none"]) %>');
