c83622c971ed65e7991d55093265b9c408735e77
Masahiro Saito <camelmasa@gmail.com>
2012-08-30 20:09:49 +0900

    fixed display roles bug.
    Merges #1901

--- core/app/helpers/spree/admin/users_helper.rb
+++ core/app/helpers/spree/admin/users_helper.rb
@@ -3,7 +3,7 @@ module Spree
     module UsersHelper
       def list_roles(user)
         # while testing spree-core itself user model does not have method roles
-        user.respond_to?(:roles) ? user.roles.collect { |role| role.name }.join(", ") : []
+        user.respond_to?(:spree_roles) ? user.spree_roles.collect { |role| role.name }.join(", ") : []
       end
     end
   end
