spree/spree

commit cb19a452095f728f7a23a8b500d3ad0a5504f31b

Do not need to explicitly declare I18n

core/app/controllers/spree/orders_controller.rb
@@ -63,8 +63,8 @@ module Spree
       redirect_to spree_core.cart_path
     end
 
-    def accurate_title
-      @order && @order.completed? ? "#{Order.model_name.human} #{@order.number}" : I18n.t(:shopping_cart)
+    def accurate_titles
+      @order && @order.completed? ? "#{Order.model_name.human} #{@order.number}" : t(:shopping_cart)
     end
   end
 end

auth/app/controllers/spree/base_controller_decorator.rb
@@ -18,7 +18,7 @@ Spree::BaseController.class_eval do
       respond_to do |format|
         format.html do
           if current_user
-            flash.now[:error] = I18n.t(:authorization_failure)
+            flash.now[:error] = t(:authorization_failure)
             render 'spree/shared/unauthorized', :layout => '/spree/layouts/spree_application'
           else
             store_location

auth/app/controllers/spree/user_sessions_controller.rb
@@ -51,6 +51,6 @@ class Spree::UserSessionsController < Devise::SessionsController
     end
 
     def accurate_title
-      I18n.t(:log_in)
+      t(:log_in)
     end
 end

auth/app/controllers/spree/users_controller.rb
@@ -45,6 +45,6 @@ class Spree::UsersController < Spree::BaseController
     end
 
     def accurate_title
-      I18n.t(:my_account)
+      t(:my_account)
     end
 end

core/app/controllers/spree/admin/payment_methods_controller.rb
@@ -10,7 +10,7 @@ module Spree
         invoke_callbacks(:create, :before)
         if @payment_method.save
           invoke_callbacks(:create, :after)
-          flash[:notice] = I18n.t(:successfully_created, :resource => I18n.t(:payment_method))
+          flash.notice = I18n.t(:successfully_created, :resource => I18n.t(:payment_method))
           respond_with(@payment_method, :location => edit_admin_payment_method_path(@payment_method))
         else
           invoke_callbacks(:create, :fails)
@@ -28,7 +28,7 @@ module Spree
         payment_method_params = params[@payment_method.class.name.underscore.gsub("/", "_")] || {}
         if @payment_method.update_attributes(params[:payment_method].merge(payment_method_params))
           invoke_callbacks(:update, :after)
-          flash[:notice] = I18n.t(:successfully_updated, :resource => I18n.t(:payment_method))
+          flash.notice = I18n.t(:successfully_updated, :resource => I18n.t(:payment_method))
           respond_with(@payment_method, :location => edit_admin_payment_method_path(@payment_method))
         else
           invoke_callbacks(:update, :fails)

core/app/controllers/spree/admin/payments_controller.rb
@@ -32,7 +32,7 @@ module Spree
 
           if @order.completed?
             @payment.process!
-            flash[:notice] = flash_message_for(@payment, :successfully_created)
+            flash.notice = flash_message_for(@payment, :successfully_created)
 
             respond_with(@payment) { |format| format.html { redirect_to admin_order_payments_path(@order) } }
           else

core/app/controllers/spree/admin/taxons_controller.rb
@@ -13,7 +13,7 @@ module Spree
             format.json {render :json => @taxon.to_json }
           end
         else
-          flash[:error] = I18n.t('errors.messages.could_not_create_taxon')
+          flash[:error] = t('errors.messages.could_not_create_taxon')
           respond_with(@taxon) do |format|
             format.html { redirect_to @taxonomy ? edit_admin_taxonomy_url(@taxonomy) : admin_taxonomies_url }
           end

core/app/controllers/spree/checkout_controller.rb
@@ -22,13 +22,13 @@ module Spree
         if @order.next
           state_callback(:after)
         else
-          flash[:error] = I18n.t(:payment_processing_failed)
+          flash[:error] = t(:payment_processing_failed)
           respond_with(@order, :location => checkout_state_path(@order.state))
           return
         end
 
         if @order.state == "complete" || @order.completed?
-          flash.notice = I18n.t(:order_processed_successfully)
+          flash.notice = t(:order_processed_successfully)
           flash[:commerce_tracking] = "nothing special"
           respond_with(@order, :location => completion_route)
         else

commit 4be71cbea1c05964524174a309fb1443a17ffda7

corrected a typo in accurate_title method that was introduced in commit cb19a45209

core/app/controllers/spree/orders_controller.rb
@@ -64,7 +64,7 @@ module Spree
       redirect_to spree.cart_path
     end
 
-    def accurate_titles
+    def accurate_title
       @order && @order.completed? ? "#{Order.model_name.human} #{@order.number}" : t(:shopping_cart)
     end
   end