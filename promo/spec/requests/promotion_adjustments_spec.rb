a120a2d83c4e28974364ddbb9367a942535ffa82
Michael Bianco <iloveitaly@gmail.com>
2012-09-13 10:34:20 -0400

    Removing duplicate create_per_product_promotion in promo spec
    [Fixes #1961]

--- promo/spec/requests/promotion_adjustments_spec.rb
+++ promo/spec/requests/promotion_adjustments_spec.rb
@@ -477,34 +477,5 @@ describe "Promotion Adjustments" do
       fill_in "card_code", :with => "123"
       click_button "Save and Continue"
     end
-
-    def create_per_product_promotion product_name, discount_amount
-      visit spree.admin_path
-      click_link "Promotions"
-      click_link "New Promotion"
-      fill_in "Name", :with => "Bundle"
-      select "Add to cart", :from => "Event"
-      click_button "Create"
-      page.should have_content("Editing Promotion")
-
-      # add product_name to last promotion
-      promotion = Spree::Promotion.last
-      promotion.rules << Spree::Promotion::Rules::Product.new()
-      product = Spree::Product.find_by_name(product_name)
-      rule = promotion.rules.last
-      rule.products << product
-      if rule.save
-        puts "Created promotion: new price for #{product_name} is #{product.price - discount_amount} (was #{product.price})"
-      else
-        puts "Failed to create promotion: price for #{product_name} is still #{product.price}"
-      end
-
-      select "Create adjustment", :from => "Add action of type"
-      within('#action_fields') { click_button "Add" }
-      select "Flat Rate (per item)", :from => "Calculator"
-      within('#actions_container') { click_button "Update" }
-      within('.calculator-fields') { fill_in "Amount", :with => discount_amount.to_s }
-      within('#actions_container') { click_button "Update" }
-    end
   end
 end
