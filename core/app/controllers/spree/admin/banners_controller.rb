f9d0dd010455ca88006213d9307c0f6be00825d1
Ryan Bigg <radarlistener@gmail.com>
2012-09-13 15:20:05 +0200

    Fix payment methods banner non-dismissal issue #1924
    Related to #1952
    
    Conflicts:
    
    	core/app/views/spree/admin/banners/_gateway.html.erb

--- core/app/controllers/spree/admin/banners_controller.rb
+++ core/app/controllers/spree/admin/banners_controller.rb
@@ -2,12 +2,12 @@ module Spree
   module Admin
     class BannersController < Spree::Admin::BaseController
       def dismiss
-        if request.xhr? and params[:id]
+        if params[:id]
           if user = try_spree_current_user
             user.dismiss_banner(params[:id])
           end
-          render :nothing => true
         end
+        render :nothing => true
       end
     end
   end
