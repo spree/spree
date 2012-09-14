cd5a8ec2b146614c2fb360cdf4fa59c830758a7f
Alberto Vena <kennyadsl@gmail.com>
2012-09-14 16:11:45 +0200

    add request spec for remove from cart link, refers to #1934
    [Fixes #1965]

--- core/spec/requests/cart_spec.rb
+++ core/spec/requests/cart_spec.rb
@@ -10,4 +10,19 @@ describe "Cart" do
     visit spree.cart_path
     lambda { find("li#link-to-cart a") }.should raise_error(Capybara::ElementNotFound)
   end
+
+  it "prevents double clicking the remove button on cart", :js => true do
+    @product = create(:product, :name => "RoR Mug", :on_hand => 1)
+
+    visit spree.root_path
+    click_link "RoR Mug"
+    click_button "add-to-cart-button"
+
+    # prevent form submit to verify button is disabled
+    page.execute_script("$('#update-cart').submit(function(){return false;})")
+
+    page.should_not have_selector('button#update-button[disabled]')
+    page.find(:css, '.delete img').click
+    page.should have_selector('button#update-button[disabled]')
+  end
 end
