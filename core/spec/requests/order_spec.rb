32e306c6b9b909c59da4914c0827e778cd7ef7f6
Alberto Vena <kennyadsl@gmail.com>
2012-09-19 01:24:40 +0200

    Add payment method name in order details page
    if not paid with credit card

--- core/spec/requests/order_spec.rb
+++ core/spec/requests/order_spec.rb
@@ -2,9 +2,26 @@ require 'spec_helper'
 
 describe 'orders' do
   let(:order) { create(:order, :shipping_method => create(:shipping_method)) }
+  let(:completed_order) { create(:completed_order_with_totals, :shipping_method => create(:shipping_method)) }
 
   it "can visit an order" do
     # Regression test for current_user call on orders/show
     lambda { visit spree.order_path(order) }.should_not raise_error
+  end
+
+  it "should have credit card info if paid with credit card" do
+    create(:payment, :order => completed_order)
+    visit spree.order_path(completed_order)
+    within '.payment-info' do
+      page.should have_content "Ending in 1111"
+    end
+  end
+
+  it "should have payment method name visible if not paid with credit card" do
+    create(:check_payment, :order => completed_order)
+    visit spree.order_path(completed_order)
+    within '.payment-info' do
+      page.should have_content "Check"
+    end
   end
 end
