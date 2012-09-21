7e9e06be1704cadfea86e9aa7262b2fdae3a7055
Laurens Nienhaus <l.nienhaus@gmail.com>
2012-09-21 13:47:26 +0200

    Cleanup scopes_spec

--- core/spec/models/product/scopes_spec.rb
+++ core/spec/models/product/scopes_spec.rb
@@ -8,18 +8,16 @@ describe "Product scopes" do
       @taxonomy = create(:taxonomy)
       @root_taxon = @taxonomy.root
       
-      @taxon = create(:taxon, :name => 'A1', :taxonomy_id => @taxonomy.id)
-      @taxon2 = create(:taxon, :name => 'A2', :taxonomy_id => @taxonomy.id)
+      @parent_taxon = create(:taxon, name: 'Parent', :taxonomy_id => @taxonomy.id, :parent => @root_taxon)
+      @child_taxon = create(:taxon, name: 'Child 1', :taxonomy_id => @taxonomy.id, :parent => @parent_taxon)
+      @parent_taxon.reload # Need to reload for descendents to show up
       
-      @taxon.move_to_child_of(@root_taxon)
-      @taxon2.move_to_child_of(@taxon)
-      
-      product.taxons << @taxon
-      product.taxons << @taxon2
+      product.taxons << @parent_taxon
+      product.taxons << @child_taxon
     end
     
     it "calling Product.in_taxon should not return duplicate records" do
-      Spree::Product.in_taxon(@taxon).length.should == 1
+      Spree::Product.in_taxon(@parent_taxon).length.should == 1
     end
   end
 end
