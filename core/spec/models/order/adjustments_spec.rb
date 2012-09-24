268404ecc6081287a158a23ba2a93cac11ea43d6
Ryan Bigg <radarlistener@gmail.com>
2012-09-24 16:36:50 +1000

    Remove ship_total and tax_total tests out to adjustment_spec

--- core/spec/models/order/adjustments_spec.rb
+++ core/spec/models/order/adjustments_spec.rb
@@ -20,6 +20,26 @@ describe Spree::Order do
     end
   end
 
+  context "totaling adjustments" do
+    let(:adjustment1) { mock_model(Spree::Adjustment, :amount => 5) }
+    let(:adjustment2) { mock_model(Spree::Adjustment, :amount => 10) }
+
+    context "#ship_total" do
+      it "should return the correct amount" do
+        order.stub_chain :adjustments, :shipping => [adjustment1, adjustment2]
+        order.ship_total.should == 15
+      end
+    end
+
+    context "#tax_total" do
+      it "should return the correct amount" do
+        order.stub_chain :adjustments, :tax => [adjustment1, adjustment2]
+        order.tax_total.should == 15
+      end
+    end
+  end
+  
+
   context "#price_adjustment_totals" do
     before { @order = Spree::Order.create! }
 
