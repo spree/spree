6ce09cf6492e927b82b7db0f79acc32da372a855
Ryan Bigg <radarlistener@gmail.com>
2012-09-26 13:58:18 +1000

    Fix stack level too deep error on Ruby 1.8 builds
    This was caused by gateway_options overriden method (in promo/app/models/payment_decorator.rb) calling itself again and again. The work around is kind of a shim just so that 1.8 works. We should revert it to how it was once we drop support for Ruby 1.8.
    
    Fixes an issue related to #1956

--- promo/app/models/spree/payment_decorator.rb
+++ promo/app/models/spree/payment_decorator.rb
@@ -1,7 +1,5 @@
 Spree::Payment.class_eval do
-  alias_method :old_gateway_options, :gateway_options
-  def gateway_options
-    options = old_gateway_options
-    options.merge!({ :discount => order.promo_total * 100 })
+  def promo_total
+    order.promo_total * 100
   end
 end
