5da297d330fdaa28ece62d869f87fd7c7a831d6e
Ryan Bigg <radarlistener@gmail.com>
2012-10-01 10:42:53 +1000

    Fix admin/general_settings spec

--- core/spec/requests/admin/configuration/general_settings_spec.rb
+++ core/spec/requests/admin/configuration/general_settings_spec.rb
@@ -10,23 +10,20 @@ describe "General Settings" do
   end
 
   context "visiting general settings (admin)" do
-    it "should be have the right content" do
+    it "should have the right content" do
       page.should have_content("General Settings")
-      page.should have_content("Site Name")
-      page.should have_content("Site URL")
-      page.should have_content("Spree demo site")
-      page.should have_content("demo.spreecommerce.com")
+      find("#site_name").value.should == "Spree Demo Site"
+      find("#site_url").value.should == "demo.spreecommerce.com"
     end
   end
 
   context "editing general settings (admin)" do
     it "should be able to update the site name" do
-      click_link "admin_general_settings_link"
-      page.should have_content("Edit General Settings")
       fill_in "site_name", :with => "Spree Demo Site99"
       click_button "Update"
 
-      page.should have_content("Spree Demo Site99")
+      page.should have_content(I18n.t(:general_settings_updated))
+      find("#site_name").value.should == "Spree Demo Site99"
     end
   end
 end
