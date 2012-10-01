c6dcf2d7593490197ca8c85e50d042dcec7d96ca
kei <kei.shiratsuchi@gmail.com>
2012-10-01 20:19:50 +0900

    Manage currency whose subunit is not 100 (like JPY)
    [Fixes #2030]

--- core/lib/spree/money.rb
+++ core/lib/spree/money.rb
@@ -3,7 +3,7 @@ require 'money'
 module Spree
   class Money
     def initialize(amount, options={})
-      @money = ::Money.new(amount * 100, Spree::Config[:currency])
+      @money = ::Money.parse([amount, Spree::Config[:currency]].join)
       @options = {}
       @options[:with_currency] = true if Spree::Config[:display_currency]
       @options[:symbol_position] = Spree::Config[:currency_symbol_position].to_sym
