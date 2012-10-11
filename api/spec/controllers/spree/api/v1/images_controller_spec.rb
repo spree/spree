538d87ae7c3f322ef148174d87a8d78a1fff37df
Ryan Bigg <radarlistener@gmail.com>
2012-10-11 11:57:50 +1100

    [api] only admins should be allowed to create, edit or delete images

--- api/spec/controllers/spree/api/v1/images_controller_spec.rb
+++ api/spec/controllers/spree/api/v1/images_controller_spec.rb
@@ -13,32 +13,53 @@ module Spree
       stub_authentication!
     end
 
-    it "can upload a new image for a variant" do
-      lambda do
-        api_post :create,
-                 :image => { :attachment => upload_image('thinking-cat.jpg'),
-                             :viewable_type => 'Spree::Variant',
-                             :viewable_id => product.master.to_param  }
-        response.status.should == 201
-        json_response.should have_attributes(attributes)
-      end.should change(Image, :count).by(1)
-    end
+    context "as an admin" do
+      sign_in_as_admin!
 
-    context "working with an existing image" do
-      let!(:product_image) { product.master.images.create!(:attachment => image('thinking-cat.jpg')) }
-
-      it "can update image data" do
-        product_image.position.should == 1
-        api_post :update, :image => { :position => 2 }, :id => product_image.id
-        response.status.should == 200
-        json_response.should have_attributes(attributes)
-        product_image.reload.position.should == 2
+      it "can upload a new image for a variant" do
+        lambda do
+          api_post :create,
+                   :image => { :attachment => upload_image('thinking-cat.jpg'),
+                               :viewable_type => 'Spree::Variant',
+                               :viewable_id => product.master.to_param  }
+          response.status.should == 201
+          json_response.should have_attributes(attributes)
+        end.should change(Image, :count).by(1)
       end
 
-      it "can delete an image" do
-        api_delete :destroy, :id => product_image.id
-        response.status.should == 200
-        lambda { product_image.reload }.should raise_error(ActiveRecord::RecordNotFound)
+      context "working with an existing image" do
+        let!(:product_image) { product.master.images.create!(:attachment => image('thinking-cat.jpg')) }
+
+        it "can update image data" do
+          product_image.position.should == 1
+          api_post :update, :image => { :position => 2 }, :id => product_image.id
+          response.status.should == 200
+          json_response.should have_attributes(attributes)
+          product_image.reload.position.should == 2
+        end
+
+        it "can delete an image" do
+          api_delete :destroy, :id => product_image.id
+          response.status.should == 200
+          lambda { product_image.reload }.should raise_error(ActiveRecord::RecordNotFound)
+        end
+      end
+    end
+
+    context "as a non-admin" do
+      it "cannot create an image" do
+        api_post :create
+        assert_unauthorized!
+      end
+
+      it "cannot update an image" do
+        api_put :update, :id => 1
+        assert_unauthorized!
+      end
+
+      it "cannot delete an image" do
+        api_delete :destroy, :id => 1
+        assert_unauthorized!
       end
     end
   end
