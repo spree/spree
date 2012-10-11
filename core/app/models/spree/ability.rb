7c8485a17546376c9067a91363dfdbaa9b02ca72
Ryan Bigg <radarlistener@gmail.com>
2012-10-11 11:51:38 +1100

    Ensure that address information cannot be updated by people who do not own it

--- core/app/models/spree/ability.rb
+++ core/app/models/spree/ability.rb
@@ -51,6 +51,11 @@ module Spree
           order.user == user || order.token && token == order.token
         end
         can :create, Order
+
+        can :read, Address do |address|
+          address.user == user
+        end
+
         #############################
         can :read, Product
         can :index, Product
