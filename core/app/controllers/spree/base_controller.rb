8eff78f1acc85194ce9e5289a8bf9159da447fdf
Ryan Bigg <radarlistener@gmail.com>
2012-10-12 12:37:17 +1100

    WIP: Splitting Core::ControllerHelpers up into modules
    Fixes #2079

--- core/app/controllers/spree/base_controller.rb
+++ core/app/controllers/spree/base_controller.rb
@@ -1,5 +1,8 @@
 require 'cancan'
 
 class Spree::BaseController < ApplicationController
-  include Spree::Core::ControllerHelpers
+  include Spree::Core::ControllerHelpers::Auth
+  include Spree::Core::ControllerHelpers::RespondWith
+  include Spree::Core::ControllerHelpers::Common
+
 end
--- core/app/controllers/spree/states_controller.rb
+++ core/app/controllers/spree/states_controller.rb
@@ -1,5 +1,5 @@
 module Spree
-  class StatesController < BaseController
+  class StatesController < Spree::StoreController
     ssl_allowed :index
 
     respond_to :js
