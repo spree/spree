161e456cc51065a20d231dd86b99cb1b01ebb42e
Ryan Bigg <radarlistener@gmail.com>
2012-10-17 08:50:35 +1100

    Remove require for spree/core/respond_with
    this is now a controller helper and will be autoloaded as necessary

--- core/lib/spree/core.rb
+++ core/lib/spree/core.rb
@@ -77,7 +77,6 @@ require 'spree/core/ext/active_record'
 require 'spree/core/delegate_belongs_to'
 
 require 'spree/core/responder'
-require 'spree/core/respond_with'
 require 'spree/core/ssl_requirement'
 require 'spree/core/store_helpers'
 require 'spree/core/calculated_adjustments'
