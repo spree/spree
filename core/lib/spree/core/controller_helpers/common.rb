48ae53ec9b2ded889bf7523d1fdc83080c57e735
Ryan Bigg <radarlistener@gmail.com>
2012-10-17 11:27:07 +1100

    Attempt to_sym on locale inside set_user_language
    Fixes broken build: https://travis-ci.org/#!/spree/spree/jobs/2818950

--- core/lib/spree/core/controller_helpers/common.rb
+++ core/lib/spree/core/controller_helpers/common.rb
@@ -54,7 +54,7 @@ module Spree
         def set_user_language
           locale = session[:locale]
           locale ||= Rails.application.config.i18n.default_locale
-          locale ||= I18n.default_locale unless I18n.available_locales.include?(locale.to_sym)
+          locale ||= I18n.default_locale unless I18n.available_locales.include?(locale.try(:to_sym))
           I18n.locale = locale.to_sym
         end
 
