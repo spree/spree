1f91e96fc69ae0f369829c75f607d5498b7a1a8e
Ryan Bigg <radarlistener@gmail.com>
2012-10-17 13:48:20 +1100

    Don't clear mail_method or payment_method password if it's not sent through with params
    Fixes #2094

---
+++ core/spec/controllers/spree/admin/payment_methods_controller_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+module Spree
+  class GatewayWithPassword < PaymentMethod
+    attr_accessible :preferred_password
+
+    preference :password, :string, :default => "password"
+  end
+end
+
+module Spree
+  describe Admin::PaymentMethodsController do
+    stub_authorization!
+
+    let(:payment_method) { Spree::GatewayWithPassword.create!(:name => "Bogus", :preferred_password => "haxme") }
+
+    # regression test for #2094
+    it "does not clear password on update" do
+      payment_method.preferred_password.should == "haxme"
+      spree_put :update, :id => payment_method.id, :payment_method => { :type => payment_method.class.to_s, :preferred_password => "" } 
+      response.should redirect_to(spree.edit_admin_payment_method_path(payment_method))
+
+      payment_method.reload
+      payment_method.preferred_password.should == "haxme"
+    end
+
+  end
+end
