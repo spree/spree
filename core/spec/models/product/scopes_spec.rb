8a303e193b3b9ca6caf3c7d6acec8ff1a8e80f35
Ryan Bigg <radarlistener@gmail.com>
2012-10-18 16:27:57 +1100

    Fix multiple products from appearing when using Product.in_taxons scope
    Fixes #1917
    
    Fixes #1974
    
    Fixes #1962

--- core/spec/models/product/scopes_spec.rb
+++ core/spec/models/product/scopes_spec.rb
@@ -2,22 +2,22 @@ require 'spec_helper'
 
 describe "Product scopes" do
   let!(:product) { create(:product) }
-  
+
   context "A product assigned to parent and child taxons" do
     before do
       @taxonomy = create(:taxonomy)
       @root_taxon = @taxonomy.root
-      
+
       @parent_taxon = create(:taxon, name: 'Parent', :taxonomy_id => @taxonomy.id, :parent => @root_taxon)
       @child_taxon = create(:taxon, name: 'Child 1', :taxonomy_id => @taxonomy.id, :parent => @parent_taxon)
       @parent_taxon.reload # Need to reload for descendents to show up
-      
+
       product.taxons << @parent_taxon
       product.taxons << @child_taxon
     end
-    
+
     it "calling Product.in_taxon should not return duplicate records" do
-      Spree::Product.in_taxon(@parent_taxon).length.should == 1
+      Spree::Product.in_taxon(@parent_taxon).to_a.should == 1
     end
   end
 end
