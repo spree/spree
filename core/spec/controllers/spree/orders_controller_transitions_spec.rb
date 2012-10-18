113336f1bcae2558b021d5895ffc4d99b8a1f44e
Ryan Bigg <radarlistener@gmail.com>
2012-10-18 15:50:42 +1100

    Add regression test for #2004

---
+++ core/spec/controllers/spree/orders_controller_transitions_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+Spree::Order.class_eval do
+  attr_accessor :did_transition
+end
+
+module Spree
+  describe OrdersController do
+    # Regression test for #2004
+    context "with a transition callback on first state" do
+      let(:order) { Spree::Order.new }
+
+      before do
+        controller.stub :current_order => order
+
+        first_state, _ = Spree::Order.checkout_steps.first
+        Spree::Order.state_machine.after_transition :to => first_state do |order|
+          order.did_transition = true
+        end
+      end
+
+      it "correctly calls the transition callback" do
+        order.did_transition.should be_nil
+        spree_put :update, { :checkout => "checkout" }, { :order_id => 1}
+        order.did_transition.should be_true
+      end
+    end
+  end
+end
