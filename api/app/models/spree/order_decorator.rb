5ab4dce812c4d34aa01ba7039b32c2e642644167
Ryan Bigg <radarlistener@gmail.com>
2012-10-25 15:00:02 +1100

    [api] allow build_from_api to accept empty line_item_attributes

--- api/app/models/spree/order_decorator.rb
+++ api/app/models/spree/order_decorator.rb
@@ -1,6 +1,7 @@
 Spree::Order.class_eval do
   def self.build_from_api(user, params)
     order = create
+    params[:line_items_attributes] ||= []
     params[:line_items_attributes].each do |line_item|
       order.add_variant(Spree::Variant.find(line_item[:variant_id]), line_item[:quantity])
     end
