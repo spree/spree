8ab59e04d1842945a3e12948c0a40a98663e31af
Ryan Bigg <radarlistener@gmail.com>
2012-10-30 07:51:17 +1100

    OrdersController#update needs respond with to determine order path

--- core/app/controllers/spree/orders_controller.rb
+++ core/app/controllers/spree/orders_controller.rb
@@ -3,6 +3,8 @@ module Spree
     rescue_from ActiveRecord::RecordNotFound, :with => :render_404
     helper 'spree/products'
 
+    respond_to :html
+
     def show
       @order = Order.find_by_number!(params[:id])
     end
@@ -12,12 +14,18 @@ module Spree
       if @order.update_attributes(params[:order])
         @order.line_items = @order.line_items.select {|li| li.quantity > 0 }
         fire_event('spree.order.contents_changed')
-        if params.has_key?(:checkout)
-          @order.next_transition.run_callbacks
-          redirect_to checkout_state_path(@order.checkout_steps.first)
-        else
-          redirect_to cart_path
+        respond_with(@order) do |format|
+          format.html do
+            if params.has_key?(:checkout)
+              @order.next_transition.run_callbacks
+              redirect_to checkout_state_path(@order.checkout_steps.first)
+            else
+              redirect_to cart_path
+            end
+          end
         end
+      else
+        respond_with(@order)
       end
     end
 
