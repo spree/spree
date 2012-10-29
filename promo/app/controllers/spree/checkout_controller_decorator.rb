f31c80cd8160bcb81947894bb9c933737df9150f
Ryan Bigg <radarlistener@gmail.com>
2012-10-30 09:21:31 +1100

    [promo] Remove excessive respond_with usage in CheckoutControllerDecorator

--- promo/app/controllers/spree/checkout_controller_decorator.rb
+++ promo/app/controllers/spree/checkout_controller_decorator.rb
@@ -22,19 +22,19 @@ Spree::CheckoutController.class_eval do
         state_callback(:after)
       else
         flash[:error] = t(:payment_processing_failed)
-        respond_with(@order, :location => checkout_state_path(@order.state))
+        redirect_to checkout_state_path(@order.state)
         return
       end
 
       if @order.state == 'complete' || @order.completed?
         flash.notice = t(:order_processed_successfully)
         flash[:commerce_tracking] = 'nothing special'
-        respond_with(@order, :location => completion_route)
+        redirect_to completion_route
       else
-        respond_with(@order, :location => checkout_state_path(@order.state))
+        redirect_to checkout_state_path(@order.state)
       end
     else
-      respond_with(@order) { |format| format.html { render :edit } }
+      render :edit
     end
   end
 
