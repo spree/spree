fe1b020a111a4233842b98d715f1ae535fdca0a0
Ryan Bigg <radarlistener@gmail.com>
2012-11-02 10:41:08 +1100

    Tidy up taxonomy.js

--- frontend/app/assets/javascripts/admin/taxonomy.js
+++ frontend/app/assets/javascripts/admin/taxonomy.js
@@ -125,65 +125,65 @@ $(document).ready(function(){
               }
             }
           },
-          "contextmenu" : {
-             "items" : function(obj) {
+          contextmenu: {
+             items: function(obj) {
                 var id_of_node = obj.attr("id");
                 var type_of_node = obj.attr("rel");
                 var menu = {};
                 if(type_of_node == "root") {
                   menu = {
-                    "create" : {
-                      "label"            : "<i class='icon-plus'></i> " + Spree.translations.add,
-                      "action"           : function (obj) { this.create(obj); }
+                    create: {
+                      label: "<i class='icon-plus'></i> " + Spree.translations.add,
+                      action: function (obj) { this.create(obj); }
                     },
-                     "paste" : {
-                       "separator_before" : true,
-                       "label"            : "<i class='icon-paste'></i> " + Spree.translations.paste,
-                       "action"           : function (obj) { is_cut = false; this.paste(obj); },
-                       "_disabled"        : is_cut == false
+                     paste: {
+                       separator_before: true,
+                       label: "<i class='icon-paste'></i> " + Spree.translations.paste,
+                       action: function (obj) { is_cut = false; this.paste(obj); },
+                       _disabled: is_cut == false
                     },
-                    "edit" : {
-                      "separator_before" : true,
-                      "label"            : "<i class='icon-edit'></i> " + Spree.translations.edit,
-                      "action"           : function (obj) { window.location = base_url + obj.attr("id") + "/edit/"; }
+                    edit : {
+                      separator_before: true,
+                      label: "<i class='icon-edit'></i> " + Spree.translations.edit,
+                      action           : function (obj) { window.location = base_url + obj.attr("id") + "/edit/"; }
                     }
                   }
                 } else {
                   menu =  {
-                    "create" : {
-                      "label"            : "<i class='icon-plus'></i> " + Spree.translations.add,
-                      "action"           : function (obj) { this.create(obj); }
+                    create: {
+                      label: "<i class='icon-plus'></i> " + Spree.translations.add,
+                      action: function (obj) { this.create(obj); }
                     },
-                    "rename" : {
-                      "label"            : "<i class='icon-pencil'></i> " + Spree.translations.rename,
-                      "action"           : function (obj) { this.rename(obj); }
+                    rename: {
+                      label: "<i class='icon-pencil'></i> " + Spree.translations.rename,
+                      action: function (obj) { this.rename(obj); }
                     },
-                    "remove" : {
-                      "label"            : "<i class='icon-trash'></i> " + Spree.translations.remove,
-                      "action"           : function (obj) { this.remove(obj); }
+                    remove: {
+                      label: "<i class='icon-trash'></i> " + Spree.translations.remove,
+                      action: function (obj) { this.remove(obj); }
                     },
-                    "cut" : {
-                      "separator_before" : true,
-                      "label"            : "<i class='icon-cut'></i> " + Spree.translations.cut,
-                      "action"           : function (obj) { is_cut = true; this.cut(obj); }
+                    cut: {
+                      separator_before : true,
+                      label: "<i class='icon-cut'></i> " + Spree.translations.cut,
+                      action: function (obj) { is_cut = true; this.cut(obj); }
                     },
-                    "paste" : {
-                      "label"            : "<i class='icon-paste'></i> " + Spree.translations.paste,
-                      "action"           : function (obj) { is_cut = false; this.paste(obj); },
-                      "_disabled"        : is_cut == false
+                    paste: {
+                      label: "<i class='icon-paste'></i> " + Spree.translations.paste,
+                      action: function (obj) { is_cut = false; this.paste(obj); },
+                      "_disabled": is_cut == false
                     },
-                    "edit" : {
-                      "separator_before" : true,
-                      "label"            : "<i class='icon-edit'></i> " + Spree.translations.edit,
-                      "action"           : function (obj) { window.location = base_url + obj.attr("id") + "/edit/"; }
+                    edit: {
+                      separator_before: true,
+                      label: "<i class='icon-edit'></i> " + Spree.translations.edit,
+                      action: function (obj) { window.location = base_url + obj.attr("id") + "/edit/"; }
                     }
-                        }
+                  }
                 }
                 return menu;
             }
           },
 
-          "plugins" : [ "themes", "json_data", "dnd", "crrm", "contextmenu"]
+          plugins: [ "themes", "json_data", "dnd", "crrm", "contextmenu"]
         }
 
         $("#taxonomy_tree").jstree(conf)
