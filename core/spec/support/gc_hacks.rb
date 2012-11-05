19ec499fb1c7008f76d0c9fc43536c582f570de4
Ryan Bigg <radarlistener@gmail.com>
2012-11-05 11:53:53 +1100

    no need for 1.9.2 statement inside gc_hacks anymore

--- core/spec/support/gc_hacks.rb
+++ core/spec/support/gc_hacks.rb
@@ -2,14 +2,12 @@
 counter = -1
 RSpec.configure do |config|
   config.after(:each) do
-    unless RUBY_VERSION =~ /1\.9\.2/
-      counter += 1
-      if counter > 9
-        GC.enable
-        GC.start
-        GC.disable
-        counter = 0
-      end
+    counter += 1
+    if counter > 9
+      GC.enable
+      GC.start
+      GC.disable
+      counter = 0
     end
   end
 
