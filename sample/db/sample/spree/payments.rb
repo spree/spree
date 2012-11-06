11586897c86821c7e9b6cf5d688013af63f47120
Ryan Bigg <radarlistener@gmail.com>
2012-11-07 09:52:30 +1100

    Explicitly set Spree::CreditCard table name within spree/payments.rb
    Fixes #2130

--- sample/db/sample/spree/payments.rb
+++ sample/db/sample/spree/payments.rb
@@ -8,6 +8,10 @@ Spree::Gateway.class_eval do
   end
 end
 
+# This table was previously called spree_creditcards, and older migrations
+# reference it as such. Make it explicit here that this table has been renamed.
+Spree::CreditCard.table_name = 'spree_credit_cards'
+
 creditcard = Spree::CreditCard.create({ :cc_type => 'visa', :month => 12, :year => 2014, :last_digits => '1111',
                                         :first_name => 'Sean', :last_name => 'Schofield',
                                         :gateway_customer_profile_id => 'BGS-1234' }, :without_protection => true)
