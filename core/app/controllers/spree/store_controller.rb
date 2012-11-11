3627423b2168775a6ce095814141c524d813aab0
Ryan Bigg <radarlistener@gmail.com>
2012-11-12 10:05:39 +1100

    Move fire_event and default_notification_payload methods out from Spree::StoreController and into ControllerHelpers::Common
    This is because spree_auth_devise requires them for its registrations controller

--- core/app/controllers/spree/store_controller.rb
+++ core/app/controllers/spree/store_controller.rb
@@ -6,20 +6,6 @@ module Spree
       render 'spree/shared/unauthorized', :layout => Spree::Config[:layout], :status => 401
     end
 
-    private
-
-    # Convenience method for firing instrumentation events with the default payload hash
-    def fire_event(name, extra_payload = {})
-      ActiveSupport::Notifications.instrument(name, default_notification_payload.merge(extra_payload))
-    end
-
-    # Creates the hash that is sent as the payload for all notifications. Specific notifications will
-    # add additional keys as appropriate. Override this method if you need additional data when
-    # responding to a notification
-    def default_notification_payload
-      {:user => try_spree_current_user, :order => current_order}
-    end
-
   end
 end
 
