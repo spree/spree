93277f3bf7b4717ec892d1d6069e771f5b959909
Ryan Bigg <radarlistener@gmail.com>
2012-11-14 09:50:33 +1100

    [frontend] use correct flash messages in Admin::ProductsController

--- frontend/app/controllers/spree/admin/products_controller.rb
+++ frontend/app/controllers/spree/admin/products_controller.rb
@@ -26,7 +26,7 @@ module Spree
         @product = Product.where(:permalink => params[:id]).first!
         @product.delete
 
-        flash.notice = I18n.t('notice_messages.product_deleted')
+        flash.success = I18n.t('notice_messages.product_deleted')
 
         respond_with(@product) do |format|
           format.html { redirect_to collection_url }
@@ -38,9 +38,9 @@ module Spree
         @new = @product.duplicate
 
         if @new.save
-          flash.notice = I18n.t('notice_messages.product_cloned')
+          flash.success = I18n.t('notice_messages.product_cloned')
         else
-          flash.notice = I18n.t('notice_messages.product_not_cloned')
+          flash.success = I18n.t('notice_messages.product_not_cloned')
         end
 
         redirect_to edit_admin_product_url(@new)
