1c2f88cb5492b12fa749686faa6d650af87d3887
Ryan Bigg <radarlistener@gmail.com>
2012-11-14 09:49:14 +1100

    [frontend] Remove useless comments in Admin::OverviewController

--- frontend/app/controllers/spree/admin/overview_controller.rb
+++ frontend/app/controllers/spree/admin/overview_controller.rb
@@ -6,10 +6,6 @@ module Spree
 
       def index
         @users = User.all
-        #@users = User.find_with_deleted(:all, :order => 'updated_at desc')
-    #  going to list today's orders, yesterday's orders, older orders
-    # have a filter / search at the top
-        # @orders, @
       end
 
     end
