17d6fb02dd8b38b2deefdddd90dc15209bcd9981
Ryan Bigg <radarlistener@gmail.com>
2012-11-14 09:57:29 +1100

    Re-add RespondWith module

--- frontend/app/controllers/spree/base_controller.rb
+++ frontend/app/controllers/spree/base_controller.rb
@@ -4,5 +4,4 @@ class Spree::BaseController < ApplicationController
   include Spree::Core::ControllerHelpers::Auth
   include Spree::Core::ControllerHelpers::RespondWith
   include Spree::Core::ControllerHelpers::Common
-
 end
