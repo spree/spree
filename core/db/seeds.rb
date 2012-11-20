51600626b67d1f2911d969dbb876e3d8ceec2d27
Ryan Bigg <radarlistener@gmail.com>
2012-11-21 10:10:44 +1100

    Re-add core/db/seeds.rb

---
+++ core/db/seeds.rb
@@ -0,0 +1,5 @@
+# Loads seed data out of default dir
+default_path = File.join(File.dirname(__FILE__), 'default')
+
+Rake::Task['db:load_dir'].reenable
+Rake::Task['db:load_dir'].invoke(default_path)
