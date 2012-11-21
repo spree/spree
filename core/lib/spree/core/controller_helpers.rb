0e8bbbbae79a30a14bdcc45725c32b25ec2ad013
Ryan Bigg <radarlistener@gmail.com>
2012-11-21 14:04:38 +1100

    Add Spree::Core::ControllerHelpers which can be included to include all its submodules

--- core/lib/spree/core/controller_helpers.rb
+++ core/lib/spree/core/controller_helpers.rb
@@ -1,10 +1,11 @@
 module Spree
   module Core
     module ControllerHelpers
-      def self.included(base)
-        base.class_eval do
+      def self.included(klass)
+        klass.class_eval do
           include Spree::Core::ControllerHelpers::Common
           include Spree::Core::ControllerHelpers::Auth
+          include Spree::Core::ControllerHelpers::RespondWith
           include Spree::Core::ControllerHelpers::Order
         end
       end
