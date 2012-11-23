82469b2b4afe5743bd044f269b932b8eae0c7739
Gregor MacDougall <gregor@freerunningtechnologies.com>
2012-11-23 11:09:56 -0800

    Explicitly retrieve price in current_currency.
    This will display prices at each step in the currency currently
    specified for the user.

--- core/app/models/spree/product.rb
+++ core/app/models/spree/product.rb
@@ -46,7 +46,9 @@ module Spree
 
     has_many :variants_including_master_and_deleted, :class_name => 'Spree::Variant'
 
-    delegate_belongs_to :master, :sku, :price, :currency, :display_amount, :display_price, :weight, :height, :width, :depth, :is_master, :has_default_price?, :cost_currency
+    has_many :prices, :through => :variants, :order => 'spree_variants.position, spree_variants.id, currency'
+
+    delegate_belongs_to :master, :sku, :price, :currency, :display_amount, :display_price, :weight, :height, :width, :depth, :is_master, :has_default_price?, :cost_currency, :price_in
     delegate_belongs_to :master, :cost_price if Variant.table_exists? && Variant.column_names.include?('cost_price')
 
     after_create :set_master_variant_defaults
