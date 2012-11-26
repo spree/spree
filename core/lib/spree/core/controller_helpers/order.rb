21cdeb7b56a508be6a22e5f3a1352573b83cf712
Ryan Bigg <radarlistener@gmail.com>
2012-11-26 15:56:53 +1100

    WIP: tracking IP addresses for order

--- core/lib/spree/core/controller_helpers/order.rb
+++ core/lib/spree/core/controller_helpers/order.rb
@@ -24,6 +24,7 @@ module Spree
           return @current_order if @current_order
           if session[:order_id]
             current_order = Spree::Order.find_by_id_and_currency(session[:order_id], current_currency, :include => :adjustments)
+            current_order.last_ip_address = ip_address
             @current_order = current_order unless current_order.try(:completed?)
           end
           if create_order_if_necessary and (@current_order.nil? or @current_order.completed?)
