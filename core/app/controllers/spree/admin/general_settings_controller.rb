eb8ae7da8201c17793c6c9de99dfeeba8baaa393
Dries Steenhouwer <steenhouwer.dries@gmail.com>
2012-11-27 10:58:49 +0100

    Add option to hide cents in price display

--- core/app/controllers/spree/admin/general_settings_controller.rb
+++ core/app/controllers/spree/admin/general_settings_controller.rb
@@ -8,7 +8,7 @@ module Spree
         @preferences_security = [:allow_ssl_in_production,
                         :allow_ssl_in_staging, :allow_ssl_in_development_and_test,
                         :check_for_spree_alerts]
-        @preferences_currency = [:display_currency]
+        @preferences_currency = [:display_currency, :hide_cents]
       end
 
       def update
