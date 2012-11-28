37c64308b9ed501985f77b81155548ad3b281b75
Ryan Bigg <radarlistener@gmail.com>
2012-11-28 16:32:50 +1100

    Don't call select2 for user_picker in spree_promo.js if that element does not exist
    Fixes #2245

--- promo/app/assets/javascripts/admin/spree_promo.js
+++ promo/app/assets/javascripts/admin/spree_promo.js
@@ -9,29 +9,31 @@ function cleanUsers(data) {
 }
 
 $(document).ready(function() {
-  $('.user_picker').select2({
-    minimumInputLength: 1,
-    multiple: true,
-    initSelection: function(element, callback) {
-      $.get(Spree.routes.user_search, { ids: element.val() }, function(data) { 
-        callback(cleanUsers(data))
-      })
-    },
-    ajax: {
-      url: Spree.routes.user_search,
-      datatype: 'json',
-      data: function(term, page) {
-        return { q: term }
+  if ($('user_picker').length > 0) {
+    $('.user_picker').select2({
+      minimumInputLength: 1,
+      multiple: true,
+      initSelection: function(element, callback) {
+        $.get(Spree.routes.user_search, { ids: element.val() }, function(data) { 
+          callback(cleanUsers(data))
+        })
       },
-      results: function(data, page) {
-        return { results: cleanUsers(data) }
+      ajax: {
+        url: Spree.routes.user_search,
+        datatype: 'json',
+        data: function(term, page) {
+          return { q: term }
+        },
+        results: function(data, page) {
+          return { results: cleanUsers(data) }
+        }
+      },
+      formatResult: function(user) {
+        return user.email;
+      },
+      formatSelection: function(user) {
+        return user.email;
       }
-    },
-    formatResult: function(user) {
-      return user.email;
-    },
-    formatSelection: function(user) {
-      return user.email;
-    }
-  });
+    });
+  }
 })
