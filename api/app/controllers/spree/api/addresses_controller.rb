ac7e94100bde1af3dfa8fb21b5e8b889b6ed550f
John Dyer <lbrapid@gmail.com>
2012-11-29 12:41:20 -0500

    Refactor AddressesController to work with rabl template responder

--- api/app/controllers/spree/api/addresses_controller.rb
+++ api/app/controllers/spree/api/addresses_controller.rb
@@ -1,16 +1,19 @@
 module Spree
   module Api
     class AddressesController < Spree::Api::BaseController
+      respond_to :json
+
       def show
         @address = Address.find(params[:id])
         authorize! :read, @address
+        respond_with(@address)
       end
 
       def update
         @address = Address.find(params[:id])
         authorize! :read, @address
         @address.update_attributes(params[:address])
-        render :show, :status => 200
+        respond_with(@address, :default_template => :show)
       end
     end
   end
