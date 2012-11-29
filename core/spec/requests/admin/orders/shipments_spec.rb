593174dda7c7fc883414192c005225060209a8db
Ryan Bigg <radarlistener@gmail.com>
2012-11-30 10:25:23 +1100

    Add OrderWalkthrough testing support class and use it to fix admin/orders/shipments_spec
    I discovered yesterday afternoon that Factory Girl isn't *really* appropriate for dealing with such a complex object as an Order within Spree.
    
    An Order comes in many different shapes and sizes and sometimes we want to deal with it at different points in it's 'life'. Creating factories for each of these points is rather difficult, and so I made the OrderWalkthrough class which is a no-bullshit class for walking through an Order up to a specific point and then working with that object.
    
    Ideally, this will help simplify a lot of the other integration tests within Spree that need to work within the context of an Order
    
    Conflicts:
    
    	core/spec/requests/admin/orders/shipments_spec.rb

--- core/spec/requests/admin/orders/shipments_spec.rb
+++ core/spec/requests/admin/orders/shipments_spec.rb
@@ -3,42 +3,39 @@ require 'spec_helper'
 describe "Shipments" do
   stub_authorization!
 
-  Spree::Zone.delete_all
-  let(:shipping_method) { create(:shipping_method, :zone => Spree::Zone.find_by_name('North America') || create(:zone, :name => 'North America')) }
-  let(:order) { create(:completed_order_with_totals, :number => "R100", :state => "complete",  :shipping_method => shipping_method) }
+  let!(:order) { OrderWalkthrough.up_to(:complete) }
 
   before(:each) do
+    # Clear all the shipments and then re-create them in this test
+
+    order.shipments.delete_all
     reset_spree_preferences do |config|
       config.allow_backorders = true
     end
-    
-    order.inventory_units.each do |iu|
-      iu.update_attribute_without_callbacks('state', 'sold')
-    end
-    
+
     visit spree.admin_path
     click_link "Orders"
-    within('table#listing_orders tbody tr:nth-child(1)') { click_link "R100" }
-    
+    within('table#listing_orders tbody tr:nth-child(1)') { click_link order.number }
   end
 
-  it "should be able to create and list shipments for an order", :js => true do
-
+  it "should be able to create and list shipments for an order" do
     click_link "Shipments"
-    
+
     click_on "New Shipment"
     check "inventory_units_1"
     click_button "Create"
     page.should have_content("successfully created!")
     order.reload
     order.shipments.count.should == 1
-    
+
+    shipment = order.shipments.last
+
     click_link "Shipments"
-    find('table.index tr:nth-child(2) td:nth-child(1)').text.should == order.shipment.number
+    find('table.index tr:nth-child(2) td:nth-child(1)').text.should == shipment.number
     find('table.index tr:nth-child(2) td:nth-child(5)').text.should == "Pending"
-    
+
     within('table.index tr:nth-child(2) td:nth-child(7)') { click_link "Edit" }
-    page.should have_content("Shipment ##{order.shipment.number}")
+    page.should have_content("Shipment ##{shipment.number}")
   end
 
 end
