4e7cda52a9c4fc842ea00edacef2ab7b9fa9eef2
Ryan Bigg <radarlistener@gmail.com>
2012-11-29 16:08:51 +1100

    Fix translation in admin/orders/order_details_spec

--- core/spec/requests/admin/orders/order_details_spec.rb
+++ core/spec/requests/admin/orders/order_details_spec.rb
@@ -55,8 +55,8 @@ describe "Order Details" do
       end
 
       I18n.backend.store_translations I18n.locale,
-        :shipment_state => { :missing => 'some text' },
-        :payment_states => { :missing => 'other text' }
+        :shipment_states => { :missing => 'some text' },
+        :payment_states  => { :missing => 'other text' }
 
       visit spree.edit_admin_order_path(order)
 
