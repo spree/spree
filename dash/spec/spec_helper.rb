0ad38fb738b6f3e562e63043d36b7addb01f7a0f
Ryan Bigg <radarlistener@gmail.com>
2012-11-29 13:15:41 +1100

    Allow Jirafe analytics data to be edited after registration
    Fixes #2238
    
    Conflicts:
    	dash/app/controllers/spree/admin/analytics_controller.rb
    	dash/config/locales/en.yml

--- dash/spec/spec_helper.rb
+++ dash/spec/spec_helper.rb
@@ -13,6 +13,7 @@ Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
 
 require 'spree/core/testing_support/factories'
 require 'spree/core/testing_support/controller_requests'
+require 'spree/core/testing_support/authorization_helpers'
 
 require 'active_record/fixtures'
 fixtures_dir = File.expand_path('../../../core/db/default', __FILE__)
