8639bbcc3b1909a339b0a60da239a49b95baa760
Ryan Bigg <radarlistener@gmail.com>
2012-12-06 10:02:52 +1100

    Only transition to complete if payment is required and there is a payments record OR if there is no payment required

--- core/app/models/spree/order.rb
+++ core/app/models/spree/order.rb
@@ -24,7 +24,7 @@ module Spree
         order.payment_required?
       }
       go_to_state :confirm, :if => lambda { |order| order.confirmation_required? }
-      go_to_state :complete, :if => lambda { |order| order.payment_required? && order.payments.exists? }
+      go_to_state :complete, :if => lambda { |order| (order.payment_required? && order.payments.exists?) || !order.payment_required? }
       remove_transition :from => :delivery, :to => :confirm
     end
 
