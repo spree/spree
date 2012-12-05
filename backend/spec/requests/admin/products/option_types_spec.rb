a9fc7e8d598bbc26b007de14cd75b20daff403f0
Ryan Bigg <radarlistener@gmail.com>
2012-12-05 11:49:01 +1100

    Allow option values to be deleted from the admin backend
    Fixes #2277

--- backend/spec/requests/admin/products/option_types_spec.rb
+++ backend/spec/requests/admin/products/option_types_spec.rb
@@ -53,4 +53,22 @@ describe "Option Types" do
       page.should have_content("foo-size 99")
     end
   end
+
+  # Regression test for #2277
+  it "can remove an option value from an option type", :js => true do
+    create(:option_value)
+    click_link "Option Types"
+    within('table#listing_option_types') { click_icon :edit }
+    all("tbody#option_values tr").count.should == 1
+    within("tbody#option_values") do
+      find('.remove_fields').click
+    end
+    # Assert that the field is hidden automatically
+    all("tbody#option_values tr").select(&:visible?).count.should == 0
+
+    # Then assert that on a page refresh that it's still not visible
+    visit page.current_url
+    # What *is* visible is a new option value field, with blank values
+    all("tbody#option_values tr input").all? { |input| input.value.blank? }
+  end
 end
