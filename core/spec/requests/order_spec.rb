c37bf3f4bb2af2e6b189c2cf3f46ae7c541ccfdf
Ryan Bigg <radarlistener@gmail.com>
2012-12-05 17:50:55 +1100

    Fix issue where a blank credit card type would cause the order details page to complain about a missing asset
    Fixes #2282
    
    Conflicts:
    
    	core/spec/requests/order_spec.rb

--- core/spec/requests/order_spec.rb
+++ core/spec/requests/order_spec.rb
@@ -7,4 +7,22 @@ describe 'orders' do
     # Regression test for current_user call on orders/show
     lambda { visit spree.order_path(order) }.should_not raise_error
   end
+
+  # Regression test for #2282
+  context "can support a credit card with blank information" do
+    before do
+      credit_card = Factory(:credit_card)
+      credit_card.update_column(:cc_type, '')
+      payment = order.payments.first
+      payment.source = credit_card
+      payment.save!
+    end
+
+    specify do
+      visit spree.order_path(order)
+      within '.payment-info' do
+        lambda { find("img") }.should raise_error(Capybara::ElementNotFound)
+      end
+    end
+  end
 end
