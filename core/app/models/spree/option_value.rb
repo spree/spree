4cc63bf8e43c671a334001bb2f3f1ac3a23cd7fa
Ryan Bigg <radarlistener@gmail.com>
2012-12-05 12:41:18 +1100

    [api] Add option values endpoint

--- core/app/models/spree/option_value.rb
+++ core/app/models/spree/option_value.rb
@@ -4,6 +4,8 @@ module Spree
     acts_as_list :scope => :option_type
     has_and_belongs_to_many :variants, :join_table => 'spree_option_values_variants', :class_name => "Spree::Variant"
 
+    validates :name, :presentation, :presence => true
+
     attr_accessible :name, :presentation
   end
 end
