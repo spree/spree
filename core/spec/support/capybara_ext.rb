3f433a39d65b1d3410c643e409cf48233bf06546
Ryan Bigg <radarlistener@gmail.com>
2012-12-06 13:52:27 +1100

    Improve select2 capybara_ext helper to work with select2 boxes that can contain multiple values
    Conflicts:
    
    	core/spec/support/capybara_ext.rb

--- core/spec/support/capybara_ext.rb
+++ core/spec/support/capybara_ext.rb
@@ -16,17 +16,14 @@ module CapybaraExt
   end
 
   def select2(within, value)
-    script = %Q{
-      $('#{within} .select2-search-field input').val('#{value}')
-      $('#{within} .select2-search-field input').keydown();
-    }
-    page.execute_script(script)
-
-    # Wait for list to populate...
-    wait_until do
-      page.find(".select2-highlighted").visible?
-    end
-    page.execute_script("$('.select2-highlighted').mouseup();")
+    # Forced narcolepsy, thanks to JavaScript
+    sleep(1)
+    page.execute_script "$('#{within} .select2-choice').mousedown();"
+    page.execute_script "$('#{within} .select2-choices').mousedown();"
+    sleep(1)
+    page.execute_script "$('.select2-search-field input.select2-input').val('#{value}').trigger('keyup-change');"
+    sleep(1)
+    page.execute_script "$('.select2-highlighted').mouseup();"
   end
 end
 
