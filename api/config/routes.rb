9954051fb28691356bddcd5676a0f8efd3c34243
John Dyer <lbrapid@gmail.com>
2012-12-06 09:11:43 -0500

    [api] Set default format to json

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -8,7 +8,7 @@ Spree::Core::Engine.routes.prepend do
     end
   end
 
-  namespace :api do
+  namespace :api, :defaults => { :format => 'json' } do
     resources :products do
       resources :variants
       resources :product_properties
