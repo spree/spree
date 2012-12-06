2ad3bc1d02dfcd83394fd82e5d3f72f7b145c868
Ryan Bigg <radarlistener@gmail.com>
2012-12-07 08:12:21 +1100

    [promo] Fix user picker
    Fixes #2291

--- promo/app/assets/javascripts/admin/spree_promo.js
+++ promo/app/assets/javascripts/admin/spree_promo.js
@@ -1,21 +1,14 @@
 //= require admin/spree_core
 //= require_tree .
 
-function cleanUsers(data) {
-  var users = $.map(data['users'], function(result) {
-    return result['user']
-  })
-  return users;
-}
-
 $(document).ready(function() {
-  if ($('user_picker').length > 0) {
+  if ($('.user_picker').length > 0) {
     $('.user_picker').select2({
       minimumInputLength: 1,
       multiple: true,
       initSelection: function(element, callback) {
         $.get(Spree.routes.user_search, { ids: element.val() }, function(data) { 
-          callback(cleanUsers(data))
+          callback(data)
         })
       },
       ajax: {
@@ -25,7 +18,7 @@ $(document).ready(function() {
           return { q: term }
         },
         results: function(data, page) {
-          return { results: cleanUsers(data) }
+          return { results: data }
         }
       },
       formatResult: function(user) {
