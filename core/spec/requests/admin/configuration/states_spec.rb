736af6e37973d588d8911961cde70424ca31be05
Ryan Bigg <radarlistener@gmail.com>
2012-12-10 11:58:04 +1100

    Force change event for country select box in states_spec to trigger

--- core/spec/requests/admin/configuration/states_spec.rb
+++ core/spec/requests/admin/configuration/states_spec.rb
@@ -38,13 +38,17 @@ describe "States" do
     it "should allow an admin to create states for non default countries", :js => true do
       click_link "States"
       set_select2_field "#country", @hungary.id
+      # Just so the change event actually gets triggered in this spec
+      # It is definitely triggered in the "real world"
+      page.execute_script("$('#country').trigger('change');")
+
       click_link "new_state_link"
       fill_in "state_name", :with => "Pest megye"
       fill_in "Abbreviation", :with => "PE"
       click_button "Create"
       page.should have_content("successfully created!")
       page.should have_content("Pest megye")
-      find("select option\[selected\]").text.should == "Hungary"
+      find("#s2id_country span").text.should == "Hungary"
     end
 
     it "should show validation errors", :js => true do
