ee070ac475d2a01e0336b3e0b11f81462ec05650
Ryan Bigg <radarlistener@gmail.com>
2012-12-18 16:24:58 +1100

    [core] Remove calculated_adjustments call from ShippingMethod
    This is now automatically done when the CalculatedAdjustment module is
    included into ShippingMethod

--- core/app/models/spree/shipping_method.rb
+++ core/app/models/spree/shipping_method.rb
@@ -14,8 +14,6 @@ module Spree
     attr_accessible :name, :zone_id, :display_on, :shipping_category_id,
                     :match_none, :match_one, :match_all
 
-    calculated_adjustments
-
     def adjustment_label
       I18n.t(:shipping)
     end
