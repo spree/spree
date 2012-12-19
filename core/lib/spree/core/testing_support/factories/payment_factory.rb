87685060c2678b713b98f5698b2731c199abc33b
Ryan Bigg <radarlistener@gmail.com>
2012-12-19 17:31:44 +1100

    Remove commented out factory from payment_factory

--- core/lib/spree/core/testing_support/factories/payment_factory.rb
+++ core/lib/spree/core/testing_support/factories/payment_factory.rb
@@ -13,19 +13,6 @@ FactoryGirl.define do
     end
   end
 
-  # factory :creditcard_txn do
-  #   payment
-  #   amount 45.75
-  #   response_code 12345
-  #   txn_type CreditcardTxn::TxnType::AUTHORIZE
-  #
-  #   # match the payment amount to the payment's value
-  #   after_create do |txn|
-  #     # txn.update_attribute(:amount, [txn.amount, txn.payment.payment].min)
-  #     txn.update_attribute(:amount, txn.payment.amount)
-  #   end
-  # end
-
   factory :check_payment, :class => Spree::Payment do
     amount 45.75
     payment_method { FactoryGirl.create(:payment_method) }
