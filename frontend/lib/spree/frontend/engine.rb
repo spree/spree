56d94de4896198464d3cfb487f8b4b3d7c84d81d
Ryan Bigg <radarlistener@gmail.com>
2012-12-20 11:25:33 +1100

    Turning backend into a gem; separating and re-arranging more things in frontend
    Add requires for backend and frontend to spree.rb

--- frontend/lib/spree/frontend/engine.rb
+++ frontend/lib/spree/frontend/engine.rb
@@ -3,33 +3,10 @@ module Spree
     class Engine < ::Rails::Engine
       config.middleware.use "Spree::Frontend::Middleware::SeoAssist"
 
-      config.autoload_paths += %W(#{config.root}/lib)
-
-      # We need to reload the routes here due to how Spree sets them up.
-      # The different facets of Spree (auth, promo, etc.) append/prepend routes to Frontend
-      # *after* Frontend has been loaded.
-      #
-      # So we wait until after initialization is complete to do one final reload.
-      # This then makes the appended/prepended routes available to the application.
-      config.after_initialize do
-        Rails.application.routes_reloader.reload!
-      end
-
-      # filter sensitive information during logging
-      initializer "spree.params.filter" do |app|
-        app.config.filter_parameters += [:password, :password_confirmation, :number]
-      end
-
       # sets the manifests / assets to be precompiled, even when initialize_on_precompile is false
       initializer "spree.assets.precompile", :group => :all do |app|
         app.config.assets.precompile += %w[
           store/all.*
-          admin/all.*
-          admin/orders/edit_form.js
-          admin/address_states.js
-          jqPlot/excanvas.min.js
-          admin/images/new.js
-          jquery.jstree/themes/apple/*
         ]
       end
 
