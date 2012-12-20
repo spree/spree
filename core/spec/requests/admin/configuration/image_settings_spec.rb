8af053210cb097cc7dfea262964a53fcae3481ae
Ryan Bigg <radarlistener@gmail.com>
2012-12-21 10:09:31 +1100

    Add field for attachment_url preference to image settings page
    Fixes #2344

---
+++ core/spec/requests/admin/configuration/image_settings_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe "image settings" do
+  stub_authorization!
+
+  before do
+    visit spree.admin_path
+    click_link "Configuration"
+    click_link "Image Settings"
+  end
+  
+  # Regression test for #2344
+  it "can update attachment_url" do
+    fill_in "Attachments URL", :with => "foobar"
+    fill_in "Attachments Default URL", :with => "barfoo"
+    fill_in "Attachments Path", :with => "bfaoro" 
+    click_button "Update"
+
+    Spree::Config[:attachment_url].should == "foobar"
+    Spree::Config[:attachment_default_url].should == "barfoo"
+    Spree::Config[:attachment_path].should == "bfaoro"
+  end
+
+end
+
