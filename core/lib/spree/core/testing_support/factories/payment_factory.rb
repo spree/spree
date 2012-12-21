166dd4c23a3ce2190b88c7ab08314953154e99c1
Ryan Bigg <radarlistener@gmail.com>
2012-12-21 11:59:27 +1100

    Remove commented out lines from payment factory

--- core/lib/spree/core/testing_support/factories/payment_factory.rb
+++ core/lib/spree/core/testing_support/factories/payment_factory.rb
@@ -6,23 +6,5 @@ FactoryGirl.define do
     order { FactoryGirl.create(:order) }
     state 'pending'
     response_code '12345'
-
-    # limit the payment amount to order's remaining balance, to avoid over-pay exceptions
-    after_create do |pmt|
-        #pmt.update_attribute(:amount, [pmt.amount, pmt.order.outstanding_balance].min)
-    end
   end
-
-  # factory :creditcard_txn do
-  #   payment
-  #   amount 45.75
-  #   response_code 12345
-  #   txn_type CreditcardTxn::TxnType::AUTHORIZE
-  #
-  #   # match the payment amount to the payment's value
-  #   after_create do |txn|
-  #     # txn.update_attribute(:amount, [txn.amount, txn.payment.payment].min)
-  #     txn.update_attribute(:amount, txn.payment.amount)
-  #   end
-  # end
 end
