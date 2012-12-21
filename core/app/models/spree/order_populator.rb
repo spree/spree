432d129c86e03597347cd223507d9386e9613d62
Ryan Bigg <radarlistener@gmail.com>
2012-12-21 16:06:43 +1100

    Move order population responsibility out of OrdersController
    The OrderPopulator class now contains the logic for checking the stock of a variant and then adding it to the order if it's valid
    
    This is intended as a fix for #2341

---
+++ core/app/models/spree/order_populator.rb
@@ -0,0 +1,67 @@
+module Spree
+  class OrderPopulator
+    attr_accessor :order, :currency
+    attr_reader :errors
+
+    def initialize(order, currency)
+      @order = order
+      @currency = currency
+      @errors = ActiveModel::Errors.new(self)
+    end
+
+    #
+    # Parameters can be passed using the following possible parameter configurations:
+    #
+    # * Single variant/quantity pairing
+    # +:variants => { variant_id => quantity }+
+    #
+    # * Multiple products at once
+    # +:products => { product_id => variant_id, product_id => variant_id }, :quantity => quantity+
+    # +:products => { product_id => variant_id, product_id => variant_id }, :quantity => { variant_id => quantity, variant_id => quantity }+
+    def populate(from_hash)
+      from_hash[:products].each do |product_id,variant_id|
+        attempt_cart_add(variant_id, from_hash[:quantity])
+      end if from_hash[:products]
+
+      from_hash[:variants].each do |variant_id, quantity|
+        attempt_cart_add(variant_id, quantity)
+      end if from_hash[:variants]
+
+      valid?
+    end
+
+    def valid?
+      errors.empty?
+    end
+
+    private
+
+    def attempt_cart_add(variant_id, quantity)
+      quantity = quantity.to_i
+      variant = Spree::Variant.find(variant_id)
+      if quantity > 0
+        if check_stock_levels(variant, quantity)
+          @order.add_variant(variant, quantity, currency)
+        end
+      end
+    end
+
+    def check_stock_levels(variant, quantity)
+      display_name = %Q{#{variant.name}}
+      display_name += %Q{ (#{variant.options_text})} unless variant.options_text.blank?
+
+      if variant.in_stock?
+        if variant.count_on_hand >= quantity
+          return true
+        else
+          errors.add(:base, %Q{There are only #{variant.count_on_hand} of #{display_name.inspect} remaining.} + 
+                            %Q{ Please select a quantity less than or equal to this value.})
+          return false
+        end
+      else
+        errors.add(:base, %Q{#{display_name.inspect} is out of stock.})
+        return false
+      end
+    end
+  end
+end
