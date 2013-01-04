47d24e2c3488f00ff618cd29456b6d080a71825b
Washington Luiz <huoxito@gmail.com>
2013-01-04 15:03:35 -0300

    Removes useless call to order#update!
    The only reason I could see for those to be there is to make a test
    pass. But the problem was on the test itself due to the way stubs work.
    Also add some doc blocks to CreateAdjustment class.

--- promo/app/models/spree/promotion/actions/create_adjustment.rb
+++ promo/app/models/spree/promotion/actions/create_adjustment.rb
@@ -1,27 +1,36 @@
 module Spree
   class Promotion
     module Actions
+      # Responsible for the creation and management of an adjustment since an
+      # an adjustment uses its originator to also update its eligiblity and amount
       class CreateAdjustment < PromotionAction
         calculated_adjustments
+
+        has_many :adjustments, :as => :originator, :dependent => :destroy
 
         delegate :eligible?, :to => :promotion
 
         before_validation :ensure_action_has_calculator
 
+        # Creates the adjustment related to a promotion for the order passed
+        # through options hash
         def perform(options = {})
-          return unless order = options[:order]
-          # Nothing to do if the promotion is already associated with the order
-          return if order.promotion_credit_exists?(promotion)
+          order = options[:order]
+          return if order.promotion_credit_exists?(self.promotion)
 
-          order.adjustments.promotion.reload.clear
-          order.update!
-          create_adjustment("#{I18n.t(:promotion)} (#{promotion.name})", order, order)
-          order.update!
+          self.create_adjustment("#{I18n.t(:promotion)} (#{promotion.name})", order, order)
         end
 
-        # override of CalculatedAdjustments#create_adjustment so promotional
-        # adjustments are added all the time. They will get their eligability
-        # set to false if the amount is 0
+        # Override of CalculatedAdjustments#create_adjustment so promotional
+        # adjustments are added all the time. They will get their eligibility
+        # set to false if the amount is 0.
+        #
+        # Currently an adjustment is created even when its promotion is not eligible.
+        # This helps to figure out later which adjustments should be eligible
+        # as the order is being updated
+        #
+        # BTW The order is updated (through order#update) every time an adjustment
+        # is saved
         def create_adjustment(label, target, calculable, mandatory=false)
           amount = compute_amount(calculable)
           params = { :amount => amount,
@@ -32,7 +41,8 @@ module Spree
           target.adjustments.create(params, :without_protection => true)
         end
 
-        # Ensure a negative amount which does not exceed the sum of the order's item_total and ship_total
+        # Ensure a negative amount which does not exceed the sum of the order's
+        # item_total and ship_total
         def compute_amount(calculable)
           [(calculable.item_total + calculable.ship_total), super.to_f.abs].min * -1
         end
