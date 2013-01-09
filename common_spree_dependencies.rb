7dfd7342efb93c1656412d29970cde89fa83816d
Ryan Bigg <radarlistener@gmail.com>
2013-01-09 11:17:37 +1100

    Freeze to rspec-rails 2.12.0 due to this commit:
    https://github.com/rspec/rspec-rails/commit/09793f885637ee4d6f5cc2c10b4f60b078acb9b5#commitcomment-2403679

--- common_spree_dependencies.rb
+++ common_spree_dependencies.rb
@@ -18,7 +18,7 @@ end
 group :test do
   gem 'guard'
   gem 'guard-rspec', '~> 0.5.0'
-  gem 'rspec-rails', '~> 2.12.0'
+  gem 'rspec-rails', '2.12.0'
   gem 'factory_girl_rails', '~> 1.7.0'
   gem 'email_spec', '~> 1.2.1'
 
