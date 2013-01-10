6dee38f8f45b73d97b9497e96359b0b077570fc8
Matteo Latini <mtylty@gmail.com>
2013-01-10 15:28:30 +0100

    Cleanup product and variant factories
    Fixes #2419

--- core/lib/spree/core/testing_support/factories/variant_factory.rb
+++ core/lib/spree/core/testing_support/factories/variant_factory.rb
@@ -1,5 +1,5 @@
 FactoryGirl.define do
-  factory :variant, :class => Spree::Variant do
+  factory :base_variant, :class => Spree::Variant do
     price 19.99
     cost_price 17.00
     sku    { Faker::Lorem.sentence }
@@ -7,10 +7,16 @@ FactoryGirl.define do
     height { BigDecimal.new("#{rand(200)}.#{rand(99)}") }
     width  { BigDecimal.new("#{rand(200)}.#{rand(99)}") }
     depth  { BigDecimal.new("#{rand(200)}.#{rand(99)}") }
-    on_hand 5
 
     # associations:
-    product { |p| p.association(:product) }
+    product { |p| p.association(:base_product) }
     option_values { [FactoryGirl.create(:option_value)] }
+  end
+
+  factory :variant, :parent => :base_variant do
+    on_hand 5
+    
+    # associations:
+    product { |p| p.association(:product) }
   end
 end
