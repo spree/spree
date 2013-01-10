9f5c3fb32c78347ba63c65799990ca8bd88270bd
Ryan Bigg <radarlistener@gmail.com>
2013-01-10 13:19:25 +1100

    Make API correctly dependent on ActionController::Metal + Spree::Api::ControllerSetup
    Fixes failing zones_controller_spec

---
+++ api/lib/spree/api/controller_setup.rb
@@ -0,0 +1,34 @@
+require 'spree/api/responders'
+
+module Spree
+  module Api
+    module ControllerSetup
+      def self.included(klass)
+        klass.class_eval do
+          include AbstractController::Rendering
+          include AbstractController::ViewPaths
+          include AbstractController::Callbacks
+          include AbstractController::Helpers
+
+          include ActiveSupport::Rescuable
+
+          include ActionController::Rendering
+          include ActionController::ImplicitRender
+          include ActionController::Rescue
+          include ActionController::MimeResponds
+          include ActionController::Head
+
+          include CanCan::ControllerAdditions
+
+          include Spree::Core::ControllerHelpers::Auth
+
+          prepend_view_path Rails.root + "app/views"
+          append_view_path File.expand_path("../../../app/views", File.dirname(__FILE__))
+
+          self.responder = Spree::Api::Responders::AppResponder
+          respond_to :json
+        end
+      end
+    end
+  end
+end
