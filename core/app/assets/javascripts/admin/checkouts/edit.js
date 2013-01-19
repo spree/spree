77e4008d243751d331d24dce73c005b4b25e479c
Washington Luiz <huoxito@gmail.com>
2013-01-19 02:47:19 -0300

    Hides shipping form if use billing is checked [backend]
    When page loads shipping form was being displayed even though 'use
    billing address' is checked

--- core/app/assets/javascripts/admin/checkouts/edit.js
+++ core/app/assets/javascripts/admin/checkouts/edit.js
@@ -61,18 +61,21 @@ $(document).ready(function() {
     })
   }
 
+  var order_use_billing_input = $('input#order_use_billing');
 
-  $('input#order_use_billing').click(function() {
-    if(!$(this).is(':checked')) {
+  var order_use_billing = function () {
+    if (!order_use_billing_input.is(':checked')) {
       $('#shipping').show();
-      $('#shipping input').prop("disabled", false);
-      $('#shipping select').prop("disabled", false);
     } else {
       $('#shipping').hide();
-      $('#shipping input').prop("disabled", true);
-      $('#shipping select').prop("disabled", true);
     }
+  };
+
+  order_use_billing_input.click(function() {
+    order_use_billing();
   });
+
+  order_use_billing();
 
   $('#guest_checkout_true').change(function() {
     $('#customer_search').val("");
@@ -87,5 +90,3 @@ $(document).ready(function() {
     })
   });
 });
-
-
