0e13c7dd4055b381468f5b7989aa9b260e31a209
Ryan Bigg <radarlistener@gmail.com>
2013-01-21 16:13:45 +1100

    Make customer details form use API to retreive state information

--- backend/app/assets/javascripts/admin/address_states.js
+++ backend/app/assets/javascripts/admin/address_states.js
@@ -1,26 +1,27 @@
 var update_state = function(region) {
   var country        = $('span#' + region + 'country .select2').select2('val');
-  var states         = state_mapper[country];
 
-  var state_select   = $('span#' + region + 'state .select2');
-  var state_input    = $('span#' + region + 'state input');
+  var state_select   = $('span#' + region + 'state select.select2');
+  var state_input    = $('span#' + region + 'state input.state_name');
 
-  if(states) {
-    state_select.html('');
-    var states_with_blank = [["",""]].concat(states);
-    $.each(states_with_blank, function(pos,id_nm) {
-      var opt = $(document.createElement('option'))
-                .attr('value', id_nm[0])
-                .html(id_nm[1]);
-      state_select.append(opt);
-    });
-    state_select.prop("disabled", false).show();
-    state_select.select2();
-    state_input.hide().prop("disabled", true);
+  $.get(Spree.routes.states_search + "?country_id=" + country, function(data) {
+    var states = data["states"]
+    if(states) {
+      state_select.html('');
+      var states_with_blank = [{name: '', id: ''}].concat(states);
+      $.each(states_with_blank, function(pos,state) {
+        var opt = $(document.createElement('option'))
+                  .attr('value', state.id)
+                  .html(state.name);
+        state_select.append(opt);
+      });
+      state_select.prop("disabled", false).show();
+      state_select.select2();
+      state_input.hide().prop("disabled", true);
 
-  } else {
-    state_input.prop("disabled", false).show();
-    state_select.select2('destroy').hide();
-  }
-
+    } else {
+      state_input.prop("disabled", false).show();
+      state_select.select2('destroy').hide();
+    }
+  })
 };
