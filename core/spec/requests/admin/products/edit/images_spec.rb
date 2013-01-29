fb3960a1d5933d0c5c2b4c9e53d2c409faf6aee0
Ryan Bigg <radarlistener@gmail.com>
2013-01-30 10:54:05 +1100

    Show variant images in admin/images if that's all there is
    Fixes #2228
    
    fix

--- core/spec/requests/admin/products/edit/images_spec.rb
+++ core/spec/requests/admin/products/edit/images_spec.rb
@@ -3,6 +3,8 @@ require 'spec_helper'
 describe "Product Images" do
   stub_authorization!
 
+  let(:file_path) { Rails.root + "../../spec/support/ror_ringer.jpeg" }
+
   context "uploading and editing an image", :js => true do
     it "should allow an admin to upload and edit an image for a product" do
       Spree::Image.attachment_definitions[:attachment].delete :storage
@@ -14,8 +16,7 @@ describe "Product Images" do
       click_icon(:edit)
       click_link "Images"
       click_link "new_image_link"
-      absolute_path = Rails.root + "../../spec/support/ror_ringer.jpeg"
-      attach_file('image_attachment', absolute_path)
+      attach_file('image_attachment', file_path)
       click_button "Update"
       page.should have_content("successfully created!")
       click_icon(:edit)
@@ -23,6 +24,18 @@ describe "Product Images" do
       click_button "Update"
       page.should have_content("successfully updated!")
       page.should have_content("ruby on rails t-shirt")
+    end
+  end
+
+  # Regression test for #2228
+  it "should see variant images" do
+    variant = create(:variant)
+    variant.images.create!(:attachment => File.open(file_path))
+    visit spree.admin_product_images_path(variant.product)
+
+    page.should_not have_content("No Images Found.")
+    within("table.index") do
+      page.should have_content(variant.options_text)
     end
   end
 end
