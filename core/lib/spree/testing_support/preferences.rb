0ca39acd2cce4693ca375d5cd6fd904b325bcf34
Ryan Bigg <radarlistener@gmail.com>
2013-02-01 13:22:48 +1100

    Update testing_support/preferences with configure_spree_preferences

--- core/lib/spree/testing_support/preferences.rb
+++ core/lib/spree/testing_support/preferences.rb
@@ -1,32 +1,31 @@
 module Spree
-  module Models
-    module TestingSupport
-      module Preferences
-        def reset_spree_preferences(&config_block)
-          Spree::Preferences::Store.instance.persistence = false
-          Spree::Preferences::Store.instance.clear_cache
+  module TestingSupport
+    module Preferences
+      # Resets all preferences to default values, you can
+      # pass a block to override the defaults with a block
+      #
+      # reset_spree_preferences do |config|
+      #   config.site_name = "my fancy pants store"
+      # end
+      #
+      def reset_spree_preferences(&config_block)
+        Spree::Preferences::Store.instance.persistence = false
+        Spree::Preferences::Store.instance.clear_cache
 
-          configure_spree_preferences &config_block if block_given?
-        end
+        config = Rails.application.config.spree.preferences
+        configure_spree_preferences &config_block if block_given?
+      end
 
-        # The preference cache is cleared before each test, so the
-        # default values will be used. You can define preferences
-        # for your spec with:
-        #
-        # configure_spree_preferences do |config|
-        #   config.site_name = "my fancy pants store"
-        # end
-        #
-        def configure_spree_preferences
-          config = Rails.application.config.spree.preferences
-          yield(config) if block_given?
-        end
+      def configure_spree_preferences
+        config = Rails.application.config.spree.preferences
+        yield(config) if block_given?
+      end
 
-        def assert_preference_unset(preference)
-          find("#preferences_#{preference}")['checked'].should be_false
-          Spree::Config[preference].should be_false
-        end
+      def assert_preference_unset(preference)
+        find("#preferences_#{preference}")['checked'].should be_false
+        Spree::Config[preference].should be_false
       end
     end
   end
 end
+
