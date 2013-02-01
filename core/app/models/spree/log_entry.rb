79464dc5a94c665368658e99368c4de026040f7a
Ryan Bigg <radarlistener@gmail.com>
2013-02-01 11:01:17 +1100

    Save LogEntry records if they are being rolled back
    Related to #1767

--- core/app/models/spree/log_entry.rb
+++ core/app/models/spree/log_entry.rb
@@ -1,5 +1,16 @@
 module Spree
   class LogEntry < ActiveRecord::Base
     belongs_to :source, :polymorphic => true
+
+    # Fix for #1767
+    # If a payment fails, we want to make sure we keep the record of it failing
+    after_rollback :save_anyway
+
+    def save_anyway
+      log = Spree::LogEntry.new
+      log.source  = source
+      log.details = details
+      log.save!
+    end
   end
 end
