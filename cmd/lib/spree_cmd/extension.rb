11dcee754d6c07897eabd26db3e7bcb14ffb595d
Sean Schofield <sean.schofield@gmail.com>
2013-02-04 16:25:42 -0500

    Version bump (1.1.6.beta)

--- cmd/lib/spree_cmd/extension.rb
+++ cmd/lib/spree_cmd/extension.rb
@@ -51,7 +51,7 @@ module SpreeCmd
       end
 
       def spree_version
-        '1.1.5'
+        '1.1.6.beta'
       end
 
       def use_prefix(prefix)
--- core/lib/spree/core/version.rb
+++ core/lib/spree/core/version.rb
@@ -1,5 +1,5 @@
 module Spree
   def self.version
-    "1.1.5"
+    "1.1.6.beta"
   end
 end
