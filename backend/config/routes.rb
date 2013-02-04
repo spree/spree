b1b0f4b03b78b2dd44099d6767e9bdf93122524d
Brian Quinn <briandquinn@gmail.com>
2013-02-04 12:24:57 +0000

    More authorization related changes:
    1) Reintroduces authorization stuff for front-end controllers from
    spree_auth_devise.
    2) Admin::BaseController now passes the class and not a new instance

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,5 +1,44 @@
 Spree::Core::Engine.routes.append do
-  root :to => "home#index"
+  root :to => 'home#index'
+
+  resources :products
+
+  match '/locale/set', :to => 'locale#set'
+
+  resources :states, :only => :index
+  resources :countries, :only => :index
+
+  # non-restful checkout stuff
+  put '/checkout/update/:state', :to => 'checkout#update', :as => :update_checkout
+  get '/checkout/:state', :to => 'checkout#edit', :as => :checkout_state
+  get '/checkout', :to => 'checkout#edit' , :as => :checkout
+
+  populate_redirect = redirect do |params, request|
+    request.flash[:error] = I18n.t(:populate_get_error)
+    request.referer || '/cart'
+  end
+
+  get '/orders/populate', :via => :get, :to => populate_redirect
+  match '/orders/:id/token/:token' => 'orders#show', :via => :get, :as => :token_order
+
+  resources :orders do
+    post :populate, :on => :collection
+
+    resources :line_items
+
+    resources :shipments do
+      member do
+        get :shipping_method
+      end
+    end
+
+  end
+  get '/cart', :to => 'orders#edit', :as => :cart
+  put '/cart', :to => 'orders#update', :as => :update_cart
+  put '/cart/empty', :to => 'orders#empty', :as => :empty_cart
+
+  # route globbing for pretty nested taxon and product paths
+  match '/t/*id', :to => 'taxons#show', :as => :nested_taxons
 
   namespace :admin do
     get '/search/users', :to => "search#users", :as => :search_users
