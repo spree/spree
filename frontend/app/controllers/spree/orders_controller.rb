1c6103713ead65dd7a6b87fc193a17142668c0da
Ryan Bigg <radarlistener@gmail.com>
2013-02-04 16:19:06 +1100

    Re-add respond_with blocks for OrdersController

--- frontend/app/controllers/spree/orders_controller.rb
+++ frontend/app/controllers/spree/orders_controller.rb
@@ -17,11 +17,15 @@ module Spree
         render :edit and return unless apply_coupon_code
         @order.line_items = @order.line_items.select {|li| li.quantity > 0 }
         fire_event('spree.order.contents_changed')
-        if params.has_key?(:checkout)
-          @order.next_transition.run_callbacks
-          redirect_to checkout_state_path(@order.checkout_steps.first)
-        else
-          redirect_to cart_path
+        respond_with(@order) do |format|
+          format.html do
+            if params.has_key?(:checkout)
+              @order.next_transition.run_callbacks
+              redirect_to checkout_state_path(@order.checkout_steps.first)
+            else
+              redirect_to cart_path
+            end
+          end
         end
       else
         respond_with(@order)
