a159a6ec463cbebcfa3ca95f871ef2ad2319ad67
Ryan Bigg <radarlistener@gmail.com>
2013-02-06 10:21:39 +1100

    Add backend and frontend to build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
  alias set_gemfile='export BUNDLE_GEMFILE="`pwd`/Gemfile"'
  bundle exec rake test_app
  cd api; set_gemfile; bundle install; bundle exec rspec spec
+ cd ../backend; set_gemfile; bundle install; bundle exec rspec spec
  cd ../core; set_gemfile; bundle install; bundle exec rspec spec
  cd ../dash; set_gemfile; bundle install; bundle exec rspec spec
- cd ../promo; set_gemfile; bundle install; bundle exec rspec spec
+ cd ../frontend; set_gemfile; bundle install; bundle exec rspec spec
