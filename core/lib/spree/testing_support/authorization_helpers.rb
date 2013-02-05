1b801401ed37faf4bbfaff3580f0b99d2574f092
Ryan Bigg <radarlistener@gmail.com>
2013-02-05 13:07:54 +1100

    Just stub authorize! to return true in AuthorizationHelpers rather than expecting it's received at all

--- core/lib/spree/testing_support/authorization_helpers.rb
+++ core/lib/spree/testing_support/authorization_helpers.rb
@@ -4,7 +4,7 @@ module Spree
       module Controller
         def stub_authorization!
           before do
-            controller.should_receive(:authorize!).and_return(true)
+            controller.stub :authorize! => true
           end
         end
       end
