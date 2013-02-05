04743a849f0122205b094b93bead90bd5d33e91e
Ryan Bigg <radarlistener@gmail.com>
2013-02-05 11:48:24 +1100

    move product_picker.js to use the API

--- backend/app/assets/javascripts/admin/product_picker.js
+++ backend/app/assets/javascripts/admin/product_picker.js
@@ -3,18 +3,24 @@ $.fn.productAutocomplete = function() {
     minimumInputLength: 1,
     multiple: true,
     initSelection: function(element, callback) {
-      $.get(Spree.routes.product_search, { ids: element.val() }, function(data) { 
-        callback(data)
+      $.get(Spree.routes.product_search, { ids: element.val().split(',') }, function(data) { 
+        callback(data['products'])
       })
     },
     ajax: {
       url: Spree.routes.product_search,
       datatype: 'json',
       data: function(term, page) {
-        return { q: term }
+        return { 
+          q: {
+            name_cont: term,
+            sku_cont: term,
+          },
+          m: "OR"
+        }
       },
       results: function(data, page) {
-        return { results: data }
+        return { results: data['products'] }
       }
     },
     formatResult: function(product) {
