218e849d94d3c29c01f577a48daad5fc07787ac6
Ryan Bigg <radarlistener@gmail.com>
2013-02-08 16:16:22 +1100

    Add strong_parameters for CheckoutController + OrdersController

--- core/app/controllers/spree/base_controller.rb
+++ core/app/controllers/spree/base_controller.rb
@@ -1,9 +1,11 @@
 require 'cancan'
+require_dependency 'spree/core/controller_helpers/strong_parameters'
 
 class Spree::BaseController < ApplicationController
   include Spree::Core::ControllerHelpers::Auth
   include Spree::Core::ControllerHelpers::RespondWith
   include Spree::Core::ControllerHelpers::Common
+  include Spree::Core::ControllerHelpers::StrongParameters
 
   respond_to :html
 end
