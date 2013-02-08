f2945d4cb55374cf76b6d567cc3b817e2f970735
Ryan Bigg <radarlistener@gmail.com>
2013-02-08 22:08:21 +1100

    Remove incorrect association definition for promotion_rules in product model

--- core/app/models/spree/product.rb
+++ core/app/models/spree/product.rb
@@ -49,8 +49,6 @@ module Spree
 
     has_many :prices, :through => :variants, :order => 'spree_variants.position, spree_variants.id, currency'
 
-    has_and_belongs_to_many :promotion_rules, :join_table => :spree_products_promotion_rules
-
     delegate_belongs_to :master, :sku, :price, :currency, :display_amount, :display_price, :weight, :height, :width, :depth, :is_master, :has_default_price?, :cost_currency, :price_in, :amount_in
     delegate_belongs_to :master, :cost_price if Variant.table_exists? && Variant.column_names.include?('cost_price')
 
