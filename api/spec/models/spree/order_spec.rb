65c5a6c7fe681eea0ef6b160cd41dae2d44d6383
Tiago Jorge <tiagomjorge@gmail.com>
2013-02-11 19:58:08 -0200

    creating order through API as guest #2545

--- api/spec/models/spree/order_spec.rb
+++ api/spec/models/spree/order_spec.rb
@@ -9,7 +9,7 @@ module Spree
       variant_id = product.master.id
       order = Order.build_from_api(user, { :line_items_attributes => { "0" => { :variant_id => variant_id, :quantity => 5 }}})
 
-      order.user.should == user
+      order.user.should == nil
       line_item = order.line_items.first
       line_item.quantity.should == 5
       line_item.variant_id.should == variant_id
