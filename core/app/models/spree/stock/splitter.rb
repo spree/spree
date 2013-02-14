d06b3905501bb8e143e7a9ace4c8fe7694da006d
cmar <christopher.mar@gmail.com>
2013-02-14 14:13:58 -0500

    move stock packages and splitters into stock directory

---
+++ core/app/models/spree/stock/splitter.rb
@@ -0,0 +1,25 @@
+module Spree
+  module Stock
+    module Splitter
+      class Base
+        attr_accessor :stock_location, :order
+
+        def initialize(stock_location, order)
+          @stock_location = stock_location
+          @order = order
+        end
+
+        def split(packages)
+          packages
+        end
+      end
+
+      class ShippingCategory < Base
+        def split(packages)
+          #TODO regroup items by shipping category
+          packages
+        end
+      end
+    end
+  end
+end
