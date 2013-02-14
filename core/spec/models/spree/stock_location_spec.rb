843992ce5a2c447fe72f358ba9863aa829fdf7a4
cmar <christopher.mar@gmail.com>
2013-02-14 11:13:13 -0500

    add variants with status of backordered or on_hand to packages

--- core/spec/models/spree/stock_location_spec.rb
+++ core/spec/models/spree/stock_location_spec.rb
@@ -5,11 +5,28 @@ module Spree
     let(:order) { create(:order_with_line_items, line_items_count: 5) }
     subject { create(:stock_location) }
 
-    it 'builds a default package of all the items' do
-      order.reload #temp fix for bug where line_items weren't loaded
-      package = subject.default_package(order)
-      package.contents.size.should eq 5
-      package.weight.should > 0
+    before :all do
+      order.reload
+      order.line_items.each do |line_item|
+        create(:stock_item,
+               variant: line_item.variant,
+               stock_location: subject)
+      end
+    end
+
+    context 'default_package' do
+      it 'contains all the items' do
+        package = subject.default_package(order)
+        package.contents.size.should eq 5
+        package.weight.should > 0
+      end
+
+      it 'variants are added as backordered without enough on_hand' do
+        subject.should_receive(:stock_status).exactly(5).times.and_return([2,3])
+        package = subject.default_package(order)
+        package.on_hand.size.should eq 5
+        package.backordered.size.should eq 5
+      end
     end
 
     it 'builds an array of packages' do
@@ -18,5 +35,6 @@ module Spree
       packages.size.should eq 1
       packages.first.contents.size.should eq 5
     end
+
   end
 end
