fb4ab8c91c6c4f25dcb06038175f8f605badc9d8
cmar <christopher.mar@gmail.com>
2013-02-14 11:13:13 -0500

    add variants with status of backordered or on_hand to packages

--- core/app/models/spree/StockPackage.rb
+++ core/app/models/spree/StockPackage.rb
@@ -1,5 +1,7 @@
 module Spree
   class StockPackage
+    ContentItem = Struct.new(:variant, :quantity, :status)
+
     attr_accessor :stock_location, :contents
 
     def initialize(stock_location)
@@ -7,17 +9,25 @@ module Spree
       @contents = Array.new
     end
 
-    def add(stock_item)
-      contents << stock_item
+    def add(variant, quantity, status=:on_hand)
+      contents << ContentItem.new(variant, quantity, status)
     end
 
     def weight
-      contents.sum &:weight
+      contents.sum { |item| item.variant.weight }
+    end
+
+    def on_hand
+      contents.select { |item| item.status == :on_hand }
+    end
+
+    def backordered
+      contents.select { |item| item.status == :backordered }
     end
 
     def inspect
-      contents.map do |stock_item|
-        "#{stock_item.variant.name}"
+      contents.map do |content_item|
+        "#{content_item.variant.name} #{content_item.quantity} #{content_item.status}"
       end.join(',')
     end
   end
