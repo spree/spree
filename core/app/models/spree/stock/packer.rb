bb415d2dcb69d9c8cdf6a6023ed0130eac529ca7
cmar <christopher.mar@gmail.com>
2013-02-14 14:54:05 -0500

    chain of responsibility for splitters

--- core/app/models/spree/stock/packer.rb
+++ core/app/models/spree/stock/packer.rb
@@ -1,12 +1,21 @@
 module Spree
   module Stock
     class Packer
+<<<<<<< HEAD
       attr_reader :stock_location, :order, :splitters
 
       def initialize(stock_location, order, splitters=[Splitter::Base])
         @stock_location = stock_location
         @order = order
         @splitters = splitters
+=======
+      attr_accessor :stock_location, :order, :splitter_class
+
+      def initialize(stock_location, order, splitter_class=Splitter::Base)
+        @stock_location = stock_location
+        @order = order
+        @splitter_class = splitter_class
+>>>>>>> chain of responsibility for splitters
       end
 
       def packages
@@ -23,6 +32,14 @@ module Spree
         package
       end
 
+<<<<<<< HEAD
+=======
+      def build_splitter
+        # TODO build a chain of splitters
+        splitter_class.new(self)
+      end
+
+>>>>>>> chain of responsibility for splitters
       private
       def build_splitter
         splitter = nil
