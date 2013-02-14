604d262c09456115938cf7a4b24ce53469a0b38d
cmar <christopher.mar@gmail.com>
2013-02-14 10:00:46 -0500

    use factory girl inheritance for order factory

--- core/lib/spree/testing_support/factories/order_factory.rb
+++ core/lib/spree/testing_support/factories/order_factory.rb
@@ -7,26 +7,36 @@ FactoryGirl.define do
     bill_address_id nil
     ship_address_id nil
     email 'foo@example.com'
-  end
 
-  factory :order_with_totals, :parent => :order do
-    after(:create) { |order| FactoryGirl.create(:line_item, :order => order) }
-  end
+    factory :order_with_line_items do
+      ignore do
+        line_items_count 5
+      end
+      after(:create) do |order, evaluator|
+        FactoryGirl.create_list(:line_item, evaluator.line_items_count, :order => order)
+      end
+    end
 
-  factory :order_with_inventory_unit_shipped, :parent => :order do
-    after(:create) do |order|
-      FactoryGirl.create(:line_item, :order => order)
-      FactoryGirl.create(:inventory_unit, :order => order, :state => 'shipped')
+    factory :order_with_totals do
+      after(:create) { |order| FactoryGirl.create(:line_item, :order => order) }
+    end
+
+    factory :order_with_inventory_unit_shipped do
+      after(:create) do |order|
+        FactoryGirl.create(:line_item, :order => order)
+        FactoryGirl.create(:inventory_unit, :order => order, :state => 'shipped')
+      end
+    end
+
+    factory :completed_order_with_totals do
+      bill_address { FactoryGirl.create(:address) }
+      ship_address { FactoryGirl.create(:address) }
+      after(:create) do |order|
+        FactoryGirl.create(:inventory_unit, :order => order, :state => 'shipped')
+      end
+      state 'complete'
+      completed_at Time.now
     end
   end
 
-  factory :completed_order_with_totals, :parent => :order_with_totals do
-    bill_address { FactoryGirl.create(:address) }
-    ship_address { FactoryGirl.create(:address) }
-    after(:create) do |order|
-      FactoryGirl.create(:inventory_unit, :order => order, :state => 'shipped')
-    end
-    state 'complete'
-    completed_at Time.now
-  end
 end
