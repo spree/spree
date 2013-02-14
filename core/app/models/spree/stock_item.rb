43ea992ce6fb90f12ef4346d603c9df5ee058668
cmar <christopher.mar@gmail.com>
2013-02-14 10:01:13 -0500

    default packaging for stock_locations

--- core/app/models/spree/stock_item.rb
+++ core/app/models/spree/stock_item.rb
@@ -3,7 +3,14 @@ module Spree
     belongs_to :stock_location
     belongs_to :variant
 
+    # can_backorder?
+    #
+    validates_presence_of :stock_location
+    validates_uniqueness_of :variant_id, :scope => :stock_location_id
+
     attr_accessible :count_on_hand, :variant
+
+    delegate :weight, :to => :variant
 
     def self.locations_for_variant(variant)
       where(variant_id: variant).map(&:stock_location)
--- core/spec/models/spree/stock_location_spec.rb
+++ core/spec/models/spree/stock_location_spec.rb
@@ -1,10 +1,22 @@
 require 'spec_helper'
 
-describe Spree::StockLocation do
-  subject { create(:stock_location) }
+module Spree
+  describe StockLocation do
+    let(:order) { create(:order_with_line_items, line_items_count: 5) }
+    subject { create(:stock_location) }
 
-  it 'determines the count_on_hand for a variant' do
-    variant = subject.stock_items.first.variant
-    subject.count_on_hand(variant.id).should eq 10
+    it 'builds a default package of all the items' do
+      order.reload #temp fix for bug where line_items weren't loaded
+      package = subject.default_package(order)
+      package.contents.size.should eq 5
+      package.weight.should > 0
+    end
+
+    it 'builds an array of packages' do
+      order.reload #temp fix for bug where line_items weren't loaded
+      packages = subject.packages(order)
+      packages.size.should eq 1
+      packages.first.contents.size.should eq 5
+    end
   end
 end
