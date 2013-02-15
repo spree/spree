e6356c4ce68aa635ca0985433475d52313d424e3
cmar <christopher.mar@gmail.com>
2013-02-15 10:42:06 -0500

    split packages by backordered status

--- core/app/models/spree/stock/packer.rb
+++ core/app/models/spree/stock/packer.rb
@@ -11,10 +11,16 @@ module Spree
 
       def packages
         build_splitter.split [default_package]
+
+        # packages container object to tell you
+        # if complete or missing
+        #
+        # packages fulfill entire order?
+        # and return missing
       end
 
       def default_package
-        package = Package.new(stock_location)
+        package = Package.new(self)
         order.line_items.each do |line_item|
           on_hand, backordered = stock_status(line_item.variant, line_item.quantity)
           package.add line_item.variant, on_hand, :on_hand if on_hand > 0
@@ -37,7 +43,7 @@ module Spree
           backordered = 0
         else
           on_hand = item.count_on_hand
-          backordered = quantity - on_hand
+          backordered = quantity - on_hand if item.can_backorder?
         end
 
         [on_hand, backordered]
---
+++ core/app/models/spree/stock/splitter/backordered.rb
@@ -0,0 +1,23 @@
+module Spree
+  module Stock
+    module Splitter
+      class Backordered < Base
+
+        def split(packages)
+          split_packages = []
+          packages.each do |package|
+            if package.on_hand.count > 0
+              split_packages << Package.new(stock_location, package.on_hand)
+            end
+
+            if package.backordered.count > 0
+              split_packages << Package.new(stock_location, package.backordered)
+            end
+          end
+          return_next split_packages
+        end
+
+      end
+    end
+  end
+end
