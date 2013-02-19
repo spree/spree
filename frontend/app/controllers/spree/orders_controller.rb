be6812d0cfa436517fff080f86234746142063aa
Michael Bianco <iloveitaly@gmail.com>
2013-02-18 20:40:58 -0500

    Standardizing 'Order #' display

--- frontend/app/controllers/spree/orders_controller.rb
+++ frontend/app/controllers/spree/orders_controller.rb
@@ -64,7 +64,7 @@ module Spree
     end
 
     def accurate_title
-      @order && @order.completed? ? "#{Order.model_name.human} #{@order.number}" : t(:shopping_cart)
+      @order && @order.completed? ? t(:order_number, :number => @order.number) : t(:shopping_cart)
     end
 
     def check_authorization
