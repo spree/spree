e611ae572903d5ab1e51bca54b51dc9c8dad70ad
cmar <christopher.mar@gmail.com>
2013-02-21 11:13:48 -0500

    the mighty Prioritizer with customizable adjuster

--- core/spec/models/spree/stock/coordinator_spec.rb
+++ core/spec/models/spree/stock/coordinator_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 module Spree
   module Stock
     describe Coordinator do
-      let(:package) { build(:stock_package) }
+      let(:package) { build(:stock_package_fulfilled) }
       let(:order) { package.order }
       let(:stock_location) { package.stock_location }
 
@@ -15,6 +15,14 @@ module Spree
       end
 
       it 'builds a list of packages for an order' do
+        StockLocation.should_receive(:all).and_return([stock_location])
+        subject.should_receive(:build_packer).and_return(double(:packages => [package]))
+
+        packages = subject.packages order
+        packages.count.should == 1
+      end
+
+      it 'deduplicates packages when two locations can fulfill' do
         StockLocation.should_receive(:all).and_return([stock_location])
         subject.should_receive(:build_packer).and_return(double(:packages => [package]))
 
