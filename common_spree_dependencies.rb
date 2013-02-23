2f2ef44e9716d86def734b1d3c4d59c6c6d8a49c
Trung Lê <joneslee85@gmail.com>
2013-02-24 06:24:22 +1100

    Bump multi_json to latest version

--- common_spree_dependencies.rb
+++ common_spree_dependencies.rb
@@ -7,17 +7,17 @@ gem 'json'
 gem 'sqlite3'
 gem 'mysql2'
 gem 'pg'
-gem 'multi_json', "1.2.0"
+gem 'multi_json'
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
-  gem 'sass-rails', "~> 3.2"
-  gem 'coffee-rails', "~> 3.2"
+  gem 'sass-rails', '~> 3.2'
+  gem 'coffee-rails', '~> 3.2'
 end
 
 group :test do
   gem 'rspec-rails', '~> 2.12.0'
-  gem "factory_girl_rails", "~> 4.2.1"
+  gem 'factory_girl_rails', '~> 4.2.1'
 
   gem 'ffaker'
   gem 'shoulda-matchers', '~> 1.0.0'
