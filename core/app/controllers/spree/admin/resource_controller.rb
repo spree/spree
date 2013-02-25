49119603d561c9149076a897b2fe076eec838bdc
Stephane Bounmy <stephanebounmy@gmail.com>
2013-02-25 23:26:31 +0100

    fix : ability params[:action] should be a symbol https://github.com/ryanb/cancan/wiki/Fetching-Records

--- core/app/controllers/spree/admin/resource_controller.rb
+++ core/app/controllers/spree/admin/resource_controller.rb
@@ -192,7 +192,7 @@ class Spree::Admin::ResourceController < Spree::Admin::BaseController
     def collection
       return parent.send(controller_name) if parent_data.present?
       if model_class.respond_to?(:accessible_by) && !current_ability.has_block?(params[:action], model_class)
-        model_class.accessible_by(current_ability, params[:action])
+        model_class.accessible_by(current_ability, params[:action].to_sym)
       else
         model_class.scoped
       end
