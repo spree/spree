2f3d741133cf5af93f4dc6dc892f12e5473bf6fb
cmar <christopher.mar@gmail.com>
2013-02-25 15:40:11 -0500

    handle 1 item packages with the weight splitter

--- core/spec/models/spree/stock/splitter/weight_spec.rb
+++ core/spec/models/spree/stock/splitter/weight_spec.rb
@@ -9,15 +9,24 @@ module Spree
 
         subject { Weight.new(packer) }
 
-        it 'splits package by weight' do
+        it 'splits and keeps splitting until all packages are underweight' do
           package = Package.new(packer.stock_location, packer.order)
           package.add variant, 1
           package.add variant, 1
+          package.add variant, 1
+          package.add variant, 1
+          packages = subject.split([package])
+          packages.size.should eq 4
+        end
+
+        it 'handles packages that can not be reduced' do
+          package = Package.new(packer.stock_location, packer.order)
+          variant.stub(:weight => 200)
+          package.add variant, 4
+          package.add variant, 4
           packages = subject.split([package])
           packages.size.should eq 2
         end
-
-        pending 'handles packages that can not be reduced'
 
       end
     end
