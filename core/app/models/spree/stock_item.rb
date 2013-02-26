630afbb7f2549556fb1bc914e16a2e3802cc8f15
Ryan Bigg <radarlistener@gmail.com>
2013-02-26 15:37:19 -0500

    Move responsibility of processing backorders from Spree::Variant to Spree::StockItem
    This will also only process backorders which are relevant for this stock item's stock location

--- core/app/models/spree/stock_item.rb
+++ core/app/models/spree/stock_item.rb
@@ -8,6 +8,8 @@ module Spree
 
     attr_accessible :count_on_hand, :variant
 
+    after_save :process_backorders
+
     delegate :weight, :to => :variant
 
     def can_backorder?
@@ -17,6 +19,35 @@ module Spree
     def self.locations_for_variant(variant)
       where(variant_id: variant).map(&:stock_location)
     end
+
+    def backordered_inventory_units
+      Spree::InventoryUnit.backordered_for_stock_item(self)
+    end
+
+    private
+      def process_backorders
+        if count_changes = changes['count_on_hand']
+          new_level = count_changes.last
+
+          if Spree::Config[:track_inventory_levels] # && !self.on_demand
+            new_level = new_level.to_i
+
+            # update backorders if level is positive
+            if new_level > 0
+              # fill backordered orders before creating new units
+              backordered_units = backordered_inventory_units
+              # "lucky" because they're within the range of possibility to be filled
+              # Inventory units are only "backfilled" up to the available product levels
+              lucky_backordered_units = backordered_units.slice(0, new_level)
+
+              lucky_backordered_units.each(&:fill_backorder)
+              new_level -= lucky_backordered_units.length
+            end
+
+            self.update_column(:count_on_hand, new_level)
+          end
+        end
+      end
 
   end
 end
