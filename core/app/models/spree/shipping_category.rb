72e50aafc9d98430998fe785e68ac7d0e3e227bc
John Dyer <lbrapid@gmail.com>
2013-03-01 13:34:43 -0500

    Set up many-to-many association between shipping methods and shipping categories

--- core/app/models/spree/shipping_category.rb
+++ core/app/models/spree/shipping_category.rb
@@ -2,7 +2,8 @@ module Spree
   class ShippingCategory < ActiveRecord::Base
     validates :name, :presence => true
     has_many :products
-    has_many :shipping_methods
+    has_many :shipping_method_categories
+    has_many :shipping_methods, :through => :shipping_method_categories
 
     attr_accessible :name
   end
