d1047d59b974e744bed8c81ca6a9cebd1f85e750
Ryan Bigg <radarlistener@gmail.com>
2013-03-03 13:45:11 -0500

    Refactor Spree::Money's with_currency and no_cents option settings

--- core/lib/spree/money.rb
+++ core/lib/spree/money.rb
@@ -7,9 +7,9 @@ module Spree
     def initialize(amount, options={})
       @money = ::Money.parse([amount, (options[:currency] || Spree::Config[:currency])].join)
       @options = {}
-      @options[:with_currency] = true if Spree::Config[:display_currency]
+      @options[:with_currency] = Spree::Config[:display_currency]
       @options[:symbol_position] = Spree::Config[:currency_symbol_position].to_sym
-      @options[:no_cents] = true if Spree::Config[:hide_cents]
+      @options[:no_cents] = Spree::Config[:hide_cents]
       @options.merge!(options)
       # Must be a symbol because the Money gem doesn't do the conversion
       @options[:symbol_position] = @options[:symbol_position].to_sym
