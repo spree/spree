e9a072e311f42a42d7d651689331f083218f302d
Alexey Topolyanskiy <devilcoders@gmail.com>
2013-03-05 15:10:28 +0200

    Various small changes (navigation/github removal/getting started button removal)

--- guides/static/shared/js/documentation.js
+++ guides/static/shared/js/documentation.js
@@ -8,7 +8,7 @@ $(function() {
 
   // Sidebar menu
   var sidebar_menu = $("#sidebar-menu")
-  
+
   sidebar_menu.find('h3 a.js-expand-btn').click(function(e){
     e.preventDefault();
     var icon = $(this).find('i');
@@ -20,16 +20,28 @@ $(function() {
       icon.parent().parent().next().stop().slideDown();
     }
     else{
-      icon.removeClass('icon-down-open').addClass('icon-right-open');  
+      icon.removeClass('icon-down-open').addClass('icon-right-open');
       sidebar_menu.find('a.active').removeClass('active');
       icon.parent().parent().next().stop().slideUp();
+    }
+  });
+
+  sidebar_menu.find('.js-guides li i').toggle(function(){
+    if($(this).parent().find('ul').length > 0){
+      $(this).removeClass('icon-right-dir').addClass('icon-down-dir');
+      $(this).parent().find('ul').stop().slideDown();
+    }
+  }, function(){
+    if($(this).parent().find('ul').length > 0){
+      $(this).removeClass('icon-down-dir').addClass('icon-right-dir');
+      $(this).parent().find('ul').stop().slideUp();
     }
   });
 
   var current_url = window.location.pathname
 
   var active_menu = sidebar_menu.find('a[href="'+current_url+'"]')
-  active_menu.addClass('active-open');  
+  active_menu.addClass('active-open');
   if(active_menu.parent().next().attr('class') == 'js-guides'){
     active_menu.parent().next().show();
     active_menu.prev().find('i.icon-right-open').removeClass('icon-right-open').addClass('icon-down-open')
@@ -37,5 +49,5 @@ $(function() {
   else {
     active_menu.parent().parent().show()
   }
-  
+
 });
