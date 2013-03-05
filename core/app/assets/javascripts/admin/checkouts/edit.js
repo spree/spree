bb52df516a0ac79176c308379f8a29c67d5489bd
Jeff Dutil <jdutil@burlingtonwebapps.com>
2013-03-05 10:39:52 -0500

    Fix user autocomplete for admin orders.
    Fixes #2655

--- core/app/assets/javascripts/admin/checkouts/edit.js
+++ core/app/assets/javascripts/admin/checkouts/edit.js
@@ -48,7 +48,7 @@ $(document).ready(function(){
       source: function(request, response) {
         var params = { q: $('#customer_search').val(),
                        authenticity_token: encodeURIComponent($('meta[name=csrf-token]').attr("content")) }
-        $.get(Spree.routes.user_search + '&' + jQuery.param(params), function(data) {
+        $.get(Spree.routes.user_search + '?' + jQuery.param(params), function(data) {
           result = prep_user_autocomplete_data(data)
           response(result);
         });
