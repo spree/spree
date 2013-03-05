1a921d9584f1614f93aa4148f535d4f7ab7b9155
Andrew Hooker <geekoncoffee@gmail.com>
2013-03-05 10:36:07 -0600

    More Spec Fixes

--- core/lib/spree/testing_support/factories/shipping_method_factory.rb
+++ core/lib/spree/testing_support/factories/shipping_method_factory.rb
@@ -1,18 +1,18 @@
 FactoryGirl.define do
   factory :shipping_method, :class => Spree::ShippingMethod do
-    zone { |a| Spree::Zone.find_by_name('GlobalZone') || a.association(:global_zone) }
+    zones { |a| [Spree::Zone.global] }
     name 'UPS Ground'
     calculator { FactoryGirl.build(:calculator) }
   end
 
   factory :free_shipping_method, :class => Spree::ShippingMethod do
-    zone { |a| Spree::Zone.find_by_name('GlobalZone') || a.association(:global_zone) }
+    zones { |a| [Spree::Zone.global] }
     name 'UPS Ground'
     calculator { FactoryGirl.build(:no_amount_calculator) }
   end
 
   factory :shipping_method_with_category, :class => Spree::ShippingMethod do
-    zone { |a| Spree::Zone.find_by_name('GlobalZone') || a.association(:global_zone) }
+    zones { |a| [Spree::Zone.global] }
     name 'UPS Ground'
     calculator { FactoryGirl.build(:calculator) }
     after(:create) do |shipping_method, evaluator|
