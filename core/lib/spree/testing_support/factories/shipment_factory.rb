77961b57d25276ad62672d4481d011bdfa58b6b4
cmar <christopher.mar@gmail.com>
2013-03-05 12:17:31 -0500

    shipment has many shipping methods, with one as selected

--- core/lib/spree/testing_support/factories/shipment_factory.rb
+++ core/lib/spree/testing_support/factories/shipment_factory.rb
@@ -1,11 +1,13 @@
 FactoryGirl.define do
   factory :shipment, :class => Spree::Shipment do
     order { FactoryGirl.create(:order) }
-    shipping_method { FactoryGirl.create(:shipping_method) }
     tracking 'U10000'
     number '100'
     cost 100.00
     address { FactoryGirl.create(:address) }
     state 'pending'
+    after(:create) do |shipment, evalulator|
+      shipment.add_shipping_method(create(:shipping_method), true)
+    end
   end
 end
