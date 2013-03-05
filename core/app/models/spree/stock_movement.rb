cc275d2f4e15ad861f8a615581a1834623b9f816
John Dyer <lbrapid@gmail.com>
2013-03-05 12:45:11 -0500

    Basic StockMovement functionality

--- core/app/models/spree/stock_movement.rb
+++ core/app/models/spree/stock_movement.rb
@@ -3,5 +3,21 @@ module Spree
     belongs_to :stock_item
 
     attr_accessible :action, :quantity
+
+    after_save :update_stock_item_quantity
+
+    validates :action, inclusion: { in: %w(sold received), message: "%{value} is not a valid action" }
+
+    private
+
+    def update_stock_item_quantity
+      variant = stock_item.variant
+
+      if action == "sold"
+        stock_item.stock_location.decrease_stock_for_variant(variant, -quantity)
+      else
+        stock_item.stock_location.increase_stock_for_variant(variant, quantity)
+      end
+    end
   end
 end
--- core/spec/models/spree/stock_movement_spec.rb
+++ core/spec/models/spree/stock_movement_spec.rb
@@ -2,9 +2,49 @@ require 'spec_helper'
 
 describe Spree::StockItem do
   let(:stock_item) { create(:stock_item, stock_location: create(:stock_location)) }
-  subject { create(:stock_movement, :sold, stock_item: stock_item) }
+  subject { build(:stock_movement, :sold, stock_item: stock_item) }
 
   it 'should belong to a stock item' do
     subject.should respond_to(:stock_item)
+  end
+
+  it 'can set the action to sold' do
+    subject.save.should be_true
+  end
+
+  it 'can set the action to received' do
+    subject.action = 'received'
+    subject.save.should be_true
+  end
+
+  it 'cannot set the action unless it is sold/received' do
+    subject.action = 'invalid'
+    subject.save.should be_false
+    subject.errors.messages[:action].should include("invalid is not a valid action")
+  end
+
+  context "when action is sold" do
+    context "after save" do
+      it "should decrement the stock item count on hand" do
+        stock_item.stock_location.should_receive(:decrease_stock_for_variant).with(
+          stock_item.variant,
+          -subject.quantity
+        )
+        subject.save
+      end
+    end
+  end
+
+  context "when action is received" do
+    context "after save" do
+      it "should increment the stock item count on hand" do
+        subject.action = 'received'
+        stock_item.stock_location.should_receive(:increase_stock_for_variant).with(
+          stock_item.variant,
+          subject.quantity
+        )
+        subject.save
+      end
+    end
   end
 end
