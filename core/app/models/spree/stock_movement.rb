90aa582ae39d9a022861601de055eae60cec3ec2
John Dyer <lbrapid@gmail.com>
2013-03-06 11:52:30 -0500

    Stock movements Admin UI

--- core/app/models/spree/stock_movement.rb
+++ core/app/models/spree/stock_movement.rb
@@ -2,11 +2,12 @@ module Spree
   class StockMovement < ActiveRecord::Base
     belongs_to :stock_item
 
-    attr_accessible :action, :quantity, :stock_item
+    attr_accessible :action, :quantity, :stock_item, :stock_item_id
 
     after_save :update_stock_item_quantity
 
     validates :action, inclusion: { in: %w(sold received), message: "%{value} is not a valid action" }
+    validates :stock_item, presence: true
 
     private
 
