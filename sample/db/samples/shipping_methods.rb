a4fe5063ee2d816ab2b7566ac3e46862057e2e52
Andrew Hooker <geekoncoffee@gmail.com>
2013-03-06 13:37:13 -0600

    Fixing Seeds for multizone

--- sample/db/samples/shipping_methods.rb
+++ sample/db/samples/shipping_methods.rb
@@ -11,27 +11,23 @@ europe_vat = Spree::Zone.find_by_name!("EU_VAT")
 shipping_methods = [
   {
     :name => "UPS Ground (USD)",
-    :zone => north_america,
-    :calculator => Spree::Calculator::FlatRate.create!,
-    :tracking_url => "http://wwwapps.ups.com/WebTracking/track?track=yes&trackNums=:tracking"
+    :zones => [north_america],
+    :calculator => Spree::Calculator::FlatRate.create!
   },
   {
     :name => "UPS Two Day (USD)",
-    :zone => north_america,
-    :calculator => Spree::Calculator::FlatRate.create!,
-    :tracking_url => "http://wwwapps.ups.com/WebTracking/track?track=yes&trackNums=:tracking"
+    :zones => [north_america],
+    :calculator => Spree::Calculator::FlatRate.create!
   },
   {
     :name => "UPS One Day (USD)",
-    :zone => north_america,
-    :calculator => Spree::Calculator::FlatRate.create!,
-    :tracking_url => "http://wwwapps.ups.com/WebTracking/track?track=yes&trackNums=:tracking"
+    :zones => [north_america],
+    :calculator => Spree::Calculator::FlatRate.create!
   },
   {
     :name => "UPS Ground (EUR)",
-    :zone => europe_vat,
-    :calculator => Spree::Calculator::FlatRate.create!,
-    :tracking_url => "http://wwwapps.ups.com/WebTracking/track?track=yes&trackNums=:tracking"
+    :zones => [europe_vat],
+    :calculator => Spree::Calculator::FlatRate.create!
   }
 ]
 
