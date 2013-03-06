2ddbd66911c65a52906df6a76194c47a57b7ecf6
cmar <christopher.mar@gmail.com>
2013-03-06 15:09:09 -0500

    allow stock_locations to be inactive

--- core/lib/spree/testing_support/factories/stock_location_factory.rb
+++ core/lib/spree/testing_support/factories/stock_location_factory.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :stock_location, :class => Spree::StockLocation do
     name 'NY Warehouse'
+    active true
 
     address1 '1600 Pennsylvania Ave NW'
     city 'Washington'
