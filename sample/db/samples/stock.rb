216fc391fa195e2d0a95fd821ed31e17f1124a79
Brian Quinn <briandquinn@gmail.com>
2013-03-07 10:57:32 +0000

    Tweak stock location sample

--- sample/db/samples/stock.rb
+++ sample/db/samples/stock.rb
@@ -1,10 +1,10 @@
 Spree::Sample.load_sample("variants")
 
-location = Spree::StockLocation.first_or_create! do |stock_location|
-  stock_location.name = 'default'
-  stock_location.active = true
-  stock_location.country =  Spree::Countries.where(iso: 'US').first
-end
+location = Spree::StockLocation.first_or_create!
+location.name = 'default'
+location.active = true
+location.country =  Spree::Country.where(iso: 'US').first
+location.save!
 
 Spree::Variant.all.each do |variant|
   location.stock_items.create(variant: variant, count_on_hand: 10)
