3e3a5d7ce2e0cf3d84b4965bdc8711e027f97ac7
Washington Luiz <huoxito@gmail.com>
2013-03-08 02:37:39 -0300

    Fix backend variant search autocomplete
    I had to include products on api/variants otherwise it could just not be
    possible to search for product names

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -21,8 +21,7 @@ Spree::Core::Engine.routes.prepend do
       end
     end
 
-    resources :variants, :only => [:index] do
-    end
+    resources :variants, :only => [:index]
 
     resources :option_types do
       resources :option_values
