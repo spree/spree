c8af7775aff8b9d6f366ce9b823813c2ef4ce24a
Jeff Dutil <jdutil@burlingtonwebapps.com>
2013-03-08 03:57:52 -0500

    Fix backorderable migration for postgresql.
    Fixes #2671

--- core/db/migrate/20130306195650_add_backorderable_to_stock_item.rb
+++ core/db/migrate/20130306195650_add_backorderable_to_stock_item.rb
@@ -1,5 +1,5 @@
 class AddBackorderableToStockItem < ActiveRecord::Migration
   def change
-    add_column :spree_stock_items, :backorderable, :boolean, :default => 1
+    add_column :spree_stock_items, :backorderable, :boolean, :default => true
   end
 end
