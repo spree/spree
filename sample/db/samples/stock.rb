4ba84d2f7034f24c2f94f3a2407cc0fff20e118b
Jeff Dutil <jdutil@burlingtonwebapps.com>
2013-03-08 04:12:14 -0500

    Fix spree_sample:load for stock.

--- sample/db/samples/stock.rb
+++ sample/db/samples/stock.rb
@@ -1,7 +1,6 @@
 Spree::Sample.load_sample("variants")
 
-location = Spree::StockLocation.first_or_create!
-location.name = 'default'
+location = Spree::StockLocation.first_or_create! name: 'default'
 location.active = true
 location.country =  Spree::Country.where(iso: 'US').first
 location.save!
