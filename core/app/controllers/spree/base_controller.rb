1f6ab460babb64e328585cd0b5530a58e50a2653
Jeff Dutil <jdutil@burlingtonwebapps.com>
2013-03-08 04:20:46 -0500

    Fix undefined method truncated_product_description in frontend.

--- core/app/controllers/spree/base_controller.rb
+++ core/app/controllers/spree/base_controller.rb
@@ -1,6 +1,7 @@
 require 'cancan'
 
 class Spree::BaseController < ApplicationController
+  helper 'spree/orders'
   include Spree::Core::ControllerHelpers::Auth
   include Spree::Core::ControllerHelpers::RespondWith
   include Spree::Core::ControllerHelpers::Common
