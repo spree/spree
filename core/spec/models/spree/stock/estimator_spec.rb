d78422fd95537b32988e915f1acb94e789586105
cmar <christopher.mar@gmail.com>
2013-03-12 09:55:01 -0400

    fix specs for spree/stock

--- core/spec/models/spree/stock/estimator_spec.rb
+++ core/spec/models/spree/stock/estimator_spec.rb
@@ -15,42 +15,34 @@ module Spree
           ShippingMethod.any_instance.stub_chain(:calculator, :available?).and_return(true)
           ShippingMethod.any_instance.stub_chain(:calculator, :compute).and_return(4.00)
           ShippingMethod.any_instance.stub_chain(:calculator, :preferences).and_return({:currency => "USD"})
+
+          package.stub(:shipping_category => shipping_category)
         end
 
         it "returns shipping rates from a shipping method if the order's ship address is in the same zone" do
-          package.should_receive(:shipping_category).and_return(shipping_category)
-
           shipping_rates = subject.shipping_rates(package)
           shipping_rates.first.cost.should eq 4.00
         end
 
         it "does not return shipping rates from a shipping method if the order's ship address is in a different zone" do
           shipping_method.zones.each{|z| z.members.delete_all}
-          package.should_receive(:shipping_category).and_return(shipping_category)
-
           shipping_rates = subject.shipping_rates(package)
           shipping_rates.should == []
         end
 
         it "does not return shipping rates from a shipping method if the calculator is not available for that order" do
           ShippingMethod.any_instance.stub_chain(:calculator, :available?).and_return(false)
-          package.should_receive(:shipping_category).and_return(shipping_category)
-
           shipping_rates = subject.shipping_rates(package)
           shipping_rates.should == []
         end
 
         it "returns shipping rates from a shipping method if the currency matches the order's currency" do
-          package.should_receive(:shipping_category).and_return(shipping_category)
-
           shipping_rates = subject.shipping_rates(package)
           shipping_rates.first.cost.should eq 4.00
         end
 
         it "does not return shipping rates from a shipping method if the currency is different than the order's currency" do
           order.currency = "GBP"
-          package.should_receive(:shipping_category).and_return(shipping_category)
-
           shipping_rates = subject.shipping_rates(package)
           shipping_rates.should == []
         end
