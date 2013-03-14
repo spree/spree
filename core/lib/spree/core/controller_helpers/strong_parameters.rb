8de2bb15d8188e323986038c16f9c8b1308306de
Ryan Bigg <radarlistener@gmail.com>
2013-03-14 12:16:27 -0700

    Add state_name, alternative_phone and company to address parameters

--- core/lib/spree/core/controller_helpers/strong_parameters.rb
+++ core/lib/spree/core/controller_helpers/strong_parameters.rb
@@ -8,7 +8,8 @@ module Spree
 
         def permitted_address_attributes
            [:firstname, :lastname, :address1, :address2,
-            :city, :country_id, :state_id, :zipcode, :phone]
+            :city, :country_id, :state_id, :zipcode, :phone,
+            :state_name, :alternative_phone, :company]
         end
 
         def permitted_source_attributes
