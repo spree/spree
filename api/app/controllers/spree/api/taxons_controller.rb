e318f315050f5a4f2c26953780eabafc0eddd941
John Sucaet <john.sucaet@gmail.com>
2013-03-14 23:06:37 +0100

    Revert "Fix issue with https connections when making api requests"
    This reverts commit faa661a96672cc0fb9f0291f2817547bb71652df.

--- api/app/controllers/spree/api/taxons_controller.rb
+++ api/app/controllers/spree/api/taxons_controller.rb
@@ -2,7 +2,6 @@ module Spree
   module Api
     class TaxonsController < Spree::Api::BaseController
       respond_to :json
-      ssl_allowed :index, :show, :jstree, :create, :update, :destroy
 
       def index
         if taxonomy
