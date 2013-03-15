9ae577f92fcdb0d6d44b5c3591bc1dbbc065a85e
Chris Hunt <c@chrishunt.co>
2013-03-15 12:13:34 -0700

    Empty user promotion rule should not be eligible
    Fixes #2749

--- promo/spec/models/promotion/rules/user_spec.rb
+++ promo/spec/models/promotion/rules/user_spec.rb
@@ -6,17 +6,13 @@ describe Spree::Promotion::Rules::User do
   context "#eligible?(order)" do
     let(:order) { Spree::Order.new }
 
-    it "should be eligible if users are not provided" do
-      users = mock("users", :none? => true)
-      rule.stub(:users => users)
-
-      rule.should be_eligible(order)
+    it "should not be eligible if users are not provided" do
+      rule.should_not be_eligible(order)
     end
 
     it "should be eligible if users include user placing the order" do
       user = mock_model(Spree::LegacyUser)
       users = [user, mock_model(Spree::LegacyUser)]
-      users.stub(:none? => false)
       rule.stub(:users => users)
       order.stub(:user => user)
 
@@ -26,7 +22,6 @@ describe Spree::Promotion::Rules::User do
     it "should not be eligible if user placing the order is not listed" do
       order.stub(:user => mock_model(Spree::LegacyUser))
       users = [mock_model(Spree::LegacyUser), mock_model(Spree::LegacyUser)]
-      users.stub(:none? => false)
       rule.stub(:users => users)
 
       rule.should_not be_eligible(order)
