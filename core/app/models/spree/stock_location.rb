dbb4ef034aa85bc865ba88289662fb07fd399022
Andrew Hooker <geekoncoffee@gmail.com>
2013-03-18 11:57:13 -0500

    Create Stock Items when Stock Location is created

--- core/app/models/spree/stock_location.rb
+++ core/app/models/spree/stock_location.rb
@@ -15,6 +15,8 @@ module Spree
 
     after_create :create_stock_items
 
+    after_create :populate_stock_items
+
     def stock_item(variant)
       stock_items.where(variant_id: variant).first
     end
