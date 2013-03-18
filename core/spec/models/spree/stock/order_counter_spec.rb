b3e692b79da78c212baa1871971b94feb4d997b7
cmar <christopher.mar@gmail.com>
2013-03-18 11:46:12 -0400

    determine variants with remaining items

--- core/spec/models/spree/stock/order_counter_spec.rb
+++ core/spec/models/spree/stock/order_counter_spec.rb
@@ -16,6 +16,7 @@ module Spree
       subject { OrderCounter.new(order) }
 
       its(:variants) { should eq [variant1, variant2] }
+      its(:variants_with_remaining) { should eq [variant1] }
 
       it 'counts ordered' do
         subject.ordered(variant1).should eq 2
