cb831a6cf48714f43fd33806a34234516a7ce38b
Jeff Dutil <jdutil@burlingtonwebapps.com>
2013-03-20 08:29:49 -0400

    Add simplecov for spec coverage reporting.
    Fixes #2723

--- api/spec/spec_helper.rb
+++ api/spec/spec_helper.rb
@@ -1,3 +1,16 @@
+if ENV["COVERAGE"]
+  # Run Coverage report
+  require 'simplecov'
+  SimpleCov.start do
+    add_group 'Controllers', 'app/controllers'
+    add_group 'Helpers', 'app/helpers'
+    add_group 'Mailers', 'app/mailers'
+    add_group 'Models', 'app/models'
+    add_group 'Views', 'app/views'
+    add_group 'Libraries', 'lib'
+  end
+end
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../dummy/config/environment", __FILE__)
