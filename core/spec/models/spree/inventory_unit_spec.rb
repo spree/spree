42561f18f272dc7b88c3211d6d68889fa6bb8bee
Brian Quinn <briandquinn@gmail.com>
2013-03-25 11:16:43 +0000

    Drop create_units and destroy_units method

--- core/spec/models/spree/inventory_unit_spec.rb
+++ core/spec/models/spree/inventory_unit_spec.rb
@@ -50,66 +50,6 @@ describe Spree::InventoryUnit do
     end
   end
 
-  context "#create_units" do
-    let(:shipment) { mock_model(Spree::Shipment) }
-    before { order.shipments.stub :detect => shipment }
-
-    context "when :allow_backorders is true" do
-      before { Spree::Config.set :allow_backorders => true }
-
-      it "should create both sold and backordered units" do
-        order.inventory_units.should_receive(:create).with({:variant => variant, :state => "sold", :shipment => shipment}, :without_protection => true).exactly(2).times
-        order.inventory_units.should_receive(:create).with({:variant => variant, :state => "backordered", :shipment => shipment}, :without_protection => true).exactly(3).times
-        Spree::InventoryUnit.create_units(order, variant, 2, 3)
-      end
-
-    end
-
-    context "when :allow_backorders is false" do
-      before { Spree::Config.set :allow_backorders => false }
-
-      it "should create sold items" do
-        order.inventory_units.should_receive(:create).with({:variant => variant, :state => "sold", :shipment => shipment}, :without_protection => true).exactly(2).times
-        Spree::InventoryUnit.create_units(order, variant, 2, 0)
-      end
-
-    end
-
-  end
-
-  context "#destroy_units" do
-    before { order.stub(:inventory_units => [mock_model(Spree::InventoryUnit, :variant_id => variant.id, :state => "sold")]) }
-
-    it "should call destroy correct number of units" do
-      order.inventory_units.each { |unit| unit.should_receive(:destroy) }
-      Spree::InventoryUnit.destroy_units(order, variant, 1)
-    end
-
-    context "when inventory_units contains backorders" do
-      before { order.stub(:inventory_units => [ mock_model(Spree::InventoryUnit, :variant_id => variant.id, :state => 'backordered'),
-                                                mock_model(Spree::InventoryUnit, :variant_id => variant.id, :state => 'sold'),
-                                                mock_model(Spree::InventoryUnit, :variant_id => variant.id, :state => 'backordered') ]) }
-
-      it "should destroy backordered units first" do
-        order.inventory_units[0].should_receive(:destroy)
-        order.inventory_units[1].should_not_receive(:destroy)
-        order.inventory_units[2].should_receive(:destroy)
-        Spree::InventoryUnit.destroy_units(order, variant, 2)
-      end
-    end
-
-    context "when inventory_units contains sold and shipped" do
-      before { order.stub(:inventory_units => [ mock_model(Spree::InventoryUnit, :variant_id => variant.id, :state => 'shipped'),
-                                                mock_model(Spree::InventoryUnit, :variant_id => variant.id, :state => 'sold') ]) }
-      # Regression test for #1652
-      it "should not destroy shipped" do
-        order.inventory_units[0].should_not_receive(:destroy)
-        order.inventory_units[1].should_receive(:destroy)
-        Spree::InventoryUnit.destroy_units(order, variant, 1)
-      end
-    end
-  end
-
   context "return!" do
     let(:inventory_unit) { Spree::InventoryUnit.create({:state => "shipped", :variant => mock_model(Spree::Variant, :on_hand => 95)}, :without_protection => true) }
 
