19e208c7b7f2368a1d11d7e178d60201202f1b7c
Ryan Bigg <radarlistener@gmail.com>
2013-03-27 17:40:18 +1100

    Revert "Don't require payment methods if payment isn't required"
    This reverts commit 318b820114d6d631e9be6ddb500e6a234612407f.
    
    This is causing build breakages:
    
    http://ci.spreecommerce.com/viewLog.html?buildId=1533&buildTypeId=bt5&tab=buildLog
    
    payment_required? is calling update_totals, which causes a massive callback
    chain and ultimately results in these tests failing

--- core/app/models/spree/order.rb
+++ core/app/models/spree/order.rb
@@ -73,7 +73,7 @@ module Spree
     validates :email, :presence => true, :if => :require_email
     validates :email, :email => true, :if => :require_email, :allow_blank => true
     validate :has_available_shipment
-    validate :has_available_payment, :if => :payment_required?
+    validate :has_available_payment
 
     make_permalink :field => :number
 
