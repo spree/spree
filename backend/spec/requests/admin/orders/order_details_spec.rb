868a58a56d47e9ccdd352d77ccf83e15afc6933a
John Dyer <lbrapid@gmail.com>
2013-03-28 14:28:51 -0400

    Admin order edit request specs

--- backend/spec/requests/admin/orders/order_details_spec.rb
+++ backend/spec/requests/admin/orders/order_details_spec.rb
@@ -4,32 +4,30 @@ require 'spec_helper'
 describe "Order Details" do
   stub_authorization!
 
-  context "edit order page" do
-
-    before do
-      create(:country)
-      create(:stock_location_with_items)
-    end
-
+  context "edit order page", :js => true do
     after(:each) { I18n.reload! }
 
     let(:product) { create(:product, :name => 'spree t-shirt', :price => 20.00) }
     let(:order) { create(:order, :state => 'complete', :completed_at => "2011-02-01 12:36:15", :number => "R100") }
-    let(:shipment) { create(:shipment, :order => order) }
+    let(:stock_location) { create(:stock_location_with_items) }
+    let(:shipment) { create(:shipment, :order => order, :stock_location => stock_location) }
 
-    it "should allow me to edit order details", :js => true do
-      shipment.add(product.master, 2)
-      visit spree.admin_path
-      click_link "Orders"
-
-      within_row(1) do
-        click_link "R100"
+    before do
+      configure_spree_preferences do |config|
+        config.allow_backorders = true
       end
+      create(:country)
+      create(:shipping_method, :name => "Default")
+      create(:product, :name => "Tote", :price => 15.00)
+      shipment.add(product.master, 2)
+    end
 
+    it "should allow me to edit order details" do
+      visit spree.edit_admin_order_path(order)
       page.should have_content("spree t-shirt")
       page.should have_content("$40.00")
 
-      within("table.stock-contents") do
+      within_row(1) do
         click_icon :edit
         fill_in "quantity", :with => "1"
       end
@@ -38,33 +36,50 @@ describe "Order Details" do
       page.should have_content("Total: $20.00")
     end
 
-    it "should render details properly" do
-      order.state = :complete
-      order.currency = 'GBP'
-      order.save!
-
+    it "can add an item to a shipment" do
       visit spree.edit_admin_order_path(order)
 
-      find(".page-title").text.strip.should == "Order #R100"
-
-      within ".additional-info" do
-        find(".state").text.should == "complete"
-        find("#shipment_status").text.should == "none"
-        find("#payment_status").text.should == "none"
+      select2_search "Tote", :from => I18n.t(:name_or_sku)
+      within("table.stock-levels") do
+        fill_in "stock_item_quantity", :with => 2
+        click_icon :plus
       end
+      sleep 1
+      page.should have_content("Total: $70.00")
+    end
 
-      I18n.backend.store_translations I18n.locale,
-        :shipment_states => { :missing => 'some text' },
-        :payment_states  => { :missing => 'other text' }
-
+    it "can remove an item from a shipment" do
       visit spree.edit_admin_order_path(order)
+      page.should have_content("spree t-shirt")
 
-      within ".additional-info" do
-        find("#order_total").text.should == "£0.00"
-        find("#shipment_status").text.should == "some text"
-        find("#payment_status").text.should == "other text"
+      within_row(1) do
+        click_icon :trash
       end
+      sleep 1
+      page.should_not have_content("spree t-shirt")
+    end
 
+    it "can add tracking information" do
+      visit spree.edit_admin_order_path(order)
+      within("table.index tr:nth-child(5)") do
+        click_icon :edit
+      end
+      fill_in "tracking", :with => "FOOBAR"
+      click_icon :ok
+      sleep 1
+      page.should have_content("Tracking: FOOBAR")
+    end
+
+    it "can add change the shipping method" do
+      order = create(:completed_order_with_totals)
+      visit spree.edit_admin_order_path(order)
+      within("table.index tr:nth-child(3)") do
+        click_icon :edit
+      end
+      select2 "Default", :from => "Shipping Method"
+      click_icon :ok
+      sleep 1
+      page.should have_content("Default:")
     end
   end
 end
