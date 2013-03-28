36ab88236aba43bfedf3d27b0cf34fa41269a72f
Stephane Bounmy <stephanebounmy@gmail.com>
2013-03-28 20:13:19 +0100

    fix spec

--- core/spec/helpers/navigation_helper_spec.rb
+++ core/spec/helpers/navigation_helper_spec.rb
@@ -4,21 +4,25 @@ require 'spec_helper'
 describe Spree::Admin::NavigationHelper do
 
   describe "#tab" do
+    before do
+      helper.stub(:cannot?).and_return false
+    end
+
     context "creating an admin tab" do
       it "should capitalize the first letter of each word in the tab's label" do
-        admin_tab = tab(:orders)
+        admin_tab = helper.tab(:orders)
         admin_tab.should include("Orders")
       end
     end
 
     it "should accept options with label and capitalize each word of it" do
-      admin_tab = tab(:orders, :label => "delivered orders")
+      admin_tab = helper.tab(:orders, :label => "delivered orders")
       admin_tab.should include("Delivered Orders")
     end
 
     it "should capitalize words with unicode characters" do
       # overview
-      admin_tab = tab(:orders, :label => "přehled")
+      admin_tab = helper.tab(:orders, :label => "přehled")
       admin_tab.should include("Přehled")
     end
   end
