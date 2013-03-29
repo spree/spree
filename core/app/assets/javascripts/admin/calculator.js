b7e213d2eceb8a58c7ce5dcfc942a8368a104ada
github@assembledgoods.com <github@assembledgoods.com>
2013-03-29 10:46:18 -0500

    When a calculator is using a preference of type :text, a textarea is shown. The calculator.js should disable/enable this textarea the same as the input from a preference of any other type.
    Fixes #2782

--- core/app/assets/javascripts/admin/calculator.js
+++ core/app/assets/javascripts/admin/calculator.js
@@ -6,11 +6,11 @@ $(function() {
     if (calculator_select.attr('value') == original_calc_type) {
       $('div.calculator-settings').show();
       $('div#calculator-settings-warning').hide();
-      $('.calculator-settings input').prop("disabled", false);
+      $('.calculator-settings').find('input,textarea').prop("disabled", false);
     } else {
       $('div.calculator-settings').hide();
       $('div#calculator-settings-warning').show();
-      $('.calculator-settings input').prop("disabled", true);
+      $('.calculator-settings').find('input,textarea').prop("disabled", true);
     }
   });
 })
