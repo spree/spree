702b4498f54e387b8692fa2161d7542f4d6bf85d
Sean O'Hara <sohara@sohara.com>
2013-03-30 14:43:00 -0400

    Change migration to avoid tiggering callback and use private send:
    Skip :after callbacks on StockLocation and StockItem when creating
    intitial instances. Use private send to set StockItem.count_on_hand
    because :count_on_hand= is now private.

--- core/db/migrate/20130213191427_create_default_stock.rb
+++ core/db/migrate/20130213191427_create_default_stock.rb
@@ -1,8 +1,12 @@
 class CreateDefaultStock < ActiveRecord::Migration
   def up
+    Spree::StockLocation.skip_callback(:create, :after, :create_stock_items)
+    Spree::StockItem.skip_callback(:save, :after, :process_backorders)
     location = Spree::StockLocation.create(name: 'default')
     Spree::Variant.all.each do |variant|
-      location.stock_items.create(variant: variant, count_on_hand: variant.count_on_hand)
+      stock_item = location.stock_items.build(variant: variant)
+      stock_item.send(:count_on_hand=, variant.count_on_hand)
+      stock_item.save!
     end
 
     remove_column :spree_variants, :count_on_hand
