697bb1925063f7e58b844d7279fd619980e08fe0
Sean O'Hara <sohara@sohara.com>
2013-03-30 15:47:32 -0400

    Fix ambiguous column name in UpdateShipmentStateForCanceledOrders migration

--- core/db/migrate/20130328130308_update_shipment_state_for_canceled_orders.rb
+++ core/db/migrate/20130328130308_update_shipment_state_for_canceled_orders.rb
@@ -1,6 +1,6 @@
 class UpdateShipmentStateForCanceledOrders < ActiveRecord::Migration
   def up
-    Spree::Shipment.joins(:order).where("spree_orders.state = 'canceled'").update_all(state: 'canceled')
+    Spree::Shipment.joins(:order).where("spree_orders.state = 'canceled'").update_all("spree_shipments.state = 'canceled'")
   end
 
   def down
