3b21b2511aca03a95d4dbc15d43856b50a0f8a3e
Andrew Hooker <geekoncoffee@gmail.com>
2013-04-01 14:37:22 -0500

    Require Shipping Calculators to Inherit from Spree::Calculator::Shipping

--- core/spec/models/spree/shipping_method_spec.rb
+++ core/spec/models/spree/shipping_method_spec.rb
@@ -1,6 +1,20 @@
 require 'spec_helper'
 
 describe Spree::ShippingMethod do
+
+  context 'calculators' do
+    let(:shipping_method){ create :shipping_method}
+
+    it "Should reject calculators that don't inherit from Spree::Calculator::Shipping::" do
+      Spree::ShippingMethod.stub_chain(:spree_calculators, :shipping_methods).and_return([
+            Spree::Calculator::Shipping::FlatPercentItemTotal,
+            Spree::Calculator::Shipping::PriceSack,
+            Spree::Calculator::DefaultTax])
+      Spree::ShippingMethod.calculators.should == [Spree::Calculator::Shipping::FlatPercentItemTotal, Spree::Calculator::Shipping::PriceSack ]
+      Spree::ShippingMethod.calculators.should_not == [Spree::Calculator::DefaultTax]
+    end
+  end
+
   context 'factory' do
     let(:shipping_method){ create :shipping_method }
 
