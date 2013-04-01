42b5569027bffbed1213d09483d3269facc65b30
Trung Lê <joneslee85@gmail.com>
2013-04-01 12:53:56 +1100

    Refactor Spree::Zone:
    * ruby19 hash syntax
    * indentation
    * scope logic refactor

--- core/app/models/spree/zone.rb
+++ core/app/models/spree/zone.rb
@@ -9,13 +9,16 @@ module Spree
     after_save :remove_previous_default
 
     alias :members :zone_members
-    accepts_nested_attributes_for :zone_members, allow_destroy: true, reject_if: ->(m) { m[:zoneable_id].blank? }
+    accepts_nested_attributes_for :zone_members, allow_destroy: true,
+      reject_if: ->(m) { m[:zoneable_id].blank? }
 
-    attr_accessible :name, :description, :default_tax, :kind, :zone_members, :zone_members_attributes
+    attr_accessible :name, :description, :default_tax, :kind, :zone_members,
+                    :zone_members_attributes
 
     def kind
-      return nil if members.empty? || members.any? { |member| member.try(:zoneable_type).nil? }
-      members.last.zoneable_type.demodulize.downcase
+      if members.any? && !members.any? { |member| member.try(:zoneable_type).nil? }
+        members.last.zoneable_type.demodulize.downcase
+      end
     end
 
     def kind=(value)
@@ -40,7 +43,9 @@ module Spree
     # Returns the matching zone with the highest priority zone type (State, Country, Zone.)
     # Returns nil in the case of no matches.
     def self.match(address)
-      return unless matches = self.includes(:zone_members).order('zone_members_count', 'created_at').select { |zone| zone.include? address }
+      return unless matches = self.includes(:zone_members).
+        order('zone_members_count', 'created_at').
+        select { |zone| zone.include? address }
 
       ['state', 'country'].each do |zone_kind|
         if match = matches.detect { |zone| zone_kind == zone.kind }
@@ -52,15 +57,11 @@ module Spree
 
     # convenience method for returning the countries contained within a zone
     def country_list
-      @countries ||=
-      case kind
-      when 'country'
-        zoneables
-      when 'state'
-        zoneables.collect(&:country)
-      else
-        nil
-      end.flatten.compact.uniq
+      @countries ||= case kind
+                     when 'country' then zoneables
+                     when 'state' then zoneables.collect(&:country)
+                     else nil
+                     end.flatten.compact.uniq
     end
 
     def <=>(other)
@@ -84,13 +85,9 @@ module Spree
       return false if zone_members.empty? || target.zone_members.empty?
 
       if kind == target.kind
-        target.zoneables.each do |target_zoneable|
-          return false unless zoneables.include?(target_zoneable)
-        end
+        return false if target.zoneables.any? { |target_zoneable| zoneables.exclude?(target_zoneable) }
       else
-        target.zoneables.each do |target_state|
-          return false unless zoneables.include?(target_state.country)
-        end
+        return false if target.zoneables.any? { |target_state| zoneables.exclude?(target_state.country) }
       end
       true
     end
@@ -98,17 +95,13 @@ module Spree
     private
 
       def remove_defunct_members
-        zone_members.each do |zone_member|
-          zone_member.destroy if zone_member.zoneable_id.nil? || zone_member.zoneable_type != "Spree::#{kind.capitalize}"
+        if zone_members.any?
+          zone_members.where('zoneable_id IS NULL OR zoneable_type != ?', "Spree::#{kind.capitalize}").destroy_all
         end
       end
 
       def remove_previous_default
-        return unless default_tax
-
-        self.class.all.each do |zone|
-          zone.update_column 'default_tax', false unless zone == self
-        end
+        Spree::Zone.where('id != ?', self.id).update_all(default_tax: false) if default_tax
       end
   end
 end
