20b5ba145bb777d38afdb25b78781c940c2b0002
Peter M. Goldstein <peter.m.goldstein@gmail.com>
2013-04-03 00:07:43 -0700

    Switch the mailers to be invoked using ids, not model objects. This makes it easier to support asynchronous mail jobs, as only simple ids need to be persisted.
    Fixes #2808

--- core/app/controllers/spree/admin/orders_controller.rb
+++ core/app/controllers/spree/admin/orders_controller.rb
@@ -101,7 +101,7 @@ module Spree
       end
 
       def resend
-        OrderMailer.confirm_email(@order, true).deliver
+        OrderMailer.confirm_email(@order.id, true).deliver
         flash[:success] = t(:order_email_resent)
 
         respond_with(@order) { |format| format.html { redirect_to :back } }
