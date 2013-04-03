011057f59e7218c32b4b9c3ca0e88619c6aa4798
Ryan Bigg <radarlistener@gmail.com>
2013-04-03 11:40:48 +1100

    Fix syntax error in order model for go_to_state :payment

--- core/app/models/spree/order.rb
+++ core/app/models/spree/order.rb
@@ -15,13 +15,13 @@ module Spree
     checkout_flow do
       go_to_state :address
       go_to_state :delivery
-      go_to_state :payment, if: lambda { |order|
+      go_to_state :payment, if: ->(order) {
         # Fix for #2191
         if order.shipments
           order.update_totals
         end
         order.payment_required?
-      end
+      }
       go_to_state :confirm, if: ->(order) { order.confirmation_required? }
       go_to_state :complete, if: ->(order) {
         (order.payment_required? && order.has_unprocessed_payments?) || !order.payment_required?
