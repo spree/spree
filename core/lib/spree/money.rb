aca50ff5d5d7317b433ff21204fa68aaea30f55d
Ryan Bigg <radarlistener@gmail.com>
2013-04-04 11:04:20 +1100

    Add currency_thousands_separator and currency_decimal_mark config options
    This fixes an issue in #2632 where the incorrect decimal mark was being used

--- core/lib/spree/money.rb
+++ core/lib/spree/money.rb
@@ -10,6 +10,8 @@ module Spree
       @options[:with_currency] = Spree::Config[:display_currency]
       @options[:symbol_position] = Spree::Config[:currency_symbol_position].to_sym
       @options[:no_cents] = Spree::Config[:hide_cents]
+      @options[:decimal_mark] = Spree::Config[:currency_decimal_mark]
+      @options[:thousands_separator] = Spree::Config[:currency_thousands_separator]
       @options.merge!(options)
       # Must be a symbol because the Money gem doesn't do the conversion
       @options[:symbol_position] = @options[:symbol_position].to_sym
