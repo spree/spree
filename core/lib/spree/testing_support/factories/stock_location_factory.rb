9030ed652e022447039b28b67f2a4e7e9b280661
Ryan Bigg <radarlistener@gmail.com>
2013-04-04 17:11:47 +1100

    Adjust count on hand for stock items based on variant in stock location factory
    Given that stock_location.stock_items is unsorted, the behaviour of calling 'first' and 'last' is effectively random, leading it update the same stock item to have 30 items on hand.
    
    By explicitly defining that we want THIS stock item to be updated with 10 and THIS one with 20, we don't run into that problem.
    
    This is only a problem on MySQL when using InnoDB and on PostgreSQL always.

--- core/lib/spree/testing_support/factories/stock_location_factory.rb
+++ core/lib/spree/testing_support/factories/stock_location_factory.rb
@@ -14,11 +14,11 @@ FactoryGirl.define do
       after(:create) do |stock_location, evaluator|
         # variant will add itself to all stock_locations in an after_create
         # creating a product will automatically create a master variant
-        create(:product)
-        create(:product)
+        product_1 = create(:product)
+        product_2 = create(:product)
 
-        stock_location.stock_items.first.adjust_count_on_hand(10)
-        stock_location.stock_items.second.adjust_count_on_hand(20)
+        stock_location.stock_items.where(:variant_id => product_1.master.id).first.adjust_count_on_hand(10)
+        stock_location.stock_items.where(:variant_id => product_2.master.id).first.adjust_count_on_hand(20)
       end
     end
   end
