9cc073812da55c86a107aca3900487b8c3577260
Ryan Bigg <radarlistener@gmail.com>
2013-04-08 16:18:15 +1000

    [frontend] Move taxon link check tests into taxons_spec (from products_spec

--- frontend/spec/requests/taxons_spec.rb
+++ frontend/spec/requests/taxons_spec.rb
@@ -30,7 +30,7 @@ describe "viewing products" do
       visit '/t/category/clothing/t-shirts'
       page.should have_meta(:description, 'Brand new Ruby on Rails TShirts')
       page.should have_meta(:keywords, 'ror, tshirt, ruby')
-    end
+
 
     it 'display title if set' do
       t_shirts.update_attributes metas
@@ -43,5 +43,74 @@ describe "viewing products" do
       page.should have_title('Category - T-Shirts - Spree Demo Site')
     end
 
+  end
+
+  context "taxon pages" do
+    include_context "custom products"
+
+    it "should be able to visit brand Ruby on Rails" do
+      within(:css, '#taxonomies') { click_link "Ruby on Rails" }
+
+      page.all('ul.product-listing li').size.should == 7
+      tmp = page.all('ul.product-listing li a').map(&:text).flatten.compact
+      tmp.delete("")
+      array = ["Ruby on Rails Bag",
+       "Ruby on Rails Baseball Jersey",
+       "Ruby on Rails Jr. Spaghetti",
+       "Ruby on Rails Mug",
+       "Ruby on Rails Ringer T-Shirt",
+       "Ruby on Rails Stein",
+       "Ruby on Rails Tote"]
+      tmp.sort!.should == array
+    end
+
+    it "should be able to visit brand Ruby" do
+      within(:css, '#taxonomies') { click_link "Ruby" }
+
+      page.all('ul.product-listing li').size.should == 1
+      tmp = page.all('ul.product-listing li a').map(&:text).flatten.compact
+      tmp.delete("")
+      tmp.sort!.should == ["Ruby Baseball Jersey"]
+    end
+
+    it "should be able to visit brand Apache" do
+      within(:css, '#taxonomies') { click_link "Apache" }
+
+      page.all('ul.product-listing li').size.should == 1
+      tmp = page.all('ul.product-listing li a').map(&:text).flatten.compact
+      tmp.delete("")
+      tmp.sort!.should == ["Apache Baseball Jersey"]
+    end
+
+    it "should be able to visit category Clothing" do
+      click_link "Clothing"
+
+      page.all('ul.product-listing li').size.should == 5
+      tmp = page.all('ul.product-listing li a').map(&:text).flatten.compact
+      tmp.delete("")
+      tmp.sort!.should == ["Apache Baseball Jersey",
+     "Ruby Baseball Jersey",
+     "Ruby on Rails Baseball Jersey",
+     "Ruby on Rails Jr. Spaghetti",
+     "Ruby on Rails Ringer T-Shirt"]
+    end
+
+    it "should be able to visit category Mugs" do
+      click_link "Mugs"
+
+      page.all('ul.product-listing li').size.should == 2
+      tmp = page.all('ul.product-listing li a').map(&:text).flatten.compact
+      tmp.delete("")
+      tmp.sort!.should == ["Ruby on Rails Mug", "Ruby on Rails Stein"]
+    end
+
+    it "should be able to visit category Bags" do
+      click_link "Bags"
+
+      page.all('ul.product-listing li').size.should == 2
+      tmp = page.all('ul.product-listing li a').map(&:text).flatten.compact
+      tmp.delete("")
+      tmp.sort!.should == ["Ruby on Rails Bag", "Ruby on Rails Tote"]
+    end
   end
 end
