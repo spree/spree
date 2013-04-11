7ea4b2119d28be94d752b06546283276342ff5f9
Ryan Bigg <radarlistener@gmail.com>
2013-04-12 09:26:39 +1000

    Return html_safe strings from variant_price_diff
    Fixes #2737
    
    Conflicts:
    	core/app/helpers/spree/products_helper.rb

--- core/app/helpers/spree/products_helper.rb
+++ core/app/helpers/spree/products_helper.rb
@@ -14,10 +14,11 @@ module Spree
     def variant_price_diff(variant)
       diff = variant.amount_in(current_currency) - variant.product.amount_in(current_currency)
       return nil if diff == 0
+      amount = Spree::Money.new(diff.abs, { :currency => current_currency }).to_html
       if diff > 0
-        "(#{t(:add)}: #{Spree::Money.new(diff.abs, { :currency => current_currency }).to_html})"
+        "(#{t(:add)}: #{amount})".html_safe
       else
-        "(#{t(:subtract)}: #{Spree::Money.new(diff.abs, { :currency => current_currency }).to_html})"
+        "(#{t(:subtract)}: #{amount})".html_safe
       end
     end
 
