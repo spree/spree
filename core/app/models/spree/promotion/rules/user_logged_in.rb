30ade50c19ced30165f7c8463c0dd67dcbb27cec
Paul Damer <pdamer@gmail.com>
2013-04-12 15:53:12 -0400

    removing extra comments

--- core/app/models/spree/promotion/rules/user_logged_in.rb
+++ core/app/models/spree/promotion/rules/user_logged_in.rb
@@ -4,13 +4,6 @@ module Spree
       class UserLoggedIn < PromotionRule
 
         def eligible?(order, options = {})
-          # this is tricky.  We couldn't use any of the devise methods since we aren't in the controller.
-          # we need to rely on the controller already having done this for us.
-
-          # The thinking is that the controller should have some sense of what state
-          # we should be in before firing events,
-          # so the controller will have to set this field.
-
           return order.try(:user).try(:anonymous?) == false
         end
 
