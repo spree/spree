f7a4e18dd6174245893a2497e2bf950993cd85b0
John Dyer <lbrapid@gmail.com>
2013-04-12 12:49:26 -0400

    Added stock location admin request specs

---
+++ backend/spec/requests/admin/configuration/stock_locations_spec.rb
@@ -0,0 +1,49 @@
+require 'spec_helper'
+
+describe "Stock Locations" do
+  stub_authorization!
+
+  before(:each) do
+    country = create(:country)
+    Spree::Config[:default_country_id] = country.id
+    visit spree.admin_path
+    click_link "Configuration"
+    click_link "Stock Locations"
+  end
+
+  it "can create a new stock location" do
+    click_link "New Stock Location"
+    fill_in "Name", with: "London"
+    check "Active"
+    click_button "Create"
+
+    page.should have_content("successfully created")
+    page.should have_content("London")
+  end
+
+  it "can delete an existing stock location", js: true do
+    create(:stock_location)
+    visit current_path
+
+    page.should have_content("NY Warehouse")
+
+    click_icon :trash
+    page.driver.browser.switch_to.alert.accept
+    visit current_path
+    page.should have_content("successfully removed")
+  end
+
+  it "can update an existing stock location" do
+    create(:stock_location)
+    visit current_path
+
+    page.should have_content("NY Warehouse")
+
+    click_icon :edit
+    fill_in "Name", with: "London"
+    click_button "Update"
+
+    page.should have_content("successfully updated")
+    page.should have_content("London")
+  end
+end
