1c78aca77f658e9832bbdba75e72da894562c9f6
Paul Damer <pdamer@gmail.com>
2013-04-12 13:42:31 -0400

    adding spec and fixing user_logged_in promo rule
    Fixes #2884

--- promo/app/models/spree/promotion/rules/user_logged_in.rb
+++ promo/app/models/spree/promotion/rules/user_logged_in.rb
@@ -4,14 +4,7 @@ module Spree
       class UserLoggedIn < PromotionRule
 
         def eligible?(order, options = {})
-          # this is tricky.  We couldn't use any of the devise methods since we aren't in the controller.
-          # we need to rely on the controller already having done this for us.
-
-          # The thinking is that the controller should have some sense of what state
-          # we should be in before firing events,
-          # so the controller will have to set this field.
-
-          return options && options[:user_signed_in]
+          return order.try(:user).try(:anonymous?) == false
         end
 
       end
