a6227f30828c03d66aae037581ddab83a41b265a
Tobias Bohwalli <hi@futhr.io>
2013-04-13 21:07:04 +0200

    [backend] Fix failing update_positions spec.
    The update_positions spec failed due to it expected initial position to
    be nil when created but its actually 1.

--- backend/spec/controllers/spree/admin/resource_controller_spec.rb
+++ backend/spec/controllers/spree/admin/resource_controller_spec.rb
@@ -3,20 +3,23 @@ require 'spec_helper'
 describe Spree::Admin::ResourceController do
   stub_authorization!
 
-  describe 'POST#update_positions' do
-
+  describe "POST#update_positions" do
     before do
-      Spree::Admin::ResourceController.any_instance.stub(:model_class).and_return Spree::Variant
+      Spree::Admin::ResourceController.any_instance.stub(:model_class).and_return(Spree::Variant)
     end
 
-    it 'returns Ok on json' do
-      variant = create(:variant)
+    let(:variant) { create(:variant) }
+
+    it "has 1 as initial position when created" do
+      variant.position.should == 1
+    end
+
+    it "returns Ok on json" do
       variant2 = create(:variant)
       expect {
-				spree_post :update_positions, :id => variant.id, :positions => { variant.id => '2', variant2.id => '1' }, :format => "js"
+				spree_post :update_positions, id: variant.id, positions: { variant.id => "2", variant2.id => "1" }, format: "js"
         variant.reload
-      }.to change(variant, :position).from(nil).to(2)
+      }.to change(variant, :position).from(1).to(2)
     end
-
   end
 end
