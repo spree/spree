spree/spree

commit 175a23211337c9ba53242b54d19f84642989e984

Remove Mass Assignment of Role IDs Thanks to Laurens Nienhaus for reporting

auth/app/controllers/spree/admin/admin_users_controller_decorator.rb
@@ -6,6 +6,47 @@ Spree::Admin::UsersController.class_eval do
 
   before_filter :load_roles, :only => [:edit, :new, :update, :create]
 
+  def create
+    if params[:user]
+      roles = params[:user].delete("role_ids")
+    end
+
+    @user = Spree::User.new(params[:user])
+    if @user.save
+
+      if roles
+        @user.roles = roles.reject(&:blank?).collect{|r| Spree::Role.find(r)}
+      end
+
+      flash.now[:notice] = t(:created_successfully)
+      render :edit
+    else
+      render :new
+    end
+  end
+
+  def update
+    if params[:user]
+      roles = params[:user].delete("role_ids")
+    end
+
+    if @user.update_attributes(params[:user])
+      if roles
+        @user.roles = roles.reject(&:blank?).collect{|r| Spree::Role.find(r)}
+      end
+
+      if params[:user][:password].present?
+        # this logic needed b/c devise wants to log us out after password changes
+        user = Spree::User.reset_password_by_token(params[:user])
+        sign_in(@user, :event => :authentication, :bypass => !Spree::Auth::Config[:signout_after_password_change])
+      end
+      flash.now[:notice] = t(:account_updated)
+      render :edit
+    else
+      render :edit
+    end
+  end
+
   private
 
     def sign_in_if_change_own_password

auth/spec/requests/admin/users_spec.rb
@@ -15,7 +15,7 @@ describe 'Users' do
       fill_in "user_password_confirmation", :with => "welcome"
       click_button "Update"
 
-      page.should have_content("successfully updated!")
+      page.should have_content("Account updated!")
     end
   end
 end

auth/spec/requests/user_spec.rb
@@ -22,8 +22,7 @@ describe "Users" do
   it "admin editing roles" do
     check "user_role_user"
     click_button "Update"
-    page.should have_content("User has been successfully updated!")
-    within('table#listing_users') { click_link "Edit" }
+    page.should have_content("Account updated!")
     find_field('user_role_user')['checked'].should be_true
   end
 

auth/app/models/spree/user.rb
@@ -15,7 +15,7 @@ module Spree
     before_destroy :check_completed_orders
 
     # Setup accessible (or protected) attributes for your model
-    attr_accessible :email, :password, :password_confirmation, :remember_me, :persistence_token, :login, :role_ids
+    attr_accessible :email, :password, :password_confirmation, :remember_me, :persistence_token, :login
 
     users_table_name = User.table_name
     roles_table_name = Role.table_name

auth/spec/controllers/users_controller_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe Spree::UsersController do
   let(:admin_user) { create(:user) }
   let(:user) { create(:user) }
+  let(:role) { create(:role) }
 
   before do
     sign_in user
@@ -39,6 +40,10 @@ describe Spree::UsersController do
         spree_put :update, { :user => create(:user) }, { :user => { :email => 'mynew@email-address.com' } }
         response.should redirect_to(spree.login_url(:only_path => true))
       end
+    end
+
+    it 'should not update roles' do
+      expect { spree_put :update, { :user => { :role_ids => [role.id] } }}.to raise_exception(ActiveModel::MassAssignmentSecurity::Error)
     end
   end
 end

commit 7966e94762bce118558582c10774edbe5f6be601

Fix user role mass assignment from backend
This was broken by commit 175a23211337c9ba53242b54d19f84642989e984.

This specifically fixes two problems:
- #2905 Mass assignment exception when creating new users through backend
- #2904 Backend user gets logged into edited user account when changing
	password

auth/app/controllers/spree/admin/admin_users_controller_decorator.rb
@@ -6,57 +6,6 @@ Spree::Admin::UsersController.class_eval do
 
   before_filter :load_roles, :only => [:edit, :new, :update, :create]
 
-  def create
-    if params[:user]
-      roles = params[:user].delete("role_ids")
-    end
-
-    @user = Spree::User.new(params[:user])
-
-    invoke_callbacks(:create, :before)
-
-    if @user.save
-      invoke_callbacks(:create, :after)
-
-      if roles
-        @user.roles = roles.reject(&:blank?).collect{|r| Spree::Role.find(r)}
-      end
-
-      flash.now[:notice] = t(:created_successfully)
-      render :edit
-    else
-      invoke_callbacks(:create, :fails)
-      render :new
-    end
-  end
-
-  def update
-    if params[:user]
-      roles = params[:user].delete("role_ids")
-    end
-
-    invoke_callbacks(:update, :before)
-
-    if @user.update_attributes(params[:user])
-      invoke_callbacks(:update, :after)
-
-      if roles
-        @user.roles = roles.reject(&:blank?).collect{|r| Spree::Role.find(r)}
-      end
-
-      if params[:user][:password].present?
-        # this logic needed b/c devise wants to log us out after password changes
-        user = Spree::User.reset_password_by_token(params[:user])
-        sign_in(@user, :event => :authentication, :bypass => !Spree::Auth::Config[:signout_after_password_change])
-      end
-      flash.now[:notice] = t(:account_updated)
-      render :edit
-    else
-      invoke_callbacks(:update, :fails)
-      render :edit
-    end
-  end
-
   private
 
     def sign_in_if_change_own_password
@@ -67,6 +16,12 @@ Spree::Admin::UsersController.class_eval do
 
     def load_roles
       @roles = Spree::Role.scoped
+    end
+
+    # subvert role_ids mass assignment protection
+    def load_resource_instance
+      role_ids = params[:user].delete(:role_ids) if params[:user] && params[:user][:role_ids]
+      super.tap { |user| user.role_ids = role_ids if role_ids }
     end
 end
 

auth/spec/requests/admin/users_spec.rb
@@ -15,7 +15,7 @@ describe 'Users' do
       fill_in "user_password_confirmation", :with => "welcome"
       click_button "Update"
 
-      page.should have_content("Account updated!")
+      page.should have_content("successfully updated!")
     end
   end
 end

auth/spec/requests/user_spec.rb
@@ -22,7 +22,8 @@ describe "Users" do
   it "admin editing roles" do
     check "user_role_user"
     click_button "Update"
-    page.should have_content("Account updated!")
+    page.should have_content("User has been successfully updated!")
+    within('table#listing_users') { click_link "Edit" }
     find_field('user_role_user')['checked'].should be_true
   end
 