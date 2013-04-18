48582e17e32581b65b76b6e873a0a9be1675dcf6
Igor Zubkov <icesik@altlinux.org>
2013-04-18 15:48:57 +0300

    Replace tabs with spaces

--- core/config/routes.rb
+++ core/config/routes.rb
@@ -142,9 +142,9 @@ Spree::Core::Engine.routes.draw do
     end
 
     resources :taxonomies do
-    	collection do
-    		post :update_positions
-    	end
+      collection do
+        post :update_positions
+      end
       member do
         get :get_children
       end
