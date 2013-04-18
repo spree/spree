a360a868b7ed701c589852c5befa1c46858defb0
cmar <christopher.mar@gmail.com>
2013-04-18 11:48:44 -0400

    transfer can receive inventory from vendor with a reference number

--- core/db/migrate/20130418125341_create_spree_stock_transfers.rb
+++ core/db/migrate/20130418125341_create_spree_stock_transfers.rb
@@ -2,6 +2,7 @@ class CreateSpreeStockTransfers < ActiveRecord::Migration
   def change
     create_table :spree_stock_transfers do |t|
       t.string :type
+      t.string :reference_number
       t.timestamps
     end
   end
