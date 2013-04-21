1a69dff2b817d0bc059eabbdbec83f373bb506e6
Washington Luiz <huoxito@gmail.com>
2013-04-21 03:26:37 -0300

    Allow users to not override default mailer configs
    Fix from_address logic in mailers, it broke when there were no
    mail_method record on the database
    
    Fixes #2918

--- core/app/mailers/spree/test_mailer.rb
+++ core/app/mailers/spree/test_mailer.rb
@@ -1,15 +1,9 @@
 module Spree
-  class TestMailer < ActionMailer::Base
-    def from_address
-      MailMethod.current.preferred_mails_from
-    end
-
+  class TestMailer < BaseMailer
     def test_email(mail_method, user)
       @mail_method = mail_method
       subject = "#{Spree::Config[:site_name]} #{t('test_mailer.test_email.subject')}"
-      mail(:to => user.email,
-           :from => from_address,
-           :subject => subject)
+      mail(to: user.email, from: from_address, subject: subject)
     end
   end
 end
--- core/spec/lib/mail_interceptor_spec.rb
+++ core/spec/lib/mail_interceptor_spec.rb
@@ -1,76 +1,44 @@
 require 'spec_helper'
 
-# We'll use the OrderMailer as a quick and easy way to test.  IF it works here - it works for all email (in theory.)
-describe Spree::OrderMailer do
-  let(:mail_method) { mock("mail_method", :preferred_mails_from => "spree@example.com", :preferred_intercept_email => nil, :preferred_mail_bcc => nil) }
-  let(:order) { Spree::Order.new(:email => "customer@example.com") }
-  let(:message) { Spree::OrderMailer.confirm_email(order) }
-  #let(:email) { mock "email" }
+# We'll use the OrderMailer as a quick and easy way to test.
+# If it works here - it works for all email (in theory)
+module Spree
+  describe OrderMailer do
+    let(:mail_method) { double('MailMethod') }
+    let(:order) { Order.new(:email => "customer@example.com") }
+    let(:message) { OrderMailer.confirm_email(order) }
 
-  before(:all) do
-    ActionMailer::Base.perform_deliveries = true
-    ActionMailer::Base.deliveries.clear
-  end
+    before { MailMethod.stub(:current => mail_method) }
 
-  context "#deliver" do
-    before do
-      ActionMailer::Base.delivery_method = :test
-      Spree::MailMethod.stub :current => mail_method
-    end
-    after { ActionMailer::Base.deliveries.clear }
-
-    it "should use the from address specified in the preference" do
-      mail_method.stub :preferred_mails_from => "no-reply@foobar.com"
-      message.deliver
-      @email = ActionMailer::Base.deliveries.first
-      @email.from.should == ["no-reply@foobar.com"]
-    end
-
-    it "should use the provided from address" do
-      mail_method.stub :preferred_mails_from => "preference@foobar.com"
-      message = ActionMailer::Base.mail(:from => "override@foobar.com", :to => "test@test.com")
-      message.deliver
-      @email = ActionMailer::Base.deliveries.first
-      @email.from.should == ["override@foobar.com"]
-    end
-
-    it "should add the bcc email when provided" do
-      mail_method.stub :preferred_mail_bcc => "bcc-foo@foobar.com"
-      message.deliver
-      @email = ActionMailer::Base.deliveries.first
-      @email.bcc.should == ["bcc-foo@foobar.com"]
-    end
-
-    context "when intercept_email is provided" do
-      it "should strip the bcc recipients" do
-        message.bcc.should be_blank
-      end
-
-      it "should strip the cc recipients" do
-        message.cc.should be_blank
-      end
-
-      it "should replace the receipient with the specified address" do
+    context "mail method preferences set" do
+      before do
+        mail_method.stub :preferred_mails_from => "no-reply@foobar.com"
+        mail_method.stub :preferred_mail_bcc => "bcc-foo@foobar.com"
         mail_method.stub :preferred_intercept_email => "intercept@foobar.com"
-        message.deliver
-        @email = ActionMailer::Base.deliveries.first
-        @email.to.should == ["intercept@foobar.com"]
       end
-      it "should modify the subject to include the original email" do
-        mail_method.stub :preferred_intercept_email => "intercept@foobar.com"
-        message.deliver
-        @email = ActionMailer::Base.deliveries.first
-        @email.subject.match(/customer@example\.com/).should be_true
+
+      it { message.deliver.from.should == ["no-reply@foobar.com"] }
+      it { message.deliver.bcc.should == ["bcc-foo@foobar.com"] }
+      it { message.deliver.to.should == ["intercept@foobar.com"] }
+
+      it "modifies the subject to include the original email" do
+        message.deliver.subject.should include(order.email)
       end
-    end
 
-    context "when intercept_mode is not provided" do
-      before { mail_method.stub :preferred_intercept_email => "" }
+      context "except intercept mail" do
+        before { mail_method.stub :preferred_intercept_email => "" }
 
-      it "should not modify the recipient" do
-        message.deliver
-        @email = ActionMailer::Base.deliveries.first
-        @email.to.should == ["customer@example.com"]
+        it "does not modify the recipient" do
+          message.deliver.to.should == ["customer@example.com"]
+        end
+      end
+
+      context "override actionmailer config set false" do
+        before { Config.override_actionmailer_config = false }
+
+        it "does not run spree interceptor" do
+          message.deliver.to.should == ["customer@example.com"]
+        end
       end
     end
   end
