9dff0e6bb08719532310c6ade9adf1f16b63a75e
Ryan Bigg <radarlistener@gmail.com>
2013-04-23 09:36:02 +1000

    Check for existance of payment? method on Order instances before calling it
    Fixes #2910

--- core/app/controllers/spree/checkout_controller.rb
+++ core/app/controllers/spree/checkout_controller.rb
@@ -76,7 +76,7 @@ module Spree
 
       def object_params
         # For payment step, filter order parameters to produce the expected nested attributes for a single payment and its source, discarding attributes for payment methods other than the one selected
-        if @order.payment?
+        if @order.respond_to?(:payment?) && @order.payment?
           if params[:payment_source].present? && source_params = params.delete(:payment_source)[params[:order][:payments_attributes].first[:payment_method_id].underscore]
             params[:order][:payments_attributes].first[:source_attributes] = source_params
           end
