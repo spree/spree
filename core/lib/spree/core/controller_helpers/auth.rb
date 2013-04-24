910abe025cca93cd6b497278e743b17106d1e149
Ryan Bigg <radarlistener@gmail.com>
2013-04-24 19:00:18 +1000

    Rely on spree_user mappings from Devise, not user
    For reasons, see commit 4a27c35fd29cf4fcafc1cf6b2401d937361610b9 in spree_auth_devise

--- core/lib/spree/core/controller_helpers/auth.rb
+++ core/lib/spree/core/controller_helpers/auth.rb
@@ -44,7 +44,7 @@ module Spree
 
           disallowed_urls.map!{ |url| url[/\/\w+$/] }
           unless disallowed_urls.include?(request.fullpath)
-            session['user_return_to'] = request.fullpath.gsub('//', '/')
+            session['spree_user_return_to'] = request.fullpath.gsub('//', '/')
           end
         end
 
@@ -55,8 +55,8 @@ module Spree
         end
 
         def redirect_back_or_default(default)
-          redirect_to(session["user_return_to"] || default)
-          session["user_return_to"] = nil
+          redirect_to(session["spree_user_return_to"] || default)
+          session["spree_user_return_to"] = nil
         end
 
         # Need to generate an API key for a user due to some actions potentially
