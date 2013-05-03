d2d2a4a5f2ec040c454edc655b52616bd0deda71
Ryan Bigg <radarlistener@gmail.com>
2013-05-03 14:55:13 +1000

    [frontend] Fix locale_spec

--- frontend/spec/requests/locale_spec.rb
+++ frontend/spec/requests/locale_spec.rb
@@ -3,9 +3,10 @@ require 'spec_helper'
 describe "setting locale" do
   before do
     I18n.locale = I18n.default_locale
-    I18n.backend.store_translations(:fr, {
-     :cart => "Panier",
-     :shopping_cart => "Panier"
+    I18n.backend.store_translations(:fr, 
+     :spree => {
+       :cart => "Panier",
+       :shopping_cart => "Panier"
     })
     Spree::Frontend::Config[:locale] = "fr"
   end
