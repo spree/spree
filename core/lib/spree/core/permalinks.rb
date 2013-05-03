a3f4d81a0dd5a5b63e38f3623cb9bf1ecbef030a
Washington Luiz <huoxito@gmail.com>
2013-05-03 01:12:35 -0300

    Use rspec render_template for layout specification
    Improve product spec, too many repeated spec and create(:product) calls

--- core/lib/spree/core/permalinks.rb
+++ core/lib/spree/core/permalinks.rb
@@ -51,14 +51,11 @@ module Spree
         permalink_value ||= generate_permalink
 
         field = self.class.permalink_field
-          # Do other links exist with this permalink?
-          other = self.class.where("#{self.class.table_name}.#{field} LIKE ?", "#{permalink_value}%")
-          if other.any?
-            # Find the existing permalink with the highest number, and increment that number.
-            # (If none of the existing permalinks have a number, this will evaluate to 1.)
-            number = other.map { |o| o.send(field)[/-(\d+)$/, 1].to_i }.max + 1
-            permalink_value += "-#{number.to_s}"
-          end
+        other = self.class.where("#{self.class.table_name}.#{field} LIKE ?", "#{permalink_value}%")
+        if other.any?
+          number = other.map { |o| o.send(field)[/-(\d+)$/, 1].to_i }.max + 1
+          permalink_value += "-#{number.to_s}"
+        end
         write_attribute(field, permalink_value)
       end
     end
