d0904ff0f48715db8a62d131899b75d2d9dc7bbf
Washington Luiz <huoxito@gmail.com>
2013-05-05 14:28:03 -0300

    Clear tax adjustments only in TaxRate#adjust
    It doesn't make sense to remove all line item adjustments from
    TaxRate#adjust because price adjustments might come from other classes
    such as ShippingMethod and PromotionAction
    
    Fixes #2981
    
    Conflicts:
    	core/app/models/spree/order.rb

--- core/app/models/spree/order.rb
+++ core/app/models/spree/order.rb
@@ -49,7 +49,7 @@ module Spree
 
     has_many :adjustments, as: :adjustable, dependent: :destroy, order: 'created_at ASC'
     has_many :line_item_adjustments, through: :line_items, source: :adjustments
-    has_many :line_items, dependent: :destroy, order: 'created_at ASC'
+    has_many :line_items, -> { order('created_at ASC') }, dependent: :destroy
     has_many :payments, dependent: :destroy
     has_many :return_authorizations, dependent: :destroy
     has_many :state_changes, as: :stateful
