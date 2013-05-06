3162d810c4ea709f758e0cebb353727c1213f9cb
Sean Schofield <sean.schofield@gmail.com>
2013-05-05 20:35:53 -0400

    Refactored more Spree.t cases

--- core/app/helpers/spree/promotion_rules_helper.rb
+++ core/app/helpers/spree/promotion_rules_helper.rb
@@ -4,10 +4,10 @@ module Spree
     def options_for_promotion_rule_types(promotion)
       existing = promotion.rules.map { |rule| rule.class.name }
       rule_names = Rails.application.config.spree.promotions.rules.map(&:name).reject{ |r| existing.include? r }
-      options = rule_names.map { |name| [ t("promotion_rule_types.#{name.demodulize.underscore}.name"), name] }
+      options = rule_names.map { |name| [ Spree.t("promotion_rule_types.#{name.demodulize.underscore}.name"), name] }
       options_for_select(options)
     end
- 
+
   end
 end
 
