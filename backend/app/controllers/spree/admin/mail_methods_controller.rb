b3940fa65b2b916fedbdae0a39ff739c7ba4ca18
Washington Luiz <huoxito@gmail.com>
2013-05-07 03:00:02 -0300

    Add missing locale entries and Spree.t()
    Fixes #2988

--- backend/app/controllers/spree/admin/mail_methods_controller.rb
+++ backend/app/controllers/spree/admin/mail_methods_controller.rb
@@ -19,12 +19,12 @@ module Spree
 
       def testmail
         if TestMailer.test_email(try_spree_current_user).deliver
-          flash[:success] = t('admin.mail_methods.testmail.delivery_success')
+          flash[:success] = Spree.t('admin.mail_methods.testmail.delivery_success')
         else
-          flash[:error] = t('admin.mail_methods.testmail.delivery_error')
+          flash[:error] = Spree.t('admin.mail_methods.testmail.delivery_error')
         end
       rescue Exception => e
-        flash[:error] = t('admin.mail_methods.testmail.error') % {:e => e}
+        flash[:error] = Spree.t('admin.mail_methods.testmail.error') % {:e => e}
       ensure
         redirect_to edit_admin_mail_method_url
       end
--- backend/app/controllers/spree/admin/taxons_controller.rb
+++ backend/app/controllers/spree/admin/taxons_controller.rb
@@ -20,7 +20,7 @@ module Spree
             format.json {render :json => @taxon.to_json }
           end
         else
-          flash[:error] = t('errors.messages.could_not_create_taxon')
+          flash[:error] = Spree.t('errors.messages.could_not_create_taxon')
           respond_with(@taxon) do |format|
             format.html { redirect_to @taxonomy ? edit_admin_taxonomy_url(@taxonomy) : admin_taxonomies_url }
           end
