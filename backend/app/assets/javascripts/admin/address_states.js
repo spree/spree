45d412455bec78eb72eb484e45e5c9a20950dead
Washington Luiz <huoxito@gmail.com>
2013-05-09 00:03:06 -0300

    Display text input if country has no state on backend
    [Fixes #2999]

--- backend/app/assets/javascripts/admin/address_states.js
+++ backend/app/assets/javascripts/admin/address_states.js
@@ -6,7 +6,7 @@ var update_state = function(region) {
 
   $.get(Spree.routes.states_search + "?country_id=" + country, function(data) {
     var states = data["states"]
-    if(states) {
+    if (states.length > 0) {
       state_select.html('');
       var states_with_blank = [{name: '', id: ''}].concat(states);
       $.each(states_with_blank, function(pos,state) {
