932c65705cd9654103549b38811b07945a2da630
John Dyer <lbrapid@gmail.com>
2013-05-09 14:10:51 -0400

    Split line items across shipments - Easily create new shipments from existing shipments - Move items between shipments on an order

--- backend/spec/requests/admin/orders/shipments_spec.rb
+++ backend/spec/requests/admin/orders/shipments_spec.rb
@@ -22,4 +22,48 @@ describe "Shipments" do
       order.reload.shipment_state.should == "shipped"
     end
   end
+
+  context "moving variants between shipments", js: true do
+    let!(:la) { create(:stock_location, name: "LA") }
+    before(:each) do
+      create(:stock_location, name: "LA")
+      visit spree.admin_path
+      click_link "Orders"
+      within_row(1) do
+        click_link "R100"
+      end
+    end
+
+    it "can move a variant to a new shipment" do
+      page.all("table.stock-contents").count.should == 1
+      within_row(1) do
+        click_icon 'resize-horizontal'
+      end
+      select2_no_label 'LA', from: 'Choose Location'
+      click_icon :ok
+
+      page.all("table.stock-contents").count.should == 2
+      order.shipments.last.stock_location.should == la
+      order.shipments.last.inventory_units.count.should == 1
+    end
+
+    it "can move a variant to an existing shipment" do
+      page.all("table.stock-contents").count.should == 1
+      within_row(1) do
+        click_icon 'resize-horizontal'
+      end
+      select2_no_label 'LA', from: 'Choose Location'
+      click_icon :ok
+
+      within_row(1) do
+        click_icon 'resize-horizontal'
+      end
+      select2_no_label "LA(#{order.reload.shipments.last.number})", from: 'Choose Location'
+      click_icon :ok
+
+      page.all("table.stock-contents").count.should == 2
+      order.shipments.last.stock_location.should == la
+      order.shipments.last.inventory_units.count.should == 2
+    end
+  end
 end
