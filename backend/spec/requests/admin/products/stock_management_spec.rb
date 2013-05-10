3209d44b1ec3ba01bd8acac1ccde7b412ac4e656
Andrew Hooker <geekoncoffee@gmail.com>
2013-05-10 10:49:05 -0500

    Skip Deleted Stock Items in Stock Management Screen

--- backend/spec/requests/admin/products/stock_management_spec.rb
+++ backend/spec/requests/admin/products/stock_management_spec.rb
@@ -11,10 +11,11 @@ describe "Stock Management" do
     context "given a product with a variant and a stock location" do
       before do
         create(:stock_location, name: 'Default')
+        secondary = create(:stock_location, name: 'Secondary')
         @product = create(:product, name: 'apache baseball cap', price: 10)
         v = @product.variants.create!(sku: 'FOOBAR')
         v.stock_items.first.update_column(:count_on_hand, 10)
-
+        secondary.stock_item(v).destroy
         click_link "Products"
         within_row(1) do
           click_icon :edit
@@ -29,6 +30,14 @@ describe "Stock Management" do
           within(:css, '.stock_location_info') do
             column_text(2).should have_content('10')
           end
+        end
+      end
+
+      it "should not show deleted stock_items" do
+        click_link "Stock Management"
+        within(:css, '.stock_location_info') do
+          page.should have_content('Default')
+          page.should_not have_content('Secondary')
         end
       end
 
