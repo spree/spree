beb9b7d88b036b97be930ee58955384ff320a24b
Washington Luiz <huoxito@gmail.com>
2013-05-17 01:37:07 -0300

    Add icon to remove stock items from product section

--- backend/app/controllers/spree/admin/stock_items_controller.rb
+++ backend/app/controllers/spree/admin/stock_items_controller.rb
@@ -1,7 +1,7 @@
 module Spree
   module Admin
     class StockItemsController < Spree::Admin::BaseController
-      before_filter :determine_backorderable
+      before_filter :determine_backorderable, only: :update
 
       def update
         stock_item.save
@@ -10,15 +10,23 @@ module Spree
         end
       end
 
+      def destroy
+        stock_item.destroy
+
+        respond_with(@stock_item) do |format|
+          format.html { redirect_to :back }
+          format.js
+        end
+      end
+
       private
+        def stock_item
+          @stock_item ||= StockItem.find(params[:id])
+        end
 
-      def stock_item
-        @stock_item ||= StockItem.find(params[:id])
-      end
-
-      def determine_backorderable
-        stock_item.backorderable = params[:stock_item].present? && params[:stock_item][:backorderable].present? ? true : false
-      end
+        def determine_backorderable
+          stock_item.backorderable = params[:stock_item].present? && params[:stock_item][:backorderable].present?
+        end
     end
   end
 end
