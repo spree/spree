60e1c3d99a0e17c3b85bed60f899bda17ed110c5
Washington Luiz <huoxito@gmail.com>
2013-05-17 01:37:07 -0300

    Add icon to remove stock items from product section

---
+++ backend/spec/controllers/spree/admin/stock_items_controller_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+module Spree
+  module Admin
+    describe StockItemsController do
+      stub_authorization!
+
+      context "formats" do
+        let!(:stock_item) { create(:variant).stock_items.first }
+
+        it "destroy stock item via js" do
+          expect {
+            spree_delete :destroy, format: :js, id: stock_item
+          }.to change{ StockItem.count }.by(-1)
+        end
+      end
+    end
+  end
+end
