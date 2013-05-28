0533648bb0ec017e3c598d90f2a1fd98f5a1737d
Washington Luiz <huoxito@gmail.com>
2013-05-27 21:25:34 -0300

    More tweaks down to 7 failures on core
      * Fix shipping method zones association
      * Update envinroment test.rb template
      * Fixes failing specs
      * require ransack rails4 branch
    
    [Fixes #3094]

--- core/spec/models/spree/order/callbacks_spec.rb
+++ core/spec/models/spree/order/callbacks_spec.rb
@@ -12,7 +12,7 @@ describe Spree::Order do
       it "o'brien@gmail.com is a valid email address" do
         order.state = 'address'
         order.email = "o'brien@gmail.com"
-        order.should be_valid
+        order.should have(:no).error_on(:email)
       end
     end
   end
@@ -36,7 +36,7 @@ describe Spree::Order do
     it "should not validate email address" do
       order.state = "cart"
       order.email = nil
-      order.should be_valid
+      order.should have(:no).error_on(:email)
     end
   end
 end
