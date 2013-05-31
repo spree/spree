c90bed3bd16aa67f4b438fc308a97d448dece121
Sean O'Hara <sohara@sohara.com>
2013-05-30 23:42:11 -0400

    Add nil object check for StockLocation#fill_status [Fixes #3131]

--- core/app/models/spree/stock_location.rb
+++ core/app/models/spree/stock_location.rb
@@ -40,18 +40,21 @@ module Spree
     end
 
     def fill_status(variant, quantity)
-      item = stock_item(variant)
+      if item = stock_item(variant)
 
-      if item.count_on_hand >= quantity
-        on_hand = quantity
-        backordered = 0
+        if item.count_on_hand >= quantity
+          on_hand = quantity
+          backordered = 0
+        else
+          on_hand = item.count_on_hand
+          on_hand = 0 if on_hand < 0
+          backordered = item.backorderable? ? (quantity - on_hand) : 0
+        end
+
+        [on_hand, backordered]
       else
-        on_hand = item.count_on_hand
-        on_hand = 0 if on_hand < 0
-        backordered = item.backorderable? ? (quantity - on_hand) : 0
+        [0, 0]
       end
-
-      [on_hand, backordered]
     end
 
     private
