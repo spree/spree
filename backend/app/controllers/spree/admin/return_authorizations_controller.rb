6c09a23974b430f95c46ec15f3f3e1a4ef30a461
Washington Luiz <huoxito@gmail.com>
2013-06-02 03:03:46 -0300

    Permit attributes to fix some controller specs
    At this point backend build is down to 1 controller spec failure which
    is probably related to a ransack bug.
    
    ps. I haven't run the request specs yet
    
    [Fixes #3141]

--- backend/app/controllers/spree/admin/return_authorizations_controller.rb
+++ backend/app/controllers/spree/admin/return_authorizations_controller.rb
@@ -3,6 +3,8 @@ module Spree
     class ReturnAuthorizationsController < ResourceController
       belongs_to 'spree/order', :find_by => :number
 
+      before_filter :permit_attributes
+
       update.after :associate_inventory_units
       create.after :associate_inventory_units
 
@@ -15,6 +17,10 @@ module Spree
       protected
         def associate_inventory_units
           (params[:return_quantity] || []).each { |variant_id, qty| @return_authorization.add_variant(variant_id.to_i, qty.to_i) }
+        end
+
+        def permit_attributes
+          params.require(:return_authorization).permit!
         end
     end
   end
