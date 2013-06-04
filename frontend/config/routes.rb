def231f4be075844360c14583e022a2633d8c1ec
Washington Luiz <huoxito@gmail.com>
2013-06-04 15:52:59 -0300

    Replace PUT for PATCH
    As PATCH is the new default HTTP method for updates in Rails 4. At this
    point frontend build looks all green \o\o/\o/\o/

--- frontend/config/routes.rb
+++ frontend/config/routes.rb
@@ -10,7 +10,7 @@ Spree::Core::Engine.routes.draw do
   resources :countries, :only => :index
 
   # non-restful checkout stuff
-  put '/checkout/update/:state', :to => 'checkout#update', :as => :update_checkout
+  patch '/checkout/update/:state', :to => 'checkout#update', :as => :update_checkout
   get '/checkout/:state', :to => 'checkout#edit', :as => :checkout_state
   get '/checkout', :to => 'checkout#edit' , :as => :checkout
 
@@ -27,7 +27,7 @@ Spree::Core::Engine.routes.draw do
   end
 
   get '/cart', :to => 'orders#edit', :as => :cart
-  put '/cart', :to => 'orders#update', :as => :update_cart
+  patch '/cart', :to => 'orders#update', :as => :update_cart
   put '/cart/empty', :to => 'orders#empty', :as => :empty_cart
 
   # route globbing for pretty nested taxon and product paths
