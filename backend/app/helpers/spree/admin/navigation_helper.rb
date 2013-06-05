b6d6bf4c72b9f6d57c3827bd4352a0bc11557b37
Washington Luiz <huoxito@gmail.com>
2013-06-05 00:49:51 -0300

    Remove `spree.root_path` reference from backend
    Since root_path is no longer defined on the backend gem

--- backend/app/helpers/spree/admin/navigation_helper.rb
+++ backend/app/helpers/spree/admin/navigation_helper.rb
@@ -31,7 +31,7 @@ module Spree
         end
 
         selected = if options[:match_path]
-          request.fullpath.starts_with?("#{spree.root_path}admin#{options[:match_path]}")
+          request.fullpath.starts_with?("admin#{options[:match_path]}")
         else
           args.include?(controller.controller_name.to_sym)
         end
