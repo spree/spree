548da6e4bfd23cda3d86c2a94772014746c9a6a4
Dana Jones <djones@larkfarm.com>
2013-06-05 10:19:16 -0500

    Fixed FlexiRate Calculator when max_items is set

--- core/app/models/spree/calculator/flexi_rate.rb
+++ core/app/models/spree/calculator/flexi_rate.rb
@@ -22,10 +22,9 @@ module Spree
       max = self.preferred_max_items.to_i
       items_count = object.line_items.map(&:quantity).sum
       items_count.times do |i|
-        # check max value to avoid divide by 0 errors
-        if (max == 0 && i == 0) || (max > 0) && (i % max == 0)
+        if i == 0
           sum += self.preferred_first_item.to_f
-        else
+        elsif ((max > 0) && (i <= (max - 1))) || (max == 0)
           sum += self.preferred_additional_item.to_f
         end
       end
