9cc8a1300c1fabf9d83416f7c0562e7f6ff69560
Washington Luiz <huoxito@gmail.com>
2013-06-07 02:32:39 -0300

    Fix spree sample load data [Fixes #3170]

--- sample/db/samples/tax_rates.rb
+++ sample/db/samples/tax_rates.rb
@@ -1,6 +1,6 @@
 north_america = Spree::Zone.find_by_name!("North America")
 clothing = Spree::TaxCategory.find_by_name!("Clothing")
-tax_rate = Spree::TaxRate.create!(
+tax_rate = Spree::TaxRate.create(
   :name => "North America",
   :zone => north_america, 
   :amount => 0.05,
