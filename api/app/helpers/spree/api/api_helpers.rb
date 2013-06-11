d8f7ccf7a3f206d19d9a65d51002d4383fddfae2
Ryan Bigg <radarlistener@gmail.com>
2013-06-11 10:19:12 +1000

    [api] variant responses now include the full description text from the product

--- api/app/helpers/spree/api/api_helpers.rb
+++ api/app/helpers/spree/api/api_helpers.rb
@@ -20,7 +20,7 @@ module Spree
       end
 
       def variant_attributes
-        [:id, :name, :sku, :price, :weight, :height, :width, :depth, :is_master, :cost_price, :permalink]
+        [:id, :name, :sku, :price, :weight, :height, :width, :depth, :is_master, :cost_price, :permalink, :description]
       end
 
       def image_attributes
