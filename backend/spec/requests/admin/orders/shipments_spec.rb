1deffcaef72cf2dffb80d96bde32e6a1f11a6bd2
Ryan Bigg <radarlistener@gmail.com>
2013-06-12 13:34:55 +1000

    Sleep inside orders/shipments_spec to also wait for an API response

--- backend/spec/requests/admin/orders/shipments_spec.rb
+++ backend/spec/requests/admin/orders/shipments_spec.rb
@@ -40,6 +40,7 @@ describe "Shipments" do
       within_row(1) { click_icon 'resize-horizontal' }
       targetted_select2 'LA', from: '#s2id_item_stock_location'
       click_icon :ok
+      sleep(1)
       page.should have_selector("table.stock-contents:eq(2)")
 
       within_row(2) { click_icon 'resize-horizontal' }
