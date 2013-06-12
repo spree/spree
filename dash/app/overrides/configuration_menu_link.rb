37830a80da633501727f5fb6807ed3d53633d11a
Sean Schofield <sean@railsdog.com>
2013-06-12 16:14:25 -0400

    Remove dash gem

--- dash/app/overrides/configuration_menu_link.rb
+++
@@ -1,5 +0,0 @@
-Deface::Override.new(:virtual_path => "spree/admin/configurations/index",
-                     :name => "add_dashboard_sidebar_link",
-                     :insert_bottom => "tbody[data-hook=admin_configurations_menu]",
-                     :partial => "spree/admin/dash/configurations/jirafe",
-                     :original => 'a74f177275dc303c9cd5994b2e24e027434c3cbb')
