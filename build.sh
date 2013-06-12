154ca74e8ad6471079528aedf1d54f055462f1e7
Andrew Hooker <geekoncoffee@gmail.com>
2013-06-12 07:16:01 -0500

    Remove Gemfile.lock within build.sh to make testing easier

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
  alias set_gemfile='export BUNDLE_GEMFILE="`pwd`/Gemfile"'
+ rm Gemfile.lock
  bundle check || bundle install
  bundle exec rake test_app
- cd api; set_gemfile; bundle install; bundle exec rspec spec
- cd ../backend; set_gemfile; bundle install; bundle exec rspec spec
- cd ../core; set_gemfile; bundle install; bundle exec rspec spec
- cd ../dash; set_gemfile; bundle install; bundle exec rspec spec
- cd ../frontend; set_gemfile; bundle install; bundle exec rspec spec
+ cd api; set_gemfile; rm Gemfile.lock; bundle install; bundle exec rspec spec
+ cd ../backend; set_gemfile; rm Gemfile.lock; bundle install; bundle exec rspec spec
+ cd ../core; set_gemfile; rm Gemfile.lock; bundle install; bundle exec rspec spec
+ cd ../dash; set_gemfile; rm Gemfile.lock; bundle install; bundle exec rspec spec
+ cd ../frontend; set_gemfile; rm Gemfile.lock; bundle install; bundle exec rspec spec
