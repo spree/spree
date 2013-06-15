8ec0e8c4148e87eb62da745c8bef8136010f7536
Lars Smit <larssmit@me.com>
2013-06-15 11:05:54 +0200

    Set notifications per Spree engine
    Related to #3205

--- build.sh
+++ build.sh
@@ -7,11 +7,30 @@ function rm_gemfile_lock(){
  fi	
 }
 
-alias set_gemfile='export BUNDLE_GEMFILE="`pwd`/Gemfile"'
+# Switching Gemfile
+function set_gemfile(){
+	echo "Switching Gemfile..."
+	export BUNDLE_GEMFILE="`pwd`/Gemfile"
+}
+
+# Spree defaults
+echo "Setup Spree defaults and creating test application..."
 rm_gemfile_lock
 bundle check || bundle install
 bundle exec rake test_app
+
+# Spree API
+echo "Setup Spree API and running RSpec..."
 cd api; set_gemfile; rm_gemfile_lock; bundle install; bundle exec rspec spec
+
+# Spree Backend
+echo "Setup Spree backend and running RSpec..."
 cd ../backend; set_gemfile; rm_gemfile_lock; bundle install; bundle exec rspec spec
+
+# Spree Core
+echo "Setup Spree core and running RSpec..."
 cd ../core; set_gemfile; rm_gemfile_lock; bundle install; bundle exec rspec spec
+
+# Spree Frontend
+echo "Setup Spree frontend and running RSpec..."
 cd ../frontend; set_gemfile; rm_gemfile_lock; bundle install; bundle exec rspec spec
