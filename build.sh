d211906190cba72fc32ed3b9e1c1952339286ebf
Lars Smit <larssmit@me.com>
2013-06-15 10:21:22 +0200

    Created function for removing Gemfile.lock + removed dash from rake tasks

--- build.sh
+++ build.sh
@@ -1,12 +1,17 @@
- alias set_gemfile='export BUNDLE_GEMFILE="`pwd`/Gemfile"'
+# Remove Gemfile.lock if it exists
+function rm_gemfile_lock(){
  if [ -e "Gemfile.lock" ] 
  then
  	echo "Removing Gemfile.lock..."
  	rm Gemfile.lock
- fi
- bundle check || bundle install
- bundle exec rake test_app
- cd api; set_gemfile; rm Gemfile.lock; bundle install; bundle exec rspec spec
- cd ../backend; set_gemfile; rm Gemfile.lock; bundle install; bundle exec rspec spec
- cd ../core; set_gemfile; rm Gemfile.lock; bundle install; bundle exec rspec spec
- cd ../frontend; set_gemfile; rm Gemfile.lock; bundle install; bundle exec rspec spec
+ fi	
+}
+
+alias set_gemfile='export BUNDLE_GEMFILE="`pwd`/Gemfile"'
+rm_gemfile_lock
+bundle check || bundle install
+bundle exec rake test_app
+cd api; set_gemfile; rm_gemfile_lock; bundle install; bundle exec rspec spec
+cd ../backend; set_gemfile; rm_gemfile_lock; bundle install; bundle exec rspec spec
+cd ../core; set_gemfile; rm_gemfile_lock; bundle install; bundle exec rspec spec
+cd ../frontend; set_gemfile; rm_gemfile_lock; bundle install; bundle exec rspec spec
