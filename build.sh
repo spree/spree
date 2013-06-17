d69df538563736af384d753d207c9f5253e67f54
Lars Smit <larssmit@me.com>
2013-06-18 09:35:06 +1000

    Fixed failing build shell script and made it more robust. Set notifications per Spree engine and added comments.
    Fixes #3205

--- build.sh
+++ build.sh
@@ -1,9 +1,36 @@
- alias set_gemfile='export BUNDLE_GEMFILE="`pwd`/Gemfile"'
- bundle check || bundle install
- bundle exec rake test_app
+# Remove Gemfile.lock if it exists
+function rm_gemfile_lock(){
+ if [ -e "Gemfile.lock" ] 
+ then
+  echo "Removing Gemfile.lock..."
+  rm Gemfile.lock
+ fi 
+}
 
- cd api; set_gemfile; bundle install; bundle exec rspec spec
- cd ../backend; set_gemfile; bundle install; bundle exec rspec spec
- cd ../core; set_gemfile; bundle install; bundle exec rspec spec
- cd ../dash; set_gemfile; bundle install; bundle exec rspec spec
- cd ../frontend; set_gemfile; bundle install; bundle exec rspec spec
+# Switching Gemfile
+function set_gemfile(){
+  echo "Switching Gemfile..."
+  export BUNDLE_GEMFILE="`pwd`/Gemfile"
+}
+
+# Spree defaults
+echo "Setup Spree defaults and creating test application..."
+rm_gemfile_lock
+bundle check || bundle install
+bundle exec rake test_app
+
+# Spree API
+echo "Setup Spree API and running RSpec..."
+cd api; set_gemfile; rm_gemfile_lock; bundle install; bundle exec rspec spec
+
+# Spree Backend
+echo "Setup Spree backend and running RSpec..."
+cd ../backend; set_gemfile; rm_gemfile_lock; bundle install; bundle exec rspec spec
+
+# Spree Core
+echo "Setup Spree core and running RSpec..."
+cd ../core; set_gemfile; rm_gemfile_lock; bundle install; bundle exec rspec spec
+
+# Spree Frontend
+echo "Setup Spree frontend and running RSpec..."
+cd ../frontend; set_gemfile; rm_gemfile_lock; bundle install; bundle exec rspec spec
