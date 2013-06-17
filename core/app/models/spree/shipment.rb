22352a26e76cfc80b883952b0d962902aae89000
Daniel Dixon <code@danieldixon.com>
2013-06-17 11:09:25 -0500

    Added :number to attr_accessible for Spree::Shipments.

--- core/app/models/spree/shipment.rb
+++ core/app/models/spree/shipment.rb
@@ -16,7 +16,7 @@ module Spree
     after_save :ensure_correct_adjustment, :update_order
 
     attr_accessor :special_instructions
-    attr_accessible :order, :special_instructions, :stock_location_id,
+    attr_accessible :order, :special_instructions, :stock_location_id, :number,
                     :tracking, :address, :inventory_units, :selected_shipping_rate_id
 
     accepts_nested_attributes_for :address
