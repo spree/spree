fc795fac5790d0192a0c2bbeaada53b65ae1e022
Ryan Bigg <radarlistener@gmail.com>
2013-06-17 16:36:02 +1000

    Ensure quantity is coerced to integer in OrderContents#add_to_line_item
    It *might* come from a controller parameter, which is a string. The API's LineItemsController#create action does this, for instance.

--- core/app/models/spree/order_contents.rb
+++ core/app/models/spree/order_contents.rb
@@ -29,7 +29,7 @@ module Spree
     def add_to_line_item(line_item, variant, quantity, currency=nil, shipment=nil)
       if line_item
         line_item.target_shipment = shipment
-        line_item.quantity += quantity
+        line_item.quantity += quantity.to_i
         line_item.currency = currency unless currency.nil?
         line_item.save
       else
