d65e8129e47e4ca3ed79526bcb8899a317405a7f
Washington Luiz <huoxito@gmail.com>
2013-06-19 01:49:21 -0300

    Use a awesome_nested_set github fork for Rails 4
    Help us get rid of the thousands of warnings due to an AR deprecation on
    how to access connection from an instance, no rc for that gem has been
    relased at this moment

--- common_spree_dependencies.rb
+++ common_spree_dependencies.rb
@@ -13,6 +13,7 @@ gem 'coffee-rails', '~> 4.0.0.rc1'
 gem 'sass-rails', '~> 4.0.0.rc1'
 
 gem 'ransack', github: 'ernie/ransack', branch: 'rails-4'
+gem 'awesome_nested_set', github: 'huoxito/awesome_nested_set', branch: 'rails4'
 
 group :test do
   gem 'capybara', '~> 2.1'
