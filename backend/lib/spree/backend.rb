baf60e7017b6e8fc0e8259b4ddd9487e18e3e228
Washington Luiz <huoxito@gmail.com>
2013-06-20 00:15:07 -0300

    Add deface back to spree gemspec
    Now that we have a rc that supports Rails 4

--- backend/lib/spree/backend.rb
+++ backend/lib/spree/backend.rb
@@ -2,6 +2,7 @@ require 'rails/all'
 require 'rails/generators'
 require 'jquery-rails'
 require 'jquery-ui-rails'
+require 'deface'
 require 'select2-rails'
 
 require 'spree/core'
--- frontend/lib/spree/frontend.rb
+++ frontend/lib/spree/frontend.rb
@@ -1,6 +1,7 @@
 require 'rails/all'
 require 'rails/generators'
 require 'jquery-rails'
+require 'deface'
 require 'select2-rails'
 
 require 'spree/core'
