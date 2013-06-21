ce0a31d8a9b054d04af3df490e346e6c8922c9ad
Ryan Bigg <radarlistener@gmail.com>
2013-06-21 19:52:57 +1000

    include variants when loading inventory_units in Shipment#to_package
    If there are 100 inventory units for a shipment, there were 100 queries for the inventory units' variants. This small alteration reduces the queries to 1.

--- core/app/models/spree/shipment.rb
+++ core/app/models/spree/shipment.rb
@@ -223,7 +223,7 @@ module Spree
 
     def to_package
       package = Stock::Package.new(stock_location, order)
-      inventory_units.each do |inventory_unit|
+      inventory_units.includes(:variant).each do |inventory_unit|
         package.add inventory_unit.variant, 1, inventory_unit.state
       end
       package
