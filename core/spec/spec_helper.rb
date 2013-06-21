ae55f0686aea08cbfecf8e0438fb57528b10b851
Jeff Dutil <jdutil@burlingtonwebapps.com>
2013-06-21 04:25:57 -0400

    Add Product#total_on_hand convenience method.

--- core/spec/spec_helper.rb
+++ core/spec/spec_helper.rb
@@ -39,7 +39,13 @@ RSpec.configure do |config|
   # instead of true.
   config.use_transactional_fixtures = true
 
-  config.before do
+  config.before(:each) do
+    if example.metadata[:js]
+      DatabaseCleaner.strategy = :truncation
+    else
+      DatabaseCleaner.strategy = :transaction
+    end
+    DatabaseCleaner.start
     reset_spree_preferences
   end
 
