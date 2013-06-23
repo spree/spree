89b2e6f5acf297cb15125245d2a8cf258d13ba5a
Ernest Wu <wuboys@gmail.com>
2013-06-24 02:18:49 +0800

    add the ability to override or decorate a sample file.

--- sample/lib/spree/sample.rb
+++ sample/lib/spree/sample.rb
@@ -1,10 +1,23 @@
 module Spree
   module Sample
     def self.load_sample(file)
-      path = File.expand_path(samples_path + "#{file}.rb")
+
+      local_path = nil
+      decorator_paths = []
+      $LOAD_PATH.each do |p|
+        lp = File.expand_path(Pathname.new(File.join(p, '..', 'db', 'samples')) + "#{file}.rb")
+        lpd = File.expand_path(Pathname.new(File.join(p, '..', 'db', 'samples')) + "#{file}_decorator.rb")
+        local_path = lp if File.exists?(lp) && !lp.to_s.include?("sample/")
+        decorator_paths << lpd if File.exists?(lpd)
+      end
+
+      path = local_path || File.expand_path(samples_path + "#{file}.rb")
       # Check to see if the specified file has been loaded before
       if !$LOADED_FEATURES.include?(path)
         require path
+        decorator_paths.each do |dpath|
+          require dpath
+        end
         puts "Loaded #{file.titleize} samples"
       end
     end
