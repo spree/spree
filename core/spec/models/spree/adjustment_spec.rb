4849c945352ec6570e4fbb28ee78a38496f3a124
Brian Alexander <balexand@gmail.com>
2013-06-25 12:42:19 -0600

    promotion adjustments should be eligible even if amount is zero
    Fixes #3263
    
    Conflicts:
    	core/app/models/spree/adjustment.rb

--- core/spec/models/spree/adjustment_spec.rb
+++ core/spec/models/spree/adjustment_spec.rb
@@ -50,11 +50,28 @@ describe Spree::Adjustment do
     end
   end
 
+  context "#promotion?" do
+    it "returns false if not promotion adjustment" do
+      expect(adjustment.promotion?).to eq false
+    end
+
+    it "returns true if promotion adjustment" do
+      adjustment.originator_type = "Spree::PromotionAction"
+      expect(adjustment.promotion?).to eq true
+    end
+  end
+
   context "#eligible? after #set_eligibility" do
     context "when amount is 0" do
       before { adjustment.amount = 0 }
       it "should be eligible if mandatory?" do
         adjustment.mandatory = true
+        adjustment.set_eligibility
+        adjustment.should be_eligible
+      end
+      it "should be eligible if `promotion?` even if not `mandatory?`" do
+        adjustment.should_receive(:promotion?).and_return(true)
+        adjustment.mandatory = false
         adjustment.set_eligibility
         adjustment.should be_eligible
       end
