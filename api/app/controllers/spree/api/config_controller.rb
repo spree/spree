7adf4d059335463054f65dd4a31321fe6c7eff2c
Ryan Bigg <radarlistener@gmail.com>
2013-06-26 10:47:35 +1000

    [api] Don't cache config stuff... for now
    In my testing of spree-marionette I was coming across a bug where there were no preferences stored in the database (which is weird, yes), and so I'm removing this caching to account for that until I can find out why that's happening.

--- api/app/controllers/spree/api/config_controller.rb
+++ api/app/controllers/spree/api/config_controller.rb
@@ -1,22 +1,6 @@
 module Spree
   module Api
     class ConfigController < Spree::Api::BaseController
-      def show
-        render_if_not_stale
-      end
-
-      def money
-        render_if_not_stale
-      end
-
-      private
-
-      def render_if_not_stale
-        last_preference = Spree::Preference.order("updated_at DESC").last
-        if stale?(:etag => last_preference, :last_modified => last_preference.updated_at)
-          render
-        end
-      end
     end
   end
 end
