32538ee77c32f74e45bd96885791f39d010945dd
cmar <christopher.mar@gmail.com>
2013-06-27 12:43:58 -0400

    allow shipping address with country or state attributes when creating orders through the api

--- api/app/models/spree/order_decorator.rb
+++ api/app/models/spree/order_decorator.rb
@@ -1,17 +1,60 @@
 Spree::Order.class_eval do
   def self.build_from_api(user, params)
     line_items = params.delete(:line_items_attributes) || []
+    ensure_country_id_from_api params[:ship_address_attributes]
+    ensure_state_id_from_api params[:ship_address_attributes]
 
     order = create(params)
 
     unless line_items.empty?
       line_items.each_key do |k|
         line_item = line_items[k]
+
+        ensure_variant_id_from_api(line_item)
+
         order.contents.add(Spree::Variant.find(line_item[:variant_id]), line_item[:quantity])
       end
     end
 
     order
+  end
+
+  def self.ensure_country_id_from_api(address)
+    return if address.nil? or address[:country_id] or address[:country].nil?
+
+    search = {}
+    if name = address[:country]['name']
+      search[:name] = name
+    elsif iso_name = address[:country]['iso_name']
+      search[:iso_name] = iso_name.upcase
+    elsif iso = address[:country]['iso']
+      search[:iso] = iso.upcase
+    elsif iso3 = address[:country]['iso3']
+      search[:iso3] = iso3.upcase
+    end
+
+    address.delete(:country)
+    address[:country_id] = Spree::Country.where(search).first!.id
+  end
+
+  def self.ensure_state_id_from_api(address)
+    return if address.nil? or address[:state_id] or address[:state].nil?
+
+    search = {}
+    if name = address[:state]['name']
+      search[:name] = name
+    elsif abbr = address[:state]['abbr']
+      search[:abbr] = abbr.upcase
+    end
+
+    address.delete(:state)
+    address[:state_id] = Spree::State.where(search).first!.id
+  end
+
+  def self.ensure_variant_id_from_api(line_item)
+    unless line_item[:variant_id]
+      line_item[:variant_id] = Spree::Variant.find_by_sku(line_item[:sku]).id if line_item.has_key?(:sku)
+    end
   end
 
   def update_line_items(line_item_params)
--- api/spec/models/spree/order_spec.rb
+++ api/spec/models/spree/order_spec.rb
@@ -4,15 +4,86 @@ module Spree
   describe Order do
     let(:user) { stub_model(LegacyUser) }
     let(:product) { create :product }
+    let(:variant_id) { product.master.id }
+    let(:sku) { product.master.sku }
+    let(:line_items) {{ "0" => { :variant_id => variant_id, :quantity => 5 }}}
+    let(:ship_address) {{
+       :address1 => '123 Testable Way',
+       :firstname => 'Fox',
+       :lastname => 'Mulder',
+       :city => 'Washington',
+       :country_id => '1',
+       :state_id => '1',
+       :zipcode => '666',
+       :phone => '666-666-6666'
+     }}
 
-    it 'can build an order from API parameters' do
-      variant_id = product.master.id
-      order = Order.build_from_api(user, { :line_items_attributes => { "0" => { :variant_id => variant_id, :quantity => 5 }}})
+    it 'can build an order from API with just line items' do
+      params = { :line_items_attributes => line_items }
+
+      order = Order.build_from_api(user, params)
 
       order.user.should == nil
       line_item = order.line_items.first
       line_item.quantity.should == 5
       line_item.variant_id.should == variant_id
+    end
+
+    it 'can build an order from API with variant sku' do
+      params = { :line_items_attributes => {
+                   "0" => { :sku => sku, :quantity => 5 } }}
+
+      order = Order.build_from_api(user, params)
+
+      line_item = order.line_items.first
+      line_item.variant_id.should == variant_id
+      line_item.quantity.should == 5
+    end
+
+    it 'can build an order from API with order attributes' do
+      params = { :ship_address_attributes => ship_address,
+                 :line_items_attributes => line_items }
+
+      order = Order.build_from_api(user, params)
+      order.ship_address.address1.should eq '123 Testable Way'
+    end
+
+    it 'can build an order from API with country attributes' do
+      ship_address.delete(:country_id)
+      ship_address[:country] = { :iso => 'US' }
+      params = { :ship_address_attributes => ship_address,
+                 :line_items_attributes => line_items }
+
+      order = Order.build_from_api(user, params)
+      order.ship_address.country.iso.should eq 'US'
+    end
+
+    it 'can build an order from API with state attributes' do
+      ship_address.delete(:state_id)
+      ship_address[:state] = { :name => 'Alabama' }
+      params = { :ship_address_attributes => ship_address,
+                 :line_items_attributes => line_items }
+
+      order = Order.build_from_api(user, params)
+      order.ship_address.state.name.should eq 'Alabama'
+    end
+
+    it 'ensures_country_id for country fields' do
+      country = create(:country)
+      [:name, :iso, :iso_name, :iso3].each do |field|
+        address = { :country => { field => country.send(field) }}
+        Order.ensure_country_id_from_api(address)
+        address[:country_id].should eq country.id
+      end
+    end
+
+    it 'ensures_state_id for state fields' do
+      state = create(:state)
+      [:name, :abbr].each do |field|
+        address = { :state => { field => state.send(field) }}
+        Order.ensure_state_id_from_api(address)
+        address[:state_id].should eq state.id
+      end
     end
   end
 end
