162c45a72d6677a3706a703ed381547df885c1a5
Ryan Bigg <radarlistener@gmail.com>
2013-07-03 14:18:25 +1000

    'Correctly' define zones association on 'create_shipping_method_zone' migration
    Fixes #3307
    
    The issue happened upgrading a 1.3 app to 2.0. What would happen is that this migration expected the table to be called shipping_methods_zones, but a later migration (and a model change in the shipment model) changes this to being spree_shipping_method_zones. This small model code here will redefine the association and appease the migration running script.

--- core/db/migrate/20130228210442_create_shipping_method_zone.rb
+++ core/db/migrate/20130228210442_create_shipping_method_zone.rb
@@ -4,7 +4,12 @@ class CreateShippingMethodZone < ActiveRecord::Migration
       t.integer :shipping_method_id
       t.integer :zone_id
     end
-
+    # This association has been corrected in a latter migration
+    # but when this database migration runs, the table is still incorrectly named
+    # 'shipping_methods_zones' instead of 'spre_shipping_methods_zones'
+    Spree::ShippingMethod.has_and_belongs_to_many :zones, :join_table => 'shipping_methods_zones',
+                                                          :class_name => 'Spree::Zone',
+                                                          :foreign_key => 'shipping_method_id'
     Spree::ShippingMethod.all.each{|sm| sm.zones << Spree::Zone.find(sm.zone_id)}
 
     remove_column :spree_shipping_methods, :zone_id
