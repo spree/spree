71c6c4f297a2687a6f054f220fa7ee6d68eb0e06
Washington Luiz <huoxito@gmail.com>
2013-07-04 15:38:25 -0300

    Order should be paid when store owe's customer money
    Fixes #3318
    Fixes #3320

--- core/app/models/spree/order.rb
+++ core/app/models/spree/order.rb
@@ -396,7 +396,7 @@ module Spree
 
     # Helper methods for checkout steps
     def paid?
-      payment_state == 'paid'
+      payment_state == 'paid' || payment_state == 'credit_owed'
     end
 
     def available_payment_methods
