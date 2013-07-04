75a6edfde1ecc306356ed897e312a61c1adf122f
Ryan Bigg <radarlistener@gmail.com>
2013-07-04 11:50:03 +1000

    Show 'No stock locations found' rather than a dodgy looking table when there are no stock locations

--- backend/spec/requests/admin/configuration/stock_locations_spec.rb
+++ backend/spec/requests/admin/configuration/stock_locations_spec.rb
@@ -30,9 +30,8 @@ describe "Stock Locations" do
     page.driver.browser.switch_to.alert.accept
     # Wait for API request to complete.
     sleep(1)
-    visit current_path
-
-    find('#listing_stock_locations').should_not have_content("NY Warehouse")
+    visit current_path 
+    page.should have_content("No stock locations found")
   end
 
   it "can update an existing stock location" do
