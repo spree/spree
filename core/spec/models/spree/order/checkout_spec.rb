d07521ef5d9e61b0880e8593f400a07970ef4860
Ryan Bigg <radarlistener@gmail.com>
2013-07-04 16:24:59 +1000

    Don't transition to delivery step if shipments are blank or if shipments DO exist, but there are no shipping rates for them

--- core/spec/models/spree/order/checkout_spec.rb
+++ core/spec/models/spree/order/checkout_spec.rb
@@ -81,6 +81,23 @@ describe Spree::Order do
         order.next!
         order.state.should == "delivery"
       end
+
+      context "cannot transition to delivery" do
+        context "if there are no shipping rates for any shipment" do
+          before do
+            order.stub(:has_available_payment)
+            shipment = FactoryGirl.create(:shipment, :order => order)
+            order.email = "user@example.com"
+            order.save!
+            shipment.shipping_rates.delete_all
+          end
+
+          specify do
+            transition = lambda { order.next! }
+            transition.should raise_error(StateMachine::InvalidTransition, /#{Spree.t(:items_cannot_be_shipped)}/)
+          end
+        end
+      end
     end
 
     context "from delivery" do
