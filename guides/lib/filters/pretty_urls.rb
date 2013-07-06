74372f222fe5a77a9d7f02aa7afcaf0d238591de
Sean Schofield <sean@spreecommerce.com>
2013-07-06 17:50:50 -0400

    Fix pretty url filter

--- guides/lib/filters/pretty_urls.rb
+++ guides/lib/filters/pretty_urls.rb
@@ -6,16 +6,26 @@ class PrettyUrls < Nanoc::Filter
 
     # Allows us to use pretty urls in markdown and add .html for local viewing
 
-    # [Hubspot Integration](hubspot_integration) => [Hubspot Integration](hubspot_integration.html)
-    # [Hubspot Integration](hubspot_integration#foo) => [Hubspot Integration](hubspot_integration.html#foo)
-
     # TODO: Don't add .html when deploying to server - just use for local development since nanoc server
     # is not smart enough to understand absence of .html and still route to the correct file
 
     #content = content.gsub(/\[(.+)\]\((\S+)\)/) do
-    content = content.gsub(/\[(.+)\]\(([^#]+)(\#\S+)?\)/) do
+    # content = content.gsub(/\[(.+)\]\(([^#]+)(\#\S+)?\)/) do
+    #   "[#{$1}](#{$2}.html#{$3})"
+    # end
+
+    # [Hubspot Integration](hubspot_integration#foo) => [Hubspot Integration](hubspot_integration.html#foo)
+    #content = content.gsub /\[(.+)\]\(([^#]+)(#\S*)?\)/ do
+    content = content.gsub /\[(.+)\]\(([^#\.]+)(#\S*)?\)/ do
       "[#{$1}](#{$2}.html#{$3})"
     end
+
+    # [Hubspot Integration](hubspot_integration) => [Hubspot Integration](hubspot_integration.html)
+    # content = content.gsub /\[(.+)\]\((\S+)\)/ do
+    content = content.gsub /\[(.+)\]\([^\.]+\)/ do      
+      "[#{$1}](#{$2}.html)"
+    end
+
     content
   end
 end
