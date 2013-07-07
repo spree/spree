83d3dbd6e0d653d8345bbc391463b1708856af46
Josh Nussbaum <joshnuss@gmail.com>
2013-07-07 00:33:34 -0400

    Makes cvv_response optional
    Fixes #3330

--- core/app/models/spree/payment/processing.rb
+++ core/app/models/spree/payment/processing.rb
@@ -155,8 +155,11 @@ module Spree
         unless response.authorization.nil?
           self.response_code = response.authorization
           self.avs_response = response.avs_result['code']
-          self.cvv_response_code = response.cvv_result['code']
-          self.cvv_response_message = response.cvv_result['message']
+
+          if response.cvv_result
+            self.cvv_response_code = response.cvv_result['code']
+            self.cvv_response_message = response.cvv_result['message']
+          end
         end
         self.send("#{success_state}!")
       else
