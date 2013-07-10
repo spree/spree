400e250578315d69cf5462bf8e389c5530ec2c04
Ryan Bigg <radarlistener@gmail.com>
2013-07-10 18:01:16 +1000

    Order transitions should be attempted when on the edit page
    Fixes #3336

--- backend/app/controllers/spree/admin/orders_controller.rb
+++ backend/app/controllers/spree/admin/orders_controller.rb
@@ -51,6 +51,12 @@ module Spree
 
       def edit
         @order.shipments.map &:refresh_rates
+        # Transition as far as we can go
+        while @order.next; end
+        # The payment step shows an error of 'No pending payments'
+        # Clearing the errors from the order object will stop this error
+        # appearing on the edit page where we don't want it to.
+        @order.errors.clear
       end
 
       def update
