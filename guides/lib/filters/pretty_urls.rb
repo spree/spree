ceb05e0351b5c12ec2155a38661068a07192cb20
Sean Schofield <sean.schofield@gmail.com>
2013-07-09 23:06:08 -0400

    Remove some previously commented out stuff from filters

--- guides/lib/filters/pretty_urls.rb
+++ guides/lib/filters/pretty_urls.rb
@@ -11,22 +11,12 @@ class PrettyUrls < Nanoc::Filter
     # TODO: Don't add .html when deploying to server - just use for local development since nanoc server
     # is not smart enough to understand absence of .html and still route to the correct file
 
-    #content = content.gsub(/\[(.+)\]\((\S+)\)/) do
-    # content = content.gsub(/\[(.+)\]\(([^#]+)(\#\S+)?\)/) do
-    #   "[#{$1}](#{$2}.html#{$3})"
-    # end
-
     # [Hubspot Integration](hubspot_integration#foo) => [Hubspot Integration](hubspot_integration.html#foo)
     #content = content.gsub /\[(.+)\]\(([^#]+)(#\S*)?\)/ do
     # [Hubspot Integration](hubspot_integration) => [Hubspot Integration](hubspot_integration.html)
     content = content.gsub /\[([^\]]*)\]\(([^[#\)]\.]+)(#\S*)?\)/ do
       "[#{$1}](#{$2}.html#{$3})"
     end
-
-    # content = content.gsub /\[(.+)\]\((\S+)\)/ do
-    # content = content.gsub /\[(.+)\]\([^\.]+\)/ do
-    #   "[#{$1}](#{$2}.html)"
-    # end
 
     content
   end
