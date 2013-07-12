0c8493d64d8c9de6476a9267772a75c886332677
Washington Luiz <huoxito@gmail.com>
2013-07-12 17:55:04 -0300

    Clean up most rspec deprecation warnings
    Too much noise when running api and core build
    
    Fixes #3376

--- api/lib/spree/api/testing_support/setup.rb
+++ api/lib/spree/api/testing_support/setup.rb
@@ -5,7 +5,7 @@ module Spree
         def sign_in_as_admin!
           let!(:current_api_user) do
             user = stub_model(Spree::LegacyUser)
-            user.should_receive(:has_spree_role?).any_number_of_times.with("admin").and_return(true)
+            user.stub(:has_spree_role?).with("admin").and_return(true)
             user
           end
         end
