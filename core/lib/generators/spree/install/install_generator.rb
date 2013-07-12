763ead9b8351e9592177d8a1434ab56bbbbca53a
Washington Luiz <huoxito@gmail.com>
2013-07-12 00:13:40 -0300

    Only create assets if component is included (on install)
    In cases users are only running a backend application, for example,
    there's no need to add frontend assets as it would only break user
    application
    
    Fixes #3368

--- core/lib/generators/spree/install/install_generator.rb
+++ core/lib/generators/spree/install/install_generator.rb
@@ -70,14 +70,19 @@ Disallow: /account
     def setup_assets
       @lib_name = 'spree'
       %w{javascripts stylesheets images}.each do |path|
-        empty_directory "app/assets/#{path}/store"
+        empty_directory "app/assets/#{path}/store" if defined? Spree::Frontend
         empty_directory "app/assets/#{path}/admin"
       end
 
-      template "app/assets/javascripts/store/all.js"
-      template "app/assets/javascripts/admin/all.js"
-      template "app/assets/stylesheets/store/all.css"
-      template "app/assets/stylesheets/admin/all.css"
+      if defined? Spree::Frontend
+        template "app/assets/javascripts/store/all.js"
+        template "app/assets/stylesheets/store/all.css"
+      end
+
+      if defined? Spree::Backend
+        template "app/assets/javascripts/admin/all.js"
+        template "app/assets/stylesheets/admin/all.css"
+      end
     end
 
     def create_overrides_directory
