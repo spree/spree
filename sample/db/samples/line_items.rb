31ae2d6120c59dca662273795a8d54c019b70e81
Ryan Bigg <radarlistener@gmail.com>
2013-07-17 13:34:58 +1000

    [sample] Move line items creation into orders.rb, ensuring that orders have shipments created before they transition into complete
    Conflicts:
    	sample/db/samples/orders.rb

--- sample/db/samples/line_items.rb
+++ sample/db/samples/line_items.rb
@@ -1,16 +1,1 @@
 Spree::Sample.load_sample("orders")
-
-first_order = Spree::Order.find_by_number!("R123456789")
-last_order = Spree::Order.find_by_number!("R987654321")
-
-first_order.line_items.create!({
-  :variant => Spree::Product.find_by_name!("Ruby on Rails Tote").master,
-  :quantity => 1,
-  :price => 15.99
-}, :without_protection => true)
-
-last_order.line_items.create!({
-  :variant => Spree::Product.find_by_name!("Ruby on Rails Bag").master,
-  :quantity => 1,
-  :price => 22.99
-}, :without_protection => true)
