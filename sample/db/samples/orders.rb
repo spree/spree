04db00be92d070bf7a07d98d533edeae82e7c65c
Ryan Bigg <radarlistener@gmail.com>
2013-07-17 13:34:58 +1000

    [sample] Move line items creation into orders.rb, ensuring that orders have shipments created before they transition into complete

--- sample/db/samples/orders.rb
+++ sample/db/samples/orders.rb
@@ -1,6 +1,7 @@
 Spree::Sample.load_sample("addresses")
 
-order = Spree::Order.create!({
+orders = []
+orders << Spree::Order.create!({
   :number => "R123456789",
   :email => "spree@example.com",
   :item_total => 150.95,
@@ -9,11 +10,8 @@ order = Spree::Order.create!({
   :shipping_address => Spree::Address.first,
   :billing_address => Spree::Address.last
 }, :without_protection => true)
-order.state = "complete"
-order.completed_at = Time.now - 1.day
-order.save!
 
-order = Spree::Order.create!({
+orders << Spree::Order.create!({
   :number => "R987654321",
   :email => "spree@example.com",
   :item_total => 15.95,
@@ -22,7 +20,23 @@ order = Spree::Order.create!({
   :shipping_address => Spree::Address.first,
   :billing_address => Spree::Address.last
 }, :without_protection => true)
-order.state = "complete"
-order.completed_at = Time.now - 1.day
-order.save!
 
+orders[0].line_items.create!({
+  :variant => Spree::Product.find_by_name!("Ruby on Rails Tote").master,
+  :quantity => 1,
+  :price => 15.99
+}, :without_protection => true)
+
+orders[1].line_items.create!({
+  :variant => Spree::Product.find_by_name!("Ruby on Rails Bag").master,
+  :quantity => 1,
+  :price => 22.99
+}, :without_protection => true)
+
+orders.each(&:create_proposed_shipments)
+
+orders.each do |order|
+  order.state = "complete"
+  order.completed_at = Time.now - 1.day
+  order.save!
+end
