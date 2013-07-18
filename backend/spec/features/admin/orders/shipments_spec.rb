50d3a47d67d72dc3dc4586a0a1a16b6d9846245e
Jeff Dutil <jdutil@burlingtonwebapps.com>
2013-07-18 11:21:40 -0400

    Fix backend specs for Capybara 2.

--- backend/spec/features/admin/orders/shipments_spec.rb
+++ backend/spec/features/admin/orders/shipments_spec.rb
@@ -16,7 +16,7 @@ describe "Shipments" do
 
     it "can ship a completed order" do
       click_link "ship"
-      sleep 1
+      wait_for_ajax
 
       page.should have_content("SHIPPED PACKAGE")
       order.reload.shipment_state.should == "shipped"
@@ -34,45 +34,20 @@ describe "Shipments" do
       end
     end
 
-    it "can move a variant to a new shipment" do
-      page.all("table.stock-contents").count.should == 1
-      within_row(1) do
-        click_icon 'resize-horizontal'
-      end
-      select2_no_label 'LA', from: 'Choose Location'
+    it "can move a variant to a new and to an existing shipment" do
+      order.shipments.count.should == 1
+
+      within_row(1) { click_icon 'resize-horizontal' }
+      targetted_select2 'LA', from: '#s2id_item_stock_location'
       click_icon :ok
-
-      page.all("table.stock-contents").count.should == 2
-      order.shipments.last.stock_location.should == la
-      order.shipments.last.inventory_units.count.should == 1
-    end
-
-    it "can move a variant to an existing shipment" do
-      page.all("table.stock-contents").count.should == 1
-      within_row(1) do
-        click_icon 'resize-horizontal'
-      end
-      select2_no_label 'LA', from: 'Choose Location'
-      click_icon :ok
-<<<<<<< HEAD
-
-      within_row(1) do
-        click_icon 'resize-horizontal'
-      end
-      select2_no_label "LA(#{order.reload.shipments.last.number})", from: 'Choose Location'
-      click_icon :ok
-
-      page.all("table.stock-contents").count.should == 2
-      order.shipments.last.stock_location.should == la
-      order.shipments.last.inventory_units.count.should == 2
-=======
-      page.should have_selector("table.stock-contents:eq(2)")
+      wait_for_ajax
+      page.should have_selector("#shipment_#{order.shipments.first.id}")
 
       within_row(2) { click_icon 'resize-horizontal' }
       targetted_select2 "LA(#{order.reload.shipments.last.number})", from: '#s2id_item_stock_location'
       click_icon :ok
-      page.should have_selector("table.stock-contents:eq(2)")
->>>>>>> Fix build specs and split shipment edit button [Fixes #3167]
+      wait_for_ajax
+      page.should have_selector("#shipment_#{order.reload.shipments.last.id}")
     end
   end
 end
