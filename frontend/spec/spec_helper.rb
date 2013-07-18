1459a08debe3aecc5c6dbcfefbbe9e00cfc98e64
Jeff Dutil <jdutil@burlingtonwebapps.com>
2013-07-18 13:34:05 -0400

    Fix API specs for Capybara 2 on Ruby 1.9.3.

--- frontend/spec/spec_helper.rb
+++ frontend/spec/spec_helper.rb
@@ -45,7 +45,6 @@ RSpec.configure do |config|
 
   config.fixture_path = File.join(File.expand_path(File.dirname(__FILE__)), "fixtures")
 
-  #config.include Devise::TestHelpers, :type => :controller
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, comment the following line or assign false
   # instead of true.
