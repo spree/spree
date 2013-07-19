64ec6b0ca96024504037ef43351d3f933adab201
Jeff Dutil <jdutil@burlingtonwebapps.com>
2013-07-19 16:21:22 -0400

    Use new_action.before callback to cleanup controller.

--- backend/app/controllers/spree/admin/stock_locations_controller.rb
+++ backend/app/controllers/spree/admin/stock_locations_controller.rb
@@ -2,16 +2,15 @@ module Spree
   module Admin
     class StockLocationsController < ResourceController
 
-      def new
+      new_action.before :set_country
+
+      private
+
+      def set_country
         if Spree::Config[:default_country_id].present?
           @stock_location.country = Spree::Country.find(Spree::Config[:default_country_id])
         else
           @stock_location.country = Spree::Country.find_by_iso('US')
-        end
-        invoke_callbacks(:new_action, :before)
-        respond_with(@object) do |format|
-          format.html { render :layout => !request.xhr? }
-          format.js   { render :layout => false }
         end
       end
 
