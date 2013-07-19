99cd539cfd15543f5a27efc4283320cd6b07b049
Eduardo Moreno <hmitsurugi@gmail.com>
2013-07-19 10:54:46 +0200

    Doesn't create redundant taxons root items. [Fixes #3413]

--- sample/db/samples/taxons.rb
+++ sample/db/samples/taxons.rb
@@ -116,6 +116,6 @@ taxons = [
 taxons.each do |taxon_attrs|
   if taxon_attrs[:parent]
     taxon_attrs[:parent] = Spree::Taxon.find_by_name!(taxon_attrs[:parent])
+    Spree::Taxon.create!(taxon_attrs, :without_protection => true)
   end
-  Spree::Taxon.create!(taxon_attrs, :without_protection => true)
 end
