73b8de379cabdcf52108755c78aa793c9b11592d
Jeff Dutil <jdutil@burlingtonwebapps.com>
2013-07-18 20:06:01 -0400

    Sets stock locations backorderable_default to false and stock item backorderable to false. Fixes #3300

--- backend/spec/features/admin/orders/shipments_spec.rb
+++ backend/spec/features/admin/orders/shipments_spec.rb
@@ -40,11 +40,13 @@ describe "Shipments" do
       within_row(1) { click_icon 'resize-horizontal' }
       targetted_select2 'LA', from: '#s2id_item_stock_location'
       click_icon :ok
+      wait_for_ajax
       page.should have_selector("#shipment_#{order.shipments.last.id}")
 
       within_row(2) { click_icon 'resize-horizontal' }
       targetted_select2 "LA(#{order.reload.shipments.last.number})", from: '#s2id_item_stock_location'
       click_icon :ok
+      wait_for_ajax
       page.should have_selector("#shipment_#{order.shipments.last.id}")
     end
   end
--- core/lib/spree/testing_support/factories/stock_item_factory.rb
+++ core/lib/spree/testing_support/factories/stock_item_factory.rb
@@ -1,7 +1,8 @@
 FactoryGirl.define do
   factory :stock_item, class: Spree::StockItem do
-    variant
+    backorderable true
     stock_location
+    variant
 
     after(:create) { |object| object.adjust_count_on_hand(10) }
   end
