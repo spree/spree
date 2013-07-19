ab27eb7d0d953b3943ac1a6407dd6121579d2794
Jeff Dutil <jdutil@burlingtonwebapps.com>
2013-07-19 16:14:58 -0400

    Prevent AR NotFound error when creating new stock location without Spree::Config[:default_country_id] being set.

--- backend/app/controllers/spree/admin/stock_locations_controller.rb
+++ backend/app/controllers/spree/admin/stock_locations_controller.rb
@@ -2,6 +2,19 @@ module Spree
   module Admin
     class StockLocationsController < ResourceController
 
+      def new
+        if Spree::Config[:default_country_id].present?
+          @stock_location.country = Spree::Country.find(Spree::Config[:default_country_id])
+        else
+          @stock_location.country = Spree::Country.find_by_iso('US')
+        end
+        invoke_callbacks(:new_action, :before)
+        respond_with(@object) do |format|
+          format.html { render :layout => !request.xhr? }
+          format.js   { render :layout => false }
+        end
+      end
+
     end
   end
 end
