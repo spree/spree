d44ed644c5d30111bc028dd90d957bef88dd8683
John Hawthorn <john@freerunningtechnologies.com>
2013-07-19 14:39:44 -0700

    extract searcher_class instantiation into module
    searcher_class was being instantiated in the same way in home_controller
    products_controller, and taxons_controller. This moves that code into a
    new Spree::Core::ControllerHelpers::Search module.
    
    Fixes #3420

--- core/app/controllers/spree/base_controller.rb
+++ core/app/controllers/spree/base_controller.rb
@@ -5,6 +5,7 @@ class Spree::BaseController < ApplicationController
   include Spree::Core::ControllerHelpers::RespondWith
   include Spree::Core::ControllerHelpers::SSL
   include Spree::Core::ControllerHelpers::Common
+  include Spree::Core::ControllerHelpers::Search
 
   respond_to :html
 end
