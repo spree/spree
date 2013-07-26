f53647bb11ef2b8863880ad1977bbfe4f5c06ab6
Ryan Bigg <radarlistener@gmail.com>
2013-07-26 15:07:39 +1000

    Cache result of insufficient_stock_lines so that it is not called for every single line item rendered on the cart page

--- core/app/models/spree/order.rb
+++ core/app/models/spree/order.rb
@@ -459,7 +459,7 @@ module Spree
     end
 
     def insufficient_stock_lines
-      line_items.select &:insufficient_stock?
+     @insufficient_stock_lines ||= line_items.select(&:insufficient_stock?)
     end
 
     def merge!(order)
