d5e51466cc8fa408f41390f81a0f1b71e5b5540d
Ryan Bigg <radarlistener@gmail.com>
2013-07-26 15:49:51 +1000

    Delete all shipping adjustments immediately prior to deleting all shipments in Order#create_proposed_shipments
    What would happen before is that each shipment would find its adjustments and call destroy on them. We don't necessarily care about all that happening because once the new adjustments have been created then the order will be updated correctly.

--- core/app/models/spree/order.rb
+++ core/app/models/spree/order.rb
@@ -529,6 +529,7 @@ module Spree
     end
 
     def create_proposed_shipments
+      adjustments.shipping.delete_all
       shipments.destroy_all
 
       packages = Spree::Stock::Coordinator.new(self).packages
