fb5f360da762c61e5c17c566fd5d189922fb2a23
Washington Luiz <huoxito@gmail.com>
2013-07-26 14:28:32 -0300

    Skip stock status when track_inventory_levels false
    No need to check stock status for any item on the order if the store
    doesn't care about inventory levels. Noticed after reading #3438
    
    Fixes #3440

--- core/app/models/spree/stock/packer.rb
+++ core/app/models/spree/stock/packer.rb
@@ -16,11 +16,15 @@ module Spree
       def default_package
         package = Package.new(stock_location, order)
         order.line_items.each do |line_item|
-          next unless stock_location.stock_item(line_item.variant)
+          if Config.track_inventory_levels
+            next unless stock_location.stock_item(line_item.variant)
 
-          on_hand, backordered = stock_location.fill_status(line_item.variant, line_item.quantity)
-          package.add line_item.variant, on_hand, :on_hand if on_hand > 0
-          package.add line_item.variant, backordered, :backordered if backordered > 0
+            on_hand, backordered = stock_location.fill_status(line_item.variant, line_item.quantity)
+            package.add line_item.variant, on_hand, :on_hand if on_hand > 0
+            package.add line_item.variant, backordered, :backordered if backordered > 0
+          else
+            package.add line_item.variant, line_item.quantity, :on_hand
+          end
         end
         package
       end
