a85abe22f177c3323ed9901eac8ba40f059aa1e5
Jeff Dutil <jdutil@burlingtonwebapps.com>
2013-07-31 16:09:30 -0400

    Add Sample to build.sh

--- build.sh
+++ build.sh
@@ -1,16 +1,16 @@
 # Remove Gemfile.lock if it exists
 function rm_gemfile_lock(){
- if [ -e "Gemfile.lock" ] 
+ if [ -e "Gemfile.lock" ]
  then
- 	echo "Removing Gemfile.lock..."
- 	rm Gemfile.lock
- fi	
+   echo "Removing Gemfile.lock..."
+   rm Gemfile.lock
+ fi
 }
 
 # Switching Gemfile
 function set_gemfile(){
-	echo "Switching Gemfile..."
-	export BUNDLE_GEMFILE="`pwd`/Gemfile"
+  echo "Switching Gemfile..."
+  export BUNDLE_GEMFILE="`pwd`/Gemfile"
 }
 
 # Spree defaults
@@ -24,13 +24,17 @@ echo "Setup Spree API and running RSpec..."
 cd api; set_gemfile; rm_gemfile_lock; bundle install; bundle exec rspec spec
 
 # Spree Backend
-echo "Setup Spree backend and running RSpec..."
+echo "Setup Spree Backend and running RSpec..."
 cd ../backend; set_gemfile; rm_gemfile_lock; bundle install; bundle exec rspec spec
 
 # Spree Core
-echo "Setup Spree core and running RSpec..."
+echo "Setup Spree Core and running RSpec..."
 cd ../core; set_gemfile; rm_gemfile_lock; bundle install; bundle exec rspec spec
 
 # Spree Frontend
-echo "Setup Spree frontend and running RSpec..."
+echo "Setup Spree Frontend and running RSpec..."
 cd ../frontend; set_gemfile; rm_gemfile_lock; bundle install; bundle exec rspec spec
+
+# Spree Sample
+echo "Setup Spree Sample and running RSpec..."
+cd ../sample; bundle install; bundle exec rake test_app; bundle exec rspec spec
