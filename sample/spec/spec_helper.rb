e99c7b1d0649ccaf13c74760d59f092807bd8979
Jeff Dutil <jdutil@burlingtonwebapps.com>
2013-08-01 15:47:09 -0400

    Add require spree_sample to spec helper to prevent failure when running build.sh.

--- sample/spec/spec_helper.rb
+++ sample/spec/spec_helper.rb
@@ -4,6 +4,7 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../dummy/config/environment", __FILE__)
 require 'rspec/rails'
 require 'ffaker'
+require 'spree_sample'
 
 RSpec.configure do |config|
   config.color = true
