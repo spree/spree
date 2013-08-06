4f4106d1c962bd9c9ae0071c3ac1a5aaa20188c5
Ryan Bigg <radarlistener@gmail.com>
2013-08-06 13:17:40 +1000

    [promo] Remove a whole bunch o' stuff from 'should allow an admin to create an automatic promotion with free shipping (no code)' that simply didn't need to be there

--- promo/spec/requests/promotion_adjustments_spec.rb
+++ promo/spec/requests/promotion_adjustments_spec.rb
@@ -197,31 +197,13 @@ describe "Promotion Adjustments" do
       visit spree.root_path
       click_link "RoR Bag"
       click_button "Add To Cart"
-      click_button "Checkout"
-
-      fill_in "Customer E-Mail", :with => "spree@example.com"
-      str_addr = "bill_address"
-      select "United States", :from => "order_#{str_addr}_attributes_country_id"
-      ['firstname', 'lastname', 'address1', 'city', 'zipcode', 'phone'].each do |field|
-        fill_in "order_#{str_addr}_attributes_#{field}", :with => "#{address.send(field)}"
-      end
-      select "#{address.state.name}", :from => "order_#{str_addr}_attributes_state_id"
-      check "order_use_billing"
-      click_button "Save and Continue"
-      click_button "Save and Continue"
-
-      choose('Credit Card')
-      fill_in "card_number", :with => "4111111111111111"
-      fill_in "card_code", :with => "123"
-      click_button "Save and Continue"
-      Spree::Order.last.total.to_f.should == 30.00 # bag(20) + shipping(10)
-      page.should_not have_content("Free Shipping")
 
       visit spree.root_path
       click_link "RoR Mug"
       click_button "Add To Cart"
-      click_button "Checkout"
+      click_button "Checkout" 
 
+      fill_in "Customer E-Mail", :with => "spree@example.com"
       str_addr = "bill_address"
       select "United States", :from => "order_#{str_addr}_attributes_country_id"
       ['firstname', 'lastname', 'address1', 'city', 'zipcode', 'phone'].each do |field|
