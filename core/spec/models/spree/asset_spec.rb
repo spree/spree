8326ee4273589eef109a5a6807353f78c49b7944
Washington Luiz <huoxito@gmail.com>
2013-08-07 21:59:26 -0300

    Fix assets_spec after rails 4 upgrade

--- core/spec/models/spree/asset_spec.rb
+++ core/spec/models/spree/asset_spec.rb
@@ -1,21 +1,16 @@
 require 'spec_helper'
 
 describe Spree::Asset do
-
   describe "#viewable" do
-
     it "touches association" do
       product = create(:custom_product)
       asset = Spree::Asset.create! { |a| a.viewable = product.master }
 
-      old_updated_at = 100.years.ago
-      product.update_column(:updated_at, old_updated_at)
+      product.update_column(:updated_at,  1.day.ago)
 
       expect do
         asset.touch
-      end.to change { product.reload.updated_at }.from(old_updated_at)
+      end.to change { product.reload.updated_at }
     end
-
   end
-
 end
