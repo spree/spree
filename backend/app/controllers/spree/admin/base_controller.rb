61f61cc41cbbc99335d57be03c1e48a33d1d7041
Philip Arndt <p@arndt.io>
2013-08-09 09:09:10 +1200

    Reduced nested conditional to compound expression
    Fixes #3527

--- backend/app/controllers/spree/admin/base_controller.rb
+++ backend/app/controllers/spree/admin/base_controller.rb
@@ -28,10 +28,8 @@ module Spree
         # Need to generate an API key for a user due to some backend actions
         # requiring authentication to the Spree API
         def generate_admin_api_key
-          if user = try_spree_current_user
-            if user.spree_api_key.blank?
-              user.generate_spree_api_key!
-            end
+          if (user = try_spree_current_user) && user.spree_api_key.blank?
+            user.generate_spree_api_key!
           end
         end
 
