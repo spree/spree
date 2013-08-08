d0529dcc05fccad7d92473ed670511a6e28d1a37
Ryan Bigg <radarlistener@gmail.com>
2013-08-08 12:41:57 +1000

    Fix breakages with stock_transfer_spec
    These have existed for a while, they've just not been run because the file was named incorrectly

--- backend/spec/features/admin/stock_transfer_spec.rb
+++ backend/spec/features/admin/stock_transfer_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 
-describe 'Stock Transfers' do
+describe 'Stock Transfers', :js => true do
   stub_authorization!
 
-  it 'transfer between 2 locations', :js => true do
+  it 'transfer between 2 locations' do
     source_location = create(:stock_location_with_items, :name => 'NY')
     destination_location = create(:stock_location, :name => 'SF')
 
@@ -14,7 +14,7 @@ describe 'Stock Transfers' do
     click_button 'Add'
     click_button 'Transfer Stock'
 
-    page.should have_content('Reference Number: PO 666')
+    page.should have_content('STOCK TRANSFER REFERENCE: PO 666')
     page.should have_content('NY')
     page.should have_content('SF')
 
@@ -24,16 +24,16 @@ describe 'Stock Transfers' do
 
   describe 'received stock transfer' do
     def it_is_received_stock_transfer(page)
-      page.should have_content('Reference Number: PO 666')
-      page.should_not have_content('Source')
-      page.should have_content('Destination')
+      page.should have_content('STOCK TRANSFER REFERENCE: PO 666')
+      page.should_not have_selector("#stock-location-source")
+      page.should have_selector("#stock-location-destination")
 
       transfer = Spree::StockTransfer.last
       transfer.should have(1).stock_movements
       transfer.source_location.should be_nil
     end
 
-    it 'receive stock to a single location', :js => true do
+    it 'receive stock to a single location' do
       source_location = create(:stock_location_with_items, :name => 'NY')
       destination_location = create(:stock_location, :name => 'SF')
 
@@ -49,14 +49,12 @@ describe 'Stock Transfers' do
       it_is_received_stock_transfer page
     end
 
-    it 'forced to only receive there is only one location', :js => true do
+    it 'forced to only receive there is only one location' do
       source_location = create(:stock_location_with_items, :name => 'NY')
 
       visit spree.new_admin_stock_transfer_path
 
       fill_in 'reference', :with => 'PO 666'
-
-      find('#transfer_receive_stock').should be_checked
 
       select('NY', :from => 'transfer_destination_location_id')
 
