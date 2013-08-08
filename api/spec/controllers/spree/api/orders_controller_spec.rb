d998f64bf217d87c555ef82236337d5b52be800c
Washington Luiz <huoxito@gmail.com>
2013-08-08 00:33:34 -0300

    User allowed attr only in orders controller [api]

--- api/spec/controllers/spree/api/orders_controller_spec.rb
+++ api/spec/controllers/spree/api/orders_controller_spec.rb
@@ -104,6 +104,8 @@ module Spree
       order.stub(:associate_user!)
       order.stub_chain(:contents, :add).and_return(line_item = double('LineItem'))
       line_item.should_receive(:update_attributes).with("special" => true)
+
+      controller.stub(permitted_line_item_attributes: [:id, :variant_id, :quantity, :special])
       api_post :create, :order => { 
         :line_items => {
           "0" => {
