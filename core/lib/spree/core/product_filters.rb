spree/spree

commit f02d25566d42f06d2ceea596ff9823d5ad06da67

Refactor spree_core:
* Use Rails4 find_by()

core/lib/spree/core/product_filters.rb
@@ -57,12 +57,12 @@ module Spree
       # below scope would be something like ["$10 - $15", "$15 - $18", "$18 - $20"]
       #
       Spree::Product.add_search_scope :price_range_any do |*opts|
-        conds = opts.map {|o| Spree::Core::ProductFilters.price_filter[:conds][o]}.reject {|c| c.nil?}
+        conds = opts.map {|o| Spree::Core::ProductFilters.price_filter[:conds][o]}.reject { |c| c.nil? }
         scope = conds.shift
         conds.each do |new_scope|
           scope = scope.or(new_scope)
         end
-        Spree::Product.joins(:master => :default_price).where(scope)
+        Spree::Product.joins(master: :default_price).where(scope)
       end
 
       def ProductFilters.format_price(amount)
@@ -71,15 +71,16 @@ module Spree
 
       def ProductFilters.price_filter
         v = Spree::Price.arel_table
-        conds = [ [ Spree.t(:under_price, :price => format_price(10))   , v[:amount].lteq(10)],
+        conds = [ [ Spree.t(:under_price, price: format_price(10))     , v[:amount].lteq(10)],
                   [ "#{format_price(10)} - #{format_price(15)}"        , v[:amount].in(10..15)],
                   [ "#{format_price(15)} - #{format_price(18)}"        , v[:amount].in(15..18)],
                   [ "#{format_price(18)} - #{format_price(20)}"        , v[:amount].in(18..20)],
-                  [ Spree.t(:or_over_price, :price => format_price(20)) , v[:amount].gteq(20)]]
-        { :name   => Spree.t(:price_range),
-          :scope  => :price_range_any,
-          :conds  => Hash[*conds.flatten],
-          :labels => conds.map {|k,v| [k,k]}
+                  [ Spree.t(:or_over_price, price: format_price(20)) , v[:amount].gteq(20)]]
+        {
+          name:   Spree.t(:price_range),
+          scope:  :price_range_any,
+          conds:  Hash[*conds.flatten],
+          labels: conds.map { |k,v| [k, k] }
         }
       end
 
@@ -98,23 +99,24 @@ module Spree
       #   rather than an inner join.
       if Spree::Property.table_exists?
         Spree::Product.add_search_scope :brand_any do |*opts|
-          conds = opts.map {|o| ProductFilters.brand_filter[:conds][o]}.reject {|c| c.nil?}
+          conds = opts.map {|o| ProductFilters.brand_filter[:conds][o]}.reject { |c| c.nil? }
           scope = conds.shift
           conds.each do |new_scope|
             scope = scope.or(new_scope)
           end
-          Spree::Product.with_property("brand").where(scope)
+          Spree::Product.with_property('brand').where(scope)
         end
 
         def ProductFilters.brand_filter
-          brand_property = Spree::Property.find_by_name("brand")
-          brands = Spree::ProductProperty.where(:property_id => brand_property).pluck(:value).uniq.map(&:to_s)
+          brand_property = Spree::Property.find_by(name: 'brand')
+          brands = Spree::ProductProperty.where(property: brand_property).pluck(:value).uniq.map(&:to_s)
           pp = Spree::ProductProperty.arel_table
           conds = Hash[*brands.map { |b| [b, pp[:value].eq(b)] }.flatten]
-          { :name   => "Brands",
-            :scope  => :brand_any,
-            :conds  => conds,
-            :labels => (brands.sort).map { |k| [k, k] }
+          {
+            name:   'Brands',
+            scope:  :brand_any,
+            conds:  conds,
+            labels: (brands.sort).map { |k| [k, k] }
           }
         end
       end
@@ -145,16 +147,16 @@ module Spree
 
         def ProductFilters.selective_brand_filter(taxon = nil)
           taxon ||= Spree::Taxonomy.first.root
-          brand_property = Spree::Property.find_by_name("brand")
-          scope = Spree::ProductProperty.where(:property_id => brand_property).
-                                         joins(:product => :taxons).
-                                         where("#{Spree::Taxon.table_name}.id" => [taxon] + taxon.descendants).
-                                         scoped
+          brand_property = Spree::Property.find_by(name: 'brand')
+          scope = Spree::ProductProperty.where(property: brand_property).
+            joins(product: :taxons).
+            where("#{Spree::Taxon.table_name}.id" => [taxon] + taxon.descendants).
+            scoped
           brands = scope.pluck(:value).uniq
           {
-            :name   => "Applicable Brands",
-            :scope  => :selective_brand_any,
-            :labels => brands.sort.map { |k| [k,k] }
+            name:   'Applicable Brands',
+            scope:  :selective_brand_any,
+            labels: brands.sort.map { |k| [k, k] }
           }
         end
       end
@@ -173,10 +175,11 @@ module Spree
       #
       def ProductFilters.taxons_below(taxon)
         return Spree::Core::ProductFilters.all_taxons if taxon.nil?
-        { :name   => "Taxons under " + taxon.name,
-          :scope  => :taxons_id_in_tree_any,
-          :labels => taxon.children.sort_by(&:position).map {|t| [t.name, t.id]},
-          :conds  => nil
+        {
+          name:   'Taxons under ' + taxon.name,
+          scope:  :taxons_id_in_tree_any,
+          labels: taxon.children.sort_by(&:position).map { |t| [t.name, t.id] },
+          conds:  nil
         }
       end
 
@@ -187,11 +190,12 @@ module Spree
       #
       # idea: expand the format to allow nesting of labels?
       def ProductFilters.all_taxons
-        taxons = Spree::Taxonomy.all.map {|t| [t.root] + t.root.descendants }.flatten
-        { :name   => "All taxons",
-          :scope  => :taxons_id_equals_any,
-          :labels => taxons.sort_by(&:name).map {|t| [t.name, t.id]},
-          :conds  => nil # not needed
+        taxons = Spree::Taxonomy.all.map { |t| [t.root] + t.root.descendants }.flatten
+        {
+          name:   'All taxons',
+          scope:  :taxons_id_equals_any,
+          labels: taxons.sort_by(&:name).map { |t| [t.name, t.id] },
+          conds:  nil # not needed
         }
       end
     end

core/app/helpers/spree/base_helper.rb
@@ -106,7 +106,7 @@ module Spree
     end
 
     def available_countries
-      checkout_zone = Zone.find_by_name(Spree::Config[:checkout_zone])
+      checkout_zone = Zone.find_by(name: Spree::Config[:checkout_zone])
 
       if checkout_zone && checkout_zone.kind == 'country'
         countries = checkout_zone.country_list

core/app/models/spree/order.rb
@@ -430,7 +430,7 @@ module Spree
     def merge!(order)
       order.line_items.each do |line_item|
         next unless line_item.currency == currency
-        current_line_item = self.line_items.find_by_variant_id(line_item.variant_id)
+        current_line_item = self.line_items.find_by(variant: line_item.variant)
         if current_line_item
           current_line_item.quantity += line_item.quantity
           current_line_item.save

core/app/models/spree/product.rb
@@ -168,14 +168,14 @@ module Spree
     end
 
     def property(property_name)
-      return nil unless prop = properties.find_by_name(property_name)
-      product_properties.find_by_property_id(prop.id).try(:value)
+      return nil unless prop = properties.find_by(name: property_name)
+      product_properties.find_by(property: prop).try(:value)
     end
 
     def set_property(property_name, property_value)
       ActiveRecord::Base.transaction do
         property = Property.where(name: property_name).first_or_create!(presentation: property_name)
-        product_property = ProductProperty.where(product_id: id, property_id: property.id).first_or_initialize
+        product_property = ProductProperty.where(product: self, property: property).first_or_initialize
         product_property.value = property_value
         product_property.save!
       end
@@ -198,7 +198,7 @@ module Spree
     # which would make AR's default finder return nil.
     # This is a stopgap for that little problem.
     def master
-      super || variants_including_master.with_deleted.where(:is_master => true).first
+      super || variants_including_master.with_deleted.where(is_master: true).first
     end
 
     private
@@ -219,7 +219,7 @@ module Spree
       end
 
       def add_properties_and_option_types_from_prototype
-        if prototype_id && prototype = Spree::Prototype.find_by_id(prototype_id)
+        if prototype_id && prototype = Spree::Prototype.find_by(id: prototype_id)
           prototype.properties.each do |property|
             product_properties.create(property: property)
           end
@@ -245,7 +245,7 @@ module Spree
 
       def punch_permalink
         update_attribute :permalink, "#{Time.now.to_i}_#{permalink}" # punch permalink with date prefix
-      end  
+      end
   end
 end
 

core/app/models/spree/product/scopes.rb
@@ -127,20 +127,20 @@ module Spree
     add_search_scope :with_option_value do |option, value|
       option_values = OptionValue.table_name
       option_type_id = case option
-        when String then OptionType.find_by_name(option) || option.to_i
+        when String then OptionType.find_by(name: option) || option.to_i
         when OptionType then option.id
         else option.to_i
       end
 
       conditions = "#{option_values}.name = ? AND #{option_values}.option_type_id = ?", value, option_type_id
-      group("spree_products.id").joins(:variants_including_master => :option_values).where(conditions)
+      group('spree_products.id').joins(variants_including_master: :option_values).where(conditions)
     end
 
     # Finds all products which have either:
     # 1) have an option value with the name matching the one given
     # 2) have a product property with a value matching the one given
     add_search_scope :with do |value|
-      includes(:variants_including_master => :option_values).
+      includes(variants_including_master: :option_values).
       includes(:product_properties).
       where("#{OptionValue.table_name}.name = ? OR #{ProductProperty.table_name}.value = ?", value, value)
     end
@@ -163,7 +163,7 @@ module Spree
     # Finds all products that have the ids matching the given collection of ids.
     # Alternatively, you could use find(collection_of_ids), but that would raise an exception if one product couldn't be found
     add_search_scope :with_ids do |*ids|
-      where(:id => ids)
+      where(id: ids)
     end
 
     # Sorts products from most popular (popularity is extracted from how many
@@ -248,10 +248,10 @@ module Spree
         taxons = Taxon.table_name
         ids_or_records_or_names.flatten.map { |t|
           case t
-          when Integer then Taxon.find_by_id(t)
+          when Integer then Taxon.find_by(id: t)
           when ActiveRecord::Base then t
           when String
-            Taxon.find_by_name(t) ||
+            Taxon.find_by(name: t) ||
             Taxon.where("#{taxons}.permalink LIKE ? OR #{taxons}.permalink = ?", "%/#{t}/", "#{t}/").first
           end
         }.compact.flatten.uniq

core/app/models/spree/product_property.rb
@@ -1,6 +1,6 @@
 module Spree
   class ProductProperty < ActiveRecord::Base
-    belongs_to :product, class_name: 'Spree::Product'
+    belongs_to :product,  class_name: 'Spree::Product'
     belongs_to :property, class_name: 'Spree::Property'
 
     validates :property, presence: true
@@ -15,7 +15,7 @@ module Spree
 
     def property_name=(name)
       unless name.blank?
-        unless property = Property.find_by_name(name)
+        unless property = Property.find_by(name: name)
           property = Property.create(name: name, presentation: name)
         end
         self.property = property

core/app/models/spree/variant/scopes.rb
@@ -8,7 +8,7 @@ module Spree
       #
       # Example:
       #
-      # product.variants_including_master.has_option(OptionType.find_by_name("shoe-size"),OptionValue.find_by_name("8"))
+      # product.variants_including_master.has_option(OptionType.find_by(name: 'shoe-size'), OptionValue.find_by(name: '8'))
       def has_option(option_type, *option_values)
         option_types = OptionType.table_name
 

core/db/default/spree/countries.rb
@@ -227,4 +227,4 @@ Spree::Country.create!([
   { name: "Saint Kitts and Nevis", iso3: "KNA", iso: "KN", iso_name: "SAINT KITTS AND NEVIS", numcode: "659" },
   { name: "Serbia", iso3: "SRB", iso: "RS", "iso_name" => "SERBIA", numcode: "999" }
 ])
-Spree::Config[:default_country_id] = Spree::Country.find_by_name("United States").id
+Spree::Config[:default_country_id] = Spree::Country.find_by(name: "United States").id

commit 96b4710c155e1344d82cb3e2af849d601c0234cd

Fix `ProductFilters.brand_filter` example
frontend build broke after f02d25566d42f06d2ceea596ff9823d5ad06da67
which highlighted the issue here (we can't pass an object as a value to
a `property_id` key (or we shouldn't)

core/lib/spree/core/product_filters.rb
@@ -109,7 +109,7 @@ module Spree
 
         def ProductFilters.brand_filter
           brand_property = Spree::Property.find_by(name: 'brand')
-          brands = Spree::ProductProperty.where(property: brand_property).pluck(:value).uniq.map(&:to_s)
+          brands = Spree::ProductProperty.where(property_id: brand_property.try(:id)).pluck(:value).uniq.map(&:to_s)
           pp = Spree::ProductProperty.arel_table
           conds = Hash[*brands.map { |b| [b, pp[:value].eq(b)] }.flatten]
           {

frontend/spec/controllers/spree/orders_controller_spec.rb
@@ -60,7 +60,7 @@ describe Spree::OrdersController do
         order.stub(:line_items).and_return([])
         order.stub(:line_items=).with([])
         order.stub(:last_ip_address=)
-        Spree::Order.stub(:find_by_id_and_currency).and_return(order)
+        Spree::Order.stub_chain(:includes, find_by: order)
       end
 
       it "should not result in a flash success" do