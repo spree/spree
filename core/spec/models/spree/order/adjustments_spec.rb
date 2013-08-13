54d5438a5b6560e7a956d72ac609688d1c5bc77f
Ryan Bigg <radarlistener@gmail.com>
2013-08-14 09:49:57 +1000

    clear_adjustments! is long gone, as is ship_total

--- core/spec/models/spree/order/adjustments_spec.rb
+++ core/spec/models/spree/order/adjustments_spec.rb
@@ -2,33 +2,9 @@ require 'spec_helper'
 describe Spree::Order do
   let(:order) { Spree::Order.new }
 
-  context "clear_adjustments" do
-
-    let(:adjustment) { double("Adjustment") }
-
-    it "destroys all order adjustments" do
-      order.stub(:adjustments => adjustment)
-      adjustment.should_receive(:destroy_all)
-      order.clear_adjustments!
-    end
-
-    it "destroy all line item adjustments" do
-      order.stub(:line_item_adjustments => adjustment)
-      adjustment.should_receive(:destroy_all)
-      order.clear_adjustments!
-    end
-  end
-
   context "totaling adjustments" do
     let(:adjustment1) { mock_model(Spree::Adjustment, :amount => 5) }
     let(:adjustment2) { mock_model(Spree::Adjustment, :amount => 10) }
-
-    context "#ship_total" do
-      it "should return the correct amount" do
-        order.stub_chain :adjustments, :shipping => [adjustment1, adjustment2]
-        order.ship_total.should == 15
-      end
-    end
 
     context "#tax_total" do
       it "should return the correct amount" do
