6f3d14a70212c97003b22435317a045092f452ca
Ryan Bigg <radarlistener@gmail.com>
2013-08-14 09:49:45 +1000

    Promotion code checking is now done in CouponApplicator, so we don't need to duplicate this inside Promotion#activate too

--- core/spec/lib/spree/promo/coupon_applicator_spec.rb
+++ core/spec/lib/spree/promo/coupon_applicator_spec.rb
@@ -6,19 +6,19 @@ describe Spree::Promo::CouponApplicator do
   end
 
   describe "#apply" do
-    let(:order) { create(:order, :state => "payment", :coupon_code => "tenoff") }
+    let(:order) do
+      create(:order_with_line_items,
+             :line_items_count => 1,
+             :state => "payment",
+             :coupon_code => "tenoff")
+    end
 
     it "can apply a coupon code to an order" do
-      flat_percent_calc = Spree::Calculator::FlatPercentItemTotal.create(:preferred_flat_percent => "10")
+      flat_rate = Spree::Calculator::FlatRate.create(:preferred_amount => "10")
       promo = Spree::Promotion.create(:name => "Discount", :event_name => "spree.checkout.coupon_code_added", :code => "tenoff", :usage_limit => "10", :starts_at => DateTime.yesterday, :expires_at => DateTime.tomorrow)
-      promo_rule = Spree::Promotion::Rules::ItemTotal.create(:preferred_operator => "gt", :preferred_amount => "1")
-      promo_rule.update_attribute(:activator_id, promo.id)
-      promo_action = Spree::Promotion::Actions::CreateAdjustment.create(:calculator_type => "Spree::Calculator::FlatPercentItemTotal")
-      promo_action.update_attribute(:activator_id, promo.id)
-      flat_percent_calc.update_attribute(:calculable_id, promo.id)
-      Spree::Order.any_instance.stub(:payment_required? => false)
-      Spree::Adjustment.any_instance.stub(:eligible => true)
-      order.update_column(:state, "payment")
+      promo_action = Spree::Promotion::Actions::CreateAdjustment.new(:promotion => promo)
+      promo_action.calculator = flat_rate
+      promo_action.save
       order.coupon_code = "tenoff"
 
       subject.apply
