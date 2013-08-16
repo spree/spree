9a40f734d4f1419f45cb76eacf6ef5b50dc70b53
Tobias Bohwalli <hi@futhr.io>
2013-08-16 17:43:18 +0200

    JS lint.

--- backend/app/assets/javascripts/admin/states.js
+++ backend/app/assets/javascripts/admin/states.js
@@ -1,9 +1,11 @@
-$(document).ready(function() {
-	$("#country").change(function() {
-		var new_state_link_href = $('#new_state_link a').attr('href');
-		var selected_country_id = $('#country option:selected').attr('value');
-	    var new_link = new_state_link_href.replace(/countries\/(\d+)/,
-	    									'countries/'+selected_country_id);
-	    $('#new_state_link a').attr('href', new_link);
-	});
+$(document).ready(function () {
+  'use strict';
+
+  $('#country').on('change', function () {
+    var new_state_link_href = $('#new_state_link a').attr('href');
+    var selected_country_id = $('#country option:selected').attr('value');
+    var new_link = new_state_link_href.replace(/countries\/(\d+)/,
+      'countries/' + selected_country_id);
+    $('#new_state_link a').attr('href', new_link);
+  });
 });
