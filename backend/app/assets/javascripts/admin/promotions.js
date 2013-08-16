da2d2bfda629887f8e32116905d9b7a8e1bda1d7
Tobias Bohwalli <hi@futhr.io>
2013-08-16 17:43:18 +0200

    [backend] Js lint. Fixes #3544

--- backend/app/assets/javascripts/admin/promotions.js
+++ backend/app/assets/javascripts/admin/promotions.js
@@ -1,17 +1,18 @@
-var initProductActions = function(){
+var initProductActions = function () {
+  'use strict';
 
   // Add classes on promotion items for design
-  $(document).on('mouseover mouseout', 'a.delete', function(event) {
-    if (event.type == 'mouseover') {
+  $(document).on('mouseover mouseout', 'a.delete', function (event) {
+    if (event.type === 'mouseover') {
       $(this).parent().addClass('action-remove');
     } else {
       $(this).parent().removeClass('action-remove');
     }
   });
 
-  $('#promotion-filters').find(".variant_autocomplete").variantAutocomplete();
+  $('#promotion-filters').find('.variant_autocomplete').variantAutocomplete();
 
-  $('.calculator-fields').each(function(){
+  $('.calculator-fields').each(function () {
     var $fields_container = $(this);
     var $type_select = $fields_container.find('.type-select');
     var $settings = $fields_container.find('.settings');
@@ -19,15 +20,15 @@ var initProductActions = function(){
     var originalType = $type_select.val();
 
     $warning.hide();
-    $type_select.change(function(){
-      if( $(this).val() == originalType ){
+    $type_select.change(function () {
+      if ($(this).val() === originalType) {
         $warning.hide();
         $settings.show();
         $settings.find('input').removeAttr('disabled');
       } else {
         $warning.show();
         $settings.hide();
-        $settings.find('input').attr('disabled', 'disabled');
+        $settings.find('input').prop('disabled', 'disabled');
       }
     });
   });
@@ -35,10 +36,10 @@ var initProductActions = function(){
   //
   // CreateLineItems Promotion Action
   //
-  ( function(){
-    var hideOrShowItemTables = function(){
-      $('.promotion_action table').each(function(){
-        if($(this).find('td').length == 0){
+  (function () {
+    var hideOrShowItemTables = function () {
+      $('.promotion_action table').each(function () {
+        if ($(this).find('td').length === 0) {
           $(this).hide();
         } else {
           $(this).show();
@@ -48,11 +49,11 @@ var initProductActions = function(){
     hideOrShowItemTables();
 
     // Remove line item
-    var setupRemoveLineItems = function(){
-      $(".remove_promotion_line_item").click(function(){
-        line_items_el = $($('.line_items_string')[0])
-        finder = RegExp($(this).data("variant-id") + "x\\d+")
-        line_items_el.val(line_items_el.val().replace(finder, ""))
+    var setupRemoveLineItems = function () {
+      $('.remove_promotion_line_item').on('click', function () {
+        var line_items_el = $($('.line_items_string')[0]);
+        var finder = new RegExp($(this).data('variant-id') + "x\\d+");
+        line_items_el.val(line_items_el.val().replace(finder, ''));
         $(this).parents('tr').remove();
         hideOrShowItemTables();
       });
@@ -60,39 +61,37 @@ var initProductActions = function(){
 
     setupRemoveLineItems();
     // Add line item to list
-    $(".promotion_action.create_line_items button.add").unbind('click').click(function(e){
+    $('.promotion_action.create_line_items button.add').unbind('click').click(function () {
       var $container = $(this).parents('.promotion_action');
-      var product_name = $container.find("input[name='add_product_name']").val();
-      var variant_id = $container.find("input[name='add_variant_id']").val();
-      var quantity = $container.find("input[name='add_quantity']").val();
-      if(variant_id){
+      var product_name = $container.find('input[name="add_product_name"]').val();
+      var variant_id = $container.find('input[name="add_variant_id"]').val();
+      var quantity = $container.find('input[name="add_quantity"]').val();
+      if (variant_id) {
         // Add to the table
-        var newRow = "<tr><td>" + product_name + "</td><td>" + quantity + "</td><td><img src='/assets/admin/icons/cross.png' /></td></tr>";
+        var newRow = '<tr><td>' + product_name + '</td><td>' + quantity + '</td><td><img src="/assets/admin/icons/cross.png"/></td></tr>';
         $container.find('table').append(newRow);
         // Add to serialized string in hidden text field
-        var $hiddenField = $container.find(".line_items_string");
-        $hiddenField.val($hiddenField.val() + "," + variant_id + "x" + quantity);
+        var $hiddenField = $container.find('.line_items_string');
+        $hiddenField.val($hiddenField.val() + ',' + variant_id + 'x' + quantity);
         setupRemoveLineItems();
         hideOrShowItemTables();
       }
       return false;
     });
 
-  } )();
+  })();
 
-}
+};
 
-$(document).ready(function() {
+$(document).ready(function () {
+
   initProductActions();
 
   // toggle fields for specific events
-  $('#promotion_event_name').change(function() {
-    $('#promotion_code_field').toggle($('#promotion_event_name').val() == 'spree.checkout.coupon_code_added');
-    $('#promotion_path_field').toggle($('#promotion_event_name').val() == 'spree.content.visited');
+  $('#promotion_event_name').on('change', function () {
+    $('#promotion_code_field').toggle($('#promotion_event_name').val() === 'spree.checkout.coupon_code_added');
+    $('#promotion_path_field').toggle($('#promotion_event_name').val() === 'spree.content.visited');
   });
   $('#promotion_event_name').change();
 
 });
-
-
-
