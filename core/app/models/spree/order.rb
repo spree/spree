33e9f7b29b6f6fe64505bcd47e98cf89cb705321
Jeff Dutil <jdutil@burlingtonwebapps.com>
2013-08-18 17:28:59 -0400

    Use order option not a scope for Rails 3.2.x.

--- core/app/models/spree/order.rb
+++ core/app/models/spree/order.rb
@@ -47,9 +47,9 @@ module Spree
     belongs_to :ship_address, foreign_key: :ship_address_id, class_name: 'Spree::Address'
     alias_attribute :shipping_address, :ship_address
 
-    has_many :adjustments, -> { order('created_at ASC') }, as: :adjustable, dependent: :destroy
+    has_many :adjustments, as: :adjustable, dependent: :destroy, order: 'created_at ASC'
     has_many :line_item_adjustments, through: :line_items, source: :adjustments
-    has_many :line_items, -> { order('created_at ASC') }, dependent: :destroy
+    has_many :line_items, dependent: :destroy, order: 'created_at ASC'
     has_many :payments, dependent: :destroy
     has_many :return_authorizations, dependent: :destroy
     has_many :state_changes, as: :stateful
