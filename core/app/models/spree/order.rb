400aa0089fb14356efbbd5c2bfe3ea0d67c1c3ed
Ryan Bigg <radarlistener@gmail.com>
2013-08-29 10:34:44 +1000

    Remove tax_total method that recalculated tax every time

--- core/app/models/spree/order.rb
+++ core/app/models/spree/order.rb
@@ -279,10 +279,6 @@ module Spree
       shipments.sum(:cost)
     end
 
-    def tax_total
-      adjustments.tax.sum(:amount)
-    end
-
     # Creates new tax charges if there are any applicable rates. If prices already
     # include taxes then price adjustments are created instead.
     def create_tax_charge!
