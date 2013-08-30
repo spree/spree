fcf2c0279cb46b9dde6660b50838749eab09b530
Ryan Bigg <radarlistener@gmail.com>
2013-08-30 12:06:48 +1000

    Address information is now only accessible through an order
    This is because we need to first look up if the user has permission to view the order. If the user has the additional permission of being able to modify the order, then they should be able to modify the address too.
    
    Fixes #3635
    
    Conflicts:
    	api/app/controllers/spree/api/addresses_controller.rb
    	core/app/models/spree/ability.rb

--- api/app/controllers/spree/api/addresses_controller.rb
+++ api/app/controllers/spree/api/addresses_controller.rb
@@ -1,16 +1,17 @@
 module Spree
   module Api
     class AddressesController < Spree::Api::BaseController
+      before_filter :find_order
 
       def show
-        @address = Address.find(params[:id])
-        authorize! :read, @address
+        authorize! :read, @order, params[:order_token]
+        find_address
         respond_with(@address)
       end
 
       def update
-        @address = Address.find(params[:id])
-        authorize! :read, @address
+        authorize! :update, @order, params[:order_token]
+        find_address
 
         if @address.update_attributes(params[:address])
           respond_with(@address, :default_template => :show)
@@ -18,6 +19,22 @@ module Spree
           invalid_resource!(@address)
         end
       end
+
+      private
+
+        def find_order
+          @order = Spree::Order.find_by_number!(params[:order_id])
+        end
+
+        def find_address
+          @address = if @order.bill_address_id == params[:id].to_i
+            @order.bill_address
+          elsif @order.ship_address_id == params[:id].to_i
+            @order.ship_address
+          else
+            raise CanCan::AccessDenied
+          end
+        end
     end
   end
 end
--- api/spec/controllers/spree/api/addresses_controller_spec.rb
+++ api/spec/controllers/spree/api/addresses_controller_spec.rb
@@ -7,26 +7,27 @@ module Spree
     before do
       stub_authentication!
       @address = create(:address)
+      @order = create(:order, :bill_address => @address)
     end
 
     context "with their own address" do
       before do
-        Address.any_instance.stub :user => current_api_user
+        Order.any_instance.stub :user => current_api_user
       end
 
       it "gets an address" do
-        api_get :show, :id => @address.id
+        api_get :show, :id => @address.id, :order_id => @order.number
         json_response['address1'].should eq @address.address1
       end
 
       it "updates an address" do
-        api_put :update, :id => @address.id,
+        api_put :update, :id => @address.id, :order_id => @order.number,
                          :address => { :address1 => "123 Test Lane" }
         json_response['address1'].should eq '123 Test Lane'
       end
       
       it "receives the errors object if address is invalid" do
-        api_put :update, :id => @address.id,
+        api_put :update, :id => @address.id, :order_id => @order.number,
                          :address => { :address1 => "" }
                          
         json_response['error'].should_not be_nil
@@ -35,18 +36,19 @@ module Spree
       end
     end
 
-    context "on somebody else's address" do
+    context "on an address that does not belong to this order" do
       before do
-        Address.any_instance.stub :user => stub_model(Spree::LegacyUser)
+        @order.bill_address_id = nil
+        @order.ship_address = nil
       end
 
       it "cannot retreive address information" do
-        api_get :show, :id => @address.id
+        api_get :show, :id => @address.id, :order_id => @order.number
         assert_unauthorized!
       end
 
       it "cannot update address information" do
-        api_get :update, :id => @address.id
+        api_get :update, :id => @address.id, :order_id => @order.number
         assert_unauthorized!
       end
     end
