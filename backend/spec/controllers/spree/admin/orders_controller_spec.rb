0fff21b41f7db577a8bddd4acafd219459022807
Ryan Bigg <radarlistener@gmail.com>
2013-09-03 14:06:18 +1000

    [backend] Correct orders_controller_spec stubbing

--- backend/spec/controllers/spree/admin/orders_controller_spec.rb
+++ backend/spec/controllers/spree/admin/orders_controller_spec.rb
@@ -60,15 +60,15 @@ describe Spree::Admin::OrdersController do
     # Regression test for #3684
     context "#edit" do
       it "does not refresh rates if the order is complete" do
-        order = stub(:order, :complete? => true, :number => "R12345")
-        controller.stub :load_order => order
+        order.stub :complete? => true
+        order.should_receive :next
         order.should_not_receive :refresh_shipment_rates
         spree_get :edit, :id => order.number
       end
 
       it "does refresh the rates if the order is incomplete" do
-        order = stub(:order, :complete? => false, :number => "R12345")
-        controller.stub :load_order => order
+        order.stub :complete? => false
+        order.should_receive :next
         order.should_receive :refresh_shipment_rates
         spree_get :edit, :id => order.number
       end
