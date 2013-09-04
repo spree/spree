766f694632f1c7e1c445ded677e14d88493b5a58
Ryan Bigg <radarlistener@gmail.com>
2013-09-04 11:11:47 +1000

    [backend] Order no longer receives next calls in OrdersController#edit

--- backend/spec/controllers/spree/admin/orders_controller_spec.rb
+++ backend/spec/controllers/spree/admin/orders_controller_spec.rb
@@ -61,14 +61,12 @@ describe Spree::Admin::OrdersController do
     context "#edit" do
       it "does not refresh rates if the order is complete" do
         order.stub :complete? => true
-        order.should_receive :next
         order.should_not_receive :refresh_shipment_rates
         spree_get :edit, :id => order.number
       end
 
       it "does refresh the rates if the order is incomplete" do
         order.stub :complete? => false
-        order.should_receive :next
         order.should_receive :refresh_shipment_rates
         spree_get :edit, :id => order.number
       end
