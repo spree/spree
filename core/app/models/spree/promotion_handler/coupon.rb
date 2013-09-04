51ad23402f1b9fdf060dc1a9329597ad18d5eeb0
Washington Luiz <huoxito@gmail.com>
2013-09-03 23:41:59 -0300

    Fetch promo code by rule
    Since coupon code are now a promotion rule rather than just a field in
    spree_promotions table

--- core/app/models/spree/promotion_handler/coupon.rb
+++ core/app/models/spree/promotion_handler/coupon.rb
@@ -2,55 +2,54 @@ module Spree
   module PromotionHandler
     class Coupon
       attr_reader :order
+      attr_accessor :error
 
       def initialize(order)
         @order = order
       end
 
       def apply
-        if @order.coupon_code.present?
+        if order.coupon_code.present?
           # check if coupon code is already applied
-          if @order.adjustments.promotion.eligible.detect { |p| p.source.promotion.code == @order.coupon_code }.present?
+          if order.adjustments.promotion.eligible.detect { |p| p.source.promotion.code == order.coupon_code }.present?
             return { :coupon_applied? => true, :notice => Spree.t(:coupon_code_already_applied) }
           else
-            promotion = Spree::Promotion.find_by(code: @order.coupon_code)
             if promotion.present?
               handle_present_promotion(promotion)
             else
-              return { :coupon_applied? => false, :error => Spree.t(:coupon_code_not_found) }
+              self.error = Spree.t(:coupon_code_not_found)
             end
           end
-        else
-          return { :coupon_applied? => true }
         end
+      end
+
+      def promotion
+        @promotion ||= Promotion.active.includes(:promotion_rules)
+          .find_by({spree_promotion_rules: {type: Promotion::Rules::CouponCode, code: order.coupon_code}})
       end
 
       private
 
       def handle_present_promotion(promotion)
-        return promotion_expired if promotion.expired?
         return promotion_usage_limit_exceeded if promotion.usage_limit_exceeded?
 
         promotion.activate(:order => @order)
-        promo = @order.adjustments.promotion.detect { |p| p.source.promotion.code == @order.coupon_code }
-        determine_promotion_application_result(promo)
-      end
-
-      def promotion_expired
-        { :coupon_applied? => false, :error =>  Spree.t(:coupon_code_expired) }
+        discount = order.adjustments.promotion.detect { |p| p.source.promotion.code == order.coupon_code }
+        determine_promotion_application_result(discount)
       end
 
       def promotion_usage_limit_exceeded
         { :coupon_applied? => false, :error =>  Spree.t(:coupon_code_max_usage) }
       end
 
-      def determine_promotion_application_result(promo)
-        previous_promo = @order.adjustments.promotion.eligible.first
-        if promo.present? and promo.eligible
+      def determine_promotion_application_result(discount)
+        previous_promo = order.adjustments.promotion.eligible.first
+
+        if discount.present? and discount.eligible
           return { :coupon_applied? => true, :success =>  Spree.t(:coupon_code_applied) }
-        elsif previous_promo.present? and promo.present?
+        elsif previous_promo.present? and discount.present?
           return { :coupon_applied? => false, :error =>  Spree.t(:coupon_code_better_exists) }
-        elsif promo.present?
+        elsif discount.present?
           return { :coupon_applied? => false, :error =>  Spree.t(:coupon_code_not_eligible) }
         else
           # if the promotion was created after the order
