4821abc95194d8d3e0b1a4d360c44e133d9141b5
Washington Luiz <huoxito@gmail.com>
2013-09-03 23:41:59 -0300

    Fetch promo code by rule
    Since coupon code are now a promotion rule rather than just a field in
    spree_promotions table

--- core/spec/models/spree/promotion_handler/coupon_spec.rb
+++ core/spec/models/spree/promotion_handler/coupon_spec.rb
@@ -3,18 +3,40 @@ require 'spec_helper'
 module Spree
   module PromotionHandler
     describe Coupon do
+      let(:order) { double("Order", coupon_code: "Huhu").as_null_object }
+      let!(:promotion) { Promotion.create name: "promo" }
+
       subject { Coupon.new(order) }
+
+      context "coupon code promotion doesnt exist" do
+        it "doesnt fetch any promotion" do
+          expect(subject.promotion).to be_blank
+        end
+
+        context "tries to apply" do
+          it "populates error message" do
+            subject.apply
+            expect(subject.error).to eq Spree.t(:coupon_code_not_found)
+          end
+        end
+      end
+
+      context "existing coupon code promotion" do
+        let!(:rule) { Promotion::Rules::CouponCode.create(promotion: promotion, code: "Huhu") }
+
+        it "fetches with given code" do
+          expect(subject.promotion).to eq promotion
+        end
+      end
 
       pending "coupon already applied to the order" do
         subject.apply
-        expect(subject.errors.first).to eq Spree.t(:coupon_code_already_applied)
+        expect(subject.error).to eq Spree.t(:coupon_code_already_applied)
       end
-
-      pending "coupon code not found"
 
       pending "coupon code hit max usage" do
         subject.apply
-        expect(subject.errors.first).to eq Spree.t(:coupon_code_max_usage)
+        expect(subject.error).to eq Spree.t(:coupon_code_max_usage)
       end
 
       context "existing eligible promo" do
