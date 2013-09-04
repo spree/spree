d5da3829df2999445f0b419cb0e2b664f4ec9445
Washington Luiz <huoxito@gmail.com>
2013-09-04 00:13:01 -0300

    Dont validate code presence if rule is a new record
    This gives the user a chance to first associate the rule with a
    promotion (via UI) and only then actually insert the coupon code and
    save the record again

--- core/app/models/spree/promotion/rules/coupon_code.rb
+++ core/app/models/spree/promotion/rules/coupon_code.rb
@@ -2,7 +2,7 @@ module Spree
   class Promotion
     module Rules
       class CouponCode < PromotionRule
-        validates :code, presence: true
+        validates :code, presence: true, unless: "self.new_record?"
 
         def eligible?(order)
           order.coupon_code.upcase == code.upcase
