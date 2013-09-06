77605ba31f053f9d580add28dcf4711d54864512
Ryan Bigg <radarlistener@gmail.com>
2013-09-06 14:50:19 +1000

    Reversion of the rule commits
    Revert "PromotionHandler::Cart scope only promotions with rules"
    
    This reverts commit 7d4457f90a3ec94a5e53bf62fbc80d2bce49c650.
    
    Revert "Include coupon code promotion rule partial in backend"
    
    This reverts commit 09eda47b66c66801470684902ed03b3d296499df.
    
    Revert "Dont validate code presence if rule is a new record"
    
    This reverts commit b1b74fadd3ba27c6821b2a14a8627143355f9386.
    
    Revert "Remove both code and timeframe fields in promo form"
    
    This reverts commit 2a061822dd70306930317ef5586bfd79a7d55582.

--- core/spec/models/spree/promotion/rules/coupon_code_spec.rb
+++ core/spec/models/spree/promotion/rules/coupon_code_spec.rb
@@ -4,23 +4,12 @@ module Spree
   class Promotion
     module Rules
       describe CouponCode do
-        context "new record" do
-          it "doesnt validate presence of new code" do
-            subject.valid?
-            expect(subject).to have(0).error_on(:code)
-          end
-        end
+        it "validates presence of code" do
+          subject.valid?
+          expect(subject).to have(1).error_on(:code)
 
-        context "persisted record" do
-          before { subject.save }
-
-          it "validates presence of code" do
-            subject.valid?
-            expect(subject).to have(1).error_on(:code)
-
-            subject.code = "Huhu"
-            expect(subject).to have(0).error_on(:code)
-          end
+          subject.code = "Huhu"
+          expect(subject).to have(0).error_on(:code)
         end
 
         context "matches order coupon code" do
