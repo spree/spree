5d7cedd2f8d57c8ac687e21188320e1ccc9a0b23
brchristian <brchristian@gmail.com>
2013-09-09 13:21:38 -0700

    Update :original SHA for promo_admin_tabs
    Fixes https://github.com/spree/spree/issues/3670

--- promo/app/overrides/promo_admin_tabs.rb
+++ promo/app/overrides/promo_admin_tabs.rb
@@ -3,4 +3,4 @@ Deface::Override.new(:virtual_path => "spree/layouts/admin",
                      :insert_bottom => "[data-hook='admin_tabs'], #admin_tabs[data-hook]",
                      :text => "<%= tab(:promotions, :url => spree.admin_promotions_path, :icon => 'icon-bullhorn') %>",
                      :disabled => false,
-                     :original => '3e847740dc3e7f924aba1ccb4cb00c7b841649e3')
+                     :original => '031652cf5a054796022506622082ab6d2693699f')
