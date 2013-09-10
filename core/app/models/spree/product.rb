4d0c4ee7403000e74465577b6c9dbad552f3bc5f
John Hawthorn <john@freerunningtechnologies.com>
2013-09-09 21:24:50 -0700

    cleanup like_any with some arel

--- core/app/models/spree/product.rb
+++ core/app/models/spree/product.rb
@@ -146,10 +146,11 @@ module Spree
     end
 
     def self.like_any(fields, values)
-      # PostgreSQL is case sensitive, so we need to use ILIKE to work around that.
-      like = ActiveRecord::Base.connection.adapter_name == 'PostgreSQL' ? 'ILIKE' : 'LIKE'
-      where_str = fields.map { |field| Array.new(values.size, "#{self.quoted_table_name}.#{field} #{like} ?").join(' OR ') }.join(' OR ')
-      self.where([where_str, values.map { |value| "%#{value}%" } * fields.size].flatten)
+      where fields.map { |field|
+        values.map { |value|
+          arel_table[field].matches("%#{value}%")
+        }.inject(:or)
+      }.inject(:or)
     end
 
     # Suitable for displaying only variants that has at least one option value.
