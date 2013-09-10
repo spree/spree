ee4b613db591b4157a637a7335b099e1ccfe6015
Ryan Bigg <radarlistener@gmail.com>
2013-09-10 15:09:05 +1000

    FlatPercentItemTotal now calculates on a per-line-item basis

--- core/app/models/spree/calculator/flat_percent_item_total.rb
+++ core/app/models/spree/calculator/flat_percent_item_total.rb
@@ -8,10 +8,8 @@ module Spree
       Spree.t(:flat_percent)
     end
 
-    def compute(object)
-      return unless object.present? and object.respond_to?(:item_total)
-      item_total = object.item_total
-      value = item_total * BigDecimal(self.preferred_flat_percent.to_s) / 100.0
+    def compute(line_item)
+      value = line_item.amount * BigDecimal(self.preferred_flat_percent.to_s) / 100.0
       (value * 100).round.to_f / 100
     end
   end
