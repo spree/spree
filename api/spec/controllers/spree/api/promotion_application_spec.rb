72a5b74c47af975fc3492580415a4cdc2dc02c0c
Ryan Bigg <radarlistener@gmail.com>
2013-09-11 16:49:16 +1000

    [api] Add ability to apply coupon codes from the API
    This will be used in frontend's checkout when the user hits 'submit' on the payment step. Once the user clicks the button, they'll see 'applying coupon code' while there's a synchronous background request that is attempting to apply this code. If the application is succesful, the coupon_code field will be cleared and the form will be submitted as normal. If it is unsuccessful, we will show them an error message.
    
    The benefit of this change is two fold: 1) applying a promotional code is computationally difficult and may cause the payment to take longer than the user has patience for and 2) that you won't lose the payment information if the coupon code is invalid. The latter being the bigger win here, imo.

---
+++ api/spec/controllers/spree/api/promotion_application_spec.rb
@@ -0,0 +1,46 @@
+require 'spec_helper'
+
+module Spree::Api
+  describe OrdersController do
+    render_views
+
+    before do
+      stub_authentication!
+    end
+
+    context "with an available promotion" do
+      let!(:order) { create(:order_with_line_items, :line_items_count => 1) }
+      let!(:promotion) do 
+        promotion = Spree::Promotion.create(name: "10% off", code: "10off")
+        calculator = Spree::Calculator::FlatPercentItemTotal.create(preferred_flat_percent: "10")
+        action = Spree::Promotion::Actions::CreateItemAdjustments.create(calculator: calculator)
+        promotion.actions << action
+        promotion
+      end
+
+      it "can apply a coupon code to the order" do
+        api_put :apply_coupon_code, :id => order.to_param, :coupon_code => "10off", :order_token => order.token
+        response.status.should == 200
+        json_response["success"].should == "The coupon code was successfully applied to your order."
+        json_response["error"].should be_blank
+        json_response["successful"].should be_true
+      end
+
+      context "with an expired promotion" do
+        before do
+          promotion.starts_at = 2.weeks.ago
+          promotion.expires_at = 1.week.ago
+          promotion.save
+        end
+
+        it "fails to apply" do
+          api_put :apply_coupon_code, :id => order.to_param, :coupon_code => "10off", :order_token => order.token
+          response.status.should == 422
+          json_response["success"].should be_blank
+          json_response["error"].should == "The coupon code is expired"
+          json_response["successful"].should be_false
+        end
+      end
+    end
+  end
+end
