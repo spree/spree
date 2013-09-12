eb42293f37d7539e7255de6baef8ec3511420388
Peter Berkenbosch <peter@pero-ict.nl>
2013-09-12 15:47:12 +0200

    [api] add :product_id and :lock_version to the variant attributes list.

--- api/app/helpers/spree/api/api_helpers.rb
+++ api/app/helpers/spree/api/api_helpers.rb
@@ -21,7 +21,7 @@ module Spree
       end
 
       def variant_attributes
-        [:id, :name, :count_on_hand, :sku, :price, :weight, :height, :width, :depth, :is_master, :cost_price, :permalink]
+        [:id, :name, :count_on_hand, :sku, :price, :weight, :height, :width, :depth, :is_master, :cost_price, :permalink, :product_id, :lock_version]
       end
 
       def image_attributes
