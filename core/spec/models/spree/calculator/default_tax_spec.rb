424530c33d26bc809acb0d13683c2c7f1e3894ff
Ryan Bigg <radarlistener@gmail.com>
2013-09-24 15:00:59 +1000

    Use discounted_amount when calculating tax for line items

--- core/spec/models/spree/calculator/default_tax_spec.rb
+++ core/spec/models/spree/calculator/default_tax_spec.rb
@@ -13,6 +13,15 @@ describe Spree::Calculator::DefaultTax do
     context "when tax is included in price" do
       let(:vat) { true }
       context "when the variant matches the tax category" do
+
+        context "when line item is discounted" do
+          before { line_item.promo_total = -1 }
+
+          it "should be equal to the item's discounted total * rate" do
+            calculator.compute(line_item).should == 1.38
+          end
+        end
+
         it "should be equal to the item total * rate" do
           calculator.compute(line_item).should == 1.43
         end
@@ -20,6 +29,15 @@ describe Spree::Calculator::DefaultTax do
     end
 
     context "when tax is not included in price" do
+
+      context "when the line item is discounted" do
+        before { line_item.promo_total = -1 }
+
+        it "should be equal to the item's discounted total * rate" do
+          calculator.compute(line_item).should == 1.45
+        end
+      end
+
       context "when the variant matches the tax category" do
         it "should be equal to the item total * rate" do
           calculator.compute(line_item).should == 1.50
