900afcd8777025ab22e5ce7e6621a8d5481b1659
Ryan Bigg <radarlistener@gmail.com>
2013-09-25 11:17:21 +1000

    Ensure that stock items cannot be seen/modified by normal API users

--- api/spec/controllers/spree/api/stock_items_controller_spec.rb
+++ api/spec/controllers/spree/api/stock_items_controller_spec.rb
@@ -13,39 +13,75 @@ module Spree
       stub_authentication!
     end
 
-    it 'gets list of stock items' do
-      api_get :index, stock_location_id: stock_location.to_param
-      json_response['stock_items'].first.should have_attributes(attributes)
-      json_response['stock_items'].first['variant']['sku'].should eq 'ABC'
+    context "as a normal user" do
+      it "cannot list stock items for a stock location" do
+        api_get :index, stock_location_id: stock_location.to_param
+        response.status.should == 404
+      end
+
+      it "cannot see a stock item" do
+        api_get :show, stock_location_id: stock_location.to_param, id: stock_item.to_param
+        response.status.should == 404
+      end
+
+      it "cannot create a stock item" do
+        variant = create(:variant)
+        params = {
+          stock_location_id: stock_location.to_param,
+          stock_item: {
+            variant_id: variant.id,
+            count_on_hand: '20'
+          }
+        }
+
+        api_post :create, params
+        response.status.should == 404
+      end
+
+      it "cannot update a stock item" do
+        api_put :update, stock_location_id: stock_location.to_param, stock_item_id: stock_item.to_param
+        response.status.should == 404
+      end
+
+      it "cannot destroy a stock item" do
+        api_delete :destroy, stock_location_id: stock_location.to_param, stock_item_id: stock_item.to_param
+        response.status.should == 404
+      end
     end
 
-    it 'requires a stock_location_id to be passed as a parameter' do
-      api_get :index
-      json_response['error'].should =~ /stock_location_id parameter must be provided/
-      response.status.should == 422
-    end
-
-    it 'can control the page size through a parameter' do
-      api_get :index, stock_location_id: stock_location.to_param, per_page: 1
-      json_response['count'].should == 1
-      json_response['current_page'].should == 1
-    end
-
-    it 'can query the results through a paramter' do
-      stock_item.update_column(:count_on_hand, 30)
-      api_get :index, stock_location_id: stock_location.to_param, q: { count_on_hand_eq: '30' }
-      json_response['count'].should == 1
-      json_response['stock_items'].first['count_on_hand'].should eq 30
-    end
-
-    it 'gets a stock item' do
-      api_get :show, stock_location_id: stock_location.to_param, id: stock_item.to_param
-      json_response.should have_attributes(attributes)
-      json_response['count_on_hand'].should eq stock_item.count_on_hand
-    end
-
-    context 'as an admin' do
+    context "as an admin" do
       sign_in_as_admin!
+
+      it 'cannot list of stock items' do
+        api_get :index, stock_location_id: stock_location.to_param
+        json_response['stock_items'].first.should have_attributes(attributes)
+        json_response['stock_items'].first['variant']['sku'].should eq 'ABC'
+      end
+
+      it 'requires a stock_location_id to be passed as a parameter' do
+        api_get :index
+        json_response['error'].should =~ /stock_location_id parameter must be provided/
+        response.status.should == 422
+      end
+
+      it 'can control the page size through a parameter' do
+        api_get :index, stock_location_id: stock_location.to_param, per_page: 1
+        json_response['count'].should == 1
+        json_response['current_page'].should == 1
+      end
+
+      it 'can query the results through a paramter' do
+        stock_item.update_column(:count_on_hand, 30)
+        api_get :index, stock_location_id: stock_location.to_param, q: { count_on_hand_eq: '30' }
+        json_response['count'].should == 1
+        json_response['stock_items'].first['count_on_hand'].should eq 30
+      end
+
+      it 'gets a stock item' do
+        api_get :show, stock_location_id: stock_location.to_param, id: stock_item.to_param
+        json_response.should have_attributes(attributes)
+        json_response['count_on_hand'].should eq stock_item.count_on_hand
+      end
 
       it 'can create a new stock item' do
         variant = create(:variant)
