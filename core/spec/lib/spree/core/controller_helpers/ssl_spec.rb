1400c667a69068b024f7368d5ac5af24a2b854b1
John Hawthorn <john@freerunningtechnologies.com>
2013-10-02 11:51:23 -0700

    Fix redirect loop with redirect_https_to_http
    force_non_ssl_redirect was testing if the actions was included in
    ssl_{required,allowed}_actions, but didn't consider the case that it was
    empty (which should mean all actions). This caused an infinite redirect
    loop on admin and checkout pages with the option enabled.
    
    This fixes the issue and simplifies the code by combining the lists of
    allowed and required ssl actions. Also added some specs.
    
    Fixes #3813

---
+++ core/spec/lib/spree/core/controller_helpers/ssl_spec.rb
@@ -0,0 +1,60 @@
+require 'spec_helper'
+
+describe Spree::Core::ControllerHelpers::SSL, :type => :controller do
+  controller do
+    include Spree::Core::ControllerHelpers::SSL
+    def index; render text: 'index'; end
+    def self.ssl_supported?; true; end
+  end
+  before do
+    @routes.draw do
+      get '/anonymous/index'
+    end
+  end
+
+  describe 'redirect to http' do
+    before { Spree::Config[:redirect_https_to_http] = true  }
+    after  { Spree::Config[:redirect_https_to_http] = false }
+    before { request.env['HTTPS'] = 'on' }
+
+    context 'allowed two actions' do
+      controller(described_class) do
+        ssl_allowed :index
+        ssl_allowed :foobar
+      end
+      specify{ controller.ssl_allowed_actions.should == [:index, :foobar] }
+      specify{ get(:index).should be_success }
+    end
+    context 'allowed a single action' do
+      controller(described_class){ ssl_allowed :index }
+      specify{ controller.ssl_allowed_actions.should == [:index] }
+      specify{ get(:index).should be_success }
+    end
+    context 'allowed all actions' do
+      controller(described_class){ ssl_allowed }
+      specify{ controller.ssl_allowed_actions.should == [] }
+      specify{ get(:index).should be_success }
+    end
+    context 'ssl not allowed' do
+      controller(described_class){ }
+      specify{ get(:index).should be_redirect }
+    end
+  end
+
+  describe 'redirect to https' do
+    context 'required a single action' do
+      controller(described_class){ ssl_required :index }
+      specify{ controller.ssl_allowed_actions.should == [:index] }
+      specify{ get(:index).should be_redirect }
+    end
+    context 'required all actions' do
+      controller(described_class){ ssl_required }
+      specify{ controller.ssl_allowed_actions.should == [] }
+      specify{ get(:index).should be_redirect }
+    end
+    context 'not required' do
+      controller(described_class){ }
+      specify{ get(:index).should be_success }
+    end
+  end
+end
