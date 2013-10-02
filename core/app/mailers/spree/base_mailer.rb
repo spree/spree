68ee4ec30a8ea8138f803913529367c0b7dedd4e
Sean O'Hara <sohara@sohara.com>
2013-10-02 15:16:45 -0400

    Add :send_core_emails config prefernce
    Allow developers to use standard rails mail configuration (in config
    files) by setting :override_actionmailer_config to false without sending
    spree core emails (e.g. order confirmation). This is useful e.g. in the
    case where devs have opted to use an external mail API such as Mandrill
    for store-related emails but still want to use ActionMailer in other
    parts of their app.

--- core/app/mailers/spree/base_mailer.rb
+++ core/app/mailers/spree/base_mailer.rb
@@ -8,5 +8,14 @@ module Spree
       Spree::Money.new(amount).to_s
     end
     helper_method :money
+
+    alias_method :orig_mail, :mail
+
+    def mail(headers={}, &block)
+      if Spree::Config[:send_core_emails]
+        orig_mail
+      end
+    end
+
   end
 end
