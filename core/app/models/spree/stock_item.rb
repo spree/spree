e38f02f9146d7a23c831984306b3f4530c4431ae
nishant-cyro <nisahnt.tuteja@vinsol.com>
2013-10-02 11:33:53 +0530

    add validation to verify that count_on_hand of backorderable should not go negative if it is not backorderable

--- core/app/models/spree/stock_item.rb
+++ core/app/models/spree/stock_item.rb
@@ -8,6 +8,7 @@ module Spree
 
     validates_presence_of :stock_location, :variant
     validates_uniqueness_of :variant_id, scope: :stock_location_id
+    validates :count_on_hand, :numericality => { :greater_than_or_equal_to => 0 }, :if => :verify_count_on_hand?
 
     delegate :weight, to: :variant
 
@@ -45,6 +46,10 @@ module Spree
     end
 
     private
+      def verify_count_on_hand?
+        count_on_hand_changed? && !backorderable? && (count_on_hand < count_on_hand_was) && (count_on_hand < 0)
+      end
+
       def count_on_hand=(value)
         write_attribute(:count_on_hand, value)
       end
