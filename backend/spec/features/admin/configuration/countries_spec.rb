421c3747b8b059a7e7374eb9e6ee8c0a3acffc37
Washington Luiz <huoxito@gmail.com>
2013-10-02 00:59:45 -0300

    Allow users to create new countries via backend UI
    Fixes #3807

--- backend/spec/features/admin/configuration/countries_spec.rb
+++ backend/spec/features/admin/configuration/countries_spec.rb
@@ -4,10 +4,14 @@ module Spree
   describe "Countries" do
     stub_authorization!
 
-    let!(:country) { create(:country) }
-
     it "deletes a state", js: true do
       visit spree.admin_countries_path
+      click_link "New Country"
+
+      fill_in "Name", with: "Brazil"
+      fill_in "Iso Name", with: "BRL"
+      click_button "Create"
+
       click_icon :trash
       page.driver.browser.switch_to.alert.accept
       wait_for_ajax
