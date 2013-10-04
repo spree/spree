7dc91640fcd2efcfafbc684472d630a3ed2a4e71
James Strong <laspluviosillas@gmail.com>
2013-10-03 23:05:12 -0500

    Add process method to BogusSimple gateway to be able to process payments.
    Fixes issue where hitting "process" on payments screen breaks with
    BogusSimple gateway.

--- core/app/models/spree/gateway/bogus_simple.rb
+++ core/app/models/spree/gateway/bogus_simple.rb
@@ -6,6 +6,14 @@ module Spree
       false
     end
 
+    def capture(money, response_code, options = {})
+      if response_code == '12345'
+        ActiveMerchant::Billing::Response.new(true, 'Bogus Gateway: Forced success', {}, :test => true, :authorization => '67890')
+      else
+        ActiveMerchant::Billing::Response.new(false, 'Bogus Gateway: Forced failure', :error => 'Bogus Gateway: Forced failure', :test => true)
+      end
+    end
+
     def authorize(money, credit_card, options = {})
       if VALID_CCS.include? credit_card.number
         ActiveMerchant::Billing::Response.new(true, 'Bogus Gateway: Forced success', {}, :test => true, :authorization => '12345', :avs_result => { :code => 'A' })
