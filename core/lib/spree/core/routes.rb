9742872fe9bf91b36b81b5689cf95620615a094c
Ryan Bigg <radarlistener@gmail.com>
2013-10-08 14:22:08 +1100

    Ensure that routes are not drawn more than once from core/routes.rb

--- core/lib/spree/core/routes.rb
+++ core/lib/spree/core/routes.rb
@@ -3,12 +3,28 @@ module Spree
     class Engine < ::Rails::Engine
       def self.add_routes(&block)
         @spree_routes ||= []
-        @spree_routes << block
+
+        # Anything that causes the application's routes to be reloaded,
+        # will cause this method to be called more than once
+        # i.e. https://github.com/plataformatec/devise/blob/31971e69e6a1bcf6c7f01eaaa44f227c4af5d4d2/lib/devise/rails.rb#L14
+        # In the case of Devise, this *only* happens in the production env
+        # This coupled with Rails 4's insistence that routes are not drawn twice,
+        # poses quite a serious problem.
+        #
+        # This is mainly why this whole file exists in the first place.
+        #
+        # Thus we need to make sure that the routes aren't drawn twice.
+        unless @spree_routes.include?(block)
+          @spree_routes << block
+        end
       end
 
       def self.append_routes(&block)
         @append_routes ||= []
-        @append_routes << block
+        # See comment in add_routes.
+        unless @append_routes.include?(block)
+          @append_routes << block
+        end
       end
 
       def self.draw_routes(&block)
@@ -17,6 +33,9 @@ module Spree
         eval_block(block) if block_given?
         @spree_routes.each { |r| eval_block(&r) }
         @append_routes.each { |r| eval_block(&r) }
+        # # Clear out routes so that they aren't drawn twice.
+        @spree_routes = []
+        @append_routes = []
       end
 
       private
