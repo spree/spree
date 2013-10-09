81f8d6336f3657d4e9430cc54beeee47c68ae1ab
brianthecoder <brian@20jeans.com>
2013-10-09 16:03:01 -0700

    remove extra include

--- core/app/controllers/spree/base_controller.rb
+++ core/app/controllers/spree/base_controller.rb
@@ -8,7 +8,6 @@ class Spree::BaseController < ApplicationController
   include Spree::Core::ControllerHelpers::Common
   include Spree::Core::ControllerHelpers::Search
   include Spree::Core::ControllerHelpers::StrongParameters
-  include Spree::Core::ControllerHelpers::Search
 
   respond_to :html
 end
