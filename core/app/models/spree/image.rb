cb30b9c6be58049b98abb2cf59ab05d536dbabbb
Ryan Bigg <radarlistener@gmail.com>
2013-10-09 13:53:49 +1100

    Use sRGB colorspace
    Fixes #3843
    Fixes #3104

--- core/app/models/spree/image.rb
+++ core/app/models/spree/image.rb
@@ -10,7 +10,7 @@ module Spree
                       default_style: :product,
                       url: '/spree/products/:id/:style/:basename.:extension',
                       path: ':rails_root/public/spree/products/:id/:style/:basename.:extension',
-                      convert_options: { all: '-strip -auto-orient' }
+                      convert_options: { all: '-strip -auto-orient -colorspace sRGB' }
 
     # save the w,h of the original image (from which others can be calculated)
     # we need to look at the write-queue for images which have not been saved yet
