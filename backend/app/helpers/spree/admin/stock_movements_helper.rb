88c8678b1d2d4c0a53752d02b0cb7ca8626eafd8
Ryan Bigg <radarlistener@gmail.com>
2013-10-14 16:22:18 +1100

    [backend] Include options text when displaying variants on the stock movements page

--- backend/app/helpers/spree/admin/stock_movements_helper.rb
+++ backend/app/helpers/spree/admin/stock_movements_helper.rb
@@ -8,6 +8,13 @@ module Spree
           ""
         end
       end
+
+      def display_variant(stock_movement)
+        variant = stock_movement.stock_item.variant
+        output = variant.name
+        output += "<br>(#{variant.options_text})" unless variant.options_text.blank?
+        output.html_safe
+      end
     end
   end
 end
