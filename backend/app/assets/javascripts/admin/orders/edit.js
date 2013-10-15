f909ba88c180769859d667c991eaec2fa5e2abbb
John Hawthorn <john@freerunningtechnologies.com>
2013-10-14 20:42:51 -0700

    Remove some dead order admin javascript
    The element referred to was removed in 72e5d47
    
    Fixes #3872

--- backend/app/assets/javascripts/admin/orders/edit.js
+++ backend/app/assets/javascripts/admin/orders/edit.js
@@ -1,22 +1,5 @@
 $(document).ready(function () {
   'use strict';
 
-  $('#add_line_item_to_order').on('click', function () {
-    if ($('#add_variant_id').val() === '') {
-      return false;
-    }
-
-    var update_target = '#' + $(this).attr('data-update');
-    $.post(this.href, {
-        'line_item[variant_id]': $('#add_variant_id').val(),
-        'line_item[quantity]': $('#add_quantity').val()
-      },
-
-      function (data) {
-        $(update_target).html(data);
-      });
-    return false;
-  });
-
   $('[data-hook="add_product_name"]').find('.variant_autocomplete').variantAutocomplete();
 });
