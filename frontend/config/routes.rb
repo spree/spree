10334b68ce69a624ce0c7af7bd6a3ad78c6e33b7
Ryan Bigg <radarlistener@gmail.com>
2013-10-21 11:57:57 +1100

    [frontend] Remove orders/destroy route

--- frontend/config/routes.rb
+++ frontend/config/routes.rb
@@ -19,7 +19,7 @@ Spree::Core::Engine.add_routes do
   get '/orders/populate', :to => populate_redirect
   get '/orders/:id/token/:token' => 'orders#show', :as => :token_order
 
-  resources :orders, :except => [:new, :create] do
+  resources :orders, :except => [:new, :create, :destroy] do
     post :populate, :on => :collection
   end
 
