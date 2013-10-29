97ea470053a5a8921668228792292b337878a95c
Ryan Bigg <radarlistener@gmail.com>
2013-10-28 23:04:42 -0400

    [frontend] Wait a couple of seconds in address_spec for poltergeist to catch up with latest changes

--- frontend/spec/features/address_spec.rb
+++ frontend/spec/features/address_spec.rb
@@ -69,6 +69,7 @@ describe "Address", inaccessible: true do
 
         select france.name, :from => @country_css
         page.find(@state_name_css).should have_content('')
+        sleep(2)
         find(@state_name_css)['class'].should_not =~ /hidden/
         find(@state_name_css)['class'].should_not =~ /required/
         find(@state_select_css)['class'].should_not =~ /required/
