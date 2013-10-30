cc19c20003a2ce2012add766d9d9dc28177e917b
Washington Luiz <huoxito@gmail.com>
2013-10-30 15:15:50 -0400

    Set state_name when state record not found [api]

--- api/app/models/spree/order_decorator.rb
+++ api/app/models/spree/order_decorator.rb
@@ -153,7 +153,12 @@ Spree::Order.class_eval do
       end
 
       address.delete(:state)
-      address[:state_id] = Spree::State.where(search).first!.id
+
+      if state = Spree::State.where(search).first
+        address[:state_id] = state.id
+      else
+        address[:state_name] = search[:name] || search[:abbr]
+      end
     rescue Exception => e
       raise "#{e.message} #{search}"
     end
