f22c269ceaad4e123c5cdf4a85fd1fc1f79169e9
Ryan Bigg <radarlistener@gmail.com>
2013-10-30 16:13:40 -0400

    Simplify Shipment#to_param code
    (lol)

--- core/app/models/spree/shipment.rb
+++ core/app/models/spree/shipment.rb
@@ -64,9 +64,7 @@ module Spree
     end
 
     def to_param
-      number if number
-      generate_shipment_number unless number
-      number.to_s.to_url.upcase
+      number
     end
 
     def backordered?
