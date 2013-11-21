e278b34f0db8bb0a193f410d1d2113ce9c16d96e
Ryan Bigg <radarlistener@gmail.com>
2013-11-22 10:53:31 +1100

    [backend] Show additional and included tax in order summary

--- core/app/models/spree/order.rb
+++ core/app/models/spree/order.rb
@@ -124,8 +124,12 @@ module Spree
       Spree::Money.new(adjustment_total, { currency: currency })
     end
 
-    def display_tax_total
-      Spree::Money.new(tax_total, { currency: currency })
+    def display_included_tax_total
+      Spree::Money.new(included_tax_total, { currency: currency })
+    end
+
+    def display_additional_tax_total
+      Spree::Money.new(additional_tax_total, { currency: currency })
     end
 
     def display_ship_total
