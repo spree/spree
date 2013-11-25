f80d15a0e00dbce6be3d29f9981ff595fea8fefd
Michael Tucker <mtucker@godaddy.com>
2013-11-25 09:49:40 -0700

    Inventory Tracking by Variant: fix resource controller model load issue

--- backend/app/controllers/spree/admin/resource_controller.rb
+++ backend/app/controllers/spree/admin/resource_controller.rb
@@ -121,7 +121,6 @@ class Spree::Admin::ResourceController < Spree::Admin::BaseController
     end
 
     def model_class
-      return "Spree::#{self.class.superclass.controller_name.classify}".constantize unless Spree.const_defined?(controller_name.classify)
       "Spree::#{controller_name.classify}".constantize
     end
 
@@ -130,8 +129,7 @@ class Spree::Admin::ResourceController < Spree::Admin::BaseController
     end
 
     def object_name
-      return controller_name.singularize if Spree.const_defined?(controller_name.classify)
-      self.class.superclass.controller_name.singularize
+      controller_name.singularize
     end
 
     def load_resource
