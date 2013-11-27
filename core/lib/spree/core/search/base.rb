44eab7c86508ddde93251eefee8e07ea792a507b
Ryan Bigg <radarlistener@gmail.com>
2013-11-27 16:10:56 +1100

    Revert back to using Product.in_taxon as the scope for TaxonsController
    This fixes frontend's taxons_spec:20

--- core/lib/spree/core/search/base.rb
+++ core/lib/spree/core/search/base.rb
@@ -33,11 +33,8 @@ module Spree
 
         protected
           def get_base_scope
-            base_scope = if taxon
-              taxon.products.active
-            else
-              Spree::Product.active
-            end
+            base_scope = Spree::Product.active
+            base_scope = base_scope.in_taxon(taxon) unless taxon.blank?
             base_scope = get_products_conditions_for(base_scope, keywords)
             base_scope = add_search_scopes(base_scope)
             base_scope
