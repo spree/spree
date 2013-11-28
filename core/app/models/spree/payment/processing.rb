402a288c9740c000ec9d78759e08d1dd91927064
Clarke Brunsdon <clarke@freerunningtechnologies.com>
2013-11-28 15:54:43 -0800

    Add customer_id to gateway options
    :customer and :email are both the email, it could be useful to uniquely
    identifiy a customer on the gateway
    
    Fixes #4051

--- core/app/models/spree/payment/processing.rb
+++ core/app/models/spree/payment/processing.rb
@@ -115,14 +115,15 @@ module Spree
       end
 
       def gateway_options
-        options = { :email    => order.email,
-                    :customer => order.email,
-                    :ip       => order.last_ip_address,
+        options = { :email       => order.email,
+                    :customer    => order.email,
+                    :customer_id => order.user_id,
+                    :ip          => order.last_ip_address,
                     # Need to pass in a unique identifier here to make some
                     # payment gateways happy.
                     #
                     # For more information, please see Spree::Payment#set_unique_identifier
-                    :order_id => gateway_order_id }
+                    :order_id    => gateway_order_id }
 
         options.merge!({ :shipping => order.ship_total * 100,
                          :tax      => order.tax_total * 100,
