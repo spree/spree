9101c258a16a2d6b6264c3ee6ccd9108ccdfc6a3
Peter Berkenbosch <peter@pero-ict.nl>
2013-11-29 16:10:59 +0100

    the case statement is no longer needed since we call the proper method automatically.
    Related to #4041

--- core/app/models/spree/calculator/default_tax.rb
+++ core/app/models/spree/calculator/default_tax.rb
@@ -6,50 +6,38 @@ module Spree
       Spree.t(:default_tax)
     end
 
-    def compute(computable)
-      case computable
-        when Spree::Shipment
-          compute_shipment(computable)
-        when Spree::LineItem
-          compute_line_item(computable)
-        when Spree::Order # for legacy reasons
-          compute_order(computable)
+    # Default tax calculator still needs to support orders for legacy reasons
+    # Orders created before Spree 2.1 had tax adjustments applied to the order, as a whole.
+    # Orders created with Spree 2.2 and after, have them applied to the line items individually.
+    def compute_order(order)
+      matched_line_items = order.line_items.select do |line_item|
+        line_item.tax_category == rate.tax_category
+      end
+
+      line_items_total = matched_line_items.sum(&:total)
+      round_to_two_places(line_items_total * rate.amount)
+    end
+
+    def compute_shipment(shipment)
+      round_to_two_places(shipment.discounted_cost * rate.amount)
+    end
+
+    def compute_line_item(line_item)
+      if line_item.tax_category == rate.tax_category
+        if rate.included_in_price
+          deduced_total_by_rate(line_item, rate)
+        else
+          round_to_two_places(line_item.discounted_amount * rate.amount)
+        end
+      else
+        0
       end
     end
 
 
-    private
 
       def rate
         self.calculable
-      end
-
-      # Default tax calculator still needs to support orders for legacy reasons
-      # Orders created before Spree 2.1 had tax adjustments applied to the order, as a whole.
-      # Orders created with Spree 2.2 and after, have them applied to the line items individually.
-      def compute_order(order)
-        matched_line_items = order.line_items.select do |line_item|
-          line_item.tax_category == rate.tax_category
-        end
-
-        line_items_total = matched_line_items.sum(&:total)
-        round_to_two_places(line_items_total * rate.amount)
-      end
-
-      def compute_shipment(shipment)
-        round_to_two_places(shipment.discounted_cost * rate.amount)
-      end
-
-      def compute_line_item(line_item)
-        if line_item.tax_category == rate.tax_category
-          if rate.included_in_price
-            deduced_total_by_rate(line_item, rate)
-          else
-            round_to_two_places(line_item.discounted_amount * rate.amount)
-          end
-        else
-          0
-        end
       end
 
       def round_to_two_places(amount)
