191f764ba48b165b32adb11b95b5001d20498312
Edger <blazesolo@gmail.com>
2013-12-01 17:56:12 +0400

    stock_movements helper fix for stock_transfer
    Fixes #4037

---
+++ backend/spec/helpers/admin/stock_movements_helper_spec.rb
@@ -0,0 +1,30 @@
+# coding: UTF-8
+require 'spec_helper'
+
+describe Spree::Admin::StockMovementsHelper do
+
+  describe "#pretty_originator" do
+
+    context "transfering between two locations" do
+      let(:destination_location) { create(:stock_location_with_items) }
+      let(:source_location) { create(:stock_location_with_items) }
+      let(:stock_item) { source_location.stock_items.order(:id).first }
+      let(:variant) { stock_item.variant }
+
+      before do
+        @stock_transfer = Spree::StockTransfer.create(reference: 'PO123')
+        variants = { variant => 5 }
+        @stock_transfer.transfer(source_location,
+                       destination_location,
+                       variants)
+        helper.pretty_originator(@stock_transfer.stock_movements.last)
+      end
+
+
+      it "returns link to stock transfer" do
+        helper.pretty_originator(@stock_transfer.stock_movements.last).should eq @stock_transfer.number
+      end
+    end
+  end
+
+end
