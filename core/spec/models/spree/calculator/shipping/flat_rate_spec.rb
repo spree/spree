23f3a0526deea5f276e789e47e1715156b2eadd3
Peter Berkenbosch <peter@pero-ict.nl>
2013-12-02 14:47:03 +0100

    use a proper PORO instance instead of a double (otherwise it wants to calculate compute_mock) make the shipping calc specs green

--- core/spec/models/spree/calculator/shipping/flat_rate_spec.rb
+++ core/spec/models/spree/calculator/shipping/flat_rate_spec.rb
@@ -5,15 +5,22 @@ module Spree
     describe FlatRate do
       let(:variant1) { build(:variant) }
       let(:variant2) { build(:variant) }
-      let(:package) { double(Stock::Package,
-                           order: mock_model(Order),
-                           contents: [Stock::Package::ContentItem.new(variant1, 2),
-                                      Stock::Package::ContentItem.new(variant2, 1)]) }
+
+      let(:package) do
+        Stock::Package.new(
+          build(:stock_location),
+          mock_model(Order),
+          [
+            Stock::Package::ContentItem.new(variant1, 2),
+            Stock::Package::ContentItem.new(variant2, 1)
+          ]
+        )
+      end
 
       subject { Calculator::Shipping::FlatRate.new(:preferred_amount => 4.00) }
 
       it 'always returns the same rate' do
-        subject.compute(package)
+        expect(subject.compute(package)).to eql 4.00
       end
     end
   end
