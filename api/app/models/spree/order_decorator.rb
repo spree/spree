8a375785b93af0a513dbf1b9158e3c51f88a7939
Magnus von Koeller <hello@magnusvk.com>
2013-12-05 11:38:55 -0500

    Make the parameters consistent across orders#create and orders#update
    Fixes #4057

--- api/app/models/spree/order_decorator.rb
+++ api/app/models/spree/order_decorator.rb
@@ -167,8 +167,12 @@ Spree::Order.class_eval do
 
   def update_line_items(line_item_params)
     return if line_item_params.blank?
-    line_item_params.each do |id, attributes|
-      self.line_items.find(id).update_attributes!(attributes)
+    line_item_params.each_value do |attributes|
+      if attributes[:id].present?
+        self.line_items.find(attributes[:id]).update_attributes!(attributes)
+      else
+        self.line_items.create!(attributes)
+      end
     end
     self.ensure_updated_shipments
   end
