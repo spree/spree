28e1aa14b8ece632d9f7bef731e16cf384615143
Ryan Bigg <radarlistener@gmail.com>
2013-12-10 17:41:47 +1100

    Only require email when past cart or address
    This means that an email field can now be provided on either the cart or address forms
    
    More importantly, the 'state' field will now say 'address' when users reach that state, rather than 'cart'
    
    Fixes #4079

--- frontend/app/controllers/spree/orders_controller.rb
+++ frontend/app/controllers/spree/orders_controller.rb
@@ -29,11 +29,7 @@ module Spree
         respond_with(@order) do |format|
           format.html do
             if params.has_key?(:checkout)
-              # Need to call next_transition.run_callbacks here, because calling
-              # next will not actually trigger the transition if the order requires an
-              # email address; which is the default behaviour.
-              # See discussion in #4079.
-              @order.next_transition.run_callbacks if @order.cart?
+              @order.next if @order.cart?
               redirect_to checkout_state_path(@order.checkout_steps.first)
             else
               redirect_to cart_path
