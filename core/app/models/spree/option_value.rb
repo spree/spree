5012ddd76d4d77b90d76ed13a4e50960ae896ca9
Ryan Bigg <radarlistener@gmail.com>
2013-12-12 09:51:44 +1100

    Specify class_name for OptionValue's option_type association
    If you attempted to assign any non Spree::OptionType object to this association, it would result in a vague uninitialized constant error
    
    Conflicts:
    	core/app/models/spree/option_value.rb

--- core/app/models/spree/option_value.rb
+++ core/app/models/spree/option_value.rb
@@ -1,6 +1,6 @@
 module Spree
   class OptionValue < ActiveRecord::Base
-    belongs_to :option_type
+    belongs_to :option_type, :class_name => 'Spree::OptionType', :touch => true
     acts_as_list scope: :option_type
     has_and_belongs_to_many :variants, join_table: 'spree_option_values_variants', class_name: "Spree::Variant"
 
