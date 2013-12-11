cf8165a6c541ba5176d01f53610d035f60c439a8
Peter Berkenbosch <peter@pero-ict.nl>
2013-12-11 10:32:21 +0100

    Make sure that api requires authentication by default
    Fixes #4096
    
    Conflicts:
    	api/app/models/spree/api_configuration.rb

--- api/app/models/spree/api_configuration.rb
+++ api/app/models/spree/api_configuration.rb
@@ -1,5 +1,6 @@
 module Spree
   class ApiConfiguration < Preferences::Configuration
+    # Make sure the api requires authentication by default to prevent unwanted access
     preference :requires_authentication, :boolean, :default => true
   end
 end
