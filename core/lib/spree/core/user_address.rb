27eeead5387cccdc9b1090f2079ab1da5f89f0a5
Ryan Bigg <radarlistener@gmail.com>
2013-12-12 09:38:04 +1100

    Do not use ship address in UserAddress if it is not present on the order

--- core/lib/spree/core/user_address.rb
+++ core/lib/spree/core/user_address.rb
@@ -14,14 +14,15 @@ module Spree
           b_address = self.bill_address || self.build_bill_address
           b_address.attributes = order.bill_address.attributes.except('id', 'updated_at', 'created_at')
           b_address.save
+          self.update_attributes(bill_address_id: b_address.id)
 
-          s_address = self.ship_address || self.build_ship_address
-          s_address.attributes = order.ship_address.attributes.except('id', 'updated_at', 'created_at')
-          s_address.save
-
-          self.update_attributes(
-            bill_address_id: b_address.id, ship_address_id: s_address.id
-          )
+          # May not be present if delivery step has been removed
+          if order.ship_address
+            s_address = self.ship_address || self.build_ship_address
+            s_address.attributes = order.ship_address.attributes.except('id', 'updated_at', 'created_at')
+            s_address.save
+            self.update_attributes(ship_address_id: s_address.id)
+          end
         end
       end
     end
