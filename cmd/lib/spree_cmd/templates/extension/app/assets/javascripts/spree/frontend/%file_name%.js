068346a6b7158376a9c0371223cf6f3a8e8ada98
Peter Berkenbosch <peter@pero-ict.nl>
2013-12-14 11:25:15 +0100

    update the comments to be a bit better.

--- cmd/lib/spree_cmd/templates/extension/app/assets/javascripts/spree/frontend/%file_name%.js
+++ cmd/lib/spree_cmd/templates/extension/app/assets/javascripts/spree/frontend/%file_name%.js
@@ -1,3 +1,2 @@
 // Placeholder manifest file.
-// the installer will append this "//= require spree/frontend/<%= file_name %>\n"
-// to the vendored assets here: vendor/assets/javascripts/spree/frontend/all.js'
+// the installer will append this file to the app vendored assets here: vendor/assets/javascripts/spree/frontend/all.js'
