9bc6c14df974bb56288eb4f5e1496800b3792428
Ryan Bigg <radarlistener@gmail.com>
2014-01-07 10:08:42 +1100

    [api] Use correct amount for ship total in orders_controller_spec

--- api/spec/controllers/spree/api/orders_controller_spec.rb
+++ api/spec/controllers/spree/api/orders_controller_spec.rb
@@ -396,8 +396,8 @@ module Spree
           it "includes the ship_total in the response" do
             api_get :show, :id => order.to_param
 
-            json_response['ship_total'].should == '10.0'
-            json_response['display_ship_total'].should == '$10.00'
+            json_response['ship_total'].should == '0.0'
+            json_response['display_ship_total'].should == '$0.00'
           end
 
           it "returns available shipments for an order" do
