66568dbbb65169b9bdbe6359a70e32f120c4354e
Peter Berkenbosch <peter@pero-ict.nl>
2014-01-06 09:47:01 +0100

    set default for api config require_authentication back to true.
    skip filters on frontend for coupon_code bypass the require_authentication setting on backend specs.
    
    Signed-off-by: Peter Berkenbosch <peter@pero-ict.nl>
    
    Fixes #4165

--- api/app/controllers/spree/api/orders_controller.rb
+++ api/app/controllers/spree/api/orders_controller.rb
@@ -2,6 +2,9 @@ module Spree
   module Api
     class OrdersController < Spree::Api::BaseController
 
+      skip_before_filter :check_for_user_or_api_key, only: :apply_coupon_code
+      skip_before_filter :authenticate_user, only: :apply_coupon_code
+
       # Dynamically defines our stores checkout steps to ensure we check authorization on each step.
       Order.checkout_steps.keys.each do |step|
         define_method step do
