24ee2d370c9241d382ec88eccf47c74428af3c32
Washington Luiz <huoxito@gmail.com>
2014-01-12 01:21:44 -0300

    Set ActiveSupport::JSON as rabl json_engine
    Make api templates render timestamps with miliseconds which I previously
    thought was a rails issue. By making rabl run ActiveSupport::JSON.encode
    instead of JSON.dump we get those miliseconds (which in turn will call
    as_json)

--- api/lib/spree/api/engine.rb
+++ api/lib/spree/api/engine.rb
@@ -9,6 +9,11 @@ module Spree
       Rabl.configure do |config|
         config.include_json_root = false
         config.include_child_root = false
+
+        # Motivation here it make it call as_json when rendering timestamps
+        # and therefore display miliseconds. Otherwise it would fall to
+        # JSON.dump which doesn't display the miliseconds
+        config.json_engine = ActiveSupport::JSON
       end
 
       config.view_versions = [1]
