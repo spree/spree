e22ab09ec281846c388e9d35e06ec224381e0f96
Ryan Bigg <radarlistener@gmail.com>
2014-01-14 13:29:19 +1100

    Cache each taxonomy individually
    This means that when a taxon within a taxonomy is updated, we only need to invalidate that taxonomy's cache and no other

--- core/app/helpers/spree/store_helper.rb
+++ core/app/helpers/spree/store_helper.rb
@@ -7,11 +7,6 @@ module Spree
       %w{thank_you}.exclude? params[:action]
     end
 
-    def cache_key_for_taxonomies
-      max_updated_at = @taxonomies.maximum(:updated_at).to_i
-      "taxonomies/all-#{max_updated_at}"
-    end
-
     def cache_key_for_taxons
       max_updated_at = @taxons.maximum(:updated_at).to_i
       parts = [@taxon.try(:id), max_updated_at].compact.join("-")
