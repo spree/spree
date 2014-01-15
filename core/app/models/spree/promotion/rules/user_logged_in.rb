5d9622d3ad164eff582da97e227c7428328787bc
Ryan Bigg <radarlistener@gmail.com>
2014-01-15 15:49:04 +1100

    Change UserLoggedInRule to use presence of a user as its check
    The logic goes that if the user is signed in, then the order will be associated to that user (through the magic of associate_user). Therefore, if an order has a user then the user has been signed in at some point in the past. If the user is not signed in, only 'email' would be set for the user
    
    Conflicts:
    	core/app/models/spree/promotion/rules/user_logged_in.rb

--- core/app/models/spree/promotion/rules/user_logged_in.rb
+++ core/app/models/spree/promotion/rules/user_logged_in.rb
@@ -2,15 +2,13 @@ module Spree
   class Promotion
     module Rules
       class UserLoggedIn < PromotionRule
-
         def applicable?(promotable)
           promotable.is_a?(Spree::Order)
         end
 
         def eligible?(order, options = {})
-          return order.try(:user).try(:anonymous?) == false
+          return order.user.present?
         end
-
       end
     end
   end
