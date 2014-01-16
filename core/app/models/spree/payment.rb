800e23a6142ec0026a4813f7c30ffc2e62d1ef2b
Alessandro Lepore <lepore.ale@gmail.com>
2014-01-16 21:09:37 +0100

    use AR 4 syntax or prefix table name on some scopes

--- core/app/models/spree/payment.rb
+++ core/app/models/spree/payment.rb
@@ -31,7 +31,7 @@ module Spree
     scope :completed, -> { with_state('completed') }
     scope :pending, -> { with_state('pending') }
     scope :failed, -> { with_state('failed') }
-    scope :valid, -> { where('state NOT IN (?)', %w(failed invalid)) }
+    scope :valid, -> { where.not(state: %w(failed invalid)) }
 
     after_rollback :persist_invalid
 
