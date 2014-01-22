aaa0a868f93c09a9eb2d6deb864dbbfd733f7203
Daniel J. Pritchett <dpritchett@gmail.com>
2014-01-22 13:23:32 -0600

    Added nuance to shipping rate estimator Now distinguishing between $0 rates (possibly valid) and nil rates which could result in a huge loss to a store in a case where a $1000 shipping fee is reported as a nil by a bad calculator.
    Also added an order-level model error message when no eligible
    shipping rate is found.

--- core/app/models/spree/stock/estimator.rb
+++ core/app/models/spree/stock/estimator.rb
@@ -15,7 +15,8 @@ module Spree
 
         shipping_methods.each do |shipping_method|
           cost = calculate_cost(shipping_method, package)
-          shipping_rates << shipping_method.shipping_rates.new(:cost => cost) unless cost.nil?
+          rate = shipping_method.shipping_rates.new(:cost => cost) unless cost.nil?
+          shipping_rates << rate unless rate.nil?
         end
 
         shipping_rates.sort_by! { |r| r.cost || 0 }
@@ -29,11 +30,18 @@ module Spree
             shipping_rates.first.selected = true
           end
         end
+        
+        notify_no_eligible_methods if shipping_rates.select(&:selected?).none?
 
         shipping_rates
       end
 
       private
+      # If all calculators claim to be unavailable, pass an error upstream
+      def notify_no_eligible_methods
+        order.errors.add(:base, Spree.t(:no_eligible_shipping_methods_found))
+      end
+
       def shipping_methods(package)
         package.shipping_methods.select do |ship_method|
           calculator = ship_method.calculator
@@ -53,9 +61,9 @@ module Spree
       end
 
       def log_calculator_exception(ship_method, exception)
-        Rails.logger.info("Something went wrong calculating rates with the #{ship_method.name} (ID=#{ship_method.id}) shipping method.")
-        Rails.logger.info("*" * 50)
-        Rails.logger.info(exception.backtrace.join("\n"))
+        Rails.logger.error("Something went wrong calculating rates with the #{ship_method.name} (ID=#{ship_method.id}) shipping method.")
+        Rails.logger.error("*" * 50)
+        Rails.logger.error(exception.backtrace.join("\n"))
       end
     end
   end
