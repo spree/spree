080e260a4d69d686175a1350e61f370c458284c9
Clarke Brunsdon <clarke@freerunningtechnologies.com>
2014-01-22 14:52:46 -0800

    Allow halting of updating_from_params callbacks by returning false
    Without specifying a :terminator to define_callbacks, its impossible to
    halt the event from happening.
    
    And yes, that is a string that is eval'd, as per:
    
      http://api.rubyonrails.org/classes/ActiveSupport/Callbacks/ClassMethods.html#method-i-define_callbacks
    
    Fixes #4244

--- core/app/models/spree/order/checkout.rb
+++ core/app/models/spree/order/checkout.rb
@@ -201,7 +201,7 @@ module Spree
             checkout_step_index(state) > checkout_step_index(self.state)
           end
 
-          define_callbacks :updating_from_params
+          define_callbacks :updating_from_params, terminator: 'result == false'
 
           set_callback :updating_from_params, :before, :update_params_payment_source
 
