4b9c195ece5581bc9a33177d83fee91a30969f20
John Dyer <lbrapid@gmail.com>
2014-01-23 14:25:40 -0500

    [api] Allow variants and option types to be created/updated through products api

--- api/app/controllers/spree/api/products_controller.rb
+++ api/app/controllers/spree/api/products_controller.rb
@@ -51,8 +51,31 @@ module Spree
 
       def update
         authorize! :update, Product
+
+        variants_attributes = params[:product].delete(:variants_attributes) || []
+        option_type_attributes = params[:product].delete(:option_types) || []
+
         @product = find_product(params[:id])
         if @product.update_attributes(params[:product])
+          variants_attributes.each do |variant_attribute|
+            # update the variant if the id is present in the payload
+            if variant_attribute['id'].present?
+              @product.variants.find(variant_attribute['id'].to_i).update_attributes(variant_attribute)
+            else
+              variant = @product.variants.new
+              variant.update_attributes(variant_attribute)
+            end
+          end
+
+          option_type_attributes.each do |name|
+            option_type = OptionType.where(name: name).first_or_initialize do |option_type|
+              option_type.presentation = name
+              option_type.save!
+            end
+
+            @product.option_types << option_type unless @product.option_types.include?(option_type)
+          end
+
           respond_with(@product, :status => 200, :default_template => :show)
         else
           invalid_resource!(@product)
