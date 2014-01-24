69868d6f49bcbd420e8de968cd1d8f4b0d09fd3d
Ryan Bigg <radarlistener@gmail.com>
2014-01-24 16:12:13 +1100

    [api] Call Adjustment#close! and not Adjustment#finalize! from create_adjustments_from_api

--- api/app/models/spree/order_decorator.rb
+++ api/app/models/spree/order_decorator.rb
@@ -92,7 +92,7 @@ Spree::Order.class_eval do
         adjustment = self.adjustments.build(:amount => a[:amount].to_f,
                                             :label => a[:label])
         adjustment.save!
-        adjustment.finalize!
+        adjustment.close!
       rescue Exception => e
         raise "Order import adjustments: #{e.message} #{a}"
       end
