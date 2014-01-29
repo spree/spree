f374ccdd00c2452c4b51c24c4cfc2d88a39462ce
Ryan Bigg <radarlistener@gmail.com>
2014-01-29 17:45:32 +1100

    [backend] Fix fields used in reports_spec test

--- backend/spec/features/admin/reports_spec.rb
+++ backend/spec/features/admin/reports_spec.rb
@@ -51,8 +51,8 @@ describe "Reports" do
       click_link "Reports"
       click_link "Sales Total"
 
-      fill_in "q_created_at_gt", :with => 1.week.ago
-      fill_in "q_created_at_lt", :with => 1.week.from_now
+      fill_in "q_completed_at_gt", :with => 1.week.ago
+      fill_in "q_completed_at_lt", :with => 1.week.from_now
       click_button "Search"
 
       page.should have_content("$300.00")
