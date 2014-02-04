0c9a743ed46d505edb8489727873902a42d92756
Washington Luiz <huoxito@gmail.com>
2014-02-04 01:46:54 -0300

    Adapt api/products eager load to new templates structure

--- api/app/controllers/spree/api/base_controller.rb
+++ api/app/controllers/spree/api/base_controller.rb
@@ -130,14 +130,14 @@ module Spree
         variants_associations = [{ option_values: :option_type }, :default_price, :prices, :images]
         if current_api_user.has_spree_role?("admin")
           scope = Product.with_deleted.accessible_by(current_ability, :read)
-            .includes(:product_properties, :option_types, variants_including_master: variants_associations)
+            .includes(:properties, :option_types, variants: variants_associations, master: variants_associations)
 
           unless params[:show_deleted]
             scope = scope.not_deleted
           end
         else
           scope = Product.accessible_by(current_ability, :read).active
-            .includes(:product_properties, :option_types, variants_including_master: variants_associations)
+            .includes(:properties, :option_types, variants: variants_associations, master: variants_associations)
         end
 
         scope
