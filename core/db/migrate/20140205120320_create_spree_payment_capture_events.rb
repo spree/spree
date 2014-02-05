50d65b515e49e2a6796b798771b649686e59b812
Ryan Bigg <radarlistener@gmail.com>
2014-02-05 15:03:26 +0000

    Use decimal field for capture_events, just like payment table

--- core/db/migrate/20140205120320_create_spree_payment_capture_events.rb
+++ core/db/migrate/20140205120320_create_spree_payment_capture_events.rb
@@ -1,7 +1,7 @@
 class CreateSpreePaymentCaptureEvents < ActiveRecord::Migration
   def change
     create_table :spree_payment_capture_events do |t|
-      t.integer :amount
+      t.decimal :amount, precision: 10, scale: 2, default: 0.0
       t.integer :payment_id
 
       t.timestamps
