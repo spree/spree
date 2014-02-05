ef1f880ce34a79c3619173bbd986a7b1c9aaa629
Francisco Orvalho <francisco.orvalho@surfdome.com>
2014-02-05 17:42:20 +0000

    Display 'Capture events' table in the Payment show page
    Fixes #4301

--- core/app/models/spree/payment_capture_event.rb
+++ core/app/models/spree/payment_capture_event.rb
@@ -1,5 +1,9 @@
 module Spree
   class PaymentCaptureEvent < ActiveRecord::Base
     belongs_to :payment, class_name: 'Spree::Payment'
+
+    def display_amount
+      Spree::Money.new(amount, { currency: payment.currency })
+    end
   end
 end
