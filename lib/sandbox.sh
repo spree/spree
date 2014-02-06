cce105076da6eac4360c67d42f97b84fe630d999
Ryan Bigg <radarlistener@gmail.com>
2014-02-06 15:32:30 +0000

    Specify branch for spree_auth_devise in sandbox script so we can use local git repos

--- lib/sandbox.sh
+++ lib/sandbox.sh
@@ -10,7 +10,7 @@ fi
 
 cd ./sandbox
 echo "gem 'spree', :path => '..'" >> Gemfile
-echo "gem 'spree_auth_devise', :github => 'spree/spree_auth_devise'" >> Gemfile
+echo "gem 'spree_auth_devise', :github => 'spree/spree_auth_devise', :branch => 'master'" >> Gemfile
 
 bundle install --gemfile Gemfile
 bundle exec rails g spree:install --auto-accept --user_class=Spree::User
