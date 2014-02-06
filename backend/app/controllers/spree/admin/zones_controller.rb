03cfe2cc345476e24d6a7c767623f92ff3d6db1c
Ryan Bigg <radarlistener@gmail.com>
2014-02-06 06:10:53 +0000

    Do not use orders_per_page to determine amount of zones returned in Admin::ZonesController
    Fixes #4305

--- backend/app/controllers/spree/admin/zones_controller.rb
+++ backend/app/controllers/spree/admin/zones_controller.rb
@@ -13,7 +13,7 @@ module Spree
           params[:q] ||= {}
           params[:q][:s] ||= "ascend_by_name"
           @search = super.ransack(params[:q])
-          @zones = @search.result.page(params[:page]).per(Spree::Config[:orders_per_page])
+          @zones = @search.result.page(params[:page]).per(params[:per_page])
         end
 
         def load_data
