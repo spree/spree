42a437bd7fa710704983b17784cb8041177019a0
Ryan Bigg <radarlistener@gmail.com>
2014-02-14 15:59:54 +1100

    [api] Use order.contents.remove to remove line items from orders

--- api/app/controllers/spree/api/line_items_controller.rb
+++ api/app/controllers/spree/api/line_items_controller.rb
@@ -25,7 +25,8 @@ module Spree
 
       def destroy
         @line_item = order.line_items.find(params[:id])
-        @line_item.destroy
+        variant = Spree::Variant.find(@line_item.variant_id)
+        @order.contents.remove(variant, @line_item.quantity)
         respond_with(@line_item, status: 204)
       end
 
