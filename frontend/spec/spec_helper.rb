5bfb45bcd8efc1c6e01478d78eb38bdd922808da
Ryan Bigg <radarlistener@gmail.com>
2014-02-14 12:29:17 +1100

    Only skip capybara inaccessible specs if accessible driver is loaded

--- frontend/spec/spec_helper.rb
+++ frontend/spec/spec_helper.rb
@@ -66,8 +66,10 @@ RSpec.configure do |config|
   # instead of true.
   config.use_transactional_fixtures = false
 
-  config.around(:each, :inaccessible => true) do |example|
-    Capybara::Accessible.skip_audit { example.run }
+  if ENV['WEBDRIVER'] == 'accessible'
+    config.around(:each, :inaccessible => true) do |example|
+      Capybara::Accessible.skip_audit { example.run }
+    end
   end
 
   config.before(:each) do
