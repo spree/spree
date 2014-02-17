df296d78bc665719a685005bbfb83badd6fdd024
Ryan Bigg <radarlistener@gmail.com>
2014-02-17 15:42:45 +1100

    Ensure prices with currencies displayed do not break variant radio buttons on cart form
    Fixes #4342
    
    Conflicts:
    	frontend/app/views/spree/products/_cart_form.html.erb

--- core/lib/spree/money.rb
+++ core/lib/spree/money.rb
@@ -199,7 +199,7 @@ module Spree
       if options[:html]
         # 1) prevent blank, breaking spaces
         # 2) prevent escaping of HTML character entities
-        output = output.gsub(" ", "&nbsp;").html_safe
+        output = output.sub(" ", "&nbsp;").html_safe
       end
       output
     end
