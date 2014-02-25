815991f9fb9ca2047c75ee5ca9768871effb11b6
Willian van der Velde <willian@pythonheads.nl>
2014-02-25 17:35:40 +0100

    Pagination and search in property list
    Fixes #4369
    
    Conflicts:
    	core/app/models/spree/app_configuration.rb

--- backend/app/controllers/spree/admin/properties_controller.rb
+++ backend/app/controllers/spree/admin/properties_controller.rb
@@ -1,11 +1,24 @@
 module Spree
   module Admin
     class PropertiesController < ResourceController
+      def index
+        respond_with(@collection)
+      end
 
-      # Looks like this action is unused
-      def filtered
-        @properties = Property.where('lower(name) LIKE ?', "%#{params[:q].mb_chars.downcase}%").order(:name)
-        render :template => "spree/admin/properties/filtered", :layout => false
+      private
+
+      def collection
+        return @collection if @collection.present?
+        # params[:q] can be blank upon pagination
+        params[:q] = {} if params[:q].blank?
+
+        @collection = super
+        @search = @collection.ransack(params[:q])
+        @collection = @search.result.
+              page(params[:page]).
+              per(Spree::Config[:properties_per_page])
+
+        @collection
       end
     end
   end
--- core/app/models/spree/app_configuration.rb
+++ core/app/models/spree/app_configuration.rb
@@ -57,6 +57,7 @@ module Spree
     preference :max_level_in_taxons_menu, :integer, default: 1 # maximum nesting level in taxons menu
     preference :orders_per_page, :integer, default: 15
     preference :prices_inc_tax, :boolean, default: false
+    preference :properties_per_page, :integer, default: 15
     preference :products_per_page, :integer, default: 12
     preference :promotions_per_page, :integer, default: 15
     preference :redirect_https_to_http, :boolean, :default => false
