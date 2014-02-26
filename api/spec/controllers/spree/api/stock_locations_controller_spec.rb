4919a53a87ae548ca276bbcd39d0328c42f23eab
Ryan Bigg <radarlistener@gmail.com>
2014-02-26 15:17:56 -0500

    Port stock locations to API to AMS

--- api/spec/controllers/spree/api/stock_locations_controller_spec.rb
+++ api/spec/controllers/spree/api/stock_locations_controller_spec.rb
@@ -59,22 +59,22 @@ module Spree
       it 'can control the page size through a parameter' do
         create(:stock_location)
         api_get :index, per_page: 1
-        json_response['count'].should == 1
-        json_response['current_page'].should == 1
-        json_response['pages'].should == 2
+        json_response['meta']['count'].should == 1
+        json_response['meta']['current_page'].should == 1
+        json_response['meta']['pages'].should == 2
       end
 
       it 'can query the results through a paramter' do
         expected_result = create(:stock_location, name: 'South America')
         api_get :index, q: { name_cont: 'south' }
-        json_response['count'].should == 1
+        json_response['meta']['count'].should == 1
         json_response['stock_locations'].first['name'].should eq expected_result.name
       end
 
       it "gets a stock location" do
         api_get :show, id: stock_location.to_param
-        json_response.should have_attributes(attributes)
-        json_response['name'].should eq stock_location.name
+        json_response['stock_location'].should have_attributes(attributes)
+        json_response['stock_location']['name'].should eq stock_location.name
       end
 
       it "can create a new stock location" do
@@ -87,7 +87,7 @@ module Spree
 
         api_post :create, params
         response.status.should == 201
-        json_response.should have_attributes(attributes)
+        json_response['stock_location'].should have_attributes(attributes)
       end
 
       it "can update a stock location" do
@@ -100,7 +100,7 @@ module Spree
 
         api_put :update, params
         response.status.should == 200
-        json_response['name'].should eq 'South Pole'
+        json_response['stock_location']['name'].should eq 'South Pole'
       end
 
       it "can delete a stock location" do
