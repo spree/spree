4544d2099b0ad96d4789c9db889037dc944e04fb
darren <darren.hicks@gmail.com>
2014-03-03 15:31:13 -0700

    Revert "Removing protected_attributes blocker in hopes of buying us some time..."
    This reverts commit 50c42d43f5de62451b7bb1aff36205879e6ec515.

--- lib/spree.rb
+++ lib/spree.rb
@@ -4,15 +4,12 @@ require 'spree_backend'
 require 'spree_frontend'
 require 'spree_sample'
 
-
-# Buying some time for us to remove protected_attributes from PYR...
-
-# begin
-#   require 'protected_attributes'
-#   puts "*" * 75
-#   puts "[FATAL] Spree does not work with the protected_attributes gem installed!"
-#   puts "You MUST remove this gem from your Gemfile. It is incompatible with Spree."
-#   puts "*" * 75
-#   exit
-# rescue LoadError
-# end
+begin
+  require 'protected_attributes'
+  puts "*" * 75
+  puts "[FATAL] Spree does not work with the protected_attributes gem installed!"
+  puts "You MUST remove this gem from your Gemfile. It is incompatible with Spree."
+  puts "*" * 75
+  exit
+rescue LoadError
+end
