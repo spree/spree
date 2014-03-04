babcf1610dc8e54df594559513843bae9c0158a7
Alessandro Lepore <lepore.ale@gmail.com>
2014-03-04 08:42:25 +0100

    fix _taxonomies.html.erb cache key

---
+++ frontend/spec/features/caching/taxons_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe 'taxons', :caching => true do
+  let!(:taxonomy) { create(:taxonomy) }
+  let!(:taxon) { create(:taxon, :taxonomy => taxonomy) }
+
+  before do
+    # warm up the cache
+    visit spree.root_path
+    assert_written_to_cache("views/spree/taxonomies/#{taxonomy.id}")
+    assert_written_to_cache("views/taxons/#{taxon.updated_at.utc.to_i}")
+
+    clear_cache_events
+  end
+
+  it "busts the cache when max_level_in_taxons_menu conf changes" do
+    Spree::Config[:max_level_in_taxons_menu] = 5
+    visit spree.root_path
+    assert_written_to_cache("views/spree/taxonomies/#{taxonomy.id}")
+    expect(cache_writes.count).to eq(1)
+  end
+end
