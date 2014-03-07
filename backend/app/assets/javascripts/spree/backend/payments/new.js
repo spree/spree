8a3a80b08abf80fbed2fcee4b429ba1caf68baf1
Washington Luiz <huoxito@gmail.com>
2014-03-06 23:20:42 -0300

    Display only payment method selected partial (backend)
    Drop a bunch of repeated ids on html

--- backend/app/assets/javascripts/spree/backend/payments/new.js
+++ backend/app/assets/javascripts/spree/backend/payments/new.js
@@ -1,16 +1,44 @@
-$(document).ready(function(){
+//= require jquery.payment
+$(document).ready(function() {
+  if ($("#new_payment").is("*")) {
+    $(".cardNumber").payment('formatCardNumber');
+    $(".cardExpiry").payment('formatCardExpiry');
+    $(".cardCode").payment('formatCardCVC');
 
-  $("#card_new").radioControlsVisibilityOfElement('#card_form');
+    $(".cardNumber").change(function() {
+      $(".ccType").val($.payment.cardType(this.value))
+    })
 
-  $('select.jump_menu').change(function(){
-    window.location = this.options[this.selectedIndex].value;
-  });
+    $('.payment_methods_radios').click(
+      function() {
+        $('.payment-methods').hide();
+        if (this.checked) {
+          $('#payment_method_' + this.value).show();
+        }
+      }
+    );
 
-  $('#cvv_link').click(function(event){
-    window_name = 'cvv_info';
-    window_options = 'left=20,top=20,width=500,height=500,toolbar=0,resizable=0,scrollbars=1';
-    window.open($(this).prop('href'), window_name, window_options);
-    event.preventDefault();
-  });
+    $('.payment_methods_radios').each(
+      function() {
+        if (this.checked) {
+          $('#payment_method_' + this.value).show();
+        } else {
+          $('#payment_method_' + this.value).hide();
+        }
+      }
+    );
 
+    $('.cvvLink').click(function(event){
+      window_name = 'cvv_info';
+      window_options = 'left=20,top=20,width=500,height=500,toolbar=0,resizable=0,scrollbars=1';
+      window.open($(this).prop('href'), window_name, window_options);
+      event.preventDefault();
+    });
+
+    $(".card_new").radioControlsVisibilityOfElement('.card_form');
+
+    $('select.jump_menu').change(function(){
+      window.location = this.options[this.selectedIndex].value;
+    });
+  }
 });
