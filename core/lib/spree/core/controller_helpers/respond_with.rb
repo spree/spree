ffd884553d537ec2b260c81f76649c7bd39158c4
Lucjan Suski <lucjansuski@gmail.com>
2014-03-11 01:07:58 +0800

    API BaseController inherits ActionController::Base
    Spree::Api::BaseController now inherits from ActionController::Base.
    Removed respond_with override, to fix being unable to pass
    default_template to options. Removed unnecessary includes.

--- core/lib/spree/core/controller_helpers/respond_with.rb
+++ core/lib/spree/core/controller_helpers/respond_with.rb
@@ -1,29 +1,3 @@
-module ActionController
-  class Base
-    def respond_with(*resources, &block)
-      raise "In order to use respond_with, first you need to declare the formats your " <<
-            "controller responds to in the class level" if self.class.mimes_for_respond_to.empty?
-
-      if collector = retrieve_collector_from_mimes(&block)
-        options = resources.size == 1 ? {} : resources.extract_options!
-
-        if defined_response = collector.response and !Spree::BaseController.spree_responders.keys.include?(self.class.to_s.to_sym)
-          if action = options.delete(:action)
-            render :action => action
-          else
-            defined_response.call
-          end
-        else
-          # The action name is needed for processing
-          options.merge!(:action_name => action_name.to_sym)
-          # If responder is not specified then pass in Spree::Responder
-          (options.delete(:responder) || Spree::Responder).call(self, resources, options)
-        end
-      end
-    end
-  end
-end
-
 module Spree
   module Core
     module ControllerHelpers
