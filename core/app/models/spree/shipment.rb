0553a1e7e2b6cf52a19cf84e0ffbfdce18f502f9
Ryan Bigg <radarlistener@gmail.com>
2014-03-17 10:24:10 +1100

    Improved speed of Shipment#manifest
    Grouping by the attributes variant_id and line_item_id showed a 2-3x improvement for big carts on the delivery page.
    
    By grouping by these attributes rather than their association return values, we're not creating N new LineItem and N new Variant objects. We're now only creating 1 of those each per group.

--- core/app/models/spree/shipment.rb
+++ core/app/models/spree/shipment.rb
@@ -178,12 +178,16 @@ module Spree
     ManifestItem = Struct.new(:line_item, :variant, :quantity, :states)
 
     def manifest
-      inventory_units.group_by(&:variant).map do |variant, units|
-        units.group_by(&:line_item).map do |line_item, units|
+      # Grouping by the ID means that we don't have to call out to the association accessor
+      # This makes the grouping by faster because it results in less SQL cache hits.
+      inventory_units.group_by(&:variant_id).map do |variant_id, units|
+        units.group_by(&:line_item_id).map do |line_item_id, units|
 
           states = {}
           units.group_by(&:state).each { |state, iu| states[state] = iu.count }
 
+          line_item = units.first.line_item
+          variant = units.first.variant
           ManifestItem.new(line_item, variant, units.length, states)
         end
       end.flatten
