e7651aa4137e08db68a4323c545b9c04e8f2d7f8
Washington Luiz <huoxito@gmail.com>
2014-03-16 17:43:12 -0300

    No need to require files already required in core

--- frontend/lib/spree/frontend.rb
+++ frontend/lib/spree/frontend.rb
@@ -1,15 +1,10 @@
 require 'rails/all'
-require 'rails/generators'
 require 'jquery-rails'
 require 'canonical-rails'
 require 'deface'
 
 require 'spree/core'
 
-require 'spree/core/mail_settings'
-require 'spree/core/mail_interceptor'
-
 require 'spree/responder'
 require 'spree/frontend/middleware/seo_assist'
-
 require 'spree/frontend/engine'
