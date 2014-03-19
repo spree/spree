a0a805928046827292d1516153651d8a2bc5088d
brchristian <brchristian@gmail.com>
2014-03-19 15:13:57 -0700

    typo fix in CreateDefaultStockMigration
    Fixes #4474

--- core/db/migrate/20130213191427_create_default_stock.rb
+++ core/db/migrate/20130213191427_create_default_stock.rb
@@ -9,7 +9,7 @@ class CreateDefaultStock < ActiveRecord::Migration
       stock_item = Spree::StockItem.unscoped.build(stock_location: location, variant: variant)
       stock_item.send(:count_on_hand=, variant.count_on_hand)
       # Avoid running default_scope defined by acts_as_paranoid, related to #3805,
-      # validations would run a query with a delete_at column tha might not be present yet
+      # validations would run a query with a delete_at column that might not be present yet
       stock_item.save! validate: false
     end
 
