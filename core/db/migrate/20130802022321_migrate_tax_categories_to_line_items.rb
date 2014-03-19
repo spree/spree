cdaeade2b7063d494d04d263dd7b77cc4986a898
brchristian <brchristian@gmail.com>
2014-03-19 14:40:55 -0700

    Removing .includes() call from MigrateTaxCategoriesToLineItems
    Addresses the bug discussed in https://github.com/spree/spree/issues/4460
    
    Note that this will make the migration slower, but less buggy in the case that LineItems refer to deleted Variants or Products.
    
    If the `acts_as_paranoid` gem can be modified to allow "unscoped" `includes()` calls, then we can reinstate the performance improvement.
    
    Fixes #4460
    Fixes #4471

--- core/db/migrate/20130802022321_migrate_tax_categories_to_line_items.rb
+++ core/db/migrate/20130802022321_migrate_tax_categories_to_line_items.rb
@@ -1,9 +1,10 @@
 class MigrateTaxCategoriesToLineItems < ActiveRecord::Migration
   def change
-  	Spree::LineItem.includes(:variant => { :product => :tax_category }).find_in_batches do |line_items|
-  	  line_items.each do |line_item|
-        line_item.update_column(:tax_category_id, line_item.product.tax_category_id)
-      end
+    Spree::LineItem.find_each do |line_item|
+      next if line_item.variant.nil?
+      next if line_item.variant.product.nil?
+      next if line_item.product.nil?
+      line_item.update_column(:tax_category_id, line_item.product.tax_category_id)
     end
   end
 end
