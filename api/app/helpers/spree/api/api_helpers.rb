3851a7a593ded6331ad88c8570d93624bfce27f5
Ryan Bigg <radarlistener@gmail.com>
2014-03-20 15:33:14 +1100

    Add tax_total back to api_helpers#order_attributes
    Conflicts:
    	api/app/helpers/spree/api/api_helpers.rb

--- api/app/helpers/spree/api/api_helpers.rb
+++ api/app/helpers/spree/api/api_helpers.rb
@@ -75,7 +75,7 @@ module Spree
         :user_id, :created_at, :updated_at, :completed_at, :payment_total,
         :shipment_state, :payment_state, :email, :special_instructions, :channel,
         :included_tax_total, :additional_tax_total, :display_included_tax_total,
-        :display_additional_tax_total, :currency,
+        :display_additional_tax_total, :tax_total, :currency
       ]
 
       @@line_item_attributes = [:id, :quantity, :price, :variant_id]
