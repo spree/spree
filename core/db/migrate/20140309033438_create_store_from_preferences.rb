8e7fd3a3a8da0142ea1d1f3c902615a178e24454
Clarke Brunsdon <clarke@freerunningtechnologies.com>
2014-03-19 22:05:31 -0700

    Updating for new preferences api
    Broken by @jhawthorn's a36381edb9014a97c7d337cb1d85989672388428
    
    Related to #4415

--- core/db/migrate/20140309033438_create_store_from_preferences.rb
+++ core/db/migrate/20140309033438_create_store_from_preferences.rb
@@ -4,14 +4,21 @@ class CreateStoreFromPreferences < ActiveRecord::Migration
 
     # we set defaults for the things we now require
     Spree::Store.new do |s|
-      s.name              = preference_store.get('spree/app_configuration/site_name') || 'Spree Demo Site'
-      s.url               = preference_store.get('spree/app_configuration/site_url') || 'demo.spreecommerce.com'
-      s.mail_from_address = preference_store.get('spree/app_configuration/mails_from') || 'spree@example.com'
+      s.name              = preference_store.get 'spree/app_configuration/site_name' do
+        'Spree Demo Site'
+      end
+      s.url               = preference_store.get 'spree/app_configuration/site_url' do
+        'demo.spreecommerce.com'
+      end
+      s.mail_from_address = preference_store.get 'spree/app_configuration/mails_from' do
+        'spree@example.com'
+      end
 
-      s.meta_description = preference_store.get 'spree/app_configuration/default_meta_description'
-      s.meta_keywords    = preference_store.get 'spree/app_configuration/default_meta_keywords'
-      s.seo_title        = preference_store.get 'spree/app_configuration/default_seo_title'
-      s.default_currency = preference_store.get 'spree/app_configuration/currency'
+      s.meta_description = preference_store.get('spree/app_configuration/default_meta_description') {}
+      s.meta_keywords    = preference_store.get('spree/app_configuration/default_meta_keywords') {}
+      s.seo_title        = preference_store.get('spree/app_configuration/default_seo_title') {}
+      s.default_currency = preference_store.get('spree/app_configuration/currency') {}
+
     end.save!
   end
 end
