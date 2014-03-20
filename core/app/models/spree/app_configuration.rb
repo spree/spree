9606cff2a42684bfca64e80f2ecc800adec2dc0c
Jordan Brough <git.j@brgh.net>
2014-03-20 15:07:23 -0600

    add searching and paging for admin promotions area
    Fixes #4478

--- core/app/models/spree/app_configuration.rb
+++ core/app/models/spree/app_configuration.rb
@@ -57,6 +57,7 @@ module Spree
     preference :orders_per_page, :integer, default: 15
     preference :prices_inc_tax, :boolean, default: false
     preference :products_per_page, :integer, default: 12
+    preference :promotions_per_page, :integer, default: 15
     preference :redirect_https_to_http, :boolean, :default => false
     preference :require_master_price, :boolean, default: true
     preference :shipment_inc_vat, :boolean, default: false
