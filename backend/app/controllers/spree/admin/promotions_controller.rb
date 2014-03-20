4050962c0716c80835a229e318a469374854998f
Jordan Brough <git.j@brgh.net>
2014-03-20 15:07:23 -0600

    add searching and paging for admin promotions area
    Fixes #4478

--- backend/app/controllers/spree/admin/promotions_controller.rb
+++ backend/app/controllers/spree/admin/promotions_controller.rb
@@ -13,6 +13,24 @@ module Spree
         def load_data
           @calculators = Rails.application.config.spree.calculators.promotion_actions_create_adjustments
         end
+
+        def collection
+          return @collection if @collection.present?
+          params[:q] ||= HashWithIndifferentAccess.new
+
+          @collection = super
+          @search = @collection.ransack(params[:q])
+          @collection = @search.result(distinct: true).
+            includes(promotion_includes).
+            page(params[:page]).
+            per(params[:per_page] || Spree::Config[:promotions_per_page])
+
+          @collection
+        end
+
+        def promotion_includes
+          [:promotion_actions]
+        end
     end
   end
 end
