2aa1ecfbfce16377ddce5fde31008fcbe0df9dbf
Ryan Bigg <radarlistener@gmail.com>
2014-03-21 16:03:49 +1100

    Enable images to be created in the API within the scope of a product/variant

--- api/app/controllers/spree/api/images_controller.rb
+++ api/app/controllers/spree/api/images_controller.rb
@@ -9,18 +9,18 @@ module Spree
 
       def create
         authorize! :create, Image
-        @image = Image.create(image_params)
+        @image = scope.images.create(image_params)
         respond_with(@image, :status => 201, :default_template => :show)
       end
 
       def update
-        @image = Image.accessible_by(current_ability, :update).find(params[:id])
+        @image = scope.images.accessible_by(current_ability, :update).find(params[:id])
         @image.update_attributes(image_params)
         respond_with(@image, :default_template => :show)
       end
 
       def destroy
-        @image = Image.accessible_by(current_ability, :destroy).find(params[:id])
+        @image = scope.images.accessible_by(current_ability, :destroy).find(params[:id])
         @image.destroy
         respond_with(@image, :status => 204)
       end
@@ -28,6 +28,14 @@ module Spree
       private
         def image_params
           params.require(:image).permit(permitted_image_attributes)
+        end
+
+        def scope
+          if params[:product_id]
+            scope = Spree::Product.friendly.find(params[:product_id])
+          elsif params[:variant_id]
+            scope = Spree::Variant.find(params[:variant_id])
+          end
         end
     end
   end
