850c04e9cd61529b246c83517780c4968c6fb146
Ryan Bigg <radarlistener@gmail.com>
2014-03-24 10:39:59 +1100

    included_in_price tax rates should be applied using the same rules as their 'additional' brethren

--- core/app/models/spree/tax_rate.rb
+++ core/app/models/spree/tax_rate.rb
@@ -28,7 +28,7 @@ module Spree
       return [] unless order.tax_zone
       all.select do |rate|
         (!rate.included_in_price && (rate.zone == order.tax_zone || rate.zone.contains?(order.tax_zone) || (order.tax_address.nil? && rate.zone.default_tax))) ||
-        rate.included_in_price
+        rate.included_in_price && (rate.zone == order.tax_zone || rate.zone.contains?(order.tax_zone) || (order.tax_address.nil? && rate.zone.default_tax))
       end
     end
 
