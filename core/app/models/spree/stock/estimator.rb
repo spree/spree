0956f363819a3c689a23a6c588ba1b089bc52134
Ryan Bigg <radarlistener@gmail.com>
2014-03-24 14:29:13 +1100

    Display exclusionary tax amounts on shipping rates

--- core/app/models/spree/stock/estimator.rb
+++ core/app/models/spree/stock/estimator.rb
@@ -32,7 +32,10 @@ module Spree
           tax_category = shipping_method.tax_category
           if tax_category
             tax_rate = tax_category.tax_rates.detect do |rate|
-              rate.zone == package.order.tax_zone
+              # If the rate's zone matches the order's zone, a positive adjustment will be applied.
+              # If the rate is from the default tax zone, then a negative adjustment will be applied.
+              # See the tests in shipping_rate_spec.rb for an example of this.d
+              rate.zone == package.order.tax_zone || rate.zone.default_tax?
             end
           end
 
