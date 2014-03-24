3c2f60ee31fb9f9da25d14a1e7d484d7edda55f2
Akarsh <akarsh1357@gmail.com>
2014-03-24 21:42:04 +0530

    Promotion Item Total Rule to include Upper limit also

--- core/app/models/spree/promotion/rules/item_total.rb
+++ core/app/models/spree/promotion/rules/item_total.rb
@@ -6,8 +6,12 @@ module Spree
       class ItemTotal < PromotionRule
         preference :amount, :decimal, default: 100.00
         preference :operator, :string, default: '>'
+        preference :amount_max, :decimal, default: 1000.00
+        preference :operator_max, :string, default: '<'
+
 
         OPERATORS = ['gt', 'gte']
+        OPERATORS_MAX = ['lt','lte']
 
         def applicable?(promotable)
           promotable.is_a?(Spree::Order)
@@ -15,7 +19,10 @@ module Spree
 
         def eligible?(order, options = {})
           item_total = order.item_total
-          item_total.send(preferred_operator == 'gte' ? :>= : :>, BigDecimal.new(preferred_amount.to_s))
+          lower_limit_condition = item_total.send(preferred_operator == 'gte' ? :>= : :>, BigDecimal.new(preferred_amount.to_s))
+          upper_limit_condition = item_total.send(preferred_operator_max == 'lte' ? :<= : :<, BigDecimal.new(preferred_amount_max.to_s))
+          lower_limit_condition and upper_limit_condition
+
         end
       end
     end
