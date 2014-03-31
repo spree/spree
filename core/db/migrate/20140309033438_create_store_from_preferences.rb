5900d753af14b412ef954019135d290390501ded
Jeff Dutil <jdutil@burlingtonwebapps.com>
2014-03-31 13:21:29 +0100

    Only create store from preferences when there is no store.

--- core/db/migrate/20140309033438_create_store_from_preferences.rb
+++ core/db/migrate/20140309033438_create_store_from_preferences.rb
@@ -1,24 +1,30 @@
 class CreateStoreFromPreferences < ActiveRecord::Migration
   def change
     preference_store = Spree::Preferences::Store.instance
+    if store = Spree::Store.where(default: true).first
+      store.meta_description = preference_store.get('spree/app_configuration/default_meta_description') {}
+      store.meta_keywords    = preference_store.get('spree/app_configuration/default_meta_keywords') {}
+      store.seo_title        = preference_store.get('spree/app_configuration/default_seo_title') {}
+      store.save!
+    else
+      # we set defaults for the things we now require
+      Spree::Store.new do |s|
+        s.name              = preference_store.get 'spree/app_configuration/site_name' do
+          'Spree Demo Site'
+        end
+        s.url               = preference_store.get 'spree/app_configuration/site_url' do
+          'demo.spreecommerce.com'
+        end
+        s.mail_from_address = preference_store.get 'spree/app_configuration/mails_from' do
+          'spree@example.com'
+        end
 
-    # we set defaults for the things we now require
-    Spree::Store.new do |s|
-      s.name              = preference_store.get 'spree/app_configuration/site_name' do
-        'Spree Demo Site'
-      end
-      s.url               = preference_store.get 'spree/app_configuration/site_url' do
-        'demo.spreecommerce.com'
-      end
-      s.mail_from_address = preference_store.get 'spree/app_configuration/mails_from' do
-        'spree@example.com'
-      end
-
-      s.meta_description = preference_store.get('spree/app_configuration/default_meta_description') {}
-      s.meta_keywords    = preference_store.get('spree/app_configuration/default_meta_keywords') {}
-      s.seo_title        = preference_store.get('spree/app_configuration/default_seo_title') {}
-      s.default_currency = preference_store.get('spree/app_configuration/currency') {}
-      s.code             = 'spree'
-    end.save!
+        s.meta_description = preference_store.get('spree/app_configuration/default_meta_description') {}
+        s.meta_keywords    = preference_store.get('spree/app_configuration/default_meta_keywords') {}
+        s.seo_title        = preference_store.get('spree/app_configuration/default_seo_title') {}
+        s.default_currency = preference_store.get('spree/app_configuration/currency') {}
+        s.code             = 'spree'
+      end.save!
+    end
   end
 end
