b72ba5d692ca0ff6cdd342d90c99c6f7865cc2f4
Washington Luiz <huoxito@gmail.com>
2014-04-01 00:40:29 -0300

    Sample should only create Store if none exists yet
    There's a migration which already create a store record. Creating it
    again on the samples breaks the spree install due to validation on
    Store#code uniqueness

--- core/db/default/spree/stores.rb
+++ core/db/default/spree/stores.rb
@@ -1,6 +1,8 @@
-Spree::Store.new do |s|
-  s.code              = 'spree'
-  s.name              = 'Spree Demo Site'
-  s.url               = 'demo.spreecommerce.com'
-  s.mail_from_address = 'spree@example.com'
-end.save!
+unless Spree::Store.count > 0
+  Spree::Store.new do |s|
+    s.code              = 'spree'
+    s.name              = 'Spree Demo Site'
+    s.url               = 'demo.spreecommerce.com'
+    s.mail_from_address = 'spree@example.com'
+  end.save!
+end
