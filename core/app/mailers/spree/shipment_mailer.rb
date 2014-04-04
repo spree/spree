e3237b82b3c7c009b9e38d390d1bef57bcc8a7f9
Chris Salzberg <chrissalzberg@gmail.com>
2014-04-04 23:38:32 +0900

    Replace Spree::Config[:site_name] with Spree::Store.current.name.
    This gets rid of the millions of deprecation warnings in the rspec
    output.

--- core/app/mailers/spree/shipment_mailer.rb
+++ core/app/mailers/spree/shipment_mailer.rb
@@ -3,7 +3,7 @@ module Spree
     def shipped_email(shipment, resend = false)
       @shipment = shipment.respond_to?(:id) ? shipment : Spree::Shipment.find(shipment)
       subject = (resend ? "[#{Spree.t(:resend).upcase}] " : '')
-      subject += "#{Spree::Config[:site_name]} #{Spree.t('shipment_mailer.shipped_email.subject')} ##{@shipment.order.number}"
+      subject += "#{Spree::Store.current.name} #{Spree.t('shipment_mailer.shipped_email.subject')} ##{@shipment.order.number}"
       mail(to: @shipment.order.email, from: from_address, subject: subject)
     end
   end
