87a2a466b59d7f5df29594e55a46096c7b6dd9bc
Jeff Dutil <JDutil@BurlingtonWebApps.com>
2014-04-08 10:55:41 +0100

    Swap quotation used around in order to fix postgres builds.

--- core/app/models/spree/zone.rb
+++ core/app/models/spree/zone.rb
@@ -20,7 +20,7 @@ module Spree
     def self.match(address)
       return unless address and matches = self.includes(:zone_members).
         order('spree_zones.zone_members_count', 'spree_zones.created_at').
-        where('(spree_zone_members.zoneable_type = "Spree::Country" AND spree_zone_members.zoneable_id = ?) OR (spree_zone_members.zoneable_type = "Spree::State" AND spree_zone_members.zoneable_id = ?)', address.country_id, address.state_id).
+        where("(spree_zone_members.zoneable_type = 'Spree::Country' AND spree_zone_members.zoneable_id = ?) OR (spree_zone_members.zoneable_type = 'Spree::State' AND spree_zone_members.zoneable_id = ?)", address.country_id, address.state_id).
         references(:zones)
 
       ['state', 'country'].each do |zone_kind|
