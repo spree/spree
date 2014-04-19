ef4f4e440151102ea7a018b262c92aa914f672d6
Washington Luiz <huoxito@gmail.com>
2014-04-18 23:29:19 -0300

    Drop deprecation warning for `relation.sum(&:block)`

--- core/app/models/spree/product.rb
+++ core/app/models/spree/product.rb
@@ -201,7 +201,7 @@ module Spree
       if self.variants_including_master.any? { |v| !v.should_track_inventory? }
         Float::INFINITY
       else
-        self.stock_items.sum(&:count_on_hand)
+        self.stock_items.to_a.sum(&:count_on_hand)
       end
     end
 
