0178131de6744c632c1bcc31c9c5a11363749b12
John Hawthorn <john.hawthorn@gmail.com>
2014-04-18 17:41:19 -0700

    Make sure line_item is assigned in specs
    As of dcbd5de we expect line_items to always be associated with an
    inventory_unit. Ensure we are assigning that in specs.

--- core/spec/models/spree/inventory_unit_spec.rb
+++ core/spec/models/spree/inventory_unit_spec.rb
@@ -22,12 +22,23 @@ describe Spree::InventoryUnit do
       shipment.tap(&:save!)
     end
 
+    let(:variant) do
+      stock_item.variant
+    end
+
+    let(:line_item) do
+      Spree::LineItem.create!(
+        variant: variant,
+        order: order
+      )
+    end
+
     let!(:unit) do
-      unit = shipment.inventory_units.build
-      unit.state = 'backordered'
-      unit.variant_id = stock_item.variant.id
-      unit.order_id = order.id
-      unit.tap(&:save!)
+      shipment.inventory_units.create!(
+        state: 'backordered',
+        line_item: line_item,
+        variant: variant
+      )
     end
 
     # Regression for #3066
@@ -43,7 +54,8 @@ describe Spree::InventoryUnit do
     it "does not find inventory units that aren't backordered" do
       on_hand_unit = shipment.inventory_units.build
       on_hand_unit.state = 'on_hand'
-      on_hand_unit.variant_id = 1
+      on_hand_unit.variant = variant
+      on_hand_unit.line_item = line_item
       on_hand_unit.save!
 
       Spree::InventoryUnit.backordered_for_stock_item(stock_item).should_not include(on_hand_unit)
@@ -53,6 +65,7 @@ describe Spree::InventoryUnit do
       other_variant_unit = shipment.inventory_units.build
       other_variant_unit.state = 'backordered'
       other_variant_unit.variant = create(:variant)
+      other_variant_unit.line_item = line_item
       other_variant_unit.save!
 
       Spree::InventoryUnit.backordered_for_stock_item(stock_item).should_not include(other_variant_unit)
@@ -94,7 +107,7 @@ describe Spree::InventoryUnit do
 
   context "variants deleted" do
     let!(:unit) do
-      Spree::InventoryUnit.create(variant: stock_item.variant)
+      create :inventory_unit
     end
 
     it "can still fetch variant" do
