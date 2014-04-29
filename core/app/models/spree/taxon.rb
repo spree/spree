de81eec4e8182bf4ea28ed72c55a38cb42df8eb7
wuboy <wuboys@gmail.com>
2014-04-30 02:10:28 +0800

    add validates_attachment to Spree::Taxon icon

--- core/app/models/spree/taxon.rb
+++ core/app/models/spree/taxon.rb
@@ -18,6 +18,7 @@ module Spree
       url: '/spree/taxons/:id/:style/:basename.:extension',
       path: ':rails_root/public/spree/taxons/:id/:style/:basename.:extension',
       default_url: '/assets/default_taxon.png'
+    validates_attachment :icon, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png"] }
 
     include Spree::Core::ProductFilters  # for detailed defs of filters
 
