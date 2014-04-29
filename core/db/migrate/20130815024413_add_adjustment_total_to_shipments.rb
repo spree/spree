aa6b31078ac052a7a4e114fe6a8563c89575d71a
Peter Berkenbosch <peter@pero-ict.nl>
2014-04-29 15:49:13 +0200

    Revert "make sure we use adjustment_total for the shipping adjustment migrations instead of cost"
    This reverts commit 2e6ca42826401958577596f85b4b523b1920ecd7.

--- core/db/migrate/20130815024413_add_adjustment_total_to_shipments.rb
+++ core/db/migrate/20130815024413_add_adjustment_total_to_shipments.rb
@@ -1,5 +1,5 @@
 class AddAdjustmentTotalToShipments < ActiveRecord::Migration
   def change
-    add_column :spree_shipments, :adjustment_total, :decimal, :precision => 10, :scale => 2, :default => 0.0 unless ActiveRecord::Base.connection.column_exists?(:spree_shipments, :adjustment_total)
+    add_column :spree_shipments, :adjustment_total, :decimal, :precision => 10, :scale => 2, :default => 0.0
   end
 end
