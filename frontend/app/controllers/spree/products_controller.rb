e207e3270a573741e7dd31aa531eb85dc1cc8fa2
Herman Moreno <herman.moreno@crowdint.com>
2014-05-01 10:28:18 -0500

    Remove useless return in the show action from the products controller

--- frontend/app/controllers/spree/products_controller.rb
+++ frontend/app/controllers/spree/products_controller.rb
@@ -13,8 +13,6 @@ module Spree
     end
 
     def show
-      return unless @product
-
       @variants = @product.variants_including_master.active(current_currency).includes([:option_values, :images])
       @product_properties = @product.product_properties.includes(:property)
       @taxon = Spree::Taxon.find(params[:taxon_id]) if params[:taxon_id]
