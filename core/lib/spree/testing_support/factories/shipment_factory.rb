422c2ef61340bea6a4cd8b340141c205e038424a
Ryan Bigg <radarlistener@gmail.com>
2014-05-05 15:11:56 +1000

    Bypass calculator factory create callback which doesn't seem to be firing
    Could be a factory_girl bug.
    
    Time spent debugging: ~1 hour

--- core/lib/spree/testing_support/factories/shipment_factory.rb
+++ core/lib/spree/testing_support/factories/shipment_factory.rb
@@ -2,14 +2,15 @@ FactoryGirl.define do
   factory :shipment, class: Spree::Shipment do
     tracking 'U10000'
     number '100'
-    cost 100.00
     state 'pending'
     order
     address
     stock_location
 
     after(:create) do |shipment, evalulator|
-      shipment.add_shipping_method(create(:shipping_method), true)
+      shipping_method = create(:shipping_method)
+      shipping_method.calculator.preferred_amount = 100
+      shipment.add_shipping_method(shipping_method, true)
 
       shipment.order.line_items.each do |line_item|
         line_item.quantity.times { shipment.inventory_units.create(variant_id: line_item.variant_id) }
