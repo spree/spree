23468b0b073afa6c55b27d2bccf2e882698e9e81
brchristian <brchristian@gmail.com>
2014-05-08 20:47:37 -0700

    No longer caching insufficient_stock_lines
    Fixes #4664
    Fixes #4666

--- core/app/models/spree/order.rb
+++ core/app/models/spree/order.rb
@@ -409,7 +409,7 @@ module Spree
     end
 
     def insufficient_stock_lines
-     @insufficient_stock_lines ||= line_items.select(&:insufficient_stock?)
+     line_items.select(&:insufficient_stock?)
     end
 
     def merge!(order, user = nil)
