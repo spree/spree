d8515d4f77cf113b94e5b113c8400d8d957bd9be
Ryan Bigg <radarlistener@gmail.com>
2014-05-09 12:04:21 +1000

    [frontend] Load taxon if params[:taxon] exists in ProductsController
    Fixes a small bug where searching *twice* for the same term results in the taxon 'scope' being forgotten

--- frontend/app/controllers/spree/products_controller.rb
+++ frontend/app/controllers/spree/products_controller.rb
@@ -1,6 +1,8 @@
 module Spree
   class ProductsController < Spree::StoreController
     before_filter :load_product, :only => :show
+    before_filter :load_taxon, :only => :index
+
     rescue_from ActiveRecord::RecordNotFound, :with => :render_404
     helper 'spree/taxons'
 
@@ -43,6 +45,10 @@ module Spree
         else
           @product = Product.active(current_currency).find_by_permalink!(params[:id])
         end
+      end
+
+      def load_taxon
+        @taxon = Spree::Taxon.find(params[:taxon]) if params[:taxon].present?
       end
   end
 end
