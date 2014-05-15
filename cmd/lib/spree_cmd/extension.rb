1890503e48868637cf3e119359ac4152407f4563
Ryan Bigg <radarlistener@gmail.com>
2014-05-15 16:26:53 +1000

    Bump to 2.1.8.beta

--- cmd/lib/spree_cmd/extension.rb
+++ cmd/lib/spree_cmd/extension.rb
@@ -50,7 +50,7 @@ module SpreeCmd
       end
 
       def spree_version
-        '2.1.7'
+        '2.1.8.beta'
       end
 
       def use_prefix(prefix)
--- core/lib/spree/core/version.rb
+++ core/lib/spree/core/version.rb
@@ -1,5 +1,5 @@
 module Spree
   def self.version
-    "2.1.7"
+    "2.1.8.beta"
   end
 end
