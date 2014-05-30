e3adc071580ed44b198a0137022c7abaa0816da3
Diego Domingues <diego.domingues16@gmail.com>
2014-05-30 17:38:36 -0300

    Fix html safe when cart is empty

--- core/app/helpers/spree/base_helper.rb
+++ core/app/helpers/spree/base_helper.rb
@@ -20,11 +20,11 @@ module Spree
         text = "#{text}: (#{Spree.t('empty')})"
         css_class = 'empty'
       else
-        text = "#{text}: (#{simple_current_order.item_count})  <span class='amount'>#{simple_current_order.display_total.to_html}</span>".html_safe
+        text = "#{text}: (#{simple_current_order.item_count})  <span class='amount'>#{simple_current_order.display_total.to_html}</span>"
         css_class = 'full'
       end
 
-      link_to text, spree.cart_path, :class => "cart-info #{css_class}"
+      link_to text.html_safe, spree.cart_path, :class => "cart-info #{css_class}"
     end
 
     # human readable list of variant options
