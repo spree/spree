1906f6a067cf467d5e91ae02dd2ae79456136aa6
Richard Wilson <richard@freerunningtech.com>
2014-06-06 15:24:05 -0700

    Ensure shipments are also destroyed when the order is emptied.

--- core/app/models/spree/order.rb
+++ core/app/models/spree/order.rb
@@ -439,8 +439,9 @@ module Spree
     def empty!
       line_items.destroy_all
       updater.update_item_count
-
       adjustments.destroy_all
+      shipments.destroy_all
+
       update_totals
       persist_totals
     end
