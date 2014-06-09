1d04a9d62333fae26faf90cf90d4b022641ba655
Damien <dameyawn@gmail.com>
2014-06-09 11:37:33 -0700

    Not needed if user model accepts nested attributes.

--- backend/app/controllers/spree/admin/users_controller.rb
+++ backend/app/controllers/spree/admin/users_controller.rb
@@ -56,9 +56,7 @@ module Spree
 
       def addresses
         if request.put?
-          bill_params = user_params[:bill_address_attributes]
-          ship_params = user_params[:ship_address_attributes]
-          if @user.bill_address.update_attributes(bill_params) && @user.ship_address.update_attributes(ship_params)
+          if @user.update_attributes(user_params)
             flash.now[:success] = Spree.t(:account_updated)
           end
 
