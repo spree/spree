fe50b4919232335adbf7373cf4a65bc381af72a1
Ryan Bigg <radarlistener@gmail.com>
2014-06-10 14:51:05 +1000

    [api] Ensure order ids are ordered in orders_controller_spec

--- api/spec/controllers/spree/api/orders_controller_spec.rb
+++ api/spec/controllers/spree/api/orders_controller_spec.rb
@@ -486,7 +486,7 @@ module Spree
 
           orders = json_response["orders"]
           expect(orders.count).to be >= 3
-          expect(orders.map { |o| o["id"] }).to eq Order.pluck(:id)
+          expect(orders.map { |o| o["id"] }.sort).to eq(Order.order(:id).pluck(:id))
         end
 
         after { ActionController::Base.perform_caching = false }
