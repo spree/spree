6ae83d86cde83064f73ece68288a64679cfef0b5
Jeff Dutil <JDutil@BurlingtonWebApps.com>
2014-06-13 14:44:46 -0400

    Update backends adjustments controller to fix order.update! call.

--- backend/app/controllers/spree/admin/adjustments_controller.rb
+++ backend/app/controllers/spree/admin/adjustments_controller.rb
@@ -1,16 +1,18 @@
 module Spree
   module Admin
     class AdjustmentsController < ResourceController
-      belongs_to 'spree/order', :find_by => :number
-      destroy.after :reload_order
-      destroy.after :update_totals
+
+      belongs_to 'spree/order', find_by: :number
+
       create.after :update_totals
+      destroy.after :update_totals
       update.after :update_totals
 
-      skip_before_filter :load_resource, :only => [:toggle_state, :edit, :update, :destroy]
+      skip_before_filter :load_resource, only: [:toggle_state, :edit, :update, :destroy]
 
-      def index
-        @adjustments = @order.all_adjustments.order("created_at ASC")
+      def destroy
+        find_adjustment
+        super
       end
 
       def edit
@@ -18,30 +20,26 @@ module Spree
         super
       end
 
+      def index
+        @adjustments = @order.all_adjustments.order("created_at ASC")
+      end
+
       def update
         find_adjustment
         super
       end
 
-      def destroy
-        find_adjustment 
-        super
-      end
-
       private
+
       def find_adjustment
         # Need to assign to @object here to keep ResourceController happy
         @adjustment = @object = parent.all_adjustments.find(params[:id])
       end
 
-      def reload_order
-        @order.reload
+      def update_totals
+        @order.reload.update!
       end
 
-      def update_totals
-        @order.updater.update_adjustment_total
-        @order.persist_totals
-      end
     end
   end
 end
