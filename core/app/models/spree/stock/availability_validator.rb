187312cf66096a3d451651311e3370daae95a3f8
Dan Kubb <dan.kubb@gmail.com>
2014-06-17 17:09:32 -0700

    Fix bug where line item quantity cannot be set to zero for out-of-stock items.
    Fixes #6372

--- core/app/models/spree/stock/availability_validator.rb
+++ core/app/models/spree/stock/availability_validator.rb
@@ -5,16 +5,20 @@ module Spree
         unit_count = line_item.inventory_units.size
         return if unit_count >= line_item.quantity
         quantity = line_item.quantity - unit_count
+        return if quantity.zero?
 
         quantifier = Stock::Quantifier.new(line_item.variant)
 
-        unless quantifier.can_supply? quantity
-          variant = line_item.variant
-          display_name = %Q{#{variant.name}}
-          display_name += %Q{ (#{variant.options_text})} unless variant.options_text.blank?
+        return if quantifier.can_supply?(quantity)
 
-          line_item.errors[:quantity] << Spree.t(:selected_quantity_not_available, :scope => :order_populator, :item => display_name.inspect)
-        end
+        variant = line_item.variant
+        display_name = "#{variant.name}"
+        display_name += " (#{variant.options_text})" unless variant.options_text.blank?
+
+        line_item.errors[:quantity] << Spree.t(
+          :selected_quantity_not_available,
+          item: display_name.inspect
+        )
       end
     end
   end
