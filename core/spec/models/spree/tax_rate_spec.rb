5379e65f4bf9361d82029efdf502e62c1e6579a0
Irvin Fan <ifan@godaddy.com>
2014-06-18 14:07:02 -0700

    Destroy associated adjustments when order is changed to move out of taxable zones.
    Fixes #4840
    Fixes #4839
    Fixes #4916

--- core/spec/models/spree/tax_rate_spec.rb
+++ core/spec/models/spree/tax_rate_spec.rb
@@ -330,6 +330,14 @@ describe Spree::TaxRate do
           it "should not create a tax refund" do
             line_item.adjustments.credit.count.should == 0
           end
+
+          it "should remove adjustments when tax_zone is removed" do
+            Spree::TaxRate.adjust(@order, @order.line_items)
+            line_item.adjustments.count.should == 2
+            @order.stub :tax_zone => nil
+            Spree::TaxRate.adjust(@order, @order.line_items)
+            line_item.adjustments.count.should == 0
+          end
         end
 
         context "when two rates apply" do
