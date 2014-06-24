c9a5d906137d0a18810bebc3245a46390758ac57
Ryan Bigg <radarlistener@gmail.com>
2014-06-24 10:38:53 +1000

    Operate on in-memory all_adjustments when summing adjustments in OrderUpdater

--- core/app/models/spree/order_updater.rb
+++ core/app/models/spree/order_updater.rb
@@ -67,9 +67,8 @@ module Spree
 
     def update_adjustment_total
       recalculate_adjustments
-      order.adjustment_total = line_items.sum(:adjustment_total) +
-                               shipments.sum(:adjustment_total)  +
-                               adjustments.eligible.sum(:amount)
+
+      order.adjustment_total = all_adjustments.sum(&:amount)
       order.included_tax_total = line_items.sum(:included_tax_total) + shipments.sum(:included_tax_total)
       order.additional_tax_total = line_items.sum(:additional_tax_total) + shipments.sum(:additional_tax_total)
 
