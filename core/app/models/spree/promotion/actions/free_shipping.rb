277304a41ea9b6b408cb79ab029f3f47da15d183
Ryan Bigg <radarlistener@fastmail.fm>
2014-06-26 11:29:14 +1000

    Skip shipments in FreeShipping PromomtionAction if promotion action has already been applied

--- core/app/models/spree/promotion/actions/free_shipping.rb
+++ core/app/models/spree/promotion/actions/free_shipping.rb
@@ -5,7 +5,7 @@ module Spree
         def perform(payload={})
           order = payload[:order]
           results = order.shipments.map do |shipment|
-            return true if promotion_credit_exists?(shipment)
+            next if promotion_credit_exists?(shipment)
 
             shipment.adjustments.build(
               order: shipment.order, 
