c2be525ffec2a58b9bde0e993ac9ac467ca2cbaf
Ryan Bigg <radarlistener@gmail.com>
2014-07-01 09:09:44 +1000

    Bump to 2.3.0

--- cmd/lib/spree_cmd/extension.rb
+++ cmd/lib/spree_cmd/extension.rb
@@ -50,7 +50,7 @@ module SpreeCmd
       end
 
       def spree_version
-        '2.3.0.beta'
+        '2.3.0'
       end
 
       def use_prefix(prefix)
--- cmd/lib/spree_cmd/installer.rb
+++ cmd/lib/spree_cmd/installer.rb
@@ -94,11 +94,11 @@ module SpreeCmd
         gem :spree, @spree_gem_options
 
         if @install_default_gateways
-          gem :spree_gateway, :git => "https://github.com/spree/spree_gateway.git", :branch => "2-1-stable"
+          gem :spree_gateway, :git => "https://github.com/spree/spree_gateway.git", :branch => "2-3-stable"
         end
 
         if @install_default_auth
-          gem :spree_auth_devise, :git => "https://github.com/spree/spree_auth_devise.git", :branch => "2-1-stable"
+          gem :spree_auth_devise, :git => "https://github.com/spree/spree_auth_devise.git", :branch => "2-3-stable"
         end
 
         run 'bundle install', :capture => true
