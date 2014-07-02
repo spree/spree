3f01f1a35d44b4deffa7fb129e7b89f0edb69ce3
Ryan Bigg <radarlistener@gmail.com>
2014-07-02 12:56:24 +1000

    Move set_shipments_cost call to Order#update_from_params
    This stops the method from being called whenever Order#checkout_steps is called and thereby reduces some queries
    
    This needs to be called in the checkout_specs to ensure that the state isn't jumped as long as this method is called.
    
    Fixes #4898
    Fixes #4979

--- core/app/models/spree/order.rb
+++ core/app/models/spree/order.rb
@@ -9,13 +9,7 @@ module Spree
     checkout_flow do
       go_to_state :address
       go_to_state :delivery
-      go_to_state :payment, if: ->(order) do
-        # TODO there should be a better fix work around for the issues this is
-        # resolving we shouldn't be setting shipments cost every time a order
-        # object is loaded
-        order.set_shipments_cost if order.shipments.any?
-        order.payment_required?
-      end
+      go_to_state :payment, if: ->(order) { order.payment_required? }
       go_to_state :confirm, if: ->(order) { order.confirmation_required? }
       go_to_state :complete
       remove_transition from: :delivery, to: :confirm
