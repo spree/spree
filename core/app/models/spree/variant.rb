3c20f8c43282b50f2a8fea7511f952a77d0f45aa
Aleks <ihunte@gmail.com>
2014-07-09 13:26:07 +0200

    Fixes #4905: Error while trying to set "weight" in Variant with "," as seperator.
    Fixes #4905
    Fixes #4939

--- core/app/models/spree/variant.rb
+++ core/app/models/spree/variant.rb
@@ -63,6 +63,10 @@ module Spree
       self[:cost_price] = parse_price(price) if price.present?
     end
 
+    def weight=(weight)
+      self[:weight] = parse_weight(weight) if weight.present?
+    end
+
     # returns number of units currently on backorder for this variant.
     def on_backorder
       inventory_units.with_state('backordered').size
@@ -193,6 +197,18 @@ module Spree
         price.gsub!(separator, '.') unless separator == '.' # then replace the locale-specific decimal separator with the standard separator if necessary
 
         price.to_d
+      end
+
+      # strips all non-weight-like characters from the weight, taking into account locale settings
+      def parse_weight(weight)
+        return weight unless weight.is_a?(String)
+
+        separator, delimiter = Spree::Config.currency_decimal_mark, Spree::Config.currency_thousands_separator
+        non_weight_characters = /[^0-9\-#{separator}]/
+        weight.gsub!(non_weight_characters, '') # strip everything else first
+        weight.gsub!(separator, '.') unless separator == '.' # then replace the locale-specific decimal separator with the standard separator if necessary
+
+        weight.to_d
       end
 
       def set_master_out_of_stock
