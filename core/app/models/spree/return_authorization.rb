6d833fc9c3a438c55e09b85c4090d5708fb18f7f
Jerrold Thompson <jet@whidbey.com>
2014-07-09 23:27:47 -0700

    clean up typo

--- core/app/models/spree/return_authorization.rb
+++ core/app/models/spree/return_authorization.rb
@@ -62,7 +62,7 @@ module Spree
       order.shipped_shipments.collect{|s| s.inventory_units.to_a}.flatten
     end
 
-    # Used when Adjustment#update! wants to update the related adjustmenrt
+    # Used when Adjustment#update! wants to update the related adjustment
     def compute_amount(*args)
       amount.abs * -1
     end
