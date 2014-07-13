bd5865b72fd346165041ec288eb28c5569fb8422
Washington Luiz <huoxito@gmail.com>
2014-07-13 15:56:09 -0300

    Re add :id to address permitted attributes
    Previously introduced in 681a966f and later removed probably by accident
    
    Conflicts:
    	frontend/spec/controllers/spree/checkout_controller_spec.rb

--- core/lib/spree/permitted_attributes.rb
+++ core/lib/spree/permitted_attributes.rb
@@ -28,7 +28,7 @@ module Spree
     mattr_reader *ATTRIBUTES
 
     @@address_attributes = [
-      :firstname, :lastname, :first_name, :last_name, 
+      :id, :firstname, :lastname, :first_name, :last_name,
       :address1, :address2, :city, :country_id, :state_id, 
       :zipcode, :phone, :state_name, :alternative_phone, :company,
       :country => [:iso, :name, :iso3, :iso_name],
