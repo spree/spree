ba0e852ba6a74915e191b47af0ddd5a10f88aa77
Jerrold Thompson <jet@whidbey.com>
2014-07-14 00:11:17 -0700

    remove leftover #binding.pry in spec helpers, tab fix

--- backend/spec/spec_helper.rb
+++ backend/spec/spec_helper.rb
@@ -86,7 +86,6 @@ RSpec.configure do |config|
   config.after(:each, :type => :feature) do
     missing_translations = page.body.scan(/translation missing: #{I18n.locale}\.(.*?)[\s<\"&]/)
     if missing_translations.any?
-      #binding.pry
       puts "Found missing translations: #{missing_translations.inspect}"
       puts "In spec: #{example.location}"
     end
