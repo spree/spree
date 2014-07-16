159201293439809ccb6a8cc544d9e6e059cfa33b
Jeff Dutil <JDutil@BurlingtonWebApps.com>
2014-07-16 14:21:24 -0400

    Include Store in ControllerHelpers.

--- core/lib/spree/core/controller_helpers.rb
+++ core/lib/spree/core/controller_helpers.rb
@@ -1,18 +1,19 @@
-require 'spree/core/controller_helpers/common'
 require 'spree/core/controller_helpers/auth'
-require 'spree/core/controller_helpers/respond_with'
+require 'spree/core/controller_helpers/common'
 require 'spree/core/controller_helpers/order'
+require 'spree/core/controller_helpers/respond_with'
+require 'spree/core/controller_helpers/store'
 
 module Spree
   module Core
     module ControllerHelpers
-      def self.included(klass)
-        klass.class_eval do
-          include Spree::Core::ControllerHelpers::Common
-          include Spree::Core::ControllerHelpers::Auth
-          include Spree::Core::ControllerHelpers::RespondWith
-          include Spree::Core::ControllerHelpers::Order
-        end
+      extend ActiveSupport::Concern
+      included do
+        include Spree::Core::ControllerHelpers::Auth
+        include Spree::Core::ControllerHelpers::Common
+        include Spree::Core::ControllerHelpers::Order
+        include Spree::Core::ControllerHelpers::RespondWith
+        include Spree::Core::ControllerHelpers::Store
       end
     end
   end
