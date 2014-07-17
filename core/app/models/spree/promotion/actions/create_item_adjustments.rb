1c475f8639efddf0a459eb0f96f3ee3662d59af9
Gregor MacDougall <gregor@freerunningtechnologies.com>
2014-07-17 11:55:42 -0700

    Refactor not query.
    Previously there was a hack in place to deal with attributes not in an
    empty list of attributes. Rails 4 has a helpful .not method which lets
    us do this a little bit better.
    
    I was in the area, so I figured I'd clean it up.

--- core/app/models/spree/promotion/actions/create_item_adjustments.rb
+++ core/app/models/spree/promotion/actions/create_item_adjustments.rb
@@ -14,12 +14,11 @@ module Spree
 
         def perform(payload = {})
           order = payload[:order]
-          # Find only the line items which have not already been adjusted by this promotion
-          # HACK: Need to use [0] because `pluck` may return an empty array, which AR helpfully
-          # coverts to meaning NOT IN (NULL) and the DB isn't happy about that.
-          already_adjusted_line_items = [0] + self.adjustments.pluck(:adjustable_id)
+
+          already_adjusted_line_items = self.adjustments.pluck(:adjustable_id)
           result = false
-          order.line_items.where("id NOT IN (?)", already_adjusted_line_items).find_each do |line_item|
+
+          order.line_items.where.not(id: already_adjusted_line_items).find_each do |line_item|
             current_result = self.create_adjustment(line_item, order)
             result ||= current_result
           end
