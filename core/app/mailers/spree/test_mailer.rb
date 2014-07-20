a19c1e85f495869b8d6a48f24007c85dd65acf52
Jeff Dutil <JDutil@BurlingtonWebApps.com>
2014-07-20 13:44:44 -0400

    Cleanup unused premailer/ink stuff.

--- core/app/mailers/spree/test_mailer.rb
+++ core/app/mailers/spree/test_mailer.rb
@@ -1,7 +1,8 @@
 module Spree
   class TestMailer < BaseMailer
     def test_email(email)
-      mail(to: email, from: from_address, subject: 'Email test successfull.')
+      subject = "#{Spree::Store.current.name} #{Spree.t('test_mailer.test_email.subject')}"
+      mail(to: email, from: from_address, subject: subject)
     end
   end
 end
