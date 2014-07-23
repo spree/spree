bb45c26589edc721b4966e72c1969523bdad1780
Matthew Fenelon <matt@fnln.me>
2014-07-23 17:43:35 +0100

    Sync changes for link_to_cart from spree/spree.

--- app/helpers/spree/base_helper_decorator.rb
+++ app/helpers/spree/base_helper_decorator.rb
@@ -1,19 +1,17 @@
 Spree::BaseHelper.module_eval do
   def link_to_cart(text = nil)
-    return "" if current_spree_page?(spree.cart_path)
-
     text = text ? h(text) : Spree.t('cart')
     css_class = nil
 
-    if current_order.nil? or current_order.item_count.zero?
-      text = "<span class='glyphicon glyphicon-shopping-cart'></span> #{text}: (#{Spree.t('empty')})".html_safe
+    if simple_current_order.nil? or simple_current_order.item_count.zero?
+      text = "<span class='glyphicon glyphicon-shopping-cart'></span> #{text}: (#{Spree.t('empty')})"
       css_class = 'empty'
     else
-      text = "<span class='glyphicon glyphicon-shopping-cart'></span> #{text}: (#{current_order.item_count})  <span class='amount'>#{current_order.display_total.to_html}</span>".html_safe
+      text = "<span class='glyphicon glyphicon-shopping-cart'></span> #{text}: (#{simple_current_order.item_count})  <span class='amount'>#{simple_current_order.display_total.to_html}</span>"
       css_class = 'full'
     end
 
-    link_to text, spree.cart_path, :class => "cart-info #{css_class}"
+    link_to text.html_safe, spree.cart_path, :class => "cart-info #{css_class}"
   end
 
   def flash_messages(opts = {})
