2ac10cccb53ab1b349893744986af0c5191b7baa
Joe Stanton <joe.stanton@red-badger.com>
2014-07-23 10:27:22 +0100

    Fixed adjustments spec

--- core/spec/models/spree/order/adjustments_spec.rb
+++ core/spec/models/spree/order/adjustments_spec.rb
@@ -25,7 +25,8 @@ describe Spree::Order do
     before do
       # Don't care about available payment methods in this test
       persisted_order.stub(:has_available_payment => false)
-      persisted_order.line_items << line_item
+      persisted_order.consignments.create!
+      persisted_order.consignments.first.line_items << line_item
       create(:adjustment, :amount => -line_item.amount, :label => "Promotion", :adjustable => line_item)
       persisted_order.state = 'delivery'
       persisted_order.save # To ensure new state_change event
