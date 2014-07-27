960d0b45044e26c6d8359f19ee89d6f41f70e084
Jeff Squires <jeff.squires@gmail.com>
2014-07-27 14:31:03 -0400

    adds per-shipping-method shipment handlers that can execute custom logic during the 'after_ship' shipping phase

---
+++ core/app/models/spree/shipment_handler.rb
@@ -0,0 +1,43 @@
+module Spree
+  class ShipmentHandler
+    class << self
+      def factory(shipment)
+        # Do we have a specialized shipping-method-specific handler? e.g:
+        # Given shipment.shipping_method = Spree::ShippingMethod::DigitalDownload
+        # do we have Spree::ShipmentHandler::DigitalDownload?
+        if sm_handler = "Spree::ShipmentHandler::#{shipment.shipping_method.name.split('::').last}".constantize rescue false
+          sm_handler.new(shipment)
+        else
+          new(shipment)
+        end        
+      end
+    end
+    
+    def initialize(shipment)
+      @shipment = shipment
+    end
+    
+    def perform
+      @shipment.inventory_units.each &:ship!
+      @shipment.process_order_payments if Spree::Config[:auto_capture_on_dispatch]
+      send_shipped_email
+      @shipment.touch :shipped_at
+      update_order_shipment_state
+    end
+
+    private
+      def send_shipped_email
+        ShipmentMailer.shipped_email(@shipment.id).deliver
+      end
+  
+      def update_order_shipment_state
+        order = @shipment.order
+  
+        new_state = OrderUpdater.new(order).update_shipment_state
+        order.update_columns(
+                             shipment_state: new_state,
+                             updated_at: Time.now,
+                             )
+      end
+  end
+end
