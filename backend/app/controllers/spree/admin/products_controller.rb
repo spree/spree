706a6bc43457f413d69007a7c33e16ebd4f2cb0f
Daniel Westendorf <daniel@prowestech.com>
2014-07-27 10:23:25 -0600

    Remove tables from includes on index query which are not needed an result in bad performance with many Variants

--- backend/app/controllers/spree/admin/products_controller.rb
+++ backend/app/controllers/spree/admin/products_controller.rb
@@ -123,9 +123,9 @@ module Spree
         end
 
         def product_includes
-          [{ :variants => [:images, { :option_values => :option_type }], :master => [:images, :default_price]}]
+          [{ :variants => [:images], :master => [:images, :default_price]}]
         end
-        
+
         def clone_object_url resource
           clone_admin_product_url resource
         end
