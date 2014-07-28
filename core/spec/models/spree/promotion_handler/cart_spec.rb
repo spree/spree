14dad56c673fb8671af1ec20d92c46a5618338a3
Adam Mueller <adam@freerunningtech.com>
2014-07-28 14:16:48 -0700

    Fix Spree::Promotion not applying all of the time.
    There was a specific case in which promotions added to an order would
    not apply to new items added to the cart. This corrects that by having
    the PromotionHandler activate all promotions already associated with the
    order as well as any with a blank code and path.
    
    Closes #5072

--- core/spec/models/spree/promotion_handler/cart_spec.rb
+++ core/spec/models/spree/promotion_handler/cart_spec.rb
@@ -82,6 +82,21 @@ module Spree
           include_context "creates the adjustment"
         end
       end
+
+      context "activates promotions associated with the order" do
+        let(:promo) { create :promotion_with_item_adjustment, adjustment_rate: 5, code: 'promo' }
+        let(:adjustable) { line_item }
+
+        before do
+          order.promotions << promo
+        end
+
+        it "creates the adjustment" do
+          expect {
+            subject.activate
+          }.to change { adjustable.adjustments.count }.by(1)
+        end
+      end
     end
   end
 end
