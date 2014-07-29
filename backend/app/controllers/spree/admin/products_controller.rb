706403a6ffaeb72f49678b6ea2d2a8fc69c7f0d5
Isaac Freeman <isaac@resolvedigital.co.nz>
2014-07-29 15:00:38 +1200

    Reorganised product form

--- backend/app/controllers/spree/admin/products_controller.rb
+++ backend/app/controllers/spree/admin/products_controller.rb
@@ -65,6 +65,16 @@ module Spree
         redirect_to edit_admin_product_url(@new)
       end
 
+      def edit
+        @variants = @product.variants
+        @variants = [@product.master] if @variants.empty?
+        @stock_locations = StockLocation.accessible_by(current_ability, :read)
+        if @stock_locations.empty?
+          flash[:error] = Spree.t(:stock_management_requires_a_stock_location)
+          redirect_to admin_stock_locations_path
+        end
+      end
+
       def stock
         @variants = @product.variants
         @variants = [@product.master] if @variants.empty?
