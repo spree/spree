f48091f8aa74cd9451ad689d506d8ce5f537cd93
Jeff Dutil <JDutil@BurlingtonWebApps.com>
2014-07-30 00:05:24 -0400

    Fix whitespacing.

--- api/spec/controllers/spree/api/credit_cards_controller_spec.rb
+++ api/spec/controllers/spree/api/credit_cards_controller_spec.rb
@@ -11,18 +11,17 @@ module Spree
       user.generate_spree_api_key!
       user
     end
-    
+
     before do
       stub_authentication!
     end
-    
+
     it "the user id doesn't exist" do
       api_get :index, user_id: 1000
-	  
-	  response.status.should == 404
-	end
-	
-	context "user does not have a credit card" do
+      response.status.should == 404
+    end
+
+    context "user does not have a credit card" do
       let(:current_api_user) do
         user = Spree.user_class.new(:email => "spree2@example.com", :id => 2)
         user.generate_spree_api_key!
@@ -36,8 +35,8 @@ module Spree
         json_response["pages"].should == 0
       end
     end
-    
-	context "user has a credit card " do
+
+    context "user has a credit card " do
       let!(:card) { create(:credit_card, user_id: current_api_user.id, gateway_customer_profile_id: "random") }
 
       it "can view all of their own credit cards" do
