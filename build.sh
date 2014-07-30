67b12ba396af1bd5b593e4639772564fa213d2af
Jordan Brough <git.j@brgh.net>
2014-07-30 07:37:28 -0600

    Improve build.sh output
    - Add "--quiet" to "bundle update" to silence gem output
    - Highlight the beginning of each suite more with some asterisks

--- build.sh
+++ build.sh
@@ -13,25 +13,35 @@ export DB=${DB:-postgres}
 
 # Spree defaults
 echo "Setup Spree defaults and creating test application..."
-bundle check || bundle update
+bundle check || bundle update --quiet
 bundle exec rake test_app
 
 # Spree API
-echo "Setup Spree API and running RSpec..."
-cd api; set_gemfile; bundle update; bundle exec rspec spec
+echo "**************************************"
+echo "* Setup Spree API and running RSpec..."
+echo "**************************************"
+cd api; set_gemfile; bundle update --quiet; bundle exec rspec spec
 
 # Spree Backend
-echo "Setup Spree Backend and running RSpec..."
-cd ../backend; set_gemfile; bundle update; bundle exec rspec spec
+echo "******************************************"
+echo "* Setup Spree Backend and running RSpec..."
+echo "******************************************"
+cd ../backend; set_gemfile; bundle update --quiet; bundle exec rspec spec
 
 # Spree Core
-echo "Setup Spree Core and running RSpec..."
-cd ../core; set_gemfile; bundle update; bundle exec rspec spec
+echo "***************************************"
+echo "* Setup Spree Core and running RSpec..."
+echo "***************************************"
+cd ../core; set_gemfile; bundle update --quiet; bundle exec rspec spec
 
 # Spree Frontend
-echo "Setup Spree Frontend and running RSpec..."
-cd ../frontend; set_gemfile; bundle update; bundle exec rspec spec
+echo "*******************************************"
+echo "* Setup Spree Frontend and running RSpec..."
+echo "*******************************************"
+cd ../frontend; set_gemfile; bundle update --quiet; bundle exec rspec spec
 
 # Spree Sample
-echo "Setup Spree Sample and running RSpec..."
-cd ../sample; set_gemfile; bundle update; bundle exec rspec spec
+echo "*****************************************"
+echo "* Setup Spree Sample and running RSpec..."
+echo "*****************************************"
+cd ../sample; set_gemfile; bundle update --quiet; bundle exec rspec spec
