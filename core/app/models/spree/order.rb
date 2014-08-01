bddbdd1b22d79cfdd1baf2f03f01659b882a0497
Jeff Dutil <JDutil@BurlingtonWebApps.com>
2014-08-01 01:46:00 -0400

    Validate uniqueness of order numbers... Why was this FIXME never fixed until now... Fixes #5102

--- core/app/models/spree/order.rb
+++ core/app/models/spree/order.rb
@@ -74,6 +74,7 @@ module Spree
 
     validates :email, presence: true, if: :require_email
     validates :email, email: true, if: :require_email, allow_blank: true
+    validates :number, uniqueness: true
     validate :has_available_shipment
 
     make_permalink field: :number
@@ -261,15 +262,11 @@ module Spree
       end
     end
 
-    # FIXME refactor this method and implement validation using validates_* utilities
     def generate_order_number
-      record = true
-      while record
+      self.number ||= loop do
         random = "R#{Array.new(9){rand(9)}.join}"
-        record = self.class.where(number: random).first
+        break random unless self.class.exists?(number: random)
       end
-      self.number = random if self.number.blank?
-      self.number
     end
 
     def shipped_shipments
