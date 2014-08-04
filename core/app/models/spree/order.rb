7cccf9bd213ebcf19487a833d11878fce8ebe662
Miguel Sancho <sanchocuba@gmail.com>
2014-08-04 12:17:31 -0400

    Was added options to generate_order_numer.
    Fixes #5126

--- core/app/models/spree/order.rb
+++ core/app/models/spree/order.rb
@@ -273,18 +273,25 @@ module Spree
       end
     end
 
-    def generate_order_number(digits = 9)
+    def generate_order_number(options = {})
+      options[:length] ||= 9
+      options[:letters] ||= false
+      options[:prefix] ||= 'R'
+      
+      possible = (0..9).to_a
+      possible += ('A'..'Z').to_a if options[:letters]
+      
       self.number ||= loop do
-         # Make a random number.
-         random = "R#{Array.new(digits){rand(10)}.join}"
-         # Use the random  number if no other order exists with it.
-         if self.class.exists?(number: random)
-           # If over half of all possible options are taken add another digit.
-           digits += 1 if self.class.count > (10 ** digits / 2)
-         else
-           break random
-         end
-       end
+        # Make a random number.
+        random = "#{options[:prefix]}#{(0...options[:length]).map { possible.shuffle.first }.join}"
+        # Use the random  number if no other order exists with it.
+        if self.class.exists?(number: random)
+          # If over half of all possible options are taken add another digit.
+          options[:length] += 1 if self.class.count > (10 ** options[:length] / 2)
+        else
+          break random
+        end
+      end	  
     end
 
     def shipped_shipments
