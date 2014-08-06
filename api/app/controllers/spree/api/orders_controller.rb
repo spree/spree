99037d7f2b365ae1cb1826ed5cbd11eb4d1dc14f
Jeff Dutil <JDutil@BurlingtonWebApps.com>
2014-08-06 12:31:59 -0400

    Fix API::Orders#create  for admins.

--- api/app/controllers/spree/api/orders_controller.rb
+++ api/app/controllers/spree/api/orders_controller.rb
@@ -31,7 +31,7 @@ module Spree
         end
 
         import_params = if @current_user_roles.include?("admin")
-          params[:order].permit!
+          params[:order].present? ? params[:order].permit! : {}
         else
           order_params
         end
