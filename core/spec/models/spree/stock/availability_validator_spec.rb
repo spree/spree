a9d3ad91189ee11a6b05641db4c537e3a52ff773
John Hawthorn <john@freerunningtechnologies.com>
2014-08-06 15:33:06 -0700

    Fix AvailabilityValidator for saved records
    Previously availability only considered inventory_units on the
    target_shipment of a line_item. Since target_shipment is not persisted,
    a saved line_item would become invalid when its variant ran out of
    available inventory even if the inventory_units were already reserved.
    
    Fixes #5208

--- core/spec/models/spree/stock/availability_validator_spec.rb
+++ core/spec/models/spree/stock/availability_validator_spec.rb
@@ -3,19 +3,26 @@ require 'spec_helper'
 module Spree
   module Stock
     describe AvailabilityValidator do
-      let!(:line_item) { double(quantity: 5, target_shipment: nil, variant_id: 1, variant: double.as_null_object, errors: double('errors')) }
+      let!(:line_item) { double(quantity: 5, variant_id: 1, variant: double.as_null_object, errors: double('errors'), inventory_units: []) }
 
       subject { described_class.new }
 
       it 'should be valid when supply is sufficient' do
         Stock::Quantifier.any_instance.stub(can_supply?: true)
-        line_item.errors.should_not_receive(:[]).with(:quantity)
+        line_item.should_not_receive(:errors)
         subject.validate(line_item)
       end
 
       it 'should be invalid when supply is insufficent' do
         Stock::Quantifier.any_instance.stub(can_supply?: false)
         line_item.errors.should_receive(:[]).with(:quantity).and_return []
+        subject.validate(line_item)
+      end
+
+      it 'should consider existing inventory_units sufficient' do
+        Stock::Quantifier.any_instance.stub(can_supply?: false)
+        line_item.should_not_receive(:errors)
+        line_item.stub(inventory_units: [double] * 5)
         subject.validate(line_item)
       end
     end
