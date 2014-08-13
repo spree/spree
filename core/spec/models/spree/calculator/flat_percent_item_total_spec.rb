a51249ccabff871b463786ff89c2cd4fd1ed63a8
Jeff Dutil <JDutil@BurlingtonWebApps.com>
2014-08-13 17:46:05 -0400

    Only compute an amount no greater than the objects amount.

--- core/spec/models/spree/calculator/flat_percent_item_total_spec.rb
+++ core/spec/models/spree/calculator/flat_percent_item_total_spec.rb
@@ -4,15 +4,21 @@ describe Spree::Calculator::FlatPercentItemTotal do
   let(:calculator) { Spree::Calculator::FlatPercentItemTotal.new }
   let(:line_item) { mock_model Spree::LineItem }
 
-  before { calculator.stub :preferred_flat_percent => 10 }
+  before { calculator.stub preferred_flat_percent: 10 }
 
   context "compute" do
     it "should round result correctly" do
-      line_item.stub :amount => 31.08
-      calculator.compute(line_item).should == 3.11
+      line_item.stub amount: 31.08
+      expect(calculator.compute(line_item)).to eq 3.11
 
-      line_item.stub :amount => 31.00
-      calculator.compute(line_item).should == 3.10
+      line_item.stub amount: 31.00
+      expect(calculator.compute(line_item)).to eq 3.10
+    end
+
+    it 'returns object.amount if computed amount is greater than object amount' do
+      calculator.stub preferred_flat_percent: 110
+      line_item.stub amount: 31.00
+      expect(calculator.compute(line_item)).to eq 31.0
     end
   end
 end
