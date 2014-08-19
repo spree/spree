e3a09226f0196f911796fe2006d2522dd1e27058
Irvin Fan <ifan@godaddy.com>
2014-08-19 12:15:40 -0700

    Create tax adjustment label with inclusive information if applicable Fixes #5244

--- core/app/models/spree/tax_rate.rb
+++ core/app/models/spree/tax_rate.rb
@@ -187,6 +187,8 @@ module Spree
         label = ""
         label << (name.present? ? name : tax_category.name) + " "
         label << (show_rate_in_label? ? "#{amount * 100}%" : "")
+        label << " (#{Spree.t(:included_in_price)})" if included_in_price?
+        label
       end
 
   end
