a780f91b802ffc7cc4484d5ff4f4c1d5a622f0d3
HoyaBoya <christopher.sun@gmail.com>
2014-08-19 12:45:44 -0400

    resolve conflict on api helpers in cherry-pick
    attempt to bring the following into 2-3-dev
    https://github.com/spree/spree/pull/5233

---
+++ api/app/controllers/spree/api/promotions_controller.rb
@@ -0,0 +1,26 @@
+module Spree
+  module Api
+    class PromotionsController < Spree::Api::BaseController
+      before_filter :requires_admin
+      before_filter :load_promotion
+
+      def show
+        if @promotion
+          respond_with(@promotion, default_template: :show)
+        else
+          raise ActiveRecord::RecordNotFound
+        end
+      end
+
+      private 
+        def requires_admin
+          return if @current_user_roles.include?("admin")
+          unauthorized and return
+        end
+
+        def load_promotion
+          @promotion = Spree::Promotion.find_by_id(params[:id]) || Spree::Promotion.with_coupon_code(params[:id])
+        end
+    end
+  end
+end
