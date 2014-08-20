7cb8905e90d018a3121da5f8e5e3a0de03ac1fc1
Peter Berkenbosch <peter@pero-ict.nl>
2014-08-20 12:22:43 +0200

    fix failing api spec. specifying additional params for line_items. all green now.

--- api/spec/controllers/spree/api/orders_controller_spec.rb
+++ api/spec/controllers/spree/api/orders_controller_spec.rb
@@ -231,7 +231,7 @@ module Spree
       Order.should_receive(:create!).and_return(order = Spree::Order.new)
       order.stub(:associate_user!)
       order.stub_chain(:contents, :add).and_return(line_item = double('LineItem'))
-      line_item.should_receive(:update_attributes).with("special" => true)
+      line_item.should_receive(:update_attributes!).with("special" => true)
 
       controller.stub(permitted_line_item_attributes: [:id, :variant_id, :quantity, :special])
       api_post :create, :order => {
