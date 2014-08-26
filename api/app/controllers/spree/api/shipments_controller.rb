300d171c37ab6506a8e5377d53702e4912e9c519
Dan Kubb <dan.kubb@gmail.com>
2014-08-26 10:11:46 -0700

    Add destroyed variants to be removed from a shipment
    * Previously, if a variant was destroyed it was impossible to remove it
      from a shipment. This change allows all variants to be removed.
    
    Fixes #5268

--- api/app/controllers/spree/api/shipments_controller.rb
+++ api/app/controllers/spree/api/shipments_controller.rb
@@ -8,7 +8,6 @@ module Spree
         @order = Spree::Order.find_by!(number: params[:shipment][:order_id])
         authorize! :read, @order
         authorize! :create, Shipment
-        variant = Spree::Variant.find(params[:variant_id])
         quantity = params[:quantity].to_i
         @shipment = @order.shipments.create(stock_location_id: params[:stock_location_id])
         @order.contents.add(variant, quantity, nil, @shipment)
@@ -44,7 +43,6 @@ module Spree
       end
 
       def add
-        variant = Spree::Variant.find(params[:variant_id])
         quantity = params[:quantity].to_i
 
         @shipment.order.contents.add(variant, quantity, nil, @shipment)
@@ -53,7 +51,6 @@ module Spree
       end
 
       def remove
-        variant = Spree::Variant.find(params[:variant_id])
         quantity = params[:quantity].to_i
 
         @shipment.order.contents.remove(variant, quantity, @shipment)
@@ -75,6 +72,10 @@ module Spree
         else
           {}
         end
+      end
+
+      def variant
+        @variant ||= Spree::Variant.unscoped.find(params.fetch(:variant_id))
       end
     end
   end
