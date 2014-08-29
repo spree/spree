30cc34a32a891d7564280e9eccb98b09f7f38a28
Peter Berkenbosch <peter@pero-ict.nl>
2014-08-29 17:31:27 +0200

    implement the code from #4488 by @k-akarsh.

--- core/app/models/spree/promotion/rules/item_total.rb
+++ core/app/models/spree/promotion/rules/item_total.rb
@@ -4,10 +4,14 @@ module Spree
   class Promotion
     module Rules
       class ItemTotal < PromotionRule
-        preference :amount, :decimal, default: 100.00
-        preference :operator, :string, default: '>'
+        preference :amount_min, :decimal, default: 100.00
+        preference :operator_min, :string, default: '>'
+        preference :amount_max, :decimal, default: 1000.00
+        preference :operator_max, :string, default: '<'
 
-        OPERATORS = ['gt', 'gte']
+
+        OPERATORS_MIN = ['gt', 'gte']
+        OPERATORS_MAX = ['lt','lte']
 
         def applicable?(promotable)
           promotable.is_a?(Spree::Order)
@@ -15,25 +19,42 @@ module Spree
 
         def eligible?(order, options = {})
           item_total = order.item_total
-          unless item_total.send(preferred_operator == 'gte' ? :>= : :>, BigDecimal.new(preferred_amount.to_s))
-            eligibility_errors.add(:base, ineligible_message)
-          end
+
+          lower_limit_condition = item_total.send(preferred_operator_min == 'gte' ? :>= : :>, BigDecimal.new(preferred_amount_min.to_s))
+          upper_limit_condition = item_total.send(preferred_operator_max == 'lte' ? :<= : :<, BigDecimal.new(preferred_amount_max.to_s))
+
+          eligibility_errors.add(:base, ineligible_message_max) unless upper_limit_condition
+          eligibility_errors.add(:base, ineligible_message_min) unless lower_limit_condition
 
           eligibility_errors.empty?
         end
 
         private
-        def formatted_amount
-          Spree::Money.new(preferred_amount).to_s
+        def formatted_amount_min
+          Spree::Money.new(preferred_amount_min).to_s
         end
 
-        def ineligible_message
-          if preferred_operator == 'gte'
-            eligibility_error_message(:item_total_less_than, amount: formatted_amount)
+        def formatted_amount_max
+          Spree::Money.new(preferred_amount_max).to_s
+        end
+
+
+        def ineligible_message_max
+          if preferred_operator_max == 'gte'
+            eligibility_error_message(:item_total_more_than_or_equal, amount: formatted_amount_max)
           else
-            eligibility_error_message(:item_total_less_than_or_equal, amount: formatted_amount)
+            eligibility_error_message(:item_total_more_than, amount: formatted_amount_max)
           end
         end
+
+        def ineligible_message_min
+          if preferred_operator_min == 'gte'
+            eligibility_error_message(:item_total_less_than, amount: formatted_amount_min)
+          else
+            eligibility_error_message(:item_total_less_than_or_equal, amount: formatted_amount_min)
+          end
+        end
+
       end
     end
   end
