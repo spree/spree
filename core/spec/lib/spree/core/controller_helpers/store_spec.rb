afee2197777b6260c948ae6962f6e2d1de392a81
Jeff Dutil <JDutil@BurlingtonWebApps.com>
2014-09-03 14:50:41 -0400

    Fix spec.

--- core/spec/lib/spree/core/controller_helpers/store_spec.rb
+++ core/spec/lib/spree/core/controller_helpers/store_spec.rb
@@ -7,7 +7,7 @@ describe Spree::Core::ControllerHelpers::Store, type: :controller do
   end
 
   describe '#current_store' do
-    let!(:store) { create :store }
+    let!(:store) { create :store, default: true }
     it 'returns current store' do
       expect(controller.current_store).to eq store
     end
