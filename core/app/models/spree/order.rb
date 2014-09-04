a1daea003802c1bd71ad9db4540ecd29c45283d1
Willian van der Velde <mail@willian.io>
2014-09-04 10:09:42 -0700

    Only cancel cancellable shipments.
    Fixes #5321
    Fixes #5299
    
    Conflicts:
    	core/app/models/spree/order.rb

--- core/app/models/spree/order.rb
+++ core/app/models/spree/order.rb
@@ -283,8 +283,34 @@ module Spree
       line_item ? line_item.quantity : 0
     end
 
+<<<<<<< HEAD
     def find_line_item_by_variant(variant)
       line_items.detect { |line_item| line_item.variant_id == variant.id }
+=======
+    def find_line_item_by_variant(variant, options = {})
+      line_items.detect { |line_item|
+                    line_item.variant_id == variant.id &&
+                    line_item_options_match(line_item, options)
+                  }
+    end
+
+    # This method enables extensions to participate in the
+    # "Are these line items equal" decision.
+    #
+    # When adding to cart, an extension would send something like:
+    # params[:product_customizations]={...}
+    #
+    # and would provide:
+    #
+    # def product_customizations_match
+    def line_item_options_match(line_item, options)
+      return true unless options
+
+      options.keys.all? do |key|
+        !self.respond_to?("#{key}_match".to_sym) ||
+        self.send("#{key}_match".to_sym, line_item, options[key])
+      end
+>>>>>>> d870c4e... Only cancel cancellable shipments.
     end
 
     # Creates new tax charges if there are any applicable rates. If prices already
@@ -630,7 +656,7 @@ module Spree
       end
 
       def after_cancel
-        shipments.each { |shipment| shipment.cancel! }
+        shipments.cancelable.each { |shipment| shipment.cancel! }
         payments.completed.each { |payment| payment.cancel! }
 
         send_cancel_email
