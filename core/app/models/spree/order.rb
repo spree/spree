d93d2e123689cc34210ff4095dd16c3162cb1e69
Jeff Dutil <JDutil@BurlingtonWebApps.com>
2014-09-05 14:54:01 -0400

    Revert "Only cancel cancellable shipments."
    This reverts commit a1daea003802c1bd71ad9db4540ecd29c45283d1.

--- core/app/models/spree/order.rb
+++ core/app/models/spree/order.rb
@@ -630,7 +630,7 @@ module Spree
       end
 
       def after_cancel
-        shipments.cancelable.each { |shipment| shipment.cancel! }
+        shipments.each { |shipment| shipment.cancel! }
         payments.completed.each { |payment| payment.cancel! }
 
         send_cancel_email
