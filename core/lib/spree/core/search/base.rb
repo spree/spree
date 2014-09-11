56af69cc0c7deaba0af4f6507f9565207d3594b9
Richard Wilson <richard@freerunningtech.com>
2014-09-11 07:57:51 -0700

    Reduce queries whenever _products are rendered.

--- core/lib/spree/core/search/base.rb
+++ core/lib/spree/core/search/base.rb
@@ -13,10 +13,9 @@ module Spree
         end
 
         def retrieve_products
-          @products_scope = get_base_scope
+          @products = get_base_scope
           curr_page = page || 1
 
-          @products = @products_scope.includes([:master => :prices])
           unless Spree::Config.show_products_without_price
             @products = @products.where("spree_prices.amount IS NOT NULL").where("spree_prices.currency" => current_currency)
           end
@@ -37,7 +36,16 @@ module Spree
             base_scope = base_scope.in_taxon(taxon) unless taxon.blank?
             base_scope = get_products_conditions_for(base_scope, keywords)
             base_scope = add_search_scopes(base_scope)
+            base_scope = add_eagerload_scopes(base_scope)
             base_scope
+          end
+
+          def add_eagerload_scopes scope
+            if include_images
+              scope.eager_load({master: [:prices, :images]})
+            else
+              scope.includes(master: :prices)
+            end
           end
 
           def add_search_scopes(base_scope)
@@ -64,6 +72,7 @@ module Spree
             @properties[:taxon] = params[:taxon].blank? ? nil : Spree::Taxon.find(params[:taxon])
             @properties[:keywords] = params[:keywords]
             @properties[:search] = params[:search]
+            @properties[:include_images] = params[:include_images]
 
             per_page = params[:per_page].to_i
             @properties[:per_page] = per_page > 0 ? per_page : Spree::Config[:products_per_page]
