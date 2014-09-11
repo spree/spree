e4c04dc576e8af9dd01c05cbcef08ed09408d611
HoyaBoya <christopher.sun@gmail.com>
2014-09-11 10:40:50 -0400

    Remove check for payment step and has payment step in checkout flow when updating payment source Fixes #5351

--- core/app/models/spree/order/checkout.rb
+++ core/app/models/spree/order/checkout.rb
@@ -301,19 +301,17 @@ module Spree
           #   }
           #
           def update_params_payment_source
-            if has_checkout_step?("payment") && self.payment?
-              if @updating_params[:payment_source].present?
-                source_params = @updating_params.delete(:payment_source)[@updating_params[:order][:payments_attributes].first[:payment_method_id].to_s]
+            if @updating_params[:payment_source].present?
+              source_params = @updating_params.delete(:payment_source)[@updating_params[:order][:payments_attributes].first[:payment_method_id].to_s]
 
-                if source_params
-                  @updating_params[:order][:payments_attributes].first[:source_attributes] = source_params
-                end
+              if source_params
+                @updating_params[:order][:payments_attributes].first[:source_attributes] = source_params
               end
+            end
 
-              if @updating_params[:order][:payments_attributes] || @updating_params[:order][:existing_card]
-                @updating_params[:order][:payments_attributes] ||= [{}]
-                @updating_params[:order][:payments_attributes].first[:amount] = self.total
-              end
+            if @updating_params[:order] && (@updating_params[:order][:payments_attributes] || @updating_params[:order][:existing_card])
+              @updating_params[:order][:payments_attributes] ||= [{}]
+              @updating_params[:order][:payments_attributes].first[:amount] = self.total
             end
           end
         end
