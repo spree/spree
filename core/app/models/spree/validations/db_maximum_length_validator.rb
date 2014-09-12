6cf14460740c12de217fcb38dd5f473f21f146f0
HoyaBoya <christopher.sun@gmail.com>
2014-09-12 10:59:10 -0400

    let value have validation of db column length
    need to rescue AR issue for spree install, ci, etc (chicken and the egg issue)
    
    do validations as a re-useable validtor
    
    explicit argument error message
    Fixes #5361

---
+++ core/app/models/spree/validations/db_maximum_length_validator.rb
@@ -0,0 +1,22 @@
+module Spree
+  module Validations
+    ##
+    # Validates a field based on the maximum length of the underlying DB field, if there is one.
+    class DbMaximumLengthValidator < ActiveModel::Validator
+
+      def initialize(options)
+        super
+        @field = options[:field].to_s
+        raise ArgumentError.new("a field must be specified to the validator") if @field.blank?
+      end
+
+      def validate(record)
+        limit = record.class.columns_hash[@field].limit
+        value = record[@field.to_sym]
+        if value && limit && value.to_s.length > limit 
+          record.errors.add(@field.to_sym, :too_long, count: limit)
+        end
+      end
+    end
+  end
+end
