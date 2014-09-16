7af4b932a6f64e32e1bb027d8b95f85869016686
Jeff Dutil <JDutil@BurlingtonWebApps.com>
2014-09-16 12:00:54 -0400

    Use email regex from Devise. Fixes #5368

--- core/lib/spree/core/validators/email.rb
+++ core/lib/spree/core/validators/email.rb
@@ -1,6 +1,6 @@
 class EmailValidator < ActiveModel::EachValidator
   def validate_each(record,attribute,value)
-    unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+    unless value =~ /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
       record.errors.add(attribute, :invalid, {:value => value}.merge!(options))
     end
   end
