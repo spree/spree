414936fadc3a0345bc239389f3c41297a776a03d
HoyaBoya <christopher.sun@gmail.com>
2014-09-24 12:51:52 -0400

    start of code for tax category
    correct field name
    
    layout
    
    layout
    
    more layout
    
    add translation, simple controller tests
    
    fix test not in 2-3-stable for tax category table

---
+++ backend/spec/controllers/spree/admin/tax_categories_controller_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+module Spree
+  module Admin
+    describe TaxCategoriesController do
+      stub_authorization!
+
+      describe 'GET #index' do
+        subject { spree_get :index }
+
+        it 'should be successful' do
+          expect(subject).to be_success
+        end
+      end
+
+      describe 'PUT #update' do
+        let(:tax_category) { create :tax_category }
+    
+        subject { spree_put :update, {id: tax_category.id, tax_category: { name: 'Foo', tax_code: 'Bar' }}}
+
+        it 'should redirect' do
+          expect(subject).to be_redirect
+        end
+
+        it 'should update' do
+          subject
+          tax_category.reload
+          expect(tax_category.name).to eq('Foo')
+          expect(tax_category.tax_code).to eq('Bar')
+        end
+      end
+    end
+  end
+end
