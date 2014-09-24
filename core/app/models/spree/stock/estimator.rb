3c4db283f4ab934f2d2d14798e259fb37429a3f9
Markus Schirp <mbj@schirp-dso.com>
2014-09-24 21:40:11 +0000

    Kill exception shadowing
    * The code used to rescue any exception. Even exceptions operators /
      developers want to stop the world.
    * When we want to allow a certain set of exceptions to be shadowed we
      should use shipment rate calculation specific exception lists that still
      allow shipment-unrelated exceptions to stop the world.
    * When the downstream libraries do not provide matchable exceptions
      these should be fixed instead of adding a debugging burden to everyone
      having to work with shipment rate calculation.
    
    Fixes #6369

--- core/app/models/spree/stock/estimator.rb
+++ core/app/models/spree/stock/estimator.rb
@@ -49,22 +49,13 @@ module Spree
       def shipping_methods(package, display_filter)
         package.shipping_methods.select do |ship_method|
           calculator = ship_method.calculator
-          begin
-            ship_method.available_to_display(display_filter) &&
-            ship_method.include?(order.ship_address) &&
-            calculator.available?(package) &&
-            (calculator.preferences[:currency].blank? ||
-             calculator.preferences[:currency] == currency)
-          rescue Exception => exception
-            log_calculator_exception(ship_method, exception)
-          end
-        end
-      end
 
-      def log_calculator_exception(ship_method, exception)
-        Rails.logger.info("Something went wrong calculating rates with the #{ship_method.name} (ID=#{ship_method.id}) shipping method.")
-        Rails.logger.info("*" * 50)
-        Rails.logger.info(exception.backtrace.join("\n"))
+          ship_method.available_to_display(display_filter) &&
+          ship_method.include?(order.ship_address) &&
+          calculator.available?(package) &&
+          (calculator.preferences[:currency].blank? ||
+           calculator.preferences[:currency] == currency)
+        end
       end
     end
   end
