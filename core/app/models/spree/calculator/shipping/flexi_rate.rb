6cd314f0d226626722e76f81265cdfe074ca986b
Brian Christian <brchristian@gmail.com>
2014-09-25 09:32:42 -0700

    refactor shipping calculator compute methods
    clearer naming for methods
    
    Fixes #5414

--- core/app/models/spree/calculator/shipping/flexi_rate.rb
+++ core/app/models/spree/calculator/shipping/flexi_rate.rb
@@ -13,11 +13,13 @@ module Spree
       end
 
       def compute_package(package)
-        content_items = package.contents
+        compute_from_quantity(package.contents.sum(&:quantity))
+      end
+
+      def compute_from_quantity(quantity)
         sum = 0
         max = self.preferred_max_items.to_i
-        items_count = content_items.map(&:quantity).sum
-        items_count.times do |i|
+        quantity.times do |i|
           # check max value to avoid divide by 0 errors
           if (max == 0 && i == 0) || (max > 0) && (i % max == 0)
             sum += self.preferred_first_item.to_f
