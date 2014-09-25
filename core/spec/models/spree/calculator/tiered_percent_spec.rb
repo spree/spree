bbd9f682766913c632573b098d07d08d9316269d
Ben A Morgan <ben@benmorgan.io>
2014-09-24 18:51:43 -0700

    arrays should be of an even count when converting to hash

--- core/spec/models/spree/calculator/tiered_percent_spec.rb
+++ core/spec/models/spree/calculator/tiered_percent_spec.rb
@@ -14,7 +14,7 @@ describe Spree::Calculator::TieredPercent do
       it { should be false }
     end
     context "when tiers is not a hash" do
-      before { calculator.preferred_tiers = ["nope"] }
+      before { calculator.preferred_tiers = ["nope", 0] }
       it { should be false }
     end
     context "when tiers is a hash" do
