c28de997f7660b84c54b1961ca0aaee8a1afecf7
Markus Schirp <mbj@schirp-dso.com>
2014-09-30 19:31:16 +0000

    Fix factories to generate unique SKUs
    * Choosing random SKUs even from a high range can yield duplicates
      resulting in build failures.
    * Kernel.rand(9999) was definitively never a good choice due the
      birthday paradoxon lets manifest a duplicate quite often
    * Uses sequencially generated SKUs defined from a single shared and
      reusable generator.
    
    Fixes #5452

--- core/lib/spree/testing_support/factories.rb
+++ core/lib/spree/testing_support/factories.rb
@@ -14,4 +14,6 @@ FactoryGirl.define do
   sequence(:random_string)      { Faker::Lorem.sentence }
   sequence(:random_description) { Faker::Lorem.paragraphs(1 + Kernel.rand(5)).join("\n") }
   sequence(:random_email)       { Faker::Internet.email }
+
+  sequence(:sku) { |n| "SKU-#{n}" }
 end
