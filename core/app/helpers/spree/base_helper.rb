d1cdb7ed09dca84ef0f6431b428fd58efb66d92c
Jeff Dutil <JDutil@BurlingtonWebApps.com>
2014-10-03 17:06:22 -0400

    Add in fix from @andrzejpolis Fixes #5384 Fixes #5386

--- core/app/helpers/spree/base_helper.rb
+++ core/app/helpers/spree/base_helper.rb
@@ -162,9 +162,10 @@ module Spree
     end
 
     private
+
     # Returns style of image or nil
     def image_style_from_method_name(method_name)
-      if style = method_name.to_s.sub(/_image$/, '')
+      if method_name.to_s.match(/_image$/) && style = method_name.to_s.sub(/_image$/, '')
         possible_styles = Spree::Image.attachment_definitions[:attachment][:styles]
         style if style.in? possible_styles.with_indifferent_access
       end
