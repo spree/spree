10e82ca0973cc36d6719679ab119e3963d7b6441
Ben A Morgan <ben@benmorgan.io>
2014-10-12 02:49:36 -0700

    set up a spec for spree prototypes

---
+++ core/spec/models/spree/prototype_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+describe Spree::Prototype do
+  it { should have_and_belong_to_many :properties }
+  it { should have_and_belong_to_many :option_types }
+
+  it { should validate_presence_of :name }
+end
