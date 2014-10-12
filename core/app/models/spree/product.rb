88221b8d02437c4af9c82986cc3e04e80f290cca
Ben A Morgan <ben@benmorgan.io>
2014-10-12 04:47:48 -0700

    whitespace

--- core/app/models/spree/product.rb
+++ core/app/models/spree/product.rb
@@ -301,8 +301,8 @@ module Spree
     # Try building a slug based on the following fields in increasing order of specificity.
     def slug_candidates
       [
-          :name,
-          [:name, :sku]
+        :name,
+        [:name, :sku]
       ]
     end
 
