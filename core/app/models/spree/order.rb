fa462b049eacd7e711d35d01e14c95e069cf37f7
Markus Schirp <mbj@schirp-dso.com>
2014-10-14 16:48:22 +0000

    Simplify and deduplicate Order#merge! implementation

--- core/app/models/spree/order.rb
+++ core/app/models/spree/order.rb
@@ -428,26 +428,12 @@ module Spree
     def merge!(order, user = nil)
       order.line_items.each do |line_item|
         next unless line_item.currency == currency
-        current_line_item = self.line_items.find_by(variant: line_item.variant)
-        if current_line_item
-          current_line_item.quantity += line_item.quantity
-          current_line_item.save
-        else
-          line_item.order_id = self.id
-          line_item.save
-        end
+
+        contents.add(line_item.variant, line_item.quantity, line_item.currency)
       end
 
-      reload
+      associate_user!(user) if user && !self.user
 
-      self.associate_user!(user) if !self.user && !user.blank?
-
-      updater.update_item_count
-      updater.update_item_total
-      updater.persist_totals
-
-      # So that the destroy doesn't take out line items which may have been re-assigned
-      order.line_items.reload
       order.destroy
     end
 
