23d7e8eaf502d238b52f85779d56eca691bb5ccd
Irvin Fan <ifan@godaddy.com>
2014-10-13 17:58:22 -0700

    Do not allow leading or trailing dot(.) in the first part of email address
    Add rspec for email validator
    
    Fixes #5483

--- core/lib/spree/core/validators/email.rb
+++ core/lib/spree/core/validators/email.rb
@@ -1,6 +1,6 @@
 class EmailValidator < ActiveModel::EachValidator
   def validate_each(record,attribute,value)
-    unless value =~ /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
+    unless value =~ /\A([^@\.]|[^@\.]([^@\s]*)[^@\.])@([^@\s]+\.)+[^@\s]+\z/
       record.errors.add(attribute, :invalid, {:value => value}.merge!(options))
     end
   end
