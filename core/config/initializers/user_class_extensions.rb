0fc03196d8e36bdbcb21c1b61ded3d3600487762
Jeff Dutil <JDutil@BurlingtonWebApps.com>
2014-10-15 17:36:12 -0400

    Simplify set_current_order. Fixes #5382

--- core/config/initializers/user_class_extensions.rb
+++ core/config/initializers/user_class_extensions.rb
@@ -2,25 +2,26 @@ Spree::Core::Engine.config.to_prepare do
   if Spree.user_class
     Spree.user_class.class_eval do
 
-      include Spree::UserReporting
       include Spree::UserApiAuthentication
+      include Spree::UserReporting
+
       has_and_belongs_to_many :spree_roles,
-                              :join_table => 'spree_roles_users',
-                              :foreign_key => "user_id",
-                              :class_name => "Spree::Role"
+                              join_table: 'spree_roles_users',
+                              foreign_key: "user_id",
+                              class_name: "Spree::Role"
 
-      has_many :spree_orders, :foreign_key => "user_id", :class_name => "Spree::Order"
+      has_many :spree_orders, foreign_key: "user_id", class_name: "Spree::Order"
 
-      belongs_to :ship_address, :class_name => 'Spree::Address'
-      belongs_to :bill_address, :class_name => 'Spree::Address'
+      belongs_to :ship_address, class_name: 'Spree::Address'
+      belongs_to :bill_address, class_name: 'Spree::Address'
 
       # has_spree_role? simply needs to return true or false whether a user has a role or not.
       def has_spree_role?(role_in_question)
-        spree_roles.where(:name => role_in_question.to_s).any?
+        spree_roles.where(name: role_in_question.to_s).any?
       end
 
       def last_incomplete_spree_order
-        spree_orders.incomplete.where(:created_by_id => self.id).order('created_at DESC').first
+        spree_orders.incomplete.where(user_id: self.id).order('created_at DESC').first
       end
     end
   end
