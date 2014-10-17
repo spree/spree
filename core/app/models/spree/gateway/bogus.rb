d0bb29e149e2d6686ec981d085b9733a11c37b5f
Paul Damer <pdamer@gmail.com>
2014-10-17 14:48:48 -0400

    make bogus gateway match actual gateways. ie. don't assign profile id for failed payments

--- core/app/models/spree/gateway/bogus.rb
+++ core/app/models/spree/gateway/bogus.rb
@@ -19,8 +19,9 @@ module Spree
 
     def create_profile(payment)
       # simulate the storage of credit card profile using remote service
-      success = VALID_CCS.include? payment.source.number
-      payment.source.update_attributes(:gateway_customer_profile_id => generate_profile_id(success))
+      if success = VALID_CCS.include?(payment.source.number)
+        payment.source.update_attributes(:gateway_customer_profile_id => generate_profile_id(success))
+      end
     end
 
     def authorize(money, credit_card, options = {})
