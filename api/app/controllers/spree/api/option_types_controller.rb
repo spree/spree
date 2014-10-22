4580d8e90f61190c05201797d1662d2b14200c33
Ben A Morgan <ben@benmorgan.io>
2014-10-22 16:51:45 -0700

    include option values when loading option values from option types fixes #5519

--- api/app/controllers/spree/api/option_types_controller.rb
+++ api/app/controllers/spree/api/option_types_controller.rb
@@ -3,9 +3,9 @@ module Spree
     class OptionTypesController < Spree::Api::BaseController
       def index
         if params[:ids]
-          @option_types = Spree::OptionType.accessible_by(current_ability, :read).where(:id => params[:ids].split(','))
+          @option_types = Spree::OptionType.includes(:option_values).accessible_by(current_ability, :read).where(id: params[:ids].split(','))
         else
-          @option_types = Spree::OptionType.accessible_by(current_ability, :read).load.ransack(params[:q]).result
+          @option_types = Spree::OptionType.includes(:option_values).accessible_by(current_ability, :read).load.ransack(params[:q]).result
         end
         respond_with(@option_types)
       end
