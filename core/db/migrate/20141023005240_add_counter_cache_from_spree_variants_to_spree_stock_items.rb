5d0d7048bb3b0c0e346cc6bb9ec4ea6ef1a51002
Ben A Morgan <ben@benmorgan.io>
2014-10-24 23:47:23 -0700

    refactor stock item column setup

--- core/db/migrate/20141023005240_add_counter_cache_from_spree_variants_to_spree_stock_items.rb
+++ core/db/migrate/20141023005240_add_counter_cache_from_spree_variants_to_spree_stock_items.rb
@@ -1,19 +1,10 @@
 class AddCounterCacheFromSpreeVariantsToSpreeStockItems < ActiveRecord::Migration
   def up
-    # Setup the column.
-    add_column :spree_variants, :stock_items_count, :integer
+    add_column :spree_variants, :stock_items_count, :integer, default: 0, null: false
 
-    # Reset the cached counters for stock items of already
-    # existing records.
     Spree::Variant.find_each do |variant|
       Spree::Variant.reset_counters(variant.id, :stock_items)
     end
-
-    # We are doing a defualt and null here because in some databases (pg)
-    # you will get an error for having a null value on an existing record.
-    # The known hack around this is to perform an add column and then a
-    # change column.
-    change_column :spree_variants, :stock_items_count, :integer, default: 0, null: false
   end
 
   def down
