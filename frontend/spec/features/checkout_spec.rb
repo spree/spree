22a8f2911efaeafe902f8f46887ba9fd3a56657e
Hnat Kubov <hnatt88@gmail.com>
2014-10-27 01:27:46 +0200

    Fix #5530: checkout form error messages not translated in production
    Precompile jquery.validation error messages for all locales
    
    Add regression test for #5530
    
    fixes #5530
    fixes #5534

--- frontend/spec/features/checkout_spec.rb
+++ frontend/spec/features/checkout_spec.rb
@@ -2,13 +2,7 @@ require 'spec_helper'
 
 describe "Checkout", type: :feature, inaccessible: true do
 
-  let!(:country) { create(:country, :states_required => true) }
-  let!(:state) { create(:state, :country => country) }
-  let!(:shipping_method) { create(:shipping_method) }
-  let!(:stock_location) { create(:stock_location) }
-  let!(:mug) { create(:product, :name => "RoR Mug") }
-  let!(:payment_method) { create(:check_payment_method) }
-  let!(:zone) { create(:zone) }
+  include_context 'checkout setup'
 
   context "visitor makes checkout as guest without registration" do
     before(:each) do
