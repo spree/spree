fc2c0957e89fe3ac81bfda6420d869287df8a688
Alberto Vena <kennyadsl@gmail.com>
2014-10-26 09:32:35 +0100

    add table name for order/line_item association fixes #5531

--- core/app/models/spree/order.rb
+++ core/app/models/spree/order.rb
@@ -37,7 +37,7 @@ module Spree
     alias_attribute :ship_total, :shipment_total
 
     has_many :state_changes, as: :stateful
-    has_many :line_items, -> { order('created_at ASC') }, dependent: :destroy, inverse_of: :order
+    has_many :line_items, -> { order("#{LineItem.table_name}.created_at ASC") }, dependent: :destroy, inverse_of: :order
     has_many :payments, dependent: :destroy
     has_many :return_authorizations, dependent: :destroy, inverse_of: :order
     has_many :adjustments, -> { order("#{Adjustment.table_name}.created_at ASC") }, as: :adjustable, dependent: :destroy
