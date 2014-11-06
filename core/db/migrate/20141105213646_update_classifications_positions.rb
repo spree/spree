3b64ed60cbd8f7095dabb053e251c78e526ea2f1
Hnat Kubov <hnatt88@gmail.com>
2014-11-06 13:59:34 +0200

    Fix migration

--- core/db/migrate/20141105213646_update_classifications_positions.rb
+++ core/db/migrate/20141105213646_update_classifications_positions.rb
@@ -1,8 +1,8 @@
 class UpdateClassificationsPositions < ActiveRecord::Migration
   def up
-    Spree::Taxon.each do |taxon|
+    Spree::Taxon.all.each do |taxon|
       taxon.classifications.each_with_index do |c12n, i|
-        c12n.set_list_position(i)
+        c12n.set_list_position(i + 1)
       end
     end
   end
