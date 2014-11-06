35316df89e777b4ed2bdcc81f062b19367d0f630
gflateman <gflateman@gmail.com>
2014-11-06 11:51:51 -0500

    Fixes typo, handles adjustable order without order_id
    fixes #5582

--- core/db/migrate/20141101231208_fix_adjustment_order_presence.rb
+++ core/db/migrate/20141101231208_fix_adjustment_order_presence.rb
@@ -2,7 +2,12 @@ class FixAdjustmentOrderPresence < ActiveRecord::Migration
   def change
     say 'Fixing adjustments without direct order reference'
     Spree::Adjustment.where(order: nil).find_each do |adjustment|
-      adjustment.udpate_attributes!(adjustable: adjustment.adjustable.order)
+      adjustable = adjustment.adjustable
+      if adjustable.is_a? Spree::Order
+        adjustment.update_attributes!(order_id: adjustable.id)
+      else
+        adjustment.update_attributes!(adjustable: adjustable.order)
+      end
     end
   end
 end
