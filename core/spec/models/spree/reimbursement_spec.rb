d2fc41a2bc558ac5edb1c594796fdf4737194769
Peter Berkenbosch <peter@pero-ict.nl>
2014-11-06 12:12:22 +0100

    fix failing spec from #5575, it's sending an id, not an object.

--- core/spec/models/spree/reimbursement_spec.rb
+++ core/spec/models/spree/reimbursement_spec.rb
@@ -143,7 +143,7 @@ describe Spree::Reimbursement, :type => :model do
     end
 
     it "triggers the reimbursement mailer to be sent" do
-      expect(Spree::ReimbursementMailer).to receive(:reimbursement_email).with(reimbursement) { double(deliver: true) }
+      expect(Spree::ReimbursementMailer).to receive(:reimbursement_email).with(reimbursement.id) { double(deliver: true) }
       subject
     end
 
