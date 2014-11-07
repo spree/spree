spree/spree

commit 5aae351b5325674865661c4f1da55b36b6603c6f

Dont transition to complete if customer has default credit card. Fixes #5327

backend/app/controllers/spree/admin/orders/customer_details_controller.rb
@@ -23,8 +23,7 @@ module Spree
             if params[:guest_checkout] == "false"
               @order.associate_user!(Spree.user_class.find(params[:user_id]), @order.email.blank?)
             end
-            while @order.next; end
-
+            @order.next!
             @order.refresh_shipment_rates
             flash[:success] = Spree.t('customer_details_updated')
             redirect_to edit_admin_order_url(@order)

backend/spec/features/admin/orders/new_order_spec.rb
@@ -4,7 +4,7 @@ describe "New Order", :type => :feature do
   let!(:stock_location) { create(:stock_location_with_items) }
   let!(:product) { create(:product) }
   let!(:state) { create(:state) }
-  let!(:user) { create(:user) }
+  let!(:user) { create(:user, ship_address: create(:address), bill_address: create(:address)) }
   let!(:payment_method) { create(:check_payment_method) }
   let!(:shipping_method) { create(:shipping_method) }
   let!(:stock_item) { product.master.stock_items.first.adjust_count_on_hand(10) }
@@ -23,13 +23,13 @@ describe "New Order", :type => :feature do
     expect(current_path).to eql(spree.edit_admin_order_customer_path(Spree::Order.last))
   end
 
-  it "completes new order succesfully withous using the cart", js: true do
-    select2_search product.name, :from => Spree.t(:name_or_sku)
+  it "completes new order succesfully without using the cart", js: true do
+    select2_search product.name, from: Spree.t(:name_or_sku)
     click_icon :plus
     click_on "Customer Details"
 
     within "#select-customer" do
-      targetted_select2_search user.email, :from => "#s2id_customer_search"
+      targetted_select2_search user.email, from: "#s2id_customer_search"
     end
 
     check "order_use_billing"
@@ -51,10 +51,10 @@ describe "New Order", :type => :feature do
 
   context "adding new item to the order", js: true do
     it "inventory items show up just fine and are also registered as shipments" do
-      select2_search product.name, :from => Spree.t(:name_or_sku)
+      select2_search product.name, from: Spree.t(:name_or_sku)
 
       within("table.stock-levels") do
-        fill_in "variant_quantity", :with => 2
+        fill_in "variant_quantity", with: 2
         click_icon :plus
       end
 
@@ -65,7 +65,7 @@ describe "New Order", :type => :feature do
       click_on "Customer Details"
 
       within "#select-customer" do
-        targetted_select2_search user.email, :from => "#s2id_customer_search"
+        targetted_select2_search user.email, from: "#s2id_customer_search"
       end
 
       check "order_use_billing"
@@ -83,11 +83,11 @@ describe "New Order", :type => :feature do
   # Regression test for #3958
   context "without a delivery step", js: true do
     before do
-      allow(Spree::Order).to receive_messages :checkout_step_names => [:address, :payment, :confirm, :complete]
+      allow(Spree::Order).to receive_messages checkout_step_names: [:address, :payment, :confirm, :complete]
     end
 
     it "can still see line items" do
-      select2_search product.name, :from => Spree.t(:name_or_sku)
+      select2_search product.name, from: Spree.t(:name_or_sku)
       click_icon :plus
       within(".line-items") do
         within(".line-item-name") do
@@ -109,7 +109,7 @@ describe "New Order", :type => :feature do
       click_on "Customer Details"
 
       within "#select-customer" do
-        targetted_select2_search user.email, :from => "#s2id_customer_search"
+        targetted_select2_search user.email, from: "#s2id_customer_search"
       end
 
       check "order_use_billing"
@@ -117,7 +117,7 @@ describe "New Order", :type => :feature do
       click_on "Update"
 
       click_on "Shipments"
-      select2_search product.name, :from => Spree.t(:name_or_sku)
+      select2_search product.name, from: Spree.t(:name_or_sku)
       click_icon :plus
       wait_for_ajax
 
@@ -130,14 +130,35 @@ describe "New Order", :type => :feature do
     end
   end
 
+  # Regression test for #5327
+  context "customer with default credit card", js: true do
+    before do
+      create(:credit_card, default: true, user: user)
+    end
+    it "transitions to delivery not to complete" do
+      click_link "Orders"
+      click_link "New Order"
+      select2_search product.name, from: Spree.t(:name_or_sku)
+      within("table.stock-levels") do
+        fill_in "variant_quantity", with: 1
+        click_icon :plus
+      end
+      wait_for_ajax
+      click_link "Customer Details"
+      targetted_select2 user.email, from: "#s2id_customer_search"
+      click_button "Update"
+      expect(Spree::Order.last.state).to eq 'delivery'
+    end
+  end
+
   def fill_in_address(kind = "bill")
-    fill_in "First Name",              :with => "John 99"
-    fill_in "Last Name",               :with => "Doe"
-    fill_in "Street Address",          :with => "100 first lane"
-    fill_in "Street Address (cont'd)", :with => "#101"
-    fill_in "City",                    :with => "Bethesda"
-    fill_in "Zip",                     :with => "20170"
-    targetted_select2_search state.name, :from => "#s2id_order_#{kind}_address_attributes_state_id"
-    fill_in "Phone",                   :with => "123-456-7890"
+    fill_in "First Name",                with: "John 99"
+    fill_in "Last Name",                 with: "Doe"
+    fill_in "Street Address",            with: "100 first lane"
+    fill_in "Street Address (cont'd)",   with: "#101"
+    fill_in "City",                      with: "Bethesda"
+    fill_in "Zip",                       with: "20170"
+    targetted_select2_search state.name, from: "#s2id_order_#{kind}_address_attributes_state_id"
+    fill_in "Phone",                     with: "123-456-7890"
   end
 end

commit f8a9d127695305d177196e52729268b05658aeba

Fix specs broken by 5aae351b5325674865661c4f1da55b36b6603c6f

backend/app/controllers/spree/admin/orders/customer_details_controller.rb
@@ -23,7 +23,7 @@ module Spree
             if params[:guest_checkout] == "false"
               @order.associate_user!(Spree.user_class.find(params[:user_id]), @order.email.blank?)
             end
-            @order.next!
+            @order.next
             @order.refresh_shipment_rates
             flash[:success] = Spree.t('customer_details_updated')
             redirect_to edit_admin_order_url(@order)

core/lib/spree/testing_support/factories/product_factory.rb
@@ -22,6 +22,12 @@ FactoryGirl.define do
     factory :product do
       tax_category { |r| Spree::TaxCategory.first || r.association(:tax_category) }
 
+      factory :product_in_stock do
+        after :create do |product|
+          product.master.stock_items.first.adjust_count_on_hand(10)
+        end
+      end
+
       factory :product_with_option_types do
         after(:create) { |product| create(:product_option_type, product: product) }
       end

backend/spec/features/admin/orders/customer_details_spec.rb
@@ -1,12 +1,13 @@
 require 'spec_helper'
 
-describe "Customer Details", type: :feature do
+describe "Customer Details", type: :feature, js: true do
   stub_authorization!
 
   let(:country) { create(:country, name: "Kangaland") }
   let(:state) { create(:state, name: "Alabama", country: country) }
   let!(:shipping_method) { create(:shipping_method, display_on: "front_end") }
   let!(:order) { create(:order, state: 'complete', completed_at: "2011-02-01 12:36:15") }
+  let!(:product) { create(:product_in_stock) }
 
   # We need a unique name that will appear for the customer dropdown
   let!(:ship_address) { create(:address, country: country, state: state, first_name: "Rumpelstiltskin") }
@@ -14,22 +15,18 @@ describe "Customer Details", type: :feature do
 
   let!(:user) { create(:user, email: 'foobar@example.com', ship_address: ship_address, bill_address: bill_address) }
 
-  before do
-    configure_spree_preferences do |config|
-      config.default_country_id = country.id
-      config.company = true
-    end
-
-    visit spree.admin_path
-    click_link "Orders"
-    within('table#listing_orders') { click_icon(:edit) }
-  end
-
-  # Regression test for #3335 & #5317
-  context "brand new order", js: true do
+  context "brand new order" do
+    # Regression test for #3335 & #5317
     it "associates a user when not using guest checkout" do
+      visit spree.admin_path
       click_link "Orders"
       click_link "New Order"
+      select2_search product.name, from: Spree.t(:name_or_sku)
+      within("table.stock-levels") do
+        fill_in "variant_quantity", with: 1
+        click_icon :plus
+      end
+      wait_for_ajax
       click_link "Customer Details"
       targetted_select2 "foobar@example.com", from: "#s2id_customer_search"
       # 5317 - Address prefills using user's default.
@@ -47,7 +44,18 @@ describe "Customer Details", type: :feature do
     end
   end
 
-  context "editing an order", js: true do
+  context "editing an order" do
+    before do
+      configure_spree_preferences do |config|
+        config.default_country_id = country.id
+        config.company = true
+      end
+
+      visit spree.admin_path
+      click_link "Orders"
+      within('table#listing_orders') { click_icon(:edit) }
+    end
+
     context "selected country has no state" do
       before { create(:country, iso: "BRA", name: "Brazil") }
 
@@ -82,6 +90,12 @@ describe "Customer Details", type: :feature do
       end
     end
 
+    it "should show validation errors" do
+      click_link "Customer Details"
+      click_button "Update"
+      expect(page).to have_content("Shipping address first name can't be blank")
+    end
+
     it "updates order email for an existing order with a user" do
       order.update_columns(ship_address_id: ship_address.id, bill_address_id: bill_address.id, state: "confirm", completed_at: nil)
       previous_user = order.user
@@ -91,50 +105,44 @@ describe "Customer Details", type: :feature do
       expect(order.user_id).to eq previous_user.id
       expect(order.user.email).to eq previous_user.email
     end
-  end
 
-  context "country associated was removed" do
-    let(:brazil) { create(:country, iso: "BRA", name: "Brazil") }
+    context "country associated was removed" do
+      let(:brazil) { create(:country, iso: "BRA", name: "Brazil") }
 
-    before do
-      order.bill_address.country.destroy
-      configure_spree_preferences do |config|
-        config.default_country_id = brazil.id
+      before do
+        order.bill_address.country.destroy
+        configure_spree_preferences do |config|
+          config.default_country_id = brazil.id
+        end
+      end
+
+      it "sets default country when displaying form" do
+        click_link "Customer Details"
+        expect(find_field("order_bill_address_attributes_country_id").value.to_i).to eq brazil.id
       end
     end
 
-    it "sets default country when displaying form" do
-      click_link "Customer Details"
-      expect(find_field("order_bill_address_attributes_country_id").value.to_i).to eq brazil.id
-    end
-  end
+    # Regression test for #942
+    context "errors when no shipping methods are available" do
+      before do
+        Spree::ShippingMethod.delete_all
+      end
 
-  it "should show validation errors" do
-    click_link "Customer Details"
-    click_button "Update"
-    expect(page).to have_content("Shipping address first name can't be blank")
-  end
+      specify do
+        click_link "Customer Details"
+        # Need to fill in valid information so it passes validations
+        fill_in "order_ship_address_attributes_firstname",  with: "John 99"
+        fill_in "order_ship_address_attributes_lastname",   with: "Doe"
+        fill_in "order_ship_address_attributes_lastname",   with: "Company"
+        fill_in "order_ship_address_attributes_address1",   with: "100 first lane"
+        fill_in "order_ship_address_attributes_address2",   with: "#101"
+        fill_in "order_ship_address_attributes_city",       with: "Bethesda"
+        fill_in "order_ship_address_attributes_zipcode",    with: "20170"
 
-  # Regression test for #942
-  context "errors when no shipping methods are available" do
-    before do
-      Spree::ShippingMethod.delete_all
-    end
-
-    specify do
-      click_link "Customer Details"
-      # Need to fill in valid information so it passes validations
-      fill_in "order_ship_address_attributes_firstname",  with: "John 99"
-      fill_in "order_ship_address_attributes_lastname",   with: "Doe"
-      fill_in "order_ship_address_attributes_lastname",   with: "Company"
-      fill_in "order_ship_address_attributes_address1",   with: "100 first lane"
-      fill_in "order_ship_address_attributes_address2",   with: "#101"
-      fill_in "order_ship_address_attributes_city",       with: "Bethesda"
-      fill_in "order_ship_address_attributes_zipcode",    with: "20170"
-
-      page.select('Alabama', from: 'order_ship_address_attributes_state_id')
-      fill_in "order_ship_address_attributes_phone", with: "123-456-7890"
-      expect { click_button "Update" }.not_to raise_error
+        page.select('Alabama', from: 'order_ship_address_attributes_state_id')
+        fill_in "order_ship_address_attributes_phone", with: "123-456-7890"
+        expect { click_button "Update" }.not_to raise_error
+      end
     end
   end
 

backend/spec/features/admin/orders/new_order_spec.rb
@@ -1,13 +1,11 @@
 require 'spec_helper'
 
 describe "New Order", :type => :feature do
-  let!(:stock_location) { create(:stock_location_with_items) }
-  let!(:product) { create(:product) }
+  let!(:product) { create(:product_in_stock) }
   let!(:state) { create(:state) }
   let!(:user) { create(:user, ship_address: create(:address), bill_address: create(:address)) }
   let!(:payment_method) { create(:check_payment_method) }
   let!(:shipping_method) { create(:shipping_method) }
-  let!(:stock_item) { product.master.stock_items.first.adjust_count_on_hand(10) }
 
   stub_authorization!
 