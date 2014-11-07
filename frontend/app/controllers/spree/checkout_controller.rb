78b02d3b0cd64dd80d8f780131f786a31719e16f
Dan Kubb <dan.kubb@gmail.com>
2014-11-07 09:56:36 -0800

    Change before_filter to before_action. Fixes #6367

--- frontend/app/controllers/spree/checkout_controller.rb
+++ frontend/app/controllers/spree/checkout_controller.rb
@@ -7,8 +7,8 @@ module Spree
     ssl_required
 
     before_action :load_order_with_lock
-    before_filter :ensure_valid_state_lock_version, only: [:update]
-    before_filter :set_state_if_present
+    before_action :ensure_valid_state_lock_version, only: [:update]
+    before_action :set_state_if_present
 
     before_filter :ensure_order_not_completed
     before_filter :ensure_checkout_allowed
