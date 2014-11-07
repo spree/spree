1ddc4c5a58ce2e43e9dfcad8e5a5b0a372ad1dac
Jeff Dutil <JDutil@BurlingtonWebApps.com>
2014-11-07 01:35:07 -0500

    Fix specs & add regression spec for #5317
    Conflicts:
    	backend/spec/features/admin/orders/customer_details_spec.rb

--- backend/app/assets/javascripts/spree/backend/checkouts/edit.js
+++ backend/app/assets/javascripts/spree/backend/checkouts/edit.js
@@ -28,26 +28,28 @@ $(document).ready(function() {
       dropdownCssClass: 'customer_search',
       formatResult: formatCustomerResult,
       formatSelection: function (customer) {
-        var billAddress = customer.bill_address;
         $('#order_email').val(customer.email);
         $('#user_id').val(customer.id);
         $('#guest_checkout_true').prop("checked", false);
         $('#guest_checkout_false').prop("checked", true);
         $('#guest_checkout_false').prop("disabled", false);
-        $('#order_bill_address_attributes_firstname').val(billAddress.firstname);
-        $('#order_bill_address_attributes_lastname').val(billAddress.lastname);
-        $('#order_bill_address_attributes_address1').val(billAddress.address1);
-        $('#order_bill_address_attributes_address2').val(billAddress.address2);
-        $('#order_bill_address_attributes_city').val(billAddress.city);
-        $('#order_bill_address_attributes_zipcode').val(billAddress.zipcode);
-        $('#order_bill_address_attributes_phone').val(billAddress.phone);
 
-        $('#order_bill_address_attributes_country_id').select2("val", billAddress.country_id).promise().done(function () {
+        var billAddress = customer.bill_address;
+        if(billAddress) {
+          $('#order_bill_address_attributes_firstname').val(billAddress.firstname);
+          $('#order_bill_address_attributes_lastname').val(billAddress.lastname);
+          $('#order_bill_address_attributes_address1').val(billAddress.address1);
+          $('#order_bill_address_attributes_address2').val(billAddress.address2);
+          $('#order_bill_address_attributes_city').val(billAddress.city);
+          $('#order_bill_address_attributes_zipcode').val(billAddress.zipcode);
+          $('#order_bill_address_attributes_phone').val(billAddress.phone);
+
+          $('#order_bill_address_attributes_country_id').select2("val", billAddress.country_id).promise().done(function () {
             update_state('b', function () {
-                $('#order_bill_address_attributes_state_id').select2("val", billAddress.state_id);
+              $('#order_bill_address_attributes_state_id').select2("val", billAddress.state_id);
             });
-        });
-
+          });
+        }
         return customer.email;
       }
     })
