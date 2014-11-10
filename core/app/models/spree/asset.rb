5a01fae498a610abc2d5372e857ec1f518ccfa05
Martin Tomov <mtomov@outlook.com>
2014-11-10 15:58:31 +0000

    Fix Asset position for different viewables. fixes #5590

--- core/app/models/spree/asset.rb
+++ core/app/models/spree/asset.rb
@@ -1,6 +1,6 @@
 module Spree
   class Asset < ActiveRecord::Base
     belongs_to :viewable, polymorphic: true, touch: true
-    acts_as_list scope: :viewable
+    acts_as_list scope: [:viewable_id, :viewable_type]
   end
 end
--- core/spec/models/spree/asset_spec.rb
+++ core/spec/models/spree/asset_spec.rb
@@ -13,4 +13,15 @@ describe Spree::Asset do
       end.to change { product.reload.updated_at }
     end
   end
+
+  describe "#acts_as_list scope" do
+    it "should start from first position for different viewables" do
+      asset1 = Spree::Asset.create(viewable_type: 'Spree::Image', viewable_id: 1)
+      asset2 = Spree::Asset.create(viewable_type: 'Spree::LineItem', viewable_id: 1)
+
+      expect(asset1.position).to eq 1
+      expect(asset2.position).to eq 1
+    end
+  end
+
 end
