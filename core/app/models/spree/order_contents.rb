spree/spree

commit 54a5e4851b2b54750cf4a17d88a6566b1b255dac

Remove n+1 line item lookup. Fixes #5481

core/app/models/spree/order_contents.rb
@@ -47,9 +47,12 @@ module Spree
         filtered_params = params.symbolize_keys
         return filtered_params if filtered_params[:line_items_attributes].nil? || filtered_params[:line_items_attributes][:id]
 
+        line_item_ids = order.line_items.pluck(:id)
+
         params[:line_items_attributes].each_pair do |id, value|
-          line_item_id = value[:id]
-          filtered_params[:line_items_attributes].delete(id) unless Spree::LineItem.find_by_id(line_item_id.to_i)
+          unless line_item_ids.include?(value[:id].to_i)
+            filtered_params[:line_items_attributes].delete(id)
+          end
         end
         filtered_params
       end

commit 4680678f1e8cb39de388a277957dfc15955cc69f

Fix spec broken by 54a5e4851b2b54750cf4a17d88a6566b1b255dac

core/app/models/spree/order_contents.rb
@@ -50,7 +50,7 @@ module Spree
         line_item_ids = order.line_items.pluck(:id)
 
         params[:line_items_attributes].each_pair do |id, value|
-          unless line_item_ids.include?(value[:id].to_i)
+          unless line_item_ids.include?(value[:id].to_i) || value[:variant_id].present?
             filtered_params[:line_items_attributes].delete(id)
           end
         end

api/spec/controllers/spree/api/orders_controller_spec.rb
@@ -350,8 +350,7 @@ module Spree
                                  :country_id => Country.first.id, :state_id => State.first.id} }
 
       before do
-        allow(Spree::LineItem).to receive(:find_by_id).and_return(Spree::LineItem.new)
-        allow_any_instance_of(Order).to receive_messages :user => current_api_user
+        allow_any_instance_of(Order).to receive_messages user: current_api_user
         order.next # Switch from cart to address
         order.bill_address = nil
         order.ship_address = nil