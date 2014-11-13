38036a95799659d1ca1897ce9c0a69cd50c9f3d9
Jeff Dutil <JDutil@BurlingtonWebApps.com>
2014-11-13 12:02:56 -0500

    Fix product specs.

--- core/app/models/spree/variant.rb
+++ core/app/models/spree/variant.rb
@@ -23,6 +23,7 @@ module Spree
     has_one :default_price,
       -> { where currency: Spree::Config[:currency] },
       class_name: 'Spree::Price',
+      dependent: :destroy,
       inverse_of: :variant
 
     delegate_belongs_to :default_price, :display_price, :display_amount, :price, :price=, :currency
