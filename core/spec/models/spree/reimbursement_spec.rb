1ace19115eaa15c3d1b7e49118d298def34fc7aa
Peter Berkenbosch <peter@pero-ict.nl>
2014-11-18 10:07:20 -0500

    rename the IncompleteReimbursement to be extactly what it is.

--- core/spec/models/spree/reimbursement_spec.rb
+++ core/spec/models/spree/reimbursement_spec.rb
@@ -129,7 +129,7 @@ describe Spree::Reimbursement, :type => :model do
       let!(:non_return_refund) { create(:refund, amount: 1, payment: payment) }
 
       it 'raises IncompleteReimbursement error' do
-        expect { subject }.to raise_error(Spree::Reimbursement::IncompleteReimbursement)
+        expect { subject }.to raise_error(Spree::Reimbursement::IncompleteReimbursementError)
       end
     end
 
