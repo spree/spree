ed4109ea0ec948f80024184bf4604162aedc2387
Dan Kubb <dan.kubb@gmail.com>
2014-11-18 15:29:38 -0800

    Fix code formatting

--- core/spec/models/spree/order/updating_spec.rb
+++ core/spec/models/spree/order/updating_spec.rb
@@ -9,6 +9,7 @@ describe Spree::Order, :type => :model do
     context "when there are update hooks" do
       before { Spree::Order.register_update_hook :foo }
       after { Spree::Order.update_hooks.clear }
+
       it "should call each of the update hooks" do
         expect(order).to receive :foo
         order.update!
