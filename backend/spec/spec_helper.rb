73a215e474947c0f26882e8099d688b999c360e0
Willian van der Velde <mail@willian.io>
2014-11-19 11:34:37 -0500

    (tmp) override for success message assertion

--- backend/spec/spec_helper.rb
+++ backend/spec/spec_helper.rb
@@ -116,3 +116,20 @@ RSpec.configure do |config|
 
   config.fail_fast = ENV['FAIL_FAST'] || false
 end
+
+
+ # REIN: Check if these overrides can be removed
+
+ module Spree
+   module TestingSupport
+     module Flash
+       def assert_flash_success(flash)
+         flash = convert_flash(flash)
+
+         within(".alert-success") do
+           expect(page).to have_content(flash)
+         end
+       end
+     end
+   end
+ end
