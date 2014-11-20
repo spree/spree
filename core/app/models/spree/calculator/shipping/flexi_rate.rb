9fb2567728a269e82bc7db770b3d1b2c466c167f
Doug Johnston <doug@ricksteves.com>
2014-11-20 15:12:28 -0800

    Improve readability in FlexiRate calculator labels

--- core/app/models/spree/calculator/shipping/flexi_rate.rb
+++ core/app/models/spree/calculator/shipping/flexi_rate.rb
@@ -3,10 +3,10 @@ require_dependency 'spree/shipping_calculator'
 module Spree
   module Calculator::Shipping
     class FlexiRate < ShippingCalculator
-      preference :first_item,      :decimal, default: 0.0
-      preference :additional_item, :decimal, default: 0.0
-      preference :max_items,       :integer, default: 0
-      preference :currency,        :string,  default: ->{ Spree::Config[:currency] }
+      preference :first_item_cost,      :decimal, default: 0.0
+      preference :additional_item_cost, :decimal, default: 0.0
+      preference :max_items,            :integer, default: 0
+      preference :currency,             :string,  default: ->{ Spree::Config[:currency] }
 
       def self.description
         Spree.t(:shipping_flexible_rate)
@@ -22,9 +22,9 @@ module Spree
         quantity.times do |i|
           # check max value to avoid divide by 0 errors
           if (max == 0 && i == 0) || (max > 0) && (i % max == 0)
-            sum += self.preferred_first_item.to_f
+            sum += self.preferred_first_item_cost.to_f
           else
-            sum += self.preferred_additional_item.to_f
+            sum += self.preferred_additional_item_cost.to_f
           end
         end
 
