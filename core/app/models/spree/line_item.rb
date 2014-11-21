a854901a5a8cf214e11c8a20655ac3aed4e40cbd
Darby <darby.perez@gmail.com>
2014-11-21 16:26:28 -0600

    Adds option parameter to line_items api controller
    resolves #5655
    
    LineItemsController class_attribute
    prevent update on empty options
    
    fixes #5672

--- core/app/models/spree/line_item.rb
+++ core/app/models/spree/line_item.rb
@@ -101,6 +101,8 @@ module Spree
     end
 
     def options=(options={})
+      return unless options.present?
+
       opts = options.dup # we will be deleting from the hash, so leave the caller's copy intact
 
       currency = opts.delete(:currency) || order.try(:currency)
