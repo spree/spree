953b11e95ec6880a5cff3eaa84b19d2b6930d431
Jeff Dutil <JDutil@BurlingtonWebApps.com>
2014-11-25 10:10:35 -0500

    Scope option value uniqueness to type. refs #5630

--- core/app/models/spree/option_value.rb
+++ core/app/models/spree/option_value.rb
@@ -4,7 +4,7 @@ module Spree
     acts_as_list scope: :option_type
     has_and_belongs_to_many :variants, join_table: 'spree_option_values_variants', class_name: "Spree::Variant"
 
-    validates :name, presence: true, uniqueness: true
+    validates :name, presence: true, uniqueness: { scope: :option_type_id }
     validates :presentation, presence: true
 
     after_touch :touch_all_variants
