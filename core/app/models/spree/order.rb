4bad17d8f2c8b4cbe268370340f7c246a2f74f3f
darbs <darby.perez@gmail.com>
2014-11-28 10:48:55 -0600

    seriously hound...

--- core/app/models/spree/order.rb
+++ core/app/models/spree/order.rb
@@ -13,9 +13,9 @@ module Spree
     include Spree::Order::Payments
 
     extend Spree::DisplayMoney
-    money_methods :outstanding_balance, :item_total, :adjustment_total,
-      :included_tax_total, :additional_tax_total, :tax_total,
-      :shipment_total, :promo_total, :total
+    money_methods :outstanding_balance, :item_total,           :adjustment_total,
+                  :included_tax_total,  :additional_tax_total, :tax_total,
+                  :shipment_total,      :promo_total,          :total
 
     alias :display_ship_total :display_shipment_total
 
