ee97e7d5cea0f67130b52bac26ac9cc47484e955
Markus Schirp <mbj@schirp-dso.com>
2014-12-02 15:31:49 -0800

    Fix code style to prepare change

--- core/spec/models/spree/order_contents_spec.rb
+++ core/spec/models/spree/order_contents_spec.rb
@@ -1,12 +1,12 @@
 require 'spec_helper'
 
-describe Spree::OrderContents, :type => :model do
+describe Spree::OrderContents, type: :model do
   let(:order) { Spree::Order.create }
   let(:variant) { create(:variant) }
 
   subject { described_class.new(order) }
 
-  context "#add" do
+  context '#add' do
     context 'given quantity is not explicitly provided' do
       it 'should add one line item' do
         line_item = subject.add(variant)
@@ -16,7 +16,7 @@ describe Spree::OrderContents, :type => :model do
     end
 
     context 'given a shipment' do
-      it "ensure shipment calls update_amounts instead of order calling ensure_updated_shipments" do
+      it 'ensure shipment calls update_amounts instead of order calling ensure_updated_shipments' do
         shipment = create(:shipment)
         expect(subject.order).to_not receive(:ensure_updated_shipments)
         expect(shipment).to receive(:update_amounts)
@@ -25,7 +25,7 @@ describe Spree::OrderContents, :type => :model do
     end
 
     context 'not given a shipment' do
-      it "ensures updated shipments" do
+      it 'ensures updated shipments' do
         expect(subject.order).to receive(:ensure_updated_shipments)
         subject.add(variant)
       end
@@ -44,7 +44,7 @@ describe Spree::OrderContents, :type => :model do
       expect(order.line_items.size).to eq(1)
     end
 
-    it "should update order totals" do
+    it 'should update order totals' do
       expect(order.item_total.to_f).to eq(0.00)
       expect(order.total.to_f).to eq(0.00)
 
@@ -54,42 +54,42 @@ describe Spree::OrderContents, :type => :model do
       expect(order.total.to_f).to eq(19.99)
     end
 
-    context "running promotions" do
+    context 'running promotions' do
       let(:promotion) { create(:promotion) }
-      let(:calculator) { Spree::Calculator::FlatRate.new(:preferred_amount => 10) }
+      let(:calculator) { Spree::Calculator::FlatRate.new(preferred_amount: 10) }
 
-      shared_context "discount changes order total" do
+      shared_context 'discount changes order total' do
         before { subject.add(variant, 1) }
         it { expect(subject.order.total).not_to eq variant.price }
       end
 
-      context "one active order promotion" do
+      context 'one active order promotion' do
         let!(:action) { Spree::Promotion::Actions::CreateAdjustment.create(promotion: promotion, calculator: calculator) }
 
-        it "creates valid discount on order" do
+        it 'creates valid discount on order' do
           subject.add(variant, 1)
           expect(subject.order.adjustments.to_a.sum(&:amount)).not_to eq 0
         end
 
-        include_context "discount changes order total"
+        include_context 'discount changes order total'
       end
 
-      context "one active line item promotion" do
+      context 'one active line item promotion' do
         let!(:action) { Spree::Promotion::Actions::CreateItemAdjustments.create(promotion: promotion, calculator: calculator) }
 
-        it "creates valid discount on order" do
+        it 'creates valid discount on order' do
           subject.add(variant, 1)
           expect(subject.order.line_item_adjustments.to_a.sum(&:amount)).not_to eq 0
         end
 
-        include_context "discount changes order total"
+        include_context 'discount changes order total'
       end
     end
   end
 
-  context "#remove" do
-    context "given an invalid variant" do
-      it "raises an exception" do
+  context '#remove' do
+    context 'given an invalid variant' do
+      it 'raises an exception' do
         expect {
           subject.remove(variant, 1)
         }.to raise_error(ActiveRecord::RecordNotFound)
@@ -106,7 +106,7 @@ describe Spree::OrderContents, :type => :model do
     end
 
     context 'given a shipment' do
-      it "ensure shipment calls update_amounts instead of order calling ensure_updated_shipments" do
+      it 'ensure shipment calls update_amounts instead of order calling ensure_updated_shipments' do
         line_item = subject.add(variant, 1)
         shipment = create(:shipment)
         expect(subject.order).to_not receive(:ensure_updated_shipments)
@@ -116,7 +116,7 @@ describe Spree::OrderContents, :type => :model do
     end
 
     context 'not given a shipment' do
-      it "ensures updated shipments" do
+      it 'ensures updated shipments' do
         line_item = subject.add(variant, 1)
         expect(subject.order).to receive(:ensure_updated_shipments)
         subject.remove(variant)
@@ -137,7 +137,7 @@ describe Spree::OrderContents, :type => :model do
       expect(order.reload.find_line_item_by_variant(variant)).to be_nil
     end
 
-    it "should update order totals" do
+    it 'should update order totals' do
       expect(order.item_total.to_f).to eq(0.00)
       expect(order.total.to_f).to eq(0.00)
 
@@ -152,52 +152,52 @@ describe Spree::OrderContents, :type => :model do
     end
   end
 
-  context "update cart" do
+  context 'update cart' do
     let!(:shirt) { subject.add variant, 1 }
 
     let(:params) do
       { line_items_attributes: {
-        "0" => { id: shirt.id, quantity: 3 }
+        '0' => { id: shirt.id, quantity: 3 }
       } }
     end
 
-    it "changes item quantity" do
+    it 'changes item quantity' do
       subject.update_cart params
       expect(shirt.reload.quantity).to eq 3
     end
 
-    it "updates order totals" do
+    it 'updates order totals' do
       expect {
         subject.update_cart params
       }.to change { subject.order.total }
     end
 
-    context "submits item quantity 0" do
+    context 'submits item quantity 0' do
       let(:params) do
         { line_items_attributes: {
-          "0" => { id: shirt.id, quantity: 0 }
+          '0' => { id: shirt.id, quantity: 0 }
         } }
       end
 
-      it "removes item from order" do
+      it 'removes item from order' do
         expect {
           subject.update_cart params
         }.to change { subject.order.line_items.count }
       end
     end
 
-    it "ensures updated shipments" do
+    it 'ensures updated shipments' do
       expect(subject.order).to receive(:ensure_updated_shipments)
       subject.update_cart params
     end
   end
 
-  context "completed order" do
+  context 'completed order' do
     let(:order) { create(:order, state: 'complete', completed_at: Time.now) }
 
     before { order.shipments.create! stock_location_id: variant.stock_location_ids.first }
 
-    it "updates order payment state" do
+    it 'updates order payment state' do
       expect {
         subject.add variant
       }.to change { order.payment_state }
