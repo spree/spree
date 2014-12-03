46d2e3fa71d7043d4a610d02bc22162d4647fe8a
Jeff Dutil <JDutil@BurlingtonWebApps.com>
2014-12-03 14:59:26 -0500

    Add back promotion files that should still exist.

--- backend/spec/features/admin/promotions/adjustments_spec.rb
+++ backend/spec/features/admin/promotions/adjustments_spec.rb
@@ -1,34 +1,33 @@
 require 'spec_helper'
 
-describe "Promotion Adjustments", :type => :feature do
+describe "Promotion Adjustments", type: :feature, js: true do
   stub_authorization!
 
-  context "coupon promotions", :js => true do
+  context "coupon promotions" do
     before(:each) do
-      visit spree.admin_path
-      click_link "Promotions"
-      click_link "New Promotion"
+      visit spree.admin_promotions_path
+      click_on "New Promotion"
     end
 
     it "should allow an admin to create a flat rate discount coupon promo" do
-      fill_in "Name", :with => "Promotion"
-      fill_in "Code", :with => "order"
+      fill_in "Name", with: "Promotion"
+      fill_in "Code", with: "order"
       click_button "Create"
       expect(page).to have_content("Editing Promotion")
 
-      select2 "Item total", :from => "Add rule of type"
+      select2 "Item total", from: "Add rule of type"
       within('#rule_fields') { click_button "Add" }
 
       eventually_fill_in "promotion_promotion_rules_attributes_#{Spree::Promotion.count}_preferred_amount_min", :with => 30
       eventually_fill_in "promotion_promotion_rules_attributes_#{Spree::Promotion.count}_preferred_amount_max", :with => 60
       within('#rule_fields') { click_button "Update" }
 
-      select2 "Create whole-order adjustment", :from => "Add action of type"
+      select2 "Create whole-order adjustment", from: "Add action of type"
       within('#action_fields') { click_button "Add" }
-      select2 "Flat Rate", :from => "Calculator"
+      select2 "Flat Rate", from: "Calculator"
       within('#actions_container') { click_button "Update" }
 
-      within('.calculator-fields') { fill_in "Amount", :with => 5 }
+      within('.calculator-fields') { fill_in "Amount", with: 5 }
       within('#actions_container') { click_button "Update" }
 
       promotion = Spree::Promotion.find_by_name("Promotion")
@@ -47,17 +46,17 @@ describe "Promotion Adjustments", :type => :feature do
     end
 
     it "should allow an admin to create a single user coupon promo with flat rate discount" do
-      fill_in "Name", :with => "Promotion"
-      fill_in "Usage Limit", :with => "1"
-      fill_in "Code", :with => "single_use"
+      fill_in "Name", with: "Promotion"
+      fill_in "Usage Limit", with: "1"
+      fill_in "Code", with: "single_use"
       click_button "Create"
       expect(page).to have_content("Editing Promotion")
 
-      select2 "Create whole-order adjustment", :from => "Add action of type"
+      select2 "Create whole-order adjustment", from: "Add action of type"
       within('#action_fields') { click_button "Add" }
-      select2 "Flat Rate", :from => "Calculator"
+      select2 "Flat Rate", from: "Calculator"
       within('#actions_container') { click_button "Update" }
-      within('#action_fields') { fill_in "Amount", :with => "5" }
+      within('#action_fields') { fill_in "Amount", with: "5" }
       within('#actions_container') { click_button "Update" }
 
       promotion = Spree::Promotion.find_by_name("Promotion")
@@ -72,22 +71,22 @@ describe "Promotion Adjustments", :type => :feature do
     end
 
     it "should allow an admin to create an automatic promo with flat percent discount" do
-      fill_in "Name", :with => "Promotion"
+      fill_in "Name", with: "Promotion"
       click_button "Create"
       expect(page).to have_content("Editing Promotion")
 
-      select2 "Item total", :from => "Add rule of type"
+      select2 "Item total", from: "Add rule of type"
       within('#rule_fields') { click_button "Add" }
 
-      eventually_fill_in "promotion_promotion_rules_attributes_1_preferred_amount_min", :with => 30
-      eventually_fill_in "promotion_promotion_rules_attributes_1_preferred_amount_max", :with => 60
+      eventually_fill_in "promotion_promotion_rules_attributes_1_preferred_amount_min", with: 30
+      eventually_fill_in "promotion_promotion_rules_attributes_1_preferred_amount_max", with: 60
       within('#rule_fields') { click_button "Update" }
 
-      select2 "Create whole-order adjustment", :from => "Add action of type"
+      select2 "Create whole-order adjustment", from: "Add action of type"
       within('#action_fields') { click_button "Add" }
-      select2 "Flat Percent", :from => "Calculator"
+      select2 "Flat Percent", from: "Calculator"
       within('#actions_container') { click_button "Update" }
-      within('.calculator-fields') { fill_in "Flat Percent", :with => "10" }
+      within('.calculator-fields') { fill_in "Flat Percent", with: "10" }
       within('#actions_container') { click_button "Update" }
 
       promotion = Spree::Promotion.find_by_name("Promotion")
@@ -106,22 +105,22 @@ describe "Promotion Adjustments", :type => :feature do
     end
 
     it "should allow an admin to create an product promo with percent per item discount" do
-      create(:product, :name => "RoR Mug")
+      create(:product, name: "RoR Mug")
 
-      fill_in "Name", :with => "Promotion"
+      fill_in "Name", with: "Promotion"
       click_button "Create"
       expect(page).to have_content("Editing Promotion")
 
-      select2 "Product(s)", :from => "Add rule of type"
+      select2 "Product(s)", from: "Add rule of type"
       within("#rule_fields") { click_button "Add" }
-      select2_search "RoR Mug", :from => "Choose products"
+      select2_search "RoR Mug", from: "Choose products"
       within('#rule_fields') { click_button "Update" }
 
-      select2 "Create per-line-item adjustment", :from => "Add action of type"
+      select2 "Create per-line-item adjustment", from: "Add action of type"
       within('#action_fields') { click_button "Add" }
-      select2 "Percent Per Item", :from => "Calculator"
+      select2 "Percent Per Item", from: "Calculator"
       within('#actions_container') { click_button "Update" }
-      within('.calculator-fields') { fill_in "Percent", :with => "10" }
+      within('.calculator-fields') { fill_in "Percent", with: "10" }
       within('#actions_container') { click_button "Update" }
 
       promotion = Spree::Promotion.find_by_name("Promotion")
@@ -139,18 +138,18 @@ describe "Promotion Adjustments", :type => :feature do
     end
 
     xit "should allow an admin to create an automatic promotion with free shipping (no code)" do
-      fill_in "Name", :with => "Promotion"
+      fill_in "Name", with: "Promotion"
       click_button "Create"
       expect(page).to have_content("Editing Promotion")
 
-      select2 "Item total", :from => "Add rule of type"
+      select2 "Item total", from: "Add rule of type"
       within('#rule_fields') { click_button "Add" }
-      eventually_fill_in "promotion_promotion_rules_attributes_1_preferred_amount", :with => "30"
+      eventually_fill_in "promotion_promotion_rules_attributes_1_preferred_amount", with: "30"
       within('#rule_fields') { click_button "Update" }
 
-      select2 "Create whole-order adjustment", :from => "Add action of type"
+      select2 "Create whole-order adjustment", from: "Add action of type"
       within('#action_fields') { click_button "Add" }
-      select2 "Free Shipping", :from => "Calculator"
+      select2 "Free Shipping", from: "Calculator"
       within('#actions_container') { click_button "Update" }
 
       promotion = Spree::Promotion.find_by_name("Promotion")
@@ -166,16 +165,16 @@ describe "Promotion Adjustments", :type => :feature do
     end
 
     it "should allow an admin to create an automatic promo requiring a landing page to be visited" do
-      fill_in "Name", :with => "Promotion"
-      fill_in "Path", :with => "content/cvv"
+      fill_in "Name", with: "Promotion"
+      fill_in "Path", with: "content/cvv"
       click_button "Create"
       expect(page).to have_content("Editing Promotion")
 
-      select2 "Create whole-order adjustment", :from => "Add action of type"
+      select2 "Create whole-order adjustment", from: "Add action of type"
       within('#action_fields') { click_button "Add" }
-      select2 "Flat Rate", :from => "Calculator"
+      select2 "Flat Rate", from: "Calculator"
       within('#actions_container') { click_button "Update" }
-      within('.calculator-fields') { fill_in "Amount", :with => "4" }
+      within('.calculator-fields') { fill_in "Amount", with: "4" }
       within('#actions_container') { click_button "Update" }
 
       promotion = Spree::Promotion.find_by_name("Promotion")
@@ -191,13 +190,13 @@ describe "Promotion Adjustments", :type => :feature do
     end
 
     it "should allow an admin to create a promotion that adds a 'free' item to the cart" do
-      create(:product, :name => "RoR Mug")
-      fill_in "Name", :with => "Promotion"
-      fill_in "Code", :with => "complex"
+      create(:product, name: "RoR Mug")
+      fill_in "Name", with: "Promotion"
+      fill_in "Code", with: "complex"
       click_button "Create"
       expect(page).to have_content("Editing Promotion")
 
-      select2 "Create line items", :from => "Add action of type"
+      select2 "Create line items", from: "Add action of type"
 
       within('#action_fields') { click_button "Add" }
 
@@ -207,11 +206,11 @@ describe "Promotion Adjustments", :type => :feature do
 
       within('#actions_container') { click_button "Update" }
 
-      select2 "Create whole-order adjustment", :from => "Add action of type"
+      select2 "Create whole-order adjustment", from: "Add action of type"
       within('#new_promotion_action_form') { click_button "Add" }
-      select2 "Flat Rate", :from => "Calculator"
+      select2 "Flat Rate", from: "Calculator"
       within('#actions_container') { click_button "Update" }
-      within('.calculator-fields') { fill_in "Amount", :with => "40.00" }
+      within('.calculator-fields') { fill_in "Amount", with: "40.00" }
       within('#actions_container') { click_button "Update" }
 
       promotion = Spree::Promotion.find_by_name("Promotion")
@@ -223,21 +222,21 @@ describe "Promotion Adjustments", :type => :feature do
     end
 
     it "ceasing to be eligible for a promotion with item total rule then becoming eligible again" do
-      fill_in "Name", :with => "Promotion"
+      fill_in "Name", with: "Promotion"
       click_button "Create"
       expect(page).to have_content("Editing Promotion")
 
-      select2 "Item total", :from => "Add rule of type"
+      select2 "Item total", from: "Add rule of type"
       within('#rule_fields') { click_button "Add" }
-      eventually_fill_in "promotion_promotion_rules_attributes_1_preferred_amount_min", :with => "50"
-      eventually_fill_in "promotion_promotion_rules_attributes_1_preferred_amount_max", :with => "150"
+      eventually_fill_in "promotion_promotion_rules_attributes_1_preferred_amount_min", with: "50"
+      eventually_fill_in "promotion_promotion_rules_attributes_1_preferred_amount_max", with: "150"
       within('#rule_fields') { click_button "Update" }
 
-      select2 "Create whole-order adjustment", :from => "Add action of type"
+      select2 "Create whole-order adjustment", from: "Add action of type"
       within('#action_fields') { click_button "Add" }
-      select2 "Flat Rate", :from => "Calculator"
+      select2 "Flat Rate", from: "Calculator"
       within('#actions_container') { click_button "Update" }
-      within('.calculator-fields') { fill_in "Amount", :with => "5" }
+      within('.calculator-fields') { fill_in "Amount", with: "5" }
       within('#actions_container') { click_button "Update" }
 
       promotion = Spree::Promotion.find_by_name("Promotion")
