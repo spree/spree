c0e55060503e34e27a88e91c595de0ff9e5e5ceb
Jeff Dutil <JDutil@BurlingtonWebApps.com>
2014-12-03 02:09:37 -0500

    Fix users specs.

--- backend/spec/features/admin/users_spec.rb
+++ backend/spec/features/admin/users_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe 'Users', :type => :feature do
+describe 'Users', type: :feature do
   stub_authorization!
   let!(:country) { create(:country) }
   let!(:user_a) { create(:user_with_addreses, email: 'a@example.com') }
