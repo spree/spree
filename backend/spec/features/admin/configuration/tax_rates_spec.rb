3db9b6cf4df73f727f0f5021e03c0e76f1e4a809
Jeff Dutil <JDutil@BurlingtonWebApps.com>
2014-12-04 13:12:05 -0500

    Remove defunkt spec.

--- backend/spec/features/admin/configuration/tax_rates_spec.rb
+++ backend/spec/features/admin/configuration/tax_rates_spec.rb
@@ -5,22 +5,10 @@ describe "Tax Rates", type: :feature, js: true do
 
   let!(:tax_rate) { create(:tax_rate, calculator: stub_model(Spree::Calculator)) }
 
-  before do
-    visit spree.admin_path
-    click_link "Configuration"
-  end
-
-  # Regression test for #535
-  it "can see a tax rate in the list if the tax category has been deleted" do
-    tax_rate.tax_category.destroy
-    expect { click_link "Tax Rates" }.not_to raise_error
-    within(:xpath, all("table tbody td")[2].path) do
-      expect(page).to have_content("N/A")
-    end
-  end
-
   # Regression test for #1422
   it "can create a new tax rate" do
+    visit spree.admin_path
+    click_link "Configuration"
     click_link "Tax Rates"
     click_link "New Tax Rate"
     fill_in "Rate", with: "0.05"
