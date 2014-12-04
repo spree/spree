368133433ae08e7b4fc89a4932ea6680dfd195ef
Peter Longyear <plongyear@bonobos.com>
2014-12-04 14:11:40 -0500

    Better gateway error handling during refund creation

---
+++ backend/spec/controllers/spree/admin/refunds_controller_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+describe Spree::Admin::RefundsController do
+  stub_authorization!
+
+  describe "POST create" do
+    context "a Spree::Core::GatewayError is raised" do
+
+      let(:payment) { create(:payment) }
+
+      subject { spree_post :create, refund: { amount: "50.0", refund_reason_id: "1" }, payment_id: payment.id }
+
+      before(:each) do
+        def controller.create
+          raise Spree::Core::GatewayError.new('An error has occurred')
+        end
+      end
+
+      it "sets an error message with the correct text" do
+        subject
+        expect(flash[:error]).to eq 'An error has occurred'
+      end
+
+      it { should render_template(:new) }
+    end
+  end
+end
