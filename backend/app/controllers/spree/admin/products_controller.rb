6ac6c19c39896abebae87e03007a2d7c550ca636
Ben A Morgan <ben@benmorgan.io>
2014-12-05 14:47:43 -0800

    fix inconsistent indentation

--- backend/app/controllers/spree/admin/products_controller.rb
+++ backend/app/controllers/spree/admin/products_controller.rb
@@ -77,58 +77,58 @@ module Spree
 
       protected
 
-        def find_resource
-          Product.with_deleted.friendly.find(params[:id])
-        end
+      def find_resource
+        Product.with_deleted.friendly.find(params[:id])
+      end
 
-        def location_after_save
-          spree.edit_admin_product_url(@product)
-        end
+      def location_after_save
+        spree.edit_admin_product_url(@product)
+      end
 
-        def load_data
-          @taxons = Taxon.order(:name)
-          @option_types = OptionType.order(:name)
-          @tax_categories = TaxCategory.order(:name)
-          @shipping_categories = ShippingCategory.order(:name)
-        end
+      def load_data
+        @taxons = Taxon.order(:name)
+        @option_types = OptionType.order(:name)
+        @tax_categories = TaxCategory.order(:name)
+        @shipping_categories = ShippingCategory.order(:name)
+      end
 
-        def collection
-          return @collection if @collection.present?
-          params[:q] ||= {}
-          params[:q][:deleted_at_null] ||= "1"
+      def collection
+        return @collection if @collection.present?
+        params[:q] ||= {}
+        params[:q][:deleted_at_null] ||= "1"
 
-          params[:q][:s] ||= "name asc"
-          @collection = super
-          @collection = @collection.with_deleted if params[:q].delete(:deleted_at_null) == '0'
-          # @search needs to be defined as this is passed to search_form_for
-          @search = @collection.ransack(params[:q])
-          @collection = @search.result.
-                distinct_by_product_ids(params[:q][:s]).
-                includes(product_includes).
-                page(params[:page]).
-                per(Spree::Config[:admin_products_per_page])
+        params[:q][:s] ||= "name asc"
+        @collection = super
+        @collection = @collection.with_deleted if params[:q].delete(:deleted_at_null) == '0'
+        # @search needs to be defined as this is passed to search_form_for
+        @search = @collection.ransack(params[:q])
+        @collection = @search.result.
+              distinct_by_product_ids(params[:q][:s]).
+              includes(product_includes).
+              page(params[:page]).
+              per(Spree::Config[:admin_products_per_page])
 
-          @collection
-        end
+        @collection
+      end
 
-        def create_before
-          return if params[:product][:prototype_id].blank?
-          @prototype = Spree::Prototype.find(params[:product][:prototype_id])
-        end
+      def create_before
+        return if params[:product][:prototype_id].blank?
+        @prototype = Spree::Prototype.find(params[:product][:prototype_id])
+      end
 
-        def update_before
-          # note: we only reset the product properties if we're receiving a post from the form on that tab
-          return unless params[:clear_product_properties]
-          params[:product] ||= {}
-        end
+      def update_before
+        # note: we only reset the product properties if we're receiving a post from the form on that tab
+        return unless params[:clear_product_properties]
+        params[:product] ||= {}
+      end
 
-        def product_includes
-          [{ :variants => [:images], :master => [:images, :default_price]}]
-        end
+      def product_includes
+        [{ :variants => [:images], :master => [:images, :default_price]}]
+      end
 
-        def clone_object_url(resource)
-          clone_admin_product_url resource
-        end
+      def clone_object_url(resource)
+        clone_admin_product_url resource
+      end
 
       private
 
