1893133ee40742e6ace714f05fb5e6c11914f2b9
Jeff Dutil <JDutil@BurlingtonWebApps.com>
2014-12-05 17:24:31 -0500

    Remove rspec-retry.

--- backend/spec/spec_helper.rb
+++ backend/spec/spec_helper.rb
@@ -55,16 +55,6 @@ RSpec.configure do |config|
   # instead of true.
   config.use_transactional_fixtures = false
 
-  # A workaround to deal with random failure caused by phantomjs. Turn it on
-  # by setting ENV['RSPEC_RETRY_COUNT']. Limit it to features tests where
-  # phantomjs is used.
-  config.before(:all, :type => :feature) do
-    if ENV['RSPEC_RETRY_COUNT']
-      config.verbose_retry       = true # show retry status in spec process
-      config.default_retry_count = ENV['RSPEC_RETRY_COUNT'].to_i
-    end
-  end
-
   config.before :suite do
     Capybara.match = :prefer_exact
     DatabaseCleaner.clean_with :truncation
