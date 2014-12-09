2e65caf4d9ff258d26e0691d5153b625cfd3abef
Ben A Morgan <ben@benmorgan.io>
2014-12-08 17:55:38 -0800

    speed up country creation
    speed up state creation
    
    fixes #5730

--- core/db/default/spree/countries.rb
+++ core/db/default/spree/countries.rb
@@ -1,19 +1,29 @@
 require 'carmen'
 
-countries = []
-Carmen::Country.all.each do |country|
-  countries << {
-    name: country.name,
-    iso3: country.alpha_3_code,
-    iso: country.alpha_2_code,
-    iso_name: country.name.upcase,
-    numcode: country.numeric_code,
-    states_required: country.subregions?
-  }
+connection      = ActiveRecord::Base.connection
+country_inserts = []
+
+country_values = -> do
+  Carmen::Country.all.each do |country|
+    name            = connection.quote country.name
+    iso3            = connection.quote country.alpha_3_code
+    iso             = connection.quote country.alpha_2_code
+    iso_name        = connection.quote country.name.upcase
+    numcode         = connection.quote country.numeric_code
+    states_required = connection.quote country.subregions?
+
+    country_inserts << [name, iso3, iso, iso_name, numcode, states_required].join(", ")
+  end
+
+  country_inserts.join("), (")
 end
 
-ActiveRecord::Base.transaction do
-  Spree::Country.create!(countries)
-end
+columns = ["name", "iso3", "iso", "iso_name", "numcode", "states_required"]
+columns = connection.adapter_name =~ /MySQL/i ? columns.join(", ") : "\"#{columns.join('", "')}\""
+
+connection.execute <<-SQL
+  INSERT INTO spree_countries (#{columns})
+  VALUES (#{country_values.call});
+SQL
 
 Spree::Config[:default_country_id] = Spree::Country.find_by(name: "United States").id
