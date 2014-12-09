4ac65150b31f37eedf7e2a98906c8e008ec08ca6
Max White <mushishi78@gmail.com>
2014-12-09 04:34:14 +0000

    Replace #promotion_credit_exists? guards on promotion actions with validation on adjustment

--- core/app/models/spree/promotion/actions/free_shipping.rb
+++ core/app/models/spree/promotion/actions/free_shipping.rb
@@ -5,14 +5,13 @@ module Spree
         def perform(payload={})
           order = payload[:order]
           results = order.shipments.map do |shipment|
-            return false if promotion_credit_exists?(shipment)
-            shipment.adjustments.create!(
+            adjustment = shipment.adjustments.new(
               order: shipment.order, 
               amount: compute_amount(shipment),
               source: self,
               label: label,
             )
-            true
+            adjustment.save
           end
           # Did we actually end up applying any adjustments?
           # If so, then this action should be classed as 'successful'
@@ -25,12 +24,6 @@ module Spree
 
         def compute_amount(shipment)
           shipment.cost * -1
-        end
-
-        private
-
-        def promotion_credit_exists?(shipment)
-          shipment.adjustments.where(:source_id => self.id).exists?
         end
       end
     end
