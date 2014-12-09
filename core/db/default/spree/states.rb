1e27a6f9445439ce1aaeea692d206838f9ca1b80
Ben A Morgan <ben@benmorgan.io>
2014-12-08 17:56:02 -0800

    speed up state creation for postgres

--- core/db/default/spree/states.rb
+++ core/db/default/spree/states.rb
@@ -1,4 +1,29 @@
-ActiveRecord::Base.transaction do
+connection = ActiveRecord::Base.connection
+case connection.adapter_name
+when "PostgreSQL"
+  state_inserts = []
+
+  state_values = -> do
+    Spree::Country.all.each do |country|
+      carmen_country = Carmen::Country.named(country.name)
+      if carmen_country.subregions?
+        carmen_country.subregions.each do |subregion|
+          state_inserts << [subregion.name, subregion.code, country.id.to_s]
+        end
+      end
+    end
+    state_inserts.map do |state_insert|
+      state_insert.map do |state_value|
+        state_value.gsub("'", "''")
+      end.join("', '")
+    end.join("'), ('")
+  end
+
+  connection.execute <<-SQL
+    INSERT INTO spree_states ("name", "abbr", "country_id")
+    VALUES ('#{state_values.call}');
+  SQL
+else
   Spree::Country.all.each do |country|
     carmen_country = Carmen::Country.named(country.name)
     @states ||= []
