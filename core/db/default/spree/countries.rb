39280450debdf1b75f8b2cea68d7381d8b7e519b
Ben A Morgan <ben@benmorgan.io>
2014-12-10 01:24:47 -0800

    remove case statement

--- core/db/default/spree/countries.rb
+++ core/db/default/spree/countries.rb
@@ -1,41 +1,23 @@
 require 'carmen'
 
 connection = ActiveRecord::Base.connection
-case connection.adapter_name
-when "PostgreSQL"
-  country_inserts = []
-  country_values = -> do
-    Carmen::Country.all.each do |country|
-      country_inserts << [country.name, country.alpha_3_code, country.alpha_2_code,
-                          country.name.upcase, country.numeric_code, "#{country.subregions?}"]
-    end
-    country_inserts.map do |country_insert|
-      country_insert.map do |country_value|
-        country_value.gsub("'", "''")
-      end.join("', '")
-    end.join("'), ('")
-  end
+country_inserts = []
 
-  connection.execute <<-SQL
-    INSERT INTO spree_countries ("name", "iso3", "iso", "iso_name", "numcode", "states_required")
-    VALUES ('#{country_values.call}');
-  SQL
-else
-  countries = []
+country_values = -> do
   Carmen::Country.all.each do |country|
-    countries << {
-      name: country.name,
-      iso3: country.alpha_3_code,
-      iso: country.alpha_2_code,
-      iso_name: country.name.upcase,
-      numcode: country.numeric_code,
-      states_required: country.subregions?
-    }
+    country_inserts << [country.name, country.alpha_3_code, country.alpha_2_code,
+                        country.name.upcase, country.numeric_code, "#{country.subregions?}"]
   end
-
-  ActiveRecord::Base.transaction do
-    Spree::Country.create!(countries)
-  end
+  country_inserts.map do |country_insert|
+    country_insert.map do |country_value|
+      country_value.gsub("'", "''")
+    end.join("', '")
+  end.join("'), ('")
 end
+
+connection.execute <<-SQL
+  INSERT INTO spree_countries ("name", "iso3", "iso", "iso_name", "numcode", "states_required")
+  VALUES ('#{country_values.call}');
+SQL
 
 Spree::Config[:default_country_id] = Spree::Country.find_by(name: "United States").id
