0518c3efa584403a302850d81ed309e005447969
Anthony D'Addeo <anthony@personalwine.com>
2014-12-10 15:19:40 -0600

    allow no variant products to be eligible for exchange

--- core/app/models/spree/return_item/exchange_variant_eligibility/same_product.rb
+++ core/app/models/spree/return_item/exchange_variant_eligibility/same_product.rb
@@ -3,7 +3,11 @@ module Spree
     class SameProduct
 
       def self.eligible_variants(variant)
-        Spree::Variant.where(product_id: variant.product_id, is_master: false).in_stock
+        if variant.product.has_variants?
+          Spree::Variant.where(product_id: variant.product_id, is_master: false).in_stock
+        else
+          Spree::Variant.where(product_id: variant.product_id, is_master: true).in_stock
+        end
       end
     end
   end
