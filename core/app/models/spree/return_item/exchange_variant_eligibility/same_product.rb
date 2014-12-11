ff43845094fbcd60773990f45eebd71a210414c4
Anthony D'Addeo <anthony@personalwine.com>
2014-12-11 13:11:34 -0600

    prevent n+1 query by using is_master

--- core/app/models/spree/return_item/exchange_variant_eligibility/same_product.rb
+++ core/app/models/spree/return_item/exchange_variant_eligibility/same_product.rb
@@ -3,10 +3,10 @@ module Spree
     class SameProduct
 
       def self.eligible_variants(variant)
-        if variant.product.has_variants?
-          Spree::Variant.where(product_id: variant.product_id, is_master: false).in_stock
-        else
+        if variant.is_master?
           Spree::Variant.where(product_id: variant.product_id, is_master: true).in_stock
+        else
+          Spree::Variant.where(product_id: variant.product_id, is_master: false).in_stock
         end
       end
     end
