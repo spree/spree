6b929a7256c48d1c6c8d0e5a13c000cd68c63883
Ben A Morgan <ben@benmorgan.io>
2014-12-12 11:26:16 -0800

    use an after commit instead of an after save

--- core/app/models/spree/stock_location.rb
+++ core/app/models/spree/stock_location.rb
@@ -12,7 +12,7 @@ module Spree
     scope :active, -> { where(active: true) }
     scope :order_default, -> { order(default: :desc, name: :asc) }
 
-    after_create :create_stock_items, :if => "self.propagate_all_variants?"
+    after_commit :create_stock_items, on: :create, if: "self.propagate_all_variants?"
     after_save :ensure_one_default
 
     def state_text
