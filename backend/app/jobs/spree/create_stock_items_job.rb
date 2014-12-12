38055f550c90285e85b14d7356dc28216db4923b
Ben A Morgan <ben@benmorgan.io>
2014-12-12 11:25:24 -0800

    remove mutex

--- backend/app/jobs/spree/create_stock_items_job.rb
+++ backend/app/jobs/spree/create_stock_items_job.rb
@@ -3,12 +3,10 @@ module Spree
     queue_as :default
 
     def perform(stock_location_id)
-      ::Mutex.new.synchronize do
-        stock_location = StockLocation.find(stock_location_id)
+      stock_location = StockLocation.find(stock_location_id)
 
-        Variant.find_each do |variant|
-          stock_location.propagate_variant variant
-        end
+      Variant.find_each do |variant|
+        stock_location.propagate_variant variant
       end
     end
   end
