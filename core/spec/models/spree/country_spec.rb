9c1aa1b8e6d25afeb2cb97dcc36c468ca852e6a4
Ben A Morgan <ben@benmorgan.io>
2014-12-12 11:20:33 -0800

    use ruby 1.9.3 syntax and update whitespace

--- core/spec/models/spree/country_spec.rb
+++ core/spec/models/spree/country_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 
-describe Spree::Country, :type => :model do
+describe Spree::Country, type: :model do
   it "can find all countries group by states required" do
-    country_states_required= Spree::Country.create({:name => "Canada", :iso_name => "CAN", :states_required => true})
-    country_states_not_required= Spree::Country.create({:name => "France", :iso_name => "FR", :states_required => false})
+    country_states_required = Spree::Country.create({ name: "Canada", iso_name: "CAN", states_required: true })
+    country_states_not_required = Spree::Country.create({ name: "France", iso_name: "FR", states_required: false })
     states_required = Spree::Country.states_required_by_country_id
     expect(states_required[country_states_required.id.to_s]).to be true
     expect(states_required[country_states_not_required.id.to_s]).to be false
