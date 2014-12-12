9f1fde982fc3b46d205cf31ab25b2f94db35a8b2
Ben A Morgan <ben@benmorgan.io>
2014-12-12 10:52:35 -0800

    move country default to a model
    fixes #5765

--- backend/app/controllers/spree/admin/stock_locations_controller.rb
+++ backend/app/controllers/spree/admin/stock_locations_controller.rb
@@ -1,25 +1,16 @@
 module Spree
   module Admin
     class StockLocationsController < ResourceController
-
       before_action :set_country, only: :new
 
       private
 
       def set_country
-        begin
-          if Spree::Config[:default_country_id].present?
-            @stock_location.country = Spree::Country.find(Spree::Config[:default_country_id])
-          else
-            @stock_location.country = Spree::Country.find_by!(iso: 'US')
-          end
-
-        rescue ActiveRecord::RecordNotFound
-          flash[:error] = Spree.t(:stock_locations_need_a_default_country)
-          redirect_to admin_stock_locations_path and return
-        end
+        @stock_location.country = Spree::Country.default
+      rescue ActiveRecord::RecordNotFound
+        flash[:error] = Spree.t(:stock_locations_need_a_default_country)
+        redirect_to admin_stock_locations_path and return
       end
-
     end
   end
 end
--- core/spec/models/spree/country_spec.rb
+++ core/spec/models/spree/country_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 
-describe Spree::Country, :type => :model do
+describe Spree::Country, type: :model do
   it "can find all countries group by states required" do
-    country_states_required= Spree::Country.create({:name => "Canada", :iso_name => "CAN", :states_required => true})
-    country_states_not_required= Spree::Country.create({:name => "France", :iso_name => "FR", :states_required => false})
+    country_states_required = Spree::Country.create({ name: "Canada", iso_name: "CAN", states_required: true })
+    country_states_not_required = Spree::Country.create({ name: "France", iso_name: "FR", states_required: false })
     states_required = Spree::Country.states_required_by_country_id
     expect(states_required[country_states_required.id.to_s]).to be true
     expect(states_required[country_states_not_required.id.to_s]).to be false
@@ -11,5 +11,20 @@ describe Spree::Country, :type => :model do
 
   it "returns that the states are required for an invalid country" do
     expect(Spree::Country.states_required_by_country_id['i do not exit']).to be true
+  end
+
+  describe '.default' do
+    let(:america) { create :country }
+    let(:canada)  { create :country, name: 'Canada' }
+
+    it 'will return the country from the config' do
+      Spree::Config[:default_country_id] = canada.id
+      expect(described_class.default.id).to eql canada.id
+    end
+
+    it 'will return the US if config is not set' do
+      america.touch
+      expect(described_class.default.id).to eql america.id
+    end
   end
 end
