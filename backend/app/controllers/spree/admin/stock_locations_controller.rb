13aa4e5029793bbaf25e1fe13bfa0e8bae09383a
Ben A Morgan <ben@benmorgan.io>
2014-12-12 10:55:01 -0800

    no need for a begin end

--- backend/app/controllers/spree/admin/stock_locations_controller.rb
+++ backend/app/controllers/spree/admin/stock_locations_controller.rb
@@ -6,12 +6,10 @@ module Spree
       private
 
       def set_country
-        begin
-          @stock_location.country = Spree::Country.default
-        rescue ActiveRecord::RecordNotFound
-          flash[:error] = Spree.t(:stock_locations_need_a_default_country)
-          redirect_to admin_stock_locations_path and return
-        end
+        @stock_location.country = Spree::Country.default
+      rescue ActiveRecord::RecordNotFound
+        flash[:error] = Spree.t(:stock_locations_need_a_default_country)
+        redirect_to admin_stock_locations_path and return
       end
     end
   end
