897cff2df3c1d88efb468657b7ca0fff3aafc0ab
Martin Tomov <mtomov@outlook.com>
2014-12-14 15:47:57 +0000

    Fix N+1 on Shipment#to_package call to variant from Differentiator
    On checkout/delivery page, a page refresh calls out to the
    `Stock::Differentiator`, which loads a variant from each `inventory_unit`
    
    The fix eliminates N+1 sql select for each variant.
    
    fixes #5775

--- core/app/models/spree/shipment.rb
+++ core/app/models/spree/shipment.rb
@@ -285,7 +285,7 @@ module Spree
 
     def to_package
       package = Stock::Package.new(stock_location)
-      inventory_units.group_by(&:state).each do |state, state_inventory_units|
+      inventory_units.includes(:variant).joins(:variant).group_by(&:state).each do |state, state_inventory_units|
         package.add_multiple state_inventory_units, state.to_sym
       end
       package
