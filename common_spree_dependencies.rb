448ba611570e016f4ba03824feb98cf0fcf608fe
Washington Luiz <huoxito@gmail.com>
2014-12-14 00:27:14 -0300

    point to rails/4-2-0

--- common_spree_dependencies.rb
+++ common_spree_dependencies.rb
@@ -18,7 +18,7 @@ end
 gem 'coffee-rails', '~> 4.0.0'
 gem 'sass-rails', '~> 5.0.0.beta1'
 
-gem 'rails', github: 'rails/rails', branch: '4-2-stable'
+gem 'rails', github: 'rails/rails', branch: '4-2-0'
 gem 'paranoia', github: 'huoxito/paranoia', branch: 'rails-4.2'
 gem 'state_machine', github: 'huoxito/state_machine', branch: 'spree-rails4.2-upgrade'
 
