richardnuno/solidus

Add OptionValue promotion rule.

backend/app/assets/javascripts/spree/backend/promotions.js
@@ -34,6 +34,54 @@ var initProductActions = function () {
   });
 
   //
+  // Option Value Promo Rule
+  //
+  if ($('#promo-rule-option-value-template').length) {
+    var optionValueSelectNameTemplate = Handlebars.compile($('#promo-rule-option-value-option-values-select-name-template').html());
+    var optionValueTemplate = Handlebars.compile($('#promo-rule-option-value-template').html());
+
+    var addOptionValue = function(product, values) {
+      $('.js-promo-rule-option-values').append(optionValueTemplate({
+        productSelect: {value: product},
+        optionValuesSelect: {value: values}
+      }));
+      var optionValue = $('.js-promo-rule-option-values .promo-rule-option-value').last();
+      optionValue.find('.js-promo-rule-option-value-product-select').productAutocomplete({multiple: false});
+      optionValue.find('.js-promo-rule-option-value-option-values-select').optionValueAutocomplete({
+        productSelect: '.js-promo-rule-option-value-product-select'
+      });
+      if (product === null) {
+        optionValue.find('.js-promo-rule-option-value-option-values-select').prop('disabled', true);
+      }
+    }
+
+    var originalOptionValues = $('.js-original-promo-rule-option-values').data('original-option-values');
+    if (!$('.js-original-promo-rule-option-values').data('loaded')) {
+      if ($.isEmptyObject(originalOptionValues)) {
+        addOptionValue(null, null);
+      } else {
+        $.each(originalOptionValues, addOptionValue);
+      }
+    }
+    $('.js-original-promo-rule-option-values').data('loaded', true);
+
+    $(document).on('click', '.js-add-promo-rule-option-value', function (event) {
+      event.preventDefault();
+      addOptionValue(null, null);
+    });
+
+    $(document).on('click', '.js-remove-promo-rule-option-value', function () {
+      $(this).parents('.promo-rule-option-value').remove();
+    });
+
+    $(document).on('change', '.js-promo-rule-option-value-product-select', function () {
+      var optionValueSelect = $(this).parents('.promo-rule-option-value').find('.js-promo-rule-option-value-option-values-select');
+      optionValueSelect.attr('name', optionValueSelectNameTemplate({product_id: $(this).val()}).trim());
+      optionValueSelect.prop('disabled', $(this).val() === "").select2("val", "");
+    });
+  }
+
+  //
   // Tiered Calculator
   //
   if ($('#tier-fields-template').length && $('#tier-input-name').length) {

core/app/models/spree/promotion/rules/option_value.rb
@@ -1,0 +1,50 @@
+module Spree
+  class Promotion
+    module Rules
+      class OptionValue < PromotionRule
+        MATCH_POLICIES = %w(any)
+        preference :match_policy, :string, default: MATCH_POLICIES.first
+        preference :eligible_values, :hash
+
+        def applicable?(promotable)
+          promotable.is_a?(Spree::Order)
+        end
+
+        def eligible?(promotable, options = {})
+          case preferred_match_policy
+          when 'any'
+            promotable.line_items.any? { |item| actionable?(item) }
+          end
+        end
+
+        def actionable?(line_item)
+
+          pid = line_item.product.id
+          ovids = line_item.variant.option_values.pluck(:id)
+
+          product_ids.include?(pid) && (value_ids(pid) - ovids).empty?
+        end
+
+        def preferred_eligible_values_with_numerification
+          values = preferred_eligible_values_without_numerification || {}
+          Hash[values.keys.map(&:to_i).zip(
+            values.values.map do |v|
+              (v.is_a?(Array) ? v : v.split(",")).map(&:to_i)
+            end
+          )]
+        end
+        alias_method_chain :preferred_eligible_values, :numerification
+
+        private
+
+        def product_ids
+          preferred_eligible_values.keys
+        end
+
+        def value_ids(product_id)
+          preferred_eligible_values[product_id]
+        end
+      end
+    end
+  end
+end

core/spec/models/spree/promotion/rules/option_value_spec.rb
@@ -1,0 +1,89 @@
+require 'spec_helper'
+
+describe Spree::Promotion::Rules::OptionValue do
+  let(:rule) { Spree::Promotion::Rules::OptionValue.new }
+
+  describe "#preferred_eligible_values" do
+    subject { rule.preferred_eligible_values }
+    it "assigns a nicely formatted hash" do
+      rule.preferred_eligible_values = Hash["5" => "1,2", "6" => "1"]
+      expect(subject).to eq Hash[5 => [1, 2], 6 => [1]]
+    end
+  end
+
+  describe "#applicable?" do
+    subject { rule.applicable?(promotable) }
+    context "when promotable is an order" do
+      let(:promotable) { Spree::Order.new }
+      it { should be true }
+    end
+    context "when promotable is not an order" do
+      let(:promotable) { Spree::LineItem.new }
+      it { should be false }
+    end
+  end
+
+  describe "#eligible?" do
+    let(:line_item) { create :line_item }
+    let(:promotable) { line_item.order }
+    subject { rule.eligible?(promotable) }
+    context "when there are any applicable line items" do
+      before do
+        rule.preferred_eligible_values = Hash[line_item.product.id => [
+          line_item.variant.option_values.pluck(:id).first
+        ]]
+      end
+      it { should be true }
+    end
+    context "when there are no applicable line items" do
+      before do
+        rule.preferred_eligible_values = Hash[99 => [99]]
+      end
+      it { should be false }
+    end
+  end
+
+  describe "#actionable?" do
+    let(:line_item) { create :line_item }
+    let(:option_value_blue) do
+      create(
+        :option_value,
+        name: 'Blue',
+        presentation: 'Blue',
+        option_type: create(
+          :option_type,
+          name: 'foo-colour',
+          presentation: 'Colour'
+        )
+      )
+    end
+    let(:option_value_medium) do
+      create(
+        :option_value,
+        name: 'Medium',
+        presentation: 'M'
+      )
+    end
+    before do
+      line_item.variant.option_values << option_value_blue
+      rule.preferred_eligible_values = Hash[product_id => option_value_ids]
+    end
+    subject { rule.actionable?(line_item) }
+    context "when the line item has the correct product" do
+      let(:product_id) { line_item.product.id }
+      context "when all of the option values match" do
+        let(:option_value_ids) { [option_value_blue.id] }
+        it { should be true }
+      end
+      context "when not all of the option values match" do
+        let(:option_value_ids) { [option_value_blue.id, option_value_medium.id] }
+        it { should be false }
+      end
+    end
+    context "when the line item's product doesn't match" do
+      let(:product_id) { 99 }
+      let(:option_value_ids) { [99] }
+      it { should be false }
+    end
+  end
+end

spree/spree

backend/app/assets/javascripts/spree/backend/promotions.js
@@ -1,4 +1,5 @@
 var initProductActions = function () {
+  /* globals Handlebars */
   'use strict';
 
   // Add classes on promotion items for design
@@ -32,6 +33,54 @@ var initProductActions = function () {
       }
     });
   });
+
+  //
+  // Option Value Promo Rule
+  //
+  if ($('#promo-rule-option-value-template').length) {
+    var optionValueSelectNameTemplate = Handlebars.compile($('#promo-rule-option-value-option-values-select-name-template').html());
+    var optionValueTemplate = Handlebars.compile($('#promo-rule-option-value-template').html());
+
+    var addOptionValue = function(product, values) {
+      $('.js-promo-rule-option-values').append(optionValueTemplate({
+        productSelect: {value: product},
+        optionValuesSelect: {value: values}
+      }));
+      var optionValue = $('.js-promo-rule-option-values .promo-rule-option-value').last();
+      optionValue.find('.js-promo-rule-option-value-product-select').productAutocomplete({multiple: false});
+      optionValue.find('.js-promo-rule-option-value-option-values-select').optionValueAutocomplete({
+        productSelect: '.js-promo-rule-option-value-product-select'
+      });
+      if (product === null) {
+        optionValue.find('.js-promo-rule-option-value-option-values-select').prop('disabled', true);
+      }
+    };
+
+    var originalOptionValues = $('.js-original-promo-rule-option-values').data('original-option-values');
+    if (!$('.js-original-promo-rule-option-values').data('loaded')) {
+      if ($.isEmptyObject(originalOptionValues)) {
+        addOptionValue(null, null);
+      } else {
+        $.each(originalOptionValues, addOptionValue);
+      }
+    }
+    $('.js-original-promo-rule-option-values').data('loaded', true);
+
+    $(document).on('click', '.js-add-promo-rule-option-value', function (event) {
+      event.preventDefault();
+      addOptionValue(null, null);
+    });
+
+    $(document).on('click', '.js-remove-promo-rule-option-value', function () {
+      $(this).parents('.promo-rule-option-value').remove();
+    });
+
+    $(document).on('change', '.js-promo-rule-option-value-product-select', function () {
+      var optionValueSelect = $(this).parents('.promo-rule-option-value').find('.js-promo-rule-option-value-option-values-select');
+      optionValueSelect.attr('name', optionValueSelectNameTemplate({productId: $(this).val()}).trim());
+      optionValueSelect.prop('disabled', $(this).val() === '').select2('val', '');
+    });
+  }
 
   //
   // Tiered Calculator

core/app/models/spree/promotion/rules/option_value.rb
@@ -1,0 +1,49 @@
+module Spree
+  class Promotion
+    module Rules
+      class OptionValue < PromotionRule
+        MATCH_POLICIES = %w(any)
+        preference :match_policy, :string, default: MATCH_POLICIES.first
+        preference :eligible_values, :hash
+
+        def applicable?(promotable)
+          promotable.is_a?(Spree::Order)
+        end
+
+        def eligible?(promotable, _options = {})
+          case preferred_match_policy
+          when 'any'
+            promotable.line_items.any? { |item| actionable?(item) }
+          end
+        end
+
+        def actionable?(line_item)
+          pid = line_item.product.id
+          ovids = line_item.variant.option_values.pluck(:id)
+
+          product_ids.include?(pid) && (value_ids(pid) - ovids).empty?
+        end
+
+        def preferred_eligible_values_with_numerification
+          values = preferred_eligible_values_without_numerification || {}
+          Hash[values.keys.map(&:to_i).zip(
+            values.values.map do |v|
+              (v.is_a?(Array) ? v : v.split(",")).map(&:to_i)
+            end
+          )]
+        end
+        alias_method_chain :preferred_eligible_values, :numerification
+
+        private
+
+        def product_ids
+          preferred_eligible_values.keys
+        end
+
+        def value_ids(product_id)
+          preferred_eligible_values[product_id]
+        end
+      end
+    end
+  end
+end

core/spec/models/spree/promotion/rules/option_value_spec.rb
@@ -1,0 +1,90 @@
+require 'spec_helper'
+
+describe Spree::Promotion::Rules::OptionValue do
+  let(:rule) { Spree::Promotion::Rules::OptionValue.new }
+
+  describe "#preferred_eligible_values" do
+    subject { rule.preferred_eligible_values }
+    it "assigns a nicely formatted hash" do
+      rule.preferred_eligible_values = Hash["5" => "1,2", "6" => "1"]
+      expect(subject).to eq Hash[5 => [1, 2], 6 => [1]]
+    end
+  end
+
+  describe "#applicable?" do
+    subject { rule.applicable?(promotable) }
+    context "when promotable is an order" do
+      let(:promotable) { Spree::Order.new }
+      it { should be true }
+    end
+    context "when promotable is not an order" do
+      let(:promotable) { Spree::LineItem.new }
+      it { should be false }
+    end
+  end
+
+  describe "#eligible?" do
+    let(:variant) { create :variant }
+    let(:line_item) { create :line_item, variant: variant }
+    let(:promotable) { line_item.order }
+    subject { rule.eligible?(promotable) }
+    context "when there are any applicable line items" do
+      before do
+        rule.preferred_eligible_values = Hash[variant.product.id => [
+          variant.option_values.pluck(:id).first
+        ]]
+      end
+      it { should be true }
+    end
+    context "when there are no applicable line items" do
+      before do
+        rule.preferred_eligible_values = Hash[99 => [99]]
+      end
+      it { should be false }
+    end
+  end
+
+  describe "#actionable?" do
+    let(:line_item) { create :line_item }
+    let(:option_value_blue) do
+      create(
+        :option_value,
+        name: 'Blue',
+        presentation: 'Blue',
+        option_type: create(
+          :option_type,
+          name: 'foo-colour',
+          presentation: 'Colour'
+        )
+      )
+    end
+    let(:option_value_medium) do
+      create(
+        :option_value,
+        name: 'Medium',
+        presentation: 'M'
+      )
+    end
+    before do
+      line_item.variant.option_values << option_value_blue
+      rule.preferred_eligible_values = Hash[product_id => option_value_ids]
+    end
+    subject { rule.actionable?(line_item) }
+    context "when the line item has the correct product" do
+      let(:product_id) { line_item.product.id }
+      context "when all of the option values match" do
+        let(:option_value_ids) { [option_value_blue.id] }
+        it { should be true }
+      end
+      context "when not all of the option values match" do
+        let(:option_value_ids) { [option_value_blue.id, option_value_medium.id] }
+        it { should be false }
+      end
+    end
+    context "when the line item's product doesn't match" do
+      let(:product_id) { 99 }
+      let(:option_value_ids) { [99] }
+      it { should be false }
+    end
+  end
+end
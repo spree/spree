b2cf717bbf01190e3cdcfd77005b54ce2e935292
Adam Mueller <adam@freerunningtech.com>
2014-11-13 11:49:12 -0800

    Add OptionValue promotion rule.

---
+++ backend/app/assets/javascripts/spree/backend/option_value_picker.js
@@ -0,0 +1,44 @@
+$.fn.optionValueAutocomplete = function (options) {
+  /* globals Spree */
+  'use strict';
+
+  // Default options
+  options = options || {};
+  var multiple = typeof(options.multiple) !== 'undefined' ? options.multiple : true;
+  var productSelect = options.productSelect;
+
+  this.select2({
+    minimumInputLength: 3,
+    multiple: multiple,
+    initSelection: function (element, callback) {
+      $.get(Spree.routes.option_value_search, {
+        ids: element.val().split(',')
+      }, function (data) {
+        callback(multiple ? data : data[0]);
+      });
+    },
+    ajax: {
+      url: Spree.routes.option_value_search,
+      datatype: 'json',
+      data: function (term) {
+        var productId = typeof(productSelect) !== 'undefined' ? $(productSelect).select2('val') : null;
+        return {
+          q: {
+            name_cont: term,
+            variants_product_id_eq: productId
+          },
+          token: Spree.api_key
+        };
+      },
+      results: function (data) {
+        return { results: data };
+      }
+    },
+    formatResult: function (optionValue) {
+      return optionValue.name;
+    },
+    formatSelection: function (optionValue) {
+      return optionValue.name;
+    }
+  });
+};
--- backend/app/assets/javascripts/spree/backend/product_picker.js
+++ backend/app/assets/javascripts/spree/backend/product_picker.js
@@ -1,14 +1,18 @@
-$.fn.productAutocomplete = function () {
+$.fn.productAutocomplete = function (options) {
   'use strict';
 
+  // Default options
+  options = options || {};
+  var multiple = typeof(options.multiple) !== 'undefined' ? options.multiple : true;
+
   this.select2({
-    minimumInputLength: 1,
-    multiple: true,
+    minimumInputLength: 3,
+    multiple: multiple,
     initSelection: function (element, callback) {
       $.get(Spree.routes.product_search, {
         ids: element.val().split(',')
       }, function (data) {
-        callback(data.products);
+        callback(multiple ? data.products : data.products[0]);
       });
     },
     ajax: {
