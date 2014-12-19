9a844fd12d7b1b3fdfccbae2c8197284ab952701
Alexey Topolianskiy <devilcoders@gmail.com>
2014-12-19 14:20:15 +0200

    New pack of styles. Msotly select2 & jquery-ui styles.

--- backend/app/assets/javascripts/spree/backend.js
+++ backend/app/assets/javascripts/spree/backend.js
@@ -8,6 +8,7 @@
 //= require jquery-ui/autocomplete
 //= require modernizr
 //= require underscore-min.js
+//= require velocity
 //= require spree
 //= require spree/backend/spree-select2
 //= require_tree .
