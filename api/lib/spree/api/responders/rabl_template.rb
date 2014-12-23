9f77d6feebd0b2d88384a43a4d06592ba04b6f0b
Washington Luiz <huoxito@gmail.com>
2014-12-23 01:16:10 -0300

    Update Responder#api_behavior signature
    method was updated in https://github.com/plataformatec/responders/commit/e19f1071feaa551d2b54a4a37ce715f25da6c49c
    and no longer takes an argument

--- api/lib/spree/api/responders/rabl_template.rb
+++ api/lib/spree/api/responders/rabl_template.rb
@@ -8,16 +8,15 @@ module Spree
           else
             super
           end
-
         rescue ActionView::MissingTemplate => e
-          api_behavior(e)
+          api_behavior
         end
 
         def template
           request.headers['X-Spree-Template'] || controller.params[:template] || options[:default_template]
         end
 
-        def api_behavior(error)
+        def api_behavior
           if controller.params[:action] == "destroy"
             # Render a blank template
             super
