835351947c023ff061b4a0c750260e580bcec70d
Jeff Dutil <JDutil@BurlingtonWebApps.com>
2015-01-02 10:54:52 -0500

    Fix syntax.

--- core/spec/models/spree/credit_card_spec.rb
+++ core/spec/models/spree/credit_card_spec.rb
@@ -2,9 +2,11 @@ require 'spec_helper'
 
 describe Spree::CreditCard do
   let(:valid_credit_card_attributes) do
-    { :number => '4111111111111111',
+    {
+      :number => '4111111111111111',
       :verification_value => '123',
       :expiry => "12 / #{Time.now.year + 1}"
+    }
   end
 
   def self.payment_states
