cae19ee00852de9284899b7938a086aa73105f4b
Jeff Dutil <JDutil@BurlingtonWebApps.com>
2015-01-06 12:39:53 -0500

    Add back spec setup removed by d79cea9f581236afc03d0ea30f7e86446e3774fb

--- core/spec/models/spree/stock/quantifier_spec.rb
+++ core/spec/models/spree/stock/quantifier_spec.rb
@@ -11,6 +11,8 @@ end
 module Spree
   module Stock
     describe Quantifier, type: :model do
+      before(:all) { Spree::StockLocation.destroy_all } # FIXME leaky database
+
       let!(:stock_location) { create :stock_location_with_items  }
       let!(:stock_item) { stock_location.stock_items.order(:id).first }
 
