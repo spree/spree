e851095b70b6f9e85c84258ed321cea6085fae7e
Washington Luiz <huoxito@gmail.com>
2015-01-07 09:36:13 -0300

    drop reference to deleted method

--- core/spec/models/spree/payment_spec.rb
+++ core/spec/models/spree/payment_spec.rb
@@ -571,22 +571,6 @@ describe Spree::Payment, :type => :model do
     end
   end
 
-  describe '#invalidate_old_payments' do
-      before {
-        Spree::Payment.skip_callback(:rollback, :after, :persist_invalid)
-      }
-      after {
-        Spree::Payment.set_callback(:rollback, :after, :persist_invalid)
-      }
-
-    it 'should not invalidate other payments if not valid' do
-      payment.save
-      invalid_payment = Spree::Payment.new(:amount => 100, :order => order, :state => 'invalid', :payment_method => gateway)
-      invalid_payment.save
-      expect(payment.reload.state).to eq('checkout')
-    end
-  end
-
   describe "#build_source" do
     let(:params) do
       {
