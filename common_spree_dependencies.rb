06722ff81ff7d471e868c9f6e465c210d0136b34
Washington Luiz <huoxito@gmail.com>
2015-01-06 23:29:37 -0300

    Revert "bundle pry for ruby 2.2"
    This reverts commit 726f41393c43c812e950e36c5f74b67a1351dd33.
    
    looks like circle ci doesnt support ruby 2.2 yet =/

--- common_spree_dependencies.rb
+++ common_spree_dependencies.rb
@@ -44,8 +44,7 @@ group :test, :development do
   platforms :ruby_19 do
     gem 'pry-debugger'
   end
-
-  platforms :ruby_20, :ruby_21, :ruby_22 do
+  platforms :ruby_20, :ruby_21 do
     gem 'pry-byebug'
   end
 end
