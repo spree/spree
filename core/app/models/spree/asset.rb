2f9da5eb15180fee0c894b50ddc3a95464b32780
Jeff Dutil <JDutil@BurlingtonWebApps.com>
2015-01-08 13:16:55 -0500

    Newer versions of acts_as_list changed default to 1.

--- core/app/models/spree/asset.rb
+++ core/app/models/spree/asset.rb
@@ -1,6 +1,6 @@
 module Spree
   class Asset < Spree::Base
     belongs_to :viewable, polymorphic: true, touch: true
-    acts_as_list scope: [:viewable_id, :viewable_type]
+    acts_as_list scope: [:viewable_id, :viewable_type], top_of_list: 0
   end
 end
