ac1be653dbe3287d71cee6621152192a61a8138e
Allie Larson <allisonlarson91@gmail.com>
2015-01-09 16:42:15 -0500

    Fix to payment_spec
    Added payment.save! to 3 failing tests that were looking for
    payment.log_entries to recieve :create!. Due to the parent not being
    saved, the method was not recieved and the test failed. Now fixed.

--- core/spec/models/spree/payment_spec.rb
+++ core/spec/models/spree/payment_spec.rb
@@ -172,6 +172,7 @@ describe Spree::Payment do
       end
 
       it "should log the response" do
+        payment.save!
         payment.log_entries.should_receive(:create!).with(:details => anything)
         payment.authorize!
       end
@@ -223,6 +224,7 @@ describe Spree::Payment do
       end
 
       it "should log the response" do
+        payment.save!
         payment.log_entries.should_receive(:create!).with(:details => anything)
         payment.purchase!
       end
@@ -450,6 +452,7 @@ describe Spree::Payment do
       end
 
       it "should log the response" do
+        payment.save!
         payment.log_entries.should_receive(:create!).with(:details => anything)
         payment.credit!
       end
