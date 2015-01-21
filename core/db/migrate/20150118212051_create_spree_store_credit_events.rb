93d805726d6ab1ff55ee805d76489c63c0933606
Peter Berkenbosch <peter@pero-ict.nl>
2015-01-21 16:38:14 +0100

    add the store_credit_event model and migration

--- core/db/migrate/20150118212051_create_spree_store_credit_events.rb
+++ core/db/migrate/20150118212051_create_spree_store_credit_events.rb
@@ -1,13 +1,14 @@
 class CreateSpreeStoreCreditEvents < ActiveRecord::Migration
   def change
     create_table :spree_store_credit_events do |t|
-      t.integer :store_credit_id,    null: false
-      t.string  :action,             null: false
-      t.decimal :amount,             precision: 8,  scale: 2
-      t.string  :authorization_code, null: false
-      t.decimal :user_total_amount,  precision: 8, scale: 2, default: 0.0, null: false
-      t.integer :originator_id
-      t.string :originator_type
+      t.integer  :store_credit_id,    null: false
+      t.string   :action,             null: false
+      t.decimal  :amount,             precision: 8,  scale: 2
+      t.string   :authorization_code, null: false
+      t.decimal  :user_total_amount,  precision: 8, scale: 2, default: 0.0, null: false
+      t.integer  :originator_id
+      t.string   :originator_type
+      t.datetime :deleted_at
       t.timestamps
     end
     add_index :spree_store_credit_events, :store_credit_id
