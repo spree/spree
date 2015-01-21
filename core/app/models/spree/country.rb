7c3a6e0dc615fb4b3059d6fdde27a0b15f9589bb
Clarke Brunsdon <clarke@freerunningtechnologies.com>
2015-01-20 20:43:05 -0800

    Remove states_required_by_country_id
    Isn't referenced anywhere in the code and doens't provide any value.

--- core/app/models/spree/country.rb
+++ core/app/models/spree/country.rb
@@ -12,12 +12,6 @@ module Spree
 
     validates :name, :iso_name, presence: true
 
-    def self.states_required_by_country_id
-      states_required = Hash.new(true)
-      all.each { |country| states_required[country.id.to_s]= country.states_required }
-      states_required
-    end
-
     def self.default
       country_id = Spree::Config[:default_country_id]
       country_id.present? ? find(country_id) : find_by!(iso: 'US')
