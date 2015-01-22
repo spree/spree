71c70a881fcff01a1084dfa1737f270d6e8398e8
Seb Ashton <seb@madebymade.co.uk>
2015-01-22 12:21:47 +0000

    Add touch: true to belongs_to relation
    The drag and drop ordering within the admin requires the cache of the taxon listing to be busted otherwise updates/changes aren't reflectly correctly

--- core/app/models/spree/classification.rb
+++ core/app/models/spree/classification.rb
@@ -3,7 +3,7 @@ module Spree
     self.table_name = 'spree_products_taxons'
     acts_as_list scope: :taxon
     belongs_to :product, class_name: "Spree::Product", inverse_of: :classifications
-    belongs_to :taxon, class_name: "Spree::Taxon", inverse_of: :classifications
+    belongs_to :taxon, class_name: "Spree::Taxon", inverse_of: :classifications, touch: true
 
     # For #3494
     validates_uniqueness_of :taxon_id, scope: :product_id, message: :already_linked
