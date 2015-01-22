48737acb96ae011415ecc9c98a6cb33556d18a0a
Seb Ashton <seb@madebymade.co.uk>
2015-01-22 12:20:46 +0000

    Add test to prove taxon updated up isn't touched

--- api/spec/controllers/spree/api/classifications_controller_spec.rb
+++ api/spec/controllers/spree/api/classifications_controller_spec.rb
@@ -4,6 +4,7 @@ module Spree
   describe Api::ClassificationsController, :type => :controller do
     let(:taxon) do
       taxon = create(:taxon)
+
       3.times do
         product = create(:product)
         product.taxons << taxon
@@ -25,13 +26,22 @@ module Spree
     context "as an admin" do
       sign_in_as_admin!
 
+      let(:last_product) { taxon.products.last }
+
       it "can change the order a product" do
-        last_product = taxon.products.last
         classification = taxon.classifications.find_by(:product_id => last_product.id)
         expect(classification.position).to eq(3)
         api_put :update, :taxon_id => taxon, :product_id => last_product, :position => 0
         expect(response.status).to eq(200)
         expect(classification.reload.position).to eq(1)
+      end
+
+      it "should touch the taxon" do
+        taxon.update_attributes(updated_at: Time.now - 10.seconds)
+        taxon_last_updated_at = taxon.updated_at
+        api_put :update, :taxon_id => taxon, :product_id => last_product, :position => 0
+        taxon.reload
+        expect(taxon_last_updated_at.to_i).to_not eq(taxon.updated_at.to_i)
       end
     end
   end
