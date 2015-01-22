742cd09b254421048bae16d3d6e61fa6f164c547
Luke Morton <luke@madebymade.co.uk>
2015-01-22 16:36:21 +0000

    Use shared context to test refunds for both included and additional taxation

--- core/spec/models/spree/calculator/refunds/default_refund_amount_spec.rb
+++ core/spec/models/spree/calculator/refunds/default_refund_amount_spec.rb
@@ -1,20 +1,25 @@
 require 'spec_helper'
 
-describe Spree::Calculator::Returns::DefaultRefundAmount, type: :model do
+shared_context 'a default refund calculator' do
   let(:line_item_quantity) { 2 }
-  let(:price) { 100.0 }
+  let(:line_item_price) { 100.0 }
   let(:tax_amount) { 0.2 }
   let(:order) { create(:order) }
-  let(:line_item) { create(:line_item, price: price, quantity: line_item_quantity) }
-  let(:pre_tax_amount) { line_item.pre_tax_amount }
+
+  let(:tax_rate) do
+    create(:tax_rate, zone: create(:zone, default_tax: true),
+                      included_in_price: tax_included_in_price,
+                      amount: tax_amount)
+  end
+
   let(:inventory_unit) { build(:inventory_unit, order: order, line_item: line_item) }
-  let(:return_item) { build(:return_item, inventory_unit: inventory_unit ) }
+
+  let(:return_item) { build(:return_item, inventory_unit: inventory_unit) }
+
   let(:calculator) { Spree::Calculator::Returns::DefaultRefundAmount.new }
 
-  before do
-    create(:tax_rate, zone: create(:zone, default_tax: true),
-                      included_in_price: true,
-                      amount: tax_amount)
+  before(:each) do
+    tax_rate
     order.line_items << line_item
     order.updater.update_totals
   end
@@ -27,22 +32,25 @@ describe Spree::Calculator::Returns::DefaultRefundAmount, type: :model do
     end
 
     context 'order adjustments' do
+      let(:promotion_action) do
+        calculator = Spree::Calculator::FlatPercentItemTotal.new
+        calculator.preferred_flat_percent = 40
+        Spree::Promotion::Actions::CreateAdjustment.create!(calculator: calculator,
+                                                            promotion: create(:promotion))
+      end
+
       let(:adjustment_amount) { order.item_total * -0.4 }
 
-      before do
-        order.adjustments << create(:adjustment, order: order,
-                                                 amount: adjustment_amount,
-                                                 eligible: true,
-                                                 label: 'Adjustment',
-                                                 source_type: 'Spree::Order')
-        order.adjustments.first.update_attributes(amount: adjustment_amount)
+      before(:each) do
+        promotion_action.perform(order: order)
+        order.save!
         order.updater.update_totals
       end
 
-      it { is_expected.to eq (pre_tax_amount - adjustment_amount.abs) / line_item_quantity }
+      it { is_expected.to be_within(0.001).of(adjustment_refund_amount) }
 
       context 'refund amount * line item quantity with tax applied' do
-        subject { (calculator.compute(return_item) * line_item_quantity) * (1 + tax_amount) }
+        subject { full_refund_amount }
         it { is_expected.to eq order.total }
       end
     end
@@ -63,7 +71,45 @@ describe Spree::Calculator::Returns::DefaultRefundAmount, type: :model do
   end
 
   context 'pre_tax_amount is zero' do
-    let(:price)  { 0.0 }
+    let(:line_item_price)  { 0.0 }
     it { should eq 0.0 }
+  end
+end
+
+describe Spree::Calculator::Returns::DefaultRefundAmount, type: :model do
+  context 'tax included in price' do
+    let(:tax_included_in_price) { true }
+    let(:pre_tax_amount) { line_item.pre_tax_amount }
+
+    let(:line_item) { create(:line_item, price: line_item_price, quantity: line_item_quantity) }
+
+    let(:adjustment_refund_amount) do
+      (line_item_price + (adjustment_amount / 2)) / (1 + tax_amount)
+    end
+
+    let(:full_refund_amount) do
+      (calculator.compute(return_item) * (1 + tax_amount)).round * line_item_quantity
+    end
+
+    it_should_behave_like 'a default refund calculator'
+  end
+
+  context 'tax additional to price' do
+    let(:tax_included_in_price) { false }
+    let(:pre_tax_amount) { line_item_price * line_item_quantity }
+
+    let(:line_item) { create(:line_item, price: line_item_price,
+                                         quantity: line_item_quantity,
+                                         pre_tax_amount: pre_tax_amount) }
+
+    let(:adjustment_refund_amount) do
+      (pre_tax_amount - adjustment_amount.abs) / line_item_quantity
+    end
+
+    let(:full_refund_amount) do
+      (calculator.compute(return_item) * line_item_quantity) + order.additional_tax_total
+    end
+
+    it_should_behave_like 'a default refund calculator'
   end
 end
