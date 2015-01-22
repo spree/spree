6ecd796581cfe2433c62b0c29ba45fa893d6fd88
Seb Ashton <seb@madebymade.co.uk>
2015-01-22 12:23:18 +0000

    Remove HashRockets

--- api/spec/controllers/spree/api/classifications_controller_spec.rb
+++ api/spec/controllers/spree/api/classifications_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 module Spree
-  describe Api::ClassificationsController, :type => :controller do
+  describe Api::ClassificationsController, type: :controller do
     let(:taxon) do
       taxon = create(:taxon)
 
@@ -18,7 +18,7 @@ module Spree
 
     context "as a user" do
       it "cannot change the order of a product" do
-        api_put :update, :taxon_id => taxon, :product_id => taxon.products.first, :position => 1
+        api_put :update, taxon_id: taxon, product_id: taxon.products.first, position: 1
         expect(response.status).to eq(401)
       end
     end
@@ -29,9 +29,9 @@ module Spree
       let(:last_product) { taxon.products.last }
 
       it "can change the order a product" do
-        classification = taxon.classifications.find_by(:product_id => last_product.id)
+        classification = taxon.classifications.find_by(product_id: last_product.id)
         expect(classification.position).to eq(3)
-        api_put :update, :taxon_id => taxon, :product_id => last_product, :position => 0
+        api_put :update, taxon_id: taxon, product_id: last_product, position: 0
         expect(response.status).to eq(200)
         expect(classification.reload.position).to eq(1)
       end
@@ -39,7 +39,7 @@ module Spree
       it "should touch the taxon" do
         taxon.update_attributes(updated_at: Time.now - 10.seconds)
         taxon_last_updated_at = taxon.updated_at
-        api_put :update, :taxon_id => taxon, :product_id => last_product, :position => 0
+        api_put :update, taxon_id: taxon, product_id: last_product, position: 0
         taxon.reload
         expect(taxon_last_updated_at.to_i).to_not eq(taxon.updated_at.to_i)
       end
