fd82c30a227e8d7e2d2b549ca3b2deaa91e11fc5
Ben A Morgan <ben@benmorgan.io>
2015-01-21 17:47:06 -0800

    move jobs up, alphabetical ordering

--- core/spec/spec_helper.rb
+++ core/spec/spec_helper.rb
@@ -4,10 +4,10 @@ if ENV["COVERAGE"]
   SimpleCov.start do
     add_group 'Controllers', 'app/controllers'
     add_group 'Helpers', 'app/helpers'
+    add_group 'Jobs', 'app/jobs'
     add_group 'Mailers', 'app/mailers'
     add_group 'Models', 'app/models'
     add_group 'Views', 'app/views'
-    add_group 'Jobs', 'app/jobs'
     add_group 'Libraries', 'lib'
   end
 end
