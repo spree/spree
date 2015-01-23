8afcb5347c5f6d392e0dbc8d9097cb2eaf66b4fe
Jeff Dutil <JDutil@BurlingtonWebApps.com>
2015-01-22 19:47:57 -0500

    Use store defaults. Fixes #5115

--- core/app/mailers/spree/base_mailer.rb
+++ core/app/mailers/spree/base_mailer.rb
@@ -2,10 +2,10 @@ module Spree
   class BaseMailer < ActionMailer::Base
 
     def from_address
-      Spree::Config[:mails_from]
+      Spree::Store.current.mails_from_address
     end
 
-    def money(amount, currency = Spree::Config[:currency])
+    def money(amount, currency = Spree::Store.current.default_currency)
       Spree::Money.new(amount, currency: currency).to_s
     end
     helper_method :money
