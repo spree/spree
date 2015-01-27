135758c74c8f94627d8dcf205c9340a2a7fc1f69
Alessio Rocco <alessio.rocco.lt@gmail.com>
2015-01-27 15:11:35 +0100

    Add promotion rule for minimum and maximum order quantity

---
+++ core/app/models/spree/promotion/rules/item_quantity.rb
@@ -0,0 +1,53 @@
+# A rule to apply to an order greater than (or greater than or equal to)
+# a specific quantity of line items
+module Spree
+  class Promotion
+    module Rules
+      class ItemQuantity < PromotionRule
+        preference :quantity_min, :integer, default: 1
+        preference :operator_min, :string, default: '>'
+        preference :quantity_max, :integer, default: 1000
+        preference :operator_max, :string, default: '<'
+
+
+        OPERATORS_MIN = ['gt', 'gte']
+        OPERATORS_MAX = ['lt','lte']
+
+        def applicable?(promotable)
+          promotable.is_a?(Spree::Order)
+        end
+
+        def eligible?(order, options = {})
+          quantity = order.quantity
+
+          lower_limit_condition = quantity.send(preferred_operator_min == 'gte' ? :>= : :>, preferred_quantity_min)
+          upper_limit_condition = quantity.send(preferred_operator_max == 'lte' ? :<= : :<, preferred_quantity_max)
+
+          eligibility_errors.add(:base, ineligible_message_max) unless upper_limit_condition
+          eligibility_errors.add(:base, ineligible_message_min) unless lower_limit_condition
+
+          eligibility_errors.empty?
+        end
+
+        private
+
+        def ineligible_message_max
+          if preferred_operator_max == 'gte'
+            eligibility_error_message(:item_quantity_more_than_or_equal, quantity: preferred_quantity_max)
+          else
+            eligibility_error_message(:item_quantity_more_than, quantity: preferred_quantity_max)
+          end
+        end
+
+        def ineligible_message_min
+          if preferred_operator_min == 'gte'
+            eligibility_error_message(:item_quantity_less_than, quantity: preferred_quantity_min)
+          else
+            eligibility_error_message(:item_quantity_less_than_or_equal, quantity: preferred_quantity_min)
+          end
+        end
+
+      end
+    end
+  end
+end
---
+++ core/spec/models/spree/promotion/rules/item_quantity_spec.rb
@@ -0,0 +1,281 @@
+require 'spec_helper'
+
+describe Spree::Promotion::Rules::ItemQuantity, :type => :model do
+  let(:rule) { Spree::Promotion::Rules::ItemQuantity.new }
+  let(:order) { double(:order) }
+
+  before { rule.preferred_quantity_min = 4 }
+  before { rule.preferred_quantity_max = 10 }
+
+  context "preferred operator_min set to gt and preferred operator_max set to lt" do
+    before do
+      rule.preferred_operator_min = 'gt'
+      rule.preferred_operator_max = 'lt'
+    end
+
+    context "and item quantity is lower than prefered maximum quantity" do
+
+      context "and item quantity is higher than prefered minimum quantity" do
+        it "should be eligible" do
+          allow(order).to receive_messages quantity: 5
+          expect(rule).to be_eligible(order)
+        end
+      end
+
+      context "and item number is equal to the prefered minimum number" do
+        before { allow(order).to receive_messages quantity: 4 }
+
+        it "should not be eligible" do
+          expect(rule).to_not be_eligible(order)
+        end
+
+        it "set an error message" do
+          rule.eligible?(order)
+          expect(rule.eligibility_errors.full_messages.first).
+            to eq "This coupon code can't be applied to orders with items quantity less than or equal to 4."
+        end
+      end
+
+      context "and quantity is lower to the prefered minimum quantity" do
+        before { allow(order).to receive_messages quantity: 3 }
+
+        it "should not be eligible" do
+          expect(rule).to_not be_eligible(order)
+        end
+
+        it "set an error message" do
+          rule.eligible?(order)
+          expect(rule.eligibility_errors.full_messages.first).
+            to eq "This coupon code can't be applied to orders with items quantity less than or equal to 4."
+        end
+      end
+    end
+
+    context "and quantity is equal to the prefered maximum quantity" do
+      before { allow(order).to receive_messages quantity: 10 }
+
+      it "should not be eligible" do
+        expect(rule).to_not be_eligible(order)
+      end
+
+      it "set an error message" do
+        rule.eligible?(order)
+        expect(rule.eligibility_errors.full_messages.first).
+          to eq "This coupon code can't be applied to orders with items quantity higher than 10."
+      end
+    end
+
+    context "and quantity is higher than the prefered maximum quantity" do
+      before { allow(order).to receive_messages quantity: 15 }
+
+      it "should not be eligible" do
+        expect(rule).to_not be_eligible(order)
+      end
+
+      it "set an error message" do
+        rule.eligible?(order)
+        expect(rule.eligibility_errors.full_messages.first).
+          to eq "This coupon code can't be applied to orders with items quantity higher than 10."
+      end
+    end
+
+  end
+
+  context "preferred operator set to gt and preferred operator_max set to lte" do
+    before do
+      rule.preferred_operator_min = 'gt'
+      rule.preferred_operator_max = 'lte'
+    end
+
+    context "and quantity is lower than prefered maximum quantity" do
+
+      context "and quantity is higher than prefered minimum quantity" do
+        it "should be eligible" do
+          allow(order).to receive_messages quantity: 6
+          expect(rule).to be_eligible(order)
+        end
+      end
+
+      context "and quantity is equal to the prefered minimum quantity" do
+
+        before { allow(order).to receive_messages quantity: 4 }
+
+        it "should not be eligible" do
+          expect(rule).to_not be_eligible(order)
+        end
+
+        it "set an error message" do
+          rule.eligible?(order)
+          expect(rule.eligibility_errors.full_messages.first).
+            to eq "This coupon code can't be applied to orders with items quantity less than or equal to 4."
+        end
+      end
+
+      context "and quantity is lower to the prefered minimum quantity" do
+        before { allow(order).to receive_messages quantity: 2 }
+
+        it "should not be eligible" do
+          expect(rule).to_not be_eligible(order)
+        end
+
+        it "set an error message" do
+          rule.eligible?(order)
+          expect(rule.eligibility_errors.full_messages.first).
+            to eq "This coupon code can't be applied to orders with items quantity less than or equal to 4."
+        end
+      end
+    end
+
+    context "and quantity is equal to the prefered maximum quantity" do
+      before { allow(order).to receive_messages quantity: 10 }
+
+      it "should not be eligible" do
+        expect(rule).to be_eligible(order)
+      end
+    end
+
+    context "and quantity is higher than the prefered maximum quantity" do
+      before { allow(order).to receive_messages quantity: 15 }
+
+      it "should not be eligible" do
+        expect(rule).to_not be_eligible(order)
+      end
+
+      it "set an error message" do
+        rule.eligible?(order)
+        expect(rule.eligibility_errors.full_messages.first).
+          to eq "This coupon code can't be applied to orders with items quantity higher than 10."
+      end
+    end
+  end
+
+  context "preferred operator set to gte and preferred operator_max set to lt" do
+    before do
+      rule.preferred_operator_min = 'gte'
+      rule.preferred_operator_max = 'lt'
+    end
+
+    context "and quantity is lower than prefered maximum quantity" do
+
+      context "and quantity is higher than prefered minimum quantity" do
+        it "should be eligible" do
+          allow(order).to receive_messages quantity: 6
+          expect(rule).to be_eligible(order)
+        end
+      end
+
+      context "and quantity is equal to the prefered minimum quantity" do
+
+        before { allow(order).to receive_messages quantity: 4 }
+
+        it "should not be eligible" do
+          expect(rule).to be_eligible(order)
+        end
+      end
+
+      context "and quantity is lower to the prefered minimum quantity" do
+        before { allow(order).to receive_messages quantity: 3 }
+
+        it "should not be eligible" do
+          expect(rule).to_not be_eligible(order)
+        end
+
+        it "set an error message" do
+          rule.eligible?(order)
+          expect(rule.eligibility_errors.full_messages.first).
+            to eq "This coupon code can't be applied to orders with less than 4 items."
+        end
+      end
+    end
+
+    context "and quantity is equal to the prefered maximum quantity" do
+      before { allow(order).to receive_messages quantity: 10 }
+
+      it "should not be eligible" do
+        expect(rule).to_not be_eligible(order)
+      end
+
+      it "set an error message" do
+        rule.eligible?(order)
+        expect(rule.eligibility_errors.full_messages.first).
+          to eq "This coupon code can't be applied to orders with items quantity higher than 10."
+      end
+    end
+
+    context "and quantity is higher than the prefered maximum quantity" do
+      before { allow(order).to receive_messages quantity: 15 }
+
+      it "should not be eligible" do
+        expect(rule).to_not be_eligible(order)
+      end
+
+      it "set an error message" do
+        rule.eligible?(order)
+        expect(rule.eligibility_errors.full_messages.first).
+          to eq "This coupon code can't be applied to orders with items quantity higher than 10."
+      end
+    end
+
+  end
+
+  context "preferred operator set to gte and preferred operator_max set to lte" do
+    before do
+      rule.preferred_operator_min = 'gte'
+      rule.preferred_operator_max = 'lte'
+    end
+
+    context "and quantity is lower than prefered maximum quantity" do
+      context "and quantity is higher than prefered minimum quantity" do
+        it "should be eligible" do
+          allow(order).to receive_messages quantity: 6
+          expect(rule).to be_eligible(order)
+        end
+      end
+
+      context "and quantity is equal to the prefered minimum quantity" do
+
+        before { allow(order).to receive_messages quantity: 4 }
+
+        it "should not be eligible" do
+          expect(rule).to be_eligible(order)
+        end
+      end
+
+      context "and quantity is lower to the prefered minimum quantity" do
+        before { allow(order).to receive_messages quantity: 2 }
+
+        it "should not be eligible" do
+          expect(rule).to_not be_eligible(order)
+        end
+
+        it "set an error message" do
+          rule.eligible?(order)
+          expect(rule.eligibility_errors.full_messages.first).
+            to eq "This coupon code can't be applied to orders with less than 4 items."
+        end
+      end
+    end
+
+    context "and quantity is equal to the prefered maximum quantity" do
+      before { allow(order).to receive_messages quantity: 10 }
+
+      it "should not be eligible" do
+        expect(rule).to be_eligible(order)
+      end
+    end
+
+    context "and quantity is higher than the prefered maximum quantity" do
+      before { allow(order).to receive_messages quantity: 15 }
+
+      it "should not be eligible" do
+        expect(rule).to_not be_eligible(order)
+      end
+
+      it "set an error message" do
+        rule.eligible?(order)
+        expect(rule.eligibility_errors.full_messages.first).
+          to eq "This coupon code can't be applied to orders with items quantity higher than 10."
+      end
+    end
+  end
+end
