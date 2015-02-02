3eb0042261e63bf74cdda82baa329425881ea2ad
Peter Berkenbosch <peter@pero-ict.nl>
2015-02-02 22:20:35 +0100

    no more environment column for payment_methods

--- core/db/migrate/20150122150102_create_spree_payment_method_store_credits.rb
+++ core/db/migrate/20150122150102_create_spree_payment_method_store_credits.rb
@@ -1,6 +1,6 @@
 class CreateSpreePaymentMethodStoreCredits < ActiveRecord::Migration
   def up
     return if Spree::PaymentMethod.find_by_type("Spree::PaymentMethod::StoreCredit")
-    Spree::PaymentMethod.create(type: "Spree::PaymentMethod::StoreCredit", name: "Store Credit", description: "Store credit.", active: true, environment: Rails.env)
+    Spree::PaymentMethod.create(type: "Spree::PaymentMethod::StoreCredit", name: "Store Credit", description: "Store credit.", active: true)
   end
 end
