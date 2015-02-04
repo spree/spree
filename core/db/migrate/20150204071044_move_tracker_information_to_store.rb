4839514524c60cd443a7e546e313ed024aa7f8f9
Ben A Morgan <ben@benmorgan.io>
2015-02-03 23:15:31 -0800

    add migration to copy tracker data

---
+++ core/db/migrate/20150204071044_move_tracker_information_to_store.rb
@@ -0,0 +1,10 @@
+class MoveTrackerInformationToStore < ActiveRecord::Migration
+  def up
+    analytics_id = Spree::Tracker.current.analytics_id
+    Spree::Store.set_preference :tracker, analytics_id
+  end
+
+  def down
+    Spree::Store.set_preference :tracker, nil
+  end
+end
