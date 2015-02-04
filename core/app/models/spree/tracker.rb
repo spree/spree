873fdb5960a5e261a71e959b2191e838c4cd3905
Ben A Morgan <ben@benmorgan.io>
2015-02-03 23:06:19 -0800

    add a deprecation warning for the spree tracker model

--- core/app/models/spree/tracker.rb
+++ core/app/models/spree/tracker.rb
@@ -1,6 +1,10 @@
 module Spree
   class Tracker < Spree::Base
     def self.current
+      ActiveSupport::Deprecation.warn(<<-EOS, caller)
+        \nThe Spree::Tracker model will be removed. To obtain the tracker, you can use either
+        `current_store.tracker' or `Spree::Store.current.tracker'.
+      EOS
       tracker = where(active: true).first
       tracker.analytics_id.present? ? tracker : nil if tracker
     end
