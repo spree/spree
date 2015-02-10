11acff3bdc6997408086d0afbdd0dd03d01777fd
Rein Aris <info@reinaris.nl>
2015-02-10 10:38:17 +0000

    Improved code quality states javascript and addresses controller

--- backend/app/assets/javascripts/spree/backend/checkouts/edit.js
+++ backend/app/assets/javascripts/spree/backend/checkouts/edit.js
@@ -45,11 +45,11 @@ $(document).ready(function() {
           $('#order_bill_address_attributes_phone').val(billAddress.phone);
           $('#order_bill_address_attributes_state_name').val(billAddress.state_name);
 
-          $('#order_bill_address_attributes_country_id').select2("val", billAddress.country_id).promise().done(function () {
-            requestStates($('#order_bill_address_attributes_state_id').parents(".js-country-states"));
+          $('#order_bill_address_attributes_country_id').select2('val', billAddress.country_id).promise().done(function () {
+            requestStates($('#order_bill_address_attributes_state_id').parents('.js-country-states'));
           });
 
-          $(".js-change-customer-bill-address-alert").show();
+          $('.js-change-customer-bill-address-alert').show();
         }
         return customer.email;
       }
