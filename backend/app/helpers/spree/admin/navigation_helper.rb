797b20840a8a1eb44e967ed05b220c0cbbae0495
Rein Aris <info@reinaris.nl>
2015-02-18 14:40:26 +0100

    Be able to override the per_page_action in the per_page form
    Rails 4 syntax
    
    Conflicts:
    	backend/app/views/spree/admin/shared/_index_table_options.html.erb

--- backend/app/helpers/spree/admin/navigation_helper.rb
+++ backend/app/helpers/spree/admin/navigation_helper.rb
@@ -101,8 +101,8 @@ module Spree
 
       # helper method to create proper url to apply per page filtering
       # fixes https://github.com/spree/spree/issues/6888
-      def per_page_dropdown_params
-        args = params.clone
+      def per_page_dropdown_params(args = nil)
+        args ||= params.clone
         args.delete(:page)
         args.delete(:per_page)
         args
