8b6c3449173a406e2b0510bc2b6798ebf5b82cdb
Ben A Morgan <ben@benmorgan.io>
2015-02-28 20:41:28 -0500

    checkouts controller hounded

--- api/app/controllers/spree/api/v1/checkouts_controller.rb
+++ api/app/controllers/spree/api/v1/checkouts_controller.rb
@@ -48,52 +48,53 @@ module Spree
         end
 
         private
-          def user_id
-            params[:order][:user_id] if params[:order]
-          end
 
-          def nested_params
-            map_nested_attributes_keys Order, params[:order] || {}
-          end
+        def user_id
+          params[:order][:user_id] if params[:order]
+        end
 
-          # Should be overriden if you have areas of your checkout that don't match
-          # up to a step within checkout_steps, such as a registration step
-          def skip_state_validation?
-            false
-          end
+        def nested_params
+          map_nested_attributes_keys Order, params[:order] || {}
+        end
 
-          def load_order(lock = false)
-            @order = Spree::Order.lock(lock).find_by!(number: params[:id])
-            raise_insufficient_quantity and return if @order.insufficient_stock_lines.present?
-            @order.state = params[:state] if params[:state]
-            state_callback(:before)
-          end
+        # Should be overriden if you have areas of your checkout that don't match
+        # up to a step within checkout_steps, such as a registration step
+        def skip_state_validation?
+          false
+        end
 
-          def raise_insufficient_quantity
-            respond_with(@order, default_template: 'spree/api/v1/orders/insufficient_quantity')
-          end
+        def load_order(lock = false)
+          @order = Spree::Order.lock(lock).find_by!(number: params[:id])
+          raise_insufficient_quantity and return if @order.insufficient_stock_lines.present?
+          @order.state = params[:state] if params[:state]
+          state_callback(:before)
+        end
 
-          def state_callback(before_or_after = :before)
-            method_name = :"#{before_or_after}_#{@order.state}"
-            send(method_name) if respond_to?(method_name, true)
-          end
+        def raise_insufficient_quantity
+          respond_with(@order, default_template: 'spree/api/v1/orders/insufficient_quantity')
+        end
 
-          def after_update_attributes
-            if nested_params && nested_params[:coupon_code].present?
-              handler = PromotionHandler::Coupon.new(@order).apply
+        def state_callback(before_or_after = :before)
+          method_name = :"#{before_or_after}_#{@order.state}"
+          send(method_name) if respond_to?(method_name, true)
+        end
 
-              if handler.error.present?
-                @coupon_message = handler.error
-                respond_with(@order, default_template: 'spree/api/v1/orders/could_not_apply_coupon')
-                return true
-              end
+        def after_update_attributes
+          if nested_params && nested_params[:coupon_code].present?
+            handler = PromotionHandler::Coupon.new(@order).apply
+
+            if handler.error.present?
+              @coupon_message = handler.error
+              respond_with(@order, default_template: 'spree/api/v1/orders/could_not_apply_coupon')
+              return true
             end
-            false
           end
+          false
+        end
 
-          def order_id
-            super || params[:id]
-          end
+        def order_id
+          super || params[:id]
+        end
       end
     end
   end
