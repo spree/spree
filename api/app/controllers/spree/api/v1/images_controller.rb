c60427595f77ffb1cf09b8cb07ab5d0e17aa3471
Ben A Morgan <ben@benmorgan.io>
2015-02-28 19:12:28 -0500

    images controller hounded

--- api/app/controllers/spree/api/v1/images_controller.rb
+++ api/app/controllers/spree/api/v1/images_controller.rb
@@ -2,7 +2,6 @@ module Spree
   module Api
     module V1
       class ImagesController < Spree::Api::BaseController
-
         def index
           @images = scope.images.accessible_by(current_ability, :read)
           respond_with(@images)
@@ -16,33 +15,34 @@ module Spree
         def create
           authorize! :create, Image
           @image = scope.images.create(image_params)
-          respond_with(@image, :status => 201, :default_template => :show)
+          respond_with(@image, status: 201, default_template: :show)
         end
 
         def update
           @image = scope.images.accessible_by(current_ability, :update).find(params[:id])
           @image.update_attributes(image_params)
-          respond_with(@image, :default_template => :show)
+          respond_with(@image, default_template: :show)
         end
 
         def destroy
           @image = scope.images.accessible_by(current_ability, :destroy).find(params[:id])
           @image.destroy
-          respond_with(@image, :status => 204)
+          respond_with(@image, status: 204)
         end
 
         private
-          def image_params
-            params.require(:image).permit(permitted_image_attributes)
-          end
 
-          def scope
-            if params[:product_id]
-              scope = Spree::Product.friendly.find(params[:product_id])
-            elsif params[:variant_id]
-              scope = Spree::Variant.find(params[:variant_id])
-            end
+        def image_params
+          params.require(:image).permit(permitted_image_attributes)
+        end
+
+        def scope
+          if params[:product_id]
+            Spree::Product.friendly.find(params[:product_id])
+          elsif params[:variant_id]
+            Spree::Variant.find(params[:variant_id])
           end
+        end
       end
     end
   end
