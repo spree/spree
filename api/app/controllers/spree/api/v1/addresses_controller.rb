012d24d03d690aae509731574e80b15e58c61889
Ben A Morgan <ben@benmorgan.io>
2015-02-28 19:11:06 -0500

    address controller hounded

--- api/app/controllers/spree/api/v1/addresses_controller.rb
+++ api/app/controllers/spree/api/v1/addresses_controller.rb
@@ -6,13 +6,13 @@ module Spree
 
         def show
           authorize! :read, @order, order_token
-          find_address
+          @address = find_address
           respond_with(@address)
         end
 
         def update
           authorize! :update, @order, order_token
-          find_address
+          @address = find_address
 
           if @address.update_attributes(address_params)
             respond_with(@address, :default_template => :show)
@@ -22,23 +22,24 @@ module Spree
         end
 
         private
-          def address_params
-            params.require(:address).permit(permitted_address_attributes)
-          end
 
-          def find_order
-            @order = Spree::Order.find_by!(number: order_id)
-          end
+        def address_params
+          params.require(:address).permit(permitted_address_attributes)
+        end
 
-          def find_address
-            @address = if @order.bill_address_id == params[:id].to_i
-              @order.bill_address
-            elsif @order.ship_address_id == params[:id].to_i
-              @order.ship_address
-            else
-              raise CanCan::AccessDenied
-            end
+        def find_order
+          @order = Spree::Order.find_by!(number: order_id)
+        end
+
+        def find_address
+          if @order.bill_address_id == params[:id].to_i
+            @order.bill_address
+          elsif @order.ship_address_id == params[:id].to_i
+            @order.ship_address
+          else
+            raise CanCan::AccessDenied
           end
+        end
       end
     end
   end
