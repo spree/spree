2962ca2036ece87def75c287d9a393ac194c9fc6
Ben A Morgan <ben@benmorgan.io>
2015-02-28 19:06:39 -0500

    specs for the redirect

--- api/spec/requests/version_spec.rb
+++ api/spec/requests/version_spec.rb
@@ -4,8 +4,15 @@ describe "Version", type: :request do
   let!(:countries) { 2.times.map { create :country } }
 
   describe "/api" do
-    it "can render" do
+    it "be a redirect" do
       get "/api/countries"
+      expect(response).to have_http_status 301
+    end
+  end
+
+  describe "/api/v1" do
+    it "be successful" do
+      get "/api/v1/countries"
       expect(response).to have_http_status 200
     end
   end
