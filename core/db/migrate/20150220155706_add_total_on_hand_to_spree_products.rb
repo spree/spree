140f56bc6735258a0001d0e50ffea6d0a69eca44
Bryan Mahoney <bryan@goDynamo.com>
2015-03-04 10:04:00 -0500

    corrects migration

--- core/db/migrate/20150220155706_add_total_on_hand_to_spree_products.rb
+++ core/db/migrate/20150220155706_add_total_on_hand_to_spree_products.rb
@@ -1,10 +1,10 @@
 class AddTotalOnHandToSpreeProducts < ActiveRecord::Migration
   def change
     add_column :spree_products, :count_on_hand, :integer, null: false, default: 0
+    # seed the counts
+    Spree::Variant.all.each { |v| v.update_counter_cache }
+    Spree::Product.all.each { |v| v.update_counter_cache }
   end
 
-  # seed the counts
-  Spree::Variant.all.each { |v| v.update_counter_cache }
-  Spree::Product.all.each { |v| v.update_counter_cache }
 
 end
