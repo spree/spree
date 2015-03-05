77394439ad70c7288846ef556848c0bccd0d60ae
Alexander Nеgоdа <alexander.negoda@gmail.com>
2015-03-05 21:29:43 +0300

    Active Record Deprecated Finders for taxons

--- sample/db/samples/taxons.rb
+++ sample/db/samples/taxons.rb
@@ -139,7 +139,7 @@ taxons = [
 
 taxons.each do |taxon_attrs|
   if taxon_attrs[:parent]
-    taxon_attrs[:parent] = Spree::Taxon.find_by_name!(taxon_attrs[:parent])
+    taxon_attrs[:parent] = Spree::Taxon.where(name: taxon_attrs[:parent]).first
     Spree::Taxon.create!(taxon_attrs)
   end
 end
