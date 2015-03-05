3c2275b7591e6c14a2e50e6ea49b14b4d30abfb9
Alexander Nеgоdа <alexander.negoda@gmail.com>
2015-03-05 21:29:43 +0300

    Active Record Deprecated Finders for taxons
    Active Record Deprecated Finders for prototypes
    
    Fixes #6120

--- sample/db/samples/prototypes.rb
+++ sample/db/samples/prototypes.rb
@@ -16,6 +16,6 @@ prototypes = [
 prototypes.each do |prototype_attrs|
   prototype = Spree::Prototype.create!(:name => prototype_attrs[:name])
   prototype_attrs[:properties].each do |property|
-    prototype.properties << Spree::Property.find_by_name!(property)
+    prototype.properties << Spree::Property.where(name: property).first
   end
 end
