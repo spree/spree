2e0c7ec1cb1388adc5ed7928ef547cbf000d49e4
Jeff Dutil <JDutil@BurlingtonWebApps.com>
2015-03-09 12:46:48 -0400

    Remove unused & unspec'd method.

--- core/app/models/spree/zone_member.rb
+++ core/app/models/spree/zone_member.rb
@@ -2,10 +2,5 @@ module Spree
   class ZoneMember < Spree::Base
     belongs_to :zone, class_name: 'Spree::Zone', counter_cache: true, inverse_of: :zone_members
     belongs_to :zoneable, polymorphic: true
-
-    def name
-      return nil if zoneable.nil?
-      zoneable.name
-    end
   end
 end
