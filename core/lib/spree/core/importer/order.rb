625b42ecc2c3d5c6d0ec463a8f718ce16b80d89a
Michael Lippold <michael.lippold@smartacus.net>
2015-03-11 21:50:08 -0700

    Use InventoryUnitBuilder in order importer
    Match shipments up to InventoryUnits
    
    Fixes #6159

--- core/lib/spree/core/importer/order.rb
+++ core/lib/spree/core/importer/order.rb
@@ -49,44 +49,45 @@ module Spree
         def self.create_shipments_from_params(shipments_hash, order)
           return [] unless shipments_hash
 
-          line_items = order.line_items
+          inventory_units = Spree::Stock::InventoryUnitBuilder.new(order).units
+
           shipments_hash.each do |s|
             begin
               shipment = order.shipments.build
               shipment.tracking       = s[:tracking]
               shipment.stock_location = Spree::StockLocation.find_by_admin_name(s[:stock_location]) || Spree::StockLocation.find_by_name!(s[:stock_location])
 
-              inventory_units = s[:inventory_units] || []
-              inventory_units.each do |iu|
-                ensure_variant_id_from_params(iu)
+              shipment_units = s[:inventory_units] || []
+              shipment_units.each do |su|
+                ensure_variant_id_from_params(su)
 
-                unit = shipment.inventory_units.build
-                unit.order = order
+                inventory_unit = inventory_units.detect { |iu| iu.variant_id.to_i == su[:variant_id].to_i }
 
-                # Spree expects a Inventory Unit to always reference a line
-                # item and variant otherwise users might get exceptions when
-                # trying to view these units. Note the Importer might not be
-                # able to find the line item if line_item.variant_id |= iu.variant_id
-                unit.variant_id = iu[:variant_id]
-                unit.line_item_id = line_items.select do |l|
-                  l.variant_id.to_i == iu[:variant_id].to_i
-                end.first.try(:id)
+                if inventory_unit.present?
+                  inventory_unit.shipment = shipment
+
+                  if s[:shipped_at].present?
+                    inventory_unit.pending = false
+                    inventory_unit.state = 'shipped'
+                  end
+
+                  inventory_unit.save!
+
+                  # Don't assign shipments to this inventory unit more than once
+                  inventory_units.delete(inventory_unit)
+                end
               end
 
-              # Mark shipped if it should be.
               if s[:shipped_at].present?
                 shipment.shipped_at = s[:shipped_at]
                 shipment.state      = 'shipped'
-                shipment.inventory_units.each do |unit|
-                  unit.state = 'shipped'
-                end
               end
 
               shipment.save!
 
               shipping_method = Spree::ShippingMethod.find_by_name(s[:shipping_method]) || Spree::ShippingMethod.find_by_admin_name!(s[:shipping_method])
-              rate = shipment.shipping_rates.create!(:shipping_method => shipping_method,
-                                                     :cost => s[:cost])
+              rate = shipment.shipping_rates.create!(shipping_method: shipping_method, cost: s[:cost])
+
               shipment.selected_shipping_rate_id = rate.id
               shipment.update_amounts
 
