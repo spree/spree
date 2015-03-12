4ed9be11051de0921ef0911516c85081fbb8c77a
Jeff Dutil <JDutil@BurlingtonWebApps.com>
2015-03-12 12:42:47 -0400

    Product properties act as list. Fixes #6157

--- core/app/models/spree/product_property.rb
+++ core/app/models/spree/product_property.rb
@@ -1,5 +1,7 @@
 module Spree
   class ProductProperty < Spree::Base
+    acts_as_list scope: :product
+
     belongs_to :product, touch: true, class_name: 'Spree::Product', inverse_of: :product_properties
     belongs_to :property, class_name: 'Spree::Property', inverse_of: :product_properties
 
