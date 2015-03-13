398a1e89eca801d5b31496c50cc9e2c5b60177ef
Francisco Trindade <frank.trindade@gmail.com>
2015-03-13 16:57:04 -0300

    added spec to test returning complete and incomplete orders;

--- backend/spec/features/admin/orders/listing_spec.rb
+++ backend/spec/features/admin/orders/listing_spec.rb
@@ -58,6 +58,16 @@ describe "Orders Listing", type: :feature, js: true do
       within("table#listing_orders") { expect(page).not_to have_content("R100") }
     end
 
+    it "should return both complete and incomplete orders when only complete orders is not checked" do
+      Spree::Order.create! email: "incomplete@example.com", completed_at: nil, state: 'cart'
+      click_on 'Filter'
+      uncheck "q_completed_at_not_null"
+      click_on 'Filter Results'
+
+      expect(page).to have_content("R200")
+      expect(page).to have_content("incomplete@example.com")
+    end
+
     it "should be able to filter risky orders" do
       click_on 'Filter'
       # Check risky and filter
