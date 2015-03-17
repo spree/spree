032f6d81204fdcd24f2593f00f5fef8fd34bf99d
Washington Luiz <huoxito@gmail.com>
2015-03-17 14:45:22 -0300

    Need to permit an array for product options ids

--- core/lib/spree/permitted_attributes.rb
+++ core/lib/spree/permitted_attributes.rb
@@ -61,7 +61,9 @@ module Spree
       :meta_keywords, :price, :sku, :deleted_at, :prototype_id,
       :option_values_hash, :weight, :height, :width, :depth,
       :shipping_category_id, :tax_category_id,
-      :taxon_ids, :option_type_ids, :cost_currency, :cost_price]
+      :taxon_ids, :cost_currency, :cost_price,
+      option_type_ids: []
+    ]
 
     @@property_attributes = [:name, :presentation]
 
@@ -101,8 +103,10 @@ module Spree
 
     @@variant_attributes = [
       :name, :presentation, :cost_price, :lock_version,
-      :position, :option_value_ids, :track_inventory,
+      :position, :track_inventory,
       :product_id, :product, :option_values_attributes, :price,
-      :weight, :height, :width, :depth, :sku, :cost_currency, options: [ :name, :value ]]
+      :weight, :height, :width, :depth, :sku, :cost_currency,
+      options: [:name, :value], option_value_ids: []
+    ]
   end
 end
