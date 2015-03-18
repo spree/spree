3e8245ea467cfa1efa605c21f43e792d67f0cb2c
Philip Arndt <git@p.arndt.io>
2015-03-18 17:19:07 +1300

    Removed accidentally committed # binding.pry
    This was introduced in 08bed4f0, seems like by mistake.
    
    Fixes #6198

--- frontend/spec/spec_helper.rb
+++ frontend/spec/spec_helper.rb
@@ -97,7 +97,6 @@ RSpec.configure do |config|
   config.after(:each, :type => :feature) do |example|
     missing_translations = page.body.scan(/translation missing: #{I18n.locale}\.(.*?)[\s<\"&]/)
     if missing_translations.any?
-      #binding.pry
       puts "Found missing translations: #{missing_translations.inspect}"
       puts "In spec: #{example.location}"
     end
