f5c82862966d65a674129fdc09198908ada16bbf
brchristian <brchristian@gmail.com>
2015-03-30 19:23:08 -0700

    Fix migration for nil orders
    I noticed that this migration crashes when `adjustable.order = nil`. Adding the following conditional will enable the migration to avoid these crashes.

--- core/db/migrate/20141101231208_fix_adjustment_order_presence.rb
+++ core/db/migrate/20141101231208_fix_adjustment_order_presence.rb
@@ -5,7 +5,7 @@ class FixAdjustmentOrderPresence < ActiveRecord::Migration
       adjustable = adjustment.adjustable
       if adjustable.is_a? Spree::Order
         adjustment.update_attributes!(order_id: adjustable.id)
-      else
+      elsif adjustable && adjustable.order
         adjustment.update_attributes!(adjustable: adjustable.order)
       end
     end
