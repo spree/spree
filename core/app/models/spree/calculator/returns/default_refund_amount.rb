207b6955303585779f52707934aaab1280695a92
Martin Meyerhoff <mamhoff@googlemail.com>
2015-04-03 16:43:37 +0200

    Correct indentation on calculator default_refund_amount.rb

--- core/app/models/spree/calculator/returns/default_refund_amount.rb
+++ core/app/models/spree/calculator/returns/default_refund_amount.rb
@@ -24,13 +24,13 @@ module Spree
       end
 
       def percentage_of_order_total(inventory_unit)
-       return 0.0 if inventory_unit.order.pre_tax_item_amount.zero?
-       weighted_line_item_pre_tax_amount(inventory_unit) / inventory_unit.order.pre_tax_item_amount
-     end
+        return 0.0 if inventory_unit.order.pre_tax_item_amount.zero?
+        weighted_line_item_pre_tax_amount(inventory_unit) / inventory_unit.order.pre_tax_item_amount
+      end
 
-     def percentage_of_line_item(inventory_unit)
-       1 / BigDecimal.new(inventory_unit.line_item.quantity)
-     end
-   end
- end
+      def percentage_of_line_item(inventory_unit)
+        1 / BigDecimal.new(inventory_unit.line_item.quantity)
+      end
+    end
+  end
 end
