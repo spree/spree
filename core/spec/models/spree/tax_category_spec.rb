d72072f14f2de1ad097d7083674a922cba86770b
Martin Meyerhoff <mamhoff@googlemail.com>
2015-04-03 17:54:54 +0200

    Add `TaxCategory#has_vat_rate_for_default_zone?`

--- core/spec/models/spree/tax_category_spec.rb
+++ core/spec/models/spree/tax_category_spec.rb
@@ -24,4 +24,35 @@ describe Spree::TaxCategory, :type => :model do
       expect(tax_category.is_default).to be true
     end
   end
+
+  context "#has_vat_rate_for_default_zone?" do
+    let(:zone) { create(:zone_with_country, default_tax: true) }
+    let(:tax_category) { create(:tax_category) }
+
+    before do
+      allow(Spree::Zone).to receive(:default_tax).and_return(zone)
+    end
+
+    subject {tax_category.has_vat_rate_for_default_zone? }
+
+    context "when there is no vat rate in the default zone" do
+      it "returns false" do
+        expect(subject).to eq(false)
+      end
+    end
+
+    context "when ther is a vat rate in the default zone" do
+      before do
+        create(
+          :tax_rate,
+          included_in_price: true,
+          zone: zone,
+          tax_category: tax_category
+        )
+      end
+      it "returns true" do
+        expect(subject).to eq(true)
+      end
+    end
+  end
 end
