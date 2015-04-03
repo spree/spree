9536bbac0b2929afea24f5c32814dd78f958c419
Martin Meyerhoff <mamhoff@googlemail.com>
2015-04-03 17:22:16 +0200

    Add a `#display_price_adding_vat` for `Spree::Price`
    It uses the default zone or the order's zone to choose and apply
    a VAT to the price of a a variant.

--- core/app/models/spree/price.rb
+++ core/app/models/spree/price.rb
@@ -22,12 +22,30 @@ module Spree
       self[:amount] = Spree::LocalizedNumber.parse(price)
     end
 
+    # This method shows the price with VAT if the variant has a VAT tax in the default zone.
+    def form_field_price
+      amount * (1 + included_tax_amount)
+    end
+
+    def display_price_adding_vat_for(order)
+      zone = order ? order.tax_zone : default_zone
+      Spree::Money.new(amount * (1 + included_tax_amount(zone)), currency: currency)
+    end
+
     # Remove variant default_scope `deleted_at: nil`
     def variant
       Spree::Variant.unscoped { super }
     end
 
+    def included_tax_amount(zone = default_zone)
+      Spree::TaxRate.included_tax_amount_for(zone, variant.tax_category)
+    end
+
     private
+
+    def default_zone
+      Spree::Zone.default_tax
+    end
 
     def check_price
       self.currency ||= Spree::Config[:currency]
--- core/spec/models/spree/price_spec.rb
+++ core/spec/models/spree/price_spec.rb
@@ -39,4 +39,93 @@ describe Spree::Price, :type => :model do
       it { is_expected.to be_valid }
     end
   end
+
+  # For correct VAT calculation, we have to be able to save prices to six digits.
+  describe 'precision' do
+    let(:amount) { "7.94356" }
+    let(:variant) { create(:variant) }
+    before do
+      @price = subject
+      @price.amount = amount
+      @price.variant_id = variant.id
+      @price.save
+    end
+
+    it 'retains high precision input' do
+      expect(@price.reload.amount).to eq(BigDecimal.new(amount))
+    end
+  end
+
+  # Used to display a price including the correct VAT
+  # for a particular order.
+  describe "#display_price_adding_vat_for(order)" do
+    let(:amount) { "10.0" }
+    let(:variant) { create(:variant) }
+    let(:current_order) {Spree::Order.new}
+    let(:price) { Spree::Price.new variant: variant, amount: amount }
+    let(:zone) { create(:zone_with_country) }
+    let(:vat) do
+      create(
+        :tax_rate,
+        included_in_price: true,
+        zone: zone,
+        tax_category: variant.tax_category,
+        amount: 0.2
+      )
+    end
+    let(:default_zone) { create(:zone_with_country, default_tax: true) }
+    let(:default_vat) do
+      create(
+        :tax_rate,
+        included_in_price: true,
+        zone: default_zone,
+        tax_category: variant.tax_category,
+        amount: 0.3
+      )
+    end
+
+    subject { price.display_price_adding_vat_for(current_order) }
+
+    context "with no default VAT" do
+      context "with no specific order tax zone" do
+        it "displays the price without vat" do
+          expect(subject).to eq(Spree::Money.new(10))
+        end
+      end
+
+      context "with a VAT in the orders zone" do
+        before do
+          expect(current_order).to receive(:tax_zone).and_return(zone)
+          vat
+        end
+
+        it "displays the price including vat" do
+          expect(subject).to eq(Spree::Money.new(12))
+        end
+      end
+    end
+
+    context "with a default VAT" do
+      before do
+        default_vat
+      end
+
+      context "with no specific order tax zone" do
+        it "displays the price without vat" do
+          expect(subject).to eq(Spree::Money.new(13))
+        end
+      end
+
+      context "with a VAT in the orders zone" do
+        before do
+          expect(current_order).to receive(:tax_zone).and_return(zone)
+          vat
+        end
+
+        it "displays the price including vat" do
+          expect(subject).to eq(Spree::Money.new(12))
+        end
+      end
+    end
+  end
 end
