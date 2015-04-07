28d48aae826ec28f7d53ed68b8248af3710b778a
yurifrl <yurifrl@hotmail.com>
2015-04-07 14:49:55 -0300

    added ams

--- api/lib/spree/api/engine.rb
+++ api/lib/spree/api/engine.rb
@@ -6,16 +6,6 @@ module Spree
       isolate_namespace Spree
       engine_name 'spree_api'
 
-      Rabl.configure do |config|
-        config.include_json_root = false
-        config.include_child_root = false
-
-        # Motivation here it make it call as_json when rendering timestamps
-        # and therefore display miliseconds. Otherwise it would fall to
-        # JSON.dump which doesn't display the miliseconds
-        config.json_engine = ActiveSupport::JSON
-      end
-
       config.view_versions = [1]
       config.view_version_extraction_strategy = :http_header
 
