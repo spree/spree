d81e9cd5e5b8e074115f95059c528b26d73db1a9
Dan Kubb <dan.kubb@gmail.com>
2015-04-07 11:53:07 -0700

    Change Spree::Payment log_entries to be removed on destroy

--- core/app/models/spree/payment.rb
+++ core/app/models/spree/payment.rb
@@ -12,7 +12,7 @@ module Spree
     belongs_to :payment_method, class_name: 'Spree::PaymentMethod', inverse_of: :payments
 
     has_many :offsets, -> { offset_payment }, class_name: "Spree::Payment", foreign_key: :source_id, as: :source
-    has_many :log_entries, as: :source, dependent: :restrict_with_exception
+    has_many :log_entries, as: :source, dependent: :destroy
     has_many :state_changes, as: :stateful, dependent: :destroy
     has_many :capture_events, :class_name => 'Spree::PaymentCaptureEvent'
     has_many :refunds, inverse_of: :payment
