c2c9c991baf80086e1b64b8c644f4fca29cd1058
yurifrl <yurifrl@hotmail.com>
2015-04-08 15:05:30 -0300

    return money class default

--- api/app/controllers/spree/api/config_controller.rb
+++ api/app/controllers/spree/api/config_controller.rb
@@ -1,6 +1,13 @@
 module Spree
   module Api
     class ConfigController < Spree::Api::BaseController
+      def money
+        render json: ::Money.new(1, Spree::Config[:currency])
+      end
+
+      def show
+        render json: { default_country_id: Spree::Config[:default_country_id] }
+      end
     end
   end
 end
