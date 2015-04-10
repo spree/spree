36ee0653581551baa1c282a29e72118ace741c07
Dan Kubb <dan.kubb@gmail.com>
2015-04-09 22:55:48 -0700

    Refactor Spree::Order#merge!
    * To return self
    * Change the order to be destroyed as soon as it is no longer used.
    * Remove optional user argument from Spree::Order#merge!

--- core/app/models/spree/order_merger.rb
+++ core/app/models/spree/order_merger.rb
@@ -7,54 +7,13 @@ module Spree
       @order = order
     end
 
-    def merge!(other_order, user = nil)
-      other_order.line_items.each do |other_order_line_item|
-        next unless other_order_line_item.currency == order.currency
-
-        current_line_item = find_matching_line_item(other_order_line_item)
-        handle_merge(current_line_item, other_order_line_item)
+    def merge!(other)
+      other.line_items.each do |line_item|
+        next unless line_item.currency == order.currency
+        order.contents.add(line_item.variant, line_item.quantity)
       end
-
-      set_user(user)
-      persist_merge
-
-      # So that the destroy doesn't take out line items which may have been re-assigned
-      other_order.line_items.reload
-      other_order.destroy
-    end
-
-    # Compare the line item of the other order with mine.
-    def find_matching_line_item(other_order_line_item)
-      order.line_items.detect do |my_li|
-        my_li.variant == other_order_line_item.variant
-      end
-    end
-
-    def set_user(user = nil)
-      order.associate_user!(user) if !order.user && !user.blank?
-    end
-
-    # The idea is the end developer can choose to override the merge
-    # to their own choosing. Default is merge with errors.
-    def handle_merge(current_line_item, other_order_line_item)
-      if current_line_item
-        current_line_item.quantity += other_order_line_item.quantity
-        handle_error(current_line_item) unless current_line_item.save
-      else
-        other_order_line_item.order_id = order.id
-        handle_error(other_order_line_item) unless other_order_line_item.save
-      end
-    end
-
-    # Change the error messages as you choose.
-    def handle_error(line_item)
-      order.errors[:base] << line_item.errors.full_messages
-    end
-
-    def persist_merge
-      updater.update_item_count
-      updater.update_item_total
-      updater.persist_totals
+      other.destroy
+      order
     end
   end
 end
--- core/spec/models/spree/order_merger_spec.rb
+++ core/spec/models/spree/order_merger_spec.rb
@@ -9,73 +9,62 @@ module Spree
     let(:user) { stub_model(Spree::LegacyUser, email: "spree@example.com") }
     let(:subject) { Spree::OrderMerger.new(order_1) }
 
-    it "destroys the other order" do
-      subject.merge!(order_2)
-      expect { order_2.reload }.to raise_error(ActiveRecord::RecordNotFound)
-    end
+    context "#merge!" do
+      let(:variant) { create(:variant) }
+      let(:order_1) { Spree::Order.create! }
+      let(:order_2) { Spree::Order.create! }
 
-    it "persist the merge" do
-      expect(subject).to receive(:persist_merge)
-      subject.merge!(order_2)
-    end
+      shared_examples '#merge!' do
+        it "destroys the other order" do
+          order_1.merge!(order_2)
+          expect { order_2.reload }.to raise_error(ActiveRecord::RecordNotFound)
+        end
 
-    context "user is provided" do
-      it "assigns user to new order" do
-        subject.merge!(order_2, user)
-        expect(order_1.user).to eq user
-      end
-    end
-
-    context "merging together two orders with line items for the same variant" do
-      before do
-        order_1.contents.add(variant, 1)
-        order_2.contents.add(variant, 1)
+        it "returns target order" do
+          expect(order_1.merge!(order_2)).to be(order_1)
+        end
       end
 
-      specify do
-        subject.merge!(order_2, user)
-        expect(order_1.line_items.count).to eq(1)
+      context "merging together two orders with line items for the same variant" do
+        include_examples '#merge!'
 
-        line_item = order_1.line_items.first!
-        expect(line_item.quantity).to eq(2)
-        expect(line_item.variant_id).to eq(variant.id)
-      end
-    end
+        before do
+          order_1.contents.add(variant, 1)
+          order_2.contents.add(variant, 1)
+        end
 
-    context "merging together two orders with different line items" do
-      let(:variant_2) { create(:variant) }
+        specify do
+          order_1.merge!(order_2)
+          expect(order_1.line_items.count).to eq(1)
 
-      before do
-        order_1.contents.add(variant, 1)
-        order_2.contents.add(variant_2, 1)
-      end
-
-      specify do
-        subject.merge!(order_2)
-        line_items = order_1.line_items.reload
-        expect(line_items.count).to eq(2)
-
-        expect(order_1.item_count).to eq 2
-        expect(order_1.item_total).to eq line_items.map(&:amount).sum
-
-        # No guarantee on ordering of line items, so we do this:
-        expect(line_items.pluck(:quantity)).to match_array([1, 1])
-        expect(line_items.pluck(:variant_id)).to match_array([variant.id, variant_2.id])
-      end
-    end
-
-    context "merging together orders with invalid line items" do
-      let(:variant_2) { create(:variant) }
-
-      before do
-        order_1.contents.add(variant, 1)
-        order_2.contents.add(variant_2, 1)
+          line_item = order_1.line_items.first!
+          expect(line_item.quantity).to eq(2)
+          expect(line_item.variant_id).to eq(variant.id)
+        end
       end
 
-      it "should create errors with invalid line items" do
-        variant_2.destroy
-        subject.merge!(order_2)
-        expect(order_1.errors.full_messages).not_to be_empty
+      context "merging together two orders with different line items" do
+        include_examples '#merge!'
+
+        let(:variant_2) { create(:variant) }
+
+        before do
+          order_1.contents.add(variant, 1)
+          order_2.contents.add(variant_2, 1)
+        end
+
+        specify do
+          order_1.merge!(order_2)
+          line_items = order_1.line_items
+          expect(line_items.count).to eq(2)
+
+          expect(order_1.item_count).to eq 2
+          expect(order_1.item_total).to eq line_items.map(&:amount).sum
+
+          # No guarantee on ordering of line items, so we do this:
+          expect(line_items.pluck(:quantity)).to match_array([1, 1])
+          expect(line_items.pluck(:variant_id)).to match_array([variant.id, variant_2.id])
+        end
       end
     end
   end
