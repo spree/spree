0d8ada83b53dddb3ea758410744784d404fe1fa0
Jeff Dutil <JDutil@BurlingtonWebApps.com>
2015-04-13 11:39:19 -0400

    Use Spree.user_class instead of Spree::LegacyUser in spec helpers.

--- api/lib/spree/api/testing_support/setup.rb
+++ api/lib/spree/api/testing_support/setup.rb
@@ -4,7 +4,7 @@ module Spree
       module Setup
         def sign_in_as_admin!
           let!(:current_api_user) do
-            user = stub_model(Spree::LegacyUser)
+            user = stub_model(Spree.user_class)
             allow(user).to receive_message_chain(:spree_roles, :pluck).and_return(["admin"])
             allow(user).to receive(:has_spree_role?).with("admin").and_return(true)
             user
