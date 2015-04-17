89586c7e299614c54a79e662a51c1f15df0e4f6b
Manuel Martínez <vmmf08@gmail.com>
2015-04-17 12:53:37 -0500

    Avoid recursion on the create_line_items promotion action
    Fixed Hound CI formatting issues
    
    Fixes #6310

--- core/spec/models/spree/promotion/actions/create_line_items_spec.rb
+++ core/spec/models/spree/promotion/actions/create_line_items_spec.rb
@@ -7,26 +7,31 @@ describe Spree::Promotion::Actions::CreateLineItems, :type => :model do
   let(:shirt) { create(:variant) }
   let(:mug) { create(:variant) }
 
+  def empty_stock(variant)
+    variant.stock_items.update_all(backorderable: false)
+    variant.stock_items.each(&:reduce_count_on_hand_to_zero)
+  end
+
   context "#perform" do
     before do
-      allow(action).to receive_messages :promotion => promotion
+      allow(action).to receive_messages promotion: promotion
       action.promotion_action_line_items.create!(
-        :variant => mug,
-        :quantity => 1
+        variant: mug,
+        quantity: 1
       )
       action.promotion_action_line_items.create!(
-        :variant => shirt,
-        :quantity => 2
+        variant: shirt,
+        quantity: 2
       )
     end
 
     context "order is eligible" do
       before do
-        allow(promotion).to receive_messages :eligible => true
+        allow(promotion).to receive_messages eligible: true
       end
 
       it "adds line items to order with correct variant and quantity" do
-        action.perform(:order => order)
+        action.perform(order: order)
         expect(order.line_items.count).to eq(2)
         line_item = order.line_items.find_by_variant_id(mug.id)
         expect(line_item).not_to be_nil
@@ -35,7 +40,7 @@ describe Spree::Promotion::Actions::CreateLineItems, :type => :model do
 
       it "only adds the delta of quantity to an order" do
         order.contents.add(shirt, 1)
-        action.perform(:order => order)
+        action.perform(order: order)
         line_item = order.line_items.find_by_variant_id(shirt.id)
         expect(line_item).not_to be_nil
         expect(line_item.quantity).to eq(2)
@@ -43,11 +48,38 @@ describe Spree::Promotion::Actions::CreateLineItems, :type => :model do
 
       it "doesn't add if the quantity is greater" do
         order.contents.add(shirt, 3)
-        action.perform(:order => order)
+        action.perform(order: order)
         line_item = order.line_items.find_by_variant_id(shirt.id)
         expect(line_item).not_to be_nil
         expect(line_item.quantity).to eq(3)
       end
+
+      it "doesn't try to add an item if it's out of stock" do
+        empty_stock(mug)
+        empty_stock(shirt)
+
+        expect(order.contents).to_not receive(:add)
+        action.perform(order: order)
+      end
+    end
+  end
+
+  describe "#item_available?" do
+    let(:item_out_of_stock) do
+      action.promotion_action_line_items.create!(:variant => mug, :quantity => 1)
+    end
+
+    let(:item_in_stock) do
+      action.promotion_action_line_items.create!(:variant => shirt, :quantity => 1)
+    end
+
+    it "returns false if the item is out of stock" do
+      empty_stock(mug)
+      expect(action.item_available? item_out_of_stock).to be false
+    end
+
+    it "returns true if the item is in stock" do
+      expect(action.item_available? item_in_stock).to be true
     end
   end
 end
