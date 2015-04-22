856cf789714ac6d8dc453b72ac9271aa4717edb7
Jeff Dutil <JDutil@BurlingtonWebApps.com>
2015-04-22 14:35:27 -0400

    Touch product when taxon assigned. Fixes #6320

--- core/app/models/spree/classification.rb
+++ core/app/models/spree/classification.rb
@@ -2,7 +2,7 @@ module Spree
   class Classification < Spree::Base
     self.table_name = 'spree_products_taxons'
     acts_as_list scope: :taxon
-    belongs_to :product, class_name: "Spree::Product", inverse_of: :classifications
+    belongs_to :product, class_name: "Spree::Product", inverse_of: :classifications, touch: true
     belongs_to :taxon, class_name: "Spree::Taxon", inverse_of: :classifications, touch: true
 
     # For #3494
