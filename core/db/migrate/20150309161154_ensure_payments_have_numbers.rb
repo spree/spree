40fca0d8b3a4ee2f693c7e57cc76e91db78f5a6c
Matijs van Zuijlen <matijs@matijs.net>
2015-04-25 13:16:58 +0200

    Update only the payment's number during migration.
    Fixes #6327
    Fixes #6328

--- core/db/migrate/20150309161154_ensure_payments_have_numbers.rb
+++ core/db/migrate/20150309161154_ensure_payments_have_numbers.rb
@@ -2,7 +2,7 @@ class EnsurePaymentsHaveNumbers < ActiveRecord::Migration
   def change
     Spree::Payment.where(number: nil).find_each do |payment|
       payment.generate_number
-      payment.save
+      payment.update_columns(number: payment.number)
     end
   end
 end
