e8be7155bd820f646651f3f4a8799e5ef062997c
Michael Lippold <michael.lippold@smartacus.net>
2015-05-13 10:18:25 -0700

    Fixed null timestamp deprecation warnings

--- core/db/migrate/20150118211500_create_spree_store_credit_categories.rb
+++ core/db/migrate/20150118211500_create_spree_store_credit_categories.rb
@@ -2,7 +2,7 @@ class CreateSpreeStoreCreditCategories < ActiveRecord::Migration
   def change
     create_table :spree_store_credit_categories do |t|
       t.string :name
-      t.timestamps
+      t.timestamps null: false
     end
   end
 end
