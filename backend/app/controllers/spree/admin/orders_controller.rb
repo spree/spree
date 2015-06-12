02939b8faa4231e5688ccc024c31156e38c9b195
Darby <darby.perez@gmail.com>
2015-06-12 11:02:37 -0500

    Add user_id to order permitted params
    Allows orders to be created when user_id passed in

--- backend/app/controllers/spree/admin/orders_controller.rb
+++ backend/app/controllers/spree/admin/orders_controller.rb
@@ -128,7 +128,7 @@ module Spree
       private
         def order_params
           params[:created_by_id] = try_spree_current_user.try(:id)
-          params.permit(:created_by_id)
+          params.permit(:created_by_id, :user_id)
         end
 
         def load_order
