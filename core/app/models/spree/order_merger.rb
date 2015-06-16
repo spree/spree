386ae5df2c8537d4c0a6f4177abc8855a0499965
darbs <darby.perez@gmail.com>
2015-06-16 16:47:16 -0500

    Delete order merge to Spree::OrderMerge
    This creates a new class that handles merging different orders. Currently an invalid order
    is merged with a valid one then the entire merge fails. This is is frequently seen when a
    non-logged in user creates an order with valid line_items then logs in with a previous order
     that has now invalid line_items. E.g. Variant is deleted, combined quantity > available, etc.
     This resolves that by silently merging and appending the appropriate errors to the destination
     order.
    
    Refactor Spree::OrderMerge to follow original design flow
    
    Renamed Spree::OrderMerge to Spree::OrderMerger and follows the original merge!
    method better. An order has a merger that handles the merge between itself and some
    other order.
    
    Revert set_current_order
    
    Refactor order_specs into order_merger
    
    Moves the merge specs from order_spec into order_merger_spec.
    Reverts to controller_helper spec.
    
    hound fixes
    
    Use Ruby 1.9 syntax for hound
    
    Fix the missing variant in Spree::OrderMerger spec
    
    Fixes #5717
    Fixes #5976

---
+++ core/app/models/spree/order_merger.rb
@@ -0,0 +1,65 @@
+module Spree
+  class OrderMerger
+    attr_accessor :order
+    delegate :updater, to: :order
+
+    def initialize(order)
+      @order = order
+    end
+
+    def merge!(other_order, user = nil)
+      other_order.line_items.each do |other_order_line_item|
+        next unless other_order_line_item.currency == order.currency
+
+        current_line_item = find_matching_line_item(other_order_line_item)
+        handle_merge(current_line_item, other_order_line_item)
+      end
+
+      set_user(user)
+      persist_merge
+
+      # So that the destroy doesn't take out line items which may have been re-assigned
+      other_order.line_items.reload
+      other_order.destroy
+    end
+
+    # Compare the line item of the other order with mine.
+    # Make sure you allow any extensions to chime in on whether or
+    # not the extension-specific parts of the line item match
+    def find_matching_line_item(other_order_line_item)
+      order.line_items.detect do |my_li|
+        my_li.variant == other_order_line_item.variant &&
+          order.line_item_comparison_hooks.all? do |hook|
+            order.send(hook, my_li, other_order_line_item.serializable_hash)
+          end
+      end
+    end
+
+    def set_user(user = nil)
+      order.associate_user!(user) if !order.user && !user.blank?
+    end
+
+    # The idea is the end developer can choose to override the merge
+    # to their own choosing. Default is merge with errors.
+    def handle_merge(current_line_item, other_order_line_item)
+      if current_line_item
+        current_line_item.quantity += other_order_line_item.quantity
+        handle_error(current_line_item) unless current_line_item.save
+      else
+        other_order_line_item.order_id = order.id
+        handle_error(other_order_line_item) unless other_order_line_item.save
+      end
+    end
+
+    # Change the error messages as you choose.
+    def handle_error(line_item)
+      order.errors[:base] << line_item.errors.full_messages
+    end
+
+    def persist_merge
+      updater.update_item_count
+      updater.update_item_total
+      updater.persist_totals
+    end
+  end
+end
