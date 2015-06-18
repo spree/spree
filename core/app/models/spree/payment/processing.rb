6162d34cb3bdfbac3416e538356b611ad9328b56
Chris Dion <chris.dion@keatonrow.com>
2015-06-18 15:51:55 -0400

    log payment method response when payment is canceled. void payment when successfully canceled.

--- core/app/models/spree/payment/processing.rb
+++ core/app/models/spree/payment/processing.rb
@@ -69,7 +69,19 @@ module Spree
       end
 
       def cancel!
-        payment_method.cancel(response_code)
+        protect_from_connection_error do
+
+          response = payment_method.cancel(response_code)
+
+          record_response(response)
+
+          if response.success?
+            self.response_code = response.authorization
+            self.void
+          else
+            gateway_error(response)
+          end
+        end
       end
 
       def gateway_options
