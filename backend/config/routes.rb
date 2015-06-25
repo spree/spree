cfe67452b04f1d7888917ded917d428cf562bbcb
Rein Aris <info@reinaris.nl>
2015-06-25 16:15:42 +0100

    Improvements to the return screens
    Improved reimbursement screens

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -114,7 +114,7 @@ Spree::Core::Engine.add_routes do
       post :apply_promotion, to: "promotions#apply_to_order", as: :apply_promotion
       post :delete_promotion, to: "promotions#delete_from_order", as: :delete_promotion
 
-      resources :reimbursements, only: [:create, :show, :edit, :update] do
+      resources :reimbursements do
         member do
           post :perform
         end
