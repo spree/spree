1df7c02731a2cafc6d937bb0e51adbf1e8a647aa
Alessandro Lepore <lepore.ale@gmail.com>
2015-07-05 11:59:31 +0200

    add Base.spree_scopes method
    does nothing but can be overwritten with useful scopes

--- frontend/app/controllers/spree/products_controller.rb
+++ frontend/app/controllers/spree/products_controller.rb
@@ -15,7 +15,10 @@ module Spree
     end
 
     def show
-      @variants = @product.variants_including_master.active(current_currency).includes([:option_values, :images])
+      @variants = @product.variants_including_master.
+                           spree_base_scopes.
+                           active(current_currency).
+                           includes([:option_values, :images])
       @product_properties = @product.product_properties.includes(:property)
       @taxon = Spree::Taxon.find(params[:taxon_id]) if params[:taxon_id]
       redirect_if_legacy_path
