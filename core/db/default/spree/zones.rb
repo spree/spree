bee2d19e3e228bd5b91ec9cee6fafe1312a9305b
odk211 <tomoki.odaka@gmail.com>
2015-07-21 11:41:10 +0900

    to shorten zones definition

--- core/db/default/spree/zones.rb
+++ core/db/default/spree/zones.rb
@@ -1,12 +1,11 @@
 eu_vat = Spree::Zone.create!(name: "EU_VAT", description: "Countries that make up the EU VAT zone.")
 north_america = Spree::Zone.create!(name: "North America", description: "USA + Canada")
-["PL", "FI", "PT", "RO", "DE", "FR", "SK", "HU", "SI", "IE", "AT", "ES", "IT",
- "BE", "SE", "LV", "BG", "GB", "LT", "CY", "LU", "MT", "DK", "NL", "EE"].
+%w(PL FI PT RO DE FR SK HU SI IE AT ES IT BE SE LV BG GB LT CY LU MT DK NL EE).
 each do |name|
   eu_vat.zone_members.create!(zoneable: Spree::Country.find_by!(iso: name))
 end
 
-["US", "CA"].each do |name|
+%w(US CA).each do |name|
   north_america.zone_members.create!(zoneable: Spree::Country.find_by!(iso: name))
 end
 
