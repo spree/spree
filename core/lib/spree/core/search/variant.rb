9308b8daf65a51b8fbe559697adb37c1c955ab3d
Michael Lippold <michael@trialguides.com>
2015-07-21 17:03:06 -0700

    Refactor variant search method

--- core/lib/spree/core/search/variant.rb
+++ core/lib/spree/core/search/variant.rb
@@ -2,35 +2,48 @@ module Spree
   module Core
     module Search
       class Variant < Base
+        SEARCH_FIELDS = %w(option_values_name product_name sku)
+
         def initialize(params)
           super
           params[:scope] ||= Spree::Variant.all
-          params[:q] ||= ''
         end
 
         def search
-          # Syntax inferred from http://ransack-demo.herokuapp.com/users/advanced_search
-          query = { g: {} }
-          grouping_count = 0
-          fields = ['option_values_name', 'product_name', 'sku']
-          params[:q].split(' ').each do |part|
-            query[:g][grouping_count.to_s] = {
-              m: 'or',
-              c: {}
-            }
-            condition_count = 0
-            fields.each do |field|
-              query[:g][grouping_count.to_s][:c][condition_count.to_s] = {
-                a: { '0' => { name: field } },
-                p: 'cont',
-                v: { '0' => { value: part } }
-              }
-              condition_count += 1
-            end
-            grouping_count += 1
-          end
-
           params[:scope].ransack(query).result.page(params[:page]).per(params[:per_page])
+        end
+
+        private
+
+        def query_strings
+          (params[:q] || '').split(' ')
+        end
+
+        def query
+          { g: query_groups }
+        end
+
+        def query_groups
+          query_strings.map.with_index { |subquery, index| [index.to_s, query_group(subquery)] }.to_h
+        end
+
+        def query_group(subquery)
+          {
+            m: 'or',
+            c: conditions(subquery)
+          }
+        end
+
+        def conditions(subquery)
+          SEARCH_FIELDS.map.with_index { |search_field, index| [index.to_s, condition(search_field, subquery)] }.to_h
+        end
+
+        def condition(search_field, subquery)
+          {
+            a: { '0' => { name: search_field } },
+            p: 'cont',
+            v: { '0' => { value: subquery } }
+          }
         end
       end
     end
