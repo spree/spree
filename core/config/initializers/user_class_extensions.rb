b410caf83dd193b1f85c1a0f1ba572ed43c77e9a
Ben A. Morgan <ben@benmorgan.io>
2015-07-26 06:42:40 -0700

    fix HABTM with role_users

--- core/config/initializers/user_class_extensions.rb
+++ core/config/initializers/user_class_extensions.rb
@@ -5,13 +5,8 @@ Spree::Core::Engine.config.to_prepare do
       include Spree::UserPaymentSource
       include Spree::UserReporting
 
-      has_and_belongs_to_many :spree_roles,
-                              join_table: 'spree_roles_users',
-                              foreign_key: "user_id",
-                              class_name: "Spree::Role"
-
-      has_many :role_users, class_name: 'Spree::RoleUser'
-      has_many :spree_roles, through: :role_users, class_name: 'Spree::Role'
+      has_many :role_users, class_name: 'Spree::RoleUser', foreign_key: :user_id
+      has_many :spree_roles, through: :role_users, class_name: 'Spree::Role', source: :role
 
       has_many :orders, foreign_key: :user_id, class_name: "Spree::Order"
 
