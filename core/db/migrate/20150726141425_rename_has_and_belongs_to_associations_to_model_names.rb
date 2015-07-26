a5b2fa9602430956076b67d9e3ad4c564ee84499
Ben A. Morgan <ben@benmorgan.io>
2015-07-26 09:45:53 -0700

    spree_option_type_prototype should be plural

--- core/db/migrate/20150726141425_rename_has_and_belongs_to_associations_to_model_names.rb
+++ core/db/migrate/20150726141425_rename_has_and_belongs_to_associations_to_model_names.rb
@@ -1,7 +1,7 @@
 class RenameHasAndBelongsToAssociationsToModelNames < ActiveRecord::Migration
   def change
     {
-      'spree_option_types_prototypes' => 'spree_option_type_prototype',
+      'spree_option_types_prototypes' => 'spree_option_type_prototypes',
       'spree_option_values_variants' => 'spree_option_value_variants',
       'spree_orders_promotions' => 'spree_order_promotions',
       'spree_products_promotion_rules' => 'spree_product_promotion_rules',
