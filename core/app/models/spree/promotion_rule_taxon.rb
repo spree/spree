978c8bc82944ad3eddb5d6ab23586e1a8019fdac
Ben A. Morgan <ben@benmorgan.io>
2015-07-26 07:04:56 -0700

    update table name for promotion rule taxons

--- core/app/models/spree/promotion_rule_taxon.rb
+++ core/app/models/spree/promotion_rule_taxon.rb
@@ -1,6 +1,6 @@
 module Spree
   class PromotionRuleTaxon < Spree::Base
-    self.table_name = 'spree_promotion_rules_taxons'
+    self.table_name = 'spree_taxons_promotion_rules'
 
     belongs_to :promotion_rule, class_name: 'Spree::PromotionRule'
     belongs_to :taxons, class_name: 'Spree::Taxon'
