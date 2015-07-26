70ee5f350513bbfb7461678eca61b1f7b9448e7b
Ben A. Morgan <ben@benmorgan.io>
2015-07-26 06:42:25 -0700

    use Spree.user_class

--- core/app/models/spree/role_user.rb
+++ core/app/models/spree/role_user.rb
@@ -3,6 +3,6 @@ module Spree
     self.table_name = 'spree_roles_users'
 
     belongs_to :role, class_name: 'Spree::Role'
-    belongs_to :user, class_name: Spree::User.to_s
+    belongs_to :user, class_name: Spree.user_class.to_s
   end
 end
