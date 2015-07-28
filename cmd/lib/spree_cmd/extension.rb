64beed632627b73d33951dd59cc1a7b897d64f87
Jeff Dutil <JDutil@BurlingtonWebApps.com>
2015-07-28 12:26:45 -0400

    Bump to beta.

--- cmd/lib/spree_cmd/extension.rb
+++ cmd/lib/spree_cmd/extension.rb
@@ -50,7 +50,7 @@ module SpreeCmd
       end
 
       def spree_version
-        '2.2.13'
+        '2.2.14.beta'
       end
 
       def use_prefix(prefix)
--- core/lib/spree/core/version.rb
+++ core/lib/spree/core/version.rb
@@ -1,5 +1,5 @@
 module Spree
   def self.version
-    '2.2.13'
+    '2.2.14.beta'
   end
 end
