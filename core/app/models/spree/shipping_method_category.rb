8ad1242cec902d13b1ba2e4a98e2e234b7263e33
Grzegorz Łuszczek <grzegorz@piklus.pl>
2015-08-30 01:44:57 +0200

    Remove class_names from Spree::ShippingMethodCategory

--- core/app/models/spree/shipping_method_category.rb
+++ core/app/models/spree/shipping_method_category.rb
@@ -1,6 +1,6 @@
 module Spree
   class ShippingMethodCategory < Spree::Base
-    belongs_to :shipping_method, class_name: 'Spree::ShippingMethod'
-    belongs_to :shipping_category, class_name: 'Spree::ShippingCategory', inverse_of: :shipping_method_categories
+    belongs_to :shipping_method
+    belongs_to :shipping_category, inverse_of: :shipping_method_categories
   end
 end
