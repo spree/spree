c4b2d01838d54b71c4470f3ee510677d7fce0a49
Grzegorz Łuszczek <grzegorz@piklus.pl>
2015-08-30 01:26:15 +0200

    Remove class_names from Spree::ProductOptionType

--- core/app/models/spree/product_option_type.rb
+++ core/app/models/spree/product_option_type.rb
@@ -1,7 +1,7 @@
 module Spree
   class ProductOptionType < Spree::Base
-    belongs_to :product, class_name: 'Spree::Product', inverse_of: :product_option_types
-    belongs_to :option_type, class_name: 'Spree::OptionType', inverse_of: :product_option_types
+    belongs_to :product, inverse_of: :product_option_types
+    belongs_to :option_type, inverse_of: :product_option_types
     acts_as_list scope: :product
   end
 end
