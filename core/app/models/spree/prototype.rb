102aa88c83f0f052bbbc752179c898bde3607021
Grzegorz Łuszczek <grzegorz@piklus.pl>
2015-08-30 01:37:18 +0200

    Remove class_names from Spree::Prototype

--- core/app/models/spree/prototype.rb
+++ core/app/models/spree/prototype.rb
@@ -1,13 +1,13 @@
 module Spree
   class Prototype < Spree::Base
-    has_many :property_prototypes, class_name: 'Spree::PropertyPrototype'
-    has_many :properties, through: :property_prototypes, class_name: 'Spree::Property'
+    has_many :property_prototypes
+    has_many :properties, through: :property_prototypes
 
-    has_many :option_type_prototypes, class_name: 'Spree::OptionTypePrototype'
-    has_many :option_types, through: :option_type_prototypes, class_name: 'Spree::OptionType'
+    has_many :option_type_prototypes
+    has_many :option_types, through: :option_type_prototypes
 
-    has_many :prototype_taxons, class_name: 'Spree::PrototypeTaxon'
-    has_many :taxons, through: :prototype_taxons, class_name: 'Spree::Taxon'
+    has_many :prototype_taxons
+    has_many :taxons, through: :prototype_taxons
 
     validates :name, presence: true
   end
