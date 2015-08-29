5dcc30ad63f875ebea05027230d4cdf23f148a78
Grzegorz Łuszczek <grzegorz@piklus.pl>
2015-08-30 01:39:55 +0200

    Remove class_name from Spree::Role

--- core/app/models/spree/role.rb
+++ core/app/models/spree/role.rb
@@ -1,6 +1,6 @@
 module Spree
   class Role < Spree::Base
-    has_many :role_users, class_name: 'Spree::RoleUser'
+    has_many :role_users
     has_many :users, through: :role_users, class_name: Spree.user_class.to_s
 
     validates :name, presence: true
