05ae6fdb7e2b32dd6a8a98b80f853f9f9615ec74
Grzegorz Łuszczek <grzegorz@piklus.pl>
2015-08-30 01:02:10 +0200

    Remove class_names from Spree::Classification

--- core/app/models/spree/classification.rb
+++ core/app/models/spree/classification.rb
@@ -2,8 +2,8 @@ module Spree
   class Classification < Spree::Base
     self.table_name = 'spree_products_taxons'
     acts_as_list scope: :taxon
-    belongs_to :product, class_name: "Spree::Product", inverse_of: :classifications, touch: true
-    belongs_to :taxon, class_name: "Spree::Taxon", inverse_of: :classifications, touch: true
+    belongs_to :product, inverse_of: :classifications, touch: true
+    belongs_to :taxon, inverse_of: :classifications, touch: true
 
     # For #3494
     validates_uniqueness_of :taxon_id, scope: :product_id, message: :already_linked
