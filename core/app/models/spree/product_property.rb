ed9ca76937aac64e9b8c2e02e620b0ed39b2896c
Grzegorz Łuszczek <grzegorz@piklus.pl>
2015-08-30 01:29:44 +0200

    Remove class_names from Spree::ProductProperty. Remove string interpolation from order. Add delegation

--- core/app/models/spree/product_property.rb
+++ core/app/models/spree/product_property.rb
@@ -2,21 +2,19 @@ module Spree
   class ProductProperty < Spree::Base
     acts_as_list scope: :product
 
-    belongs_to :product, touch: true, class_name: 'Spree::Product', inverse_of: :product_properties
-    belongs_to :property, class_name: 'Spree::Property', inverse_of: :product_properties
+    belongs_to :product, touch: true, inverse_of: :product_properties
+    belongs_to :property, inverse_of: :product_properties
 
     validates :property, presence: true
 
     validates_with Spree::Validations::DbMaximumLengthValidator, field: :value
 
-    default_scope { order("#{self.table_name}.position") }
+    default_scope { order(:position) }
 
     self.whitelisted_ransackable_attributes = ['value']
 
     # virtual attributes for use with AJAX completion stuff
-    def property_name
-      property.name if property
-    end
+    delegate :name, to: :property, allow_nil: true
 
     def property_name=(name)
       if name.present?
