e4e2edc6a1c9e13a1dacfc66d3b64faacfb0e16e
Grzegorz Łuszczek <grzegorz@piklus.pl>
2015-08-30 01:40:19 +0200

    Remove class_name from Spree::RuleTUser. Not need to cast to string already evaluated class

--- core/app/models/spree/role_user.rb
+++ core/app/models/spree/role_user.rb
@@ -1,6 +1,6 @@
 module Spree
   class RoleUser < Spree::Base
-    belongs_to :role, class_name: 'Spree::Role'
-    belongs_to :user, class_name: Spree.user_class.to_s
+    belongs_to :role
+    belongs_to :user, class_name: Spree.user_class
   end
 end
