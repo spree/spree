6b18bdfcbcfd65b1a6fbc682599f7e8bf9d9b794
Tanmay Sinha <tanmay3011@gmail.com>
2015-09-24 11:39:49 +0530

    Use public send instead of send to protect privacy of methods

--- core/app/models/spree/address.rb
+++ core/app/models/spree/address.rb
@@ -25,7 +25,7 @@ module Spree
     end
 
     def self.default(user = nil, kind = "bill")
-      if user && user_address = user.send(:"#{kind}_address")
+      if user && user_address = user.public_send(:"#{kind}_address")
         user_address.clone
       else
         build_default
