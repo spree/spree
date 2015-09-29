8934d5b63e799af4a0f1bd4d7dd985affea93fdd
nishant-cyro <nishant.tuteja@vinsol.com>
2015-09-30 01:40:20 +0530

    Remove rspec-activemodel-mocks-patch.rb as the issue has been fixed in 1.0.2 version, require latest version in common_spree_dependencies.rb

--- core/spec/support/rspec-activemodel-mocks-patch.rb
+++
@@ -1,8 +0,0 @@
-# Manually applied the patch from https://github.com/jdelStrother/rspec-activemodel-mocks/commit/1211c347c5a574739616ccadf4b3b54686f9051f
-unless Gem.loaded_specs['rspec-activemodel-mocks'].version.to_s =~ /1.0.\d/
-  raise "RSpec-ActiveModel-Mocks version has changed, please check if the behaviour has already been fixed: https://github.com/rspec/rspec-activemodel-mocks/pull/10
-If so, this patch might be obsolete-"
-end
-RSpec::ActiveModel::Mocks::Mocks::ActiveRecordInstanceMethods.class_eval do
-  alias_method :_read_attribute, :[]
-end
