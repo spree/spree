3a7bb413ea28a7038787668f5322f3da40330cf1
nishant-cyro <nishant.tuteja@vinsol.com>
2015-10-06 00:55:03 +0530

    Merge multiple delegate calls in product into one

--- core/app/models/spree/product.rb
+++ core/app/models/spree/product.rb
@@ -62,12 +62,10 @@ module Spree
     has_many :line_items, through: :variants_including_master
     has_many :orders, through: :line_items
 
-    delegate_belongs_to :master, :sku, :price, :currency, :display_amount, :display_price, :weight, :height, :width, :depth, :is_master, :has_default_price?, :cost_currency, :price_in, :amount_in
+    delegate_belongs_to :master, :sku, :price, :currency, :display_amount, :display_price, :weight, :height, :width, :depth,
+    :is_master, :has_default_price?, :cost_currency, :price_in, :amount_in, :cost_price, :images
 
-    delegate_belongs_to :master, :cost_price
-
-    delegate :images, to: :master, prefix: true
-    alias_method :images, :master_images
+    alias_method :master_images, :images
 
     has_many :variant_images, -> { order(:position) }, source: :images, through: :variants_including_master
 
