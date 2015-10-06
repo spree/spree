0e055e4aea43b4dbee40db9abced4b2757b8318a
Alessandro Lepore <lepore.ale@gmail.com>
2015-10-06 14:30:00 +0200

    allow optional checkout steps numbers

--- frontend/spec/helpers/frontend_helper_spec.rb
+++ frontend/spec/helpers/frontend_helper_spec.rb
@@ -37,5 +37,21 @@ module Spree
       taxon = create(:taxon, name: "iphone")
       expect(spree.nested_taxons_path(taxon)).to eq("/t/iphone")
     end
+
+    context '#checkout_progress' do
+      before do
+        @order = create(:order, state: 'address')
+      end
+
+      it 'does not include numbers by default' do
+        output = checkout_progress
+        expect(output).to_not include('1.')
+      end
+
+      it 'has option to include numbers' do
+        output = checkout_progress(numbers: true)
+        expect(output).to include('1.')
+      end
+    end
   end
 end
