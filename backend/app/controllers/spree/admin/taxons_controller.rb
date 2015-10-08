9d6cde8d769eb8c96a0d46272230507add87149d
Tanmay Sinha <tanmay3011@gmail.com>
2015-10-08 22:09:13 +0530

    Fix spacing issue

--- backend/app/controllers/spree/admin/taxons_controller.rb
+++ backend/app/controllers/spree/admin/taxons_controller.rb
@@ -22,7 +22,7 @@ module Spree
         @taxon = @taxonomy.taxons.build(params[:taxon])
         if @taxon.save
           respond_with(@taxon) do |format|
-            format.json {render json: @taxon.to_json }
+            format.json { render json: @taxon.to_json }
           end
         else
           flash[:error] = Spree.t('errors.messages.could_not_create_taxon')
@@ -59,8 +59,8 @@ module Spree
         rename_child_taxons if @update_children
 
         respond_with(@taxon) do |format|
-          format.html {redirect_to edit_admin_taxonomy_url(@taxonomy) }
-          format.json {render json: @taxon.to_json }
+          format.html { redirect_to edit_admin_taxonomy_url(@taxonomy) }
+          format.json { render json: @taxon.to_json }
         end
       end
 
