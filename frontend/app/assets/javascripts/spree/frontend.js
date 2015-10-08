7cf93d9f7e9ef4b97e83f59a5df1fff42fee4c5a
Alessandro Lepore <lepore.ale@gmail.com>
2015-10-08 17:17:10 +0200

    assets requires cleanup
    requires all the files on the manifest, remove some duplicate requires, put modernizr on top (right?)

--- frontend/app/assets/javascripts/spree/frontend.js
+++ frontend/app/assets/javascripts/spree/frontend.js
@@ -1,6 +1,9 @@
 //= require bootstrap-sprockets
+//= require jquery.payment
 //= require jquery.validate/jquery.validate.min
 //= require spree
 //= require spree/frontend/cart
 //= require spree/frontend/checkout
+//= require spree/frontend/checkout/address
+//= require spree/frontend/checkout/payment
 //= require spree/frontend/product
