e0c3e5cccbe7eaf267c4a90e65b38237fbc4e413
James Whelton <james@whelton.io>
2015-10-09 22:23:17 +0400

    checks if search param is a hash and present

--- core/lib/spree/core/search/base.rb
+++ core/lib/spree/core/search/base.rb
@@ -70,7 +70,7 @@ module Spree
               else
                 base_scope = base_scope.merge(Spree::Product.ransack({scope_name => scope_attribute}).result)
               end
-            end if search
+            end if search.is_a?(Hash)
             base_scope
           end
 
