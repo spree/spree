604c6c90c1f6771990bc2dd8ef4e1d475a95c11a
Martin Meyerhoff <mamhoff@gmail.com>
2015-10-13 20:26:54 +0200

    Remove foreign key constraint from migration

--- core/db/migrate/20151013111930_create_spree_user_payment_sources.rb
+++ core/db/migrate/20151013111930_create_spree_user_payment_sources.rb
@@ -1,7 +1,7 @@
 class CreateSpreeUserPaymentSources < ActiveRecord::Migration
   def change
     create_table :spree_user_payment_sources do |t|
-      t.references :user, index: true, foreign_key: true
+      t.references :user, index: true, foreign_key: false
       t.references :payment_source, polymorphic: true, index: {name: "index_users_payment_sources"}
       t.boolean :default
 
