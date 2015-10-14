bee9806d9cfc4ac834e6b457ef5d70726beeb457
Martin Meyerhoff <mamhoff@gmail.com>
2015-10-14 20:53:13 +0200

    Use instance_double and allow other payment sources than credit cards

--- core/spec/support/concerns/payment_source.rb
+++ core/spec/support/concerns/payment_source.rb
@@ -13,35 +13,35 @@ RSpec.shared_examples "a payment source" do
 
   context "#can_capture?" do
     it "should be true if payment is pending" do
-      payment = mock_model(Spree::Payment, pending?: true, created_at: Time.current)
+      payment = instance_double(Spree::Payment, pending?: true, created_at: Time.current)
       expect(payment_source.can_capture?(payment)).to be true
     end
 
     it "should be true if payment is checkout" do
-      payment = mock_model(Spree::Payment, pending?: false, checkout?: true, created_at: Time.current)
+      payment = instance_double(Spree::Payment, pending?: false, checkout?: true, created_at: Time.current)
       expect(payment_source.can_capture?(payment)).to be true
     end
   end
 
   context "#can_void?" do
     it "should be true if payment is not void" do
-      payment = mock_model(Spree::Payment, failed?: false, void?: false)
+      payment = instance_double(Spree::Payment, failed?: false, void?: false)
       expect(payment_source.can_void?(payment)).to be true
     end
   end
 
   context "#can_credit?" do
     it "should be false if payment is not completed" do
-      payment = mock_model(Spree::Payment, completed?: false)
+      payment = instance_double(Spree::Payment, completed?: false)
       expect(payment_source.can_credit?(payment)).to be false
     end
 
     it "should be false when credit_allowed is zero" do
-      payment = mock_model(
+      payment = instance_double(
         Spree::Payment,
         completed?: true,
         credit_allowed: 0,
-        order: mock_model(Spree::Order, payment_state: 'credit_owed')
+        order: instance_double(Spree::Order, payment_state: 'credit_owed')
       )
       expect(payment_source.can_credit?(payment)).to be false
     end
@@ -49,21 +49,21 @@ RSpec.shared_examples "a payment source" do
 
   context "#first_name" do
     before do
-      credit_card.name = "Ludwig van Beethoven"
+      payment_source.name = "Ludwig van Beethoven"
     end
 
     it "extracts the first name" do
-      expect(credit_card.first_name).to eq "Ludwig"
+      expect(payment_source.first_name).to eq "Ludwig"
     end
   end
 
   context "#last_name" do
     before do
-      credit_card.name = "Ludwig van Beethoven"
+      payment_source.name = "Ludwig van Beethoven"
     end
 
     it "extracts the last name" do
-      expect(credit_card.last_name).to eq "van Beethoven"
+      expect(payment_source.last_name).to eq "van Beethoven"
     end
   end
 end
