29eee3989696ef93bfd6f6a17867c202c9621634
Martin Meyerhoff <mamhoff@gmail.com>
2015-10-27 10:49:28 +0100

    Do not store the needless pre_tax_amount

--- core/app/models/spree/tax_rate.rb
+++ core/app/models/spree/tax_rate.rb
@@ -36,19 +36,19 @@ module Spree
     # Pre-tax amounts must be stored so that we can calculate
     # correct rate amounts in the future. For example:
     # https://github.com/spree/spree/issues/4318#issuecomment-34723428
-    def self.store_pre_tax_amount(item, rates)
-      pre_tax_amount = case item
-                       when Spree::LineItem then item.discounted_amount
-                       when Spree::Shipment then item.discounted_cost
-                       end
-
-      included_rates = rates.select(&:included_in_price)
-      if included_rates.any?
-        pre_tax_amount /= (1 + included_rates.map(&:amount).sum)
-      end
-
-      item.update_column(:pre_tax_amount, pre_tax_amount)
-    end
+    # def self.store_pre_tax_amount(item, rates)
+    #   pre_tax_amount = case item
+    #                    when Spree::LineItem then item.discounted_amount
+    #                    when Spree::Shipment then item.discounted_cost
+    #                    end
+    #
+    #   included_rates = rates.select(&:included_in_price)
+    #   if included_rates.any?
+    #     pre_tax_amount /= (1 + included_rates.map(&:amount).sum)
+    #   end
+    #
+    #   item.update_column(:pre_tax_amount, pre_tax_amount)
+    # end
 
     # Deletes all tax adjustments, then applies all applicable rates
     # to relevant items.
@@ -67,7 +67,6 @@ module Spree
         relevant_rates = rates.select do |rate|
           rate.tax_category == item.tax_category
         end
-        store_pre_tax_amount(item, relevant_rates)
         relevant_rates.each do |rate|
           rate.adjust(order, item)
         end
