d99956eda3fbce18665738b267b714334e0e1bb3
dilpreet92 <dilpreet@vinsol.com>
2015-10-29 16:37:09 +0530

    add allow blank to uniqueness

--- core/app/models/spree/shipping_category.rb
+++ core/app/models/spree/shipping_category.rb
@@ -1,6 +1,6 @@
 module Spree
   class ShippingCategory < Spree::Base
-    validates :name, presence: true, uniqueness: true
+    validates :name, presence: true, uniqueness: { allow_blank: true }
 
     with_options inverse_of: :shipping_category do
       has_many :products
