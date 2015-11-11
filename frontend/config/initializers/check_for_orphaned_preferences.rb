035705ff0b468021e5d09df57d7a99d08a5103c5
Markus Schirp <mbj@schirp-dso.com>
2015-11-11 02:09:43 +0000

    Remove now redundant check for orphaned preferences

--- frontend/config/initializers/check_for_orphaned_preferences.rb
+++
@@ -1,9 +0,0 @@
-begin
-  required_column_names = ["owner_id", "owner_type", "name"]     
-  if (Spree::Preference.column_names & required_column_names) == required_column_names
-     ActiveRecord::Base.connection.execute("SELECT owner_id, owner_type, name, value FROM spree_preferences WHERE 'key' IS NULL").each do |pref|
-       warn "[WARNING] Orphaned preference `#{pref[2]}` with value `#{pref[3]}` for #{pref[1]} with id of: #{pref[0]}, you should reset the preference value manually."
-     end
-  end
-rescue
-end
