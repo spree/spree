b6427387bdea7777343911f60bec577fdf7c8f90
Markus Schirp <mbj@schirp-dso.com>
2015-11-14 04:28:29 +0000

    Reduce install generator

--- core/lib/generators/spree/install/install_generator.rb
+++ core/lib/generators/spree/install/install_generator.rb
@@ -1,177 +1,45 @@
 require 'rails/generators'
-require 'bundler'
-require 'bundler/cli'
 
 module Spree
   class InstallGenerator < Rails::Generators::Base
-    class_option :migrate, :type => :boolean, :default => true, :banner => 'Run Spree migrations'
-    class_option :seed, :type => :boolean, :default => true, :banner => 'load seed data (migrations must be run)'
-    class_option :auto_accept, :type => :boolean
-    class_option :user_class, :type => :string
-    class_option :admin_email, :type => :string
-    class_option :admin_password, :type => :string
-    class_option :lib_name, :type => :string, :default => 'spree'
-    class_option :enforce_available_locales, :type => :boolean, :default => nil
+    class_option :lib_name, type: :string
 
     def self.source_paths
-      paths = self.superclass.source_paths
-      paths << File.expand_path('../templates', "../../#{__FILE__}")
-      paths << File.expand_path('../templates', "../#{__FILE__}")
-      paths << File.expand_path('../templates', __FILE__)
-      paths.flatten
-    end
-
-    def prepare_options
-      @run_migrations = options[:migrate]
-      @load_seed_data = options[:seed]
-
-      unless @run_migrations
-        @load_seed_data = false
-      end
+      superclass.source_paths + [File.join(__dir__, 'templates')]
     end
 
     def add_files
-      template 'config/initializers/spree.rb', 'config/initializers/spree.rb'
-    end
-
-    def additional_tweaks
-      return unless File.exists? 'public/robots.txt'
-      append_file "public/robots.txt", <<-ROBOTS
-User-agent: *
-Disallow: /checkout
-Disallow: /cart
-Disallow: /orders
-Disallow: /user
-Disallow: /account
-Disallow: /api
-Disallow: /password
-      ROBOTS
+      template('config/initializers/spree.rb', 'config/initializers/spree.rb')
     end
 
     def setup_assets
-      @lib_name = 'spree'
-      %w{javascripts stylesheets images}.each do |path|
-        empty_directory "vendor/assets/#{path}/spree/frontend" if defined? Spree::Frontend || Rails.env.test?
-        empty_directory "vendor/assets/#{path}/spree/backend" if defined? Spree::Backend || Rails.env.test?
+      %w[
+        javascripts/spree/frontend/all.js
+        stylesheets/spree/frontend/all.css
+        javascripts/spree/backend/all.js
+        stylesheets/spree/backend/all.css
+      ].each do |name|
+        template("vendor/assets/#{name}")
       end
-
-      if defined? Spree::Frontend || Rails.env.test?
-        template "vendor/assets/javascripts/spree/frontend/all.js"
-        template "vendor/assets/stylesheets/spree/frontend/all.css"
-      end
-
-      if defined? Spree::Backend || Rails.env.test?
-        template "vendor/assets/javascripts/spree/backend/all.js"
-        template "vendor/assets/stylesheets/spree/backend/all.css"
-      end
-    end
-
-    def create_overrides_directory
-      empty_directory "app/overrides"
     end
 
     def configure_application
-      application <<-APP
-
-    config.to_prepare do
-      # Load application's model / class decorators
-      Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
-        Rails.configuration.cache_classes ? require(c) : load(c)
-      end
-
-      # Load application's view overrides
-      Dir.glob(File.join(File.dirname(__FILE__), "../app/overrides/*.rb")) do |c|
-        Rails.configuration.cache_classes ? require(c) : load(c)
-      end
-    end
+      application(<<-'APP'.strip_heredoc)
+        config.to_prepare do
+          Dir.glob(File.join(__dir__, '../app/**/*.rb'), &method(:require))
+        end
       APP
-
-      if !options[:enforce_available_locales].nil?
-        application <<-APP
-    # Prevent this deprecation message: https://github.com/svenfuchs/i18n/commit/3b6e56e
-    I18n.enforce_available_locales = #{options[:enforce_available_locales]}
-        APP
-      end
-    end
-
-    def include_seed_data
-      append_file "db/seeds.rb", <<-SEEDS
-\n
-Spree::Core::Engine.load_seed if defined?(Spree::Core)
-Spree::Auth::Engine.load_seed if defined?(Spree::Auth)
-      SEEDS
     end
 
     def install_migrations
-      say_status :copying, "migrations"
       silence_stream(STDOUT) do
-        silence_warnings { rake 'railties:install:migrations' }
-      end
-    end
-
-    def create_database
-      say_status :creating, "database"
-      silence_stream(STDOUT) do
-        silence_stream(STDERR) do
-          silence_warnings { rake 'db:create' }
-        end
-      end
-    end
-
-    def run_migrations
-      if @run_migrations
-        say_status :running, "migrations"
-        quietly { rake 'db:migrate' }
-      else
-        say_status :skipping, "migrations (don't forget to run rake db:migrate)"
-      end
-    end
-
-    def populate_seed_data
-      if @load_seed_data
-        say_status :loading,  "seed data"
-        rake_options=[]
-        rake_options << "AUTO_ACCEPT=1" if options[:auto_accept]
-        rake_options << "ADMIN_EMAIL=#{options[:admin_email]}" if options[:admin_email]
-        rake_options << "ADMIN_PASSWORD=#{options[:admin_password]}" if options[:admin_password]
-
-        cmd = lambda { rake("db:seed #{rake_options.join(' ')}") }
-        if options[:auto_accept] || (options[:admin_email] && options[:admin_password])
-          quietly &cmd
-        else
-          cmd.call
-        end
-      else
-        say_status :skipping, "seed data (you can always run rake db:seed)"
+        rake('railties:install:migrations')
       end
     end
 
     def notify_about_routes
-      insert_into_file File.join('config', 'routes.rb'), :after => "Rails.application.routes.draw do\n" do
-        %Q{
-  # This line mounts Spree's routes at the root of your application.
-  # This means, any requests to URLs such as /products, will go to Spree::ProductsController.
-  # If you would like to change where this engine is mounted, simply change the :at option to something different.
-  #
-  # We ask that you don't use the :as option here, as Spree relies on it being the default of "spree"
-  mount Spree::Core::Engine, :at => '/'
-        }
-      end
-
-      unless options[:quiet]
-        puts "*" * 50
-        puts "We added the following line to your application's config/routes.rb file:"
-        puts " "
-        puts "    mount Spree::Core::Engine, :at => '/'"
-      end
-    end
-
-    def complete
-      unless options[:quiet]
-        puts "*" * 50
-        puts "Spree has been installed successfully. You're all ready to go!"
-        puts " "
-        puts "Enjoy!"
+      insert_into_file('config/routes.rb', after: 'Rails.application.routes.draw do') do
+        "\nmount Spree::Core::Engine, at: '/'"
       end
     end
   end
