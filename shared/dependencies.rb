081c2fde8e7629347b38b21d3e8082ba52600cf9
Markus Schirp <mbj@schirp-dso.com>
2015-11-14 03:09:05 +0000

    Remove platform specific dependencies
    * We do not support / use these tools

--- shared/dependencies.rb
+++ shared/dependencies.rb
@@ -3,10 +3,6 @@
 # the one component of Spree.
 source 'https://rubygems.org'
 
-platforms :jruby do
-  gem 'jruby-openssl'
-end
-
 group :test do
   gem 'capybara',                  '~> 2.4'
   gem 'database_cleaner',          '~> 1.3'
@@ -22,13 +18,4 @@ group :test do
   gem 'timecop',                   '~> 0.8.0'
   gem 'poltergeist',               '=  1.5.0'
   gem 'with_model',                '~> 1.2.1'
-end
-
-group :test, :development do
-  platforms :ruby_19 do
-    gem 'pry-debugger'
-  end
-  platforms :ruby_20, :ruby_21 do
-    gem 'pry-byebug'
-  end
 end
