e7b9dcfd01073406e33913c9a51fc34f43292cf1
Markus Schirp <mbj@schirp-dso.com>
2015-12-14 04:08:21 +0000

    Remove buggy / unused i18n extensions
    * They are not threadsave
    * Underspecified
    * Provide no visible production value

--- core/lib/spree/i18n.rb
+++ core/lib/spree/i18n.rb
@@ -1,6 +1,5 @@
 require 'i18n'
 require 'active_support/core_ext/array/extract_options'
-require 'spree/i18n/base'
 
 module Spree
   extend ActionView::Helpers::TranslationHelper
@@ -10,8 +9,6 @@ module Spree
     # Add spree namespace and delegate to Rails TranslationHelper for some nice
     # extra functionality. e.g return reasonable strings for missing translations
     def translate(*args)
-      @virtual_path = virtual_path
-
       options = args.extract_options!
       options[:scope] = [*options[:scope]].unshift(:spree)
       args << options
@@ -19,19 +16,5 @@ module Spree
     end
 
     alias_method :t, :translate
-
-    def context
-      Spree::ViewContext.context
-    end
-
-    def virtual_path
-      if context
-        path = context.instance_variable_get("@virtual_path")
-
-        if path
-          path.gsub(/spree/, '')
-        end
-      end
-    end
   end
 end
