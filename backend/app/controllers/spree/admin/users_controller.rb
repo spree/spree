d656316ef4e4ce8136991f785a067cdf6af33ce1
Vishal Zambre <vishal.zambre@weboniselab.com>
2015-12-20 15:16:34 +0530

    called super method in user collection

--- backend/app/controllers/spree/admin/users_controller.rb
+++ backend/app/controllers/spree/admin/users_controller.rb
@@ -95,8 +95,9 @@ module Spree
 
         def collection
           return @collection if @collection.present?
+          @collection = super
           if request.xhr? && params[:q].present?
-            @collection = Spree.user_class.includes(:bill_address, :ship_address)
+            @collection = @collection.includes(:bill_address, :ship_address)
                               .where("spree_users.email #{LIKE} :search
                                      OR (spree_addresses.firstname #{LIKE} :search AND spree_addresses.id = spree_users.bill_address_id)
                                      OR (spree_addresses.lastname  #{LIKE} :search AND spree_addresses.id = spree_users.bill_address_id)
@@ -105,7 +106,7 @@ module Spree
                                     { :search => "#{params[:q].strip}%" })
                               .limit(params[:limit] || 100)
           else
-            @search = Spree.user_class.ransack(params[:q])
+            @search = @collection.ransack(params[:q])
             @collection = @search.result.page(params[:page]).per(Spree::Config[:admin_products_per_page])
           end
         end
