74817c47e3b274a96bc4bed671d262e72fa82d0d
Sasha Cooper <jinksy@gmail.com>
2016-01-14 14:36:39 +0000

    Compact array with errant nils

--- core/app/models/spree/stock/coordinator.rb
+++ core/app/models/spree/stock/coordinator.rb
@@ -27,11 +27,11 @@ module Spree
       # to build a package because it would be empty. Plus we avoid errors down
       # the stack because it would assume the stock location has stock items
       # for the given order
-      # 
+      #
       # Returns an array of Package instances
       def build_packages(packages = Array.new)
         StockLocation.active.each do |stock_location|
-          next unless stock_location.stock_items.where(:variant_id => inventory_units.map(&:variant_id).uniq).exists?
+          next unless stock_location.stock_items.where(:variant_id => inventory_units.compact.map(&:variant_id).uniq).exists?
 
           packer = build_packer(stock_location, inventory_units)
           packages += packer.packages
