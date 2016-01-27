54e2f46242d49f995a85557a853281e16a5004bf
nishant-cyro <nishant.tuteja@vinsol.com>
2016-01-27 23:00:48 +0530

    Remove validation unique_per_promotion in promotion_rule and modify options_for_promotion_rule_types for same
    Fix spec
    
    Fix hound issue

--- backend/spec/helpers/promotion_rules_helper_spec.rb
+++ backend/spec/helpers/promotion_rules_helper_spec.rb
@@ -1,12 +1,12 @@
 require 'spec_helper'
 module Spree
  describe Spree::PromotionRulesHelper, :type => :helper do
-   it "does not include existing rules in options" do
+   it "should include existing rules in options" do
      promotion = Spree::Promotion.new
      promotion.promotion_rules << Spree::Promotion::Rules::ItemTotal.new
 
      options = helper.options_for_promotion_rule_types(promotion)
-     expect(options).not_to match(/ItemTotal/)
+     expect(options).to match(/ItemTotal/)
    end
  end
 end
