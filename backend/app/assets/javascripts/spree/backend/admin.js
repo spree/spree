f66763aa45b9be7c7cc43740275a2aad9f92ef09
Damian Legawiec <damian@sparksolutions.co>
2016-01-31 15:21:22 +0100

    fixes #6888 and all other issues related to per_page dropdown

--- backend/app/assets/javascripts/spree/backend/admin.js
+++ backend/app/assets/javascripts/spree/backend/admin.js
@@ -149,6 +149,32 @@ jQuery(function($) {
     }
   });
 
+  // per page dropdown
+  // preserves all selected filters / queries supplied by user
+  // changes only per_page value
+  $(".js-per-page-select").change(function() {
+    var form  = $(this).closest(".js-per-page-form");
+    var url   = form.attr('action');
+    var value = $(this).val().toString();
+    if (url.match(/\?/)) {
+      url += "&per_page=" + value;
+    } else {
+      url += "?per_page=" + value;
+    }
+    window.location = url;
+  });
+
+  // injects per_page settings to all available search forms
+  // so when user changes some filters / queries per_page is preserved
+  $(document).ready(function() {
+    var perPageDropdown = $(".js-per-page-select:first");
+    if (perPageDropdown.length) {
+      var perPageValue = perPageDropdown.val().toString();
+      var perPageInput = '<input type="hidden" name="per_page" value=' + perPageValue + ' />';
+      $("#table-filter form").append(perPageInput);
+    }
+  });
+
   // Enable sidebar toggle
   $("[data-toggle='offcanvas']").click(function(e) {
     e.preventDefault();
