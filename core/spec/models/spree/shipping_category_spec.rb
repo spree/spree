d30790f87ee033036cb1ecb4105543dc6469b6bc
dilpreet92 <dilpreet@vinsol.com>
2016-02-02 11:24:47 +0530

    Added specs for the validation and uniquness

--- core/spec/models/spree/shipping_category_spec.rb
+++ core/spec/models/spree/shipping_category_spec.rb
@@ -2,4 +2,19 @@ require 'spec_helper'
 
 describe Spree::ShippingCategory, :type => :model do
 
+  describe '#validations' do
+    it 'should have a valid factory' do
+      expect(shipping_category).to be_valid
+    end
+
+    it 'should require name' do
+      expect(FactoryGirl.build(:shipping_category, name: '')).not_to be_valid
+    end
+
+    it 'should validate uniqueness' do
+      FactoryGirl.create(:shipping_category, name: 'Test')
+      expect(FactoryGirl.build(:shipping_category, name: 'Test')).not_to be_valid
+    end
+  end
+
 end
