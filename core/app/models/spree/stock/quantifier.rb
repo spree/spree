72af3055d0dee2cd5f55aca4d1e8e5a40c419c44
Priyank Gupta <priyank.gupta@vinsol.com>
2016-02-04 16:38:08 +0530

    admin should not be available to order unavailable products

--- core/app/models/spree/stock/quantifier.rb
+++ core/app/models/spree/stock/quantifier.rb
@@ -1,7 +1,7 @@
 module Spree
   module Stock
     class Quantifier
-      attr_reader :stock_items
+      attr_reader :stock_items, :variant
 
       def initialize(variant)
         @variant = variant
@@ -9,7 +9,7 @@ module Spree
       end
 
       def total_on_hand
-        if @variant.should_track_inventory?
+        if variant.should_track_inventory?
           stock_items.sum(:count_on_hand)
         else
           Float::INFINITY
@@ -21,7 +21,7 @@ module Spree
       end
 
       def can_supply?(required = 1)
-        total_on_hand >= required || backorderable?
+        variant.available? && (total_on_hand >= required || backorderable?)
       end
 
     end
--- core/app/models/spree/variant.rb
+++ core/app/models/spree/variant.rb
@@ -64,6 +64,10 @@ module Spree
     self.whitelisted_ransackable_associations = %w[option_values product prices default_price]
     self.whitelisted_ransackable_attributes = %w[weight sku]
 
+    def available?
+      !discontinued? && product.available?
+    end
+
     def self.active(currency = nil)
       not_discontinued.joins(:prices).where(deleted_at: nil).where('spree_prices.currency' => currency || Spree::Config[:currency]).where('spree_prices.amount IS NOT NULL')
     end
