1d03936852a145c87a7b97d01ed56906c9e12a06
nishant-cyro <nishant.tuteja@vinsol.com>
2016-02-08 16:03:48 +0530

    Fixes #6642, create a new promotion action create_adjusted_line_items
    Modify create_adjusted_line_items.rb
    
    Add specs for create_adjusted_line_items
    
    Fixes #6642, add a new promotiona action to create line_item with adjustment, write specs for same
    
    Modify spec
    
    Modify _create_adjusted_line_items.html.erb
    
    Fix indentation for create_adjusted_line_items.rb and create_adjusted_line_item_spec.rb
    
    Fix indentation for create_adjusted_line_item_spec.rb
    
    Modify create_adjusted_line_item_spec.rb
    
    Add deprecation warning in create_line_items promotion action

--- core/lib/spree/core/engine.rb
+++ core/lib/spree/core/engine.rb
@@ -98,6 +98,7 @@ module Spree
           Promotion::Actions::CreateAdjustment,
           Promotion::Actions::CreateItemAdjustments,
           Promotion::Actions::CreateLineItems,
+          Promotion::Actions::CreateAdjustedLineItems,
           Promotion::Actions::FreeShipping]
       end
 
