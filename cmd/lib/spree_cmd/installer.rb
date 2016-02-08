a05bb8c0367bb8440ede5473318164968f74cdde
Damian Legawiec <damian@sparksolutions.co>
2016-02-08 13:29:13 +0100

    spree cmd installer: don't use strict versioning, prevents version lock for minor versions

--- cmd/lib/spree_cmd/installer.rb
+++ cmd/lib/spree_cmd/installer.rb
@@ -128,7 +128,7 @@ module SpreeCmd
       def gem(name, gem_options={})
         say_status :gemfile, name
         parts = ["'#{name}'"]
-        parts << ["'#{gem_options.delete(:version)}'"] if gem_options[:version]
+        parts << ["'~> #{gem_options.delete(:version)}'"] if gem_options[:version]
         gem_options.each { |key, value| parts << "#{key}: '#{value}'" }
         append_file 'Gemfile', "\ngem #{parts.join(', ')}", :verbose => false
       end
