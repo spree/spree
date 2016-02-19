63c37dc613556963f6a397b97e19cd47445720fb
Tanmay Sinha <tanmay3011@gmail.com>
2016-02-19 08:05:44 +0530

    [ci skip] Typo fix: change lazyoading to lazy loading

--- backend/app/controllers/spree/admin/orders_controller.rb
+++ backend/app/controllers/spree/admin/orders_controller.rb
@@ -36,7 +36,7 @@ module Spree
 
         @search = Order.preload(:user).accessible_by(current_ability, :index).ransack(params[:q])
 
-        # lazyoading other models here (via includes) may result in an invalid query
+        # lazy loading other models here (via includes) may result in an invalid query
         # e.g. SELECT  DISTINCT DISTINCT "spree_orders".id, "spree_orders"."created_at" AS alias_0 FROM "spree_orders"
         # see https://github.com/spree/spree/pull/3919
         @orders = @search.result(distinct: true).
