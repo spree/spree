64099a5288cc07b13c461283f2b0241d5c4042ae
aishmita-aggarwal <aishmita@vinsol.com>
2016-02-23 15:16:33 +0530

    Add * to all mandatory fields using base_helper

--- backend/config/initializers/form_builder.rb
+++ backend/config/initializers/form_builder.rb
@@ -9,7 +9,11 @@ class ActionView::Helpers::FormBuilder
   def error_message_on(method, options = {})
     @template.error_message_on(@object_name, method, objectify_options(options))
   end
+
+  def required_label_markup(method, text = nil, options = {}, &block)
+    @template.label(@object_name, method, @template.required_label_markup(text || method),
+                    objectify_options(options), &block)
+  end
 end
 
 ActionView::Base.field_error_proc = Proc.new{ |html_tag, instance| "<span class=\"field_with_errors\">#{html_tag}</span>".html_safe }
-
