a8715c285788997e69287cd0cb9fbc2f78f1e31a
Tanmay Sinha <tanmay3011@gmail.com>
2016-02-23 20:42:02 +0530

    Add case_sensitive option to uniquesness of shipping category

--- core/spec/models/spree/shipping_category_spec.rb
+++ core/spec/models/spree/shipping_category_spec.rb
@@ -1,19 +1,13 @@
 require 'spec_helper'
 
-describe Spree::ShippingCategory, :type => :model do
+describe Spree::ShippingCategory, type: :model do
 
   describe '#validations' do
     it 'should have a valid factory' do
       expect(FactoryGirl.build(:shipping_category)).to be_valid
     end
 
-    it 'should require name' do
-      expect(FactoryGirl.build(:shipping_category, name: '')).not_to be_valid
-    end
-
-    it 'should validate uniqueness' do
-      FactoryGirl.create(:shipping_category, name: 'Test')
-      expect(FactoryGirl.build(:shipping_category, name: 'Test')).not_to be_valid
-    end
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_uniqueness_of(:name).case_insensitive.allow_blank }
   end
 end
