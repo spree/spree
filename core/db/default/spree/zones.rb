20e2816d278040f3afb15f8d6c512f800796584c
aishmita-aggarwal <aishmita@vinsol.com>
2016-02-24 17:02:04 +0530

    Modify zone sample data for adding kind

--- core/db/default/spree/zones.rb
+++ core/db/default/spree/zones.rb
@@ -1,5 +1,5 @@
-eu_vat = Spree::Zone.create!(name: "EU_VAT", description: "Countries that make up the EU VAT zone.")
-north_america = Spree::Zone.create!(name: "North America", description: "USA + Canada")
+eu_vat = Spree::Zone.create!(name: "EU_VAT", description: "Countries that make up the EU VAT zone.", kind: 'country')
+north_america = Spree::Zone.create!(name: "North America", description: "USA + Canada", kind: 'country')
 %w(PL FI PT RO DE FR SK HU SI IE AT ES IT BE SE LV BG GB LT CY LU MT DK NL EE).
 each do |name|
   eu_vat.zone_members.create!(zoneable: Spree::Country.find_by!(iso: name))
