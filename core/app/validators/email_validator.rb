c953d05c399ea5d75566ce267e41430dd9ba9a6c
nishant-cyro <nishant.tuteja@vinsol.com>
2016-02-25 13:04:06 +0530

    Delete validators/email.rb in lib/validators and move them to app/validators
    Add lib/spree/core/validators/ with deprecation warning

--- core/app/validators/email_validator.rb
+++ core/app/validators/email_validator.rb
@@ -1,6 +1,5 @@
 class EmailValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    warn "`EmailValidator` in 'lib/spree/core' is deprecated. Use `EmailValidator` in 'app/validators' instead."
     unless value =~ %r{\A(([A-Za-z0-9]+_+)|
                           ([A-Za-z0-9]+\-+)|
                           ([A-Za-z0-9]+\.+)|
