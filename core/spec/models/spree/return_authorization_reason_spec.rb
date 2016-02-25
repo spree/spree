0b4f428dbf7b4cbb0c5c2b935fe84494cafd015d
Tanmay Sinha <tanmay3011@gmail.com>
2016-02-25 19:49:20 +0530

    Add refund_reason spec and avoid .return_processing_reason raising exception if refund_reason not found

--- core/spec/models/spree/return_authorization_reason_spec.rb
+++ core/spec/models/spree/return_authorization_reason_spec.rb
@@ -4,4 +4,8 @@ describe Spree::ReturnAuthorizationReason do
   describe 'Associations' do
     it { is_expected.to have_many(:return_authorizations).dependent(:restrict_with_error) }
   end
+
+  describe 'Included Modules' do
+    it { expect(described_class.ancestors).to include(Spree::NamedType) }
+  end
 end
