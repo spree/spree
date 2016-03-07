5419d7407320560fed75483aa8ff0926365838d7
nishant-cyro <nishant.tuteja@vinsol.com>
2016-03-08 02:39:07 +0530

    Modify javascript_exists?, stylesheet_exists? and file_exists? in install_generator.rb
    Modify install_generater.rb

--- core/lib/generators/spree/install/install_generator.rb
+++ core/lib/generators/spree/install/install_generator.rb
@@ -191,18 +191,18 @@ Spree::Auth::Engine.load_seed if defined?(Spree::Auth)
     protected
 
     def javascript_exists?(script)
-      extensions = %w(.coffee .erb .coffee.erb) + [""]
+      extensions = %w(.js.coffee .js.erb .js.coffee.erb .js)
       file_exists?(extensions, script)
     end
 
     def stylesheet_exists?(stylesheet)
-      extensions = %w(.scss .erb .scss.erb) + [""]
+      extensions = %w(.css.scss .css.erb .css.scss.erb .css)
       file_exists?(extensions, stylesheet)
     end
 
     def file_exists?(extensions, filename)
       extensions.detect do |extension|
-        File.exists?("#{filename}.css#{extension}")
+        File.exists?("#{filename}#{extension}")
       end
     end
   end
