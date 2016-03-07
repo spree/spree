7f4a4b56edfe34c38bf9f2fab06b8a94a2431abc
Tanmay Sinha <tanmay3011@gmail.com>
2016-03-08 00:29:04 +0530

    Avoid raising exception from return_processing_reason since it is already handled in reimbursement reason where we have a validation on reason

--- core/app/models/spree/refund_reason.rb
+++ core/app/models/spree/refund_reason.rb
@@ -7,7 +7,7 @@ module Spree
     has_many :refunds, dependent: :restrict_with_error
 
     def self.return_processing_reason
-      find_by!(name: RETURN_PROCESSING_REASON, mutable: false)
+      find_by(name: RETURN_PROCESSING_REASON, mutable: false)
     end
   end
 end
