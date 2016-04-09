0c45ed28059bd068ff6f39fef7dc1c39890979d9
Damian Legawiec <damian@sparksolutions.co>
2016-04-09 15:48:08 +0200

    Spree CMD installer removed completely
    Installer was buggy and starting from Spree 3.0.5 favoured install
    process is standard as for any other gem (add spree to `Gemfile`, run
    `bundle install` and run generators) as described in README.md

--- cmd/lib/spree_cmd.rb
+++ cmd/lib/spree_cmd.rb
@@ -2,15 +2,10 @@ require 'thor'
 require 'thor/group'
 
 case ARGV.first
-  when 'version', '-v', '--version'
-    puts Gem.loaded_specs['spree_cmd'].version
-  when 'extension'
-    ARGV.shift
-    require 'spree_cmd/extension'
-    SpreeCmd::Extension.start
-  else
-    warn "[WARNING] Spree CMD Installer is deprecated. Please follow installation instructions at https://github.com/spree/spree#getting-started"
-    ARGV.shift
-    require 'spree_cmd/installer'
-    SpreeCmd::Installer.start
+when 'version', '-v', '--version'
+  puts Gem.loaded_specs['spree_cmd'].version
+when 'extension'
+  ARGV.shift
+  require 'spree_cmd/extension'
+  SpreeCmd::Extension.start
 end
