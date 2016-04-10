6008bee3bac9140c163b6bf8145517c8e3276c59
Damian Legawiec <damian@sparksolutions.co>
2016-04-10 17:52:28 +0200

    Admin Panel: removing non-existent assets from asset manifest

--- backend/lib/spree/backend/engine.rb
+++ backend/lib/spree/backend/engine.rb
@@ -20,12 +20,9 @@ module Spree
         app.config.assets.precompile += %w[
           spree/backend/all*
           spree/backend/address_states.js
-          jqPlot/excanvas.min.js
-          spree/backend/images/new.js
           jquery.jstree/themes/spree/*
           fontawesome-webfont*
           select2_locale*
-          jquery.alerts/images/*
         ]
       end
     end
