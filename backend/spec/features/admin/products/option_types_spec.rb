b536620c485ecf37988ba56a36dfd6ba8cf610e8
Marta Wyszynska <marwyszynska@gmail.com>
2016-04-26 16:01:37 +0200

    Admin panel breadcrumbs - properties/option types changing presentation to name in breadcrumbs

--- backend/spec/features/admin/products/option_types_spec.rb
+++ backend/spec/features/admin/products/option_types_spec.rb
@@ -59,7 +59,7 @@ describe "Option Types", type: :feature, js: true do
     create(:option_value, option_type: option_type)
     click_link "Option Types"
     within('table#listing_option_types') { click_icon :edit }
-    expect(page).to have_content(option_type.presentation)
+    expect(page).to have_content(option_type.name)
     expect(all("tbody#option_values tr").count).to eq(1)
     within("tbody#option_values") do
       find('.spree_remove_fields').click
