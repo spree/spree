78a0b68e710a46d8e41e8fccaf7fb9d4343ad5a5
Marta Wyszynska <marwyszynska@gmail.com>
2016-04-29 13:04:45 +0200

    samples fix - prototypes

--- sample/db/samples/prototypes.rb
+++ sample/db/samples/prototypes.rb
@@ -1,21 +1,22 @@
 prototypes = [
   {
-    :name => "Shirt",
-    :properties => ["Manufacturer", "Brand", "Model", "Shirt Type", "Sleeve Type", "Material", "Fit", "Gender"]
+    name: "Shirt",
+    properties: ["Manufacturer", "Brand", "Model", "Shirt Type", "Sleeve Type", "Material", "Fit", "Gender"]
   },
   {
-    :name => "Bag",
-    :properties => ["Type", "Size", "Material"]
+    name: "Bag",
+    properties: ["Type", "Size", "Material"]
   },
   {
-    :name => "Mugs",
-    :properties => ["Size", "Type"]
+    name: "Mugs",
+    properties: ["Size", "Type"]
   }
 ]
 
 prototypes.each do |prototype_attrs|
-  prototype = Spree::Prototype.create!(:name => prototype_attrs[:name])
-  prototype_attrs[:properties].each do |property|
-    prototype.properties << Spree::Property.where(name: property).first
+  prototype = Spree::Prototype.where(name: prototype_attrs[:name]).first_or_create!
+  prototype_attrs[:properties].each do |property_name|
+    property = Spree::Property.where(name: property_name).first
+    prototype.properties << property unless prototype.properties.include?(property)
   end
 end
