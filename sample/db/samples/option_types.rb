8047801810371c523606fcd11c91707469835b58
Marta Wyszynska <marwyszynska@gmail.com>
2016-04-29 12:12:10 +0200

    samples fix - option types

--- sample/db/samples/option_types.rb
+++ sample/db/samples/option_types.rb
@@ -1,12 +1,16 @@
-Spree::OptionType.create!([
+option_types_attributes = [
   {
-    :name => "tshirt-size",
-    :presentation => "Size",
-    :position => 1
+    name: "tshirt-size",
+    presentation: "Size",
+    position: 1
   },
   {
-    :name => "tshirt-color",
-    :presentation => "Color",
-    :position => 2
+    name: "tshirt-color",
+    presentation: "Color",
+    position: 2
   }
-])
+]
+
+option_types_attributes.each do |attrs|
+  Spree::OptionType.where(attrs).first_or_create!
+end
