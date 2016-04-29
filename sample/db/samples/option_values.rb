e9916eafb10afcd5bb25c3cb77d4dd527b607a9d
Marta Wyszynska <marwyszynska@gmail.com>
2016-04-29 12:40:10 +0200

    samples fix - option values

--- sample/db/samples/option_values.rb
+++ sample/db/samples/option_values.rb
@@ -3,47 +3,51 @@ Spree::Sample.load_sample("option_types")
 size = Spree::OptionType.find_by_presentation!("Size")
 color = Spree::OptionType.find_by_presentation!("Color")
 
-Spree::OptionValue.create!([
+option_values_attributes = [
   {
-    :name => "Small",
-    :presentation => "S",
-    :position => 1,
-    :option_type => size
+    name: "Small",
+    presentation: "S",
+    position: 1,
+    option_type: size
   },
   {
-    :name => "Medium",
-    :presentation => "M",
-    :position => 2,
-    :option_type => size
+    name: "Medium",
+    presentation: "M",
+    position: 2,
+    option_type: size
   },
   {
-    :name => "Large",
-    :presentation => "L",
-    :position => 3,
-    :option_type => size
+    name: "Large",
+    presentation: "L",
+    position: 3,
+    option_type: size
   },
   {
-    :name => "Extra Large",
-    :presentation => "XL",
-    :position => 4,
-    :option_type => size
+    name: "Extra Large",
+    presentation: "XL",
+    position: 4,
+    option_type: size
   },
   {
-    :name => "Red",
-    :presentation => "Red",
-    :position => 1,
-    :option_type => color,
+    name: "Red",
+    presentation: "Red",
+    position: 1,
+    option_type: color,
   },
   {
-    :name => "Green",
-    :presentation => "Green",
-    :position => 2,
-    :option_type => color,
+    name: "Green",
+    presentation: "Green",
+    position: 2,
+    option_type: color,
   },
   {
-    :name => "Blue",
-    :presentation => "Blue",
-    :position => 3,
-    :option_type => color
+    name: "Blue",
+    presentation: "Blue",
+    position: 3,
+    option_type: color
   }
-])
+]
+
+option_values_attributes.each do |attrs|
+  Spree::OptionValue.where(attrs).first_or_create!
+end
