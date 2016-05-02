c91c9aa3eb7e7e2a1390ac64b5cd772825017c45
Marta Wyszynska <marwyszynska@gmail.com>
2016-05-02 20:31:47 +0200

    samples fix - adjustments

--- sample/db/samples/adjustments.rb
+++ sample/db/samples/adjustments.rb
@@ -3,18 +3,20 @@ Spree::Sample.load_sample("orders")
 first_order = Spree::Order.find_by_number!("R123456789")
 last_order = Spree::Order.find_by_number!("R987654321")
 
-first_order.adjustments.create!(
-  :amount => 0,
-  :source => Spree::TaxRate.find_by_name!("North America"),
-  :order  => first_order,
-  :label => "Tax",
-  :state => "open",
-  :mandatory => true)
+first_order.adjustments.where(
+  source: Spree::TaxRate.find_by_name!("North America"),
+  order: first_order,
+  label: "Tax",
+  state: "open",
+  mandatory: true).first_or_create! do |adj|
+    adj.amount = 0
+  end
 
-last_order.adjustments.create!(
-  :amount => 0,
-  :source => Spree::TaxRate.find_by_name!("North America"),
-  :order  => last_order,
-  :label => "Tax",
-  :state => "open",
-  :mandatory => true)
+last_order.adjustments.where(
+  source: Spree::TaxRate.find_by_name!("North America"),
+  order: last_order,
+  label: "Tax",
+  state: "open",
+  mandatory: true).first_or_create! do |adj|
+    adj.amount = 0
+  end
