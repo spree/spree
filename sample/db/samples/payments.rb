dea12ceb76d2b16274140077b2bf45c48f340fc1
Marta Wyszynska <marwyszynska@gmail.com>
2016-05-04 10:11:35 +0200

    samples fix - payments

--- sample/db/samples/payments.rb
+++ sample/db/samples/payments.rb
@@ -1,5 +1,5 @@
 # create payments based on the totals since they can't be known in YAML (quantities are random)
-method = Spree::PaymentMethod.where(:name => 'Credit Card', :active => true).first
+method = Spree::PaymentMethod.where(name: 'Credit Card', active: true).first
 
 # Hack the current method so we're able to return a gateway without a RAILS_ENV
 Spree::Gateway.class_eval do
@@ -12,11 +12,18 @@ end
 # reference it as such. Make it explicit here that this table has been renamed.
 Spree::CreditCard.table_name = 'spree_credit_cards'
 
-creditcard = Spree::CreditCard.create(:cc_type => 'visa', :month => 12, :year => 2.years.from_now.year, :last_digits => '1111',
-                                      :name => 'Sean Schofield', :gateway_customer_profile_id => 'BGS-1234')
+credit_card = Spree::CreditCard.where(cc_type: 'visa',
+                                      month: 12,
+                                      year: 2.years.from_now.year,
+                                      last_digits: '1111',
+                                      name: 'Sean Schofield',
+                                      gateway_customer_profile_id: 'BGS-1234').first_or_create!
 
-Spree::Order.all.each_with_index do |order, index|
+Spree::Order.all.each_with_index do |order, _index|
   order.update_with_updater!
-  payment = order.payments.create!(:amount => order.total, :source => creditcard.clone, :payment_method => method)
-  payment.update_columns(:state => 'pending', :response_code => '12345')
+  payment = order.payments.where(amount: BigDecimal(order.total, 4),
+                                 source: credit_card.clone,
+                                 payment_method: method).first_or_create!
+
+  payment.update_columns(state: 'pending', response_code: '12345')
 end
