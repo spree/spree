c61d2cc76518e20ad0405c795e28a0e5fb03d1ab
Krzysztof Rybka <krzysztof.rybka@gmail.com>
2016-06-02 21:37:18 +0200

    update poltergeist to ~> 1.9.0

--- common_spree_dependencies.rb
+++ common_spree_dependencies.rb
@@ -31,7 +31,7 @@ group :test do
   gem 'rspec-rails', '~> 3.4.2'
   gem 'simplecov'
   gem 'webmock', '1.8.11'
-  gem 'poltergeist', '1.6.0'
+  gem 'poltergeist', '~> 1.9.0'
   gem 'timecop'
   gem 'with_model'
   gem 'mutant-rspec', '~> 0.8.0'
