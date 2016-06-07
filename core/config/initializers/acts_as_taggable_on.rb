c307389c5412421f376bd7e2479283ad48c30acd
Priyank Gupta <priyank.gupta@vinsol.com>
2016-06-07 14:40:45 +0530

    remove binary collation config as already alteration is already done in migration

--- core/config/initializers/acts_as_taggable_on.rb
+++ core/config/initializers/acts_as_taggable_on.rb
@@ -1,7 +1,4 @@
 require "acts-as-taggable-on"
-if ActsAsTaggableOn::Utils.using_mysql?
-  ActsAsTaggableOn.force_binary_collation = true
-end
 
 ActsAsTaggableOn::Tag.class_eval do
   self.table_name_prefix = "spree_"
