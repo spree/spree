8072978d304be8f25e75147ce8f8278587e8b482
Vishal Zambre <v.zambre@gmail.com>
2016-07-01 18:14:12 +0530

    corrected reprecated method

--- core/app/models/spree/gateway.rb
+++ core/app/models/spree/gateway.rb
@@ -17,7 +17,7 @@ module Spree
       gateway_options = options
       gateway_options.delete :login if gateway_options.has_key?(:login) and gateway_options[:login].nil?
       if gateway_options[:server]
-        ActiveMerchant::Billing::Base.gateway_mode = gateway_options[:server].to_sym
+        ActiveMerchant::Billing::Base.mode = gateway_options[:server].to_sym
       end
       @provider ||= provider_class.new(gateway_options)
     end
