b0e989e2cdd360dcc453237b6143485bb00ce39a
Damian Legawiec <damian@sparksolutions.co>
2016-08-24 16:01:23 +0200

    Use keyword arguments in API specs
    ActionController::TestCase HTTP request methods without keyword params
    is deprecated.

--- api/spec/requests/ransackable_attributes_spec.rb
+++ api/spec/requests/ransackable_attributes_spec.rb
@@ -7,7 +7,7 @@ describe "Ransackable Attributes" do
     it "does not allow the filtering of variants by order attributes" do
       2.times { create(:variant) }
 
-      get "/api/v1/variants?q[orders_email_start]=#{order.email}", token: user.spree_api_key
+      get "/api/v1/variants?q[orders_email_start]=#{order.email}", params: { token: user.spree_api_key }
 
       variants_response = JSON.parse(response.body)
       expect(variants_response['total_count']).to eq(Spree::Variant.count)
@@ -18,7 +18,7 @@ describe "Ransackable Attributes" do
     it "does not allow the filtering of variants by user attributes" do
       2.times { create(:variant) }
 
-      get "/api/v1/variants?q[orders_user_email_start]=#{order.user.email}", token: user.spree_api_key
+      get "/api/v1/variants?q[orders_user_email_start]=#{order.user.email}", params: { token: user.spree_api_key }
 
       variants_response = JSON.parse(response.body)
       expect(variants_response['total_count']).to eq(Spree::Variant.count)
@@ -31,7 +31,7 @@ describe "Ransackable Attributes" do
       variant = create(:variant, product: product)
       other_variant = create(:variant)
 
-      get "/api/v1/variants?q[product_name_or_sku_cont]=fritos", token: user.spree_api_key
+      get "/api/v1/variants?q[product_name_or_sku_cont]=fritos", params: { token: user.spree_api_key }
 
       skus = JSON.parse(response.body)['variants'].map { |variant| variant['sku'] }
       expect(skus).to include variant.sku
@@ -44,7 +44,7 @@ describe "Ransackable Attributes" do
       product = create(:product, meta_title: "special product")
       other_product = create(:product)
 
-      get "/api/v1/products?q[meta_title_cont]=special", token: user.spree_api_key
+      get "/api/v1/products?q[meta_title_cont]=special", params: { token: user.spree_api_key }
 
       products_response = JSON.parse(response.body)
       expect(products_response['total_count']).to eq(Spree::Product.count)
@@ -54,7 +54,7 @@ describe "Ransackable Attributes" do
       product = create(:product)
       other_product = create(:product)
 
-      get "/api/v1/products?q[id_eq]=#{product.id}", token: user.spree_api_key
+      get "/api/v1/products?q[id_eq]=#{product.id}", params: { token: user.spree_api_key }
 
       product_names = JSON.parse(response.body)['products'].map { |product| product['name'] }
       expect(product_names).to include product.name
@@ -67,7 +67,7 @@ describe "Ransackable Attributes" do
       product = create(:product, name: "Fritos")
       other_product = create(:product)
 
-      get "/api/v1/products?q[name_cont]=fritos", token: user.spree_api_key
+      get "/api/v1/products?q[name_cont]=fritos", params: { token: user.spree_api_key }
 
       product_names = JSON.parse(response.body)['products'].map { |product| product['name'] }
       expect(product_names).to include product.name
