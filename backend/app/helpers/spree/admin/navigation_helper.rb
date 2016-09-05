921770893d8fb136d73e5609fef4267b6c5534d4
Damian Legawiec <damian@sparksolutions.co>
2016-09-05 11:37:24 +0200

    Fixes some funky per_page dropdown specs

--- backend/app/helpers/spree/admin/navigation_helper.rb
+++ backend/app/helpers/spree/admin/navigation_helper.rb
@@ -83,9 +83,11 @@ module Spree
           per_page_options = %w{5 15 30 45 60}
         end
 
+        selected_option = params[:per_page].try(:to_i) || per_page_default
+
         select_tag(:per_page,
-                   options_for_select(per_page_options, params[:per_page].try(:to_i) || per_page_default),
-                   class: 'form-control pull-right js-per-page-select')
+                   options_for_select(per_page_options, selected_option),
+                   class: "form-control pull-right js-per-page-select per-page-selected-#{selected_option}")
       end
 
       # helper method to create proper url to apply per page filtering
