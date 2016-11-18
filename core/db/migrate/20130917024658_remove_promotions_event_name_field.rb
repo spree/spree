c77c4b38572c2934dae9f90c0a08380931799ef6
deepakmahakale <deepakmahakale@gmail.com>
2016-11-18 15:47:26 +0530

    Add type while removing column to enable rollback
    Add type while removing the column to avoid the following error
    
    > remove_column is only reversible if given a type.

--- core/db/migrate/20130917024658_remove_promotions_event_name_field.rb
+++ core/db/migrate/20130917024658_remove_promotions_event_name_field.rb
@@ -1,5 +1,5 @@
 class RemovePromotionsEventNameField < ActiveRecord::Migration[4.2]
   def change
-    remove_column :spree_promotions, :event_name
+    remove_column :spree_promotions, :event_name, :string
   end
 end
