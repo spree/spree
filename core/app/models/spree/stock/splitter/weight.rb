fd622deba72581e2658ae457f52be35ea87bb8db
Nimish <nimish@vinsol.com>
2016-11-30 15:00:27 +0530

    Optimise the spree weight splitter

--- core/app/models/spree/stock/splitter/weight.rb
+++ core/app/models/spree/stock/splitter/weight.rb
@@ -9,26 +9,55 @@ module Spree
         end
 
         def split(packages)
+          generated_packages = []
           packages.each do |package|
-            removed_contents = reduce package
-            packages << build_package(removed_contents) unless removed_contents.empty?
+            generated_packages.push *reduce(package)
           end
+          packages.push *generated_packages
           return_next packages
         end
 
         private
         def reduce(package)
-          removed = []
-          while package.weight > self.threshold
-            contents = package.contents_by_weight
-            break if contents.size == 1
-            # Deleting the second heaviest item in the package should yield best results
-            removed << package.contents.delete(contents[1])
+          contents = package.contents_by_weight
+          # Treat current package as one of the generated packages for convenience and add the heaviest item
+          # This also prevents an additional package if no fit is possible
+          package.contents.clear
+          package.contents << contents.shift
+          generated = [package]
+          while contents.present?
+
+            package_to_use = choose_package generated, contents.first
+
+            if package_to_use.nil?
+              package_to_use = build_package
+              generated << package_to_use
+            end
+
+            package_to_use.contents << contents.shift
           end
-          removed
+
+          generated.drop 1 # Drop the original package to ensure only generated packages are returned
+        end
+
+        def choose_package(generated_packages, content_to_add)
+          # Implements worst fit
+          # See: http://www.labri.fr/perso/eyraud/pmwiki/uploads/Main/BinPackingSurvey.pdf, for survey of other techniques
+          package_to_use     = nil
+          available_space    = -1
+
+          generated_packages.each do |generated_package|
+            generated_package_weight = generated_package.weight
+            if ((generated_package_weight + content_to_add.weight <= self.threshold) &&
+                (available_space < self.threshold - generated_package_weight))
+              package_to_use = generated_package
+              available_space = self.threshold - generated_package_weight
+            end
+          end
+
+          package_to_use
         end
       end
     end
   end
 end
-
