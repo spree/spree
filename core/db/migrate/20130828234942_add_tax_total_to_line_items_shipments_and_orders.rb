24524310d00750f7506385983984a210dfe0fe8b
Deepak Mahakale <deepakmahakale@gmail.com>
2016-12-01 19:18:57 +0530

    Check presence of column with column_exists?
    Check presence of column with column_exists?

--- core/db/migrate/20130828234942_add_tax_total_to_line_items_shipments_and_orders.rb
+++ core/db/migrate/20130828234942_add_tax_total_to_line_items_shipments_and_orders.rb
@@ -3,6 +3,6 @@ class AddTaxTotalToLineItemsShipmentsAndOrders < ActiveRecord::Migration[4.2]
     add_column :spree_line_items, :tax_total, :decimal, precision: 10, scale: 2, default: 0.0
     add_column :spree_shipments, :tax_total, :decimal, precision: 10, scale: 2, default: 0.0
     # This column may already be here from a 2.1.x migration
-    add_column :spree_orders, :tax_total, :decimal, precision: 10, scale: 2, default: 0.0 unless Spree::Order.column_names.include?("tax_total")
+    add_column :spree_orders, :tax_total, :decimal, precision: 10, scale: 2, default: 0.0 unless column_exists? :spree_orders, :tax_total, :decimal
   end
 end
