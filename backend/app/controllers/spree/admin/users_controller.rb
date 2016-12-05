573c5295c280ce023cd1d43ceaa9823e653d544b
Damian Legawiec <damian@sparksolutions.co>
2016-12-05 17:42:44 +0100

    Admin::UsersController is html only endpoint, no need to handle XHR requests

--- backend/app/controllers/spree/admin/users_controller.rb
+++ backend/app/controllers/spree/admin/users_controller.rb
@@ -80,19 +80,8 @@ module Spree
         def collection
           return @collection if @collection.present?
           @collection = super
-          if request.xhr? && params[:q].present?
-            @collection = @collection.includes(:bill_address, :ship_address)
-                              .where("spree_users.email #{LIKE} :search
-                                     OR (spree_addresses.firstname #{LIKE} :search AND spree_addresses.id = spree_users.bill_address_id)
-                                     OR (spree_addresses.lastname  #{LIKE} :search AND spree_addresses.id = spree_users.bill_address_id)
-                                     OR (spree_addresses.firstname #{LIKE} :search AND spree_addresses.id = spree_users.ship_address_id)
-                                     OR (spree_addresses.lastname  #{LIKE} :search AND spree_addresses.id = spree_users.ship_address_id)",
-                                    { search: "#{params[:q].strip}%" })
-                              .limit(params[:limit] || 100)
-          else
-            @search = @collection.ransack(params[:q])
-            @collection = @search.result.page(params[:page]).per(Spree::Config[:admin_products_per_page])
-          end
+          @search = @collection.ransack(params[:q])
+          @collection = @search.result.page(params[:page]).per(Spree::Config[:admin_products_per_page])
         end
 
       private
