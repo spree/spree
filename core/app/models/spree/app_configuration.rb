747e07e8549a42c56e331ba095d8b77340b3d6c3
Damian Legawiec <damian@sparksolutions.co>
2016-12-05 14:10:18 +0100

    Use Kaminari.config.default_per_page for default values of all pagination preferences in Admin Panel

--- core/app/models/spree/app_configuration.rb
+++ core/app/models/spree/app_configuration.rb
@@ -23,10 +23,10 @@ module Spree
     preference :address_requires_state, :boolean, default: true # should state/state_name be required
     preference :admin_interface_logo, :string, default: 'admin/logo.png'
     preference :admin_path, :string, default: '/admin'
-    preference :admin_products_per_page, :integer, default: 30
-    preference :admin_orders_per_page, :integer, default: 30
-    preference :admin_properties_per_page, :integer, default: 30
-    preference :admin_promotions_per_page, :integer, default: 30
+    preference :admin_products_per_page, :integer, default: Kaminari.config.default_per_page
+    preference :admin_orders_per_page, :integer, default: Kaminari.config.default_per_page
+    preference :admin_properties_per_page, :integer, default: Kaminari.config.default_per_page
+    preference :admin_promotions_per_page, :integer, default: Kaminari.config.default_per_page
     preference :admin_show_version, :boolean, default: true
     preference :allow_checkout_on_gateway_error, :boolean, default: false
     preference :allow_guest_checkout, :boolean, default: true
