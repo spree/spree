21d41c7c1fd4f5d54ad02d0066aefe4931c4b576
wuboy <wuboys@gmail.com>
2017-02-19 22:23:52 +0800

    use SecureRandom for number generator
    use sample() with random seed may get into concurrency issue when two threads create number at the same time.
    It would be better to use SecureRandom instead.

--- core/lib/spree/core/number_generator.rb
+++ core/lib/spree/core/number_generator.rb
@@ -1,18 +1,13 @@
 module Spree
   module Core
     class NumberGenerator < Module
-      BASE           = 10
       DEFAULT_LENGTH = 9
-      NUMBERS        = (0..9).to_a.freeze
-      LETTERS        = ('A'..'Z').to_a.freeze
-
-      attr_accessor :prefix, :length
 
       def initialize(options)
         @random     = Random.new
         @prefix     = options.fetch(:prefix)
         @length     = options.fetch(:length, DEFAULT_LENGTH)
-        @candidates = NUMBERS + (options[:letters] ? LETTERS : [])
+        @letters    = options[:letters]
       end
 
       def included(host)
@@ -35,17 +30,13 @@ module Spree
       def generate_permalink(host)
         length = @length
 
-        loop do
-          candidate = new_candidate(length)
-          return candidate unless host.exists?(number: candidate)
-
-          # If over half of all possible options are taken add another digit.
-          length += 1 if host.count > Rational(BASE**length, 2)
+        candidate = @letters ? 36 : 10
+        number = loop do
+          random_token = SecureRandom.random_number(candidate**length).to_s(candidate).rjust(length, "0")
+          break random_token unless host.exists?(number: random_token)
         end
-      end
 
-      def new_candidate(length)
-        @prefix + length.times.map { @candidates.sample(random: @random) }.join
+        return @prefix + number
       end
     end # Permalink
   end # Core
