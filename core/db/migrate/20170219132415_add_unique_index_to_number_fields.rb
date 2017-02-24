320093a366fc52f7cd3f4440889ce07317943919
wuboy <wuboys@gmail.com>
2017-02-24 18:05:44 +0800

    fix for hound

--- core/db/migrate/20170219132415_add_unique_index_to_number_fields.rb
+++ core/db/migrate/20170219132415_add_unique_index_to_number_fields.rb
@@ -1,13 +1,13 @@
 class AddUniqueIndexToNumberFields < ActiveRecord::Migration
   def change
-    remove_index :spree_orders, :number
-    add_index :spree_orders, :number, :unique => true
-    add_index :spree_payments, :number, :unique => true
-    add_index :spree_reimbursements, :number, :unique => true
-    add_index :spree_return_authorizations, :number, :unique => true
+    remove_index spree_orders: :number
+    add_index :spree_orders, :number, unique: true
+    add_index :spree_payments, :number, unique: true
+    add_index :spree_reimbursements, :number, unique: true
+    add_index :spree_return_authorizations, :number, unique: true
     remove_index :spree_shipments, name: :index_shipments_on_number
-    add_index :spree_shipments, :number, :unique => true
+    add_index :spree_shipments, :number, unique: true
     remove_index :spree_stock_transfers, :number
-    add_index :spree_stock_transfers, :number, :unique => true
+    add_index :spree_stock_transfers, :number, unique: true
   end
 end
