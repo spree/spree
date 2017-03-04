bf848fb3f8d8ed6f2653b3ad23f85ef86c15e675
Davide Petilli <7h3.k3rn31@gmail.com>
2017-03-04 15:31:59 +0100

    Minor changes to workaround Globalize bug
    This is a workaround to the bug globalize/globalize#423 which is included
    in spree-contrib/spree_globalize. See pull request '#6677' for more.
    
    Pass the first argument of send as symbol

--- backend/app/controllers/spree/admin/resource_controller.rb
+++ backend/app/controllers/spree/admin/resource_controller.rb
@@ -158,7 +158,8 @@ class Spree::Admin::ResourceController < Spree::Admin::BaseController
   def parent
     if parent_data.present?
       @parent ||= parent_data[:model_class].
-          send("find_by_#{parent_data[:find_by]}", params["#{resource.model_name}_id"])
+        # Don't use `find_by_attribute_name` to workaround globalize/globalize#423 bug
+        send(:find_by, parent_data[:find_by].to_s => params["#{resource.model_name}_id"])
       instance_variable_set("@#{resource.model_name}", @parent)
     else
       nil
