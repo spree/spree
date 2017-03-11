0c66b28dff70d302b023b7c773e2aa55b5d883bd
wuboy <wuboys@gmail.com>
2017-03-11 18:08:58 +0800

    fix line too long

--- core/db/migrate/20170219132415_add_unique_index_to_number_fields.rb
+++ core/db/migrate/20170219132415_add_unique_index_to_number_fields.rb
@@ -1,14 +1,20 @@
-class AddUniqueIndexToNumberFields < ActiveRecord::Migration
+class AddUniqueIndexToNumberFields < ActiveRecord::Migration[5.0]
   def change
     remove_index :spree_orders, :number
     add_index :spree_orders, :number, :unique => true
-    remove_index :spree_payments, name: :index_spree_payments_on_number if index_name_exists?(:spree_payments, :index_spree_payments_on_number, nil)
+    if index_name_exists?(:spree_payments, :index_spree_payments_on_number, nil)
+      remove_index :spree_payments, name: :index_spree_payments_on_number
+    end
     add_index :spree_payments, :number, :unique => true
     add_index :spree_reimbursements, :number, :unique => true
     add_index :spree_return_authorizations, :number, :unique => true
-    remove_index :spree_shipments, name: :index_shipments_on_number if index_name_exists?(:spree_shipments, :index_shipments_on_number, nil)
+    if index_name_exists?(:spree_shipments, :index_shipments_on_number, nil)
+      remove_index :spree_shipments, name: :index_shipments_on_number
+    end
     add_index :spree_shipments, :number, :unique => true
-    remove_index :spree_stock_transfers, :number if index_exists?(:spree_stock_transfers, :number)
+    if index_exists?(:spree_stock_transfers, :number)
+      remove_index :spree_stock_transfers, :number
+    end
     add_index :spree_stock_transfers, :number, :unique => true
   end
 end
