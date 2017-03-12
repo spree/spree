cc184424ffcc7e32bccb08c0f785842c003eacd5
wuboy <wuboys@gmail.com>
2017-03-12 14:44:45 +0800

    fix number generator

--- core/lib/spree/core/number_generator.rb
+++ core/lib/spree/core/number_generator.rb
@@ -1,7 +1,11 @@
+
 module Spree
   module Core
     class NumberGenerator < Module
+      BASE           = 10      
       DEFAULT_LENGTH = 9
+
+      attr_accessor :prefix, :length
 
       def initialize(options)
         @random     = Random.new
@@ -30,14 +34,20 @@ module Spree
       def generate_permalink(host)
         length = @length
 
-        candidate = @letters ? 36 : 10
-        number = loop do
-          random_token = SecureRandom.random_number(candidate**length).to_s(candidate).rjust(length, '0')
-          break random_token unless host.exists?(number: random_token)
+        loop do
+          candidate = new_candidate(length)
+          return candidate unless host.exists?(number: candidate)
+          
+          # If over half of all possible options are taken add another digit.
+          length += 1 if host.count > Rational(BASE**length, 2)
         end
 
-        @prefix + number
       end
+
+      def new_candidate(length)
+        characters = @letters ? 36 : 10
+        @prefix + SecureRandom.random_number(characters**length).to_s(characters).rjust(length, '0')
+      end      
     end # Permalink
   end # Core
 end # Spree
