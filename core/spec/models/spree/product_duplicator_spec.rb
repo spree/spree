f197b6a5a83979c2985f66d3f05dd1b358012feb
Dawid Jaskot <dawid.jaskot@netguru.co>
2017-03-18 09:16:12 +0100

    Add spec for multiple cloning + refactor code with rubocop

--- core/spec/models/spree/product_duplicator_spec.rb
+++ core/spec/models/spree/product_duplicator_spec.rb
@@ -1,53 +1,55 @@
-require 'spec_helper'
+require "spec_helper"
 
 module Spree
-
   describe Spree::ProductDuplicator, type: :model do
+    let(:product) { create(:product, properties: [create(:property, name: "MyProperty")]) }
+    let!(:duplicator) { Spree::ProductDuplicator.new(product) }
 
-    let(:product) { create(:product, properties: [create(:property, name: "MyProperty")])}
-    let!(:duplicator) { Spree::ProductDuplicator.new(product)}
-
-    let(:image) { File.open(File.expand_path('../../../fixtures/thinking-cat.jpg', __FILE__)) }
-    let(:params) { {viewable_id: product.master.id, viewable_type: 'Spree::Variant', attachment: image, alt: "position 1", position: 1} }
+    let(:image) { File.open(File.expand_path("../../../fixtures/thinking-cat.jpg", __FILE__)) }
+    let(:params) do
+      {
+        viewable_id: product.master.id,
+        viewable_type: "Spree::Variant",
+        attachment: image,
+        alt: "position 1",
+        position: 1,
+      }
+    end
 
     before do
       Spree::Image.create(params)
     end
 
     it "will duplicate the product" do
-      expect{duplicator.duplicate}.to change{Spree::Product.count}.by(1)
+      expect { duplicator.duplicate }.to change { Spree::Product.count }.by(1)
     end
 
-    context 'when image duplication enabled' do
+    it "will duplicate already duplicated product" do
+      expect { 3.times { duplicator.duplicate } }.to change { Spree::Product.count }.by(3)
+    end
 
+    context "when image duplication enabled" do
       it "will duplicate the product images" do
-        expect{duplicator.duplicate}.to change{Spree::Image.count}.by(1)
+        expect { duplicator.duplicate }.to change { Spree::Image.count }.by(1)
       end
-
     end
 
-    context 'when image duplication disabled' do
-
+    context "when image duplication disabled" do
       let!(:duplicator) { Spree::ProductDuplicator.new(product, false) }
 
       it "will not duplicate the product images" do
-        expect{duplicator.duplicate}.to change{Spree::Image.count}.by(0)
+        expect { duplicator.duplicate }.to change { Spree::Image.count }.by(0)
       end
-
     end
 
-    context 'image duplication default' do
-
-      context 'when default is set to true' do
-
-        it 'clones images if no flag passed to initializer' do
-          expect{duplicator.duplicate}.to change{Spree::Image.count}.by(1)
+    context "image duplication default" do
+      context "when default is set to true" do
+        it "clones images if no flag passed to initializer" do
+          expect { duplicator.duplicate }.to change { Spree::Image.count }.by(1)
         end
-
       end
 
-      context 'when default is set to false' do
-
+      context "when default is set to false" do
         before do
           ProductDuplicator.clone_images_default = false
         end
@@ -56,16 +58,14 @@ module Spree
           ProductDuplicator.clone_images_default = true
         end
 
-        it 'does not clone images if no flag passed to initializer' do
-          expect{ProductDuplicator.new(product).duplicate}.to change{Spree::Image.count}.by(0)
+        it "does not clone images if no flag passed to initializer" do
+          expect { ProductDuplicator.new(product).duplicate }.to change { Spree::Image.count }.by(0)
         end
-
       end
-
     end
 
     context "product attributes" do
-      let!(:new_product) {duplicator.duplicate}
+      let!(:new_product) { duplicator.duplicate }
 
       it "will set an unique name" do
         expect(new_product.name).to eql "COPY OF #{product.name}"
@@ -82,22 +82,21 @@ module Spree
     end
 
     context "with variants" do
-      let(:option_type) { create(:option_type, name: "MyOptionType")}
-      let(:option_value1) { create(:option_value, name: "OptionValue1", option_type: option_type)}
-      let(:option_value2) { create(:option_value, name: "OptionValue2", option_type: option_type)}
+      let(:option_type) { create(:option_type, name: "MyOptionType") }
+      let(:option_value1) { create(:option_value, name: "OptionValue1", option_type: option_type) }
+      let(:option_value2) { create(:option_value, name: "OptionValue2", option_type: option_type) }
 
       let!(:variant1) { create(:variant, product: product, option_values: [option_value1]) }
       let!(:variant2) { create(:variant, product: product, option_values: [option_value2]) }
 
-      it  "will duplciate the variants" do
+      it "will duplciate the variants" do
         # will change the count by 3, since there will be a master variant as well
-        expect{duplicator.duplicate}.to change{Spree::Variant.count}.by(3)
+        expect { duplicator.duplicate }.to change { Spree::Variant.count }.by(3)
       end
 
       it "will not duplicate the option values" do
-        expect{duplicator.duplicate}.to change{Spree::OptionValue.count}.by(0)
+        expect { duplicator.duplicate }.to change { Spree::OptionValue.count }.by(0)
       end
-
     end
   end
 end
