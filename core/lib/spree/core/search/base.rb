aa8030970fad31b5efeeec55f419b522e4582773
Dawid Jaskot <dawid.jaskot@netguru.co>
2017-03-21 21:33:28 +0100

    Preload tax_category to omit N+1 queries

--- core/lib/spree/core/search/base.rb
+++ core/lib/spree/core/search/base.rb
@@ -57,6 +57,7 @@ module Spree
             # separate queries most of the time but opt for a join as soon as any
             # `where` constraints affecting joined tables are added to the search;
             # which is the case as soon as a taxon is added to the base scope.
+            scope = scope.preload(:tax_category)
             scope = scope.preload(master: :prices)
             scope = scope.preload(master: :images) if include_images
             scope
