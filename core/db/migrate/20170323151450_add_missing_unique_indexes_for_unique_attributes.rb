a4594122aa661eb89afc76b53511f4d457921b5e
symarys <maria.syczewska@sparksolutions.co>
2017-03-27 15:43:19 +0200

    Ensure there are no empty or duplicated fields in indexed columns

--- core/db/migrate/20170323151450_add_missing_unique_indexes_for_unique_attributes.rb
+++ core/db/migrate/20170323151450_add_missing_unique_indexes_for_unique_attributes.rb
@@ -1,26 +1,38 @@
 class AddMissingUniqueIndexesForUniqueAttributes < ActiveRecord::Migration[5.0]
   def change
     tables = {
-      spree_countries: [:name, :iso_name],
-      spree_option_types: [:name],
-      spree_option_values: [:name],
-      spree_promotions: [:path],
-      spree_refund_reasons: [:name],
-      spree_reimbursement_types: [:name],
-      spree_return_authorization_reasons: [:name],
-      spree_roles: [:name],
-      spree_shipping_categories: [:name],
-      spree_stores: [:code],
-      spree_tax_categories: [:name],
-      spree_trackers: [:analytics_id],
-      spree_zones: [:name]
+      country: [:name, :iso_name],
+      option_type: [:name],
+      option_value: [:name],
+      promotion: [:path],
+      refund_reason: [:name],
+      reimbursement_type: [:name],
+      return_authorization_reason: [:name],
+      role: [:name],
+      shipping_category: [:name],
+      store: [:code],
+      tax_category: [:name],
+      tracker: [:analytics_id],
+      zone: [:name]
     }
 
     tables.each do |table, columns|
+      table_class = "Spree::#{table.to_s.classify}".constantize
+      table_name = table_class.table_name
+
       columns.each do |column|
-        unless index_exists?(table, column, unique: true)
-          remove_index table, column if index_exists?(table, column)
-          add_index table, column, unique: true
+        unless index_exists?(table_name, column, unique: true)
+          attributes = table_class.unscoped.group(column).having('sum(1) > 1').pluck(column)
+          instances = table_class.where(column => [nil, attributes])
+
+          instances.find_each do |instance|
+            column_value = 'Unique String ' + SecureRandom.urlsafe_base64(8).upcase.delete('/+=_-')[0, 8]
+            instance.send("#{column}=", column_value)
+            instance.save
+          end
+
+          remove_index table_name, column if index_exists?(table_name, column)
+          add_index table_name, "lower(#{column})", unique: true
         end
       end
     end
