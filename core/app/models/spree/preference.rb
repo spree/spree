f5143f4bd30d2c06cdb01eadd2ac66f1297e495c
symarys <maria.syczewska@sparksolutions.co>
2017-03-27 15:42:21 +0200

    Add proper validation options

--- core/app/models/spree/preference.rb
+++ core/app/models/spree/preference.rb
@@ -1,5 +1,5 @@
 class Spree::Preference < Spree::Base
   serialize :value
 
-  validates :key, presence: true, uniqueness: true
+  validates :key, presence: true, uniqueness: { case_sensitive: false, allow_blank: true }
 end
