6b044d23ff4665d74458050a909b47f49b6cb51a
Josh Powell <josh@innovativeops.co>
2017-05-04 14:31:52 -0400

    remove deprecation warning passing class_name

--- core/app/models/spree/promotion_rule_user.rb
+++ core/app/models/spree/promotion_rule_user.rb
@@ -1,7 +1,7 @@
 module Spree
   class PromotionRuleUser < Spree::Base
     belongs_to :promotion_rule, class_name: 'Spree::PromotionRule'
-    belongs_to :user, class_name: Spree.user_class
+    belongs_to :user, class_name: Spree.user_class.to_s
 
     validates :user, :promotion_rule, presence: true
     validates :user_id, uniqueness: { scope: :promotion_rule_id }, allow_nil: true
