7b6feb284dfe43d24a78603c88b79be6dcfce17f
Dawid Jaskot <dawid.jaskot@gmail.com>
2017-05-11 18:26:11 +0200

    Fix for #7450 (#7992)

--- core/app/models/spree/order.rb
+++ core/app/models/spree/order.rb
@@ -46,7 +46,7 @@ module Spree
     checkout_flow do
       go_to_state :address
       go_to_state :delivery
-      go_to_state :payment, if: ->(order) { order.payment_required? }
+      go_to_state :payment, if: ->(order) { order.payment? || order.payment_required? }
       go_to_state :confirm, if: ->(order) { order.confirmation_required? }
       go_to_state :complete
       remove_transition from: :delivery, to: :confirm
