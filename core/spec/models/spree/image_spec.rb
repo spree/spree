a5f6147bda172a4d02bc23f621e1b669ffd5d55f
Josh Powell <josh@innovativeops.co>
2017-05-23 13:19:19 -0400

    [fix] deprecation warning - image

--- core/spec/models/spree/image_spec.rb
+++ core/spec/models/spree/image_spec.rb
@@ -3,6 +3,6 @@ require 'spec_helper'
 describe Spree::Image, type: :model do
 
   describe 'callbacks' do
-    it { is_expected.to callback(:find_dimensions).before(:save).if(:attachment_updated_at_changed?) }
+    it { is_expected.to callback(:find_dimensions).before(:save).if(:saved_change_to_attachment_updated_at?) }
   end
 end
