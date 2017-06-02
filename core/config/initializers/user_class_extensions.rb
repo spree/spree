1d90b82bb4ccf8422de8fa88b33f5e9e4f3e5eca
Damian Legawiec <damian@sparksolutions.co>
2017-06-02 10:54:01 +0200

    Remved deprecated automatic UserMethods injection

--- core/config/initializers/user_class_extensions.rb
+++
@@ -1,8 +0,0 @@
-# Ensure that Spree.user_class includes the UserMethods concern
-
-Spree::Core::Engine.config.to_prepare do
-  if Spree.user_class && !Spree.user_class.included_modules.include?(Spree::UserMethods)
-    ActiveSupport::Deprecation.warn "#{Spree.user_class} must include Spree::UserMethods"
-    Spree.user_class.include Spree::UserMethods
-  end
-end
