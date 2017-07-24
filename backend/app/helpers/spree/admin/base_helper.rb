67f8e5d87a67373c5ab7fb2168ae66ad468f2b2f
Fabrizio Monti <fabrizio.monti@welaika.com>
2017-07-24 14:53:04 +0200

    Admin field_container now supports options like content_tag does

--- backend/app/helpers/spree/admin/base_helper.rb
+++ backend/app/helpers/spree/admin/base_helper.rb
@@ -21,7 +21,10 @@ module Spree
         if error_message_on(model, method).present?
           css_classes << 'withError'
         end
-        content_tag(:div, capture(&block), class: css_classes.join(' '), id: "#{model}_#{method}_field")
+        content_tag(
+          :div, capture(&block),
+          options.merge(class: css_classes.join(' '), id: "#{model}_#{method}_field")
+        )
       end
 
       def error_message_on(object, method, options = {})
