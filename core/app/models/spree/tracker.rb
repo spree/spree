86b135b980bebb026093e28aea54cf256843e7c9
Punkbooster <ugliar.arsen@gmail.com>
2017-07-27 13:24:09 +0200

    Add cache to trackers current method

--- core/app/models/spree/tracker.rb
+++ core/app/models/spree/tracker.rb
@@ -3,13 +3,23 @@ module Spree
     TRACKER_ENGINES = %i(google_analytics segment).freeze
     enum engine: TRACKER_ENGINES
 
+    after_commit :clear_cache
+
     validates :analytics_id, presence: true, uniqueness: { scope: :engine, case_sensitive: false }
 
     scope :active, -> { where(active: true) }
 
     def self.current(engine = :google_analytics)
-      tracker = send(engine).active.first
+      tracker = Rails.cache.fetch("current_tracker/#{engine}") do
+        send(engine).active.first
+      end
       tracker&.analytics_id? ? tracker : nil
+    end
+
+    def clear_cache
+      TRACKER_ENGINES.each do |engine|
+        Rails.cache.delete("current_tracker/#{engine}")
+      end
     end
   end
 end
