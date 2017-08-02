a67f0e14984b159cb02f156396f80232a15f6804
wuboy <wuboys@gmail.com>
2017-08-02 18:42:22 +0800

    always record canceled_at when cancel an order

--- core/app/models/spree/order.rb
+++ core/app/models/spree/order.rb
@@ -548,8 +548,7 @@ module Spree
       transaction do
         cancel!
         update_columns(
-          canceler_id: user.id,
-          canceled_at: Time.current,
+          canceler_id: user.id
         )
       end
     end
@@ -655,8 +654,11 @@ module Spree
       # Free up authorized store credits
       payments.store_credits.pending.each(&:void!)
 
-      send_cancel_email
+      send_cancel_email   
       update_with_updater!
+      update_columns(
+        canceled_at: Time.current,
+      )         
     end
 
     def send_cancel_email
