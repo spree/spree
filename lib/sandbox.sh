ce6f384303f5a975dd576eed42d019bb7d63737a
Damian Legawiec <damian@sparksolutions.co>
2017-08-07 18:31:14 +0200

    Updated sandbox setup script
    Allows to select database type, skips git, spring, yarn and other
    unused rails stuff

--- lib/sandbox.sh
+++ lib/sandbox.sh
@@ -1,8 +1,34 @@
 #!/bin/sh
 # Used in the sandbox rake task in Rakefile
 
+set -e
+
+case "$DB" in
+postgres)
+  RAILSDB="postgresql"
+  ;;
+mysql)
+  RAILSDB="mysql"
+  ;;
+sqlite|'')
+  RAILSDB="sqlite3"
+  ;;
+*)
+  echo "Invalid DB specified: $DB"
+  exit 1
+  ;;
+esac
+
 rm -rf ./sandbox
-bundle exec rails new sandbox --skip-bundle
+bundle exec rails new sandbox --database="$RAILSDB" \
+  --skip-bundle \
+  --skip-git \
+  --skip-keeps \
+  --skip-rc \
+  --skip-spring \
+  --skip-test \
+  --skip-yarn
+
 if [ ! -d "sandbox" ]; then
   echo 'sandbox rails application failed'
   exit 1
@@ -22,5 +48,7 @@ end
 RUBY
 
 bundle install --gemfile Gemfile
+bundle exec rails db:drop || true
+bundle exec rails db:create
 bundle exec rails g spree:install --auto-accept --user_class=Spree::User --enforce_available_locales=true
 bundle exec rails g spree:auth:install
