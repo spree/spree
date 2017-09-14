c9447c1d4b45cca0eec3e181c2dfa1f5b13b56fa
Bartosz Bonisławski <b.bonislawski@hotmail.com>
2017-09-14 17:20:24 +0200

    add enable_coupons configuration option

--- frontend/app/models/spree/frontend_configuration.rb
+++ frontend/app/models/spree/frontend_configuration.rb
@@ -1,5 +1,6 @@
 module Spree
   class FrontendConfiguration < Preferences::Configuration
+    preference :coupon_codes_enabled, :boolean, default: true # Determines if we show coupon code form at cart and checkout
     preference :locale, :string, default: Rails.application.config.i18n.default_locale
   end
 end
