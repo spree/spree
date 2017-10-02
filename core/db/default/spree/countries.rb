8e613ed1146f712b41062413e8e8dd6a529e4220
Antony Ryabov <anton.ryabov@onetwotrip.com>
2017-10-02 22:42:41 +0300

    GH-8371: Rewrited countries seeds for ActiveRecord using

--- core/db/default/spree/countries.rb
+++ core/db/default/spree/countries.rb
@@ -1,30 +1,15 @@
 require 'carmen'
 
-connection      = ApplicationRecord.connection
-country_inserts = []
-
-country_values = -> do
-  Carmen::Country.all.each do |country|
-    name            = connection.quote country.name
-    iso3            = connection.quote country.alpha_3_code
-    iso             = connection.quote country.alpha_2_code
-    iso_name        = connection.quote country.name.upcase
-    numcode         = connection.quote country.numeric_code
-    states_required = connection.quote country.subregions?
-
-    country_inserts << [name, iso3, iso, iso_name, numcode, states_required].join(", ")
-  end
-
-  country_inserts.join("), (")
+Carmen::Country.all.each do |country|
+  Spree::Country.where(
+    name: country.name,
+    iso3: country.alpha_3_code,
+    iso: country.alpha_2_code,
+    iso_name: country.name.upcase,
+    numcode: country.numeric_code,
+    states_required: country.subregions?
+  ).first_or_create
 end
-
-columns = ["name", "iso3", "iso", "iso_name", "numcode", "states_required"]
-columns = connection.adapter_name =~ /MySQL/i ? columns.join(", ") : "\"#{columns.join('", "')}\""
-
-connection.execute <<-SQL
-  INSERT INTO spree_countries (#{columns})
-  VALUES (#{country_values.call});
-SQL
 
 Spree::Config[:default_country_id] = Spree::Country.find_by(iso: "US").id
 
