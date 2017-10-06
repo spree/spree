fc5326f8a39969f79dfd0b9237f1bc77455affbd
Damian Legawiec <damian@sparksolutions.co>
2017-10-06 11:50:33 +0200

    Deprecate ContentController#show because of dynamic render path vulnerability

--- frontend/app/controllers/spree/content_controller.rb
+++ frontend/app/controllers/spree/content_controller.rb
@@ -9,6 +9,26 @@ module Spree
     respond_to :html
 
     def show
+      ActiveSupport::Deprecation.warn(<<-EOS, caller)
+        ContentController#show is deprecated and will be removed in Spree 3.5
+        Please don't use dynamic render paths and just declare your actions in
+        ContentController decorator, eg.
+
+        routes.rb:
+        get '/content/custom_page', to: 'content#custom_page', as: :custom_page
+
+        controllers/spree/content_controller_decorator.rb:
+        Spree::ContentController.class_eval do
+          def custom_page
+          end
+        end
+
+        change links from:
+        spree.content_path('custom_page')
+
+        to:
+        spree.custom_page
+      EOS
       render action: params[:path]
     end
 
