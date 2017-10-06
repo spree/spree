a5bc62fa45b4a8fc2f548cd419b0c4fc2c6adb4d
Damian Legawiec <damian@sparksolutions.co>
2017-10-06 12:37:05 +0200

    Remove previously deprecated ContentController#show
    See https://github.com/spree/spree/pull/8379

--- frontend/config/routes.rb
+++ frontend/config/routes.rb
@@ -25,6 +25,6 @@ Spree::Core::Engine.add_routes do
 
   get '/unauthorized', to: 'home#unauthorized', as: :unauthorized
   get '/content/cvv', to: 'content#cvv', as: :cvv
-  get '/content/*path', to: 'content#show', as: :content
+  get '/content/test', to: 'content#test'
   get '/cart_link', to: 'store#cart_link', as: :cart_link
 end
