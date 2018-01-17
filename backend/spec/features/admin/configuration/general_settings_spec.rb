723760873ac8277bb5b35350298ff486cceb9424
Bartosz Bonisławski <b.bonislawski@hotmail.com>
2018-01-17 13:07:59 +0100

    remove editing store from general settings

--- backend/spec/features/admin/configuration/general_settings_spec.rb
+++ backend/spec/features/admin/configuration/general_settings_spec.rb
@@ -8,31 +8,11 @@ describe 'General Settings', type: :feature do
     visit spree.edit_admin_general_settings_path
   end
 
-  context 'visiting general settings (admin)' do
-    it 'has the right content' do
-      expect(page).to have_content('General Settings')
-      expect(find('#store_name').value).to eq('Test Store')
-      expect(find('#store_url').value).to eq('test.example.org')
-      expect(find('#store_mail_from_address').value).to eq('test@example.org')
-      expect(page).not_to have_content(Spree.t(:security_settings))
-    end
-  end
-
-  context 'editing general settings (admin)' do
-    it 'is able to update the site name' do
-      fill_in 'store_name', with: 'Spree Demo Site99'
-      fill_in 'store_mail_from_address', with: 'spree@example.org'
-      click_button 'Update'
-
-      assert_successful_update_message(:general_settings)
-      expect(find('#store_name').value).to eq('Spree Demo Site99')
-      expect(find('#store_mail_from_address').value).to eq('spree@example.org')
-    end
-  end
-
   context 'clearing the cache', js: true do
     it 'clears the cache' do
       expect(page).not_to have_content(Spree.t(:clear_cache_ok))
+      visit spree.edit_admin_general_settings_path
+      expect(page).to_not have_content(Spree.t(:clear_cache_ok))
       expect(page).to have_content(Spree.t(:clear_cache_warning))
 
       page.accept_confirm do
