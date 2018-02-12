14146fa2fad1ac926a9b7719ad230f41ac685fce
Saravana Kumar V(Saro) <vsk62.gate@gmail.com>
2018-02-12 10:13:10 +0530

    Enable searching for a taxon within a given taxonomy. Fixes #8502

--- api/spec/controllers/spree/api/v1/taxons_controller_spec.rb
+++ api/spec/controllers/spree/api/v1/taxons_controller_spec.rb
@@ -1,11 +1,17 @@
 require 'spec_helper'
 
+def expect_single_taxon_result(taxon_name)
+  expect(json_response['taxons'].count).to eq(1)
+  expect(json_response['taxons'].first['name']).to eq(taxon_name)
+end
+
 module Spree
   describe Api::V1::TaxonsController, type: :controller do
     render_views
 
     let!(:taxonomy) { create(:taxonomy) }
-    let!(:taxon) { create(:taxon, name: 'Ruby', taxonomy: taxonomy, parent_id: taxonomy.taxons.first.id) }
+    let!(:taxon) { create(:taxon, name: 'Ruby', taxonomy: taxonomy, parent_id: taxonomy.root.id) }
+    let!(:rust_taxon) { create(:taxon, name: 'Rust', taxonomy: taxonomy, parent_id: taxonomy.root.id) }
     let!(:taxon2) { create(:taxon, name: 'Rails', taxonomy: taxonomy, parent_id: taxon.id) }
     let!(:taxon3) { create(:taxon, name: 'React', taxonomy: taxonomy, parent_id: taxon2.id) }
     let(:attributes) { ['id', 'name', 'pretty_name', 'permalink', 'parent_id', 'taxonomy_id', 'meta_title', 'meta_description'] }
@@ -31,18 +37,31 @@ module Spree
       end
 
       it 'paginates through taxons' do
-        new_taxon = create(:taxon, name: 'Go', taxonomy: taxonomy, parent_id: taxonomy.taxons.first.id)
+        new_taxon = create(:taxon, name: 'Go', taxonomy: taxonomy, parent_id: taxonomy.root.id)
         taxonomy.root.children << new_taxon
-        expect(taxonomy.root.children.count).to eql(2)
+        expect(taxonomy.root.children.count).to eql(3)
         api_get :index, taxonomy_id: taxonomy.id, page: 1, per_page: 1
         expect(json_response['count']).to eql(1)
-        expect(json_response['total_count']).to eql(2)
+        expect(json_response['total_count']).to eql(3)
         expect(json_response['current_page']).to eql(1)
         expect(json_response['per_page']).to eql(1)
-        expect(json_response['pages']).to eql(2)
+        expect(json_response['pages']).to eql(3)
       end
 
       describe 'searching' do
+        context 'within a taxonomy' do
+          before do
+            api_get :index, taxonomy_id: taxonomy.id, q: { name_cont: name }
+          end
+
+          context 'searching for top level taxon' do
+            let(:name) { 'Ruby' }
+            it 'returns the matching taxons' do
+              expect_single_taxon_result 'Ruby'
+            end
+          end
+        end
+
         context 'with a name' do
           before do
             api_get :index, q: { name_cont: name }
@@ -52,8 +71,7 @@ module Spree
             let(:name) { 'Ruby' }
 
             it 'returns an array including the matching taxon' do
-              expect(json_response['taxons'].count).to eq(1)
-              expect(json_response['taxons'].first['name']).to eq 'Ruby'
+              expect_single_taxon_result 'Ruby'
             end
           end
 
@@ -73,9 +91,11 @@ module Spree
 
             expect(json_response['taxons'].first['name']).to eq taxonomy.root.name
             children = json_response['taxons'].first['taxons']
-            expect(children.count).to eq 1
+            expect(children.count).to eq 2
             expect(children.first['name']).to eq taxon.name
             expect(children.first['taxons'].count).to eq 1
+            expect(children.second['name']).to eq rust_taxon.name
+            expect(children.second['taxons'].count).to eq 0
           end
         end
       end
@@ -126,7 +146,7 @@ module Spree
         expect(json_response).to have_attributes(attributes)
         expect(response.status).to eq(201)
 
-        expect(taxonomy.reload.root.children.count).to eq 2
+        expect(taxonomy.reload.root.children.count).to eq 3
         taxon = Spree::Taxon.where(name: 'Colors').first
 
         expect(taxon.parent_id).to eq taxonomy.root.id
@@ -137,8 +157,8 @@ module Spree
         taxonomy.root.children << taxon2
         api_put :update, taxonomy_id: taxonomy.id, id: taxon.id, taxon: { parent_id: taxon.parent_id, child_index: 2 }
         expect(response.status).to eq(200)
-        expect(taxonomy.reload.root.children[0]).to eql taxon2
-        expect(taxonomy.reload.root.children[1]).to eql taxon
+        expect(taxonomy.reload.root.children[0]).to eql rust_taxon
+        expect(taxonomy.reload.root.children[1]).to eql taxon2
       end
 
       it 'cannot create a new taxon with invalid attributes' do
@@ -147,7 +167,7 @@ module Spree
         expect(json_response['error']).to eq('Invalid resource. Please fix errors and try again.')
         errors = json_response['errors']
 
-        expect(taxonomy.reload.root.children.count).to eq 1
+        expect(taxonomy.reload.root.children.count).to eq 2
       end
 
       it 'cannot create another root taxon' do
@@ -164,7 +184,7 @@ module Spree
         expect(errors['taxonomy_id']).not_to be_nil
         expect(errors['taxonomy_id'].first).to eq 'Invalid taxonomy id.'
 
-        expect(taxonomy.reload.root.children.count).to eq 1
+        expect(taxonomy.reload.root.children.count).to eq 2
       end
 
       it 'can destroy' do
