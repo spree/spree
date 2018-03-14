db6b8e4713bc9b8d4b9042c851785fcc7a2091f2
Dmitriy Kanununnikov <dmitriy.kanunnikov@gmail.com>
2018-03-14 18:16:38 +0100

    Add preview product button on products edit page

--- backend/spec/features/admin/products/edit/products_spec.rb
+++ backend/spec/features/admin/products/edit/products_spec.rb
@@ -4,13 +4,15 @@ describe 'Product Details', type: :feature, js: true do
   stub_authorization!
 
   context 'editing a product' do
-    it 'lists the product details' do
+    before do
       create(:product, name: 'Bún thịt nướng', sku: 'A100',
                        description: 'lorem ipsum', available_on: '2013-08-14 01:02:03')
 
       visit spree.admin_products_path
       within_row(1) { click_icon :edit }
+    end
 
+    it 'lists the product details' do
       click_link 'Details'
 
       expect(find('.content-header h1').text.strip).to eq('Products / Bún thịt nướng')
@@ -24,17 +26,17 @@ describe 'Product Details', type: :feature, js: true do
     end
 
     it 'handles slug changes' do
-      create(:product, name: 'Bún thịt nướng', sku: 'A100',
-                       description: 'lorem ipsum', available_on: '2011-01-01 01:01:01')
-
-      visit spree.admin_products_path
-      within('table.table tbody tr:nth-child(1)') do
-        click_icon(:edit)
-      end
-
       fill_in 'product_slug', with: 'random-slug-value'
       click_button 'Update'
       expect(page).to have_content('successfully updated!')
+    end
+
+    it 'has a link to preview a product' do
+      allow(Spree::Core::Engine).to receive(:frontend_available?).and_return(true)
+      allow_any_instance_of(Spree::BaseHelper).to receive(:product_url).and_return('http://example.com/products/product-slug')
+      click_link 'Details'
+      expect(page).to have_css('#admin_preview_product')
+      expect(page).to have_link Spree.t(:preview_product), href: 'http://example.com/products/product-slug'
     end
   end
 end
