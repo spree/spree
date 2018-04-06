5fb069567e6f94c1960cb78ea057051503e3bb0b
Bartosz Bonisławski <b.bonislawski@hotmail.com>
2018-04-06 11:03:13 +0200

    use chromedriver in extension generator

--- cmd/lib/spree_cmd/templates/extension/spec/support/capybara.rb
+++ cmd/lib/spree_cmd/templates/extension/spec/support/capybara.rb
@@ -1,12 +1,17 @@
 require 'capybara/rspec'
-require 'capybara/rails'
-require 'capybara/poltergeist'
+require 'capybara-screenshot'
 require 'capybara-screenshot/rspec'
+require 'capybara/rails'
 
 RSpec.configure do
-  Capybara.javascript_driver = :poltergeist
+  Capybara.register_driver :chrome do |app|
+    Capybara::Selenium::Driver.new app,
+      browser: :chrome,
+      options: Selenium::WebDriver::Chrome::Options.new(args: %w[headless disable-gpu window-size=1920,1080])
+  end
+  Capybara.javascript_driver = :chrome
 
-  Capybara.register_driver(:poltergeist) do |app|
-    Capybara::Poltergeist::Driver.new(app, js_errors: true, timeout: 60)
+  Capybara::Screenshot.register_driver(:chrome) do |driver, path|
+    driver.browser.save_screenshot(path)
   end
 end
