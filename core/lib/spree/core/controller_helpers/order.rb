8e185e3670c66aacd0c2372d94b80905f3534355
Bartosz Bonisławski <b.bonislawski@hotmail.com>
2018-04-06 12:40:30 +0200

    always save ip address when creating order

--- core/lib/spree/core/controller_helpers/order.rb
+++ core/lib/spree/core/controller_helpers/order.rb
@@ -29,19 +29,20 @@ module Spree
         def current_order(options = {})
           options[:create_order_if_necessary] ||= false
 
-          return @current_order if @current_order
+          if @current_order
+            @current_order.last_ip_address = ip_address
+            return @current_order
+          end
 
           @current_order = find_order_by_token_or_user(options, true)
 
           if options[:create_order_if_necessary] && (@current_order.nil? || @current_order.completed?)
             @current_order = Spree::Order.create!(current_order_params)
             @current_order.associate_user! try_spree_current_user if try_spree_current_user
+            @current_order.last_ip_address = ip_address
           end
 
-          if @current_order
-            @current_order.last_ip_address = ip_address
-            return @current_order
-          end
+          @current_order
         end
 
         def associate_user
