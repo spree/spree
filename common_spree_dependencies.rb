afa6644f91bcbb57737693637251b9763de2744d
Matthew Kennedy <m.kennedy@me.com>
2018-04-11 14:09:54 +0100

    Move Dependencies to one file, and edit travis.yml to work
    NOTE See comment in travis.yml
    
    Final new line missing code climate.

--- common_spree_dependencies.rb
+++ common_spree_dependencies.rb
@@ -14,32 +14,35 @@ platforms :jruby do
 end
 
 platforms :ruby do
-  gem 'mysql2'
+  gem 'mysql2', '~> 0.4.10'
   gem 'pg', '~> 0.18'
 end
 
 group :test do
-  gem 'capybara', '~> 2.16'
+  gem 'bootstrap-sass', '>= 3.3.5.1', '< 3.4'
+  gem 'capybara', '~> 2.4'
   gem 'capybara-screenshot', '~> 1.0'
+  gem 'canonical-rails', '~> 0.2.0'
   gem 'database_cleaner', '~> 1.3'
   gem 'email_spec'
   gem 'factory_bot_rails', '~> 4.8'
+  gem 'jquery-rails', '~> 4.3'
+  gem 'jquery-ui-rails', '~> 6.0.1'
   gem 'launchy'
+  gem 'rabl', '~> 0.13.1'
+  gem 'rack-test', '0.7.0'
+  gem 'rails-controller-testing'
   gem 'rspec-activemodel-mocks', '~> 1.0'
   gem 'rspec-collection_matchers'
   gem 'rspec-its'
   gem 'rspec-rails', '~> 3.7.2'
   gem 'rspec-retry'
   gem 'rspec_junit_formatter'
+  gem 'select2-rails', '3.5.9.1' # 3.5.9.2 breaks several specs
+  gem 'selenium-webdriver'
   gem 'simplecov'
   gem 'webmock', '~> 3.0.1'
-  gem 'selenium-webdriver'
   gem 'timecop'
-  gem 'rails-controller-testing'
-end
-
-group :test, :development do
-  gem 'rubocop', require: false
-  gem 'rubocop-rspec', require: false
-  gem 'pry-byebug'
+  gem 'versioncake', '~> 3.3.0'
+  gem 'with_model'
 end
