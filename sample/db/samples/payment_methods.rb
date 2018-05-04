cc4f94bdb58ee8104281370d41fb188776f28e87
= <=>
2018-05-04 14:43:29 +0200

    squash all commits

--- sample/db/samples/payment_methods.rb
+++ sample/db/samples/payment_methods.rb
@@ -1,11 +1,11 @@
 Spree::Gateway::Bogus.where(
-  name: "Credit Card",
-  description: "Bogus payment gateway.",
+  name: 'Credit Card',
+  description: 'Bogus payment gateway.',
   active: true
 ).first_or_create!
 
 Spree::PaymentMethod::Check.where(
-  name: "Check",
-  description: "Pay by check.",
+  name: 'Check',
+  description: 'Pay by check.',
   active: true
 ).first_or_create!
--- sample/lib/spree/sample.rb
+++ sample/lib/spree/sample.rb
@@ -5,7 +5,7 @@ module Spree
       path = if File.exist?(File.join(Rails.root, 'db', 'samples', "#{file}.rb"))
                File.expand_path(File.join(Rails.root, 'db', 'samples', "#{file}.rb"))
              else
-        # Otherwise we will use this gems default file.
+               # Otherwise we will use this gems default file.
                File.expand_path(samples_path + "#{file}.rb")
              end
       # Check to see if the specified file has been loaded before
@@ -14,8 +14,6 @@ module Spree
         puts "Loaded #{file.titleize} samples"
       end
     end
-
-    private
 
     def self.samples_path
       Pathname.new(File.join(File.dirname(__FILE__), '..', '..', 'db', 'samples'))
