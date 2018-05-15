68cad21af17d791742c75854a57f49208334cfc0
holylance <holylance82@gmail.com>
2018-05-15 16:45:19 +0200

    fix all rubocop error in frontend

--- frontend/app/helpers/spree/frontend_helper.rb
+++ frontend/app/helpers/spree/frontend_helper.rb
@@ -8,23 +8,21 @@ module Spree
     def spree_breadcrumbs(taxon, separator = '&nbsp;')
       return '' if current_page?('/') || taxon.nil?
       separator = raw(separator)
-      crumbs = [content_tag(:li, content_tag(:span, link_to(content_tag(:span, Spree.t(:home), itemprop: 'name'), spree.root_path, itemprop: 'url') + separator, itemprop: 'item'), itemscope: 'itemscope', itemtype: 'https://schema.org/ListItem', itemprop: 'itemListElement')]
-      if taxon
-        crumbs << content_tag(:li, content_tag(:span, link_to(content_tag(:span, Spree.t(:products), itemprop: 'name'), spree.products_path, itemprop: 'url') + separator, itemprop: 'item'), itemscope: 'itemscope', itemtype: 'https://schema.org/ListItem', itemprop: 'itemListElement')
-        crumbs << taxon.ancestors.collect { |ancestor| content_tag(:li, content_tag(:span, link_to(content_tag(:span, ancestor.name, itemprop: 'name'), seo_url(ancestor), itemprop: 'url') + separator, itemprop: 'item'), itemscope: 'itemscope', itemtype: 'https://schema.org/ListItem', itemprop: 'itemListElement') } unless taxon.ancestors.empty?
-        crumbs << content_tag(:li, content_tag(:span, link_to(content_tag(:span, taxon.name, itemprop: 'name'), seo_url(taxon), itemprop: 'url'), itemprop: 'item'), class: 'active', itemscope: 'itemscope', itemtype: 'https://schema.org/ListItem', itemprop: 'itemListElement')
-      else
-        crumbs << content_tag(:li, content_tag(:span, Spree.t(:products), itemprop: 'item'), class: 'active', itemscope: 'itemscope', itemtype: 'https://schema.org/ListItem', itemprop: 'itemListElement')
-      end
+      crumbs = generate_crumbs([], Spree.t(:home), spree.root_path, separator)
+      crumbs = if taxon
+                 generate_crumbs_with_taxon(crumbs, taxon, separator)
+               else
+                 generate_crumbs_without_taxon(crumbs)
+               end
       crumb_list = content_tag(:ol, raw(crumbs.flatten.map(&:mb_chars).join), class: 'breadcrumb', itemscope: 'itemscope', itemtype: 'https://schema.org/BreadcrumbList')
       content_tag(:nav, crumb_list, id: 'breadcrumbs', class: 'col-md-12')
     end
 
     def breadcrumbs(taxon, separator = '&nbsp;')
-      ActiveSupport::Deprecation.warn(<<-EOS, caller)
+      ActiveSupport::Deprecation.warn(<<-WARNING_STR, caller)
         Spree::FrontendHelper#breadcrumbs was renamed to Spree::FrontendHelper#spree_breadcrumbs
         and will be removed in Spree 3.6. Please update your code to avoid problems after update
-      EOS
+      WARNING_STR
       spree_breadcrumbs(taxon, separator)
     end
 
@@ -45,7 +43,7 @@ module Spree
 
         css_classes << 'next' if state_index == current_index + 1
         css_classes << 'active' if state == @order.state
-        css_classes << 'first' if state_index == 0
+        css_classes << 'first' if state_index.zero?
         css_classes << 'last' if state_index == states.length - 1
         # No more joined classes. IE6 is not a target browser.
         # Hack: Stops <a> being wrapped round previous items twice.
@@ -62,9 +60,7 @@ module Spree
       ignore_types = ['order_completed'].concat(Array(opts[:ignore_types]).map(&:to_s) || [])
 
       flash.each do |msg_type, text|
-        unless ignore_types.include?(msg_type)
-          concat(content_tag(:div, text, class: "alert alert-#{msg_type}"))
-        end
+        concat(content_tag(:div, text, class: "alert alert-#{msg_type}")) unless ignore_types.include?(msg_type)
       end
       nil
     end
@@ -77,7 +73,8 @@ module Spree
         text = "<span class='glyphicon glyphicon-shopping-cart'></span> #{text}: (#{Spree.t('empty')})"
         css_class = 'empty'
       else
-        text = "<span class='glyphicon glyphicon-shopping-cart'></span> #{text}: (#{simple_current_order.item_count})  <span class='amount'>#{simple_current_order.display_total.to_html}</span>"
+        text = "<span class='glyphicon glyphicon-shopping-cart'></span> #{text}: (#{simple_current_order.item_count})"\
+          "  <span class='amount'>#{simple_current_order.display_total.to_html}</span>"
         css_class = 'full'
       end
 
@@ -93,6 +90,64 @@ module Spree
         end
         safe_join(taxons, "\n")
       end
+    end
+
+    private
+
+    def generate_crumbs_with_taxon(crumbs, taxon, separator)
+      crumbs = generate_crumbs(crumbs, Spree.t(:products), spree.products_path, separator)
+
+      unless taxon.ancestors.empty?
+        taxon.ancestors.each do |ancestor|
+          crumbs = generate_crumbs(crumbs, ancestor.name, seo_url(ancestor), separator)
+        end
+      end
+
+      crumbs = generate_active_crumbs(crumbs, taxon.name, seo_url(taxon))
+      crumbs
+    end
+
+    def generate_crumbs(crumbs, name, path, separator)
+      crumbs << content_tag(
+        :li,
+        content_tag(
+          :span,
+          link_to(content_tag(:span, name, itemprop: 'name'), path, itemprop: 'url') + separator,
+          itemprop: 'item'
+        ),
+        itemscope: 'itemscope',
+        itemtype: 'https://schema.org/ListItem',
+        itemprop: 'itemListElement'
+      )
+      crumbs
+    end
+
+    def generate_active_crumbs(crumbs, name, path)
+      crumbs << content_tag(
+        :li,
+        content_tag(
+          :span,
+          link_to(content_tag(:span, name, itemprop: 'name'), path, itemprop: 'url'),
+          itemprop: 'item'
+        ),
+        class: 'active',
+        itemscope: 'itemscope',
+        itemtype: 'https://schema.org/ListItem',
+        itemprop: 'itemListElement'
+      )
+      crumbs
+    end
+
+    def generate_crumbs_without_taxon(crumbs)
+      crumbs << content_tag(
+        :li,
+        content_tag(:span, Spree.t(:products), itemprop: 'item'),
+        class: 'active',
+        itemscope: 'itemscope',
+        itemtype: 'https://schema.org/ListItem',
+        itemprop: 'itemListElement'
+      )
+      crumbs
     end
   end
 end
