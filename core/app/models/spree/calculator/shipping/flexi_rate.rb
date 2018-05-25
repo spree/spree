577ecc6352299442969175abc22da5085d4932f3
Yuriy Kushnir <kushnir.yb@gmail.com>
2018-05-25 11:43:05 +0200

    Fix Flexi Rate calculators #8042

--- core/app/models/spree/calculator/shipping/flexi_rate.rb
+++ core/app/models/spree/calculator/shipping/flexi_rate.rb
@@ -13,22 +13,22 @@ module Spree
       end
 
       def compute_package(package)
-        compute_from_quantity(package.contents.sum(&:quantity))
+        quantity = package.contents.sum(&:quantity)
+
+        compute_from_quantity(quantity)
       end
 
-      def compute_from_quantity(quantity)
-        sum = 0
-        max = preferred_max_items.to_i
-        quantity.times do |i|
-          # check max value to avoid divide by 0 errors
-          sum += if (max == 0 && i == 0) || (max > 0) && (i % max == 0)
-                   preferred_first_item.to_f
-                 else
-                   preferred_additional_item.to_f
-                 end
-        end
+      delegate :compute_from_quantity, to: :flexi_rate_calculator
 
-        sum
+      private
+
+      def flexi_rate_calculator
+        ::Spree::Calculator::FlexiRate.new(
+          preferred_additional_item: preferred_additional_item,
+          preferred_first_item:      preferred_first_item,
+          preferred_max_items:       preferred_max_items,
+          preferred_currency:        preferred_currency
+        )
       end
     end
   end
