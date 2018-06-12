8415207edfe1f8b5b05deaf9f0a8d0a908d60301
WitekL <witold.leicht@gmail.com>
2018-06-12 08:49:17 +0200

    Change http status codes to numerical representation

--- api/app/controllers/spree/api/v2/storefront/cart_controller.rb
+++ api/app/controllers/spree/api/v2/storefront/cart_controller.rb
@@ -7,33 +7,33 @@ module Spree
             spree_authorize! :create, Spree::Order
 
             order = spree_current_order || dependencies[:create_cart].call(user: spree_current_user, store: spree_current_store).value
-            render json: serialize_order(order), status: :created
+            render json: serialize_order(order), status: 201
           end
 
           def add_item
             variant = Spree::Variant.find(params[:variant_id])
 
             if spree_current_order.nil?
-              render json: { error: "Order doesn't exist" }, status: :not_found
+              render json: { error: "Order doesn't exist" }, status: 404
             else
               spree_authorize! :update, spree_current_order, order_token
               spree_authorize! :show, variant
 
               dependencies[:add_item_to_cart].call(order: spree_current_order, variant: variant, quantity: params[:quantity])
-              render json: serialized_current_order, status: :ok
+              render json: serialized_current_order, status: 200
             end
           end
 
           def remove_item
             if spree_current_order.nil?
-              render json: { error: "Order doesn't exist" }, status: :not_found
+              render json: { error: "Order doesn't exist" }, status: 404
             else
               line_item = spree_current_order.line_items.find(params[:line_item_id])
 
               spree_authorize! :update, spree_current_order, order_token
 
               dependencies[:remove_item_from_cart].call(order: spree_current_order, line_item: line_item)
-              render json: serialized_current_order, status: :ok
+              render json: serialized_current_order, status: 200
             end
           end
 
