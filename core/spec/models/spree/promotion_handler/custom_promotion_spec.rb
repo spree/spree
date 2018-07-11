fbbd0eaa6a2c4c20c6cf3f57c15d69502174692a
Luis Enrique <luisenrike9303@gmail.com>
2018-07-11 14:05:08 -0400

    failing test

---
+++ core/spec/models/spree/promotion_handler/custom_promotion_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+class CustomPromotionRule < Spree::PromotionRule
+  def applicable?(promotable)
+    false
+  end
+end
+
+module Spree
+  module PromotionHandler
+    describe Cart, type: :model do
+      let(:promotion) { Promotion.create(name: 'At line items') }
+      let(:line_item) { create(:line_item) }
+      let(:order) { line_item.order }
+      let(:rule) { CustomPromotionRule.create(promotion: promotion) }
+
+      it 'should not be eligible' do
+        promotion.promotion_rules << rule
+        expect(promotion.eligible?(order)).to be false
+      end
+    end
+  end
+end
