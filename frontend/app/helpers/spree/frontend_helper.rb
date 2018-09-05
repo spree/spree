aed7ac37226fcdc6e0545d04c90b5d75ea18d33e
Alexey Topolyanskiy <devilcoders@protonmail.ch>
2018-09-05 15:51:15 +0200

    Update sidebar nav classes

--- frontend/app/helpers/spree/frontend_helper.rb
+++ frontend/app/helpers/spree/frontend_helper.rb
@@ -83,7 +83,7 @@ module Spree
 
       content_tag :div, class: 'list-group' do
         taxons = root_taxon.children.map do |taxon|
-          css_class = current_taxon&.self_and_ancestors&.include?(taxon) ? 'list-group-item active' : 'list-group-item'
+          css_class = current_taxon&.self_and_ancestors&.include?(taxon) ? 'list-group-item list-group-item-action active' : 'list-group-item list-group-item-action'
           link_to(taxon.name, seo_url(taxon), class: css_class) + taxons_tree(taxon, current_taxon, max_level - 1)
         end
         safe_join(taxons, "\n")
