d8c56bfa7c35a673ebd1d17426a5f37f029feda9
Alexey Topolyanskiy <devilcoders@protonmail.ch>
2018-09-08 13:12:54 +0200

    Product thumbnails fixes. Header padding.

--- frontend/app/assets/javascripts/spree/frontend/product.js
+++ frontend/app/assets/javascripts/spree/frontend/product.js
@@ -3,33 +3,40 @@
 
 Spree.ready(function ($) {
   Spree.addImageHandlers = function () {
-    var thumbnails = $('#product-images ul.thumbnails');
-    ($('#main-image')).data('selectedThumb', ($('#main-image img')).attr('src'));
-    ($('#main-image')).data('selectedThumbAlt', ($('#main-image img')).attr('alt'))
-    thumbnails.find('li').eq(0).addClass('selected')
+    var thumbnails = $("#product-images ul.thumbnails")
 
-    thumbnails.find('a').on('click', function (event) {
-      ($('#main-image')).data('selectedThumb', ($(event.currentTarget)).attr('href'));
-      ($('#main-image')).data('selectedThumbId', ($(event.currentTarget)).parent().attr('id'));
-      ($('#main-image')).data('selectedThumbAlt', ($(event.currentTarget)).find('img').attr('alt'))
-      thumbnails.find('li').removeClass('selected');
-      ($(event.currentTarget)).parent('li').addClass('selected')
+    ($("#main-image")).data("selectedThumb", ($("#main-image img")).attr("src"))
+
+    thumbnails
+      .find("li")
+      .eq(0)
+      .addClass("selected")
+      .find('img')
+      .addClass('border-primary')
+
+    thumbnails.find("a").on("click", function (event) {
+      ($("#main-image")).data("selectedThumb", ($(event.currentTarget)).attr("href"))
+      ($("#main-image")).data("selectedThumbId", ($(event.currentTarget)).parent().attr("id"))
+      thumbnails.find("li").removeClass("selected")
+      thumbnails.find('img').removeClass('border-primary')
+      $(this).find('img').addClass('border-primary')
+      ($(event.currentTarget)).parent("li").addClass("selected")
       return false
     })
 
-    thumbnails.find('li').on('mouseenter', function (event) {
-      return ($('#main-image img'))
-        .attr({ 'src': ($(event.currentTarget)).find('a').attr('href'), 'alt': ($(event.currentTarget)).find('img').attr('alt') })
+    thumbnails.find("li").on("mouseenter", function (event) {
+      $(this).find('img').addClass('border-success')
+      return ($("#main-image img")).attr("src", ($(event.currentTarget)).find("a").attr("href"))
     })
 
-    return thumbnails.find('li').on('mouseleave', function (event) {
-      return ($('#main-image img'))
-        .attr({ 'src': ($('#main-image')).data('selectedThumb'), 'alt': ($('#main-image')).data('selectedThumbAlt') })
+    return thumbnails.find("li").on("mouseleave", function (event) {
+      $(this).find('img').removeClass('border-success')
+      return ($("#main-image img")).attr("src", ($("#main-image")).data("selectedThumb"))
     })
   }
 
   Spree.showVariantImages = function (variantId) {
-    ($('li.vtmb')).hide();
+    ($('li.vtmb')).hide()
     ($('li.tmb-' + variantId)).show()
     var currentThumb = $('#' + ($('#main-image')).data('selectedThumbId'))
 
@@ -42,10 +49,10 @@ Spree.ready(function ($) {
 
       var newImg = thumb.find('a').attr('href')
 
-      var newAlt = thumb.find('img').attr('alt');
+      var newAlt = thumb.find('img').attr('alt')
       ($('#product-images ul.thumbnails li')).removeClass('selected')
-      thumb.addClass('selected');
-      ($('#main-image img')).attr({ 'src': newImg, 'alt': newAlt });
+      thumb.addClass('selected')
+      ($('#main-image img')).attr({ 'src': newImg, 'alt': newAlt })
       ($('#main-image')).data({ 'selectedThumb': newImg, 'selectedThumbAlt': newAlt })
       return ($('#main-image')).data('selectedThumbId', thumb.attr('id'))
     }
