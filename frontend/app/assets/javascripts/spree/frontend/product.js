c1e209f135c8d6e8b068ed36a38ef1c3bfa733d4
Avneet Singh Malhotra <avneet@vinsol.com>
2018-09-13 19:31:16 +0530

    Fix product & it's variants alt attribute value displayed on frontend

--- frontend/app/assets/javascripts/spree/frontend/product.js
+++ frontend/app/assets/javascripts/spree/frontend/product.js
@@ -1,88 +1,93 @@
 Spree.ready(function ($) {
-
   Spree.addImageHandlers = function () {
-    var thumbnails = $("#product-images ul.thumbnails");
+    var thumbnails = $('#product-images ul.thumbnails')
 
-    ($("#main-image")).data("selectedThumb", ($("#main-image img")).attr("src"));
+    $('#main-image').data('selectedThumb', $('#main-image img').attr('src'))
+    $('#main-image').data('selectedThumbAlt', $('#main-image img').attr('alt'))
 
     thumbnails
-      .find("li")
+      .find('li')
       .eq(0)
-      .addClass("selected")
+      .addClass('selected')
       .find('img')
-      .addClass('border-primary');
+      .addClass('border-primary')
 
-    thumbnails.find("a").on("click", function (event) {
-      ($("#main-image")).data("selectedThumb", ($(event.currentTarget)).attr("href"));
-      ($("#main-image")).data("selectedThumbId", ($(event.currentTarget)).parent().attr("id"));
-      thumbnails.find("li").removeClass("selected");
-      thumbnails.find('img').removeClass('border-primary');
-      $(this).find('img').addClass('border-primary');
-      ($(event.currentTarget)).parent("li").addClass("selected");
-      return false;
-    });
+    thumbnails.find('a').on('click', function (event) {
+      $('#main-image').data('selectedThumb', ($(event.currentTarget)).attr('href'))
+      $('#main-image').data('selectedThumbId', ($(event.currentTarget)).parent().attr('id'))
+      $('#main-image').data('selectedThumbAlt', ($(event.currentTarget)).find('img').attr('alt'))
+      thumbnails.find('li').removeClass('selected')
+      thumbnails.find('img').removeClass('border-primary')
+      $(this).find('img').addClass('border-primary')
+      $(event.currentTarget).parent('li').addClass('selected')
+      return false
+    })
 
-    thumbnails.find("li").on("mouseenter", function (event) {
-      $(this).find('img').addClass('border-success');
-      return ($("#main-image img")).attr("src", ($(event.currentTarget)).find("a").attr("href"));
-    });
+    thumbnails.find('li').on('mouseenter', function (event) {
+      $(this).find('img').addClass('border-success')
+      return ($('#main-image img'))
+        .attr({'src': ($(event.currentTarget)).find('a').attr('href'), 'alt': ($(event.currentTarget)).find('img').attr('alt')})
+    })
 
-    return thumbnails.find("li").on("mouseleave", function (event) {
-      $(this).find('img').removeClass('border-success');
-      return ($("#main-image img")).attr("src", ($("#main-image")).data("selectedThumb"));
-    });
-  };
-
-  Spree.showVariantImages = function (variantId) {
-    ($("li.vtmb")).hide();
-    ($("li.tmb-" + variantId)).show();
-    var currentThumb = $("#" + ($("#main-image")).data("selectedThumbId"));
-
-    if (!currentThumb.hasClass("vtmb + variantId")) {
-      var thumb = $(($("#product-images ul.thumbnails li:visible.vtmb")).eq(0));
-
-      if (!(thumb.length > 0)) {
-        thumb = $(($("#product-images ul.thumbnails li:visible")).eq(0));
-      }
-
-      var newImg = thumb.find("a").attr("href");
-      ($("#product-images ul.thumbnails li")).removeClass("selected");
-      thumb.addClass("selected");
-      ($("#main-image img")).attr("src", newImg);
-      ($("#main-image")).data("selectedThumb", newImg);
-      return ($("#main-image")).data("selectedThumbId", thumb.attr("id"));
-    }
-  };
-
-  Spree.updateVariantPrice = function (variant) {
-    var variantPrice = variant.data("price");
-
-    if (variantPrice) {
-      return ($(".price.selling")).text(variantPrice);
-    }
-  };
-
-  Spree.disableCartForm = function (variant) {
-    var inStock = variant.data("in-stock");
-    return $("#add-to-cart-button").attr("disabled", !inStock);
-  };
-
-  var radios = $("#product-variants input[type='radio']");
-
-  if (radios.length > 0) {
-    var selectedRadio = $("#product-variants input[type='radio'][checked='checked']");
-    Spree.showVariantImages(selectedRadio.attr("value"));
-    Spree.updateVariantPrice(selectedRadio);
-    Spree.disableCartForm(selectedRadio);
-
-    radios.click(function (event) {
-      $("#product-variants").find('li.active').removeClass("active");
-      $(this).closest("li").addClass("active");
-      Spree.showVariantImages(this.value);
-      Spree.updateVariantPrice($(this));
-      return Spree.disableCartForm($(this));
-    });
+    return thumbnails.find('li').on('mouseleave', function (event) {
+      $(this).find('img').removeClass('border-success')
+      return ($('#main-image img'))
+        .attr({'src': ($('#main-image')).data('selectedThumb'), 'alt': ($('#main-image')).data('selectedThumbAlt')})
+    })
   }
 
-  return Spree.addImageHandlers();
-});
+  Spree.showVariantImages = function (variantId) {
+    ($('li.vtmb')).hide();
+    ($('li.tmb-' + variantId)).show()
+    var currentThumb = $('#' + ($('#main-image')).data('selectedThumbId'))
+
+    if (!currentThumb.hasClass('vtmb + variantId')) {
+      var thumb = $(($('#product-images ul.thumbnails li:visible.vtmb')).eq(0))
+
+      if (!(thumb.length > 0)) {
+        thumb = $(($('#product-images ul.thumbnails li:visible')).eq(0))
+      }
+
+      var newImg = thumb.find('a').attr('href')
+      var newAlt = thumb.find('img').attr('alt')
+
+      $('#product-images ul.thumbnails li').removeClass('selected')
+      thumb.addClass('selected');
+      ($('#main-image img')).attr({'src': newImg, 'alt': newAlt});
+      ($('#main-image')).data({'selectedThumb': newImg, 'selectedThumbAlt': newAlt})
+      return ($('#main-image')).data('selectedThumbId', thumb.attr('id'))
+    }
+  }
+
+  Spree.updateVariantPrice = function (variant) {
+    var variantPrice = variant.data('price')
+
+    if (variantPrice) {
+      return ($('.price.selling')).text(variantPrice)
+    }
+  }
+
+  Spree.disableCartForm = function (variant) {
+    var inStock = variant.data('in-stock')
+    return $('#add-to-cart-button').attr('disabled', !inStock)
+  }
+
+  var radios = $("#product-variants input[type='radio']")
+
+  if (radios.length > 0) {
+    var selectedRadio = $("#product-variants input[type='radio'][checked='checked']")
+    Spree.showVariantImages(selectedRadio.attr('value'))
+    Spree.updateVariantPrice(selectedRadio)
+    Spree.disableCartForm(selectedRadio)
+
+    radios.click(function (event) {
+      $('#product-variants').find('li.active').removeClass('active')
+      $(this).closest('li').addClass('active')
+      Spree.showVariantImages(this.value)
+      Spree.updateVariantPrice($(this))
+      return Spree.disableCartForm($(this))
+    })
+  }
+
+  return Spree.addImageHandlers()
+})
