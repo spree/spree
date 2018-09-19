5dbb21fed8066f38221b233fa63f40cdc8a1cf53
Michał Kamil Przybysz <michal.przybysz@sparksolutions.co>
2018-09-19 14:38:41 +0200

    Convert frontend CoffeeScript files to js files

---
+++ frontend/app/assets/javascripts/spree/frontend/cart.js
@@ -0,0 +1,38 @@
+//= require spree/frontend/coupon_manager
+
+Spree.ready(function ($) {
+  var formUpdateCart = $('form#update-cart');
+  if (formUpdateCart.length) {
+    $('form#update-cart a.delete').show().one('click', function () {
+      $(this).parents('.line-item').first().find('input.line_item_quantity').val(0);
+      $(this).parents('form').first().submit();
+      return false;
+    });
+  }
+  formUpdateCart.submit(function (event) {
+    var input = {
+      couponCodeField: $('#order_coupon_code'),
+      couponStatus: $('#coupon_status')
+    };
+    var updateButton = $('form#update-cart #update-button');
+    updateButton.attr('disabled', true);
+    if ($.trim(input.couponCodeField.val()).length > 0) {
+      if (new CouponManager(input).applyCoupon()) {
+        this.submit();
+        return true;
+      } else {
+        updateButton.attr('disabled', false);
+        event.preventDefault();
+        return false;
+      }
+    }
+  });
+});
+
+Spree.fetch_cart = function () {
+  return $.ajax({
+    url: Spree.pathFor('cart_link'),
+  }).done(function (data) {
+    return $('#link-to-cart').html(data);
+  });
+};
