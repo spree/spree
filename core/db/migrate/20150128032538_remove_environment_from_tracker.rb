376f67f685c740e9e93c43b28138023dca619aa6
Bartosz Bonisławski <b.bonislawski@hotmail.com>
2018-09-20 06:07:44 +0200

    Check if tracker model exists before migrating data on it

--- core/db/migrate/20150128032538_remove_environment_from_tracker.rb
+++ core/db/migrate/20150128032538_remove_environment_from_tracker.rb
@@ -1,4 +1,6 @@
 class RemoveEnvironmentFromTracker < ActiveRecord::Migration[4.2]
+  class Spree::Tracker < Spree::Base; end
+
   def up
     Spree::Tracker.where('environment != ?', Rails.env).update_all(active: false)
     remove_column :spree_trackers, :environment
