8d44b9d37fd04a0498cfdf986fa5540378d8addb
Alexey Topolyanskiy <devilcoders@protonmail.ch>
2018-10-08 17:22:24 +0200

    Fix few tests

--- backend/app/helpers/spree/admin/navigation_helper.rb
+++ backend/app/helpers/spree/admin/navigation_helper.rb
@@ -157,7 +157,7 @@ module Spree
       end
 
       def link_to_with_icon(icon_name, text, url, options = {})
-        options[:class] = (options[:class].to_s + " icon-link with-tip action-#{icon_name}").strip
+        options[:class] = (options[:class].to_s + " icon-link with-tip icon-#{icon_name} action-#{icon_name}").strip
         options[:title] = text if options[:no_text]
         text = options[:no_text] ? '' : content_tag(:span, text, class: 'text')
         options.delete(:no_text)
