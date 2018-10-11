e3b8df3b34d8613df4cebcafd5857207e0fbc00d
Yuriy Kushnir <kushnir.yb@gmail.com>
2018-10-11 12:43:26 +0200

    Fix failing feature specs

--- backend/spec/features/admin/orders/order_details_spec.rb
+++ backend/spec/features/admin/orders/order_details_spec.rb
@@ -211,16 +211,28 @@ describe 'Order Details', type: :feature, js: true do
       end
 
       context 'variant out of stock and not backorderable' do
+        let(:tote) { create(:product, name: 'Tote', price: 15.00) }
+
         before do
-          product.master.stock_items.first.update_column(:backorderable, false)
-          product.master.stock_items.first.update_column(:count_on_hand, 0)
+          tote.master.stock_items.first.update(backorderable: false)
+          tote.master.stock_items.first.update(count_on_hand: 0)
         end
 
-        it 'displays out of stock instead of add button' do
-          select2_search product.name, from: Spree.t(:name_or_sku)
+        it 'does not add a product to the order' do
+          select2_search tote.name, from: Spree.t(:name_or_sku)
 
           within('table.stock-levels') do
-            expect(page).to have_content(Spree.t(:out_of_stock))
+            fill_in 'variant_quantity', with: 1
+
+            accept_alert do
+              click_icon :add
+            end
+          end
+
+          wait_for_ajax
+
+          within('.line-items') do
+            expect(page).to_not have_content tote.name
           end
         end
       end
