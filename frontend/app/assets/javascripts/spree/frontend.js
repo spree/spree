8ca028c35af6b954881df75d799a59659e9b2d81
Damian Legawiec <damian@sparksolutions.co>
2018-10-19 13:42:15 +0200

    Added StoreController#api_tokens endpoint
    This will be used to fetch order token and user token to authorize API v2 requests fron the rails frontend

--- frontend/app/assets/javascripts/spree/frontend.js
+++ frontend/app/assets/javascripts/spree/frontend.js
@@ -1,10 +1,12 @@
 //= require bootstrap-sprockets
 //= require jquery.payment
 //= require spree
+//= require spree/frontend/api_tokens
 //= require spree/frontend/cart
 //= require spree/frontend/checkout
 //= require spree/frontend/checkout/address
 //= require spree/frontend/checkout/payment
 //= require spree/frontend/product
 
+Spree.routes.api_tokens = Spree.pathFor('api_tokens')
 Spree.routes.api_v2_storefront_cart_apply_coupon_code = Spree.pathFor('api/v2/storefront/cart/apply_coupon_code')
