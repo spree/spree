9edfa2b145b5a0c6cddcfcaae69e7807c665fdee
Damian Legawiec <damian@sparksolutions.co>
2018-11-06 19:27:39 +0100

    spec fix

--- core/spec/spec_helper.rb
+++ core/spec/spec_helper.rb
@@ -73,16 +73,6 @@ RSpec.configure do |config|
   # Clean out the database state before the tests run
   config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)
-    DatabaseCleaner.strategy = :transaction
-  end
-
-  # Wrap all db isolated tests in a transaction
-  config.around(db: :isolate) do |example|
-    DatabaseCleaner.cleaning(&example)
-  end
-
-  config.around do |example|
-    Timeout.timeout(30, &example)
   end
 
   config.order = :random
