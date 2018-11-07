56a85fbe589ec91f0caab6262dede84dcdff8772
Damian Legawiec <damian@sparksolutions.co>
2018-11-07 17:54:38 +0100

    MySQL migrations timestamp precision fix
    https://github.com/solidusio/solidus/pull/2598

--- core/db/migrate/20130301162924_create_shipping_method_categories.rb
+++ core/db/migrate/20130301162924_create_shipping_method_categories.rb
@@ -4,7 +4,7 @@ class CreateShippingMethodCategories < ActiveRecord::Migration[4.2]
       t.integer :shipping_method_id, null: false
       t.integer :shipping_category_id, null: false
 
-      t.timestamps null: false
+      t.timestamps null: false, precision: 6
     end
 
     add_index :spree_shipping_method_categories, :shipping_method_id
--- core/db/migrate/20140309024355_create_spree_stores.rb
+++ core/db/migrate/20140309024355_create_spree_stores.rb
@@ -18,7 +18,7 @@ class CreateSpreeStores < ActiveRecord::Migration[4.2]
         t.string :code
         t.boolean :default, default: false, null: false
 
-        t.timestamps null: false
+        t.timestamps null: false, precision: 6
       end
     end
   end
