f9bd38d7927886a037af4022aedbff184774f682
Damian Legawiec <damian@sparksolutions.co>
2018-11-09 14:54:21 +0100

    Use rails profile for Simplecov and exclude migrations and spec folders

--- core/spec/spec_helper.rb
+++ core/spec/spec_helper.rb
@@ -1,15 +1,19 @@
 if ENV['COVERAGE']
   # Run Coverage report
   require 'simplecov'
-  SimpleCov.start do
-    add_group 'Controllers', 'app/controllers'
-    add_group 'Helpers', 'app/helpers'
+  SimpleCov.start 'rails' do
+    add_group 'Finders', 'app/finders'
     add_group 'Mailers', 'app/mailers'
-    add_group 'Models', 'app/models'
-    add_group 'Views', 'app/views'
-    add_group 'Jobs', 'app/jobs'
+    add_group 'Paginators', 'app/paginators'
     add_group 'Services', 'app/services'
+    add_group 'Sorters', 'app/sorters'
+    add_group 'Validators', 'app/validators'
     add_group 'Libraries', 'lib'
+
+    add_filter '/bin/'
+    add_filter '/db/'
+    add_filter '/script/'
+    add_filter '/spec/'
 
     coverage_dir "#{ENV['COVERAGE_DIR']}/core" if ENV['COVERAGE_DIR']
   end
