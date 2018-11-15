960df84e066ae75f1ba78a3f6ca1b3a1930ff3f7
Damian Legawiec <damian@sparksolutions.co>
2018-11-15 15:29:29 +0100

    Fix rubocop issies with Core services

--- core/app/finders/spree/line_item/find_by_variant.rb
+++ core/app/finders/spree/line_item/find_by_variant.rb
@@ -4,6 +4,7 @@ module Spree
       def execute(order:, variant:, options: {})
         order.line_items.detect do |line_item|
           next unless line_item.variant_id == variant.id
+
           CompareLineItems.new.call(order: order, line_item: line_item, options: options).value
         end
       end
