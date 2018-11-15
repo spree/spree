a628ace4809a8bfd23123d96f561847ebd8c4fb6
Damian Legawiec <damian@sparksolutions.co>
2018-11-15 12:16:08 +0100

    Fix zone seeds to be able to re-run multiple times

--- core/db/default/spree/zones.rb
+++ core/db/default/spree/zones.rb
@@ -1,11 +1,10 @@
-eu_vat = Spree::Zone.create!(name: "EU_VAT", description: "Countries that make up the EU VAT zone.", kind: 'country')
-north_america = Spree::Zone.create!(name: "North America", description: "USA + Canada", kind: 'country')
+eu_vat = Spree::Zone.where(name: 'EU_VAT', description: 'Countries that make up the EU VAT zone.', kind: 'country').first_or_create!
+north_america = Spree::Zone.where(name: 'North America', description: 'USA + Canada', kind: 'country').first_or_create!
 
-%w(PL FI PT RO DE FR SK HU SI IE AT ES IT BE SE LV BG GB LT CY LU MT DK NL EE HR CZ GR).
-each do |name|
-  eu_vat.zone_members.create!(zoneable: Spree::Country.find_by!(iso: name))
+%w(PL FI PT RO DE FR SK HU SI IE AT ES IT BE SE LV BG GB LT CY LU MT DK NL EE HR CZ GR).each do |name|
+  eu_vat.zone_members.where(zoneable: Spree::Country.find_by!(iso: name)).first_or_create!
 end
 
 %w(US CA).each do |name|
-  north_america.zone_members.create!(zoneable: Spree::Country.find_by!(iso: name))
+  north_america.zone_members.where(zoneable: Spree::Country.find_by!(iso: name)).first_or_create!
 end
