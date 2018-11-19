50edb5aa7c310551636d175013a1b85b7972f7d4
Damian Legawiec <damian@sparksolutions.co>
2018-11-19 10:11:25 +0100

    Added API v2 Storefront Cart Remove Coupon Code endpoint

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -138,6 +138,7 @@ Spree::Core::Engine.add_routes do
           delete 'remove_line_item/:line_item_id', to: 'cart#remove_line_item', as: :cart_remove_line_item
           patch  :set_quantity
           patch  :apply_coupon_code
+          delete 'remove_coupon_code/:coupon_code', to: 'cart#remove_coupon_code', as: :cart_remove_coupon_code
         end
 
         resource :checkout, controller: :checkout, only: %i[update] do
