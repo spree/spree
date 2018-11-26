f58910ead6b25cfcc50b19a40424e9e6ccb3f84f
Damian Legawiec <damian@sparksolutions.co>
2018-11-26 12:18:12 +0100

    Added tests for fetching JSON API nested resources

--- api/spec/requests/spree/api/v2/resource_includes_spec.rb
+++ api/spec/requests/spree/api/v2/resource_includes_spec.rb
@@ -5,6 +5,7 @@ describe 'API v2 JSON API Resource Includes Spec', type: :request do
   let!(:products) { create_list(:product, 5) }
   let!(:product) { create(:product) }
   let!(:product_option_type) { create(:product_option_type, product: product) }
+  let!(:option_value) { create(:option_value, option_type: product_option_type.option_type) }
   let(:default_variant) { product.master }
 
   shared_examples 'default resources' do
@@ -20,6 +21,15 @@ describe 'API v2 JSON API Resource Includes Spec', type: :request do
       expect(json_response['included']).to be_present
       expect(json_response['included']).to include(have_type('variant').and have_id(default_variant.id.to_s))
       expect(json_response['included']).not_to include(have_type('option_type'))
+    end
+  end
+
+  shared_examples 'nested requested resources' do
+    it 'are returned' do
+      expect(json_response['included']).to be_present
+      expect(json_response['included']).not_to include(have_type('variant').and have_id(default_variant.id.to_s))
+      expect(json_response['included']).to include(have_type('option_type'))
+      expect(json_response['included']).to include(have_type('option_value'))
     end
   end
 
@@ -44,9 +54,17 @@ describe 'API v2 JSON API Resource Includes Spec', type: :request do
       end
 
       context 'present param' do
-        before { get "/api/v2/storefront/products/#{product.id}?include=default_variant" }
+        context 'without nested resources' do
+          before { get "/api/v2/storefront/products/#{product.id}?include=default_variant" }
 
-        it_behaves_like 'requested resources'
+          it_behaves_like 'requested resources'
+        end
+
+        context 'with nested resources' do
+          before { get "/api/v2/storefront/products/#{product.id}?include=option_types,option_types.option_values" }
+
+          it_behaves_like 'nested requested resources'
+        end
       end
     end
   end
@@ -66,9 +84,17 @@ describe 'API v2 JSON API Resource Includes Spec', type: :request do
       end
 
       context 'present param' do
-        before { get '/api/v2/storefront/products?include=default_variant' }
+        context 'without nested resources' do
+          before { get '/api/v2/storefront/products?include=default_variant' }
 
-        it_behaves_like 'requested resources'
+          it_behaves_like 'requested resources'
+        end
+
+        context 'with nested resources' do
+          before { get '/api/v2/storefront/products?include=option_types,option_types.option_values' }
+
+          it_behaves_like 'nested requested resources'
+        end
       end
     end
   end
