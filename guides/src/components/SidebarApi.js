cfe19662bd8828721db1df9c92e9c3f2e4c862ed
Alexey Topolyanskiy <devilcoders@protonmail.ch>
2018-12-17 18:59:00 +0100

    Add more links to sidebar api

--- guides/src/components/SidebarApi.js
+++ guides/src/components/SidebarApi.js
@@ -1,6 +1,6 @@
 import * as React from 'react'
 
-import Sidebar from '../../../components/Sidebar'
+import Sidebar from './Sidebar'
 
 const sidebarData = [
   {
@@ -16,6 +16,45 @@ const sidebarData = [
           },
           frontmatter: {
             title: 'Authentication'
+          }
+        }
+      },
+      {
+        node: {
+          fields: {
+            isIndex: false,
+            rootSection: 'API v2',
+            section: 'API v2',
+            slug: 'api/v2/extend-the-api'
+          },
+          frontmatter: {
+            title: 'Extend The API'
+          }
+        }
+      },
+      {
+        node: {
+          fields: {
+            isIndex: false,
+            rootSection: 'API v2',
+            section: 'API v2',
+            slug: 'api/v2/storefront'
+          },
+          frontmatter: {
+            title: 'Storefront'
+          }
+        }
+      },
+      {
+        node: {
+          fields: {
+            isIndex: true,
+            rootSection: 'API v2',
+            section: 'API v2',
+            slug: 'api/v2/'
+          },
+          frontmatter: {
+            title: 'Overview'
           }
         }
       }
--- guides/src/pages/api/v2/index.js
+++ guides/src/pages/api/v2/index.js
@@ -1,32 +1,11 @@
 import * as React from 'react'
 
 import Layout from '../../../components/Layout'
-import Sidebar from '../../../components/Sidebar'
-
-const sidebarData = [
-  {
-    section: 'API v2',
-    edges: [
-      {
-        node: {
-          fields: {
-            isIndex: false,
-            rootSection: 'API v2',
-            section: 'API v2',
-            slug: 'api/v2/authentication'
-          },
-          frontmatter: {
-            title: 'Authentication'
-          }
-        }
-      }
-    ]
-  }
-]
+import SidebarApi from '../../../components/SidebarApi'
 
 const IndexPage = () => (
   <Layout activeRootSection="api/v2">
-    <Sidebar nav={sidebarData} />
+    <SidebarApi />
   </Layout>
 )
 
