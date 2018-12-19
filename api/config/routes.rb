ca78543ad40adb1edb433f7095ddf77f24fc8b74
Damian Legawiec <damian@sparksolutions.co>
2018-12-19 09:02:39 +0100

    Removed SwaggerController
    OpenAPI documentation is part of the new Guides 2.0, we don't need this.

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -129,8 +129,6 @@ Spree::Core::Engine.add_routes do
     end
 
     namespace :v2 do
-      get 'storefront.yml', to: 'swagger#storefront', as: 'swagger_storefront', format: 'yml' if Rails.env.development? || ENV['EXPOSE_SWAGGER']
-
       namespace :storefront do
         resource :cart, controller: :cart, only: %i[show create] do
           post   :add_item
