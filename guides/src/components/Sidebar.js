72174992dd9f78d995d28df6a966ad339f0e195d
Alexey Topolyanskiy <devilcoders@protonmail.ch>
2018-12-19 15:23:03 +0100

    More functional and simpler Sidebar  component

--- guides/src/components/Sidebar.js
+++ guides/src/components/Sidebar.js
@@ -1,24 +1,23 @@
 import * as React from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'gatsby'
-import {
-  join,
-  juxt,
-  compose,
-  toUpper,
-  head,
-  tail,
-  equals,
-  or,
-  length,
-  filter
-} from 'ramda'
+import { equals, or, length, filter } from 'ramda'
 import startCase from 'lodash.startcase'
 import { cx } from 'emotion'
 
 import style from '../utils/styles'
+import capitalize from '../utils/capitalize'
 
 import SidebarRootLink from './SidebarRootLink'
+
+const byOnlyNonIndexNodes = item => !item.node.fields.isIndex
+const byOnlyIndexNodes = item => item.node.fields.isIndex
+
+const navBlockIndex = block => filter(byOnlyIndexNodes, block)
+const normalizeNavBlock = block => filter(byOnlyNonIndexNodes, block)
+
+const getNavBlockIndexSlug = block =>
+  navBlockIndex(block)[0]['node']['fields']['slug']
 
 export default class Sidebar extends React.Component {
   static propTypes = {
@@ -31,17 +30,6 @@ export default class Sidebar extends React.Component {
     openSection: null
   }
 
-  capitalizeSectionTitle = compose(
-    join(''),
-    juxt([
-      compose(
-        toUpper,
-        head
-      ),
-      tail
-    ])
-  )
-
   sectionIsOpen = section =>
     or(
       equals(section, this.state.openSection),
@@ -50,36 +38,9 @@ export default class Sidebar extends React.Component {
 
   _toggleSection = section => {
     this.setState(prevState => {
-      if (prevState.openSection !== section) {
-        return { openSection: section }
-      } else {
-        return { openSection: null }
-      }
+      return { openSection: prevState.openSection !== section ? section : null }
     })
   }
-
-  normalizeNavBlock = block => {
-    const nav = []
-
-    block.map(item => {
-      if (!item.node.fields.isIndex) {
-        nav.push({
-          title: item.node.frontmatter.title,
-          url: item.node.fields.slug
-        })
-      }
-    })
-
-    return nav
-  }
-
-  navBlockIndex = block => {
-    const hasIndex = item => item.node.fields.isIndex === true
-    return filter(hasIndex, block)
-  }
-
-  getNavBlockIndexSlug = block =>
-    this.navBlockIndex(block)[0]['node']['fields']['slug']
 
   render() {
     const { isMobile } = this.props
@@ -100,21 +61,19 @@ export default class Sidebar extends React.Component {
               <ul className="list ma0 pl0">
                 {this.props.nav.map((item, index) => (
                   <React.Fragment key={index}>
-                    {length(this.normalizeNavBlock(item.edges)) > 0 && (
+                    {length(normalizeNavBlock(item.edges)) > 0 && (
                       <li key={index}>
                         <SidebarRootLink
                           isSingleRoot={length(this.props.nav) < 2}
-                          title={this.capitalizeSectionTitle(
-                            startCase(item.section)
-                          )}
+                          title={capitalize(startCase(item.section))}
                           isOpen={this.sectionIsOpen(item.section)}
                           toggleSection={() =>
                             this._toggleSection(item.section)
                           }
-                          itemsLength={length(this.navBlockIndex(item.edges))}
+                          itemsLength={length(navBlockIndex(item.edges))}
                           href={
-                            length(this.navBlockIndex(item.edges)) > 0
-                              ? this.getNavBlockIndexSlug(item.edges)
+                            length(navBlockIndex(item.edges)) > 0
+                              ? getNavBlockIndexSlug(item.edges)
                               : false
                           }
                         />
@@ -134,19 +93,17 @@ export default class Sidebar extends React.Component {
                             'list pl2 ml4 mb4'
                           )}
                         >
-                          {this.normalizeNavBlock(item.edges).map(
-                            (item, index) => (
-                              <li key={index}>
-                                <Link
-                                  to={item.url}
-                                  activeClassName="spree-green fw5"
-                                  className="link gray db mv2 fw4"
-                                >
-                                  {item.title}
-                                </Link>
-                              </li>
-                            )
-                          )}
+                          {normalizeNavBlock(item.edges).map((item, index) => (
+                            <li key={index}>
+                              <Link
+                                to={item.node.fields.slug}
+                                activeClassName="spree-green fw5"
+                                className="link gray db mv2 fw4"
+                              >
+                                {item.node.frontmatter.title}
+                              </Link>
+                            </li>
+                          ))}
                         </ul>
                       </li>
                     )}
