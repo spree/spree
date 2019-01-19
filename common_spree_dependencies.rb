61185801902e06443321863561c06b01d141325a
Damian Legawiec <damian@sparksolutions.co>
2019-01-19 19:19:06 +0100

    Meet Core requirements for Rails 6.0

--- common_spree_dependencies.rb
+++ common_spree_dependencies.rb
@@ -7,6 +7,10 @@ gem 'sass-rails'
 gem 'sqlite3', '~> 1.4.0', platforms: [:ruby, :mingw, :mswin, :x64_mingw]
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw]
 
+# temp Rails 6 gems
+gem 'acts-as-taggable-on', '~> 6.0', github: 'mbleigh/acts-as-taggable-on', branch: 'master'
+gem 'awesome_nested_set', '~> 3.1.4', github: 'collectiveidea/awesome_nested_set', branch: 'master'
+
 platforms :jruby do
   gem 'jruby-openssl'
   gem 'activerecord-jdbcsqlite3-adapter'
