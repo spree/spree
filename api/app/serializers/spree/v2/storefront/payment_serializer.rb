c55566463f4556eec5c6a17b5fa8d95a1896952d
Przemek Kosakowski <p.kosakowski@gmail.com>
2019-01-21 12:46:42 +0100

    Add test for include=payments.source case

--- api/app/serializers/spree/v2/storefront/payment_serializer.rb
+++ api/app/serializers/spree/v2/storefront/payment_serializer.rb
@@ -5,6 +5,7 @@ module Spree
         set_type :payment
 
         has_one :source, polymorphic: true
+        has_one :payment_method
 
         attributes :amount, :response_code, :number, :cvv_response_code, :cvv_response_message,
                    :payment_method_id, :payment_method_name
