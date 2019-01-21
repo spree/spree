1efbbbbcc3650fef8ef35860a914d83afac56d67
Przemek Kosakowski <p.kosakowski@gmail.com>
2019-01-21 10:19:31 +0100

    Add serializers: StoreCreditEvent, StoreCredit, StoreCreditType

--- api/app/serializers/spree/v2/storefront/payment_serializer.rb
+++ api/app/serializers/spree/v2/storefront/payment_serializer.rb
@@ -4,6 +4,8 @@ module Spree
       class PaymentSerializer < BaseSerializer
         set_type :payment
 
+        has_one :source, polymorphic: true
+
         attributes :amount, :response_code, :number, :cvv_response_code, :cvv_response_message,
                    :payment_method_id, :payment_method_name
       end
