b73a7a7e97ec24d3c800f59211bc68b0b37b609a
Przemek Kosakowski <p.kosakowski@gmail.com>
2019-01-22 13:33:40 +0100

    Updates Storefront v2 Taxons endpoint to use Dependencies system

--- api/app/models/spree/api_dependencies.rb
+++ api/app/models/spree/api_dependencies.rb
@@ -29,6 +29,7 @@ module Spree
       @storefront_country_serializer = Spree::V2::Storefront::CountrySerializer
       @storefront_user_serializer = Spree::V2::Storefront::UserSerializer
       @storefront_shipment_serializer = Spree::V2::Storefront::ShipmentSerializer
+      @storefront_taxon_serializer = Spree::V2::Storefront::TaxonSerializer
       @storedront_payment_method_serializer = Spree::V2::Storefront::PaymentMethodSerializer
       @storefront_product_serializer = Spree::V2::Storefront::ProductSerializer
 
@@ -45,6 +46,7 @@ module Spree
       @storefront_completed_order_finder = Spree::Dependencies.completed_order_finder
       @storefront_credit_card_finder = Spree::Dependencies.credit_card_finder
       @storefront_products_finder = Spree::Dependencies.products_finder
+      @storefront_taxon_finder = Spree::Dependencies.taxon_finder
     end
 
     attr_accessor :storefront_cart_create_service, :storefront_cart_add_item_service,
@@ -55,6 +57,6 @@ module Spree
                   :storefront_cart_serializer, :storefront_credit_card_serializer, :storefront_credit_card_finder, :storefront_shipment_serializer, :storedront_payment_method_serializer,
                   :storefront_country_finder, :storefront_country_serializer, :storefront_current_order_finder, :storefront_completed_order_finder,
                   :storefront_order_sorter, :storefront_collection_paginator, :storefront_user_serializer, :storefront_products_sorter,
-                  :storefront_products_finder, :storefront_product_serializer
+                  :storefront_products_finder, :storefront_product_serializer, :storefront_taxon_serializer, :storefront_taxon_finder
   end
 end
