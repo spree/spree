718ab467772c09036712787c92a08e87039f6d7b
Damian Legawiec <damian@sparksolutions.co>
2019-01-22 18:23:18 +0100

    Move another part of the app to dependency system

--- frontend/app/controllers/spree/checkout_controller.rb
+++ frontend/app/controllers/spree/checkout_controller.rb
@@ -162,7 +162,7 @@ module Spree
 
     def add_store_credit_payments
       if params.key?(:apply_store_credit)
-        Spree::Checkout::AddStoreCredit.call(order: @order)
+        add_store_credit_service.call(order: @order)
 
         # Remove other payment method parameters.
         params[:order].delete(:payments_attributes)
@@ -176,7 +176,7 @@ module Spree
 
     def remove_store_credit_payments
       if params.key?(:remove_store_credit)
-        Spree::Checkout::RemoveStoreCredit.call(order: @order)
+        remove_store_credit_service.call(order: @order)
         redirect_to checkout_state_path(@order.state) and return
       end
     end
@@ -193,6 +193,14 @@ module Spree
 
     def set_cache_header
       response.headers['Cache-Control'] = 'no-store'
+    end
+
+    def add_store_credit_service
+      Spree::Dependencies.add_store_credit_service.constantize
+    end
+
+    def remove_store_credit_service
+      Spree::Dependencies.remove_store_credit_service.constantize
     end
   end
 end
