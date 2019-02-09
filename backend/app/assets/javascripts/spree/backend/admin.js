ce8f0e0140849b14b56ae89e865208119369e1e8
Damian Legawiec <damian@sparksolutions.co>
2019-02-09 22:49:51 +0100

    Fixed Admin Panel active filters

--- backend/app/assets/javascripts/spree/backend/admin.js
+++ backend/app/assets/javascripts/spree/backend/admin.js
@@ -149,7 +149,7 @@ jQuery(function ($) {
 
       label = ransackField(label.text()) + ': ' + ransackValue
 
-      filter = '<span class="js-filter badge badge-secondary" data-ransack-field="' + ransack_field + '">' + label + '<span class="icon icon-delete js-delete-filter"></span></span>'
+      filter = '<span class="js-filter badge badge-secondary" data-ransack-field="' + ransackFieldId + '">' + label + '<span class="icon icon-delete js-delete-filter"></span></span>'
       $(".js-filters").append(filter).show()
     }
   })
