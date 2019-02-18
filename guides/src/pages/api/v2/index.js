ec2e4358e82755f042665662e5918506fd61bae7
Damian Legawiec <damian@sparksolutions.co>
2019-02-18 21:47:21 +0100

    Updated API v2 guides section

--- guides/src/pages/api/v2/index.js
+++ guides/src/pages/api/v2/index.js
@@ -1,13 +1,18 @@
 import * as React from 'react'
+import { Link } from 'gatsby'
 
 import Layout from '../../../components/Layout'
 import openApiNav from '../../../utils/openApiNav'
 import ExternalLink from '../../../components/ExternalLink'
+import H1 from '../../../components/base/H1'
+import H2 from '../../../components/base/H2'
+import H3 from '../../../components/base/H3'
+import P from '../../../components/base/P'
 
 const IndexPage = () => (
   <Layout activeSection="API V2" nav={openApiNav}>
-    <h1>REST API v2</h1>
-    <p>
+    <H1>REST API v2</H1>
+    <P>
       Modern lightweight REST API based on &nbsp;
       <ExternalLink url="https://jsonapi.org/format/">
         JSON API schema
@@ -21,38 +26,57 @@ const IndexPage = () => (
         doorkeeper
       </ExternalLink>
       &nbsp;gem.
-    </p>
+    </P>
+    <H2>REST API v2 components</H2>
+    <H3>
+      <Link to="/api/v2/storefront">Storefront</Link>
+    </H3>
+    <P>
+      All necessary API endpoints to build a custom Storefront in any technology (JavaScript/Mobile).
+      <br />
+      Resources such as Products and Taxons are made publicly accessible by default.
+      <br />
+      Simplified <Link to="/api/v2/storefront#section/Authentication">authentication</Link>
+      &nbsp;to support both signed in users and guest checkouts.
+    </P>
+    <H3>
+      Platform <em>(Work in Progress)</em>
+    </H3>
+    <P>
+      API endpoints for any admin level privileges actions. Designed to connect 3rd party systems like WMS, retail Points of Sale, etc.
+      <br />
+      We plan on including Platform API in one of the Spree 4.x releases.
+    </P>
 
-    <strong>REST API v2</strong> consists of 2 parts:
+    <H2>JSON API features supported</H2>
+    All REST API v2 endpoints support these JSON API features:
     <ul>
       <li>
-        <a href="/api/v2/storefront">Storefront</a>
-        <br />
-        All necessary API endpoints to build a custom Storefront in
-        any technology (JavaScript/Mobile)
-      </li>
-      <li>
-        Platform (Work in Progress)
-        <br />
-        API endpoints for any admin level privileges actions.
-        Designed for connecting to 3rd party systems like WMS, retail Points of Sale, etc
-      </li>
-    </ul>
-
-    REST API v2 supports those JSON API features:
-    <ul>
-      <li>
-        Returning&nbsp;
         <ExternalLink url="https://jsonapi.org/format/#document-resource-object-relationships">
           Relationships
         </ExternalLink>
-        &nbsp;of objects in the response JSON
+        <br />
+        Returns a set of related objects IDs the same response JSON
       </li>
       <li>
-        Declaring and fetching Related Resources
+        <ExternalLink url="https://jsonapi.org/format/#fetching-includes">
+          Fetching Related Resources
+        </ExternalLink>
+        <br />
+        In one API request you can fetch multiple resources related to the main
+        object, eg. Product with Variants and Images of those Variants.
+        Thanks to this you don't need to make multiple requests to the API greatly
+        simplyfying your frontend code and also creating a smoother experience
+        for your users.
       </li>
       <li>
-        Sparse Fieldsets to fetch only selected attributes of objects and their relationships
+        <ExternalLink url="https://jsonapi.org/format/#fetching-sparse-fieldsets">
+          Sparse Fieldsets
+        </ExternalLink>
+        <br />
+        Fetch only selected attributes of objects and their relationships.
+        Thanks to this JSON response will be lean and exactly how you require it
+        without any overload and overfetching.
       </li>
     </ul>
   </Layout>
