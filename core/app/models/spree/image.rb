878b40fba96486d37357e0cc22a34b4c30f789fa
jkojro <janusz.kojro@sparksoluitons.co>
2019-03-21 15:13:33 +0100

    bug/remove image.inheritance_column = nil

--- core/app/models/spree/image.rb
+++ core/app/models/spree/image.rb
@@ -2,13 +2,6 @@ module Spree
   class Image < Asset
     include Configuration::ActiveStorage
     include Rails.application.routes.url_helpers
-  
-    # In Rails 5.x class constants are being undefined/redefined during the code reloading process
-    # in a rails development environment, after which the actual ruby objects stored in those class constants
-    # are no longer equal (subclass == self) what causes error ActiveRecord::SubclassNotFound
-    # Invalid single-table inheritance type: Spree::Image is not a subclass of Spree::Image.
-    # The line below prevents the error.
-    self.inheritance_column = nil
 
     def styles
       self.class.styles.map do |_, size|
