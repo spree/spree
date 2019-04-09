5ee6dd556da27d4c3f76c355682be15102f0133b
jkojro <janusz.kojro@sparksoluitons.co>
2019-04-09 11:59:55 +0200

    Country factory with unique ISO

--- core/lib/spree/testing_support/factories/country_factory.rb
+++ core/lib/spree/testing_support/factories/country_factory.rb
@@ -2,8 +2,8 @@ FactoryBot.define do
   factory :country, class: Spree::Country do
     sequence(:iso_name) { |n| "ISO_NAME_#{n}" }
     sequence(:name)     { |n| "NAME_#{n}" }
-    iso                 { 'US' }
-    iso3                { 'USA' }
+    sequence(:iso)      { |n| "I#{n}" }
+    sequence(:iso3)     { |n| "IS#{n}" }
     numcode             { 840 }
   end
 end
