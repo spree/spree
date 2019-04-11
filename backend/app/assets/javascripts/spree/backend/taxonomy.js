7dd140321c9ca81660351c863f5fbb25b8d1ba7c
PSzyszkaSpark <pawel.szyszka@sparksolutions.co>
2019-04-11 09:51:03 +0200

    Updates taxons positions in taxons_controller.rb and taxonomy.js

--- backend/app/assets/javascripts/spree/backend/taxonomy.js
+++ backend/app/assets/javascripts/spree/backend/taxonomy.js
@@ -20,6 +20,7 @@ function handleMove (e, data) {
       _method: 'put',
       'taxon[parent_id]': newParent.prop('id'),
       'taxon[child_index]': position,
+      'taxon[position]': position,
       token: Spree.api_key
     }
   }).fail(function () {
@@ -42,6 +43,7 @@ function handleCreate (e, data) {
       'taxon[name]': name,
       'taxon[parent_id]': newParent.prop('id'),
       'taxon[child_index]': position,
+      'taxon[position]': position,
       token: Spree.api_key
     }
   }).done(function (data) {
