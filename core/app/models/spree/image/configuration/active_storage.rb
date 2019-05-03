eaab54d396fab384acc4d40fe5a4bc608ae26fa0
Damian Legawiec <damian@sparksolutions.co>
2019-05-03 13:52:44 +0200

    ActiveStorage fix: don't try to purge attachments  that aren't saved

--- core/app/models/spree/image/configuration/active_storage.rb
+++ core/app/models/spree/image/configuration/active_storage.rb
@@ -29,14 +29,12 @@ module Spree
 
           def check_attachment_presence
             unless attachment.attached?
-              attachment.purge
               errors.add(:attachment, :attachment_must_be_present)
             end
           end
 
           def check_attachment_content_type
             if attachment.attached? && !attachment.content_type.in?(accepted_image_types)
-              attachment.purge
               errors.add(:attachment, :not_allowed_content_type)
             end
           end
