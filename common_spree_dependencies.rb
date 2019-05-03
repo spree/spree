026bf4160f2269a23dd1207059a99858b6abd9bd
Damian Legawiec <damian@sparksolutions.co>
2019-05-03 11:55:38 +0200

    Use Rails 6 fork of acts as taggable on

--- common_spree_dependencies.rb
+++ common_spree_dependencies.rb
@@ -8,7 +8,7 @@ gem 'sqlite3', '~> 1.4.0', platforms: [:ruby, :mingw, :mswin, :x64_mingw]
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw]
 
 # temp Rails 6 gems
-gem 'acts-as-taggable-on', '~> 6.0', github: 'mbleigh/acts-as-taggable-on', branch: 'master'
+gem 'acts-as-taggable-on', '~> 6.0', github: 'spark-solutions/acts-as-taggable-on', branch: 'fix/rails-6-and-failing-specs'
 gem 'awesome_nested_set', '~> 3.1.4', github: 'collectiveidea/awesome_nested_set', branch: 'master'
 
 platforms :jruby do
