92ebae00686dc5692d84c35389906024ced62e28
Thomas Walpole <twalpole@gmail.com>
2019-06-18 16:31:10 -0700

    Remove unnecessary have_title matcher - Capybara already provides it

--- core/lib/spree/testing_support/capybara_ext.rb
+++ core/lib/spree/testing_support/capybara_ext.rb
@@ -142,21 +142,6 @@ RSpec::Matchers.define :have_meta do |name, expected|
   end
 end
 
-RSpec::Matchers.define :have_title do |expected|
-  match do |_actual|
-    has_css?('title', text: expected, visible: false)
-  end
-
-  failure_message do |actual|
-    actual = first('title')
-    if actual
-      "expected that title would have been '#{expected}' but was '#{actual.text}'"
-    else
-      "expected that title would exist with '#{expected}'"
-    end
-  end
-end
-
 RSpec.configure do |c|
   c.include CapybaraExt
 end
