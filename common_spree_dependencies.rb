365beb4cc6dbb270c16abd3bd72bfe2aaf5be769
Damian Legawiec <damian@sparksolutions.co>
2019-08-19 13:36:09 +0200

    Use master branch of state_machines-activemodel for Rails 6 support

--- common_spree_dependencies.rb
+++ common_spree_dependencies.rb
@@ -10,6 +10,7 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw]
 # temp Rails 6 gems
 gem 'acts-as-taggable-on', '~> 6.0', github: 'spark-solutions/acts-as-taggable-on', branch: 'fix/rails-6-and-failing-specs'
 gem 'awesome_nested_set', '~> 3.1.4', github: 'collectiveidea/awesome_nested_set', branch: 'master'
+gem 'state_machines-activemodel', github: 'state-machines/state_machines-activemodel', branch: 'master'
 
 platforms :jruby do
   gem 'jruby-openssl'
