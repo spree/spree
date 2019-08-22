c3a8bf0672738431be28ea14dc40e73636006675
Matthew Kennedy <m.kennedy@me.com>
2019-08-22 22:56:44 +0100

    commit

--- core/app/models/spree/calculator/flat_rate_item_total_difrent.rb
+++ core/app/models/spree/calculator/flat_rate_item_total_difrent.rb
@@ -10,14 +10,20 @@ module Spree
     end
 
     def compute(object = nil)
-
       order = object.order
-
-      # 1
-      # Is it possible to get applicable line_items total amount, rather than order.amount
+      # Is it possible to get the total amount from all promotion applicable line items 
+      flat_percent = (preferred_amount * 100) / (order.amount)
 
       if object && preferred_currency.casecmp(object.currency.upcase).zero?
-        (preferred_amount / order.amount) * object.amount
+        computed_amount = (object.amount * flat_percent / 100)
+
+        # We don't want to cause the promotion adjustments to push the order into a negative total.
+        if computed_amount > object.amount
+          object.amount
+        else
+          computed_amount
+        end
+
       else
         0
       end
