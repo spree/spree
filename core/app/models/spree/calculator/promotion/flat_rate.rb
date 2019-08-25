7fac401b287796c29d25623ed4f7e59a73701b2a
Matthew Kennedy <m.kennedy@me.com>
2019-08-25 10:30:48 +0100

    Change commenting

--- core/app/models/spree/calculator/promotion/flat_rate.rb
+++ core/app/models/spree/calculator/promotion/flat_rate.rb
@@ -10,17 +10,14 @@ module Spree
         Spree.t(:flat_rate)
       end
 
-      # In this process we occasionnally get a rounding error of 1 penny.
-      # Is there a way for the last line item to get calculated using
-      # what ever is remaining from the preffered_amount and therfor the discount would always amount to the preffered amout.
-
-      # Or maybe im over thinking this and there is a simpler solution to the rounding error.
-
       def compute(object = nil, line_items_total = 0)
         if object && preferred_currency.casecmp(object.currency.upcase).zero?
           if line_items_total == 0
             preferred_amount
           else
+            # In this process we occasionnally get a rounding error of 1 penny.
+            # Is there a way for the last line item to get calculated using
+            # what ever is remaining from the preffered_amount and therfor the discount would always amount to the preffered amout.
             (preferred_amount / line_items_total) * object.amount
           end
         else
