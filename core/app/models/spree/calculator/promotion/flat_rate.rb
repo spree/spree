0bcda1611e05a8f63e9b420873d1bbc85f84a86f
Matthew Kennedy <m.kennedy@me.com>
2019-08-26 23:48:06 +0100

    Test

--- core/app/models/spree/calculator/promotion/flat_rate.rb
+++ core/app/models/spree/calculator/promotion/flat_rate.rb
@@ -17,11 +17,7 @@ module Spree
             preferred_amount
           else
             # If this is a Line Item type of Adjustment do the following to each line item.
-            unless object.id == last_actionable_line_item_id || !last_actionable_line_item_id == nil
-              # For most of the line items do the process below, and also if it is the only apllicable line item in the order.
-              percentage_of_applicable = (object.amount / actionable_line_items_total)
-              (preferred_amount * percentage_of_applicable).round(2)
-            else
+            if object.id == last_actionable_line_item_id && !last_actionable_line_item_id.nil?
               # If there is more than one item in the order have the last applicable line item
               # eat the remainder of the discount, to pick up any rounding errors from the previous items calculations.
               # This ensures the adjustment total always matched the specified preffered_amount.
@@ -31,6 +27,10 @@ module Spree
                 discounts_used << (preferred_amount * percentage_of_applicable).round(2)
               end
               preferred_amount - discounts_used.inject(:+)
+            else
+              # For most of the line items do the process below, and also if it is the only apllicable line item in the order.
+              percentage_of_applicable = (object.amount / actionable_line_items_total)
+              (preferred_amount * percentage_of_applicable).round(2)
             end
           end
         else
