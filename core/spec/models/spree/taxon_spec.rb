c0b55046420a0f8a6b842b5468011e11de1e82ed
PSzyszkaSpark <pawel.szyszka@sparksolutions.co>
2019-09-11 10:32:20 +0200

    Removes deprecation warning in taxon_spec.rb

--- core/spec/models/spree/taxon_spec.rb
+++ core/spec/models/spree/taxon_spec.rb
@@ -86,7 +86,15 @@ describe Spree::Taxon, type: :model do
     let!(:taxonomy) { create(:taxonomy) }
 
     it 'does not error out' do
-      expect { taxonomy.root.children.unscoped.where(name: 'Some name', parent_id: taxonomy.taxons.first.id).first_or_create }.not_to raise_error
+      expect { select_or_create_taxonomy }.not_to raise_error
+    end
+
+    def select_or_create_taxonomy
+      if taxonomy.root.children.unscoped.where(name: 'Some name', parent_id: taxonomy.taxons.first.id).first.nil?
+        Spree::Taxon.create!(name: "Some name", parent_id: taxonomy.taxons.first.id)
+      else
+        taxonomy.root.children.unscoped.where(name: 'Some name', parent_id: taxonomy.taxons.first.id).first
+      end
     end
   end
 
