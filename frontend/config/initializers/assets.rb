5b8cbcd3b8a5a42e4a6103436cd5f47e91790368
Damian Legawiec <damian@sparksolutions.co>
2019-09-12 18:04:39 +0200

    Revert "Add glyphicons to asset precompile"
    This reverts commit e769872c124473fb55910d01bff4ae212a5a9d97.

--- frontend/config/initializers/assets.rb
+++ frontend/config/initializers/assets.rb
@@ -1,4 +1,1 @@
 Rails.application.config.assets.precompile += %w(favicon.ico credit_cards/* spree/frontend/checkout/shipment)
-# fix issues with glyphicons
-Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
-Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
