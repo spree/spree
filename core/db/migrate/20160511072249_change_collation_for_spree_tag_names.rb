cc290c5b6f1e4be4234b48690f9d92844b48c05b
Damian Legawiec <damian@sparksolutions.co>
2019-09-23 11:38:06 +0200

    Fixes #9527 acts as taggable on migration fails when theres no acts as taggable on gem installed
    Tag support was removed in Spree 4.0, with this fix it will be easier to
    migrate Spree 3.7 applications to 4.0

--- core/db/migrate/20160511072249_change_collation_for_spree_tag_names.rb
+++ core/db/migrate/20160511072249_change_collation_for_spree_tag_names.rb
@@ -2,7 +2,7 @@
 # work properly
 class ChangeCollationForSpreeTagNames < ActiveRecord::Migration[4.2]
   def up
-    if ActsAsTaggableOn::Utils.using_mysql?
+    if defined?(ActsAsTaggableOn) && ActsAsTaggableOn::Utils.using_mysql?
       execute("ALTER TABLE spree_tags MODIFY name varchar(255) CHARACTER SET utf8 COLLATE utf8_bin;")
     end
   end
