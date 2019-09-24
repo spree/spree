02a9bd79bc31ce387fa240134a5f4db8a36c3b45
mbajur <mbajur@gmail.com>
2019-09-24 21:53:07 +0200

    Add feature specs for JSON-LD hashes

---
+++ frontend/spec/features/json_ld_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+describe 'JSON-LD hashes', type: :feature, inaccessible: true do
+  include_context 'custom products'
+
+  shared_examples 'it contains JSON-LD hash' do
+    it 'contains JSON-LD hash' do
+      expect(page).to have_selector('script[type="application/ld+json"]', visible: false)
+    end
+  end
+
+  before { visit spree.root_path }
+
+  context 'home page' do
+    it_behaves_like 'it contains JSON-LD hash'
+  end
+
+  context 'products page' do
+    before { visit spree.products_path }
+
+    it_behaves_like 'it contains JSON-LD hash'
+  end
+
+  context 'product page' do
+    before { click_link 'Ruby on Rails Baseball Jersey' }
+
+    it_behaves_like 'it contains JSON-LD hash'
+  end
+
+  context 'taxon page' do
+    before { click_link 'Bags' }
+
+    it_behaves_like 'it contains JSON-LD hash'
+  end
+end
