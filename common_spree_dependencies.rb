c73cadb1e63f82e4978a2efa2806ff3eb42ae3f4
Damian Legawiec <damian@sparksolutions.co>
2019-09-27 16:31:49 +0200

    Bump webmock to 3.7

--- common_spree_dependencies.rb
+++ common_spree_dependencies.rb
@@ -29,7 +29,7 @@ group :test do
   gem 'rspec_junit_formatter'
   gem 'jsonapi-rspec'
   gem 'simplecov'
-  gem 'webmock', '~> 3.0.1'
+  gem 'webmock', '~> 3.7'
   gem 'timecop'
   gem 'rails-controller-testing'
 end
