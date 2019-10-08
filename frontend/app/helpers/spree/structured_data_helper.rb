f3c60ae0f65ae684164c457b13165de74861db54
Damian Legawiec <damian@sparksolutions.co>
2019-10-08 09:23:09 +0200

    Added caching for JSON LD structured data (#9572)

--- frontend/app/helpers/spree/structured_data_helper.rb
+++ frontend/app/helpers/spree/structured_data_helper.rb
@@ -13,24 +13,26 @@ module Spree
     private
 
     def structured_product_hash(product)
-      {
-        '@context': 'https://schema.org/',
-        '@type': 'Product',
-        '@id': "#{spree.root_url}product_#{product.id}",
-        url: spree.product_url(product),
-        name: product.name,
-        image: structured_images(product),
-        description: product.description,
-        sku: product.sku,
-        offers: {
-          '@type': 'Offer',
-          price: product.price,
-          priceCurrency: current_currency,
-          availability: product.in_stock? ? 'InStock' : 'OutOfStock',
+      Rails.cache.fetch("spree/structured-data/#{product.cache_key}") do
+        {
+          '@context': 'https://schema.org/',
+          '@type': 'Product',
+          '@id': "#{spree.root_url}product_#{product.id}",
           url: spree.product_url(product),
-          availabilityEnds: product.discontinue_on ? product.discontinue_on.strftime('%F') : ''
+          name: product.name,
+          image: structured_images(product),
+          description: product.description,
+          sku: product.sku,
+          offers: {
+            '@type': 'Offer',
+            price: product.price,
+            priceCurrency: current_currency,
+            availability: product.in_stock? ? 'InStock' : 'OutOfStock',
+            url: spree.product_url(product),
+            availabilityEnds: product.discontinue_on ? product.discontinue_on.strftime('%F') : ''
+          }
         }
-      }
+      end
     end
 
     def structured_images(product)
