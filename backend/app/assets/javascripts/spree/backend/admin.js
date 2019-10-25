33e007b5cfe9e8c2668de00cc3257c90335011ed
Matthew Kennedy <m.kennedy@me.com>
2019-10-25 18:06:35 +0100

    improve

--- backend/app/assets/javascripts/spree/backend/admin.js
+++ backend/app/assets/javascripts/spree/backend/admin.js
@@ -107,7 +107,7 @@ jQuery(function ($) {
 
       label = ransackField(label.text()) + ': ' + ransackValue
 
-      filter = '<span class="js-filter badge badge-secondary" data-ransack-field="' + ransackFieldId + '">' + label + '<span class="icon icon-delete js-delete-filter"></span></span>'
+      filter = '<span class="js-filter badge badge-secondary" data-ransack-field="' + ransackFieldId + '">' + label + '<span class="icon icon-close icon-white icon-sm ml-2 js-delete-filter"></span></span>'
       $(".js-filters").append(filter).show()
     }
   })
