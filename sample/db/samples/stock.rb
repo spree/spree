575f3aca1c2f088247415a38fee10b6a9b533684
Kirill Kaiumov <kirill.kaiumov@sparksolutions.co>
2019-10-25 15:31:09 +0200

    Update Stock seeds

--- sample/db/samples/stock.rb
+++ sample/db/samples/stock.rb
@@ -1,19 +1,20 @@
 Spree::Sample.load_sample('variants')
 
 country =  Spree::Country.find_by(iso: 'US')
-location = Spree::StockLocation.first_or_create!(name: 'default',
-                                                 address1: 'Example Street',
-                                                 city: 'City',
-                                                 zipcode: '12345',
-                                                 country: country,
-                                                 state: country.states.first)
-location.active = true
-location.save!
+location = Spree::StockLocation.find_or_create_by!(name: 'default')
+location.update_attributes!(
+  address1: 'Example Street',
+  city: 'City',
+  zipcode: '12345',
+  country: country,
+  state: country.states.first,
+  active: true
+)
 
 Spree::Variant.all.each do |variant|
   next if variant.is_master? && variant.product.has_variants?
 
   variant.stock_items.each do |stock_item|
-    Spree::StockMovement.create(quantity: 10, stock_item: stock_item)
+    Spree::StockMovement.create(quantity: rand(20..50), stock_item: stock_item)
   end
 end
