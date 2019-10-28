8a05b3fcc3567ce7ddd9c916bf8b095517a7d40d
Kirill Kaiumov <kirill.kaiumov@sparksolutions.co>
2019-10-28 11:11:48 +0100

    Set correct Store URL in seeds

--- core/db/default/spree/stores.rb
+++ core/db/default/spree/stores.rb
@@ -3,7 +3,7 @@ unless Spree::Store.where(code: 'spree').exists?
   Spree::Store.new do |s|
     s.code              = 'spree'
     s.name              = 'Spree Demo Site'
-    s.url               = 'example.com'
+    s.url               = Rails.application.routes.default_url_options[:host] || 'example.com'
     s.mail_from_address = 'spree@example.com'
     s.default_currency  = 'USD'
   end.save!
--- sample/db/samples/stores.rb
+++
@@ -1,4 +0,0 @@
-spree_store = Spree::Store.find_by!(code: 'spree')
-spree_store.default_currency = 'USD'
-spree_store.url = Rails.application.routes.default_url_options[:host]
-spree_store.save!
