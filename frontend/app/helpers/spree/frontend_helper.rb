ad674da8292aed52b8c8f9a9206b1705f090988e
Punkbooster <ugliar.arsen@gmail.com>
2019-11-18 16:01:51 +0100

    Add missing frontend helper method & remove a duplicate one

--- frontend/app/helpers/spree/frontend_helper.rb
+++ frontend/app/helpers/spree/frontend_helper.rb
@@ -29,6 +29,22 @@ module Spree
       flashes.html_safe
     end
 
+    def link_to_cart(text = nil)
+      text = text ? h(text) : Spree.t('cart')
+      css_class = nil
+
+      if simple_current_order.nil? || simple_current_order.item_count.zero?
+        text = "<span class='glyphicon glyphicon-shopping-cart'></span> #{text}: (#{Spree.t('empty')})"
+        css_class = 'empty'
+      else
+        text = "<span class='glyphicon glyphicon-shopping-cart'></span> #{text}: (#{simple_current_order.item_count})
+                <span class='amount'>#{simple_current_order.display_total.to_html}</span>"
+        css_class = 'full'
+      end
+
+      link_to text.html_safe, spree.cart_path, class: "cart-info nav-link #{css_class}"
+    end
+
     def checkout_progress(numbers: false)
       states = @order.checkout_steps - ['complete']
       items = states.each_with_index.map do |state, i|
