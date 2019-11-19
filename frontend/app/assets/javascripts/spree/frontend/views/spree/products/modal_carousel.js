e24351ca7f0637d3165b8d95ee1ff51068f7e194
Tomek Urban <tomasz.urban@sparksolutions.co>
2019-11-19 16:13:23 +0100

    SD-421 Fix/single image broken pdp javascript

--- frontend/app/assets/javascripts/spree/frontend/views/spree/products/modal_carousel.js
+++ frontend/app/assets/javascripts/spree/frontend/views/spree/products/modal_carousel.js
@@ -1,36 +1,33 @@
 Spree.ready(function($) {
   var $modalCarousel = $('#productModalThumbnailsCarousel')
-  var modalThumbnails = new ThumbnailsCarousel($, $modalCarousel)
-
-  var selectedThumbnailVariantId =  function() {
-    var $selectedThumbnail = $('.row .product-thumbnails-carousel-item-single.product-thumbnails-carousel-item-single--visible .selected')
-    return $selectedThumbnail[0].parentNode.getAttribute('data-variant-id')
-  }
+  new ThumbnailsCarousel($, $modalCarousel)
 
   var activeSingleImageIndex = function(sourceWrappingClass) {
-    var $activeSingleImage = $('.' + sourceWrappingClass + ' .product-details-single [data-variant-id="' + selectedThumbnailVariantId() + '"].active')
+    var $activeSingleImage = $('.' + sourceWrappingClass + ' .product-details-single [data-variant-id].active')
     return $activeSingleImage.index()
   }
 
-  var selectModalThumbnail = function(variantId, imgIndex) {
-    var modalThumbnails = $('#productModalThumbnailsCarousel > div > div.carousel-item.product-thumbnails-carousel-item.active > div > div')
-    $(modalThumbnails.children('[data-variant-id="' + variantId + '"]').get(imgIndex).getElementsByTagName('img')[0]).addClass('selected')
+  var selectModalThumbnail = function(imgIndex) {
+    var $modalThumbnails = $('#productModalThumbnailsCarousel > div > div.carousel-item.product-thumbnails-carousel-item.active > div > div')
+    var $modalThumbnailsChildren = $modalThumbnails.children('[data-variant-id]')
+    if ($modalThumbnailsChildren.length > 0) {
+      $($modalThumbnailsChildren.get(imgIndex).getElementsByTagName('img')[0]).addClass('selected')
+    }
   }
 
-  var activateModalSingleImg = function(variantId, imgIndex, targetWrappingClass) {
-    $('.' + targetWrappingClass + ' .product-details-single [data-variant-id="' + variantId + '"].active').removeClass('active')
-    var $modalActiveSingleImage = $($('.' + targetWrappingClass + ' .product-details-single [data-variant-id="' + variantId + '"]').parent().children().get(imgIndex))
+  var activateModalSingleImg = function(imgIndex, targetWrappingClass) {
+    $('.' + targetWrappingClass + ' .product-details-single [data-variant-id].active').removeClass('active')
+    var $modalActiveSingleImage = $($('.' + targetWrappingClass + ' .product-details-single [data-variant-id]').parent().children().get(imgIndex))
     $modalActiveSingleImage.addClass('active')
   }
 
   $('#picturesModal').on('show.bs.modal', function() {
-    var variantId = selectedThumbnailVariantId()
     var rowActiveSingleImageIndex = activeSingleImageIndex('row')
-    selectModalThumbnail(variantId, rowActiveSingleImageIndex - 1)
-    activateModalSingleImg(variantId, rowActiveSingleImageIndex, 'modal')
+    selectModalThumbnail(rowActiveSingleImageIndex - 1)
+    activateModalSingleImg(rowActiveSingleImageIndex, 'modal')
   })
 
   $('#picturesModal').on('hide.bs.modal', function() {
-    activateModalSingleImg(selectedThumbnailVariantId(), activeSingleImageIndex('modal'), 'row')
+    activateModalSingleImg(activeSingleImageIndex('modal'), 'row')
   })
 })
