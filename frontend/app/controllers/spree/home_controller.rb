50b730b80aebbda344131d3fafdb16f13012efcb
Kirill Kaiumov <kirill.kaiumov@sparksolutions.co>
2019-11-25 11:53:12 +0100

    Move remaining frontend controllers

--- frontend/app/controllers/spree/home_controller.rb
+++ frontend/app/controllers/spree/home_controller.rb
@@ -4,10 +4,28 @@ module Spree
     respond_to :html
 
     def index
-      @searcher = build_searcher(params.merge(include_images: true))
-      @products = @searcher.retrieve_products
-      @products = @products.includes(:possible_promotions) if @products.respond_to?(:includes)
+      @bestsellers_products = load_taxon_products('Bestsellers')
+      @trending_products = load_taxon_products('Trending')
       @taxonomies = Spree::Taxonomy.includes(root: :children)
+
+      @combined_products = [@bestsellers_products, @trending_products].flatten.uniq
+    end
+
+    private
+
+    def load_taxon_products(taxon_name)
+      Spree::Product.joins(:taxons)
+                    .where(spree_taxons: { name: taxon_name })
+                    .eager_load(
+                      :variants_including_master,
+                      master: [
+                        :default_price,
+                        { images: { attachment_attachment: :blob } }
+                      ]
+                    )
+                    .available
+                    .limit(12)
+                    .to_a
     end
   end
 end
--- frontend/app/controllers/spree/taxons_controller.rb
+++ frontend/app/controllers/spree/taxons_controller.rb
@@ -5,12 +5,12 @@ module Spree
     respond_to :html
 
     def show
-      @taxon = Taxon.friendly.find(params[:id])
-      return unless @taxon
+      @taxon = Spree::Taxon.friendly.find(params[:id])
 
       @searcher = build_searcher(params.merge(taxon: @taxon.id, include_images: true))
       @products = @searcher.retrieve_products
       @taxonomies = Spree::Taxonomy.includes(root: :children)
+      @option_types = Spree::OptionType.includes(:option_values)
     end
 
     private
