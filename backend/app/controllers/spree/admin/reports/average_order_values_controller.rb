052a654cbd71a864e34df2fd29de995293b67d5e
Piotr Leniec <piotr.leniec@sparksolutions.co>
2019-11-29 12:10:32 +0100

    add an empty view for average order values report

---
+++ backend/app/controllers/spree/admin/reports/average_order_values_controller.rb
@@ -0,0 +1,10 @@
+module Spree
+  module Admin
+    module Reports
+      class AverageOrderValuesController < Spree::Admin::BaseController
+        def show
+        end
+      end
+    end
+  end
+end
