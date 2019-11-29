f63d4662fc0c042ec460ad9cf9d37ed365982a37
Piotr Leniec <piotr.leniec@sparksolutions.co>
2019-11-29 10:17:33 +0100

    WIP

--- backend/app/controllers/spree/admin/reports/total_sales_controller.rb
+++ backend/app/controllers/spree/admin/reports/total_sales_controller.rb
@@ -3,6 +3,14 @@ module Spree
     module Reports
       class TotalSalesController < Spree::Admin::BaseController
         def show
+          orders = Spree::Admin::Reports::TotalSalesQuery.new.call
+
+          respond_to do |format|
+            format.html
+            format.json do
+              render json: Spree::Admin::Reports::TotalSales::JsonSerializer.new.call(orders)
+            end
+          end
         end
       end
     end
