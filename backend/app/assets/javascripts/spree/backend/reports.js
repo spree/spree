a034911077ee7a9ac017889f5b66b2c6a1b1052c
Alexey Topolyanskiy <devilcoders@protonmail.ch>
2019-12-02 12:22:02 +0200

    Add UI to group chart data

--- backend/app/assets/javascripts/spree/backend/reports.js
+++ backend/app/assets/javascripts/spree/backend/reports.js
@@ -64,26 +64,77 @@ function getData (json, key) {
 }
 
 /**
+ * @param {string} reportId String ID of the report, comes from data-id attr.
+ * @returns {string} API URL
+ */
+function getApiURL (reportId) {
+  return '/admin/reports/' + reportId + '.json' + window.location.search
+}
+
+/**
  * @param {object} data Data referencing information for data fetching
  * @param {Chart} chart Chart instance
  */
 function updateChart (data, chart) {
-  return fetch(`/admin/reports/${data.id}.json`)
+  return fetch(getApiURL(data.id))
     .then(function (response) {
       return response.json()
     })
     .then(function (json) {
       chart.data.datasets[0].data = getData(json, data.dataKey)
       chart.data.labels = getData(json, data.labelKey)
-    }).finally(function () {
+    })
+    .finally(function () {
       chart.update()
     })
 }
 
-function initReports () {
-  const nodes = getChartNodes('reports--chart')
+/**
+ * @param {HTMLSelectElement} node DOM select not to get index of
+ * @param {string} param The param from URL to search through select options
+ * @returns {number} Index of option value that should be selected
+ */
+function getParamSelectedIndex (node, param) {
+  const options = []
+  const selectedValue = new Uri(window.location.search).getQueryParamValue(param)
 
-  return Array.prototype.forEach.call(nodes, function (node) {
+  Array.prototype.forEach.call(node.options, function (el) {
+    options.push(el.value)
+  })
+
+  return options.indexOf(selectedValue)
+}
+
+function initFilter () {
+  const groupBySelect = document.getElementById('reports-group-by')
+
+  // Set selected value from URL param
+  groupBySelect.selectedIndex = getParamSelectedIndex(groupBySelect, 'group_by')
+
+  // Change browser URL and init request when user select option
+  groupBySelect.addEventListener('change', function (e) {
+    const url = new Uri(window.location).replaceQueryParam(
+      'group_by',
+      e.target.options[e.target.selectedIndex].value
+    ).toString()
+
+    window.history.pushState({}, '', url)
+
+    return initReports()
+  })
+
+  // Listen to browser history change and init requests
+  window.onpopstate = function (e) {
+    groupBySelect.selectedIndex = getParamSelectedIndex(groupBySelect, 'group_by')
+
+    return initReports()
+  }
+}
+
+function initReports () {
+  const chartNodes = getChartNodes('reports--chart')
+
+  return Array.prototype.forEach.call(chartNodes, function (node) {
     const data = getChartData(node)
     const chart = createChart(node, data.type, {
       labels: [],
@@ -96,4 +147,5 @@ function initReports () {
 
 document.addEventListener('DOMContentLoaded', function () {
   initReports()
+  initFilter()
 })
