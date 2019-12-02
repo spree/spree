c7dd0b4d588df4c52c5aa7ef86732e03dd82ba3e
Damian Legawiec <damian@sparksolutions.co>
2019-12-02 23:14:12 +0100

    Fixes security issue with API v2 Order information
    Attacker could expose Order information using brute force to guess
    Order numbers. This patch fixes it by requiring Order token to obtain
    Order information from API v2 Order Status endpoint.

--- api/spec/requests/spree/api/v2/storefront/order_status_spec.rb
+++ api/spec/requests/spree/api/v2/storefront/order_status_spec.rb
@@ -6,16 +6,26 @@ describe 'Storefront API v2 OrderStatus spec', type: :request do
   include_context 'API v2 tokens'
 
   describe '#show' do
-    context 'as a guest user' do
-      before { get "/api/v2/storefront/order_status/#{order.number}" }
+    context 'with existing Order number' do
+      context 'as a guest user without token' do
+        before { get "/api/v2/storefront/order_status/#{order.number}" }
 
-      it_behaves_like 'returns valid cart JSON'
-    end
+        it_behaves_like 'returns 404 HTTP status'
+      end
 
-    context 'as a guest user with token' do
-      before { get "/api/v2/storefront/order_status/#{order.number}", headers: headers_order_token }
+      context 'as a guest user with invalid token' do
+        let(:headers_order_token) { { 'X-Spree-Order-Token' => 'WRONG' } }
 
-      it_behaves_like 'returns valid cart JSON'
+        before { get "/api/v2/storefront/order_status/#{order.number}", headers: headers_order_token }
+
+        it_behaves_like 'returns 404 HTTP status'
+      end
+
+      context 'as a guest user with valid token' do
+        before { get "/api/v2/storefront/order_status/#{order.number}", headers: headers_order_token }
+
+        it_behaves_like 'returns valid cart JSON'
+      end
     end
 
     context 'with non-existing Order number' do
