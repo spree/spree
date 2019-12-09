446b1f6ff4be314edb99f3bf6917a8c8fce44974
mbajur <mbajur@gmail.com>
2019-12-09 16:57:45 +0100

    Final fixes

--- backend/app/controllers/spree/admin/reports/total_orders_controller.rb
+++ backend/app/controllers/spree/admin/reports/total_orders_controller.rb
@@ -3,17 +3,21 @@ module Spree
     module Reports
       class TotalOrdersController < Spree::Admin::BaseController
         def show
-          objects = Spree::Admin::Reports::Orders::TotalOrders.new(params).call
-
           respond_to do |format|
             format.html
             format.json do
-              render json: Spree::Admin::Reports::TotalOrders::JsonSerializer.new.call(objects)
+              render json: Spree::Admin::Reports::TotalOrders::JsonSerializer.new.call(filtered_data)
             end
             format.csv do
-              render plain: Spree::Admin::Reports::TotalOrders::CsvSerializer.new.call(objects)
+              render plain: Spree::Admin::Reports::TotalOrders::CsvSerializer.new.call(filtered_data)
             end
           end
+        end
+
+        private
+
+        def filtered_data
+          Spree::Admin::Reports::Orders::TotalOrders.new(params).call
         end
       end
     end
--- backend/spec/requests/spree/admin/reports/total_orders_spec.rb
+++ backend/spec/requests/spree/admin/reports/total_orders_spec.rb
@@ -8,23 +8,29 @@ describe 'Admin Reports - total orders spec', type: :request do
   let!(:order3) { create(:completed_order_with_totals) }
 
   before do
-    order1.update(completed_at: '2019-11-29')
-    order2.update(completed_at: '2019-11-28')
-    order3.update(completed_at: '2019-10-27')
+    order1.update(completed_at: '2019-10-11')
+    order2.update(completed_at: '2019-10-14')
+    order3.update(completed_at: '2019-10-15')
   end
 
-  describe 'total_sales#show' do
-    before { get '/admin/reports/total_orders.json' }
+  let(:params) do
+    { completed_at_min: '2019-10-11', completed_at_max: '2019-10-17' }
+  end
 
-    let(:json_response) { JSON.parse(response.body) }
+  describe 'total_orders#show' do
+    context 'with valid date range' do
+      before { get '/admin/reports/total_orders.json', params: params }
 
-    it 'returns 200 HTTP status' do
-      expect(response).to have_http_status(:ok)
-    end
+      let(:json_response) { JSON.parse(response.body) }
 
-    it 'return JSON data for charts' do
-      expect(json_response['labels']).to eq ['2019-10-27', '2019-11-28', '2019-11-29']
-      expect(json_response['data']).to   eq [1, 1, 1]
+      it 'returns 200 HTTP status' do
+        expect(response).to have_http_status(:ok)
+      end
+
+      it 'return JSON data for charts' do
+        expect(json_response['labels']).to eq ['2019-10-11', '2019-10-12', '2019-10-13', '2019-10-14', '2019-10-15', '2019-10-16', '2019-10-17']
+        expect(json_response['data']).to   eq [1, 0, 0, 1, 1, 0, 0]
+      end
     end
   end
 end
