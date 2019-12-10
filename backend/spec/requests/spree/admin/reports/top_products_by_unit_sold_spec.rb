41f81d909f53298dc8a80ac1626cf39562111a23
mbajur <mbajur@gmail.com>
2019-12-10 14:01:15 +0100

    TopProductsByUnitSold request specs

---
+++ backend/spec/requests/spree/admin/reports/top_products_by_unit_sold_spec.rb
@@ -0,0 +1,45 @@
+require 'spec_helper'
+
+describe 'Admin Reports - top products by unit sold spec', type: :request do
+  stub_authorization!
+
+  let!(:order1) { create(:completed_order_with_totals) }
+  let!(:order2) { create(:completed_order_with_totals) }
+  let!(:order3) { create(:completed_order_with_totals) }
+
+  let(:product1) { create(:product) }
+  let(:product2) { create(:product) }
+
+  let(:variant1) { create(:variant, product: product1)}
+
+  before do
+    order1.update(completed_at: '2019-10-11')
+    order2.update(completed_at: '2019-10-14')
+    order3.update(completed_at: '2019-10-15')
+
+    order1.line_items.first.update(quantity: 3, variant: product1.master)
+    order2.line_items.first.update(quantity: 2, variant: variant1)
+    order3.line_items.first.update(quantity: 1, variant: product2.master)
+  end
+
+  let(:params) do
+    { completed_at_min: '2019-10-11', completed_at_max: '2019-10-17' }
+  end
+
+  describe 'top_products_by_unit_sold#show' do
+    context 'with valid date range' do
+      before { get '/admin/reports/top_products_by_unit_sold.json', params: params }
+
+      let(:json_response) { JSON.parse(response.body) }
+
+      it 'returns 200 HTTP status' do
+        expect(response).to have_http_status(:ok)
+      end
+
+      it 'return JSON data for charts' do
+        expect(json_response['labels']).to eq [product1.master.sku, variant1.sku, product2.master.sku]
+        expect(json_response['data']).to   eq [3, 2, 1]
+      end
+    end
+  end
+end
