8769612dd0d8760d9c8f1bbc1fd3f9459f061a93
Damian Legawiec <damian@sparksolutions.co>
2020-01-03 16:37:02 +0100

    Fixed StructuredDataHelper caching to include currency and locale

--- frontend/app/helpers/spree/structured_data_helper.rb
+++ frontend/app/helpers/spree/structured_data_helper.rb
@@ -13,7 +13,7 @@ module Spree
     private
 
     def structured_product_hash(product)
-      Rails.cache.fetch("spree/structured-data/#{product.cache_key}") do
+      Rails.cache.fetch(common_product_cache_keys + ["spree/structured-data/#{product.cache_key}"]) do
         {
           '@context': 'https://schema.org/',
           '@type': 'Product',
