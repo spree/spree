a7d378c841b49f76cedab802febafd47d1a96875
Damian Legawiec <damian@sparksolutions.co>
2020-01-12 11:33:44 +0100

    Added full page caching for Home page

--- frontend/app/controllers/spree/home_controller.rb
+++ frontend/app/controllers/spree/home_controller.rb
@@ -6,6 +6,8 @@ module Spree
     def index
       @bestsellers_products = load_taxon_products('Bestsellers')
       @trending_products = load_taxon_products('Trending')
+
+      fresh_when etag: etag, last_modified: last_modified, public: true
     end
 
     private
@@ -23,6 +25,30 @@ module Spree
         available.
         order('spree_products_taxons.position').
         limit(12)
+    end
+
+    def products_updated_at
+      @products_updated_at ||= [
+        @bestsellers_products&.maximum(:updated_at),
+        @trending_products&.maximum(:updated_at)
+      ].compact
+      @products_updated_at
+    end
+
+    def etag
+      [
+        store_etag,
+        products_updated_at,
+        additional_cache_key
+      ]
+    end
+
+    def last_modified
+      (products_updated_at + [current_store.updated_at]).max.utc
+    end
+
+    def additional_cache_key
+      # add your own project specific cache key here
     end
   end
 end
