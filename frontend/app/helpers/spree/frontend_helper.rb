a370a86f598f933f215e5330896feba8873f9912
Alexey Topolyanskiy <devilcoders@protonmail.ch>
2020-01-22 11:55:10 +0100

    Move meta tags to main head and use better helpers

--- frontend/app/helpers/spree/frontend_helper.rb
+++ frontend/app/helpers/spree/frontend_helper.rb
@@ -181,17 +181,6 @@ module Spree
       set.join(', ')
     end
 
-    def og_image_pdp(product)
-      image = default_image_for_product_or_variant(product)
-      image_url = image&.url('large') || asset_path('noimage/large.png')
-
-      main_app.url_for(image_url)
-    end
-
-    def og_description(product)
-      sanitize product_description(product).truncate(200)
-    end
-
     def taxons_tree(root_taxon, current_taxon, max_level = 1)
       return '' if max_level < 1 || root_taxon.leaf?
 
