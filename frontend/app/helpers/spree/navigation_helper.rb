2ad20097def63a279aa655bb27fcf818561adb45
Damian Legawiec <damian@sparksolutions.co>
2020-01-22 15:25:46 +0100

    Added support for multi store config for spree storefront

--- frontend/app/helpers/spree/navigation_helper.rb
+++ frontend/app/helpers/spree/navigation_helper.rb
@@ -1,7 +1,7 @@
 module Spree
   module NavigationHelper
     def spree_navigation_data
-      SpreeStorefrontConfig[:navigation] || []
+      SpreeStorefrontConfig.dig(current_store.code, :navigation) || SpreeStorefrontConfig.dig(:default, :navigation) || []
     # safeguard for older Spree installs that don't have spree_navigation initializer
     # or spree.yml file present
     rescue
