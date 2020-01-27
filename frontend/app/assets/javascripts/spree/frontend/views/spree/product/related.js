04a6a5fc622ee898991ee52df5a18d32c728b80e
PSzyszkaSpark <pawel.szyszka@sparksolutions.co>
2020-01-27 13:33:36 +0100

    Corrects routes, show.html, related.js and frontend.js

--- frontend/app/assets/javascripts/spree/frontend/views/spree/product/related.js
+++ frontend/app/assets/javascripts/spree/frontend/views/spree/product/related.js
@@ -1,7 +1,6 @@
-Spree.fetchRelatedProductcs = function (slug, id) {
+Spree.fetchRelatedProductcs = function (id) {
   return $.ajax({
-    url: Spree.routes.product_related(slug),
-    data: { id: id }
+    url: Spree.routes.product_related(id)
   }).done(function (data) {
     $('#related-products').html(data);
     $('.carousel').carouselBootstrap4()
