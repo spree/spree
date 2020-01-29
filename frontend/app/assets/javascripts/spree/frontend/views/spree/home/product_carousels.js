e68be3f3cf525ac45c388f00dd712d57fe806556
Damian Legawiec <damian@sparksolutions.co>
2020-01-29 09:28:35 +0100

    Load carousels when they enter viewport

--- frontend/app/assets/javascripts/spree/frontend/views/spree/home/product_carousels.js
+++ frontend/app/assets/javascripts/spree/frontend/views/spree/home/product_carousels.js
@@ -7,15 +7,31 @@ Spree.fetchProductCarousel = function (taxonId, htmlContainer) {
   })
 }
 
+$.fn.isInViewport = function () {
+  var elementTop = $(this).offset().top;
+  var elementBottom = elementTop + $(this).outerHeight();
+  var viewportTop = $(window).scrollTop();
+  var viewportBottom = viewportTop + $(window).height();
+  return elementBottom > viewportTop && elementTop < viewportBottom;
+};
+
 document.addEventListener('turbolinks:load', function () {
   var homePage = $('body#home')
 
   if (homePage.length) {
-    $('div[data-product-carousel').each(function (_index, element) {
-      var productCarousel = $(this)
-      var taxonId = productCarousel.attr('data-product-carousel-taxon-id')
+    $(window).on('resize scroll', function () {
+      $('div[data-product-carousel').each(function (_index, element) {
+        var container = $(element)
+        var productCarousel = $(this)
+        var carouselLoaded = productCarousel.attr('data-product-carousel-loaded')
 
-      Spree.fetchProductCarousel(taxonId, $(element))
+        if (container.isInViewport() && !carouselLoaded) {
+          var taxonId = productCarousel.attr('data-product-carousel-taxon-id')
+          productCarousel.attr('data-product-carousel-loaded', 'true')
+
+          Spree.fetchProductCarousel(taxonId, container)
+        }
+      })
     })
   }
 })
