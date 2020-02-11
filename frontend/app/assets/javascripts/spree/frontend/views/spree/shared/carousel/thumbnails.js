9aa5cf075bddcc94c6da65a83a7eae1380eb03e1
Piotr Milewski <piotr.milewski@sparksolutions.co>
2020-02-11 12:58:59 +0100

    [SD-743] refactor selectImage function, remove $ from variable names

--- frontend/app/assets/javascripts/spree/frontend/views/spree/shared/carousel/thumbnails.js
+++ frontend/app/assets/javascripts/spree/frontend/views/spree/shared/carousel/thumbnails.js
@@ -1,39 +1,40 @@
-function ThumbnailsCarousel($, $carousel) {
+function ThumbnailsCarousel($, carousel) {
   var VISIBLE_IMAGE_SELECTOR = '.product-thumbnails-carousel-item-single--visible img'
   var SELECTED_IMAGE_CLASS = 'selected'
   var self = this
-  var $modalCarouselId = 'productModalThumbnailsCarousel'
-  var $modalCarousel = $('#productModalThumbnailsCarousel')
-  var $zoomClickObject = $('.product-carousel-overlay-modal-opener')
+  var modalCarouselId = 'productModalThumbnailsCarousel'
+  var modalCarousel = $(`#${modalCarouselId}`)
+  var zoomClickObject = $('.product-carousel-overlay-modal-opener')
 
   this.constructor = function() {
     this.bindEventHandlers()
   }
 
   this.bindEventHandlers = function() {
-    $zoomClickObject.on('click', this.handleZoomClick)
-    $carousel.on('click', 'img', this.handleImageClick)
-    $carousel.on('thumbnails:ready', this.handleThumbnailsReady)
+    zoomClickObject.on('click', this.handleZoomClick)
+    carousel.on('click', 'img', this.handleImageClick)
+    carousel.on('thumbnails:ready', this.handleThumbnailsReady)
 
     $('body').on('single_carousel:slide', this.handleSingleCarouselSlide)
   }
 
   this.handleImageClick = function(event) {
-    self.selectImage($(event.target), $(event.target))
+    self.selectImage(event, $(event.target))
   }
 
   this.handleThumbnailsReady = function(_event) {
-    var image = $carousel.find(VISIBLE_IMAGE_SELECTOR).eq(0)
+    var image = carousel.find(VISIBLE_IMAGE_SELECTOR).eq(0)
 
-    self.selectImage(_event.target, image)
+    self.selectImage(_event, image)
   }
 
   this.handleSingleCarouselSlide = function(_event, imageIndex) {
+    var image;
     if (_event.target.id === 'productModalCarousel') {
-      var image = $modalCarousel.find('[data-product-carousel-to-slide=' + imageIndex + '] img')
+      image = modalCarousel.find('[data-product-carousel-to-slide=' + imageIndex + '] img')
       self.unselectModalImages()
     } else {
-      var image = $carousel.find('[data-product-carousel-to-slide=' + imageIndex + '] img')
+      image = carousel.find('[data-product-carousel-to-slide=' + imageIndex + '] img')
       self.unselectThumbanilsImages()
     }
 
@@ -41,7 +42,10 @@ function ThumbnailsCarousel($, $carousel) {
   }
 
   this.selectImage = function(_event, image) {
-    if (_event.id === $modalCarouselId || $($($($($($(_event).parent()).parent()).parent()).parent()).parent()).parent()[0].id === $modalCarouselId) {
+    var clickedElement = _event.target
+    var productModalThumbnailClicked = $(clickedElement).closest('.product-thumbnails-carousel').is(`#${modalCarouselId}`)
+
+    if (clickedElement.id === modalCarouselId || productModalThumbnailClicked) {
       this.unselectModalImages()
     } else {
       this.unselectThumbanilsImages()
@@ -51,26 +55,26 @@ function ThumbnailsCarousel($, $carousel) {
   }
 
   this.unselectThumbanilsImages = function() {
-    $carousel.find('img').removeClass(SELECTED_IMAGE_CLASS)
+    carousel.find('img').removeClass(SELECTED_IMAGE_CLASS)
   }
 
   this.unselectModalImages = function() {
-    $modalCarousel.find('img').removeClass(SELECTED_IMAGE_CLASS)
+    modalCarousel.find('img').removeClass(SELECTED_IMAGE_CLASS)
   }
 
   this.handleZoomClick = function(_event) {
-    var $selectedImageId = self.getSelectedImageId()
-    var image = $modalCarousel.find(VISIBLE_IMAGE_SELECTOR).eq($selectedImageId)
+    var selectedImageId = self.getSelectedImageId()
+    var image = modalCarousel.find(VISIBLE_IMAGE_SELECTOR).eq(selectedImageId)
     self.unselectModalImages()
 
     image.addClass(SELECTED_IMAGE_CLASS)
   }
 
   this.getSelectedImageId = function() {
-    var $selectedImages = $('#productThumbnailsCarousel').find('img.d-block.w-100.lazyloaded.selected')
+    var selectedImages = $('#productThumbnailsCarousel').find('img.d-block.w-100.lazyloaded.selected')
 
-    if ($selectedImages.length > 1) {
-      return $($selectedImages[1]).parent()[0].dataset.productCarouselToSlide
+    if (selectedImages.length > 1) {
+      return $(selectedImages[1]).parent()[0].dataset.productCarouselToSlide
     } else {
       return '0'
     }
@@ -97,7 +101,7 @@ Spree.ready(function($) {
       )
     }
 
-    Spree.showThumbnailsCarouselVariantImages = function($carousel, variantId) {
+    Spree.showThumbnailsCarouselVariantImages = function(carousel, variantId) {
       var carouselSlideSelector = '.product-thumbnails-carousel-item'
       var carouselSlideContainerSelector =
         '.product-thumbnails-carousel-item-content'
@@ -107,52 +111,52 @@ Spree.ready(function($) {
       var carouselPerPageAttributeName = 'data-product-carousel-per-page'
       var carouselEmptyClass = 'product-thumbnails-carousel--empty'
 
-      $carousel.carouselBootstrap4('dispose')
+      carousel.carouselBootstrap4('dispose')
       var qualifiedSlides = 0
-      var perPage = parseInt($carousel.attr(carouselPerPageAttributeName)) || 1
-      var $slides = $carousel.find(carouselSlideSelector)
+      var perPage = parseInt(carousel.attr(carouselPerPageAttributeName)) || 1
+      var slides = carousel.find(carouselSlideSelector)
 
-      $carousel
+      carousel
         .find(carouselItemSelector)
         .each(function(_itemIndex, slideElement) {
           // Switch item visibility in the carousel based on picked variant.
 
           var targetSlideIndex
-          var $slide = $(slideElement)
+          var slide = $(slideElement)
           var qualifies =
-            $slide.attr(variantIdAttributeName) === variantId ||
-            $slide.attr(isMasterVariantAttributeName) === 'true'
+            slide.attr(variantIdAttributeName) === variantId ||
+            slide.attr(isMasterVariantAttributeName) === 'true'
 
           if (qualifies) {
             qualifiedSlides += 1
-            $slide.attr(carouselToSlideAttributeName, qualifiedSlides - 1)
+            slide.attr(carouselToSlideAttributeName, qualifiedSlides - 1)
           }
 
           targetSlideIndex = Math.max(0, Math.ceil(qualifiedSlides / perPage) - 1)
-          $slide.detach()
-          $slides
+          slide.detach()
+          slides
             .eq(targetSlideIndex)
             .find(carouselSlideContainerSelector)
-            .append($slide)
-          $slide.toggleClass(enabledCarouselSingleClass, qualifies)
+            .append(slide)
+          slide.toggleClass(enabledCarouselSingleClass, qualifies)
         })
       var enabledSlidesCount = Math.ceil(qualifiedSlides / perPage)
-      $carousel
+      carousel
         .find(carouselSlideSelector)
         .each(function(slideIndex, slideElement) {
-          var $slide = $(slideElement)
-          $slide.toggleClass(
+          var slide = $(slideElement)
+          slide.toggleClass(
             enabledCarouselItemClass,
             slideIndex < enabledSlidesCount
           )
-          $slide.toggleClass(activeCarouselItemClass, slideIndex === 0)
+          slide.toggleClass(activeCarouselItemClass, slideIndex === 0)
         })
 
       // If there are no images to show after picking a variant, disable the carousel.
-      $carousel.toggleClass(carouselEmptyClass, enabledSlidesCount === 0)
+      carousel.toggleClass(carouselEmptyClass, enabledSlidesCount === 0)
 
-      $carousel.carouselBootstrap4()
-      $carousel.trigger('thumbnails:ready')
+      carousel.carouselBootstrap4()
+      carousel.trigger('thumbnails:ready')
     }
 
     $('#product-details').on('variant_id_change', function(options) {
@@ -166,8 +170,8 @@ Spree.ready(function($) {
       })
     })
 
-    var $carousel = $('#productThumbnailsCarousel')
+    var carousel = $('#productThumbnailsCarousel')
 
-    ThumbnailsCarousel($, $carousel)
+    ThumbnailsCarousel($, carousel)
   }
 })
