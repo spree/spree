226ecbf26d43898e1ef5d326a9ea2ef8a3357cf4
AleksandraKaminska <aleksandra-kaminska@outlook.com>
2020-02-18 14:26:09 +0100

    use only css to change clor border on radio inputs

--- frontend/app/assets/javascripts/spree/frontend/views/spree/products/index.js
+++ frontend/app/assets/javascripts/spree/frontend/views/spree/products/index.js
@@ -1,56 +1,37 @@
 Spree.ready(function($) {
-  $('#sort-by-overlay-show-button').click(function() { $('#sort-by-overlay').show() })
-  $('#sort-by-overlay-hide-button').click(function() { $('#sort-by-overlay').hide() })
+  $('#sort-by-overlay-show-button').click(function() {
+    $('#sort-by-overlay').show();
+  });
+  $('#sort-by-overlay-hide-button').click(function() {
+    $('#sort-by-overlay').hide();
+  });
 
-  $('#filter-by-overlay-show-button').click(function() { $('#filter-by-overlay').show() })
-  $('#filter-by-overlay-hide-button').click(function() { $('#filter-by-overlay').hide() })
+  $('#filter-by-overlay-show-button').click(function() {
+    $('#filter-by-overlay').show();
+  });
+  $('#filter-by-overlay-hide-button').click(function() {
+    $('#filter-by-overlay').hide();
+  });
 
   function closeNoProductModal() {
-    $('#no-product-available').removeClass('shown')
-    $('#overlay').removeClass('shown')
+    $('#no-product-available').removeClass('shown');
+    $('#overlay').removeClass('shown');
   }
 
-  $('#no-product-available-close-button').click(closeNoProductModal)
-  $('#no-product-available-hide-button').click(closeNoProductModal)
+  $('#no-product-available-close-button').click(closeNoProductModal);
+  $('#no-product-available-hide-button').click(closeNoProductModal);
 
   $('.plp-overlay-card-item').click(function() {
-    $(this).toggleClass('plp-overlay-card-item--selected')
-  })
-
-  var allOptionsBorders = $('.color-select-border')
-  var addToCartButton = $('#add-to-cart-button')
-
-  function removeSelectedBorders() {
-    allOptionsBorders.each(function() {
-      $(this).removeClass('color-select-border--selected')
-    })
-  }
-
-  $('.color-select').click(function() {
-    var borderElement = $(this).find('.color-select-border')
-
-    removeSelectedBorders()
-    borderElement.addClass('color-select-border--selected')
-  })
-
-  if (allOptionsBorders.length && addToCartButton !== null) {
-    var colorsClassList = []
-
-    allOptionsBorders.each(function(index, optionBorder) {
-      colorsClassList.push(optionBorder.classList)
-    })
-
-    if (!colorsClassList.includes('color-select-border--selected')) {
-      removeSelectedBorders()
-      allOptionsBorders[0].classList.add('color-select-border--selected')
-    }
-  }
+    $(this).toggleClass('plp-overlay-card-item--selected');
+  });
 
   $('.plp-overlay-ul-li').click(function() {
-    $('.plp-overlay-ul-li--active').removeClass('plp-overlay-ul-li--active')
-      .addClass('plp-overlay-ul-li')
+    $('.plp-overlay-ul-li--active')
+      .removeClass('plp-overlay-ul-li--active')
+      .addClass('plp-overlay-ul-li');
 
-    $(this).removeClass('plp-overlay-ul-li')
-      .addClass('plp-overlay-ul-li--active')
-  })
-})
+    $(this)
+      .removeClass('plp-overlay-ul-li')
+      .addClass('plp-overlay-ul-li--active');
+  });
+});
