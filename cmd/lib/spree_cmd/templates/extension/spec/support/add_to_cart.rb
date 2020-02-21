6ca8a05110f3f14dc0b23fc134640c81f9edbac9
Damian Legawiec <damian@sparksolutions.co>
2020-02-21 05:56:02 +0100

    Added `add_to_cart` method for extensions using Spree storefront

---
+++ cmd/lib/spree_cmd/templates/extension/spec/support/add_to_cart.rb
@@ -0,0 +1,15 @@
+def add_to_cart(product)
+  visit spree.product_path(product)
+
+  expect(page).to have_selector('form#add-to-cart-form')
+  expect(page).to have_selector(:button, id: 'add-to-cart-button', disabled: false)
+  click_button 'add-to-cart-button'
+  expect(page).to have_content(Spree.t(:added_to_cart))
+
+  if block_given?
+    yield
+  else
+    click_link 'View cart'
+    expect(page).to have_content 'YOUR SHOPPING CART'
+  end
+end
