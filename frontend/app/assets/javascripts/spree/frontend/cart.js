719422abe16d9183148ef32fc074b26791a8fe17
Damian Legawiec <damian@sparksolutions.co>
2020-02-27 10:36:00 +0100

    Improve cart management JS code

--- frontend/app/assets/javascripts/spree/frontend/cart.js
+++ frontend/app/assets/javascripts/spree/frontend/cart.js
@@ -12,24 +12,24 @@ Spree.ready(function ($) {
       }
     }
 
-    $('form#update-cart a.delete').show().one('click', function () {
+    formUpdateCart.find('a.delete').show().one('click', function () {
       $(this).parents('.shopping-cart-item').first().find('input.shopping-cart-item-quantity-input').val(0)
       clearInvalidCouponField()
-      $(this).parents('form').first().submit()
+      formUpdateCart.submit()
       return false
     })
-    $('form#update-cart input.shopping-cart-item-quantity-input').on('keyup', function(e) {
+    formUpdateCart.find('input.shopping-cart-item-quantity-input').on('keyup', function(e) {
       var itemId = $(this).attr('data-id')
       var value = $(this).val()
       var newValue = isNaN(value) || value === '' ? value : parseInt(value, 10)
       var targetInputs = $("form#update-cart input.shopping-cart-item-quantity-input[data-id='" + itemId + "']")
       $(targetInputs).val(newValue)
     })
-    $('form#update-cart input.shopping-cart-item-quantity-input').on('change', function(e) {
+    formUpdateCart.find('input.shopping-cart-item-quantity-input').on('change', function(e) {
       clearInvalidCouponField()
       formUpdateCart.submit()
     })
-    $('form#update-cart button.shopping-cart-item-quantity-decrease-btn').off('click').on('click', function() {
+    formUpdateCart.find('button.shopping-cart-item-quantity-decrease-btn').off('click').on('click', function() {
       var itemId = $(this).attr('data-id')
       var input = $("input[data-id='" + itemId + "']")
       var inputValue = parseInt($(input).val(), 10)
@@ -40,7 +40,7 @@ Spree.ready(function ($) {
         formUpdateCart.submit()
       }
     })
-    $('form#update-cart button.shopping-cart-item-quantity-increase-btn').off('click').on('click', function() {
+    formUpdateCart.find('button.shopping-cart-item-quantity-increase-btn').off('click').on('click', function() {
       var itemId = $(this).attr('data-id')
       var input = $("input[data-id='" + itemId + "']")
       var inputValue = parseInt($(input).val(), 10)
@@ -49,7 +49,7 @@ Spree.ready(function ($) {
       clearInvalidCouponField()
       formUpdateCart.submit()
     })
-    $('form#update-cart button#shopping-cart-coupon-code-button').off('click').on('click', function(event) {
+    formUpdateCart.find('button#shopping-cart-coupon-code-button').off('click').on('click', function(event) {
       var couponCodeField = $('#order_coupon_code');
 
       if (!$.trim(couponCodeField.val()).length) {
@@ -58,7 +58,7 @@ Spree.ready(function ($) {
       }
     })
 
-    $('form#update-cart button#shopping-cart-remove-coupon-code-button').off('click').on('click', function(event) {
+    formUpdateCart.find('button#shopping-cart-remove-coupon-code-button').off('click').on('click', function(event) {
       var input = {
         appliedCouponCodeField: $('#order_applied_coupon_code'),
         couponCodeField: $('#order_coupon_code'),
@@ -81,7 +81,7 @@ Spree.ready(function ($) {
       couponStatus: $('#coupon_status'),
       couponButton: $('#shopping-cart-coupon-code-button')
     }
-    var updateButton = $('form#update-cart #update-button')
+    var updateButton = formUpdateCart.find('#update-button')
     updateButton.attr('disabled', true)
     if ($.trim(input.couponCodeField.val()).length > 0) {
       // eslint-disable-next-line no-undef
