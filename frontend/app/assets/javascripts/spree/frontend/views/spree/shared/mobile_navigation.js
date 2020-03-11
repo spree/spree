9e971230de3ce9f92131a612bd5c085db188df73
PSzyszkaSpark <pawel.szyszka@sparksolutions.co>
2020-03-11 15:02:55 +0100

    Revetrs changes in mobile_navigation.js

--- frontend/app/assets/javascripts/spree/frontend/views/spree/shared/mobile_navigation.js
+++ frontend/app/assets/javascripts/spree/frontend/views/spree/shared/mobile_navigation.js
@@ -18,8 +18,6 @@ Spree.ready(function($) {
       this.onCloseClick = this.onCloseClick.bind(this);
       this.onBackClick = this.onBackClick.bind(this);
       this.closeAllCategories = this.closeAllCategories.bind(this);
-      this.addMobileNavigationClasses = this.addMobileNavigationClasses.bind(this);
-      this.removeMobileNavigationclasses = this.removeMobileNavigationclasses.bind(this);
 
       window.addEventListener('resize', this.onResize);
       window.addEventListener('turbolinks:request-start', this.onCloseClick);
@@ -64,14 +62,14 @@ Spree.ready(function($) {
 
   MobileNavigationManager.prototype.open = function() {
     this.navigationOpen = true;
-    this.addMobileNavigationClasses(['shown', 'mobile-navigation-overflow'])
+    this.mobileNavigation.classList.add('shown','mobile-navigation-overflow');
     document.body.style.overflow = "hidden";
     this.overlay.classList.add('shown');
   }
 
   MobileNavigationManager.prototype.close = function() {
     this.navigationOpen = false;
-    this.removeMobileNavigationClasses(['shown', 'mobile-navigation-overflow'])
+    this.mobileNavigation.classList.remove('shown', 'mobile-navigation-overflow');
     document.body.style.overflow = "";
     this.overlay.classList.remove('shown');
   }
@@ -81,7 +79,7 @@ Spree.ready(function($) {
     var subList = document.querySelector('ul[data-category=' + category + ']');
     if (subList) {
       subList.classList.add('shown');
-      this.removeMobileNavigationClasses(['mobile-navigation-overflow'])
+      this.mobileNavigation.classList.remove('mobile-navigation-overflow');
       this.backButton.classList.add('shown');
     }
     return false;
@@ -92,11 +90,11 @@ Spree.ready(function($) {
     var subList = document.querySelector('ul[data-category=' + category + ']');
     if (subList) {
       subList.classList.remove('shown');
-      this.addMobileNavigationClasses(['mobile-navigation-overflow'])
+      this.mobileNavigation.classList.add('mobile-navigation-overflow');
     }
     if (this.openedCategories[this.openedCategories.length - 1] === 'main') {
       this.backButton.classList.remove('shown');
-      this.addMobileNavigationClasses(['mobile-navigation-overflow'])
+      this.mobileNavigation.classList.add('mobile-navigation-overflow');
     }
     return false;
   }
@@ -105,7 +103,7 @@ Spree.ready(function($) {
     var subList = document.querySelector('ul[data-category=' + category + ']');
     subList.style.transition = 'none';
     subList.classList.remove('shown');
-    this.addMobileNavigationClasses(['mobile-navigation-overflow'])
+    this.mobileNavigation.classList.add('mobile-navigation-overflow');
     setTimeout(function(){ subList.style.transition = ''; }, 500);
   }
 
@@ -117,18 +115,6 @@ Spree.ready(function($) {
       this.closeCategory(category);
     }
     this.backButton.classList.remove('shown');
-  }
-
-  MobileNavigationManager.prototype.addMobileNavigationClasses = function(classList) {
-    $.each(classList, function(index, className) {
-      this.mobileNavigation.classList.add(className);
-    })
-  }
-
-  MobileNavigationManager.prototype.removeMobileNavigationclasses = function(category) {
-    $.each(classList, function(index, className) {
-      this.mobileNavigation.classList.remove(className);
-    })
   }
 
   new MobileNavigationManager();
