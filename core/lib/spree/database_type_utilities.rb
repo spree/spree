90cc4878975491d620523e560203614c7872ce9d
Nathaniel Nande <nathaniel.nande@sparksolutions.co>
2020-04-03 17:12:51 +0200

    Fixed RuboCop offenses

--- core/lib/spree/database_type_utilities.rb
+++ core/lib/spree/database_type_utilities.rb
@@ -1,11 +1,11 @@
 module Spree
-  module  DatabaseTypeUtilities
+  module DatabaseTypeUtilities
     def self.maximum_value_for(data_type)
       case data_type
       when :integer
         ActiveModel::Type::Integer.new.instance_eval { range.max }
       else
-        raise ArgumentError.new('Currently only :integer argument is acceptable')
+        raise ArgumentError, 'Currently only :integer argument is acceptable'
       end
     end
   end
