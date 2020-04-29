ad45cd08a1e381a37b6b3463d4b0ca40a99b4ba4
ordumonde <sacha@ordumonde.com>
2020-04-29 17:51:15 +0200

    Issue-9231 correction of wrong condition

--- core/app/models/spree/order_updater.rb
+++ core/app/models/spree/order_updater.rb
@@ -93,7 +93,7 @@ module Spree
     end
 
     def prorata_corrected_tax_amount
-      return 1 unless line_items.sum(:included_tax_total) + line_items.sum(:additional_tax_total) > 0 && adjustments.eligible.sum(:amount) > 0
+      return 1 unless line_items.sum(:included_tax_total) + line_items.sum(:additional_tax_total) > 0 && adjustments.eligible.sum(:amount) != 0
 
       prorata_adjustments = line_items.sum do |item|
         item_tax = item.additional_tax_total + item.included_tax_total
