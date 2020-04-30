90ea815b1737d8a3d9a282ce9275741b4e115d28
Viacheslav Mayorov <mayorovvn@gmail.com>
2020-04-30 10:44:11 +0300

    fix bug: product cache key contains promotion class name and its address in memory (#10126)

--- core/app/helpers/spree/products_helper.rb
+++ core/app/helpers/spree/products_helper.rb
@@ -67,7 +67,7 @@ module Spree
       cache_key_elements = common_product_cache_keys
       cache_key_elements += [
         product.cache_key_with_version,
-        product.possible_promotions
+        product.possible_promotions.map(&:cache_key)
       ]
 
       cache_key_elements.compact.join('/')
