cd9624f1e0397c6c8cd0c746e255ac84855cc94b
Serguei Okladnikov <oklaspec@gmail.com>
2020-05-01 18:20:33 +0000

    Enable product filtering within multiple `taxons` #10132

--- core/app/models/spree/product.rb
+++ core/app/models/spree/product.rb
@@ -110,7 +110,7 @@ module Spree
 
     alias options product_option_types
 
-    self.whitelisted_ransackable_associations = %w[stores variants_including_master master variants]
+    self.whitelisted_ransackable_associations = %w[taxons stores variants_including_master master variants]
     self.whitelisted_ransackable_attributes = %w[description name slug discontinue_on]
     self.whitelisted_ransackable_scopes = %w[not_discontinued]
 
