spree/spree

commit 7dad5d4c60d8e31878d56bd5632a11a85dc3a3ec

Display products from requested category and its descendants on category page

core/app/finders/spree/products/find.rb
@@ -8,7 +8,7 @@ module Spree
         @skus             = String(params.dig(:filter, :skus)).split(',')
         @price            = String(params.dig(:filter, :price)).split(',').map(&:to_f)
         @currency         = params[:currency] || current_currency
-        @taxons           = String(params.dig(:filter, :taxons)).split(',')
+        @taxons           = taxon_ids(params.dig(:filter, :taxons))
         @name             = params.dig(:filter, :name)
         @options          = params.dig(:filter, :options).try(:to_unsafe_hash)
         @option_value_ids = params.dig(:filter, :option_value_ids)
@@ -158,6 +158,12 @@ module Spree
 
       def include_discontinued(products)
         discontinued ? products : products.available
+      end
+
+      def taxon_ids(taxon_id)
+        return unless (taxon = Spree::Taxon.find_by(id: taxon_id))
+
+        taxon.self_and_descendants.ids.map(&:to_s)
       end
     end
   end

core/spec/finders/spree/products/find_spec.rb
@@ -9,7 +9,7 @@ module Spree
     let!(:deleted_product)      { create(:product, deleted_at: Time.current - 1.day) }
     let!(:discontinued_product) { create(:product, discontinue_on: Time.current - 1.day) }
 
-    context 'include deleted' do
+    context 'include discontinued' do
       it 'returns products with discontinued' do
         params = {
           filter: {
@@ -137,6 +137,38 @@ module Spree
         it 'returns products that have both options' do
           expect(products).to match_array([product_3])
         end
+      end
+    end
+
+    describe 'filter by taxons' do
+      subject(:products) do
+        described_class.new(
+            scope: Spree::Product.all,
+            params: params,
+            current_currency: 'USD'
+        ).execute
+      end
+
+      let(:params) { { filter: { taxons: parent_taxon.id } } }
+
+      let(:parent_taxon) { child_taxon.parent }
+      let(:child_taxon) { create(:taxon) }
+
+      shared_examples 'returns distinct products associated both to self and descendants' do
+        it { expect(products).to match_array [product, product_2] }
+      end
+
+      before do
+        parent_taxon.products << product
+        child_taxon.products << product_2
+      end
+
+      it_behaves_like 'returns distinct products associated both to self and descendants'
+
+      context 'when product is already related to both taxons' do
+        before { parent_taxon.products << product_2 }
+
+        it_behaves_like 'returns distinct products associated both to self and descendants'
       end
     end
 

commit d72561d8c8e9744b2a9a8334e281ec40bc65e649

Fixes requesting products from multiple taxon in API v2
This was broken by https://github.com/spree/spree/commit/7dad5d4c60d8e31878d56bd5632a11a85dc3a3ec

 Please enter the commit message for your changes. Lines starting

core/app/finders/spree/products/find.rb
@@ -172,10 +172,11 @@ module Spree
         discontinued ? products : products.available
       end
 
-      def taxon_ids(taxon_id)
-        return unless (taxon = Spree::Taxon.find_by(id: taxon_id))
+      def taxon_ids(taxons_ids)
+        return if taxons_ids.nil? || taxons_ids.to_s.blank?
 
-        taxon.cached_self_and_descendants_ids.map(&:to_s)
+        taxons = Spree::Taxon.where(id: taxons_ids.to_s.split(','))
+        taxons.map(&:cached_self_and_descendants_ids).flatten.compact.uniq.map(&:to_s)
       end
     end
   end

core/spec/finders/spree/products/find_spec.rb
@@ -149,26 +149,41 @@ module Spree
         ).execute
       end
 
-      let(:params) { { filter: { taxons: parent_taxon.id } } }
-
       let(:parent_taxon) { child_taxon.parent }
       let(:child_taxon) { create(:taxon) }
 
-      shared_examples 'returns distinct products associated both to self and descendants' do
-        it { expect(products).to match_array [product, product_2] }
-      end
+      context 'one taxon is requested in params' do
+        let(:params) { { filter: { taxons: parent_taxon.id } } }
 
-      before do
-        parent_taxon.products << product
-        child_taxon.products << product_2
-      end
+        shared_examples 'returns distinct products associated both to self and descendants' do
+          it { expect(products).to match_array [product, product_2] }
+        end
 
-      it_behaves_like 'returns distinct products associated both to self and descendants'
-
-      context 'when product is already related to both taxons' do
-        before { parent_taxon.products << product_2 }
+        before do
+          parent_taxon.products << product
+          child_taxon.products << product_2
+        end
 
         it_behaves_like 'returns distinct products associated both to self and descendants'
+
+        context 'when product is already related to both taxons' do
+          before { parent_taxon.products << product_2 }
+
+          it_behaves_like 'returns distinct products associated both to self and descendants'
+        end
+      end
+
+      context 'multiple taxons are requested' do
+        let(:params) { { filter: { taxons: "#{taxon.id},#{taxon_2.id}" } } }
+        let(:taxon) { create(:taxon) }
+        let(:taxon_2) { create(:taxon) }
+
+        before do
+          taxon.products << product
+          taxon_2.products << product_2
+        end
+
+        it { expect(products).to match_array [product, product_2] }
       end
     end
 