0c860ca03b736958a550a2b4f9bb412fee3f21a0
Robert Nowakowski <aplegatt@gmail.com>
2020-05-08 15:25:34 +0200

    Updates migrations with column_exists checks

--- core/db/migrate/20200421095017_add_compare_at_amount_to_spree_prices.rb
+++ core/db/migrate/20200421095017_add_compare_at_amount_to_spree_prices.rb
@@ -1,5 +1,7 @@
 class AddCompareAtAmountToSpreePrices < ActiveRecord::Migration[6.0]
   def change
-    add_column :spree_prices, :compare_at_amount, :decimal, precision: 10, scale: 2
+    unless column_exists?(:spree_prices, :compare_at_amount)
+      add_column :spree_prices, :compare_at_amount, :decimal, precision: 10, scale: 2
+    end
   end
 end
