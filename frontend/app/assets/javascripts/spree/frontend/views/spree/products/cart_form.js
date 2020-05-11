66115b7c7190e9b7705121f2afe0bcd371c3cd82
Matthew Kennedy <m.kennedy@me.com>
2020-05-11 14:39:01 +0100

    Move variant selcect functions into new file.

--- frontend/app/assets/javascripts/spree/frontend/views/spree/products/cart_form.js
+++ frontend/app/assets/javascripts/spree/frontend/views/spree/products/cart_form.js
@@ -1,5 +1,6 @@
 //= require spree/api/storefront/cart
 //= require ../shared/product_added_modal
+//= require ../shared/variant_select
 
 var OPTIONS_CONTAINER = 'ul#product-variants'
 var ADD_TO_CART_FORM_SELECTOR = '.add-to-cart-form'
@@ -13,10 +14,6 @@ var AVAILABILITY_TEMPLATES = {
   backorderable: '.availability-template-backorderable',
   outOfStock: '.availability-template-out-of-stock'
 }
-
-let getQueryString = window.location.search;
-let urlParams = new URLSearchParams(getQueryString);
-let variantIdFromUrl = urlParams.get('variant')
 
 function CartForm($, $cartForm) {
   this.constructor = function() {
@@ -35,85 +32,22 @@ function CartForm($, $cartForm) {
     this.$compareAtPrice = $cartForm.find('.compare-at-price')
     this.$variantIdInput = $cartForm.find(VARIANT_ID_SELECTOR)
 
-    this.initializeForm()
+    if (urlParams.has('variant')) {
+      this.setSelectedVariantFromUrl()
+    } else {
+      this.initializeForm()
+    }
+
   }
 
   this.initializeForm = function() {
-    if (urlParams.has('variant')) {
-      this.getVariantOptionsById(variantIdFromUrl)
-      this.selectedOptions.sort((a, b) => (a.dataset.optionTypeIndex > b.dataset.optionTypeIndex) ? 1 : -1)
-      this.clickListOptions(this.selectedOptions)
-      this.updateStructuredData()
+    if (this.withOptionValues) {
+      var $optionValue = this.firstCheckedOptionValue()
+      this.applyCheckedOptionValue($optionValue, true)
     } else {
-      if (this.withOptionValues) {
-        var $optionValue = this.firstCheckedOptionValue()
-        this.applyCheckedOptionValue($optionValue, true)
-      } else {
-        this.updateAddToCart()
-        this.triggerVariantImages()
-      }
+      this.updateAddToCart()
+      this.triggerVariantImages()
     }
-  }
-
-  this.getVariantOptionsById = function(variantIdFromUrl) {
-    for (const v of this.variants) {
-      if (parseInt(v.id) === parseInt(variantIdFromUrl)) {
-        this.sortOptionValues(v.option_values);
-      }
-    }
-  }
-
-  this.sortOptionValues = function(optVals) {
-    const container = document.querySelector(OPTIONS_CONTAINER);
-    const target = container.querySelectorAll(OPTION_VALUE_SELECTOR);
-    optVals.forEach(buidArray)
-
-    function buidArray(item, index) {
-      for (const t of target) {
-        if (parseInt(t.value) === item.id && t.dataset.presentation === item.presentation) {
-          this.selectedOptions.push(t)
-        }
-      }
-    }
-  }
-
-  this.clickListOptions = function(list) {
-    list.forEach(selectOpts)
-
-    function selectOpts(item, index) {
-      item.click()
-      var $t = $(item)
-      this.applyCheckedOptionValue($t)
-    }
-  }
-
-  this.updateStructuredData = function() {
-    const variant = this.selectedVariant()
-    const host = window.location.host;
-    const script = document.getElementById('productStructuredData');
-    const obj = JSON.parse(script.firstChild.nodeValue);
-    const firstLayer = obj[0]
-    const offers = obj[0].offers
-
-    if (variant.purchasable) {
-      offers.availability = 'InStock'
-    } else {
-      offers.availability = 'OutOfStock'
-    }
-
-    if (variant.sku.length > 1) {
-      firstLayer.sku = variant.sku
-    }
-
-    firstLayer.url = window.location.href
-    offers.url = window.location.href
-    offers.price = variant.display_price
-
-    if (Array.isArray(variant.images) && (variant.images.length)) {
-      firstLayer.image = host + variant.images[0].url_product
-    }
-
-    script.firstChild.nodeValue = JSON.stringify(obj);
   }
 
   this.bindEventHandlers = function() {
---
+++ frontend/app/assets/javascripts/spree/frontend/views/spree/shared/variant_select.js
@@ -0,0 +1,71 @@
+let getQueryString = window.location.search;
+let urlParams = new URLSearchParams(getQueryString);
+let variantIdFromUrl = urlParams.get('variant')
+
+this.getVariantOptionsById = function(variantIdFromUrl) {
+  for (const v of this.variants) {
+    if (parseInt(v.id) === parseInt(variantIdFromUrl)) {
+      this.sortOptionValues(v.option_values);
+    }
+  }
+}
+
+this.setSelectedVariantFromUrl = function () {
+  this.getVariantOptionsById(variantIdFromUrl)
+  this.selectedOptions.sort((a, b) => (a.dataset.optionTypeIndex > b.dataset.optionTypeIndex) ? 1 : -1)
+  this.clickListOptions(this.selectedOptions)
+  this.updateStructuredData()
+}
+
+this.sortOptionValues = function(optVals) {
+  const container = document.querySelector(OPTIONS_CONTAINER);
+  const target = container.querySelectorAll(OPTION_VALUE_SELECTOR);
+  optVals.forEach(buidArray)
+
+  function buidArray(item, index) {
+    for (const t of target) {
+      if (parseInt(t.value) === item.id && t.dataset.presentation === item.presentation) {
+        this.selectedOptions.push(t)
+      }
+    }
+  }
+}
+
+this.clickListOptions = function(list) {
+  list.forEach(selectOpts)
+
+  function selectOpts(item, index) {
+    item.click()
+    var $t = $(item)
+    this.applyCheckedOptionValue($t)
+  }
+}
+
+this.updateStructuredData = function() {
+  const variant = this.selectedVariant()
+  const host = window.location.host;
+  const script = document.getElementById('productStructuredData');
+  const obj = JSON.parse(script.firstChild.nodeValue);
+  const firstLayer = obj[0]
+  const offers = obj[0].offers
+
+  if (variant.purchasable) {
+    offers.availability = 'InStock'
+  } else {
+    offers.availability = 'OutOfStock'
+  }
+
+  if (variant.sku.length > 1) {
+    firstLayer.sku = variant.sku
+  }
+
+  firstLayer.url = window.location.href
+  offers.url = window.location.href
+  offers.price = variant.display_price
+
+  if (Array.isArray(variant.images) && (variant.images.length)) {
+    firstLayer.image = host + variant.images[0].url_product
+  }
+
+  script.firstChild.nodeValue = JSON.stringify(obj);
+}
