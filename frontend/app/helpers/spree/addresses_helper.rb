1b329a6707a4d6d8f7592bfc23ae1c69d2e1d903
Matthew Kennedy <m.kennedy@me.com>
2020-06-03 18:21:02 +0100

    Fix CC errors

--- frontend/app/helpers/spree/addresses_helper.rb
+++ frontend/app/helpers/spree/addresses_helper.rb
@@ -7,7 +7,7 @@ module Spree
           yield
         else
           is_required = Spree::Address.required_fields.include?(method)
-          method_name = I18n.t("activerecord.attributes.spree/address.#{method}").to_s
+          method_name = I18n.t("activerecord.attributes.spree/address.#{method}")
           required = Spree.t(:required)
           form.text_field(method,
                           class: [is_required ? 'required' : nil, 'spree-flat-input'].compact,
@@ -29,18 +29,18 @@ module Spree
                                class: have_states ? 'required form-control spree-flat-select' : 'hidden',
                                aria: { label: Spree.t(:state) },
                                disabled: !have_states) +
-
-          form.label(Spree.t(:state).downcase, Spree.t(:state).upcase, class: have_states ? 'select-only-label text-uppercase' : 'select-only-label hidden') +
-          form.text_field(:state_name,
-                          class: !have_states ? 'required spree-flat-input' : 'hidden spree-flat-input',
-                          disabled: have_states,
-                          placeholder: Spree.t(:state)) +
           form.label(Spree.t(:state).downcase,
                      Spree.t(:state).upcase,
-                     class: !have_states ? 'text-field-only-label text-uppercase' : 'text-field-only-label hidden') +
+                     class: have_states ? 'select-only-label text-uppercase' : 'select-only-label hidden') +
+            form.text_field(:state_name,
+                            class: !have_states ? 'required spree-flat-input' : 'hidden spree-flat-input',
+                            disabled: have_states,
+                            placeholder: Spree.t(:state)) +
+              form.label(Spree.t(:state).downcase,
+                         Spree.t(:state).upcase,
+                         class: !have_states ? 'text-field-only-label text-uppercase' : 'text-field-only-label hidden') +
         image_tag('arrow.svg', class: !have_states ? 'hidden position-absolute spree-flat-select-arrow' : 'position-absolute spree-flat-select-arrow')
       ].join.tr('"', "'").delete("\n")
-
           content_tag(:noscript, form.text_field(:state_name, class: 'required')) +
           javascript_tag("document.write(\"<span class='d-block position-relative'>#{state_elements.html_safe}</span>\");")
     end
