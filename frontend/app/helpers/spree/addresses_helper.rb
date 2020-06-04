d0b4c304c2838c85493f5ad3568dc76dd8eae106
Matthew Kennedy <m.kennedy@me.com>
2020-06-04 22:26:21 +0100

    All CSS Slide in Form labels on Address Fields. (#10227)
    * Form labels
    
    * Update further plus bug fixes
    
    * Code Climate indentation
    
    * Fix CC errors
    
    * CC Fixes
    
    * CC
    
    * CC
    
    * Fix issues with Code Climate
    
    * CSS/JS for state label, not 2 labels for one form el.
    
    * Fix missing required or not required maker on State
    
    * Use single quotes for CC
    
    * Spelling fix
    
    * Zipcode needs a unique require status set from boolean in DB.
    
    * Javascript updates label and placeholder for zipcode.
    
    * Improve css, fix CC issues.
    
    * Allow for translations in zipcode placeholder and label.
    
    * Fix
    
    * Fix problem with setting upcase.
    
    * Neaten up code. Fix missing required class on zipcode.
    
    * Make select exact same height as input
    
    * Tidy up further, and use notation of required for translation.
    
    * Never used.
    
    * Update frontend/app/helpers/spree/addresses_helper.rb
    
    Co-authored-by: Damian Legawiec <damian@sparksolutions.co>

--- frontend/app/helpers/spree/addresses_helper.rb
+++ frontend/app/helpers/spree/addresses_helper.rb
@@ -2,7 +2,7 @@
 module Spree
   module AddressesHelper
     def address_field(form, method, address_id = 'b', &handler)
-      content_tag :p, id: [address_id, method].join, class: 'form-group checkout-content-inner-field' do
+      content_tag :div, id: [address_id, method].join, class: 'form-group checkout-content-inner-field has-float-label' do
         if handler
           yield
         else
@@ -13,9 +13,27 @@ module Spree
                           class: [is_required ? 'required' : nil, 'spree-flat-input'].compact,
                           required: is_required,
                           placeholder: is_required ? "#{method_name} #{required}" : method_name,
-                          aria: { label: method_name })
+                          aria: { label: method_name }) +
+            form.label(method_name,
+                       is_required ? "#{method_name} #{required}" : method_name,
+                       class: 'text-uppercase')
         end
       end
+    end
+
+    def address_zipcode(form, country, _address_id = 'b')
+      country ||= Spree::Country.default
+      is_required = country.zipcode_required?
+      method_name = Spree.t(:zipcode)
+      required = Spree.t(:required)
+      form.text_field(:zipcode,
+                      class: [is_required ? 'required' : nil, 'spree-flat-input'].compact,
+                      required: is_required,
+                      placeholder: is_required ? "#{method_name} #{required}" : method_name,
+                      aria: { label: Spree.t(:zipcode) }) +
+        form.label(:zipcode,
+                   is_required ? "#{method_name} #{required}" : method_name,
+                   class: 'text-uppercase')
     end
 
     def address_state(form, country, _address_id = 'b')
@@ -24,12 +42,20 @@ module Spree
       state_elements = [
         form.collection_select(:state_id, country.states.order(:name),
                               :id, :name,
-                               { prompt: Spree.t(:state).upcase },
-                               class: have_states ? 'required form-control spree-flat-select' : 'hidden',
+                               { prompt: Spree.t(:state) },
+                               class: [have_states ? 'required' : 'hidden', 'spree-flat-select'].compact,
                                aria: { label: Spree.t(:state) },
                                disabled: !have_states) +
-          form.text_field(:state_name, class: !have_states ? 'required' : 'hidden', disabled: have_states) +
-          image_tag('arrow.svg', class: 'position-absolute spree-flat-select-arrow')
+          form.text_field(:state_name,
+                          class: [!have_states ? 'required' : 'hidden', 'spree-flat-input'].compact,
+                          aria: { label: Spree.t(:state) },
+                          disabled: have_states,
+                          placeholder: Spree.t(:state) + " #{Spree.t(:required)}") +
+          form.label(Spree.t(:state).downcase,
+                     raw(Spree.t(:state) + content_tag(:abbr, " #{Spree.t(:required)}")),
+                     class: [have_states ? 'state-select-label' : nil, ' text-uppercase'].compact) +
+          image_tag('arrow.svg',
+                    class: [!have_states ? 'hidden' : nil, 'position-absolute spree-flat-select-arrow'].compact)
       ].join.tr('"', "'").delete("\n")
 
       content_tag(:noscript, form.text_field(:state_name, class: 'required')) +
