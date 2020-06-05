560c62bb78cd22f62d50c2f3a4abe7c7b6c2eb2d
Krzysztof Kalisz <krzysztof.kalisz@sparksolutions.co>
2020-06-05 10:49:45 +0200

    Fix selection of sorting products

--- frontend/app/assets/javascripts/spree/frontend/views/spree/products/index.js
+++ frontend/app/assets/javascripts/spree/frontend/views/spree/products/index.js
@@ -30,12 +30,7 @@ Spree.ready(function($) {
   });
 
   $('.plp-overlay-ul-li').click(function() {
-    $('.plp-overlay-ul-li--active')
-      .removeClass('plp-overlay-ul-li--active')
-      .addClass('plp-overlay-ul-li');
-
-    $(this)
-      .removeClass('plp-overlay-ul-li')
-      .addClass('plp-overlay-ul-li--active');
+    $('.plp-overlay-ul-li').removeClass('plp-overlay-ul-li--active')
+    $(this).addClass('plp-overlay-ul-li--active');
   });
 });
