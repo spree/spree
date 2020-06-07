6c26b025d2fcbc22e87fbdf88d4f76d599937c59
Matthew Kennedy <m.kennedy@me.com>
2020-06-07 13:13:11 +0100

    Remove user_id from EXCLUDED_KEYS_FOR_COMPARISION.

--- core/app/models/spree/address.rb
+++ core/app/models/spree/address.rb
@@ -13,7 +13,7 @@ module Spree
     # we're not freezing this on purpose so developers can extend and manage
     # those attributes depending of the logic of their applications
     ADDRESS_FIELDS = %w(firstname lastname company address1 address2 city state zipcode country phone)
-    EXCLUDED_KEYS_FOR_COMPARISION = %w(id updated_at created_at deleted_at user_id)
+    EXCLUDED_KEYS_FOR_COMPARISION = %w(id updated_at created_at deleted_at)
 
     belongs_to :country, class_name: 'Spree::Country'
     belongs_to :state, class_name: 'Spree::State', optional: true
