c451a836b905bf76b4e19317edbe011597aa99e6
Derek Johnson <hello@imderek.com>
2020-06-16 17:30:42 -0400

    add_new_order_notifications_email_to_spree_stores migration timestamp reverted to current; validation only applies to model when col exists

--- core/app/models/spree/store.rb
+++ core/app/models/spree/store.rb
@@ -8,7 +8,9 @@ module Spree
       validates :name, :url, :mail_from_address, :default_currency
     end
 
-    validates :new_order_notifications_email, email: { allow_blank: true }
+    connection.column_exists?(:spree_stores, :new_order_notifications_email) do
+      validates :new_order_notifications_email, email: { allow_blank: true }
+    end
 
     before_save :ensure_default_exists_and_is_unique
     before_destroy :validate_not_default
