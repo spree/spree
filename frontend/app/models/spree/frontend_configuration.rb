7eeea2e7c1ead98f9dd7eb4425a591563e0f30eb
AleksandraKaminska <aleksandra-kaminska@outlook.com>
2020-06-23 12:05:40 +0200

    [SDP-357] Add additional filter partials

--- frontend/app/models/spree/frontend_configuration.rb
+++ frontend/app/models/spree/frontend_configuration.rb
@@ -3,6 +3,7 @@ module Spree
     preference :coupon_codes_enabled, :boolean, default: true # Determines if we show coupon code form at cart and checkout
     preference :locale, :string, default: Rails.application.config.i18n.default_locale
     preference :products_filters, :array, default: %w(keywords price sort_by)
+    preference :additional_filters_partials, :array, default: %w()
     preference :remember_me_enabled, :boolean, default: true
   end
 end
