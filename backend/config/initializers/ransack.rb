b4d0e8ea30367ac1196d5062bd72a56bf7e89b31
Matthew Kennedy <m.kennedy@me.com>
2020-06-28 00:35:45 +0100

    Correct arrows

--- backend/config/initializers/ransack.rb
+++ backend/config/initializers/ransack.rb
@@ -1,7 +1,7 @@
 Ransack.configure do |config|
   # Globally set the up arrow to an icon, and the down and default arrows to unicode.
   config.custom_arrows = {
-    down_arrow: '<svg width="100%" height="100%" viewBox="0 0 2134 2134" version="1.1" xmlns="http://www.w3.org/2000/svg"
+    up_arrow: '<svg width="100%" height="100%" viewBox="0 0 2134 2134" version="1.1" xmlns="http://www.w3.org/2000/svg"
                       xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/"
                       style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;">
                       <path fill="currentColor"
@@ -10,7 +10,7 @@ Ransack.configure do |config|
                             24.038 87.019,0c12.02,-12.019 18.029,-27.725 18.029,-43.51c0,-15.785 -6.009,-31.49 -18.029,
                             -43.509l-923.077,-923.077c-24.038,-24.039 -62.98,-24.039 -87.019,
                             0Z" style="fill-rule:nonzero;stroke:currentColor;stroke-width:75px;"/></svg>',
-    up_arrow: '<svg width="100%" height="100%" viewBox="0 0 2134 2134" version="1.1"
+    down_arrow: '<svg width="100%" height="100%" viewBox="0 0 2134 2134" version="1.1"
                     xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
                     xml:space="preserve" xmlns:serif="http://www.serif.com/"
                     style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;">
