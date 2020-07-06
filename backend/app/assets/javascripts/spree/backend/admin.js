17c46ae9b007e5806651954c57e346b6958794d7
Matthew Kennedy <m.kennedy@me.com>
2020-07-06 18:50:13 +0100

    Flatpickr translatable.

--- backend/app/assets/javascripts/spree/backend/admin.js
+++ backend/app/assets/javascripts/spree/backend/admin.js
@@ -179,11 +179,68 @@ $.fn.radioControlsVisibilityOfElement = function (dependentElementSelector) {
 }
 
 document.addEventListener("DOMContentLoaded", function(){
+  flatpickr.l10ns.default.firstDayOfWeek = Spree.translations.firstDayOfWeek,
+  flatpickr.l10ns.default.rangeSeparator = Spree.translations.rangeSeparator,
+  flatpickr.l10ns.default.weekAbbreviation = Spree.translations.weekAbbreviation,
+  flatpickr.l10ns.default.scrollTitle = Spree.translations.scrollTitle,
+  flatpickr.l10ns.default.toggleTitle = Spree.translations.toggleTitle,
+  flatpickr.l10ns.default.amPM = [Spree.translations.am, Spree.translations.pm],
+  flatpickr.l10ns.default.yearAriaLabel = Spree.translations.yearAriaLabel,
+  flatpickr.l10ns.default.hourAriaLabel = Spree.translations.hourAriaLabel,
+  flatpickr.l10ns.default.minuteAriaLabel = Spree.translations.minuteAriaLabel,
+  flatpickr.l10ns.default.weekdays = {
+      shorthand: [
+          Spree.translations.sun,
+          Spree.translations.mon,
+          Spree.translations.tue,
+          Spree.translations.wed,
+          Spree.translations.thu,
+          Spree.translations.fri,
+          Spree.translations.sat],
+      longhand: [
+          Spree.translations.lh_sun,
+          Spree.translations.lh_mon,
+          Spree.translations.lh_tue,
+          Spree.translations.lh_wed,
+          Spree.translations.lh_thu,
+          Spree.translations.lh_fri,
+          Spree.translations.lh_sat
+      ]
+  },
+  flatpickr.l10ns.default.months = {
+          shorthand: [
+              Spree.translations.jan,
+              Spree.translations.feb,
+              Spree.translations.mar,
+              Spree.translations.apr,
+              Spree.translations.may,
+              Spree.translations.jun,
+              Spree.translations.jul,
+              Spree.translations.aug,
+              Spree.translations.sep,
+              Spree.translations.oct,
+              Spree.translations.nov,
+              Spree.translations.dec,
+          ],
+          longhand: [
+              Spree.translations.lh_jan,
+              Spree.translations.lh_feb,
+              Spree.translations.lh_mar,
+              Spree.translations.lh_apr,
+              Spree.translations.lh_may,
+              Spree.translations.lh_jun,
+              Spree.translations.lh_jul,
+              Spree.translations.lh_aug,
+              Spree.translations.lh_sep,
+              Spree.translations.lh_oct,
+              Spree.translations.lh_nov,
+              Spree.translations.lh_dec,
+          ]
+  },
 
   flatpickr('.datepickr',{
     wrap: true,
     disableMobile: true,
-    locale: Spree.translations.flatpickr_local,
     dateFormat: Spree.translations.date_picker,
     monthSelectorType: 'static',
     ariaDateFormat: Spree.translations.date_picker,
@@ -192,7 +249,6 @@ document.addEventListener("DOMContentLoaded", function(){
   dateTo = flatpickr('.datepickrTo',{
     wrap: true,
     disableMobile: true,
-    locale: Spree.translations.flatpickr_local,
     dateFormat: Spree.translations.date_picker,
     monthSelectorType: 'static',
     ariaDateFormat: Spree.translations.date_picker,
@@ -202,7 +258,6 @@ document.addEventListener("DOMContentLoaded", function(){
     wrap: true,
     disableMobile: true,
     monthSelectorType: 'static',
-    locale: Spree.translations.flatpickr_local,
     dateFormat: Spree.translations.date_picker,
     ariaDateFormat: Spree.translations.date_picker,
     onReady:function(selectedDates){
@@ -212,7 +267,6 @@ document.addEventListener("DOMContentLoaded", function(){
 			 dateTo.set('minDate', selectedDates[0])
 		},
   })
-
 })
 
 
