ec6514350cdbca82a4dcdf1ef51a901eb348dcde
Matthew Kennedy <m.kennedy@me.com>
2020-07-20 13:00:46 +0100

    Fix z-index on Select2 caused by overlay mask issues

--- backend/app/assets/javascripts/spree/backend/spree-select2.js
+++ backend/app/assets/javascripts/spree/backend/spree-select2.js
@@ -1,7 +1,26 @@
-jQuery(function ($) {
-  // Make select beautiful
+document.addEventListener('DOMContentLoaded', function() {
+  // Custom Select2
+
+  // Fix for Select2 v3.x using overlay mask to close any opened Select2 instances
+  // The overlay mask stops you lowering the z-index of the opend Select2 instance
+  // This is not needed if upgraded to Select2 v4.x
+  window.addEventListener('click', function(e) {
+    var select2Mask = document.getElementById('select2-drop-mask')
+    var select2Drop = document.getElementById('select2-drop')
+
+    if (select2Drop) {
+      if (select2Drop.contains(e.target)) {
+        // Click in the Select2 dropdown and do nothing...
+      } else {
+        if (select2Mask) {
+          $(select2Mask).click()
+        }
+      }
+    }
+  })
+
+  // Inititate Select2 on any select element with the class .select2
   $('select.select2').select2({
-    allowClear: true,
-    dropdownAutoWidth: false
+    allowClear: true
   })
 })
