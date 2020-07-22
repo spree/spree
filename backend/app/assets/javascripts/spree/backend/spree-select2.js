6c54748808f6b0cf0e7f968a645b739bdbc73ee2
Matthew Kennedy <m.kennedy@me.com>
2020-07-22 08:04:44 +0100

    Improve js for Select2 z-index fix

--- backend/app/assets/javascripts/spree/backend/spree-select2.js
+++ backend/app/assets/javascripts/spree/backend/spree-select2.js
@@ -1,23 +1,22 @@
 document.addEventListener('DOMContentLoaded', function() {
-  // Custom Select2
 
-  // Fix for Select2 v3.x using overlay mask to close any opened Select2 instances
-  // The overlay mask stops you lowering the z-index of the opend Select2 instance
-  // This is not needed if upgraded to Select2 v4.x
+  // Inititate Select2 on any select element with the class .select2
+  $('select.select2').select2({
+    allowClear: true
+  })
+
+  // BELOW: z-index fix for Select2 v3.x to lower the z-index of an opened Select2.
+  // The javascript below is not needed if Spree is updated to use Select2 v4.x
   window.addEventListener('click', function(e) {
     var select2Drop = document.getElementById('select2-drop')
 
     if (select2Drop) {
       if (select2Drop.contains(e.target)) {
-        // Click in the Select2 dropdown and do nothing...
+        // Clicking inside the Select2 dropdown does nothing...
       } else {
+        // Clicking outside the Select2 dropdown close all open Select2 dropdowns.
         $('*').select2('close')
       }
     }
-  })
-
-  // Inititate Select2 on any select element with the class .select2
-  $('select.select2').select2({
-    allowClear: true
   })
 })
