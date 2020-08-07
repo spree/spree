347b0179e76c97c9b4f6802426d3a76d9904ebf7
Robert Nowakowski <aplegatt@gmail.com>
2020-08-07 14:57:41 +0200

    Adds specs for currency settings

--- backend/spec/features/admin/general_settings/prices_settings_updating_spec.rb
+++ backend/spec/features/admin/general_settings/prices_settings_updating_spec.rb
@@ -4,28 +4,54 @@ describe 'Updating currencies settings', type: :feature, js: true do
   let!(:store) { create(:store, default: true) }
   stub_authorization!
 
-  before do
-    reset_spree_preferences do |config|
-      config.allow_currency_change = false
-      config.show_currency_selector = false
+  describe 'enabling currency settings' do
+    before do
+      reset_spree_preferences do |config|
+        config.allow_currency_change = false
+        config.show_currency_selector = false
+      end
+    end
+
+    it 'allows to enable currency settings' do
+      visit spree.edit_admin_general_settings_path
+
+      # Test initial state
+      expect(page).to have_unchecked_field('allow_currency_change')
+      expect(page).to have_unchecked_field('show_currency_selector')
+
+      # Interact with the form
+      check('allow_currency_change')
+      check('show_currency_selector')
+      click_button 'Update'
+
+      # Test final state
+      expect(page).to have_content 'General Settings has been successfully updated!'
+      expect(page).to have_checked_field('allow_currency_change')
+      expect(page).to have_checked_field('show_currency_selector')
     end
   end
 
-  it 'allows to update supported currencies' do
-    visit spree.edit_admin_general_settings_path
+  describe 'disabling currency settings' do
+    before do
+      reset_spree_preferences do |config|
+        config.allow_currency_change = true
+        config.show_currency_selector = true
+      end
+    end
 
-    # Test initial state
-    expect(page).to have_unchecked_field('allow_currency_change')
-    expect(page).to have_unchecked_field('show_currency_selector')
+    it 'allows to disable currency settings' do
+      visit spree.edit_admin_general_settings_path
 
-    # Interact with the form
-    check('allow_currency_change')
-    check('show_currency_selector')
-    click_button 'Update'
+      expect(page).to have_checked_field('allow_currency_change')
+      expect(page).to have_checked_field('show_currency_selector')
 
-    # Test final state
-    expect(page).to have_content 'General Settings has been successfully updated!'
-    expect(page).to have_checked_field('allow_currency_change')
-    expect(page).to have_checked_field('show_currency_selector')
+      uncheck('allow_currency_change')
+      uncheck('show_currency_selector')
+      click_button 'Update'
+
+      expect(page).to have_content 'General Settings has been successfully updated!'
+      expect(page).to have_unchecked_field('allow_currency_change')
+      expect(page).to have_unchecked_field('show_currency_selector')
+    end
   end
 end
