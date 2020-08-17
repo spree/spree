0fadab6212b6b1f5f8e19abb88ed4846a4b01d0b
Krzysztof Kalisz <krzysztof.kalisz@sparksolutions.co>
2020-08-17 15:27:57 +0200

    SD-892 Fix CC issues

--- frontend/app/helpers/spree/orders_helper.rb
+++ frontend/app/helpers/spree/orders_helper.rb
@@ -3,11 +3,12 @@ module Spree
     def order_just_completed?(order)
       flash[:order_completed] && order.present?
     end
+
     def order_has_free_shipping?(order)
-      order.promotions
-          .joins(:promotion_actions)
-          .where(spree_promotion_actions: { type: 'Spree::Promotion::Actions::FreeShipping'})
-          .exists?
+      order.promotions.
+        joins(:promotion_actions).
+        where(spree_promotion_actions: { type: 'Spree::Promotion::Actions::FreeShipping' }).
+        exists?
     end
   end
 end
