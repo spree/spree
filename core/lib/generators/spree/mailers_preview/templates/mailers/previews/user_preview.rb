8079d195194e950e4f2a158e4d9f3f4b7ec64351
AleksandraKaminska <aleksandra-kaminska@outlook.com>
2020-08-25 15:30:48 +0200

    Add user_mailers previews

---
+++ core/lib/generators/spree/mailers_preview/templates/mailers/previews/user_preview.rb
@@ -0,0 +1,11 @@
+class UserPreview < ActionMailer::Preview
+  def reset_password_instructions
+    Spree::UserMailer.reset_password_instructions(Spree::User.first, 'your_token', { current_store_id: Spree::Store.first.id })
+  end
+
+  if Spree::Auth::Config[:confirmable]
+    def confirmation_instructions
+      Spree::UserMailer.confirmation_instructions(Spree::User.first, 'your_token')
+    end
+  end
+end
