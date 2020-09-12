6c238ba6fb69395423954930a6b293a6396f0664
Matthew Kennedy <m.kennedy@me.com>
2020-09-12 15:07:29 +0100

    Update how states import.

--- core/db/default/spree/countries.rb
+++ core/db/default/spree/countries.rb
@@ -6,13 +6,14 @@ Carmen::Country.all.each do |country|
     iso3: country.alpha_3_code,
     iso: country.alpha_2_code,
     iso_name: country.name.upcase,
-    numcode: country.numeric_code,
-    states_required: country.subregions?
+    numcode: country.numeric_code
   ).first_or_create
 end
 
 Spree::Config[:default_country_id] = Spree::Country.find_by(iso: 'US').id
 
-# find countries that do not use postal codes (by iso) and set 'zipcode_required' to false for them.
-
+# Find countries that do not use postal codes (by iso) and set 'zipcode_required' to false for them.
 Spree::Country.where(iso: Spree::Address::NO_ZIPCODE_ISO_CODES).update_all(zipcode_required: false)
+
+# Find all countries that require a state (province) at checkout and set 'states_required' to true.
+Spree::Country.where(iso: Spree::Address::STATES_REQUIRED).update_all(states_required: true)
--- core/db/default/spree/states.rb
+++ core/db/default/spree/states.rb
@@ -3,10 +3,20 @@ Spree::Country.where(states_required: true).each do |country|
   next unless carmen_country
 
   carmen_country.subregions.each do |subregion|
-    country.states.where(
-      name: subregion.name,
-      abbr: subregion.code,
-      country_id: country.id
-    ).first_or_create
+    if subregion.subregions?
+      subregion.subregions.each do |province|
+        country.states.where(
+          name: province.name,
+          abbr: province.code,
+          country_id: country.id
+        ).first_or_create
+      end
+    else
+      country.states.where(
+        name: subregion.name,
+        abbr: subregion.code,
+        country_id: country.id
+      ).first_or_create
+    end
   end
 end
