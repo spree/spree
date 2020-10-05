a753c2a1680f0d47de116f0a4ef65437ba82034b
Damian Legawiec <damian@sparksolutions.co>
2020-10-05 16:10:39 +0200

    Expose Order#pre_tax_total and Order#display_pre_tax_total in Cart API v2

--- api/app/serializers/spree/v2/storefront/cart_serializer.rb
+++ api/app/serializers/spree/v2/storefront/cart_serializer.rb
@@ -9,7 +9,7 @@ module Spree
                    :display_included_tax_total, :tax_total, :currency, :state, :token, :email,
                    :display_item_total, :display_ship_total, :display_adjustment_total, :display_tax_total,
                    :promo_total, :display_promo_total, :item_count, :special_instructions, :display_total,
-                   :pre_tax_item_amount, :display_pre_tax_item_amount
+                   :pre_tax_item_amount, :display_pre_tax_item_amount, :pre_tax_total, :display_pre_tax_total
 
         has_many   :line_items
         has_many   :variants
