711265b6a541fadeaec676425be3ab3d013163d7
KacperMekarski <kacper.mekarski@sparksolutions.co>
2020-10-06 13:55:21 +0200

    SD-949 rename r_m_a to its fullname return_merchandise_authorization

--- core/app/models/spree/return_item/eligibility_validator/return_merchandise_authorization_required.rb
+++ core/app/models/spree/return_item/eligibility_validator/return_merchandise_authorization_required.rb
@@ -1,5 +1,5 @@
 module Spree
-  class ReturnItem::EligibilityValidator::RMARequired < Spree::ReturnItem::EligibilityValidator::BaseValidator
+  class ReturnItem::EligibilityValidator::ReturnMerchandiseAuthorizationRequired < Spree::ReturnItem::EligibilityValidator::BaseValidator
     def eligible_for_return?
       if @return_item.return_authorization.present?
         true
