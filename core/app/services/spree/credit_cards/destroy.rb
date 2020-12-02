3e3587c773eb32a7458beeeb54bfba102ad17af3
Damian Legawiec <damian@sparksolutions.co>
2020-12-02 12:55:29 +0100

    Fix CC destroy service

--- core/app/services/spree/credit_cards/destroy.rb
+++ core/app/services/spree/credit_cards/destroy.rb
@@ -4,14 +4,41 @@ module Spree
       prepend Spree::ServiceModule::Base
 
       def call(card:)
-        ActiveRecord::Base.transaction do
-          card.payments.
-            valid.
-            joins(:order).
-            merge(Spree::Order.incomplete).
-            each(&:void!)
-          card.destroy
+        ApplicationRecord.transaction do
+          run :invalidate_payments
+          run :void_payments
+          run :destroy
         end
+      end
+
+      protected
+
+      def invalidate_payments(card:)
+        if payment_scope(card).checkout.each(&:invalidate!)
+          success(card: card)
+        else
+          failure('invalidating payments failure')
+        end
+      end
+
+      def void_payments(card:)
+        if payment_scope(card).where.not(state: :checkout).each(&:void!)
+          success(card: card)
+        else
+          failure('voiding payments failure')
+        end
+      end
+
+      def destroy(card:)
+        if card.destroy!
+          success(card: card)
+        else
+          failure(card.errors.full_messages.to_sentance)
+        end
+      end
+
+      def payment_scope(card)
+        card.payments.valid.joins(:order).merge(Spree::Order.incomplete)
       end
     end
   end
