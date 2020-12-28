bd40e7c6889555390569866473cdca8ea6ab2a57
Matthew Kennedy <m.kennedy@me.com>
2020-12-28 22:44:55 +0000

    Fix width issue. Remove JS that was for <v4

--- backend/app/assets/javascripts/spree/backend/spree-select2.js
+++ backend/app/assets/javascripts/spree/backend/spree-select2.js
@@ -8,21 +8,6 @@ document.addEventListener('DOMContentLoaded', function() {
       text: 'Select an option'
     }
   })
-
-  // BELOW: z-index fix for Select2 v3.x to lower the z-index of an opened Select2.
-  // The javascript below is not needed if Spree is updated to use Select2 v4.x
-  window.addEventListener('click', function(e) {
-    var select2Drop = document.getElementById('select2-drop')
-
-    if (select2Drop) {
-      if (select2Drop.contains(e.target)) {
-        // Clicking inside the Select2 dropdown does nothing...
-      } else {
-        // Clicking outside the Select2 dropdown close all open Select2 dropdowns.
-        $('*').select2('close')
-      }
-    }
-  })
 })
 
 $.fn.addSelect2Options = function (data) {
@@ -48,4 +33,6 @@ $.fn.addSelect2Options = function (data) {
   })
 }
 
+$.fn.select2.defaults.set("width", "style")
+$.fn.select2.defaults.set("dropdownAutoWidth", false)
 $.fn.select2.defaults.set('theme', 'bootstrap4')
