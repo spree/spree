2475ff135e62561ccffbd08af587a92ab2721122
Damian Legawiec <damian@sparksolutions.co>
2020-12-30 14:19:55 +0100

    Fixed I18n for Ruby 3.0

--- core/lib/spree/i18n.rb
+++ core/lib/spree/i18n.rb
@@ -1,20 +1,19 @@
 require 'i18n'
 require 'active_support/core_ext/array/extract_options'
-require 'spree/i18n/base'
 require 'action_view'
 
 module Spree
-  extend ::ActionView::Helpers::TranslationHelper
-  extend ::ActionView::Helpers::TagHelper
+  class TranslationHelperWrapper
+    include ActionView::Helpers::TranslationHelper
+  end
 
   class << self
     # Add spree namespace and delegate to Rails TranslationHelper for some nice
     # extra functionality. e.g return reasonable strings for missing translations
-    def translate(*args)
-      options = args.extract_options!
+    def translate(key, options = {})
       options[:scope] = [*options[:scope]].unshift(:spree).uniq
-      args << options
-      super(*args)
+
+      TranslationHelperWrapper.new.translate(key, **options)
     end
 
     alias t translate
