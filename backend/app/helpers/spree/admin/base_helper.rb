3e1c7a8c9ac991b5e06b9632e296907f510f87df
Matthew Kennedy <m.kennedy@me.com>
2021-01-13 09:59:24 +0000

    No need for the colon as labels now follow check-boxes.

--- backend/app/helpers/spree/admin/base_helper.rb
+++ backend/app/helpers/spree/admin/base_helper.rb
@@ -131,16 +131,16 @@ module Spree
           if object.has_preference?(key)
             case key
             when :currency
-              content_tag(:div, form.label("preferred_#{key}", Spree.t(key) + ': ') +
+              content_tag(:div, form.label("preferred_#{key}", Spree.t(key)) +
                 (form.select "preferred_#{key}", currency_options(object.preferences[key]), {}, { class: 'form-control select2' }),
                           class: 'form-group')
             else
               if object.preference_type(key) == :boolean
                 content_tag(:div, preference_field_for(form, "preferred_#{key}", type: object.preference_type(key)) +
-                  form.label("preferred_#{key}", Spree.t(key) + ': ', class: 'form-check-label'),
+                  form.label("preferred_#{key}", Spree.t(key), class: 'form-check-label'),
                             class: 'form-group form-check')
               else
-                content_tag(:div, form.label("preferred_#{key}", Spree.t(key) + ': ') +
+                content_tag(:div, form.label("preferred_#{key}", Spree.t(key)) +
                   preference_field_for(form, "preferred_#{key}", type: object.preference_type(key)),
                             class: 'form-group')
               end
