d2df8d6058ecbd445725c5c212ec1adb756cad66
Matthew Kennedy <m.kennedy@me.com>
2021-01-13 13:24:15 +0000

    Fixes For Issues With Placeholders

--- backend/app/assets/javascripts/spree/backend/spree-select2.js
+++ backend/app/assets/javascripts/spree/backend/spree-select2.js
@@ -1,8 +1,22 @@
 document.addEventListener('DOMContentLoaded', function() {
-  // Inititate Select2 on any select element with the class .select2
-  $('select.select2').select2({
-    allowClear: true,
-    placeholder: Spree.translations.select_an_option
+  // Inititate a standard Select2 on any select element with the class .select2
+  // Remember to add a place holder in the HTML as needed.
+  $('select.select2').select2()
+
+  // Inititate a Select2 with the option to clear, on any select element with the class .select2-clear
+  // Set: include_blank: true in your ERB.
+  // A placeholder is auto added here as it is required to clear the Select2
+  // If a custom placeholder is required, you will need to set up a custom instance.
+  $('select.select2-clear').select2({
+    placeholder: Spree.translations.select_an_option,
+    allowClear: true
+  })
+
+  // Inititate a Select2 with the multiple set to true, on any select element with the class .select2-multiple
+  // Set include_blank: false in your ERB.
+  // Manually add a placeholder in the html or ERB as needed.
+  $('select.select2-multiple').select2({
+    multiple: true
   })
 })
 
