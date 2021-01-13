7439f2aad26e84aeac5c9ac57741386a58fe8f50
Damian Legawiec <damian@sparksolutions.co>
2021-01-13 08:55:23 +0100

    Fix migration version for Rails 6.0 support

--- core/db/migrate/20210112193440_remove_contact_email_from_spree_stores.rb
+++ core/db/migrate/20210112193440_remove_contact_email_from_spree_stores.rb
@@ -1,4 +1,4 @@
-class RemoveContactEmailFromSpreeStores < ActiveRecord::Migration[6.1]
+class RemoveContactEmailFromSpreeStores < ActiveRecord::Migration[6.0]
   def change
     remove_column :spree_stores, :contact_email
   end
