14b6915371934b4233bb10767bee617ed89df66e
Matthew Kennedy <m.kennedy@me.com>
2021-01-13 21:32:17 +0000

    Improve comments.

--- backend/app/assets/javascripts/spree/backend/spree-select2.js
+++ backend/app/assets/javascripts/spree/backend/spree-select2.js
@@ -1,12 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
-  // Inititate a standard Select2 on any select element with the class .select2
+  // Initiate a standard Select2 on any select element with the class .select2
   // Remember to add a place holder in the HTML as needed.
   $('select.select2').select2()
 
-  // Inititate a Select2 with the option to clear, on any select element with the class .select2-clear
-  // Set: include_blank: true in your ERB.
-  // A placeholder is auto added here as it is required to clear the Select2
-  // If a custom placeholder is required, you will need to set up a custom instance.
+  // Initiate a Select2 with the option to clear, on any select element with the class .select2-clear
+  // Set: include_blank: true in the ERB.
+  // A placeholder is auto-added here as it is required to clear the Select2.
   $('select.select2-clear').select2({
     placeholder: Spree.translations.select_an_option,
     allowClear: true
