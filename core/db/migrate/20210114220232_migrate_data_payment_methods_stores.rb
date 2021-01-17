e95df3b78d3d9a270e56cda45b9261aee233dd89
Matthew Kennedy <m.kennedy@me.com>
2021-01-17 11:45:41 +0000

    Put the data migration into its own migration file.

--- core/db/migrate/20210114220232_migrate_data_payment_methods_stores.rb
+++ core/db/migrate/20210114220232_migrate_data_payment_methods_stores.rb
@@ -1,4 +1,4 @@
-class RemoveStoreIdFromSpreePaymentMethods < ActiveRecord::Migration[6.0]
+class MigrateDataPaymentMethodsStores < ActiveRecord::Migration[6.0]
   def up
     Spree::PaymentMethod.all.each do |payment_method|
       next if payment_method.store_ids.any?
@@ -11,9 +11,5 @@ class RemoveStoreIdFromSpreePaymentMethods < ActiveRecord::Migration[6.0]
 
       payment_method.save
     end
-  end
-
-  def change
-    remove_column :spree_payment_methods, :store_id
   end
 end
