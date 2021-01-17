d9b899b22b6801761c4e02e5904c01ca5e141293
Matthew Kennedy <m.kennedy@me.com>
2021-01-17 17:15:52 +0000

    Associate sample payment methods to all existing stores.

--- sample/db/samples/payment_methods.rb
+++ sample/db/samples/payment_methods.rb
@@ -2,10 +2,14 @@ Spree::Gateway::Bogus.where(
   name: 'Credit Card',
   description: 'Bogus payment gateway.',
   active: true
-).first_or_create!
+).first_or_create! do |payment_method|
+  payment_method.store_ids = Spree::Store.ids
+end
 
 Spree::PaymentMethod::Check.where(
   name: 'Check',
   description: 'Pay by check.',
   active: true
-).first_or_create!
+).first_or_create! do |payment_method|
+  payment_method.store_ids = Spree::Store.ids
+end
