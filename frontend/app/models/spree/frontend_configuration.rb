74b66b0d7103cff1ce98349462caafe6cb08b53d
Kacper Mękarski <43354669+KacperMekarski@users.noreply.github.com>
2021-01-18 17:20:12 +0100

    [SD-1098] Added preference to toggle HTTP caching for Storefrront (#10688)
    * Added preference to toggle HTTP caching for Storefrront
    
    `Spree::Frontend::Config[:http_cache_enabled]` is now used to determine if we should cache Home page, Product page, Product list & Taxon page. By default, it's enabled. For flexibility,  you can now disable this altogether. Useful for private/internal websites.
    
    * SD-1098 remove trailing whitespace
    
    * SD-1098 create http_cache_enabled? method as frontend helper
    
    * SD-1098 include Spree::CacheHelper & add basic tests
    
    * Update frontend/app/controllers/spree/products_controller.rb
    
    Co-authored-by: Damian Legawiec <damian@sparksolutions.co>
    
    * Update frontend/app/controllers/spree/products_controller.rb
    
    Co-authored-by: Damian Legawiec <damian@sparksolutions.co>
    
    Co-authored-by: Damian Legawiec <damian@sparksolutions.co>

--- frontend/app/models/spree/frontend_configuration.rb
+++ frontend/app/models/spree/frontend_configuration.rb
@@ -1,6 +1,7 @@
 module Spree
   class FrontendConfiguration < Preferences::Configuration
     preference :coupon_codes_enabled, :boolean, default: true # Determines if we show coupon code form at cart and checkout
+    preference :http_cache_enabled, :boolean, default: true
     preference :locale, :string, default: Rails.application.config.i18n.default_locale
     preference :products_filters, :array, default: %w(keywords price sort_by)
     preference :additional_filters_partials, :array, default: %w()
