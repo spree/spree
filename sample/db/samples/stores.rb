6ac0e333bc281dce84704670f646a99c3fce4261
Damian Legawiec <damian@sparksolutions.co>
2021-01-19 16:10:31 +0100

    Set proper checkout zones/default countries for new stores

--- sample/db/samples/stores.rb
+++ sample/db/samples/stores.rb
@@ -1,9 +1,16 @@
+default_store = Spree::Store.default
+default_store.checkout_zone = Spree::Zone.find_by(name: 'North America')
+default_store.default_country = Spree::Country.default
+default_store.save!
+
 eu_store = Spree::Store.find_or_initialize_by(code: 'eustore')
 eu_store.name = 'EU Store'
 eu_store.url = 'eu.spreecommerce.org'
 eu_store.mail_from_address = 'eustore@example.com'
 eu_store.default_currency = 'EUR'
 eu_store.default_locale = 'de'
+eu_store.checkout_zone = Spree::Zone.find_by(name: 'EU_VAT')
+eu_store.default_country = Spree::Country.find_by(iso: 'DE')
 eu_store.save!
 
 uk_store = Spree::Store.find_or_initialize_by(code: 'ukstore')
@@ -12,4 +19,6 @@ uk_store.url = 'uk.spreecommerce.org'
 uk_store.mail_from_address = 'ukstore@example.com'
 uk_store.default_currency = 'GBP'
 uk_store.default_locale = 'en'
+uk_store.checkout_zone = Spree::Zone.find_by(name: 'UK_VAT')
+uk_store.default_country = Spree::Country.find_by(iso: 'GB')
 uk_store.save!
