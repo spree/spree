d7848e7d5f24f887fbc30059a9f0ab91ad3bdfe1
Damian Legawiec <damian@sparksolutions.co>
2021-01-20 08:47:25 +0100

    Make Ability class easier to extend/modify

--- core/app/models/spree/ability.rb
+++ core/app/models/spree/ability.rb
@@ -23,55 +23,70 @@ module Spree
     end
 
     def initialize(user)
-      # add cancancan aliasing
-      alias_action :delete, to: :destroy
-      alias_action :create, :update, :destroy, to: :modify
+      alias_cancan_delete_action
 
       user ||= Spree.user_class.new
 
       if user.respond_to?(:has_spree_role?) && user.has_spree_role?('admin')
-        can :manage, :all
+        apply_admin_permissions(user)
       else
-        can :read, Country
-        can :read, OptionType
-        can :read, OptionValue
-        can :create, Order
-        can :show, Order do |order, token|
-          order.user == user || order.token && token == order.token
-        end
-        can :update, Order do |order, token|
-          !order.completed? && (order.user == user || order.token && token == order.token)
-        end
-        can :manage, Address, user_id: user.id
-        can :read, CreditCard, user_id: user.id
-        can :read, Product
-        can :read, ProductProperty
-        can :read, Property
-        can :create, Spree.user_class
-        can [:show, :update, :destroy], Spree.user_class, id: user.id
-        can :read, State
-        can :read, Store
-        can :read, Taxon
-        can :read, Taxonomy
-        can :read, Variant
-        can :read, Zone
+        apply_user_permissions(user)
       end
 
       # Include any abilities registered by extensions, etc.
+      # this is legacy behaviour and should be removed in Spree 5.0
       Ability.abilities.merge(abilities_to_register).each do |clazz|
         merge clazz.new(user)
       end
 
-      # Protect admin role
-      cannot [:update, :destroy], Role, name: ['admin']
+      protect_admin_role
     end
 
-    private
+    protected
 
     # you can override this method to register your abilities
     # this method has to return array of classes
     def abilities_to_register
       []
+    end
+
+    def alias_cancan_delete_action
+      alias_action :delete, to: :destroy
+      alias_action :create, :update, :destroy, to: :modify
+    end
+
+    def apply_admin_permissions(user)
+      can :manage, :all
+    end
+
+    def apply_user_permissions(user)
+      can :read, ::Spree::Country
+      can :read, ::Spree::OptionType
+      can :read, ::Spree::OptionValue
+      can :create, ::Spree::Order
+      can :show, ::Spree::Order do |order, token|
+        order.user == user || order.token && token == order.token
+      end
+      can :update, ::Spree::Order do |order, token|
+        !order.completed? && (order.user == user || order.token && token == order.token)
+      end
+      can :manage, ::Spree::Address, user_id: user.id
+      can :read, ::Spree::CreditCard, user_id: user.id
+      can :read, ::Spree::Product
+      can :read, ::Spree::ProductProperty
+      can :read, ::Spree::Property
+      can :create, ::Spree.user_class
+      can [:show, :update, :destroy], ::Spree.user_class, id: user.id
+      can :read, ::Spree::State
+      can :read, ::Spree::Store
+      can :read, ::Spree::Taxon
+      can :read, ::Spree::Taxonomy
+      can :read, ::Spree::Variant
+      can :read, ::Spree::Zone
+    end
+
+    def protect_admin_role
+      cannot [:update, :destroy], ::Spree::Role, name: ['admin']
     end
   end
 end
