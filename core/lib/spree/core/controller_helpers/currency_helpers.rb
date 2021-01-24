7c04765e419d14c0b3555561aa019b1bbaf2f844
Damian Legawiec <damian@sparksolutions.co>
2021-01-24 13:01:09 +0100

    Extract supported_currencies_for_all_stores method to CurrencyHelpers

--- core/lib/spree/core/controller_helpers/currency_helpers.rb
+++ core/lib/spree/core/controller_helpers/currency_helpers.rb
@@ -4,10 +4,20 @@ module Spree
       module CurrencyHelpers
         def self.included(receiver)
           receiver.send :helper_method, :supported_currencies
+          receiver.send :helper_method, :supported_currencies_for_all_stores
         end
 
         def supported_currencies
           current_store.supported_currencies_list
+        end
+
+        def supported_currencies_for_all_stores
+          @supported_currencies_for_all_stores ||= begin
+            (
+              Spree::Store.pluck(:supported_currencies).map { |c| c&.split(',') }.flatten + Spree::Store.pluck(:default_currency)
+            ).
+              compact.uniq.map { |code| ::Money::Currency.find(code.strip) }
+          end
         end
       end
     end
