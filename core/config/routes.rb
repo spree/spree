b9cb5b1a787b634007ba304b8510ad98a901e409
Damian Legawiec <damian@sparksolutions.co>
2021-01-26 20:22:24 +0100

    Move forbidden/unnauthorized outsode of Spree Frontend

--- core/config/routes.rb
+++ core/config/routes.rb
@@ -1,5 +1,6 @@
 Spree::Core::Engine.add_routes do
-  get '/forbidden', to: 'home#forbidden', as: :forbidden
+  get '/forbidden', to: 'errors#forbidden', as: :forbidden
+  get '/unauthorized', to: 'errors#unauthorized', as: :unauthorized
 end
 
 Spree::Core::Engine.draw_routes
