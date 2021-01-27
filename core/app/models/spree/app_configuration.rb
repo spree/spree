d3e88e1d668492fb56eb4fd7b335b24ba40ef608
Damian Legawiec <damian@sparksolutions.co>
2021-01-27 14:38:12 +0100

    Show store selector by default

--- core/app/models/spree/app_configuration.rb
+++ core/app/models/spree/app_configuration.rb
@@ -69,7 +69,7 @@ module Spree
     preference :credit_to_new_allocation, :boolean, default: false
 
     # Multi store configurations
-    preference :show_store_selector, :boolean, default: false
+    preference :show_store_selector, :boolean, default: true
 
     # searcher_class allows spree extension writers to provide their own Search class
     def searcher_class
