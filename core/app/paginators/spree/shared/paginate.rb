b6719b07a585067787bfa8a1292ab11af6ddd186
agnieszkajacek <agnieszka.jacek90@gmail.com>
2021-02-08 13:34:10 +0100

    [SD-1171] Set per page limit and do not allow for 0 value

--- core/app/paginators/spree/shared/paginate.rb
+++ core/app/paginators/spree/shared/paginate.rb
@@ -4,7 +4,14 @@ module Spree
       def initialize(collection, params)
         @collection = collection
         @page       = params[:page]
-        @per_page   = params[:per_page]
+
+        default_pages = Spree::Api::Config[:api_v2_per_page_limit]
+
+        if params[:per_page].to_i.between?(1, default_pages)
+          @per_page = params[:per_page]
+        else
+          @per_page = default_pages.to_i
+        end
       end
 
       def call
