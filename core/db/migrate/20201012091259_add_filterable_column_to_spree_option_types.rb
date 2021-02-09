4fe858c9d23441f34cdbfe4c655bb5c37312f9c3
Bogusław Tolarz <btolarz@gmail.com>
2021-02-09 11:42:14 +0100

    Check column and index presence in db in filtrable for option types migration

--- core/db/migrate/20201012091259_add_filterable_column_to_spree_option_types.rb
+++ core/db/migrate/20201012091259_add_filterable_column_to_spree_option_types.rb
@@ -1,6 +1,10 @@
 class AddFilterableColumnToSpreeOptionTypes < ActiveRecord::Migration[6.0]
   def change
-    add_column :spree_option_types, :filterable, :boolean, default: true, null: false
-    add_index :spree_option_types, :filterable
+    unless column_exists?(:spree_option_types, :filterable)
+      add_column :spree_option_types, :filterable, :boolean, default: true, null: false
+    end
+    unless index_exists?(:spree_option_types, :filterable)
+      add_index :spree_option_types, :filterable
+    end
   end
 end
