c74b8c289c6b35cefe93eb201970a94e97e15fb4
Damian Legawiec <damian@sparksolutions.co>
2021-02-12 16:48:36 +0100

    Move Admin::CurrencyHelper to CurrencyHelper
    To be able to use it in Storefront as well

---
+++ core/app/helpers/spree/currency_helper.rb
@@ -0,0 +1,12 @@
+module Spree
+  module CurrencyHelper
+    def currency_options(selected_value = nil)
+      selected_value ||= Spree::Config[:currency]
+      currencies = ::Money::Currency.table.map do |_code, details|
+        iso = details[:iso_code]
+        [iso, "#{details[:name]} (#{iso})"]
+      end
+      options_from_collection_for_select(currencies, :first, :last, selected_value)
+    end
+  end
+end
