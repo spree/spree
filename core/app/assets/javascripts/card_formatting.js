38fbeca28f7b4d404c7f6cf012ed5989e1e57bcb
Matthew Kennedy <m.kennedy@me.com>
2021-02-14 20:30:23 +0000

    Fix CC Issues

--- core/app/assets/javascripts/card_formatting.js
+++ core/app/assets/javascripts/card_formatting.js
@@ -1,35 +1,25 @@
-/* global Cleave, validateCardElements */
+/* global Cleave */
+/* exported validateCardElements */
 
 function validateCardElements () {
   if (document.querySelector('.cardNumber')) {
     document.querySelectorAll('.cardNumber').forEach(function (cardNumber) {
       /* eslint-disable no-new */
-      new Cleave(cardNumber, {
-        creditCard: true,
-        onCreditCardTypeChanged: function (type) {
-          $('.ccType').val(type)
-        }
-      })
+      new Cleave(cardNumber, { creditCard: true, onCreditCardTypeChanged: function (type) { $('.ccType').val(type) } })
     })
   }
 
   if (document.querySelector('.cardExpiry')) {
     document.querySelectorAll('.cardExpiry').forEach(function (cardExpiry) {
       /* eslint-disable no-new */
-      new Cleave(cardExpiry, {
-        date: true,
-        datePattern: ['m', Spree.translations.card_expire_year_format]
-      })
+      new Cleave(cardExpiry, { date: true, datePattern: ['m', Spree.translations.card_expire_year_format] })
     })
   }
 
   if (document.querySelector('.cardCode')) {
     document.querySelectorAll('.cardCode').forEach(function (cardCode) {
       /* eslint-disable no-new */
-      new Cleave(cardCode, {
-        numericOnly: true,
-        blocks: [3]
-      })
+      new Cleave(cardCode, { numericOnly: true, blocks: [3] })
     })
   }
 }
