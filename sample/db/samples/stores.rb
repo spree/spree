37f445f18d8dd3c11f2bc5e26240a2453ff3bddd
Damian Legawiec <damian@sparksolutions.co>
2021-02-15 21:45:44 +0100

    Additional multi language/currency settings for Stores created in samples

--- sample/db/samples/stores.rb
+++ sample/db/samples/stores.rb
@@ -1,6 +1,8 @@
 default_store = Spree::Store.default
 default_store.checkout_zone = Spree::Zone.find_by(name: 'North America')
 default_store.default_country = Spree::Country.default
+default_store.supported_currencies = 'CAD,USD'
+default_store.supported_locales = 'en,fr'
 default_store.url = Rails.env.development? ? 'localhost:3000' : 'demo.spreecommerce.org'
 default_store.save!
 
@@ -10,6 +12,7 @@ eu_store.url = Rails.env.development? ? 'eu.lvh.me:3000' : 'demo-eu.spreecommerc
 eu_store.mail_from_address = 'eustore@example.com'
 eu_store.default_currency = 'EUR'
 eu_store.default_locale = 'de'
+eu_store.supported_locales = 'de,fr,es'
 eu_store.checkout_zone = Spree::Zone.find_by(name: 'EU_VAT')
 eu_store.default_country = Spree::Country.find_by(iso: 'DE')
 eu_store.save!
@@ -24,11 +27,15 @@ uk_store.checkout_zone = Spree::Zone.find_by(name: 'UK_VAT')
 uk_store.default_country = Spree::Country.find_by(iso: 'GB')
 uk_store.save!
 
-currencies = %w[EUR GBP]
+currencies = %w[EUR GBP CAD]
 Spree::Price.where(currency: 'USD').each do |price|
   currencies.each do |currency|
     new_price = Spree::Price.find_or_initialize_by(currency: currency, variant: price.variant)
-    new_price.amount = price.amount
+    new_price.amount = if %w[EUR GBP].include?(currency)
+                         price.amount * 0.8
+                       else
+                         price.amount * 1.2
+                       end
     new_price.save
   end
 end
