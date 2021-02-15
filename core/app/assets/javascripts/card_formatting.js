77d65b77a416618a5bec0e2f773deb1461cc7b68
Matthew Kennedy <m.kennedy@me.com>
2021-02-15 23:16:12 +0000

    Improve javascript.

--- core/app/assets/javascripts/card_formatting.js
+++ core/app/assets/javascripts/card_formatting.js
@@ -2,69 +2,52 @@
 
 /* eslint-disable no-unused-vars */
 function formatCardNumber (wrapperElement, cardNumberInput, cardTypeInput) {
-  if (document.querySelector(wrapperElement)) {
-    document.querySelectorAll(wrapperElement).forEach(function (cardPaymentSet) {
-      if (cardNumberInput) {
-        var targetNumberInput = cardPaymentSet.querySelector(cardNumberInput)
-      } else {
-        console.warn('Please identify your card number input using the second function argument')
-      }
+  if (!document.querySelector(wrapperElement)) return
+  if (!cardNumberInput) return console.warn('Identify the card number input using the second function argument')
+  if (!cardTypeInput) return console.warn('Identify the card type input using the third function argument')
 
-      if (cardTypeInput) {
-        var targetCardType = cardPaymentSet.querySelector(cardTypeInput)
-      } else {
-        console.warn('Please identify your card type input using the third function argument')
-      }
+  document.querySelectorAll(wrapperElement).forEach(function (cardPaymentSet) {
+    var targetNumberInput = cardPaymentSet.querySelector(cardNumberInput)
+    var targetCardType = cardPaymentSet.querySelector(cardTypeInput)
 
-      if (cardNumberInput && cardTypeInput) {
-        /* eslint-disable no-new */
-        new Cleave(targetNumberInput, {
-          creditCard: true,
-          onCreditCardTypeChanged: function (type) {
-            targetCardType.value = type
-          }
-        })
+    /* eslint-disable no-new */
+    new Cleave(targetNumberInput, {
+      creditCard: true,
+      onCreditCardTypeChanged: function (type) {
+        targetCardType.value = type
       }
     })
-  }
+  })
 }
 
 /* eslint-disable no-unused-vars */
 function formatCardExpiry (wrapperElement, cardExpiry) {
-  if (document.querySelector(wrapperElement)) {
-    document.querySelectorAll(wrapperElement).forEach(function (cardPaymentSet) {
-      if (cardExpiry) {
-        var targetCardExpiry = cardPaymentSet.querySelector(cardExpiry)
-      } else {
-        console.warn('Please identify your card expiry input field using the second argument in this function')
-      }
+  if (!document.querySelector(wrapperElement)) return
+  if (!cardExpiry) return console.warn('Identify the expiry input field using the second function argument')
 
-      if (targetCardExpiry) {
-        /* eslint-disable no-new */
-        new Cleave(targetCardExpiry, {
-          date: true,
-          datePattern: ['m', Spree.translations.card_expire_year_format]
-        })
-      }
+  document.querySelectorAll(wrapperElement).forEach(function (cardPaymentSet) {
+    var targetCardExpiry = cardPaymentSet.querySelector(cardExpiry)
+
+    /* eslint-disable no-new */
+    new Cleave(targetCardExpiry, {
+      date: true,
+      datePattern: ['m', Spree.translations.card_expire_year_format]
     })
-  }
+  })
 }
 
 /* eslint-disable no-unused-vars */
 function formatCardCode (wrapperElement, cardCode) {
-  if (document.querySelector(wrapperElement)) {
-    document.querySelectorAll(wrapperElement).forEach(function (cardPaymentSet) {
-      if (cardCode) {
-        var targetCardCode = cardPaymentSet.querySelector(cardCode)
-      } else {
-        console.warn('Please identify your card CVV code input field')
-      }
+  if (!document.querySelector(wrapperElement)) return
+  if (!cardCode) return console.warn('Identify the CVV code input field using the second function argument')
 
-      /* eslint-disable no-new */
-      new Cleave(targetCardCode, {
-        numericOnly: true,
-        blocks: [3]
-      })
+  document.querySelectorAll(wrapperElement).forEach(function (cardPaymentSet) {
+    var targetCardCode = cardPaymentSet.querySelector(cardCode)
+
+    /* eslint-disable no-new */
+    new Cleave(targetCardCode, {
+      numericOnly: true,
+      blocks: [3]
     })
-  }
+  })
 }
