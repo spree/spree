7bb345b3f9cdae01e5d1d65be00fbdf358faa6bd
Bogusław Tolarz <btolarz@gmail.com>
2021-02-16 12:28:55 +0100

    Added Api v2 Error handler specs

---
+++ api/spec/services/spree/api/error_handler_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+module Spree
+  describe Api::ErrorHandler do
+    class CustomException < StandardError
+      def backtrace
+        ['a', 'b']
+      end
+    end
+
+    let(:message) { 'error' }
+    let(:exception) { CustomException.new(message) }
+
+    subject { described_class.call(exception: exception) }
+    let(:result) { subject }
+
+    it 'returns result with error' do
+      expect(Rails.logger).to receive(:error).with(message)
+      expect(Rails.logger).to receive(:error).with(exception.backtrace.join("\n"))
+
+      expect(result.value).to eq({ exception: exception, message: message })
+    end
+  end
+end
