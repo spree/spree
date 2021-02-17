0a44a4af7a162f8050a733550dabba4aa81a1ddb
Bogusław Tolarz <btolarz@gmail.com>
2021-02-17 13:45:34 +0100

    Use BaseController serialize_resource for checkout controller (#10791)
    * User BaseController for checkout controller
    
    * Update api/app/controllers/concerns/spree/api/v2/storefront/order_concern.rb
    
    Co-authored-by: Damian Legawiec <damian@sparksolutions.co>

--- api/app/controllers/concerns/spree/api/v2/storefront/order_concern.rb
+++ api/app/controllers/concerns/spree/api/v2/storefront/order_concern.rb
@@ -34,12 +34,16 @@ module Spree
             )
           end
 
-          def serialize_order(order)
-            resource_serializer.new(order.reload, include: resource_includes, fields: sparse_fields).serializable_hash
+          def serialized_current_order
+            serialize_resource(spree_current_order)
           end
 
-          def serialized_current_order
-            serialize_order(spree_current_order)
+          def serialize_order(order)
+            ActiveSupport::Deprecation.warn(<<-DEPRECATION, caller)
+              `OrderConcern#serialize_order` is deprecated and will be removed in Spree 5.0.
+              Please use `serializer_resource` method
+            DEPRECATION
+            serialize_resource(order)
           end
         end
       end
