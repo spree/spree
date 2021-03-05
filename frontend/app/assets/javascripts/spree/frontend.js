26b5dd82cd794204e3ea9dba87c1963da94f00ea
Damian Legawiec <damian@sparksolutions.co>
2021-03-05 13:38:58 +0100

    Use `Spree.localizedPathFor` for couponc code apply/remove in CouponManager
    Fixes Uncaught ReferenceError: SPREE_LOCALE is not defined errors
    
    https://spree-commerce.slack.com/archives/C0JCDUK0D/p1614946872046300

--- frontend/app/assets/javascripts/spree/frontend.js
+++ frontend/app/assets/javascripts/spree/frontend.js
@@ -44,8 +44,8 @@
 
 Spree.routes.api_tokens = Spree.pathFor('api_tokens')
 Spree.routes.ensure_cart = Spree.pathFor('ensure_cart')
-Spree.routes.api_v2_storefront_cart_apply_coupon_code = Spree.pathFor('api/v2/storefront/cart/apply_coupon_code')
-Spree.routes.api_v2_storefront_cart_remove_coupon_code = Spree.pathFor('api/v2/storefront/cart/remove_coupon_code')
+Spree.routes.api_v2_storefront_cart_apply_coupon_code = Spree.localizedPathFor('api/v2/storefront/cart/apply_coupon_code')
+Spree.routes.api_v2_storefront_cart_remove_coupon_code = function(couponCode) { return Spree.localizedPathFor('api/v2/storefront/cart/remove_coupon_code/' + couponCode) }
 Spree.routes.product = function(id) { return Spree.localizedPathFor('products/' + id) }
 Spree.routes.product_related = function(id) { return Spree.localizedPathFor('products/' + id + '/related') }
 Spree.routes.product_carousel = function (taxonId) { return Spree.localizedPathFor('product_carousel/' + taxonId) }
