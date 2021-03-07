5991669356c44635bb86e444e35fe86a05053a44
Damian Legawiec <damian@sparksolutions.co>
2021-03-07 19:47:51 +0100

    Fixed segfault
    https://github.com/rails/sprockets/issues/633#issuecomment-774762509

--- backend/config/initializers/assets.rb
+++ backend/config/initializers/assets.rb
@@ -1,1 +1,5 @@
 Rails.application.config.assets.precompile << 'spree_backend_manifest.js'
+
+Rails.application.config.assets.configure do |env|
+  env.export_concurrent = false
+end
--- frontend/config/initializers/assets.rb
+++ frontend/config/initializers/assets.rb
@@ -1,1 +1,4 @@
 Rails.application.config.assets.precompile << 'spree_frontend_manifest.js'
+Rails.application.config.assets.configure do |env|
+  env.export_concurrent = false
+end
