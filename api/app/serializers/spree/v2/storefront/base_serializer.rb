07b8eddff38d643d78042d604783278edd230bae
Damian Legawiec <damian@sparksolutions.co>
2021-03-16 21:43:41 +0100

    Added caching to API v2 serializers (WIP)

--- api/app/serializers/spree/v2/storefront/base_serializer.rb
+++ api/app/serializers/spree/v2/storefront/base_serializer.rb
@@ -1,8 +1,22 @@
+require 'active_support/cache'
+
 module Spree
   module V2
     module Storefront
       class BaseSerializer
         include JSONAPI::Serializer
+
+        cache_options(store: Rails.cache, namespace: 'jsonapi-serializer', expires_in: 1.hour)
+
+        def self.record_cache_options(options, fieldset, include_list, params)
+          opts = options.dup
+
+          params_cache_key = [params[:currency], params[:user]&.cache_key, params[:store]&.cache_key].join('-')
+
+          opts[:namespace] += '-' + params_cache_key
+
+          super(opts, fieldset, include_list, params)
+        end
       end
     end
   end
