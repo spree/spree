49d33bb08b1a9c93ec46b78d4bd3626c49dddd7d
Marcelo Pinheiro <marcelo.lannorte@gmail.com>
2021-03-18 09:46:48 -0300

    update spree

--- frontend/app/controllers/spree/checkout_controller.rb
+++ frontend/app/controllers/spree/checkout_controller.rb
@@ -28,7 +28,25 @@ module Spree
 
     # Updates the order and advances to the next state (when possible.)
     def update
-      
+      byebug
+      if @order.update_from_params(params, permitted_checkout_attributes, request.headers.env)
+        @order.temporary_address = !params[:save_user_address]
+        unless @order.next
+          flash[:error] = @order.errors.full_messages.join("\n")
+          redirect_to(checkout_state_path(@order.state)) && return
+        end
+
+        if @order.completed?
+          @current_order = nil
+          flash.notice = Spree.t(:order_processed_successfully)
+          flash['order_completed'] = true
+          redirect_to completion_route
+        else
+          redirect_to checkout_state_path(@order.state)
+        end
+      else
+        render :edit
+      end
     end
 
     private
