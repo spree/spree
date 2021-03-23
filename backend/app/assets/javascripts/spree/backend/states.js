03d491fa1ffc015b5f3e220749018e7279958246
Matthew Kennedy <m.kennedy@me.com>
2021-03-23 21:10:21 +0000

    Remove added blank line.

--- backend/app/assets/javascripts/spree/backend/states.js
+++ backend/app/assets/javascripts/spree/backend/states.js
@@ -4,7 +4,6 @@ $(document).ready(function () {
   if ($('#new_state_link').length) {
     $('#country').on('change', function () {
       var newStateLinkHref = $('#new_state_link').prop('href')
-
       var selectedCountryId = $('#country option:selected').prop('value')
       var newLink = newStateLinkHref.replace(/countries\/(\d+)/,
         'countries/' + selectedCountryId)
