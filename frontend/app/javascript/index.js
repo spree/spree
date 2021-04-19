11587bb9683f839d54d04486b445518a7741b3e9
Damian Legawiec <damian@sparksolutions.co>
2021-04-19 20:59:35 +0200

    Added Stimulus application skeleton

--- frontend/app/javascript/index.js
+++ frontend/app/javascript/index.js
@@ -1,3 +1,8 @@
+import { Application } from "stimulus"
+
+const application = Application.start()
+application.register('coupon-code', Controllers.CouponCodeController)
+
 export default {
   start() {
     console.log('Spree Frontend initialized')
