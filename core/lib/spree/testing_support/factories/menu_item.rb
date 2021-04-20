f55da187cbb0cc80aaa713077ca4e5262f076c4e
Matthew Kennedy <m.kennedy@me.com>
2021-04-20 11:36:34 +0100

    Add unique code to tests.

--- core/lib/spree/testing_support/factories/menu_item.rb
+++ core/lib/spree/testing_support/factories/menu_item.rb
@@ -3,6 +3,7 @@ FactoryBot.define do
     name { 'Link To Somewhere' }
     item_type { 'Link' }
     subtitle { 'Some Descriptive Text!' }
+    unique_code { generate(:random_string) }
     linked_resource_type { 'URL' }
     url { 'https://test.spree.com' }
   end
