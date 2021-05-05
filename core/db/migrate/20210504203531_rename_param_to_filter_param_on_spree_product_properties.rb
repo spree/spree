2a5a8545165cd7d1c9a93dcc0e67ab94aa62531a
Damian Legawiec <damian@sparksolutions.co>
2021-05-05 14:34:01 +0200

    Fix migration for Rails 6.0

--- core/db/migrate/20210504203531_rename_param_to_filter_param_on_spree_product_properties.rb
+++ core/db/migrate/20210504203531_rename_param_to_filter_param_on_spree_product_properties.rb
@@ -1,4 +1,4 @@
-class RenameParamToFilterParamOnSpreeProductProperties < ActiveRecord::Migration[6.1]
+class RenameParamToFilterParamOnSpreeProductProperties < ActiveRecord::Migration[6.0]
   def change
     rename_column :spree_product_properties, :param, :filter_param
   end
