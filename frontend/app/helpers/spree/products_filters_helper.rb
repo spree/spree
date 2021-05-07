a212b03c8b1f52024a00d606f8a13818fc14b5a9
Łukasz Adamczyk <adamczyk.lukasz.jerzy@gmail.com>
2021-05-07 13:02:35 +0200

    add styles for desktop and mobile

--- frontend/app/helpers/spree/products_filters_helper.rb
+++ frontend/app/helpers/spree/products_filters_helper.rb
@@ -1,16 +1,34 @@
 module Spree
   module ProductsFiltersHelper
-    def min_price_filter_input
-      price_filter_input(name: :min_price, value: prices.first, placeholder: Spree.t(:min))
+    def min_price_filter_input(**html_options)
+      price_filter_input(
+        name: :min_price,
+        value: prices.first,
+        placeholder: "#{currency_symbol(current_currency)} #{Spree.t(:min)}",
+        **html_options
+      )
     end
 
-    def max_price_filter_input
-      price_filter_input(name: :max_price, value: prices.last, placeholder: Spree.t(:max))
+    def max_price_filter_input(**html_options)
+      price_filter_input(
+        name: :max_price,
+        value: prices.last,
+        placeholder: "#{currency_symbol(current_currency)} #{Spree.t(:max)}",
+        **html_options
+      )
     end
 
-    def price_filter_input(name:, value:, placeholder:)
+    def price_filter_input(name:, value:, placeholder:, **html_options)
       price_value = value&.zero? ? '' : value
-      number_field_tag(name, price_value, id: name, min: 0, step: 1, placeholder: placeholder)
+      style_class = "spree-flat-input #{html_options[:class]}"
+
+      number_field_tag(
+        name, price_value,
+        id: name,
+        class: style_class,
+        min: 0, step: 1, placeholder: placeholder,
+        **html_options.except(:class)
+      )
     end
 
     private
