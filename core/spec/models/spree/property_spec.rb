224cbaf1b0cbb4b4f687e35c2b8ffbc466cccf37
Damian Legawiec <damian@sparksolutions.co>
2021-05-11 17:10:05 +0200

    Move logic from views to helpers

--- core/spec/models/spree/property_spec.rb
+++ core/spec/models/spree/property_spec.rb
@@ -6,4 +6,16 @@ describe Spree::Property, type: :model do
 
     it { expect { subject.save! }.to change(subject, :filter_param).from(nil).to('brand-name') }
   end
+
+  describe '#uniq_values' do
+    let(:property) { create(:property) }
+
+    before do
+      create(:product_property, property: property, value: 'Some Value')
+      create(:product_property, property: property, value: 'Some Value')
+      create(:product_property, property: property, value: 'Another 10% Value')
+    end
+
+    it { expect(property.uniq_values).to eq([['some-value', 'Some Value'], ['another-10-value', 'Another 10% Value']]) }
+  end
 end
