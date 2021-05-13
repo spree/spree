825db94167511b89e491efa1b9d8b6f3a42c7007
Łukasz Adamczyk <adamczyk.lukasz.jerzy@gmail.com>
2021-05-13 12:04:48 +0200

    add specs for filtering by properties in Products::Find

--- core/lib/spree/testing_support/factories/property_factory.rb
+++ core/lib/spree/testing_support/factories/property_factory.rb
@@ -8,5 +8,17 @@ FactoryBot.define do
       presentation { 'Brand' }
       filter_param { 'brand' }
     end
+
+    trait :manufacturer do
+      name         { 'manufacturer' }
+      presentation { 'Manufacturer' }
+      filter_param { 'manufacturer' }
+    end
+
+    trait :material do
+      name         { 'material' }
+      presentation { 'Material' }
+      filter_param { 'material' }
+    end
   end
 end
