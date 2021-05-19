0020e87e622a9b92cbf680cbcfee48b7c0a8799b
Łukasz Adamczyk <adamczyk.lukasz.jerzy@gmail.com>
2021-05-19 16:16:44 +0200

    fix caching of Option Values filters

--- core/app/models/spree/option_value_variant.rb
+++ core/app/models/spree/option_value_variant.rb
@@ -1,7 +1,7 @@
 module Spree
   class OptionValueVariant < Spree::Base
     belongs_to :option_value, class_name: 'Spree::OptionValue'
-    belongs_to :variant, class_name: 'Spree::Variant'
+    belongs_to :variant, touch: true, class_name: 'Spree::Variant'
 
     validates :option_value, :variant, presence: true
     validates :option_value_id, uniqueness: { scope: :variant_id }
