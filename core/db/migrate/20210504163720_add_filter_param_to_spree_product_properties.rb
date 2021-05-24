67b8c5cd648ce4a6b11ae2c4fe76486385ba7088
Damian Legawiec <damian@sparksolutions.co>
2021-05-24 17:57:00 +0200

    Move setting filter_params from DB migrations to AR callbacks only when needed

--- core/db/migrate/20210504163720_add_filter_param_to_spree_product_properties.rb
+++ core/db/migrate/20210504163720_add_filter_param_to_spree_product_properties.rb
@@ -3,12 +3,6 @@ class AddFilterParamToSpreeProductProperties < ActiveRecord::Migration[6.0]
     unless column_exists?(:spree_product_properties, :filter_param)
       add_column :spree_product_properties, :filter_param, :string
       add_index :spree_product_properties, :filter_param
-
-      # generate filter params
-      Spree::ProductProperty.reset_column_information
-      Spree::ProductProperty.find_each do |property|
-        property.save
-      end
     end
   end
 end
--- core/db/migrate/20210505114659_add_filter_param_to_spree_properties.rb
+++ core/db/migrate/20210505114659_add_filter_param_to_spree_properties.rb
@@ -3,12 +3,6 @@ class AddFilterParamToSpreeProperties < ActiveRecord::Migration[6.0]
     unless column_exists?(:spree_properties, :filter_param)
       add_column :spree_properties, :filter_param, :string
       add_index :spree_properties, :filter_param
-
-      # generate filter params
-      Spree::Property.reset_column_information
-      Spree::Property.find_each do |property|
-        property.save
-      end
     end
   end
 end
