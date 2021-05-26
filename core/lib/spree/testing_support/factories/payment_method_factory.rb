29cc1f0c02fdb83381e90e59838ff500f2aaebfd
Łukasz Adamczyk <adamczyk.lukasz.jerzy@gmail.com>
2021-05-26 10:35:21 +0200

    assign Store to Payment Method factory

--- core/lib/spree/testing_support/factories/payment_method_factory.rb
+++ core/lib/spree/testing_support/factories/payment_method_factory.rb
@@ -1,19 +1,30 @@
 FactoryBot.define do
-  factory :check_payment_method, class: Spree::PaymentMethod::Check do
+  factory :payment_method, class: Spree::PaymentMethod do
+    name { 'Test' }
+
+    before(:create) do |payment_method|
+      if payment_method.stores.empty?
+        store = Spree::Store.first || create(:store)
+        payment_method.stores << [store]
+      end
+    end
+  end
+
+  factory :check_payment_method, parent: :payment_method, class: Spree::PaymentMethod::Check do
     name { 'Check' }
   end
 
-  factory :credit_card_payment_method, class: Spree::Gateway::Bogus do
+  factory :credit_card_payment_method, parent: :payment_method, class: Spree::Gateway::Bogus do
     name { 'Credit Card' }
   end
 
   # authorize.net was moved to spree_gateway.
   # Leaving this factory in place with bogus in case anyone is using it.
-  factory :simple_credit_card_payment_method, class: Spree::Gateway::BogusSimple do
+  factory :simple_credit_card_payment_method, parent: :payment_method, class: Spree::Gateway::BogusSimple do
     name { 'Credit Card' }
   end
 
-  factory :store_credit_payment_method, class: Spree::PaymentMethod::StoreCredit do
+  factory :store_credit_payment_method, parent: :payment_method, class: Spree::PaymentMethod::StoreCredit do
     type          { 'Spree::PaymentMethod::StoreCredit' }
     name          { 'Store Credit' }
     description   { 'Store Credit' }
