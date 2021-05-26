a037f916d4ed338f3c2a9b3e974473effb41ff60
Łukasz Adamczyk <adamczyk.lukasz.jerzy@gmail.com>
2021-05-26 12:01:43 +0200

    [SD-1331] Assign Store to Payment Method factory (#11063)
    * assign Store to Payment Method factory
    
    * use a default Store for the Payment Method factory
    
    * adjust specs

--- core/lib/spree/testing_support/factories/payment_method_factory.rb
+++ core/lib/spree/testing_support/factories/payment_method_factory.rb
@@ -1,15 +1,28 @@
 FactoryBot.define do
-  factory :check_payment_method, class: Spree::PaymentMethod::Check do
+  factory :payment_method, class: Spree::PaymentMethod do
+    name { 'Test' }
+
+    before(:create) do |payment_method|
+      if payment_method.stores.empty?
+        default_store = Spree::Store.default.persisted? ? Spree::Store.default : nil
+        store = default_store || create(:store)
+
+        payment_method.stores << [store]
+      end
+    end
+  end
+
+  factory :check_payment_method, parent: :payment_method, class: Spree::PaymentMethod::Check do
     name { 'Check' }
   end
 
-  factory :credit_card_payment_method, class: Spree::Gateway::Bogus do
+  factory :credit_card_payment_method, parent: :payment_method, class: Spree::Gateway::Bogus do
     name { 'Credit Card' }
   end
 
   # authorize.net was moved to spree_gateway.
   # Leaving this factory in place with bogus in case anyone is using it.
-  factory :simple_credit_card_payment_method, class: Spree::Gateway::BogusSimple do
+  factory :simple_credit_card_payment_method, parent: :payment_method, class: Spree::Gateway::BogusSimple do
     name { 'Credit Card' }
   end
 
