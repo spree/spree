65a560eccc24b91ee72037066c9d5c7332231435
Łukasz Adamczyk <adamczyk.lukasz.jerzy@gmail.com>
2021-05-28 09:41:38 +0200

    update migration

--- core/db/migrate/20210527094055_create_spree_products_stores.rb
+++ core/db/migrate/20210527094055_create_spree_products_stores.rb
@@ -8,10 +8,14 @@ class CreateSpreeProductsStores < ActiveRecord::Migration[5.2]
 
     add_index :spree_products_stores, [:product_id, :store_id], unique: true
 
-    default_store_id = Spree::Store.default.id
-    prepared_values = Spree::Product.with_deleted.order(:id).ids.map { |id| "(#{id}, #{default_store_id}, '#{Time.current.to_s(:db)}', '#{Time.current.to_s(:db)}')" }.join(', ')
-    return if prepared_values.empty?
+    stores = Spree::Store.all
+    product_ids = Spree::Product.with_deleted.order(:id).ids
 
-    execute "INSERT INTO spree_products_stores (product_id, store_id, created_at, updated_at) VALUES #{prepared_values};"
+    stores.find_each do |store|
+      prepared_values = product_ids.map { |id| "(#{id}, #{store.id}, '#{Time.current.to_s(:db)}', '#{Time.current.to_s(:db)}')" }.join(', ')
+      next if prepared_values.empty?
+
+      execute "INSERT INTO spree_products_stores (product_id, store_id, created_at, updated_at) VALUES #{prepared_values};"
+    end
   end
 end
