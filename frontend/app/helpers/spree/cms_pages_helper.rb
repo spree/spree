39ad92ba336aa112b9382d631178338275a392d0
Matthew Kennedy <m.kennedy@me.com>
2021-06-10 17:35:29 +0100

    Move logic into view helpers frontend.

--- frontend/app/helpers/spree/cms_pages_helper.rb
+++ frontend/app/helpers/spree/cms_pages_helper.rb
@@ -4,44 +4,46 @@ module Spree
       page.content.present? && !page.sections?
     end
 
-    def build_section(section)
-      if section.full_width_on_small
-        case section.width
-        when 'Half'
-          element = 'aside'
-          css_width = 'col-12 col-md-6 cms_half_section half_section_on_md_up'
-        when 'Full'
-          element = 'div'
-          css_width = 'col-12 cms_full_section'
-        else
-          element = 'div'
-          css_width = ''
-        end
+    def section_tags(section, width, fit, &block)
+      inner_content = content_tag(:div, class: fit, &block)
+      if section.width == 'Half'
+        content_tag(:aside, inner_content, class: width)
       else
-        case section.width
-        when 'Half'
-          element = 'aside'
-          css_width = 'col-6 cms_half_section'
-        when 'Full'
-          element = 'div'
-          css_width = 'col-12 cms_full_section'
-        else
-          element = 'div'
-          css_width = ''
-        end
+        content_tag(:div, inner_content, class: width)
       end
+    end
+
+    def build_section(section)
+      css_width = if section.full_width_on_small
+                    case section.width
+                    when 'Half'
+                      'cms_section col-12 col-md-6 cms_half_section half_section_on_md_up'
+                    when 'Full'
+                      'cms_section col-12 cms_full_section'
+                    else
+                      ''
+                    end
+                  else
+                    case section.width
+                    when 'Half'
+                      'cms_section col-6 cms_half_section'
+                    when 'Full'
+                      'cms_section col-12 cms_full_section'
+                    else
+                      ''
+                    end
+                  end
 
       fit = case section.fit
-                 when 'Screen'
-                   'full-width '
-                 else
-                   ''
-                 end
+            when 'Screen'
+              'full-width '
+            else
+              ''
+            end
 
-      render "spree/shared/cms/sections/#{spree_resource_path(section)}", section: section,
-                                                                          width: css_width,
-                                                                          fit: fit,
-                                                                          element: element
+      section_tags(section, css_width, fit) do
+        render "spree/shared/cms/sections/#{spree_resource_path(section)}", section: section
+      end
     end
   end
 end
