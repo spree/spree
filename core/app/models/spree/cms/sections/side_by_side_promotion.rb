8620755da8684e6e08a5e166959c27ecdd27d25a
Matthew Kennedy <m.kennedy@me.com>
2021-06-14 14:45:49 +0100

    Improve frontend views for sections side-by-side promos

--- core/app/models/spree/cms/sections/side_by_side_promotion.rb
+++ core/app/models/spree/cms/sections/side_by_side_promotion.rb
@@ -4,6 +4,15 @@ module Spree::Cms::Sections
 
     store :content, accessors: [:permalink_one, :title_one, :subtitle_one,
                                 :permalink_two, :title_two, :subtitle_two], coder: JSON
+    store :settings, accessors: [:gutters], coder: JSON
+
+    def gutters_sizes
+      ['Use gutters', 'No Gutters']
+    end
+
+    def gutters?
+      gutters == 'Use gutters'
+    end
 
     private
 
