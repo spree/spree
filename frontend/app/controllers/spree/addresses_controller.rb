d577763093bdb90b2aad38208f49ad08f1b625bf
KacperMekarski <kacper.mekarski@sparksolutions.co>
2021-07-08 17:12:31 +0200

    remove bindnnig.pry

--- frontend/app/controllers/spree/addresses_controller.rb
+++ frontend/app/controllers/spree/addresses_controller.rb
@@ -49,12 +49,10 @@ module Spree
     end
 
     def create_service
-      binding.pry
       Spree::Dependencies.account_create_address_service.constantize
     end
 
     def update_service
-      binding.pry
       Spree::Dependencies.account_update_address_service.constantize
     end
   end
