7e67007813c1071f25b4502784642c337fe0c9b5
Matthew Kennedy <m.kennedy@me.com>
2021-07-15 14:47:09 +0100

    Fix feature tests for Stores plus add a controller test checking for redirect to stores index page using new domain URL after create.

--- backend/spec/features/admin/store_spec.rb
+++ backend/spec/features/admin/store_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe 'Stores', type: :feature, js: true do
+describe 'Stores', type: :feature do
   stub_authorization!
 
   context 'creating new store' do
@@ -11,8 +11,12 @@ describe 'Stores', type: :feature, js: true do
 
       click_link 'Configurations'
       click_link 'Stores'
-      click_link 'New Store'
+      within '#contentHeaderRow' do
+        click_link 'New Store'
+      end
     end
+
+    after { Capybara.app_host = nil }
 
     it 'has required fields' do
       expect(page).to have_field(id: 'store_name')
@@ -37,18 +41,22 @@ describe 'Stores', type: :feature, js: true do
     it 'creates a new store' do
       fill_in 'Name', with: 'Store name'
       fill_in 'Code', with: 'example_store'
-      fill_in 'URL', with: 'store.example.com'
+      fill_in 'URL', with: 'http://another-store.lvh.me'
       fill_in 'Mail from address', with: 'store@example.com'
 
       click_button 'Create'
 
-      expect(page).to have_content('successfully created!')
+      Capybara.app_host = 'http://another-store.lvh.me'
+
+      expect(page).to have_content('http://another-store.lvh.me')
+      expect(page).to have_content('example_store')
+      expect(page).to have_content('Stores')
     end
 
     it 'creates a new store with footer and seo data' do
       fill_in 'Name', with: 'Store name'
       fill_in 'Code', with: 'example_store'
-      fill_in 'URL', with: 'store.example.com'
+      fill_in 'URL', with: 'http://one-another-store.lvh.me'
       fill_in 'Mail from address', with: 'store@example.com'
       fill_in 'Description', with: 'New store description'
       fill_in 'Address', with: 'New store address 123, City 123'
@@ -61,11 +69,14 @@ describe 'Stores', type: :feature, js: true do
 
       click_button 'Create'
 
-      expect(page).to have_content('successfully created!')
+      Capybara.app_host = 'http://one-another-store.lvh.me'
+
+      expect(page).to have_content('http://one-another-store.lvh.me')
+      expect(page).to have_content('Stores')
     end
   end
 
-  context 'editing existing store' do
+  context 'editing existing store', js: true do
     let!(:updated_zone) { create(:zone, name: 'EU_VAT') }
 
     before do
@@ -95,7 +106,7 @@ describe 'Stores', type: :feature, js: true do
     end
   end
 
-  context 'with checkout_zone preference set in spree config file' do
+  context 'with checkout_zone preference set in spree config file', js: true do
     let!(:store) { create(:store, name: 'Some existing store') }
     let!(:zone) { create(:zone, name: 'Asia') }
 
@@ -117,7 +128,7 @@ describe 'Stores', type: :feature, js: true do
     end
   end
 
-  context 'with checkout_zone_id attribute set for store' do
+  context 'with checkout_zone_id attribute set for store', js: true do
     let!(:store) { create(:store, name: 'Some existing store', checkout_zone_id: zone.id) }
     let!(:zone) { create(:zone, name: 'Asia') }
 
@@ -139,7 +150,7 @@ describe 'Stores', type: :feature, js: true do
     end
   end
 
-  context 'without checkout_zone_id attribute and checkout_zone preference' do
+  context 'without checkout_zone_id attribute and checkout_zone preference', js: true do
     let!(:store) { create(:store, name: 'Some existing store', checkout_zone_id: nil) }
 
     before do
