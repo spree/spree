10e394db995909d3254ace9bd8f3eab1ce55ef5e
Damian Legawiec <damian@sparksolutions.co>
2021-07-15 18:36:00 +0200

    Allow to pass arguments to extension rake task

--- core/lib/spree/testing_support/extension_rake.rb
+++ core/lib/spree/testing_support/extension_rake.rb
@@ -2,8 +2,8 @@ require 'spree/testing_support/common_rake'
 
 desc 'Generates a dummy app for testing an extension'
 namespace :extension do
-  task :test_app, [:user_class] do |_t, _args|
+  task :test_app, [:user_class] do |_t, args|
     Spree::DummyGeneratorHelper.inject_extension_requirements = true
-    Rake::Task['common:test_app'].invoke
+    Rake::Task['common:test_app'].execute(args)
   end
 end
