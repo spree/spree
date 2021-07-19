10846ca0034809771b205b48427db8cd64ff2ff7
Tomek Urban <tomasz.urban@sparksolutions.co>
2021-07-19 15:15:32 +0200

    SD-1358 Use migration for older rails version

--- core/db/migrate/20210714102459_add_deleted_at_to_spree_stores.rb
+++ core/db/migrate/20210714102459_add_deleted_at_to_spree_stores.rb
@@ -1,4 +1,4 @@
-class AddDeletedAtToSpreeStores < ActiveRecord::Migration[6.1]
+class AddDeletedAtToSpreeStores < ActiveRecord::Migration[5.2]
   def change
     add_column :spree_stores, :deleted_at, :datetime unless column_exists? :spree_stores, :deleted_at
     add_index :spree_stores, :deleted_at
