a2c7b3807866ddde3d6a532754b821a548e222e6
Tomek Urban <tomasz.urban@sparksolutions.co>
2021-07-21 11:25:03 +0200

    SD-1266 Use keyword arguments

--- core/app/models/concerns/spree/user_reporting.rb
+++ core/app/models/concerns/spree/user_reporting.rb
@@ -1,28 +1,35 @@
 module Spree
   module UserReporting
-    def display_lifetime_value(store = nil, currency = nil)
-      store ||= Store.default
-      Spree::Money.new(lifetime_value(store), currency: currency || store.default_currency)
+    def display_lifetime_value(store: Store.default, currency: store.default_curremcy)
+      Spree::Money.new(lifetime_value(store: store, currency: currency), currency: currency)
     end
 
-    def lifetime_value(store = nil)
-      store ||= Store.default
-      store.orders.complete.sum(:total)
+    def lifetime_value(**args)
+      order_calculate(**args,
+                      operation: :sum,
+                      column: :total)
     end
 
-    def display_average_order_value(store = nil, currency = nil)
-      store ||= Store.default
-      Spree::Money.new(average_order_value(store), currency: currency || store.default_currency)
+    def display_average_order_value(store: Store.default, currency: store.default_currency)
+      Spree::Money.new(average_order_value(store: store, currency: currency), currency: currency)
     end
 
-    def average_order_value(store = nil)
-      store ||= Store.default
-      store.orders.complete.average(:total).to_f
+    def average_order_value(**args)
+      order_calculate(**args,
+                      operation: :average,
+                      column: :total)
     end
 
     def order_count(store = nil)
       store ||= Store.default
-      store.orders.complete.size.to_f
+      order_calculate(store: store,
+                      currency: store.supported_currencies.split(','),
+                      operation: :count,
+                      column: :all)
+    end
+
+    def order_calculate(store:, currency:, operation:, column:)
+      store.orders.complete.where(currency: currency).calculate(operation, column)
     end
   end
 end
