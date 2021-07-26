da3b8ea12ee633bd7c5a4c555dc933d870b91712
KacperMekarski <kacper.mekarski@sparksolutions.co>
2021-07-26 12:18:34 +0200

    add tests, fix yml description

--- core/app/services/spree/cart/destroy.rb
+++ core/app/services/spree/cart/destroy.rb
@@ -5,6 +5,8 @@ module Spree
 
       def call(order:)
         run :check_if_can_be_destroyed
+        run :cancel_shipments
+        run :void_payments
         run :destroy_order
       end
 
@@ -12,6 +14,18 @@ module Spree
 
       def check_if_can_be_destroyed(order:)
         return failure(Spree.t(:cannot_be_destroyed)) unless order&.can_be_destroyed?
+
+        success(order: order)
+      end
+
+      def cancel_shipments(order:)
+        order.shipments.each(&:cancel!)
+
+        success(order: order)
+      end
+
+      def void_payments(order:)
+        order.payments.each(&:void!)
 
         success(order: order)
       end
--- core/spec/services/spree/cart/destroy_spec.rb
+++ core/spec/services/spree/cart/destroy_spec.rb
@@ -6,10 +6,27 @@ module Spree
 
     context 'when order is given' do
       context 'when can be destroyed' do
-        let(:order) { create(:order) }
+        let(:order) { create(:order_with_line_items) }
+        let(:shipment) { order.shipments.take }
+        let!(:payment) { create(:payment, amount: order.total, order: order) }
+        let!(:line_item_ids) { order.line_item_ids }
+        let!(:shipment_ids) { order.shipment_ids }
+        let!(:payment_ids) { order.payment_ids }
 
         it 'returns success' do
           expect(subject.success?).to be true
+        end
+
+        it 'voids pending payments' do
+          expect_any_instance_of(Spree::Payment).to receive(:void!).exactly(order.payments.count).times
+
+          subject
+        end
+
+        it 'cancel not shipped shipments' do
+          expect_any_instance_of(Spree::Shipment).to receive(:cancel!).exactly(order.shipments.count).times
+
+          subject
         end
 
         it 'destroys the order' do
@@ -18,6 +35,26 @@ module Spree
           subject
 
           expect(order.destroyed?).to be true
+        end
+
+        it 'destroys line_items, shipments and payments' do
+          subject
+
+          expect(Spree::LineItem.where(id: line_item_ids)).to be_empty
+          expect(Spree::Shipment.where(id: shipment_ids)).to be_empty
+          expect(Spree::Payment.where(id: payment_ids)).to be_empty
+        end
+
+        context 'when empty service is called first' do
+          before { Spree::Cart::Empty.call(order: order) }
+
+          it 'destroys the order' do
+            expect(order.destroyed?).not_to be true
+
+            subject
+
+            expect(order.destroyed?).to be true
+          end
         end
       end
 
