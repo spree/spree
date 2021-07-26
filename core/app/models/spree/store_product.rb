44b7cc527f9f80ee98fdd88a883f35d3005c8500
Tomek Urban <tomasz.urban@sparksolutions.co>
2021-07-26 10:24:30 +0200

    SD-1358 Small refactor

--- core/app/models/spree/store_product.rb
+++ core/app/models/spree/store_product.rb
@@ -5,9 +5,17 @@ module Spree
     belongs_to :store, class_name: 'Spree::Store', touch: true
     belongs_to :product, class_name: 'Spree::Product', touch: true
 
-    after_destroy -> { product.destroy! if Spree::StoreProduct.where.not(store_id: store.id).find_by(product_id: product.id).nil? }
+    after_destroy :destroy_associated_product
 
     validates :store, :product, presence: true
     validates :store_id, uniqueness: { scope: :product_id }
+
+    protected
+
+    def destroy_associated_product
+      return unless Spree::StoreProduct.where(product_id: product.id).not(store_id: store.id).any?
+
+      product.destroy!
+    end
   end
 end
