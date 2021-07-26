012220dacb6354a50becff47c1b568d330316b97
Damian Legawiec <damian@sparksolutions.co>
2021-07-26 10:37:54 +0200

    Update core/db/migrate/20210714102459_add_deleted_at_to_spree_stores.rb

--- core/db/migrate/20210714102459_add_deleted_at_to_spree_stores.rb
+++ core/db/migrate/20210714102459_add_deleted_at_to_spree_stores.rb
@@ -1,6 +1,6 @@
 class AddDeletedAtToSpreeStores < ActiveRecord::Migration[5.2]
   def change
     add_column :spree_stores, :deleted_at, :datetime unless column_exists?(:spree_stores, :deleted_at)
-    add_index :spree_stores, :deleted_at
+    add_index :spree_stores, :deleted_at unless index_exists?(:spree_stores, :deleted_at)
   end
 end
