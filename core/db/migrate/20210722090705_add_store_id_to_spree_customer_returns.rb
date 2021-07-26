870c059d46826ffaa04cbe86b0ae61f892423665
Szymon Iwacz <szymon@iwacz.pl>
2021-07-26 12:26:51 +0200

    Fix AddStoreIdToSpreeCustomerReturns migration

--- core/db/migrate/20210722090705_add_store_id_to_spree_customer_returns.rb
+++ core/db/migrate/20210722090705_add_store_id_to_spree_customer_returns.rb
@@ -4,7 +4,8 @@ class AddStoreIdToSpreeCustomerReturns < ActiveRecord::Migration[5.2]
       add_column :spree_customer_returns, :store_id, :bigint
       add_index :spree_customer_returns, :store_id
       Spree::CustomerReturn.reset_column_information
-      Spree::CustomerReturn.find_each { |cr| cr.update_column(:store_id, cr.order.store_id) }
+      default_store_id = Spree::Store.default.id
+      Spree::CustomerReturn.find_each { |cr| cr.update_column(:store_id, cr.order&.store_id || default_store_id) }
     end
   end
 end
