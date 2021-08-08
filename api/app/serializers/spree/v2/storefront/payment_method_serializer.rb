b21f565a21b336db4301b692260a4f273dc8a54b
Rafal Cymerys <rafal@upsidelab.io>
2021-08-08 17:05:52 +0200

    Add publishable key and intents to payment method serializer

--- api/app/serializers/spree/v2/storefront/payment_method_serializer.rb
+++ api/app/serializers/spree/v2/storefront/payment_method_serializer.rb
@@ -5,6 +5,15 @@ module Spree
         set_type :payment_method
 
         attributes :type, :name, :description
+
+        attribute :preferences do |payment_method|
+          {
+            intents: payment_method.preferences[:intents],
+            publishable_key: payment_method.preferences[:publishable_key],
+            server: payment_method.preferences[:server],
+            test_mode: payment_method.preferences[:test_mode]
+          }
+        end
       end
     end
   end
