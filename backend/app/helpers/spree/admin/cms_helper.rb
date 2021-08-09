c04fa8ae368e7082dac799b2b51e2f3fc39964bf
Matthew Kennedy <m.kennedy@me.com>
2021-08-09 12:48:01 +0100

    Test in cms_page_helper, reduce duplication.

--- backend/app/helpers/spree/admin/cms_helper.rb
+++ backend/app/helpers/spree/admin/cms_helper.rb
@@ -1,28 +1,14 @@
 module Spree
   module Admin
     module CmsHelper
-      def page_preview_link(page)
-        page_localization = if page.locale == current_store.default_locale
-                              nil
-                            else
-                              page.locale
-                            end
-        button_link_to(Spree.t('admin.cms.preview_page'),
-                       spree_storefront_resource_url(page, locale: page_localization),
-                       class: 'btn-outline-secondary',
-                       icon: 'view.svg',
-                       id: 'admin_preview_product',
-                       target: :blank)
-      end
-
-      def preview_url(page)
+      def cms_page_preview_url(page)
         page_localization = if page.locale == current_store.default_locale
                               nil
                             else
                               page.locale
                             end
 
-        spree_storefront_resource_url(page, locale: page_localization) + "?no_cache=#{rand(1...10000)}"
+        spree_storefront_resource_url(page, locale: page_localization)
       end
     end
   end
