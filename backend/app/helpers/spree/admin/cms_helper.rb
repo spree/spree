b7547cf397045af8ad4abf8966f6cf0cdb2d053d
Matthew Kennedy <m.kennedy@me.com>
2021-08-09 14:20:14 +0100

    [SD-1405] - As a Admin on Edit Page I always see live preview, even without the Frontend installed (#11222)
    * use spree_storefront_resource_url for page url
    
    * Add tests for URL's generated for CMS Pages.
    
    * Test in cms_page_helper, reduce duplication.
    
    * Remove test on base helper.

--- backend/app/helpers/spree/admin/cms_helper.rb
+++ backend/app/helpers/spree/admin/cms_helper.rb
@@ -1,29 +1,14 @@
 module Spree
   module Admin
     module CmsHelper
-      def page_preview_link(page)
-        return unless frontend_available?
+      def cms_page_preview_url(page)
+        page_localization = if page.locale == current_store.default_locale
+                              nil
+                            else
+                              page.locale
+                            end
 
-        url = if page.homepage?
-                spree.root_url + page.locale
-              else
-                spree.page_url(page.locale, page.slug)
-              end
-
-        button_link_to(Spree.t('admin.cms.preview_page'), url, class: 'btn-outline-secondary',
-                                                               icon: 'view.svg',
-                                                               id: 'admin_preview_product',
-                                                               target: :blank)
-      end
-
-      def preview_url(page)
-        return unless frontend_available?
-
-        if page.homepage?
-          spree.root_path + page.locale + "?no_cache=#{rand(1...10000)}"
-        else
-          "/#{page.locale + spree.page_path(page.slug)}?no_cache=#{rand(1...10000)}"
-        end
+        spree_storefront_resource_url(page, locale: page_localization)
       end
     end
   end
