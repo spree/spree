c6e9f86ad6520dc8db9a848727e99c1785055109
Matthew Kennedy <m.kennedy@me.com>
2021-08-18 14:39:38 +0100

    Revert change to single_store_resource.rb

--- core/app/models/concerns/spree/single_store_resource.rb
+++ core/app/models/concerns/spree/single_store_resource.rb
@@ -3,9 +3,7 @@ module Spree
     extend ActiveSupport::Concern
 
     included do
-      if has_attribute?(:store_id)
-        scope :for_store, ->(store) { where(store_id: store.id) }
-      end
+      scope :for_store, ->(store) { where(store_id: store.id) }
     end
   end
 end
