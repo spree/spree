0b97fa0aa7ed6aeccbf2ac9e7664b90fb6e0ece6
Damian Legawiec <damian@sparksolutions.co>
2021-08-18 17:40:17 +0200

    Update image_serializer.rb

--- api/app/serializers/spree/v2/storefront/image_serializer.rb
+++ api/app/serializers/spree/v2/storefront/image_serializer.rb
@@ -2,11 +2,9 @@ module Spree
   module V2
     module Storefront
       class ImageSerializer < BaseSerializer
-        include Rails.application.routes.url_helpers
-
         set_type :image
 
-        attributes :styles, :original_url
+        attributes :viewable_type, :viewable_id, :styles
 
         attribute :transformed_url do |image, params|
           image.generate_url(size: params.dig(:image_transformation, :size))
