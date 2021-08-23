aa477b564bc34167bf4f0015e02a7f91b72e89f3
Szymon Iwacz <szymon@iwacz.pl>
2021-08-23 07:35:48 +0200

    Fix api v2 products available on specs

--- api/spec/requests/spree/api/v2/platform/products_spec.rb
+++ api/spec/requests/spree/api/v2/platform/products_spec.rb
@@ -169,6 +169,8 @@ describe 'API V2 Platform Products Spec' do
       end
 
       context 'sorting by available_on' do
+        before { store.products.each_with_index { |p, i| p.update(available_on: Time.current - i.days) } }
+
         context 'ascending order' do
           before { get '/api/v2/platform/products?sort=available_on', headers: bearer_token }
 
