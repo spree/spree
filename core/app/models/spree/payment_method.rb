640a6f7de8c72b1f6c75a790002179e2d6fa10b3
Rafal Cymerys <rafal@upsidelab.io>
2021-08-30 15:23:56 +0200

    Add publishable preferences to payment methods

--- core/app/models/spree/payment_method.rb
+++ core/app/models/spree/payment_method.rb
@@ -87,5 +87,17 @@ module Spree
 
       store_ids.include?(store.id)
     end
+
+    def publishable_preferences
+      publishable_preference_keys.each_with_object({}) do |key, hash|
+        hash[key] = preferences[key]
+      end
+    end
+
+    private
+
+    def publishable_preference_keys
+      []
+    end
   end
 end
