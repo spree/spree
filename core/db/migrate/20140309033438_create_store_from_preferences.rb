ea90c90f43d979130e330943382bac42768965ed
Konrad Gorazd <konrad.gorazd@gmail.com>
2021-08-31 11:13:02 +0200

    [SD-1358] As a Admin I should not be able to permanently delete Store

--- core/db/migrate/20140309033438_create_store_from_preferences.rb
+++ core/db/migrate/20140309033438_create_store_from_preferences.rb
@@ -7,6 +7,21 @@ class CreateStoreFromPreferences < ActiveRecord::Migration[4.2]
       end
     end
 
+    # workaround for missing deleted_at column added in later migrations
+    unless Spree::Store.column_names.include?('deleted_at')
+      ActiveRecord::Validations::UniquenessParanoiaValidator.module_eval do
+        def build_relation(klass, *args)
+          super
+        end
+      end
+
+      Spree::Store.class_eval do
+        def self.paranoia_scope
+          unscoped
+        end
+      end
+    end
+
     preference_store = Spree::Preferences::Store.instance
     if store = Spree::Store.where(default: true).first
       store.meta_description = preference_store.get('spree/app_configuration/default_meta_description') {}
