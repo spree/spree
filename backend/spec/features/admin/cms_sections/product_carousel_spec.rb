1bbde01a7c738a795e3b50c2311746fc493dc81e
Matthew Kennedy <m.kennedy@me.com>
2021-08-31 17:00:15 +0100

    Write admin feature tests for product carousel section

--- backend/spec/features/admin/cms_sections/product_carousel_spec.rb
+++ backend/spec/features/admin/cms_sections/product_carousel_spec.rb
@@ -1,13 +1,14 @@
 require 'spec_helper'
 
-describe 'Rich Text Content section', type: :feature do
+describe 'Product Carousel section', type: :feature do
   stub_authorization!
 
-  section_type = 'Rich Text Content'
+  section_type = 'Product Carousel'
 
   let!(:store) { Spree::Store.default }
   let!(:feature_page) { create(:cms_feature_page, store: store, title: 'Test Page') }
-  let!(:section) { create(:cms_rich_text_content_section, cms_page: feature_page, name: "Test #{section_type}") }
+  let!(:taxon) { create(:taxon) }
+  let!(:section) { create(:cms_product_carousel_section, cms_page: feature_page, name: "Test #{section_type}") }
 
   before do
     visit spree.edit_admin_cms_page_cms_section_path(feature_page, section)
@@ -20,17 +21,14 @@ describe 'Rich Text Content section', type: :feature do
       expect(page).to have_content("Options For: #{ section_type}")
     end
 
-    it 'saves WYSIWYG content to database' do
-      rte_content = 'Ipsum blanditiis labore voluptates vero asperiores ullam excepturi'
-
-      page.execute_script("$(tinymce.editors[0].setContent('#{rte_content}'))")
+    it 'saves taxon path and loads it back into the view database' do
+      select2 taxon.name, from: 'Taxon', search: true
 
       click_on 'Update'
 
-      expect(page).to have_field(id: 'cms_section_rte_content', with: "<p>#{rte_content}</p>", visible: :hidden, disabled: false)
-      assert_admin_flash_alert_success('Section "Test Rich Text Content" has been successfully updated!')
+      expect(page).to have_content(taxon.permalink)
+      assert_admin_flash_alert_success('Section "Test Product Carousel" has been successfully updated!')
     end
-
 
     it 'allows changing of the section name' do
       fill_in 'Name *', with: 'My New Section Name'
