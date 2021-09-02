4d9878bc2387dd2dd92269951c04a1fb0a35ef65
Piotr Milewski <piotr.milewski@sparksolutions.co>
2021-09-02 15:04:24 +0200

    [SD-1429] Rename Spree CMD to Spree CLI

--- cli/lib/spree_cli.rb
+++ cli/lib/spree_cli.rb
@@ -3,9 +3,9 @@ require 'thor/group'
 
 case ARGV.first
 when 'version', '-v', '--version'
-  puts Gem.loaded_specs['spree_cmd'].version
+  puts Gem.loaded_specs['spree_cli'].version
 when 'extension'
   ARGV.shift
-  require 'spree_cmd/extension'
-  SpreeCmd::Extension.start
+  require 'spree_cli/extension'
+  SpreeCli::Extension.start
 end
