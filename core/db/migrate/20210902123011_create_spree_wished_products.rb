5da084e2d186a47af3071bd11278115303b08b37
Matthew Kennedy <m.kennedy@me.com>
2021-09-06 21:16:52 +0100

    Add unique index on [:variant_id, :wishlist_id]

--- core/db/migrate/20210902123011_create_spree_wished_products.rb
+++ core/db/migrate/20210902123011_create_spree_wished_products.rb
@@ -10,6 +10,7 @@ class CreateSpreeWishedProducts < ActiveRecord::Migration[5.2]
       t.timestamps
     end
 
+    add_index :spree_wished_products, [:variant_id, :wishlist_id], unique: true unless index_exists?(:spree_wished_products, [:variant_id, :wishlist_id])
     add_index :spree_wished_products, :variant_id unless index_exists?(:spree_wished_products, :variant_id)
     add_index :spree_wished_products, :wishlist_id unless index_exists?(:spree_wished_products, :wishlist_id)
   end
