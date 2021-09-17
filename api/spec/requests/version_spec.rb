44217b59d311c4cfe17beb867abacc2cf8c9bc69
Damian Legawiec <damian@sparksolutions.co>
2021-09-17 20:47:24 +0200

    Remove APIs' magic 404 logic
    This can lead to strange scenarios, especially when spree is mounted in an existing Rails app

--- api/spec/requests/version_spec.rb
+++ api/spec/requests/version_spec.rb
@@ -3,13 +3,6 @@ require 'spec_helper'
 describe 'Version', type: :request do
   before { create_list(:country, 2) }
 
-  describe '/api' do
-    it 'returns 404' do
-      get '/api/countries'
-      expect(response).to have_http_status 404
-    end
-  end
-
   describe '/api/v1' do
     it 'be successful' do
       get '/api/v1/countries'
