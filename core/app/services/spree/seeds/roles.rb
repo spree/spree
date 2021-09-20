33d568ced67c5eddb4dddab89796951cd2004f3a
Damian Legawiec <damian@sparksolutions.co>
2021-09-20 20:04:58 +0200

    Utilize `ActiveRecord#insert_all` to cut seed loading time (#11352)

--- core/app/services/spree/seeds/roles.rb
+++ core/app/services/spree/seeds/roles.rb
@@ -3,8 +3,6 @@ module Spree
     class Roles
       prepend Spree::ServiceModule::Base
 
-      EXCLUDED_COUNTRIES = ['AQ', 'AX', 'GS', 'UM', 'HM', 'IO', 'EH', 'BV', 'TF'].freeze
-
       def call
         Spree::Role.where(name: 'admin').first_or_create!
         Spree::Role.where(name: 'user').first_or_create!
