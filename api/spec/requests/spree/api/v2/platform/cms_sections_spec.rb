f2e382c1e5723e6b38621a6099a239b4d3ac7da5
Matthew Kennedy <m.kennedy@me.com>
2021-09-27 17:55:37 +0100

    Add CMS Pages to Platform openAPI Docs

--- api/spec/requests/spree/api/v2/platform/cms_sections_spec.rb
+++ api/spec/requests/spree/api/v2/platform/cms_sections_spec.rb
@@ -34,7 +34,7 @@ describe 'Platform API v2 CmsSections', type: :request do
 
       it 'repositions section from position 4 to position 1' do
         # acts_as_list is not zero indexed so moving to
-        # position 0 results in a seved postiton at 1.
+        # position 0 results in a saved postiton at 1.
         section_d.reload
         expect(section_d.position).to eq(1)
       end
@@ -51,7 +51,7 @@ describe 'Platform API v2 CmsSections', type: :request do
 
       it 'moves item from position 1 to position 4' do
         # acts_as_list is not zero indexed so moving to
-        # position 3 results in a seved postiton at 4.
+        # position 3 results in a saved postiton at 4.
         section_a.reload
         expect(section_a.position).to eq(4)
       end
