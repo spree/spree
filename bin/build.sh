cd16cd0b16599b5cf02d3877f10fbd466f5a04ed
Damian Legawiec <damian@sparksolutions.co>
2021-10-01 15:19:53 +0200

    Include sample tests on CI

--- bin/build.sh
+++ bin/build.sh
@@ -39,3 +39,9 @@ echo "* Setup Spree Emails and running RSpec..."
 echo "*******************************************"
 cd ../emails; prepare_app; bundle exec rspec spec
 
+# Spree Sample
+echo "*******************************************"
+echo "* Setup Spree Sample and running RSpec..."
+echo "*******************************************"
+cd ../sample; prepare_app; bundle exec rspec spec
+
