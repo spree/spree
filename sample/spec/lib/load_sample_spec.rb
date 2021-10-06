765c8790a3db1b38bbdefec56a2e69fb9e508e07
Damian Legawiec <damian@sparksolutions.co>
2021-10-06 13:25:30 +0200

    Update load_sample_spec.rb

--- sample/spec/lib/load_sample_spec.rb
+++ sample/spec/lib/load_sample_spec.rb
@@ -1,12 +1,9 @@
 require 'spec_helper'
 
 describe 'Load samples' do
-  before { Spree::Seeds::All.call }
-
   it 'doesnt raise any error' do
-    skip if Rails::VERSION::MAJOR == 5
-
     expect do
+      Spree::Seeds::All.call
       SpreeSample::Engine.load_samples
     end.not_to raise_error
   end
