1188e35b8b7a73cc6256c024f7c6945b76c7d8b1
Damian Legawiec <damian@sparksolutions.co>
2021-10-06 10:53:49 +0200

    fix

--- bin/build-ci.rb
+++ bin/build-ci.rb
@@ -21,7 +21,7 @@ class Project
     @name = name
   end
 
-  ALL = %w[api core emails sample].map(&method(:new)).freeze
+  ALL = %w[emails api core sample].map(&method(:new)).freeze
   CORE_GEMS = %w[api core].freeze
 
   # Install subproject
