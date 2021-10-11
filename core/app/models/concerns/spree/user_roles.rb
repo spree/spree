ff80e89f60a957d0a43b63e5e1af1455d25f96f4
Damian Legawiec <damian@sparksolutions.co>
2021-10-11 13:19:43 +0200

    Added `UserRoles#spree_admin?`, `UserRoles#spree_admin_created?`

--- core/app/models/concerns/spree/user_roles.rb
+++ core/app/models/concerns/spree/user_roles.rb
@@ -6,18 +6,36 @@ module Spree
       has_many :role_users, class_name: 'Spree::RoleUser', foreign_key: :user_id, dependent: :destroy
       has_many :spree_roles, through: :role_users, class_name: 'Spree::Role', source: :role
 
-      scope :admin, -> { joins(:spree_roles).where(Spree::Role.table_name => { name: 'admin' }) }
+      scope :spree_admin, -> { joins(:spree_roles).where(Spree::Role.table_name => { name: 'admin' }) }
 
       # has_spree_role? simply needs to return true or false whether a user has a role or not.
       def has_spree_role?(role_name)
         spree_roles.exists?(name: role_name)
       end
 
+      def self.admin
+        ActiveSupport::Deprecation.warn('`User#admin` is deprecated and will be removed in Spree 5. Please use `User#spree_admin`')
+
+        spree_admin
+      end
+
       def self.admin_created?
-        admin.exists?
+        ActiveSupport::Deprecation.warn('`User#admin_created?` is deprecated and will be removed in Spree 5. Please use `User#spree_admin_created?`')
+
+        spree_admin_created?
       end
 
       def admin?
+        ActiveSupport::Deprecation.warn('`User#admin?` is deprecated and will be removed in Spree 5. Please use `User#spree_admin?`')
+
+        spree_admin?
+      end
+
+      def self.spree_admin_created?
+        spree_admin.exists?
+      end
+
+      def spree_admin?
         has_spree_role?('admin')
       end
     end
