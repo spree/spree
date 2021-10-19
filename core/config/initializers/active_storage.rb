3e10c0dc0acb47e9eda6c970c7d72c3765bb6058
Damian Legawiec <damian@sparksolutions.co>
2021-10-19 15:13:50 +0200

    Remove re-declaration of an outdated job from the initializer

--- core/config/initializers/active_storage.rb
+++ core/config/initializers/active_storage.rb
@@ -1,7 +1,1 @@
-class ActiveStorage::PurgeJob < ActiveStorage::BaseJob
-  def perform(blob)
-    blob.purge unless blob.attachments.present?
-  end
-end
-
 Rails.application.config.active_storage.content_types_to_serve_as_binary.delete('image/svg+xml')
