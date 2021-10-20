1cbafa0484ed10dde172f931375eac69222968b0
Damian Legawiec <damian@sparksolutions.co>
2021-10-20 11:34:14 +0200

    Skip flaky test on MySQL
    Generally, creating/deleting tables on the fly in tests is not a good idea :)
    
    Replaced `database_cleaner` with `database_cleaner-active_record`

--- common_spree_dependencies.rb
+++ common_spree_dependencies.rb
@@ -31,7 +31,7 @@ group :test do
   gem 'capybara', '~> 3.24'
   gem 'capybara-screenshot', '~> 1.0'
   gem 'capybara-select-2'
-  gem 'database_cleaner', '~> 1.3'
+  gem 'database_cleaner-active_record'
   gem 'email_spec'
   gem 'factory_bot_rails', '~> 6.0'
   gem 'multi_json'
