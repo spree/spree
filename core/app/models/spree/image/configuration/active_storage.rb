81a43a54b6ada3289f19cb69b4fe08bba53cd410
Kacper Mękarski <43354669+KacperMekarski@users.noreply.github.com>
2021-10-22 14:07:29 +0200

    SD-1481 Change Image content type validation to use  gem (#11419)

--- core/app/models/spree/image/configuration/active_storage.rb
+++ core/app/models/spree/image/configuration/active_storage.rb
@@ -5,10 +5,9 @@ module Spree
         extend ActiveSupport::Concern
 
         included do
-          validate :check_attachment_presence
-          validate :check_attachment_content_type
-
           has_one_attached :attachment
+
+          validates :attachment, attached: true, content_type: /\Aimage\/.*\z/
 
           default_scope { includes(attachment_attachment: :blob) }
 
@@ -31,22 +30,6 @@ module Spree
 
           def default_style
             :product
-          end
-
-          def accepted_image_types
-            %w(image/jpeg image/jpg image/png image/gif)
-          end
-
-          def check_attachment_presence
-            unless attachment.attached?
-              errors.add(:attachment, :attachment_must_be_present)
-            end
-          end
-
-          def check_attachment_content_type
-            if attachment.attached? && !attachment.content_type.in?(accepted_image_types)
-              errors.add(:attachment, :not_allowed_content_type)
-            end
           end
         end
       end
