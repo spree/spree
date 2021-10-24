ae1a1dc11422f2bad9b9b5906f85a42567fc498e
Matthew Kennedy <m.kennedy@me.com>
2021-10-24 22:29:39 +0100

    Allow for double nested attributes create and update on promotion_action_line_items

--- api/app/controllers/spree/api/v2/platform/promotions_controller.rb
+++ api/app/controllers/spree/api/v2/platform/promotions_controller.rb
@@ -10,10 +10,25 @@ module Spree
           end
 
           def scope_includes
-            [:promotion_category, :promotion_rules, :promotion_actions]
+            [:promotion_category, :promotion_rules, :promotion_actions, :promotion_action_line_items]
           end
 
           def spree_permitted_attributes
+            promotion_actions = []
+            promotion_rules = []
+
+            resource.promotion_actions.each do |promotion_action|
+              promotion_action.defined_preferences.each do |preference|
+                promotion_actions << "preferred_#{preference}".to_sym
+              end
+            end
+
+            resource.promotion_rules.each do |promotion_rule|
+              promotion_rule.defined_preferences.each do |preference|
+                promotion_rules << "preferred_#{preference}".to_sym
+              end
+            end
+
             additional_permitted_attributes = if action_name == 'update'
                                                 [:id]
                                               else
@@ -23,8 +38,20 @@ module Spree
             Spree::Promotion.json_api_permitted_attributes + [
               :store_ids,
               {
-                promotion_actions_attributes: Spree::PromotionAction.json_api_permitted_attributes.concat(additional_permitted_attributes),
-                promotion_rules_attributes: Spree::PromotionRule.json_api_permitted_attributes.concat(additional_permitted_attributes)
+                promotion_actions_attributes: Spree::PromotionAction.
+                                                             json_api_permitted_attributes.
+                                                             concat(additional_permitted_attributes,
+                                                                    promotion_actions) + [
+                                                                      {
+                                                                        promotion_action_line_items_attributes: Spree::PromotionActionLineItem.
+                                                                                         json_api_permitted_attributes.
+                                                                                         concat(additional_permitted_attributes)
+                                                                      }
+                                                                    ],
+
+                promotion_rules_attributes: Spree::PromotionRule.
+                                                             json_api_permitted_attributes.
+                                                             concat(additional_permitted_attributes, promotion_rules)
               }
             ]
           end
