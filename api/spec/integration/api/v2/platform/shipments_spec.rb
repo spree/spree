1331a33a438c3eec08197318ed59cb01fbf6ded7
Damian Legawiec <damian@sparksolutions.co>
2021-10-26 21:55:38 +0200

    Shipments API pt I

--- api/spec/integration/api/v2/platform/shipments_spec.rb
+++ api/spec/integration/api/v2/platform/shipments_spec.rb
@@ -10,7 +10,8 @@ describe 'Shipments API', swagger: true do
     filter_examples: [{ name: 'filter[state_eq]', example: 'complete' }]
   }
 
-  let(:id) { create(:shipment).id }
+  let(:order) { create(:order_ready_to_ship) }
+  let(:id) { create(:shipment, order: order).id }
   let(:records_list) { create_list(:shipment, 2) }
   let(:store) { Spree::Store.default }
   let(:valid_create_param_value) do
@@ -37,23 +38,99 @@ describe 'Shipments API', swagger: true do
     include_examples 'DELETE record', resource_name
   end
 
-  %w[ready ship cancel resume pend].each do |state|
-    path "/api/v2/platform/#{resource_path}/{id}/#{state}" do
-      patch "#{state.capitalize} #{resource_name.articleize}" do
-        tags resource_name.pluralize
-        security [ bearer_auth: [] ]
-        description "#{state.capitalize} #{resource_name.articleize}"
-        operationId "advance-#{resource_name.parameterize.to_sym}"
-        consumes 'application/json'
-        parameter name: :id, in: :path, type: :string
-        json_api_include_parameter(options[:include_example])
+  path "/api/v2/platform/#{resource_path}/{id}/ready" do
+    patch "Mark Shipment as ready to be shipped" do
+      tags resource_name.pluralize
+      security [ bearer_auth: [] ]
+      description "Marks Shipment as ready to be shipped"
+      operationId "ready-shipment"
+      consumes 'application/json'
+      parameter name: :id, in: :path, type: :string
+      json_api_include_parameter(options[:include_example])
 
-        response '200', 'record updated' do
-          run_test!
-        end
-        it_behaves_like 'record not found'
-        it_behaves_like 'authentication failed'
+      response '200', 'record updated' do
+        run_test!
       end
+      it_behaves_like 'record not found'
+      it_behaves_like 'authentication failed'
+    end
+  end
+
+  path "/api/v2/platform/#{resource_path}/{id}/ship" do
+    let(:id) { create(:shipment, order: order, state: :ready).id }
+
+    patch "Mark Shipment as shipped" do
+      tags resource_name.pluralize
+      security [ bearer_auth: [] ]
+      description "Marks Shipment as shipped"
+      operationId "ship-shipment"
+      consumes 'application/json'
+      parameter name: :id, in: :path, type: :string
+      json_api_include_parameter(options[:include_example])
+
+      response '200', 'record updated' do
+        run_test!
+      end
+      it_behaves_like 'record not found'
+      it_behaves_like 'authentication failed'
+    end
+  end
+
+  path "/api/v2/platform/#{resource_path}/{id}/cancel" do
+    patch "Cancels the Shipment" do
+      tags resource_name.pluralize
+      security [ bearer_auth: [] ]
+      description "Cancels the Shipment"
+      operationId "cancel-shipment"
+      consumes 'application/json'
+      parameter name: :id, in: :path, type: :string
+      json_api_include_parameter(options[:include_example])
+
+      response '200', 'record updated' do
+        run_test!
+      end
+      it_behaves_like 'record not found'
+      it_behaves_like 'authentication failed'
+    end
+  end
+
+  path "/api/v2/platform/#{resource_path}/{id}/resume" do
+    let(:id) { create(:shipment, order: order, state: :canceled).id }
+
+    patch "Resumes the Shipment" do
+      tags resource_name.pluralize
+      security [ bearer_auth: [] ]
+      description "Resumes previously canceled Shipment"
+      operationId "resume-shipment"
+      consumes 'application/json'
+      parameter name: :id, in: :path, type: :string
+      json_api_include_parameter(options[:include_example])
+
+      response '200', 'record updated' do
+        run_test!
+      end
+      it_behaves_like 'record not found'
+      it_behaves_like 'authentication failed'
+    end
+  end
+
+  path "/api/v2/platform/#{resource_path}/{id}/pend" do
+    let(:id) { create(:shipment, order: order, state: :ready).id }
+
+    patch "Moves Shipment back to pending state" do
+      tags resource_name.pluralize
+      security [ bearer_auth: [] ]
+      description "Moves Shipment back to pending state"
+      operationId "pend-shipment"
+      consumes 'application/json'
+      parameter name: :id, in: :path, type: :string
+      json_api_include_parameter(options[:include_example])
+
+      response '200', 'record updated' do
+        run_test!
+      end
+      it_behaves_like 'record not found'
+      it_behaves_like 'authentication failed'
     end
   end
 end
