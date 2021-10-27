bc14cf192eca7be6eb6f6ff35e974eb808421ebb
Matthew Kennedy <m.kennedy@me.com>
2021-10-27 16:47:01 +0100

    Use Rails way of managing has_many association through API requests. (#11435)
    * Use Rails way of managing has_many association through API requests.
    
    * Remove cms_sections, include example for shipping categories.

--- api/app/controllers/spree/api/v2/platform/shipping_methods_controller.rb
+++ api/app/controllers/spree/api/v2/platform/shipping_methods_controller.rb
@@ -3,32 +3,7 @@ module Spree
     module V2
       module Platform
         class ShippingMethodsController < ResourceController
-          def create
-            resource = model_class.new(permitted_resource_params)
-            assign_shipping_categories(resource, params)
-            ensure_current_store(resource)
-
-            if resource.save
-              render_serialized_payload(201) { serialize_resource(resource) }
-            else
-              render_error_payload(resource.errors)
-            end
-          end
-
-          def update
-            assign_shipping_categories(resource, params)
-            super
-          end
-
           private
-
-          def assign_shipping_categories(resource, params)
-            shipping_category_ids = Spree::ShippingCategory.where(id: params[:shipping_category_ids]).ids
-
-            if resource.class.method_defined?(:shipping_categories) && shipping_category_ids.present?
-              resource.shipping_category_ids = shipping_category_ids.compact.uniq
-            end
-          end
 
           def model_class
             Spree::ShippingMethod
