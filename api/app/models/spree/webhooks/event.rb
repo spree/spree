ab2f3a9db0088799a56e7adb14ad07ede59064e5
seb <11888191+sebastian-palma@users.noreply.github.com>
2021-10-27 15:39:53 -0300

    Update api/app/models/spree/webhooks/event.rb
    Co-authored-by: Damian Legawiec <damian@sparksolutions.co>

--- api/app/models/spree/webhooks/event.rb
+++ api/app/models/spree/webhooks/event.rb
@@ -1,7 +1,7 @@
 module Spree
   module Webhooks
     class Event < Spree::Webhooks::Base
-      validates :name, presence: true
+      validates :name, :subscriber, presence: true
 
       belongs_to :subscriber, inverse_of: :events, optional: false
     end
