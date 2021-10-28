c43cead107d965d74ae930da031f2f793cf2ab80
Matthew Kennedy <m.kennedy@me.com>
2021-10-28 13:26:07 +0100

    Add serializer spec attributes.

--- api/app/serializers/spree/api/v2/platform/payment_method_serializer.rb
+++ api/app/serializers/spree/api/v2/platform/payment_method_serializer.rb
@@ -3,7 +3,7 @@ module Spree
     module V2
       module Platform
         class PaymentMethodSerializer < BaseSerializer
-          attributes :name, :type, :description, :active, :display_on, :auto_capture, :position
+          attributes :name, :type, :description, :active, :display_on, :auto_capture, :position, :created_at, :updated_at, :deleted_at
 
           attribute :preferences do |payment_method|
             payment_method.preferences
