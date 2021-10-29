9c5bf0e27c0034ee41b91653b4d15e1a96c9373b
Matthew Kennedy <m.kennedy@me.com>
2021-10-29 10:50:47 +0100

    [SD-1501] - Fix 500 Error on Adjustments Serializer. (#11440)

--- api/app/serializers/spree/api/v2/platform/adjustment_serializer.rb
+++ api/app/serializers/spree/api/v2/platform/adjustment_serializer.rb
@@ -7,7 +7,12 @@ module Spree
 
           belongs_to :order
           belongs_to :adjustable, polymorphic: true
-          belongs_to :source, polymorphic: true
+          belongs_to :source, polymorphic: {
+            Spree::Promotion::Actions::FreeShipping => :promotion_action,
+            Spree::Promotion::Actions::CreateAdjustment => :promotion_action,
+            Spree::Promotion::Actions::CreateItemAdjustments => :promotion_action,
+            Spree::Promotion::Actions::CreateLineItems => :promotion_action
+          }
         end
       end
     end
