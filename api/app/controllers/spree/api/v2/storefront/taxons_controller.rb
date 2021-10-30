6d95ad2824d9921418582dfc84f63c1f060a1ab3
Sebastian Bean <seb@bean.la>
2021-10-29 19:24:55 -0700

    add param to include products in taxon

--- api/app/controllers/spree/api/v2/storefront/taxons_controller.rb
+++ api/app/controllers/spree/api/v2/storefront/taxons_controller.rb
@@ -41,7 +41,7 @@ module Spree
           end
 
           def serializer_params
-            super.merge(include_products: action_name == 'show')
+            super.merge(include_products: action_name == 'show' || params[:include_products] == 'true')
           end
         end
       end
