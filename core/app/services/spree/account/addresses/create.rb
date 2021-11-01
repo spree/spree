211e44325b680d0ba7fa2732bf0ead5b814ade1e
Damian Legawiec <damian@sparksolutions.co>
2021-11-01 15:49:59 +0100

    Re-organize Address services to be use outside of Account context (#11453)

--- core/app/services/spree/account/addresses/create.rb
+++ core/app/services/spree/account/addresses/create.rb
@@ -1,23 +1,7 @@
 module Spree
   module Account
     module Addresses
-      class Create
-        prepend Spree::ServiceModule::Base
-        include Spree::Account::Addresses::Helper
-
-        attr_accessor :country
-
-        def call(user:, address_params:)
-          fill_country_and_state_ids(address_params)
-
-          address = user.addresses.new(address_params)
-          if address.save
-            assign_to_user_as_default(user: user, address_id: address.id)
-            success(address)
-          else
-            failure(address)
-          end
-        end
+      class Create < Spree::Addresses::Create
       end
     end
   end
