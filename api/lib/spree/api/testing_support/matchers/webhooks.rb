a31d5581bfabc1e07f87e8a7a106b9b78e02bafb
Sebastian Palma <github.vnhnhm@gmail.com>
2021-11-03 14:04:10 -0300

    Update emit_webhook_event failure messages.

--- api/lib/spree/api/testing_support/matchers/webhooks.rb
+++ api/lib/spree/api/testing_support/matchers/webhooks.rb
@@ -13,18 +13,21 @@ RSpec::Matchers.define :emit_webhook_event do |event_to_emit|
     ENV['DISABLE_SPREE_WEBHOOKS'] = 'true'
   end
 
-  failure_message do |obj_method|
+  def block_body_definition(obj_method)
     # positive look-behinds must have a fixed length, using straighforward match instead
-    proc_body = obj_method.source.squish[/(expect *({|do) *)(.*?)( *(}|end).to)/, 3]
-    "Expected that executing `#{proc_body}` emits the `#{event_to_emit}` Webhook event.\n" \
-      "Check that `#{proc_body}` does implement `queue_webhooks_requests!` for " \
+    obj_method.source.squish[/(expect *({|do) *)(.*?)( *(}|end).(not_)*to)/, 3]
+  end
+
+  failure_message do |obj_method|
+    block_body_def = block_body_definition(obj_method)
+    "Expected that executing `#{block_body_def}` emits the `#{event_to_emit}` Webhook event.\n" \
+      "Check that `#{block_body_def}` does implement `queue_webhooks_requests!` for " \
       "`#{event_to_emit}` with the following body: #{body}."
   end
 
   failure_message_when_negated do |obj_method|
-    proc_body = obj_method.source.squish[/(expect *({|do) *)(.*?)( *(}|end).not_to)/, 3]
-    "Expected that executing `#{proc_body}` does not emit the `#{event_to_emit}` Webhook event " \
-      "with the following body: #{body}."
+    "Expected that executing `#{block_body_definition(obj_method)}` does not " \
+      "emit the `#{event_to_emit}` Webhook event with the following body: #{body}."
   end
 
   supports_block_expectations
