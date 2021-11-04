792bb46ebe39323582ae0b0a8e2fd0fa6d338a34
Aleksandar Petrushev <aleksandar.petrushev@sparksolutions.co>
2021-11-04 14:16:33 +0100

    Do not use #reload in decorator

--- api/app/models/spree/api/webhooks/stock_movement_decorator.rb
+++ api/app/models/spree/api/webhooks/stock_movement_decorator.rb
@@ -3,10 +3,9 @@ module Spree
     module Webhooks
       module StockMovementDecorator
         def update_stock_item_quantity
-          variant = stock_item.variant
-          variant_in_stock_before_update = variant.in_stock?
+          variant_in_stock_before_update = stock_item.variant
           super
-          variant.reload
+          variant = stock_item.variant
           variant.queue_webhooks_requests!('variant.out_of_stock') if variant_in_stock_before_update && !variant.in_stock?
         end
       end
