eb34bf5ea81ed414e66a4da857ae23b45a0be932
Aleksandar Petrushev <aleksandar.petrushev@sparksolutions.co>
2021-11-04 14:58:21 +0100

    Add spec for variant out of stock before and after update

--- api/app/models/spree/api/webhooks/stock_movement_decorator.rb
+++ api/app/models/spree/api/webhooks/stock_movement_decorator.rb
@@ -3,10 +3,16 @@ module Spree
     module Webhooks
       module StockMovementDecorator
         def update_stock_item_quantity
-          variant_in_stock_before_update = stock_item.variant.in_stock?
+          variant_in_stock_before_update = variant_in_stock?
           super
           variant = stock_item.variant
-          variant.queue_webhooks_requests!('variant.out_of_stock') if variant_in_stock_before_update && !variant.in_stock?
+          variant.queue_webhooks_requests!('variant.out_of_stock') if variant_in_stock_before_update && !variant_in_stock?
+        end
+
+        private
+
+        def variant_in_stock?
+          stock_item.variant.in_stock?
         end
       end
     end
