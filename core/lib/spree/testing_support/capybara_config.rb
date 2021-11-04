2f290931486604ad079b9a91942653430f742a66
Damian Legawiec <damian@sparksolutions.co>
2021-11-04 09:03:00 +0100

    Brought capybara config up to date

--- core/lib/spree/testing_support/capybara_config.rb
+++ core/lib/spree/testing_support/capybara_config.rb
@@ -1,28 +1,23 @@
 require 'capybara-screenshot/rspec'
 
-Capybara.save_path = ENV['CIRCLE_ARTIFACTS'] if ENV['CIRCLE_ARTIFACTS']
+Capybara.configure do |config|
+  config.save_path = ENV['CIRCLE_ARTIFACTS'] if ENV['CIRCLE_ARTIFACTS']
+  config.server = :puma
+  config.default_driver = :rack_test
+  config.javascript_driver = :selenium_chrome_headless
+  config.default_max_wait_time = ENV.fetch('CAPYBARA_MAX_WAIT_TIME', 45).to_i
+  config.always_include_port = true
+  config.match = :smart
+  config.ignore_hidden_elements = true
+end
 
 if ENV['WEBDRIVER'] == 'accessible'
   require 'capybara/accessible'
   Capybara.javascript_driver = :accessible
-else
-  Capybara.register_driver :chrome do |app|
-    Selenium::WebDriver.logger.level = :error
+end
 
-    Capybara::Selenium::Driver.new app,
-      browser: :chrome,
-      options: Selenium::WebDriver::Chrome::Options.new(
-        args: %w[no-sandbox disable-dev-shm-usage disable-popup-blocking headless disable-gpu window-size=1920,1080 --enable-features=NetworkService,NetworkServiceInProcess --disable-features=VizDisplayCompositor],
-        log_level: :error
-      )
-  end
-  Capybara.javascript_driver = :chrome
-
-  Capybara::Screenshot.register_driver(:chrome) do |driver, path|
-    driver.browser.save_screenshot(path)
+RSpec.configure do |config|
+  config.before(:each, js: true) do
+    Capybara.page.driver.browser.manage.window.resize_to(1400, 900)
   end
 end
-Capybara.default_max_wait_time = ENV.fetch('CAPYBARA_MAX_WAIT_TIME', 45).to_i
-Capybara.server = :puma
-
-Capybara.always_include_port = true
