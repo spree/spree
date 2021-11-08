5fec7145d97781adb4cdd4bb1be5eafb2ce3d4c6
Matthew Kennedy <m.kennedy@me.com>
2021-11-08 18:00:30 +0000

    Remove concern.

--- api/app/controllers/spree/api/v2/platform/promotions_controller.rb
+++ api/app/controllers/spree/api/v2/platform/promotions_controller.rb
@@ -3,8 +3,6 @@ module Spree
     module V2
       module Platform
         class PromotionsController < ResourceController
-          include ::Spree::Api::V2::PromotionHelper
-
           private
 
           def model_class
@@ -13,6 +11,41 @@ module Spree
 
           def scope_includes
             [:promotion_category, :promotion_rules, :promotion_actions]
+          end
+
+          def spree_permitted_attributes
+            promotion_rules_permitted_attributes = [:preferred_match_policy, :preferred_country_id, :preferred_amount_min,
+                                                    :preferred_operator_min, :preferred_amount_max, :preferred_operator_max,
+                                                    :preferred_eligible_values]
+
+            calculator_permitted_attributes = [:preferred_flat_percent, :preferred_amount, :preferred_currency, :preferred_first_item,
+                                               :preferred_additional_item, :preferred_max_items, :preferred_percent, :preferred_minimal_amount,
+                                               :preferred_normal_amount, :preferred_discount_amount, :preferred_currency, :preferred_base_amount,
+                                               :preferred_tiers, :preferred_base_percent]
+
+            additional_permitted_attributes = action_name == 'update' ? [:id] : []
+
+            super + [
+              {
+                promotion_actions_attributes: Spree::PromotionAction.
+                    json_api_permitted_attributes.
+                    concat(additional_permitted_attributes) + [
+                      {
+                        promotion_action_line_items_attributes: Spree::PromotionActionLineItem.
+                                                              json_api_permitted_attributes.
+                                                              concat(additional_permitted_attributes),
+                        calculator_attributes: Spree::Calculator.
+                                                              json_api_permitted_attributes.
+                                                              concat(additional_permitted_attributes,
+                                                                     calculator_permitted_attributes)
+                      }
+                    ],
+
+                promotion_rules_attributes: Spree::PromotionRule.
+                    json_api_permitted_attributes.
+                    concat(additional_permitted_attributes, promotion_rules_permitted_attributes)
+              }
+            ]
           end
         end
       end
