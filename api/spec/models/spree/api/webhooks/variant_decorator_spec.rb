2b1323860acea09caefd794029f3772bbc21547f
Sebastian Palma <github.vnhnhm@gmail.com>
2021-11-09 13:47:20 -0300

    Emit variant.discountinued on after_update_commit.

--- api/spec/models/spree/api/webhooks/variant_decorator_spec.rb
+++ api/spec/models/spree/api/webhooks/variant_decorator_spec.rb
@@ -3,13 +3,35 @@ require 'spec_helper'
 describe Spree::Api::Webhooks::VariantDecorator do
   let(:variant) { create(:variant) }
 
-  describe '#discontinue!' do
-    context 'emitting variant.discontinued' do
-      subject { variant.discontinue! }
+  context 'emitting variant.discontinued' do
+    let(:body) { Spree::Api::V2::Platform::VariantSerializer.new(variant).serializable_hash.to_json }
 
-      let(:body) { Spree::Api::V2::Platform::VariantSerializer.new(variant).serializable_hash.to_json }
+    context 'when variant discontinued_on changes' do
+      context 'when the new value is "present"' do
+        it do
+          expect do
+            variant.discontinue!
+          end.to emit_webhook_event('variant.discontinued')
+        end
+      end
 
-      it { expect { subject }.to emit_webhook_event('variant.discontinued') }
+      context 'when the new value is not "present"' do
+        before { variant.update(discontinue_on: Date.yesterday) }
+
+        it do
+          expect do
+            variant.update(discontinue_on: nil)
+          end.not_to emit_webhook_event('variant.discontinued')
+        end
+      end
+    end
+
+    context 'when variant discontinued_on does not change' do
+      it do
+        expect do
+          variant.update(width: 180)
+        end.not_to emit_webhook_event('variant.discontinued')
+      end
     end
   end
 end
