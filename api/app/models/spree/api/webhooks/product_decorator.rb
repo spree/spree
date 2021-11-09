967e4773a8008089e8100520551ba8c6ffbce39f
Sebastian Palma <github.vnhnhm@gmail.com>
2021-11-09 14:29:06 -0300

    Emit product.discountinued on after_update_commit.

--- api/app/models/spree/api/webhooks/product_decorator.rb
+++ api/app/models/spree/api/webhooks/product_decorator.rb
@@ -2,8 +2,16 @@ module Spree
   module Api
     module Webhooks
       module ProductDecorator
-        def discontinue!
-          super
+        def self.prepended(base)
+          base.after_update_commit :queue_webhooks_requests_for_product_discontinued!
+        end
+
+        private
+
+        def queue_webhooks_requests_for_product_discontinued!
+          return unless discontinue_on_previously_changed?
+          return if (change = discontinue_on_previous_change).blank? || change.last.blank?
+
           queue_webhooks_requests!('product.discontinued')
         end
       end
--- api/spec/models/spree/api/webhooks/product_decorator_spec.rb
+++ api/spec/models/spree/api/webhooks/product_decorator_spec.rb
@@ -2,11 +2,36 @@ require 'spec_helper'
 
 describe Spree::Api::Webhooks::ProductDecorator do
   let(:product) { create(:product) }
-  let(:body) { Spree::Api::V2::Platform::ProductSerializer.new(product).serializable_hash.to_json }
 
-  describe '#discontinue!' do
-    it 'emits the product.discontinued event' do
-      expect { product.discontinue! }.to emit_webhook_event('product.discontinued')
+  context 'emitting product.discontinued' do
+    let(:body) { Spree::Api::V2::Platform::ProductSerializer.new(product).serializable_hash.to_json }
+
+    context 'when product discontinued_on changes' do
+      context 'when the new value is "present"' do
+        it do
+          expect do
+            product.discontinue!
+          end.to emit_webhook_event('product.discontinued')
+        end
+      end
+
+      context 'when the new value is not "present"' do
+        before { product.update(discontinue_on: Date.yesterday) }
+
+        it do
+          expect do
+            product.update(discontinue_on: nil)
+          end.not_to emit_webhook_event('product.discontinued')
+        end
+      end
+    end
+
+    context 'when product discontinued_on does not change' do
+      it do
+        expect do
+          product.update(width: 180)
+        end.not_to emit_webhook_event('product.discontinued')
+      end
     end
   end
 end
