3bd794ff18ce73d70d5e151b7b46df6697efa10c
Matthew Kennedy <m.kennedy@me.com>
2021-11-09 23:29:28 +0000

    Removed unused actions from routes.rb

--- api/app/controllers/spree/api/v2/platform/classifications_controller.rb
+++ api/app/controllers/spree/api/v2/platform/classifications_controller.rb
@@ -3,21 +3,6 @@ module Spree
     module V2
       module Platform
         class ClassificationsController < ResourceController
-          def reposition
-            spree_authorize! :update, resource if spree_current_user.present?
-
-            result = classification_reposition_service.call(
-              classification: resource,
-              position: permitted_resource_params[:position]
-            )
-
-            if result.success?
-              render_serialized_payload { serialize_resource(result.value) }
-            else
-              render_error_payload(result.error)
-            end
-          end
-
           private
 
           def model_class
@@ -27,12 +12,8 @@ module Spree
           def scope_includes
             [
               taxon: [],
-              product: [:variants_including_master, :variant_images, :master, variants: [:prices]]
+              product: [:variants_including_master, :variant_images, :master, { variants: [:prices] }]
             ]
-          end
-
-          def classification_reposition_service
-            Spree::Dependencies.classification_reposition_service.constantize
           end
         end
       end
