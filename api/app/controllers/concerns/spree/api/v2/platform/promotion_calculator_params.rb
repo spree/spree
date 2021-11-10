ef74ade7d9ea3e648d352687ad2cba35e73ea6c3
Matthew Kennedy <m.kennedy@me.com>
2021-11-10 16:48:42 +0000

    Fix promotion calculators params.

--- api/app/controllers/concerns/spree/api/v2/platform/promotion_calculator_params.rb
+++ api/app/controllers/concerns/spree/api/v2/platform/promotion_calculator_params.rb
@@ -6,9 +6,9 @@ module Spree
           private
 
           def calculator_params
-            [:preferred_flat_percent, :preferred_amount, :preferred_currency, :preferred_first_item, :preferred_additional_item,
+            [:preferred_flat_percent, :preferred_amount, :preferred_first_item, :preferred_additional_item,
              :preferred_max_items, :preferred_percent, :preferred_minimal_amount, :preferred_normal_amount,
-             :preferred_discount_amount, :preferred_currency, :preferred_base_amount, :preferred_tiers, :preferred_base_percent]
+             :preferred_discount_amount, :preferred_currency, :preferred_base_amount, :preferred_base_percent, { preferred_tiers: {} }]
           end
         end
       end
