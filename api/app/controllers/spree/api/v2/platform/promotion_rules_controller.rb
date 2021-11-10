fc3aef428af8f6b1f7694a192f9e0edd3892b845
Matthew Kennedy <m.kennedy@me.com>
2021-11-10 10:34:12 +0000

    Setup promotion rule params in reusable include.

--- api/app/controllers/spree/api/v2/platform/promotion_rules_controller.rb
+++ api/app/controllers/spree/api/v2/platform/promotion_rules_controller.rb
@@ -3,6 +3,8 @@ module Spree
     module V2
       module Platform
         class PromotionRulesController < ResourceController
+          include ::Spree::Api::V2::Platform::PromotionRuleParams
+
           private
 
           def model_class
@@ -11,6 +13,10 @@ module Spree
 
           def scope_includes
             [:promotion]
+          end
+
+          def spree_permitted_attributes
+            super + rule_params
           end
         end
       end
--- api/app/controllers/spree/api/v2/platform/promotions_controller.rb
+++ api/app/controllers/spree/api/v2/platform/promotions_controller.rb
@@ -3,6 +3,9 @@ module Spree
     module V2
       module Platform
         class PromotionsController < ResourceController
+          include ::Spree::Api::V2::Platform::PromotionRuleParams
+          include ::Spree::Api::V2::Platform::PromotionCalculatorParams
+
           private
 
           def model_class
@@ -14,13 +17,6 @@ module Spree
           end
 
           def spree_permitted_attributes
-            rule_params = [:preferred_match_policy, :preferred_country_id, :preferred_amount_min, :preferred_operator_min, :preferred_amount_max,
-                           :preferred_operator_max, { taxon_ids: [], user_ids: [], product_ids: [], preferred_eligible_values: {} }]
-
-            calculator_params = [:preferred_flat_percent, :preferred_amount, :preferred_currency, :preferred_first_item, :preferred_additional_item,
-                                 :preferred_max_items, :preferred_percent, :preferred_minimal_amount, :preferred_normal_amount,
-                                 :preferred_discount_amount, :preferred_currency, :preferred_base_amount, :preferred_tiers, :preferred_base_percent]
-
             conditional_params = action_name == 'update' ? [:id] : []
 
             super + [{ promotion_actions_attributes: Spree::PromotionAction.json_api_permitted_attributes.concat(conditional_params) + [{
