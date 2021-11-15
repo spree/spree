1445a89b0c24dd96fbbd62d2d214606642f818e1
Damian Legawiec <damian@sparksolutions.co>
2021-11-15 12:13:53 +0100

    Added Shipments::AddItem service

--- core/app/services/spree/shipments/create.rb
+++ core/app/services/spree/shipments/create.rb
@@ -1,9 +1,11 @@
+# creates new shipment record
+# if selected item (variant) is already added to order it will update line item
+# if not - it will create new line item record
 module Spree
   module Shipments
     class Create
       prepend Spree::ServiceModule::Base
-
-      attr_accessor :shipment
+      include Helper
 
       def call(store:, shipment_attributes:)
         ActiveRecord::Base.transaction do
@@ -33,31 +35,13 @@ module Spree
       end
 
       def create_shipment(order:, stock_location:, variant:, quantity:)
-        @shipment = order.shipments.create(
+        shipment = order.shipments.create(
           order_id: order.id,
           stock_location_id: stock_location.id
         )
         return failure(shipment) unless shipment.persisted?
 
         success(order: order, variant: variant, quantity: quantity, shipment: shipment)
-      end
-
-      def return_shipment(line_item)
-        success(line_item.target_shipment)
-      end
-
-      def add_or_update_line_item(order:, variant:, quantity:, shipment:)
-        result = add_item_service.call(order: order, variant: variant, quantity: quantity, options: { shipment: shipment})
-
-        if result.success?
-          success(shipment.reload)
-        else
-          failure(result.value, result.error)
-        end
-      end
-
-      def add_item_service
-        Spree::Dependencies.cart_add_item_service.constantize
       end
     end
   end
