c611dbad8f03e0f11a99048d845bf98468fc872e
Matthew Kennedy <m.kennedy@me.com>
2021-11-22 14:10:16 +0000

    Add tests for behavior.

--- core/spec/models/spree/taxonomy_spec.rb
+++ core/spec/models/spree/taxonomy_spec.rb
@@ -16,4 +16,36 @@ describe Spree::Taxonomy, type: :model do
       expect { Spree::Taxon.find(@child_taxon.id) }.to raise_error(ActiveRecord::RecordNotFound)
     end
   end
+
+  describe '#set_root_taxon_name' do
+    before do
+      @taxonomy = create(:taxonomy, name: 'Clothing')
+    end
+
+    context 'when Taxonomy is created' do
+      it 'sets the root Taxonomy name to match' do
+        expect(@taxonomy.root.name).to eq('Clothing')
+      end
+    end
+
+    context 'when Taxonomy name is updated' do
+      it 'changes the root Taxon name to match' do
+        @taxonomy.update(name: 'Soft Goods')
+        @taxonomy.save!
+        @taxonomy.reload
+
+        expect(@taxonomy.root.name).to eq('Soft Goods')
+      end
+    end
+
+    context 'when Taxonomy position is updated' do
+      it 'does not change the root Taxon name' do
+        @taxonomy.update(position: 2)
+        @taxonomy.save!
+        @taxonomy.reload
+
+        expect(@taxonomy.root.name).to eq('Clothing')
+      end
+    end
+  end
 end
