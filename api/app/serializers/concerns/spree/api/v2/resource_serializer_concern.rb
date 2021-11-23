9087fa551ad745aa5e952ebcc4e5f6fd2d9d6c24
Sebastian Palma <github.vnhnhm@gmail.com>
2021-11-23 15:50:12 +0100

    Convert money display attributes in serializers to string.

--- api/app/serializers/concerns/spree/api/v2/resource_serializer_concern.rb
+++ api/app/serializers/concerns/spree/api/v2/resource_serializer_concern.rb
@@ -12,9 +12,20 @@ module Spree
           # include standard attributes
           base.attributes(*model_klazz.json_api_columns)
           # include money attributes
-          base.attributes(*model_klazz.new.methods.find_all do |m|
-            m.to_s.match(/display_/) && !m.to_s.match(/\=/) && !([Spree::Product, Spree::Variant].include?(model_klazz) && m == :display_amount)
-          end)
+          display_getter_methods(model_klazz).each do |method_name|
+            base.attribute(method_name) do |object|
+              object.public_send(method_name).to_s
+            end
+          end
+        end
+
+        def self.display_getter_methods(model_klazz)
+          model_klazz.new.methods.find_all do |method_name|
+            method_name.to_s.match(/display_/) && # if it's a display_ prefixed method, but
+              !method_name.to_s.match(/\=/)    && # is not a setter method, and
+              # display_amount but not for Spree::Product or Spree::Variants
+              !([Spree::Product, Spree::Variant].include?(model_klazz) && method_name == :display_amount)
+          end
         end
       end
     end
