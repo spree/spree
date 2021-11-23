d6c7018e8381c00d2b3fc0294f2adc4082e0413c
Aleksandar Petrushev <aleksandar.petrushev@sparksolutions.co>
2021-11-23 15:43:20 +0100

    Add specs for OptionValues Finder ordering

--- core/app/finders/spree/option_values/find_available.rb
+++ core/app/finders/spree/option_values/find_available.rb
@@ -9,20 +9,12 @@ module Spree
       end
 
       def execute
-        find_available(scope, products_scope).select(select_args).order(order_args)
+        find_available(scope, products_scope)
       end
 
       private
 
       attr_reader :scope, :products_scope
-
-      def select_args
-        "#{OptionValue.table_name}.*, #{OptionType.table_name}.position AS option_type_position"
-      end
-
-      def order_args
-        "option_type_position, #{OptionValue.table_name}.position"
-      end
     end
   end
 end
