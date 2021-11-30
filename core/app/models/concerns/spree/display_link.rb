54f775e596eb484d36c3c7189ae0236658cd0d4f
Matthew Kennedy <m.kennedy@me.com>
2021-11-30 23:54:12 +0000

    extend ActiveSupport::Concern from DisplayLink

--- core/app/models/concerns/spree/display_link.rb
+++ core/app/models/concerns/spree/display_link.rb
@@ -1,25 +1,29 @@
 module Spree
   module DisplayLink
-    belongs_to :linked_resource, polymorphic: true
+    extend ActiveSupport::Concern
 
-    def link
-      case linked_resource_type
-      when 'Spree::Taxon'
-        return if linked_resource&.permalink.blank?
+    included do
+      belongs_to :linked_resource, polymorphic: true
 
-        "/#{Spree::Config[:storefront_taxons_path]}/#{linked_resource.permalink}"
-      when 'Spree::Product'
-        return if linked_resource&.slug.blank?
+      def link
+        case linked_resource_type
+        when 'Spree::Taxon'
+          return if linked_resource&.permalink.blank?
 
-        "/#{Spree::Config[:storefront_products_path]}/#{linked_resource.slug}"
-      when 'Spree::CmsPage'
-        return if linked_resource&.slug.blank?
+          "/#{Spree::Config[:storefront_taxons_path]}/#{linked_resource.permalink}"
+        when 'Spree::Product'
+          return if linked_resource&.slug.blank?
 
-        "/#{Spree::Config[:storefront_pages_path]}/#{linked_resource.slug}"
-      when 'Spree::Linkable::HomePage'
-        '/'
-      when 'Spree::Linkable::Uri'
-        destination
+          "/#{Spree::Config[:storefront_products_path]}/#{linked_resource.slug}"
+        when 'Spree::CmsPage'
+          return if linked_resource&.slug.blank?
+
+          "/#{Spree::Config[:storefront_pages_path]}/#{linked_resource.slug}"
+        when 'Spree::Linkable::HomePage'
+          '/'
+        when 'Spree::Linkable::Uri'
+          destination
+        end
       end
     end
   end
