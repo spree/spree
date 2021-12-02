4a27a71ceefa2552039ba58fb5ac6aef1e6da29b
Aleksandar Petrushev <aleksandar.petrushev@sparksolutions.co>
2021-12-02 17:08:14 +0100

    Sort OptionValues by position in filter metadata

--- core/app/finders/spree/option_values/find_available.rb
+++ core/app/finders/spree/option_values/find_available.rb
@@ -9,12 +9,20 @@ module Spree
       end
 
       def execute
-        find_available(scope, products_scope)
+        find_available(scope, products_scope).select(select_args).order(order_args)
       end
 
       private
 
       attr_reader :scope, :products_scope
+
+      def select_args
+        "#{OptionValue.table_name}.*, #{OptionType.table_name}.position AS option_type_position"
+      end
+
+      def order_args
+        "option_type_position, #{OptionValue.table_name}.position"
+      end
     end
   end
 end
