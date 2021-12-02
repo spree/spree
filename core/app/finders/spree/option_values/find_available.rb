ef1e419bf6a5c845acc81343dcaeb2f889e078e6
Aleksandar Petrushev <aleksandar.petrushev@sparksolutions.co>
2021-12-02 16:59:34 +0100

    Resolve conflicts

--- core/app/finders/spree/option_values/find_available.rb
+++ core/app/finders/spree/option_values/find_available.rb
@@ -3,18 +3,26 @@ module Spree
     class FindAvailable
       include ProductFilterable
 
-      def initialize(scope: OptionValue.spree_base_scopes.order(position: :asc), products_scope: Product.spree_base_scopes)
-        @scope = scope
+      def initialize(scope: OptionValue.spree_base_scopes, products_scope: Product.spree_base_scopes)
+        @scope = scope.order(position: :asc)
         @products_scope = products_scope
       end
 
       def execute
-        find_available(scope, products_scope)
+        find_available(scope, products_scope).select(select_args).order(order_args)
       end
 
       private
 
       attr_reader :scope, :products_scope
+
+      def select_args
+        "#{OptionValue.table_name}.*, #{OptionType.table_name}.position AS option_type_position"
+      end
+
+      def order_args
+        "option_type_position, #{OptionValue.table_name}.position"
+      end
     end
   end
 end
