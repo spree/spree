48b6aba8f52d0ef32ef4791a56dd55ce94db6e22
Damian Legawiec <damian@sparksolutions.co>
2021-12-07 08:34:30 +0100

    Include sprockets

--- common_spree_dependencies.rb
+++ common_spree_dependencies.rb
@@ -24,6 +24,8 @@ platforms :ruby do
   end
 end
 
+gem 'sprockets-rails', '>= 2.0.0'
+
 if ENV['RAILS_VERSION']&.match(/7\.0\.0/) || !ENV['RAILS_VERSION']
   gem 'paranoia', github: 'payrollhero/paranoia', branch: 'rails7'
   gem 'awesome_nested_set', github: 'damianlegawiec/awesome_nested_set', branch: 'master'
