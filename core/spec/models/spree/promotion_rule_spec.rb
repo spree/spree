a2427dee6aac0a97ff2c8bf09587917723bdcf53
Aleksandar Petrushev <aleksandar.petrushev@sparksolutions.co>
2021-12-07 12:10:31 +0100

    Fix failing specs

--- core/spec/models/spree/promotion_rule_spec.rb
+++ core/spec/models/spree/promotion_rule_spec.rb
@@ -10,18 +10,17 @@ module Spree
       end
     end
 
+    let!(:promotion) { create(:promotion) }
+
     it 'forces developer to implement eligible? method' do
       expect { BadTestRule.new.eligible? }.to raise_error(ArgumentError)
     end
 
     it 'validates unique rules for a promotion' do
-      p1 = TestRule.new
-      p1.promotion_id = 1
-      p1.save
+      TestRule.create!(promotion: promotion)
 
-      p2 = TestRule.new
-      p2.promotion_id = 1
-      expect(p2).not_to be_valid
+      promotion_rule = TestRule.new(promotion: promotion)
+      expect(promotion_rule).not_to be_valid
     end
   end
 end
