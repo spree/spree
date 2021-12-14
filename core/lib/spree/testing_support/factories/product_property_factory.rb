5fa8941c822b5affe62dae2df4407971a5c92078
Aleksandar Petrushev <aleksandar.petrushev@sparksolutions.co>
2021-12-14 10:49:24 +0100

    Add spec for product property value presence

--- core/lib/spree/testing_support/factories/product_property_factory.rb
+++ core/lib/spree/testing_support/factories/product_property_factory.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :product_property, class: Spree::ProductProperty do
     product { create(:product, stores: [create(:store)]) }
+    value { "val-#{rand(50)}" }
     property
   end
 end
