ab97fe59fafbfe50d8ded47d9629327ab0047400
Damian Legawiec <damian@getvendo.com>
2021-12-30 10:04:27 +0100

    Update 20211229162122_disable_propagate_all_variants_by_default.rb

--- core/db/migrate/20211229162122_disable_propagate_all_variants_by_default.rb
+++ core/db/migrate/20211229162122_disable_propagate_all_variants_by_default.rb
@@ -1,4 +1,4 @@
-class DisablePropagateAllVariantsByDefault < ActiveRecord::Migration[7.0]
+class DisablePropagateAllVariantsByDefault < ActiveRecord::Migration[5.2]
   def change
     change_column_default :spree_stock_locations, :propagate_all_variants, false
   end
