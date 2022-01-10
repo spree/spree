932fa4311ee7f86dc36c8564f5678bcf79a05b70
Aleksandar Petrushev <aleksandar.petrushev@sparksolutions.co>
2022-01-10 15:33:37 +0100

    Remove default_country_id from Storefront StoreSerializer (#11598)

--- api/app/serializers/spree/v2/storefront/store_serializer.rb
+++ api/app/serializers/spree/v2/storefront/store_serializer.rb
@@ -4,9 +4,9 @@ module Spree
       class StoreSerializer < BaseSerializer
         set_type :store
 
-        attributes :name, :url, :meta_description, :meta_keywords, :seo_title, :default_currency, :default, :supported_currencies, :facebook,
-                   :twitter, :instagram, :default_locale, :customer_support_email, :default_country_id, :description,
-                   :address, :contact_phone, :supported_locales, :settings
+        attributes :name, :url, :meta_description, :meta_keywords, :seo_title, :default_currency,
+                   :default, :supported_currencies, :facebook, :twitter, :instagram, :default_locale, :customer_support_email,
+                   :description, :address, :contact_phone, :supported_locales, :settings
 
         has_many :menus
         has_many :cms_pages
