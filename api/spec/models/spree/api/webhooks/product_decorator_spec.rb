e958358d5a9950629e608553e45e55b5052ea699
Aleksandar Petrushev <aleksandar.petrushev@getvendo.com>
2022-01-27 14:18:35 +0100

    Fix expected webhook_payload_body in failing spec

--- api/spec/models/spree/api/webhooks/product_decorator_spec.rb
+++ api/spec/models/spree/api/webhooks/product_decorator_spec.rb
@@ -2,17 +2,15 @@ require 'spec_helper'
 
 describe Spree::Api::Webhooks::ProductDecorator do
   let(:product) { create(:product) }
-  let(:webhook_payload_body) { Spree::Api::V2::Platform::ProductSerializer.new(product).serializable_hash }
+  let(:webhook_payload_body) do
+    Spree::Api::V2::Platform::ProductSerializer.new(
+      product,
+      include: Spree::Api::V2::Platform::ProductSerializer.relationships_to_serialize.keys
+    ).serializable_hash
+  end
   let!(:webhook_subscriber) { create(:webhook_subscriber, :active, subscriptions: [event_name]) }
 
   context 'emitting product.discontinued' do
-    let(:webhook_payload_body) do
-      Spree::Api::V2::Platform::ProductSerializer.new(
-        product,
-        include: Spree::Api::V2::Platform::ProductSerializer.relationships_to_serialize.keys
-      ).serializable_hash
-    end
-    let!(:webhook_subscriber) { create(:webhook_subscriber, :active, subscriptions: [event_name]) }
     let(:event_name) { 'product.discontinued' }
 
     context 'when product discontinued_on changes' do
