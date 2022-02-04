0f4bfa706445dbb6519a677dcfa2876e209895f1
Aleksandar Petrushev <aleksandar.petrushev@getvendo.com>
2022-02-04 14:48:41 +0100

    Update 20220201103922_add_first_name_and_last_name_to_spree_users.rb

--- core/db/migrate/20220201103922_add_first_name_and_last_name_to_spree_users.rb
+++ core/db/migrate/20220201103922_add_first_name_and_last_name_to_spree_users.rb
@@ -1,8 +1,8 @@
 class AddFirstNameAndLastNameToSpreeUsers < ActiveRecord::Migration[5.2]
   def change
     if Spree.user_class.present?
-      add_column :spree_users, :first_name, :string unless column_exists?(:spree_users, :first_name)
-      add_column :spree_users, :last_name, :string unless column_exists?(:spree_users, :last_name)
+      add_column Spree.user_class.table_name, :first_name, :string unless column_exists?(:spree_users, :first_name)
+      add_column Spree.user_class.table_name, :last_name, :string unless column_exists?(:spree_users, :last_name)
     end
   end
 end
