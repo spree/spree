48e838114f10f117a517c18a23bea0d5b221c680
Damian Legawiec <damian@sparksolutions.co>
2022-03-02 15:20:22 +0100

    We shouldn't track Roles with webhooks (#11652)

--- core/app/models/spree/role.rb
+++ core/app/models/spree/role.rb
@@ -1,9 +1,6 @@
 module Spree
   class Role < Spree::Base
     include UniqueName
-    if defined?(Spree::Webhooks)
-      include Spree::Webhooks::HasWebhooks
-    end
 
     has_many :role_users, class_name: 'Spree::RoleUser', dependent: :destroy
     has_many :users, through: :role_users, class_name: Spree.user_class.to_s
