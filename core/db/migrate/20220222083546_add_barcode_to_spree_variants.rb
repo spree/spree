6ec965255cacacf48086d673b9e090636e5814df
Damian Legawiec <damian@sparksolutions.co>
2022-03-16 15:58:17 +0100

    Fix migration version to allow it to be used with Rails > 7 (#11664)

--- core/db/migrate/20220222083546_add_barcode_to_spree_variants.rb
+++ core/db/migrate/20220222083546_add_barcode_to_spree_variants.rb
@@ -1,4 +1,4 @@
-class AddBarcodeToSpreeVariants < ActiveRecord::Migration[7.0]
+class AddBarcodeToSpreeVariants < ActiveRecord::Migration[5.2]
   def change
     add_column :spree_variants, :barcode, :string
     add_index :spree_variants, :barcode
