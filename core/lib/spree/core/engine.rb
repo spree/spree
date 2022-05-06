05bbf15e58017249f4ea002c2819b47d17fcb164
Damian Legawiec <damian@getvendo.com>
2022-05-06 15:06:08 +0200

    fix

--- core/lib/spree/core/engine.rb
+++ core/lib/spree/core/engine.rb
@@ -11,9 +11,11 @@ module Spree
                                :adjusters,
                                :stock_splitters,
                                :promotions,
+                               :cms,
                                :line_item_comparison_hooks)
       SpreeCalculators = Struct.new(:shipping_methods, :tax_rates, :promotion_actions_create_adjustments, :promotion_actions_create_item_adjustments)
       PromoEnvironment = Struct.new(:rules, :actions)
+      CmsEnvironment   = Struct.new(:sections)
       isolate_namespace Spree
       engine_name 'spree'
 
@@ -122,7 +124,8 @@ module Spree
           Promotion::Actions::FreeShipping
         ]
 
-        Rails.application.config.spree.cms.section_types = [
+        Rails.application.config.spree.cms = CmsEnvironment.new
+        Rails.application.config.spree.cms.sections = [
           Spree::Cms::Sections::HeroImage,
           Spree::Cms::Sections::FeaturedArticle,
           Spree::Cms::Sections::ProductCarousel,
