6a8ca63947ee054cd778b7b8b4549b6fa0dca763
vrastello <vincent.rastello@spin.pm>
2022-06-03 14:23:09 -0700

    changing decorator again

--- core/app/models/friendly_id/slug_decorator.rb
+++ core/app/models/friendly_id/slug_decorator.rb
@@ -1,9 +1,7 @@
 module FriendlyId
   module SlugDecorator
     def self.prepended(base)
-      include Discard::Model
       base.discard_column = :deleted_at
-      default_scope -> { kept }
     end
   end
 end
