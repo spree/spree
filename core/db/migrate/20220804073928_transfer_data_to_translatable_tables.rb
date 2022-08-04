113f7aad8ad8549b669a00191084f7a20927388d
adrianryt <adrianryt3@gmal.com>
2022-08-04 14:02:39 +0200

    Update tables after migrating translatable data

--- core/db/migrate/20220804073928_transfer_data_to_translatable_tables.rb
+++ core/db/migrate/20220804073928_transfer_data_to_translatable_tables.rb
@@ -1,4 +1,3 @@
-# This migration comes from spree (originally 20220804073928)
 class TransferDataToTranslatableTables < ActiveRecord::Migration[7.0]
   DEFAULT_LOCALE = 'en'
   PRODUCTS_TABLE = 'spree_products'
@@ -10,19 +9,56 @@ class TransferDataToTranslatableTables < ActiveRecord::Migration[7.0]
     # Products
     ActiveRecord::Base.connection.execute("
       INSERT INTO #{PRODUCT_TRANSLATIONS_TABLE} (name, description, locale, spree_product_id, created_at, updated_at, meta_description, meta_keywords, meta_title, slug)
-      SELECT name, description, '#{DEFAULT_LOCALE}' as  locale, id, created_at, updated_at, meta_description, meta_keywords, meta_title, slug FROM #{PRODUCTS_TABLE}
+      SELECT name, description, '#{DEFAULT_LOCALE}' as locale, id, created_at, updated_at, meta_description, meta_keywords, meta_title, slug FROM #{PRODUCTS_TABLE}
+                                          ")
+    ActiveRecord::Base.connection.execute("
+      UPDATE #{PRODUCTS_TABLE}
+      SET name=null, description=null, meta_description=null, meta_keywords=null, meta_title=null, slug=null;
                                           ")
     #Taxons
     ActiveRecord::Base.connection.execute("
       INSERT INTO #{TAXON_TRANSLATIONS_TABLE} (name, description, locale, spree_taxon_id, created_at, updated_at)
-      SELECT name, description, '#{DEFAULT_LOCALE}' as  locale, id, created_at, updated_at FROM #{TAXONS_TABLE}
+      SELECT name, description, '#{DEFAULT_LOCALE}' as locale, id, created_at, updated_at FROM #{TAXONS_TABLE}
+                                          ")
+    ActiveRecord::Base.connection.execute("
+      UPDATE #{TAXONS_TABLE}
+      SET name=null, description=null
                                           ")
   end
 
   def down
     ActiveRecord::Base.connection.execute("
+      UPDATE #{PRODUCTS_TABLE} as products
+      SET (name,
+           description,
+           meta_description,
+           meta_keywords,
+           meta_title,
+           slug) =
+          (t_products.name,
+           t_products.description,
+           t_products.meta_description,
+           t_products.meta_keywords,
+           t_products.meta_title,
+           t_products.slug)
+      FROM #{PRODUCT_TRANSLATIONS_TABLE} AS t_products
+      WHERE t_products.spree_product_id = products.id
+    ")
+
+    ActiveRecord::Base.connection.execute("
       TRUNCATE TABLE #{PRODUCT_TRANSLATIONS_TABLE}
                                           ")
+
+    ActiveRecord::Base.connection.execute("
+      UPDATE #{TAXONS_TABLE} as taxons
+      SET (name,
+           description) =
+          (t_taxons.name,
+           t_taxons.description)
+      FROM #{TAXON_TRANSLATIONS_TABLE} AS t_taxons
+      WHERE t_taxons.spree_taxon_id = taxons.id
+    ")
+
     ActiveRecord::Base.connection.execute("
       TRUNCATE TABLE #{TAXON_TRANSLATIONS_TABLE}
                                           ")
