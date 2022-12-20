521d54c0ef39a4607cac6c433ab264e2a88194cd
wjwitek <weronikaj.witek@gmail.com>
2022-12-20 13:26:10 +0100

    change migration file to work for other user types

--- core/db/migrate/20221215151408_add_selected_locale_to_spree_users.rb
+++ core/db/migrate/20221215151408_add_selected_locale_to_spree_users.rb
@@ -1,5 +1,8 @@
 class AddSelectedLocaleToSpreeUsers < ActiveRecord::Migration[7.0]
   def change
-    add_column :spree_users, :selected_locale, :string
+    if Spree.user_class.present?
+      users_table_name = Spree.user_class.table_name
+      add_column users_table_name, :selected_locale, :string unless column_exists?(users_table_name, :selected_locale)
+    end
   end
 end
