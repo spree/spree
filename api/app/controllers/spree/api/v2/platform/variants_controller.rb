eb47edf7851824af0e4737266d40a27b9e71aee8
nciemniak <nciemniak@gmail.com>
2022-12-28 16:33:12 +0100

    move join product translation code into a method

--- api/app/controllers/spree/api/v2/platform/variants_controller.rb
+++ api/app/controllers/spree/api/v2/platform/variants_controller.rb
@@ -16,10 +16,7 @@ module Spree
           def collection
             # if filtering on products, manually join on product translation to workaround mobility-ransack issue
             if params.key?(:filter) && params[:filter].keys.any? { |k| k.include? 'product' }
-              @collection ||= scope.joins(:product).
-                              joins("LEFT OUTER JOIN #{Product::Translation.table_name} #{Product.translation_table_alias}
-                                           ON #{Product.translation_table_alias}.spree_product_id = #{Product.table_name}.id
-                                           AND #{Product.translation_table_alias}.locale = '#{Mobility.locale}'").
+              @collection ||= scope.join_product_translations.
                               ransack(params[:filter]).result
             else
               super
