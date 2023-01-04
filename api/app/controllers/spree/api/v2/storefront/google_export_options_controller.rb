834f1b3d32ea03500640546185abe07a767af761
wjwitek <weronikaj.witek@gmail.com>
2023-01-04 12:40:04 +0100

    move controller from platform to storefront

--- api/app/controllers/spree/api/v2/storefront/google_export_options_controller.rb
+++ api/app/controllers/spree/api/v2/storefront/google_export_options_controller.rb
@@ -1,7 +1,7 @@
 module Spree
   module Api
     module V2
-      module Platform
+      module Storefront
         class GoogleExportOptionsController < ResourceController
           def show
             @options = GoogleExportOption.find_by(spree_store_id: params[:store_id])
