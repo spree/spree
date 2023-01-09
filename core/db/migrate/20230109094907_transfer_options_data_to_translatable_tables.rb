8f2e005bbc14b4162b334d5de79030ab0d029db3
nciemniak <nciemniak@gmail.com>
2023-01-09 11:58:44 +0100

    migration for transferring option data to translations table

---
+++ core/db/migrate/20230109094907_transfer_options_data_to_translatable_tables.rb
@@ -0,0 +1,47 @@
+class TransferOptionsDataToTranslatableTables < ActiveRecord::Migration[6.1]
+  DEFAULT_LOCALE = 'en'
+
+  def up
+    # Option Types
+    ActiveRecord::Base.connection.execute("
+      INSERT INTO spree_option_type_translations (name, presentation, locale, spree_option_type_id, created_at, updated_at)
+      SELECT name, presentation, '#{DEFAULT_LOCALE}', id, created_at, updated_at
+      FROM spree_option_types;
+
+      UPDATE spree_option_types
+      SET name=null, presentation=null;
+    ")
+
+    # Option Values
+    ActiveRecord::Base.connection.execute("
+      INSERT INTO spree_option_value_translations (name, presentation, locale, spree_option_value_id, created_at, updated_at)
+      SELECT name, presentation, '#{DEFAULT_LOCALE}', id, created_at, updated_at
+      FROM spree_option_values;
+
+      UPDATE spree_option_values
+      SET name=null, presentation=null;
+    ")
+  end
+
+  def down
+    # Option Types
+    ActiveRecord::Base.connection.execute("
+      UPDATE spree_option_types as option_types
+      SET (name, presentation) = (t_option_types.name, t_option_types.presentation)
+      FROM spree_option_type_translations AS t_option_types
+      WHERE t_option_types.spree_option_type_id = option_types.id;
+
+      TRUNCATE TABLE spree_option_type_translations;
+    ")
+
+    # Option Values
+    ActiveRecord::Base.connection.execute("
+      UPDATE spree_option_values as option_values
+      SET (name, presentation) = (t_option_values.name, t_option_values.presentation)
+      FROM spree_option_value_translations AS t_option_values
+      WHERE t_option_values.spree_option_value_id = option_values.id;
+
+      TRUNCATE TABLE spree_option_value_translations;
+    ")
+  end
+end
