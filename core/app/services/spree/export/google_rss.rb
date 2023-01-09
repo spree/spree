bced95ba5dfb53391da0426def627badf3e75cf7
wjwitek <weronikaj.witek@gmail.com>
2023-01-09 15:22:58 +0100

    add comments

--- core/app/services/spree/export/google_rss.rb
+++ core/app/services/spree/export/google_rss.rb
@@ -25,7 +25,32 @@ module Spree
 
       private
 
+      def add_store_information_to_xml(xml)
+        xml.title @store.name
+        xml.link @store.url
+        xml.description @store.meta_description
+      end
+
+      def add_variant_information_to_xml(xml, product, variant)
+        return if get_image_link(variant, product).nil?
+
+        xml.item do
+          xml['g'].id variant.id
+          xml['g'].title format_title(product, variant)
+          xml['g'].description get_description(product, variant)
+          xml['g'].link "#{@store.url}/#{product.slug}"
+          xml['g'].image_link get_image_link(variant, product)
+          xml['g'].price format_price(variant)
+          xml['g'].availability get_availability(product)
+          xml['g'].availability_date product.available_on.xmlschema
+
+          add_optional_information(xml, product)
+        end
+      end
+
       def format_title(product, variant)
+        # Title of a variant is created by joining title of a product and variant's option_values, as they are
+        # what differentiaties it from other variants.
         title = product.name
         variant.option_values.find_each do |option_value|
           title << " - #{option_value.name}"
@@ -33,39 +58,10 @@ module Spree
         title
       end
 
-      def add_variant_information_to_xml(xml, product, variant)
-        return if get_image_link(variant, product).nil?
-
-        xml.item do
-          add_product_information_to_xml(xml, variant, product)
-        end
-      end
-
-      def add_product_information_to_xml(xml, variant, product)
-        xml['g'].id variant.id
-        xml['g'].title format_title(product, variant)
-        xml['g'].description get_description(product, variant)
-        xml['g'].link "#{@store.url}/#{product.slug}"
-        xml['g'].image_link get_image_link(variant, product)
-        xml['g'].price format_price(variant)
-        xml['g'].availability get_availability(product)
-        xml['g'].availability_date product.available_on.xmlschema
-
-        add_optional_information(xml, product)
-      end
-
       def get_description(product, variant)
         return product.description unless product.description.nil?
 
         format_title(product, variant)
-      end
-
-      def add_optional_information(xml, product)
-        @options.enabled_keys.each do |key|
-          if @options.send(key) && !product.property(key.to_s).nil?
-            xml['g'].send(key, product.property(key.to_s))
-          end
-        end
       end
 
       def get_image_link(variant, product)
@@ -80,6 +76,10 @@ module Spree
         img
       end
 
+      def format_price(variant)
+        "#{variant.cost_price} #{variant.cost_currency}"
+      end
+
       def get_availability(product)
         return 'in stock' if product.available_on.past?
         return 'backorder' unless product.available_on.nil?
@@ -87,15 +87,47 @@ module Spree
         'out of stock'
       end
 
-      def format_price(variant)
-        "#{variant.cost_price} #{variant.cost_currency}"
+      def add_optional_information(xml, product)
+        @options.enabled_keys.each do |key|
+          if @options.send(key) && !product.property(key.to_s).nil?
+            xml['g'].send(key, product.property(key.to_s))
+          end
+        end
       end
 
-      def add_store_information_to_xml(xml)
-        xml.title @store.name
-        xml.link @store.url
-        xml.description @store.meta_description
-      end
+      # example of modifing optional information
+      #
+      # By default, this code assumes that any information that is not required by Google
+      # (see https://support.google.com/merchants/answer/160589?hl=en) is stored in Spree::Products's properties.
+      # If it's in other column you can modify add_optional_information like this:
+      # def add_optional_information(xml, product)
+      #         @options.enabled_keys.each do |key|
+      #           if @options.send(key) && !product.property(key.to_s).nil?
+      #             xml['g'].send(key, product.property(key.to_s))
+      #           end
+      #         end
+      #         if !product.column_name.nil?
+      #           xml['g'].attribute product.column_name
+      #         end
+      #       end
+      #
+      # If the column is part of variant or for example is option value, you will need to add variant to function's
+      # arguments and modify code analogically as above.
+      #
+      # def add_optional_information(xml, product, variant)
+      #   @options.enabled_keys.each do |key|
+      #     if @options.send(key) && !product.property(key.to_s).nil?
+      #       xml['g'].send(key, product.property(key.to_s))
+      #     end
+      #   end
+      #   if !variant.column_name.nil?
+      #     xml['g'].attribute variant.column_name
+      #   end
+      #   size_value = variant.option_value("size")
+      #   if !size_value.nil?
+      #     xml['g'].size size_value
+      #   end
+      # end
     end
   end
 end
