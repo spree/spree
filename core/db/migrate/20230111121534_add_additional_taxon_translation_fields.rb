5182213b5ffa18bdfdac838217c424ccccd0f3fe
nciemniak <nciemniak@gmail.com>
2023-01-11 13:19:52 +0100

    add additional translatable taxon fields to match globalize

---
+++ core/db/migrate/20230111121534_add_additional_taxon_translation_fields.rb
@@ -0,0 +1,8 @@
+class AddAdditionalTaxonTranslationFields < ActiveRecord::Migration[6.1]
+  def change
+    add_column :spree_taxon_translations, :meta_title, :string, if_not_exists: true
+    add_column :spree_taxon_translations, :meta_description, :string, if_not_exists: true
+    add_column :spree_taxon_translations, :meta_keywords, :string, if_not_exists: true
+    add_column :spree_taxon_translations, :permalink, :string, if_not_exists: true
+  end
+end
