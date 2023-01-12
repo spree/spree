0cf01c1742b389eebd05646e9c2cb7a817ed715f
wjwitek <weronikaj.witek@gmail.com>
2023-01-12 12:59:10 +0100

    add testnig of optional attributes

--- core/lib/spree/testing_support/factories/google_feed_setting_factory.rb
+++ core/lib/spree/testing_support/factories/google_feed_setting_factory.rb
@@ -1,11 +1,6 @@
 FactoryBot.define do
   factory :google_feed_setting, class: Spree::GoogleFeedSetting do
-    store          { Spree::Store.find(1) }
-    #spree_store_id { 1 }
     id             { 1 }
     brand          { true }
-    material       { false }
-    color          { true }
-    size           { false }
   end
 end
--- core/spec/services/spree/export/google_rss_spec.rb
+++ core/spec/services/spree/export/google_rss_spec.rb
@@ -29,56 +29,82 @@ module Spree
       end
     end
 
-    context 'item attributes are generated correctly' do
+    context 'required item attributes are generated correctly' do
       before do
         allow(subject).to receive(:store).and_return(store)
       end
 
-      context 'required information' do
-        it 'id' do
-          expect(result).to include("<g:id>#{variant.id}</g:id>").once
+      it 'id' do
+        expect(result).to include("<g:id>#{variant.id}</g:id>").once
+      end
+
+      it 'title' do
+        expect(result).to include("<g:title>#{product.name} - #{variant.option_values.first.name}</g:title>").once
+      end
+
+      it 'description' do
+        expect(result).to include("<g:description>#{product.description}</g:description>").once
+      end
+
+      it 'link' do
+        expect(result).to include("<g:link>#{store.url}/#{product.slug}</g:link>").once
+      end
+
+      it 'image link' do
+        expect(result).to include("<g:image_link>#{variant.images.first.plp_url}</g:image_link>").once
+      end
+
+      it 'price' do
+        expect(result).to include("<g:price>#{variant.price} #{variant.cost_currency}</g:price>").once
+      end
+
+      context 'availability date is in the past' do
+        it 'product is in stock' do
+          expect(result).to include('<g:availability>in stock</g:availability>')
         end
 
-        it 'title' do
-          expect(result).to include("<g:title>#{product.name} - #{variant.option_values.first.name}</g:title>").once
+        it 'product availability date is the same' do
+          expect(result).to include("<g:availability_date>#{product.available_on.xmlschema}</g:availability_date>")
+        end
+      end
+
+      context 'availability date is in the future' do
+        let(:product) { create(:product, stores: [store], available_on: 1.year.from_now) }
+
+        it 'product is on backorder' do
+          expect(result).to include('<g:availability>backorder</g:availability>')
+        end
+      end
+
+      context 'availability date is nil' do
+        let(:product) { create(:product, stores: [store], available_on: nil) }
+
+        it 'product is out of stock' do
+          expect(result).to include('<g:availability>out of stock</g:availability>')
         end
 
-        it 'description' do
-          expect(result).to include("<g:description>#{product.description}</g:description>").once
+        it 'product availability date is nil' do
+          expect(result).not_to include('<g:availability_date>')
         end
+      end
+    end
 
-        it 'link' do
-          expect(result).to include("<g:link>#{store.url}/#{product.slug}</g:link>").once
-        end
+    context 'optional item attributes are generated correctly' do
+      let(:product) { create(:product_with_properties) }
 
-        it 'image link' do
-          expect(result).to include("<g:image_link>#{variant.images.first.plp_url}</g:image_link>").once
-        end
+      before do
+        allow(subject).to receive(:store).and_return(store)
+      end
 
-        it 'price' do
-          expect(result).to include("<g:price>#{variant.price} #{variant.cost_currency}</g:price>").once
-        end
+      it 'adds brand to item attributes' do
+        expect(result).to include("<g:brand>#{product.property('brand')}</g:brand>")
+      end
 
-        context 'availability date is in the past' do
-          it 'product is in stock' do
-            expect(result).to include('<g:availability>in stock</g:availability>')
-          end
-        end
+      context 'brand option is set to false' do
+        let(:setting) { create(:google_feed_setting, store: store, brand: false) }
 
-        context 'availability date is in the future' do
-          let(:product) { create(:product, stores: [store], available_on: 1.year.from_now) }
-
-          it 'product is on backorder' do
-            expect(result).to include('<g:availability>backorder</g:availability>')
-          end
-        end
-
-        context 'availability date is nil' do
-          let(:product) { create(:product, stores: [store], available_on: nil) }
-
-          it 'product is out of stock' do
-            expect(result).to include('<g:availability>out of stock</g:availability>')
-          end
+        it 'does not add brand to item attributes' do
+          expect(result).not_to include('<g:brand>')
         end
       end
     end
