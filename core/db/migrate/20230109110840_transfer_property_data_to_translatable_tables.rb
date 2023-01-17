4b997b77834b7fc4b3c0d3cc04e0a4ed594bead2
nciemniak <nciemniak@gmail.com>
2023-01-17 11:40:43 +0100

    update migrations for mysql compatibility

--- core/db/migrate/20230109110840_transfer_property_data_to_translatable_tables.rb
+++ core/db/migrate/20230109110840_transfer_property_data_to_translatable_tables.rb
@@ -7,25 +7,27 @@ class TransferPropertyDataToTranslatableTables < ActiveRecord::Migration[6.1]
 
     if not Spree::Property::Translation.exists?
       ActiveRecord::Base.connection.execute("
-      INSERT INTO spree_property_translations (name, presentation, filter_param, locale, spree_property_id, created_at, updated_at)
-      SELECT name, presentation, filter_param, '#{DEFAULT_LOCALE}', id, created_at, updated_at
-      FROM spree_properties;
-
-      UPDATE spree_properties
-      SET name=null, presentation=null, filter_param=null;
-    ")
+        INSERT INTO spree_property_translations (name, presentation, filter_param, locale, spree_property_id, created_at, updated_at)
+        SELECT name, presentation, filter_param, '#{DEFAULT_LOCALE}', id, created_at, updated_at
+        FROM spree_properties;
+      ")
+      ActiveRecord::Base.connection.execute("
+        UPDATE spree_properties
+        SET name=null, presentation=null, filter_param=null;
+      ")
     end
 
     # Product Properties
     if not Spree::ProductProperty::Translation.exists?
       ActiveRecord::Base.connection.execute("
-      INSERT INTO spree_product_property_translations (value, filter_param, locale, spree_product_property_id, created_at, updated_at)
-      SELECT value, filter_param, '#{DEFAULT_LOCALE}', id, created_at, updated_at
-      FROM spree_product_properties;
-
-      UPDATE spree_product_properties
-      SET value=null, filter_param=null;
-    ")
+        INSERT INTO spree_product_property_translations (value, filter_param, locale, spree_product_property_id, created_at, updated_at)
+        SELECT value, filter_param, '#{DEFAULT_LOCALE}', id, created_at, updated_at
+        FROM spree_product_properties;
+      ")
+      ActiveRecord::Base.connection.execute("
+        UPDATE spree_product_properties
+        SET value=null, filter_param=null;
+      ")
     end
   end
 
@@ -38,7 +40,8 @@ class TransferPropertyDataToTranslatableTables < ActiveRecord::Migration[6.1]
       SET (name, presentation, filter_param) = (t_properties.name, t_properties.presentation, t_properties.filter_param)
       FROM spree_property_translations AS t_properties
       WHERE t_properties.spree_property_id = properties.id;
-
+    ")
+    ActiveRecord::Base.connection.execute("
       TRUNCATE TABLE spree_property_translations;
     ")
 
@@ -48,7 +51,8 @@ class TransferPropertyDataToTranslatableTables < ActiveRecord::Migration[6.1]
       SET (value, filter_param) = (t_product_properties.value, t_product_properties.filter_param)
       FROM spree_product_property_translations AS t_product_properties
       WHERE t_product_properties.spree_product_property_id = product_properties.id;
-
+    ")
+    ActiveRecord::Base.connection.execute("
       TRUNCATE TABLE spree_product_property_translations;
     ")
   end
