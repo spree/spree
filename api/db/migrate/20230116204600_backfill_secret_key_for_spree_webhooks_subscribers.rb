39a2b93e890dddaafdfd26ead82983b7149426d3
Stefan Vermaas <stefanvermaas@pm.me>
2023-02-13 20:24:07 +0100

    Downgrade migrations to Rails 6.1

--- api/db/migrate/20230116204600_backfill_secret_key_for_spree_webhooks_subscribers.rb
+++ api/db/migrate/20230116204600_backfill_secret_key_for_spree_webhooks_subscribers.rb
@@ -1,4 +1,4 @@
-class BackfillSecretKeyForSpreeWebhooksSubscribers < ActiveRecord::Migration[7.0]
+class BackfillSecretKeyForSpreeWebhooksSubscribers < ActiveRecord::Migration[6.1]
   def change
     Spree::Webhooks::Subscriber.where(secret_key: nil).find_each(&:regenerate_secret_key)
   end
--- api/db/migrate/20230116205000_change_secret_key_to_non_null_column.rb
+++ api/db/migrate/20230116205000_change_secret_key_to_non_null_column.rb
@@ -1,4 +1,4 @@
-class ChangeSecretKeyToNonNullColumn < ActiveRecord::Migration[7.0]
+class ChangeSecretKeyToNonNullColumn < ActiveRecord::Migration[6.1]
   def change
     change_column_null :spree_webhooks_subscribers, :secret_key, false
   end
