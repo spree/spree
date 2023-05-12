42184dcc7033898edbad46a7c544ff2c5e24f0d0
Rafal Cymerys <rafal@upsidelab.io>
2023-05-12 16:38:49 +0200

    WIP

--- core/lib/spree/testing_support/capybara_config.rb
+++ core/lib/spree/testing_support/capybara_config.rb
@@ -1,10 +1,24 @@
 require 'capybara-screenshot/rspec'
 
+Capybara.register_driver :selenium_chrome_headless_ci do |app|
+  version = Capybara::Selenium::Driver.load_selenium
+  options_key = Capybara::Selenium::Driver::CAPS_VERSION.satisfied_by?(version) ? :capabilities : :options
+  browser_options = ::Selenium::WebDriver::Chrome::Options.new.tap do |opts|
+    opts.add_argument('--headless')
+    opts.add_argument('--disable-gpu') if Gem.win_platform?
+    # Workaround https://bugs.chromium.org/p/chromedriver/issues/detail?id=2650&q=load&sort=-id&colspec=ID%20Status%20Pri%20Owner%20Summary
+    opts.add_argument('--disable-site-isolation-trials')
+    opts.add_argument('--whitelisted-ips=')
+  end
+
+  Capybara::Selenium::Driver.new(app, **{ :browser => :chrome, options_key => browser_options })
+end
+
 Capybara.configure do |config|
   config.save_path = ENV['CIRCLE_ARTIFACTS'] if ENV['CIRCLE_ARTIFACTS']
   config.server = :puma
   config.default_driver = :rack_test
-  config.javascript_driver = :selenium_chrome_headless
+  config.javascript_driver = :selenium_chrome_headless_ci
   config.default_max_wait_time = ENV.fetch('CAPYBARA_MAX_WAIT_TIME', 45).to_i
   config.always_include_port = true
   config.match = :smart
