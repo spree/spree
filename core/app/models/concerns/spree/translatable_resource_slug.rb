8a250543df121464b7e1822fbb05b92416c05e6b
Rafal Cymerys <rafal@upsidelab.io>
2023-07-12 16:20:15 +0200

    Set fallback locale for Mobility based on current store

--- core/app/models/concerns/spree/translatable_resource_slug.rb
+++ core/app/models/concerns/spree/translatable_resource_slug.rb
@@ -4,12 +4,10 @@ module Spree
 
     included do
       def localized_slugs_for_store(store)
-        localized_slugs = Hash[translations.pluck(:locale, :slug)]
-        default_locale = store.default_locale
         supported_locales = store.supported_locales_list
 
         supported_locales.each_with_object({}) do |locale, hash|
-          hash[locale] = localized_slugs[locale] || localized_slugs[default_locale]
+          hash[locale] = I18n.with_locale(locale) { slug }
         end
       end
     end
