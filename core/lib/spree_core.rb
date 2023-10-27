217e261fca43fcf4c61456803380fb58f267e1fa
Rafal Cymerys <rafal@upsidelab.io>
2023-10-27 18:33:12 +0200

    Support Rails 7.1 in Ransack

--- core/lib/spree_core.rb
+++ core/lib/spree_core.rb
@@ -1,4 +1,7 @@
 require 'friendly_id/paranoia'
 require 'mobility/plugins/store_based_fallbacks'
+if Rails::VERSION::STRING >= '7.1.0'
+  require 'ransack/context_decorator'
+end
 
 require 'spree/core'
