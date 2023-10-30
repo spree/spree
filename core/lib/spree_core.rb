b19e4f7912bfaa0e5f3d6b7d27c46a103901217f
Rafal Cymerys <rafal@upsidelab.io>
2023-10-30 19:43:27 +0100

    Upgrade ransack and remove momkey patch

--- core/lib/spree_core.rb
+++ core/lib/spree_core.rb
@@ -1,7 +1,4 @@
 require 'friendly_id/paranoia'
 require 'mobility/plugins/store_based_fallbacks'
-if Rails::VERSION::STRING >= '7.1.0'
-  require 'ransack/context_decorator'
-end
 
 require 'spree/core'
