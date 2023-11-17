336d078c87a49dc6428e7810cc05b850b54002ce
Tomasz Donarski <tomasz.donarski.dev@gmail.com>
2023-11-17 10:09:32 +0100

    Add FK to a migration

--- core/db/migrate/20231114182646_add_promotion_batch_reference_to_promotions.rb
+++ core/db/migrate/20231114182646_add_promotion_batch_reference_to_promotions.rb
@@ -1,5 +1,5 @@
 class AddPromotionBatchReferenceToPromotions < ActiveRecord::Migration[7.1]
   def change
-    add_reference(:spree_promotions, :promotion_batch)
+    add_reference(:spree_promotions, :promotion_batch, foreign_key: { to_table: :spree_promotion_batches})
   end
 end
