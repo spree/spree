6f53d8405d4b8d67fe0dc19817b03a990c9aeab1
Mateusz Porębski <mateusz.porebski99@gmail.com>
2023-11-27 12:19:34 +0100

    Reversed changes in config.yml, deleted bash files

--- bin/brakeman.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-set -euxo pipefail
-
-./bin/build-ci.rb install
-bundle exec brakeman -p api/ --ignore-config api/brakeman.ignore --skip-files app/controllers/spree/api/v1/ --exit-on-warn --exit-on-error
-bundle exec brakeman -p core/ --ignore-config core/brakeman.ignore --exit-on-warn --exit-on-error
