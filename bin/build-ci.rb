2dd9c640c7ac2d58636f15699686ebf484089790
Mateusz Porębski <mateusz.porebski99@gmail.com>
2023-11-28 17:47:10 +0100

    restore old version of build-ci.rb

--- bin/build-ci.rb
+++ bin/build-ci.rb
@@ -105,8 +105,11 @@ class Project
   #
   # @return [Boolean]
   #   the success of the tests
+  # def run_tests
+  #   system("circleci tests glob \"./**/*_spec.rb\" | circleci tests run --command=\"xargs --verbose bundle exec rspec #{rspec_arguments.join(' ')}\" --verbose")
+  # end
   def run_tests
-    system("circleci tests glob \"./**/*_spec.rb\" | circleci tests run --command=\"xargs --verbose bundle exec rspec #{rspec_arguments.join(' ')}\" --verbose")
+    system("bundle exec rspec #{rspec_arguments.join(' ')}")
   end
 
   def rspec_arguments(custom_name = name)
@@ -168,8 +171,8 @@ class Project
   #
   # @return [Array<Project>]
   def self.current_projects
-    # NODE_INDEX.step(ALL.length - 1, NODE_TOTAL).map(&ALL.method(:fetch))
-    ALL
+    NODE_INDEX.step(ALL.length - 1, NODE_TOTAL).map(&ALL.method(:fetch))
+    # ALL
   end
   private_class_method :current_projects
 
