e94615faaa4106e18d1b986a8350e9dbf8ffd3f2
Damian Legawiec <damian@getvendo.com>
2024-02-24 22:32:58 +0100

    Update test.rb

--- core/lib/generators/spree/dummy/templates/rails/test.rb
+++ core/lib/generators/spree/dummy/templates/rails/test.rb
@@ -15,7 +15,7 @@ Rails.application.configure do
   # this is usually not necessary, and can slow down your test suite. However, it's
   # recommended that you enable it in continuous integration systems to ensure eager
   # loading is working properly before deploying your code.
-  config.eager_load = ENV["CI"].present?
+  config.eager_load = false
 
   # Configure public file server for tests with Cache-Control for performance.
   config.public_file_server.enabled = true
