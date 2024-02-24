17aa12fca0aefcd6f7db5e9a05314c4e2afa6863
Damian Legawiec <damian@getvendo.com>
2024-02-24 22:39:51 +0100

    fixes

--- core/lib/generators/spree/dummy/templates/rails/test.rb
+++ core/lib/generators/spree/dummy/templates/rails/test.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   # config.action_view.annotate_rendered_view_with_filenames = true
 
   # Raise error when a before_action's only/except options reference missing actions
-  config.action_controller.raise_on_missing_callback_actions = true
+  # config.action_controller.raise_on_missing_callback_actions = true
 
   routes.default_url_options = { host: 'localhost', port: 3000 }
 end
