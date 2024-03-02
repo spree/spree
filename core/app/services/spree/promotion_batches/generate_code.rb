bc30c632ddf0bded7f4391d4bc12c4c423a26150
Damian Legawiec <damian@sparksolutions.co>
2024-03-02 10:09:00 +0100

    Revert "Fix/generate codes with correct length" (#12008)

--- core/app/services/spree/promotion_batches/generate_code.rb
+++ core/app/services/spree/promotion_batches/generate_code.rb
@@ -6,10 +6,9 @@ module Spree
       end
 
       def call(random_characters:, prefix: nil, suffix: nil)
-        bytes_to_generate = random_characters - random_characters.div(2)
         [
           prefix,
-          @random.hex(bytes_to_generate)[...random_characters].upcase,
+          @random.hex(random_characters).upcase,
           suffix
         ].join('')
       end
