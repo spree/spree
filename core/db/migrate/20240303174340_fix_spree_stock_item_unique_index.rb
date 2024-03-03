7e02dc9c5b9394daf61196e60b21ecf8442495d5
Damian Legawiec <damian@getvendo.com>
2024-03-03 18:52:47 +0100

    Update 20240303174340_fix_spree_stock_item_unique_index.rb

--- core/db/migrate/20240303174340_fix_spree_stock_item_unique_index.rb
+++ core/db/migrate/20240303174340_fix_spree_stock_item_unique_index.rb
@@ -1,4 +1,4 @@
-class FixSpreeStockItemUniqueIndex < ActiveRecord::Migration[7.1]
+class FixSpreeStockItemUniqueIndex < ActiveRecord::Migration[6.1]
   def change
     remove_index :spree_stock_items, name: 'stock_item_by_loc_var_id_deleted_at' if index_exists?(:spree_stock_items, [:stock_location_id, :variant_id], name: 'stock_item_by_loc_var_id_deleted_at')
 
