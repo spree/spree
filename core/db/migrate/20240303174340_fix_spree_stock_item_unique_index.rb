aeea4699141181ef7481441afdb342d748bd175c
Damian Legawiec <damian@getvendo.com>
2024-03-03 19:45:31 +0100

    Update 20240303174340_fix_spree_stock_item_unique_index.rb

--- core/db/migrate/20240303174340_fix_spree_stock_item_unique_index.rb
+++ core/db/migrate/20240303174340_fix_spree_stock_item_unique_index.rb
@@ -5,7 +5,22 @@ class FixSpreeStockItemUniqueIndex < ActiveRecord::Migration[6.1]
     unless index_exists?(:spree_stock_items, ['variant_id', 'stock_location_id'], name: 'index_spree_stock_items_unique_without_deleted_at')
       # MySQL doesn't support partial indexes
       if ActiveRecord::Base.connection.adapter_name == 'Mysql2'
-        add_index(:spree_stock_items, ['variant_id', 'stock_location_id', 'deleted_at'], name: 'index_spree_stock_items_unique_without_deleted_at', unique: true)
+        reversible do |dir|
+          dir.up do
+            execute <<-SQL
+              CREATE UNIQUE INDEX index_spree_stock_items_unique_without_deleted_at
+              ON spree_stock_items(
+                stock_location_id,
+                variant_id,
+                (COALESCE(deleted_at, CAST('1970-01-01' AS DATETIME)))
+              );
+            SQL
+          end
+
+          dir.down do
+            remove_index :spree_stock_items, name: :index_spree_stock_items_unique_without_deleted_at
+          end
+        end
       else
         add_index(
           :spree_stock_items,
