5b330b27eabc464420f80904188bbb6a90eb41d5
Damian Legawiec <damian@sparksolutions.co>
2024-06-23 11:06:38 +0200

    Improve `TaxRate#label` to display it right (#12060)
    * Improve `TaxRate#label` to display it right
    
    * Update tax_rate.rb
    
    * fixes

--- core/app/models/spree/tax_rate.rb
+++ core/app/models/spree/tax_rate.rb
@@ -115,11 +115,13 @@ module Spree
 
     def amount_for_label
       return '' unless show_rate_in_label?
+      return '' if amount.zero?
 
       ' ' + ActiveSupport::NumberHelper::NumberToPercentageConverter.convert(
         amount * 100,
         locale: I18n.locale,
-        strip_insignificant_zeros: true
+        strip_insignificant_zeros: true,
+        precision: 2
       )
     end
   end
