649a8d4a79507d357d8fdf49b09ea90313000a55
Łukasz Adamczyk <lukas.adamczyk@getvendo.com>
2024-07-15 11:07:41 +0200

    Add Spree::Seeds::PaymentMethods for creating a Store Credit payment method

---
+++ core/app/services/spree/seeds/payment_methods.rb
@@ -0,0 +1,20 @@
+module Spree
+  module Seeds
+    class PaymentMethods
+      prepend Spree::ServiceModule::Base
+
+      def call
+        store = Spree::Store.default
+
+        payment_method = Spree::PaymentMethod::StoreCredit.find_or_initialize_by(
+          name: 'Store Credit',
+          description: 'Store Credit',
+          active: true
+        )
+
+        payment_method.stores << store if payment_method.new_record?
+        payment_method.save!
+      end
+    end
+  end
+end
