aa0d5c63ee1b6f822d8c38eb7f93902b582c6bb5
Damian Legawiec <damian@sparksolutions.co>
2024-07-25 15:50:24 +0200

    Update core/db/migrate/20240725124530_add_refunder_to_spree_refunds.rb

--- core/db/migrate/20240725124530_add_refunder_to_spree_refunds.rb
+++ core/db/migrate/20240725124530_add_refunder_to_spree_refunds.rb
@@ -1,4 +1,4 @@
-class AddRefunderToSpreeRefunds < ActiveRecord::Migration[7.1]
+class AddRefunderToSpreeRefunds < ActiveRecord::Migration[6.1]
   def change
     add_reference :spree_refunds, :refunder, index: true, if_not_exists: true
   end
