29d77d0c109b9614a6a3e87d3d78c69de0dfc1af
Łukasz Adamczyk <lukas.adamczyk@getvendo.com>
2024-07-25 16:01:00 +0200

    Correct `db/migrate/20240725124530_add_refunder_to_spree_refunds.rb` migration

--- core/db/migrate/20240725124530_add_refunder_to_spree_refunds.rb
+++ core/db/migrate/20240725124530_add_refunder_to_spree_refunds.rb
@@ -1,5 +1,6 @@
 class AddRefunderToSpreeRefunds < ActiveRecord::Migration[6.1]
   def change
-    add_reference :spree_refunds, :refunder, index: true, if_not_exists: true
+    add_column :spree_refunds, :refunder_id, :bigint, if_not_exists: true
+    add_index :spree_refunds, :refunder_id, if_not_exists: true
   end
 end
