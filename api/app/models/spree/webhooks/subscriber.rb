5a0f7788285c82178d49d40174ab0d96c63d9f68
Damian Legawiec <damian@sparksolutions.co>
2024-07-29 10:40:00 +0200

    Added `WebhookSubscriber#latest_event_at` (#12090)
    * Added `WebhookSubscriber#latest_event_at`
    
    * Update subscriber.rb

--- api/app/models/spree/webhooks/subscriber.rb
+++ api/app/models/spree/webhooks/subscriber.rb
@@ -25,6 +25,10 @@ module Spree
 
       before_save :parse_subscriptions
 
+      def latest_event_at
+        events.order(:created_at).last&.created_at
+      end
+      
       def self.with_urls_for(event)
         where(
           case ActiveRecord::Base.connection.adapter_name
