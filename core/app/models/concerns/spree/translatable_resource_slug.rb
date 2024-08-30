0c16f289a3d5fadce82eb7fcddc7046d533d34c0
Damian Legawiec <damian@getvendo.com>
2024-08-30 09:34:18 +0200

    skip using `I18n.with_locale`

--- core/app/models/concerns/spree/translatable_resource_slug.rb
+++ core/app/models/concerns/spree/translatable_resource_slug.rb
@@ -5,9 +5,8 @@ module Spree
     included do
       def localized_slugs_for_store(store)
         supported_locales = store.supported_locales_list
-
         supported_locales.each_with_object({}) do |locale, hash|
-          hash[locale] = I18n.with_locale(locale) { slug }
+          hash[locale] = translations.find_by(locale: locale)&.slug || translations.find_by(locale: store.default_locale)&.slug || slug
         end
       end
     end
