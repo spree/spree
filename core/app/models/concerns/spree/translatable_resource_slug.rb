1aab9a9f5d0931493c5cb29b9ac03e9ab995c20a
Damian Legawiec <damian@getvendo.com>
2024-09-04 18:16:01 +0200

    Update translatable_resource_slug.rb

--- core/app/models/concerns/spree/translatable_resource_slug.rb
+++ core/app/models/concerns/spree/translatable_resource_slug.rb
@@ -5,6 +5,7 @@ module Spree
     included do
       def localized_slugs_for_store(store)
         supported_locales = store.supported_locales_list
+
         supported_locales.each_with_object({}) do |locale, hash|
           hash[locale] = I18n.with_locale(locale) { slug }
         end
