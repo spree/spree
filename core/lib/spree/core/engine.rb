1e7286dfbfbeb4aef56682c510f161834c1abbe8
Damian Legawiec <damian@getvendo.com>
2024-09-06 14:31:31 +0200

    Fixed setting yaml_column_permitted_classes when it's nil (#12153)

--- core/lib/spree/core/engine.rb
+++ core/lib/spree/core/engine.rb
@@ -26,6 +26,7 @@ module Spree
 
       initializer 'spree.environment', before: :load_config_initializers do |app|
         app.config.spree = Environment.new(SpreeCalculators.new, Spree::Core::Configuration.new, Spree::Core::RuntimeConfiguration.new, Spree::Core::Dependencies.new)
+        app.config.active_record.yaml_column_permitted_classes ||= []
         app.config.active_record.yaml_column_permitted_classes << [Symbol, BigDecimal, ActiveSupport::HashWithIndifferentAccess]
         Spree::Config = app.config.spree.preferences
         Spree::RuntimeConfig = app.config.spree.runtime_preferences
