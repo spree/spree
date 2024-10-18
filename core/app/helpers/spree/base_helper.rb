9d98ae42dbe8df393bc067e9c35c6f68a1515508
Damian Legawiec <damian@getvendo.com>
2024-10-18 07:13:13 -0700

    Update core/app/helpers/spree/base_helper.rb

--- core/app/helpers/spree/base_helper.rb
+++ core/app/helpers/spree/base_helper.rb
@@ -218,6 +218,11 @@ module Spree
       product_or_variant.default_image
     end
 
+    def base_cache_key
+      Spree::Deprecation.warn('`base_cache_key` is deprecated and will be removed in Spree 6. Please use `spree_base_cache_key` instead')
+      spree_base_cache_key
+    end
+
     def spree_base_cache_key
       [I18n.locale, current_currency, defined?(try_spree_current_user) && try_spree_current_user.present?,
        defined?(try_spree_current_user) && try_spree_current_user.try(:has_spree_role?, 'admin')]
