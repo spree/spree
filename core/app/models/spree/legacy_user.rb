edfdd3f4136f01bcd00e49d5744dbfca8519e0ee
Damian Legawiec <damian@getvendo.com>
2024-11-08 12:18:13 +0100

    Fixed `ExportMailer`

--- core/app/models/spree/legacy_user.rb
+++ core/app/models/spree/legacy_user.rb
@@ -8,6 +8,6 @@ module Spree
 
     self.table_name = 'spree_users'
 
-    attr_accessor :password, :password_confirmation, :firstname, :lastname
+    attr_accessor :password, :password_confirmation, :firs_tname, :last_name
   end
 end
