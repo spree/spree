5fac3276ac403206d7bb4d8db961927eafc1e9ca
Damian Legawiec <damian@getvendo.com>
2024-11-14 12:07:37 +0100

    Open source new admin settings (part 1)

---
+++ admin/app/controllers/spree/admin/return_authorization_reasons_controller.rb
@@ -0,0 +1,6 @@
+module Spree
+  module Admin
+    class ReturnAuthorizationReasonsController < ResourceController
+    end
+  end
+end
---
+++ admin/app/controllers/spree/admin/zones_controller.rb
@@ -0,0 +1,26 @@
+module Spree
+  module Admin
+    class ZonesController < ResourceController
+      before_action :load_data, except: :index
+
+      def new
+        @zone.zone_members.build
+      end
+
+      protected
+
+      def collection
+        params[:q] ||= {}
+        params[:q][:s] ||= 'name asc'
+        @search = super.ransack(params[:q])
+        @zones = @search.result.page(params[:page]).per(params[:per_page])
+      end
+
+      def load_data
+        @countries = Country.order(:name)
+        @states = State.order(:name)
+        @zones = Zone.order(:name)
+      end
+    end
+  end
+end
