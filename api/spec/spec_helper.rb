7510b7dd6411be0c9d9962fe0b6b02953e2592ec
Łukasz Adamczyk <lukas.adamczyk@getvendo.com>
2024-11-28 16:22:47 +0100

    Update API specs

--- api/spec/spec_helper.rb
+++ api/spec/spec_helper.rb
@@ -88,6 +88,8 @@ RSpec.configure do |config|
     # Request specs to paths with ?locale=xx don't reset the locale afterwards
     # Some tests assume that the current locale is :en, so we ensure it here
     I18n.locale = :en
+
+    ActiveJob::Base.queue_adapter = :test
   end
 
   config.order = :random
