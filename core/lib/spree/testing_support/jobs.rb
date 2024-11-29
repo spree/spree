add1c7603913819b27823e3c2fbeb9bd5a05e9c0
Damian Legawiec <damian@getvendo.com>
2024-11-29 10:54:16 +0100

    Update jobs.rb

--- core/lib/spree/testing_support/jobs.rb
+++ core/lib/spree/testing_support/jobs.rb
@@ -1,11 +1,3 @@
 RSpec.configure do |config|
   config.include ActiveJob::TestHelper
-
-  config.before(:each, :job) do
-    ActiveJob::Base.queue_adapter = :test
-  end
-
-  config.after(:each, :job) do
-    ActiveJob::Base.queue_adapter = :inline
-  end
 end
