6466d136f0af1b2e93279129c58e1734dca6850e
Łukasz Adamczyk <lukas.adamczyk@getvendo.com>
2025-01-23 14:58:24 +0100

    Fix removing store images

---
+++ core/db/migrate/20250123135358_add_custom_code_fields_to_spree_store.rb
@@ -0,0 +1,7 @@
+class AddCustomCodeFieldsToSpreeStore < ActiveRecord::Migration[6.1]
+  def change
+    add_column :spree_stores, :storefront_custom_code_head, :text
+    add_column :spree_stores, :storefront_custom_code_body_start, :text
+    add_column :spree_stores, :storefront_custom_code_body_end, :text
+  end
+end
