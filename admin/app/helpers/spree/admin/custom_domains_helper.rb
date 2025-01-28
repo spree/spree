f8639e5e21979c33610153ee15f126a69f8895a8
Damian Legawiec <damian@getvendo.com>
2025-01-28 18:56:26 +0100

    Update custom_domains_helper.rb

--- admin/app/helpers/spree/admin/custom_domains_helper.rb
+++ admin/app/helpers/spree/admin/custom_domains_helper.rb
@@ -2,7 +2,7 @@ module Spree
   module Admin
     module CustomDomainsHelper
       def entri_enabled?
-        defined?(Entri) && Rails.application.credentials.dig(:entri, :app_id).present? && Rails.application.credentials.dig(:entri, :secret).present?
+        false
       end
     end
   end
