ba78960e23132cd22b20bb4ec3661e91fff82718
Damian Legawiec <damian@getvendo.com>
2025-01-29 14:52:47 +0100

    Update tests_database_ci.sh

--- bin/tests_database_ci.sh
+++ bin/tests_database_ci.sh
@@ -2,6 +2,7 @@
 
 set -euxo pipefail
 
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 32EE5355A6BC6E42
 sudo apt-get update && sudo apt-get install libvips42
 bundle install
 ./bin/build-ci.rb install
