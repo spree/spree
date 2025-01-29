554ec2d911c0e74db23d3e879368ef0807261311
Damian Kaczmarczyk <git@vegann.space>
2025-01-29 09:56:39 +0100

    Draft metadata editor

--- admin/app/helpers/spree/admin/dropdown_helper.rb
+++ admin/app/helpers/spree/admin/dropdown_helper.rb
@@ -1,11 +1,20 @@
 module Spree
   module Admin
-    module TranslationsHelper
+    module DropdownHelper
       def link_to_edit_translations(resource, classes: 'text-left dropdown-item')
         link_to_with_icon(
           'language',
           Spree.t(:translations),
           spree.edit_admin_translation_path(resource, resource_type: resource.class.to_s),
+          class: classes
+        )
+      end
+
+      def link_to_edit_metadata(resource, classes: 'text-left dropdown-item')
+        link_to_with_icon(
+          'database',
+          Spree.t(:metadata),
+          spree.edit_admin_metadata_path(resource, resource_type: resource.class.to_s),
           class: classes
         )
       end
