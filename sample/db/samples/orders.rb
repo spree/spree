73c819731bfd4cec27be5fe96ba156a89bc35c08
Łukasz Adamczyk <lukas.adamczyk@getvendo.com>
2025-01-29 13:13:22 +0100

    Fix orders sample

--- sample/db/samples/orders.rb
+++ sample/db/samples/orders.rb
@@ -12,9 +12,9 @@ orders << store.orders.where(
   email: 'spree@example.com',
   currency: 'USD'
 ).first_or_create! do |order|
-  order.item_total = product_1.master.amount_in(order.currency)
-  order.adjustment_total = product_1.master.amount_in(order.currency)
-  order.total = product_1.master.amount_in(order.currency) * 2
+  order.item_total = product_1.default_variant.amount_in(order.currency)
+  order.adjustment_total = product_1.default_variant.amount_in(order.currency)
+  order.total = product_1.default_variant.amount_in(order.currency) * 2
 end
 
 orders << store.orders.where(
@@ -22,26 +22,26 @@ orders << store.orders.where(
   email: 'spree@example.com',
   currency: 'USD'
 ).first_or_create! do |order|
-  order.item_total = product_2.master.amount_in(order.currency)
-  order.adjustment_total = product_2.master.amount_in(order.currency)
-  order.total = product_2.master.amount_in(order.currency) * 2
+  order.item_total = product_2.default_variant.amount_in(order.currency)
+  order.adjustment_total = product_2.default_variant.amount_in(order.currency)
+  order.total = product_2.default_variant.amount_in(order.currency) * 2
   order.shipping_address = Spree::Address.first
   order.billing_address = Spree::Address.last
 end
 
 unless orders[0].line_items.any?
   orders[0].line_items.new(
-    variant: product_1.master,
+    variant: product_1.default_variant,
     quantity: 1,
-    price: product_1.master.amount_in(orders[0].currency)
+    price: product_1.default_variant.amount_in(orders[0].currency)
   ).save!
 end
 
 unless orders[1].line_items.any?
   orders[1].line_items.new(
-    variant: product_2.master,
+    variant: product_2.default_variant,
     quantity: 1,
-    price: product_1.master.amount_in(orders[1].currency)
+    price: product_1.default_variant.amount_in(orders[1].currency)
   ).save!
 end
 
