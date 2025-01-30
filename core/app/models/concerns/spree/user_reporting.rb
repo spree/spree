8408d58b794f099195b113508aebbe11a6d9330d
Damian Legawiec <damian@getvendo.com>
2025-01-30 15:20:52 +0100

    More reporting methods for User class

--- core/app/models/concerns/spree/user_reporting.rb
+++ core/app/models/concerns/spree/user_reporting.rb
@@ -6,7 +6,7 @@ module Spree
     def report_values_for(report_name, store)
       store ||= Store.default
 
-      completed_orders(store).pluck(:currency).uniq.each_with_object([]) do |currency, arr|
+      completed_orders_for_store(store).pluck(:currency).uniq.each_with_object([]) do |currency, arr|
         arr << send("display_#{report_name}", store: store, currency: currency)
       end
     end
@@ -31,16 +31,24 @@ module Spree
                       column: :all)
     end
 
+    def amount_spent_in(currency)
+      completed_orders.where(currency: currency).sum(:total)
+    end
+
+    def display_amount_spent_in(currency)
+      Money.new(amount_spent_in(currency), currency: currency).to_html
+    end
+
     private
 
     def order_calculate(operation:, column:, store: nil, currency: nil)
       store ||= Store.default
       currency ||= store.default_currency
 
-      completed_orders(store).where(currency: currency).calculate(operation, column) || BigDecimal('0.00')
+      completed_orders_for_store(store).where(currency: currency).calculate(operation, column) || BigDecimal('0.00')
     end
 
-    def completed_orders(store)
+    def completed_orders_for_store(store)
       orders.for_store(store).complete.order(currency: :desc)
     end
   end
