f6be6165295d51f17ee3b49ef2f2f04fc0f6271f
Damian Legawiec <damian@getvendo.com>
2025-01-31 17:28:56 +0100

    [Skip CI] Update announcement_bar.rb

--- core/app/models/spree/page_sections/announcement_bar.rb
+++ core/app/models/spree/page_sections/announcement_bar.rb
@@ -5,6 +5,7 @@ module Spree
 
       before_validation :set_default_text, on: :create
 
+      BACKGROUND_COLOR_DEFAULT = '#F5F5F4'
       TOP_PADDING_DEFAULT = 8
       BOTTOM_PADDING_DEFAULT = 8
       TOP_BORDER_WIDTH_DEFAULT = 0
