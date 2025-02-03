d58cddfb23a9ecddaedc46431684f26be452e1d7
Łukasz Adamczyk <lukas.adamczyk@getvendo.com>
2025-02-03 11:28:23 +0100

    Fix initializing the color picker, hide Taxon List page in the page builder

--- admin/app/javascript/spree/admin/controllers/color_picker_controller.js
+++ admin/app/javascript/spree/admin/controllers/color_picker_controller.js
@@ -13,7 +13,7 @@ export default class extends Controller {
     this.picker = Pickr.create({
       el: this.pickerTarget,
       theme: 'classic',
-      default: this.inputTarget.value.length > 0 ? this.inputTargetValue : this.defaultColorValue,
+      default: this.inputTarget.value.length > 0 ? this.inputTarget.value : this.defaultColorValue,
       useAsButton: true,
 
       components: {
