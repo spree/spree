9cecd776f15e97fb762524d3949cfd730b7fbff9
Łukasz Adamczyk <lukas.adamczyk@getvendo.com>
2025-02-05 11:12:22 +0100

    Use `Spree::Store#admin_users` for post authors list

--- admin/app/helpers/spree/admin/posts_helper.rb
+++ admin/app/helpers/spree/admin/posts_helper.rb
@@ -2,7 +2,7 @@ module Spree
   module Admin
     module PostsHelper
       def post_authors_select_options
-        Spree.admin_user_class.spree_admin.map { |user| [user.name, user.id] }
+        current_store.admin_users.map { |user| [user.name, user.id] }
       end
     end
   end
