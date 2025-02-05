995146c00c73a05d8ed618536d1d584a91dffebf
Łukasz Adamczyk <lukas.adamczyk@getvendo.com>
2025-02-05 09:36:24 +0100

    Open source Post and Post Category admin views

--- admin/app/helpers/spree/admin/tags_helper.rb
+++ admin/app/helpers/spree/admin/tags_helper.rb
@@ -8,6 +8,17 @@ module Spree
       def tags_json_array
         @tags_json_array ||= tags_scope.pluck(:id, :name).map { |id, name| { id: id, name: name } }.as_json
       end
+
+      def post_tags_scope
+        @post_tags_scope ||= ActsAsTaggableOn::Tag.
+                             joins(:taggings).
+                             where('taggings.taggable_type = ?', 'Spree::Post').
+                             for_context(:tags)
+      end
+
+      def post_tags_json_array
+        @post_tags_json_array ||= post_tags_scope.pluck(:id, :name).map { |id, name| { id: id, name: name } }.as_json
+      end
     end
   end
 end
