0dc2427482c72256110e5c3072d699ca26fefdd2
Damian Legawiec <damian@getvendo.com>
2025-02-08 15:28:42 +0100

    Add ability to include custom head partials in storefront
    This will be useful for gem / integration authors to inject custom JS. etc

--- storefront/lib/spree/storefront/engine.rb
+++ storefront/lib/spree/storefront/engine.rb
@@ -3,11 +3,14 @@ require_relative 'runtime_configuration'
 module Spree
   module Storefront
     class Engine < ::Rails::Engine
+      Environment = Struct.new(:head_partials)
+
       initializer 'spree.storefront.configuration', before: :load_config_initializers do |_app|
         Spree::Storefront::RuntimeConfig = Spree::Storefront::RuntimeConfiguration.new
       end
 
       initializer 'spree.storefront.assets' do |app|
+        app.config.spree_storefront = Environment.new
         app.config.assets.paths << root.join('app/javascript')
         app.config.assets.paths << root.join('vendor/javascript')
         app.config.assets.precompile += %w[spree_storefront_manifest]
@@ -17,6 +20,10 @@ module Spree
         app.config.importmap.paths << root.join('config/importmap.rb')
         # https://github.com/rails/importmap-rails?tab=readme-ov-file#sweeping-the-cache-in-development-and-test
         app.config.importmap.cache_sweepers << root.join('app/javascript')
+      end
+
+      config.after_initialize do
+        Rails.application.config.spree_storefront.head_partials = []
       end
     end
   end
