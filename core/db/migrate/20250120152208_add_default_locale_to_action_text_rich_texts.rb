8f319974481539ee79e95c5d782b926096bcd658
Damian Legawiec <damian@getvendo.com>
2025-02-10 14:17:25 +0100

    Update 20250120152208_add_default_locale_to_action_text_rich_texts.rb

--- core/db/migrate/20250120152208_add_default_locale_to_action_text_rich_texts.rb
+++ core/db/migrate/20250120152208_add_default_locale_to_action_text_rich_texts.rb
@@ -1,4 +1,4 @@
-class AddDefaultLocaleToActionTextRichTexts < ActiveRecord::Migration[7.2]
+class AddDefaultLocaleToActionTextRichTexts < ActiveRecord::Migration[6.1]
   def change
     if ActionText::RichText.column_defaults['locale'].nil?
       change_column_default :action_text_rich_texts, :locale, from: nil, to: :en
