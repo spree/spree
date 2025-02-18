f3538f5bfe550f1dd0b90dbb1b445d4fa1b0e51e
Damian Legawiec <damian@getvendo.com>
2025-02-18 11:00:00 +0100

    Update core/db/migrate/20250217171018_create_action_text_video_embeds.rb

--- core/db/migrate/20250217171018_create_action_text_video_embeds.rb
+++ core/db/migrate/20250217171018_create_action_text_video_embeds.rb
@@ -1,6 +1,6 @@
 class CreateActionTextVideoEmbeds < ActiveRecord::Migration[7.2]
   def change
-    create_table :action_text_video_embeds do |t|
+    create_table :action_text_video_embeds, if_not_exists: true do |t|
       t.string :url, null: false
       t.string :thumbnail_url, null: false
       t.text :raw_html, null: false
