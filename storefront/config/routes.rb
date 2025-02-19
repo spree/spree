e2935978aabf402abe06e6fdafb91e44eabd089d
Damian Legawiec <damian@getvendo.com>
2025-02-19 18:58:32 +0100

    Open source storefront wishlists

--- storefront/config/routes.rb
+++ storefront/config/routes.rb
@@ -1,5 +1,11 @@
 Spree::Core::Engine.add_routes do
   scope '(:locale)', locale: /#{Spree.available_locales.join('|')}/, defaults: { locale: nil } do
+    namespace :account do
+      resource :wishlist, only: [:show], controller: '/spree/wishlists' do
+        resources :wished_items, only: [:create, :destroy]
+      end
+    end
+
     root to: 'home#index'
   end
 end
