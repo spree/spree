f02b000d740b5102901063f11a9210d634ea71cc
Damian Kaczmarczyk <git@vegann.space>
2025-02-21 11:34:33 +0100

    Fix formating

--- core/lib/spree/testing_support/factories/page_section_factory.rb
+++ core/lib/spree/testing_support/factories/page_section_factory.rb
@@ -1,16 +1,16 @@
 FactoryBot.define do
   factory :page_section, class: Spree::PageSection do
-      pageable { Spree::Page.find_by!(name: 'Homepage') }
-      pageable_type { 'Spree::Page' }
+    pageable { Spree::Page.find_by!(name: 'Homepage') }
+    pageable_type { 'Spree::Page' }
 
-      factory :featured_taxons_page_section, class: Spree::PageSections::FeaturedTaxons
+    factory :featured_taxons_page_section, class: Spree::PageSections::FeaturedTaxons
 
-      factory :featured_taxon_page_section, class: Spree::PageSections::FeaturedTaxon
+    factory :featured_taxon_page_section, class: Spree::PageSections::FeaturedTaxon
 
-      factory :header_page_section, class: Spree::PageSections::Header
+    factory :header_page_section, class: Spree::PageSections::Header
 
-      factory :announcement_bar_page_section, class: Spree::PageSections::AnnouncementBar
+    factory :announcement_bar_page_section, class: Spree::PageSections::AnnouncementBar
 
-      factory :rich_text_page_section, class: Spree::PageSections::RichText
+    factory :rich_text_page_section, class: Spree::PageSections::RichText
   end
 end
