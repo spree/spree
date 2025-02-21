61b375fadcaf804ea04dc91b075bbb3f3c22679e
Damian Legawiec <damian@getvendo.com>
2025-02-21 16:35:17 +0100

    Use `BaseController` for both Admin/Storefront

--- admin/app/controllers/spree/admin/base_controller.rb
+++ admin/app/controllers/spree/admin/base_controller.rb
@@ -1,15 +1,6 @@
 module Spree
   module Admin
-    class BaseController < ApplicationController
-      include Spree::Core::ControllerHelpers::Auth
-      include Spree::Core::ControllerHelpers::Store
-      include Spree::Core::ControllerHelpers::StrongParameters
-      include Spree::Core::ControllerHelpers::Locale
-      include Spree::Core::ControllerHelpers::Currency
-      include Spree::Core::ControllerHelpers::Turbo
-
-      respond_to :html
-
+    class BaseController < Spree::BaseController
       layout 'spree/admin'
 
       helper 'spree/base'
--- core/app/controllers/spree/base_controller.rb
+++ core/app/controllers/spree/base_controller.rb
@@ -3,8 +3,6 @@ require_dependency 'spree/core/controller_helpers/strong_parameters'
 
 class Spree::BaseController < ApplicationController
   include Spree::Core::ControllerHelpers::Auth
-  include Spree::Core::ControllerHelpers::Common
-  include Spree::Core::ControllerHelpers::Search
   include Spree::Core::ControllerHelpers::Store
   include Spree::Core::ControllerHelpers::StrongParameters
   include Spree::Core::ControllerHelpers::Locale
