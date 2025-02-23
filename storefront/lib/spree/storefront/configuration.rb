b0cde325cc929712437d0e8a146e3491490a3c9e
Damian Legawiec <damian@getvendo.com>
2025-02-23 23:01:05 +0100

    Storefront: added search suggestions

--- storefront/lib/spree/storefront/configuration.rb
+++ storefront/lib/spree/storefront/configuration.rb
@@ -5,6 +5,7 @@ module Spree
     class Configuration < ::Spree::Preferences::RuntimeConfiguration
       preference :products_per_page, :integer, default: 20
 
+      preference :search_min_query_length, :integer, default: 2
     end
   end
 end
