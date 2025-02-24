4464e15c38dd7a9a5d7f6bde2f2b94eba76a8fdc
Damian Legawiec <damian@getvendo.com>
2025-02-24 22:32:18 +0100

    Fixed Account base controller loading of user
    `@user` is needed for some forms in the account section

--- storefront/app/controllers/spree/account/base_controller.rb
+++ storefront/app/controllers/spree/account/base_controller.rb
@@ -2,11 +2,18 @@ module Spree
   module Account
     class BaseController < Spree::StoreController
       before_action :require_user
+      before_action :set_user
 
       protected
 
       def accurate_title
         Spree.t(:my_account)
+      end
+
+      private
+
+      def set_user
+        @user = spree_current_user
       end
     end
   end
