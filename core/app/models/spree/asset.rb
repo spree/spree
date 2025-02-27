66c43d52f6854e4edbdbdbc1a5f12a60691c2e72
Łukasz Adamczyk <lukas.adamczyk@getvendo.com>
2025-02-27 12:41:00 +0100

    Fix creating products in admin on SQLite and MySQL

--- core/app/models/spree/asset.rb
+++ core/app/models/spree/asset.rb
@@ -16,7 +16,14 @@ module Spree
     default_scope { includes(attachment_attachment: :blob) }
 
     store_accessor :private_metadata, :session_uploaded_assets_uuid
-    scope :with_session_uploaded_assets_uuid, ->(uuid) { where("#{table_name}.private_metadata @> ?", { session_uploaded_assets_uuid: uuid }.to_json) }
+    scope :with_session_uploaded_assets_uuid, lambda { |uuid|
+      case ActiveRecord::Base.connection.adapter_name
+      when 'PostgreSQL'
+        where("#{table_name}.private_metadata @> ?", { session_uploaded_assets_uuid: uuid }.to_json)
+      when 'Mysql2', 'SQLite'
+        where("JSON_EXTRACT(private_metadata, '$.session_uploaded_assets_uuid') = '#{uuid}'")
+      end
+    }
 
     def product
       @product ||= viewable_type == 'Spree::Variant' ? viewable&.product : nil
