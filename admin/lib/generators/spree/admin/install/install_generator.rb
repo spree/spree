209584dc142f078dacf2e11c7b35f798a1fbc78b
Damian Legawiec <damian@getvendo.com>
2025-02-27 12:39:49 +0100

    Improve `spree:admin:install` generator

--- admin/lib/generators/spree/admin/install/install_generator.rb
+++ admin/lib/generators/spree/admin/install/install_generator.rb
@@ -15,6 +15,26 @@ module Spree
         end
 
         def install
+          if Rails.root && Rails.root.join("Procfile.dev").exist?
+            append_to_file 'Procfile.dev', "\nadmin_css: bin/rails dartsass:watch" unless File.read('Procfile.dev').include?('admin_css:')
+          else
+            create_file 'Procfile.dev', "admin_css: bin/rails dartsass:watch\n"
+          end
+
+          say "Add bin/dev to start foreman"
+          copy_file "dev", "bin/dev", force: true
+          chmod "bin/dev", 0755, verbose: false
+
+          empty_directory Rails.root.join('app/assets/builds') if Rails.root
+
+          unless File.exist?('app/assets/config/manifest.js')
+            create_file 'app/assets/config/manifest.js', "//= link_tree ../builds\n"
+
+            say "Ensure foreman is installed"
+            run "gem install foreman"
+          else
+            append_to_file 'app/assets/config/manifest.js', "\n//= link_tree ../builds" unless File.read('app/assets/config/manifest.js').include?('//= link_tree ../builds')
+          end
         end
       end
     end
