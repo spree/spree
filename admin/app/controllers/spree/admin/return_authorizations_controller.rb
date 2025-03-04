ff102fcbde45f43fe6741010c2ab464954c593b0
Damian Kaczmarczyk <git@vegann.space>
2025-03-04 17:24:09 +0100

    Update admin/app/controllers/spree/admin/return_authorizations_controller.rb
    Co-authored-by: Damian Legawiec <damian@getvendo.com>

--- admin/app/controllers/spree/admin/return_authorizations_controller.rb
+++ admin/app/controllers/spree/admin/return_authorizations_controller.rb
@@ -22,8 +22,7 @@ module Spree
 
         # @search needs to be defined as this is passed to search_form_for
         @search = current_store.return_authorizations.accessible_by(current_ability, :index).ransack(params[:q])
-        per_page = params[:per_page]
-        @collection = @search.result.order(created_at: :desc).page(params[:page]).per(per_page)
+        @collection = @search.result.order(created_at: :desc).page(params[:page]).per(params[:per_page])
       end
     end
   end
