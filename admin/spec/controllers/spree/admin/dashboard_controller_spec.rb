64051205f78be2262a1f0b1e0d5a282b209b1947
Damian Legawiec <damian@getvendo.com>
2025-03-10 13:58:11 +0100

    use turbo frame layout, added more specs

--- admin/spec/controllers/spree/admin/dashboard_controller_spec.rb
+++ admin/spec/controllers/spree/admin/dashboard_controller_spec.rb
@@ -19,6 +19,24 @@ describe Spree::Admin::DashboardController, type: :controller do
         get :analytics
         expect(response).to render_template(:analytics)
       end
+
+      context 'with data' do
+        before do
+          create_list(:completed_order_with_totals, 3, store: store)
+        end
+
+        it 'renders analytics' do
+          get :analytics
+          expect(response).to render_template(:analytics)
+        end
+
+        it 'renders top products' do
+          get :analytics
+          expect(assigns(:top_products)).to be_present
+          expect(response.body).to include('Top products')
+          expect(response.body).to include('View report')
+        end
+      end
     end
   end
 
