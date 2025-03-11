7b47681ed6c83e50e860129ab13afd243e591b15
Łukasz Adamczyk <lukas.adamczyk@getvendo.com>
2025-03-11 15:09:25 +0100

    Update specs

--- core/spec/models/spree/refund_reason_spec.rb
+++ core/spec/models/spree/refund_reason_spec.rb
@@ -10,7 +10,40 @@ describe Spree::RefundReason do
       end
 
       context 'default refund reason not present' do
-        it { expect(described_class.return_processing_reason).to eq(nil) }
+        it 'creates a new refund reason on the fly' do
+          expect(described_class.return_processing_reason).to be_present
+          expect(described_class.return_processing_reason.name).to eq('Return processing')
+        end
+      end
+    end
+
+    describe '.order_canceled_reason' do
+      context 'order canceled reason present' do
+        let!(:refund_reason) { create(:refund_reason, name: 'Order Canceled') }
+
+        it { expect(described_class.order_canceled_reason).to eq(refund_reason) }
+      end
+
+      context 'order canceled reason not present' do
+        it 'creates a new refund reason on the fly' do
+          expect(described_class.order_canceled_reason).to be_present
+          expect(described_class.order_canceled_reason.name).to eq('Order Canceled')
+        end
+      end
+    end
+
+    describe '.shipment_canceled_reason' do
+      context 'shipment canceled reason present' do
+        let!(:refund_reason) { create(:refund_reason, name: 'Shipment Canceled') }
+
+        it { expect(described_class.shipment_canceled_reason).to eq(refund_reason) }
+      end
+
+      context 'shipment canceled reason not present' do
+        it 'creates a new refund reason on the fly' do
+          expect(described_class.shipment_canceled_reason).to be_present
+          expect(described_class.shipment_canceled_reason.name).to eq('Shipment Canceled')
+        end
       end
     end
   end
