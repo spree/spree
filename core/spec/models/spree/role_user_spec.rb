44285064c88ad8e180efb31c985a167bd2641535
Damian Kaczmarczyk <git@vegann.space>
2025-03-13 12:53:13 +0100

    Better role user spec

--- core/spec/models/spree/role_user_spec.rb
+++ core/spec/models/spree/role_user_spec.rb
@@ -1,10 +1,15 @@
 require 'spec_helper'
 
+class AdminUser < Spree.base_class
+  self.table_name = 'spree_users'
+  include Spree::UserRoles
+end
+
 describe Spree::RoleUser do
   describe 'with different user types' do
     let(:role) { create(:role, name: 'test_role') }
     let(:spree_user) { create(:user) }
-    let(:admin_user) { create(:admin_user) }
+    let(:admin_user) { AdminUser.new(id: 99) }
 
     it 'can associate with different user types' do
       spree_role_user = described_class.create!(role: role, user: spree_user)
@@ -16,7 +21,7 @@ describe Spree::RoleUser do
       expect(spree_role_user.user_type).to eq(spree_user.class.to_s)
 
       expect(admin_role_user.user).to eq(admin_user)
-      expect(admin_role_user.user_type).to eq(admin_user.class.to_s)
+      expect(admin_role_user.user_type).to eq('AdminUser')
     end
   end
 end
