dd1d63a78c7fe8cabff7e4ca6d2530c939dbe3c4
Damian Legawiec <damian@getvendo.com>
2025-03-15 22:57:36 +0100

    Admin: Improve modals, fix JS error in promotion modals

--- admin/app/javascript/spree/admin/helpers/bootstrap.js
+++ admin/app/javascript/spree/admin/helpers/bootstrap.js
@@ -21,8 +21,21 @@ const removeTooltips = () => {
   })
 }
 
+const resetDialogModal = () => {
+  const modalElement = document.getElementById('dialog_modal')
+  const modalTitle = modalElement ? modalElement.querySelector('.modal-title') : null
+  const modalBody = modalElement ? modalElement.querySelector('.modal-body') : null
+  if (modalBody) {
+    modalBody.innerHTML = '<div class="spinner-border text-light mx-auto my-5" role="status"><span class="sr-only">Loading...</span></div>'
+  }
+  if (modalTitle) {
+    modalTitle.innerHTML = 'Loading...'
+  }
+}
+
 $('.modal').on('show.bs.modal', lockScroll)
 $('.modal').on('hide.bs.modal', unlockScroll)
+$('.modal').on('hidden.bs.modal	', resetDialogModal)
 
 document.addEventListener("turbo:click", removeTooltips)
 document.addEventListener("turbo:load", initTooltips)
