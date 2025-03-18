a0fa9e4c71b3f3bdf79180e87a3978555cf2d89d
Damian Legawiec <damian@getvendo.com>
2025-03-18 18:19:13 +0100

    Revert "Update store.rb"
    This reverts commit cb63db6dda1509aae7a435ecc70f8d8836a71d45.

--- core/lib/spree/testing_support/store.rb
+++ core/lib/spree/testing_support/store.rb
@@ -7,12 +7,6 @@ RSpec.configure do |config|
     end
   end
 
-  config.before(:each) do
-    unless self.class.metadata[:without_global_store]
-      allow_any_instance_of(Spree::Store).to receive(:default).and_return(@default_store)
-    end
-  end
-
   config.after(:each) do
     unless self.class.metadata[:without_global_store]
       @default_store&.products = []
