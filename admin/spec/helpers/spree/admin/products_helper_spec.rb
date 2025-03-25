a14153425920c2680756195d44ca47eb0a5533c2
Damian Legawiec <damian@sparksolutions.co>
2025-03-25 13:46:08 +0100

    Fixed `Admin::ProductsHelper#media_form_assets` method (#12557)
    To always return an empty array and not break the new product screen plus added some more tests

--- admin/spec/helpers/spree/admin/products_helper_spec.rb
+++ admin/spec/helpers/spree/admin/products_helper_spec.rb
@@ -1,6 +1,12 @@
 require 'spec_helper'
 
 describe Spree::Admin::ProductsHelper do
+  let(:user) { create(:admin_user) }
+
+  before do
+    allow(helper).to receive(:current_ability).and_return(Spree::Ability.new(user))
+  end
+
   describe '#available_status' do
     context 'when product is active' do
       let(:product) { build(:product, status: :active) }
@@ -39,6 +45,37 @@ describe Spree::Admin::ProductsHelper do
 
       it 'returns the correct status' do
         expect(available_status(product)).to eq('Deleted')
+      end
+    end
+  end
+
+  describe '#media_form_assets' do
+    context 'when variant is persisted' do
+      let(:variant) { build(:variant) }
+
+      it 'returns the correct assets' do
+        expect(helper.media_form_assets(variant)).to eq(variant.images)
+      end
+    end
+
+    context 'when variant is not persisted' do
+      let(:variant) { build(:variant, id: nil) }
+
+      it 'returns an empty array' do
+        expect(helper.media_form_assets(variant)).to eq([])
+      end
+    end
+
+    context 'when there are session uploaded assets' do
+      let(:variant) { build(:variant, id: nil) }
+      let(:session_uploaded_assets) { [create(:image)] }
+
+      before do
+        allow(helper).to receive(:session_uploaded_assets).and_return(session_uploaded_assets)
+      end
+
+      it 'returns the correct assets' do
+        expect(helper.media_form_assets(variant)).to eq(session_uploaded_assets)
       end
     end
   end
