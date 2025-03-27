75ae50873d38d725608ea2da61eda5a6b3910199
Damian Legawiec <damian@getvendo.com>
2025-03-27 18:00:36 +0100

    Ability to disable admin updater widget via config
    ```ruby
    Spree::Admin::RuntimeConfig.admin_updater_enabled = false
    ```

--- admin/lib/spree/admin/runtime_configuration.rb
+++ admin/lib/spree/admin/runtime_configuration.rb
@@ -2,7 +2,7 @@ module Spree
   module Admin
     class RuntimeConfiguration < ::Spree::Preferences::RuntimeConfiguration
       preference :admin_path, :string, default: '/admin'
-      preference :admin_show_version, :boolean, default: true
+      preference :admin_updater_enabled, :boolean, default: true
     end
   end
 end
