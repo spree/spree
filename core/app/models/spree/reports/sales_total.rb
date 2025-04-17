e0cf2cb12093bab9248efbe57809b0da104761cb
Damian Legawiec <damian@getvendo.com>
2025-04-17 22:21:00 +0200

    Fixes admin permissions to better adjust features (#12665)
    * Fixes admin permissions to better adjust features
    
    * Update _stock_location.html.erb

--- core/app/models/spree/reports/sales_total.rb
+++ core/app/models/spree/reports/sales_total.rb
@@ -2,12 +2,16 @@ module Spree
   module Reports
     class SalesTotal < Spree::Report
       def line_items_scope
-        store.line_items.where(
+        scope = store.line_items.where(
           order: Spree::Order.complete.where(
             currency: currency,
             completed_at: (date_from.to_time.beginning_of_day)..(date_to.to_time.end_of_day)
           )
         ).includes(:order, variant: :product)
+
+        scope = scope.where(vendor_id: vendor.id) if defined?(vendor) && vendor.present?
+
+        scope
       end
     end
   end
