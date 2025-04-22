918839ab7cf74b7427d16ba280fabbe75f2cf216
Damian Legawiec <damian@getvendo.com>
2025-04-22 09:20:34 +0200

    Admin: Improve Posts management (#12678)
    * added sorting
    * added autocomplete to selects/dropdowns

--- admin/app/controllers/spree/admin/posts_controller.rb
+++ admin/app/controllers/spree/admin/posts_controller.rb
@@ -1,6 +1,8 @@
 module Spree
   module Admin
     class PostsController < ResourceController
+      before_action :load_post_categories
+
       def select_options
         render json: current_store.posts.published.to_tom_select_json
       end
@@ -17,6 +19,10 @@ module Spree
 
         @search = @collection.ransack(params[:q])
         @collection = @search.result.page(params[:page]).per(params[:per_page])
+      end
+
+      def load_post_categories
+        @post_categories = current_store.post_categories.accessible_by(current_ability).order(:title)
       end
     end
   end
