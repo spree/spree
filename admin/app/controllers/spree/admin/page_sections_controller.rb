071e96415c7f40d7ddfb0f32c4532f100d80f672
Damian Legawiec <damian@getvendo.com>
2025-04-22 18:55:37 +0200

    Add ability to register custom page sections per theme

--- admin/app/controllers/spree/admin/page_sections_controller.rb
+++ admin/app/controllers/spree/admin/page_sections_controller.rb
@@ -7,7 +7,7 @@ module Spree
 
       def create
         page_section_type = params.dig(:page_section, :type)
-        allowed_types = Rails.application.config.spree.page_sections.map(&:to_s)
+        allowed_types = @pageable.theme.available_page_sections.map(&:to_s)
 
         if allowed_types.include?(page_section_type) && page_section_type.safe_constantize.present?
           @page_section = page_section_type.constantize.new(permitted_resource_params)
