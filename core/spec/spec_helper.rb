9954204443ad9f769f84145394e0eb4368d5753b
Damian Legawiec <damian@getvendo.com>
2025-05-03 20:22:37 +0200

    Fixed rspec rails 8 issues

--- core/spec/spec_helper.rb
+++ core/spec/spec_helper.rb
@@ -54,7 +54,6 @@ RSpec.configure do |config|
   config.color = true
   config.default_formatter = 'doc'
   config.fail_fast = ENV['FAIL_FAST'] || false
-  config.fixture_path = File.join(__dir__, 'fixtures')
   config.infer_spec_type_from_file_location!
   config.mock_with :rspec
   config.raise_errors_for_deprecations!
