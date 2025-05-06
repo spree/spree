0a35d6b9e70cd976c85449233bb9eb4d12abc147
Michał Brożek <michal.brozek@getvendo.com>
2025-05-06 17:05:08 +0200

    ADD git_card_id to orders

--- core/db/migrate/20250506073057_create_spree_gift_cards_and_spree_gift_card_batches.rb
+++ core/db/migrate/20250506073057_create_spree_gift_cards_and_spree_gift_card_batches.rb
@@ -26,11 +26,14 @@ class CreateSpreeGiftCardsAndSpreeGiftCardBatches < ActiveRecord::Migration[7.2]
       t.timestamps
     end
 
+    add_index :spree_gift_cards, :code, unique: true
+
     add_column :spree_store_credits, :gift_card_id, :bigint, if_not_exists: true
     add_column :spree_store_credits, :expires_at, :datetime, if_not_exists: true
-    add_column :spree_store_credits, :state, :integer, default: 0, index: true, if_not_exists: true
-
-    add_index :spree_gift_cards, :code, unique: true
+    add_column :spree_store_credits, :state, :integer, default: 0, if_not_exists: true
     add_index :spree_store_credits, :gift_card_id, if_not_exists: true
+
+    add_column :spree_orders, :gift_card_id, :bigint, if_not_exists: true
+    add_index :spree_orders, :gift_card_id, if_not_exists: true
   end
 end
