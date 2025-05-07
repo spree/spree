9e58e4c3f76e1b6341b7b72adeda6e59961303f2
Michał Brożek <michal.brozek@getvendo.com>
2025-05-07 12:17:27 +0200

    Move validate gift card method

--- api/app/controllers/spree/api/v2/storefront/checkout_controller.rb
+++ api/app/controllers/spree/api/v2/storefront/checkout_controller.rb
@@ -111,6 +111,16 @@ module Spree
             end
           end
 
+          def validate_gift_card_data
+            if params[:gift_card_code].present? && spree_current_order.gift_card.nil?
+              render json: { error: 'Gift card is not found' }, status: 422
+            elsif params[:gift_card_amount].to_d != spree_current_order.gift_card_total.to_d
+              render json: { error: 'Gift card amount is not valid' }, status: 422
+            else
+              render json: { success: true }, status: 200
+            end
+          end
+
           private
 
           def resource_serializer
