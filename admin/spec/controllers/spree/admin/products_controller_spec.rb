b4bc1e989bb7e73f9b5c6563f92473d53f921f27
Łukasz Adamczyk <lukas.adamczyk@getvendo.com>
2025-05-07 10:35:15 +0200

    Fix checkout for products with untracked inventory (#12724)

--- admin/spec/controllers/spree/admin/products_controller_spec.rb
+++ admin/spec/controllers/spree/admin/products_controller_spec.rb
@@ -996,13 +996,12 @@ RSpec.describe Spree::Admin::ProductsController, type: :controller do
         create(:stock_item, count_on_hand: 20, variant: product.master)
       end
 
-      it 'updates stock items' do
+      it 'updates stock item count on hand to 0' do
         expect(product.master.stock_items.reload.count).to be > 0
 
         send_request
 
         expect(product.reload.track_inventory).to be(false)
-        expect(product.master.stock_items.reload.first.backorderable).to be(true)
         expect(product.master.stock_items.reload.first.count_on_hand).to eq(0)
       end
     end
