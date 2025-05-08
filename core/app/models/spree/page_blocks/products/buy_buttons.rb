c41e289d4e7564d0d2e20e0ca8adb943a0df6ad1
Łukasz Adamczyk <lukas.adamczyk@getvendo.com>
2025-05-08 17:56:06 +0200

    Page builder: ability to add more blocks to in sections (#12733)
    * Page builder: ability to add more blocks to in sections
    
    * Normalize i18n

--- core/app/models/spree/page_blocks/products/buy_buttons.rb
+++ core/app/models/spree/page_blocks/products/buy_buttons.rb
@@ -6,6 +6,14 @@ module Spree
         BOTTOM_PADDING_DEFAULT = 20
 
         preference :text_color, :string
+
+        def icon_name
+          'shopping-cart-plus'
+        end
+
+        def display_name
+          Spree.t('page_blocks.products.buy_buttons.display_name')
+        end
       end
     end
   end
