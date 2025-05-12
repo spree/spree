701654889ff413d3c93d3705b7045372067326e8
Damian Legawiec <damian@getvendo.com>
2025-05-12 20:50:15 +0200

    Bump `dompurify` to 3.2.5

--- admin/config/importmap.rb
+++ admin/config/importmap.rb
@@ -15,8 +15,8 @@ pin 'Chart.bundle', to: 'Chart.bundle.js', preload: ['application-spree-admin']
 pin 'local-time', preload: ['application-spree-admin'] # @3.0.2
 pin 'mapkick/bundle', to: 'mapkick.bundle.js', preload: ['application-spree-admin']
 pin "jquery", to: 'jquery.min.js', preload: ['application-spree-admin'] # @3.7.1
-pin 'dompurify', preload: ['application-spree-admin'] # @3.2.3
 pin 'bootstrap', to: 'bootstrap--dist--js--bootstrap.bundle.min.js.js', preload: ['application-spree-admin'] # @4.6.2
+pin 'dompurify', preload: ['application-spree-admin'] # @3.2.5
 
 # Stimulus components
 pin '@stimulus-components/rails-nested-form', to: '@stimulus-components--rails-nested-form.js', preload: ['application-spree-admin'] # @5.0.0
