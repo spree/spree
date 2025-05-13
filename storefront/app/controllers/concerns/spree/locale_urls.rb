3d20a34860aacc9f55fe9c9d5db5745cc43fa82b
Damian Legawiec <damian@getvendo.com>
2025-05-13 15:05:05 +0200

    Improve `redirect_to_default_locale` method to only pass safe parameters (#12745)

--- storefront/app/controllers/concerns/spree/locale_urls.rb
+++ storefront/app/controllers/concerns/spree/locale_urls.rb
@@ -21,7 +21,19 @@ module Spree
     def redirect_to_default_locale
       return if params[:locale].blank? || supported_locale?(params[:locale])
 
-      redirect_to url_for(request.parameters.merge(locale: nil))
+      # Only include safe parameters in the redirect
+      safe_params = {
+        controller: params[:controller],
+        action: params[:action],
+        locale: nil
+      }
+
+      # Add any additional safe parameters that should be preserved
+      %i[id format currency category_id tag].each do |param|
+        safe_params[param] = params[param] if params[param].present?
+      end
+
+      redirect_to url_for(safe_params)
     end
   end
 end
