2ac492fd41b2a7e844b444aba4373ddc85d5be58
Damian Legawiec <damian@getvendo.com>
2025-05-13 15:17:47 +0200

    Improve brakeman reporting

--- bin/brakeman.sh
+++ bin/brakeman.sh
@@ -2,8 +2,8 @@
 
 set -eux
 
-bundle exec brakeman -p api/ --exit-on-warn --exit-on-error
-bundle exec brakeman -p core/ --exit-on-warn --exit-on-error
-bundle exec brakeman -p storefront/ --exit-on-warn --exit-on-error
-bundle exec brakeman -p admin/ --exit-on-warn --exit-on-error
-bundle exec brakeman -p emails/ --exit-on-warn --exit-on-error
+bundle exec brakeman -p api/ --exit-on-warn --exit-on-error -o brakeman-api.html
+bundle exec brakeman -p core/ --exit-on-warn --exit-on-error -o brakeman-core.html
+bundle exec brakeman -p storefront/ --exit-on-warn --exit-on-error -o brakeman-storefront.html
+bundle exec brakeman -p admin/ --exit-on-warn --exit-on-error -o brakeman-admin.html
+bundle exec brakeman -p emails/ --exit-on-warn --exit-on-error -o brakeman-emails.html
