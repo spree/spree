39a1d42d359546c2391d25f896fe0b87328f8ce9
Damian Legawiec <damian@getvendo.com>
2025-05-22 14:03:56 +0200

    Fixed admin reimbursement types management
    Fixed persisting type, allow to modify mutable

--- admin/spec/controllers/spree/admin/reimbursement_types_controller_spec.rb
+++ admin/spec/controllers/spree/admin/reimbursement_types_controller_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Spree::Admin::ReimbursementTypesController, type: :controller do
   render_views
 
   let(:reimbursement_type) { create(:reimbursement_type) }
-  let(:valid_attributes) { { name: 'Test Type', active: true } }
+  let(:valid_attributes) { { name: 'Test Type', active: true, mutable: true, type: 'Spree::ReimbursementType::Credit' } }
   let(:invalid_attributes) { { name: '', active: nil } }
 
   describe 'GET #index' do
@@ -28,6 +28,12 @@ RSpec.describe Spree::Admin::ReimbursementTypesController, type: :controller do
         expect {
           post :create, params: { reimbursement_type: valid_attributes }
         }.to change(Spree::ReimbursementType, :count).by(1)
+
+        reimbursement_type = Spree::ReimbursementType.last
+        expect(reimbursement_type.name).to eq('Test Type')
+        expect(reimbursement_type.active).to be true
+        expect(reimbursement_type.mutable).to be true
+        expect(reimbursement_type.type).to eq('Spree::ReimbursementType::Credit')
       end
 
       it 'redirects to the reimbursement types edit screen' do
@@ -60,13 +66,15 @@ RSpec.describe Spree::Admin::ReimbursementTypesController, type: :controller do
 
   describe 'PUT #update' do
     context 'with valid params' do
-      let(:new_attributes) { { name: 'Updated Type', active: false } }
+      let(:new_attributes) { { name: 'Updated Type', active: false, mutable: false, type: 'Spree::ReimbursementType::OriginalPayment' } }
 
       it 'updates the requested reimbursement type' do
         put :update, params: { id: reimbursement_type.to_param, reimbursement_type: new_attributes }
         reimbursement_type.reload
         expect(reimbursement_type.name).to eq('Updated Type')
         expect(reimbursement_type.active).to be false
+        expect(reimbursement_type.mutable).to be false
+        expect(reimbursement_type.type).to eq('Spree::ReimbursementType::OriginalPayment')
       end
 
       it 'redirects to the reimbursement type edit screen' do
