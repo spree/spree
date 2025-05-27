258221d3ec731121aa6faccb7d3acb79db746ff3
Damian Legawiec <damian@getvendo.com>
2025-05-27 16:16:05 +0200

    Update core/db/migrate/20250527134027_add_company_to_spree_stock_locations.rb

--- core/db/migrate/20250527134027_add_company_to_spree_stock_locations.rb
+++ core/db/migrate/20250527134027_add_company_to_spree_stock_locations.rb
@@ -1,5 +1,5 @@
 class AddCompanyToSpreeStockLocations < ActiveRecord::Migration[7.2]
   def change
-    add_column :spree_stock_locations, :company, :string
+    add_column :spree_stock_locations, :company, :string, if_not_exists: true
   end
 end
