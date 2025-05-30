32482371a04141c8e9ab98b643408cde77cc5c13
Damian Kaczmarczyk <git@vegann.space>
2025-05-30 13:45:28 +0200

    fix whitespace

--- core/db/migrate/20250530101236_enable_pg_trgm_extension.rb
+++ core/db/migrate/20250530101236_enable_pg_trgm_extension.rb
@@ -6,7 +6,7 @@ class EnablePgTrgmExtension < ActiveRecord::Migration[7.2]
   end
 
   def down
-    if supports_extensions? && extension_available?('pg_trgm') &&extension_enabled?('pg_trgm')
+    if supports_extensions? && extension_available?('pg_trgm') && extension_enabled?('pg_trgm')
       disable_extension 'pg_trgm'
     end
   end
