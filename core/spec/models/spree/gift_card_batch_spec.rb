fd9280271fa31260af1651dcfdffef187507bed7
Damian Legawiec <damian@getvendo.com>
2025-06-15 10:47:22 +0200

    fixes

--- core/spec/models/spree/gift_card_batch_spec.rb
+++ core/spec/models/spree/gift_card_batch_spec.rb
@@ -1,17 +1,3 @@
-# == Schema Information
-#
-# Table name: spree_gift_card_batches
-#
-#  id                   :bigint           not null, primary key
-#  amount               :decimal(10, 2)   default(10.0), not null
-#  codes_count          :integer          default(1), not null
-#  expires_at           :date
-#  minimum_order_amount :decimal(10, 2)   default(0.0)
-#  prefix               :string
-#  created_at           :datetime         not null
-#  updated_at           :datetime         not null
-#  tenant_id            :bigint           not null
-#
 require 'spec_helper'
 
 RSpec.describe Spree::GiftCardBatch, type: :model do
