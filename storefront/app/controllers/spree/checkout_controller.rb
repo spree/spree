e17994ee8abf7475678821e712d5a3e4c3dc7996
Łukasz Adamczyk <lukas.adamczyk@getvendo.com>
2025-06-18 10:28:09 +0200

    Redirect a guest to sign up page when trying to enter checkout (#12824)
    * Redirect a guest to sign up page when trying to enter checkout
    
    * Redirect the guest to the login page if entering checkout for an order with assigned user

--- storefront/app/controllers/spree/checkout_controller.rb
+++ storefront/app/controllers/spree/checkout_controller.rb
@@ -4,7 +4,6 @@ module Spree
     include Spree::CheckoutHelper
     include Spree::CheckoutAnalyticsHelper
 
-    before_action :require_user, unless: -> { current_store.prefers_guest_checkout? }
     before_action :load_order
     before_action :remove_out_of_stock_items, only: [:edit, :update]
 
@@ -191,6 +190,8 @@ module Spree
             redirect_to spree_login_path
           end
         end
+      elsif !current_store.prefers_guest_checkout?
+        require_user(redirect_path: spree_signup_path)
       end
 
       # completed orders shouldn't be updated anymore
