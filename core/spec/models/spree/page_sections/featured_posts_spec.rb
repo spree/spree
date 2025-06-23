03ba983e62c63c99ba7f01cb2439f988cd4189f0
Damian Kaczmarczyk <git@vegann.space>
2025-06-23 14:42:01 +0200

    Better spec

--- core/spec/models/spree/page_sections/featured_posts_spec.rb
+++ core/spec/models/spree/page_sections/featured_posts_spec.rb
@@ -2,15 +2,15 @@ require 'spec_helper'
 
 RSpec.describe Spree::PageSections::FeaturedTaxon do
   describe '#posts' do
+    subject(:result) { section.posts }
+
     let!(:posts) { create_list(:post, 3, published_at: nil) }
     let!(:published_posts) { create_list(:post, 3, published_at: Time.current) }
     let(:section) { build(:featured_posts_page_section, preferred_max_posts_to_show: 2) }
 
     it 'returns newsest posts with limit' do
-      expect(section.posts).not_to include(posts[0])
-      expect(section.posts).not_to include(posts[1])
-      expect(section.posts).to include(published_posts[1])
-      expect(section.posts).to include(published_posts[2])
+      expect(result).not_to include(*posts)
+      expect(result).to eq(published_posts.sort_by(&:published_at).reverse!.first(2))
     end
   end
 end
