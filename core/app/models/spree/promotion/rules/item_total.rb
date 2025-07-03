55abbba9aff271efe7f5b848cc99658ef2bbae4b
Josef Hovad <josef@hovad.cz>
2025-07-03 13:07:38 +0200

    Align promotion rule eligibility error message with currency of the order. (#12762)
    * Align promotion rule eligibility error message with currency of the order.
    
    * Fix passing of the order arg.
    
    * Update item_total_spec.rb
    
    ---------
    
    Co-authored-by: Damian Legawiec <damian@getvendo.com>

--- core/app/models/spree/promotion/rules/item_total.rb
+++ core/app/models/spree/promotion/rules/item_total.rb
@@ -27,39 +27,39 @@ module Spree
             upper_limit_condition = true
           end
 
-          eligibility_errors.add(:base, ineligible_message_max) unless upper_limit_condition
-          eligibility_errors.add(:base, ineligible_message_min) unless lower_limit_condition
+          eligibility_errors.add(:base, ineligible_message_max(order)) unless upper_limit_condition
+          eligibility_errors.add(:base, ineligible_message_min(order)) unless lower_limit_condition
 
           eligibility_errors.empty?
         end
 
         private
 
-        def formatted_amount_min
-          Spree::Money.new(preferred_amount_min).to_s
+        def formatted_amount_min(order)
+          Spree::Money.new(preferred_amount_min, currency: order.currency).to_s
         end
 
-        def formatted_amount_max
+        def formatted_amount_max(order)
           if preferred_amount_max.present?
-            Spree::Money.new(preferred_amount_max).to_s
+            Spree::Money.new(preferred_amount_max, currency: order.currency).to_s
           else
             Spree.t('no_maximum')
           end
         end
 
-        def ineligible_message_max
+        def ineligible_message_max(order)
           if preferred_operator_max == 'lt'
-            eligibility_error_message(:item_total_more_than_or_equal, amount: formatted_amount_max)
+            eligibility_error_message(:item_total_more_than_or_equal, amount: formatted_amount_max(order))
           else
-            eligibility_error_message(:item_total_more_than, amount: formatted_amount_max)
+            eligibility_error_message(:item_total_more_than, amount: formatted_amount_max(order))
           end
         end
 
-        def ineligible_message_min
+        def ineligible_message_min(order)
           if preferred_operator_min == 'gte'
-            eligibility_error_message(:item_total_less_than, amount: formatted_amount_min)
+            eligibility_error_message(:item_total_less_than, amount: formatted_amount_min(order))
           else
-            eligibility_error_message(:item_total_less_than_or_equal, amount: formatted_amount_min)
+            eligibility_error_message(:item_total_less_than_or_equal, amount: formatted_amount_min(order))
           end
         end
       end
