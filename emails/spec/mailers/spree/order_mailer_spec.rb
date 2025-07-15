1cabb8216af0123b890b5851b4f1f4535c1852ea
Damian Legawiec <damian@getvendo.com>
2025-07-15 14:15:44 +0200

    Update order_mailer_spec.rb

--- emails/spec/mailers/spree/order_mailer_spec.rb
+++ emails/spec/mailers/spree/order_mailer_spec.rb
@@ -273,13 +273,13 @@ describe Spree::OrderMailer, type: :mailer do
     end
 
     context 'with custom domain' do
-      let!(:custom_domain) { create(:custom_domain, store: store, url: 'custom.example.com') }
+      let!(:custom_domain) { create(:custom_domain, store: store, url: 'my-store.com') }
 
       it 'uses custom domain for URLs in emails' do
         email = described_class.confirm_email(order).deliver_now
-        expect(ActionMailer::Base.default_url_options[:host]).to eq('custom.example.com')
+        expect(ActionMailer::Base.default_url_options[:host]).to eq('my-store.com')
         # testing HTML body
-        expect(email.body.parts.last.body).to include('custom.example.com')
+        expect(email.body.parts.last.body).to include('my-store.com')
         expect(email.body.parts.last.body).not_to include(store.url)
       end
     end
