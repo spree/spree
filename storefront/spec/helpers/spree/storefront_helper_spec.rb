bfe943d021220bde034a7c0d9fac6c293c0f97da
Kyryl Ωliinyk <kyryloliinyk@gmail.com>
2025-07-18 10:11:25 +0300

    Fix | add missing :uk svg country icon. (#12953)
    * Fix: add missing :uk svg_country_icon
    
    refactor #svg_country_icon helper method
    
    * Fix typo

--- storefront/spec/helpers/spree/storefront_helper_spec.rb
+++ storefront/spec/helpers/spree/storefront_helper_spec.rb
@@ -95,4 +95,22 @@ describe Spree::StorefrontHelper, type: :helper do
       end
     end
   end
+
+  describe '#svg_country_icon' do
+    it 'returns correct flag class for uk language code' do
+      expect(svg_country_icon(['uk', :uk].sample)).to include('fi fi-ua')
+    end
+
+    it 'returns correct flag class for en language code' do
+      expect(svg_country_icon(['en', :en].sample)).to include('fi fi-us')
+    end
+
+    it 'returns correct flag class for ja language code' do
+      expect(svg_country_icon(['ja', :ja].sample)).to include('fi fi-jp')
+    end
+
+    it 'returns correct flag class for unknown language code' do
+      expect(svg_country_icon('fr')).to include('fi fi-fr')
+    end
+  end
 end
