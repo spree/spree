30da330af3b32a1d7529a539b39c02211dd3802b
Damian Legawiec <damian@getvendo.com>
2025-07-25 16:41:49 +0200

    Improve Custom Domains / Internal URL UI
    Allow to set URL directly (not only code) when multi-store is not enabled

--- admin/app/controllers/spree/admin/custom_domains_controller.rb
+++ admin/app/controllers/spree/admin/custom_domains_controller.rb
@@ -9,10 +9,6 @@ module Spree
         spree.admin_custom_domains_path
       end
 
-      def create_turbo_stream_enabled?
-        helpers.entri_enabled?
-      end
-
       def location_after_save
         spree.admin_custom_domains_path
       end
