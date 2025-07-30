be4d37edbc9ac66accf9c27db1ff84b0c1ad388c
Łukasz Adamczyk <lukas.adamczyk@getvendo.com>
2025-07-30 17:53:43 +0200

    Add unique indexes to option types, option values and properties

---
+++ core/db/migrate/20250730154601_add_unique_index_on_spree_properties_name.rb
@@ -0,0 +1,21 @@
+class AddUniqueIndexOnSpreePropertiesName < ActiveRecord::Migration[7.2]
+  def change
+    # Rename duplicated properties
+    duplicates = Spree::Property.select(:name).group(:name).having('COUNT(*) > 1').reorder('').pluck(:name)
+
+    duplicates.each do |duplicate_name|
+      properties = Spree::Property.where(name: duplicate_name)
+
+      properties.each_with_index do |property, index|
+        next if index == 0 # Keep the first one unchanged
+
+        new_name = "#{duplicate_name}_#{index}"
+        property.update_columns(name: new_name, updated_at: Time.current)
+      end
+    end
+
+    # Add indexes
+    remove_index :spree_properties, :name, if_exists: true
+    add_index :spree_properties, :name, unique: true, if_not_exists: true
+  end
+end
