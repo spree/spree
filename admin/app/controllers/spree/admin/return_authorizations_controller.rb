945a6252b57fce1052ea075a1f9f053dcbe9d445
Damian Legawiec <damian@sparksolutions.co>
2025-08-02 17:10:55 +0200

    DRY-up admin views and utilize universal partials/components (#12991)
    * DRY-up admin views and utilize universal partials/components
    
    * table header fixes

--- admin/app/controllers/spree/admin/return_authorizations_controller.rb
+++ admin/app/controllers/spree/admin/return_authorizations_controller.rb
@@ -1,6 +1,9 @@
 module Spree
   module Admin
     class ReturnAuthorizationsController < ResourceController
+      add_breadcrumb_icon 'receipt-refund'
+      add_breadcrumb Spree.t(:returns), :admin_customer_returns_path
+
       def index; end
 
       def cancel
