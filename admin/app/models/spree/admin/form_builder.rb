1829e1b9cee052ea3d7810d5ddf3f5feda8dd449
Damian Legawiec <damian@getvendo.com>
2025-08-03 10:05:42 +0200

    Add Spree Admin form builder to clean up forms

---
+++ admin/app/models/spree/admin/form_builder.rb
@@ -0,0 +1,100 @@
+module Spree
+  module Admin
+    class FormBuilder < ActionView::Helpers::FormBuilder
+      def error_message_on(method, options = {})
+        @template.error_message_on(@object_name, method, objectify_options(options))
+      end
+
+      def spree_text_field(method, options = {})
+        options[:class] ||= 'form-control'
+        @template.content_tag(:div, class: 'form-group') do
+          @template.label(@object_name, method, get_label(method, options)) +
+            @template.text_field(@object_name, method, objectify_options(options)) +
+            @template.error_message_on(@object_name, method) + field_help(method, options)
+        end
+      end
+
+      def spree_number_field(method, options = {})
+        options[:class] ||= 'form-control'
+        @template.content_tag(:div, class: 'form-group') do
+          @template.label(@object_name, method, get_label(method, options)) +
+            @template.number_field(@object_name, method, objectify_options(options)) +
+            @template.error_message_on(@object_name, method) + field_help(method, options)
+        end
+      end
+
+      def spree_date_field(method, options = {})
+        options[:class] ||= 'form-control'
+        @template.content_tag(:div, class: 'form-group') do
+          @template.label(@object_name, method, get_label(method, options)) +
+            @template.date_field(@object_name, method, objectify_options(options)) +
+            @template.error_message_on(@object_name, method) + field_help(method, options)
+        end
+      end
+
+      def spree_text_area(method, options = {})
+        @template.content_tag(:div, class: 'form-group') do
+          options[:class] ||= 'form-control'
+          options[:rows] ||= 5
+          options[:data] ||= { controller: 'textarea-autogrow' }
+
+          @template.label(@object_name, method, get_label(method, options)) +
+            @template.text_area(@object_name, method, objectify_options(options)) +
+            @template.error_message_on(@object_name, method) + field_help(method, options)
+        end
+      end
+
+      def spree_rich_text_area(method, options = {})
+        @template.content_tag(:div, class: 'form-group') do
+          @template.label(@object_name, method, get_label(method, options)) +
+            @template.content_tag(:div, class: 'trix-container') do
+              @template.rich_text_area(@object_name, method, objectify_options(options))
+            end +
+            @template.error_message_on(@object_name, method) + field_help(method, options)
+        end
+      end
+
+      def spree_collection_select(method, collection, value_method, text_method, options = {})
+        @template.content_tag(:div, class: 'form-group') do
+          @template.label(@object_name, method, get_label(method, options)) +
+            @template.collection_select(@object_name, method, collection, value_method, text_method, options, { class: 'custom-select' })
+            @template.error_message_on(@object_name, method) + field_help(method, options)
+        end
+      end
+
+      def spree_check_box(method, options = {})
+        @template.content_tag(:div, class: 'form-group') do
+          @template.content_tag(:div, class: 'custom-control custom-checkbox') do
+            @template.check_box(@object_name, method, objectify_options(options.merge(class: 'custom-control-input'))) +
+            @template.label(@object_name, method, get_label(method, options), class: 'custom-control-label')
+            @template.error_message_on(@object_name, method) + field_help(method, options)
+          end
+        end
+      end
+
+      def field_help(_method, options = {})
+        @template.content_tag(:span, class: 'form-text text-muted mt-2') do
+          options[:help]
+        end
+      end
+
+      private
+
+      def get_label(method, options)
+        translated_label = if options[:label]
+                              options[:label]
+                            elsif I18n.exists?("spree.#{method}")
+                              I18n.t("spree.#{method}")
+                            else
+                              I18n.t("activerecord.attributes.spree/#{@object_name.to_s.underscore}.#{method}")
+                            end
+
+        required_label = options[:required] ? ' ' + @template.required_span_tag : ''
+
+        help_bubble = options[:help_bubble] ? ' ' + @template.help_bubble(options[:help_bubble]) : ''
+
+        @template.raw(translated_label + required_label + help_bubble)
+      end
+    end
+  end
+end
