cc52951ed39b24527383b8a1b0373b8497a29a55
Damian Legawiec <damian@getvendo.com>
2025-08-06 14:14:07 +0200

    Revert "Skip propshaft for sandbox app/dummy generator (#12955)"
    This reverts commit 3cda5f7cf824cfdf72fb60deb3f6fcf55f61354f.

--- bin/sandbox.sh
+++ bin/sandbox.sh
@@ -26,7 +26,6 @@ bundle exec rails new sandbox --database="$RAILSDB" \
   --skip-keeps \
   --skip-rc \
   --skip-test \
-  --skip-asset-pipeline
 
 if [ ! -d "sandbox" ]; then
   echo 'sandbox rails application failed'
--- core/lib/generators/spree/dummy/dummy_generator.rb
+++ core/lib/generators/spree/dummy/dummy_generator.rb
@@ -38,7 +38,6 @@ module Spree
       opts[:skip_spring] = true
       opts[:skip_test] = true
       opts[:skip_bootsnap] = true
-      opts[:skip_asset_pipeline] = true # skip installing propshaft, we're still using sprockets as a dependency
 
       puts 'Generating dummy Rails application...'
       invoke Rails::Generators::AppGenerator,
