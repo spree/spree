80312bced4cc07f3843e5bf7ada9b59a8d39485f
Damian Legawiec <damian@sparksolutions.co>
2025-08-07 21:31:34 +0200

    Fixed: proper json/jsonb check implemented (#13002)
    * Fixed: proper json/jsonb check implemented
    
    * Update 20250605131334_add_missing_fields_to_users.rb
    
    * Update 20250605131334_add_missing_fields_to_users.rb

--- core/db/migrate/20250605131334_add_missing_fields_to_users.rb
+++ core/db/migrate/20250605131334_add_missing_fields_to_users.rb
@@ -1,25 +1,35 @@
-# this fields were not present when someone used a custom user class
+# these fields were not present when someone used a custom user class
 # so we need to ensure this is setup properly
 class AddMissingFieldsToUsers < ActiveRecord::Migration[7.2]
   def change
     users_table = Spree.user_class.table_name
-    add_column users_table, :login, :string, if_not_exists: true
+    admin_users_table = Spree.admin_user_class.table_name
 
-    json_type = if users_table.respond_to?(:jsonb)
-      :jsonb
-    else
-      :json
+    change_table users_table do |t|
+      t.string :login unless column_exists?(users_table, :login)
+
+      if t.respond_to? :jsonb
+        t.jsonb :public_metadata unless column_exists?(users_table, :public_metadata)
+        t.jsonb :private_metadata unless column_exists?(users_table, :private_metadata)
+      else
+        t.json :public_metadata unless column_exists?(users_table, :public_metadata)
+        t.json :private_metadata unless column_exists?(users_table, :private_metadata)
+      end
     end
-
-    add_column users_table, :public_metadata, json_type, if_not_exists: true
-    add_column users_table, :private_metadata, json_type, if_not_exists: true
 
     add_reference users_table, :bill_address, if_not_exists: true
     add_reference users_table, :ship_address, if_not_exists: true
 
-    admin_users_table = Spree.admin_user_class.table_name
-    add_column admin_users_table, :login, :string, if_not_exists: true
-    add_column admin_users_table, :public_metadata, json_type, if_not_exists: true
-    add_column admin_users_table, :private_metadata, json_type, if_not_exists: true
+    change_table admin_users_table do |t|
+      t.string :login unless column_exists?(admin_users_table, :login)
+
+      if t.respond_to? :jsonb
+        t.jsonb :public_metadata unless column_exists?(admin_users_table, :public_metadata)
+        t.jsonb :private_metadata unless column_exists?(admin_users_table, :private_metadata)
+      else
+        t.json :public_metadata unless column_exists?(admin_users_table, :public_metadata)
+        t.json :private_metadata unless column_exists?(admin_users_table, :private_metadata)
+      end
+    end
   end
 end
