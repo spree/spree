f5d91b88680f2b1d2bc725252ec76a1578db9235
Damian Legawiec <damian@getvendo.com>
2025-08-12 14:19:56 +0200

    Update policies_controller_spec.rb

--- storefront/spec/controllers/spree/policies_controller_spec.rb
+++ storefront/spec/controllers/spree/policies_controller_spec.rb
@@ -10,42 +10,12 @@ describe Spree::PoliciesController, type: :controller do
   end
 
   describe 'GET #show' do
-    context 'with valid policy types' do
-      it 'renders privacy_policy' do
-        get :show, params: { id: 'privacy_policy' }
-        expect(assigns(:policy)).to eq(store.customer_privacy_policy)
-        expect(response).to render_template(:show)
-        expect(response).to have_http_status(:ok)
-      end
+    let(:policy) { create(:policy, store: store) }
 
-      it 'renders terms_of_service' do
-        get :show, params: { id: 'terms_of_service' }
-        expect(assigns(:policy)).to eq(store.customer_terms_of_service)
-        expect(response).to render_template(:show)
-        expect(response).to have_http_status(:ok)
-      end
-
-      it 'renders returns_policy' do
-        get :show, params: { id: 'returns_policy' }
-        expect(assigns(:policy)).to eq(store.customer_returns_policy)
-        expect(response).to render_template(:show)
-        expect(response).to have_http_status(:ok)
-      end
-
-      it 'renders shipping_policy' do
-        get :show, params: { id: 'shipping_policy' }
-        expect(assigns(:policy)).to eq(store.customer_shipping_policy)
-        expect(response).to render_template(:show)
-        expect(response).to have_http_status(:ok)
-      end
-    end
-
-    context 'with invalid policy type' do
-      it 'raises RecordNotFound error' do
-        expect {
-          get :show, params: { id: 'invalid_policy' }
-        }.to raise_error(ActiveRecord::RecordNotFound)
-      end
+    it 'renders the policy' do
+      get :show, params: { id: policy.slug }
+      expect(response).to be_successful
+      expect(response.body).to include(policy.body.to_plain_text)
     end
   end
 end
