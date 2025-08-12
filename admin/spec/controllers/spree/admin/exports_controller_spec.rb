92f6213beed73c79513a80f422318464aeaebc5f
Jawaria Waris <jawaria.waris@sparksolutions.co>
2025-08-12 06:16:53 +0500

    Add correct params to export controller

--- admin/spec/controllers/spree/admin/exports_controller_spec.rb
+++ admin/spec/controllers/spree/admin/exports_controller_spec.rb
@@ -8,9 +8,9 @@ describe Spree::Admin::ExportsController, type: :controller do
   let(:store) { @default_store }
 
   describe '#index' do
-    let!(:export) { create(:product_export, store: store) }
-
     subject { get :index }
+
+    let!(:export) { create(:product_export, store: store) }
 
     it 'renders the index template' do
       subject
@@ -75,16 +75,45 @@ describe Spree::Admin::ExportsController, type: :controller do
         expect(assigns(:export).search_params).to be_nil
       end
     end
+
+    context 'when exporting Orders with date filters' do
+      let(:params) do
+        {
+          export: {
+            type: 'Spree::Exports::Orders',
+            format: 'csv',
+            search_params: {
+              completed_at_gt: "#{Date.current} 00:00:00 GMT-0700 (Pacific Standard Time)",
+              completed_at_lt: "#{Date.current} 00:00:00 GMT-0700 (Pacific Standard Time)"
+            }.to_json,
+            record_selection: 'filtered'
+          }
+        }
+      end
+
+      it 'processes date filters for Orders exports' do
+        expect { subject }.to change(Spree::Exports::Orders, :count).by(1)
+
+        export = Spree::Exports::Orders.last
+
+        # Parse the processed search params
+        processed_params = JSON.parse(export.search_params)
+
+        # Verify that end_of_day was applied to completed_at_lt
+        expect(processed_params['completed_at_lt']).to include('23:59:59')
+        expect(processed_params['completed_at_gt']).to include('00:00:00')
+      end
+    end
   end
 
   describe '#show' do
+    subject { get :show, params: { id: export.id } }
+
     let(:export) { create(:product_export, store: store) }
 
     before do
       allow_any_instance_of(Spree::Exports::Products).to receive_message_chain(:attachment, :url).and_return('http://example.com/test.csv')
     end
-
-    subject { get :show, params: { id: export.id } }
 
     it 'downloads the export' do
       subject
