a48dd0b642424033833383a0bee2dd8d558219d7
Jawaria Waris <jawaria.waris@sparksolutions.co>
2025-08-18 17:39:43 +0500

    Remove exports controller specs

--- admin/spec/controllers/spree/admin/exports_controller_spec.rb
+++ admin/spec/controllers/spree/admin/exports_controller_spec.rb
@@ -75,35 +75,6 @@ describe Spree::Admin::ExportsController, type: :controller do
         expect(assigns(:export).search_params).to be_nil
       end
     end
-
-    context 'when exporting Orders with date filters' do
-      let(:params) do
-        {
-          export: {
-            type: 'Spree::Exports::Orders',
-            format: 'csv',
-            search_params: {
-              completed_at_gt: "#{Date.current} 00:00:00 GMT-0700 (Pacific Standard Time)",
-              completed_at_lt: "#{Date.current} 00:00:00 GMT-0700 (Pacific Standard Time)"
-            }.to_json,
-            record_selection: 'filtered'
-          }
-        }
-      end
-
-      it 'processes date filters for Orders exports' do
-        expect { subject }.to change(Spree::Exports::Orders, :count).by(1)
-
-        export = Spree::Exports::Orders.last
-
-        # Parse the processed search params
-        processed_params = JSON.parse(export.search_params)
-
-        # Verify that end_of_day was applied to completed_at_lt
-        expect(processed_params['completed_at_lt']).to include('23:59:59')
-        expect(processed_params['completed_at_gt']).to include('00:00:00')
-      end
-    end
   end
 
   describe '#show' do
