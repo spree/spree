dfc93b9c249befeffca82a49a07e61b3b2eeea61
Damian Legawiec <damian@sparksolutions.co>
2025-08-26 17:57:54 +0200

    Update core/db/migrate/20250825175217_add_missing_page_builder_indexes.rb

--- core/db/migrate/20250825175217_add_missing_page_builder_indexes.rb
+++ core/db/migrate/20250825175217_add_missing_page_builder_indexes.rb
@@ -1,4 +1,4 @@
-class AddMissingPageBuilderIndexes < ActiveRecord::Migration[8.0]
+class AddMissingPageBuilderIndexes < ActiveRecord::Migration[7.2]
   def change
     add_index :spree_themes, %w[store_id default], name: 'index_spree_themes_on_store_id_and_default', if_not_exists: true
     add_index :spree_page_links, %w[position parent_type parent_id], name: 'index_spree_page_links_parent_with_position', if_not_exists: true
