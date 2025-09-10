e5d9615588e63cb1eb72b9e23a0a4d6d4abc2c40
Damian Legawiec <damian@sparksolutions.co>
2025-09-10 14:49:22 +0200

    Update core/db/migrate/20250826093602_create_spree_newsletter_subscribers.rb

--- core/db/migrate/20250826093602_create_spree_newsletter_subscribers.rb
+++ core/db/migrate/20250826093602_create_spree_newsletter_subscribers.rb
@@ -2,7 +2,7 @@ class CreateSpreeNewsletterSubscribers < ActiveRecord::Migration[7.2]
   def change
     create_table :spree_newsletter_subscribers do |t|
       t.string :email, index: { unique: true }, null: false
-      t.bigint :user_id, index: true
+      t.references :user, index: true
       t.datetime :verified_at, index: true
       t.string :verification_token, index: { unique: true }
 
