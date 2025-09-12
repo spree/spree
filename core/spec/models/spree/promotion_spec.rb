c5f0ce7f6a13fb0fd858a7f86f4ec38db405c637
Damian Legawiec <damian@sparksolutions.co>
2025-09-12 14:17:31 +0200

    Remove promotion touching when applying promo on checkout (#13095)
    * Only touch coupon code promotions during apply
    
    * removing touching altogether
    
    * Update adjustment_spec.rb

--- core/spec/models/spree/promotion_spec.rb
+++ core/spec/models/spree/promotion_spec.rb
@@ -289,12 +289,6 @@ describe Spree::Promotion, type: :model do
         expect(promotion.activate(payload)).to be true
         expect(promotion.orders.first).to eql order
       end
-
-      it 'touches the promotion' do
-        previous_updated_at = promotion.updated_at
-        expect(promotion.activate(payload)).to be true
-        expect(promotion.reload.updated_at).not_to eq(previous_updated_at)
-      end
     end
 
     context 'when not activated' do
@@ -304,12 +298,6 @@ describe Spree::Promotion, type: :model do
         expect(promotion.orders).to be_empty
         expect(promotion.activate(payload)).to be_falsey
         expect(promotion.orders).to be_empty
-      end
-
-      it "doesn't the promotion" do
-        previous_updated_at = promotion.updated_at
-        expect(promotion.activate(payload)).to be_falsey
-        expect(promotion.reload.updated_at).to eq(previous_updated_at)
       end
     end
   end
