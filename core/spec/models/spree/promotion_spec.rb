e1c7baf74ab18ebbe3a8a90820b42ad95c3d1a0b
Damian Legawiec <damian@sparksolutions.co>
2025-09-12 14:56:11 +0200

    Added `Promotion#cached_rules` to cache promotion rules (#13096)
    Added `Promotion#cached_roles` to cache promotion rules
    
    this is super handy when applying a promo to a large number of line items

--- core/spec/models/spree/promotion_spec.rb
+++ core/spec/models/spree/promotion_spec.rb
@@ -857,4 +857,10 @@ describe Spree::Promotion, type: :model do
       end
     end
   end
+
+  describe '#cached_rules' do
+    it 'returns the rules' do
+      expect(promotion.cached_rules).to eq(promotion.rules)
+    end
+  end
 end
