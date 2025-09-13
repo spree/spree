5ee0c3b80928cb34dba00835833a374546610966
Damian Legawiec <damian@getvendo.com>
2025-09-13 15:25:47 +0200

    Update 20250913130044_add_page_links_counter_cache_to_spree_stores.rb

--- core/db/migrate/20250913130044_add_page_links_counter_cache_to_spree_stores.rb
+++ core/db/migrate/20250913130044_add_page_links_counter_cache_to_spree_stores.rb
@@ -1,7 +1,8 @@
 class AddPageLinksCounterCacheToSpreeStores < ActiveRecord::Migration[8.0]
   def change
-    add_column :spree_stores, :page_links_count, :integer, default: 0
+    add_column :spree_stores, :page_links_count, :integer, default: 0, null: false
 
+    Spree::Store.reset_column_information
     Spree::Store.find_each do |store|
       Spree::Store.reset_counters(store.id, :page_links)
     end
