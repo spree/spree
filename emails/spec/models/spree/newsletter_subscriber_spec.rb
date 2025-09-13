43842143fa9dc99a9ab54d294b0c89e102994673
Damian Legawiec <damian@sparksolutions.co>
2025-09-13 20:19:15 +0200

    Respect `Store#send_consumer_transactional_emails` setting in newslet… (#13105)

--- emails/spec/models/spree/newsletter_subscriber_spec.rb
+++ emails/spec/models/spree/newsletter_subscriber_spec.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 describe Spree::NewsletterSubscriber, type: :model do
+  let(:store) { Spree::Store.default }
   let(:subscriber) { create(:newsletter_subscriber) }
 
   describe '#deliver_newsletter_email_verification' do
@@ -9,6 +10,18 @@ describe Spree::NewsletterSubscriber, type: :model do
       expect(Spree::NewsletterMailer).to receive(:email_confirmation).with(subscriber).and_return(double(deliver_later: true))
 
       deliver_newsletter_email_verification
+    end
+
+    context 'when send_consumer_transactional_emails store setting is disabled' do
+      before do
+        allow(Spree::Current).to receive(:store).and_return(store)
+        allow(store).to receive(:prefers_send_consumer_transactional_emails?).and_return(false)
+      end
+
+      it 'does not call newsletter mailer' do
+        expect(Spree::NewsletterMailer).not_to receive(:email_confirmation)
+        deliver_newsletter_email_verification
+      end
     end
   end
 end
