ea9915e686bdf72e898f9a61679773363eb65091
Damian Legawiec <damian@sparksolutions.co>
2025-09-13 09:28:44 +0200

    Update confirmation email URL to use current store and custom domain if present

--- emails/app/mailers/spree/newsletter_mailer.rb
+++ emails/app/mailers/spree/newsletter_mailer.rb
@@ -2,7 +2,7 @@ module Spree
   class NewsletterMailer < BaseMailer
     def email_confirmation(subscriber)
       @subscriber = subscriber
-      @confirm_email_url = spree.verify_newsletter_subscribers_url(token: @subscriber.verification_token, host: Spree::Store.default.url)
+      @confirm_email_url = spree.verify_newsletter_subscribers_url(token: @subscriber.verification_token, host: Spree::Current.store.url_or_custom_domain)
       mail(to: @subscriber.email, from: from_address, subject: Spree.t('newsletter_mailer.email_confirmation.subject'))
     end
   end
