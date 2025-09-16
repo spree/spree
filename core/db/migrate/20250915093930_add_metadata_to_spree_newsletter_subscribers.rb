8b5a85b8e06ad2ee4c42216cfe8e1cd6044c9830
Szymon Chorobski <szymon.chorobski@getvendo.com>
2025-09-16 10:40:01 +0100

    migration fix

--- core/db/migrate/20250915093930_add_metadata_to_spree_newsletter_subscribers.rb
+++ core/db/migrate/20250915093930_add_metadata_to_spree_newsletter_subscribers.rb
@@ -1,10 +1,12 @@
-class AddPublicMetadataToSpreeNewsletterSubscribers < ActiveRecord::Migration[7.2]
+class AddMetadataToSpreeNewsletterSubscribers < ActiveRecord::Migration[7.2]
   def up
     change_table :spree_newsletter_subscribers do |t|
       if t.respond_to? :jsonb
-        t.jsonb :public_metadata, default: {}
+        t.jsonb :public_metadata
+        t.jsonb :private_metadata
       else
-        t.json :public_metadata, default: {}
+        t.json :public_metadata
+        t.json :private_metadata
       end
     end
   end
@@ -12,6 +14,7 @@ class AddPublicMetadataToSpreeNewsletterSubscribers < ActiveRecord::Migration[7.
   def down
     change_table :spree_newsletter_subscribers do |t|
       t.remove :public_metadata
+      t.remove :private_metadata
     end
   end
 end
