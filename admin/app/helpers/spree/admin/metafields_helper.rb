cdc51b9d232dc7f232bbdbe3b222107cb11bc00c
Damian Legawiec <damian@getvendo.com>
2025-09-23 00:04:28 +0200

    fix

--- admin/app/helpers/spree/admin/metafields_helper.rb
+++ admin/app/helpers/spree/admin/metafields_helper.rb
@@ -1,17 +1,6 @@
 module Spree
   module Admin
     module MetafieldsHelper
-      def sorted_metafields(resource, display_on: nil)
-        metafield_definitions = Spree::MetafieldDefinition.for_resource_type(resource.class.to_s)
-        metafield_definitions = metafield_definitions.where(display_on: display_on) if display_on.present?
-        metafield_definitions = metafield_definitions.order(:name)
-
-        metafield_definitions.map do |definition|
-          existing_metafield = resource.metafields.find { |mf| mf.metafield_definition_id == definition.id }
-          existing_metafield || resource.metafields.build(metafield_definition: definition)
-        end
-      end
-
       def metafield_definition_resource_types
         Rails.application.config.spree.metafield_enabled_resources.map(&:to_s)
       end
