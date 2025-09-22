8aa338d6facef8364df735de8a34e205e0d8be97
Damian Legawiec <damian@getvendo.com>
2025-09-22 16:24:45 +0200

    wip

--- admin/app/controllers/spree/admin/metafield_definitions_controller.rb
+++ admin/app/controllers/spree/admin/metafield_definitions_controller.rb
@@ -3,26 +3,6 @@ module Spree
     class MetafieldDefinitionsController < ResourceController
       private
 
-      def model_class
-        Spree::MetafieldDefinition
-      end
-
-      def collection
-        return @collection if @collection.present?
-
-        params[:q] ||= {}
-        params[:q][:s] ||= 'name asc'
-
-        @search = super.ransack(params[:q])
-        @collection = @search.result(distinct: true)
-
-        @collection
-      end
-
-      def permitted_resource_params
-        params.require(:metafield_definition).permit(Spree::PermittedAttributes.metafield_definition_attributes)
-      end
-
       def location_after_save
         collection_url
       end
