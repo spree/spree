61149c4fb12ac5b3b5d23d5964ca8a68ccfbea3f
Damian Legawiec <damian@getvendo.com>
2025-09-26 10:14:41 +0200

    Update rich_text.rb

--- core/app/models/spree/metafields/rich_text.rb
+++ core/app/models/spree/metafields/rich_text.rb
@@ -1,10 +1,13 @@
 module Spree
   module Metafields
     class RichText < Spree::Metafield
+      # Avoid collision with the `value` column used by other metafield types
+      self.ignored_columns = %w[value]
+
       has_rich_text :value
 
       def serialize_value
-        value.body.to_s
+        value&.body&.to_s
       end
     end
   end
