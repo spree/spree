d38f3409d236ae4800bf13ae818151688e97331d
Damian Legawiec <damian@getvendo.com>
2025-10-02 13:53:33 +0200

    fixes

--- admin/app/javascript/spree/admin/controllers/sortable_auto_submit_controller.js
+++ admin/app/javascript/spree/admin/controllers/sortable_auto_submit_controller.js
@@ -2,6 +2,9 @@ import Sortable from "stimulus-sortable"
 
 export default class extends Sortable {
   async onUpdate({ item, newIndex }) {
-    item.closest('form').requestSubmit()
+    const form = item.closest('form')
+    if (form) {
+      form.requestSubmit()
+    }
   }
 }
