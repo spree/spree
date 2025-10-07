a2f31d2a4ea349323b685931a6a88a1dbbe73b6a
Damian Legawiec <damian@getvendo.com>
2025-10-07 14:15:09 +0200

    Fixes #13168 undefined currency_options_for_select
    This regression was introduced afrer removing `currency_select` gem. Replaced with Spree native methods. Also deprecated some old currency methods and unified currency dropdowns across the application

--- core/app/helpers/spree/currency_helper.rb
+++ core/app/helpers/spree/currency_helper.rb
@@ -1,18 +1,23 @@
 module Spree
   module CurrencyHelper
+    # Returns the list of all currencies as options for a select field.
+    # By default the value is the default currency of the default store.
+    # @param selected_value [String] the selected value
+    # @return [String] the options for a select field
     def currency_options(selected_value = nil)
       selected_value ||= Spree::Store.default.default_currency
       currencies = ::Money::Currency.table.map do |_code, details|
-        iso = details[:iso_code]
-        [iso, "#{details[:name]} (#{iso})"]
+        currency_presentation(details[:iso_code])
       end
-      options_from_collection_for_select(currencies, :first, :last, selected_value)
+      options_from_collection_for_select(currencies, :last, :first, selected_value)
     end
 
+    # Returns the list of supported currencies for the current store as options for a select field.
+    # @return [String] the options for a select field
     def supported_currency_options
       return if current_store.nil?
 
-      current_store.supported_currencies_list.map(&:iso_code).map { |currency| currency_presentation(currency) }
+      @supported_currency_options ||= current_store.supported_currencies_list.map(&:iso_code).map { |currency| currency_presentation(currency) }
     end
 
     def should_render_currency_dropdown?
@@ -21,22 +26,32 @@ module Spree
       current_store.supported_currencies_list.size > 1
     end
 
+    # Returns the currency symbol for the given currency.
+    # @param currency [String] the currency ISO code
+    # @return [String] the currency symbol
     def currency_symbol(currency)
       ::Money::Currency.find(currency).symbol
     end
 
+    # @param currency [String] the currency ISO code
+    # @return [Array] the currency presentation and ISO code
     def currency_presentation(currency)
-      label = [currency_symbol(currency), currency].compact.join(' ')
+      currency_money = currency_money(currency)
+      label = "#{currency_money.name} (#{currency_money.iso_code})"
 
       [label, currency]
     end
 
+    # Returns the list of supported currencies for the current store.
+    # @return [Array<Money::Currency>] the list of supported currencies
     def preferred_currencies
+      Spree::Deprecation.warn('preferred_currencies is deprecated and will be removed in Spree 6.0. Use current_store.supported_currencies_list instead.')
       @preferred_currencies ||= current_store.supported_currencies_list
     end
 
     def preferred_currencies_select_options
-      preferred_currencies.map { |currency| ["#{currency.name} - #{currency.iso_code}", currency] }
+      Spree::Deprecation.warn('preferred_currencies_select_options is deprecated and will be removed in Spree 6.0. Use supported_currency_options instead.')
+      preferred_currencies.map { |currency| currency_presentation(currency) }
     end
 
     def currency_money(currency = current_currency)
