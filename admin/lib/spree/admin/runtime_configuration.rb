f0bc9527807058fed5fa45cbb1cd9e4a44d41ea9
Damian Legawiec <damian@getvendo.com>
2025-10-14 11:50:11 +0200

    Add ability to include custom JS/stimulus controllers in Admin

--- admin/lib/spree/admin/runtime_configuration.rb
+++ admin/lib/spree/admin/runtime_configuration.rb
@@ -10,6 +10,8 @@ module Spree
       preference :admin_records_per_page, :integer, default: Kaminari.config.default_per_page
       preference :admin_products_per_page, :integer, default: Kaminari.config.default_per_page
       preference :admin_orders_per_page, :integer, default: Kaminari.config.default_per_page
+
+      preference :include_application_importmap, :boolean, default: false
     end
   end
 end
