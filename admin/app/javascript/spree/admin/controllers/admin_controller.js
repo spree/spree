54affa8d99d83d22840c0c6eff58d4ebc255f230
Damian Legawiec <damian@getvendo.com>
2025-10-20 15:58:10 +0200

    fixed back to dashboard ESC hijacking

--- admin/app/javascript/spree/admin/controllers/admin_controller.js
+++ admin/app/javascript/spree/admin/controllers/admin_controller.js
@@ -3,15 +3,6 @@ import { Controller } from '@hotwired/stimulus'
 export default class extends Controller {
   static targets = ['close', 'save']
 
-  close(event) {
-    // https://github.com/hotwired/stimulus/issues/743
-    if (event.type == "keydown" && !(event instanceof KeyboardEvent)) return
-
-    if (this.hasCloseTarget) {
-      window.Turbo.visit(this.closeTarget.href)
-    }
-  }
-
   save(event) {
     // https://github.com/hotwired/stimulus/issues/743
     if (event.type == "keydown" && !(event instanceof KeyboardEvent)) return
