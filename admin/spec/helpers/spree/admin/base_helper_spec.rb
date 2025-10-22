37cf06082cd6a22d5653d35e8ed7101a148d4593
Jawaria Waris <jawaria.waris@sparksolutions.co>
2025-10-22 13:04:47 +0500

    Fix admin time helpers and tooltip (#13207)
    * Fix admin time helpers and tooltip
    
    * Update time to not strip tags

--- admin/spec/helpers/spree/admin/base_helper_spec.rb
+++ admin/spec/helpers/spree/admin/base_helper_spec.rb
@@ -20,4 +20,17 @@ describe Spree::Admin::BaseHelper do
       end
     end
   end
+
+  describe '#spree_time_ago' do
+    it 'returns the local time ago with a tooltip' do
+      time = Time.zone.parse('2025-10-21 12:00')
+      html = helper.spree_time_ago(time)
+      expect(html).to include('<time datetime="2025-10-21T12:00')
+      expect(html).to include('tooltip-container')
+    end
+
+    it 'returns empty string for blank time' do
+      expect(helper.spree_time_ago(nil)).to eq('')
+    end
+  end
 end
