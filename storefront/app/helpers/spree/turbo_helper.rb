3d9793b2a11011cf0c74e402c55961ae29b66111
Jawaria Waris <jawaria.waris@sparksolutions.co>
2025-10-30 03:24:50 +0500

    Update cart total in turbo streams (#13227)
    * Update cart total in turbo streams
    
    * Update cart item count in turbo streams

--- storefront/app/helpers/spree/turbo_helper.rb
+++ storefront/app/helpers/spree/turbo_helper.rb
@@ -7,7 +7,8 @@ module Spree
     end
 
     def spree_turbo_update_cart(order = current_order)
-      turbo_stream.update_all '.cart-counter', order.item_count.positive? ? order.item_count : ''
+      turbo_stream.update_all '.cart-counter', order&.item_count&.positive? ? order&.item_count : ''
+      turbo_stream.update_all '.cart-total', order&.display_item_total.to_s
     end
   end
 end
