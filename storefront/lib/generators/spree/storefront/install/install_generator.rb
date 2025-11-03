44d5ca2b17937e510c840d4ad43352064298f4c4
Damian Legawiec <damian@sparksolutions.co>
2025-11-03 16:36:53 +0100

    Allow Tailwind v4 for Storefront! (#13240)
    * Allow Tailwind v4 for Storefront!
    
    * Update install_generator.rb

--- storefront/lib/generators/spree/storefront/install/install_generator.rb
+++ storefront/lib/generators/spree/storefront/install/install_generator.rb
@@ -15,7 +15,8 @@ module Spree
         end
 
         def install
-          template 'application.tailwind.css', 'app/assets/stylesheets/application.tailwind.css'
+          empty_directory Rails.root.join('app/assets/tailwind') if Rails.root && !Rails.root.join('app/assets/tailwind').exist?
+          template 'application.css', 'app/assets/tailwind/application.css'
           template 'tailwind.config.js', 'config/tailwind.config.js'
 
           if Rails.root && Rails.root.join("Procfile.dev").exist?
