0747fc4c20fd85d554a3c993540ba0f983b1f9e5
Damian Legawiec <damian@getvendo.com>
2025-11-05 17:00:42 +0100

    Fixed: `TurboHelper#spree_turbo_update_cart` updating cart total, but not cart counter

--- storefront/app/helpers/spree/turbo_helper.rb
+++ storefront/app/helpers/spree/turbo_helper.rb
@@ -7,8 +7,10 @@ module Spree
     end
 
     def spree_turbo_update_cart(order = current_order)
-      turbo_stream.update_all '.cart-counter', order&.item_count&.positive? ? order&.item_count : ''
-      turbo_stream.update_all '.cart-total', order&.display_item_total.to_s
+      [
+        turbo_stream.update_all('.cart-counter', order&.item_count&.positive? ? order&.item_count : ''),
+        turbo_stream.update_all('.cart-total', order&.display_item_total.to_s)
+      ].join.html_safe
     end
   end
 end
