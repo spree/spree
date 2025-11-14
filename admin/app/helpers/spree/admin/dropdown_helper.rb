780af6ce21d56fb8631a58e9d36631b8c67acfcb
Damian Legawiec <damian@getvendo.com>
2025-11-14 16:34:24 +0100

    Admin: fixed dropdowns overlapping issues

--- admin/app/helpers/spree/admin/dropdown_helper.rb
+++ admin/app/helpers/spree/admin/dropdown_helper.rb
@@ -3,7 +3,23 @@ module Spree
     module DropdownHelper
       def dropdown(options = {}, &block)
         options[:class] = ['dropdown'] + Array(options[:class])
-        options[:data] = { controller: 'dropdown' }.merge(options[:data] || {})
+
+        # Extract direction option for backward compatibility and convert to Floating UI placement
+        placement = case options.delete(:direction)
+        when 'left'
+          'bottom-end'
+        when 'top'
+          'top-start'
+        when 'top-left'
+          'top-end'
+        else
+          options[:placement] || 'bottom-start'
+        end
+
+        options[:data] = {
+          controller: 'dropdown',
+          dropdown_placement_value: placement
+        }.merge(options[:data] || {})
         content_tag(:div, options, &block)
       end
 
@@ -19,22 +35,8 @@ module Spree
 
       def dropdown_menu(options = {}, &block)
         options[:class] = ['dropdown-container hidden'] + Array(options[:class])
-
-        # Extract direction option for backward compatibility and convert to Floating UI placement
-        placement = case options.delete(:direction)
-        when 'left'
-          'bottom-end'
-        when 'top'
-          'top-start'
-        when 'top-left'
-          'top-end'
-        else
-          options[:placement] || 'bottom-start'
-        end
-
         options[:data] = {
-          dropdown_target: 'menu',
-          dropdown_placement_value: placement
+          dropdown_target: 'menu'
         }.merge(options[:data] || {})
 
         content_tag(:div, options, &block)
