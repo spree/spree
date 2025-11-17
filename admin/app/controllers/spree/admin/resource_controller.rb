d922c209f1ad7ff102b0df09e72556e2127c4824
Damian Legawiec <damian@getvendo.com>
2025-11-17 11:23:20 +0100

    Update resource_controller.rb

--- admin/app/controllers/spree/admin/resource_controller.rb
+++ admin/app/controllers/spree/admin/resource_controller.rb
@@ -1,7 +1,7 @@
 class Spree::Admin::ResourceController < Spree::Admin::BaseController
   include Spree::Admin::Callbacks
 
-  helper_method :new_object_url, :edit_object_url, :object_url, :collection_url, :model_class, :search, :collection
+  helper_method :new_object_url, :edit_object_url, :object_url, :collection_url, :model_class
   before_action :load_resource
   before_action :set_currency, :set_current_store, only: [:new, :create]
 
