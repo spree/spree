a588009249cc1f163103671bbd7e82ad40025f8b
Damian Legawiec <damian@sparksolutions.co>
2025-11-18 13:05:37 +0100

    Fix 403s when trying to download external images (#13282)

--- core/app/jobs/spree/images/save_from_url_job.rb
+++ core/app/jobs/spree/images/save_from_url_job.rb
@@ -1,10 +1,11 @@
 require 'open-uri'
+require 'openssl'
 
 module Spree
   module Images
     class SaveFromUrlJob < ::Spree::BaseJob
       queue_as Spree.queues.images
-      retry_on ActiveRecord::RecordInvalid, OpenURI::HTTPError, wait: :polynomially_longer, attempts: 2
+      retry_on ActiveRecord::RecordInvalid, OpenURI::HTTPError, wait: :polynomially_longer, attempts: Spree::Config.images_save_from_url_job_attempts.to_i
       discard_on URI::InvalidURIError
 
       def perform(viewable_id, viewable_type, external_url, external_id = nil, position = nil)
@@ -33,7 +34,17 @@ module Spree
           raise URI::InvalidURIError, "Invalid URL scheme: #{uri.scheme}. Only http and https are allowed."
         end
 
-        file = uri.open
+        file = uri.open(
+          'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+          'Accept' => 'image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8',
+          'Accept-Language' => 'en-US,en;q=0.9',
+          'Accept-Encoding' => 'gzip, deflate, br',
+          'Cache-Control' => 'no-cache',
+          'Pragma' => 'no-cache',
+          read_timeout: 60,
+          ssl_verify_mode: OpenSSL::SSL::VERIFY_PEER,
+          redirect: true
+        )
         filename = File.basename(uri.path)
 
         image.attachment.attach(io: file, filename: filename)
