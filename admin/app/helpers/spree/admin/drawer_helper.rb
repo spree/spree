86965889d25ced39426324d864f5819cbad243d8
Damian Legawiec <damian@getvendo.com>
2025-11-25 13:12:07 +0100

    Update drawer_helper.rb

--- admin/app/helpers/spree/admin/drawer_helper.rb
+++ admin/app/helpers/spree/admin/drawer_helper.rb
@@ -1,18 +1,18 @@
 module Spree
   module Admin
     module DrawerHelper
-      def drawer_header(title, action = 'drawer#close')
+      def drawer_header(title, controller_name = 'drawer')
         content_tag(:div, class: 'drawer-header') do
-          content_tag(:h5, title, class: 'drawer-title') + drawer_close_button(action)
+          content_tag(:h5, title, class: 'drawer-title') + drawer_close_button(controller_name)
         end.html_safe
       end
 
-      def drawer_close_button(action = 'drawer#close')
-        button_tag('', type: 'button', class: 'btn-close', data: { action: action, dismiss: 'drawer', aria_label: Spree.t(:close) }).html_safe
+      def drawer_close_button(controller_name = 'drawer')
+        button_tag('', type: 'button', class: 'btn-close', data: { action: "#{controller_name}#close", dismiss: controller_name, aria_label: Spree.t(:close) }).html_safe
       end
 
-      def drawer_discard_button(action = 'drawer#close')
-        button_tag(type: 'button', class: 'btn btn-light', data: { action: action, dismiss: 'drawer' }) do
+      def drawer_discard_button(controller_name = 'drawer')
+        button_tag(type: 'button', class: 'btn btn-light', data: { action: "#{controller_name}#close", dismiss: controller_name }) do
           Spree.t('actions.discard')
         end.html_safe
       end
