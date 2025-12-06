6f656e37002925580470be272c311102cce56c30
Damian Legawiec <damian@getvendo.com>
2025-12-06 12:13:20 +0100

    replaced parts of bootstrap utility classes with tailwind counter parts

--- admin/app/javascript/spree/admin/controllers/sticky_controller.js
+++ admin/app/javascript/spree/admin/controllers/sticky_controller.js
@@ -16,9 +16,13 @@ export default class extends Controller {
 
   handleScroll() {
     if (window.scrollY > this.thresholdValue) {
-      this.element.classList.add('sticky-top')
+      this.element.classList.add('sticky')
+      this.element.classList.add('top-0')
+      this.element.classList.add('z-[1070]')
     } else {
-      this.element.classList.remove('sticky-top')
+      this.element.classList.remove('sticky')
+      this.element.classList.remove('top-0')
+      this.element.classList.remove('z-[1070]')
     }
   }
 }
