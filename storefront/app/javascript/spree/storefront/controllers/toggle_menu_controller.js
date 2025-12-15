3343133a2f92a61e0a1b18146af983574a0ef460
Filip Cichorek <cichorek.filip@gmail.com>
2025-12-15 17:48:53 +0100

    fix: toogle menu height

--- storefront/app/javascript/spree/storefront/controllers/toggle_menu_controller.js
+++ storefront/app/javascript/spree/storefront/controllers/toggle_menu_controller.js
@@ -7,6 +7,12 @@ export default class ToggleMenu extends Toggle {
 
   connect() {
     super.connect()
+    this.setupDynamicHeight()
+  }
+
+  disconnect() {
+    super.disconnect()
+    this.cleanupObservers()
   }
 
   hide(e) {
@@ -17,6 +23,7 @@ export default class ToggleMenu extends Toggle {
   }
 
   toggle(e) {
+    this.updateDynamicHeight()
     super.toggle(e)
     if (this.openValue) {
       this.buttonTarget.classList.add('menu-open')
@@ -28,4 +35,55 @@ export default class ToggleMenu extends Toggle {
       unlockScroll()
     }
   }
+
+  setupDynamicHeight() {
+    this.updateDynamicHeight()
+    this.setupObservers()
+  }
+
+  updateDynamicHeight() {
+    if (!this.hasContentTarget) return
+    
+    const navElement = this.element.querySelector('nav[aria-label="Top"]')
+    const navPaddingBottom = navElement ? 
+      parseInt(getComputedStyle(navElement).paddingBottom, 10) || 0 : 0
+    
+    const contentTopDistance = this.contentTarget.getBoundingClientRect().top
+    const calculatedHeight = `calc(100dvh - ${contentTopDistance}px)`
+    
+    this.contentTarget.style.height = calculatedHeight
+    const firstChild = this.contentTarget.firstElementChild
+    if (firstChild) {
+      firstChild.style.marginTop = `${navPaddingBottom}px`
+    }
+  }
+
+  setupObservers() {
+    const debouncedUpdate = () => {
+      clearTimeout(this.updateTimeout)
+      this.updateTimeout = setTimeout(() => this.updateDynamicHeight(), 16)
+    }
+
+    if (typeof ResizeObserver !== 'undefined') {
+      this.resizeObserver = new ResizeObserver(debouncedUpdate)
+      const navElement = this.element.querySelector('nav[aria-label="Top"]')
+      if (navElement) {
+        this.resizeObserver.observe(navElement)
+      }
+    }
+
+    this.windowResizeHandler = debouncedUpdate
+    window.addEventListener('resize', this.windowResizeHandler)
+  }
+
+  cleanupObservers() {
+    if (this.resizeObserver) {
+      this.resizeObserver.disconnect()
+    }
+    if (this.windowResizeHandler) {
+      window.removeEventListener('resize', this.windowResizeHandler)
+    }
+    clearTimeout(this.updateTimeout)
+  }
+
 }
