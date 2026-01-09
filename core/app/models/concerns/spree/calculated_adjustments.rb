a166c3df9f085ae774c4d331fc0c03e9c8f24f8b
Damian Legawiec <damian@sparksolutions.co>
2026-01-09 10:07:36 +0100

    Revert "Added `CalculatedAdjustments#cached_calculator`" (#13426)
    Revert "Added `CalculatedAdjustments#cached_calculator` (#13400)"
    
    This reverts commit 952d76c79f11fa91842110b83ac04b634e7ede37.

--- core/app/models/concerns/spree/calculated_adjustments.rb
+++ core/app/models/concerns/spree/calculated_adjustments.rb
@@ -6,7 +6,7 @@ module Spree
       has_one :calculator, class_name: 'Spree::Calculator', as: :calculable, inverse_of: :calculable, dependent: :destroy, autosave: true
       accepts_nested_attributes_for :calculator
       validates :calculator, presence: true
-      delegate :compute, to: :cached_calculator
+      delegate :compute, to: :calculator
 
       scope :with_calculator, ->(calculator) { joins(:calculator).where(calculator: { type: calculator.to_s }) }
 
@@ -15,22 +15,12 @@ module Spree
       end
 
       def calculator_type
-        cached_calculator.class.to_s if cached_calculator
+        calculator.class.to_s if calculator
       end
 
       def calculator_type=(calculator_type)
         klass = calculator_type.constantize if calculator_type
         self.calculator = klass.new if klass && !calculator.instance_of?(klass)
-      end
-
-      # Returns the calculator using Rails.cache to avoid repeated database lookups.
-      #
-      # @return [Object, nil] The calculator object (TaxRate, PromotionAction, etc.)
-      def cached_calculator
-        Rails.cache.fetch("#{cache_key_with_version}/calculator") { calculator }
-      rescue TypeError
-        # Handle objects that can't be serialized (e.g., mock objects in tests)
-        calculator
       end
 
       private
