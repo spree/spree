b529dd99298b4528a7231d3d7129fd1d5b16cfe4
Damian Legawiec <damian@getvendo.com>
2026-01-14 12:35:56 +0100

    Use global pricing context per request to speed up Pricing

--- core/app/models/spree/current.rb
+++ core/app/models/spree/current.rb
@@ -1,6 +1,6 @@
 module Spree
   class Current < ::ActiveSupport::CurrentAttributes
-    attribute :store, :currency, :zone
+    attribute :store, :currency, :zone, :price_lists, :global_pricing_context
 
     def store
       super || Spree::Store.default
@@ -12,6 +12,18 @@ module Spree
 
     def zone
       super || Spree::Zone.default_tax || store&.checkout_zone
+    end
+
+    def price_lists
+      @price_lists ||= super || Spree::PriceList.for_context(global_pricing_context)
+    end
+
+    def global_pricing_context
+      @global_pricing_context ||= Spree::Pricing::Context.new(
+        currency: currency,
+        store: store,
+        zone: zone
+      )
     end
   end
 end
