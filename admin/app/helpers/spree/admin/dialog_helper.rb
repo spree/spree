aa56fdb63caabe3897afc1b0939fe39ddfac9e5b
Damian Legawiec <damian@getvendo.com>
2026-01-17 12:26:06 +0100

    Added `dialog` and `drawer` methods to fully implement Dialog / Drawer components
    Without the need to write HTML/ERB code

--- admin/app/helpers/spree/admin/dialog_helper.rb
+++ admin/app/helpers/spree/admin/dialog_helper.rb
@@ -1,6 +1,36 @@
 module Spree
   module Admin
     module DialogHelper
+      # Renders a dialog container element
+      # @param id [String, nil] the id of the dialog element
+      # @param controller_name [String] the Stimulus controller name (default: 'dialog')
+      # @param html_options [Hash] additional HTML attributes
+      # @yield the content to render inside the dialog
+      # @return [String] the dialog HTML
+      #
+      # @example Basic usage
+      #   <%= dialog do %>
+      #     <%= dialog_header("Confirm Delete") %>
+      #     <div class="dialog-body">Are you sure?</div>
+      #     <div class="dialog-footer">
+      #       <%= dialog_discard_button %>
+      #       <%= button_tag "Delete", class: "btn btn-danger" %>
+      #     </div>
+      #   <% end %>
+      #
+      # @example With custom id and controller
+      #   <%= dialog(id: 'my-dialog', controller_name: 'custom-dialog') do %>
+      #     ...
+      #   <% end %>
+      def dialog(id: nil, controller_name: 'dialog', **html_options, &block)
+        html_options[:class] = "dialog #{html_options[:class]}".strip
+        html_options[:data] ||= {}
+        html_options[:data]["#{controller_name}-target".to_sym] = 'dialog'
+        html_options[:id] = id if id.present?
+
+        content_tag(:dialog, html_options, &block)
+      end
+
       def dialog_header(title, controller_name = 'dialog')
         content_tag(:div, class: 'dialog-header') do
           content_tag(:h5, title, class: 'dialog-title') + dialog_close_button(controller_name)
