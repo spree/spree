9e3a026cf613007b34c81e3f5f2d62be2c225168
Damian Legawiec <damian@getvendo.com>
2026-01-22 22:22:04 +0100

    Fix extension rake: default headless spree

--- core/lib/spree/testing_support/extension_rake.rb
+++ core/lib/spree/testing_support/extension_rake.rb
@@ -4,6 +4,6 @@ desc 'Generates a dummy app for testing an extension'
 namespace :extension do
   task :test_app, [:authentication, :user_class] do |_t, args|
     Spree::DummyGeneratorHelper.inject_extension_requirements = true
-    Rake::Task['common:test_app'].execute(args.with_defaults(install_admin: true, install_storefront: true))
+    Rake::Task['common:test_app'].execute(args)
   end
 end
