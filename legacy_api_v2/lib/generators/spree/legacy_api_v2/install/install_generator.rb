9f42ce9e5ceb16d95c1d171893339de7e6fc7e5c
Damian Legawiec <damian@getvendo.com>
2026-01-27 13:10:24 +0100

    Move API v2 into `spree_legacy_api_v2` gem

---
+++ legacy_api_v2/lib/generators/spree/legacy_api_v2/install/install_generator.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+module Spree
+  module LegacyApiV2
+    module Generators
+      class InstallGenerator < Rails::Generators::Base
+        class_option :migrate, type: :boolean, default: true
+
+        def add_migrations
+          run 'bundle exec rake railties:install:migrations FROM=spree_legacy_api_v2'
+        end
+
+        def run_migrations
+          run_migrations = options[:migrate] || ['', 'y', 'Y'].include?(ask('Would you like to run the migrations now? [Y/n]'))
+          if run_migrations
+            run 'bin/rails db:migrate'
+          else
+            puts 'Skipping rails db:migrate, don\'t forget to run it!'
+          end
+        end
+      end
+    end
+  end
+end
