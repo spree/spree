94918db9b2da513247dd22594b12034ecfd5e7c6
Szymon Chorobski <szymon.chorobski@getvendo.com>
2026-02-02 18:37:06 +0100

    fix[v-3312]: fix store credit total method

--- core/app/models/spree/order/store_credit.rb
+++ core/app/models/spree/order/store_credit.rb
@@ -66,7 +66,10 @@ module Spree
       # @return [BigDecimal] The total amount of store credits applied to the order.
       def total_applied_store_credit
         if payments.loaded?
-          payments.find_all(&:store_credit?).find_all(&:valid?).sum(&:amount) || BigDecimal::ZERO
+          payments.
+            find_all(&:store_credit?).
+            select { |payment| payment.state.in?(Spree::Payment::INVALID_STATES) }.
+            sum(&:amount) || BigDecimal::ZERO
         else
           payments.store_credits.valid.sum(:amount)
         end
