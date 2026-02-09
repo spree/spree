28419524686f94302a00ea2d2bcc8b46ee4d8c98
Damian Legawiec <damian@getvendo.com>
2026-02-09 14:54:13 +0100

    Move all rails engines to `backend/engines`

--- bin/brakeman.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -eux
-
-bundle exec brakeman -p api/ --exit-on-warn --exit-on-error -o brakeman-api.html -o brakeman-api.json
-bundle exec brakeman -p core/ --exit-on-warn --exit-on-error -o brakeman-core.html -o brakeman-core.json
-bundle exec brakeman -p admin/ --exit-on-warn --exit-on-error -o brakeman-admin.html -o brakeman-admin.json
-bundle exec brakeman -p emails/ --exit-on-warn --exit-on-error -o brakeman-emails.html -o brakeman-emails.json
