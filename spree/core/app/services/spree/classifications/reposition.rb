98f16768af691ec082f55044fc09e1d44247e4d4
Damian Legawiec <damian@getvendo.com>
2026-02-22 21:22:46 +0100

    Mark Spree::Classifications::Reposition as deprecated for removal in Spree 5.5
    Add deprecation warning via Spree::Deprecation and YARD doc annotation.

--- spree/core/app/services/spree/classifications/reposition.rb
+++ spree/core/app/services/spree/classifications/reposition.rb
@@ -1,9 +1,14 @@
 module Spree
   module Classifications
+    # @deprecated This service is deprecated and will be removed in Spree 5.5.
     class Reposition
       prepend Spree::ServiceModule::Base
 
       def call(classification:, position:)
+        Spree::Deprecation.warn(
+          "#{self.class.name} is deprecated and will be removed in Spree 5.5.",
+          caller_locations(2)
+        )
         if position.is_a?(String) && !position.match(/^\d+$/)
           return failure(nil, I18n.t('errors.messages.not_a_number'))
         end
