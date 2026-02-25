228a7aa85e8f3964ac237b3cc51625ee81241a99
Damian Legawiec <damian@getvendo.com>
2026-02-25 14:38:48 +0100

    Update utils.test.ts

--- packages/create-spree-app/tests/utils.test.ts
+++ packages/create-spree-app/tests/utils.test.ts
@@ -1,4 +1,4 @@
-import { describe, it, expect, vi, beforeEach } from 'vitest'
+import { describe, it, expect, beforeEach } from 'vitest'
 import { detectPackageManager, generateSecretKeyBase, installCommand, runCommand } from '../src/utils'
 
 describe('detectPackageManager', () => {
