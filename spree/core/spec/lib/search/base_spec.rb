78a12af1218b3437cb6949dd541dfde375a45a3a
Damian Legawiec <damian@sparksolutions.co>
2026-02-26 22:36:47 +0100

    Remove dead Spree::Core::ProductFilters module (#13672)
    Remove Spree::Core::ProductFilters and Taxon#applicable_filters
    
    Dead code since Spree 3/4, replaced by Spree::Api::V3::FiltersAggregator.

--- spree/core/spec/lib/search/base_spec.rb
+++ spree/core/spec/lib/search/base_spec.rb
@@ -4,11 +4,7 @@ xdescribe Spree::Core::Search::Base do
   let(:product1) { create(:product, name: 'RoR Mug', price: 9.00) }
   let!(:product2) { create(:product, name: 'RoR Shirt', price: 11.00) }
   let(:taxon) { create(:taxon, name: 'Ruby on Rails') }
-  let(:pln_price) { create(:price, variant_id: product1.master.id, price: 5, currency: 'PLN') }
-
   before do
-    include Spree::Core::ProductFilters
-
     product1.taxons << taxon
   end
 
@@ -45,32 +41,6 @@ xdescribe Spree::Core::Search::Base do
     params[:page] = '2'
     searcher = described_class.new(ActionController::Parameters.new(params))
     expect(searcher.retrieve_products.count).to eq(1)
-  end
-
-  it 'maps search params to named scopes' do
-    params = { per_page: '', search: { 'price_range_any' => ['Under $10.00'] } }
-    searcher = described_class.new(ActionController::Parameters.new(params))
-    expect(searcher.send(:extended_base_scope).to_sql).to match(/<= 10/)
-    expect(searcher.retrieve_products.count).to eq(1)
-  end
-
-  it 'maps multiple price_range_any filters' do
-    params = { per_page: '', search: { 'price_range_any' => ['Under $10.00', '$10.00 - $15.00'] } }
-    searcher = described_class.new(ActionController::Parameters.new(params))
-    expect(searcher.send(:extended_base_scope).to_sql).to match(/<= 10/)
-    expect(searcher.send(:extended_base_scope).to_sql).to match(/between 10.0 and 15.0|BETWEEN 10 AND 15/i)
-    expect(searcher.retrieve_products.count).to eq(2)
-  end
-
-  xit 'accepts multiple currencies' do
-    pln_price
-
-    params_pln = { per_page: '', search: { 'price_range_any' => ['Under 10.00 zł', '10.00 zł - 15.00 zł'] } }
-    searcher_pln = described_class.new(ActionController::Parameters.new(params_pln))
-    searcher_pln.current_currency = 'PLN'
-    expect(searcher_pln.send(:extended_base_scope).to_sql).to match(/<= 10/)
-    expect(searcher_pln.send(:extended_base_scope).to_sql).to match(/between 10.0 and 15.0|BETWEEN 10 AND 15/i)
-    expect(searcher_pln.retrieve_products.count).to eq(1)
   end
 
   it 'uses ransack if scope not found' do
