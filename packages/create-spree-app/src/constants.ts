8ad748dd6b8fa17ba57c8d4bde5cd2f42e063fba
Damian Legawiec <damian@getvendo.com>
2026-03-04 12:25:41 +0100

    Rename nextjs-starter to just storefront

--- packages/create-spree-app/src/constants.ts
+++ packages/create-spree-app/src/constants.ts
@@ -3,7 +3,7 @@ export const SPREE_VERSION_TAG = 'latest'
 export const DEFAULT_SPREE_PORT = 3000
 export const STOREFRONT_PORT = 3001
 
-export const STOREFRONT_REPO = 'https://github.com/spree/nextjs-starter-spree.git'
+export const STOREFRONT_REPO = 'https://github.com/spree/storefront.git'
 
 export const DEFAULT_ADMIN_EMAIL = 'spree@example.com'
 export const DEFAULT_ADMIN_PASSWORD = 'spree123'
