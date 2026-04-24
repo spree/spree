cdd8b53ab9e9fe7720b38cb758fc6ec056b82e94
Damian Legawiec <damian@getvendo.com>
2026-04-24 15:59:59 +0200

    Update 20260424000001_add_unique_index_to_spree_payments_response_code.rb

--- spree/core/db/migrate/20260424000001_add_unique_index_to_spree_payments_response_code.rb
+++ spree/core/db/migrate/20260424000001_add_unique_index_to_spree_payments_response_code.rb
@@ -1,8 +1,16 @@
 class AddUniqueIndexToSpreePaymentsResponseCode < ActiveRecord::Migration[7.2]
   def change
-    add_index :spree_payments, [:order_id, :payment_method_id, :response_code],
-              unique: true,
-              where: 'response_code IS NOT NULL',
-              name: 'idx_payments_order_method_response_code'
+    if ActiveRecord::Base.connection.adapter_name == 'Mysql2'
+      # MySQL doesn't support partial indexes, but treats NULL as distinct
+      # in unique indexes so multiple payments with NULL response_code are allowed
+      add_index :spree_payments, [:order_id, :payment_method_id, :response_code],
+                unique: true,
+                name: 'idx_payments_order_method_response_code'
+    else
+      add_index :spree_payments, [:order_id, :payment_method_id, :response_code],
+                unique: true,
+                where: 'response_code IS NOT NULL',
+                name: 'idx_payments_order_method_response_code'
+    end
   end
 end
