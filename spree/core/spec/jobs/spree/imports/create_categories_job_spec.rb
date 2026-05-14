7e27d6de13454bf4f0b9018eca7c678367d7e00b
Kacper Mekarski <kacper.mekarski@gmail.com>
2026-05-14 11:21:44 +0200

    improve specs

--- spree/core/spec/jobs/spree/imports/create_categories_job_spec.rb
+++ spree/core/spec/jobs/spree/imports/create_categories_job_spec.rb
@@ -38,21 +38,21 @@ RSpec.describe Spree::Imports::CreateCategoriesJob, type: :job do
       )
     end
 
-    it 'clears existing taxons when given an empty list' do
+    it 'matches taxonomies and taxons by case insensitive name' do
       men_taxonomy = create(:taxonomy, name: 'Men', store: store)
       clothing_taxon = create(:taxon, name: 'Clothing', taxonomy: men_taxonomy, parent: men_taxonomy.root)
-      product.taxons = [clothing_taxon]
+      create(:taxon, name: 'Shirts', taxonomy: men_taxonomy, parent: clothing_taxon)
 
-      described_class.perform_now(product.id, store.id, [])
+      described_class.perform_now(product.id, store.id, ['men -> clothing -> shirts'])
 
-      expect(product.reload.taxons).to be_empty
+      expect(product.reload.taxons.map(&:pretty_name)).to contain_exactly(
+        'Men -> Clothing -> Shirts'
+      )
     end
 
     it 'skips invalid category paths' do
       described_class.perform_now(product.id, store.id, ['Men -> -> Shirts', ' -> ', '   '])
 
-      men_taxonomy = store.taxonomies.find_by(name: 'Men')
-      expect(men_taxonomy).to be_present
       expect(product.reload.taxons.map(&:pretty_name)).to contain_exactly('Men -> Shirts')
     end
 
@@ -61,8 +61,19 @@ RSpec.describe Spree::Imports::CreateCategoriesJob, type: :job do
       described_class.perform_now(product.id, store.id, ['Men -> Clothing'])
 
       expect(product.reload.taxons.map(&:pretty_name)).to contain_exactly('Men -> Clothing')
-      expect(Spree::Taxonomy.where(name: 'Men').count).to eq(1)
     end
 
+    context 'when given an empty list' do
+      it 'clears existing taxons' do
+        men_taxonomy = create(:taxonomy, name: 'Men', store: store)
+        clothing_taxon = create(:taxon, name: 'Clothing', taxonomy: men_taxonomy, parent: men_taxonomy.root)
+        product.taxons = [clothing_taxon]
+        product.save!
+
+        described_class.perform_now(product.id, store.id, [])
+
+        expect(product.reload.taxons).to be_empty
+      end
+    end
   end
 end
