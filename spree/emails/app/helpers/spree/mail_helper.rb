f8935b509de58011d27b4c2224052902ea100c42
Kacper Mekarski <kacper.mekarski@gmail.com>
2026-07-03 12:02:20 +0200

    Fix displaying product images in order emails

--- spree/emails/app/helpers/spree/mail_helper.rb
+++ spree/emails/app/helpers/spree/mail_helper.rb
@@ -4,7 +4,9 @@ module Spree
     include Spree::ImagesHelper
 
     def variant_image_url(variant)
-      image = variant.primary_media
+      Spree::Deprecation.warn(
+        'Spree::MailHelper#variant_image_url is deprecated. Use spree_image_url instead. Will be removed in Spree 6.0.'
+      )
       image.present? && image.attached? ? spree_image_url(image, variant: :mini) : image_url('noimage/small.png')
     end
 
